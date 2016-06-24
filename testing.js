49a9585a1f204fe85e2546a548a79189b0a5f464
GitHub <noreply@github.com>
2016-06-24 19:53:10 +0000

    1143b03 fix(core/testing): move ComponentFixture to core (#9386)

--- testing.js
+++ testing.js
@@ -10,6 +10,7 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 __export(require('./testing/async'));
+__export(require('./testing/component_fixture'));
 __export(require('./testing/fake_async'));
 __export(require('./testing/test_injector'));
 __export(require('./testing/testing'));
