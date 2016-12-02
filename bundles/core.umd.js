e0245c68c3eb143f56eb696019da7278f7584d40
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 22:46:41 +0000

    3ff6554 fix(language-service): update to use `CompilerHost` from compiler-cli (#13189)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-dfd8140
+ * @license Angular v2.3.0-rc.0-3ff6554
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12949,7 +12949,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-dfd8140');
+    var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-3ff6554');
 
     exports.VERSION = VERSION;
     exports.createPlatform = createPlatform;
