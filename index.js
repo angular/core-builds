67a4c2604a8b18c2e76739bb862af3b0c3394e6f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 22:44:39 +0000

    dfd8140 Fix format

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ import { Version } from './src/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-75d1617');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-dfd8140');
 //# sourceMappingURL=index.js.map
