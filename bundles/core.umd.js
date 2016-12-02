b0e6bae374fe816eb35df893790e5a8e00863dc8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 23:31:52 +0000

    6cf7a1b refactor(build): fix build location of compiler-cli esm module (#13212)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-d46b8de
+ * @license Angular v2.3.0-rc.0-6cf7a1b
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12949,7 +12949,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-d46b8de');
+    var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-6cf7a1b');
 
     exports.VERSION = VERSION;
     exports.createPlatform = createPlatform;
--- index.js
+++ index.js
@@ -15,5 +15,5 @@ import { Version } from './src/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-d46b8de');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-6cf7a1b');
 //# sourceMappingURL=index.js.map
