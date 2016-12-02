b70b40061f235c2be9e3ae0b6b0b28d8bd2fe70f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 23:31:33 +0000

    d46b8de fix(router): runs guards every time when unsuccessfully navigating to the same url over and over again (#13209)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-bbb7a39
+ * @license Angular v2.3.0-rc.0-d46b8de
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- index.js
+++ index.js
@@ -15,5 +15,5 @@ import { Version } from './src/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-bbb7a39');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-d46b8de');
 //# sourceMappingURL=index.js.map
