a14cf985896d67b21562d8ff333add96feb3a0e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-05 18:37:32 +0000

    d2e5198 docs(contributing): fix styleguide link and typos (#13198)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-6cf7a1b
+ * @license Angular v2.3.0-rc.0-d2e5198
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12949,7 +12949,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-6cf7a1b');
+    var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-d2e5198');
 
     exports.VERSION = VERSION;
     exports.createPlatform = createPlatform;
--- index.js
+++ index.js
@@ -15,5 +15,5 @@ import { Version } from './src/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-6cf7a1b');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-d2e5198');
 //# sourceMappingURL=index.js.map
