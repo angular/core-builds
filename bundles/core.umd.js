00c92a20dbef09963718541f61e87996d118a24d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 01:23:21 +0000

    4a09c81 fix(language-service): do not throw for invalid metadata (#13261)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-16efb13
+ * @license Angular v2.3.0-rc.0-4a09c81
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-16efb13');
+    var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-4a09c81');
 
     /**
      *  Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-16efb13');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-4a09c81');
 //# sourceMappingURL=version.js.map
