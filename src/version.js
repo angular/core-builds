7cb7150331445955c02fc12fb6b83bf3fd877ebb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 22:07:45 +0000

    13b41bd chore(release): update version of tsc-wrapped to 0.4.2

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-cf52284');
+export var /** @type {?} */ VERSION = new Version('2.3.0-13b41bd');
 //# sourceMappingURL=version.js.map
