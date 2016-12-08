733835e1051d0fe7f9423e90a7b42bb71369c88d
Victor Berchet <victor@suumit.com>
2016-12-08 19:37:33 +0000

    9591a08 fix(router): Use T type in Resolve interface (#13242)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-65965c2
+ * @license Angular v2.3.0-9591a08
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-65965c2');
+    var /** @type {?} */ VERSION = new Version('2.3.0-9591a08');
 
     /**
      *  Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-65965c2');
+export var /** @type {?} */ VERSION = new Version('2.3.0-9591a08');
 //# sourceMappingURL=version.js.map
