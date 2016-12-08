dafa7230e386660f9f853722e4a4b71a686c8cd8
GitHub <noreply@github.com>
2016-12-08 23:20:28 +0000

    4398056 fix(compiler): fix transpiled ES5 code (#13322)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-1b54788');
+export var /** @type {?} */ VERSION = new Version('2.3.0-4398056');
 //# sourceMappingURL=version.js.map
