c113c2d41b87ae19523ba5970cad8fe3b6106b41
GitHub <noreply@github.com>
2016-12-08 22:01:43 +0000

    1b54788 build(tslint): enable no-inner-declarations (#13316)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-9591a08
+ * @license Angular v2.3.0-1b54788
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-9591a08');
+    var /** @type {?} */ VERSION = new Version('2.3.0-1b54788');
 
     /**
      *  Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-9591a08');
+export var /** @type {?} */ VERSION = new Version('2.3.0-1b54788');
 //# sourceMappingURL=version.js.map
