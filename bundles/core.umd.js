99c1c3a64c675171665b7f6a40f67dd322529d54
Victor Berchet <victor@suumit.com>
2016-12-09 18:56:49 +0000

    56c361f test(compiler): test i18n explicit id

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-72361fb
+ * @license Angular v2.3.0-56c361f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-72361fb');
+    var /** @type {?} */ VERSION = new Version('2.3.0-56c361f');
 
     /**
      *  Allows to refer to references which are not yet defined.
