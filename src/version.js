efca55b28ca9de39375dbe4d65efc68ec1d2de77
Victor Berchet <victor@suumit.com>
2016-12-09 19:33:08 +0000

    c8baf51 style: clang-format the code

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-9a8423d');
+export var /** @type {?} */ VERSION = new Version('2.3.0-c8baf51');
 //# sourceMappingURL=version.js.map
