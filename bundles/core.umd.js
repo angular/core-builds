75c4e228e6d26b9016955a75a68df126cd9cb1c0
Victor Berchet <victor@suumit.com>
2016-12-09 23:50:31 +0000

    90c2235 feat(http): simplify URLSearchParams creation (#13338)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-a006c14
+ * @license Angular v2.3.0-90c2235
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-a006c14');
+    var /** @type {?} */ VERSION = new Version('2.3.0-90c2235');
 
     /**
      *  Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-a006c14');
+export var /** @type {?} */ VERSION = new Version('2.3.0-90c2235');
 //# sourceMappingURL=version.js.map
