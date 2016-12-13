8e711af4e2a45f973273f9dfcb9fde25695c092a
Victor Berchet <victor@suumit.com>
2016-12-13 00:58:25 +0000

    1d0ed6f docs(core): update OnDestroy description (#13369)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-6f330a5
+ * @license Angular v2.3.0-1d0ed6f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-6f330a5');
+    var /** @type {?} */ VERSION = new Version('2.3.0-1d0ed6f');
 
     /**
      *  Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-6f330a5');
+export var /** @type {?} */ VERSION = new Version('2.3.0-1d0ed6f');
 //# sourceMappingURL=version.js.map
