64b0a91462e6f9573dd8f171e4782196ce2b46ec
GitHub <noreply@github.com>
2016-12-13 03:22:28 +0000

    4b3d135 fix(compiler): xmb `<ph>` tags should not self close (#13413)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-1d0ed6f
+ * @license Angular v2.3.0-4b3d135
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-1d0ed6f');
+    var /** @type {?} */ VERSION = new Version('2.3.0-4b3d135');
 
     /**
      *  Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-1d0ed6f');
+export var /** @type {?} */ VERSION = new Version('2.3.0-4b3d135');
 //# sourceMappingURL=version.js.map
