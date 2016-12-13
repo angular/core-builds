f3e43c8329c1869d73238abdf1f39e8c8da6e3df
Victor Berchet <victor@suumit.com>
2016-12-13 19:46:16 +0000

    714f2af ci(browser providers): update browsers in SL and BS (#13431)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-2b90cd5');
+export var /** @type {?} */ VERSION = new Version('2.3.0-714f2af');
 //# sourceMappingURL=version.js.map
