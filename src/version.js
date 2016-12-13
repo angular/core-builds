590df36f8036224eb4a04732c36687ee1e582241
Victor Berchet <victor@suumit.com>
2016-12-13 19:33:41 +0000

    9ec0a4e feat(language-service): warn when a method isn't called in an event (#13437)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-4b3d135');
+export var /** @type {?} */ VERSION = new Version('2.3.0-9ec0a4e');
 //# sourceMappingURL=version.js.map
