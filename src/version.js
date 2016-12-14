b5c68286204abe161f0740dc21cbbcbf80337583
Victor Berchet <victor@suumit.com>
2016-12-14 22:45:33 +0000

    aa3769b fix(compiler): resolver should merge host bindings and listeners (#13474)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-d4ddb60');
+export var /** @type {?} */ VERSION = new Version('2.3.0-aa3769b');
 //# sourceMappingURL=version.js.map
