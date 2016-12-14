1ffc1993e5b569abbd8b1d73763e192a13632bb3
Victor Berchet <victor@suumit.com>
2016-12-14 22:49:33 +0000

    aa40366 fix(compiler): fix simplify a reference without a name

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-aa3769b
+ * @license Angular v2.3.0-aa40366
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-aa3769b');
+    var /** @type {?} */ VERSION = new Version('2.3.0-aa40366');
 
     /**
      *  Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-aa3769b');
+export var /** @type {?} */ VERSION = new Version('2.3.0-aa40366');
 //# sourceMappingURL=version.js.map
