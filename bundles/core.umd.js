fef41a83278ff4c679b095816ea7c21321c520fe
Victor Berchet <victor@suumit.com>
2016-12-14 17:07:13 +0000

    821b8f0 fix(forms): ensure select[multiple] retains selections

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-7b0a867
+ * @license Angular v2.3.0-821b8f0
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.3.0-7b0a867');
+    var /** @type {?} */ VERSION = new Version('2.3.0-821b8f0');
 
     /**
      *  Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-7b0a867');
+export var /** @type {?} */ VERSION = new Version('2.3.0-821b8f0');
 //# sourceMappingURL=version.js.map
