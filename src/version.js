f0c40f37afd184d6f0e8dc03961aa4ecbdd71955
Victor Berchet <victor@suumit.com>
2016-12-15 00:55:49 +0000

    6cefccb build: bump angular to 4.0.0-beta.0 & tsc-wrapped to 0.5.0

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-fa9e21e');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-6cefccb');
 //# sourceMappingURL=version.js.map
