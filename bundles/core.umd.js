bf6a2b5d6fe8ccc3b049c30f3a4a5ac9ef930e94
Chuck Jazdzewski <chuckj@google.com>
2016-12-15 21:21:17 +0000

    732f446 docs(common): fix ngIf example (#13496)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-f0e0925
+ * @license Angular v4.0.0-beta.0-732f446
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-f0e0925');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-732f446');
 
     /**
      *  Allows to refer to references which are not yet defined.
