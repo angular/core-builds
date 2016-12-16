eed3c87e7819fb19fd3176d21598246cdc3dfcd6
Chuck Jazdzewski <chuckj@google.com>
2016-12-16 01:20:16 +0000

    55dfa1b test(forms): refactor integration tests to improve speed (#13500)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-0fe3cd9');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-55dfa1b');
 //# sourceMappingURL=version.js.map
