5942912a98e5af2bd22e9f21aa077605a34e60ba
Chuck Jazdzewski <chuckj@google.com>
2016-12-16 23:34:55 +0000

    4568d5d refactor(core): fix typo (#13515)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-c6e8939
+ * @license Angular v4.0.0-beta.0-4568d5d
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1149,7 +1149,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-c6e8939');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-4568d5d');
 
     /**
      *  Allows to refer to references which are not yet defined.
@@ -8205,19 +8205,19 @@
     /**
      *  Creates a factory for a platform
       * *
-     * @param {?} parentPlaformFactory
+     * @param {?} parentPlatformFactory
      * @param {?} name
      * @param {?=} providers
      * @return {?}
      */
-    function createPlatformFactory(parentPlaformFactory, name, providers) {
+    function createPlatformFactory(parentPlatformFactory, name, providers) {
         if (providers === void 0) { providers = []; }
         var /** @type {?} */ marker = new OpaqueToken("Platform: " + name);
         return function (extraProviders) {
             if (extraProviders === void 0) { extraProviders = []; }
             if (!getPlatform()) {
-                if (parentPlaformFactory) {
-                    parentPlaformFactory(providers.concat(extraProviders).concat({ provide: marker, useValue: true }));
+                if (parentPlatformFactory) {
+                    parentPlatformFactory(providers.concat(extraProviders).concat({ provide: marker, useValue: true }));
                 }
                 else {
                     createPlatform(ReflectiveInjector.resolveAndCreate(providers.concat(extraProviders).concat({ provide: marker, useValue: true })));
