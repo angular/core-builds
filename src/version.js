48e0c70069611b733a36a423536ed0a40ee85b2d
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 21:01:22 +0000

    eb2ceff fix(router): should reset location if a navigation by location is successful (#13545)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-f49ab56');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-eb2ceff');
 //# sourceMappingURL=version.js.map
