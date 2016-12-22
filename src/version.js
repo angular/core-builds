22e7784ba6bd5a600bad9e08ba19e7d11d7f42db
Hans <hans@hansl.ca>
2016-12-22 20:49:42 +0000

    67380d4 fix(testing): improve misleading error message when don't call compileComponents (#13543)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-e7ece6c');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-67380d4');
 //# sourceMappingURL=version.js.map
