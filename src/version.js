50ea4ab23b13fdce9f9c893ddd6e63f763c6fa09
Hans <hans@hansl.ca>
2016-12-27 21:58:25 +0000

    174334d fix(router): routerLink support of null/undefined (#13380)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-9c69703');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-174334d');
 //# sourceMappingURL=version.js.map
