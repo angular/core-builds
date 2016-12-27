bb77f61369bb68c3adef2762809f61d3cb520e61
Hans <hans@hansl.ca>
2016-12-27 23:38:24 +0000

    881eb89 fix(Compiler): allow "." in attribute selectors (#13653)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-0eca960');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-881eb89');
 //# sourceMappingURL=version.js.map
