26d4e0b045f6da638a32ca53aaa71001838b0854
Igor Minar <iminar@google.com>
2017-01-05 19:46:45 +0000

    21030e9 fix(core): animations no longer silently exits if the element is not apart of the DOM (#13763)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-889b48d
+ * @license Angular v4.0.0-beta.1-21030e9
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1051,7 +1051,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-889b48d');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-21030e9');
 
     /**
      * Inject decorator and metadata.
