68ecd4d0d79b9572a8fa382e63f056785f5c4d6e
Igor Minar <iminar@google.com>
2017-01-05 18:35:20 +0000

    f822f95 docs(common): add an example how to bind multiple classes based on a single parameter (#13779)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-9898d8f
+ * @license Angular v4.0.0-beta.1-f822f95
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1051,7 +1051,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-9898d8f');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-f822f95');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-9898d8f');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-f822f95');
 //# sourceMappingURL=version.js.map
