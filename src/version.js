9d833d6d009dddaf3ff079d45a83c6620e2b89b1
Igor Minar <iminar@google.com>
2017-01-05 19:46:24 +0000

    889b48d fix(core): animations should blend in all previously transitioned styles into next animation if interrupted (#13148)

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-1bd04e9');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-889b48d');
 //# sourceMappingURL=version.js.map
