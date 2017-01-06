9cc934fa6c8abed58fac9b2bc1abc0b2b596315d
Igor Minar <igor@angularjs.org>
2017-01-06 02:08:16 +0000

    1c85e99 chore(tsc-wrapped): bump version number to 4.0.0-beta.2

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-8063b0d');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.2-1c85e99');
 //# sourceMappingURL=version.js.map
