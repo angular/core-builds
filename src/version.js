45ec31a0e5f32bf86954ba83604e3882ce184ed9
Matias Niemelä <matias@yearofmoo.com>
2017-01-09 23:52:08 +0000

    6397824 fix(router): throw an error when navigate to null/undefined path

--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.4.2-6164eb2');
+export var /** @type {?} */ VERSION = new Version('2.4.2-6397824');
 //# sourceMappingURL=version.js.map
