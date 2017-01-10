32298a032a7e58828f671eb224a19ae67f4ebd47
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 00:09:24 +0000

    61ba223 fix(router): throw an error when navigate to null/undefined path

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-6164eb2
+ * @license Angular v2.4.2-61ba223
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1103,7 +1103,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.4.2-6164eb2');
+    var /** @type {?} */ VERSION = new Version('2.4.2-61ba223');
 
     /**
      *  Allows to refer to references which are not yet defined.
