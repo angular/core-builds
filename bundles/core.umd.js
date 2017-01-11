8599db7711a3c49bc0831039e2f2c99efcf61bc3
Alex Eagle <alexeagle@google.com>
2017-01-11 01:13:48 +0000

    ed32340 chore(owners): configure pullapprove.com

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-d4d3782
+ * @license Angular v4.0.0-beta.2-ed32340
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1051,7 +1051,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.2-d4d3782');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.2-ed32340');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.2-d4d3782');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.2-ed32340');
 //# sourceMappingURL=version.js.map
