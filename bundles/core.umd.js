bd3eab695af76c438aa217f19dd89a33b1b3f06e
Miško Hevery <misko@hevery.com>
2017-01-13 21:28:20 +0000

    5237b1c chore(compiler-cli): Move calculateEmitPath into CompilerHost (#13904)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-dc63cef
+ * @license Angular v4.0.0-beta.3-5237b1c
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1051,7 +1051,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-dc63cef');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-5237b1c');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -41,5 +41,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-dc63cef');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-5237b1c');
 //# sourceMappingURL=version.js.map
