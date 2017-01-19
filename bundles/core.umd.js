92f3ee3b4ea847a3e8dde5b960d59727cec94347
Miško Hevery <misko@hevery.com>
2017-01-19 00:14:55 +0000

    1200cf2 fix(http): don't create a blob out of ArrayBuffer when type is application/octet-stream (#13992)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-635bf02
+ * @license Angular v4.0.0-beta.3-1200cf2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-635bf02');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-1200cf2');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-635bf02');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-1200cf2');
 //# sourceMappingURL=version.js.map
