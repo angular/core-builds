e661a58834982e59a605feba033fb256a21a3799
Miško Hevery <misko@hevery.com>
2017-01-19 01:21:22 +0000

    992647d docs(changelog): add changelog for 4.0.0-beta.4

---
+++ src/i18n/tokens.js
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { InjectionToken } from '../di/injection_token';
+/**
+ * @experimental i18n support is experimental.
+ */
+export var /** @type {?} */ LOCALE_ID = new InjectionToken('LocaleId');
+/**
+ * @experimental i18n support is experimental.
+ */
+export var /** @type {?} */ TRANSLATIONS = new InjectionToken('Translations');
+/**
+ * @experimental i18n support is experimental.
+ */
+export var /** @type {?} */ TRANSLATIONS_FORMAT = new InjectionToken('TranslationsFormat');
+//# sourceMappingURL=tokens.js.map
