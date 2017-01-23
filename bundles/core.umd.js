37f3216a4fecd100b12582b5b296313975785000
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:41:28 +0000

    e21e9c5 feat(upgrade): Support ng-model in downgraded components (#10578)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-d3a3a8e
+ * @license Angular v4.0.0-beta.4-e21e9c5
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-d3a3a8e');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-e21e9c5');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-d3a3a8e');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-e21e9c5');
 //# sourceMappingURL=version.js.map
