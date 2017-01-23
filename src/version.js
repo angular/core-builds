0146cd1bcdabdb436a8981b0be4110bf845a3243
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:08:32 +0000

    dff6ee3 ci: validate the message of each new commit as part of the CI linting

--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-0589f93');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-dff6ee3');
 //# sourceMappingURL=version.js.map
