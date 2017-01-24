ed00c1e3c3eee54fa070dec115c9d1a34500fb70
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-24 21:07:36 +0000

    0c7726d feat(tsc-wrapped): Support of vinyl like config file was added (#13987)

--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-83361d8');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-0c7726d');
 //# sourceMappingURL=version.js.map
