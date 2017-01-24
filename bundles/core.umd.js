66b93d4cf6d9a3aaea07762307b2301f8c06dc37
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-24 23:22:49 +0000

    6152eb2 fix(upgrade/static): ensure upgraded injector is initialized early enough (#14065)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-1c24271
+ * @license Angular v4.0.0-beta.4-6152eb2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-1c24271');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-6152eb2');
 
     /**
      * Inject decorator and metadata.
