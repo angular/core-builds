d81edd59bc986d6390bf8f8ac8415e08a0dbf1b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 21:34:27 +0000

    d2859cd style(compiler): run format

--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-4931a61');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-d2859cd');
 //# sourceMappingURL=version.js.map
