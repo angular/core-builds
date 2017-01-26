27c9e60a996cc097591c6b65108cb8a2d5ff533c
Victor Berchet <victor@suumit.com>
2017-01-26 02:00:33 +0000

    1e729d7 feat(core): add query support to view engine

--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-05b2b49');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-1e729d7');
 //# sourceMappingURL=version.js.map
