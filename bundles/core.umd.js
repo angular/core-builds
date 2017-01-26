eba1377706c28ec0b7aaa4ecf8d7731da39df1c0
Victor Berchet <victor@suumit.com>
2017-01-26 01:57:13 +0000

    3ef73c2 feat(NgComponentOutlet): add NgModule support to NgComponentOutlet directive (#14088)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-4106d18
+ * @license Angular v4.0.0-beta.5-3ef73c2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-4106d18');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-3ef73c2');
 
     /**
      * Inject decorator and metadata.
