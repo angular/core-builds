3d9f6ae1a991bede7ede24f6d5139b2a8ab57806
Miško Hevery <misko@hevery.com>
2017-01-27 21:22:16 +0000

    da41a95 docs: branding fixes (#14132)

--- src/linker/compiler.js
+++ src/linker/compiler.js
@@ -98,7 +98,7 @@ export class Compiler {
      * Exposes the CSS-style selectors that have been used in `ngContent` directives within
      * the template of the given component.
      * This is used by the `upgrade` library to compile the appropriate transclude content
-     * in the Angular 1 wrapper component.
+     * in the AngularJS wrapper component.
      * @param {?} component
      * @return {?}
      */
