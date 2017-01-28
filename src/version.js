88afcd23a6e9e85d7463518cdb1ac20a16a48fa6
Miško Hevery <misko@hevery.com>
2017-01-28 05:02:35 +0000

    0854a5d style(docs): update copyright years (#13736)

--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-df7f5fc');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-0854a5d');
 //# sourceMappingURL=version.js.map
