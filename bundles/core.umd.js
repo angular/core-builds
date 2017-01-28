4361f217412bf61c9754bd35991e72df7be40bbc
Igor Minar <iminar@google.com>
2017-01-28 02:40:41 +0000

    24ea3f0 test(compiler): don't print warnings from closure compiler (#14159)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-3368f29
+ * @license Angular v4.0.0-beta.5-24ea3f0
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-3368f29');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-24ea3f0');
 
     /**
      * Inject decorator and metadata.
