8ee3dcf4c60b2bd07ae402bd4cad44845667c86a
Miško Hevery <misko@hevery.com>
2017-02-01 00:52:06 +0000

    8270bec fix: failing integration test

--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-2205829');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-8270bec');
 //# sourceMappingURL=version.js.map
