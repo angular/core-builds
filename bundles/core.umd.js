91240643af407e09b45dfb3b535353fe709997ae
Igor Minar <iminar@google.com>
2017-02-03 08:28:08 +0000

    0ba5beb build: modularize the gulp file to be easier to maintain (#14259)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-a909643
+ * @license Angular v4.0.0-beta.6-0ba5beb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-a909643');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-0ba5beb');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-a909643');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-0ba5beb');
 //# sourceMappingURL=version.js.map
