4df5459e11ba899c2e63eb6a54f90c6d195f2c86
Miško Hevery <misko@hevery.com>
2017-02-07 19:55:43 +0000

    a378aab docs(compiler): incorrect method reference (#14314)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-7036e04
+ * @license Angular v4.0.0-beta.6-a378aab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-7036e04');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-a378aab');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-7036e04');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-a378aab');
 //# sourceMappingURL=version.js.map
