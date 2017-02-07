d45a64a8589aa3ba3bffac7c4e5a9310b602f180
Igor Minar <iminar@google.com>
2017-02-07 08:22:23 +0000

    d4ffa47 fix(aio): correctly render decorator docs (#14328)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-80b66ed
+ * @license Angular v4.0.0-beta.6-d4ffa47
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-80b66ed');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-d4ffa47');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-80b66ed');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-d4ffa47');
 //# sourceMappingURL=version.js.map
