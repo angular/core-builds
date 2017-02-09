afd73be59517f7242c8cfd9c7f99199f61d428f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-02-09 23:31:20 +0000

    6af9840 wip: introduce LogAdapter for logging methods

--- src/version.js
+++ src/version.js
@@ -44,5 +44,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-45cc444');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-6af9840');
 //# sourceMappingURL=version.js.map
