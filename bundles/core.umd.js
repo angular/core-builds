c5a9f5035e218ffd6a874223f525dd127fce0fba
Miško Hevery <misko@hevery.com>
2017-02-09 20:17:06 +0000

    541de26 fix(router): do not finish bootstrap until all the routes are resolved (#14327)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.7-74cb575
+ * @license Angular v2.4.7-541de26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1117,7 +1117,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.4.7-74cb575');
+    var /** @type {?} */ VERSION = new Version('2.4.7-541de26');
 
     /**
      * Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.4.7-74cb575');
+export var /** @type {?} */ VERSION = new Version('2.4.7-541de26');
 //# sourceMappingURL=version.js.map
