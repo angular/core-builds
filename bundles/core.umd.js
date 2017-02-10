eb035e1b8ee80283b7585653550788b41fdcc06a
Igor Minar <iminar@google.com>
2017-02-10 01:41:36 +0000

    b988733 test(aio): remove unnecessary hack from e2e tests (#14356)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-44bb337
+ * @license Angular v4.0.0-beta.7-b988733
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-44bb337');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-b988733');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -44,5 +44,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-44bb337');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-b988733');
 //# sourceMappingURL=version.js.map
