8d1158d9a1a948e133362de86bda988ad4889099
Misko Hevery <misko@hevery.com>
2017-02-11 05:24:57 +0000

    2769370 ci: fix validate-commit-messages

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-d3f174a
+ * @license Angular v4.0.0-beta.7-2769370
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-d3f174a');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-2769370');
 
     /**
      * Inject decorator and metadata.
