67abd0292af9718b1c8d3266ed686c2579ab5bdd
Miško Hevery <misko@hevery.com>
2017-02-13 23:08:33 +0000

    1dc9be4 fix(aio): support `ValueModule` symbols in the TypeScript source (#14464)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-221b7a1
+ * @license Angular v4.0.0-beta.7-1dc9be4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1131,7 +1131,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-221b7a1');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-1dc9be4');
 
     /**
      * Inject decorator and metadata.
