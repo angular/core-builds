024eb5e27af93237be46047e69acaf62b9c25557
Igor Minar <iminar@google.com>
2017-02-18 07:15:40 +0000

    c4a6263 feat(aio): replace `take(1)` with more semantic `first()` (#14571)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-ab26b65
+ * @license Angular v4.0.0-beta.7-c4a6263
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1133,7 +1133,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-ab26b65');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-c4a6263');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -44,5 +44,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-ab26b65');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-c4a6263');
 //# sourceMappingURL=version.js.map
