ad9efe4ff92de45e924924fb7b198c8943401c21
Igor Minar <igor@angularjs.org>
2017-02-18 23:36:30 +0000

    1bdf706 release: cut v4.0.0-beta.8

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-5129e8e
+ * @license Angular v4.0.0-beta.8-1bdf706
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1133,7 +1133,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-5129e8e');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-1bdf706');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -44,5 +44,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-5129e8e');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-1bdf706');
 //# sourceMappingURL=version.js.map
