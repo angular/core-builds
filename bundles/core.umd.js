4ee54de8a400afcd10752302e81bcd1ccea9a606
Victor Berchet <victor@suumit.com>
2017-02-21 00:39:17 +0000

    5f3c844 refactor(router): misc

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-601fd3e
+ * @license Angular v4.0.0-beta.8-5f3c844
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1133,7 +1133,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-601fd3e');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-5f3c844');
 
     /**
      * Inject decorator and metadata.
--- src/version.js
+++ src/version.js
@@ -44,5 +44,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-601fd3e');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-5f3c844');
 //# sourceMappingURL=version.js.map
