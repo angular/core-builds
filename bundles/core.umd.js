7d580e9ae60272a2053c115b381de71c369a6001
Victor Berchet <victor@suumit.com>
2017-02-21 00:55:55 +0000

    8742432 feat(forms): add option to use browser's native validation and angular forms (#13566)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-551fe50
+ * @license Angular v4.0.0-beta.8-8742432
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1133,7 +1133,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-551fe50');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-8742432');
 
     /**
      * Inject decorator and metadata.
