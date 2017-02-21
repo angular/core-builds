5abf2e2d6e4c174a671b24ece22e93461bd71293
GitHub <noreply@github.com>
2017-02-21 02:56:44 +0000

    2a191ca fix(router): do not finish bootstrap until all the routes are resolved (#14608)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-c2e0f71
+ * @license Angular v4.0.0-beta.8-2a191ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1133,7 +1133,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-c2e0f71');
+    var /** @type {?} */ VERSION = new Version('4.0.0-beta.8-2a191ca');
 
     /**
      * Inject decorator and metadata.
