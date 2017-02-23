536132a7665f908b1a679946d9831be2fd417e28
Igor Minar <iminar@google.com>
2017-02-23 05:17:08 +0000

    175dbce build: check yarn version in `check-environment` (#14499)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-801b090
+ * @license Angular v4.0.0-beta.8-175dbce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1065,7 +1065,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-801b090');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-175dbce');
 
 /**
  * Inject decorator and metadata.
