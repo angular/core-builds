9bbbfa97aad383c46934dc89df346b5a8f16ebe3
Igor Minar <iminar@google.com>
2017-02-24 05:01:56 +0000

    39f56fa feat(compiler-cli): add an `outFile` option to ng-xi18n

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1011,7 +1011,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new Version('4.0.0-beta.8-e99d721');
+    var /** @type {?} */VERSION = new Version('4.0.0-beta.8-39f56fa');
 
     /**
      * Inject decorator and metadata.
