3e155d5f24333d9b16aec493546f5fa2d4493f6c
Igor Minar <igor@angularjs.org>
2017-02-25 02:29:02 +0000

    c7d1b36 docs: update release schedule due to bad 4.0.0-rc.0 release

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-2602b03
+ * @license Angular v4.0.0-rc.1-c7d1b36
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -864,7 +864,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-2602b03');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-c7d1b36');
 
 /**
  * Inject decorator and metadata.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1011,7 +1011,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new Version('4.0.0-beta.8-2602b03');
+    var /** @type {?} */VERSION = new Version('4.0.0-rc.1-c7d1b36');
 
     /**
      * Inject decorator and metadata.
