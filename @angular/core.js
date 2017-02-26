fe89c043e66ea19716a2aba1acd6ed99583c15c6
GitHub <noreply@github.com>
2017-02-26 04:39:13 +0000

    213e210 docs: changelog - add anchor to the FESM section

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-c868457
+ * @license Angular v4.0.0-rc.1-213e210
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -864,7 +864,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-c868457');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-213e210');
 
 /**
  * Inject decorator and metadata.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1011,7 +1011,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new Version('4.0.0-rc.1-c868457');
+    var /** @type {?} */VERSION = new Version('4.0.0-rc.1-213e210');
 
     /**
      * Inject decorator and metadata.
