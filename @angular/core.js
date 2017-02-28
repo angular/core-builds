46c358cff5a9da6cf1c2ace253f5c2259aaaa3fd
Igor Minar <iminar@google.com>
2017-02-28 00:38:26 +0000

    e58cb7b docs: fix animations module instructions in the changelog (#14729)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-5caab71
+ * @license Angular v4.0.0-rc.1-e58cb7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -864,7 +864,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-5caab71');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-e58cb7b');
 
 /**
  * Inject decorator and metadata.
