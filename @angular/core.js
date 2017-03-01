95e8cc3d20e3b40695169ad797e395f449cd8131
Igor Minar <iminar@google.com>
2017-03-01 23:56:45 +0000

    b6e6fc1 docs: fix changelog to list npm command for Windows (#14812)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-dd49901
+ * @license Angular v4.0.0-rc.1-b6e6fc1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -857,7 +857,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-dd49901');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-b6e6fc1');
 
 /**
  * Inject decorator and metadata.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1003,7 +1003,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new Version('4.0.0-rc.1-dd49901');
+    var /** @type {?} */VERSION = new Version('4.0.0-rc.1-b6e6fc1');
 
     /**
      * Inject decorator and metadata.
