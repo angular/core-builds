e58fcd7e530f1e9f4ac7cf211576294e473c5e57
Igor Minar <iminar@google.com>
2017-03-01 21:43:48 +0000

    7a66a41 test(language-service): test `@angular/language-service` can be loaded by tsserver.js (#14721)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1003,7 +1003,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new Version('4.0.0-rc.1-79fc1e3');
+    var /** @type {?} */VERSION = new Version('4.0.0-rc.1-7a66a41');
 
     /**
      * Inject decorator and metadata.
