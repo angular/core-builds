0e9bc3072070ce7bcc895fc5e5fa523c42732ebb
Igor Minar <igor@angularjs.org>
2017-03-02 07:42:51 +0000

    49aa508 docs: add 2.4.9 release notes

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.2-180b705
+ * @license Angular v4.0.0-rc.2-49aa508
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -857,7 +857,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.2-180b705');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.2-49aa508');
 
 /**
  * Inject decorator and metadata.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1003,7 +1003,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new Version('4.0.0-rc.2-180b705');
+    var /** @type {?} */VERSION = new Version('4.0.0-rc.2-49aa508');
 
     /**
      * Inject decorator and metadata.
