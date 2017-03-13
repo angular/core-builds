2b6ccf24bf1e15d8c4773a7c25fb458390a089d0
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 16:53:49 +0000

    fa1920a feat(aio): enable deep-linking on deployed apps (until prerendering is done) (#15049)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-71cd295
+ * @license Angular v4.0.0-rc.3-fa1920a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-71cd295');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-fa1920a');
 
 /**
  * Inject decorator and metadata.
