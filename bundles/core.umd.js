be2da17ca910b61dcb4cf6e3e9885c6470371c16
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:18:57 +0000

    e7dab7e feat(aio): hard code a "Home" image link into the toolbar

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-893652a
+ * @license Angular v4.0.0-rc.3-e7dab7e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-893652a');
+    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-e7dab7e');
     /**
      * Inject decorator and metadata.
      *
