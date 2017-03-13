a24bbaef14fe4dc8265e6d820ff9acde6efa0303
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:13:16 +0000

    893652a fix(aio): upgrade to zone.js 0.7.8 (#15099)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6559425
+ * @license Angular v4.0.0-rc.3-893652a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-6559425');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-893652a');
 
 /**
  * Inject decorator and metadata.
