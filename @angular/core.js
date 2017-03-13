2f720f30549a9ccea8ab63a9dbd82b9c45b45ce1
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 17:48:32 +0000

    0e2dd76 build: remove obsolete `ci-lite/` directory and files (#14994)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff71eff
+ * @license Angular v4.0.0-rc.3-0e2dd76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ff71eff');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-0e2dd76');
 
 /**
  * Inject decorator and metadata.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff71eff
+ * @license Angular v4.0.0-rc.3-0e2dd76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ff71eff');
+    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-0e2dd76');
     /**
      * Inject decorator and metadata.
      *
