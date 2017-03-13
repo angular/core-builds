22719caf70f975c2ffb17b538cd227643c7bb823
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 16:53:10 +0000

    71cd295 ci: add wardbell and gkalpak to aio approvers (#15030)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6c8638c
+ * @license Angular v4.0.0-rc.3-71cd295
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-6c8638c');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-71cd295');
 
 /**
  * Inject decorator and metadata.
