27abd717ecf18ef3a4260c73261ffa1d55ef86c2
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 22:57:17 +0000

    778f7d6 fix(forms): remove equalsTo validator (#15050)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-2c5a671
+ * @license Angular v4.0.0-rc.3-778f7d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
