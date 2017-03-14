a8274deeb97ab6b2e7f52fec7c5ab0ee83dd6bc9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 15:22:34 +0000

    423bfb0 ci(aio): whitelist 'aio-contributors' for deploying PR previews

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2f250c1
+ * @license Angular v4.0.0-rc.3-423bfb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-2f250c1');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-423bfb0');
 
 /**
  * Inject decorator and metadata.
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-2f250c1
+ * @license Angular v4.0.0-rc.3-423bfb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
