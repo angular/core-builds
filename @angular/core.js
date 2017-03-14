f24fdb70741f7263376f36603153d50daddf6d79
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 01:53:17 +0000

    1c1085b feat(aio): add google analytics (#15081)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-914797a
+ * @license Angular v4.0.0-rc.3-1c1085b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-914797a');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-1c1085b');
 
 /**
  * Inject decorator and metadata.
