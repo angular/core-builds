8a9f649e7b257449b168618250867dffbce63414
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:11:11 +0000

    8850098 build(aio): ensure API docs are generated in the correct place (#15103)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-e6c81d2
+ * @license Angular v4.0.0-rc.3-8850098
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-e6c81d2');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-8850098');
 
 /**
  * Inject decorator and metadata.
