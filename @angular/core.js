53a64fc706f8250d874c20477ff3c6dceb6e612d
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 21:25:15 +0000

    0aad270 refactor(platform-browser): move platform-browser/animations to animations/browser (#15130)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-221899a
+ * @license Angular v4.0.0-rc.3-0aad270
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-221899a');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-0aad270');
 
 /**
  * Inject decorator and metadata.
