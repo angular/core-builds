40ad2bd538df04d2260613598b78d1a07976f298
GitHub <noreply@github.com>
2017-03-15 16:45:30 +0000

    b00fe20 fix(compiler): support interface types in injectable constuctors (#14894)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-5c0ea20
+ * @license Angular v4.0.0-rc.3-b00fe20
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
