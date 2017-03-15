9f604d296abd0595fcd1cbbddfd2f554df755684
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 23:24:12 +0000

    bf98d9d fix(aio): scroll to hash fragment element on URL change

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-a9b1880
+ * @license Angular v4.0.0-rc.3-bf98d9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
