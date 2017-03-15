d1ca83fa28e769ba68e26069068aeb8f4f12f0ab
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 16:46:43 +0000

    36ce0af fix(animations): support multiple state names per state() call (#15147)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-b00fe20
+ * @license Angular v4.0.0-rc.3-36ce0af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-b00fe20');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-36ce0af');
 
 /**
  * Inject decorator and metadata.
