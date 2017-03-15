5af3f5680e53bae0484c9415af34a4aa4d2f850b
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:27:32 +0000

    3b1956b fix(compiler): warning prints "WARNING" instead of "ERROR" (#15125)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff60c04
+ * @license Angular v4.0.0-rc.3-3b1956b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ff60c04');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3b1956b');
 
 /**
  * Inject decorator and metadata.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff60c04
+ * @license Angular v4.0.0-rc.3-3b1956b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ff60c04');
+    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3b1956b');
     /**
      * Inject decorator and metadata.
      *
