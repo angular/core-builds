756d61bc6adc2f87a20a4090c315e158f57e5ea3
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 04:03:11 +0000

    c10c060 feat(common): support `as` syntax in template/* bindings (#15025)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-5fe2d8f
+ * @license Angular v4.0.0-rc.3-c10c060
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-5fe2d8f');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-c10c060');
 
 /**
  * Inject decorator and metadata.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-5fe2d8f
+ * @license Angular v4.0.0-rc.3-c10c060
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-5fe2d8f');
+    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-c10c060');
     /**
      * Inject decorator and metadata.
      *
