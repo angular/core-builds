734b6612ec17306580e08c6e914632c8db164296
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 22:46:34 +0000

    62d5543 fix(animations): make sure easing values are applied to an empty animate() step (#15174)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f1b33ab
+ * @license Angular v4.0.0-rc.3-62d5543
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-f1b33ab');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-62d5543');
 
 /**
  * Inject decorator and metadata.
