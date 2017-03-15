231c0cb0acccfd2061aff53bf826de03cf9a5333
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:30:24 +0000

    3c15916 fix(compiler): Improve error message for missing annotations (#14724)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-959a03a
+ * @license Angular v4.0.0-rc.3-3c15916
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -824,7 +824,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-959a03a');
+var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3c15916');
 /**
  * Inject decorator and metadata.
  *
