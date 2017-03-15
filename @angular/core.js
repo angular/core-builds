0ab20951ba3495d6238088957a12497090fc11a9
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:26:24 +0000

    ff60c04 test(platform-server): add initial e2e tests for platform-server (#15061)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-13686bb
+ * @license Angular v4.0.0-rc.3-ff60c04
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-13686bb');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ff60c04');
 
 /**
  * Inject decorator and metadata.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-13686bb
+ * @license Angular v4.0.0-rc.3-ff60c04
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-13686bb');
+    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ff60c04');
     /**
      * Inject decorator and metadata.
      *
