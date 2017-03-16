07b41e3c1e4308f6c1475df7b9c645112c4036b8
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:28:09 +0000

    013d806 fix(platform-server): handle styles with extra ':'s correctly (#15189)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-dd36d41
+ * @license Angular v4.0.0-rc.3-013d806
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -790,7 +790,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-dd36d41');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-013d806');
 
 /**
  * Inject decorator and metadata.
