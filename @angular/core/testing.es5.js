aff4324016759646979f6e1550467bc673c9915a
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 20:41:43 +0000

    d2fbbb4 fix(core): update peer dep on zone.js to ^0.8.4 (#15209)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-77fd91d
+ * @license Angular v4.0.0-rc.3-d2fbbb4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-77fd91d
+ * @license Angular v4.0.0-rc.3-d2fbbb4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@
     /**
      * \@stable
      */
-    var VERSION = new Version('4.0.0-rc.3-77fd91d');
+    var VERSION = new Version('4.0.0-rc.3-d2fbbb4');
     /**
      * Inject decorator and metadata.
      *
