86869d804519a14b7b50e55512de6fdc15314359
Igor Minar <iminar@google.com>
2017-03-16 06:50:21 +0000

    480a407 build(aio): implement prerendering

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-923d0c5
+ * @license Angular v4.0.0-rc.3-480a407
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -790,7 +790,7 @@ class Version {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-923d0c5');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-480a407');
 
 /**
  * Inject decorator and metadata.
