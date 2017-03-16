c6fbdc162b9a5f37b3470bc1d5becd1fad08735c
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 16:30:27 +0000

    f92d644 ci: disable Android 4.1, 4.2 and 4.3 in SauceLabs (#14461)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-923d0c5
+ * @license Angular v4.0.0-rc.3-f92d644
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-923d0c5');
+    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-f92d644');
     /**
      * Inject decorator and metadata.
      *
