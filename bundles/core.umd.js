c53e4c082e1ac66b7ee06821b122fb874a090c63
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 20:14:05 +0000

    992aa17 build: add source maps into NPM distribution (#15159)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a4076c7
+ * @license Angular v4.0.0-rc.3-992aa17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-a4076c7');
+    var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-992aa17');
     /**
      * Inject decorator and metadata.
      *
@@ -13526,3 +13526,4 @@
     exports.ɵx = DebugContext;
 
 }));
+//# sourceMappingURL=core.umd.js.map
