d9538f76c3961f3f02cf3be45fb4b02413c1b9af
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 03:39:17 +0000

    b7212f5 docs: add changelog for 4.0.0-rc.4

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-410aa33
+ * @license Angular v4.0.0-rc.4-b7212f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
