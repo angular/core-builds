fbf08dcfc5f6ff1f1178259c0e5754c87bd175ed
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 02:50:45 +0000

    6b57928 docs: add changelog for 2.4.10

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.9-b0ae464
+ * @license Angular v2.4.10-6b57928
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1117,7 +1117,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new Version('2.4.9-b0ae464');
+    var /** @type {?} */ VERSION = new Version('2.4.10-6b57928');
 
     /**
      * Allows to refer to references which are not yet defined.
--- src/version.js
+++ src/version.js
@@ -43,5 +43,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.4.9-b0ae464');
+export var /** @type {?} */ VERSION = new Version('2.4.10-6b57928');
 //# sourceMappingURL=version.js.map
