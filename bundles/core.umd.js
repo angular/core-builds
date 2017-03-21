ce7282a0ea999f1c008ab4e9b6a4a31258370553
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 15:53:24 +0000

    11d5309 build(aio): fix paths to "index" pages

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-81b6a5f
+ * @license Angular v4.0.0-rc.5-11d5309
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-81b6a5f
+ * @license Angular v4.0.0-rc.5-11d5309
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -865,7 +865,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-rc.5-81b6a5f');
+var VERSION = new Version('4.0.0-rc.5-11d5309');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
