b14afb876597f577314f36ba76bb322d77070904
Igor Minar <iminar@google.com>
2017-03-22 22:29:23 +0000

    a50d79d fix(compiler-cli): adding missing format xliff for the extractor (#15386)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-64285a2');
+const VERSION = new Version('4.0.0-rc.5-a50d79d');
 
 /**
  * @license
