4021aefe8ffe4e1faa3a68174fab843ff682a379
Igor Minar <iminar@google.com>
2017-03-22 20:45:02 +0000

    64285a2 docs: update 4.0.0-rc.5 release schedule (#15288)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-941f194
+ * @license Angular v4.0.0-rc.5-64285a2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-941f194');
+const VERSION = new Version('4.0.0-rc.5-64285a2');
 
 /**
  * @license
