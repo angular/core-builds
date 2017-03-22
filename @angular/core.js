d638edd6a32b808b659bf1828ba20418dd9ec134
Miško Hevery <misko@hevery.com>
2017-03-22 16:57:39 +0000

    c584997 fix(tsc-wrapped): use windows friendly path normalization in bundler (#15374)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-1bcbcfd
+ * @license Angular v4.0.0-rc.5-c584997
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-1bcbcfd');
+const VERSION = new Version('4.0.0-rc.5-c584997');
 
 /**
  * @license
