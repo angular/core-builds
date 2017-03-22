04a0bb9a0c08ec10f3dc97e3ad7c0fbc32e00f48
Igor Minar <iminar@google.com>
2017-03-22 20:44:36 +0000

    941f194 docs: spelling errors (#15375)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-8b4edcc
+ * @license Angular v4.0.0-rc.5-941f194
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-8b4edcc');
+const VERSION = new Version('4.0.0-rc.5-941f194');
 
 /**
  * @license
