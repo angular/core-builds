8dd35f501227ac05e4c65207a66c3ea5f4507ae9
Victor Berchet <victor@suumit.com>
2017-03-23 20:09:26 +0000

    92084f2 fix(platform-browser): setAttribute should work with xmlns namespace (#14874)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-08f2f08
+ * @license Angular v4.0.0-rc.6-92084f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-08f2f08');
+const VERSION = new Version('4.0.0-rc.6-92084f2');
 
 /**
  * @license
