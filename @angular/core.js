d0e31065737fb18546ade15cdc7e9782c6080a1b
Igor Minar <iminar@google.com>
2017-03-23 04:44:56 +0000

    de87c47 docs: spelling errors

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-08d8675
+ * @license Angular v4.0.0-rc.5-de87c47
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-08d8675');
+const VERSION = new Version('4.0.0-rc.5-de87c47');
 
 /**
  * @license
