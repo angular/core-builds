18613aa8d07c603d21be6391a822c9f00270a872
Igor Minar <iminar@google.com>
2017-03-23 00:35:08 +0000

    9319b5f docs(common): update ngFor docs to new `as` syntax (#15166)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-edb2571
+ * @license Angular v4.0.0-rc.5-9319b5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-edb2571');
+const VERSION = new Version('4.0.0-rc.5-9319b5f');
 
 /**
  * @license
