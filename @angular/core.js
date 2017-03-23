502a0ae7ef921e9b0ae1a09f41df24e49e3f5d61
Igor Minar <iminar@google.com>
2017-03-23 04:41:44 +0000

    08d8675 fix(compiler): only log template deprecation warning once (#15364)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-98cb974
+ * @license Angular v4.0.0-rc.5-08d8675
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-98cb974');
+const VERSION = new Version('4.0.0-rc.5-08d8675');
 
 /**
  * @license
