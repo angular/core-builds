a2f05b494185df450135a52c7c542a2c23f47a50
Igor Minar <iminar@google.com>
2017-03-24 15:37:39 +0000

    d92930e docs: fixed broken links (#15455)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c2892da
+ * @license Angular v4.0.0-d92930e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-c2892da');
+const VERSION = new Version('4.0.0-d92930e');
 
 /**
  * @license
