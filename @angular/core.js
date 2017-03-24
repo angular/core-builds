b5a8c82ef107b5d37b26a95caf4826d87ecfb577
Victor Berchet <victor@suumit.com>
2017-03-24 21:54:06 +0000

    ea848f7 docs: clarify querying all descendants (#15400)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b8d5f87
+ * @license Angular v4.0.0-ea848f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-b8d5f87');
+const VERSION = new Version('4.0.0-ea848f7');
 
 /**
  * @license
