53fcbad8a5a71ae246163075b450f396f7ef0847
Igor Minar <igor@angularjs.org>
2017-03-24 04:13:56 +0000

    c2892da docs: revert the move of CONTRIBUTING.md to docs/CONTRIBUTING.md

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-e1b09e3
+ * @license Angular v4.0.0-c2892da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-e1b09e3');
+const VERSION = new Version('4.0.0-c2892da');
 
 /**
  * @license
