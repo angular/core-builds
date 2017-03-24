e1f2962f18a99d76f68d088e488008cbe01956e2
GitHub <noreply@github.com>
2017-03-24 00:37:07 +0000

    db0dca3 docs: add a note about exclusion of symbols prefixed with ɵ from our public api surface (#15440)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-7a715b2
+ * @license Angular v4.0.0-db0dca3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-7a715b2');
+const VERSION = new Version('4.0.0-db0dca3');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-7a715b2
+ * @license Angular v4.0.0-db0dca3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
