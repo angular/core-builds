737b836deab510eae5cae7c198b594a5f6ec6ae5
Victor Berchet <victor@suumit.com>
2017-03-24 22:27:00 +0000

    53b89ec fix(compiler): allow single quotes into named interpolations (#15461)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c17b912
+ * @license Angular v4.0.0-53b89ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-c17b912');
+const VERSION = new Version('4.0.0-53b89ec');
 
 /**
  * @license
