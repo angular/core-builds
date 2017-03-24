86b3c3e20d61da0d574c0c2308e396e098512031
GitHub <noreply@github.com>
2017-03-24 01:01:25 +0000

    c65b754 docs: add the 4.0.0 code name to the changelog

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-db0dca3
+ * @license Angular v4.0.0-c65b754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-db0dca3');
+const VERSION = new Version('4.0.0-c65b754');
 
 /**
  * @license
