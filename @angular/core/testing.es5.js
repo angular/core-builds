015ea5bea6db07427d530508104f407f02ae4bc1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 07:42:53 +0000

    b283038 build(aio): upgrade @angular/service-worker to v1.0.0-beta.8

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-afb17a5
+ * @license Angular v4.0.0-b283038
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
