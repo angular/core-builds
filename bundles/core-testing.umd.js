3cab876553fea309c4d625e0ad5cbe6292a5777a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 14:39:20 +0000

    c9e51d9 feat(aio): add new classes, table and images styling (#15478)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b283038
+ * @license Angular v4.0.0-c9e51d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-b283038
+ * @license Angular v4.0.0-c9e51d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
