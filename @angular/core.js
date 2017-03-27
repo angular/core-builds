ace2dc9713bd7cc49362b4cf35a42f04f153a95d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 13:53:44 +0000

    6605dd1 docs(aio): correct spelling in overview

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-837ed78
+ * @license Angular v4.0.0-6605dd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-837ed78');
+const VERSION = new Version('4.0.0-6605dd1');
 
 /**
  * @license
