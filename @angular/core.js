1bd5f7267438c48d4ca4ca995f3cd88629ac3ac6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 08:37:32 +0000

    a6fd22c docs(aio): fix typo

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1616cae
+ * @license Angular v4.0.0-a6fd22c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-1616cae');
+const VERSION = new Version('4.0.0-a6fd22c');
 
 /**
  * @license
