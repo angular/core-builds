22def3f1458c52de0340f8b1a7043a52f81a7535
Igor Minar <iminar@google.com>
2017-03-28 15:41:31 +0000

    eac99c1 build(aio): do not HTML format code-example contents (#15554)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b7a89ce
+ * @license Angular v4.0.0-eac99c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-b7a89ce');
+const VERSION = new Version('4.0.0-eac99c1');
 
 /**
  * @license
