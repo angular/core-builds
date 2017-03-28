cfb0f760ca9de81f68e7db8ddd74268dfeb1dce3
Victor Berchet <victor@suumit.com>
2017-03-28 21:58:40 +0000

    861953c feat(aio): add LiveExampleComponent (#15544)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-aa16ccd
+ * @license Angular v4.0.0-861953c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
