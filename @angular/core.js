39bb335a6b2f902ee497a32630f327f599a7ecb8
Victor Berchet <victor@suumit.com>
2017-03-28 23:24:08 +0000

    a580f8c fix(animations): make sure style calculations are not computed too early (#15540)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-f368381
+ * @license Angular v4.0.0-a580f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-f368381');
+const VERSION = new Version('4.0.0-a580f8c');
 
 /**
  * @license
