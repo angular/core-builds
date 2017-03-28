680708a5335d2314762ad78d98225fa793ca6a58
Victor Berchet <victor@suumit.com>
2017-03-28 00:54:38 +0000

    a9ae4da ci: add tbosch and vicb as approvers of the language service (#15530)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-15662ef');
+const VERSION = new Version('4.0.0-a9ae4da');
 
 /**
  * @license
