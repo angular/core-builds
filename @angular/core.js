efacb9b732e9c7240bb628a44c8c8465e6d3a6d2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-28 10:08:14 +0000

    9995191 fix(aio): selectedNodes should work for URLs ending with a slash

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9e0d7be
+ * @license Angular v4.0.0-9995191
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-9e0d7be');
+const VERSION = new Version('4.0.0-9995191');
 
 /**
  * @license
