a95b90531ecee58956dc4669eb05a9b1450a6bb8
Victor Berchet <victor@suumit.com>
2017-03-29 23:46:11 +0000

    1dda05a docs: add changelog for 4.0.1

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-14fd78f
+ * @license Angular v4.0.0-1dda05a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
