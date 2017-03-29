0fe4d7025d45cb3dac22d64ee0329afdd21cc453
Victor Berchet <victor@suumit.com>
2017-03-29 20:54:12 +0000

    23bf348 refactor(router): cleanup & simplification (#15436)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-902bb2f
+ * @license Angular v4.0.0-23bf348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
