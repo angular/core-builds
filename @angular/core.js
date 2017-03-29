4740b2d68f38cdb4682daf884a2034a4963a7a58
Victor Berchet <victor@suumit.com>
2017-03-29 21:35:26 +0000

    9f2acf5 feat(aio): refactors AppComponent and its kids + sidenav hiding (#15570)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9c77a7c
+ * @license Angular v4.0.0-9f2acf5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-9c77a7c');
+const VERSION = new Version('4.0.0-9f2acf5');
 
 /**
  * @license
