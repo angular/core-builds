def65f1bf00299a83996f2d3a8a7f30df57d692d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-30 22:07:59 +0000

    8ef621a fix(compiler): fix inheritance for AOT with summaries (#15583)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-9e883f5
+ * @license Angular v4.1.0-beta.0-8ef621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-9e883f5');
+const VERSION = new Version('4.1.0-beta.0-8ef621a');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-9e883f5
+ * @license Angular v4.1.0-beta.0-8ef621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
