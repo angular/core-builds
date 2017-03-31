9b41250b5b112c722e67e8f452bea233e4a4e4f6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 15:38:17 +0000

    46f8a6d docs(aio): remove unnecessary blank lines

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-0aa90c6
+ * @license Angular v4.1.0-beta.0-46f8a6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-0aa90c6');
+const VERSION = new Version('4.1.0-beta.0-46f8a6d');
 
 /**
  * @license
