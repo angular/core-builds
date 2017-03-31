1d641bc64412352381ce74846e23039b33b4663b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 10:40:52 +0000

    0aa90c6 feat(aio): add presskit and correct "/logos/angular2/"

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-4c21114
+ * @license Angular v4.1.0-beta.0-0aa90c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-4c21114');
+const VERSION = new Version('4.1.0-beta.0-0aa90c6');
 
 /**
  * @license
