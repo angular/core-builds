1e8a2b39c4867cdfac34bda0e5bf935d00ba665e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 20:24:02 +0000

    62f9738 test(aio): enable linting of `transforms/` and add rules for jasmine

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8c726ea
+ * @license Angular v4.1.0-beta.0-62f9738
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-8c726ea');
+const VERSION = new Version('4.1.0-beta.0-62f9738');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-8c726ea
+ * @license Angular v4.1.0-beta.0-62f9738
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
