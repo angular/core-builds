9e241c8205a72180ce6a55cdfd6a5a7c9af3108f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 10:56:13 +0000

    928e254 fix(aio): currentNode url should not contain hash or search

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-34a1990
+ * @license Angular v4.1.0-beta.0-928e254
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-34a1990');
+const VERSION = new Version('4.1.0-beta.0-928e254');
 
 /**
  * @license
