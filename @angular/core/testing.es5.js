7d2bf2a33ff0646dd19304202eb4e797b2bc6bbb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 21:44:18 +0000

    ae70293 feat(aio): add DtComponent to view/change the raw contents

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-1e623a3
+ * @license Angular v4.1.0-beta.0-ae70293
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
