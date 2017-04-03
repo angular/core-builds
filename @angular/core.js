76e8d6d821f50f48bfe8991ad28d1e3f1c28e5cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-03 22:56:52 +0000

    0e38bf9 build(aio): new migration

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-309bae0
+ * @license Angular v4.1.0-beta.0-0e38bf9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-309bae0');
+const VERSION = new Version('4.1.0-beta.0-0e38bf9');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-309bae0
+ * @license Angular v4.1.0-beta.0-0e38bf9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
