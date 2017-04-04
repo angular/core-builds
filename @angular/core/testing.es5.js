8e72af2c593a44551a1da5721c920b3baf563619
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-04 20:30:54 +0000

    7b005aa style(router): fix typo in router preloader method

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-7764c5c
+ * @license Angular v4.1.0-beta.0-7b005aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
