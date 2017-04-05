cacbc81fe532287323c66ea154706e8ebf9b2311
Matias Niemelä <matias@yearofmoo.com>
2017-04-05 06:08:57 +0000

    294a205 feat(animations): wip: introduce a wave of new animation features

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-f4f621a
+ * @license Angular v4.1.0-beta.0-294a205
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-f4f621a');
+const VERSION = new Version('4.1.0-beta.0-294a205');
 
 /**
  * @license
