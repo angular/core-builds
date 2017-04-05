6a43d42407c2eba632837e94e8cf2797d9a0eb61
Matias Niemelä <matias@yearofmoo.com>
2017-04-05 02:46:08 +0000

    f4f621a feat(animations): wip: introduce a wave of new animation features

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-c933b75
+ * @license Angular v4.1.0-beta.0-f4f621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -860,7 +860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-c933b75');
+var VERSION = new Version('4.1.0-beta.0-f4f621a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
