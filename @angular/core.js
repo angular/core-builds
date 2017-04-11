4d7c88656255d1810632a5a66e0d2b5262fa0015
Hans Larsen <hans@hansl.ca>
2017-04-11 23:03:26 +0000

    14a2d1a docs: add changelog for 4.0.2

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.1-6b79ab5
+ * @license Angular v4.0.2-14a2d1a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.1-6b79ab5');
+const VERSION = new Version('4.0.2-14a2d1a');
 
 /**
  * @license
