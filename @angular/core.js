2f31271425fa2833f75be5f989b821a70ce78318
Hans <hans@hansl.ca>
2017-04-11 18:32:00 +0000

    91b2e39 ci: add jasonaden to the router

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-a487563
+ * @license Angular v4.1.0-beta.0-91b2e39
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-a487563');
+const VERSION = new Version('4.1.0-beta.0-91b2e39');
 
 /**
  * @license
