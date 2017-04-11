b87fafcad1a49933fd614a03cc168566d27da09c
Hans <hans@hansl.ca>
2017-04-11 23:18:24 +0000

    04fb29b test(aio): add test for escaped newline in CodeComponent

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-3ced940
+ * @license Angular v4.1.0-beta.0-04fb29b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-3ced940');
+const VERSION = new Version('4.1.0-beta.0-04fb29b');
 
 /**
  * @license
