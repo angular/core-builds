5fda41141696a25c388dfb3fe3a1a2d6efaf3281
Igor Minar <iminar@google.com>
2017-04-12 19:43:33 +0000

    8659bd2 docs(aio): template -> ng-template (#15912)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-4a052cd
+ * @license Angular v4.1.0-beta.0-8659bd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -860,7 +860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-4a052cd');
+var VERSION = new Version('4.1.0-beta.0-8659bd2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-4a052cd
+ * @license Angular v4.1.0-beta.0-8659bd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
