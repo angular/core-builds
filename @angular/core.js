df19300e9a74e8d0e5ee74072d78047502ba74d6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:25:49 +0000

    909264f hr update

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-0614754
+ * @license Angular v4.1.0-beta.0-909264f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-0614754');
+const VERSION = new Version('4.1.0-beta.0-909264f');
 
 /**
  * @license
