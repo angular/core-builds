003f9cad65770be0219173cbccefe00984e63120
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:18:07 +0000

    0614754 docs(aio): final migration from old site

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-8f1359d');
+const VERSION = new Version('4.1.0-beta.0-0614754');
 
 /**
  * @license
