f1581f401aa8016560bf40d3ac4a5b2a197a3ea1
Hans <hans@hansl.ca>
2017-04-12 21:32:20 +0000

    f09fd6e test(aio): add sidenav tests and refactor related tests

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-909264f
+ * @license Angular v4.1.0-beta.0-f09fd6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-909264f');
+const VERSION = new Version('4.1.0-beta.0-f09fd6e');
 
 /**
  * @license
