546a32f59d44af3c51e8d9a602ed683970fbc2ea
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:12:47 +0000

    fdb3f26 refactor(compiler): cleanup (#15960)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-14b7dfa
+ * @license Angular v4.1.0-beta.1-fdb3f26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-14b7dfa');
+const VERSION = new Version('4.1.0-beta.1-fdb3f26');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-14b7dfa
+ * @license Angular v4.1.0-beta.1-fdb3f26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
