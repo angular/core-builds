0af4f86aa67e6cc3bddd222e503795a5489d28f9
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:10:47 +0000

    9394835 fix(platform-server): handle innerText (#15818)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-d263595
+ * @license Angular v4.1.0-beta.1-9394835
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-d263595');
+const VERSION = new Version('4.1.0-beta.1-9394835');
 
 /**
  * @license
