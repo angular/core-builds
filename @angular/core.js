c35d39b5139e62ab9263516c09b101e70061c239
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:18:14 +0000

    ce47d33 fix(compiler): ignore calls to unresolved symbols in metadata (#15970)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-2e47a0d
+ * @license Angular v4.1.0-beta.1-ce47d33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-2e47a0d');
+const VERSION = new Version('4.1.0-beta.1-ce47d33');
 
 /**
  * @license
