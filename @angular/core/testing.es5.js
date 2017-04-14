88618dcb508eea930f992af49cbbb713d1786b5a
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 18:36:05 +0000

    86396a4 Revert "fix(compiler): ignore calls to unresolved symbols in metadata (#15970)"

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-cb5a7ef
+ * @license Angular v4.1.0-beta.1-86396a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
