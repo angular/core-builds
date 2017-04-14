854c068995b1c08f29125b0bbbef6283bf692a4e
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 21:10:23 +0000

    c36ec9b fix(http): Update types for TypeScript nullability support

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-86396a4
+ * @license Angular v4.1.0-beta.1-c36ec9b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-86396a4');
+const VERSION = new Version('4.1.0-beta.1-c36ec9b');
 
 /**
  * @license
