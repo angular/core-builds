8790cf12f47d430234bc4b052d7cea4d3a9c15e0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 16:31:12 +0000

    eda2a7b ci(aio): do not fail if PWA score testing fails on staging

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-196203f
+ * @license Angular v4.1.0-beta.1-eda2a7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -860,7 +860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-196203f');
+var VERSION = new Version('4.1.0-beta.1-eda2a7b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
