445a5e4be31b0312a54ebf7b147fc7cd49523cc0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 12:53:51 +0000

    46b0c7a docs(aio): fix links to quickstart on homepage

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-b6a0098
+ * @license Angular v4.1.0-beta.1-46b0c7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
