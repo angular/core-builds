e372314a7f9bc8f0975f61e9c56c20fadddde5d0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 21:44:50 +0000

    7433da5 build(aio): give intellisense to the examples

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-eda2a7b
+ * @license Angular v4.1.0-beta.1-7433da5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
