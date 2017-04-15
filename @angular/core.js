fff8e58ee74e718737bdd9439a80058aeac1795d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:23 +0000

    c0b1bbe feat(aio): add log rotation in preview server

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8479e9e
+ * @license Angular v4.1.0-beta.1-c0b1bbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-8479e9e');
+const VERSION = new Version('4.1.0-beta.1-c0b1bbe');
 
 /**
  * @license
