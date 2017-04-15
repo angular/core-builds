b0c7f1b52fa6583b55d0e2d7e24b153406a3cab5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 12:49:37 +0000

    b6a0098 ci(aio): make `deploy-staging.sh` executable

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-7d986ae
+ * @license Angular v4.1.0-beta.1-b6a0098
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-7d986ae');
+const VERSION = new Version('4.1.0-beta.1-b6a0098');
 
 /**
  * @license
