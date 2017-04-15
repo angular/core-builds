e0e67a1360b69777367b8986cd9a0db574b2246c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:54:32 +0000

    7d986ae feat(aio): smaller typography, code header pad

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a2bdcc9
+ * @license Angular v4.1.0-beta.1-7d986ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-a2bdcc9');
+const VERSION = new Version('4.1.0-beta.1-7d986ae');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-a2bdcc9
+ * @license Angular v4.1.0-beta.1-7d986ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
