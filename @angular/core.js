1e87e8d1cb7e43945e1368df3b4104e1fe61528b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 09:00:10 +0000

    9a50844 feat(aio): force plunker to embedded-style on narrow (mobile) screens

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-3f307ff
+ * @license Angular v4.1.0-beta.1-9a50844
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-3f307ff');
+const VERSION = new Version('4.1.0-beta.1-9a50844');
 
 /**
  * @license
