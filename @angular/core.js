ed015cb9c2862f27bdbf986e154ea7759bd995d4
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 16:41:17 +0000

    5a88d2f fix(language-service): only use canonical symbols

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-7165eb1
+ * @license Angular v4.1.0-beta.1-5a88d2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-7165eb1');
+const VERSION = new Version('4.1.0-beta.1-5a88d2f');
 
 /**
  * @license
