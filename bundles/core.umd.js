4d3e54d9f87c1b628368c33dde0d8814047e4fc6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-17 07:08:57 +0000

    e20a722 feat(aio): when select node in sidebar close other expanded nodes

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-abecf3e
+ * @license Angular v4.1.0-beta.1-e20a722
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-abecf3e
+ * @license Angular v4.1.0-beta.1-e20a722
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -867,7 +867,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-abecf3e');
+var VERSION = new Version('4.1.0-beta.1-e20a722');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
