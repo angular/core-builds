6764cd5ceab7639693831777b1a2cf14dc0e63f6
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 21:52:34 +0000

    90814e4 feat(upgrade): fixes for allow setting the angularjs lib at runtime

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-38a7e0d
+ * @license Angular v4.1.0-beta.1-90814e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
