60f06099189c39ac3300707e4f8bb5b6238e6c78
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-17 13:02:33 +0000

    7165eb1 build(aio): add div with class around content

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-e20a722
+ * @license Angular v4.1.0-beta.1-7165eb1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-e20a722');
+const VERSION = new Version('4.1.0-beta.1-7165eb1');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-e20a722
+ * @license Angular v4.1.0-beta.1-7165eb1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
