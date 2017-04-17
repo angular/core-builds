68b43a18d9bfd765634fc78590c2924f82c8c201
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:31:24 +0000

    5b141fb fix(compiler): support `<ng-container>` whatever the namespace

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-2688842
+ * @license Angular v4.1.0-beta.1-5b141fb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
