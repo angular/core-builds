80fcb70fda4a5c53d09106ff685a21211fe223fb
Tobias Bosch <tbosch1009@gmail.com>
2017-04-18 22:37:25 +0000

    ecd0348 build(aio): extract the title from the content if necessary

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5293794
+ * @license Angular v4.1.0-beta.1-ecd0348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-5293794');
+const VERSION = new Version('4.1.0-beta.1-ecd0348');
 
 /**
  * @license
