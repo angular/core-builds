e1e3da2179ceb1c79ca72784d69de9533ecd1d46
Tobias Bosch <tbosch1009@gmail.com>
2017-04-18 00:59:42 +0000

    7dfa0b9 fix(aio): about page

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-90814e4
+ * @license Angular v4.1.0-beta.1-7dfa0b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-90814e4');
+const VERSION = new Version('4.1.0-beta.1-7dfa0b9');
 
 /**
  * @license
