747e4d2d889443166548bf0f7e8f4ad3a2a07160
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 15:12:12 +0000

    e07b7ea docs(aio): fix broken regions

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-73c7503
+ * @license Angular v4.1.0-beta.1-e07b7ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-73c7503
+ * @license Angular v4.1.0-beta.1-e07b7ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
