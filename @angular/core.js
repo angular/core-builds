8b7c8fa55d28e9fa76bb7c0a52e5f47a95a9fbe6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 08:37:04 +0000

    e951612 fix(aio): set the pageId to the file-not-found URL if the doc is not available

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-b668c2c
+ * @license Angular v4.1.0-beta.1-e951612
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-b668c2c');
+const VERSION = new Version('4.1.0-beta.1-e951612');
 
 /**
  * @license
