7012cb008c52431802e17d44ec5dfd8946a4323e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 07:53:23 +0000

    b668c2c build(aio): IDEs should only parse the `src` files

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-ecd0348
+ * @license Angular v4.1.0-beta.1-b668c2c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-ecd0348');
+const VERSION = new Version('4.1.0-beta.1-b668c2c');
 
 /**
  * @license
