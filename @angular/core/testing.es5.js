ce0acaec78d510554a2b052a1203add79db20d22
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 13:23:51 +0000

    73c7503 fix(aio): sidenav padding

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-79db6c3
+ * @license Angular v4.1.0-beta.1-73c7503
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
