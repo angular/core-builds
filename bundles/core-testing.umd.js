4057c7cba6436ddadf2d849d1b41c66bc845398c
Miško Hevery <misko@hevery.com>
2017-04-20 03:56:03 +0000

    bfd5f27 feat(aio): add e2e (#15976)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-e07b7ea
+ * @license Angular v4.1.0-beta.1-bfd5f27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-e07b7ea
+ * @license Angular v4.1.0-beta.1-bfd5f27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
