95366fae44ccec817f3e53251212f8a3e2a6351e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 09:55:01 +0000

    ab03852 fix(aio): accessibility items in search and home

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-f902581
+ * @license Angular v4.1.0-beta.1-ab03852
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-f902581');
+const VERSION = new Version('4.1.0-beta.1-ab03852');
 
 /**
  * @license
