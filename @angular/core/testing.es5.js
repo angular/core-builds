3f7ead9e32fc69463c90886f4eb65fcac5cc8fc3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 09:33:50 +0000

    f902581 fix(aio): handle clicks deep within anchor tags

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-745731e
+ * @license Angular v4.1.0-beta.1-f902581
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
