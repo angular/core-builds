cec64da0131e7560874561de1509b36a33614a17
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-21 15:47:14 +0000

    87ec8d1 docs(aio): remove doble title from setup

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-b0c5d21
+ * @license Angular v4.1.0-rc.0-87ec8d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
