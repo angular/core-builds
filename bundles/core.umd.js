f9e47bb3299ef24553ef8198ebe96c9352d63356
Miško Hevery <misko@hevery.com>
2017-04-21 03:28:44 +0000

    5461182 docs(aio): make glossary links work again (#16201)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-cbe95f3
+ * @license Angular v4.1.0-beta.1-5461182
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-cbe95f3
+ * @license Angular v4.1.0-beta.1-5461182
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -867,7 +867,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-cbe95f3');
+var VERSION = new Version('4.1.0-beta.1-5461182');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
