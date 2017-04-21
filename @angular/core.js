a58d7ecb9e2f3278db1d44637f1456c5ceba0e3c
Miško Hevery <misko@hevery.com>
2017-04-21 03:33:33 +0000

    9730351 build(aio): compute which examples to include when watching guide docs (#16213)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5461182
+ * @license Angular v4.1.0-beta.1-9730351
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-5461182');
+const VERSION = new Version('4.1.0-beta.1-9730351');
 
 /**
  * @license
