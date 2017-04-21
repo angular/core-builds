c53128f62f0298f5ce6483c7639004c73c585dfa
Miško Hevery <misko@hevery.com>
2017-04-21 20:55:42 +0000

    7520ddc feat(aio): dont set query params during search #16125 (#16217)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-9c1318d
+ * @license Angular v4.1.0-rc.0-7520ddc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-9c1318d');
+const VERSION = new Version('4.1.0-rc.0-7520ddc');
 
 /**
  * @license
