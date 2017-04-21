8b46bbea065a16f6a22df78678a4c2536ce02d0c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-21 15:29:04 +0000

    b0c5d21 fix(aio): header anchor placement

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-cbe5f78
+ * @license Angular v4.1.0-rc.0-b0c5d21
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
