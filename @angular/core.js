56828cba9d033f8850c9e890e12b6061b8da9b16
Miško Hevery <misko@hevery.com>
2017-04-21 03:26:06 +0000

    cbe95f3 feat(aio): add code-of-conduct to footer #16105 (#16216)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-134f542
+ * @license Angular v4.1.0-beta.1-cbe95f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-134f542');
+const VERSION = new Version('4.1.0-beta.1-cbe95f3');
 
 /**
  * @license
