2f4da6d6a472a6a8536d19650a15038114365605
Miško Hevery <misko@hevery.com>
2017-04-21 17:27:15 +0000

    062fc4a docs(aio): change hardcoded links to the old site (#16196)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-cf034f7
+ * @license Angular v4.1.0-rc.0-062fc4a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-cf034f7');
+const VERSION = new Version('4.1.0-rc.0-062fc4a');
 
 /**
  * @license
