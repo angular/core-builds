c7930a6477811a3f85b8ce2da32fca7b8c0cd3d1
Miško Hevery <misko@hevery.com>
2017-04-21 05:42:29 +0000

    d855e90 docs: add changelog for 4.1.0-rc.0

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9730351
+ * @license Angular v4.1.0-rc.0-d855e90
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-9730351');
+const VERSION = new Version('4.1.0-rc.0-d855e90');
 
 /**
  * @license
