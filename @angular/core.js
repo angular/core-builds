a7335cb81bc9993b8ccbc117dfa12ecdae735eb3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-23 22:06:46 +0000

    3cad5da build(aio): refactor dgeni packages

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-7a8bd99
+ * @license Angular v4.1.0-rc.0-3cad5da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-7a8bd99');
+const VERSION = new Version('4.1.0-rc.0-3cad5da');
 
 /**
  * @license
