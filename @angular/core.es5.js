d3324fa0f13269a764cc1a28f0ba23a1728372cd
Miško Hevery <misko@hevery.com>
2017-04-24 17:10:06 +0000

    2991221 docs(aio): remove duplicate `as` (#16265)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-d7719aa
+ * @license Angular v4.1.0-rc.0-2991221
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -860,7 +860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-rc.0-d7719aa');
+var VERSION = new Version('4.1.0-rc.0-2991221');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
