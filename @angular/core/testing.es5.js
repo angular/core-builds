91ad97753949f63c7d47180d24517235877a71e3
Miško Hevery <misko@hevery.com>
2017-04-24 18:44:41 +0000

    249cd8c docs(aio): remove _blank target (#16205)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-d1fb066
+ * @license Angular v4.1.0-rc.0-249cd8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
