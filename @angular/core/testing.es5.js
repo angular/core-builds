a287ee9745039a0ecb58c3f6c8920ba77f598d4b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 13:20:04 +0000

    4ac5096 docs(aio): revised "Docs" page

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-c3fa880
+ * @license Angular v4.1.0-rc.0-4ac5096
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
