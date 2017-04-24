b6c43909826a9797dac3d5e5dcc2ccb2ee326c0a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 14:01:47 +0000

    d7719aa perf(aio): improve unit test rebuild time

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-54e587a
+ * @license Angular v4.1.0-rc.0-d7719aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-54e587a');
+const VERSION = new Version('4.1.0-rc.0-d7719aa');
 
 /**
  * @license
