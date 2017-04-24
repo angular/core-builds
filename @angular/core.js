b7effab44569cfd3f0e0030ace48f7b139274d0a
Miško Hevery <misko@hevery.com>
2017-04-24 17:14:15 +0000

    feda017 docs(aio): Fix decorator JS feature explanation (#16214)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-2991221
+ * @license Angular v4.1.0-rc.0-feda017
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-2991221');
+const VERSION = new Version('4.1.0-rc.0-feda017');
 
 /**
  * @license
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-2991221
+ * @license Angular v4.1.0-rc.0-feda017
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-2991221
+ * @license Angular v4.1.0-rc.0-feda017
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -867,7 +867,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-rc.0-2991221');
+var VERSION = new Version('4.1.0-rc.0-feda017');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
