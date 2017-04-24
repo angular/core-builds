21bd2fd9ff362ec16b09ca986857038c19e3b719
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 13:37:29 +0000

    54e587a fix(aio): fix URL redirection for API pages

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-4ac5096
+ * @license Angular v4.1.0-rc.0-54e587a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-4ac5096');
+const VERSION = new Version('4.1.0-rc.0-54e587a');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-4ac5096
+ * @license Angular v4.1.0-rc.0-54e587a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
