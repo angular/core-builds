f7bc172a0346a237a5b123e2b373bd090a14a7c2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 07:02:29 +0000

    11b2f62 feat(aio): don't animate sidenav on launch.

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-e7c37d7
+ * @license Angular v4.1.0-rc.0-11b2f62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-e7c37d7');
+const VERSION = new Version('4.1.0-rc.0-11b2f62');
 
 /**
  * @license
