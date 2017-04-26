515799d75062227a26ded3430bed7081afaf2bdc
GitHub <noreply@github.com>
2017-04-26 19:29:09 +0000

    a28f5ee docs: update 4.1.0 changelog, remove aio

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c0c5013
+ * @license Angular v4.1.0-a28f5ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-c0c5013');
+const VERSION = new Version('4.1.0-a28f5ee');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-c0c5013
+ * @license Angular v4.1.0-a28f5ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
