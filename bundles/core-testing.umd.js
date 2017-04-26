796c61bd125e56794ef03844f2da8bcfba019342
Miško Hevery <misko@hevery.com>
2017-04-26 19:23:19 +0000

    b9723f9 docs: add changelog for 4.1.0

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-bb52e22
+ * @license Angular v4.1.0-b9723f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-bb52e22
+ * @license Angular v4.1.0-b9723f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
