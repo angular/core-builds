f3a64674bfe18774da24ef142dab00de8fcf04aa
Miško Hevery <misko@hevery.com>
2017-04-28 22:52:34 +0000

    a2b2afb feat(aio): add marketing class to AppComponent <aio-shell> on mkt page (#16395)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-b70b960
+ * @license Angular v4.1.0-a2b2afb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-b70b960');
+const VERSION = new Version('4.1.0-a2b2afb');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-b70b960
+ * @license Angular v4.1.0-a2b2afb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
