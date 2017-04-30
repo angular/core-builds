507c0fe282117050b4cf4a8cc3fb72ddecba5513
Miško Hevery <misko@hevery.com>
2017-04-30 16:28:24 +0000

    f74dafc docs(aio): Add numbers and steps to Tutorial navigation (#16421)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8d300ff
+ * @license Angular v4.1.0-f74dafc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-8d300ff');
+const VERSION = new Version('4.1.0-f74dafc');
 
 /**
  * @license
