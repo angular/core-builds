9574d5b14d0ee43c173d43400ed99ac898484943
Miško Hevery <misko@hevery.com>
2017-04-30 16:30:30 +0000

    de8de84 test(aio): confirm ga tracks user’s # fragment clicks within a page (#16441)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-f74dafc
+ * @license Angular v4.1.0-de8de84
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
