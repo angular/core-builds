3b73d0964503a4bcc84cf60868558eeb3761ef73
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:07:19 +0000

    aaa5628 fix(aio): correct position of sidenav icons

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-65a8f7f
+ * @license Angular v4.1.0-aaa5628
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -860,7 +860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-65a8f7f');
+var VERSION = new Version('4.1.0-aaa5628');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
