a203e02a9aa284346b59d8a746aebcfcd96c536f
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:08:21 +0000

    ff32c53 docs(aio): fix lots of broken links

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c8fd904
+ * @license Angular v4.1.0-ff32c53
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-c8fd904
+ * @license Angular v4.1.0-ff32c53
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
