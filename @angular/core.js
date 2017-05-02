b5a42879121418af355548eac4aff4ba57958bfb
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 23:08:52 +0000

    c946a92 refactor(compiler): simplify AOT tests

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-21c96a5
+ * @license Angular v4.1.0-c946a92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-21c96a5');
+const VERSION = new Version('4.1.0-c946a92');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-21c96a5
+ * @license Angular v4.1.0-c946a92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
