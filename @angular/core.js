5f096239032847a1bedeb7d650bb8290b6776096
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:06:50 +0000

    eb56ab3 fix(aio): remove top bar menu item focus jump

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-f29c6bb
+ * @license Angular v4.1.0-eb56ab3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-f29c6bb');
+const VERSION = new Version('4.1.0-eb56ab3');
 
 /**
  * @license
