11b03804881e4d72a1d9c4f7590d968523a3c365
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:49:34 +0000

    9ca2b4c feat(aio): set “avoid” class by convention (filename has “.avoid.”)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-8760bf7
+ * @license Angular v4.1.0-9ca2b4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8760bf7
+ * @license Angular v4.1.0-9ca2b4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-8760bf7
+ * @license Angular v4.1.0-9ca2b4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
