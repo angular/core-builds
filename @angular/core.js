10f997d5889d33f6c8c3845526d8e95026caa131
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:59:22 +0000

    464701a docs(aio): revert removal of tutorial examples

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-5b96fb9
+ * @license Angular v4.1.0-464701a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-5b96fb9');
+const VERSION = new Version('4.1.0-464701a');
 
 /**
  * @license
