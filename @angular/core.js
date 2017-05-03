382781e9752b0e9190e2591b8d54d75a93786978
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 08:38:57 +0000

    5ba8c14 docs(aio): remove unnecessary anchor links from styleguide

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ea9d8a6
+ * @license Angular v4.1.0-5ba8c14
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-ea9d8a6');
+const VERSION = new Version('4.1.0-5ba8c14');
 
 /**
  * @license
