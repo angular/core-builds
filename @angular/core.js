b053740cc4289620118f2eab88cebf7a18c05910
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:03:09 +0000

    f29c6bb docs(aio): fix missing title warnings

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-464701a');
+const VERSION = new Version('4.1.0-f29c6bb');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
