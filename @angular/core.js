8566e93738df378e57205e5d408ede56cef920aa
Martin Probst <martin@probst.io>
2017-05-04 18:12:18 +0000

    478d2ce fix(compiler): avoid a `...null` spread in extraction

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-83d9533
+ * @license Angular v4.1.0-478d2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-83d9533');
+const VERSION = new Version('4.1.0-478d2ce');
 
 /**
  * @license
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-83d9533
+ * @license Angular v4.1.0-478d2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-83d9533
+ * @license Angular v4.1.0-478d2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -867,7 +867,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-83d9533');
+var VERSION = new Version('4.1.0-478d2ce');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
