85124c88c61460e4b98ebbd9acdbc4b92d2a4810
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 11:41:18 +0000

    aec65de refactor(aio): simplify DocViewer

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-919ff12
+ * @license Angular v4.1.0-aec65de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-919ff12');
+const VERSION = new Version('4.1.0-aec65de');
 
 /**
  * @license
