fb8a646f6db7aacbd927becac4994b258b972493
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 22:38:06 +0000

    cd28df6 docs: add changelog for 4.2.0-beta.0

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1092292
+ * @license Angular v4.2.0-beta.0-cd28df6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-1092292');
+const VERSION = new Version('4.2.0-beta.0-cd28df6');
 
 /**
  * @license
