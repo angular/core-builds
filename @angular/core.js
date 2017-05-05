806c511e3f0fc49414975e42e1aff422533143ed
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 21:00:08 +0000

    42dc2c1 refactor(aio): remove redundant test

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-9da6340
+ * @license Angular v4.2.0-beta.0-42dc2c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-9da6340');
+const VERSION = new Version('4.2.0-beta.0-42dc2c1');
 
 /**
  * @license
