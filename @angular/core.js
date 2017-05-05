a416598d0106ba8b25d9b07a426d1a41ab85253a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:26:43 +0000

    2714644 docs(aio): fix broken links discovered May 4th

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-a8379a4
+ * @license Angular v4.2.0-beta.0-2714644
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-a8379a4');
+const VERSION = new Version('4.2.0-beta.0-2714644');
 
 /**
  * @license
