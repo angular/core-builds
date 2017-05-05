fc08841a236bd1120c3c957268cc53c2b5d84b2f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:05:05 +0000

    a8379a4 docs(aio): remove toc from all marketing pages

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.0-b7caa3e
+ * @license Angular v4.2.0-beta.0-a8379a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
