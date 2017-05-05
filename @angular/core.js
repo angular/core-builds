9c451b8c06b3d834f9c2cb0b65e9dc746eb97c74
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 20:40:44 +0000

    9da6340 fix(http): flatten metadata for @angular/http/testing

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-547c363
+ * @license Angular v4.2.0-beta.0-9da6340
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-547c363');
+const VERSION = new Version('4.2.0-beta.0-9da6340');
 
 /**
  * @license
