c5d1ea01b4b65be68036d6662c48ed21831e3f7b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:49:43 +0000

    b9ed97c fix(aio): do not display a Toc initially

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-799be9c
+ * @license Angular v4.2.0-beta.0-b9ed97c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-799be9c');
+const VERSION = new Version('4.2.0-beta.0-b9ed97c');
 
 /**
  * @license
