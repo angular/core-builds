877f3966d3d1dfec431f5b0c7053ef7bfda2c91d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-07 07:31:12 +0000

    6dc6777 docs(aio): move links from nav groups to items

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-5cf6426
+ * @license Angular v4.2.0-beta.0-6dc6777
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-5cf6426');
+const VERSION = new Version('4.2.0-beta.0-6dc6777');
 
 /**
  * @license
