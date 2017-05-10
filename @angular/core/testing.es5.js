ce489e36822831535f686c3fc4532881911b5a48
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-10 23:52:21 +0000

    d28a3f7 docs(router): Change CanDeactivate to CanLoad (#16237)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-162dffb
+ * @license Angular v4.2.0-beta.1-d28a3f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
