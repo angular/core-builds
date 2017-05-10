0c6a2439675f7cdf566a84d9f333faa25bf8429f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 12:10:02 +0000

    56833a6 docs(aio): more content fixes

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.0-efa2928
+ * @license Angular v4.2.0-beta.0-56833a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -860,7 +860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.0-efa2928');
+var VERSION = new Version('4.2.0-beta.0-56833a6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-efa2928
+ * @license Angular v4.2.0-beta.0-56833a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-efa2928');
+const VERSION = new Version('4.2.0-beta.0-56833a6');
 
 /**
  * @license
