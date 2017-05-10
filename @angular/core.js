dd32bab3b8a2fe0c4e80c1ce120a626bb34dd7bd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 07:07:01 +0000

    954c08e fix(aio): remove unwanted code styling from styleguide doc

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-a2dcb7b
+ * @license Angular v4.2.0-beta.0-954c08e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-a2dcb7b');
+const VERSION = new Version('4.2.0-beta.0-954c08e');
 
 /**
  * @license
