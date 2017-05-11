75125e2973cab67336aceac3bb518e0e72d0f6df
Igor Minar <iminar@google.com>
2017-05-11 22:20:19 +0000

    decb4cc ci(aio): create previews for all PRs that touch non-spec files in `aio/` or `packages/` (#16733)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-44c7ac0
+ * @license Angular v4.2.0-beta.1-decb4cc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
