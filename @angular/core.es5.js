029e3ad0de50e2e5fca89bf03d3f2d4e69ce6ecf
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-12 17:21:01 +0000

    b83fe6f fix(upgrade): Prevent renaming of $inject property (#16706)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-b016984
+ * @license Angular v4.2.0-beta.1-b83fe6f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -860,7 +860,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-b016984');
+var VERSION = new Version('4.2.0-beta.1-b83fe6f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
