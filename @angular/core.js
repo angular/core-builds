9e69967f81aae6e1cde38015d2e2f797d301045a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-12 13:47:22 +0000

    b016984 docs(aio): resize images

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-90bc5b2
+ * @license Angular v4.2.0-beta.1-b016984
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-90bc5b2');
+const VERSION = new Version('4.2.0-beta.1-b016984');
 
 /**
  * @license
