b7fea279f2e568628294a4eedb26c87fa7a7aba6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 09:03:28 +0000

    4419585 docs(aio): fix toh e2e test

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-39b92f7
+ * @license Angular v4.2.0-beta.1-4419585
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-39b92f7');
+const VERSION = new Version('4.2.0-beta.1-4419585');
 
 /**
  * @license
