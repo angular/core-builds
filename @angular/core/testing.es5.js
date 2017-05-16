1b7ab26d0a14399a37746b48f398a2d573f8c259
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-16 23:53:08 +0000

    d761059 fix(compiler-cli): allow '==' to compare nullable types (#16731)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-c805082
+ * @license Angular v4.2.0-beta.1-d761059
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-c805082
+ * @license Angular v4.2.0-beta.1-d761059
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-c805082
+ * @license Angular v4.2.0-beta.1-d761059
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -872,7 +872,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-c805082');
+var VERSION = new Version('4.2.0-beta.1-d761059');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
