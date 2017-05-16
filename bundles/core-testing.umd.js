1cc33fbd51e8af1623a09efa98aa7e8915529e5c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 10:02:02 +0000

    221f85a fix(aio): no white band below toolbar when mobile

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-a68ad6d
+ * @license Angular v4.2.0-beta.1-221f85a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-a68ad6d
+ * @license Angular v4.2.0-beta.1-221f85a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
