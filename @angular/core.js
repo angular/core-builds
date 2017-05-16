2df913197f27b3fc675e28b1ef7073054b13df39
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-16 17:38:45 +0000

    af99cf2 build(aio): compute `{@link ...} titles more effectively (#16813)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-221f85a
+ * @license Angular v4.2.0-beta.1-af99cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-221f85a');
+const VERSION = new Version('4.2.0-beta.1-af99cf2');
 
 /**
  * @license
