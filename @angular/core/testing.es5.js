bc67afc2a70b02432207376315b33553639f4dba
Igor Minar <iminar@google.com>
2017-05-17 04:23:42 +0000

    593fe5e build(aio): enable HTTP/2 on the preview server (#16826)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-54a6e4f
+ * @license Angular v4.2.0-beta.1-593fe5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-54a6e4f
+ * @license Angular v4.2.0-beta.1-593fe5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-54a6e4f
+ * @license Angular v4.2.0-beta.1-593fe5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -872,7 +872,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-54a6e4f');
+var VERSION = new Version('4.2.0-beta.1-593fe5e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
