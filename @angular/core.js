a0cf83a5accdbfa5aee804c4f8a1ce8843ba7a1f
Jason Aden <jasonaden@google.com>
2017-05-18 00:47:12 +0000

    7edc5e9 docs: add changelog for 4.1.3

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.2-6f039d7
+ * @license Angular v4.1.3-7edc5e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.2-6f039d7');
+const VERSION = new Version('4.1.3-7edc5e9');
 
 /**
  * @license
