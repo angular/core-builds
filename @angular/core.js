91f64ccb4a5bc7d0cc55afe40f118c3e8f42f8cd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-18 15:10:10 +0000

    6531806 build(aio): do not ignore `is-on-https` in lighthouse check

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-0e13a59
+ * @license Angular v4.2.0-beta.1-6531806
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-0e13a59');
+const VERSION = new Version('4.2.0-beta.1-6531806');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-0e13a59
+ * @license Angular v4.2.0-beta.1-6531806
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
