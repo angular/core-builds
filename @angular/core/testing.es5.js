62591384f33f715b5b1756ce7fdd26b60d06ce0a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-18 07:37:52 +0000

    71c3103 build(aio): reduce images weight

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-198edb3
+ * @license Angular v4.2.0-beta.1-71c3103
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
