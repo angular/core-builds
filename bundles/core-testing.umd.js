7b589ad0b4ae500c231fd0d076247f7723a5faf6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-18 14:03:09 +0000

    3361a7b build(aio): minor `update-preview-server.sh` improvements

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-9466908
+ * @license Angular v4.2.0-beta.1-3361a7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-9466908
+ * @license Angular v4.2.0-beta.1-3361a7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
