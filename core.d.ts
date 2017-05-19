73983520cb22ddd916b05f5decab13b0845c7987
Jason Aden <jasonaden@google.com>
2017-05-19 23:52:31 +0000

    3d382dc docs: add changelog for 4.2.0-rc.0

---
+++ core.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { AnimationMetadataType as ɵx, animate as ɵz, group as ɵba, keyframes as ɵbe, sequence as ɵbb, state as ɵbd, style as ɵbc, transition as ɵbf, trigger as ɵy } from './src/animation/dsl';
+export { _iterableDiffersFactory as ɵl, _keyValueDiffersFactory as ɵm, _localeFactory as ɵn } from './src/application_module';
+export { ApplicationRef_ as ɵf } from './src/application_ref';
+export { _appIdRandomProviderFactory as ɵg } from './src/application_tokens';
+export { defaultIterableDiffers as ɵh, defaultKeyValueDiffers as ɵi } from './src/change_detection/change_detection';
+export { DefaultIterableDifferFactory as ɵj } from './src/change_detection/differs/default_iterable_differ';
+export { DefaultKeyValueDifferFactory as ɵk } from './src/change_detection/differs/default_keyvalue_differ';
+export { ReflectiveInjector_ as ɵc } from './src/di/reflective_injector';
+export { ReflectiveDependency as ɵd, resolveReflectiveProviders as ɵe } from './src/di/reflective_provider';
+export { wtfEnabled as ɵo } from './src/profile/profile';
+export { createScope as ɵq, detectWTF as ɵp, endTimeRange as ɵt, leave as ɵr, startTimeRange as ɵs } from './src/profile/wtf_impl';
+export { PlatformReflectionCapabilities as ɵu } from './src/reflection/platform_reflection_capabilities';
+export { makeParamDecorator as ɵa, makePropDecorator as ɵb } from './src/util/decorators';
+export { _def as ɵv } from './src/view/provider';
+export { DebugContext as ɵw } from './src/view/types';
