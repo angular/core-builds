13abfcd0e09b6a4a314d7170c02c44dfead44ccf
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 18:04:00 +0000

    6cb93c1 fix(animations): only require one flushMicrotasks call when testing animations

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-eed67dd
+ * @license Angular v4.2.0-beta.1-6cb93c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -865,7 +865,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-eed67dd');
+var VERSION = new Version('4.2.0-beta.1-6cb93c1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
