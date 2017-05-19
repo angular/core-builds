b3f1d4cee05bc32498d536bba5971637a6fdc2c5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 09:06:16 +0000

    8a0e565 docs(aio): tidy up docs that use float clearing

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-a42322d
+ * @license Angular v4.2.0-beta.1-8a0e565
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -865,7 +865,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-a42322d');
+var VERSION = new Version('4.2.0-beta.1-8a0e565');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-a42322d
+ * @license Angular v4.2.0-beta.1-8a0e565
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-a42322d
+ * @license Angular v4.2.0-beta.1-8a0e565
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
