d6dce67566e37e9961b6afd57384998f3ef3323f
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 17:26:02 +0000

    98849de build: fix public-api:update gulp task (#16859)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-afd703d
+ * @license Angular v4.2.0-rc.0-98849de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -865,7 +865,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-afd703d');
+var VERSION = new Version('4.2.0-rc.0-98849de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
