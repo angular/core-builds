ea4eb1236aadf90780c840c7f76cc97bb7c8f134
GitHub <noreply@github.com>
2017-05-22 17:35:51 +0000

    11c10b2 fix(tsc-wrapped): resolve short-hand literal values to locals (#16873)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-98849de
+ * @license Angular v4.2.0-rc.0-11c10b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-98849de');
+const VERSION = new Version('4.2.0-rc.0-11c10b2');
 
 /**
  * @license
