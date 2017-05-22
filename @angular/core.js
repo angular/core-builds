3fa81dde46d6c645665e3f2a643a419296811970
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 18:40:23 +0000

    08dfe91 feat(compiler): add location note to extracted xliff2 files (#16791)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-85d4c4b
+ * @license Angular v4.2.0-rc.0-08dfe91
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -829,7 +829,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-85d4c4b');
+const VERSION = new Version('4.2.0-rc.0-08dfe91');
 
 /**
  * @license
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-85d4c4b
+ * @license Angular v4.2.0-rc.0-08dfe91
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-85d4c4b
+ * @license Angular v4.2.0-rc.0-08dfe91
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -872,7 +872,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-85d4c4b');
+var VERSION = new Version('4.2.0-rc.0-08dfe91');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
