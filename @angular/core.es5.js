feb4aebd96892603f1d9fb15e02c3f8e0657c09d
Chuck Jazdzewski <chuckj@google.com>
2017-05-24 20:29:59 +0000

    966eb2f aio: add h1 title to floating table of contents (#16959)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-b0c5018
+ * @license Angular v4.2.0-rc.0-966eb2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-b0c5018');
+var VERSION = new Version('4.2.0-rc.0-966eb2f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
