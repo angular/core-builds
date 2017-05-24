c7732785c471ae761d379bb07af796bd952635e0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-24 07:39:37 +0000

    9e17a14 fix(aio): align top-level nav-items consistently

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-eba59aa
+ * @license Angular v4.2.0-rc.0-9e17a14
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-eba59aa');
+var VERSION = new Version('4.2.0-rc.0-9e17a14');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
