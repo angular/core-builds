40ea77b52db7ed06d1c7cba3c00dc26465918355
Chuck Jazdzewski <chuckj@google.com>
2017-05-24 18:41:24 +0000

    b0c5018 build(aio): better error message for invalid links (#16993)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-19a509a
+ * @license Angular v4.2.0-rc.0-b0c5018
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-19a509a');
+var VERSION = new Version('4.2.0-rc.0-b0c5018');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
