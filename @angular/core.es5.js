ad2cb635b875d64c77db56bad73b931ab4171c5f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-24 08:20:20 +0000

    19a509a fix(aio): use a special version of the TopBar if the screen is narrow

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-9e17a14
+ * @license Angular v4.2.0-rc.0-19a509a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-9e17a14');
+var VERSION = new Version('4.2.0-rc.0-19a509a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
