88295665923ff8f8d1d2680df3aa074e9e62d01d
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 16:18:15 +0000

    c0981b8 Revert "fix(animations): use a lightweight renderer for non-animation components (#17003)"

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-6949510
+ * @license Angular v4.2.0-rc.0-c0981b8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-6949510');
+var VERSION = new Version('4.2.0-rc.0-c0981b8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-6949510
+ * @license Angular v4.2.0-rc.0-c0981b8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-6949510');
+const VERSION = new Version('4.2.0-rc.0-c0981b8');
 
 /**
  * @license
