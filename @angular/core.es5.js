ead2299610a516d71e69194087f3d5865ad20d36
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:00:22 +0000

    e10d763 fix(aio): correctly handle re-navigation to the empty path (#16997)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-573b861
+ * @license Angular v4.2.0-rc.0-e10d763
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-573b861');
+var VERSION = new Version('4.2.0-rc.0-e10d763');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
