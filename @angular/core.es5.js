bdf7e20345c0abb0e1337a1ff38913701ce2f9aa
Igor Minar <igor@angularjs.org>
2017-05-26 08:45:38 +0000

    9e7e178 build(aio): add google webmaster tools verification file

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-db3113b
+ * @license Angular v4.2.0-rc.0-9e7e178
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-db3113b');
+var VERSION = new Version('4.2.0-rc.0-9e7e178');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
