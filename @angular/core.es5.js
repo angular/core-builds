e449c7aa8aab542810e97280d412aa3453192204
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 01:18:56 +0000

    5aa53d7 refactor(compiler): change `bundle` to `flatModuleIndex` in the code (#17028)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-3ab86bd
+ * @license Angular v4.2.0-rc.0-5aa53d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-3ab86bd');
+var VERSION = new Version('4.2.0-rc.0-5aa53d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
