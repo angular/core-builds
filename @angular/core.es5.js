3cab9eb4d97a1c05f8ef1523bbb3f91f4f9d2838
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 19:46:22 +0000

    cd5bc64 docs(aio): update headers for toc (#16969)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-5f723cb
+ * @license Angular v4.2.0-rc.0-cd5bc64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-5f723cb');
+var VERSION = new Version('4.2.0-rc.0-cd5bc64');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
