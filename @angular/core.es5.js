84515dc57a82620228ca25e12c32bce1507858e5
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 22:54:02 +0000

    51d7a65 docs: add changelog for 4.2.0-rc.1

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-d108479
+ * @license Angular v4.2.0-rc.1-51d7a65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-d108479');
+var VERSION = new Version('4.2.0-rc.1-51d7a65');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
