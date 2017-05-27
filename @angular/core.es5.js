46d2765af28f0353c09f9c476b20bed8149533af
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-27 10:36:40 +0000

    b8b91d3 feat(aio): marketing design refresh

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-0018acf
+ * @license Angular v4.2.0-rc.1-b8b91d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-0018acf');
+var VERSION = new Version('4.2.0-rc.1-b8b91d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
