d65f80a5a882a220a570a5b680619b5b62229376
Victor Berchet <victor@suumit.com>
2017-05-30 20:38:07 +0000

    e513808 build: update router package.json placeholder (#17060)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-8ffa483
+ * @license Angular v4.2.0-rc.1-e513808
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-8ffa483');
+const VERSION = new Version('4.2.0-rc.1-e513808');
 
 /**
  * @license
