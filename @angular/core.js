27a9402d12d4c16a820ee0fa1f5cec62671d9bed
Victor Berchet <victor@suumit.com>
2017-05-30 22:33:40 +0000

    b056adc fix(aio): fix SideNav items alignment (#17106)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-0d894a1
+ * @license Angular v4.2.0-rc.1-b056adc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-0d894a1');
+const VERSION = new Version('4.2.0-rc.1-b056adc');
 
 /**
  * @license
