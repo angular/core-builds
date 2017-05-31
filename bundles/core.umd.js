d510f12a80ff67f99457f483aba1048071d73d86
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-31 06:04:10 +0000

    cb2cb7c build(aio): display "call" and "new" members of interfaces in API docs

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-dfbbbb5
+ * @license Angular v4.2.0-rc.1-cb2cb7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-dfbbbb5
+ * @license Angular v4.2.0-rc.1-cb2cb7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-dfbbbb5');
+var VERSION = new Version('4.2.0-rc.1-cb2cb7c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
