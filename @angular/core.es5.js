d1f7a6cad49e7f905b1bd2495aa1d447dd8cbb0b
GitHub <noreply@github.com>
2017-05-31 21:53:59 +0000

    1408357 docs: add details about the WeakMap polyfill for 4.x in the changelog (#17146)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-1f9a3dd
+ * @license Angular v4.2.0-rc.1-1408357
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-1f9a3dd');
+var VERSION = new Version('4.2.0-rc.1-1408357');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1f9a3dd
+ * @license Angular v4.2.0-rc.1-1408357
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-1f9a3dd');
+const VERSION = new Version('4.2.0-rc.1-1408357');
 
 /**
  * @license
