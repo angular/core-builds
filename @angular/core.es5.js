5c961bf996432cb0d1c2aa49ca9f187f36f4aad1
GitHub <noreply@github.com>
2017-05-31 11:05:27 +0000

    1f9a3dd fix(aio): fix typo in web worker check (#17133)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-11505fa');
+var VERSION = new Version('4.2.0-rc.1-1f9a3dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
