c80365039f74803a74d7aeaf2c6734bd994ee0cb
GitHub <noreply@github.com>
2017-06-01 20:43:46 +0000

    598fdad docs: update the release schedule (#17162)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-ad6a57e
+ * @license Angular v4.2.0-rc.1-598fdad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-ad6a57e');
+const VERSION = new Version('4.2.0-rc.1-598fdad');
 
 /**
  * @license
