6f7132cdf8a05fde7c8000a113e36bdc965931f1
Victor Berchet <victor@suumit.com>
2017-06-01 21:20:55 +0000

    068133e fix(animations): do not retain deleted nodes during an non-removal animation (#17153)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-598fdad
+ * @license Angular v4.2.0-rc.1-068133e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-598fdad');
+var VERSION = new Version('4.2.0-rc.1-068133e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-598fdad
+ * @license Angular v4.2.0-rc.1-068133e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-598fdad');
+const VERSION = new Version('4.2.0-rc.1-068133e');
 
 /**
  * @license
