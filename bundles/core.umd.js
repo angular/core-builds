7b9c95a6f2ca56a247dbb35fe81734a92abfe695
Victor Berchet <victor@suumit.com>
2017-06-01 22:19:29 +0000

    82ec02d docs: add changelog for 4.2.0-rc.2

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-7822187
+ * @license Angular v4.2.0-rc.2-82ec02d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-7822187
+ * @license Angular v4.2.0-rc.2-82ec02d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-7822187');
+var VERSION = new Version('4.2.0-rc.2-82ec02d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
