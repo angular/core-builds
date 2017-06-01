a1b21c7d18491a80887f6b2718db431de34411ec
Victor Berchet <victor@suumit.com>
2017-06-01 21:23:48 +0000

    7822187 fix(aio): fix scrolling to top (#17102)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-068133e
+ * @license Angular v4.2.0-rc.1-7822187
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-068133e');
+const VERSION = new Version('4.2.0-rc.1-7822187');
 
 /**
  * @license
