feeb0dcc1c85edb2f24b95f8bae9a89d391fd9db
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-02 21:49:47 +0000

    4c32cb9 fix(router): opening links in new window

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-d5ce086
+ * @license Angular v4.2.0-rc.2-4c32cb9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-d5ce086');
+var VERSION = new Version('4.2.0-rc.2-4c32cb9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-d5ce086
+ * @license Angular v4.2.0-rc.2-4c32cb9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-d5ce086');
+const VERSION = new Version('4.2.0-rc.2-4c32cb9');
 
 /**
  * @license
