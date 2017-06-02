6c4080d4ce4613cf2efe2c6574e42ca71ab1cd4b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-02 21:50:13 +0000

    35f714e fix(aio): place progress bar at the top

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-4c32cb9
+ * @license Angular v4.2.0-rc.2-35f714e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
