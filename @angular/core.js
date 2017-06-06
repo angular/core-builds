a9dd96527dbd4bc1d73ef67d28eb1a7f473a4320
Igor Minar <iminar@google.com>
2017-06-06 07:07:24 +0000

    784347f fix(aio): ensure all views can indicate the active node (#17194)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-42176a7
+ * @license Angular v4.2.0-rc.2-784347f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-42176a7');
+const VERSION = new Version('4.2.0-rc.2-784347f');
 
 /**
  * @license
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-42176a7
+ * @license Angular v4.2.0-rc.2-784347f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-42176a7
+ * @license Angular v4.2.0-rc.2-784347f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-42176a7');
+var VERSION = new Version('4.2.0-rc.2-784347f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
