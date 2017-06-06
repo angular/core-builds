787d678f0014aedae4dd43fb4ed3f08ccfdf704b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 20:29:14 +0000

    bfdd339 fix(aio): make `ScrollSpy` respond quicker to `scroll` events

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-b8979c8
+ * @license Angular v4.2.0-rc.2-bfdd339
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-b8979c8
+ * @license Angular v4.2.0-rc.2-bfdd339
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-b8979c8');
+var VERSION = new Version('4.2.0-rc.2-bfdd339');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
