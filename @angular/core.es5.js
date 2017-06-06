127f84ade6b14d9afbfadd9bce8fa243ea5a9ae2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 21:28:00 +0000

    269bbe0 fix(upgrade): call setInterval outside the Angular zone

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-bb2fc6b
+ * @license Angular v4.2.0-rc.2-269bbe0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-bb2fc6b');
+var VERSION = new Version('4.2.0-rc.2-269bbe0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-bb2fc6b
+ * @license Angular v4.2.0-rc.2-269bbe0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-bb2fc6b');
+const VERSION = new Version('4.2.0-rc.2-269bbe0');
 
 /**
  * @license
