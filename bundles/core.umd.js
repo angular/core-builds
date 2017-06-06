270b98513b78e8c44f2c6776e51da7022ae83b99
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 20:45:12 +0000

    bb2fc6b refactor(platform-browser): Remove setGlobalVar from DOM adapter

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-bfdd339
+ * @license Angular v4.2.0-rc.2-bb2fc6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-bfdd339
+ * @license Angular v4.2.0-rc.2-bb2fc6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-bfdd339');
+var VERSION = new Version('4.2.0-rc.2-bb2fc6b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
