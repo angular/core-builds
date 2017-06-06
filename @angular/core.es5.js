11e149d46456b58932e3dbaad4d0c0ed25d63f0e
Igor Minar <iminar@google.com>
2017-06-06 06:14:27 +0000

    76b4b80 fix(aio): fix search box overlap for small devices (#17075)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-7c78282
+ * @license Angular v4.2.0-rc.2-76b4b80
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-7c78282');
+var VERSION = new Version('4.2.0-rc.2-76b4b80');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
