cb7a3611c0183653dedef3acb1511ffc54e1f3f7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-07 14:00:36 +0000

    d837bfc fix(aio): fix scrolling to an element

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-4759975
+ * @license Angular v4.2.0-rc.2-d837bfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-4759975');
+var VERSION = new Version('4.2.0-rc.2-d837bfc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-4759975
+ * @license Angular v4.2.0-rc.2-d837bfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-4759975
+ * @license Angular v4.2.0-rc.2-d837bfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-4759975');
+var VERSION = new Version('4.2.0-rc.2-d837bfc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
