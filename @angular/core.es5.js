53c08b38e9bfc64ca637be0700167b4341982964
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 18:51:35 +0000

    3ddd28d fix(aio): update contrast on mediumgray color

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-fe6b39d
+ * @license Angular v4.2.0-rc.2-3ddd28d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-fe6b39d');
+var VERSION = new Version('4.2.0-rc.2-3ddd28d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-fe6b39d
+ * @license Angular v4.2.0-rc.2-3ddd28d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-fe6b39d');
+const VERSION = new Version('4.2.0-rc.2-3ddd28d');
 
 /**
  * @license
