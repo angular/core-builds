5abc1bfcef39672db3ee9c14d0470c41fa24c42d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 19:59:20 +0000

    a4a2901 fix(aio): remove `...` separator from search results

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-cba2b3c
+ * @license Angular v4.2.0-rc.2-a4a2901
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-cba2b3c');
+const VERSION = new Version('4.2.0-rc.2-a4a2901');
 
 /**
  * @license
