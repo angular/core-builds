6a266d37cbc448c3782ca07fe137493da406f31f
GitHub <noreply@github.com>
2017-06-08 22:47:19 +0000

    576d6d8 feat(aio): update the home page and docs landing page content (#17325)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-a222c3e
+ * @license Angular v4.2.0-rc.2-576d6d8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-a222c3e');
+var VERSION = new Version('4.2.0-rc.2-576d6d8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
