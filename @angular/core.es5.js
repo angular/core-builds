3bd56f697a3ae387fd24fdae12d1dc88a1b2dba2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:18:15 +0000

    a222c3e fix(forms): fix min and max validator behavior on non-numbers

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-41a765d
+ * @license Angular v4.2.0-rc.2-a222c3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-41a765d');
+var VERSION = new Version('4.2.0-rc.2-a222c3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
