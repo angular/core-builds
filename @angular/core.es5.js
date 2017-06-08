928f0301823c005360ce37cda3f8a56931a2451e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:17:39 +0000

    41a765d fix(aio): re-color code pretty print

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-f370fd3
+ * @license Angular v4.2.0-rc.2-41a765d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-f370fd3');
+var VERSION = new Version('4.2.0-rc.2-41a765d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-f370fd3
+ * @license Angular v4.2.0-rc.2-41a765d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-f370fd3');
+const VERSION = new Version('4.2.0-rc.2-41a765d');
 
 /**
  * @license
