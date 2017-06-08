4ae92fd7d93ba4248fb1075ca174aaf9ed412e9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 17:43:16 +0000

    e9886d7 fix(animations): evaluate substitutions on option param values

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-022835b
+ * @license Angular v4.2.0-rc.2-e9886d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-022835b');
+const VERSION = new Version('4.2.0-rc.2-e9886d7');
 
 /**
  * @license
