ad800479ba66a606d9eef69f537ab093a5880f26
Igor Minar <iminar@google.com>
2017-06-08 19:12:04 +0000

    67ef0f0 fix(aio): api list column overflow fix (#17300)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-e9886d7
+ * @license Angular v4.2.0-rc.2-67ef0f0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-e9886d7');
+var VERSION = new Version('4.2.0-rc.2-67ef0f0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
