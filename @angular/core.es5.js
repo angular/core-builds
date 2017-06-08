fc803688f74730feff9f435e7e8c44e5963fcbdf
Igor Minar <iminar@google.com>
2017-06-08 08:21:04 +0000

    33ba3e3 fix(aio): smoother transition from page with SideNav to homepage (#17312)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-8be2e4c
+ * @license Angular v4.2.0-rc.2-33ba3e3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-8be2e4c');
+var VERSION = new Version('4.2.0-rc.2-33ba3e3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
