a17e734af66c3e4e24f3e46b0513167aca5098e4
Igor Minar <iminar@google.com>
2017-06-08 19:20:30 +0000

    680128b fix(aio): alert, subsection and callout width fix (#17303)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-67ef0f0
+ * @license Angular v4.2.0-rc.2-680128b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-67ef0f0');
+const VERSION = new Version('4.2.0-rc.2-680128b');
 
 /**
  * @license
