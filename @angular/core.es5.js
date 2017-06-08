fdbf279598c00cca59fed0adb01ad9c120658859
Igor Minar <iminar@google.com>
2017-06-08 19:47:47 +0000

    391ed63 fix(aio): copy code button overlap fix on mobile (#17302)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-75a311f
+ * @license Angular v4.2.0-rc.2-391ed63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-75a311f');
+var VERSION = new Version('4.2.0-rc.2-391ed63');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
