4ab4818b5ddf6ede49bd98b06672aefbbc4b1288
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 20:13:50 +0000

    7467354 docs(aio): document the non-null assertion operator

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-391ed63
+ * @license Angular v4.2.0-rc.2-7467354
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-391ed63');
+var VERSION = new Version('4.2.0-rc.2-7467354');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
