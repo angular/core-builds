ce74e659c59f87b35146865b3321c18c94a62395
Igor Minar <iminar@google.com>
2017-06-09 20:57:59 +0000

    33df13a build(aio): update to 4.2 (#17369)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-3a99af2
+ * @license Angular v4.2.0-33df13a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-3a99af2');
+var VERSION = new Version('4.2.0-33df13a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
