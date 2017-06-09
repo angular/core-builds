aff7125a57395e12a0ae33ea2a08cb2b54051e9e
Igor Minar <iminar@google.com>
2017-06-09 21:53:43 +0000

    0020dad docs(aio): update examples to 4.2 (#17367)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-65d49d5
+ * @license Angular v4.2.0-0020dad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-65d49d5');
+const VERSION = new Version('4.2.0-0020dad');
 
 /**
  * @license
