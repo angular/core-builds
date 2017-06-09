bf73b326442e7739bc02dfef0ac1e9e0c38f225e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 13:55:47 +0000

    a0b30e5 docs(aio): remove example specific `.gitignore` files

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-e894f5c
+ * @license Angular v4.2.0-a0b30e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-e894f5c');
+var VERSION = new Version('4.2.0-a0b30e5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
