75faa4927165fee92b4c41e816e1afd33313f922
Igor Minar <iminar@google.com>
2017-06-09 20:50:35 +0000

    503fd1f docs(aio): fix image width on the homepage

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-ce18c68');
+var VERSION = new Version('4.2.0-503fd1f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
