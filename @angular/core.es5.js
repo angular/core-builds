553a47488a9e89114deed7bfacc3a31aaad0ed69
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 13:05:55 +0000

    e894f5c fix(aio): expand the main content width when there is no ToC

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-8524187
+ * @license Angular v4.2.0-e894f5c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-8524187');
+var VERSION = new Version('4.2.0-e894f5c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
