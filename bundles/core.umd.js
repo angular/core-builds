e3a47602f85dd686c60076e599c39e9aa34b49bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:20:07 +0000

    0268818 build(aio): update Google Analytics property id for the prod environment

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-234268e
+ * @license Angular v4.2.1-0268818
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.1-234268e
+ * @license Angular v4.2.1-0268818
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-234268e');
+var VERSION = new Version('4.2.1-0268818');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
