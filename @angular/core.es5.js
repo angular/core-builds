807fe5187ad4ce9dbd0d38da277ebd9ff14ce4d2
Igor Minar <igor@angularjs.org>
2017-06-12 23:49:53 +0000

    5e79449 build(aio): make deploy-to-firebase.sh executable

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-0268818
+ * @license Angular v4.2.2-5e79449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-0268818');
+var VERSION = new Version('4.2.2-5e79449');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-0268818
+ * @license Angular v4.2.2-5e79449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-0268818');
+const VERSION = new Version('4.2.2-5e79449');
 
 /**
  * @license
