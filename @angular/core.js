0593be4aa70ef62d2e39fd24aec99d4f58c99e12
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:31:00 +0000

    f840afb ci: disable platform-server integration test as it is currently broken

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-93d1b4e
+ * @license Angular v4.2.2-f840afb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-93d1b4e');
+const VERSION = new Version('4.2.2-f840afb');
 
 /**
  * @license
