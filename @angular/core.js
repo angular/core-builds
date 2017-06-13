98c547a3727f1ecd2519d357e24c572f0d47688a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:07:58 +0000

    93d1b4e refactor(compiler): remove duplicate code

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fa81c8e
+ * @license Angular v4.2.2-93d1b4e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-fa81c8e');
+const VERSION = new Version('4.2.2-93d1b4e');
 
 /**
  * @license
