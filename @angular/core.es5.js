7dd3cb20f0c9b37c7c3651abcb16d455ef1a0d0e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 06:41:24 +0000

    0658e1d docs(aio): rename Upgrade docs to cheatsheet

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-d2d8e5d
+ * @license Angular v4.2.2-0658e1d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-d2d8e5d');
+var VERSION = new Version('4.2.2-0658e1d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d2d8e5d
+ * @license Angular v4.2.2-0658e1d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-d2d8e5d
+ * @license Angular v4.2.2-0658e1d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-d2d8e5d');
+var VERSION = new Version('4.2.2-0658e1d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
