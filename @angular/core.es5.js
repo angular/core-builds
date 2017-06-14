21e76ea745279ef7891b900e220b658f01bb0c50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-14 17:10:18 +0000

    d378a29 ci(aio): fail the build if the PWA score is too low

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-2bdf2fe
+ * @license Angular v4.2.2-d378a29
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-2bdf2fe');
+var VERSION = new Version('4.2.2-d378a29');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
