8073d99f53fb318ff82e34a2755d5fc07cf7229f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 11:36:37 +0000

    38fc2a0 fix(aio): fix trackBy demo in template-syntax article

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-0c07f8c
+ * @license Angular v4.2.2-38fc2a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-0c07f8c');
+const VERSION = new Version('4.2.2-38fc2a0');
 
 /**
  * @license
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-0c07f8c
+ * @license Angular v4.2.2-38fc2a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-0c07f8c
+ * @license Angular v4.2.2-38fc2a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-0c07f8c');
+var VERSION = new Version('4.2.2-38fc2a0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
