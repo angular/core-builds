d0edab02abc69635f8108f83e64ea70b63417839
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:04:29 +0000

    06a0cf2 docs(aio): http guide shows how to import toPromise operator from rxjs

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-a8afa65
+ * @license Angular v4.2.2-06a0cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-a8afa65');
+const VERSION = new Version('4.2.2-06a0cf2');
 
 /**
  * @license
