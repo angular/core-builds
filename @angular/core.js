8a6ae5f1d66dae7cf1a8b98a11d176e4a79e4837
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:05:23 +0000

    01173b9 fix(aio): do not log messages in production

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-06a0cf2
+ * @license Angular v4.2.2-01173b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-06a0cf2');
+const VERSION = new Version('4.2.2-01173b9');
 
 /**
  * @license
