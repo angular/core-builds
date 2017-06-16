7ed9299ddcf368b00a9333d2bdaebfdc65dfedce
Hans Larsen <hans@hansl.ca>
2017-06-16 18:48:37 +0000

    623b9c6 test(compiler): fix typo "mamespace"

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-8a547ee
+ * @license Angular v4.2.3-623b9c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-8a547ee');
+var VERSION = new Version('4.2.3-623b9c6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-8a547ee
+ * @license Angular v4.2.3-623b9c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.3-8a547ee
+ * @license Angular v4.2.3-623b9c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-8a547ee');
+var VERSION = new Version('4.2.3-623b9c6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
