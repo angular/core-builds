57e8b3c8c2f03c89e50f547c1c48150618ef3a90
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:21:20 +0000

    2cc931e fix(aio): add missing redirect rule for `/styleguide`

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-ce00fa3
+ * @license Angular v4.2.2-2cc931e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-ce00fa3');
+var VERSION = new Version('4.2.2-2cc931e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
