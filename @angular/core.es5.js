bc9971dd6f4ff06f7a436dff6ba19f6b72b75cc4
Hans Larsen <hans@hansl.ca>
2017-06-16 21:13:43 +0000

    ab81c1c build(aio): update @angular/service-worker to 1.0.0-beta.16

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-71a8ef5');
+var VERSION = new Version('4.2.3-ab81c1c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-71a8ef5');
+var VERSION = new Version('4.2.3-ab81c1c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
