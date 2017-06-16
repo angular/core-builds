b17d69596dbaceeef03d760ea91cbda5c071848a
Hans Larsen <hans@hansl.ca>
2017-06-16 17:16:25 +0000

    8a547ee docs: add changelog for 4.2.3

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-b8c39cd
+ * @license Angular v4.2.3-8a547ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-b8c39cd');
+var VERSION = new Version('4.2.3-8a547ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b8c39cd
+ * @license Angular v4.2.3-8a547ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-b8c39cd
+ * @license Angular v4.2.3-8a547ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-b8c39cd');
+var VERSION = new Version('4.2.3-8a547ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
