d252e7f4a657501d7cabc868a20fe36f7d20f85c
Hans <hans@hansl.ca>
2017-06-16 19:34:24 +0000

    adc1b12 fix(aio): correctly redirect `/docs/ts/latest` and `/styleguide`

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-9315ab8
+ * @license Angular v4.2.2-adc1b12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- @angular/core/testing.js
+++ @angular/core/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-9315ab8
+ * @license Angular v4.2.2-adc1b12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
