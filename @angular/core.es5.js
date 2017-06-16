c1e270430eaf14d00f0aabc770fd71e97694c2bb
Hans Larsen <hans@hansl.ca>
2017-06-16 17:01:47 +0000

    b8c39cd fix(forms): temp roll back breaking change with min/max directives

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-9c7a84d
+ * @license Angular v4.2.2-b8c39cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-9c7a84d');
+var VERSION = new Version('4.2.2-b8c39cd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-9c7a84d
+ * @license Angular v4.2.2-b8c39cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-9c7a84d');
+const VERSION = new Version('4.2.2-b8c39cd');
 
 /**
  * @license
