8f2dea3c4a5f9fa1371274412e1e781b3fdf6dbd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:27:09 +0000

    956a7e9 docs(aio): re-add biography entry for devversion

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-2cc931e
+ * @license Angular v4.2.2-956a7e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-2cc931e');
+var VERSION = new Version('4.2.2-956a7e9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
