8b6038b8fd9b62f5170204cd227f0d63693dc9cf
Hans <hans@hansl.ca>
2017-06-16 19:34:22 +0000

    f58211d fix(aio): improve no-javascript screen

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-9315ab8
+ * @license Angular v4.2.2-f58211d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-9315ab8');
+var VERSION = new Version('4.2.2-f58211d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-9315ab8
+ * @license Angular v4.2.2-f58211d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-9315ab8
+ * @license Angular v4.2.2-f58211d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-9315ab8');
+var VERSION = new Version('4.2.2-f58211d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
