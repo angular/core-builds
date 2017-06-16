08ee71ef841effa636daed9c92cf1c4278613469
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:27:37 +0000

    9c7a84d docs(aio): re-add biography entry for devversion

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-dbc6a4c
+ * @license Angular v4.2.2-9c7a84d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-dbc6a4c');
+const VERSION = new Version('4.2.2-9c7a84d');
 
 /**
  * @license
