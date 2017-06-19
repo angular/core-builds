48c0d2e98467d20512d1b0c3775b4e69532625c5
Hans <hans@hansl.ca>
2017-06-19 22:18:42 +0000

    319ce18 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-aa92f3a
+ * @license Angular v4.2.2-319ce18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-aa92f3a');
+var VERSION = new Version('4.2.2-319ce18');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
