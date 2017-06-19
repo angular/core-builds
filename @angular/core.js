c12e6779b749e67547f68d6351ed35a97f1378d5
Hans Larsen <hans@hansl.ca>
2017-06-19 18:46:38 +0000

    afbb6bb docs: remove aio from changelog

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-4f37f86');
+const VERSION = new Version('4.2.2-afbb6bb');
 
 /**
  * @license
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-4f37f86');
+var VERSION = new Version('4.2.2-afbb6bb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
