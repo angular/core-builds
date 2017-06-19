58d8ec23cd62ed0fff2ae8bb23117cccdeb87c4c
Hans Larsen <hans@hansl.ca>
2017-06-19 22:12:44 +0000

    09512c7 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3607a48
+ * @license Angular v4.2.3-09512c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-3607a48');
+const VERSION = new Version('4.2.3-09512c7');
 
 /**
  * @license
