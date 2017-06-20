58fa297fac12482643040082a114d63dde9f3ef5
Hans <hans@hansl.ca>
2017-06-20 19:20:25 +0000

    4e6be15 fix(language-service): rollup `tslib` into the language service package

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-3e685f9
+ * @license Angular v4.2.2-4e6be15
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-3e685f9');
+var VERSION = new Version('4.2.2-4e6be15');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-3e685f9
+ * @license Angular v4.2.2-4e6be15
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
