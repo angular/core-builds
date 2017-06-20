82be0dff14d5ec6e09aae5539a96e35c1a7e74e7
Hans <hans@hansl.ca>
2017-06-20 21:54:23 +0000

    46ddf50 fix(tsc-wrapped): skip collecting metadata for default functions

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-8c89cc4
+ * @license Angular v4.2.2-46ddf50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
