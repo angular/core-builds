f2fcb3d66a352f2369ff1629e56f3fe3427b1fc9
Hans Larsen <hans@hansl.ca>
2017-06-20 20:33:50 +0000

    f997937 docs(aio): update resource listing

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-77860a0
+ * @license Angular v4.2.3-f997937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-77860a0');
+var VERSION = new Version('4.2.3-f997937');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
