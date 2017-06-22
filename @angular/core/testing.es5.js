c9f7533dc1777ef02713869548130487651484a9
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:40:28 +0000

    5dd5bfd docs(aio): remove "_" from private property name

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-1a947e4
+ * @license Angular v4.3.0-beta.0-5dd5bfd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
