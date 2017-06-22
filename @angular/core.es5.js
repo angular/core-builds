6784837cdd1d0a2667e0ddae5cc4b5835b6af672
Hans Larsen <hans@hansl.ca>
2017-06-22 00:43:39 +0000

    8a09015 docs: add changelog for 4.2.4

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-d203136
+ * @license Angular v4.2.4-8a09015
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-d203136');
+var VERSION = new Version('4.2.4-8a09015');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
