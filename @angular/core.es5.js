6208db1825b45dfd3c1d265edf102514f5465a9d
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:18:13 +0000

    133b5e6 docs(aio): added link to Russian Angular Courses

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-92bcfef
+ * @license Angular v4.3.0-beta.0-133b5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-92bcfef');
+var VERSION = new Version('4.3.0-beta.0-133b5e6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
