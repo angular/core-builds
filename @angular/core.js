498642e82ae3e0406de5ee177060165bad9a9692
Hans Larsen <hans@hansl.ca>
2017-06-22 00:48:18 +0000

    1fc0d05 docs: add changelog for 4.3.0-beta.0

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-02a38d3
+ * @license Angular v4.3.0-beta.0-1fc0d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-02a38d3');
+const VERSION = new Version('4.3.0-beta.0-1fc0d05');
 
 /**
  * @license
