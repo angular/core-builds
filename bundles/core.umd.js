26053adfdc336dd2daa2059ad78efc42f04f11aa
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:33:34 +0000

    400486c docs(aio): correct typos in Tour of Heroes `HeroSearchService`

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-133b5e6
+ * @license Angular v4.3.0-beta.0-400486c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-133b5e6
+ * @license Angular v4.3.0-beta.0-400486c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-133b5e6');
+var VERSION = new Version('4.3.0-beta.0-400486c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
