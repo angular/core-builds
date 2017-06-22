58a7def90ee72cec9cac7294192641bc2b8ee024
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:06:29 +0000

    8c129d7 docs: Fix some non-code being formatted as code in the router docs

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-5c576d3
+ * @license Angular v4.3.0-beta.0-8c129d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-5c576d3
+ * @license Angular v4.3.0-beta.0-8c129d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-5c576d3');
+var VERSION = new Version('4.3.0-beta.0-8c129d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
