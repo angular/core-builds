aecb8aaab0bf15c6d1593b131fbe5c80e9ec36f0
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:10:39 +0000

    68b64a2 docs: add missing colon in the Constants section of NAMING.md

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-8c129d7
+ * @license Angular v4.3.0-beta.0-68b64a2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-8c129d7');
+var VERSION = new Version('4.3.0-beta.0-68b64a2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-8c129d7
+ * @license Angular v4.3.0-beta.0-68b64a2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
