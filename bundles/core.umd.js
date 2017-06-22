651ec0d9bed97b2f3910ccea4f84eeecd332eaaa
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:48:26 +0000

    67ffbae docs(aio): fix promises link in toh-pt4

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-5dd5bfd
+ * @license Angular v4.3.0-beta.0-67ffbae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-5dd5bfd
+ * @license Angular v4.3.0-beta.0-67ffbae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-5dd5bfd');
+var VERSION = new Version('4.3.0-beta.0-67ffbae');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
