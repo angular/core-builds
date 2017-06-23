bcefc6f8bab6b8f9f79d93d7eb2fc7506d68793b
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 22:14:19 +0000

    81734cf refactor(compiler): add tracking comment for self-import detection

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-b754e60
+ * @license Angular v4.3.0-beta.0-81734cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-b754e60');
+var VERSION = new Version('4.3.0-beta.0-81734cf');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-b754e60
+ * @license Angular v4.3.0-beta.0-81734cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-b754e60
+ * @license Angular v4.3.0-beta.0-81734cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-b754e60');
+var VERSION = new Version('4.3.0-beta.0-81734cf');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
