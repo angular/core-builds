ba744b35760f765dac039326b3b518656b696fca
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 17:48:59 +0000

    9344c86 docs: fix “under to hood” typo in changelog.md

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.4-353de51
+ * @license Angular v4.2.4-9344c86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
