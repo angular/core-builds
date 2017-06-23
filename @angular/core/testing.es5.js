1646d1b0629c15e120e917e190f21fe48e8b02ac
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:49:38 +0000

    08ecfd8 build: No longer need to bazel build twice

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-3ce9d51
+ * @license Angular v4.3.0-beta.0-08ecfd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
