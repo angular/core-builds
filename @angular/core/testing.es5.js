f96bf3e28159f7edf20497511ff9e3d836dd6ba4
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:51:25 +0000

    97bb374 fix(aio): Typo in Setup Anatomy documentation page

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-f365a0f
+ * @license Angular v4.3.0-beta.0-97bb374
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-f365a0f
+ * @license Angular v4.3.0-beta.0-97bb374
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-f365a0f
+ * @license Angular v4.3.0-beta.0-97bb374
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-f365a0f');
+var VERSION = new Version('4.3.0-beta.0-97bb374');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
