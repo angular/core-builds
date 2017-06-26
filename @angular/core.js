196e4b16aa8fd1c958bba2a53a30adbb49593f2d
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:44:49 +0000

    f365a0f docs(aio): animations typos fixed

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-233044e
+ * @license Angular v4.3.0-beta.0-f365a0f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-233044e');
+const VERSION = new Version('4.3.0-beta.0-f365a0f');
 
 /**
  * @license
