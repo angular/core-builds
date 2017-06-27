568503e436e15738d116ec7b776c7396814a353b
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 23:47:07 +0000

    6c1a8da docs: correct grammar in CONTRIBUTING.md

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-d699c35
+ * @license Angular v4.3.0-beta.0-6c1a8da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
