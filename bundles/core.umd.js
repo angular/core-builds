0bb85bc8f22d916e3b4c797bcf7c61a9e42ff3b2
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 18:30:25 +0000

    d699c35 fix(animations): do not remove container nodes when children are queried by a parent animation

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-34f3832
+ * @license Angular v4.3.0-beta.0-d699c35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-34f3832
+ * @license Angular v4.3.0-beta.0-d699c35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-34f3832');
+var VERSION = new Version('4.3.0-beta.0-d699c35');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
