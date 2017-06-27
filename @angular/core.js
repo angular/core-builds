6b56f05907bd585cd107d462d28d187134ea277e
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 00:28:40 +0000

    68fc65d ci: add npm postinstall back to the lint step so node_modules doesn't get out of date

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-693f79e
+ * @license Angular v4.3.0-beta.0-68fc65d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-693f79e');
+const VERSION = new Version('4.3.0-beta.0-68fc65d');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-693f79e
+ * @license Angular v4.3.0-beta.0-68fc65d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
