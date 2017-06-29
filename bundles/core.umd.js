e8fbdcdb94e4081426b9ba44d4b69987d089e905
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:05:38 +0000

    96b1703 feat(aio): cross reference readme and docs styleguide

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-e47a77f
+ * @license Angular v4.3.0-beta.0-96b1703
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-e47a77f
+ * @license Angular v4.3.0-beta.0-96b1703
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-e47a77f');
+var VERSION = new Version('4.3.0-beta.0-96b1703');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
