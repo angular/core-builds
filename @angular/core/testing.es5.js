af6fa4d52632db24fd52a313cd59b3393402d985
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 22:08:45 +0000

    1ac9085 docs: add changelog for 4.3.0-beta.1

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-96b1703
+ * @license Angular v4.3.0-beta.1-1ac9085
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
