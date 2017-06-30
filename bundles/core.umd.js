a55052d96136dfb5cc0db0441c4d7939e8f4ccc2
Stephen Fluin <stephen.fluin@gmail.com>
2017-06-30 19:12:33 +0000

    69ec333 docs(aio): Add resources contributor info

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-67519eb
+ * @license Angular v4.3.0-beta.1-69ec333
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-67519eb
+ * @license Angular v4.3.0-beta.1-69ec333
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-67519eb');
+var VERSION = new Version('4.3.0-beta.1-69ec333');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
