0d2112d69b82df481b6cb26d9908ab551d7ceed2
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-30 12:32:11 +0000

    1a476b8 ci(aio): use correct URL for preview in PWA score test

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.5-27b87e8
+ * @license Angular v4.2.5-1a476b8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-27b87e8
+ * @license Angular v4.2.5-1a476b8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.5-27b87e8
+ * @license Angular v4.2.5-1a476b8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.5-27b87e8');
+var VERSION = new Version('4.2.5-1a476b8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
