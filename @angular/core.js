c94a81677ffae209a786a90519f3c87e748dc111
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:23:48 +0000

    b052ef5 fix(aio): do not include hidden content in window title

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-4459e0c
+ * @license Angular v4.3.0-beta.1-b052ef5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-4459e0c');
+const VERSION = new Version('4.3.0-beta.1-b052ef5');
 
 /**
  * @license
