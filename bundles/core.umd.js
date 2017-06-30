4b9d3b156b2229affd871a929d660f84f78663fa
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:14:55 +0000

    4459e0c fix(aio): restore visible sidenav keyboard focus; shift TOC up

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-40921bb
+ * @license Angular v4.3.0-beta.1-4459e0c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-40921bb
+ * @license Angular v4.3.0-beta.1-4459e0c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-40921bb');
+var VERSION = new Version('4.3.0-beta.1-4459e0c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
