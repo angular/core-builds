42e7bffce9c5264ff69c55da4404c1bbe022ac50
GitHub <noreply@github.com>
2017-07-01 18:01:35 +0000

    8a1a989 feat(router): add router-level events for GuardsCheck and Resolve (#17601)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-b479ed9
+ * @license Angular v4.3.0-beta.1-8a1a989
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-b479ed9');
+var VERSION = new Version('4.3.0-beta.1-8a1a989');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-b479ed9
+ * @license Angular v4.3.0-beta.1-8a1a989
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
