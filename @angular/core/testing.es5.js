6f30bfcdd2b4d7d5333a548b70c11830601ffedb
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:06:47 +0000

    10bfa8a ci(aio): avoid printing too large PWA report (and restore `print-logs.sh`)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.5-c460757
+ * @license Angular v4.2.5-10bfa8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
