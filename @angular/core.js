5a1df48c504b0ff3fb8845d0a7dfbd9dca229d1d
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 18:32:17 +0000

    ec58246 docs(aio): add DevExtreme to the UI Components section of resources.json (#17939)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-1bb2476
+ * @license Angular v4.3.0-beta.1-ec58246
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-1bb2476');
+const VERSION = new Version('4.3.0-beta.1-ec58246');
 
 /**
  * @license
