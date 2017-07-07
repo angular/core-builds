92b16449c0731d0f31f36fff1d051fd60ad91bad
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:04:16 +0000

    227dbbc fix(language-service): ignore hover of symbols not in the TypeScript program (#17969)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-ae27af7
+ * @license Angular v4.3.0-beta.1-227dbbc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
