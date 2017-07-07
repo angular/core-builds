2939469582e41a8ce50b4cdb2f8bd734e7c2848f
Jason Aden <jasonaden@angular.io>
2017-07-07 16:12:59 +0000

    11db3bd fix(upgrade): bring the dynamic version closer to the static one (#17971)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-3b2d2c4
+ * @license Angular v4.3.0-beta.1-11db3bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
