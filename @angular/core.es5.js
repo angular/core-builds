486138540b80f907f3a8d637b4c14deb0567943f
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:49:07 +0000

    9c3386b fix(compiler): remove i18n markup even if no translations (#17999)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-2ba3ada
+ * @license Angular v4.3.0-beta.1-9c3386b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-2ba3ada');
+var VERSION = new Version('4.3.0-beta.1-9c3386b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-2ba3ada
+ * @license Angular v4.3.0-beta.1-9c3386b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
