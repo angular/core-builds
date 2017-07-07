016fc4370d25fd75740b5b5ddac73c9331171d27
GitHub <noreply@github.com>
2017-07-07 00:26:28 +0000

    ae27af7 fix(router): encode URLs the same way AngularJS did (closer to spec) (#17890)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-c69fff1
+ * @license Angular v4.3.0-beta.1-ae27af7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
