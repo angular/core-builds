c033731a6087958e80f2d994664752b33737cb19
Jason Aden <jasonaden@google.com>
2017-07-08 05:20:32 +0000

    05a33d5 docs: add changelog for 4.2.6

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-09f1609
+ * @license Angular v4.3.0-beta.1-05a33d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-09f1609');
+var VERSION = new Version('4.3.0-beta.1-05a33d5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
