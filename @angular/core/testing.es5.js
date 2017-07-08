9ef765a53a2310a11b70ab6e5a1d6dd35cfa072f
GitHub <noreply@github.com>
2017-07-08 00:33:29 +0000

    09f1609 style(http): fix linting error in http/testing (#18002)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-c723d42
+ * @license Angular v4.3.0-beta.1-09f1609
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
