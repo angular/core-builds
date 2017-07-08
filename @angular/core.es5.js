eb12d077a23d61454189311063eda461a3b161e1
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-08 00:26:29 +0000

    c723d42 refactor: fix typos (#18000)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-9dd550f
+ * @license Angular v4.3.0-beta.1-c723d42
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-9dd550f');
+var VERSION = new Version('4.3.0-beta.1-c723d42');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
