1e70653f4183e5a84da45b37dffb2d7cc143b5fe
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:25:28 +0000

    3f29348 ci: add aio/content to aio group

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.6-09261e6
+ * @license Angular v4.2.6-3f29348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.6-09261e6');
+var VERSION = new Version('4.2.6-3f29348');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-09261e6
+ * @license Angular v4.2.6-3f29348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.6-09261e6
+ * @license Angular v4.2.6-3f29348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.6-09261e6');
+var VERSION = new Version('4.2.6-3f29348');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
