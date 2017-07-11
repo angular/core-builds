a2961ff5889a0797a6f532108ca0470c4ef3afaf
Igor Minar <iminar@google.com>
2017-07-11 19:34:24 +0000

    df06e8b fix(router): fix outdated homepage url in NPM package (#17899)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-db3bcc9
+ * @license Angular v4.3.0-rc.0-df06e8b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-db3bcc9');
+var VERSION = new Version('4.3.0-rc.0-df06e8b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
