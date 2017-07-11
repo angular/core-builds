12bed8bdc2ed2f6ed65aa94e921f089d0221efea
Igor Minar <iminar@google.com>
2017-07-11 19:14:23 +0000

    b6aad07 build: remove duplicate dependency on TypeScript

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4d45fe6
+ * @license Angular v4.3.0-rc.0-b6aad07
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-4d45fe6');
+const VERSION = new Version('4.3.0-rc.0-b6aad07');
 
 /**
  * @license
