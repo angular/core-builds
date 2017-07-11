f6248100074148e6d414062474deea36ce6a9e2c
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:33:26 +0000

    dbfd717 build(aio): add terms from heading to the search index

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.6-3f29348
+ * @license Angular v4.2.6-dbfd717
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
