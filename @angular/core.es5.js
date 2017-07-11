a58ef5faa8e17fafe8a43afb3e6cb66b0847e8cc
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:19:44 +0000

    09261e6 docs(aio): fix dangling links

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.6-c6aacf5
+ * @license Angular v4.2.6-09261e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.6-c6aacf5');
+var VERSION = new Version('4.2.6-09261e6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
