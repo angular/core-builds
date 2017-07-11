cf56dc180cff53427978a04aa36615e683e52707
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:24:39 +0000

    e8bbf86 build(aio): add terms from heading to the search index

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b30c5fc
+ * @license Angular v4.3.0-rc.0-e8bbf86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-b30c5fc
+ * @license Angular v4.3.0-rc.0-e8bbf86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-b30c5fc');
+var VERSION = new Version('4.3.0-rc.0-e8bbf86');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
