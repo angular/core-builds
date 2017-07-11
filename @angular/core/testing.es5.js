200864df243aae08c7909d5a4460c5828232235e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 10:36:55 +0000

    7c4ac68 build(aio): remove unused script and GitHub token

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-2055634
+ * @license Angular v4.3.0-rc.0-7c4ac68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
