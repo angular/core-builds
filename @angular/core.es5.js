f2aec64d0b7a4ea212d032b2778990624e03b264
GitHub <noreply@github.com>
2017-07-11 23:17:23 +0000

    edb8375 docs: adjust the release schedule with the 4.3.0-rc.0 release date

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-e110a80
+ * @license Angular v4.3.0-rc.0-edb8375
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-e110a80');
+var VERSION = new Version('4.3.0-rc.0-edb8375');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-e110a80
+ * @license Angular v4.3.0-rc.0-edb8375
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
