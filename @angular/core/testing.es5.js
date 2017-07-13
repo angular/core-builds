e70a24f10a763202a62eee05d907cae81a42e954
Igor Minar <iminar@google.com>
2017-07-13 22:28:41 +0000

    db96c96 fix(aio): add <hr> in between overloads

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-ce0f4f0
+ * @license Angular v4.3.0-rc.0-db96c96
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
