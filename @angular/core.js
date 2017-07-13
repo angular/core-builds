1a3b59b91532f2c18e30ee38c689a9af4e1801a7
Igor Minar <igor@angularjs.org>
2017-07-13 22:53:15 +0000

    72143e8 revert: ci(aio): exclude changes in aio/content folder

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-45ffe54
+ * @license Angular v4.3.0-rc.0-72143e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-45ffe54');
+const VERSION = new Version('4.3.0-rc.0-72143e8');
 
 /**
  * @license
