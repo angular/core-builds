2be547624c7b5d760cf4f303cc37ab2ac38db213
Igor Minar <iminar@google.com>
2017-07-13 19:35:00 +0000

    ffda3e4 build(aio): simplify the primary overload's anchor

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-a7ea008
+ * @license Angular v4.3.0-rc.0-ffda3e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-a7ea008
+ * @license Angular v4.3.0-rc.0-ffda3e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-a7ea008');
+var VERSION = new Version('4.3.0-rc.0-ffda3e4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
