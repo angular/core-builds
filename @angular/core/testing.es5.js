012e27b8bf60af1a269219daae9ca81fb05a6799
Igor Minar <iminar@google.com>
2017-07-13 22:42:03 +0000

    1bf7ba8 ci(aio): exclude changes in aio/content folder

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-db96c96
+ * @license Angular v4.3.0-rc.0-1bf7ba8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
