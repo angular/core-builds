1a6248bdcdb6b0ef50b52f153345215a94689dd6
Igor Minar <igor@angularjs.org>
2017-07-13 23:44:52 +0000

    3d85f72 revert: revert: ci(aio): exclude changes in aio/content folder

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-9f28e83
+ * @license Angular v4.3.0-rc.0-3d85f72
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-9f28e83
+ * @license Angular v4.3.0-rc.0-3d85f72
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-9f28e83');
+var VERSION = new Version('4.3.0-rc.0-3d85f72');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
