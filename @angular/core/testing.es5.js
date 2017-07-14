471a8f8dcd17cb97bf2414a7849a510c1c685784
Igor Minar <iminar@google.com>
2017-07-14 00:39:08 +0000

    dd04f09 feat(common): on-by-default XSRF support in HttpClient (#18108)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-3d85f72
+ * @license Angular v4.3.0-rc.0-dd04f09
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
