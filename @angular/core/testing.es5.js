62e57c5289f1c16f96b700ba2bc61e1456452bd0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:39:40 +0000

    a0b06be docs: add changelog for 4.3.0

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-e0fa727
+ * @license Angular v4.3.0-a0b06be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
