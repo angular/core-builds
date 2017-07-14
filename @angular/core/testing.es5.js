f3cb4404e364eef7ee1fcff9690639edbd89e89f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 21:28:40 +0000

    30e76fc feat(upgrade): support lazy-loading Angular module into AngularJS app

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-a0b06be
+ * @license Angular v4.3.0-30e76fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
