c999884448493106c190c6d0966a260d3beffc0f
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 14:53:00 +0000

    34834a9 fix(aio): remove unused `news.html` file

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-6e2ddcc
+ * @license Angular v4.3.0-34834a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-6e2ddcc');
+const VERSION = new Version('4.3.0-34834a9');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-6e2ddcc
+ * @license Angular v4.3.0-34834a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
