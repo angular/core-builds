99ff9d34a8eaaeec4d2bf3e754d86d1626612d3f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:39:09 +0000

    c6ad212 docs(aio): fix HttpClient's interceptor sample

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-8c81c62
+ * @license Angular v4.3.0-c6ad212
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-8c81c62
+ * @license Angular v4.3.0-c6ad212
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-8c81c62
+ * @license Angular v4.3.0-c6ad212
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-8c81c62');
+var VERSION = new Version('4.3.0-c6ad212');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
