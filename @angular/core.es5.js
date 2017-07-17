080ed5a45e392b5df104b98c9ade67a8acde8bce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:34:03 +0000

    8c81c62 docs: fix HttpClient logging's sample

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-47b3ecd
+ * @license Angular v4.3.0-8c81c62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-47b3ecd');
+var VERSION = new Version('4.3.0-8c81c62');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
