5c620c949abd6d15700706e876d41e4a4f0b2c70
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 23:53:59 +0000

    5344be5 fix(animations): make sure @.disabled works in non-animation components

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-23146c9
+ * @license Angular v4.3.0-5344be5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
