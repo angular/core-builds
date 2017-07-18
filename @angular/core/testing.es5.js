4970a134710806e10298fe62c1fe57d0753e332d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:39:00 +0000

    4ce29f3 fix(platform-server): provide XhrFactory for HttpClient

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-f19bd5f
+ * @license Angular v4.3.0-4ce29f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
