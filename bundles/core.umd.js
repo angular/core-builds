e51127811b8ae07979b68207b4db0a3e51ecac8a
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:30:46 +0000

    37c626e fix(aio): remove title attribute from `CodeExampleComponent`

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-f0a1109
+ * @license Angular v4.3.0-37c626e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-f0a1109
+ * @license Angular v4.3.0-37c626e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-f0a1109');
+var VERSION = new Version('4.3.0-37c626e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
