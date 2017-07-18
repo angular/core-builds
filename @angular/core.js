4939e5ce896bd3d8d33facaf7e1edd9d6bcf821d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:00:14 +0000

    95635c1 fix(compiler): ensure jit external id arguments names are unique

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e20cfe1
+ * @license Angular v4.3.0-95635c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-e20cfe1');
+const VERSION = new Version('4.3.0-95635c1');
 
 /**
  * @license
