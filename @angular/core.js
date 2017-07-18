86bbc257700e0e9b0ee8c68080584d713dc04dae
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:12:51 +0000

    f0a1109 fix(aio): add quote to module

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c39e7d1
+ * @license Angular v4.3.0-f0a1109
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-c39e7d1');
+const VERSION = new Version('4.3.0-f0a1109');
 
 /**
  * @license
