46c8737022abe66c39c339594c2dde4e410fe748
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 22:39:55 +0000

    d22f8f5 fix(animations): always camelcase style property names that contain auto styles

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-8076482
+ * @license Angular v4.3.0-d22f8f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
