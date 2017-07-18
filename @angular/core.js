7f133672b661ce68ec5ba6dd879e33bc3be8360c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:44:31 +0000

    f19bd5f docs(http): Make name of injected HttpTestingController consistent

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-17b7bc3
+ * @license Angular v4.3.0-f19bd5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-17b7bc3');
+const VERSION = new Version('4.3.0-f19bd5f');
 
 /**
  * @license
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-17b7bc3
+ * @license Angular v4.3.0-f19bd5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
