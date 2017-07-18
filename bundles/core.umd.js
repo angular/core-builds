71ca83599bf636b6521bdb07ff43a5c1c670d767
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:40:11 +0000

    5c62e30 fix(common): send flushed body as error instead of null

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-256bc8a
+ * @license Angular v4.3.0-5c62e30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-256bc8a
+ * @license Angular v4.3.0-5c62e30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-256bc8a');
+var VERSION = new Version('4.3.0-5c62e30');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
