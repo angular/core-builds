002571ba0d18974bcacba4cd5fedd7d8f6ff7658
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:08:55 +0000

    c39e7d1 fix(aio): do not wrap `<code-tabs>` tab labels

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-799bffb
+ * @license Angular v4.3.0-c39e7d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-799bffb');
+const VERSION = new Version('4.3.0-c39e7d1');
 
 /**
  * @license
