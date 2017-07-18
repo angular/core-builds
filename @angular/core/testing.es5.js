30fb3f73f9b5bf3e8a26b9b69f1dd651788d6a4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 22:40:10 +0000

    5db6f38 fix(animations): do not crash animations if a nested component fires CD during CD

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-d22f8f5
+ * @license Angular v4.3.0-5db6f38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
