f64cdcf41a47aa8ad950e22474cd534b49c09744
Miško Hevery <misko@hevery.com>
2017-07-20 23:27:27 +0000

    54e0244 Revert "docs: Remove unneeded file (#18106)"

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-8d28191
+ * @license Angular v5.0.0-beta.0-54e0244
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-8d28191');
+var VERSION = new Version('5.0.0-beta.0-54e0244');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-8d28191
+ * @license Angular v5.0.0-beta.0-54e0244
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-8d28191
+ * @license Angular v5.0.0-beta.0-54e0244
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-8d28191');
+var VERSION = new Version('5.0.0-beta.0-54e0244');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
