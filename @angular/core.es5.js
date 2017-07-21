df376a4a2e81d18687ffb95688afa8b8a55dbb1c
Miško Hevery <misko@hevery.com>
2017-07-21 18:34:19 +0000

    abee785 refactor(tsc-wrapped): update tsc-wrapped to pass strictNullCheck (#18160)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-e0a9625
+ * @license Angular v5.0.0-beta.0-abee785
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-e0a9625');
+var VERSION = new Version('5.0.0-beta.0-abee785');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-e0a9625
+ * @license Angular v5.0.0-beta.0-abee785
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-e0a9625
+ * @license Angular v5.0.0-beta.0-abee785
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-e0a9625');
+var VERSION = new Version('5.0.0-beta.0-abee785');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
