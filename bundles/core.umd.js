558c4809e256b4fdd871daa1310ee2bce9029370
Miško Hevery <misko@hevery.com>
2017-07-22 00:11:43 +0000

    6fc5940 build: Bazel builds ngfactories for packages/core (#18289)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-b7a6f52
+ * @license Angular v5.0.0-beta.0-6fc5940
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-b7a6f52
+ * @license Angular v5.0.0-beta.0-6fc5940
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1107,7 +1107,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-b7a6f52');
+var VERSION = new Version('5.0.0-beta.0-6fc5940');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
