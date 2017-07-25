892de81c6105fa5547bbdd69078153db83ea4f6a
Miško Hevery <misko@hevery.com>
2017-07-25 22:54:43 +0000

    91ab39c docs(aio) - Fixed link to the glossary dash-case term (#18311)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-6279e50
+ * @license Angular v5.0.0-beta.0-91ab39c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1075,7 +1075,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-6279e50');
+var VERSION = new Version('5.0.0-beta.0-91ab39c');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-6279e50
+ * @license Angular v5.0.0-beta.0-91ab39c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
