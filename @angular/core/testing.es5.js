a1a6cd9075078e258958b57d0974568d289cb2fe
Miško Hevery <misko@hevery.com>
2017-07-25 23:11:20 +0000

    b582e2b docs(aio): update examples to 4.3

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
