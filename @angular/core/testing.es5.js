954298e769d95ba33d83144e052bc79857fb75d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:53:25 +0000

    b6c4af6 feat(compiler-cli): automatically lower lambda expressions in metadata

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-381471d
+ * @license Angular v5.0.0-beta.1-b6c4af6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
