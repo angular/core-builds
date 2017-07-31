4894d4a1c07db9f439b7814d5fbe32f1f929838e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:46:50 +0000

    381471d fix(compiler): fix for element needing implicit parent placed in top-level ng-container

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-ebef5e6
+ * @license Angular v5.0.0-beta.1-381471d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
