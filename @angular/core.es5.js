7df5c94d457f5de38642236199ecfcd382317de5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 21:35:27 +0000

    cc2a4c4 build(aio): fix warning about missing `<h1>`

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-a11542a
+ * @license Angular v5.0.0-beta.1-cc2a4c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1075,7 +1075,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.1-a11542a');
+var VERSION = new Version('5.0.0-beta.1-cc2a4c4');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
