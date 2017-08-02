e6239e5d7402bb884d4b98b82630b49e000c565c
Victor Berchet <victor@suumit.com>
2017-08-02 23:11:11 +0000

    9adf40a build(aio): use cli 1.3.0-rc (#18290)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-89c6161
+ * @license Angular v5.0.0-beta.2-9adf40a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
