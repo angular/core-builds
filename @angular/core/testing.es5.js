81749fe00849c42d027e1251f50670b4f659648d
Victor Berchet <victor@suumit.com>
2017-08-02 22:50:28 +0000

    dbc5c58 docs: improve github labels by introducing "PR target" labels (#18436)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-baf4ce0
+ * @license Angular v4.3.3-dbc5c58
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
