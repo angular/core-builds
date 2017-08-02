20098eb6a91d710eabbfb262a0c04b6c40b319f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-02 21:00:40 +0000

    1e1af7f docs: changelog for 5.0.0-beta.2 release

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-49cd851
+ * @license Angular v5.0.0-beta.2-1e1af7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1075,7 +1075,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.1-49cd851');
+var VERSION = new Version('5.0.0-beta.2-1e1af7f');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-49cd851
+ * @license Angular v5.0.0-beta.2-1e1af7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.1-49cd851
+ * @license Angular v5.0.0-beta.2-1e1af7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1107,7 +1107,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.1-49cd851');
+var VERSION = new Version('5.0.0-beta.2-1e1af7f');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
