fa1775622aa7f2383ae098a09edb9271818c29d8
Victor Berchet <victor@suumit.com>
2017-08-02 23:57:05 +0000

    3923c30 docs(aio): fix missing anchor-open in i18n documentation (#18476)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-99017bf
+ * @license Angular v5.0.0-beta.2-3923c30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-99017bf
+ * @license Angular v5.0.0-beta.2-3923c30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1107,7 +1107,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.2-99017bf');
+var VERSION = new Version('5.0.0-beta.2-3923c30');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
