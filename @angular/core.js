e54ef6be26776e10c9127ca258510140e5cadcd2
Victor Berchet <victor@suumit.com>
2017-08-03 00:40:59 +0000

    e0021d4 refactor(platform-browser): compiler platform-browser packages cleanly (#18464)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-65d0888
+ * @license Angular v4.3.3-e0021d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-65d0888');
+const VERSION = new Version('4.3.3-e0021d4');
 
 /**
  * @license
