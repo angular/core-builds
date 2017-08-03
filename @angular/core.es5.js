165043cc6514a2a079f107970ae0c1d0372c5b08
Victor Berchet <victor@suumit.com>
2017-08-03 00:41:40 +0000

    ea07856 refactor(upgrade): compile upgrade cleanly with TypeScript 2.4 (#18461)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-7c47b62
+ * @license Angular v5.0.0-beta.2-ea07856
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1075,7 +1075,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.2-7c47b62');
+var VERSION = new Version('5.0.0-beta.2-ea07856');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-7c47b62
+ * @license Angular v5.0.0-beta.2-ea07856
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
