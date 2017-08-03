b922ac45744b10982d781eccdf8856656b231142
Victor Berchet <victor@suumit.com>
2017-08-03 18:24:09 +0000

    e7e7622 fix(compiler): ignore @import in multi-line css (#18452)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-4176832
+ * @license Angular v4.3.3-e7e7622
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
