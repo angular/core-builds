cebd4c0fde200a43548b6d98f8caa14a208ce6e0
Victor Berchet <victor@suumit.com>
2017-08-03 01:23:12 +0000

    4176832 refactor(router): compile router cleanly with TypeScript 2.4 (#18465)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-e0021d4
+ * @license Angular v4.3.3-4176832
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-e0021d4
+ * @license Angular v4.3.3-4176832
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.3-e0021d4');
+var VERSION = new Version('4.3.3-4176832');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
