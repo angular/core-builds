095edad6fdd28dda6989c73153a2d3be76ca275b
Victor Berchet <victor@suumit.com>
2017-08-04 16:45:10 +0000

    c421cca ci(aio): compute AIO deployment mode

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-c7b72aa
+ * @license Angular v4.3.3-c421cca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-c7b72aa');
+const VERSION = new Version('4.3.3-c421cca');
 
 /**
  * @license
