ab23b662856b73ab370f8a1351c11bb1a1436856
Victor Berchet <victor@suumit.com>
2017-08-07 18:04:25 +0000

    3212f8c docs(aio): typo & update the bio (#18559)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-c421cca
+ * @license Angular v4.3.3-3212f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
