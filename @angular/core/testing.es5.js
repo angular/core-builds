2b51ac250ab1ac15b4ade71a659b53519a87ca13
Victor Berchet <victor@suumit.com>
2017-08-09 23:10:19 +0000

    ff5c58b feat(forms): add default updateOn values for groups and arrays (#18536)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-dca50de
+ * @license Angular v5.0.0-beta.2-ff5c58b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
