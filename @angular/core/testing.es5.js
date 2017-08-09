bcdd631d94e4882709967e021a0113f1839155a7
Victor Berchet <victor@suumit.com>
2017-08-09 21:49:21 +0000

    b14250b test(aio): fix the `deploy-to-firebase` tests

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-2f9d8ff
+ * @license Angular v5.0.0-beta.2-b14250b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
