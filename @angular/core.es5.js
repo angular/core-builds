81c4e7331b4296e0fc82b7d106a77798d52ccd88
Victor Berchet <victor@suumit.com>
2017-08-09 21:57:57 +0000

    3efc88f test(aio): fix the `deploy-to-firebase` tests

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-71f5e78
+ * @license Angular v4.3.3-3efc88f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.3-71f5e78');
+var VERSION = new Version('4.3.3-3efc88f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-71f5e78
+ * @license Angular v4.3.3-3efc88f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-71f5e78');
+const VERSION = new Version('4.3.3-3efc88f');
 
 /**
  * @license
