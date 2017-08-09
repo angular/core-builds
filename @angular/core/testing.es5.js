942bb1d82aaae68db66ffb3b8108ed8a1ba4fbad
Victor Berchet <victor@suumit.com>
2017-08-09 21:56:22 +0000

    fd6ae57 fix(aio): add missing code snippet (#18547)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-b14250b
+ * @license Angular v5.0.0-beta.2-fd6ae57
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
