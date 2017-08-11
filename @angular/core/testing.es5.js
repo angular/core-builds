ca236ca1bf6ff1d6cd0aea5e01dc508c87b62681
Hans <hans@hansl.ca>
2017-08-11 20:50:55 +0000

    27d901a refactor(compiler-cli): cleanup API for transformer based ngc

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-cac130e
+ * @license Angular v5.0.0-beta.3-27d901a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-cac130e
+ * @license Angular v5.0.0-beta.3-27d901a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
