20765ff9a8368aea4655ea2f2324a8fe1c43d488
Hans <hans@hansl.ca>
2017-08-11 03:49:13 +0000

    679608d refactor(compiler-cli): use the transformer based compiler by default

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-06faac8
+ * @license Angular v5.0.0-beta.3-679608d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1078,7 +1078,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.3-06faac8');
+var VERSION = new Version('5.0.0-beta.3-679608d');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-06faac8
+ * @license Angular v5.0.0-beta.3-679608d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.3-06faac8
+ * @license Angular v5.0.0-beta.3-679608d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1110,7 +1110,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.3-06faac8');
+var VERSION = new Version('5.0.0-beta.3-679608d');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
