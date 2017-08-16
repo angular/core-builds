ef411ed7dfeab25c47e847426ee1e8eac5d6b1e3
Hans <hans@hansl.ca>
2017-08-16 00:09:32 +0000

    64b4be9 fix(compiler): Don't strip CSS source maps

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-0a73e8d
+ * @license Angular v5.0.0-beta.3-64b4be9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
