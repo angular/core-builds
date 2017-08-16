5fa2db39fa8a66a3d3e472348678950b1ad5afe5
Hans Larsen <hans@hansl.ca>
2017-08-16 18:04:46 +0000

    a1624f2 fix(forms): re-assigning options should not clear select

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-b2f4d53
+ * @license Angular v4.3.4-a1624f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
