f1fb6fe2a923298f14d842c485cc3f0fd3df140d
Hans <hans@hansl.ca>
2017-08-16 00:16:38 +0000

    697c6ed docs: remove TypeScript to JavaScript guide & sample

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-64b4be9
+ * @license Angular v5.0.0-beta.3-697c6ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
