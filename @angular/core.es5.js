4147fdde4dc7034cbd7942aa35d6f9f1d6228931
Hans <hans@hansl.ca>
2017-08-16 00:29:19 +0000

    c65f18a docs(forms): fix reactive-forms guide typo

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-697c6ed
+ * @license Angular v5.0.0-beta.3-c65f18a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -831,7 +831,7 @@ var ViewMetadata = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.3-697c6ed');
+var VERSION = new Version('5.0.0-beta.3-c65f18a');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-697c6ed
+ * @license Angular v5.0.0-beta.3-c65f18a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
