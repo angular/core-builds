32c49bb73a6a2ddac28b78ae2bd6fc107732e5e2
Miško Hevery <misko@hevery.com>
2017-08-21 22:22:24 +0000

    55d151a refactor(common): remove usage of deprecated Renderer (#17528)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-5a1b9a3
+ * @license Angular v5.0.0-beta.4-55d151a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -763,7 +763,7 @@ ViewEncapsulation[ViewEncapsulation.None] = "None";
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-5a1b9a3');
+const VERSION = new Version('5.0.0-beta.4-55d151a');
 
 /**
  * @fileoverview added by tsickle
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-5a1b9a3
+ * @license Angular v5.0.0-beta.4-55d151a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
