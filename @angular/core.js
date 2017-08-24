1cf6d4201d1ea31ddcf4a635b7fe777ac0d9f344
GitHub <noreply@github.com>
2017-08-24 14:56:57 +0000

    3db84c8 build: fix size limits for CLI to make CI green (#18849)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4
+ * @license Angular v5.0.0-beta.4-3db84c8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -763,7 +763,7 @@ ViewEncapsulation[ViewEncapsulation.None] = "None";
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4');
+const VERSION = new Version('5.0.0-beta.4-3db84c8');
 
 /**
  * @fileoverview added by tsickle
