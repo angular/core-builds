69354cbe4f98cfb57af81e68f51551bd488645a0
Miško Hevery <misko@hevery.com>
2017-08-28 20:20:43 +0000

    92dbaf3 ci(aio): Fix deploy to firebase scripts. (#18910)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-f0c681a
+ * @license Angular v5.0.0-beta.4-92dbaf3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -763,7 +763,7 @@ ViewEncapsulation[ViewEncapsulation.None] = "None";
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-f0c681a');
+const VERSION = new Version('5.0.0-beta.4-92dbaf3');
 
 /**
  * @fileoverview added by tsickle
--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-f0c681a
+ * @license Angular v5.0.0-beta.4-92dbaf3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
