d1f670847a8a6440737144a76b65da8535263c52
Miško Hevery <misko@hevery.com>
2017-08-29 00:12:07 +0000

    72c7b6e feat: deprecate @angular/http in favor of @angular/common/http (#18906)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-2e714f9
+ * @license Angular v5.0.0-beta.5-72c7b6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -763,7 +763,7 @@ ViewEncapsulation[ViewEncapsulation.None] = "None";
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-2e714f9');
+const VERSION = new Version('5.0.0-beta.5-72c7b6e');
 
 /**
  * @fileoverview added by tsickle
