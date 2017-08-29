e4b74632b94f5c45b00cd8e28f244cba6a5817ea
Miško Hevery <misko@hevery.com>
2017-08-29 00:15:27 +0000

    dfe2bad build: Add GitHub scripts for rebasing PRs (#18359)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.6-f09a266
+ * @license Angular v4.3.6-dfe2bad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
