27408b549dea58e87865eaa704518b625488a870
Jason Aden <jasonaden@google.com>
2017-08-30 00:46:57 +0000

    1b1d5f1 feat(common): accept object map for HttpClient headers & params (#18490)

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.5-65e26d7
+ * @license Angular v5.0.0-beta.5-1b1d5f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -799,7 +799,7 @@ ViewEncapsulation[ViewEncapsulation.None] = "None";
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.5-65e26d7');
+var VERSION = new Version('5.0.0-beta.5-1b1d5f1');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-65e26d7
+ * @license Angular v5.0.0-beta.5-1b1d5f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -763,7 +763,7 @@ ViewEncapsulation[ViewEncapsulation.None] = "None";
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-65e26d7');
+const VERSION = new Version('5.0.0-beta.5-1b1d5f1');
 
 /**
  * @fileoverview added by tsickle
