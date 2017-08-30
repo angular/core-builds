35e6eb8000555963b143b8399bfeb89ad4151df2
Jason Aden <jasonaden@google.com>
2017-08-30 00:43:08 +0000

    65e26d7 fix(common): fix improper packaging for @angular/common/http (#18613)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.5-fdd5010
+ * @license Angular v5.0.0-beta.5-65e26d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
