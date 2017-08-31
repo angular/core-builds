3f1a61dd6e53e19fa64db92736fd8533f86f1042
Jason Aden <jasonaden@google.com>
2017-08-31 19:23:46 +0000

    56b751e Revert "refactor(compiler-cli): expose ngtools api separately (#18952)"

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.5-57abe8d
+ * @license Angular v5.0.0-beta.5-56b751e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
