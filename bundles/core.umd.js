a78fcfa978bfdf198ea2026b10d2edd1ddbfea55
Miško Hevery <misko@hevery.com>
2017-09-01 19:09:58 +0000

    450a13d fix(upgrade): deprecate the dynamic version of `ngUpgrade`

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-56238fe
+ * @license Angular v5.0.0-beta.5-450a13d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.5-56238fe
+ * @license Angular v5.0.0-beta.5-450a13d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -906,7 +906,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.5-56238fe');
+var VERSION = new Version('5.0.0-beta.5-450a13d');
 
 /**
  * @fileoverview added by tsickle
--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-56238fe
+ * @license Angular v5.0.0-beta.5-450a13d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -816,7 +816,7 @@ ViewEncapsulation[ViewEncapsulation.None] = "None";
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-56238fe');
+const VERSION = new Version('5.0.0-beta.5-450a13d');
 
 /**
  * @fileoverview added by tsickle
