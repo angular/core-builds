e37e5c76c4799dc30cf34d929d817073fa8ea916
Miško Hevery <misko@hevery.com>
2017-09-02 05:19:42 +0000

    8036d05 release: cut the 4.4.0-RC.0 release

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-7d137d7
+ * @license Angular v4.4.0-RC.0-8036d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-7d137d7');
+const VERSION = new Version('4.4.0-RC.0-8036d05');
 
 /**
  * @license
