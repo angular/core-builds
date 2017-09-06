52aef8f0a96619c8349d936dec1cf45db94a84e9
Miško Hevery <misko@hevery.com>
2017-09-06 04:59:30 +0000

    d2707f1 build: cleanup duplicated code now in @bazel/typescript (#19004)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-15945c8
+ * @license Angular v5.0.0-beta.6-d2707f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.6-15945c8
+ * @license Angular v5.0.0-beta.6-d2707f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -869,7 +869,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-15945c8');
+var VERSION = new Version('5.0.0-beta.6-d2707f1');
 
 /**
  * @fileoverview added by tsickle
