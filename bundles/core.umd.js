82702e15acedac801eff66f70dc830adb4ae5804
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 23:51:22 +0000

    112e777 build(aio): update karma & systemjs config for HttpClient

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-a176cd6
+ * @license Angular v5.0.0-beta.6-112e777
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.6-a176cd6
+ * @license Angular v5.0.0-beta.6-112e777
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -869,7 +869,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-a176cd6');
+var VERSION = new Version('5.0.0-beta.6-112e777');
 
 /**
  * @fileoverview added by tsickle
