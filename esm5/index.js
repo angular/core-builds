5b460dfc022d2d3101b9110e965b6c10df66f734
Matias Niemelä <matias@yearofmoo.com>
2017-09-12 18:22:54 +0000

    9ab9437 build(aio): auto-link more code items

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-ca5aeba
+ * @license Angular v5.0.0-beta.6-9ab9437
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -694,7 +694,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-ca5aeba');
+var VERSION = new Version('5.0.0-beta.6-9ab9437');
 
 /**
  * @fileoverview added by tsickle
