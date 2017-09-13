d5cbf6bd0f8013469903f7d8177822a66f94e814
Matias Niemelä <matias@yearofmoo.com>
2017-09-13 21:32:02 +0000

    42c69d3 docs: add changelog for 5.0.0-beta.7

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { AnimationMetadataType as ɵx, animate as ɵz, group as ɵba, keyframes as ɵbe, sequence as ɵbb, state as ɵbd, style as ɵbc, transition as ɵbf, trigger as ɵy } from './src/animation/dsl';
+export { _iterableDiffersFactory as ɵm, _keyValueDiffersFactory as ɵn, _localeFactory as ɵo } from './src/application_module';
+export { ApplicationRef_ as ɵg, PlatformRef_ as ɵf } from './src/application_ref';
+export { _appIdRandomProviderFactory as ɵh } from './src/application_tokens';
+export { defaultIterableDiffers as ɵi, defaultKeyValueDiffers as ɵj } from './src/change_detection/change_detection';
+export { DefaultIterableDifferFactory as ɵk } from './src/change_detection/differs/default_iterable_differ';
+export { DefaultKeyValueDifferFactory as ɵl } from './src/change_detection/differs/default_keyvalue_differ';
+export { StaticInjector as ɵb } from './src/di/injector';
+export { ReflectiveInjector_ as ɵc } from './src/di/reflective_injector';
+export { ReflectiveDependency as ɵd, resolveReflectiveProviders as ɵe } from './src/di/reflective_provider';
+export { wtfEnabled as ɵp } from './src/profile/profile';
+export { createScope as ɵr, detectWTF as ɵq, endTimeRange as ɵu, leave as ɵs, startTimeRange as ɵt } from './src/profile/wtf_impl';
+export { makeParamDecorator as ɵa } from './src/util/decorators';
+export { _def as ɵv } from './src/view/provider';
+export { DebugContext as ɵw } from './src/view/types';
