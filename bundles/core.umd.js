93c05a551f01f9b9078455a3723408636d12268d
Matias Niemelä <matias@yearofmoo.com>
2017-09-14 03:08:00 +0000

    4695c69 refactor(compiler): remove all source-level traces to tsc-wrapped (#18966)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-42c69d3
+ * @license Angular v5.0.0-beta.7-4695c69
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-42c69d3
+ * @license Angular v5.0.0-beta.7-4695c69
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -733,7 +733,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-42c69d3');
+var VERSION = new Version('5.0.0-beta.7-4695c69');
 
 /**
  * @fileoverview added by tsickle
