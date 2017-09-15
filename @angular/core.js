1a9414914b2c5ed68f64d92c09dec074ed072aa0
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 18:06:45 +0000

    e8d1858 revert: build(platform-browser): fix typo

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-dd2d1be
+ * @license Angular v4.4.0-RC.0-e8d1858
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-dd2d1be');
+const VERSION = new Version('4.4.0-RC.0-e8d1858');
 
 /**
  * @license
