1a9af9d122c602fb4cef7a359a362fda637e0740
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:10:54 +0000

    0bafd03 revert: test(packaging): added test for source map correctness

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-e8d1858
+ * @license Angular v4.4.0-RC.0-0bafd03
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.0-RC.0-e8d1858
+ * @license Angular v4.4.0-RC.0-0bafd03
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.0-RC.0-e8d1858');
+var VERSION = new Version('4.4.0-RC.0-0bafd03');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
