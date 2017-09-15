2f92baecf472861a856e06f1bfc34f5e5d7c0906
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:36:55 +0000

    b20c5d2 fix(upgrade): remove code setting id attribute. (#19182)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.0-RC.0-0bafd03
+ * @license Angular v4.4.0-RC.0-b20c5d2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
