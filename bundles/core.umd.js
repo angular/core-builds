a08faf62dc751f5fb6d694e9d8833c926fff6681
Igor Minar <igor@angularjs.org>
2017-09-19 23:32:27 +0000

    9d2236a ci: update yarn to 1.0.2 (#19270)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e17128e
+ * @license Angular v5.0.0-beta.7-9d2236a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-e17128e
+ * @license Angular v5.0.0-beta.7-9d2236a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -733,7 +733,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-e17128e');
+var VERSION = new Version('5.0.0-beta.7-9d2236a');
 
 /**
  * @fileoverview added by tsickle
