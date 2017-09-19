4e724840e6ded04784e99df0ec1c7af5d3327f52
Igor Minar <igor@angularjs.org>
2017-09-19 21:32:45 +0000

    351331a ci: update yarn to 1.0.2 (#19270)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-49d122e
+ * @license Angular v4.4.2-351331a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.2-49d122e');
+const VERSION = new Version('4.4.2-351331a');
 
 /**
  * @license
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-49d122e
+ * @license Angular v4.4.2-351331a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.2-49d122e
+ * @license Angular v4.4.2-351331a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.2-49d122e');
+var VERSION = new Version('4.4.2-351331a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
