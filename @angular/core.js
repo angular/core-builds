d510dc39aa4e74f59b5567bad7c2cf0fe2e73149
Igor Minar <igor@angularjs.org>
2017-09-21 00:30:46 +0000

    ae52851 fix(tsc-wrapped): add metadata for `type` declarations (#19040)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-e2e8ba6
+ * @license Angular v4.4.3-ae52851
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-e2e8ba6');
+const VERSION = new Version('4.4.3-ae52851');
 
 /**
  * @license
