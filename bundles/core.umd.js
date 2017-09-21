02b86dffc72d657f5b704f037e7d7a5464569d58
Igor Minar <igor@angularjs.org>
2017-09-21 00:13:25 +0000

    4586fcc refactor(upgrade): use correct property name (#19180)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9e84719
+ * @license Angular v5.0.0-beta.7-4586fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-9e84719
+ * @license Angular v5.0.0-beta.7-4586fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-9e84719');
+var VERSION = new Version('5.0.0-beta.7-4586fcc');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9e84719
+ * @license Angular v5.0.0-beta.7-4586fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-9e84719');
+const VERSION = new Version('5.0.0-beta.7-4586fcc');
 
 /**
  * @fileoverview added by tsickle
