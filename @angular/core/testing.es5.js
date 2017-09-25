8b070b9f041d34e8976b42db25eacaedde646ba8
Victor Berchet <victor@suumit.com>
2017-09-25 21:00:36 +0000

    c7aa8a1 fix(compiler): skip &nbsp; when trimming / removing whitespaces (#19310)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.3-5c99b01
+ * @license Angular v4.4.3-c7aa8a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
