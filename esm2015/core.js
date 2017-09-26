8db744c4e369efb963820118c1cf3134884cb921
Victor Berchet <victor@suumit.com>
2017-09-26 17:04:01 +0000

    2cf52c1 test: Enable sass compiler for bazel integration test (#19357)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-27c6638
+ * @license Angular v5.0.0-beta.7-2cf52c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-27c6638');
+const VERSION = new Version('5.0.0-beta.7-2cf52c1');
 
 /**
  * @fileoverview added by tsickle
