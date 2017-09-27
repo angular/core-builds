eb3eb6aa0203039b44a9fd9b1943c2ed63274a61
Victor Berchet <victor@suumit.com>
2017-09-27 22:44:12 +0000

    334caf8 docs(aio): add copy about NgForOf (#18686)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-48c6ece
+ * @license Angular v5.0.0-beta.7-334caf8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-48c6ece');
+const VERSION = new Version('5.0.0-beta.7-334caf8');
 
 /**
  * @fileoverview added by tsickle
