44452d0e4e0bd7c6f7746d4e585c3ec60726766a
Victor Berchet <victor@suumit.com>
2017-09-27 17:31:30 +0000

    627f048 fix(compiler): implement i18n with new compiler

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-8a0e458
+ * @license Angular v5.0.0-beta.7-627f048
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-8a0e458
+ * @license Angular v5.0.0-beta.7-627f048
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-8a0e458');
+var VERSION = new Version('5.0.0-beta.7-627f048');
 
 /**
  * @fileoverview added by tsickle
