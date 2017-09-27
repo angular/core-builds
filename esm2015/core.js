3a54e1ac0e20017baafb755ab52cb4820f17c371
Victor Berchet <victor@suumit.com>
2017-09-27 21:01:46 +0000

    f7c7038 docs(aio): update metadata documentation to include expression lowering (#19203)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-627f048
+ * @license Angular v5.0.0-beta.7-f7c7038
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-627f048');
+const VERSION = new Version('5.0.0-beta.7-f7c7038');
 
 /**
  * @fileoverview added by tsickle
