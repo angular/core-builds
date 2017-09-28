4e9f1f02abd7bc9351799ca436e0cc2499ffa3c0
Victor Berchet <victor@suumit.com>
2017-09-28 18:26:38 +0000

    a9d32a3 fix(router): resolve and guards should be able to reject with null and undefined (#19418)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b3db3f8
+ * @license Angular v5.0.0-beta.7-a9d32a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-b3db3f8');
+const VERSION = new Version('5.0.0-beta.7-a9d32a3');
 
 /**
  * @fileoverview added by tsickle
