eeac89f5b353668b4931d7a23e20a3993648c21e
Victor Berchet <victor@suumit.com>
2017-09-28 16:51:44 +0000

    c1b029a fix(language-service): do not report errors for `OpaqueToken` (#19427)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-82e4923
+ * @license Angular v5.0.0-beta.7-c1b029a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-82e4923
+ * @license Angular v5.0.0-beta.7-c1b029a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-82e4923');
+var VERSION = new Version('5.0.0-beta.7-c1b029a');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-82e4923
+ * @license Angular v5.0.0-beta.7-c1b029a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-82e4923');
+const VERSION = new Version('5.0.0-beta.7-c1b029a');
 
 /**
  * @fileoverview added by tsickle
