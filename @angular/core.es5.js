b5b3c7f36785a2069db3348a9f87ff1109956a41
Victor Berchet <victor@suumit.com>
2017-09-28 19:44:15 +0000

    d489ad5 docs: add changelog for 4.4.4

--- @angular/core.es5.js
+++ @angular/core.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.3-7fc2dce
+ * @license Angular v4.4.4-d489ad5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -796,7 +796,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.3-7fc2dce');
+var VERSION = new Version('4.4.4-d489ad5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
