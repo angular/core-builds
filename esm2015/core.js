01c98c3d895ad56e0b96d9fa8482ff2c33ba3bf6
Victor Berchet <victor@suumit.com>
2017-09-28 19:42:59 +0000

    bed8ac7 docs: add changelog for 4.4.4

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-a9d32a3
+ * @license Angular v5.0.0-beta.7-bed8ac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-a9d32a3');
+const VERSION = new Version('5.0.0-beta.7-bed8ac7');
 
 /**
  * @fileoverview added by tsickle
