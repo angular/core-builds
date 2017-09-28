d619ae67c160774a741ffbe764c5efd61d5de0cf
Victor Berchet <victor@suumit.com>
2017-09-28 18:16:17 +0000

    b3db3f8 fix(compiler): do not consider a reference with members as a reference (#19454)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-ff5b050
+ * @license Angular v5.0.0-beta.7-b3db3f8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-ff5b050');
+var VERSION = new Version('5.0.0-beta.7-b3db3f8');
 
 /**
  * @fileoverview added by tsickle
