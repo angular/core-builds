fc7d71376f8beacabe7d76da7f01fd1c0fe9e5d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-09-29 16:47:48 +0000

    0038712 fix(compiler): add typings for `COMPILED`.

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-d442b68
+ * @license Angular v5.0.0-rc.0-0038712
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-d442b68');
+var VERSION = new Version('5.0.0-rc.0-0038712');
 
 /**
  * @fileoverview added by tsickle
