e6f03e3c040c50108c48bc6f05ce9119d93be466
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-02 15:45:32 +0000

    745b59f perf(compiler): only emit changed files for incremental compilation

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-0038712
+ * @license Angular v5.0.0-rc.0-745b59f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.0-0038712
+ * @license Angular v5.0.0-rc.0-745b59f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-0038712');
+var VERSION = new Version('5.0.0-rc.0-745b59f');
 
 /**
  * @fileoverview added by tsickle
