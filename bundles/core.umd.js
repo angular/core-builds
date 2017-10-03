207fdfc67b09569ddbe63fe0dac2c6089872f053
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 15:38:23 +0000

    9783c92 build(aio): fix various API rendering issues

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-7cecaf2
+ * @license Angular v5.0.0-rc.0-9783c92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.0-7cecaf2
+ * @license Angular v5.0.0-rc.0-9783c92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-7cecaf2');
+var VERSION = new Version('5.0.0-rc.0-9783c92');
 
 /**
  * @fileoverview added by tsickle
