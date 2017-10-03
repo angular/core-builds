4140a7d5586e1efc6156ad580f48aeae25e5ebfd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 17:15:58 +0000

    7c5ecb5 fix(compiler): also count generated files to determine whether to use single file emit

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-e3a4ece
+ * @license Angular v5.0.0-rc.0-7c5ecb5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.0-e3a4ece
+ * @license Angular v5.0.0-rc.0-7c5ecb5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-e3a4ece');
+var VERSION = new Version('5.0.0-rc.0-7c5ecb5');
 
 /**
  * @fileoverview added by tsickle
