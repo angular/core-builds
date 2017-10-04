ecb80196a5889c9c31a646f608fedc0ce19bc41c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 23:40:40 +0000

    01f7112 fix(compiler): don’t use `ng://` in AOT source maps, and never point to the original source file

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-c80e02b
+ * @license Angular v5.0.0-rc.0-01f7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.0-c80e02b');
+const VERSION = new Version('5.0.0-rc.0-01f7112');
 
 /**
  * @fileoverview added by tsickle
