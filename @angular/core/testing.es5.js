6875c5616e429744d66466af207533c5c77b303d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 20:04:17 +0000

    6b96b06 docs(aio): fix typo in preview server config file comment

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.4-df91fd0
+ * @license Angular v4.4.4-6b96b06
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
