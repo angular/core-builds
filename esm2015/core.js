74c54378d4eddba463112d6907219f662a1b8838
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:27:24 +0000

    c80e02b docs(aio): document Custom Async validators

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-fbc9537
+ * @license Angular v5.0.0-rc.0-c80e02b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.0-fbc9537');
+const VERSION = new Version('5.0.0-rc.0-c80e02b');
 
 /**
  * @fileoverview added by tsickle
