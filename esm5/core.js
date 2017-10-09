a04047dfd1ec59201973a2c836b5d873a505d13c
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 20:06:53 +0000

    6651541 fix(router): do not call `location.go` when skipping a navigation (#19463)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-c3a5269
+ * @license Angular v5.0.0-rc.1-6651541
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-c3a5269');
+var VERSION = new Version('5.0.0-rc.1-6651541');
 
 /**
  * @fileoverview added by tsickle
