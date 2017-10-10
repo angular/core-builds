a2ccac2aa24d1acd8b635b6792e7da248667fd07
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 21:09:36 +0000

    dfa0973 docs(aio): update animations for CLI (#19551)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-ac50bd6
+ * @license Angular v5.0.0-rc.1-dfa0973
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-ac50bd6');
+const VERSION = new Version('5.0.0-rc.1-dfa0973');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-ac50bd6
+ * @license Angular v5.0.0-rc.1-dfa0973
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-ac50bd6');
+var VERSION = new Version('5.0.0-rc.1-dfa0973');
 
 /**
  * @fileoverview added by tsickle
