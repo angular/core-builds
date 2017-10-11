1b1e6edeccf66435f4ce5ae6871afd62c0b01723
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 22:54:59 +0000

    28c29d5 build: fix rxjs version for aio (#19585)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-65ca7fd
+ * @license Angular v4.4.4-28c29d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-65ca7fd
+ * @license Angular v4.4.4-28c29d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.4-65ca7fd');
+var VERSION = new Version('4.4.4-28c29d5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
