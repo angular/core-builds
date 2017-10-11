e5fa0eb05df12209d138f70dee621b719ac59596
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 20:41:16 +0000

    3acf9c7 docs(aio): update banner ad (#19669)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-566606e
+ * @license Angular v5.0.0-rc.1-3acf9c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-566606e');
+var VERSION = new Version('5.0.0-rc.1-3acf9c7');
 
 /**
  * @fileoverview added by tsickle
