6693ca406906a8142e17789b8c173cb35d4d274f
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 00:29:21 +0000

    8b57130 build(aio): append information about links in and out of docs (#19583)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-b0befd7
+ * @license Angular v5.0.0-rc.1-8b57130
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.1-b0befd7
+ * @license Angular v5.0.0-rc.1-8b57130
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-b0befd7');
+var VERSION = new Version('5.0.0-rc.1-8b57130');
 
 /**
  * @fileoverview added by tsickle
