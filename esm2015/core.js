141952f12a9b06eddd9aa1817a975eb323878a36
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 19:19:10 +0000

    566606e ci: freeze yarn lockfile when installing dependencies (#19616)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-46992b4
+ * @license Angular v5.0.0-rc.1-566606e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-46992b4');
+const VERSION = new Version('5.0.0-rc.1-566606e');
 
 /**
  * @fileoverview added by tsickle
