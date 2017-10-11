fc5852526cca94648bed845e9916220efbddf4ad
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 18:17:47 +0000

    46992b4 Revert "fix(animations): ensure animateChild() works with all inner leave animations (#19006) (#19532)"

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-8b57130
+ * @license Angular v5.0.0-rc.1-46992b4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-8b57130');
+const VERSION = new Version('5.0.0-rc.1-46992b4');
 
 /**
  * @fileoverview added by tsickle
