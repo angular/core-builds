e092a13b0d132eaad28c9e1e692191da4a3d363a
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 21:23:37 +0000

    522ec9a docs: add changelog for 5.0.0-rc.2

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-94a925a
+ * @license Angular v5.0.0-rc.2-522ec9a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-94a925a');
+const VERSION = new Version('5.0.0-rc.2-522ec9a');
 
 /**
  * @fileoverview added by tsickle
