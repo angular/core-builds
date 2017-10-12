29d14b241bd9d7f305d2d33a07f3609e485f1e95
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 00:04:37 +0000

    a22121d perf(compiler): skip type check and emit in bazel in some cases. (#19646)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-3acf9c7
+ * @license Angular v5.0.0-rc.1-a22121d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.1-3acf9c7
+ * @license Angular v5.0.0-rc.1-a22121d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-3acf9c7');
+var VERSION = new Version('5.0.0-rc.1-a22121d');
 
 /**
  * @fileoverview added by tsickle
