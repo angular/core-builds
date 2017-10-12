502ceb319f0c7e5688ffe5c3a94acc0a0c064e1f
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 20:27:45 +0000

    912068e docs: add changelog for 4.4.5

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.4-f27f6e4
+ * @license Angular v4.4.5-912068e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-f27f6e4
+ * @license Angular v4.4.5-912068e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-f27f6e4
+ * @license Angular v4.4.5-912068e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -828,7 +828,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.4-f27f6e4');
+var VERSION = new Version('4.4.5-912068e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
