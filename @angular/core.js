e69a54d5e477723818d1cac9f0658c8e4578a602
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:59:31 +0000

    18f1b01 build(aio): consider `devDependencies` when overwriting dependencies of local Angular packages (#19687)

--- @angular/core.js
+++ @angular/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-591dcc2
+ * @license Angular v4.4.5-18f1b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-591dcc2');
+const VERSION = new Version('4.4.5-18f1b01');
 
 /**
  * @license
