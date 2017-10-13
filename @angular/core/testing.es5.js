9295c45cf5a00536e20600d87a6838d49537a123
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 22:50:27 +0000

    1be22df ci(aio): raise payload limit to accommodate the new search feature (#19704)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-f983a6c
+ * @license Angular v4.4.5-1be22df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
