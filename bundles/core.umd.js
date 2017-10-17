2fc0b296732d99b723767848fd53df727aefad2e
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 18:04:02 +0000

    15a8429 fix(service-worker): add missing annotation for SwPush (#19721)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-d035175
+ * @license Angular v5.0.0-rc.2-15a8429
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.2-d035175
+ * @license Angular v5.0.0-rc.2-15a8429
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-d035175');
+var VERSION = new Version('5.0.0-rc.2-15a8429');
 
 /**
  * @fileoverview added by tsickle
