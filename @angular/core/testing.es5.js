64f5bd1c665352b7df7c5b1d7b7bf49141c6fd2d
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 18:03:37 +0000

    ce0ac46 style: fix formatting of check-node-modules (#19720)

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-1be22df
+ * @license Angular v4.4.5-ce0ac46
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
