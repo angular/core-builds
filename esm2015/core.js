dfa7998e7490bdf622af91a6d4e93d4e92784227
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 22:18:04 +0000

    64b3619 build: don’t make BrowserStack required as it fails with “Access denied”

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-507290d
+ * @license Angular v5.0.0-rc.2-64b3619
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.2-507290d');
+const VERSION = new Version('5.0.0-rc.2-64b3619');
 
 /**
  * @fileoverview added by tsickle
