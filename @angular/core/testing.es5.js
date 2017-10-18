1f36773e6ed0020a34d726298cd1a2d272c23bfa
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 17:47:50 +0000

    3342a82 fix(aio): make tests less flaky (#19784) PR Close #19784

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-630c19f
+ * @license Angular v4.4.5-3342a82
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
