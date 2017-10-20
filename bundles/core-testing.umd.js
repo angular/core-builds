1669bfa7e0b40887fedcf7fdd3bc35edb77fbaea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:03:36 +0000

    820bb7b revert: ci: use chrome stable (#18307)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-14380ff
+ * @license Angular v5.0.0-rc.3-820bb7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-14380ff
+ * @license Angular v5.0.0-rc.3-820bb7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
