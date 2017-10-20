675d961e3214d45d838a1fd6e13c4d3cee0c7893
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:03:00 +0000

    14380ff build: add warning about changing ngtools_api2

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-441e01c
+ * @license Angular v5.0.0-rc.3-14380ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-441e01c
+ * @license Angular v5.0.0-rc.3-14380ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
