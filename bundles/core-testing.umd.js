237ad793adb123bc13981e6329d4963b14c769c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:40:48 +0000

    910735d build: fix `yarn install` command (`--freeze-lockfile` --> `--frozen-lockfile`)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-820bb7b');
+const VERSION = new Version('5.0.0-rc.3-910735d');
 
 /**
  * @fileoverview added by tsickle
