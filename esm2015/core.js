8dab399ad3c85a952ec82e597bf09d7687ab065d
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 01:13:48 +0000

    bde5701 docs: add changelog for 5.0.0-rc.4

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-6bed189
+ * @license Angular v5.0.0-rc.4-bde5701
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-6bed189');
+const VERSION = new Version('5.0.0-rc.4-bde5701');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-6bed189
+ * @license Angular v5.0.0-rc.4-bde5701
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.3-6bed189');
+var VERSION = new Version('5.0.0-rc.4-bde5701');
 
 /**
  * @fileoverview added by tsickle
