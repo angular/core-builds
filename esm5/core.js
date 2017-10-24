bf44253cd4893720d88047303d6e1d62ab33147d
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 23:43:54 +0000

    6cc042e fix(compiler-cli): produce correct paths for windows output (#19915)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-f1108fe
+ * @license Angular v5.0.0-rc.5-6cc042e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.5-f1108fe');
+var VERSION = new Version('5.0.0-rc.5-6cc042e');
 
 /**
  * @fileoverview added by tsickle
