c4133cb80ff93518aa8dbca03308d06b7c1a7cce
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 07:10:20 +0000

    b51d57d docs: add changelog for 5.0.0-rc.5

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.4-a0ae120
+ * @license Angular v5.0.0-rc.5-b51d57d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.4-a0ae120
+ * @license Angular v5.0.0-rc.5-b51d57d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.4-a0ae120');
+var VERSION = new Version('5.0.0-rc.5-b51d57d');
 
 /**
  * @fileoverview added by tsickle
