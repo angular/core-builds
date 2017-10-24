f6c884a385409395de35efbbae4003f3c3ea6796
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 05:18:29 +0000

    a82f863 fix(compiler-cli): report all diagnostic error messages (#19886)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.4-bde5701
+ * @license Angular v5.0.0-rc.4-a82f863
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.4-bde5701');
+const VERSION = new Version('5.0.0-rc.4-a82f863');
 
 /**
  * @fileoverview added by tsickle
