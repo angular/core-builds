5eebc4977fc0c954c77354eb07d56a99a04fa3b6
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 05:17:29 +0000

    a0ae120 fix(compiler-cli): report all diagnostic error messages (#19886)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.4-bde5701
+ * @license Angular v5.0.0-rc.4-a0ae120
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.4-bde5701');
+const VERSION = new Version('5.0.0-rc.4-a0ae120');
 
 /**
  * @fileoverview added by tsickle
