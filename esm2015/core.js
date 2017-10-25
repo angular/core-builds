0ca3239fe20d13cc54068a233f6ab9e4186f9210
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 20:10:41 +0000

    680bcf7 build: update to rxjs@5.5.2 (#19931)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-ef08330
+ * @license Angular v5.0.0-rc.5-680bcf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.5-ef08330');
+const VERSION = new Version('5.0.0-rc.5-680bcf7');
 
 /**
  * @fileoverview added by tsickle
