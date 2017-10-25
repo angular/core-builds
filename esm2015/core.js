3ce442192481166b698b6927bc2b3d02ae02e333
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 18:51:15 +0000

    ef08330 fix(compiler): automatically set `emitDecoratorMetadata` when `"annotationsAs": "static fields”` (#19927)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-6cc042e
+ * @license Angular v5.0.0-rc.5-ef08330
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.5-6cc042e');
+const VERSION = new Version('5.0.0-rc.5-ef08330');
 
 /**
  * @fileoverview added by tsickle
