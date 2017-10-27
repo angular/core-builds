76d04fa4e7872801fcc269f9ec2aff79f3f13a68
Matias Niemelä <matias@yearofmoo.com>
2017-10-27 02:14:09 +0000

    31797d3 fix(compiler): make watch mode work on windows (#19953)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-eca822b
+ * @license Angular v5.0.0-rc.6-31797d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.6-eca822b');
+const VERSION = new Version('5.0.0-rc.6-31797d3');
 
 /**
  * @fileoverview added by tsickle
