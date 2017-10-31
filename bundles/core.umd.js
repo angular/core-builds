fde3df01a1b3cae115ae82e4a5d9069e919c1deb
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:44:54 +0000

    7553ce9 docs: add changelog for 5.0.0-rc.9

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-3bdbb18
+ * @license Angular v5.0.0-rc.9-7553ce9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.8-3bdbb18
+ * @license Angular v5.0.0-rc.9-7553ce9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-3bdbb18');
+var VERSION = new Version('5.0.0-rc.9-7553ce9');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-3bdbb18
+ * @license Angular v5.0.0-rc.9-7553ce9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-3bdbb18');
+const VERSION = new Version('5.0.0-rc.9-7553ce9');
 
 /**
  * @fileoverview added by tsickle
