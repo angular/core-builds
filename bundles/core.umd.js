952d0a3bbb4ea4da3fd3986ad05c38c55ce4d388
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:44:10 +0000

    951bd33 build: add `release` as a valid commit message subject (#19955)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-420852e
+ * @license Angular v5.0.0-rc.8-951bd33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.8-420852e
+ * @license Angular v5.0.0-rc.8-951bd33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-420852e');
+var VERSION = new Version('5.0.0-rc.8-951bd33');
 
 /**
  * @fileoverview added by tsickle
