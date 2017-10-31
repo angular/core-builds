574b2e3c632aec26f0c8ab38af3befdd0281aef0
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 06:06:27 +0000

    5225fdb build(aio): add i18n boilerplate type (#20004)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-f5b7f2b
+ * @license Angular v5.0.0-rc.9-5225fdb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.9-f5b7f2b');
+const VERSION = new Version('5.0.0-rc.9-5225fdb');
 
 /**
  * @fileoverview added by tsickle
