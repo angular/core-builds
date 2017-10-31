294ba86d96427bd46c9a0c878958fde88153f2ca
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:25:42 +0000

    54480f7 fix(compiler): report errors properly in G3 in certain conditions (#20041)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-bf57df3
+ * @license Angular v5.0.0-rc.8-54480f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-bf57df3');
+const VERSION = new Version('5.0.0-rc.8-54480f7');
 
 /**
  * @fileoverview added by tsickle
