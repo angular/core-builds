9b28229d2e3f81f1046167db98ecfd437f6c55f6
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:32:57 +0000

    13f8648 fix: add missing globals from each rollup configuration (#20028)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-54480f7
+ * @license Angular v5.0.0-rc.8-13f8648
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-54480f7');
+const VERSION = new Version('5.0.0-rc.8-13f8648');
 
 /**
  * @fileoverview added by tsickle
