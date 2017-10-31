ab2ac25b8951d6ed19042800ebe7a780f389ed81
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 00:51:59 +0000

    04eb80c fix(compiler): always use relative paths to refer to generated code

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-308fc8e');
+var VERSION = new Version('5.0.0-rc.8-04eb80c');
 
 /**
  * @fileoverview added by tsickle
