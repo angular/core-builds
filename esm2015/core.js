e560eb8c1a16865c6b50a04800eaf237b4cb1991
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:53:00 +0000

    280dada docs(aio): fix typo in attribute-directives

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-c30eff8
+ * @license Angular v5.0.0-280dada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-c30eff8');
+const VERSION = new Version('5.0.0-280dada');
 
 /**
  * @fileoverview added by tsickle
