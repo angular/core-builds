3b7dc8c4527d1e3fd8ad49b7dd91c7efa46fa269
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:36:52 +0000

    a363be6 docs(aio): upgrade to v5 and cli 1.5

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e1fb65c
+ * @license Angular v5.0.0-a363be6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-e1fb65c
+ * @license Angular v5.0.0-a363be6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-e1fb65c');
+var VERSION = new Version('5.0.0-a363be6');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e1fb65c
+ * @license Angular v5.0.0-a363be6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-e1fb65c');
+const VERSION = new Version('5.0.0-a363be6');
 
 /**
  * @fileoverview added by tsickle
