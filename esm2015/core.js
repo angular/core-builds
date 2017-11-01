e24f46ea8adfd484f24307db4e020a5e0b0645f0
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:45:48 +0000

    ca129ba docs(aio): upgrade to v5 and cli 1.5

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-171ae15
+ * @license Angular v5.0.0-ca129ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-171ae15');
+const VERSION = new Version('5.0.0-ca129ba');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-171ae15
+ * @license Angular v5.0.0-ca129ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-171ae15');
+var VERSION = new Version('5.0.0-ca129ba');
 
 /**
  * @fileoverview added by tsickle
