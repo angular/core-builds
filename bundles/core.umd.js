8359195971a2ce772e4c9ef17616de43d48e0109
Victor Berchet <victor@suumit.com>
2017-11-02 22:12:17 +0000

    e846953 docs: AOT guide for CLI #19510 (#19818)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-18197bd
+ * @license Angular v5.0.0-e846953
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-18197bd
+ * @license Angular v5.0.0-e846953
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-18197bd');
+var VERSION = new Version('5.0.0-e846953');
 
 /**
  * @fileoverview added by tsickle
