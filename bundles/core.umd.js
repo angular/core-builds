b6712f59b7e15339631c4039c1ca9e6cd5d351c7
Victor Berchet <victor@suumit.com>
2017-11-02 23:23:09 +0000

    65d57a0 build(aio): avoid building before running the local PWA tests

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-9975486
+ * @license Angular v5.1.0-beta.0-65d57a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-9975486
+ * @license Angular v5.1.0-beta.0-65d57a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-9975486');
+var VERSION = new Version('5.1.0-beta.0-65d57a0');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-9975486
+ * @license Angular v5.1.0-beta.0-65d57a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-9975486');
+const VERSION = new Version('5.1.0-beta.0-65d57a0');
 
 /**
  * @fileoverview added by tsickle
