d13743cf71fc6f00395ead4d378abc6d85aee9c7
Igor Minar <igor@angular.io>
2017-11-02 00:26:04 +0000

    3db7112 docs(aio): fix linting for universal (#20086)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-2ea76cc
+ * @license Angular v5.1.0-beta.0-3db7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-2ea76cc');
+var VERSION = new Version('5.1.0-beta.0-3db7112');
 
 /**
  * @fileoverview added by tsickle
