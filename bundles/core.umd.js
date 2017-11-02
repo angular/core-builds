a457ca3a4328d471408998faa69fa023577d9d8b
Victor Berchet <victor@suumit.com>
2017-11-02 22:39:51 +0000

    067e926 build(aio): do not fail the initial doc-gen when running docs-watch

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-8639995
+ * @license Angular v5.0.0-067e926
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-8639995
+ * @license Angular v5.0.0-067e926
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-8639995');
+var VERSION = new Version('5.0.0-067e926');
 
 /**
  * @fileoverview added by tsickle
