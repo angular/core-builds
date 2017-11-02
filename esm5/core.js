3c6e33ba584c64db0fc4abea7a7ed99f35485bd8
Victor Berchet <victor@suumit.com>
2017-11-02 23:16:09 +0000

    37a740b build: remove local yarn (#19981)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-067e926
+ * @license Angular v5.0.0-37a740b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-067e926');
+var VERSION = new Version('5.0.0-37a740b');
 
 /**
  * @fileoverview added by tsickle
