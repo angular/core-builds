3dc034b289a10b94fed0f47dfb8e9bd380ceaa25
Victor Berchet <victor@suumit.com>
2017-11-03 19:34:39 +0000

    567cc26 docs: Component Styles guide for CLI (#19791)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-169cedd
+ * @license Angular v5.1.0-beta.0-567cc26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-169cedd');
+const VERSION = new Version('5.1.0-beta.0-567cc26');
 
 /**
  * @fileoverview added by tsickle
