90ff0d89a0c6f749c57a081b051b798f1c4320d3
Victor Berchet <victor@suumit.com>
2017-11-03 18:50:07 +0000

    c7fd172 build(aio): make plunker works with rxjs operators (#20124)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-5079d2d
+ * @license Angular v5.1.0-beta.0-c7fd172
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-5079d2d');
+const VERSION = new Version('5.1.0-beta.0-c7fd172');
 
 /**
  * @fileoverview added by tsickle
