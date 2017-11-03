2fb7981a344628bbd1f1915e2a59f8d7c959e83f
Victor Berchet <victor@suumit.com>
2017-11-03 19:57:12 +0000

    e4cd3b0 test(aio): make e2e tests less flakey

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-9811bef
+ * @license Angular v5.0.0-e4cd3b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-9811bef
+ * @license Angular v5.0.0-e4cd3b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-9811bef');
+var VERSION = new Version('5.0.0-e4cd3b0');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-9811bef
+ * @license Angular v5.0.0-e4cd3b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-9811bef');
+const VERSION = new Version('5.0.0-e4cd3b0');
 
 /**
  * @fileoverview added by tsickle
