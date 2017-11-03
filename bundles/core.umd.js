29497ccafec4eae9bc4e0091e065377095fcad02
Victor Berchet <victor@suumit.com>
2017-11-03 19:46:14 +0000

    9811bef docs: Bootstrapping guide prose for CLI (#19777)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e6f16a7
+ * @license Angular v5.0.0-9811bef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-e6f16a7
+ * @license Angular v5.0.0-9811bef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-e6f16a7');
+var VERSION = new Version('5.0.0-9811bef');
 
 /**
  * @fileoverview added by tsickle
