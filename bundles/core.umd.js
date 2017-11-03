361f3c8c4f9949bcd306247fa60152b387588989
Victor Berchet <victor@suumit.com>
2017-11-03 19:32:24 +0000

    169cedd docs: Bootstrapping guide prose for CLI (#19777)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-1d19d61
+ * @license Angular v5.1.0-beta.0-169cedd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-1d19d61
+ * @license Angular v5.1.0-beta.0-169cedd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-1d19d61');
+var VERSION = new Version('5.1.0-beta.0-169cedd');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-1d19d61
+ * @license Angular v5.1.0-beta.0-169cedd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-1d19d61');
+const VERSION = new Version('5.1.0-beta.0-169cedd');
 
 /**
  * @fileoverview added by tsickle
