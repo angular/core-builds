a9996dd220933ef7d67800ec9769b90b374e314e
Victor Berchet <victor@suumit.com>
2017-11-03 19:14:10 +0000

    03f080b docs: Deployment guide for CLI (#19839)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-26f8299
+ * @license Angular v5.1.0-beta.0-03f080b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-26f8299');
+var VERSION = new Version('5.1.0-beta.0-03f080b');
 
 /**
  * @fileoverview added by tsickle
