d73d2ddf2fed88dd5e708c69ffb5c13051361723
Victor Berchet <victor@suumit.com>
2017-11-06 18:23:20 +0000

    8b50ed0 docs(aio): fixed case-typo for code (#20196)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3997d97
+ * @license Angular v5.1.0-beta.0-8b50ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-3997d97
+ * @license Angular v5.1.0-beta.0-8b50ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-3997d97');
+var VERSION = new Version('5.1.0-beta.0-8b50ed0');
 
 /**
  * @fileoverview added by tsickle
