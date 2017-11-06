d618193adbf1df005d227843bef11d7e2ae9a4b1
Victor Berchet <victor@suumit.com>
2017-11-06 18:37:35 +0000

    3ab0963 docs(aio): update ToH for CLI (#19811)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-7083791
+ * @license Angular v5.0.0-3ab0963
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-7083791
+ * @license Angular v5.0.0-3ab0963
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-7083791');
+var VERSION = new Version('5.0.0-3ab0963');
 
 /**
  * @fileoverview added by tsickle
