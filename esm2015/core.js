96c714f40a69b4f98ccf5d2089312967ed975c85
Victor Berchet <victor@suumit.com>
2017-11-06 20:05:42 +0000

    511e56d docs(common): fix mis-ordered lines (#20221)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-1f1741c
+ * @license Angular v5.0.0-511e56d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-1f1741c');
+const VERSION = new Version('5.0.0-511e56d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-1f1741c
+ * @license Angular v5.0.0-511e56d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-1f1741c');
+var VERSION = new Version('5.0.0-511e56d');
 
 /**
  * @fileoverview added by tsickle
