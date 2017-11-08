ff510318580cfeaa050d54586e3ed0b2855fa8d9
Victor Berchet <victor@suumit.com>
2017-11-08 18:50:35 +0000

    119034c docs: add changelog for 5.0.1

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-6e8e3bd
+ * @license Angular v5.1.0-beta.0-119034c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-6e8e3bd');
+const VERSION = new Version('5.1.0-beta.0-119034c');
 
 /**
  * @fileoverview added by tsickle
