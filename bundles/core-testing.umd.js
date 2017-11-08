13e6986d08aebffa851308e641a2e94972d75706
Victor Berchet <victor@suumit.com>
2017-11-08 19:23:43 +0000

    9de45fa docs: add changelog for 5.0.1

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-511e56d
+ * @license Angular v5.0.1-9de45fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-511e56d
+ * @license Angular v5.0.1-9de45fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
