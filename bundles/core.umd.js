cac4a792839ad55ab7d72302d24ea88eaf957754
Victor Berchet <victor@suumit.com>
2017-11-08 22:36:39 +0000

    78ba39b docs: add changelog for 5.1.0-beta.0

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-119034c
+ * @license Angular v5.1.0-beta.0-78ba39b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-119034c
+ * @license Angular v5.1.0-beta.0-78ba39b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-119034c');
+var VERSION = new Version('5.1.0-beta.0-78ba39b');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-119034c
+ * @license Angular v5.1.0-beta.0-78ba39b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-119034c');
+const VERSION = new Version('5.1.0-beta.0-78ba39b');
 
 /**
  * @fileoverview added by tsickle
