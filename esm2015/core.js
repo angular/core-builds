504d85a61d42027af09c2b870f08b314bd4cff41
Jason Aden <jasonaden@google.com>
2017-11-10 00:24:07 +0000

    424a323 fix(compiler): show explanatory text in template errors (#20313)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-28985cb
+ * @license Angular v5.0.1-424a323
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.1-28985cb');
+const VERSION = new Version('5.0.1-424a323');
 
 /**
  * @fileoverview added by tsickle
