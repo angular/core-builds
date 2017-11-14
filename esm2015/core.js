e9c736653f1f6b9e65b99d66b8c364ea3c52baff
Jason Aden <jasonaden@google.com>
2017-11-14 22:06:44 +0000

    20aafff fix(animations): ensure final state() styles are applied within @.disabled animations (#20267)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-12af6d3
+ * @license Angular v5.1.0-beta.0-20aafff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-12af6d3');
+const VERSION = new Version('5.1.0-beta.0-20aafff');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-12af6d3
+ * @license Angular v5.1.0-beta.0-20aafff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-12af6d3');
+var VERSION = new Version('5.1.0-beta.0-20aafff');
 
 /**
  * @fileoverview added by tsickle
