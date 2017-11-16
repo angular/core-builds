19708249ee53f19e7587d7e72bfc2a51cbc6b880
Miško Hevery <misko@hevery.com>
2017-11-16 22:03:56 +0000

    b1f8eb1 docs: add changelog for 5.0.2

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-1285792
+ * @license Angular v5.0.2-b1f8eb1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.1-1285792');
+const VERSION = new Version('5.0.2-b1f8eb1');
 
 /**
  * @fileoverview added by tsickle
