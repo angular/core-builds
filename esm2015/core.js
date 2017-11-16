abbe56b67bf8aadfc3b754b50c7028ff682fdae4
Miško Hevery <misko@hevery.com>
2017-11-16 04:50:32 +0000

    1285792 fix(aio): fix window title on Home page (#20440)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-2d6126e
+ * @license Angular v5.0.1-1285792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.1-2d6126e');
+const VERSION = new Version('5.0.1-1285792');
 
 /**
  * @fileoverview added by tsickle
