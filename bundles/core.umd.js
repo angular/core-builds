fd0a107f51ac9b1d43d7a1ee9245a26c15b58f4f
Miško Hevery <misko@hevery.com>
2017-11-16 06:59:03 +0000

    dcfffbf build: fix build failures (#20470)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-336041a
+ * @license Angular v5.1.0-beta.0-dcfffbf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-336041a
+ * @license Angular v5.1.0-beta.0-dcfffbf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-336041a');
+var VERSION = new Version('5.1.0-beta.0-dcfffbf');
 
 /**
  * @fileoverview added by tsickle
