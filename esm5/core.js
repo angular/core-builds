f2a37109410dfde7d7e87892f35e6122f01e02db
Miško Hevery <misko@hevery.com>
2017-11-18 00:37:12 +0000

    69c53c3 fix(forms): updateOn should check if change occurred (#20358)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-365712e
+ * @license Angular v5.1.0-beta.1-69c53c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-365712e');
+var VERSION = new Version('5.1.0-beta.1-69c53c3');
 
 /**
  * @fileoverview added by tsickle
