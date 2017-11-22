6a859a377074d0f3868499afbfc4414668e2fea1
Miško Hevery <misko@hevery.com>
2017-11-22 21:44:49 +0000

    65a40e6 docs: add changelog for 5.0.3

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-686c4ef
+ * @license Angular v5.0.3-65a40e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.2-686c4ef
+ * @license Angular v5.0.3-65a40e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.2-686c4ef');
+var VERSION = new Version('5.0.3-65a40e6');
 
 /**
  * @fileoverview added by tsickle
