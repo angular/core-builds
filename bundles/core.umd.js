65cfd480bc03cc8fbc40f693936e7db148d62b8a
Miško Hevery <misko@hevery.com>
2017-11-22 15:13:45 +0000

    ac93f12 ci: Update 1% payload size test (#20524)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-7cf5e95
+ * @license Angular v5.1.0-beta.1-ac93f12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.1-7cf5e95
+ * @license Angular v5.1.0-beta.1-ac93f12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-7cf5e95');
+var VERSION = new Version('5.1.0-beta.1-ac93f12');
 
 /**
  * @fileoverview added by tsickle
