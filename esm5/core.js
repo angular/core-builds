4ecb67cd2dc9772a007b064cd4cdb94a38cd1da4
Miško Hevery <misko@hevery.com>
2017-11-22 16:58:00 +0000

    a0819d3 docs(aio): add a French onsite training ressource (#19889) PR Close #19889

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-ac93f12
+ * @license Angular v5.1.0-beta.1-a0819d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-ac93f12');
+var VERSION = new Version('5.1.0-beta.1-a0819d3');
 
 /**
  * @fileoverview added by tsickle
