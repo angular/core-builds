1dbd6f6ddd7469b32bf01f265140896bfd31c2e8
Miško Hevery <misko@hevery.com>
2017-11-22 21:23:44 +0000

    686c4ef docs(aio): add ngATL header (#20590)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-efce396
+ * @license Angular v5.0.2-686c4ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.2-efce396
+ * @license Angular v5.0.2-686c4ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
