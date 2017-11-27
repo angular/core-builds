c1ed0d423a88587a02a5a3a8776cc79289bca4b9
Miško Hevery <misko@hevery.com>
2017-11-27 01:29:24 +0000

    330bb2a release: cut the 5.1.0-beta.2 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0444e13
+ * @license Angular v5.1.0-beta.2-330bb2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.1-0444e13
+ * @license Angular v5.1.0-beta.2-330bb2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-0444e13');
+var VERSION = new Version('5.1.0-beta.2-330bb2a');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0444e13
+ * @license Angular v5.1.0-beta.2-330bb2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-0444e13');
+var VERSION = new Version('5.1.0-beta.2-330bb2a');
 
 /**
  * @fileoverview added by tsickle
