d28d32a75db44380c94d5a6cc22c673cb7b998f4
Miško Hevery <misko@hevery.com>
2017-11-29 22:34:11 +0000

    ba850b3 Revert "fix(core): should use native addEventListener in ngZone (#20672)"

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-65a2cb8
+ * @license Angular v5.1.0-beta.2-ba850b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.2-65a2cb8');
+const VERSION = new Version('5.1.0-beta.2-ba850b3');
 
 /**
  * @fileoverview added by tsickle
