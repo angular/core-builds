aa9f3ae59440f9b91ec5f7a5526bf27c7f0bdd53
Miško Hevery <misko@hevery.com>
2017-11-29 04:44:51 +0000

    c2b3792 fix(animations): ensure multi-level leave animations work (#19455)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-8bb42df
+ * @license Angular v5.1.0-beta.2-c2b3792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.2-8bb42df
+ * @license Angular v5.1.0-beta.2-c2b3792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-8bb42df');
+var VERSION = new Version('5.1.0-beta.2-c2b3792');
 
 /**
  * @fileoverview added by tsickle
