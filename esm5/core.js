f8cda6b95682939b459f726f35ce7c187b271f6d
Misko Hevery <misko@hevery.com>
2017-11-30 04:50:42 +0000

    9e69d97 style: broken build due to missing new lines

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-b450c83
+ * @license Angular v5.0.3-9e69d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.3-b450c83');
+var VERSION = new Version('5.0.3-9e69d97');
 
 /**
  * @fileoverview added by tsickle
