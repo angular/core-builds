9edbc77e1ec52eb9b72aa64220e9835a3b817503
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:41:25 +0000

    f841fbe fix(compiler-cli): propagate ts.SourceFile moduleName into metadata

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-b9a91a5
+ * @license Angular v5.1.0-rc.0-f841fbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-rc.0-b9a91a5
+ * @license Angular v5.1.0-rc.0-f841fbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.0-b9a91a5');
+var VERSION = new Version('5.1.0-rc.0-f841fbe');
 
 /**
  * @fileoverview added by tsickle
