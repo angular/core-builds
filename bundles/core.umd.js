4df42791c1715df093cab6f5c3731744fe09c57d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:41:09 +0000

    b9a91a5 fix(service-worker): don't crash if SW not supported

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-60a3081
+ * @license Angular v5.1.0-rc.0-b9a91a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-rc.0-60a3081
+ * @license Angular v5.1.0-rc.0-b9a91a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.0-60a3081');
+var VERSION = new Version('5.1.0-rc.0-b9a91a5');
 
 /**
  * @fileoverview added by tsickle
