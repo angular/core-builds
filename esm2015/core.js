27f18675f1cca892fcfc113c468a3fdac69280c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 23:39:47 +0000

    9dc310e docs: add changelog for 5.0.5

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.4-445d833
+ * @license Angular v5.0.5-9dc310e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.4-445d833');
+const VERSION = new Version('5.0.5-9dc310e');
 
 /**
  * @fileoverview added by tsickle
