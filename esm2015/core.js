084bf9b775f40e8ac4d93fbe2cb7e6166a99e0b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:45:36 +0000

    f582620 fix(service-worker): use relative path for ngsw.json

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f841fbe
+ * @license Angular v5.1.0-rc.0-f582620
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-rc.0-f841fbe');
+const VERSION = new Version('5.1.0-rc.0-f582620');
 
 /**
  * @fileoverview added by tsickle
