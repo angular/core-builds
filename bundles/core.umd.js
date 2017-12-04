36f393841435b96fd595c41aa1cbe41a1b49165b
Igor Minar <igor@angularjs.org>
2017-12-04 23:15:50 +0000

    7e7ff2e ci: fix the payload-size checking scripts (#20683)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.0-d507057');
+var VERSION = new Version('5.1.0-rc.1-7e7ff2e');
 
 /**
  * @fileoverview added by tsickle
