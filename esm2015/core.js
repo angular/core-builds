a8ff06ab3db0afc2e95c37a92b848e7e0d4c9c35
Igor Minar <igor@angularjs.org>
2017-12-06 06:20:31 +0000

    9efb5da docs(aio): Fix typo (#20732)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-860ef8c
+ * @license Angular v5.0.5-9efb5da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-860ef8c');
+const VERSION = new Version('5.0.5-9efb5da');
 
 /**
  * @fileoverview added by tsickle
