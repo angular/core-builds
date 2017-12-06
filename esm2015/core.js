991722638afa2b856ecaa5946eab3c95f6bbeb9a
Igor Minar <igor@angularjs.org>
2017-12-06 15:30:51 +0000

    eb57bc3 fix(animations): ensure DOM is cleaned up after multiple @trigger leave animations finish (#20740)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-d824cc2
+ * @license Angular v5.0.5-eb57bc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-d824cc2');
+const VERSION = new Version('5.0.5-eb57bc3');
 
 /**
  * @fileoverview added by tsickle
