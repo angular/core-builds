6f7e48d992422e0c1fdf320035b2c9f75d726bfa
Igor Minar <igor@angularjs.org>
2017-12-06 15:15:23 +0000

    c91c0d4 fix(service-worker): esm2015 points to wrong path (#20800)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-9efb5da');
+var VERSION = new Version('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-9efb5da');
+const VERSION = new Version('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle
