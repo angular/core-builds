f3985527393ea5621c4e6912692448816d9a4eb7
Igor Minar <igor@angularjs.org>
2017-12-06 06:08:01 +0000

    a2fde2b ci(core): Improve the payload size message (#20786)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-e37f7c0
+ * @license Angular v5.0.5-a2fde2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.5-e37f7c0
+ * @license Angular v5.0.5-a2fde2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-e37f7c0');
+var VERSION = new Version('5.0.5-a2fde2b');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-e37f7c0
+ * @license Angular v5.0.5-a2fde2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-e37f7c0');
+const VERSION = new Version('5.0.5-a2fde2b');
 
 /**
  * @fileoverview added by tsickle
