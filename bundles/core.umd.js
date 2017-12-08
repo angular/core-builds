74459356a7a8f24a8bcc370859ee1b2cd13d5b27
Jason Aden <jasonaden@google.com>
2017-12-08 18:31:21 +0000

    ddada6e build(aio): upgrade to latest `@angular/cli` (#18428)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-225baf4
+ * @license Angular v5.1.0-ddada6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-225baf4
+ * @license Angular v5.1.0-ddada6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-225baf4');
+var VERSION = new Version('5.1.0-ddada6e');
 
 /**
  * @fileoverview added by tsickle
