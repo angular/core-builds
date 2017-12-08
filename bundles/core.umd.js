6a74b2a6985e6e4f4acdbc5a86a34c0f7f35f4d8
Jason Aden <jasonaden@google.com>
2017-12-08 01:29:06 +0000

    baeec4d fix(router): NavigatonError and NavigationCancel should be emitted after resetting the URL (#20803)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-9dd60a5
+ * @license Angular v5.1.0-baeec4d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-9dd60a5
+ * @license Angular v5.1.0-baeec4d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-9dd60a5');
+var VERSION = new Version('5.1.0-baeec4d');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-9dd60a5
+ * @license Angular v5.1.0-baeec4d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-9dd60a5');
+const VERSION = new Version('5.1.0-baeec4d');
 
 /**
  * @fileoverview added by tsickle
