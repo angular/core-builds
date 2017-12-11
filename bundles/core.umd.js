6385583b45bd34c196356f5b3c2b11fbca32c064
Jason Aden <jasonaden@google.com>
2017-12-11 19:55:57 +0000

    9fbf850 build: remove bazel option --noshow_results (#20943)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-d09d497
+ * @license Angular v5.1.0-9fbf850
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-d09d497
+ * @license Angular v5.1.0-9fbf850
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-d09d497');
+var VERSION = new Version('5.1.0-9fbf850');
 
 /**
  * @fileoverview added by tsickle
