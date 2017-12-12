96690ae7958438467ed2620633fd22462ca47581
Jason Aden <jasonaden@google.com>
2017-12-12 00:26:04 +0000

    81d497c build: pin ChromeDriver version (#20940)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-70cd124
+ * @license Angular v5.1.0-81d497c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-70cd124
+ * @license Angular v5.1.0-81d497c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-70cd124');
+var VERSION = new Version('5.1.0-81d497c');
 
 /**
  * @fileoverview added by tsickle
