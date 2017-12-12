d1f946af1cec4e956a3a7c9ae2489a79bc8694be
Jason Aden <jasonaden@google.com>
2017-12-12 20:03:05 +0000

    be9a737 fix(compiler-cli): merge @fileoverview comments. (#20870)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-1f46949
+ * @license Angular v5.1.0-be9a737
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-1f46949
+ * @license Angular v5.1.0-be9a737
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-1f46949');
+var VERSION = new Version('5.1.0-be9a737');
 
 /**
  * @fileoverview added by tsickle
