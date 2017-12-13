fb63d4ca3670c9572e30f2c831f089395eee81b6
GitHub <noreply@github.com>
2017-12-13 19:43:33 +0000

    7fd441e ci: fix invalid bazel query expression

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-c8010f4
+ * @license Angular v5.1.0-7fd441e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-c8010f4
+ * @license Angular v5.1.0-7fd441e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-c8010f4');
+var VERSION = new Version('5.1.0-7fd441e');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-c8010f4
+ * @license Angular v5.1.0-7fd441e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-c8010f4');
+const VERSION = new Version('5.1.0-7fd441e');
 
 /**
  * @fileoverview added by tsickle
