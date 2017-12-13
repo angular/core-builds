2942edd53de1e81d2dc7b3701cd27c80f4f318be
Alex Eagle <alexeagle@google.com>
2017-12-13 23:21:14 +0000

    541a391 ci: parallelize bazel build and test

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-d3dbb9d
+ * @license Angular v5.2.0-beta.0-541a391
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.0-d3dbb9d');
+const VERSION = new Version('5.2.0-beta.0-541a391');
 
 /**
  * @fileoverview added by tsickle
