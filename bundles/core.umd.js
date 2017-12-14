4e00152c1b4f3d5042ef72885cc44d4b15364770
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 16:57:16 +0000

    2686219 ci: sync CircleCI config from master to 5.0.x

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -706,7 +706,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-eb57bc3');
+var VERSION = new Version('5.0.5-2686219');
 
 /**
  * @fileoverview added by tsickle
