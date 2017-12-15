9f1de9044122e7354bcd1dfa80657f9b4069f2cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:35:34 +0000

    0b2d636 fix(router): replaceURL when reacting to a change coming from angularjs

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-a474276
+ * @license Angular v5.2.0-beta.0-0b2d636
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.0-a474276');
+const VERSION = new Version('5.2.0-beta.0-0b2d636');
 
 /**
  * @fileoverview added by tsickle
