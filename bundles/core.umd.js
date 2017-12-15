db284bc7c8656a83074aa0a6961338b28fc34d0a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:38:40 +0000

    c011ffa test(aio): correct usage of fakeAsync and inject together in test

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-756dd34
+ * @license Angular v5.1.1-c011ffa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.1-756dd34
+ * @license Angular v5.1.1-c011ffa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.1-756dd34');
+var VERSION = new Version('5.1.1-c011ffa');
 
 /**
  * @fileoverview added by tsickle
