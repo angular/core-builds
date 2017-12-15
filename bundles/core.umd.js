30769a53c8071ba1a34628e8d5ef35ad4e5ceb0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 23:40:10 +0000

    20e1cc0 fix(service-worker): check for updates on navigation

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-0b2d636
+ * @license Angular v5.2.0-beta.0-20e1cc0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-beta.0-0b2d636
+ * @license Angular v5.2.0-beta.0-20e1cc0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-beta.0-0b2d636');
+var VERSION = new Version('5.2.0-beta.0-20e1cc0');
 
 /**
  * @fileoverview added by tsickle
