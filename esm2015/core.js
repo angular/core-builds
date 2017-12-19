21bfe22432cae2ac39785dcf367fc89b4c680291
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-19 19:31:33 +0000

    f593552 docs(aio): Rename service worker files, update examples, move service worker under Techniques

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-8458647
+ * @license Angular v5.2.0-beta.0-f593552
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.0-8458647');
+const VERSION = new Version('5.2.0-beta.0-f593552');
 
 /**
  * @fileoverview added by tsickle
