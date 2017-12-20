1a905003725a1c01b37f652fa192697798d96878
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-20 22:13:42 +0000

    abca7c0 docs: add changelog for 5.2.0-beta.1

---
+++ core.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { AnimationMetadataType as ɵu, animate as ɵw, group as ɵx, keyframes as ɵbb, sequence as ɵy, state as ɵba, style as ɵz, transition as ɵbc, trigger as ɵv } from './src/animation/dsl';
+export { _iterableDiffersFactory as ɵj, _keyValueDiffersFactory as ɵk, _localeFactory as ɵl } from './src/application_module';
+export { _appIdRandomProviderFactory as ɵe } from './src/application_tokens';
+export { defaultIterableDiffers as ɵf, defaultKeyValueDiffers as ɵg } from './src/change_detection/change_detection';
+export { DefaultIterableDifferFactory as ɵh } from './src/change_detection/differs/default_iterable_differ';
+export { DefaultKeyValueDifferFactory as ɵi } from './src/change_detection/differs/default_keyvalue_differ';
+export { ReflectiveInjector_ as ɵb } from './src/di/reflective_injector';
+export { ReflectiveDependency as ɵc, resolveReflectiveProviders as ɵd } from './src/di/reflective_provider';
+export { wtfEnabled as ɵm } from './src/profile/profile';
+export { createScope as ɵo, detectWTF as ɵn, endTimeRange as ɵr, leave as ɵp, startTimeRange as ɵq } from './src/profile/wtf_impl';
+export { makeParamDecorator as ɵa } from './src/util/decorators';
+export { _def as ɵs } from './src/view/provider';
+export { DebugContext as ɵt } from './src/view/types';
