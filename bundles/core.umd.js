3aea2473309f264324327fea8c5da4eca4de4962
Igor Minar <igor@angularjs.org>
2017-12-21 21:40:29 +0000

    dc53248 build: update pullapprove:

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-d1f4500
+ * @license Angular v5.1.2-dc53248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-d1f4500
+ * @license Angular v5.1.2-dc53248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-d1f4500');
+var VERSION = new Version('5.1.2-dc53248');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-d1f4500
+ * @license Angular v5.1.2-dc53248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-d1f4500');
+const VERSION = new Version('5.1.2-dc53248');
 
 /**
  * @fileoverview added by tsickle
