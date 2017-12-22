22f983a56ae167e289778e44b70104f76cfa79df
Igor Minar <igor@angularjs.org>
2017-12-22 17:38:38 +0000

    8c99175 fix(router): fix wildcard route with lazy loaded module (again) (#18139)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-fa0e8ef
+ * @license Angular v5.1.2-8c99175
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-fa0e8ef');
+const VERSION = new Version('5.1.2-8c99175');
 
 /**
  * @fileoverview added by tsickle
