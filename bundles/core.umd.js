4de2be7736bf3f5c53981d1632020b9d51f11d53
Igor Minar <igor@angularjs.org>
2017-12-22 17:51:17 +0000

    86a36ea fix(animations): avoid infinite loop with multiple blocked sub triggers (#21119)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-5a7bf36
+ * @license Angular v5.2.0-beta.1-86a36ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-beta.1-5a7bf36
+ * @license Angular v5.2.0-beta.1-86a36ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-beta.1-5a7bf36');
+var VERSION = new Version('5.2.0-beta.1-86a36ea');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-5a7bf36
+ * @license Angular v5.2.0-beta.1-86a36ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.1-5a7bf36');
+const VERSION = new Version('5.2.0-beta.1-86a36ea');
 
 /**
  * @fileoverview added by tsickle
