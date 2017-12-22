651b20b1c12be2211eb1e414acd619063102a967
Igor Minar <igor@angularjs.org>
2017-12-22 04:30:21 +0000

    41abcc3 docs(service-worker): fix word wrap (#21114)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-4337768
+ * @license Angular v5.1.2-41abcc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-4337768
+ * @license Angular v5.1.2-41abcc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-4337768');
+var VERSION = new Version('5.1.2-41abcc3');
 
 /**
  * @fileoverview added by tsickle
