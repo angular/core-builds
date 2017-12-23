bd5d38b2b7d000144f52538850a81d212d2a44fd
Igor Minar <igor@angular.io>
2017-12-23 05:58:26 +0000

    12702b2 docs(changelog): fix typo in 5.1.1 (#21007)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-5de91fe');
+var VERSION = new Version('5.1.2-12702b2');
 
 /**
  * @fileoverview added by tsickle
