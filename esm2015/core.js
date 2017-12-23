d2455efd9fb1911d56628ad67eb6c486a8bd7f51
Igor Minar <igor@angular.io>
2017-12-23 05:54:52 +0000

    d787b55 build(common): generate ts declarations for i18n locale files (#21127)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-3e34fa8
+ * @license Angular v5.1.2-d787b55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-3e34fa8');
+const VERSION = new Version('5.1.2-d787b55');
 
 /**
  * @fileoverview added by tsickle
