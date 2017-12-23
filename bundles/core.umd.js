0fccda36e8e5f306a4f71243d53f3ff41a6d0e3d
Igor Minar <igor@angular.io>
2017-12-23 05:56:50 +0000

    5de91fe docs(forms): add text about min() and max() as functions (#21110)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-d787b55
+ * @license Angular v5.1.2-5de91fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-d787b55
+ * @license Angular v5.1.2-5de91fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-d787b55');
+var VERSION = new Version('5.1.2-5de91fe');
 
 /**
  * @fileoverview added by tsickle
