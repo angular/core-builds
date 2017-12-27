213b4b1c426815e0253d530c6d3937d2d4afb99a
Igor Minar <igor@angular.io>
2017-12-27 21:35:15 +0000

    66043e0 docs(aio): Sort in the api type dropdown (#21030) (#21176)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-12702b2');
+var VERSION = new Version('5.1.2-66043e0');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-12702b2');
+var VERSION = new Version('5.1.2-66043e0');
 
 /**
  * @fileoverview added by tsickle
