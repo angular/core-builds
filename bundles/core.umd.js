16b2cbedc609d58e63fcf03d90e6c40164eead0f
Kara Erickson <karakara@google.com>
2018-01-04 22:39:14 +0000

    cc0b0a4 fix(aio): do not redirect pages on "next" deployment (#21027)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b9d8ac5
+ * @license Angular v5.1.3-cc0b0a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-b9d8ac5
+ * @license Angular v5.1.3-cc0b0a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-b9d8ac5');
+var VERSION = new Version('5.1.3-cc0b0a4');
 
 /**
  * @fileoverview added by tsickle
