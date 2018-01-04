16f6843dd1cdedb394072a6f9014124ea112a4e5
Kara Erickson <karakara@google.com>
2018-01-04 22:38:39 +0000

    6588ca8 fix(aio): do not redirect pages on "next" deployment (#21027)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-81823e8
+ * @license Angular v5.2.0-rc.0-6588ca8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-rc.0-81823e8
+ * @license Angular v5.2.0-rc.0-6588ca8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-81823e8');
+var VERSION = new Version('5.2.0-rc.0-6588ca8');
 
 /**
  * @fileoverview added by tsickle
