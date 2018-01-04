20a803c9d01d0a8f1e93bb789ad154ce4c42bf7c
Misko Hevery <misko@hevery.com>
2018-01-04 00:21:16 +0000

    d138b38 docs: add changelog for 5.1.3

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-00308fc');
+var VERSION = new Version('5.1.3-d138b38');
 
 /**
  * @fileoverview added by tsickle
