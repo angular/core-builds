3284cbf34eaedb88c45b6ae685e2539eb2eb7751
Kara Erickson <karakara@google.com>
2018-01-05 20:16:34 +0000

    8dd93f8 docs: remove broken issuestats.com badges (#21334) PR Close #21334

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-17e85cd
+ * @license Angular v5.1.3-8dd93f8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-17e85cd');
+var VERSION = new Version('5.1.3-8dd93f8');
 
 /**
  * @fileoverview added by tsickle
