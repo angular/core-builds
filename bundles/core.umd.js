24ee461fbdd69ef512036a63d7fc573e2b12fbc4
Kara Erickson <karakara@google.com>
2018-01-05 22:40:26 +0000

    a1af559 build(core): update payload size of hello world (#21340)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-e2cc4db
+ * @license Angular v5.1.3-a1af559
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-e2cc4db
+ * @license Angular v5.1.3-a1af559
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-e2cc4db');
+var VERSION = new Version('5.1.3-a1af559');
 
 /**
  * @fileoverview added by tsickle
