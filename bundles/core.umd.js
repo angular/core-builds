d8f2c0711bc1085fbb6e539a2fff210ef76645d4
Kara Erickson <karakara@google.com>
2018-01-05 20:19:33 +0000

    52cb79c build(aio): add `@nodoc` alias to the `@internal` tag (#21024)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-8dd93f8
+ * @license Angular v5.1.3-52cb79c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-8dd93f8
+ * @license Angular v5.1.3-52cb79c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-8dd93f8');
+var VERSION = new Version('5.1.3-52cb79c');
 
 /**
  * @fileoverview added by tsickle
