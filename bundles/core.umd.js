3449d9a4a98c3d2c09077c156281821672dc6382
Kara Erickson <karakara@google.com>
2018-01-08 19:33:05 +0000

    7269fe5 ci: reformat itegration/_payload-limits.json to be easier to read (#21344)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-e994752
+ * @license Angular v5.2.0-rc.0-7269fe5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-rc.0-e994752
+ * @license Angular v5.2.0-rc.0-7269fe5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-e994752');
+var VERSION = new Version('5.2.0-rc.0-7269fe5');
 
 /**
  * @fileoverview added by tsickle
