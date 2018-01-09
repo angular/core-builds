e2f7654099d4e2750f8cf7c70f77f4e1b8a92365
Kara Erickson <karakara@google.com>
2018-01-09 19:26:47 +0000

    7c34807 build(aio): upgrade to zone.js@0.8.19 (#21349)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-84f6b14
+ * @license Angular v5.1.3-7c34807
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-84f6b14
+ * @license Angular v5.1.3-7c34807
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-84f6b14');
+var VERSION = new Version('5.1.3-7c34807');
 
 /**
  * @fileoverview added by tsickle
