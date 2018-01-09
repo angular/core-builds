3ada513b33ee1d5f3458e1b79b65f897d6123f0e
Kara Erickson <karakara@google.com>
2018-01-09 18:50:23 +0000

    39f81b2 build: upgrade Bazel to 0.9.0 (#21335)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-0602aaa');
+var VERSION = new Version('5.1.3-39f81b2');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-0602aaa');
+var VERSION = new Version('5.1.3-39f81b2');
 
 /**
  * @fileoverview added by tsickle
