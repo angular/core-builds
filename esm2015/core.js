65f57c88c47ef8809052b0a47c52bf54bfca6df7
GitHub <noreply@github.com>
2018-01-09 18:52:12 +0000

    0d1611f docs: incorporate PR review feedback

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-d2808aa
+ * @license Angular v5.2.0-rc.0-0d1611f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-d2808aa');
+const VERSION = new Version('5.2.0-rc.0-0d1611f');
 
 /**
  * @fileoverview added by tsickle
