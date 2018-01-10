22da58b07e235203bc759bf25dca7024f7ed9a0b
Kara Erickson <karakara@google.com>
2018-01-10 19:33:00 +0000

    908129e docs(core): provide note for unit test changes with the `ng-star-inserted` className (#21450)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-04dd971');
+var VERSION = new Version('5.2.0-908129e');
 
 /**
  * @fileoverview added by tsickle
