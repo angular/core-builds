3272bc1cff5b8bd2efb0b428d6471984e82968ad
Alex Eagle <alexeagle@google.com>
2018-01-10 21:49:57 +0000

    c32e833 fix(compiler): cache external reference resolution (#21359)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7bdd9ae
+ * @license Angular v5.2.0-c32e833
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-7bdd9ae
+ * @license Angular v5.2.0-c32e833
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-7bdd9ae');
+var VERSION = new Version('5.2.0-c32e833');
 
 /**
  * @fileoverview added by tsickle
