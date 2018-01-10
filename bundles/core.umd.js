510bf4a68e5b153ae61dc06e63d1f8cc737bf5d7
Alex Eagle <alexeagle@google.com>
2018-01-10 21:02:32 +0000

    0a5a878 build(aio): upgrade to uglify-es@3.3.5 (#21350)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-76c781f
+ * @license Angular v5.2.0-0a5a878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-76c781f
+ * @license Angular v5.2.0-0a5a878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-76c781f');
+var VERSION = new Version('5.2.0-0a5a878');
 
 /**
  * @fileoverview added by tsickle
