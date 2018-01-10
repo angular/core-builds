f5c30e43eb94308e0f61db4097ca9a5a2de91dc0
Alex Eagle <alexeagle@google.com>
2018-01-10 21:08:59 +0000

    3804d20 docs(bazel): document an installation issue (#21401)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-0a5a878
+ * @license Angular v5.2.0-3804d20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-0a5a878
+ * @license Angular v5.2.0-3804d20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-0a5a878');
+var VERSION = new Version('5.2.0-3804d20');
 
 /**
  * @fileoverview added by tsickle
