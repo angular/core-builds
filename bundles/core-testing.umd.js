d2496569bfbf853ed26bf95ed7948a15dfbceaa2
Alex Eagle <alexeagle@google.com>
2018-01-10 21:30:17 +0000

    7493b8a build: upgrade yarn to 1.3.2 (#21406)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
