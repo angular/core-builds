5f25028d3b4871d0922e137bbf353529d1165ea5
Alex Eagle <alexeagle@google.com>
2018-01-10 22:49:10 +0000

    7b463df fix(aio): preserve static Observable methods (#21351)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-650f5fb
+ * @license Angular v5.2.0-7b463df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-650f5fb
+ * @license Angular v5.2.0-7b463df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-650f5fb');
+var VERSION = new Version('5.2.0-7b463df');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-650f5fb
+ * @license Angular v5.2.0-7b463df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-650f5fb');
+const VERSION = new Version('5.2.0-7b463df');
 
 /**
  * @fileoverview added by tsickle
