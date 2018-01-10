d42c978d27e1e995781b3589c6bd4a103d73e0cb
Alex Eagle <alexeagle@google.com>
2018-01-10 20:58:25 +0000

    ff6047c build(aio): upgrade to uglify-es@3.3.5 (#21350)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a76ab1f
+ * @license Angular v5.2.0-ff6047c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-a76ab1f
+ * @license Angular v5.2.0-ff6047c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-a76ab1f');
+var VERSION = new Version('5.2.0-ff6047c');
 
 /**
  * @fileoverview added by tsickle
