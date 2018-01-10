abd9590331b92fee26f739081ddb2ef9f2867e5c
Alex Eagle <alexeagle@google.com>
2018-01-10 22:16:25 +0000

    650f5fb build(bazel): fix merge conflict between c4f02e2 and ef956a2 (#21453)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c32e833
+ * @license Angular v5.2.0-650f5fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-c32e833
+ * @license Angular v5.2.0-650f5fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-c32e833');
+var VERSION = new Version('5.2.0-650f5fb');
 
 /**
  * @fileoverview added by tsickle
