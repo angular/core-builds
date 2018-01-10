d19a91f755644bc90af1c9ad24e2d1d2130c836d
Alex Eagle <alexeagle@google.com>
2018-01-10 23:39:52 +0000

    92984ba docs: update the release schedule with v6 info (#21435)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-137b9c7
+ * @license Angular v5.2.0-92984ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-137b9c7
+ * @license Angular v5.2.0-92984ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-137b9c7
+ * @license Angular v5.2.0-92984ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-137b9c7
+ * @license Angular v5.2.0-92984ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-137b9c7');
+var VERSION = new Version('5.2.0-92984ba');
 
 /**
  * @fileoverview added by tsickle
