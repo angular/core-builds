0f95f6ff2e4e1cb153e92cc812eb5d1f91b93e21
Alex Eagle <alexeagle@google.com>
2018-01-10 20:37:57 +0000

    72246d4 ci: update forms approval list to include guides (#21449)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d2aa8ac
+ * @license Angular v5.2.0-72246d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-d2aa8ac
+ * @license Angular v5.2.0-72246d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-d2aa8ac');
+var VERSION = new Version('5.2.0-72246d4');
 
 /**
  * @fileoverview added by tsickle
