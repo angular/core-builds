60a740f83f3792605c629991593decc523983ea0
Alex Eagle <alexeagle@google.com>
2018-01-10 23:53:09 +0000

    6a97b5b docs(ivy): add Ivy as a recognized label (#21428)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-aad1126
+ * @license Angular v5.2.0-6a97b5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-aad1126');
+const VERSION = new Version('5.2.0-6a97b5b');
 
 /**
  * @fileoverview added by tsickle
