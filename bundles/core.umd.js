414a1562663fa622cfb4969e7843b11adec10c49
Alex Eagle <alexeagle@google.com>
2018-01-11 00:47:49 +0000

    3db02d2 docs: fix lazy-loading example (#20306)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-a931a41');
+var VERSION = new Version('5.2.0-3db02d2');
 
 /**
  * @fileoverview added by tsickle
