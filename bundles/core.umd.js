1088ab166eeceba375ed986aa1047df906bb5356
Alex Eagle <alexeagle@google.com>
2018-01-11 00:41:33 +0000

    e64b1e9 fix(compiler): make `.ngsummary.json` files idempotent (#21448)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-6be9c04
+ * @license Angular v5.2.0-e64b1e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-6be9c04
+ * @license Angular v5.2.0-e64b1e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-6be9c04');
+var VERSION = new Version('5.2.0-e64b1e9');
 
 /**
  * @fileoverview added by tsickle
