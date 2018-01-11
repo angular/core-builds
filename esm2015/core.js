ce8b44050902285cb6d961767a0b8c580e00c43c
Alex Eagle <alexeagle@google.com>
2018-01-11 15:04:38 +0000

    55fd82e docs: fix release schedule date (#21469) PR Close #21469

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7f3d0bb
+ * @license Angular v5.2.0-55fd82e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-7f3d0bb');
+const VERSION = new Version('5.2.0-55fd82e');
 
 /**
  * @fileoverview added by tsickle
