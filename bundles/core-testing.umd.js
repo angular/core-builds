3d33917eaee76231c4b67ea8517ea2b5413893b5
Alex Eagle <alexeagle@google.com>
2018-01-11 01:25:17 +0000

    c6eb9ee build: update polyfill size (#21461)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-456f57d
+ * @license Angular v5.2.0-c6eb9ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-456f57d
+ * @license Angular v5.2.0-c6eb9ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
