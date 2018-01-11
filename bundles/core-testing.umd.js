d2f35d5514b562c5df8201f4550ea164baf0cb40
Alex Eagle <alexeagle@google.com>
2018-01-11 01:29:25 +0000

    7f3d0bb build: update polyfill size (#21461)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-3db02d2
+ * @license Angular v5.2.0-7f3d0bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-3db02d2
+ * @license Angular v5.2.0-7f3d0bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-3db02d2
+ * @license Angular v5.2.0-7f3d0bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-3db02d2
+ * @license Angular v5.2.0-7f3d0bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-3db02d2');
+var VERSION = new Version('5.2.0-7f3d0bb');
 
 /**
  * @fileoverview added by tsickle
