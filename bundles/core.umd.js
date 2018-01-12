aa560f502ea932dd268e9e706ff86cfb00176c51
Alex Eagle <alexeagle@google.com>
2018-01-12 19:06:03 +0000

    9b5a485 perf(ivy): add missing dom element in render3_function tree benchmark (#21476)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-50ca01e
+ * @license Angular v5.2.0-9b5a485
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-50ca01e
+ * @license Angular v5.2.0-9b5a485
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-50ca01e');
+var VERSION = new Version('5.2.0-9b5a485');
 
 /**
  * @fileoverview added by tsickle
