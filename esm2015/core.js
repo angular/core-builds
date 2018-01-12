1984c6939106c329d6f6a3b6b0b10c6edce156e9
Alex Eagle <alexeagle@google.com>
2018-01-12 18:24:57 +0000

    9005a6f feat(aio): implement survey notification link (#21371)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-3c6a506
+ * @license Angular v5.2.0-9005a6f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-3c6a506');
+const VERSION = new Version('5.2.0-9005a6f');
 
 /**
  * @fileoverview added by tsickle
