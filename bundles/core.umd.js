e132d0aa1abee6404748c4db82dfab7899e0f6db
Alex Eagle <alexeagle@google.com>
2018-01-12 20:28:12 +0000

    bb62458 docs: fix release schedule date (#21474)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-cffa0fe
+ * @license Angular v5.2.0-bb62458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-cffa0fe
+ * @license Angular v5.2.0-bb62458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-cffa0fe');
+var VERSION = new Version('5.2.0-bb62458');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-cffa0fe
+ * @license Angular v5.2.0-bb62458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-cffa0fe');
+const VERSION = new Version('5.2.0-bb62458');
 
 /**
  * @fileoverview added by tsickle
