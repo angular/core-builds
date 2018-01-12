680484d139bde58337d09e78f9733493afafaf95
Alex Eagle <alexeagle@google.com>
2018-01-12 23:07:17 +0000

    cc9419d fix(language-service): Clear caches when program changes (#21337)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d5393c7
+ * @license Angular v5.2.0-cc9419d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-d5393c7');
+const VERSION = new Version('5.2.0-cc9419d');
 
 /**
  * @fileoverview added by tsickle
