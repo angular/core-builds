422423a1ffe50e66008b52f472233af936e7e866
Alex Eagle <alexeagle@google.com>
2018-01-12 21:55:21 +0000

    8e9cd57 fix(common): set correct timezone for ISO8601 dates in Safari (#21506)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-f23896f
+ * @license Angular v5.2.0-8e9cd57
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-f23896f
+ * @license Angular v5.2.0-8e9cd57
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-f23896f');
+var VERSION = new Version('5.2.0-8e9cd57');
 
 /**
  * @fileoverview added by tsickle
