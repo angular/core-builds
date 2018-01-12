0f70a68ced4819b9fc66acc861991752fdb1a968
Alex Eagle <alexeagle@google.com>
2018-01-12 22:20:06 +0000

    977978e test(common): make date pipe tests work in more timezones (#21379)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-eb70966
+ * @license Angular v5.2.0-977978e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-eb70966');
+const VERSION = new Version('5.2.0-977978e');
 
 /**
  * @fileoverview added by tsickle
