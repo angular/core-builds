3db09145401f7429bb58744cc093f2c76da34c73
Alex Eagle <alexeagle@google.com>
2018-01-12 20:27:43 +0000

    f23896f docs: fix release schedule date (#21474)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-0e59d18
+ * @license Angular v5.2.0-f23896f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-0e59d18
+ * @license Angular v5.2.0-f23896f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-0e59d18');
+var VERSION = new Version('5.2.0-f23896f');
 
 /**
  * @fileoverview added by tsickle
