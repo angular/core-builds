da45ab51661ff1292dc3ade4e6d6b0a0561af367
Alex Eagle <alexeagle@google.com>
2018-01-12 18:49:34 +0000

    0e59d18 ci: add angular robot config file (#21489)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-54c8a32
+ * @license Angular v5.2.0-0e59d18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-54c8a32
+ * @license Angular v5.2.0-0e59d18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-54c8a32');
+var VERSION = new Version('5.2.0-0e59d18');
 
 /**
  * @fileoverview added by tsickle
