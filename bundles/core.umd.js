f26db6de690641646113a27c4cfdf7624892346f
Alex Eagle <alexeagle@google.com>
2018-01-12 23:03:44 +0000

    43e1520 fix(language-service): Clear caches when program changes (#21337)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-2d44a2a
+ * @license Angular v5.2.0-43e1520
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-2d44a2a
+ * @license Angular v5.2.0-43e1520
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-2d44a2a');
+var VERSION = new Version('5.2.0-43e1520');
 
 /**
  * @fileoverview added by tsickle
