bc3466e825903822b1aa24cdfd5e9c68b7572766
Alex Eagle <alexeagle@google.com>
2018-01-12 22:57:23 +0000

    71dd92b style(aio): enforce strict TypeScript checks (#21342)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-977978e
+ * @license Angular v5.2.0-71dd92b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-977978e
+ * @license Angular v5.2.0-71dd92b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-977978e');
+var VERSION = new Version('5.2.0-71dd92b');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-977978e
+ * @license Angular v5.2.0-71dd92b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-977978e');
+const VERSION = new Version('5.2.0-71dd92b');
 
 /**
  * @fileoverview added by tsickle
