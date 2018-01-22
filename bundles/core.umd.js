8075221d9b431635a4d7afbbbf49278bea6f54e1
Miško Hevery <misko@hevery.com>
2018-01-22 23:59:01 +0000

    95fbb7d build: Update to latest rules_typescript. (#21675)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-a64af40
+ * @license Angular v5.9.9-6-beta.0-95fbb7d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.9.9-6-beta.0-a64af40
+ * @license Angular v5.9.9-6-beta.0-95fbb7d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.9.9-6-beta.0-a64af40');
+var VERSION = new Version('5.9.9-6-beta.0-95fbb7d');
 
 /**
  * @fileoverview added by tsickle
