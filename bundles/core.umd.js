cd5729f88261a6f0a7c485b37160a533fc8e128b
Miško Hevery <misko@hevery.com>
2018-01-23 18:43:54 +0000

    21f7669 refactor(bazel): pass around tsconfig as a file, not a path (#21614)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-4b68fdc
+ * @license Angular v5.2.1-21f7669
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.1-4b68fdc
+ * @license Angular v5.2.1-21f7669
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.1-4b68fdc');
+var VERSION = new Version('5.2.1-21f7669');
 
 /**
  * @fileoverview added by tsickle
