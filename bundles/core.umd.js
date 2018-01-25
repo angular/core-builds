b1c014d2556da9c1bd2a9e73ec7067a127730e35
Misko Hevery <misko@hevery.com>
2018-01-25 06:30:23 +0000

    d08785d docs: add changelog for 6.0.0-beta.1

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-e4b11b2
+ * @license Angular v6.0.0-beta.1-d08785d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.9.9-6-beta.0-e4b11b2
+ * @license Angular v6.0.0-beta.1-d08785d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.9.9-6-beta.0-e4b11b2');
+var VERSION = new Version('6.0.0-beta.1-d08785d');
 
 /**
  * @fileoverview added by tsickle
