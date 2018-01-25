7899e631040af3659bda8b17b5d6924492d4c858
Miško Hevery <misko@hevery.com>
2018-01-25 22:02:35 +0000

    23596b3 docs(aio): fix missing stylesheet in component-styles example (#21772)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-5269ce2
+ * @license Angular v6.0.0-beta.1-23596b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.1-5269ce2
+ * @license Angular v6.0.0-beta.1-23596b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-5269ce2');
+var VERSION = new Version('6.0.0-beta.1-23596b3');
 
 /**
  * @fileoverview added by tsickle
