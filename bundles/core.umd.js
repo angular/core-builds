c291f93899a0e73e6f0daa05ea7d20fb3831d9c2
Miško Hevery <misko@hevery.com>
2018-01-26 19:03:33 +0000

    ba045e8 docs: add notes on email used for CLA (#21754)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-35a0721
+ * @license Angular v5.2.2-ba045e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-35a0721
+ * @license Angular v5.2.2-ba045e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-35a0721');
+var VERSION = new Version('5.2.2-ba045e8');
 
 /**
  * @fileoverview added by tsickle
