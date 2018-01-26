de39266b0a8b06e0a91eaf1d4e560c0d6d75f464
Miško Hevery <misko@hevery.com>
2018-01-26 18:50:56 +0000

    108fa15 fix(aio): close SideNav on non-sidenav doc on wide screen (#21538)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-e608052
+ * @license Angular v6.0.0-beta.1-108fa15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.1-e608052
+ * @license Angular v6.0.0-beta.1-108fa15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-e608052');
+var VERSION = new Version('6.0.0-beta.1-108fa15');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-e608052
+ * @license Angular v6.0.0-beta.1-108fa15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-e608052');
+const VERSION = new Version('6.0.0-beta.1-108fa15');
 
 /**
  * @fileoverview added by tsickle
