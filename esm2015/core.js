d8aaeb47af65f2605df651cf562d4e1490bed190
Jason Aden <jasonaden@google.com>
2018-01-26 23:19:48 +0000

    e0b31db build: merge-pr now checks that PR status is green before proceeding (#21810)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-8df56fe
+ * @license Angular v6.0.0-beta.1-e0b31db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-8df56fe');
+const VERSION = new Version('6.0.0-beta.1-e0b31db');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-8df56fe
+ * @license Angular v6.0.0-beta.1-e0b31db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-8df56fe');
+var VERSION = new Version('6.0.0-beta.1-e0b31db');
 
 /**
  * @fileoverview added by tsickle
