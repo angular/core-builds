31ea3a6bf6ed8118326b0296f89a88116070ccc4
Jason Aden <jasonaden@google.com>
2018-01-26 23:24:07 +0000

    7410941 build: merge-pr now checks that PR status is green before proceeding (#21810)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-250c8da
+ * @license Angular v5.2.2-7410941
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-250c8da
+ * @license Angular v5.2.2-7410941
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-250c8da');
+var VERSION = new Version('5.2.2-7410941');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-250c8da
+ * @license Angular v5.2.2-7410941
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.2-250c8da');
+const VERSION = new Version('5.2.2-7410941');
 
 /**
  * @fileoverview added by tsickle
