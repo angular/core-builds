540f1185d1e2db5e1d295f31a5f1eac6ffe092a1
Misko Hevery <misko@hevery.com>
2018-01-27 19:19:43 +0000

    6a7689d build: update to latest bazel rules (#21821)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-696ba01
+ * @license Angular v5.2.2-6a7689d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.2-696ba01
+ * @license Angular v5.2.2-6a7689d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
