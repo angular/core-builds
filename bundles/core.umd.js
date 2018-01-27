c0d4cdd52bce15c78c03e7629eae44b436df0f5c
Misko Hevery <misko@hevery.com>
2018-01-27 19:09:47 +0000

    6245637 fix(ivy): correct query read logic after merges (#21749)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-ab69f12
+ * @license Angular v6.0.0-beta.1-6245637
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.1-ab69f12
+ * @license Angular v6.0.0-beta.1-6245637
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-ab69f12');
+var VERSION = new Version('6.0.0-beta.1-6245637');
 
 /**
  * @fileoverview added by tsickle
