6a66ca4e9724647c9964a92fb0c1542d2fc7252e
Jason Aden <jasonaden@google.com>
2018-01-27 00:29:42 +0000

    696ba01 fix(aio): don't set noindex metatag in the static index.html (#21816)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-81d64d6
+ * @license Angular v5.2.2-696ba01
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-81d64d6
+ * @license Angular v5.2.2-696ba01
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-81d64d6');
+var VERSION = new Version('5.2.2-696ba01');
 
 /**
  * @fileoverview added by tsickle
