6a6b45d95a5379c3333d15ae3284cde9420d9b28
Jason Aden <jasonaden@google.com>
2018-01-29 20:16:34 +0000

    3f5ead3 fix(aio): missing plural s in preserveWhiteSpaces example (#21854)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-a89e709
+ * @license Angular v5.2.2-3f5ead3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-a89e709
+ * @license Angular v5.2.2-3f5ead3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-a89e709');
+var VERSION = new Version('5.2.2-3f5ead3');
 
 /**
  * @fileoverview added by tsickle
