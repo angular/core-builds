6acc2d599acc5a62c2872f57909d8ad467cb9bd5
Jason Aden <jasonaden@google.com>
2018-01-29 20:07:30 +0000

    18174e5 feat(ivy): support ng-content projection in the ivy compiler (#21764)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-72265f7
+ * @license Angular v6.0.0-beta.1-18174e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.1-72265f7
+ * @license Angular v6.0.0-beta.1-18174e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-72265f7');
+var VERSION = new Version('6.0.0-beta.1-18174e5');
 
 /**
  * @fileoverview added by tsickle
