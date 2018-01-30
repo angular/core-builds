0c50eaaf8ff3b9c8d8a1cbd62268abc901a190bf
Jason Aden <jasonaden@google.com>
2018-01-30 00:33:16 +0000

    11b1267 docs: document debugging a Node test in VSCode/Bazel (#21868)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-dd48df1
+ * @license Angular v6.0.0-beta.1-11b1267
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.1-dd48df1
+ * @license Angular v6.0.0-beta.1-11b1267
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-dd48df1');
+var VERSION = new Version('6.0.0-beta.1-11b1267');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-dd48df1
+ * @license Angular v6.0.0-beta.1-11b1267
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-dd48df1');
+var VERSION = new Version('6.0.0-beta.1-11b1267');
 
 /**
  * @fileoverview added by tsickle
