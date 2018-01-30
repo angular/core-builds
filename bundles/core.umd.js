13ffc0dd19701e04891100a403cb914f8702824b
Jason Aden <jasonaden@google.com>
2018-01-30 20:04:04 +0000

    c2f5ed5 fix(common): generate closure-locale data file with exported plural functions (#21873)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-5d75df8
+ * @license Angular v5.2.2-c2f5ed5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-5d75df8
+ * @license Angular v5.2.2-c2f5ed5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-5d75df8');
+var VERSION = new Version('5.2.2-c2f5ed5');
 
 /**
  * @fileoverview added by tsickle
