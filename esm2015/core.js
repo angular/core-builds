e7cff7d5c76fca81f21e5486d1eef979825715e3
Jason Aden <jasonaden@google.com>
2018-01-31 18:45:23 +0000

    c83c416 build(aio): upgrade to dgeni-packages 0.24.0 (#21802)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-9dca5f2
+ * @license Angular v6.0.0-beta.1-c83c416
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-9dca5f2');
+const VERSION = new Version('6.0.0-beta.1-c83c416');
 
 /**
  * @fileoverview added by tsickle
