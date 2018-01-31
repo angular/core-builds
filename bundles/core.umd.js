bcc576ea326c77f0f308cb11f8763e3e1a74ed8c
Jason Aden <jasonaden@google.com>
2018-01-31 19:19:16 +0000

    2aa460b docs: add http guide sample and adjust text (#21326)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-e0022ae
+ * @license Angular v5.2.2-2aa460b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-e0022ae
+ * @license Angular v5.2.2-2aa460b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-e0022ae');
+var VERSION = new Version('5.2.2-2aa460b');
 
 /**
  * @fileoverview added by tsickle
