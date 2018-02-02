49e5e7b9b5a827d42a454c02eeb60df5b2aef227
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:22:34 +0000

    f816666 fix(ivy): generate lifecycle pattern (#21865)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-1242839
+ * @license Angular v6.0.0-beta.2-f816666
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.2-1242839
+ * @license Angular v6.0.0-beta.2-f816666
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-1242839');
+var VERSION = new Version('6.0.0-beta.2-f816666');
 
 /**
  * @fileoverview added by tsickle
