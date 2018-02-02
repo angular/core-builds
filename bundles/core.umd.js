c0f82bf2117d7fb6e8811d47dca27e0cbd2fb2ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:39:30 +0000

    11ec80a docs: add docs for IE (#21824)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-75eecdc
+ * @license Angular v5.2.3-11ec80a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-75eecdc
+ * @license Angular v5.2.3-11ec80a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-75eecdc');
+var VERSION = new Version('5.2.3-11ec80a');
 
 /**
  * @fileoverview added by tsickle
