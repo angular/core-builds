00dd926a0a2dc87cf486918f689bc7b709658398
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:12:23 +0000

    3cc1d76 fix(ivy): generate correct interpolations (#21946)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-65cf1ad
+ * @license Angular v6.0.0-beta.2-3cc1d76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-65cf1ad');
+var VERSION = new Version('6.0.0-beta.2-3cc1d76');
 
 /**
  * @fileoverview added by tsickle
