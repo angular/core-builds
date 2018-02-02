5f5e6525ad7da14a4502a948f274078677a99f32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:11:02 +0000

    965eecc build(aio): move zip and live-example generation to `yarn predocs` task (#21970)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-c4fb696');
+var VERSION = new Version('5.2.3-965eecc');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.3-c4fb696');
+const VERSION = new Version('5.2.3-965eecc');
 
 /**
  * @fileoverview added by tsickle
