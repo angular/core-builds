e7b8f3639bb851bb61ecb4bba5fc78943f844fda
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:22:57 +0000

    f791e9f fix(core): fix #20582, don't need to wrap zone in location change listener (#20640)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-3aa7e02
+ * @license Angular v6.0.0-beta.2-f791e9f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -688,7 +688,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.2-3aa7e02');
+const VERSION = new Version('6.0.0-beta.2-f791e9f');
 
 /**
  * @fileoverview added by tsickle
