a324f7dc507e129f44d850a6bbf6c26601186e97
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:46:34 +0000

    973607f ci: mark PRs with rejection as not green (#21922) PR Close #21922

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-b155ae1
+ * @license Angular v5.2.3-973607f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -716,7 +716,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-b155ae1');
+var VERSION = new Version('5.2.3-973607f');
 
 /**
  * @fileoverview added by tsickle
