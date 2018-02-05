2f770196e3230c846fee156bad262bd0a77e532d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:00:15 +0000

    664f7fa build(aio): add API static members to search index (#21988)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-973607f
+ * @license Angular v5.2.3-664f7fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-973607f
+ * @license Angular v5.2.3-664f7fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-973607f');
+var VERSION = new Version('5.2.3-664f7fa');
 
 /**
  * @fileoverview added by tsickle
