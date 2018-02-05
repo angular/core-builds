84ecfe2f489a461cae10a1406bcc3b554aaa8384
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:04:35 +0000

    adb1d62 docs: clarify npm/yarn commands, add blank lines to mix md/html in table (#21606)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-664f7fa
+ * @license Angular v5.2.3-adb1d62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-664f7fa
+ * @license Angular v5.2.3-adb1d62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-664f7fa');
+var VERSION = new Version('5.2.3-adb1d62');
 
 /**
  * @fileoverview added by tsickle
