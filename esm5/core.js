3ed8ed83366c076adac5b7a3af0ada7a851b755b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-06 16:31:08 +0000

    4d62be6 feat(ivy): memoize array literals in render3 (#21973)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-2707012
+ * @license Angular v6.0.0-beta.2-4d62be6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -723,7 +723,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-2707012');
+var VERSION = new Version('6.0.0-beta.2-4d62be6');
 
 /**
  * @fileoverview added by tsickle
