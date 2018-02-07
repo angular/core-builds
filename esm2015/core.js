e58b1a242f2964c92bc8a9178a3dc0574fdd3a78
Miško Hevery <misko@hevery.com>
2018-02-07 20:25:13 +0000

    44ea80b docs(forms): Custom Validator example selector name incorrect. (#20464)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-530b824
+ * @license Angular v5.2.4-44ea80b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-530b824');
+const VERSION = new Version('5.2.4-44ea80b');
 
 /**
  * @fileoverview added by tsickle
