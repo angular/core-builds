0c1d0af168cab5b4ebbc060ab7330a1626977b7c
Miško Hevery <misko@hevery.com>
2018-02-07 20:57:50 +0000

    56b9591 fix(forms): prevent event emission on enable/disable when emitEvent is false (#12366) (#21018)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-103727a
+ * @license Angular v5.2.4-56b9591
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.4-103727a
+ * @license Angular v5.2.4-56b9591
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-103727a');
+var VERSION = new Version('5.2.4-56b9591');
 
 /**
  * @fileoverview added by tsickle
