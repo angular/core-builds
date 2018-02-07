ec18ffe67a700d88008e817188949a8db1f080bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-07 19:59:17 +0000

    5b06069 docs: add changelog for 6.0.0-beta.3

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-370ab66
+ * @license Angular v6.0.0-beta.3-5b06069
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.2-370ab66');
+const VERSION = new Version('6.0.0-beta.3-5b06069');
 
 /**
  * @fileoverview added by tsickle
