a16f479fa4936b22d484c0560d8d33c9cf4a279e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-07 19:41:25 +0000

    530b824 docs: add changelog for 5.2.4

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c6645e7
+ * @license Angular v5.2.4-530b824
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-c6645e7');
+var VERSION = new Version('5.2.4-530b824');
 
 /**
  * @fileoverview added by tsickle
