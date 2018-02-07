63aa5a6f1320cdb707a4ac781561958883551eac
Miško Hevery <misko@hevery.com>
2018-02-07 20:52:00 +0000

    0bcfae7 fix(forms): prevent event emission on enable/disable when emitEvent is false (#12366) (#21018)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-140e7c0
+ * @license Angular v6.0.0-beta.3-0bcfae7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -723,7 +723,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-140e7c0');
+var VERSION = new Version('6.0.0-beta.3-0bcfae7');
 
 /**
  * @fileoverview added by tsickle
