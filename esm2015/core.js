7c403950fa1a0c0501bc6560a2e4447e3718dc4b
Miško Hevery <misko@hevery.com>
2018-02-08 18:25:19 +0000

    dcf64a0 fix(bazel): improve error message for missing assets (#22096)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-a9545ab
+ * @license Angular v6.0.0-beta.3-dcf64a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-a9545ab');
+const VERSION = new Version('6.0.0-beta.3-dcf64a0');
 
 /**
  * @fileoverview added by tsickle
