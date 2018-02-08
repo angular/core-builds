f26f6ee53ad9deda860c9820f6e46d6bed47f1f4
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 17:18:32 +0000

    a751649 fix(core): use appropriate inert document strategy for Firefox & Safari (#17019)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-3f5a3d6
+ * @license Angular v6.0.0-beta.3-a751649
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-3f5a3d6
+ * @license Angular v6.0.0-beta.3-a751649
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -761,7 +761,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-3f5a3d6');
+var VERSION = new Version('6.0.0-beta.3-a751649');
 
 /**
  * @fileoverview added by tsickle
