7ea582b1c44fde57d1443d3f660575b7635d69f5
Miško Hevery <misko@hevery.com>
2018-02-08 18:21:53 +0000

    c5ec8d9 fix(bazel): improve error message for missing assets (#22096)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-de561f3
+ * @license Angular v5.2.4-c5ec8d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-de561f3');
+const VERSION = new Version('5.2.4-c5ec8d9');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-de561f3
+ * @license Angular v5.2.4-c5ec8d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-de561f3');
+var VERSION = new Version('5.2.4-c5ec8d9');
 
 /**
  * @fileoverview added by tsickle
