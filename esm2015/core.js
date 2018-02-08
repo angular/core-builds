44710a42cdbe662c9d5d4553c2b91ad178f37300
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:43:27 +0000

    67cf712 docs(aio): remove ngATL banner from homepage (#22060)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-16e5b86
+ * @license Angular v6.0.0-beta.3-67cf712
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-16e5b86');
+const VERSION = new Version('6.0.0-beta.3-67cf712');
 
 /**
  * @fileoverview added by tsickle
