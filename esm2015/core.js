4bb5d4e6d9e8885c200873bbb973e5e4ca52f18a
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:46:00 +0000

    799edd1 docs(aio): remove lifecycle hooks img (#21425)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-ffcb5dd
+ * @license Angular v5.2.4-799edd1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-ffcb5dd');
+const VERSION = new Version('5.2.4-799edd1');
 
 /**
  * @fileoverview added by tsickle
