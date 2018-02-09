8baf243339fec052bc95c15a6548e9468206d5f5
Miško Hevery <misko@hevery.com>
2018-02-09 21:37:37 +0000

    aa456ed refactor(ivy): validate that identifier identity in emitted output (#21877)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-bc1e229
+ * @license Angular v6.0.0-beta.3-aa456ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-bc1e229
+ * @license Angular v6.0.0-beta.3-aa456ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -761,7 +761,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-bc1e229');
+var VERSION = new Version('6.0.0-beta.3-aa456ed');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-bc1e229
+ * @license Angular v6.0.0-beta.3-aa456ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-bc1e229');
+const VERSION = new Version('6.0.0-beta.3-aa456ed');
 
 /**
  * @fileoverview added by tsickle
