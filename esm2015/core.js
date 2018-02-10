ade9cd8f1abf93d1422a2132436cd04ef44e944a
Misko Hevery <misko@hevery.com>
2018-02-10 04:32:27 +0000

    92d7060 Revert "build(bazel): allow ng_modules to elide .ngsummary.closure.js files (#22107)"

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7e9b120
+ * @license Angular v6.0.0-beta.3-92d7060
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-7e9b120');
+const VERSION = new Version('6.0.0-beta.3-92d7060');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7e9b120
+ * @license Angular v6.0.0-beta.3-92d7060
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -723,7 +723,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-7e9b120');
+var VERSION = new Version('6.0.0-beta.3-92d7060');
 
 /**
  * @fileoverview added by tsickle
