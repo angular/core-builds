6b41a8bd777ee69b06fbd6e90cedab41e75abc29
Miško Hevery <misko@hevery.com>
2018-02-12 18:44:07 +0000

    0ed64af docs(aio): put structural directives back in the nav (#21856)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-a616dd6
+ * @license Angular v5.2.4-0ed64af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-a616dd6');
+var VERSION = new Version('5.2.4-0ed64af');
 
 /**
  * @fileoverview added by tsickle
