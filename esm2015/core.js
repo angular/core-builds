f06fa6525979a4855cd0a4560d655cc34a419436
Miško Hevery <misko@hevery.com>
2018-02-12 22:55:15 +0000

    7f9b1b7 docs(aio): add angular-playground to resources (#22042)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-1e94846
+ * @license Angular v6.0.0-beta.3-7f9b1b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-1e94846');
+const VERSION = new Version('6.0.0-beta.3-7f9b1b7');
 
 /**
  * @fileoverview added by tsickle
