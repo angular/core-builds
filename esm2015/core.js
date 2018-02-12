aea4919ad9213235b932d143cbb2f03ad4c3b6c7
Miško Hevery <misko@hevery.com>
2018-02-12 18:57:10 +0000

    1e94846 docs(aio): add angular.schule to resources (#22164)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-88bec23
+ * @license Angular v6.0.0-beta.3-1e94846
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -689,7 +689,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-88bec23');
+const VERSION = new Version('6.0.0-beta.3-1e94846');
 
 /**
  * @fileoverview added by tsickle
