b39063905e86f53a9e1f253b824fb4bf575357ee
Miško Hevery <misko@hevery.com>
2018-02-12 18:33:44 +0000

    a616dd6 fix(aio): remove broken span closing tag (#22146)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-d57fd0b
+ * @license Angular v5.2.4-a616dd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.4-d57fd0b
+ * @license Angular v5.2.4-a616dd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-d57fd0b');
+var VERSION = new Version('5.2.4-a616dd6');
 
 /**
  * @fileoverview added by tsickle
