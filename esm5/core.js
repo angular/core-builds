cef3710cd80d7361dca05a7df58c70e92a17b25a
Miško Hevery <misko@hevery.com>
2018-02-13 19:06:32 +0000

    b75cf3f ci: update ngbot config file (#22173)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-4f19491
+ * @license Angular v6.0.0-beta.3-b75cf3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2030,7 +2030,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-4f19491');
+var VERSION = new Version('6.0.0-beta.3-b75cf3f');
 
 /**
  * @fileoverview added by tsickle
