d577290d20b4db4a29a77537790013795fb3536b
Miško Hevery <misko@hevery.com>
2018-02-13 00:20:11 +0000

    eb48750 docs(aio): fix typo in "preserveWhitespaces" example (#22182)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-b333919
+ * @license Angular v6.0.0-beta.3-eb48750
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-b333919
+ * @license Angular v6.0.0-beta.3-eb48750
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2068,7 +2068,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-b333919');
+var VERSION = new Version('6.0.0-beta.3-eb48750');
 
 /**
  * @fileoverview added by tsickle
