ea1f9b1b59892e2258edf03629b5d8f8fe7c0d4c
Miško Hevery <misko@hevery.com>
2018-02-13 00:17:49 +0000

    be59c3a fix(common): weaken AsyncPipe transform signature (#22169)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-235a235
+ * @license Angular v6.0.0-beta.3-be59c3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1858,7 +1858,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-235a235');
+const VERSION = new Version('6.0.0-beta.3-be59c3a');
 
 /**
  * @fileoverview added by tsickle
