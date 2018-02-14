c31b6bc8705f5f2a630ca480f975cfd3f8ddb886
Misko Hevery <misko@hevery.com>
2018-02-14 06:06:50 +0000

    fa7d890 docs: add changelog for 6.0.0-beta.4

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-a589ca0
+ * @license Angular v6.0.0-beta.4-fa7d890
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1858,7 +1858,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-a589ca0');
+const VERSION = new Version('6.0.0-beta.4-fa7d890');
 
 /**
  * @fileoverview added by tsickle
