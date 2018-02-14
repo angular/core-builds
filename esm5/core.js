eec21643a300bfa39a3750dcbc468c6aed07a4df
Misko Hevery <misko@hevery.com>
2018-02-14 05:53:42 +0000

    106b435 docs: add changelog for 5.2.5

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-c4f841f
+ * @license Angular v5.2.5-106b435
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-c4f841f');
+var VERSION = new Version('5.2.5-106b435');
 
 /**
  * @fileoverview added by tsickle
