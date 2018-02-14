fb895ca0dc7e0ba3b3d74a4081ef075943fcda1e
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 22:49:05 +0000

    5bb9fca build: comment-out chromium version checking code temporarily (#22232)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-fa7d890
+ * @license Angular v6.0.0-beta.4-5bb9fca
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1858,7 +1858,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-fa7d890');
+const VERSION = new Version('6.0.0-beta.4-5bb9fca');
 
 /**
  * @fileoverview added by tsickle
