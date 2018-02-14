dd84a8e47794ba39c95f7ae903af9da778c487c3
Misko Hevery <misko@hevery.com>
2018-02-14 06:12:27 +0000

    fa7d890 docs: add changelog for 6.0.0-beta.4

---
+++ core.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { AnimationMetadataType as ɵbi, animate as ɵbk, group as ɵbl, keyframes as ɵbp, sequence as ɵbm, state as ɵbo, style as ɵbn, transition as ɵbq, trigger as ɵbj } from './src/animation/dsl';
+export { _iterableDiffersFactory as ɵn, _keyValueDiffersFactory as ɵo, _localeFactory as ɵq } from './src/application_module';
+export { _appIdRandomProviderFactory as ɵh } from './src/application_tokens';
+export { defaultIterableDiffers as ɵi, defaultKeyValueDiffers as ɵj } from './src/change_detection/change_detection';
+export { DefaultIterableDifferFactory as ɵk } from './src/change_detection/differs/default_iterable_differ';
+export { DefaultKeyValueDifferFactory as ɵl } from './src/change_detection/differs/default_keyvalue_differ';
+export { ReflectiveInjector_ as ɵd } from './src/di/reflective_injector';
+export { ReflectiveDependency as ɵf, resolveReflectiveProviders as ɵg } from './src/di/reflective_provider';
+export { wtfEnabled as ɵu } from './src/profile/profile';
+export { createScope as ɵx, detectWTF as ɵw, endTimeRange as ɵba, leave as ɵy, startTimeRange as ɵz } from './src/profile/wtf_impl';
+export { getOrCreateContainerRef as ɵbf, getOrCreateInjectable as ɵbe, getOrCreateNodeInjector as ɵbd, getOrCreateTemplateRef as ɵbg } from './src/render3/di';
+export { stringify as ɵbh } from './src/render3/util';
+export { makeParamDecorator as ɵa, makePropDecorator as ɵc } from './src/util/decorators';
+export { _def as ɵbb } from './src/view/provider';
+export { DebugContext as ɵbc } from './src/view/types';
