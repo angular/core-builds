b07533fd5809a6b3e5a7a83735f86a069a07e925
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 23:54:12 +0000

    0c88d5d style: fix typos boostrap to bootstrap (#21917)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-0b8b06e
+ * @license Angular v5.2.5-0c88d5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-0b8b06e');
+const VERSION = new Version('5.2.5-0c88d5d');
 
 /**
  * @fileoverview added by tsickle
