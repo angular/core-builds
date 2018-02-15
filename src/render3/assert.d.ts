326434902b2dbab6506d16470de4c0bdb1aea82a
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 23:13:27 +0000

    f628797 Revert "refactor(ivy): assertion (#22189)"

--- src/render3/assert.d.ts
+++ src/render3/assert.d.ts
@@ -1,14 +1,15 @@
+export declare function assertNumber(actual: any, name: string): void;
+export declare function assertEqual<T>(actual: T, expected: T, name: string, serializer?: ((v: T) => string)): void;
+export declare function assertLessThan<T>(actual: T, expected: T, name: string): void;
+export declare function assertNotNull<T>(actual: T, name: string): void;
+export declare function assertNotEqual<T>(actual: T, expected: T, name: string): void;
 /**
- * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Throws an error with a message constructed from the arguments.
  *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * @param actual The actual value (e.g. 3)
+ * @param expected The expected value (e.g. 5)
+ * @param name The name of the value being checked (e.g. attrs.length)
+ * @param operator The comparison operator (e.g. <, >, ==)
+ * @param serializer Function that maps a value to its display value
  */
-export declare function assertNumber(actual: any, msg: string): void;
-export declare function assertEqual<T>(actual: T, expected: T, msg: string): void;
-export declare function assertNotEqual<T>(actual: T, expected: T, msg: string): void;
-export declare function assertSame<T>(actual: T, expected: T, msg: string): void;
-export declare function assertLessThan<T>(actual: T, expected: T, msg: string): void;
-export declare function assertNull<T>(actual: T, msg: string): void;
-export declare function assertNotNull<T>(actual: T, msg: string): void;
+export declare function assertThrow<T>(actual: T, expected: T, name: string, operator: string, serializer?: ((v: T) => string)): never;
