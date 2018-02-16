26c820ef83c2950066254f30fc48933978b46003
Victor Berchet <victor@suumit.com>
2018-02-16 23:30:34 +0000

    ce5e8fa fix(common): correct mapping of Observable methods (#20518)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-185a6ab
+ * @license Angular v5.2.5-ce5e8fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-185a6ab');
+var VERSION = new Version('5.2.5-ce5e8fa');
 
 /**
  * @fileoverview added by tsickle
