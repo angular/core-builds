1d5c862ae14f698b791d10a9dbd07493f11901c9
Victor Berchet <victor@suumit.com>
2018-02-16 23:34:31 +0000

    2639b4b fix(common): correct mapping of Observable methods (#20518)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f1a0632
+ * @license Angular v6.0.0-beta.4-2639b4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1858,7 +1858,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-f1a0632');
+const VERSION = new Version('6.0.0-beta.4-2639b4b');
 
 /**
  * @fileoverview added by tsickle
