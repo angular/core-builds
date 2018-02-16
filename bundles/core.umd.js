239103df71cd64f6fa5d2f5da221045262af63cc
Victor Berchet <victor@suumit.com>
2018-02-16 17:34:29 +0000

    f1a0632 feat(core): set preserveWhitespaces to false by default (#22046)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-d241532
+ * @license Angular v6.0.0-beta.4-f1a0632
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-d241532
+ * @license Angular v6.0.0-beta.4-f1a0632
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2068,7 +2068,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-d241532');
+var VERSION = new Version('6.0.0-beta.4-f1a0632');
 
 /**
  * @fileoverview added by tsickle
