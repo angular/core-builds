92e01d34b2188b14b3c40ce011eacaa836eb6eac
Victor Berchet <victor@suumit.com>
2018-02-18 21:40:55 +0000

    4ec40c6 fix(aio): improve announcement-bar layout with wide logos (#22272)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-7fa2d4b
+ * @license Angular v6.0.0-beta.4-4ec40c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1858,7 +1858,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-7fa2d4b');
+const VERSION = new Version('6.0.0-beta.4-4ec40c6');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-7fa2d4b
+ * @license Angular v6.0.0-beta.4-4ec40c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2030,7 +2030,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-7fa2d4b');
+var VERSION = new Version('6.0.0-beta.4-4ec40c6');
 
 /**
  * @fileoverview added by tsickle
