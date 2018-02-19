c770bf4e53208837b2d88d0e24bdc3daf2cee26a
Victor Berchet <victor@suumit.com>
2018-02-19 04:10:30 +0000

    cdfedc1 fix(common): fix merge error in ng_if.ts

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c726d1d
+ * @license Angular v5.2.5-cdfedc1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-c726d1d');
+const VERSION = new Version('5.2.5-cdfedc1');
 
 /**
  * @fileoverview added by tsickle
