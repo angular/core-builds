c0efbadc447988138ac20350eb3b236b630bab3b
Victor Berchet <victor@suumit.com>
2018-02-20 18:32:53 +0000

    a346d28 test(language-service): fix minor typos (#21372) PR Close #21372

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-09b4612
+ * @license Angular v5.2.5-a346d28
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-09b4612');
+const VERSION = new Version('5.2.5-a346d28');
 
 /**
  * @fileoverview added by tsickle
