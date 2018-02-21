ddcac486993ceb4c551f7147ba124be23f00e422
Victor Berchet <victor@suumit.com>
2018-02-21 00:03:24 +0000

    236a932 build: update tsickle dep from compiler-cli (#22295) PR Close #22295

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-99909bb
+ * @license Angular v6.0.0-beta.4-236a932
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-99909bb
+ * @license Angular v6.0.0-beta.4-236a932
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2068,7 +2068,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-99909bb');
+var VERSION = new Version('6.0.0-beta.4-236a932');
 
 /**
  * @fileoverview added by tsickle
