1f1d1d2a0b6de97eceaa647387d7720a968715e9
Victor Berchet <victor@suumit.com>
2018-02-21 19:27:29 +0000

    484802c build: make git revert messages valid (#22339)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-ee53577
+ * @license Angular v5.2.5-484802c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-ee53577');
+const VERSION = new Version('5.2.5-484802c');
 
 /**
  * @fileoverview added by tsickle
