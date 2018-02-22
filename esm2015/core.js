a9044e213d6b5d34851f6b8b858558b7e5c7b095
Victor Berchet <victor@suumit.com>
2018-02-22 01:43:47 +0000

    6091a95 docs: add changelog for 6.0.0-beta.5

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-5c89d6b
+ * @license Angular v6.0.0-beta.5-6091a95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-5c89d6b');
+const VERSION = new Version('6.0.0-beta.5-6091a95');
 
 /**
  * @fileoverview added by tsickle
