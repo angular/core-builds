79f0d587fd390249105e2bc991490bef52194c11
Victor Berchet <victor@suumit.com>
2018-02-22 19:43:46 +0000

    e86b64b feat(forms): allow markAsPending to emit events (#20212)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-90e9c59
+ * @license Angular v6.0.0-beta.5-e86b64b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1858,7 +1858,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-90e9c59');
+const VERSION = new Version('6.0.0-beta.5-e86b64b');
 
 /**
  * @fileoverview added by tsickle
