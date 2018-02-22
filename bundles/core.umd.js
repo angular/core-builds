4e9ee59686573eb3bb14633c7df26a2ac1572fe8
Victor Berchet <victor@suumit.com>
2018-02-22 22:20:56 +0000

    45eff4c fix(router): don't mutate route configs (#22358)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b3ffeaa
+ * @license Angular v6.0.0-beta.5-45eff4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-b3ffeaa
+ * @license Angular v6.0.0-beta.5-45eff4c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2072,7 +2072,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-b3ffeaa');
+var VERSION = new Version('6.0.0-beta.5-45eff4c');
 
 /**
  * @fileoverview added by tsickle
