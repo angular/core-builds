1d3d8a26883a38981fd9808475811b8a5433bded
Victor Berchet <victor@suumit.com>
2018-02-22 21:55:40 +0000

    f194d00 build: disable bazel-out symlink (#22375)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b7544cc
+ * @license Angular v6.0.0-beta.5-f194d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-b7544cc
+ * @license Angular v6.0.0-beta.5-f194d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2072,7 +2072,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-b7544cc');
+var VERSION = new Version('6.0.0-beta.5-f194d00');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b7544cc
+ * @license Angular v6.0.0-beta.5-f194d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-b7544cc');
+const VERSION = new Version('6.0.0-beta.5-f194d00');
 
 /**
  * @fileoverview added by tsickle
