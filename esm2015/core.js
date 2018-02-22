e38a0f97ba596940e45558ac0988625815d060d4
Victor Berchet <victor@suumit.com>
2018-02-22 22:28:35 +0000

    150bac3 docs: fix deployment sample path (#22048)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-1776239
+ * @license Angular v5.2.6-150bac3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.6-1776239');
+const VERSION = new Version('5.2.6-150bac3');
 
 /**
  * @fileoverview added by tsickle
