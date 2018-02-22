daefae642413035d885a29b44ae8dc8e8790e9a9
Victor Berchet <victor@suumit.com>
2018-02-22 18:45:34 +0000

    ca06af4 build: allow passing node options to ngc. (#22245)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-6091a95
+ * @license Angular v6.0.0-beta.5-ca06af4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-6091a95
+ * @license Angular v6.0.0-beta.5-ca06af4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2072,7 +2072,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-6091a95');
+var VERSION = new Version('6.0.0-beta.5-ca06af4');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-6091a95
+ * @license Angular v6.0.0-beta.5-ca06af4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-6091a95');
+const VERSION = new Version('6.0.0-beta.5-ca06af4');
 
 /**
  * @fileoverview added by tsickle
