51f5396a65e9b144bcd56c3fb178fcc96e02838a
Victor Berchet <victor@suumit.com>
2018-02-23 00:32:01 +0000

    b2f366b fix(animations): only use the WA-polyfill alongside AnimationBuilder (#22143)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-9eecb0b
+ * @license Angular v6.0.0-beta.5-b2f366b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-9eecb0b
+ * @license Angular v6.0.0-beta.5-b2f366b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2072,7 +2072,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-9eecb0b');
+var VERSION = new Version('6.0.0-beta.5-b2f366b');
 
 /**
  * @fileoverview added by tsickle
