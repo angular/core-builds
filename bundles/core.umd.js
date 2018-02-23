b18f575cae8a5916a50b1011706bc553bcdf60dc
Victor Berchet <victor@suumit.com>
2018-02-23 18:16:29 +0000

    17c1577 ci: don't use bazel git_repository rule (#22406)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-150bac3
+ * @license Angular v5.2.6-17c1577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.6-150bac3
+ * @license Angular v5.2.6-17c1577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.6-150bac3');
+var VERSION = new Version('5.2.6-17c1577');
 
 /**
  * @fileoverview added by tsickle
