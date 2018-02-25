98dc6c16981691fba527788860aa85bd0892d4e2
Victor Berchet <victor@suumit.com>
2018-02-25 18:26:47 +0000

    13ab91e fix(upgrade): fix empty transclusion content with AngularJS@>=1.5.8 (#22167)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a403229
+ * @license Angular v6.0.0-beta.5-13ab91e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-a403229');
+const VERSION = new Version('6.0.0-beta.5-13ab91e');
 
 /**
  * @fileoverview added by tsickle
