f70b6408706245bff437b479dcc7a641f56335d7
Victor Berchet <victor@suumit.com>
2018-02-25 18:30:45 +0000

    a9a0e27 fix(upgrade): fix empty transclusion content with AngularJS@>=1.5.8 (#22167)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-4a08745
+ * @license Angular v5.2.6-a9a0e27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.6-4a08745
+ * @license Angular v5.2.6-a9a0e27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.6-4a08745');
+var VERSION = new Version('5.2.6-a9a0e27');
 
 /**
  * @fileoverview added by tsickle
