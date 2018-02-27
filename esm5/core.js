911b7f2b53d58483d9a43c3214aae9abb9246d07
Alex Eagle <alexeagle@google.com>
2018-02-27 02:11:43 +0000

    ef99126 test(aio): move forgotten e2e test to the correct folder (#22445)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-40ba009
+ * @license Angular v6.0.0-beta.5-ef99126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-40ba009');
+var VERSION = new Version('6.0.0-beta.5-ef99126');
 
 /**
  * @fileoverview added by tsickle
