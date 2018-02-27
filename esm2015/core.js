9dc9925221cf3e87801d6d7207fb41eb04db8b90
Alex Eagle <alexeagle@google.com>
2018-02-27 02:16:17 +0000

    82a7912 docs: update i18n guide for projects that don't use the cli (#21767)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-ef99126
+ * @license Angular v6.0.0-beta.5-82a7912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-ef99126');
+const VERSION = new Version('6.0.0-beta.5-82a7912');
 
 /**
  * @fileoverview added by tsickle
