4240d0334fc1810d67cb950d7545ed1c7f94d222
Alex Eagle <alexeagle@google.com>
2018-02-27 02:07:12 +0000

    de02a7a fix(platform-server): generate correct stylings for camel case names (#22263)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-2a38d93
+ * @license Angular v5.2.6-de02a7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.6-2a38d93
+ * @license Angular v5.2.6-de02a7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.6-2a38d93');
+var VERSION = new Version('5.2.6-de02a7a');
 
 /**
  * @fileoverview added by tsickle
