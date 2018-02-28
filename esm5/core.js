3d9ffadf4fb57fc181a9060fad12a4f7c4e1b776
Alex Eagle <alexeagle@google.com>
2018-02-28 23:19:38 +0000

    288851c release: add ng update package group metadata to angular (#22482)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-9eaf1bb
+ * @license Angular v6.0.0-beta.5-288851c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-9eaf1bb');
+var VERSION = new Version('6.0.0-beta.5-288851c');
 
 /**
  * @fileoverview added by tsickle
