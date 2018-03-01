00308e8f53068d39a033ab76083ad76087004859
Alex Eagle <alexeagle@google.com>
2018-03-01 00:13:02 +0000

    e751a0a docs: add changelog for 6.0.0-beta.6

---
+++ core.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { AnimationMetadataType as ɵbo, animate as ɵbq, group as ɵbr, keyframes as ɵbv, sequence as ɵbs, state as ɵbu, style as ɵbt, transition as ɵbw, trigger as ɵbp } from './src/animation/dsl';
+export { _iterableDiffersFactory as ɵn, _keyValueDiffersFactory as ɵo, _localeFactory as ɵq } from './src/application_module';
+export { _appIdRandomProviderFactory as ɵi } from './src/application_tokens';
+export { defaultIterableDiffers as ɵj, defaultKeyValueDiffers as ɵk } from './src/change_detection/change_detection';
+export { DefaultIterableDifferFactory as ɵl } from './src/change_detection/differs/default_iterable_differ';
+export { DefaultKeyValueDifferFactory as ɵm } from './src/change_detection/differs/default_keyvalue_differ';
+export { ReflectiveInjector_ as ɵf } from './src/di/reflective_injector';
+export { ReflectiveDependency as ɵg, resolveReflectiveProviders as ɵh } from './src/di/reflective_provider';
+export { wtfEnabled as ɵu } from './src/profile/profile';
+export { createScope as ɵx, detectWTF as ɵw, endTimeRange as ɵba, leave as ɵy, startTimeRange as ɵz } from './src/profile/wtf_impl';
+export { getOrCreateChangeDetectorRef as ɵbe, getOrCreateContainerRef as ɵbg, getOrCreateInjectable as ɵbf, getOrCreateNodeInjector as ɵbd, getOrCreateTemplateRef as ɵbh } from './src/render3/di';
+export { bindingUpdated as ɵbk, bindingUpdated2 as ɵbm, bindingUpdated4 as ɵbn, checkAndUpdateBinding as ɵbl, consumeBinding as ɵbj, getCreationMode as ɵbi } from './src/render3/instructions';
+export { makeParamDecorator as ɵc, makePropDecorator as ɵd } from './src/util/decorators';
+export { _def as ɵbb } from './src/view/provider';
+export { DebugContext as ɵbc } from './src/view/types';
