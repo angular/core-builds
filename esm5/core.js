444a028ce41121ccaea48d1586b672519e473718
Alex Eagle <alexeagle@google.com>
2018-03-01 16:46:34 +0000

    8bb2f5c docs(compiler): ivy separate compilation design document (#22480)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-94707fe
+ * @license Angular v6.0.0-beta.6-8bb2f5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-94707fe');
+var VERSION = new Version('6.0.0-beta.6-8bb2f5c');
 
 /**
  * @fileoverview added by tsickle
