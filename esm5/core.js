626d48417481267ec0adb45c3a6523347150c95a
Alex Eagle <alexeagle@google.com>
2018-03-01 17:04:50 +0000

    fe0d53f build: Add support for bazelOptions.maxCacheSizeMb in ngc-wrapped. (#22511)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-f8e70fb
+ * @license Angular v5.2.7-fe0d53f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -717,7 +717,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.7-f8e70fb');
+var VERSION = new Version('5.2.7-fe0d53f');
 
 /**
  * @fileoverview added by tsickle
