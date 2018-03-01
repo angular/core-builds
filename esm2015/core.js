3652643994f268fb0599522cf2ffa213136004e5
Alex Eagle <alexeagle@google.com>
2018-03-01 16:38:07 +0000

    f8e70fb build(aio): initialise `exampleMap` correctly (#22502)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-27962f8
+ * @license Angular v5.2.7-f8e70fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.7-27962f8');
+const VERSION = new Version('5.2.7-f8e70fb');
 
 /**
  * @fileoverview added by tsickle
