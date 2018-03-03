f138157d3e35011520d972b6675585275880de5d
Alex Eagle <alexeagle@google.com>
2018-03-03 17:50:36 +0000

    a3f7e30 build: update to latest bazel rules (#22558)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-fe0d53f
+ * @license Angular v5.2.7-a3f7e30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.7-fe0d53f
+ * @license Angular v5.2.7-a3f7e30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -755,7 +755,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.7-fe0d53f');
+var VERSION = new Version('5.2.7-a3f7e30');
 
 /**
  * @fileoverview added by tsickle
