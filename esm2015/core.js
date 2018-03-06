ae899c5395456e6b596d9b57e9e37aacb5bbc633
Alex Eagle <alexeagle@google.com>
2018-03-06 15:21:09 +0000

    fa974c7 fix(router): fix URL serialization so special characters are only encoded where needed (#22337)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2c75acc
+ * @license Angular v6.0.0-beta.6-fa974c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-2c75acc');
+const VERSION = new Version('6.0.0-beta.6-fa974c7');
 
 /**
  * @fileoverview added by tsickle
