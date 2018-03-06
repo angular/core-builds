edf859e9ccb630adf4fa5dd048921eecbbaa1dc0
Alex Eagle <alexeagle@google.com>
2018-03-06 18:01:53 +0000

    d0db9de docs: fix cli-quickstart doc and specs (#22338)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4c40812
+ * @license Angular v6.0.0-beta.6-d0db9de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-4c40812');
+const VERSION = new Version('6.0.0-beta.6-d0db9de');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4c40812
+ * @license Angular v6.0.0-beta.6-d0db9de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-4c40812');
+var VERSION = new Version('6.0.0-beta.6-d0db9de');
 
 /**
  * @fileoverview added by tsickle
