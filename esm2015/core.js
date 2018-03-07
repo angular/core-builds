299ee682334de7a2bbed02699a12c50b6ec7033f
Alex Eagle <alexeagle@google.com>
2018-03-07 23:31:24 +0000

    8ad4ae0 docs: add changelog for 6.0.0-beta.7

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-ad8fb84
+ * @license Angular v6.0.0-beta.7-8ad4ae0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-ad8fb84');
+const VERSION = new Version('6.0.0-beta.7-8ad4ae0');
 
 /**
  * @fileoverview added by tsickle
