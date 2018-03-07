27ea4bb48bc0469bcfca0b10f84b6e9f69902dce
Alex Eagle <alexeagle@google.com>
2018-03-07 19:18:00 +0000

    4f744cc docs: update RELEASE_SCHEDULE.md by pushing out v6 rc by one week

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-363dfa5
+ * @license Angular v6.0.0-beta.6-4f744cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-363dfa5');
+const VERSION = new Version('6.0.0-beta.6-4f744cc');
 
 /**
  * @fileoverview added by tsickle
