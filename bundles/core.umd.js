f6b0044c27e368aa196218a645258a056949eaa8
Kara Erickson <karakara@google.com>
2018-03-08 19:18:53 +0000

    c499c8f build: update angular-bot g3sync paths (#22637)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4c089c1
+ * @license Angular v6.0.0-beta.7-c499c8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-4c089c1
+ * @license Angular v6.0.0-beta.7-c499c8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2072,7 +2072,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-4c089c1');
+var VERSION = new Version('6.0.0-beta.7-c499c8f');
 
 /**
  * @fileoverview added by tsickle
