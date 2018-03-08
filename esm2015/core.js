48841f0d0f9daa7f4773023a6edfade7272786de
Kara Erickson <karakara@google.com>
2018-03-08 02:41:52 +0000

    065bcc5 docs: add HeroService to code tabs and fix headers (#22373)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-8ad4ae0
+ * @license Angular v6.0.0-beta.7-065bcc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-8ad4ae0');
+const VERSION = new Version('6.0.0-beta.7-065bcc5');
 
 /**
  * @fileoverview added by tsickle
