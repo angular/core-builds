e651a0a9d6e5d1a73a4f709fd2728f88020b4968
Kara Erickson <karakara@google.com>
2018-03-09 17:52:47 +0000

    b5be18f feat(compiler-cli): add resource inlining to ngc (#22615)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-1e6cc42
+ * @license Angular v6.0.0-beta.7-b5be18f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1862,7 +1862,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-1e6cc42');
+const VERSION = new Version('6.0.0-beta.7-b5be18f');
 
 /**
  * @fileoverview added by tsickle
