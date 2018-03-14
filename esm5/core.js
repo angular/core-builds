97d9b163adcd02298c3f4a2f37609656774813aa
Kara Erickson <karakara@google.com>
2018-03-14 03:17:32 +0000

    48636f3 build(aio): compute stability and deprecate `@stable` tag (#22674)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-bd9d4df
+ * @license Angular v6.0.0-beta.7-48636f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2043,7 +2043,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-bd9d4df');
+var VERSION = new Version('6.0.0-beta.7-48636f3');
 
 /**
  * @fileoverview added by tsickle
