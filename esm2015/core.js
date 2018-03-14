a2d83876fc753e30ea5dc9e01f416bd00b5766ef
Kara Erickson <karakara@google.com>
2018-03-14 18:20:41 +0000

    9df13ad docs(aio): add ng-japan 2018 to events (#22750)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-049757b
+ * @license Angular v6.0.0-beta.7-9df13ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1871,7 +1871,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-049757b');
+const VERSION = new Version('6.0.0-beta.7-9df13ad');
 
 /**
  * @fileoverview added by tsickle
