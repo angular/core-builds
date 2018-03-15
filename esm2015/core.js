c6a920781e7fbb10e04c2a72ce91d8f662d1b16b
Miško Hevery <misko@hevery.com>
2018-03-15 22:14:54 +0000

    02e6ac2 docs: incorporate suggestions and corrections from gkalpak (#21569)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-a011654
+ * @license Angular v6.0.0-beta.7-02e6ac2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1874,7 +1874,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-a011654');
+const VERSION = new Version('6.0.0-beta.7-02e6ac2');
 
 /**
  * @fileoverview added by tsickle
