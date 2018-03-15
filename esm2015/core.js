48686f9b6ea63cc144466b60dafe1e0964ebf53e
Miško Hevery <misko@hevery.com>
2018-03-15 19:03:22 +0000

    db0afa9 fix(compiler-cli): emit correct css string escape sequences (#22776)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-5298b2b
+ * @license Angular v5.2.9-db0afa9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-5298b2b');
+const VERSION = new Version('5.2.9-db0afa9');
 
 /**
  * @fileoverview added by tsickle
