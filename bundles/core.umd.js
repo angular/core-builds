68f5af40fa5b9243461b3203dd8c50f2ac07c2d7
Miško Hevery <misko@hevery.com>
2018-03-16 04:39:02 +0000

    f9247e4 build: enable importHelpers in tsconfig (#22812)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-44de10e
+ * @license Angular v6.0.0-beta.7-f9247e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-44de10e
+ * @license Angular v6.0.0-beta.7-f9247e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2084,7 +2084,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-44de10e');
+var VERSION = new Version('6.0.0-beta.7-f9247e4');
 
 /**
  * @fileoverview added by tsickle
