ca98430191adab3eca9aebf15a6e8456b3d55a0e
Miško Hevery <misko@hevery.com>
2018-03-20 04:06:08 +0000

    5391f96 fix(upgrade): two-way binding and listening for event (#22772)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-aca4735
+ * @license Angular v5.2.9-5391f96
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -718,7 +718,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-aca4735');
+var VERSION = new Version('5.2.9-5391f96');
 
 /**
  * @fileoverview added by tsickle
