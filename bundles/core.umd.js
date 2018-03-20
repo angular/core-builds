b5ce395a63b16ddc0a56f496fe974190197bdd4f
Igor Minar <igor@angular.io>
2018-03-20 20:51:45 +0000

    67f570c ci: update ngbot config for g3 (#22882)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4648597
+ * @license Angular v6.0.0-beta.7-67f570c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-4648597
+ * @license Angular v6.0.0-beta.7-67f570c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2235,7 +2235,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-4648597');
+var VERSION = new Version('6.0.0-beta.7-67f570c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4648597
+ * @license Angular v6.0.0-beta.7-67f570c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2194,7 +2194,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-4648597');
+var VERSION = new Version('6.0.0-beta.7-67f570c');
 
 /**
  * @fileoverview added by tsickle
