0c9975e67e67cb3e773cada437a4df6697c1519d
Igor Minar <igor@angular.io>
2018-03-20 20:56:45 +0000

    7d095b9 fix: correct several esm2015 entry-points in package.jsons (#22892)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-beta.7-689f351');
+export const /** @type {?} */ VERSION = new Version('6.0.0-beta.7-7d095b9');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-beta.7-689f351');
+export var /** @type {?} */ VERSION = new Version('6.0.0-beta.7-7d095b9');
 //# sourceMappingURL=version.js.map
