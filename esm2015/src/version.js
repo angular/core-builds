44742fe4dd9d28eda2685dce3f2c30c1360da663
Miško Hevery <misko@hevery.com>
2018-03-20 04:08:16 +0000

    2b3de63 fix(upgrade): two-way binding and listening for event (#22772)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-beta.7-5c387a7');
+export const /** @type {?} */ VERSION = new Version('6.0.0-beta.7-2b3de63');
 //# sourceMappingURL=version.js.map
