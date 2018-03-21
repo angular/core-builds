64d14b963fb4789de7aedba5477e346ff60773b0
Igor Minar <igor@angular.io>
2018-03-21 20:47:00 +0000

    b12ea30 test: remove gulp public-api:update docs (#22914) PR Close #22914

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-8c2a578');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-b12ea30');
 //# sourceMappingURL=version.js.map
