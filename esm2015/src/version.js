8568d38101edfc266368fa57cd77c1dcaa0dad20
Igor Minar <igor@angular.io>
2018-03-21 06:25:55 +0000

    73261a8 docs: add changelog for 6.0.0-rc.0

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-beta.7-623d769');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-73261a8');
 //# sourceMappingURL=version.js.map
