7000f941bce891fc763667725d8ed9b669473da3
Igor Minar <igor@angular.io>
2018-03-21 20:51:35 +0000

    50761fb test: remove gulp public-api:update docs (#22914) PR Close #22914

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-65f8943
+ * @license Angular v5.2.9-50761fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -684,7 +684,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-65f8943');
+const VERSION = new Version('5.2.9-50761fb');
 
 /**
  * @fileoverview added by tsickle
