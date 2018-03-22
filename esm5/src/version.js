ad57cabad110557281970728b2c32f1562aef6cf
Matias Niemelä <matias@yearofmoo.com>
2018-03-22 22:52:56 +0000

    d77bb46 revert: docs(ivy): update STATUS.md with compiler work breakdown (#22874)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-bb58664');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-d77bb46');
 //# sourceMappingURL=version.js.map
