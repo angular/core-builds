f04347a2526e56edfc20b690cb43d8fd20dca349
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:38:36 +0000

    bb58664 build: rm --noimplicit_deps from bazel query (#22912)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-bfe35da
+ * @license Angular v6.0.0-rc.0-bb58664
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-rc.0-bfe35da
+ * @license Angular v6.0.0-rc.0-bb58664
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2235,7 +2235,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-bfe35da');
+var VERSION = new Version('6.0.0-rc.0-bb58664');
 
 /**
  * @fileoverview added by tsickle
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-bfe35da');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-bb58664');
 //# sourceMappingURL=version.js.map
