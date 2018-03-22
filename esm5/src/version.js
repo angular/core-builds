529c5b3eea2b59fbd22a275e6695ec8d2c6f5d48
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 19:49:28 +0000

    32105c8 test: temporarily disable ng_package test (#22933)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-f461f43');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-32105c8');
 //# sourceMappingURL=version.js.map
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-f461f43
+ * @license Angular v6.0.0-rc.0-32105c8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2194,7 +2194,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-f461f43');
+var VERSION = new Version('6.0.0-rc.0-32105c8');
 
 /**
  * @fileoverview added by tsickle
