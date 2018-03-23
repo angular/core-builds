bda176426dc0b980f4a4c6f76d3d968eb0d25900
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:34:39 +0000

    4be8b3f docs: update available platforms for `test.sh` (#22958)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-8d1e640
+ * @license Angular v5.2.9-4be8b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -684,7 +684,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-8d1e640');
+const VERSION = new Version('5.2.9-4be8b3f');
 
 /**
  * @fileoverview added by tsickle
