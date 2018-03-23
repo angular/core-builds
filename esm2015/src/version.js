7635b6de6d68695bcb44cd904ab582521e28938c
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:11:20 +0000

    8ca26a9 ci(compiler): run compiler tests in bazel (#22900)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-3498106');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-8ca26a9');
 //# sourceMappingURL=version.js.map
