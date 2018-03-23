50ce13eb7105fdf9dd83a2228c540f6a1d4d8758
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:37:45 +0000

    bcaa07b refactor(compiler): move handling of translations to the ConstantPool (#22942)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-16f021c
+ * @license Angular v6.0.0-rc.0-bcaa07b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-rc.0-16f021c
+ * @license Angular v6.0.0-rc.0-bcaa07b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2235,7 +2235,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-16f021c');
+var VERSION = new Version('6.0.0-rc.0-bcaa07b');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-16f021c');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-bcaa07b');
 //# sourceMappingURL=version.js.map
