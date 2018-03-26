f2885fff057d0fbbd96dbc385c89207eaf6491d8
Matias Niemelä <matias@yearofmoo.com>
2018-03-26 20:54:38 +0000

    bf6a416 revert: docs(common): add `HttpParamsOptions` to the public API (#20332)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-7b7757d
+ * @license Angular v6.0.0-rc.0-bf6a416
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2195,7 +2195,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-7b7757d');
+var VERSION = new Version('6.0.0-rc.0-bf6a416');
 
 /**
  * @fileoverview added by tsickle
