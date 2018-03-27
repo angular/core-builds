b873fe5159d78b1bf0d01bb5508e0f3073c5705b
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 02:56:07 +0000

    27e14b2 feat(bazel): prefix private-export (barred-latin-o) symbols (#23007)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-2388f24');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-27e14b2');
 //# sourceMappingURL=version.js.map
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-2388f24
+ * @license Angular v6.0.0-rc.0-27e14b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-2388f24');
+const VERSION = new Version('6.0.0-rc.0-27e14b2');
 
 /**
  * @fileoverview added by tsickle
