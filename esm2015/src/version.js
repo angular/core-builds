aa46f84b3d1754b01233da9bbd352d0abe65f1ae
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 02:52:12 +0000

    2388f24 build(bazel): add entry_point_name attr to ng_package (#22963)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-7a406a3');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-2388f24');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-7a406a3');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-2388f24');
 //# sourceMappingURL=version.js.map
