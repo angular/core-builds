6b1a87432acfc37e17a0aa84e99396645e95ccfb
GitHub <noreply@github.com>
2018-03-30 23:23:13 +0000

    d6bf71a docs(compiler): initial outline of Ivy Compiler Architecture design doc (#22916)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-9cd4465');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-d6bf71a');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-9cd4465');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-d6bf71a');
 //# sourceMappingURL=version.js.map
