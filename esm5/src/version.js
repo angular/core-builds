b066910b17bfb7809f253a52fe799bd1648335c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:54:52 +0000

    32a41bc test(ivy): fixes in the TodoMVC example (#23161)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-f48e215');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-32a41bc');
 //# sourceMappingURL=version.js.map
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-f48e215
+ * @license Angular v6.0.0-rc.1-32a41bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2195,7 +2195,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-f48e215');
+var VERSION = new Version('6.0.0-rc.1-32a41bc');
 
 /**
  * @fileoverview added by tsickle
