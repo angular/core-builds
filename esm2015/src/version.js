d6d01db280c1d638bb061dd8d871e58b9fb85a15
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:44:27 +0000

    550433a feat(compiler-cli): lower loadChildren fields to allow dynamic module paths (#23088)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-4506230');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-550433a');
 //# sourceMappingURL=version.js.map
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-4506230
+ * @license Angular v6.0.0-rc.1-550433a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2195,7 +2195,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-4506230');
+var VERSION = new Version('6.0.0-rc.1-550433a');
 
 /**
  * @fileoverview added by tsickle
