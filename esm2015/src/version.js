7e14f08bf314254edc0f679416923a80d683ae2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:52:29 +0000

    f48e215 build(service-worker): properly build npm_package with Bazel (#23090)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-f99cb5c');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-f48e215');
 //# sourceMappingURL=version.js.map
