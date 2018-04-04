a692c7795ac93b094cc28af5a7ee36105e723869
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:15:23 +0000

    3900c36 refactor(common): simplify NgClass code, add comments (#21937)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-9703079
+ * @license Angular v5.2.9-3900c36
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -684,7 +684,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-9703079');
+const VERSION = new Version('5.2.9-3900c36');
 
 /**
  * @fileoverview added by tsickle
