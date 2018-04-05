3125ea3e274de03578babbc842d2f8c05e35ff46
Igor Minar <igor@angular.io>
2018-04-05 19:21:35 +0000

    f3d2623 docs: add changelog for 6.0.0-rc.2

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-fc813f6');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.2-f3d2623');
 //# sourceMappingURL=version.js.map
