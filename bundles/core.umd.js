5bf6576ab0158cc1af99cc025c7569331e308a01
Igor Minar <igor@angular.io>
2018-04-05 17:29:30 +0000

    8aa49ac fix(aio): update trusted GitHub teams (angular-core --> team) (#23181)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-3900c36
+ * @license Angular v5.2.9-8aa49ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.9-3900c36
+ * @license Angular v5.2.9-8aa49ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -756,7 +756,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-3900c36');
+var VERSION = new Version('5.2.9-8aa49ac');
 
 /**
  * @fileoverview added by tsickle
