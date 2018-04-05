a3a3147a317ca1c4f457d89d298d54e9beb4e554
Igor Minar <igor@angular.io>
2018-04-05 23:53:39 +0000

    b5f41f2 docs: fixed live example for the lifecycle hooks. (#23201)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.2-629629d');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.2-b5f41f2');
 //# sourceMappingURL=version.js.map
