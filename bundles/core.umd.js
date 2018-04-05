235abbd486e43f779261881c023dbc2aa95031f4
Igor Minar <igor@angular.io>
2018-04-05 18:00:14 +0000

    3b607e6 refactor(aio): remove unused images (#23018)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-ce43e96
+ * @license Angular v6.0.0-rc.1-3b607e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-rc.1-ce43e96
+ * @license Angular v6.0.0-rc.1-3b607e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2238,7 +2238,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-ce43e96');
+var VERSION = new Version('6.0.0-rc.1-3b607e6');
 
 /**
  * @fileoverview added by tsickle
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-ce43e96');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-3b607e6');
 //# sourceMappingURL=version.js.map
