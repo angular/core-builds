4c85f341f80f250e176e861064d0a23b2a821167
Igor Minar <igor@angular.io>
2018-04-05 22:08:54 +0000

    5cd36c7 build: improve the publish-next script (#23206)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-f3d2623
+ * @license Angular v6.0.0-rc.2-5cd36c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-rc.2-f3d2623
+ * @license Angular v6.0.0-rc.2-5cd36c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2238,7 +2238,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.2-f3d2623');
+var VERSION = new Version('6.0.0-rc.2-5cd36c7');
 
 /**
  * @fileoverview added by tsickle
