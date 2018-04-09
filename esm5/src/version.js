308a55cd6f4dd50089d410234b54ccc480b96a4f
Igor Minar <igor@angular.io>
2018-04-09 22:40:42 +0000

    30a6861 build: update zone.js to 0.8.26 to fix some regression bugs (#23227)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.3-3e8eef6');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.3-30a6861');
 //# sourceMappingURL=version.js.map
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-3e8eef6
+ * @license Angular v6.0.0-rc.3-30a6861
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2197,7 +2197,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-3e8eef6');
+var VERSION = new Version('6.0.0-rc.3-30a6861');
 
 /**
  * @fileoverview added by tsickle
