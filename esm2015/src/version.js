1a3b18356286d0102743a8c5dfe8ccfa9bbc7704
Igor Minar <igor@angular.io>
2018-04-09 22:41:30 +0000

    5a298b1 fix(common): workaround UMD/webpack limitation (#23271)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.3-30a6861');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.3-5a298b1');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.3-30a6861');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.3-5a298b1');
 //# sourceMappingURL=version.js.map
