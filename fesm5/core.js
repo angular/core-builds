95ba60e3c34e623ad432bb108450900f3f305979
Igor Minar <igor@angular.io>
2018-04-10 20:38:32 +0000

    f1db789 test(ivy): update todo app to http://todomvc.com (#23168)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-8555a3a
+ * @license Angular v6.0.0-rc.3-f1db789
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2197,7 +2197,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-8555a3a');
+var VERSION = new Version('6.0.0-rc.3-f1db789');
 
 /**
  * @fileoverview added by tsickle
