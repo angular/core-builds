e2d512cbf9dcb1421117b784fe2dcc9ceb6f6e8b
Igor Minar <igor@angular.io>
2018-04-13 06:43:37 +0000

    75febe7 docs(upgrade): fix detail regarding bootstrapping order (#23225) (#23270)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.4-993eeab');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.4-75febe7');
 //# sourceMappingURL=version.js.map
