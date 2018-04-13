645d7b9b30489889d495a553ce336bbaa3e3d5d4
Igor Minar <igor@angular.io>
2018-04-13 07:52:00 +0000

    3bd682f docs(aio): Add link to Japanese localization (#20630) PR Close #20630

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-2bb7838
+ * @license Angular v6.0.0-rc.4-3bd682f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-rc.4-2bb7838
+ * @license Angular v6.0.0-rc.4-3bd682f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2238,7 +2238,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-2bb7838');
+var VERSION = new Version('6.0.0-rc.4-3bd682f');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-2bb7838
+ * @license Angular v6.0.0-rc.4-3bd682f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2025,7 +2025,7 @@ class Version {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.4-2bb7838');
+const VERSION = new Version('6.0.0-rc.4-3bd682f');
 
 /**
  * @fileoverview added by tsickle
