48da139ca4fb44d97054abe1a6be59aa4be589b7
Igor Minar <igor@angular.io>
2018-04-13 17:01:22 +0000

    a3204f8 test(aio): fix `DocViewerComponent` tests (#23359)

--- esm5/core.js
+++ esm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-61bddeb
+ * @license Angular v5.2.9-a3204f8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -718,7 +718,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-61bddeb');
+var VERSION = new Version('5.2.9-a3204f8');
 
 /**
  * @fileoverview added by tsickle
