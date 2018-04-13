e7550c4c434cf039c0b82dd27d251e9c574e9d0d
Igor Minar <igor@angular.io>
2018-04-13 21:42:40 +0000

    490772e docs(aio): Update file tree to match Angular CLI V6 structure (#23367)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-e7ef027
+ * @license Angular v6.0.0-rc.4-490772e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2025,7 +2025,7 @@ class Version {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.4-e7ef027');
+const VERSION = new Version('6.0.0-rc.4-490772e');
 
 /**
  * @fileoverview added by tsickle
