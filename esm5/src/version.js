2539bbd44d3b9818c6541a8befbb2d883b61bfff
Igor Minar <igor@angular.io>
2018-04-13 23:51:45 +0000

    7e581da ci: remove travis e2e_2 job (#22810)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.4-2c09b70');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.4-7e581da');
 //# sourceMappingURL=version.js.map
