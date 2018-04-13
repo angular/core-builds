1ccacb73b3ff437d8f24d90428d963535f5dc041
Igor Minar <igor@angular.io>
2018-04-13 07:26:23 +0000

    3cf5719 docs: lock version of in-memory API (#23242)

--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-47229fa
+ * @license Angular v5.2.9-3cf5719
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -684,7 +684,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-47229fa');
+const VERSION = new Version('5.2.9-3cf5719');
 
 /**
  * @fileoverview added by tsickle
