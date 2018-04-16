6bfb14d6e2dd5d22d11d9b2205b7186102ee3071
Igor Minar <igor@angular.io>
2018-04-16 07:05:15 +0000

    896811d docs(aio): add missing word in the Component metadata section (#23384)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-fb59b2d');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-896811d');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-fb59b2d');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-896811d');
 //# sourceMappingURL=version.js.map
