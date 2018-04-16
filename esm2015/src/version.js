8e1fef32fffe983df69eb90015bd84d609238c7b
Igor Minar <igor@angular.io>
2018-04-16 16:26:18 +0000

    c3280b2 docs(aio): change `HeroService` to `MessagesComponent` (#23397)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-9c29127');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-c3280b2');
 //# sourceMappingURL=version.js.map
