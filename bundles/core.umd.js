c8a68dae0d3e27e2f6aedd5fa46325626d9289a6
Jason Aden <jasonaden@google.com>
2018-04-17 21:44:52 +0000

    402f452 docs(aio): add front page campaign for the ng-conf live stream (#23391)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-d2e7c99
+ * @license Angular v5.2.10-402f452
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.10-d2e7c99
+ * @license Angular v5.2.10-402f452
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -756,7 +756,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.10-d2e7c99');
+var VERSION = new Version('5.2.10-402f452');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-d2e7c99
+ * @license Angular v5.2.10-402f452
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -684,7 +684,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.10-d2e7c99');
+const VERSION = new Version('5.2.10-402f452');
 
 /**
  * @fileoverview added by tsickle
