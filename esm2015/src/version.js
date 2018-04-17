847431884065a3164fa72f1f1c23b30898779ab6
Jason Aden <jasonaden@google.com>
2018-04-17 21:17:25 +0000

    7de69ba ci(aio): fix `aio-monitoring` tests (#23390)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-010a4ef');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-7de69ba');
 //# sourceMappingURL=version.js.map
