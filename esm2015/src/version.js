b3bb84854446bbcab6bc2f552840b6f0dfcc37d0
Victor Berchet <victor@suumit.com>
2018-04-20 23:33:43 +0000

    4662878 refactor(compiler): refactor template binding parsing (#23460)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-ca776c5');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-4662878');
 //# sourceMappingURL=version.js.map
