69c553f84b7bc6eba239776425bd657576735910
Victor Berchet <victor@suumit.com>
2018-04-23 17:31:33 +0000

    999ab0a ci: add alxhub as owner to a few packages (#23510)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-ba47997');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-999ab0a');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-ba47997');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-999ab0a');
 //# sourceMappingURL=version.js.map
