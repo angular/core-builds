8e29631b9727397d7758d03bc0335cf0a32cbdd4
Alex Eagle <alexeagle@google.com>
2018-04-23 23:42:38 +0000

    549311f ci: fix snapshot publishing

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+73.sha-57310b7
+ * @license Angular v6.0.0-rc.5+73.sha-549311f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2035,7 +2035,7 @@ class Version {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+73.sha-57310b7');
+const VERSION = new Version('6.0.0-rc.5+73.sha-549311f');
 
 /**
  * @fileoverview added by tsickle
