effe135b41066d37821f1ce58cba678b10f6d5e7
Victor Berchet <victor@suumit.com>
2018-04-24 22:11:01 +0000

    04c18ac docs: fix typo (#23514)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-1b26dd8');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-04c18ac');
 //# sourceMappingURL=version.js.map
