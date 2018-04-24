db730d61643cd2481a6a69bcd96998e5264e219d
Victor Berchet <victor@suumit.com>
2018-04-24 21:24:39 +0000

    0bc8443 fix(compiler): avoid a crash in ngc-wrapped. (#23468)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-db17231');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-0bc8443');
 //# sourceMappingURL=version.js.map
