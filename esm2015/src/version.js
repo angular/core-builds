c54e4982a4e6da909d0d961feab38105b281e304
Igor Minar <igor@angular.io>
2018-04-27 14:55:23 +0000

    098a3ef docs: correct more typos (#23565)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-5021bdd');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-098a3ef');
 //# sourceMappingURL=version.js.map
