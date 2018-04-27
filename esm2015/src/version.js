8dd29c0ab134682a4dfcb7b67702127152b6dfaf
Igor Minar <igor@angular.io>
2018-04-27 15:17:39 +0000

    91bf598 docs(aio): update docs error in guide/http (#23567)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-08e9290');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-91bf598');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-08e9290');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-91bf598');
 //# sourceMappingURL=version.js.map
