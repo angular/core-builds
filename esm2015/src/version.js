e2aa4833307f2a4f132385390347d9576bcaaf6c
Igor Minar <igor@angular.io>
2018-05-02 23:17:53 +0000

    5fc4299 refactor(aio): move right margin from `.home` image to `.home` anchor (#23624)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -38,5 +38,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.6-91d4da0');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.6-5fc4299');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -47,5 +47,5 @@ function Version_tsickle_Closure_declarations() {
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.6-91d4da0');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.6-5fc4299');
 //# sourceMappingURL=version.js.map
