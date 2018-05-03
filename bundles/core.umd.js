93589d1b1faa52e61a439b86fa14bc35301f16b3
Igor Minar <igor@angular.io>
2018-05-03 23:01:57 +0000

    d8d4f65 build: update the scripts/release/post-check script for 6.0.x

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+125.sha-2c4850d
+ * @license Angular v6.0.0-rc.5+126.sha-d8d4f65
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+125.sha-2c4850d');
+var VERSION = new Version('6.0.0-rc.5+126.sha-d8d4f65');
 
 /**
  * @license
