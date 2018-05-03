6d0cf55e4d9d00de7ab6dc40251575d0bc20510d
Igor Minar <igor@angular.io>
2018-05-03 22:30:07 +0000

    b19216d fix(aio): correct project id for deployment of archive sites

--- @angular/core/testing.es5.js
+++ @angular/core/testing.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.7-84fc1a3
+ * @license Angular v4.4.7-b19216d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
