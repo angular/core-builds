a8fbc96d71735915d43d6d58792fdd1f475efc58
Igor Minar <igor@angular.io>
2018-05-05 16:01:17 +0000

    19262d9 Revert "style(animations): fix short param names (#23668)"

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+25.sha-3807599
+ * @license Angular v6.0.0+27.sha-19262d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1654,7 +1654,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0+25.sha-3807599');
+var VERSION = new Version('6.0.0+27.sha-19262d9');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+25.sha-3807599
+ * @license Angular v6.0.0+27.sha-19262d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2035,7 +2035,7 @@ class Version {
 /**
  *
  */
-const VERSION = new Version('6.0.0+25.sha-3807599');
+const VERSION = new Version('6.0.0+27.sha-19262d9');
 
 /**
  * @fileoverview added by tsickle
