78ed78f2dc90af9345d26f0927760d09e17bab32
Igor Minar <igor@angular.io>
2018-05-08 21:19:12 +0000

    b0eca85 refactor(compiler): compile{Component,Directive} take only local information (#23545)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+141.sha-fc03427
+ * @license Angular v6.0.0-rc.5+144.sha-b0eca85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2037,7 +2037,7 @@ class Version {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+141.sha-fc03427');
+const VERSION = new Version('6.0.0-rc.5+144.sha-b0eca85');
 
 /**
  * @fileoverview added by tsickle
