f000332b7166e96f1e66c83206b8af31b86e5647
Igor Minar <igor@angular.io>
2018-05-08 21:19:32 +0000

    fe7f48c docs(aio): Upgrade example dependencies to Angular V6 (#23660)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+28.sha-5581e97
+ * @license Angular v6.0.0+30.sha-fe7f48c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2035,7 +2035,7 @@ class Version {
 /**
  *
  */
-const VERSION = new Version('6.0.0+28.sha-5581e97');
+const VERSION = new Version('6.0.0+30.sha-fe7f48c');
 
 /**
  * @fileoverview added by tsickle
