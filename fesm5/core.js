81ce952bf6c0ff30b7a73dc410ce3215d38c1bac
Igor Minar <igor@angular.io>
2018-05-09 19:24:23 +0000

    7b06fa8 build(aio): include `navigation.json` changes in docs-watch (#23698)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+32.sha-3d71289
+ * @license Angular v6.0.0+33.sha-7b06fa8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1570,7 +1570,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0+32.sha-3d71289');
+var VERSION = new Version('6.0.0+33.sha-7b06fa8');
 
 /**
  * @license
