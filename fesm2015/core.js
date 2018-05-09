567a4183ad09377cfb5977ead8e9a61a42499010
Igor Minar <igor@angular.io>
2018-05-09 19:22:18 +0000

    75b8eda ci: Remove Chuck from pullapprove (#23798)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+30.sha-fe7f48c
+ * @license Angular v6.0.0+31.sha-75b8eda
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2035,7 +2035,7 @@ class Version {
 /**
  *
  */
-const VERSION = new Version('6.0.0+30.sha-fe7f48c');
+const VERSION = new Version('6.0.0+31.sha-75b8eda');
 
 /**
  * @fileoverview added by tsickle
