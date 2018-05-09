07d0ef75ae88de0aac795756efaf3f9c1e521337
Igor Minar <igor@angular.io>
2018-05-09 19:31:55 +0000

    2254ac2 fix(service-worker): correctly handle requests with empty `clientId` (#23625)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+38.sha-2b31b6d
+ * @license Angular v6.0.0+37.sha-2254ac2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2035,7 +2035,7 @@ class Version {
 /**
  *
  */
-const VERSION = new Version('6.0.0+38.sha-2b31b6d');
+const VERSION = new Version('6.0.0+37.sha-2254ac2');
 
 /**
  * @fileoverview added by tsickle
