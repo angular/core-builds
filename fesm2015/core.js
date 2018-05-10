4a62a78203e5741730e5beeca451deb4d57571b9
Igor Minar <igor@angular.io>
2018-05-10 23:33:18 +0000

    2b578f5 docs(elements): add angular element term to glossary (#23807)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+46.sha-12dcb31
+ * @license Angular v6.0.0+47.sha-2b578f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0+46.sha-12dcb31');
+const VERSION = new Version('6.0.0+47.sha-2b578f5');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+46.sha-12dcb31
+ * @license Angular v6.0.0+47.sha-2b578f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1567,7 +1567,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0+46.sha-12dcb31');
+var VERSION = new Version('6.0.0+47.sha-2b578f5');
 
 /**
  * @license
