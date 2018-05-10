3ecebf0cc8b35d161aee1e6d53141aa3b00bb0bc
Igor Minar <igor@angular.io>
2018-05-10 23:30:50 +0000

    fe3679a style: remove empty comments (#23404)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+163.sha-c4221da
+ * @license Angular v6.0.0-rc.5+162.sha-fe3679a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1569,7 +1569,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+163.sha-c4221da');
+var VERSION = new Version('6.0.0-rc.5+162.sha-fe3679a');
 
 /**
  * @license
