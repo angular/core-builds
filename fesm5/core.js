ac25da1f2cf87a1ca6484f8d9e7c379937a42367
Igor Minar <igor@angular.io>
2018-05-11 00:07:50 +0000

    089fe83 build: update to latest TypeScript rules (#23828)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+164.sha-b1cda36
+ * @license Angular v6.0.0-rc.5+165.sha-089fe83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1569,7 +1569,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+164.sha-b1cda36');
+var VERSION = new Version('6.0.0-rc.5+165.sha-089fe83');
 
 /**
  * @license
