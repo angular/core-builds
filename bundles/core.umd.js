202eb65a1b379f804e685e4f9579aba0c877ec97
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 21:06:11 +0000

    d889f57 build(aio): display types of API const docs correctly (#23850)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+172.sha-816bc8a
+ * @license Angular v6.0.0-rc.5+173.sha-d889f57
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1653,7 +1653,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+172.sha-816bc8a');
+var VERSION = new Version('6.0.0-rc.5+173.sha-d889f57');
 
 /**
  * @license
