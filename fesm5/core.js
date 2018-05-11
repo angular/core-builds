25fe502555b2f382872fc52c546d1d8065bb3170
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 21:57:35 +0000

    cfde36d fix(compiler): generate constant array for i18n attributes (#23837)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+173.sha-d889f57
+ * @license Angular v6.0.0-rc.5+174.sha-cfde36d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1569,7 +1569,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+173.sha-d889f57');
+var VERSION = new Version('6.0.0-rc.5+174.sha-cfde36d');
 
 /**
  * @license
