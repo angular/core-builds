2e6d0cbb014f6f071cb37d3c8ce893144fb12b19
Igor Minar <igor@angular.io>
2018-05-11 06:22:56 +0000

    4ecae64 build: replace the old publish script with a new bazel-based one

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+166.sha-5e307d5
+ * @license Angular v6.0.0-rc.5+167.sha-4ecae64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+166.sha-5e307d5');
+const VERSION = new Version('6.0.0-rc.5+167.sha-4ecae64');
 
 /**
  * @fileoverview added by tsickle
