cb09f336018ff80aec5578ac75f2d153e88cae3d
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 22:34:58 +0000

    d4b8b24 fix(elements): prevent closure renaming of platform properties (#23843)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+174.sha-cfde36d
+ * @license Angular v6.0.0-rc.5+175.sha-d4b8b24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1653,7 +1653,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+174.sha-cfde36d');
+var VERSION = new Version('6.0.0-rc.5+175.sha-d4b8b24');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+174.sha-cfde36d
+ * @license Angular v6.0.0-rc.5+175.sha-d4b8b24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+174.sha-cfde36d');
+const VERSION = new Version('6.0.0-rc.5+175.sha-d4b8b24');
 
 /**
  * @fileoverview added by tsickle
