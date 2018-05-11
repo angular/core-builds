2b6b5fbf74d3c1aeed4e108056b9b38650ab208d
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 16:55:57 +0000

    1b83b3f docs(aio): add Cory Rylan to GDE resources (#23840)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+52.sha-14d4625
+ * @license Angular v6.0.0+53.sha-1b83b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0+52.sha-14d4625');
+const VERSION = new Version('6.0.0+53.sha-1b83b3f');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+52.sha-14d4625
+ * @license Angular v6.0.0+53.sha-1b83b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1567,7 +1567,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0+52.sha-14d4625');
+var VERSION = new Version('6.0.0+53.sha-1b83b3f');
 
 /**
  * @license
