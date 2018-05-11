dc611be6a1e5ad4215331701c48d5f634f4a25b1
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 17:00:21 +0000

    56be337 fix(aio): make background transparent in 144x144 PWA icon (#23851)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+53.sha-1b83b3f
+ * @license Angular v6.0.0+54.sha-56be337
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0+53.sha-1b83b3f');
+var VERSION = new Version('6.0.0+54.sha-56be337');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+53.sha-1b83b3f
+ * @license Angular v6.0.0+54.sha-56be337
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0+53.sha-1b83b3f');
+const VERSION = new Version('6.0.0+54.sha-56be337');
 
 /**
  * @fileoverview added by tsickle
