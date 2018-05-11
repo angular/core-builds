254960a277e0fc88637b8f23c00bd25caec8eb06
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 20:27:55 +0000

    d2a8687 fix(animations): do not throw errors when a destroyed component is animated (#23836)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+170.sha-474dbf0
+ * @license Angular v6.0.0-rc.5+171.sha-d2a8687
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+170.sha-474dbf0');
+const VERSION = new Version('6.0.0-rc.5+171.sha-d2a8687');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+170.sha-474dbf0
+ * @license Angular v6.0.0-rc.5+171.sha-d2a8687
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1569,7 +1569,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+170.sha-474dbf0');
+var VERSION = new Version('6.0.0-rc.5+171.sha-d2a8687');
 
 /**
  * @license
