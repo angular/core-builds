bda4a3621123184b8c282d7194cf9164038cb2bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 20:06:14 +0000

    66b2d78 build: only match version tags for BUILD_SCM_VERSION (#23903)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+178.sha-67b8d57
+ * @license Angular v6.0.0-rc.5+179.sha-66b2d78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+178.sha-67b8d57');
+const VERSION = new Version('6.0.0-rc.5+179.sha-66b2d78');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+178.sha-67b8d57
+ * @license Angular v6.0.0-rc.5+179.sha-66b2d78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1569,7 +1569,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+178.sha-67b8d57');
+var VERSION = new Version('6.0.0-rc.5+179.sha-66b2d78');
 
 /**
  * @license
