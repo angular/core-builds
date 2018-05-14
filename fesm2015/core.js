72b69625f006edc7b19793194a003bbb705898d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 22:56:55 +0000

    d4b6c41 fix(benchpress): Fix promise chain in chrome_driver_extension. (#23458)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+179.sha-66b2d78
+ * @license Angular v6.0.0-rc.5+180.sha-d4b6c41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+179.sha-66b2d78');
+const VERSION = new Version('6.0.0-rc.5+180.sha-d4b6c41');
 
 /**
  * @fileoverview added by tsickle
