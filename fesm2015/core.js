3a92c1f778d2c3df0d5ec1fb4f1a18c79a28f131
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 17:59:32 +0000

    02acb5e build(aio): improve `enum` API rendering (#23872)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+175.sha-d4b8b24
+ * @license Angular v6.0.0-rc.5+177.sha-02acb5e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+175.sha-d4b8b24');
+const VERSION = new Version('6.0.0-rc.5+177.sha-02acb5e');
 
 /**
  * @fileoverview added by tsickle
