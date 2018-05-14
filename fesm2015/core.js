6b12434e8c99e29995eeb874ee334086c7dc8ac3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 18:07:38 +0000

    67b8d57 docs(aio): use heroesUrl (#23884)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+176.sha-a2e8b3a
+ * @license Angular v6.0.0-rc.5+178.sha-67b8d57
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2034,7 +2034,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+176.sha-a2e8b3a');
+const VERSION = new Version('6.0.0-rc.5+178.sha-67b8d57');
 
 /**
  * @fileoverview added by tsickle
