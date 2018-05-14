533d4c682c3160f8a645accb15f5ef9cea654bc2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 18:00:52 +0000

    734d37b build(aio): improve `enum` API rendering (#23872)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+57.sha-bc20638
+ * @license Angular v6.0.0+58.sha-734d37b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0+57.sha-bc20638');
+var VERSION = new Version('6.0.0+58.sha-734d37b');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+57.sha-bc20638
+ * @license Angular v6.0.0+58.sha-734d37b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0+57.sha-bc20638');
+const VERSION = new Version('6.0.0+58.sha-734d37b');
 
 /**
  * @fileoverview added by tsickle
