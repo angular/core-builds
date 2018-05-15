d452a2be89611ccfed3e5693aa8167a893bec454
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 19:03:05 +0000

    ad6052e test: switch to ts_web_test_suite (#23859)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+60.sha-e9d1709
+ * @license Angular v6.0.0+61.sha-ad6052e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0+60.sha-e9d1709');
+const VERSION = new Version('6.0.0+61.sha-ad6052e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+60.sha-e9d1709
+ * @license Angular v6.0.0+61.sha-ad6052e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1567,7 +1567,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0+60.sha-e9d1709');
+var VERSION = new Version('6.0.0+61.sha-ad6052e');
 
 /**
  * @license
