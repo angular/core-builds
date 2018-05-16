2555cf3ab94e7bf37bbd94f92bc850ff44456ad4
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:02:46 +0000

    41c2030 docs(aio): fix path to observables guide (#23858) PR Close #23858

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+4.sha-daee41a
+ * @license Angular v6.0.2+5.sha-41c2030
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.2+4.sha-daee41a');
+var VERSION = new Version('6.0.2+5.sha-41c2030');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+4.sha-daee41a
+ * @license Angular v6.0.2+5.sha-41c2030
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+4.sha-daee41a');
+const VERSION = new Version('6.0.2+5.sha-41c2030');
 
 /**
  * @fileoverview added by tsickle
