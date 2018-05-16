874284d64ffefacd379da804189d7fc260fcf7f5
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:04:21 +0000

    52cbe89 docs(aio): add Angular Conf Australia to events (#22929)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+7.sha-f3361ab
+ * @license Angular v6.0.2+6.sha-52cbe89
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+7.sha-f3361ab');
+const VERSION = new Version('6.0.2+6.sha-52cbe89');
 
 /**
  * @fileoverview added by tsickle
