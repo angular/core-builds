e0f55bfc33e113e5a9fcf3b80d6040a0ab4fbfb4
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:00:24 +0000

    daee41a docs(aio): update HTTP error test example (#22844)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+3.sha-68bd45b
+ * @license Angular v6.0.2+4.sha-daee41a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+3.sha-68bd45b');
+const VERSION = new Version('6.0.2+4.sha-daee41a');
 
 /**
  * @fileoverview added by tsickle
