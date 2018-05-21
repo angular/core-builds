5afd32732cbde99efc677b4039aeed4161f56dc5
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:40:54 +0000

    eec231d docs(aio): add ant design of angular in resources (#23953)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+13.sha-2a83dbb
+ * @license Angular v6.0.2+17.sha-eec231d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.2+13.sha-2a83dbb');
+var VERSION = new Version('6.0.2+17.sha-eec231d');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+13.sha-2a83dbb
+ * @license Angular v6.0.2+17.sha-eec231d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+13.sha-2a83dbb');
+const VERSION = new Version('6.0.2+17.sha-eec231d');
 
 /**
  * @fileoverview added by tsickle
