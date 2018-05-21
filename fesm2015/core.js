d08b326569d300d2a1f6d95f6dbe9b432f027c3d
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:48:56 +0000

    4226070 docs(aio): Add missing dependencies and files to testing zip file download (#23948)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+16.sha-95344d6
+ * @license Angular v6.0.2+18.sha-4226070
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+16.sha-95344d6');
+const VERSION = new Version('6.0.2+18.sha-4226070');
 
 /**
  * @fileoverview added by tsickle
