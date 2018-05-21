1dbee51e329627df42873a23dd5ea89472abf7c7
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:45:39 +0000

    95344d6 fix(aio): avoid unnecessary re-calculations in live-examples (#23960)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+17.sha-eec231d
+ * @license Angular v6.0.2+16.sha-95344d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+17.sha-eec231d');
+const VERSION = new Version('6.0.2+16.sha-95344d6');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+17.sha-eec231d
+ * @license Angular v6.0.2+16.sha-95344d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1567,7 +1567,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.2+17.sha-eec231d');
+var VERSION = new Version('6.0.2+16.sha-95344d6');
 
 /**
  * @license
