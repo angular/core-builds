fff1e5eeac7225c18d39a77c9b7d1ecbb5c6b973
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:35:29 +0000

    4007d00 docs(aio): add mhartington to gde (#23777)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+10.sha-43ee10f
+ * @license Angular v6.0.2+12.sha-4007d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.2+10.sha-43ee10f');
+var VERSION = new Version('6.0.2+12.sha-4007d00');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+10.sha-43ee10f
+ * @license Angular v6.0.2+12.sha-4007d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+10.sha-43ee10f');
+const VERSION = new Version('6.0.2+12.sha-4007d00');
 
 /**
  * @fileoverview added by tsickle
