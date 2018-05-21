84241864f80628af95b79af3ec8587922b7614ae
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:40:33 +0000

    2a83dbb style(aio): remove background from `lazy-loading` concept icon (#23950)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+11.sha-61e32ac
+ * @license Angular v6.0.2+13.sha-2a83dbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.2+11.sha-61e32ac');
+var VERSION = new Version('6.0.2+13.sha-2a83dbb');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+11.sha-61e32ac
+ * @license Angular v6.0.2+13.sha-2a83dbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+11.sha-61e32ac');
+const VERSION = new Version('6.0.2+13.sha-2a83dbb');
 
 /**
  * @fileoverview added by tsickle
