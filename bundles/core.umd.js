49314ecab93c0682158a4375d0dcfa72613beff4
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 21:55:51 +0000

    469b1e4 docs: add doc to event-management api (#23656)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+22.sha-7fee1fd
+ * @license Angular v6.0.2+23.sha-469b1e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.2+22.sha-7fee1fd');
+var VERSION = new Version('6.0.2+23.sha-469b1e4');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+22.sha-7fee1fd
+ * @license Angular v6.0.2+23.sha-469b1e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.2+22.sha-7fee1fd');
+const VERSION = new Version('6.0.2+23.sha-469b1e4');
 
 /**
  * @fileoverview added by tsickle
