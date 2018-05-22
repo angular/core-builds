9dbf072b01056ef04fd75809b6a706c008fcdc6d
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 19:32:00 +0000

    0ee5b7e fix(service-worker): check platformBrowser before accessing navigator.serviceWorker (#21231)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+19.sha-23f2a70
+ * @license Angular v6.0.2+21.sha-0ee5b7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1567,7 +1567,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.2+19.sha-23f2a70');
+var VERSION = new Version('6.0.2+21.sha-0ee5b7e');
 
 /**
  * @license
