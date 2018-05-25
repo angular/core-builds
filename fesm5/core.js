b9120c0007a56cfae4b7e99c00a5847260b43e2b
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:18:53 +0000

    3e39fef refactor(aio): improve logging output in `update-preview-server.sh` (#24071)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+222.sha-36cc72e
+ * @license Angular v6.0.0-rc.5+223.sha-3e39fef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1588,7 +1588,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+222.sha-36cc72e');
+var VERSION = new Version('6.0.0-rc.5+223.sha-3e39fef');
 
 /**
  * @license
