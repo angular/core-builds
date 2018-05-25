5a79ac9682bb5a6c36b810d67b3218ab5cd881b2
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:16:42 +0000

    01b5acd fix(compiler): generate core-compliant hostBindings property (#24087)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+219.sha-609e6b9
+ * @license Angular v6.0.0-rc.5+221.sha-01b5acd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2078,7 +2078,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+219.sha-609e6b9');
+const VERSION = new Version('6.0.0-rc.5+221.sha-01b5acd');
 
 /**
  * @fileoverview added by tsickle
