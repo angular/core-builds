def14db8d94e9852d4e24fc7444c1cb987ebfc76
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 23:11:06 +0000

    96655f7 style(compiler): fix typo and formatting (#23729)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+19.sha-253f509
+ * @license Angular v6.0.3+20.sha-96655f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.3+19.sha-253f509');
+var VERSION = new Version('6.0.3+20.sha-96655f7');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+19.sha-253f509
+ * @license Angular v6.0.3+20.sha-96655f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.3+19.sha-253f509');
+const VERSION = new Version('6.0.3+20.sha-96655f7');
 
 /**
  * @fileoverview added by tsickle
