58ebf322ca1f1dcb73846c577ceb176e13738763
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:20:41 +0000

    5a02ae2 docs: update lts and labs practices (#23922)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+10.sha-7698afe
+ * @license Angular v6.0.3+18.sha-5a02ae2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1567,7 +1567,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.3+10.sha-7698afe');
+var VERSION = new Version('6.0.3+18.sha-5a02ae2');
 
 /**
  * @license
