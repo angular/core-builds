e192319c4b599472a80e3270eb3a6664937384bc
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:28:06 +0000

    1eafd04 build(ivy): support alternate compilation modes to enable Ivy testing (#24056)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+241.sha-c2e1311
+ * @license Angular v6.0.0-rc.5+243.sha-1eafd04
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2078,7 +2078,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+241.sha-c2e1311');
+const VERSION = new Version('6.0.0-rc.5+243.sha-1eafd04');
 
 /**
  * @fileoverview added by tsickle
