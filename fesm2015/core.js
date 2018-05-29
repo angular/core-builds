5582c6650e3cf0afd9849a63e9c8ae7e56f3c409
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:16:55 +0000

    d74078f docs(http): correct spelling error (#23675)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+233.sha-ddd6124
+ * @license Angular v6.0.0-rc.5+234.sha-d74078f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2078,7 +2078,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+233.sha-ddd6124');
+const VERSION = new Version('6.0.0-rc.5+234.sha-d74078f');
 
 /**
  * @fileoverview added by tsickle
