27aff1d897dd332620d419bcff3208dc92efa42d
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:21:31 +0000

    c2e1311 refactor(aio): rename method (loadContainingCustomElements --> loadContainedCustomElements) (#23944)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+234.sha-d74078f
+ * @license Angular v6.0.0-rc.5+241.sha-c2e1311
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2078,7 +2078,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+234.sha-d74078f');
+const VERSION = new Version('6.0.0-rc.5+241.sha-c2e1311');
 
 /**
  * @fileoverview added by tsickle
