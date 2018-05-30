9ca6af8d5a18e899693d16e3992c550687be2d3b
Victor Berchet <victor@suumit.com>
2018-05-30 17:49:19 +0000

    3ed7fc6 docs: update docs to use HttpClientModule instead of HttpModule (#22727)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+254.sha-5b25c07
+ * @license Angular v6.0.0-rc.5+253.sha-3ed7fc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1672,7 +1672,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+254.sha-5b25c07');
+var VERSION = new Version('6.0.0-rc.5+253.sha-3ed7fc6');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+254.sha-5b25c07
+ * @license Angular v6.0.0-rc.5+253.sha-3ed7fc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2078,7 +2078,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+254.sha-5b25c07');
+const VERSION = new Version('6.0.0-rc.5+253.sha-3ed7fc6');
 
 /**
  * @fileoverview added by tsickle
