ec26818f5ab14db69f3f381583db05693d80b359
Victor Berchet <victor@suumit.com>
2018-05-30 22:07:33 +0000

    4122502 docs: remove unfinished observables file (#23801)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+33.sha-e9f2203
+ * @license Angular v6.0.3+34.sha-4122502
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1651,7 +1651,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.3+33.sha-e9f2203');
+var VERSION = new Version('6.0.3+34.sha-4122502');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+33.sha-e9f2203
+ * @license Angular v6.0.3+34.sha-4122502
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.3+33.sha-e9f2203');
+const VERSION = new Version('6.0.3+34.sha-4122502');
 
 /**
  * @fileoverview added by tsickle
