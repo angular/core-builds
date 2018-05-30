4f9c67860460908edd2a1f396974a3b97b37c884
Victor Berchet <victor@suumit.com>
2018-05-30 19:27:22 +0000

    d249852 build: update rules_webtesting (#24198)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+27.sha-707dd59
+ * @license Angular v6.0.3+28.sha-d249852
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.3+27.sha-707dd59');
+const VERSION = new Version('6.0.3+28.sha-d249852');
 
 /**
  * @fileoverview added by tsickle
