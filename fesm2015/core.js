3ee8097572aa31972ed2c44dfc5b27922e7c3707
Victor Berchet <victor@suumit.com>
2018-05-30 17:48:15 +0000

    5b25c07 docs(bazel): improve error message for ng_package with no metadata (#22964)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+251.sha-2e21690
+ * @license Angular v6.0.0-rc.5+254.sha-5b25c07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2078,7 +2078,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+251.sha-2e21690');
+const VERSION = new Version('6.0.0-rc.5+254.sha-5b25c07');
 
 /**
  * @fileoverview added by tsickle
