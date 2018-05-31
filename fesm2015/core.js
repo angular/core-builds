6eedb799b8718e0d2e9c13625cd4fe0e57166155
Victor Berchet <victor@suumit.com>
2018-05-31 17:34:33 +0000

    6a663a4 fix(platform-server): don't reflect innerHTML property to attibute (#24213)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+286.sha-ec57133
+ * @license Angular v6.0.0-rc.5+287.sha-6a663a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+286.sha-ec57133');
+const VERSION = new Version('6.0.0-rc.5+287.sha-6a663a4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+286.sha-ec57133
+ * @license Angular v6.0.0-rc.5+287.sha-6a663a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1591,7 +1591,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+286.sha-ec57133');
+var VERSION = new Version('6.0.0-rc.5+287.sha-6a663a4');
 
 /**
  * @license
