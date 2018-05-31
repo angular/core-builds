460520a072be1b707123e8be6757773923aebef9
Victor Berchet <victor@suumit.com>
2018-05-31 00:56:17 +0000

    a10bdef docs(aio): Add GDE Kim Maida to contributors

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+43.sha-2f377db
+ * @license Angular v6.0.3+44.sha-a10bdef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.3+43.sha-2f377db');
+const VERSION = new Version('6.0.3+44.sha-a10bdef');
 
 /**
  * @fileoverview added by tsickle
