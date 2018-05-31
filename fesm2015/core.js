dd9e82a1d16a213540bbfea369cc3c0076510ba1
Victor Berchet <victor@suumit.com>
2018-05-31 00:26:51 +0000

    5d6074e docs: fix typo (#24210)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+275.sha-b492b9e
+ * @license Angular v6.0.0-rc.5+282.sha-5d6074e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+275.sha-b492b9e');
+const VERSION = new Version('6.0.0-rc.5+282.sha-5d6074e');
 
 /**
  * @fileoverview added by tsickle
