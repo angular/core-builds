2c3f019100a115484223b689b0d7e7ea0a28ed03
Victor Berchet <victor@suumit.com>
2018-05-31 00:55:57 +0000

    49d5de6 docs(aio): Add GDE Kim Maida to contributors

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+283.sha-4ab70fb
+ * @license Angular v6.0.0-rc.5+284.sha-49d5de6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1675,7 +1675,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+283.sha-4ab70fb');
+var VERSION = new Version('6.0.0-rc.5+284.sha-49d5de6');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+283.sha-4ab70fb
+ * @license Angular v6.0.0-rc.5+284.sha-49d5de6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+283.sha-4ab70fb');
+const VERSION = new Version('6.0.0-rc.5+284.sha-49d5de6');
 
 /**
  * @fileoverview added by tsickle
