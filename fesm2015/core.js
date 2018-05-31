4ad8b91074a7a1ca2e2ddc156cfdd71b07bff4c3
Victor Berchet <victor@suumit.com>
2018-05-31 23:35:06 +0000

    b18cf21 build(bazel): re-enable packages/upgrade/test:test_web test with static_files in ts_web_test_suite (#24214)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+46.sha-43e3073
+ * @license Angular v6.0.3+48.sha-b18cf21
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.3+46.sha-43e3073');
+const VERSION = new Version('6.0.3+48.sha-b18cf21');
 
 /**
  * @fileoverview added by tsickle
