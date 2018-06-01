cb7f8940fd91a09c9a850fbf2578f38d95773aaf
Victor Berchet <victor@suumit.com>
2018-06-01 21:04:38 +0000

    5cbcb56 build(bazel): update bazel integration test to test secondary angular imports such as @angular/common/http (#24170)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+296.sha-08f943a
+ * @license Angular v6.0.0-rc.5+298.sha-5cbcb56
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1619,7 +1619,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+296.sha-08f943a');
+var VERSION = new Version('6.0.0-rc.5+298.sha-5cbcb56');
 
 /**
  * @license
