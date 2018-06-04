544c07a3375ff4aaa536c99324fe9345495d33a4
Victor Berchet <victor@suumit.com>
2018-06-04 16:59:34 +0000

    0690622 docs(common): improve deprecation notices to be parsed by tslint

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+303.sha-5794506
+ * @license Angular v6.0.0-rc.5+304.sha-0690622
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1619,7 +1619,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+303.sha-5794506');
+var VERSION = new Version('6.0.0-rc.5+304.sha-0690622');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+303.sha-5794506
+ * @license Angular v6.0.0-rc.5+304.sha-0690622
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+303.sha-5794506');
+const VERSION = new Version('6.0.0-rc.5+304.sha-0690622');
 
 /**
  * @fileoverview added by tsickle
