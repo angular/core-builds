6513b4e393b9ec6e2090fd4fe2602e93097c0499
Victor Berchet <victor@suumit.com>
2018-06-04 20:51:37 +0000

    08a18b8 refactor(common): Remove ngOnChanges from NgForOf (#23378)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+308.sha-255463e
+ * @license Angular v6.0.0-rc.5+309.sha-08a18b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1619,7 +1619,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+308.sha-255463e');
+var VERSION = new Version('6.0.0-rc.5+309.sha-08a18b8');
 
 /**
  * @license
