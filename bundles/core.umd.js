5aab7faeb6c593579cc05b824259da5c635b20e5
Victor Berchet <victor@suumit.com>
2018-06-04 17:55:22 +0000

    a3d9878 docs(aio): remove an extraneous apostrophe (#24293) PR Close #24293

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+52.sha-0e1919c
+ * @license Angular v6.0.3+54.sha-a3d9878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1595,7 +1595,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.3+52.sha-0e1919c');
+var VERSION = new Version('6.0.3+54.sha-a3d9878');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+52.sha-0e1919c
+ * @license Angular v6.0.3+54.sha-a3d9878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.3+52.sha-0e1919c');
+const VERSION = new Version('6.0.3+54.sha-a3d9878');
 
 /**
  * @fileoverview added by tsickle
