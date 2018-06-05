23823ab16f9d66aadee954e4ba9d66787abfc753
Victor Berchet <victor@suumit.com>
2018-06-05 20:59:26 +0000

    9c40375 refactor(ivy): misc minor fixes in the JIT compiler (#24308)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+309.sha-08a18b8
+ * @license Angular v6.0.0-rc.5+311.sha-9c40375
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.0-rc.5+309.sha-08a18b8');
+const VERSION = new Version('6.0.0-rc.5+311.sha-9c40375');
 
 /**
  * @fileoverview added by tsickle
