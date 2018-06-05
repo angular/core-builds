bac9c4c18575c94433008c824d272e412b5a230a
Victor Berchet <victor@suumit.com>
2018-06-05 21:03:44 +0000

    ea3127e build(bazel): ran format (#24279)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+58.sha-faf60d9
+ * @license Angular v6.0.3+62.sha-ea3127e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1511,7 +1511,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.3+58.sha-faf60d9');
+var VERSION = new Version('6.0.3+62.sha-ea3127e');
 
 /**
  * @license
