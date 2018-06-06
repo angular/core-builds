3654086a3b63cb351ddb09ce715cfa9c986c2e3b
Victor Berchet <victor@suumit.com>
2018-06-06 19:00:22 +0000

    acf270d feat(ivy): added new namespace and element instructions to JIT environment (#23899)

--- src/render3/interfaces/renderer.d.ts
+++ src/render3/interfaces/renderer.d.ts
@@ -13,6 +13,7 @@ export declare type Renderer3 = ObjectOrientedRenderer3 | ProceduralRenderer3;
  * */
 export interface ObjectOrientedRenderer3 {
     createElement(tagName: string): RElement;
+    createElementNS(namespace: string, name: string): RElement;
     createTextNode(data: string): RText;
     querySelector(selectors: string): RElement | null;
 }
