72a2a126b499e910e7cfb75889ab007c2003975f
Victor Berchet <victor@suumit.com>
2018-06-06 17:54:00 +0000

    23c50e2 build(docs-infra): log warning rather than error if content errors are not fatal (#24320)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+66.sha-0ae8ea2
+ * @license Angular v6.0.3+67.sha-23c50e2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.3+66.sha-0ae8ea2');
+const VERSION = new Version('6.0.3+67.sha-23c50e2');
 
 /**
  * @fileoverview added by tsickle
