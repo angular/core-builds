11c0f64e1a623f7db4fe8e650f0efb3f9eed6b00
Victor Berchet <victor@suumit.com>
2018-06-06 19:29:19 +0000

    47814b4 release: cut the v6.0.4 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+337.sha-700e55c
+ * @license Angular v6.0.0-rc.5+338.sha-47814b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1619,7 +1619,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+337.sha-700e55c');
+var VERSION = new Version('6.0.0-rc.5+338.sha-47814b4');
 
 /**
  * @license
