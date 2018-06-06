7aeb88035e7d12b2bf9eff6908121e848c3fcf77
Victor Berchet <victor@suumit.com>
2018-06-06 20:59:33 +0000

    9dd647b release: cut the v6.1.0-beta.0 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+338.sha-47814b4
+ * @license Angular v6.1.0-beta.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1535,7 +1535,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.0-rc.5+338.sha-47814b4');
+var VERSION = new Version('6.1.0-beta.0');
 
 /**
  * @license
