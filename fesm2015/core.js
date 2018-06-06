da61b4285ed767999e93bb5eb2c7a44b3e6d0b02
Victor Berchet <victor@suumit.com>
2018-06-06 17:49:50 +0000

    0ae8ea2 build(aio): ensure the correct decorator properties are merged (#24289)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+65.sha-19deca1
+ * @license Angular v6.0.3+66.sha-0ae8ea2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.3+65.sha-19deca1');
+const VERSION = new Version('6.0.3+66.sha-0ae8ea2');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+65.sha-19deca1
+ * @license Angular v6.0.3+66.sha-0ae8ea2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1511,7 +1511,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.3+65.sha-19deca1');
+var VERSION = new Version('6.0.3+66.sha-0ae8ea2');
 
 /**
  * @license
