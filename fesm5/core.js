3dcc5a67d3f784dcd017beb10de8dca894cf789c
Victor Berchet <victor@suumit.com>
2018-06-06 01:54:51 +0000

    19deca1 fix(animations): retain trigger-state for nodes that are moved around (#24238)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+64.sha-dc3e8aa
+ * @license Angular v6.0.3+65.sha-19deca1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1511,7 +1511,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.3+64.sha-dc3e8aa');
+var VERSION = new Version('6.0.3+65.sha-19deca1');
 
 /**
  * @license
