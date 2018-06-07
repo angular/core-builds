9b65c477cdb1ac3781d4d0a93cd97c5cf7a7cc7c
Miško Hevery <misko@hevery.com>
2018-06-07 22:19:49 +0000

    cfa5b65 build(bazel): fix ng_package rollup root dir for fesm2015 output (#24298)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4
+ * @license Angular v6.0.4+1.sha-cfa5b65
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1511,7 +1511,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.4');
+var VERSION = new Version('6.0.4+1.sha-cfa5b65');
 
 /**
  * @license
