edec33f71ef92fa6d5369cae61489b6d2244de77
Miško Hevery <misko@hevery.com>
2018-06-08 23:05:37 +0000

    1b253e1 fix(ivy): special case [style] and [class] bindings for future use (#23232)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+25.sha-8c1ac28
+ * @license Angular v6.1.0-beta.0+26.sha-1b253e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.0+25.sha-8c1ac28');
+const VERSION = new Version('6.1.0-beta.0+26.sha-1b253e1');
 
 /**
  * @fileoverview added by tsickle
