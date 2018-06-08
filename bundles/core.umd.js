046cc77abd32f551e274c01b493b4e6721ca2236
Miško Hevery <misko@hevery.com>
2018-06-08 22:59:54 +0000

    5ef7a07 docs(ivy): add `<ng-container>` to the remaining work items (#24381)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+23.sha-1135563
+ * @license Angular v6.1.0-beta.0+24.sha-5ef7a07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1622,7 +1622,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.0+23.sha-1135563');
+var VERSION = new Version('6.1.0-beta.0+24.sha-5ef7a07');
 
 /**
  * @license
