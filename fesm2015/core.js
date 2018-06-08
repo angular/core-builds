ce3144af0cfef5eaab2018219c4a442af8ece0ef
Miško Hevery <misko@hevery.com>
2018-06-08 21:29:10 +0000

    9f877f4 build(docs-infra): ensure stability is computed before the API list (#24356)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+18.sha-d4c66d5
+ * @license Angular v6.1.0-beta.0+20.sha-9f877f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2084,7 +2084,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.0+18.sha-d4c66d5');
+const VERSION = new Version('6.1.0-beta.0+20.sha-9f877f4');
 
 /**
  * @fileoverview added by tsickle
