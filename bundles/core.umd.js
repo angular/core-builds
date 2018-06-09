4ef0c41efae78c7a1e01aa699974ac98ade46bb6
Miško Hevery <misko@hevery.com>
2018-06-09 00:13:06 +0000

    8aa70c2 docs: adds information about the VSCode clang-format extension (#24351)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+27.sha-49c5234
+ * @license Angular v6.1.0-beta.0+28.sha-8aa70c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1622,7 +1622,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.0+27.sha-49c5234');
+var VERSION = new Version('6.1.0-beta.0+28.sha-8aa70c2');
 
 /**
  * @license
