7c90efa7f67b4fde628850195281d2d4adf49174
Miško Hevery <misko@hevery.com>
2018-06-09 00:08:46 +0000

    0139173 fix(animations): always render end-state styles for orphaned DOM nodes (#24236)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+6.sha-f3c9954
+ * @license Angular v6.0.4+7.sha-0139173
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1511,7 +1511,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.4+6.sha-f3c9954');
+var VERSION = new Version('6.0.4+7.sha-0139173');
 
 /**
  * @license
