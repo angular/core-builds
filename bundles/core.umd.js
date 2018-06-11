d8b3777051406fe4f9ab02c405132bce89fb72db
Miško Hevery <misko@hevery.com>
2018-06-11 16:50:25 +0000

    5e8bf2f build(docs-infra): ensure `dist/` directory is cleaned before running `tsc --watch` (#24372)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+30.sha-29eb24b
+ * @license Angular v6.1.0-beta.0+32.sha-5e8bf2f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1622,7 +1622,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.0+30.sha-29eb24b');
+var VERSION = new Version('6.1.0-beta.0+32.sha-5e8bf2f');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+30.sha-29eb24b
+ * @license Angular v6.1.0-beta.0+32.sha-5e8bf2f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2095,7 +2095,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.0+30.sha-29eb24b');
+const VERSION = new Version('6.1.0-beta.0+32.sha-5e8bf2f');
 
 /**
  * @fileoverview added by tsickle
