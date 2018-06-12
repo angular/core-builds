6f31c10bbe9c01ce3de7570f5cb1505a838a6ff4
Miško Hevery <misko@hevery.com>
2018-06-12 19:19:21 +0000

    c2b5ebf build: update buildifier to latest (#24296)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+40.sha-1316024
+ * @license Angular v6.1.0-beta.0+42.sha-c2b5ebf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10863,7 +10863,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.0+40.sha-1316024');
+var VERSION = new Version('6.1.0-beta.0+42.sha-c2b5ebf');
 
 /**
  * @license
