304032e3a4768b5ab55cf8df9c3d7490484536c0
Miško Hevery <misko@hevery.com>
2018-06-13 21:07:27 +0000

    9a9a7de docs: add message property to compose-message component (#24310)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+15.sha-451d996
+ * @license Angular v6.0.4+22.sha-9a9a7de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2032,7 +2032,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.4+15.sha-451d996');
+const VERSION = new Version('6.0.4+22.sha-9a9a7de');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+15.sha-451d996
+ * @license Angular v6.0.4+22.sha-9a9a7de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1511,7 +1511,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.4+15.sha-451d996');
+var VERSION = new Version('6.0.4+22.sha-9a9a7de');
 
 /**
  * @license
