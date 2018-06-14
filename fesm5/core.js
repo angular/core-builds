1e829586f3d91c25ca4471a5e82e7e87dfd7102c
Miško Hevery <misko@hevery.com>
2018-06-14 00:13:34 +0000

    03f93b3 Revert "build: update to Bazel 0.14.0 (#24296)" (#24492)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1
+ * @license Angular v6.1.0-beta.1+1.sha-03f93b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1539,7 +1539,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.1');
+var VERSION = new Version('6.1.0-beta.1+1.sha-03f93b3');
 
 /**
  * @license
