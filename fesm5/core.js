73fe0046e19b2e58e5072d4625dd9ed902f09d6c
Miško Hevery <misko@hevery.com>
2018-06-14 00:08:36 +0000

    3ccb449 release: cut the v6.1.0-beta.1 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+56.sha-503a524
+ * @license Angular v6.1.0-beta.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1539,7 +1539,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.0+56.sha-503a524');
+var VERSION = new Version('6.1.0-beta.1');
 
 /**
  * @license
