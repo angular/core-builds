aef6fd2c46ff451e7358fc64536cad4122987f27
Miško Hevery <misko@hevery.com>
2018-06-14 21:44:59 +0000

    7c8159b test(bazel): fix flakey bazel integration e2e test (#24522)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+24.sha-5aa12c7
+ * @license Angular v6.1.0-beta.1+25.sha-7c8159b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1602,7 +1602,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.1+24.sha-5aa12c7');
+var VERSION = new Version('6.1.0-beta.1+25.sha-7c8159b');
 
 /**
  * @license
