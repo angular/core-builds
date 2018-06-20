15f05bf78eb160f3b26490cfab56a5196eb3c610
Miško Hevery <misko@hevery.com>
2018-06-20 23:50:39 +0000

    e81982e release: cut the v6.0.6 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+36.sha-776bc38
+ * @license Angular v6.0.6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1984,7 +1984,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.5+36.sha-776bc38');
+const VERSION = new Version('6.0.6');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+36.sha-776bc38
+ * @license Angular v6.0.6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1490,7 +1490,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.5+36.sha-776bc38');
+var VERSION = new Version('6.0.6');
 
 /**
  * @license
