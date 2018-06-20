54ebd7733b9ca8bcb51ed291047ee3367072e3dd
Miško Hevery <misko@hevery.com>
2018-06-20 18:30:58 +0000

    776bc38 test(animations): properly reference body node for SSR environments (#23300)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+28.sha-74f07f4
+ * @license Angular v6.0.5+36.sha-776bc38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1984,7 +1984,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.5+28.sha-74f07f4');
+const VERSION = new Version('6.0.5+36.sha-776bc38');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+28.sha-74f07f4
+ * @license Angular v6.0.5+36.sha-776bc38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1490,7 +1490,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.5+28.sha-74f07f4');
+var VERSION = new Version('6.0.5+36.sha-776bc38');
 
 /**
  * @license
