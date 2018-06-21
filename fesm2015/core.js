76ad6e57d45169ca19009ca2cafd23cf898573a8
Miško Hevery <misko@hevery.com>
2018-06-21 20:53:22 +0000

    637805a docs: update `lowercase` pipe example in "AngularJS to Angular" guide (#24588)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+10.sha-7b2b1af
+ * @license Angular v6.1.0-beta.2+11.sha-637805a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10922,7 +10922,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.2+10.sha-7b2b1af');
+const VERSION = new Version('6.1.0-beta.2+11.sha-637805a');
 
 /**
  * @license
