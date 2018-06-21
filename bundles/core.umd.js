ecdae8f9a16103dcb4164fe55ebfa30eaa88be98
Miško Hevery <misko@hevery.com>
2018-06-21 00:25:18 +0000

    1f532aa feat(docs-infra): Add GitHub and Twitter external icon links to topnav toolbar (#24542)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6
+ * @license Angular v6.0.6+1.sha-1f532aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1574,7 +1574,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.6');
+var VERSION = new Version('6.0.6+1.sha-1f532aa');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6
+ * @license Angular v6.0.6+1.sha-1f532aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1984,7 +1984,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.6');
+const VERSION = new Version('6.0.6+1.sha-1f532aa');
 
 /**
  * @fileoverview added by tsickle
