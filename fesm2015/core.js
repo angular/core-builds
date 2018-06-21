91cc88d97fb946e40cde2168c93d6868affecf37
Miško Hevery <misko@hevery.com>
2018-06-21 00:22:42 +0000

    def354d release: cut the v6.1.0-beta.2 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+43.sha-9782736
+ * @license Angular v6.1.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10483,7 +10483,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.1+43.sha-9782736');
+const VERSION = new Version('6.1.0-beta.2');
 
 /**
  * @license
