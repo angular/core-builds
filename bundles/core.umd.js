f9753b521dfcedebf379c9504da242883ab98e8a
Miško Hevery <misko@hevery.com>
2018-06-25 17:43:29 +0000

    2a68ba4 docs: fix misdirected group links (#24569)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+25.sha-941d2cd
+ * @license Angular v6.1.0-beta.2+27.sha-2a68ba4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11495,7 +11495,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.2+25.sha-941d2cd');
+var VERSION = new Version('6.1.0-beta.2+27.sha-2a68ba4');
 
 /**
  * @license
