6a36eb1711852ccc2c41209833d638c6292e3477
Miško Hevery <misko@hevery.com>
2018-06-25 16:11:21 +0000

    fba3f10 docs: add guide-angular.wishtack.io to education resources (#24585)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+17.sha-39c7769
+ * @license Angular v6.1.0-beta.2+19.sha-fba3f10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11318,7 +11318,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.2+17.sha-39c7769');
+var VERSION = new Version('6.1.0-beta.2+19.sha-fba3f10');
 
 /**
  * @license
