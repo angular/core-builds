a93968cb8b88deae4dbbca3ae75e57ef438c0749
Jason Aden <jasonaden@google.com>
2018-06-25 17:15:42 +0000

    d244523 docs: test api doc for pipes (#24141)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+23.sha-f841e36
+ * @license Angular v6.1.0-beta.2+26.sha-d244523
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11403,7 +11403,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.2+23.sha-f841e36');
+var VERSION = new Version('6.1.0-beta.2+26.sha-d244523');
 
 /**
  * @license
