dfbc98d74b1d8cec48b99e46587319b61bcc353e
Miško Hevery <misko@hevery.com>
2018-06-25 16:28:50 +0000

    f841e36 ci: scripts to review PRs locally (#24623)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+22.sha-f229449
+ * @license Angular v6.1.0-beta.2+23.sha-f841e36
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1517,7 +1517,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.2+22.sha-f229449');
+var VERSION = new Version('6.1.0-beta.2+23.sha-f841e36');
 
 /**
  * @license
