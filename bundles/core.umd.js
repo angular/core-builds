717dfdb5c66a4916f1253d4a3fefe6ff4f506938
Pete Bacon Darwin <pete@bacondarwin.com>
2018-06-25 13:49:38 +0000

    db21dbf test(ivy): fix ngcc Esm2015ReflectionHost tests

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+44.sha-368a7ed
+ * @license Angular v6.1.0-beta.1+45.sha-db21dbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10971,7 +10971,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.1+44.sha-368a7ed');
+var VERSION = new Version('6.1.0-beta.1+45.sha-db21dbf');
 
 /**
  * @license
