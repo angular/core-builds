0747bafcbb18630730fe08cdd6696ae70f1418e4
Miško Hevery <misko@hevery.com>
2018-06-25 17:29:07 +0000

    393db94 docs: fix misdirected group links (#24569)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+11.sha-65744e4
+ * @license Angular v6.0.6+12.sha-393db94
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1984,7 +1984,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.6+11.sha-65744e4');
+const VERSION = new Version('6.0.6+12.sha-393db94');
 
 /**
  * @fileoverview added by tsickle
