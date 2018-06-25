5d418835ee2a89fa85497749ecd2b93c3b4faff0
Miško Hevery <misko@hevery.com>
2018-06-25 15:56:23 +0000

    8919577 docs: update Angular CLI option for sourcemaps (#24437)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+4.sha-84eff42
+ * @license Angular v6.0.6+5.sha-8919577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1574,7 +1574,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.6+4.sha-84eff42');
+var VERSION = new Version('6.0.6+5.sha-8919577');
 
 /**
  * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+4.sha-84eff42
+ * @license Angular v6.0.6+5.sha-8919577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1984,7 +1984,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.6+4.sha-84eff42');
+const VERSION = new Version('6.0.6+5.sha-8919577');
 
 /**
  * @fileoverview added by tsickle
