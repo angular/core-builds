bf19ed2d53ad0ad6365f52ab64e0189aee87e427
Jason Aden <jasonaden@google.com>
2018-06-25 17:09:20 +0000

    56bc869 test(aio): fix upgrade-phonecat examples e2e tests (#24583)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+8.sha-0b356d4
+ * @license Angular v6.0.6+10.sha-56bc869
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1984,7 +1984,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.6+8.sha-0b356d4');
+const VERSION = new Version('6.0.6+10.sha-56bc869');
 
 /**
  * @fileoverview added by tsickle
