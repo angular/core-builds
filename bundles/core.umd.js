0d5108ca9fe692ecf2aff9e0739d79a8e76ba833
Miško Hevery <misko@hevery.com>
2018-06-25 16:14:07 +0000

    0b356d4 fix(animations): set animations styles properly on platform-server (#24624)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+7.sha-a886659
+ * @license Angular v6.0.6+8.sha-0b356d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1574,7 +1574,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.6+7.sha-a886659');
+var VERSION = new Version('6.0.6+8.sha-0b356d4');
 
 /**
  * @license
