3eb5487068a4f67764b7d2da32ab6fa7db4f2a6a
Miško Hevery <misko@hevery.com>
2018-06-25 16:03:53 +0000

    c95437f build(bazel): Turning on strictPropertyInitialization for Angular. (#24572)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+15.sha-71b0c3d
+ * @license Angular v6.1.0-beta.2+18.sha-c95437f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10922,7 +10922,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.2+15.sha-71b0c3d');
+const VERSION = new Version('6.1.0-beta.2+18.sha-c95437f');
 
 /**
  * @license
