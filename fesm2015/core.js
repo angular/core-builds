a0b1654ab77147e58a114a2cd14986a3e06f61b9
Jason Aden <jasonaden@google.com>
2018-06-26 18:32:04 +0000

    3513ae4 docs(aio): fix adding to template driven forms (#23743)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+17.sha-426f971
+ * @license Angular v6.0.6+22.sha-3513ae4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2089,7 +2089,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.6+17.sha-426f971');
+const VERSION = new Version('6.0.6+22.sha-3513ae4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+17.sha-426f971
+ * @license Angular v6.0.6+22.sha-3513ae4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.6+17.sha-426f971');
+var VERSION = new Version('6.0.6+22.sha-3513ae4');
 
 /**
  * @license
