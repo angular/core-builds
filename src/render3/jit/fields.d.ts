a2083dd299f308d35f0cf3831fcea58bbc59a6d6
Jason Aden <jasonaden@google.com>
2018-06-26 18:30:25 +0000

    855e8ad fix(ivy): use closure-safe field name for JIT of ngInjectableDef (#24632)

--- src/render3/jit/fields.d.ts
+++ src/render3/jit/fields.d.ts
@@ -1,4 +1,6 @@
 export declare const NG_COMPONENT_DEF: string;
 export declare const NG_DIRECTIVE_DEF: string;
+export declare const NG_INJECTABLE_DEF: string;
+export declare const NG_INJECTOR_DEF: string;
 export declare const NG_PIPE_DEF: string;
 export declare const NG_MODULE_DEF: string;
