dcddaac549e87ad2deeb1731b31d6da58de56caf
Jason Aden <jasonaden@google.com>
2018-06-26 18:32:24 +0000

    cf0968f build(docs-infra): support hiding constructors of injectable classes (#24529)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+33.sha-cf0968f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2152,7 +2152,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.2+32.sha-855e8ad');
+const VERSION = new Version('6.1.0-beta.2+33.sha-cf0968f');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+33.sha-cf0968f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1617,7 +1617,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.2+32.sha-855e8ad');
+var VERSION = new Version('6.1.0-beta.2+33.sha-cf0968f');
 
 /**
  * @license
