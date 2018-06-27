19bf8debbf99eebba5a51eb5b1ce1f224c8afc8f
Jason Aden <jasonaden@google.com>
2018-06-27 22:09:10 +0000

    6d68f3e docs(changelog): remove repeating blocks (#24654) PR Close #24654

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+40.sha-50fb13f
+ * @license Angular v6.1.0-beta.2+41.sha-6d68f3e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11143,7 +11143,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.2+40.sha-50fb13f');
+const VERSION = new Version('6.1.0-beta.2+41.sha-6d68f3e');
 
 /**
  * @license
