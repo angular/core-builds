f20c49c5003dfd5a60c0c687f65d10bce3eb2e8c
Jason Aden <jasonaden@google.com>
2018-06-27 22:34:19 +0000

    abed2cd refactor(upgrade): fix examples for `strictPropertyInitialization` and remove internal comments (#18487) (#18487)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+43.sha-8dec381
+ * @license Angular v6.1.0-beta.2+53.sha-abed2cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11539,7 +11539,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.2+43.sha-8dec381');
+var VERSION = new Version('6.1.0-beta.2+53.sha-abed2cd');
 
 /**
  * @license
