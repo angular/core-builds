532149342bd6fc8ed520387d21cac9a5f6d32b12
Miško Hevery <misko@hevery.com>
2018-06-28 22:50:47 +0000

    d876700 build(docs-infra): render short description of parameters in API docs (#24537)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+14.sha-99bdd25
+ * @license Angular v6.1.0-beta.3+15.sha-d876700
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11587,7 +11587,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+14.sha-99bdd25');
+var VERSION = new Version('6.1.0-beta.3+15.sha-d876700');
 
 /**
  * @license
