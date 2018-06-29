15c4faf16ccf5d6d8bcc88e82887426b7c4e3d44
Misko Hevery <misko@hevery.com>
2018-06-29 14:29:51 +0000

    0f1de35 docs(core): rephrase doc for Injector.get (#24670)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+4.sha-d89f57f
+ * @license Angular v6.0.7+5.sha-0f1de35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2089,7 +2089,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.7+4.sha-d89f57f');
+const VERSION = new Version('6.0.7+5.sha-0f1de35');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+4.sha-d89f57f
+ * @license Angular v6.0.7+5.sha-0f1de35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.7+4.sha-d89f57f');
+var VERSION = new Version('6.0.7+5.sha-0f1de35');
 
 /**
  * @license
