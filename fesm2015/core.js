a9abab35885aac1bdeb58e8ccc668d55d85d0d2f
Misko Hevery <misko@hevery.com>
2018-06-29 14:28:38 +0000

    ff84c5c fix(common): properly update collection reference in NgForOf (#24684)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+18.sha-87ddbdf
+ * @license Angular v6.1.0-beta.3+19.sha-ff84c5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11395,7 +11395,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+18.sha-87ddbdf');
+const VERSION = new Version('6.1.0-beta.3+19.sha-ff84c5c');
 
 /**
  * @license
