ccf89484f51c29e18fe1df361a15f022b66125d5
Miško Hevery <misko@hevery.com>
2018-07-02 22:09:33 +0000

    4e7a44c docs: fix typo in pipes guide (#24452)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+21.sha-d1805d0
+ * @license Angular v6.1.0-beta.3+22.sha-4e7a44c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11383,7 +11383,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+21.sha-d1805d0');
+const VERSION = new Version('6.1.0-beta.3+22.sha-4e7a44c');
 
 /**
  * @license
