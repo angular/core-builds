37415c8f19493c58b5a8fe231e061c6f7692d782
Miško Hevery <misko@hevery.com>
2018-07-03 23:05:16 +0000

    b6af870 feat(ivy): AOT support for compilation of @Pipes (#24703)

--- src/linker/view_container_ref.d.ts
+++ src/linker/view_container_ref.d.ts
@@ -34,9 +34,9 @@ export declare abstract class ViewContainerRef {
      * Anchor element that specifies the location of this container in the containing View.
      * <!-- TODO: rename to anchorElement -->
      */
-    readonly abstract element: ElementRef;
-    readonly abstract injector: Injector;
-    readonly abstract parentInjector: Injector;
+    abstract readonly element: ElementRef;
+    abstract readonly injector: Injector;
+    abstract readonly parentInjector: Injector;
     /**
      * Destroys all Views in this container.
      */
@@ -48,7 +48,7 @@ export declare abstract class ViewContainerRef {
     /**
      * Returns the number of Views currently attached to this container.
      */
-    readonly abstract length: number;
+    abstract readonly length: number;
     /**
      * Instantiates an Embedded View based on the {@link TemplateRef `templateRef`} and inserts it
      * into this container at the specified `index`.
