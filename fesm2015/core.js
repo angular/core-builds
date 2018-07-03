ceefadeeebbee6edcfc1e861c2b3e63d284d5474
Miško Hevery <misko@hevery.com>
2018-07-03 16:24:52 +0000

    0922228 fix(core): use addCustomEqualityTester instead of overriding toEqual (#22983)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+27.sha-c94a2c9
+ * @license Angular v6.1.0-beta.3+28.sha-0922228
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2166,7 +2166,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+27.sha-c94a2c9');
+const VERSION = new Version('6.1.0-beta.3+28.sha-0922228');
 
 /**
  * @fileoverview added by tsickle
