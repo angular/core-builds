efc0c2fd5dbf83aa165b2626acb8ee8613349b7d
Miško Hevery <misko@hevery.com>
2018-07-06 19:08:28 +0000

    f62876b fix(ivy): pipes are pure by default (#24750)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+35.sha-b6af870
+ * @license Angular v6.1.0-beta.3+46.sha-f62876b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2284,7 +2284,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+35.sha-b6af870');
+const VERSION = new Version('6.1.0-beta.3+46.sha-f62876b');
 
 /**
  * @fileoverview added by tsickle
