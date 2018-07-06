6c19bd85bc3b051b9c6fa73afeafd8fdb8c86216
Miško Hevery <misko@hevery.com>
2018-07-06 18:18:49 +0000

    f974c48 docs: describe rounding behaviour of 'DecimalPipe' (#24303)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+40.sha-5686123
+ * @license Angular v6.1.0-beta.3+41.sha-f974c48
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1631,7 +1631,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+40.sha-5686123');
+var VERSION = new Version('6.1.0-beta.3+41.sha-f974c48');
 
 /**
  * @license
