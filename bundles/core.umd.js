897a63403b53d47cf5e6012c59e0a31a890a2815
Miško Hevery <misko@hevery.com>
2018-07-06 21:33:48 +0000

    f936b8c docs: refactored ng-container code (#22742)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+47.sha-1e74ea9
+ * @license Angular v6.1.0-beta.3+56.sha-f936b8c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11788,7 +11788,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+47.sha-1e74ea9');
+    var VERSION = new Version('6.1.0-beta.3+56.sha-f936b8c');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+47.sha-1e74ea9
+ * @license Angular v6.1.0-beta.3+56.sha-f936b8c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11302,7 +11302,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+47.sha-1e74ea9');
+const VERSION = new Version('6.1.0-beta.3+56.sha-f936b8c');
 
 /**
  * @license
