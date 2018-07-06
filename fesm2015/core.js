9e641e8c2fa11da0804aafd15045bb4a798441cb
Miško Hevery <misko@hevery.com>
2018-07-06 18:13:26 +0000

    69c8226 docs: add app.module to changed documents (#23876)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+15.sha-ada486a
+ * @license Angular v6.0.7+17.sha-69c8226
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2103,7 +2103,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.0.7+15.sha-ada486a');
+const VERSION = new Version('6.0.7+17.sha-69c8226');
 
 /**
  * @fileoverview added by tsickle
