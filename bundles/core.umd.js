b8cd870e59504dfb19419acc89a588579c1922e5
Miško Hevery <misko@hevery.com>
2018-07-06 18:20:31 +0000

    b719905 docs: clarify faqs about services (#24079)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+38.sha-56a8533
+ * @license Angular v6.1.0-beta.3+39.sha-b719905
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11919,7 +11919,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+38.sha-56a8533');
+var VERSION = new Version('6.1.0-beta.3+39.sha-b719905');
 
 /**
  * @license
