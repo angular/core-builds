a690465761ea37535e187f32ae6b604ca4ae2da4
Miško Hevery <misko@hevery.com>
2018-07-06 19:09:44 +0000

    f62876b fix(ivy): pipes are pure by default (#24750)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+35.sha-b6af870
+ * @license Angular v6.1.0-beta.3+46.sha-f62876b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11928,7 +11928,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+35.sha-b6af870');
+var VERSION = new Version('6.1.0-beta.3+46.sha-f62876b');
 
 /**
  * @license
