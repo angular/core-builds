b6aec4510464fb86085e693674f9754cd029e298
Miško Hevery <misko@hevery.com>
2018-07-06 18:21:05 +0000

    5686123 docs(aio): added a link to Angular Zero online course (Traditional Chinese) (#24228)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+39.sha-b719905
+ * @license Angular v6.1.0-beta.3+40.sha-5686123
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11425,7 +11425,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+39.sha-b719905');
+const VERSION = new Version('6.1.0-beta.3+40.sha-5686123');
 
 /**
  * @license
