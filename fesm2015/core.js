4ce6730c4f6feabe77319c6dee6ae238ed6a4362
Miško Hevery <misko@hevery.com>
2018-07-06 18:17:59 +0000

    b72dbc8 docs(router): add `paramsInheritanceStrategy` documentation (#22590)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+36.sha-8fe8b8f
+ * @license Angular v6.1.0-beta.3+37.sha-b72dbc8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11425,7 +11425,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+36.sha-8fe8b8f');
+const VERSION = new Version('6.1.0-beta.3+37.sha-b72dbc8');
 
 /**
  * @license
