07e8127d7824ef967074301f35d3b3343ba43e00
Miško Hevery <misko@hevery.com>
2018-07-06 21:37:07 +0000

    250527c feat(service-worker): add support for `?` in SW config globbing (#24105)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+55.sha-d571a51
+ * @license Angular v6.1.0-beta.3+58.sha-250527c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11303,7 +11303,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+55.sha-d571a51');
+const VERSION = new Version('6.1.0-beta.3+58.sha-250527c');
 
 /**
  * @license
