8441bb9b99586765bab11ba629e027a3c64d9e23
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-09 12:03:56 +0000

    da016d0 feat(ivy): implement ngcc `Esm5FileParser`

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+83.sha-15ca58a
+ * @license Angular v6.1.0-beta.3+84.sha-da016d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __spread, __param, __assign, __read, __values } from 'tslib';
+import { __decorate, __metadata, __assign, __param, __extends, __spread, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -12193,7 +12193,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+83.sha-15ca58a');
+var VERSION = new Version('6.1.0-beta.3+84.sha-da016d0');
 
 /**
  * @license
