ae00b3d3f31517c97d809e0bad18f79cba7d6419
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-09 10:30:43 +0000

    15ca58a feat(ivy): implement ngcc `Esm5ReflectionHost`

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+82.sha-ac61c99
+ * @license Angular v6.1.0-beta.3+83.sha-15ca58a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1938,7 +1938,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+82.sha-ac61c99');
+const VERSION = new Version('6.1.0-beta.3+83.sha-15ca58a');
 
 /**
  * @fileoverview added by tsickle
