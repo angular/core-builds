056d8ff07f690a429ed348286c9aa73d70a8bb3d
Miško Hevery <misko@hevery.com>
2018-07-10 19:40:36 +0000

    0d5f2d3 fix(compiler-cli): Use typescript to resolve modules for metadata (#22856)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+70.sha-a167bca
+ * @license Angular v6.1.0-beta.3+71.sha-0d5f2d3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11755,7 +11755,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+70.sha-a167bca');
+const VERSION = new Version('6.1.0-beta.3+71.sha-0d5f2d3');
 
 /**
  * @license
