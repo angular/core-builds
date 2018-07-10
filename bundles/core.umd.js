00a4cb24937b1a3454ac329ae3ed8494c4861d12
Miško Hevery <misko@hevery.com>
2018-07-10 19:28:50 +0000

    197387d fix(platform-browser): mark Meta and Title services as tree shakable providers (#24815)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+67.sha-1089261
+ * @license Angular v6.1.0-beta.3+68.sha-197387d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12241,7 +12241,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+67.sha-1089261');
+    var VERSION = new Version('6.1.0-beta.3+68.sha-197387d');
 
     /**
      * @license
