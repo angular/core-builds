918fd866e09efa5d157781bef802446dd2891e44
Miško Hevery <misko@hevery.com>
2018-07-10 19:39:33 +0000

    0d5f2d3 fix(compiler-cli): Use typescript to resolve modules for metadata (#22856)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+70.sha-a167bca
+ * @license Angular v6.1.0-beta.3+71.sha-0d5f2d3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12256,7 +12256,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+70.sha-a167bca');
+    var VERSION = new Version('6.1.0-beta.3+71.sha-0d5f2d3');
 
     /**
      * @license
