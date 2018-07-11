ca1d167ed34a5cb509d37e5cc4e2ef93452e6060
Miško Hevery <misko@hevery.com>
2018-07-11 21:38:23 +0000

    b9e095a release: cut the v6.0.8 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+86.sha-81a9db2
+ * @license Angular v6.1.0-beta.3+88.sha-b9e095a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+86.sha-81a9db2');
+    var VERSION = new Version('6.1.0-beta.3+88.sha-b9e095a');
 
     /**
      * @license
