2f33b5a12aea19fd44f1f89561a53e373ccd1d5d
Miško Hevery <misko@hevery.com>
2018-07-11 03:01:24 +0000

    ffb9dc6 docs: fix incorrect forms selector references (#22631)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+85.sha-b7823e7
+ * @license Angular v6.1.0-beta.3+83.sha-ffb9dc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1624,7 +1624,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+85.sha-b7823e7');
+var VERSION = new Version('6.1.0-beta.3+83.sha-ffb9dc6');
 
 /**
  * @license
