92fa516ae2db26ab6aa693237c969d67c3c294e4
Miško Hevery <misko@hevery.com>
2018-07-11 21:39:44 +0000

    b9e095a release: cut the v6.0.8 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+86.sha-81a9db2
+ * @license Angular v6.1.0-beta.3+88.sha-b9e095a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __assign, __param, __extends, __spread, __values, __read } from 'tslib';
+import { __decorate, __param, __metadata, __assign, __extends, __spread, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -12184,7 +12184,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+86.sha-81a9db2');
+var VERSION = new Version('6.1.0-beta.3+88.sha-b9e095a');
 
 /**
  * @license
