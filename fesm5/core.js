c16f5d6d9521c95280604a1d145b4664629117c7
Miško Hevery <misko@hevery.com>
2018-07-11 03:04:48 +0000

    05e3e4d docs(forms): update API reference for form validators (#24734)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+83.sha-ffb9dc6
+ * @license Angular v6.1.0-beta.3+87.sha-05e3e4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1624,7 +1624,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+83.sha-ffb9dc6');
+var VERSION = new Version('6.1.0-beta.3+87.sha-05e3e4d');
 
 /**
  * @license
