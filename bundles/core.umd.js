caf82be91436cc1c20ad6e35e904aa8f6e6ad09e
Miško Hevery <misko@hevery.com>
2018-07-11 02:54:00 +0000

    a527c69 fix(common): do not round factional seconds (#24831)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+79.sha-8057664
+ * @license Angular v6.1.0-beta.3+80.sha-a527c69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+79.sha-8057664');
+    var VERSION = new Version('6.1.0-beta.3+80.sha-a527c69');
 
     /**
      * @license
