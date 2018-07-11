5fdba0b867794a27304ceb72020440326b702c50
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-11 22:22:06 +0000

    6b3f5dd feat(ivy): implement ngcc ESM5 bits

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+84.sha-da016d0
+ * @license Angular v6.1.0-beta.3+86.sha-6b3f5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+84.sha-da016d0');
+    var VERSION = new Version('6.1.0-beta.3+86.sha-6b3f5dd');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+84.sha-da016d0
+ * @license Angular v6.1.0-beta.3+86.sha-6b3f5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1938,7 +1938,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+84.sha-da016d0');
+const VERSION = new Version('6.1.0-beta.3+86.sha-6b3f5dd');
 
 /**
  * @fileoverview added by tsickle
