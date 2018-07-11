fc3feb74b445d3ee1f9b326b26ca67b3abf9201c
Miško Hevery <misko@hevery.com>
2018-07-11 02:58:31 +0000

    505b54b docs: fix typos referencing inline component styles (#22557)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+80.sha-a527c69
+ * @license Angular v6.1.0-beta.3+81.sha-505b54b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11746,7 +11746,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+80.sha-a527c69');
+const VERSION = new Version('6.1.0-beta.3+81.sha-505b54b');
 
 /**
  * @license
