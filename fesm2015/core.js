0d998b993dfcf8de8d5d62d49f2887d8650af6f8
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:52:19 +0000

    e295206 docs: update component styles doc regarding relative URL (#24471)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+6.sha-6bf8aef
+ * @license Angular v6.0.8+8.sha-e295206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1791,7 +1791,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const /** @type {?} */ VERSION = new Version('6.0.8+6.sha-6bf8aef');
+const /** @type {?} */ VERSION = new Version('6.0.8+8.sha-e295206');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+6.sha-6bf8aef
+ * @license Angular v6.0.8+8.sha-e295206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1604,7 +1604,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.8+6.sha-6bf8aef');
+var VERSION = new Version('6.0.8+8.sha-e295206');
 
 /**
  * @license
