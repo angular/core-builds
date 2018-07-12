0d7ad176d0a34463ccdc488caf891806cb17e6db
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:55:57 +0000

    0e7501b build: fix windows scripts (#23121)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+8.sha-e295206
+ * @license Angular v6.0.8+7.sha-0e7501b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1791,7 +1791,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const /** @type {?} */ VERSION = new Version('6.0.8+8.sha-e295206');
+const /** @type {?} */ VERSION = new Version('6.0.8+7.sha-0e7501b');
 
 /**
  * @fileoverview added by tsickle
