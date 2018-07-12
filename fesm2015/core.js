a59b47f0afb1ba656bc6ef641719edf4954c992e
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 19:11:08 +0000

    0d6b74d docs: fix typo in component architecture guide (#24832)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+91.sha-52d11f6
+ * @license Angular v6.1.0-beta.3+92.sha-0d6b74d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1938,7 +1938,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+91.sha-52d11f6');
+const VERSION = new Version('6.1.0-beta.3+92.sha-0d6b74d');
 
 /**
  * @fileoverview added by tsickle
