f4195330b87302b44380d4f8cca02050af174268
Miško Hevery <misko@hevery.com>
2018-07-12 16:51:25 +0000

    52d11f6 release: cut the v6.0.9 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+89.sha-0b4d85e
+ * @license Angular v6.1.0-beta.3+91.sha-52d11f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11747,7 +11747,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+89.sha-0b4d85e');
+const VERSION = new Version('6.1.0-beta.3+91.sha-52d11f6');
 
 /**
  * @license
