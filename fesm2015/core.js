170a150d8bf30c13ae5b01c69eddad9bf3cc2537
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:44:18 +0000

    43baa23 docs(forms): update form builder API reference (#24693)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+4.sha-48415ed
+ * @license Angular v6.0.8+5.sha-43baa23
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1791,7 +1791,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const /** @type {?} */ VERSION = new Version('6.0.8+4.sha-48415ed');
+const /** @type {?} */ VERSION = new Version('6.0.8+5.sha-43baa23');
 
 /**
  * @fileoverview added by tsickle
