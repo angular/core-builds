8ea99f2dc8cc7a25d10bc0e463083a2dab8c931e
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 19:13:33 +0000

    0d6b74d docs: fix typo in component architecture guide (#24832)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+91.sha-52d11f6
+ * @license Angular v6.1.0-beta.3+92.sha-0d6b74d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12232,7 +12232,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+91.sha-52d11f6');
+    var VERSION = new Version('6.1.0-beta.3+92.sha-0d6b74d');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+91.sha-52d11f6
+ * @license Angular v6.1.0-beta.3+92.sha-0d6b74d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11746,7 +11746,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+91.sha-52d11f6');
+const VERSION = new Version('6.1.0-beta.3+92.sha-0d6b74d');
 
 /**
  * @license
