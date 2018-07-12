a6d6775a3236d4478103602c52bfc63563a80a2f
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:43:25 +0000

    328971f feat(router): add urlUpdateStrategy allow updating the browser URL at the beginning of navigation (#24820)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+87.sha-05e3e4d
+ * @license Angular v6.1.0-beta.3+94.sha-328971f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12184,7 +12184,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+87.sha-05e3e4d');
+var VERSION = new Version('6.1.0-beta.3+94.sha-328971f');
 
 /**
  * @license
