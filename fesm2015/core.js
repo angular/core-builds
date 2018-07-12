67f838d8b574140e734c7a24c2b238e5d69aa448
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:49:03 +0000

    6bf8aef docs(aio): Add Angular Training to list of training companies (#23907) PR Close #23907

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.8+5.sha-43baa23
+ * @license Angular v6.0.8+6.sha-6bf8aef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1791,7 +1791,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const /** @type {?} */ VERSION = new Version('6.0.8+5.sha-43baa23');
+const /** @type {?} */ VERSION = new Version('6.0.8+6.sha-6bf8aef');
 
 /**
  * @fileoverview added by tsickle
