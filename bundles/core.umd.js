51c7478c31b93c5340769de7e18f1408d3916a36
Matias Niemelä <matias@yearofmoo.com>
2018-07-16 20:33:17 +0000

    d42a662 build: disable IE web worker tests (#24908)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+9.sha-1f163cd
+ * @license Angular v6.0.9+10.sha-d42a662
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1657,7 +1657,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.0.9+9.sha-1f163cd');
+    var VERSION = new Version('6.0.9+10.sha-d42a662');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+9.sha-1f163cd
+ * @license Angular v6.0.9+10.sha-d42a662
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1604,7 +1604,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.9+9.sha-1f163cd');
+var VERSION = new Version('6.0.9+10.sha-d42a662');
 
 /**
  * @license
