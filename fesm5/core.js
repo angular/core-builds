e77d2f8f0c93e651e56710cc1615160d65fce67d
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 13:54:24 +0000

    ee37256 feat(ivy): implement initial ngcc package transformer

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+129.sha-ae04929
+ * @license Angular v6.1.0-beta.3+130.sha-ee37256
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __decorate, __metadata, __param, __assign, __extends, __spread, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -12218,7 +12218,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-beta.3+129.sha-ae04929');
+var VERSION = new Version('6.1.0-beta.3+130.sha-ee37256');
 
 /**
  * @license
