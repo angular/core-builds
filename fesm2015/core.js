1dce85ca8010388b659fed577bec6b136ff40052
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:19:40 +0000

    06a3398 build: rename angular_devkit dependency to angular_cli (#24842)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+118.sha-ba3eb8b
+ * @license Angular v6.1.0-beta.3+119.sha-06a3398
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11929,7 +11929,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+118.sha-ba3eb8b');
+const VERSION = new Version('6.1.0-beta.3+119.sha-06a3398');
 
 /**
  * @license
