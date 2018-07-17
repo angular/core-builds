4538a2766a5126940948d8c0520ba1f5b823b8a7
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:20:11 +0000

    0b28732 docs: typos in directives docs (#24665)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+119.sha-06a3398
+ * @license Angular v6.1.0-beta.3+120.sha-0b28732
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -468,7 +468,7 @@ const ivyEnabled = false;
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** *
- * Type of the Component metadata.
+ * Type of the Directive metadata.
   @type {?} */
 const Directive = makeDecorator('Directive', (dir = {}) => dir, undefined, undefined, (type, meta) => (() => { })(type, meta));
 /** *
@@ -1938,7 +1938,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+119.sha-06a3398');
+const VERSION = new Version('6.1.0-beta.3+120.sha-0b28732');
 
 /**
  * @fileoverview added by tsickle
