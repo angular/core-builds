4ae00491d94af6ff341d65689b57b7b54f2e34c1
Matias Niemelä <matias@yearofmoo.com>
2018-07-18 18:36:21 +0000

    23dc9a9 docs: fix typo in user input guide (#22630)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+120.sha-0b28732
+ * @license Angular v6.1.0-beta.3+121.sha-23dc9a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+120.sha-0b28732');
+    var VERSION = new Version('6.1.0-beta.3+121.sha-23dc9a9');
 
     /**
      * @license
