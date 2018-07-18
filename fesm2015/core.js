6868f9fb5ade33f66bdc65bd89e5def109009b1a
Matias Niemelä <matias@yearofmoo.com>
2018-07-18 18:38:49 +0000

    23dc9a9 docs: fix typo in user input guide (#22630)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+120.sha-0b28732
+ * @license Angular v6.1.0-beta.3+121.sha-23dc9a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11929,7 +11929,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+120.sha-0b28732');
+const VERSION = new Version('6.1.0-beta.3+121.sha-23dc9a9');
 
 /**
  * @license
