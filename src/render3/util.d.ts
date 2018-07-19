dedca802d1aae72a69ad4fad3dd819ba3b50d8b3
Matias Niemelä <matias@yearofmoo.com>
2018-07-19 22:06:23 +0000

    d76531d fix(animations): @internal must use JSDoc tags. (#24928)

--- src/render3/util.d.ts
+++ src/render3/util.d.ts
@@ -1,10 +1,5 @@
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
+import { LElementNode } from './interfaces/node';
+import { LViewData } from './interfaces/view';
 /**
 * Must use this method for CD (instead of === ) since NaN !== NaN
 */
@@ -21,3 +16,13 @@ export declare function notImplemented(): Error;
  * Flattens an array in non-recursive way. Input arrays are not modified.
  */
 export declare function flatten(list: any[]): any[];
+/** Retrieves a value from any `LViewData`. */
+export declare function loadInternal<T>(index: number, arr: LViewData): T;
+export declare function assertDataInRangeInternal(index: number, arr: any[]): void;
+/** Retrieves an element value from the provided `viewData`.
+  *
+  * Elements that are read may be wrapped in a style context,
+  * therefore reading the value may involve unwrapping that.
+  */
+export declare function loadElementInternal(index: number, arr: LViewData): LElementNode;
+export declare function readElementValue(value: LElementNode | any[]): LElementNode;
