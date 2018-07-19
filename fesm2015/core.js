bde592bc9f73e36ba9405c771082ed379c1e06ca
Matias Niemelä <matias@yearofmoo.com>
2018-07-19 23:35:23 +0000

    f9669e5 release: cut the v6.1.0-rc.3 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+122.sha-d76531d
+ * @license Angular v6.1.0-rc.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11929,7 +11929,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-beta.3+122.sha-d76531d');
+const VERSION = new Version('6.1.0-rc.3');
 
 /**
  * @license
