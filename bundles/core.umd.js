0d8582bc580b3e26052e893613f24d2e40d1d062
Matias Niemelä <matias@yearofmoo.com>
2018-07-19 23:32:59 +0000

    f9669e5 release: cut the v6.1.0-rc.3 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+122.sha-d76531d
+ * @license Angular v6.1.0-rc.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-beta.3+122.sha-d76531d');
+    var VERSION = new Version('6.1.0-rc.3');
 
     /**
      * @license
