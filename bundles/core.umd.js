ec46d92d98d1e24a6f33da4c69136ab250292be3
Victor Berchet <victor@suumit.com>
2018-07-20 19:02:55 +0000

    1c533c9 build(docs-infra): add support for examples of type `elements` (#24840)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+3.sha-270176b
+ * @license Angular v6.1.0-rc.3+8.sha-1c533c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-rc.3+3.sha-270176b');
+    var VERSION = new Version('6.1.0-rc.3+8.sha-1c533c9');
 
     /**
      * @license
