57e75fd852afd2e4e73c053871d4fe23d90e8f8d
Victor Berchet <victor@suumit.com>
2018-07-20 19:57:55 +0000

    4834fda fix(service-worker): don't include sourceMappingURL in ngsw-worker (#24877)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+28.sha-f2aeb70
+ * @license Angular v6.0.9+29.sha-4834fda
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1791,7 +1791,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const /** @type {?} */ VERSION = new Version('6.0.9+28.sha-f2aeb70');
+const /** @type {?} */ VERSION = new Version('6.0.9+29.sha-4834fda');
 
 /**
  * @fileoverview added by tsickle
