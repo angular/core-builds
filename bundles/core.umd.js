5b8c3e2acc4def5b3fc04ff6a4d0fa148c7839b9
Victor Berchet <victor@suumit.com>
2018-07-20 19:39:40 +0000

    b8f740b docs: remove closing parenthesis from provides guide (#24935)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+15.sha-23766b8
+ * @license Angular v6.1.0-rc.3+16.sha-b8f740b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-rc.3+15.sha-23766b8');
+    var VERSION = new Version('6.1.0-rc.3+16.sha-b8f740b');
 
     /**
      * @license
