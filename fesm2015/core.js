25a220ccdfcb242009e841fa7d4850e6f10192fd
Victor Berchet <victor@suumit.com>
2018-07-20 19:03:20 +0000

    270176b docs: more info on currency digits (#24661)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+1.sha-2aab1c9
+ * @license Angular v6.1.0-rc.3+3.sha-270176b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11930,7 +11930,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-rc.3+1.sha-2aab1c9');
+const VERSION = new Version('6.1.0-rc.3+3.sha-270176b');
 
 /**
  * @license
