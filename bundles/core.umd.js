190b9107860c85f87f85bbf83cd101b5cc33c65b
Victor Berchet <victor@suumit.com>
2018-07-23 17:19:03 +0000

    4263245 docs: update rxjs link to version 6 (#24269)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+44.sha-6b859da
+ * @license Angular v6.1.0-rc.3+46.sha-4263245
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12526,7 +12526,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-rc.3+44.sha-6b859da');
+    var VERSION = new Version('6.1.0-rc.3+46.sha-4263245');
 
     /**
      * @license
