db50ed28c66462ec4d1dfcc4bf81cd4019108f8f
Victor Berchet <victor@suumit.com>
2018-07-23 17:21:46 +0000

    7729bb2 docs: fix instructions for switching directories (#24439)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+46.sha-4263245
+ * @license Angular v6.1.0-rc.3+47.sha-7729bb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12173,7 +12173,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-rc.3+46.sha-4263245');
+const VERSION = new Version('6.1.0-rc.3+47.sha-7729bb2');
 
 /**
  * @license
