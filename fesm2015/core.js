1738548ef078ddcf065c2d2e1cf1421e5a885ccb
Victor Berchet <victor@suumit.com>
2018-07-23 17:33:55 +0000

    7c89af3 docs: square odds example in rxjs guide (#24947)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+48.sha-168c2a6
+ * @license Angular v6.1.0-rc.3+50.sha-7c89af3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+48.sha-168c2a6');
+const VERSION = new Version('6.1.0-rc.3+50.sha-7c89af3');
 
 /**
  * @fileoverview added by tsickle
