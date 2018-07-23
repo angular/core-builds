8ba960285dcf063144235421c16ef000e2f88d31
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-23 07:05:03 +0000

    8a7b0e9 fixup! feat(ivy): implement esm2015 and esm5 reflection hosts

--- src/render3/styling.d.ts
+++ src/render3/styling.d.ts
@@ -10,66 +10,81 @@ import { LElementNode } from './interfaces/node';
 import { Renderer3 } from './interfaces/renderer';
 /**
  * The styling context acts as a styling manifest (shaped as an array) for determining which
- * styling properties have been assigned via the provided `updateStyleMap` and `updateStyleProp`
- * functions. There are also two initialization functions `allocStylingContext` and
- * `createStylingContextTemplate` which are used to initialize and/or clone the context.
+ * styling properties have been assigned via the provided `updateStylingMap`, `updateStyleProp`
+ * and `updateClassProp` functions. There are also two initialization functions
+ * `allocStylingContext` and `createStylingContextTemplate` which are used to initialize
+ * and/or clone the context.
  *
  * The context is an array where the first two cells are used for static data (initial styling)
  * and dirty flags / index offsets). The remaining set of cells is used for multi (map) and single
  * (prop) style values.
  *
  * each value from here onwards is mapped as so:
- * [i] = mutation/type flag for the style value
+ * [i] = mutation/type flag for the style/class value
  * [i + 1] = prop string (or null incase it has been removed)
  * [i + 2] = value string (or null incase it has been removed)
  *
  * There are three types of styling types stored in this context:
  *   initial: any styles that are passed in once the context is created
  *            (these are stored in the first cell of the array and the first
- *             value of this array is always `null` even if no initial styles exist.
+ *             value of this array is always `null` even if no initial styling exists.
  *             the `null` value is there so that any new styles have a parent to point
  *             to. This way we can always assume that there is a parent.)
  *
- *   single: any styles that are updated using `updateStyleProp` (fixed set)
+ *   single: any styles that are updated using `updateStyleProp` or `updateClassProp` (fixed set)
  *
- *   multi: any styles that are updated using `updateStyleMap` (dynamic set)
+ *   multi: any styles that are updated using `updateStylingMap` (dynamic set)
  *
- * Note that context is only used to collect style information. Only when `renderStyles`
+ * Note that context is only used to collect style information. Only when `renderStyling`
  * is called is when the styling payload will be rendered (or built as a key/value map).
  *
- * When the context is created, depending on what initial styles are passed in, the context itself
- * will be pre-filled with slots based on the initial style properties. Say for example we have a
- * series of initial styles that look like so:
+ * When the context is created, depending on what initial styling values are passed in, the
+ * context itself will be pre-filled with slots based on the initial style properties. Say
+ * for example we have a series of initial styles that look like so:
  *
  *   style="width:100px; height:200px;"
+ *   class="foo"
  *
  * Then the initial state of the context (once initialized) will look like so:
  *
  * ```
  * context = [
- *   [null, '100px', '200px'],  // property names are not needed since they have already been
+ *   [null, '100px', '200px', true],  // property names are not needed since they have already been
  * written to DOM.
+ *
+ *   1, // this instructs how many `style` values there are so that class index values can be
+ * offsetted
  *
  *   configMasterVal,
  *
- *   // 2
+ *   // 3
  *   'width',
- *   pointers(1, 8);  // Point to static `width`: `100px` and multi `width`.
+ *   pointers(1, 12);  // Point to static `width`: `100px` and multi `width`.
  *   null,
  *
- *   // 5
+ *   // 6
  *   'height',
- *   pointers(2, 11); // Point to static `height`: `200px` and multi `height`.
+ *   pointers(2, 15); // Point to static `height`: `200px` and multi `height`.
  *   null,
  *
- *   // 8
+ *   // 9
+ *   'foo',
+ *   pointers(1, 18);  // Point to static `foo`: `true` and multi `foo`.
+ *   null,
+ *
+ *   // 12
  *   'width',
- *   pointers(1, 2);  // Point to static `width`: `100px` and single `width`.
+ *   pointers(1, 3);  // Point to static `width`: `100px` and single `width`.
  *   null,
  *
- *   // 11
+ *   // 15
  *   'height',
- *   pointers(2, 5);  // Point to static `height`: `200px` and single `height`.
+ *   pointers(2, 6);  // Point to static `height`: `200px` and single `height`.
+ *   null,
+ *
+ *   // 18
+ *   'foo',
+ *   pointers(3, 9);  // Point to static `foo`: `true` and single `foo`.
  *   null,
  * ]
  *
@@ -80,29 +95,45 @@ import { Renderer3 } from './interfaces/renderer';
  * }
  * ```
  *
- * The values are duplicated so that space is set aside for both multi ([style])
- * and single ([style.prop]) values. The respective config values (configValA, configValB, etc...)
- * are a combination of the StylingFlags with two index values: the `initialIndex` (which points to
- * the index location of the style value in the initial styles array in slot 0) and the
- * `dynamicIndex` (which points to the matching single/multi index position in the context array
- * for the same prop).
+ * The values are duplicated so that space is set aside for both multi ([style] and [class])
+ * and single ([style.prop] or [class.named]) values. The respective config values
+ * (configValA, configValB, etc...) are a combination of the StylingFlags with two index
+ * values: the `initialIndex` (which points to the index location of the style value in
+ * the initial styles array in slot 0) and the `dynamicIndex` (which points to the
+ * matching single/multi index position in the context array for the same prop).
  *
- * This means that every time `updateStyleProp` is called it must be called using an index value
- * (not a property string) which references the index value of the initial style when the context
- * was created. This also means that `updateStyleProp` cannot be called with a new property
- * (only `updateStyleMap` can include new CSS properties that will be added to the context).
+ * This means that every time `updateStyleProp` or `updateClassProp` are called then they
+ * must be called using an index value (not a property string) which references the index
+ * value of the initial style prop/class when the context was created. This also means that
+ * `updateStyleProp` or `updateClassProp` cannot be called with a new property (only
+ * `updateStylingMap` can include new CSS properties that will be added to the context).
  */
-export interface StylingContext extends Array<InitialStyles | number | string | null> {
+export interface StylingContext extends Array<InitialStyles | number | string | boolean | LElementNode | null> {
+    /**
+     * Location of element that is used as a target for this context.
+     */
+    [0]: LElementNode | null;
     /**
      * Location of initial data shared by all instances of this style.
      */
-    [0]: InitialStyles;
+    [1]: InitialStyles;
     /**
      * A numeric value representing the configuration status (whether the context is dirty or not)
      * mixed together (using bit shifting) with a index value which tells the starting index value
      * of where the multi style entries begin.
      */
-    [1]: number;
+    [2]: number;
+    /**
+     * A numeric value representing the class index offset value. Whenever a single class is
+     * applied (using `elementClassProp`) it should have an styling index value that doesn't
+     * need to take into account any style values that exist in the context.
+     */
+    [3]: number;
+    /**
+     * The last CLASS STRING VALUE that was interpreted by elementStylingMap. This is cached
+     * So that the algorithm can exit early incase the string has not changed.
+     */
+    [4]: string | null;
 }
 /**
  * The initial styles is populated whether or not there are any initial styles passed into
@@ -112,7 +143,7 @@ export interface StylingContext extends Array<InitialStyles | number | string | 
  * All other entries in this array are of `string` value and correspond to the values that
  * were extracted from the `style=""` attribute in the HTML code for the provided template.
  */
-export interface InitialStyles extends Array<string | null> {
+export interface InitialStyles extends Array<string | null | boolean> {
     [0]: null;
 }
 /**
@@ -122,13 +153,18 @@ export interface InitialStyles extends Array<string | null> {
 export declare const enum StylingFlags {
     None = 0,
     Dirty = 1,
-    BitCountSize = 1
+    Class = 2,
+    BitCountSize = 2,
+    BitMask = 3
 }
 /** Used as numeric pointer values to determine what cells to update in the `StylingContext` */
 export declare const enum StylingIndex {
-    InitialStylesPosition = 0,
-    MasterFlagPosition = 1,
-    SingleStylesStartPosition = 2,
+    ElementPosition = 0,
+    InitialStylesPosition = 1,
+    MasterFlagPosition = 2,
+    ClassOffsetPosition = 3,
+    CachedCssClassString = 4,
+    SingleStylesStartPosition = 5,
     FlagsOffset = 0,
     PropertyOffset = 1,
     ValueOffset = 2,
@@ -142,7 +178,7 @@ export declare const enum StylingIndex {
  * A pre-computed template is designed to be computed once for a given element
  * (instructions.ts has logic for caching this).
  */
-export declare function allocStylingContext(templateStyleContext: StylingContext): StylingContext;
+export declare function allocStylingContext(lElement: LElementNode | null, templateStyleContext: StylingContext): StylingContext;
 /**
  * Creates a styling context template where styling information is stored.
  * Any styles that are later referenced using `updateStyleProp` must be
@@ -157,44 +193,66 @@ export declare function allocStylingContext(templateStyleContext: StylingContext
  *    -> ['width', 'height', SPECIAL_ENUM_VAL, 'width', '100px']
  *       This implies that `width` and `height` will be later styled and that the `width`
  *       property has an initial value of `100px`.
- */
-export declare function createStylingContextTemplate(initialStyleDeclarations?: (string | InitialStylingFlags)[] | null): StylingContext;
-/**
- * Sets and resolves all `multi` styles on an `StylingContext` so that they can be
- * applied to the element once `renderStyles` is called.
  *
- * All missing styles (any values that are not provided in the new `styles` param)
- * will resolve to `null` within their respective positions in the context.
+ * @param initialClassDeclarations a list of class declarations and initial class values
+ *    that are used later within the styling context.
+ *
+ *    -> ['foo', 'bar', SPECIAL_ENUM_VAL, 'foo', true]
+ *       This implies that `foo` and `bar` will be later styled and that the `foo`
+ *       class will be applied to the element as an initial class since it's true
+ */
+export declare function createStylingContextTemplate(initialStyleDeclarations?: (string | boolean | InitialStylingFlags)[] | null, initialClassDeclarations?: (string | boolean | InitialStylingFlags)[] | null): StylingContext;
+/**
+ * Sets and resolves all `multi` styling on an `StylingContext` so that they can be
+ * applied to the element once `renderStyling` is called.
+ *
+ * All missing styles/class (any values that are not provided in the new `styles`
+ * or `classes` params) will resolve to `null` within their respective positions
+ * in the context.
  *
  * @param context The styling context that will be updated with the
  *    newly provided style values.
  * @param styles The key/value map of CSS styles that will be used for the update.
+ * @param classes The key/value map of CSS class names that will be used for the update.
  */
-export declare function updateStyleMap(context: StylingContext, styles: {
+export declare function updateStylingMap(context: StylingContext, styles: {
     [key: string]: any;
-} | null): void;
+} | null, classes?: {
+    [key: string]: any;
+} | string | null): void;
 /**
- * Sets and resolves a single CSS style on a property on an `StylingContext` so that they
- * can be applied to the element once `renderElementStyles` is called.
+ * Sets and resolves a single styling property/value on the provided `StylingContext` so
+ * that they can be applied to the element once `renderStyling` is called.
  *
- * Note that prop-level styles are considered higher priority than styles that are applied
- * using `updateStyleMap`, therefore, when styles are rendered then any styles that
- * have been applied using this function will be considered first (then multi values second
- * and then initial values as a backup).
+ * Note that prop-level styling values are considered higher priority than any styling that
+ * has been applied using `updateStylingMap`, therefore, when styling values are rendered
+ * then any styles/classes that have been applied using this function will be considered first
+ * (then multi values second and then initial values as a backup).
  *
  * @param context The styling context that will be updated with the
  *    newly provided style value.
  * @param index The index of the property which is being updated.
  * @param value The CSS style value that will be assigned
  */
-export declare function updateStyleProp(context: StylingContext, index: number, value: string | null): void;
+export declare function updateStyleProp(context: StylingContext, index: number, value: string | boolean | null): void;
 /**
- * Renders all queued styles using a renderer onto the given element.
+ * This method will toggle the referenced CSS class (by the provided index)
+ * within the given context.
+ *
+ * @param context The styling context that will be updated with the
+ *    newly provided class value.
+ * @param index The index of the CSS class which is being updated.
+ * @param addOrRemove Whether or not to add or remove the CSS class
+ */
+export declare function updateClassProp(context: StylingContext, index: number, addOrRemove: boolean): void;
+/**
+ * Renders all queued styling using a renderer onto the given element.
  *
  * This function works by rendering any styles (that have been applied
- * using `updateStyleMap` and `updateStyleProp`) onto the
- * provided element using the provided renderer. Just before the styles
- * are rendered a final key/value style map will be assembled.
+ * using `updateStylingMap`) and any classes (that have been applied using
+ * `updateStyleProp`) onto the provided element using the provided renderer.
+ * Just before the styles/classes are rendered a final key/value style map
+ * will be assembled (if `styleStore` or `classStore` are provided).
  *
  * @param lElement the element that the styles will be rendered on
  * @param context The styling context that will be used to determine
@@ -202,10 +260,13 @@ export declare function updateStyleProp(context: StylingContext, index: number, 
  * @param renderer the renderer that will be used to apply the styling
  * @param styleStore if provided, the updated style values will be applied
  *    to this key/value map instead of being renderered via the renderer.
- * @returns an object literal. `{ color: 'red', height: 'auto'}`.
+ * @param classStore if provided, the updated class values will be applied
+ *    to this key/value map instead of being renderered via the renderer.
  */
-export declare function renderStyles(lElement: LElementNode, context: StylingContext, renderer: Renderer3, styleStore?: {
+export declare function renderStyling(context: StylingContext, renderer: Renderer3, styleStore?: {
     [key: string]: any;
+}, classStore?: {
+    [key: string]: boolean;
 }): void;
 export declare function isContextDirty(context: StylingContext): boolean;
 export declare function setContextDirty(context: StylingContext, isDirtyYes: boolean): void;
