a1378275c9c8aa948fdce7f819b3711b23b8a2f5
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-23 07:19:18 +0000

    0650f32 fixup! build: add dependencies to be used by ngcc

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+70.sha-8a7b0e9
+ * @license Angular v6.1.0-rc.3+71.sha-0650f32
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __decorate, __metadata, __assign, __param, __extends, __spread, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -12465,7 +12465,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-rc.3+70.sha-8a7b0e9');
+var VERSION = new Version('6.1.0-rc.3+71.sha-0650f32');
 
 /**
  * @license
