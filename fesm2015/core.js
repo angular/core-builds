ff746bbfaeb629743519b308696afe8c7e9f17cd
Victor Berchet <victor@suumit.com>
2018-07-23 17:28:23 +0000

    168c2a6 docs: add Truly-UI to resources (#24615) PR Close #24615

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+47.sha-7729bb2
+ * @license Angular v6.1.0-rc.3+48.sha-168c2a6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12173,7 +12173,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-rc.3+47.sha-7729bb2');
+const VERSION = new Version('6.1.0-rc.3+48.sha-168c2a6');
 
 /**
  * @license
