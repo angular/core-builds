8be2142e47f57165167828ae2af5e1346a9e1e98
Victor Berchet <victor@suumit.com>
2018-07-23 20:42:26 +0000

    6eca367 docs: tests for number/percent/currency pipe (#25028)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+39.sha-060867d
+ * @license Angular v6.0.9+40.sha-6eca367
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1674,7 +1674,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.0.9+39.sha-060867d');
+    var VERSION = new Version('6.0.9+40.sha-6eca367');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+39.sha-060867d
+ * @license Angular v6.0.9+40.sha-6eca367
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1622,7 +1622,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.9+39.sha-060867d');
+var VERSION = new Version('6.0.9+40.sha-6eca367');
 
 /**
  * @license
