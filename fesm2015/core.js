8efa4b6dd2b83126a164e3ebc978dc32a4eedd22
Victor Berchet <victor@suumit.com>
2018-07-23 16:49:43 +0000

    d3656bc docs: refactor style guide example 01-01 (#22738)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+29.sha-4834fda
+ * @license Angular v6.0.9+30.sha-d3656bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1791,7 +1791,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const /** @type {?} */ VERSION = new Version('6.0.9+29.sha-4834fda');
+const /** @type {?} */ VERSION = new Version('6.0.9+30.sha-d3656bc');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+29.sha-4834fda
+ * @license Angular v6.0.9+30.sha-d3656bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1604,7 +1604,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.0.9+29.sha-4834fda');
+var VERSION = new Version('6.0.9+30.sha-d3656bc');
 
 /**
  * @license
