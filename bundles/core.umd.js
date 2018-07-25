ab94266457ad55aa576b68c703b76a43dc04ac42
Victor Berchet <victor@suumit.com>
2018-07-25 22:30:26 +0000

    48d7205 release: cut the v6.1.0 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
+ * @license Angular v6.1.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15116,7 +15116,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-rc.3+75.sha-e1c6fd5');
+    var VERSION = new Version('6.1.0');
 
     /**
      * @license
