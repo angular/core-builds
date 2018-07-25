beaa0c3f00e329388774aa3624b4284fbc4a1f0d
Victor Berchet <victor@suumit.com>
2018-07-25 04:43:22 +0000

    1e79014 docs: replace angular/http with HttpClient (#25066)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+65.sha-65e18dc
+ * @license Angular v6.1.0-rc.3+67.sha-1e79014
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+65.sha-65e18dc');
+const VERSION = new Version('6.1.0-rc.3+67.sha-1e79014');
 
 /**
  * @fileoverview added by tsickle
