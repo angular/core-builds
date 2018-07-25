a99a647810545152952108c19f65a076a1ebf933
Victor Berchet <victor@suumit.com>
2018-07-25 04:48:05 +0000

    777bd41 docs: replace angular/http with HttpClient (#25068)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+67.sha-1e79014
+ * @license Angular v6.1.0-rc.3+68.sha-777bd41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15049,7 +15049,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-rc.3+67.sha-1e79014');
+    var VERSION = new Version('6.1.0-rc.3+68.sha-777bd41');
 
     /**
      * @license
