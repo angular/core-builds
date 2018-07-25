fca7ea41bb2ca1b7a9d649584dee019ba7e641de
Victor Berchet <victor@suumit.com>
2018-07-25 19:22:44 +0000

    968f153 fix(router): Fix _lastPathIndex in deeply nested empty paths (#22394)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+73.sha-1e28495
+ * @license Angular v6.1.0-rc.3+74.sha-968f153
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14641,7 +14641,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-rc.3+73.sha-1e28495');
+const VERSION = new Version('6.1.0-rc.3+74.sha-968f153');
 
 /**
  * @license
