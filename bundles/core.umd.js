79b1e2d5e7b61704d26964f86be81e5bd6d6a3dd
Victor Berchet <victor@suumit.com>
2018-07-25 06:06:16 +0000

    d4ac969 Revert "docs: refactor style guide example 03-06 (#24996)"

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+69.sha-c205516
+ * @license Angular v6.1.0-rc.3+70.sha-d4ac969
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15049,7 +15049,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-rc.3+69.sha-c205516');
+    var VERSION = new Version('6.1.0-rc.3+70.sha-d4ac969');
 
     /**
      * @license
