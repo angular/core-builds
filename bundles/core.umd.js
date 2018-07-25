16bbc409abcb74b405b99c95c8756670e4b62977
Victor Berchet <victor@suumit.com>
2018-07-25 18:21:53 +0000

    0bcf20c docs(animations): typo fix in the comments (#22652)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+71.sha-cf81823
+ * @license Angular v6.1.0-rc.3+72.sha-0bcf20c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15049,7 +15049,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-rc.3+71.sha-cf81823');
+    var VERSION = new Version('6.1.0-rc.3+72.sha-0bcf20c');
 
     /**
      * @license
