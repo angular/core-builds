17bd656965c3796ffffb908f611e97976bc9c98f
Victor Berchet <victor@suumit.com>
2018-07-25 04:58:56 +0000

    c205516 docs: refactor ngmodules example (#25072)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+68.sha-777bd41
+ * @license Angular v6.1.0-rc.3+69.sha-c205516
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15013,7 +15013,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-rc.3+68.sha-777bd41');
+var VERSION = new Version('6.1.0-rc.3+69.sha-c205516');
 
 /**
  * @license
