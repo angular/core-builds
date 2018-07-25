703646816abc977769347efa8fc464423514f569
Victor Berchet <victor@suumit.com>
2018-07-25 15:59:56 +0000

    cf81823 docs: refactor style guide example 03-06 (#24996)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+70.sha-d4ac969
+ * @license Angular v6.1.0-rc.3+71.sha-cf81823
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15049,7 +15049,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0-rc.3+70.sha-d4ac969');
+    var VERSION = new Version('6.1.0-rc.3+71.sha-cf81823');
 
     /**
      * @license
