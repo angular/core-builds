e0e8d9cc1e63347753a013bc342ef75c3361c350
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-07-26 17:36:54 +0000

    70174d1 refactor(ivy): do not deep import from ngtsc into ngcc

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+85.sha-70668f7
+ * @license Angular v6.1.0+15.sha-70174d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -151,39 +151,6 @@
         // TODO @JiaLiPassion, remove this after all library updated to
         // newest version of zone.js(0.8.25)
         return asyncFallback(fn);
-    }
-
-    /**
-     * @license
-     * Copyright Google Inc. All Rights Reserved.
-     *
-     * Use of this source code is governed by an MIT-style license that can be
-     * found in the LICENSE file at https://angular.io/license
-     */
-    /**
-     * Converts an `async` function, with `await`, into a function which is compatible with Jasmine test
-     * framework.
-     *
-     * For asynchronous function blocks, Jasmine expects `it` (and friends) to take a function which
-     * takes a `done` callback. (Jasmine does not understand functions which return `Promise`.) The
-     * `jasmineAwait()` wrapper converts the test function returning `Promise` into a function which
-     * Jasmine understands.
-     *
-     *
-     * Example:
-     * ```
-     * it('...', jasmineAwait(async() => {
-     *   doSomething();
-     *   await asyncFn();
-     *   doSomethingAfter();
-     * }));
-     * ```
-     *
-     */
-    function jasmineAwait(fn) {
-        return function (done) {
-            fn().then(done, done.fail);
-        };
     }
 
     /**
@@ -1542,7 +1509,6 @@
      */
 
     exports.async = async;
-    exports.jasmineAwait = jasmineAwait;
     exports.ComponentFixture = ComponentFixture;
     exports.resetFakeAsyncZone = resetFakeAsyncZone;
     exports.fakeAsync = fakeAsync;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+85.sha-70668f7
+ * @license Angular v6.1.0+15.sha-70174d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14693,7 +14693,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0-rc.3+85.sha-70668f7');
+const VERSION = new Version('6.1.0+15.sha-70174d1');
 
 /**
  * @license
