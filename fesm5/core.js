f6232e6609631b29cbaab1485b7b91a744692d64
Igor Minar <igor@angular.io>
2018-07-27 01:07:38 +0000

    3169edd fix(ivy): don't crash in listLazyRoutes() (#25080)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
+ * @license Angular v6.1.0+8.sha-3169edd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14279,9 +14279,13 @@ function compileNgModule$1(type, ngModule) {
                 var meta = {
                     type: wrap(type),
                     bootstrap: flatten$1(ngModule.bootstrap || EMPTY_ARRAY$3).map(wrap),
-                    declarations: declarations.map(wrap),
-                    imports: flatten$1(ngModule.imports || EMPTY_ARRAY$3).map(expandModuleWithProviders).map(wrap),
-                    exports: flatten$1(ngModule.exports || EMPTY_ARRAY$3).map(expandModuleWithProviders).map(wrap),
+                    declarations: declarations.map(wrapReference),
+                    imports: flatten$1(ngModule.imports || EMPTY_ARRAY$3)
+                        .map(expandModuleWithProviders)
+                        .map(wrapReference),
+                    exports: flatten$1(ngModule.exports || EMPTY_ARRAY$3)
+                        .map(expandModuleWithProviders)
+                        .map(wrapReference),
                     emitInline: true,
                 };
                 var res = compileNgModule(meta);
@@ -14433,6 +14437,10 @@ function expandModuleWithProviders(value) {
 }
 function wrap(value) {
     return new WrappedNodeExpr(value);
+}
+function wrapReference(value) {
+    var wrapped = wrap(value);
+    return { value: wrapped, type: wrapped };
 }
 function isModuleWithProviders(value) {
     return value.ngModule !== undefined;
@@ -15013,7 +15021,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0-rc.3+75.sha-e1c6fd5');
+var VERSION = new Version('6.1.0+8.sha-3169edd');
 
 /**
  * @license
