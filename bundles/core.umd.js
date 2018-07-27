a609e2cf5a408e88e7da3dc35f44c85aa9cd294e
Igor Minar <igor@angular.io>
2018-07-27 17:54:35 +0000

    e8d4211 feat(docs-infra): allow notification bar to show arbitrary content (#25020)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+15.sha-a3cf61b
+ * @license Angular v6.1.0+17.sha-e8d4211
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1700,7 +1700,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0+15.sha-a3cf61b');
+    var VERSION = new Version('6.1.0+17.sha-e8d4211');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+15.sha-a3cf61b
+ * @license Angular v6.1.0+17.sha-e8d4211
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+15.sha-a3cf61b');
+const VERSION = new Version('6.1.0+17.sha-e8d4211');
 
 /**
  * @fileoverview added by tsickle
