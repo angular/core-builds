63182f06d69c2426561d19aad6a986ed620fa65a
Igor Minar <igor@angular.io>
2018-07-27 00:49:13 +0000

    601064e build(bazel): add comment about angular bazel rules API re-export from /index.bzl (#24663)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0
+ * @license Angular v6.1.0+13.sha-601064e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0');
+const VERSION = new Version('6.1.0+13.sha-601064e');
 
 /**
  * @fileoverview added by tsickle
