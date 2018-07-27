f7d6143388bd1338ab5801bd1b170ae5366cf457
Igor Minar <igor@angular.io>
2018-07-27 17:58:18 +0000

    36a7705 feat(docs-infra): allow notification bar to show arbitrary content (#25020)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+7.sha-e86f3d9
+ * @license Angular v6.1.0+9.sha-36a7705
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+7.sha-e86f3d9');
+const VERSION = new Version('6.1.0+9.sha-36a7705');
 
 /**
  * @fileoverview added by tsickle
