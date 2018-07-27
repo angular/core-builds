126890637838b7f4cd1fc919709188ea2142036c
Igor Minar <igor@angular.io>
2018-07-27 00:44:00 +0000

    f2bf828 build(bazel): add comment about angular bazel rules API re-export from /index.bzl (#24663)

--- testing/src/testing_internal.d.ts
+++ testing/src/testing_internal.d.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/// <reference types="jasmine" />
 export { AsyncTestCompleter } from './async_test_completer';
 export { inject } from './test_bed';
 export * from './logger';
