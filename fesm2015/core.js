7454a3eb61bc5eeed506afd6879aa8b994b75ec1
Igor Minar <igor@angular.io>
2018-07-27 19:29:56 +0000

    3426784 test: fix typings for `DoneFn` (#25163)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+17.sha-e8d4211
+ * @license Angular v6.1.0+18.sha-3426784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14701,7 +14701,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+17.sha-e8d4211');
+const VERSION = new Version('6.1.0+18.sha-3426784');
 
 /**
  * @license
