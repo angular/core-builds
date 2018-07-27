60a93e3669de9c1de497ca91bfa21b80200eb20b
Igor Minar <igor@angular.io>
2018-07-27 17:54:14 +0000

    e86f3d9 docs: refactor feature modules example (#25069)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+6.sha-738f296
+ * @license Angular v6.1.0+7.sha-e86f3d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+6.sha-738f296');
+const VERSION = new Version('6.1.0+7.sha-e86f3d9');
 
 /**
  * @fileoverview added by tsickle
