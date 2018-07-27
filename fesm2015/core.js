4c08e53e213f688c8d6ebb824d1e35ca2527d19a
Igor Minar <igor@angular.io>
2018-07-27 17:46:17 +0000

    a1b185b docs: Change unnecessary step in ToH-Tutorial (#25059)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+13.sha-601064e
+ * @license Angular v6.1.0+14.sha-a1b185b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14649,7 +14649,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+13.sha-601064e');
+const VERSION = new Version('6.1.0+14.sha-a1b185b');
 
 /**
  * @license
