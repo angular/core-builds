89dc5fce009ec1045afe66bde5bbe1de9af10149
Igor Minar <igor@angular.io>
2018-07-27 00:46:36 +0000

    f2bf828 build(bazel): add comment about angular bazel rules API re-export from /index.bzl (#24663)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0
+ * @license Angular v6.1.0+5.sha-f2bf828
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14641,7 +14641,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0');
+const VERSION = new Version('6.1.0+5.sha-f2bf828');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0
+ * @license Angular v6.1.0+5.sha-f2bf828
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15013,7 +15013,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0');
+var VERSION = new Version('6.1.0+5.sha-f2bf828');
 
 /**
  * @license
