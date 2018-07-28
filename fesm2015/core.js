fe0b5e5dda585712dee97bdcb1274f21b343acc8
Igor Minar <igor@angular.io>
2018-07-28 01:06:45 +0000

    f82b6b2 build(bazel): fix typos in comments (#25172)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+40.sha-a87d44c
+ * @license Angular v6.1.0+41.sha-f82b6b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14916,7 +14916,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+40.sha-a87d44c');
+const VERSION = new Version('6.1.0+41.sha-f82b6b2');
 
 /**
  * @license
