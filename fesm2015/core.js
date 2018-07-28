8d679fb39245daceb4630ed23eb5d7b64e687f88
Igor Minar <igor@angular.io>
2018-07-28 00:55:05 +0000

    a87d44c refactor(ivy): do not deep import from ngtsc into ngcc (#24897)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+25.sha-8e65891
+ * @license Angular v6.1.0+40.sha-a87d44c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+25.sha-8e65891');
+const VERSION = new Version('6.1.0+40.sha-a87d44c');
 
 /**
  * @fileoverview added by tsickle
