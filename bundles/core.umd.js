0c8c64f22074676002104c6a2e5f14c3a7ecbc30
Igor Minar <igor@angular.io>
2018-07-30 22:21:42 +0000

    9a6d26e docs: refactor pipe example to use the HttpClient (#22741)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+43.sha-6a797d5
+ * @license Angular v6.1.0+44.sha-9a6d26e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15426,7 +15426,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0+43.sha-6a797d5');
+    var VERSION = new Version('6.1.0+44.sha-9a6d26e');
 
     /**
      * @license
