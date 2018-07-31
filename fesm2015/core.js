0b513ae927aefa0ed41bf4bfbef4f3b8e7bf5701
Igor Minar <igor@angular.io>
2018-07-31 00:41:43 +0000

    e99d860 feat(compiler): add "original" placeholder value on extracted XMB (#25079)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+51.sha-24789e9
+ * @license Angular v6.1.0+52.sha-e99d860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+51.sha-24789e9');
+const VERSION = new Version('6.1.0+52.sha-e99d860');
 
 /**
  * @fileoverview added by tsickle
