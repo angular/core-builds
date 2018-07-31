0813d313e50c468358402383da0e49bbda03df96
Igor Minar <igor@angular.io>
2018-07-31 20:59:54 +0000

    fa59748 build: make `postinstall` script compatible with Windows (#25232)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+21.sha-fa38828
+ * @license Angular v6.1.0+20.sha-fa59748
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14641,7 +14641,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+21.sha-fa38828');
+const VERSION = new Version('6.1.0+20.sha-fa59748');
 
 /**
  * @license
