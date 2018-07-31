20bb9bc4b1d40ced7bea341b73d54e653c2b417e
Igor Minar <igor@angular.io>
2018-07-31 00:36:57 +0000

    785fb5c docs(aio): add StrongBrew to the trainer list (#24891)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+14.sha-22ebd53
+ * @license Angular v6.1.0+16.sha-785fb5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+14.sha-22ebd53');
+const VERSION = new Version('6.1.0+16.sha-785fb5c');
 
 /**
  * @fileoverview added by tsickle
