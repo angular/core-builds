9475e3b73e3e3f3b28ab43a365f861d5a79506d7
Igor Minar <igor@angular.io>
2018-07-31 20:04:44 +0000

    3f6fc00 docs(forms): fix incorrect variables naming in the comments (#25150)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+55.sha-e3834b7
+ * @license Angular v6.1.0+59.sha-3f6fc00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15020,7 +15020,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+55.sha-e3834b7');
+const VERSION = new Version('6.1.0+59.sha-3f6fc00');
 
 /**
  * @license
