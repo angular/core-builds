24073ef11b6d13e031abb4a429baa7bc3400c742
Igor Minar <igor@angular.io>
2018-07-31 05:30:36 +0000

    74964bd docs: fix link to "Override component providers" (#24967)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+16.sha-785fb5c
+ * @license Angular v6.1.0+17.sha-74964bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+16.sha-785fb5c');
+const VERSION = new Version('6.1.0+17.sha-74964bd');
 
 /**
  * @fileoverview added by tsickle
