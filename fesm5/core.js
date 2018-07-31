b74b8845da5c874546f657c34e58d78b93547eb5
Igor Minar <igor@angular.io>
2018-07-31 05:35:30 +0000

    e3834b7 feat(ivy): support change detection on the root view (#25085)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+56.sha-3f20a2f
+ * @license Angular v6.1.0+55.sha-e3834b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __extends, __spread, __read, __values } from 'tslib';
+import { __extends, __spread, __assign, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15428,7 +15428,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+56.sha-3f20a2f');
+var VERSION = new Version('6.1.0+55.sha-e3834b7');
 
 /**
  * @license
