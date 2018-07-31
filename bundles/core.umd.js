c0962706c0f34e2b2146583883babdd5f192a41d
Igor Minar <igor@angular.io>
2018-07-31 20:06:14 +0000

    5254d34 build(bazel): update to rules_nodejs 0.11.2 and latest rules_typescript (#25169)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+59.sha-3f6fc00
+ * @license Angular v6.1.0+58.sha-5254d34
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1700,7 +1700,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0+59.sha-3f6fc00');
+    var VERSION = new Version('6.1.0+58.sha-5254d34');
 
     /**
      * @license
