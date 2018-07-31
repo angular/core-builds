38194713b0d3df35d3e3db622e6a0916b5796cb8
Igor Minar <igor@angular.io>
2018-07-31 20:57:06 +0000

    fa38828 docs(aio): add short description for entryComponents (#21360)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+18.sha-875efa8
+ * @license Angular v6.1.0+21.sha-fa38828
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1642,7 +1642,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+18.sha-875efa8');
+var VERSION = new Version('6.1.0+21.sha-fa38828');
 
 /**
  * @license
