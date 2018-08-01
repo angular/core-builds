5d0ab63b7df9edf65a3df2d464d04c7ba074e877
Igor Minar <igor@angular.io>
2018-08-01 22:30:23 +0000

    06af794 test(upgrade): run tests against AngularJS v1.7.x as well (#25231)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+83.sha-bde4402
+ * @license Angular v6.1.0+85.sha-06af794
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15092,7 +15092,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+83.sha-bde4402');
+const VERSION = new Version('6.1.0+85.sha-06af794');
 
 /**
  * @license
