ccc646cdd0791223bc2c4d32823048bff326643e
Igor Minar <igor@angular.io>
2018-08-01 20:09:25 +0000

    2651604 refactor(animations): do not use short parameter names (#25198)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+24.sha-3ab31a4
+ * @license Angular v6.1.0+27.sha-2651604
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+24.sha-3ab31a4');
+const VERSION = new Version('6.1.0+27.sha-2651604');
 
 /**
  * @fileoverview added by tsickle
