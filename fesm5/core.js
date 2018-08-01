e6107fdfe86844548274df01c021b196ba6b6ebf
Igor Minar <igor@angular.io>
2018-08-01 20:14:11 +0000

    4976a58 docs: update to account for CLI changes (#25223)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+70.sha-d5b7383
+ * @license Angular v6.1.0+67.sha-4976a58
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15511,7 +15511,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+70.sha-d5b7383');
+var VERSION = new Version('6.1.0+67.sha-4976a58');
 
 /**
  * @license
