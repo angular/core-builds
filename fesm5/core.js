48fa08c7c664035715912bb4d4133a4b80294057
Igor Minar <igor@angular.io>
2018-08-01 20:14:31 +0000

    ec31f6b docs(router): clarify scroll position wording (#25077)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+27.sha-2651604
+ * @license Angular v6.1.0+30.sha-ec31f6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15013,7 +15013,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+27.sha-2651604');
+var VERSION = new Version('6.1.0+30.sha-ec31f6b');
 
 /**
  * @license
