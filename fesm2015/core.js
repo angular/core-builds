fae3f02e66d336ad0d3c86df147497b5a4da291f
Igor Minar <igor@angular.io>
2018-08-01 20:10:28 +0000

    7075c41 docs(changelog): remove reverted feature entry (#25206)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+66.sha-bafe1a0
+ * @license Angular v6.1.0+69.sha-7075c41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15158,7 +15158,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+66.sha-bafe1a0');
+const VERSION = new Version('6.1.0+69.sha-7075c41');
 
 /**
  * @license
