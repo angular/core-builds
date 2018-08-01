5410eeca254babdb25ff5219acf54735914e6a76
Igor Minar <igor@angular.io>
2018-08-01 20:23:32 +0000

    a9ecf4b docs: refactor lazy loading modules example (#25071)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+75.sha-01e6dab
+ * @license Angular v6.1.0+74.sha-a9ecf4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param } from 'tslib';
+import { __decorate, __param, __metadata } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15158,7 +15158,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+75.sha-01e6dab');
+const VERSION = new Version('6.1.0+74.sha-a9ecf4b');
 
 /**
  * @license
