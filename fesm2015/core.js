3c327b8db8e7cf61fdbbf265ee63de4b25bb0ace
Igor Minar <igor@angular.io>
2018-08-01 20:11:28 +0000

    bafe1a0 build(bazel): fix typo in protractor test target definition (#25235)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+65.sha-c8a4fb1
+ * @license Angular v6.1.0+66.sha-bafe1a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15095,7 +15095,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+65.sha-c8a4fb1');
+const VERSION = new Version('6.1.0+66.sha-bafe1a0');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+65.sha-c8a4fb1
+ * @license Angular v6.1.0+66.sha-bafe1a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15511,7 +15511,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+65.sha-c8a4fb1');
+var VERSION = new Version('6.1.0+66.sha-bafe1a0');
 
 /**
  * @license
