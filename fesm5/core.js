f2533a5c47003349ebcf582b2dc95a2811f6d4b4
Igor Minar <igor@angular.io>
2018-08-01 20:22:15 +0000

    a9ecf4b docs: refactor lazy loading modules example (#25071)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+75.sha-01e6dab
+ * @license Angular v6.1.0+74.sha-a9ecf4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1691,7 +1691,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+75.sha-01e6dab');
+var VERSION = new Version('6.1.0+74.sha-a9ecf4b');
 
 /**
  * @license
