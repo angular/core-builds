cc9cb5d0f3e1d2add434db527bf7d2f17acac32e
Igor Minar <igor@angular.io>
2018-08-01 20:28:10 +0000

    7a4fb44 docs(aio): add Kevin Yang to GDE resources (#24791)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+74.sha-a9ecf4b
+ * @license Angular v6.1.0+77.sha-7a4fb44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15158,7 +15158,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+74.sha-a9ecf4b');
+const VERSION = new Version('6.1.0+77.sha-7a4fb44');
 
 /**
  * @license
