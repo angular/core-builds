cbd3889666001cdd40d1720dc1ec9a1ef5c07d13
Igor Minar <igor@angular.io>
2018-08-02 21:52:43 +0000

    58698d7 release: cut the v6.1.1 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+52.sha-e26c25a
+ * @license Angular v6.1.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+52.sha-e26c25a');
+const VERSION = new Version('6.1.1');
 
 /**
  * @fileoverview added by tsickle
