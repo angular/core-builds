e68f9fda126aa75e72f886c09fc5c119dafd16f2
Igor Minar <igor@angular.io>
2018-08-02 00:18:37 +0000

    d5d034a docs: update reactiveconf 2018 in events (#24739)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+43.sha-5ca35b3
+ * @license Angular v6.1.0+44.sha-d5d034a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14638,7 +14638,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+43.sha-5ca35b3');
+const VERSION = new Version('6.1.0+44.sha-d5d034a');
 
 /**
  * @license
