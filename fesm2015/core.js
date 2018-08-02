e0346b12aadba13e2cce6d78b70e9b59ed87bbaf
Igor Minar <igor@angular.io>
2018-08-02 17:37:49 +0000

    74bce18 Revert "docs: refactor http module import for style guide app.module (#25001)" (#25263)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+97.sha-3ba5220
+ * @license Angular v6.1.0+98.sha-74bce18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2008,7 +2008,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+97.sha-3ba5220');
+const VERSION = new Version('6.1.0+98.sha-74bce18');
 
 /**
  * @fileoverview added by tsickle
