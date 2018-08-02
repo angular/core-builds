135524b867454f2aa9e1c1927317cccf4a2fa97d
Igor Minar <igor@angular.io>
2018-08-02 00:01:50 +0000

    0a6a3f3 ci: update pullapprove groups and add docs (#25257)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+41.sha-3a60138
+ * @license Angular v6.1.0+42.sha-0a6a3f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14638,7 +14638,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+41.sha-3a60138');
+const VERSION = new Version('6.1.0+42.sha-0a6a3f3');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+41.sha-3a60138
+ * @license Angular v6.1.0+42.sha-0a6a3f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15010,7 +15010,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+41.sha-3a60138');
+var VERSION = new Version('6.1.0+42.sha-0a6a3f3');
 
 /**
  * @license
