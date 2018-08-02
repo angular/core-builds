ab56d3c00dd83de11679737bd11f567b8200fe44
Igor Minar <igor@angular.io>
2018-08-02 17:39:22 +0000

    74bce18 Revert "docs: refactor http module import for style guide app.module (#25001)" (#25263)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+97.sha-3ba5220
+ * @license Angular v6.1.0+98.sha-74bce18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __assign, __extends, __spread, __read, __values } from 'tslib';
+import { __decorate, __metadata, __assign, __param, __extends, __spread, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15571,7 +15571,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+97.sha-3ba5220');
+var VERSION = new Version('6.1.0+98.sha-74bce18');
 
 /**
  * @license
