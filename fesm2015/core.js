70266afd1c293a906cefd4605359d98b5767a0b4
Igor Minar <igor@angular.io>
2018-08-02 16:56:30 +0000

    3ba5220 refactor(forms): `ngForm` element selector has been deprecated in favor of `ng-form` (#23721)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+96.sha-5982425
+ * @license Angular v6.1.0+97.sha-3ba5220
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15155,7 +15155,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+96.sha-5982425');
+const VERSION = new Version('6.1.0+97.sha-3ba5220');
 
 /**
  * @license
