fc8cf96e5061275708b96e2983e00d43564ccc99
Igor Minar <igor@angular.io>
2018-08-02 01:15:46 +0000

    a224df4 feat(docs-infra): support sending Google Analytics events (#25042)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+89.sha-d46a961
+ * @license Angular v6.1.0+90.sha-a224df4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15155,7 +15155,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+89.sha-d46a961');
+const VERSION = new Version('6.1.0+90.sha-a224df4');
 
 /**
  * @license
