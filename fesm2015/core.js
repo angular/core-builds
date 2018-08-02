ece54f267f5c680a1587ba199a008222e3041e1e
Igor Minar <igor@angular.io>
2018-08-02 16:52:46 +0000

    5982425 test(common): TokenExtractor should extend HttpXsrfTokenExtractor in xsrf spec (#24649)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+94.sha-e60737f
+ * @license Angular v6.1.0+96.sha-5982425
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2008,7 +2008,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+94.sha-e60737f');
+const VERSION = new Version('6.1.0+96.sha-5982425');
 
 /**
  * @fileoverview added by tsickle
