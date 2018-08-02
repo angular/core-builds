649434e37bf37c9881d29f9f36931a096adc224b
Igor Minar <igor@angular.io>
2018-08-02 00:06:12 +0000

    5ca35b3 docs: Update the link to the Jasmine docs (#25175)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+42.sha-0a6a3f3
+ * @license Angular v6.1.0+43.sha-5ca35b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14690,7 +14690,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.0+42.sha-0a6a3f3');
+const VERSION = new Version('6.1.0+43.sha-5ca35b3');
 
 /**
  * @license
