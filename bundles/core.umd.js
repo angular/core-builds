4729992cc546f172127c2ebd8e5acf57624f70cf
Igor Minar <igor@angular.io>
2018-08-02 00:00:33 +0000

    0a6a3f3 ci: update pullapprove groups and add docs (#25257)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+41.sha-3a60138
+ * @license Angular v6.1.0+42.sha-0a6a3f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15119,7 +15119,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.0+41.sha-3a60138');
+    var VERSION = new Version('6.1.0+42.sha-0a6a3f3');
 
     /**
      * @license
