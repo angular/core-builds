7895fcc312a2a876a3745e9bc91cbcb1c05cbddd
Igor Minar <igor@angular.io>
2018-08-02 22:40:13 +0000

    4f741e7 docs: release notes for the v6.1.1 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0
+ * @license Angular v7.0.0-beta.0+1.sha-4f741e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2008,7 +2008,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0');
+const VERSION = new Version('7.0.0-beta.0+1.sha-4f741e7');
 
 /**
  * @fileoverview added by tsickle
