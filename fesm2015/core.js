504a13193a5afc079a6e426678953dd07f78c151
Igor Minar <igor@angular.io>
2018-08-02 16:12:21 +0000

    9ddf269 docs(elements): add section about custom element typings in `elements` guide (#25219)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+45.sha-1ef2eae
+ * @license Angular v6.1.0+48.sha-9ddf269
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.0+45.sha-1ef2eae');
+const VERSION = new Version('6.1.0+48.sha-9ddf269');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+45.sha-1ef2eae
+ * @license Angular v6.1.0+48.sha-9ddf269
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1642,7 +1642,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.0+45.sha-1ef2eae');
+var VERSION = new Version('6.1.0+48.sha-9ddf269');
 
 /**
  * @license
