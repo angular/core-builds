8d0cbc853a5be98ac182897c13457f82fe8a58eb
Kara Erickson <karakara@google.com>
2018-08-03 18:14:27 +0000

    0822dc7 feat(ivy): generate .ngfactory stubs if requested (#25176)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+5.sha-b38931b
+ * @license Angular v7.0.0-beta.0+9.sha-0822dc7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1691,7 +1691,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.0+5.sha-b38931b');
+var VERSION = new Version('7.0.0-beta.0+9.sha-0822dc7');
 
 /**
  * @license
