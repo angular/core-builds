34ccc206479aefb097878d7470061fd5430e515c
Kara Erickson <karakara@google.com>
2018-08-03 18:15:56 +0000

    0822dc7 feat(ivy): generate .ngfactory stubs if requested (#25176)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+5.sha-b38931b
+ * @license Angular v7.0.0-beta.0+9.sha-0822dc7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15195,7 +15195,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.0+5.sha-b38931b');
+const VERSION = new Version('7.0.0-beta.0+9.sha-0822dc7');
 
 /**
  * @license
