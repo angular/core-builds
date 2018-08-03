7da0100c3e9eead5893d4ae6aaef331b02aaa688
Kara Erickson <karakara@google.com>
2018-08-03 23:17:05 +0000

    0fb41e5 test(docs-infra): log docs examples e2e spec paths to aid debugging (#25293)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+2.sha-3f43dbb
+ * @license Angular v6.1.1+3.sha-0fb41e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14638,7 +14638,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.1+2.sha-3f43dbb');
+const VERSION = new Version('6.1.1+3.sha-0fb41e5');
 
 /**
  * @license
