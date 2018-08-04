c6d44728261b61a8e0774283ca7e941dce815709
Kara Erickson <karakara@google.com>
2018-08-04 21:50:44 +0000

    2e16039 build: skip ivy builds when not publishing (#25299)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+4.sha-0c9c2ac
+ * @license Angular v6.1.1+5.sha-2e16039
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.1+4.sha-0c9c2ac');
+const VERSION = new Version('6.1.1+5.sha-2e16039');
 
 /**
  * @fileoverview added by tsickle
