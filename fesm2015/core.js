891d9799b031e26e17d59c498da45f6513795ab0
Kara Erickson <karakara@google.com>
2018-08-06 22:31:41 +0000

    8cbb836 docs: clarify heroes example (#21216)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+16.sha-6e7d5f0
+ * @license Angular v6.1.1+19.sha-8cbb836
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.1+16.sha-6e7d5f0');
+const VERSION = new Version('6.1.1+19.sha-8cbb836');
 
 /**
  * @fileoverview added by tsickle
