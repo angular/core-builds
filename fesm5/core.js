47633ba57f749565b5486b57669547545b6eb3c0
Kara Erickson <karakara@google.com>
2018-08-06 22:26:59 +0000

    40904ce docs: add missing word in outputs description. (#25330)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+13.sha-a54c049
+ * @license Angular v6.1.1+12.sha-40904ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15010,7 +15010,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.1+13.sha-a54c049');
+var VERSION = new Version('6.1.1+12.sha-40904ce');
 
 /**
  * @license
