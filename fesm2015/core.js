20395c75e640464afa1d36c1f654b571a9283342
Kara Erickson <karakara@google.com>
2018-08-06 22:31:43 +0000

    6e7d5f0 docs(core): fix tree-shakable spelling (#24057)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+17.sha-66547d8
+ * @license Angular v6.1.1+16.sha-6e7d5f0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.1+17.sha-66547d8');
+const VERSION = new Version('6.1.1+16.sha-6e7d5f0');
 
 /**
  * @license
