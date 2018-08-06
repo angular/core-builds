374154c78e1870a8084bfdfb0deac9e2a7fb50ad
Kara Erickson <karakara@google.com>
2018-08-06 22:24:29 +0000

    0c028a0 docs: remove code in universal hero detail component (#25215)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+12.sha-40904ce
+ * @license Angular v6.1.1+14.sha-0c028a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1700,7 +1700,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.1+12.sha-40904ce');
+    var VERSION = new Version('6.1.1+14.sha-0c028a0');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+12.sha-40904ce
+ * @license Angular v6.1.1+14.sha-0c028a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __spread, __read, __assign, __extends, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -1642,7 +1642,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.1+12.sha-40904ce');
+var VERSION = new Version('6.1.1+14.sha-0c028a0');
 
 /**
  * @license
