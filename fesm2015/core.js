9a9717ef08757b89c99c036084610e6de0ee6602
Kara Erickson <karakara@google.com>
2018-08-06 22:55:58 +0000

    6861bc5 docs: clarify heroes example (#21216)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+34.sha-b8887dd
+ * @license Angular v7.0.0-beta.0+35.sha-6861bc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param } from 'tslib';
+import { __decorate, __param, __metadata } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15210,7 +15210,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.0+34.sha-b8887dd');
+const VERSION = new Version('7.0.0-beta.0+35.sha-6861bc5');
 
 /**
  * @license
