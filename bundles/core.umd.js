96280a4cc726cbe0d4b941f8946dc99f55e3eacf
Kara Erickson <karakara@google.com>
2018-08-06 19:44:56 +0000

    c66794c docs: several fixes for NPM package guide (#20186)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+8.sha-a57f8a1
+ * @license Angular v6.1.1+10.sha-c66794c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15052,7 +15052,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.1+8.sha-a57f8a1');
+    var VERSION = new Version('6.1.1+10.sha-c66794c');
 
     /**
      * @license
