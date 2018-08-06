047b945bbaa5080be98d51db52d13222cc59e58b
Kara Erickson <karakara@google.com>
2018-08-06 22:55:06 +0000

    b75a985 test(upgrade): reduce flaky-ness by increasing timeout (#24937)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+20.sha-415519a
+ * @license Angular v6.1.1+23.sha-b75a985
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15121,7 +15121,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.1+20.sha-415519a');
+    var VERSION = new Version('6.1.1+23.sha-b75a985');
 
     /**
      * @license
