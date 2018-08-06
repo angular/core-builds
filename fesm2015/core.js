d3ae1379bd7d33fadcb48db5f0e8cf813e84c2ad
Kara Erickson <karakara@google.com>
2018-08-06 17:34:15 +0000

    478eca3 docs(aio): add Made with Angular (#21297)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+5.sha-2e16039
+ * @license Angular v6.1.1+6.sha-478eca3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1951,7 +1951,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.1+5.sha-2e16039');
+const VERSION = new Version('6.1.1+6.sha-478eca3');
 
 /**
  * @fileoverview added by tsickle
