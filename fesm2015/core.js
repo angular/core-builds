4db73d5378af81d0f88014411fd07fc53bbd4352
Kara Erickson <karakara@google.com>
2018-08-06 22:53:54 +0000

    415519a docs: update to 2nd edition of Learning Angular (#20934)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+18.sha-8d0f8bd
+ * @license Angular v6.1.1+20.sha-415519a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.1+18.sha-8d0f8bd');
+const VERSION = new Version('6.1.1+20.sha-415519a');
 
 /**
  * @license
