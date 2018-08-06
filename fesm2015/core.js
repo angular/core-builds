35314b52de1f52b50acb628da3c0d69125adbe95
Kara Erickson <karakara@google.com>
2018-08-06 20:14:29 +0000

    637ae13 docs(http): fixed example unit test for error catching (#25306)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+26.sha-c67f1bb
+ * @license Angular v7.0.0-beta.0+25.sha-637ae13
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15208,7 +15208,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.0+26.sha-c67f1bb');
+const VERSION = new Version('7.0.0-beta.0+25.sha-637ae13');
 
 /**
  * @license
