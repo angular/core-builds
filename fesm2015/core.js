593e3688d37f70f23604ac7820610fe316e5a676
Kara Erickson <karakara@google.com>
2018-08-06 22:24:05 +0000

    88f01f5 docs: fix typo (#25331) PR Close #25331

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+10.sha-c66794c
+ * @license Angular v6.1.1+11.sha-88f01f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14690,7 +14690,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.1+10.sha-c66794c');
+const VERSION = new Version('6.1.1+11.sha-88f01f5');
 
 /**
  * @license
