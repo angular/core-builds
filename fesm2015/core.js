7d18a2b3bd430d1e8989285d23051dd886f700a9
Kara Erickson <karakara@google.com>
2018-08-07 18:51:21 +0000

    155d938 docs: refining code of tutorial 7 routing (#22151)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+23.sha-b75a985
+ * @license Angular v6.1.1+25.sha-155d938
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.1+23.sha-b75a985');
+const VERSION = new Version('6.1.1+25.sha-155d938');
 
 /**
  * @license
