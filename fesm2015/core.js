a65c2f4665a497fb8b2db0d82ca65f8572678a6e
Kara Erickson <karakara@google.com>
2018-08-07 20:04:05 +0000

    2f23533 docs(aio): Angular course in Portuguese #21836

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+26.sha-054fbbe
+ * @license Angular v6.1.1+27.sha-2f23533
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.1+26.sha-054fbbe');
+const VERSION = new Version('6.1.1+27.sha-2f23533');
 
 /**
  * @license
