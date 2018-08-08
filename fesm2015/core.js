1c7d46fc939f3646e911d60e10fc505cfc3a78f2
Kara Erickson <karakara@google.com>
2018-08-08 21:53:18 +0000

    638ff76 docs: add ngrx book to the docs (#23389)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+7.sha-a3f1e2c
+ * @license Angular v7.0.0-beta.1+4.sha-638ff76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2010,7 +2010,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+7.sha-a3f1e2c');
+const VERSION = new Version('7.0.0-beta.1+4.sha-638ff76');
 
 /**
  * @fileoverview added by tsickle
