33ae632916015f33216520317c532cd4c107474b
Kara Erickson <karakara@google.com>
2018-08-08 21:43:43 +0000

    a2593cb release: cut the v7.0.0-beta.1 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+51.sha-8434794
+ * @license Angular v7.0.0-beta.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2010,7 +2010,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+51.sha-8434794');
+const VERSION = new Version('7.0.0-beta.1');
 
 /**
  * @fileoverview added by tsickle
