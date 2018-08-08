8f8bb730a86035dff1717d7d4a2645a6192e141c
Kara <kara@users.noreply.github.com>
2018-08-08 22:13:14 +0000

    2951e72 build(bazel): update to rules_nodejs 0.11.2 and latest rules_typescript (#25365)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+5.sha-6480d1b
+ * @license Angular v6.1.2+7.sha-2951e72
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15012,7 +15012,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.2+5.sha-6480d1b');
+var VERSION = new Version('6.1.2+7.sha-2951e72');
 
 /**
  * @license
