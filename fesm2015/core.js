b8bb4e55ebc611b5b95afead468e4fccfbf74071
Kara Erickson <karakara@google.com>
2018-08-08 21:44:59 +0000

    a2593cb release: cut the v7.0.0-beta.1 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+51.sha-8434794
+ * @license Angular v7.0.0-beta.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param } from 'tslib';
+import { __decorate, __param, __metadata } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15246,7 +15246,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.0+51.sha-8434794');
+const VERSION = new Version('7.0.0-beta.1');
 
 /**
  * @license
