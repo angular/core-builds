f00a522ef05c605e369842c20f563385cce01539
Kara Erickson <karakara@google.com>
2018-08-08 22:03:06 +0000

    6480d1b docs: make css multiline in styleguide for consistency (#25300)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+1.sha-f39551c
+ * @license Angular v6.1.2+5.sha-6480d1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.2+1.sha-f39551c');
+const VERSION = new Version('6.1.2+5.sha-6480d1b');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+1.sha-f39551c
+ * @license Angular v6.1.2+5.sha-6480d1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -516,8 +516,6 @@ function flushFallback(maxTurns) {
 function discardPeriodicTasksFallback() {
     /** @type {?} */
     const zoneSpec = _getFakeAsyncZoneSpec();
-    /** @type {?} */
-    const pendingTimers = zoneSpec.pendingPeriodicTimers;
     zoneSpec.pendingPeriodicTimers.length = 0;
 }
 /**
