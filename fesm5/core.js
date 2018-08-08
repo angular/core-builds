f0ac94d0d8bcec032185379807aa7ed2c4959d8b
Kara Erickson <karakara@google.com>
2018-08-08 23:28:16 +0000

    b64fed1 fix(ivy): prevent ngDevMode from getting removed too early (#25371)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+50.sha-732026c
+ * @license Angular v7.0.0-beta.1+10.sha-b64fed1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1293,7 +1293,7 @@ function callHooks(data, arr) {
 var __global$1 = typeof window != 'undefined' && window || typeof self != 'undefined' && self ||
     typeof global != 'undefined' && global;
 function ngDevModeResetPerfCounters() {
-    __global$1.ngDevMode = {
+    return __global$1.ngDevMode = {
         firstTemplatePass: 0,
         tNode: 0,
         tView: 0,
@@ -1325,7 +1325,7 @@ function ngDevModeResetPerfCounters() {
  * as much early warning and errors as possible.
  */
 if (typeof ngDevMode === 'undefined' || ngDevMode) {
-    ngDevModeResetPerfCounters();
+    __global$1.ngDevMode = ngDevModeResetPerfCounters();
 }
 
 /** Called when directives inject each other (creating a circular dependency) */
@@ -15599,7 +15599,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.0+50.sha-732026c');
+var VERSION = new Version('7.0.0-beta.1+10.sha-b64fed1');
 
 /**
  * @license
