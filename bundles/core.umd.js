2c3e89da1d9d3ac9efdba625ff677a76b78fed0d
Kara Erickson <karakara@google.com>
2018-08-08 19:00:00 +0000

    4b1a825 Revert "build: update Bazel to 0.16 (#25316)" (#25391)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+28.sha-01e6255
+ * @license Angular v6.1.1+29.sha-4b1a825
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15054,7 +15054,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.1+28.sha-01e6255');
+    var VERSION = new Version('6.1.1+29.sha-4b1a825');
 
     /**
      * @license
