b5b1019054e9513b88028c359401e6487bee4e18
Ben Lesh <ben@benlesh.com>
2018-08-09 02:59:32 +0000

    fda30cb build: stop printing source-map-support warning (#25339)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+6.sha-3449f1e
+ * @license Angular v6.1.2+8.sha-fda30cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.2+6.sha-3449f1e');
+var VERSION = new Version('6.1.2+8.sha-fda30cb');
 
 /**
  * @license
