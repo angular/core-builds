885a19e2d39308e6f1a8d38ecbfb4dce7d05e9b2
Ben Lesh <ben@benlesh.com>
2018-08-10 17:29:28 +0000

    cb05f9b docs: fix typo in testing guide  (closes #25400) (#25418)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+10.sha-d0f089a
+ * @license Angular v6.1.2+9.sha-cb05f9b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15064,7 +15064,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.2+10.sha-d0f089a');
+var VERSION = new Version('6.1.2+9.sha-cb05f9b');
 
 /**
  * @license
