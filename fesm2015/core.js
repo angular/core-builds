a4041c03fdaf1c9ca4625ecc6963e23561470ddf
Ben Lesh <ben@benlesh.com>
2018-08-14 02:06:38 +0000

    fb2c524 fix(bazel): correct type concatenated to devmode_js (#25467)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+26.sha-97d8b5e
+ * @license Angular v7.0.0-beta.1+27.sha-fb2c524
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18668,7 +18668,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+26.sha-97d8b5e');
+const VERSION = new Version('7.0.0-beta.1+27.sha-fb2c524');
 
 /**
  * @fileoverview added by tsickle
