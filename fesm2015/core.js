be4eefc1163619c97e70e16292b9b0456be968d4
Ben Lesh <ben@benlesh.com>
2018-08-14 05:40:13 +0000

    151e4b9 docs: add link to Yarn in README (#24856)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+9.sha-cb05f9b
+ * @license Angular v6.1.2+11.sha-151e4b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.2+9.sha-cb05f9b');
+const VERSION = new Version('6.1.2+11.sha-151e4b9');
 
 /**
  * @license
