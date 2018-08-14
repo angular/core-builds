b646e0a6ea7fa23a33c548fe8b6d40878cf41958
Ben Lesh <ben@benlesh.com>
2018-08-14 05:49:19 +0000

    5f08bdf ci: github robot should enforce that all requested reviews are submitted (#25336)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+11.sha-151e4b9
+ * @license Angular v6.1.2+13.sha-5f08bdf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.2+11.sha-151e4b9');
+const VERSION = new Version('6.1.2+13.sha-5f08bdf');
 
 /**
  * @license
