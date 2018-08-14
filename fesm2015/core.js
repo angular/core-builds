af4f759995b33e4b2a01ab887e0ac1284461b2f7
Ben Lesh <ben@benlesh.com>
2018-08-14 06:01:23 +0000

    c869b14 build: drop unused re-export of json_marshal. (#25449)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+29.sha-e0314b5
+ * @license Angular v7.0.0-beta.1+30.sha-c869b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18670,7 +18670,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+29.sha-e0314b5');
+const VERSION = new Version('7.0.0-beta.1+30.sha-c869b14');
 
 /**
  * @fileoverview added by tsickle
