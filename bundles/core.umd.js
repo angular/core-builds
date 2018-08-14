a92f0c056386d7d30c9e4322dd8b05580b3f43a2
Ben Lesh <ben@benlesh.com>
2018-08-14 05:55:00 +0000

    f1ed022 docs: fix typo in Architecture overview page (#25438)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+13.sha-5f08bdf
+ * @license Angular v6.1.2+12.sha-f1ed022
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15121,7 +15121,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.2+13.sha-5f08bdf');
+    var VERSION = new Version('6.1.2+12.sha-f1ed022');
 
     /**
      * @license
