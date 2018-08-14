a3c5fc69485fb2f244c6b1ee395ae6a1b7dfad5e
Ben Lesh <ben@benlesh.com>
2018-08-14 20:12:25 +0000

    e906bf4 docs: add Accelebrate to resources (#23204)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+12.sha-f1ed022
+ * @license Angular v6.1.2+14.sha-e906bf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15064,7 +15064,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.2+12.sha-f1ed022');
+var VERSION = new Version('6.1.2+14.sha-e906bf4');
 
 /**
  * @license
