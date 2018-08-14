e7804f446eed7bae20bbcc08e3bf5157b5250104
Ben Lesh <ben@benlesh.com>
2018-08-14 05:41:32 +0000

    3ac7070 docs: fix typo in Architecture overview page (#25438)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+28.sha-1bb3014
+ * @license Angular v7.0.0-beta.1+31.sha-3ac7070
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15298,7 +15298,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.1+28.sha-1bb3014');
+    var VERSION = new Version('7.0.0-beta.1+31.sha-3ac7070');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+28.sha-1bb3014
+ * @license Angular v7.0.0-beta.1+31.sha-3ac7070
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -14827,7 +14827,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.1+28.sha-1bb3014');
+const VERSION = new Version('7.0.0-beta.1+31.sha-3ac7070');
 
 /**
  * @license
