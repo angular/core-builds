2f839bc277b93391cae54101377ab0e57acdd474
Ben Lesh <ben@benlesh.com>
2018-08-15 01:44:46 +0000

    9c1311c docs(core): Correct spelling error in directives docs (#25377)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+23.sha-1319ff4
+ * @license Angular v6.1.2+22.sha-9c1311c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15054,7 +15054,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.2+23.sha-1319ff4');
+    var VERSION = new Version('6.1.2+22.sha-9c1311c');
 
     /**
      * @license
