75e20231d91245cc3a5edaa22117e563d84c1adb
Ben Lesh <ben@benlesh.com>
2018-08-15 01:12:12 +0000

    1319ff4 fix(service-worker): `Cache-Control: no-cache` on assets breaks service worker (#25408)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+21.sha-2ce9348
+ * @license Angular v6.1.2+23.sha-1319ff4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.2+21.sha-2ce9348');
+const VERSION = new Version('6.1.2+23.sha-1319ff4');
 
 /**
  * @license
