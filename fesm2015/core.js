fe1ba8a81901b610dd4e39ba9d7453e49644d5bb
Ben Lesh <ben@benlesh.com>
2018-08-15 01:43:23 +0000

    9c1311c docs(core): Correct spelling error in directives docs (#25377)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+23.sha-1319ff4
+ * @license Angular v6.1.2+22.sha-9c1311c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.2+23.sha-1319ff4');
+const VERSION = new Version('6.1.2+22.sha-9c1311c');
 
 /**
  * @license
