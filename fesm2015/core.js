b3e680879f98f8a1fbbde5f8320454850eaacb9b
Ben Lesh <ben@benlesh.com>
2018-08-15 01:04:36 +0000

    be2cf4d docs(core): Correct spelling error in directives docs (#25377)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+47.sha-a0a29fd
+ * @license Angular v7.0.0-beta.1+50.sha-be2cf4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16197,7 +16197,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+47.sha-a0a29fd');
+const VERSION = new Version('7.0.0-beta.1+50.sha-be2cf4d');
 
 /**
  * @fileoverview added by tsickle
