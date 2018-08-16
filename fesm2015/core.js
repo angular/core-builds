b152db1981500ad11ad75f328b10c6765fe23c5d
Ben Lesh <ben@benlesh.com>
2018-08-16 00:05:39 +0000

    4ce70b9 release: cut the vv7.0.0-beta.2 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+52.sha-1f11039
+ * @license Angular v7.0.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12464,7 +12464,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.1+52.sha-1f11039');
+const VERSION = new Version('7.0.0-beta.2');
 
 /**
  * @license
