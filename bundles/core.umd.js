894c255a94da735ca1c2e0f0017a98a27717e55f
Jason Aden <jasonaden@google.com>
2018-08-16 22:14:22 +0000

    36fb4f4 docs: reactive forms guide copy edits (#25417)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3
+ * @license Angular v6.1.3+1.sha-36fb4f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15054,7 +15054,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.3');
+    var VERSION = new Version('6.1.3+1.sha-36fb4f4');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.3
+ * @license Angular v6.1.3+1.sha-36fb4f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __extends, __spread, __values, __read } from 'tslib';
+import { __assign, __extends, __spread, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15012,7 +15012,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.3');
+var VERSION = new Version('6.1.3+1.sha-36fb4f4');
 
 /**
  * @license
