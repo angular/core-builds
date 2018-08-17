f3fae1df3dab06e302e203c4f79447c59cd5dda6
Jason Aden <jasonaden@google.com>
2018-08-17 01:25:57 +0000

    07d8d39 feat(router): add UrlSegment[] to CanLoad interface (#13127)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.2+19.sha-116946f
+ * @license Angular v7.0.0-beta.2+20.sha-07d8d39
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __extends, __spread, __read, __values } from 'tslib';
+import { __extends, __spread, __assign, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -11509,7 +11509,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.2+19.sha-116946f');
+var VERSION = new Version('7.0.0-beta.2+20.sha-07d8d39');
 
 /**
  * @license
