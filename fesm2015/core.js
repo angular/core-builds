6c51d4fc2819f4db120b8d6c9e6d314e5fa4bae6
Jason Aden <jasonaden@google.com>
2018-08-17 21:56:48 +0000

    2b810a4 docs(docs-infra): the `build.sh` script was renamed to `create-image.sh` (#25554)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+14.sha-a5cc9db
+ * @license Angular v6.1.3+13.sha-2b810a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.3+14.sha-a5cc9db');
+const VERSION = new Version('6.1.3+13.sha-2b810a4');
 
 /**
  * @license
