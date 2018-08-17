3d3552922889f1ed303e8ff25631d7479c1d08aa
Jason Aden <jasonaden@google.com>
2018-08-17 22:41:51 +0000

    f2aa9c6 refactor(ivy): use generated consts value to set binding index (#25533)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+27.sha-d2be3d5
+ * @license Angular v7.0.0-beta.2+26.sha-f2aa9c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11472,7 +11472,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.2+27.sha-d2be3d5');
+    var VERSION = new Version('7.0.0-beta.2+26.sha-f2aa9c6');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+27.sha-d2be3d5
+ * @license Angular v7.0.0-beta.2+26.sha-f2aa9c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13892,7 +13892,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+27.sha-d2be3d5');
+const VERSION = new Version('7.0.0-beta.2+26.sha-f2aa9c6');
 
 /**
  * @fileoverview added by tsickle
