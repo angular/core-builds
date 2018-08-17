36673d9ba5c2d2d6408a32fac9f657b2d231efd6
Jason Aden <jasonaden@google.com>
2018-08-17 00:16:12 +0000

    116946f style: tslint enforces no debugger statements left behind (#25532)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+18.sha-503905c
+ * @license Angular v7.0.0-beta.2+19.sha-116946f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1201,6 +1201,7 @@
         }
     }
     function throwError(msg) {
+        // tslint:disable-next-line
         debugger; // Left intentionally for better debugger experience.
         throw new Error("ASSERTION ERROR: " + msg);
     }
@@ -11550,7 +11551,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.2+18.sha-503905c');
+    var VERSION = new Version('7.0.0-beta.2+19.sha-116946f');
 
     /**
      * @license
@@ -11564,10 +11565,7 @@
     *
     * @Annotation
     */
-    var Injectable = makeDecorator('Injectable', undefined, undefined, undefined, function (type, meta) {
-        debugger;
-        return R3_COMPILE_INJECTABLE(type, meta);
-    });
+    var Injectable = makeDecorator('Injectable', undefined, undefined, undefined, function (type, meta) { return R3_COMPILE_INJECTABLE(type, meta); });
 
     /**
      * @license
