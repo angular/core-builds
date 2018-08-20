25a85257a729cd81338d5026e607326dc9b69253
Jason Aden <jasonaden@google.com>
2018-08-20 19:29:52 +0000

    292b435 docs: fix typo in reactive forms guide (#25543)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+18.sha-77d9975
+ * @license Angular v6.1.3+16.sha-292b435
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15054,7 +15054,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.3+18.sha-77d9975');
+    var VERSION = new Version('6.1.3+16.sha-292b435');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+18.sha-77d9975
+ * @license Angular v6.1.3+16.sha-292b435
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.3+18.sha-77d9975');
+const VERSION = new Version('6.1.3+16.sha-292b435');
 
 /**
  * @license
