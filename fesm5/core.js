f62f59034e60f3859e779332b6afe53cba44734e
Jason Aden <jasonaden@google.com>
2018-08-20 19:35:19 +0000

    78750a7 docs: fix typo in service worker getting started guide (#25512)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+16.sha-292b435
+ * @license Angular v6.1.3+19.sha-78750a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15012,7 +15012,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.3+16.sha-292b435');
+var VERSION = new Version('6.1.3+19.sha-78750a7');
 
 /**
  * @license
