ba479351074a442a063e0b9f8e32d7bc11c1caaa
Jason Aden <jasonaden@google.com>
2018-08-20 19:13:37 +0000

    77d9975 build(aio): update dgeni-packages to 0.26.3 to fix reference types issue (#25528)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+15.sha-5939c42
+ * @license Angular v6.1.3+18.sha-77d9975
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.3+15.sha-5939c42');
+const VERSION = new Version('6.1.3+18.sha-77d9975');
 
 /**
  * @license
