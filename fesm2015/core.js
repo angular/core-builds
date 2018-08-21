fa6b016a9406eda0bed410075bb74778beb9ca79
Jason Aden <jasonaden@google.com>
2018-08-21 19:30:46 +0000

    7e61645 fix(router): default scroll position restoration to disabled (#25586)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+20.sha-46b0ce9
+ * @license Angular v6.1.3+21.sha-7e61645
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.3+20.sha-46b0ce9');
+const VERSION = new Version('6.1.3+21.sha-7e61645');
 
 /**
  * @license
