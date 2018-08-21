c12a86772f7bb21cdc134569f01dbaf443e937b9
Jason Aden <jasonaden@google.com>
2018-08-21 21:37:19 +0000

    f33dbf4 docs(changelog): add v6.1.3 release to main CHANGELOG.md (#25603)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+38.sha-6176974
+ * @license Angular v7.0.0-beta.2+39.sha-f33dbf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11251,7 +11251,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.2+38.sha-6176974');
+const VERSION = new Version('7.0.0-beta.2+39.sha-f33dbf4');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.2+38.sha-6176974
+ * @license Angular v7.0.0-beta.2+39.sha-f33dbf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __assign, __param, __extends, __spread, __read, __values } from 'tslib';
+import { __extends, __spread, __decorate, __metadata, __param, __assign, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -11532,7 +11532,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.2+38.sha-6176974');
+var VERSION = new Version('7.0.0-beta.2+39.sha-f33dbf4');
 
 /**
  * @license
