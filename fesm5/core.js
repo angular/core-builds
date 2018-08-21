d44e3ad47ff56f9bb8b265fec97523a2fe4a2e01
Jason Aden <jasonaden@google.com>
2018-08-21 19:37:21 +0000

    1fa9790 docs: Improve docs for downgrading a service (#19371)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+21.sha-7e61645
+ * @license Angular v6.1.3+22.sha-1fa9790
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15012,7 +15012,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.3+21.sha-7e61645');
+var VERSION = new Version('6.1.3+22.sha-1fa9790');
 
 /**
  * @license
