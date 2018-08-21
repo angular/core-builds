49aa1020ec11aaaec1bbc21d97574a2cea9e4a2c
Jason Aden <jasonaden@google.com>
2018-08-21 19:34:50 +0000

    1fa9790 docs: Improve docs for downgrading a service (#19371)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+21.sha-7e61645
+ * @license Angular v6.1.3+22.sha-1fa9790
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.3+21.sha-7e61645');
+const VERSION = new Version('6.1.3+22.sha-1fa9790');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.3+21.sha-7e61645
+ * @license Angular v6.1.3+22.sha-1fa9790
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __extends, __assign, __values } from 'tslib';
+import { __spread, __read, __assign, __extends, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.3+21.sha-7e61645');
+var VERSION = new Version('6.1.3+22.sha-1fa9790');
 
 /**
  * @license
