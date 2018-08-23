915d258fef1549037e1a09e871e9c3d692d8e6f1
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:51:28 +0000

    668bfce fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+28.sha-eb1fe19
+ * @license Angular v7.0.0-beta.3+29.sha-668bfce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11259,7 +11259,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.3+28.sha-eb1fe19');
+const VERSION = new Version('7.0.0-beta.3+29.sha-668bfce');
 
 /**
  * @license
