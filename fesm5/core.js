553f047f0d1b20dd3747102deac38a059bc61be3
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 22:39:16 +0000

    4e82a76 docs(elements): fix typo (tranformation --> transformation) (#25600)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.4+3.sha-51d5b43
+ * @license Angular v6.1.4+4.sha-4e82a76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __assign, __extends, __spread, __read, __values } from 'tslib';
+import { __decorate, __metadata, __assign, __param, __extends, __spread, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15064,7 +15064,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.4+3.sha-51d5b43');
+var VERSION = new Version('6.1.4+4.sha-4e82a76');
 
 /**
  * @license
