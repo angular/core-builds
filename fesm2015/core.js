fc0fcaf5103eef103c0956d716390e6d748956d4
Jason Aden <jasonaden@google.com>
2018-08-23 00:41:58 +0000

    8de57c9 release: cut the v6.1.4 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+23.sha-ace4e4f
+ * @license Angular v6.1.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.3+23.sha-ace4e4f');
+const VERSION = new Version('6.1.4');
 
 /**
  * @license
