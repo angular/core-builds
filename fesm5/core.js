a24f133b2eac86f67985085874329664c74d1a9e
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:46:44 +0000

    e40519c build(docs-infra): render all overloads if they are abstract (#25670)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+7.sha-b560189
+ * @license Angular v6.1.4+8.sha-e40519c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.4+7.sha-b560189');
+var VERSION = new Version('6.1.4+8.sha-e40519c');
 
 /**
  * @license
