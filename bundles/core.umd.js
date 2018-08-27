5616c4cf53a575f67a5b72ca81c408963fa75fc8
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 21:19:01 +0000

    3634575 test(common): fix double instantiation in NgSwitch test (#25632)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+39.sha-9bcd8c2
+ * @license Angular v7.0.0-beta.3+57.sha-3634575
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11601,7 +11601,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.3+39.sha-9bcd8c2');
+    var VERSION = new Version('7.0.0-beta.3+57.sha-3634575');
 
     /**
      * @license
