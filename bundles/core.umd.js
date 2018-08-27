f66779eda5ed31ee1410188afc72eb6fca09c3d4
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:41:10 +0000

    b560189 build(docs-infra): remove "annotations" section from API pages (#25677)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+6.sha-59cfc8a
+ * @license Angular v6.1.4+7.sha-b560189
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1702,7 +1702,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.4+6.sha-59cfc8a');
+    var VERSION = new Version('6.1.4+7.sha-b560189');
 
     /**
      * @license
