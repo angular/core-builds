721f393dda7229b6bab62bd75e2ec36c6f11dae2
Matias Niemelä <matias@yearofmoo.com>
2018-08-28 18:42:42 +0000

    b069514 fix(docs-infra): temporary SW fix (for online mode) (#25692)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+67.sha-0024d68
+ * @license Angular v7.0.0-beta.3+68.sha-b069514
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11626,7 +11626,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.3+67.sha-0024d68');
+var VERSION = new Version('7.0.0-beta.3+68.sha-b069514');
 
 /**
  * @license
