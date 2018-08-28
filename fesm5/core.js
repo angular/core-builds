18984175b4037d39167d0a7ffa25648fae956a87
Miško Hevery <misko@hevery.com>
2018-08-28 19:20:55 +0000

    0fe708f ci: remove `vicb` from `pullapprove.yml` (#25702)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+68.sha-b069514
+ * @license Angular v7.0.0-beta.3+69.sha-0fe708f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11626,7 +11626,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.3+68.sha-b069514');
+var VERSION = new Version('7.0.0-beta.3+69.sha-0fe708f');
 
 /**
  * @license
