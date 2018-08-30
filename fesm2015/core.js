7f7751434a6ea4412e03d5486cd073baf2c3f1ef
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 00:22:06 +0000

    72b3b27 docs: release notes for the v6.1.6 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+76.sha-f394ba0
+ * @license Angular v7.0.0-beta.3+77.sha-72b3b27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14129,7 +14129,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.3+76.sha-f394ba0');
+const VERSION = new Version('7.0.0-beta.3+77.sha-72b3b27');
 
 /**
  * @fileoverview added by tsickle
