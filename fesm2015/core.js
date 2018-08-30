1973c6bbe2d9c3549cca23679dea29374155bacc
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 19:01:58 +0000

    2e09115 docs: edit and organize di guide (#21915)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6
+ * @license Angular v6.1.6+1.sha-2e09115
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.6');
+const VERSION = new Version('6.1.6+1.sha-2e09115');
 
 /**
  * @license
