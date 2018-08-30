31d64f13853adff1f3b6ac8c2d61394aac2d2301
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 00:35:28 +0000

    4a8d56a release: cut the v6.1.6 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.5+2.sha-0a3dd87
+ * @license Angular v6.1.6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.5+2.sha-0a3dd87');
+const VERSION = new Version('6.1.6');
 
 /**
  * @fileoverview added by tsickle
