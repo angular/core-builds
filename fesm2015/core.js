43f6797d750983f82b6842f63d1a3702a474e094
Misko Hevery <misko@hevery.com>
2018-08-31 22:38:28 +0000

    a4405d7 docs(forms): update API reference for reactive and template-driven forms modules (#25687)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+8.sha-88f7ddb
+ * @license Angular v6.1.6+9.sha-a4405d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.6+8.sha-88f7ddb');
+const VERSION = new Version('6.1.6+9.sha-a4405d7');
 
 /**
  * @license
