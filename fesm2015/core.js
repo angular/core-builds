cc354ae868c3cbefa552d20f17a34aa0500af012
Misko Hevery <misko@hevery.com>
2018-08-31 19:12:43 +0000

    31349fd build(bazel): make resolveTypeReferenceDirectives override work with both ts 2.9 & ts 3.0 (#25581)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+29.sha-20b9c61
+ * @license Angular v7.0.0-beta.4+31.sha-31349fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14106,7 +14106,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.4+29.sha-20b9c61');
+const VERSION = new Version('7.0.0-beta.4+31.sha-31349fd');
 
 /**
  * @fileoverview added by tsickle
