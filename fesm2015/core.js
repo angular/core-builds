e9f255afa931936998b6da39b78773fc53f2d46c
Misko Hevery <misko@hevery.com>
2018-08-31 18:17:33 +0000

    20b9c61 perf(ivy): speed up ngcc ivy switch processing (#25534)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+23.sha-a469c2c
+ * @license Angular v7.0.0-beta.4+29.sha-20b9c61
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11368,7 +11368,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.4+23.sha-a469c2c');
+const VERSION = new Version('7.0.0-beta.4+29.sha-20b9c61');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+23.sha-a469c2c
+ * @license Angular v7.0.0-beta.4+29.sha-20b9c61
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11649,7 +11649,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.4+23.sha-a469c2c');
+var VERSION = new Version('7.0.0-beta.4+29.sha-20b9c61');
 
 /**
  * @license
