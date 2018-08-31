96d40c06a48ef896ed5cae221c7d49171f3b1e64
Misko Hevery <misko@hevery.com>
2018-08-31 05:43:21 +0000

    ed6b68b fix(bazel): protractor rule should include *.e2e-spec.js (#25701)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+1.sha-2e09115
+ * @license Angular v6.1.6+2.sha-ed6b68b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.6+1.sha-2e09115');
+const VERSION = new Version('6.1.6+2.sha-ed6b68b');
 
 /**
  * @fileoverview added by tsickle
