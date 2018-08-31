8fb31c86342cdaf531a9f68ee8ea24bfcd9e87e1
Misko Hevery <misko@hevery.com>
2018-08-31 18:10:20 +0000

    a469c2c feat(ivy): produce contextual diagnostics in ngtsc mode (#25647)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+21.sha-b424b31
+ * @license Angular v7.0.0-beta.4+23.sha-a469c2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11368,7 +11368,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.4+21.sha-b424b31');
+const VERSION = new Version('7.0.0-beta.4+23.sha-a469c2c');
 
 /**
  * @license
