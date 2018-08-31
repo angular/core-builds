67174604bfb2addf3f4272ab4cd5559271e116c3
Misko Hevery <misko@hevery.com>
2018-08-31 05:44:44 +0000

    ed6b68b fix(bazel): protractor rule should include *.e2e-spec.js (#25701)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+1.sha-2e09115
+ * @license Angular v6.1.6+2.sha-ed6b68b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15121,7 +15121,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.6+1.sha-2e09115');
+    var VERSION = new Version('6.1.6+2.sha-ed6b68b');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+1.sha-2e09115
+ * @license Angular v6.1.6+2.sha-ed6b68b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.6+1.sha-2e09115');
+const VERSION = new Version('6.1.6+2.sha-ed6b68b');
 
 /**
  * @license
