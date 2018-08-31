1b215ce2e7da3eae30fee810bcbe4208bb855da2
Miško Hevery <misko@hevery.com>
2018-08-31 18:58:07 +0000

    88f7ddb build: fix bad merge onto patch branch (#25729)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+2.sha-ed6b68b
+ * @license Angular v6.1.6+8.sha-88f7ddb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.6+2.sha-ed6b68b');
+const VERSION = new Version('6.1.6+8.sha-88f7ddb');
 
 /**
  * @license
