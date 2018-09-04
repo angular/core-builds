a3454adbe76a3a52457d8c90c637fd6b1e05b213
Misko Hevery <misko@hevery.com>
2018-09-04 21:40:39 +0000

    ad7be50 ci: ensure build-packages-dist works on OS/X bash (#25591)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+12.sha-4fe369e
+ * @license Angular v6.1.6+10.sha-ad7be50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.6+12.sha-4fe369e');
+const VERSION = new Version('6.1.6+10.sha-ad7be50');
 
 /**
  * @fileoverview added by tsickle
