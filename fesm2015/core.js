94e281662d148fa0e6abe619c29e15eb557d01d6
Misko Hevery <misko@hevery.com>
2018-09-04 20:45:48 +0000

    d8930bb docs: correct misspellings and add missing punctuation in tutorial (#25676)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+9.sha-a4405d7
+ * @license Angular v6.1.6+11.sha-d8930bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.6+9.sha-a4405d7');
+const VERSION = new Version('6.1.6+11.sha-d8930bb');
 
 /**
  * @fileoverview added by tsickle
