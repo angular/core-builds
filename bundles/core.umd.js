725937fad2eb4f120b62a88b970e94049a911bf4
Misko Hevery <misko@hevery.com>
2018-09-04 20:48:27 +0000

    d8930bb docs: correct misspellings and add missing punctuation in tutorial (#25676)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+9.sha-a4405d7
+ * @license Angular v6.1.6+11.sha-d8930bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15054,7 +15054,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.6+9.sha-a4405d7');
+    var VERSION = new Version('6.1.6+11.sha-d8930bb');
 
     /**
      * @license
