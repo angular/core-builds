5726b841775c4f9f24d182bba966a0117dd931e4
Misko Hevery <misko@hevery.com>
2018-09-05 19:27:44 +0000

    27f0817 docs(changelog): fix version 6.1.5 typo (#25760) PR Close #25760

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+18.sha-6f7df8a
+ * @license Angular v6.1.6+16.sha-27f0817
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.6+18.sha-6f7df8a');
+const VERSION = new Version('6.1.6+16.sha-27f0817');
 
 /**
  * @fileoverview added by tsickle
