369afb6d022c997fbb1aac2bcbca90e4e48191e9
Misko Hevery <misko@hevery.com>
2018-09-05 19:21:19 +0000

    4596fc0 refactor(docs-infra): bump polyfills payload limit (#25806)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+14.sha-46de203
+ * @license Angular v6.1.6+15.sha-4596fc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.6+14.sha-46de203');
+const VERSION = new Version('6.1.6+15.sha-4596fc0');
 
 /**
  * @license
