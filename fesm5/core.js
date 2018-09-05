8dc8bbcfc5e9156145da34b6933f54d13c9485e4
Misko Hevery <misko@hevery.com>
2018-09-05 20:14:13 +0000

    dcad054 build: update ngcontainer to node 10.9.0 (#25812)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.4+57.sha-397b047
+ * @license Angular v7.0.0-beta.4+59.sha-dcad054
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __spread, __read, __decorate, __metadata, __extends, __param, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __read, __assign, __param, __extends, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -11761,7 +11761,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.4+57.sha-397b047');
+var VERSION = new Version('7.0.0-beta.4+59.sha-dcad054');
 
 /**
  * @license
