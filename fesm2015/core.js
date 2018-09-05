076a5526f485101d76f2c47cdd3b2001d15fb86b
Misko Hevery <misko@hevery.com>
2018-09-05 19:26:06 +0000

    6f7df8a feat(docs-infra): disable "status" selector in API list when displaying only packages (#25718)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+17.sha-92298e5
+ * @license Angular v6.1.6+18.sha-6f7df8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.6+17.sha-92298e5');
+const VERSION = new Version('6.1.6+18.sha-6f7df8a');
 
 /**
  * @license
