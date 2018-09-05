92b29d4d2add367bac1071e80fdc0f70f952a64b
Misko Hevery <misko@hevery.com>
2018-09-05 20:05:59 +0000

    d96e962 test(ivy): mock the filesystem in ngcc main() integration tests (#25557)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+54.sha-2a672a9
+ * @license Angular v7.0.0-beta.4+56.sha-d96e962
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11391,7 +11391,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.4+54.sha-2a672a9');
+const VERSION = new Version('7.0.0-beta.4+56.sha-d96e962');
 
 /**
  * @license
