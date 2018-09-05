0ae73651491933bc7e17c8afdca104a0a898ebc8
Misko Hevery <misko@hevery.com>
2018-09-05 19:26:56 +0000

    92298e5 build(docs-infra): do not render internals in package API pages (#25723)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+19.sha-23a96dc
+ * @license Angular v6.1.6+17.sha-92298e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15054,7 +15054,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.6+19.sha-23a96dc');
+    var VERSION = new Version('6.1.6+17.sha-92298e5');
 
     /**
      * @license
@@ -19439,7 +19439,6 @@
     exports.SkipSelf = SkipSelf;
     exports.Host = Host;
     exports.NgZone = NgZone;
-    exports.ɵNoopNgZone = NoopNgZone;
     exports.RenderComponentType = RenderComponentType;
     exports.Renderer = Renderer;
     exports.Renderer2 = Renderer2;
