22e6e87c1b797bf1557ebb15811f528a4d8c8494
Misko Hevery <misko@hevery.com>
2018-09-05 19:20:18 +0000

    d752a89 build(docs-infra): ensure root node_modules exists (#25811)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+15.sha-4596fc0
+ * @license Angular v6.1.6+13.sha-d752a89
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.6+15.sha-4596fc0');
+const VERSION = new Version('6.1.6+13.sha-d752a89');
 
 /**
  * @fileoverview added by tsickle
