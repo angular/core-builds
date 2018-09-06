e0b7765aa8cd73e69c3588ab0e2bb464cc2d7723
Igor Minar <igor@angular.io>
2018-09-06 23:43:50 +0000

    e244b51 build(bazel): update bazel integration to build angular from source (#25774)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+7.sha-f67229e
+ * @license Angular v6.1.7+3.sha-e244b51
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.7+7.sha-f67229e');
+const VERSION = new Version('6.1.7+3.sha-e244b51');
 
 /**
  * @license
