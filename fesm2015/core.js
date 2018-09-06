16143a669cec36fab7223bd22f7c6ad415b285a8
Igor Minar <igor@angular.io>
2018-09-06 23:41:07 +0000

    f707f54 build: update to Node 10 (#25822)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+8.sha-b9a5ce1
+ * @license Angular v6.1.7+6.sha-f707f54
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.7+8.sha-b9a5ce1');
+const VERSION = new Version('6.1.7+6.sha-f707f54');
 
 /**
  * @license
