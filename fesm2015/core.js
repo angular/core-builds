359a3be1ff39f4d496a8f7d1a76e814d01dd0ee5
Igor Minar <igor@angular.io>
2018-09-06 23:22:57 +0000

    b9a5ce1 docs(service-worker): update http-server command (#25845) PR Close #25845

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+28.sha-d1063c6
+ * @license Angular v6.1.7+8.sha-b9a5ce1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.6+28.sha-d1063c6');
+const VERSION = new Version('6.1.7+8.sha-b9a5ce1');
 
 /**
  * @license
