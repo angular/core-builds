79cf91046ce73afd38bde64022c30d3cd64ab4d4
Igor Minar <igor@angular.io>
2018-09-06 23:21:37 +0000

    b9a5ce1 docs(service-worker): update http-server command (#25845) PR Close #25845

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.6+28.sha-d1063c6
+ * @license Angular v6.1.7+8.sha-b9a5ce1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify } from '@angular/core';
-import { __spread, __values, __decorate, __extends } from 'tslib';
+import { __extends, __decorate, __spread, __values } from 'tslib';
 
 /**
  * @license
