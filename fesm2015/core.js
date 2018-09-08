2218e46346482144e7fb75c98a6bd15d10d3881d
Igor Minar <igor@angular.io>
2018-09-08 00:12:15 +0000

    96eb79b build: add support for running builds outside of sandbox on Mac. (#25870)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+9.sha-83a1334
+ * @license Angular v7.0.0-beta.5+10.sha-96eb79b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14537,7 +14537,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+9.sha-83a1334');
+const VERSION = new Version('7.0.0-beta.5+10.sha-96eb79b');
 
 /**
  * @fileoverview added by tsickle
