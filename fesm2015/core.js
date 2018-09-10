0f123a9fdebd6a9a8be70857852ca1617c9f891f
Igor Minar <igor@angular.io>
2018-09-10 19:14:41 +0000

    ed266da docs(aio): add ng-sq-ui to resources (#25874)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+11.sha-91d7993
+ * @license Angular v7.0.0-beta.5+13.sha-ed266da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14544,7 +14544,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+11.sha-91d7993');
+const VERSION = new Version('7.0.0-beta.5+13.sha-ed266da');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.5+11.sha-91d7993
+ * @license Angular v7.0.0-beta.5+13.sha-ed266da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read, __assign, __values } from 'tslib';
+import { __extends, __spread, __assign, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -11969,7 +11969,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.5+11.sha-91d7993');
+var VERSION = new Version('7.0.0-beta.5+13.sha-ed266da');
 
 /**
  * @license
