82bffda746914697b9f3d62596861d844783289e
Igor Minar <igor@angular.io>
2018-09-11 15:12:44 +0000

    c5b594e docs(bazel): add info on options for nodejs_test rule (#25877)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+22.sha-86a3be8
+ * @license Angular v7.0.0-beta.5+23.sha-c5b594e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11708,7 +11708,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.5+22.sha-86a3be8');
+const VERSION = new Version('7.0.0-beta.5+23.sha-c5b594e');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.5+22.sha-86a3be8
+ * @license Angular v7.0.0-beta.5+23.sha-c5b594e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __read, __assign, __values } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __spread, __assign, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -11995,7 +11995,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.5+22.sha-86a3be8');
+var VERSION = new Version('7.0.0-beta.5+23.sha-c5b594e');
 
 /**
  * @license
