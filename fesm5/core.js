46cb20b351b03b6f2e66b09b7b6735be8d8d0cc5
Igor Minar <igor@angular.io>
2018-09-11 16:11:18 +0000

    f3b552f build(bazel): remove outdated "cfg = "data"" references (#25434)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+23.sha-c5b594e
+ * @license Angular v7.0.0-beta.5+24.sha-f3b552f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12083,7 +12083,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.5+23.sha-c5b594e');
+var VERSION = new Version('7.0.0-beta.5+24.sha-f3b552f');
 
 /**
  * @license
