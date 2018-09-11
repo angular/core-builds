682568b2375101b10cfd09a6514f8b48bf7e3af2
Igor Minar <igor@angular.io>
2018-09-11 15:10:59 +0000

    86a3be8 docs(ivy): add explanation of LViewData (#25779)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+21.sha-d5bd86a
+ * @license Angular v7.0.0-beta.5+22.sha-86a3be8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14704,7 +14704,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+21.sha-d5bd86a');
+const VERSION = new Version('7.0.0-beta.5+22.sha-86a3be8');
 
 /**
  * @fileoverview added by tsickle
