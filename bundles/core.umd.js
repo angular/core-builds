06368a60e4a6ae78e2db43084110326e58553a48
Igor Minar <igor@angular.io>
2018-09-12 00:24:01 +0000

    00d3666 fix(compiler): Fix look up of entryComponents in AOT Summaries (#24892)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+29.sha-8dc2b11
+ * @license Angular v7.0.0-beta.5+27.sha-00d3666
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12140,7 +12140,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.5+29.sha-8dc2b11');
+    var VERSION = new Version('7.0.0-beta.5+27.sha-00d3666');
 
     /**
      * @license
