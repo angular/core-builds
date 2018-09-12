616959c0cd00744ba63fef70ffcdd96ddbcfc842
Igor Minar <igor@angular.io>
2018-09-12 00:42:48 +0000

    8d7fbb6 docs(router): change description of parameter usage for navigation functions (#21840)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+11.sha-55a1ce7
+ * @license Angular v6.1.7+13.sha-8d7fbb6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14692,7 +14692,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.7+11.sha-55a1ce7');
+const VERSION = new Version('6.1.7+13.sha-8d7fbb6');
 
 /**
  * @license
