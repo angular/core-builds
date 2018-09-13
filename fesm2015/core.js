289c25494d71ead0f53a6f7e34a7d410ad9402bb
Ben Lesh <ben@benlesh.com>
2018-09-13 23:58:06 +0000

    076374b docs: delete old comments from example (#25931)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+12.sha-a31cfc5
+ * @license Angular v6.1.7+15.sha-076374b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.7+12.sha-a31cfc5');
+const VERSION = new Version('6.1.7+15.sha-076374b');
 
 /**
  * @fileoverview added by tsickle
