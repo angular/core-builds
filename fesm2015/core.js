2a42a9ad2e7d5a072d52964c579187be63fc0c1c
Ben Lesh <ben@benlesh.com>
2018-09-14 00:01:07 +0000

    076374b docs: delete old comments from example (#25931)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+12.sha-a31cfc5
+ * @license Angular v6.1.7+15.sha-076374b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.7+12.sha-a31cfc5');
+const VERSION = new Version('6.1.7+15.sha-076374b');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+12.sha-a31cfc5
+ * @license Angular v6.1.7+15.sha-076374b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15012,7 +15012,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.7+12.sha-a31cfc5');
+var VERSION = new Version('6.1.7+15.sha-076374b');
 
 /**
  * @license
