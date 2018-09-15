ae70c3b9fb7f7436d225cca06ebdcaba94bc5d81
Ben Lesh <ben@benlesh.com>
2018-09-15 00:48:47 +0000

    8f81dba docs: fix typo in bootstrapping guide (#25939)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+33.sha-aedebaf
+ * @license Angular v7.0.0-beta.5+34.sha-8f81dba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12044,7 +12044,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.5+33.sha-aedebaf');
+    var VERSION = new Version('7.0.0-beta.5+34.sha-8f81dba');
 
     /**
      * @license
