4c82c14549854588c592045be1e078c0a9d58772
Ben Lesh <ben@benlesh.com>
2018-09-15 01:22:18 +0000

    d9bd860 docs: fix typo in bootstrapping guide (#25939)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+15.sha-076374b
+ * @license Angular v6.1.7+16.sha-d9bd860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14640,7 +14640,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.7+15.sha-076374b');
+const VERSION = new Version('6.1.7+16.sha-d9bd860');
 
 /**
  * @license
