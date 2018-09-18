057c16e95fdf3703d9a3f6001377c025235187f6
Alex Eagle <alexeagle@google.com>
2018-09-18 23:38:29 +0000

    48d7f4e fix(bazel): move bazel managed runtime deps for downstream usage (#25690)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+21.sha-110c81f
+ * @license Angular v6.1.7+36.sha-48d7f4e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.7+21.sha-110c81f');
+var VERSION = new Version('6.1.7+36.sha-48d7f4e');
 
 /**
  * @license
