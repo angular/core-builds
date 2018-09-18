b6d34e26b9c6b4c5f099ffa9cc67624919659b42
Ben Lesh <ben@benlesh.com>
2018-09-18 21:11:16 +0000

    56283ed build: update .nvmrc file to correct node version (#25992)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+50.sha-96ee898
+ * @license Angular v7.0.0-beta.5+46.sha-56283ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12086,7 +12086,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.5+50.sha-96ee898');
+var VERSION = new Version('7.0.0-beta.5+46.sha-56283ed');
 
 /**
  * @license
