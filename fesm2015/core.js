230f7e84499ddd8f872171995a23ad98b1d6b5f0
Ben Lesh <ben@benlesh.com>
2018-09-18 21:41:51 +0000

    c693624 build: update .nvmrc file to correct node version (#25992)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+17.sha-274dc1e
+ * @license Angular v6.1.7+19.sha-c693624
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14713,7 +14713,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.7+17.sha-274dc1e');
+const VERSION = new Version('6.1.7+19.sha-c693624');
 
 /**
  * @license
