66768d42e5bded88ccc0dd03d08cbf0a0c9dc841
Ben Lesh <ben@benlesh.com>
2018-09-18 21:38:40 +0000

    274dc1e build(bazel): update to rules_typescript 0.17.0 & rules_nodejs 0.13.4 (#25977)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+18.sha-6c8791e
+ * @license Angular v6.1.7+17.sha-274dc1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __read, __extends, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.7+18.sha-6c8791e');
+var VERSION = new Version('6.1.7+17.sha-274dc1e');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.7+18.sha-6c8791e
+ * @license Angular v6.1.7+17.sha-274dc1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify } from '@angular/core';
-import { __extends, __decorate, __spread, __values } from 'tslib';
+import { __spread, __values, __decorate, __extends } from 'tslib';
 
 /**
  * @license
