b0e50bd82e899987f0823dc6d43a3419e5bc6d9e
Ben Lesh <ben@benlesh.com>
2018-09-19 18:21:01 +0000

    f91b045 docs(animations): updated animation docs (#24206)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+54.sha-e8bab13
+ * @license Angular v7.0.0-beta.5+55.sha-f91b045
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11998,7 +11998,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.5+54.sha-e8bab13');
+var VERSION = new Version('7.0.0-beta.5+55.sha-f91b045');
 
 /**
  * @license
