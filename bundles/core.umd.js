31048faad9d1bdf6875727587d5f1708da6007a2
Ben Lesh <ben@benlesh.com>
2018-09-19 22:58:50 +0000

    ca1e56d release: cut the v7.0.0-beta.6 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+59.sha-d0e710d
+ * @license Angular v7.0.0-beta.6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12039,7 +12039,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.5+59.sha-d0e710d');
+    var VERSION = new Version('7.0.0-beta.6');
 
     /**
      * @license
