5adc3ab9e178684b81c5c925e10ba5c76ac1e623
Kara Erickson <karakara@google.com>
2018-09-19 23:59:38 +0000

    d7e5bbf feat(compiler-cli): add support to extend `angularCompilerOptions` (#22717)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6
+ * @license Angular v7.0.0-beta.6+6.sha-d7e5bbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14744,7 +14744,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6');
+const VERSION = new Version('7.0.0-beta.6+6.sha-d7e5bbf');
 
 /**
  * @fileoverview added by tsickle
