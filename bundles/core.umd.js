3deb55ab12eb81a55c91c935b9a84c1ab1a0e953
Ben Lesh <ben@benlesh.com>
2018-09-19 19:20:14 +0000

    5099b79 docs: copy edit (#25582)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+37.sha-df878a6
+ * @license Angular v6.1.7+42.sha-5099b79
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8150,7 +8150,7 @@
      * when the `live` property is set to false, and reattaches it when the property
      * becomes true.
      *
-     * <code-example path="core/ts/change_detect/change-detection.ts" region="detach"></code-example>
+     * <code-example path="core/ts/change_detect/change-detection.ts" region="reattach"></code-example>
      *
      */
     var ChangeDetectorRef = /** @class */ (function () {
@@ -9179,11 +9179,9 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /**
-     * Represents an instance of a Component created via a {@link ComponentFactory}.
-     *
-     * `ComponentRef` provides access to the Component Instance as well other objects related to this
-     * Component Instance and allows you to destroy the Component Instance via the {@link #destroy}
-     * method.
+     * Represents a component created by a `ComponentFactory`.
+     * Provides access to the component instance and related objects,
+     * and provides the means of destroying the instance.
      *
      */
     var ComponentRef = /** @class */ (function () {
@@ -14260,15 +14258,20 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /**
-     * Represents an Embedded Template that can be used to instantiate Embedded Views.
+     * Represents an embedded template that can be used to instantiate embedded views.
+     * To instantiate embedded views based on a template, use the `ViewContainerRef`
+     * method `createEmbeddedView()`.
      *
-     * You can access a `TemplateRef`, in two ways. Via a directive placed on a `<ng-template>` element
-     * (or directive prefixed with `*`) and have the `TemplateRef` for this Embedded View injected into
-     * the constructor of the directive using the `TemplateRef` Token. Alternatively you can query for
-     * the `TemplateRef` from a Component or a Directive via {@link Query}.
+     * Access a `TemplateRef` instance by placing a directive on an `<ng-template>`
+     * element (or directive prefixed with `*`). The `TemplateRef` for the embedded view
+     * is injected into the constructor of the directive,
+     * using the `TemplateRef` token.
      *
-     * To instantiate Embedded Views based on a Template, use {@link ViewContainerRef#
-     * createEmbeddedView}, which will create the View and attach it to the View Container.
+     * You can also use a `Query` to find a `TemplateRef` associated with
+     * a component or a directive.
+     *
+     * @see `ViewContainerRef`
+     * @see [Navigate the Component Tree with DI](guide/dependency-injection-navtree)
      *
      */
     var TemplateRef$1 = /** @class */ (function () {
@@ -14285,21 +14288,17 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /**
-     * Represents a container where one or more Views can be attached.
+     * Represents a container where one or more views can be attached to a component.
      *
-     * The container can contain two kinds of Views. Host Views, created by instantiating a
-     * {@link Component} via {@link #createComponent}, and Embedded Views, created by instantiating an
-     * {@link TemplateRef Embedded Template} via {@link #createEmbeddedView}.
+     * Can contain *host views* (created by instantiating a
+     * component with the `createComponent()` method), and *embedded views*
+     * (created by instantiating a `TemplateRef` with the `createEmbeddedView()` method).
      *
-     * The location of the View Container within the containing View is specified by the Anchor
-     * `element`. Each View Container can have only one Anchor Element and each Anchor Element can only
-     * have a single View Container.
+     * A view container instance can contain other view containers,
+     * creating a [view hierarchy](guide/glossary#view-tree).
      *
-     * Root elements of Views attached to this container become siblings of the Anchor Element in
-     * the Rendered View.
-     *
-     * To access a `ViewContainerRef` of an Element, you can either place a {@link Directive} injected
-     * with `ViewContainerRef` on the Element, or you obtain it via a {@link ViewChild} query.
+     * @see `ComponentRef`
+     * @see `EmbeddedViewRef`
      *
      */
     var ViewContainerRef$1 = /** @class */ (function () {
@@ -15142,7 +15141,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.7+37.sha-df878a6');
+    var VERSION = new Version('6.1.7+42.sha-5099b79');
 
     /**
      * @license
@@ -15384,6 +15383,14 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    /**
+     * Represents an Angular [view](guide/glossary#view),
+     * specifically the [host view](guide/glossary#view-tree) that is defined by a component.
+     * Also serves as the base class
+     * that adds destroy methods for [embedded views](guide/glossary#view-tree).
+     *
+     * @see `EmbeddedViewRef`
+     */
     var ViewRef$1 = /** @class */ (function (_super) {
         __extends(ViewRef, _super);
         function ViewRef() {
@@ -15392,21 +15399,21 @@
         return ViewRef;
     }(ChangeDetectorRef));
     /**
-     * Represents an Angular View.
+     * Represents an Angular [view](guide/glossary#view) in a view container.
+     * An [embedded view](guide/glossary#view-tree) can be referenced from a component
+     * other than the hosting component whose template defines it, or it can be defined
+     * independently by a `TemplateRef`.
      *
-     * <!-- TODO: move the next two paragraphs to the dev guide -->
-     * A View is a fundamental building block of the application UI. It is the smallest grouping of
-     * Elements which are created and destroyed together.
+     * Properties of elements in a view can change, but the structure (number and order) of elements in
+     * a view cannot. Change the structure of elements by inserting, moving, or
+     * removing nested views in a view container.
      *
-     * Properties of elements in a View can change, but the structure (number and order) of elements in
-     * a View cannot. Changing the structure of Elements can only be done by inserting, moving or
-     * removing nested Views via a `ViewContainerRef`. Each View can contain many View Containers.
-     * <!-- /TODO -->
+     * @see `ViewContainerRef`
      *
      * @usageNotes
-     * ### Example
      *
-     * Given this template...
+     * The following template breaks down into two separate `TemplateRef` instances,
+     * an outer one and an inner one.
      *
      * ```
      * Count: {{items.length}}
@@ -15415,9 +15422,7 @@
      * </ul>
      * ```
      *
-     * We have two `TemplateRef`s:
-     *
-     * Outer `TemplateRef`:
+     * This is the outer `TemplateRef`:
      *
      * ```
      * Count: {{items.length}}
@@ -15426,15 +15431,13 @@
      * </ul>
      * ```
      *
-     * Inner `TemplateRef`:
+     * This is the inner `TemplateRef`:
      *
      * ```
      *   <li>{{item}}</li>
      * ```
      *
-     * Notice that the original template is broken down into two separate `TemplateRef`s.
-     *
-     * The outer/inner `TemplateRef`s are then assembled into views like so:
+     * The outer and inner `TemplateRef` instances are assembled into views as follows:
      *
      * ```
      * <!-- ViewRef: outer-0 -->
@@ -15776,6 +15779,10 @@
     function _localeFactory(locale) {
         return locale || 'en-US';
     }
+    /**
+     * A built-in [dependency injection token](guide/glossary#di-token)
+     * that is used to configure the root injector for bootstrapping.
+     */
     var APPLICATION_MODULE_PROVIDERS = [
         {
             provide: ApplicationRef,
@@ -15798,8 +15805,12 @@
         },
     ];
     /**
-     * This module includes the providers of @angular/core that are needed
-     * to bootstrap components via `ApplicationRef`.
+     * Configures the root injector for an app with
+     * providers of `@angular/core` dependencies that `ApplicationRef` needs
+     * to bootstrap components.
+     *
+     * Re-exported by `BrowserModule`, which is included automatically in the root
+     * `AppModule` when you create a new app with the CLI `new` command.
      *
      * @experimental
      */
