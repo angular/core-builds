fa085e105608132ef3392afb6a598bf050b6589d
Kara Erickson <karakara@google.com>
2018-09-20 02:07:10 +0000

    41ac58a docs: copy-edit (#25732)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+6.sha-d7e5bbf
+ * @license Angular v7.0.0-beta.6+12.sha-41ac58a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11710,7 +11710,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.6+6.sha-d7e5bbf');
+const VERSION = new Version('7.0.0-beta.6+12.sha-41ac58a');
 
 /**
  * @license
