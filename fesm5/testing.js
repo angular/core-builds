8bc0f7ce5f857a111ef6d2712f40b9673561244d
Kara Erickson <karakara@google.com>
2018-09-20 01:43:39 +0000

    f0d7054 fix(forms): remove forms file from patch (#26025)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.8+3.sha-c7a6adc
+ * @license Angular v6.1.8+8.sha-f0d7054
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify } from '@angular/core';
-import { __spread, __values, __decorate, __extends } from 'tslib';
+import { __extends, __decorate, __spread, __values } from 'tslib';
 
 /**
  * @license
