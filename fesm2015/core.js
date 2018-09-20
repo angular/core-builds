68844758b663708c2f8328d7767637ac437ab559
Kara Erickson <karakara@google.com>
2018-09-20 00:44:50 +0000

    c7a6adc docs(forms): update form apis based on review feedback (#25724)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8
+ * @license Angular v6.1.8+3.sha-c7a6adc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18560,7 +18560,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.8');
+const VERSION = new Version('6.1.8+3.sha-c7a6adc');
 
 /**
  * @fileoverview added by tsickle
