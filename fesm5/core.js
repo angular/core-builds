4ac8fc7134f66f8d4639ea1b030ad9f0946b8562
Kara Erickson <karakara@google.com>
2018-09-20 18:21:47 +0000

    f1cb460 ci: only run `aio_preview` job on PR builds (#26030)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8+10.sha-0ec925b
+ * @license Angular v6.1.8+11.sha-f1cb460
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __read, __extends, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.8+10.sha-0ec925b');
+var VERSION = new Version('6.1.8+11.sha-f1cb460');
 
 /**
  * @license
