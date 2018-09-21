e8b829de82696f2926a6e4f54e68a649d5154f2b
Kara Erickson <karakara@google.com>
2018-09-21 18:58:32 +0000

    05d1b84 fix(docs-infra): ensure that only search is removed from URL on click (#26056)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8+12.sha-61b4c26
+ * @license Angular v6.1.8+18.sha-05d1b84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15032,7 +15032,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.8+12.sha-61b4c26');
+var VERSION = new Version('6.1.8+18.sha-05d1b84');
 
 /**
  * @license
