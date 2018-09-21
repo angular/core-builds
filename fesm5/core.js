2609c8e6d14df68fbd903c40e9b95909295da288
Kara Erickson <karakara@google.com>
2018-09-21 18:05:25 +0000

    9931bd7 build(docs-infra): do not include license comment in first API doc (#26050)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+33.sha-026b60c
+ * @license Angular v7.0.0-beta.6+30.sha-9931bd7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12069,7 +12069,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.6+33.sha-026b60c');
+var VERSION = new Version('7.0.0-beta.6+30.sha-9931bd7');
 
 /**
  * @license
