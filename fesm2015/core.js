e7953593dc36b09c51a56b57b6c2287815695595
Kara Erickson <karakara@google.com>
2018-09-21 18:50:53 +0000

    a135f48 docs: add Sajee to contributors (#26028)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+11.sha-f1cb460
+ * @license Angular v6.1.8+13.sha-a135f48
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18560,7 +18560,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.8+11.sha-f1cb460');
+const VERSION = new Version('6.1.8+13.sha-a135f48');
 
 /**
  * @fileoverview added by tsickle
