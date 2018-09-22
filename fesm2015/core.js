a6297a7f5040a9469b121502b94f540e1571a7c9
Kara Erickson <karakara@google.com>
2018-09-22 01:37:34 +0000

    37f3b92 build(docs-infra): sort NgModule exports by id (#26051)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+14.sha-ee9f0b5
+ * @license Angular v6.1.8+20.sha-37f3b92
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.8+14.sha-ee9f0b5');
+const VERSION = new Version('6.1.8+20.sha-37f3b92');
 
 /**
  * @license
