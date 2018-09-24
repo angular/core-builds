6ed790f727764e90260131bef86103996eceb212
Kara Erickson <karakara@google.com>
2018-09-24 16:55:57 +0000

    47eb212 docs: fix Sajee info in contributors (#26063)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+51.sha-b8422b4
+ * @license Angular v7.0.0-beta.6+52.sha-47eb212
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11713,7 +11713,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.6+51.sha-b8422b4');
+const VERSION = new Version('7.0.0-beta.6+52.sha-47eb212');
 
 /**
  * @license
