44d9d515af33f3a67ef54336d42b14ca343d6f27
Kara Erickson <karakara@google.com>
2018-09-24 19:21:20 +0000

    23ec88e refactor(ivy): remove unreferenced utils file (#26076)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+55.sha-2bd767c
+ * @license Angular v7.0.0-beta.6+56.sha-23ec88e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12004,7 +12004,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.6+55.sha-2bd767c');
+var VERSION = new Version('7.0.0-beta.6+56.sha-23ec88e');
 
 /**
  * @license
