eed2823d52958d18c5e6ae7fe8df23bade025aff
Kara Erickson <karakara@google.com>
2018-09-26 02:25:08 +0000

    c7e2930 docs: fix issues related to tutorial. (#24445)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+60.sha-6a62ed2
+ * @license Angular v7.0.0-beta.6+61.sha-c7e2930
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11965,7 +11965,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.6+60.sha-6a62ed2');
+    var VERSION = new Version('7.0.0-beta.6+61.sha-c7e2930');
 
     /**
      * @license
