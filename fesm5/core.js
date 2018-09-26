a4a58df92494760dd8e84f797aa92d6e6430ee6c
Kara Erickson <karakara@google.com>
2018-09-26 02:32:13 +0000

    d191085 docs: cleanup minor changes for forms overview (#25663)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+61.sha-c7e2930
+ * @license Angular v7.0.0-beta.6+79.sha-d191085
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -11926,7 +11926,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.6+61.sha-c7e2930');
+var VERSION = new Version('7.0.0-beta.6+79.sha-d191085');
 
 /**
  * @license
