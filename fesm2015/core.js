e8d76c0fb862873666f852fa1939cea059c15544
Kara Erickson <karakara@google.com>
2018-09-26 23:56:17 +0000

    5881f34 docs: firefox web components info (#26118)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9
+ * @license Angular v6.1.9+1.sha-5881f34
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1950,7 +1950,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.9');
+const VERSION = new Version('6.1.9+1.sha-5881f34');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9
+ * @license Angular v6.1.9+1.sha-5881f34
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.9');
+var VERSION = new Version('6.1.9+1.sha-5881f34');
 
 /**
  * @license
