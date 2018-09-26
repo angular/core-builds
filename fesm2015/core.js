ee5e0cfa616ceac53ab09d62663808a85fb862e6
Kara Erickson <karakara@google.com>
2018-09-26 02:30:48 +0000

    d191085 docs: cleanup minor changes for forms overview (#25663)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+61.sha-c7e2930
+ * @license Angular v7.0.0-beta.6+79.sha-d191085
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11644,7 +11644,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.6+61.sha-c7e2930');
+const VERSION = new Version('7.0.0-beta.6+79.sha-d191085');
 
 /**
  * @license
