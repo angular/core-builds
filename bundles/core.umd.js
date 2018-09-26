70d728514f9b90b122b8419ca76c74130ea25599
Kara Erickson <karakara@google.com>
2018-09-26 21:14:53 +0000

    acffa22 release: cut the v6.1.9 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+65.sha-159e8b4
+ * @license Angular v6.1.9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15138,7 +15138,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.8+65.sha-159e8b4');
+    var VERSION = new Version('6.1.9');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+65.sha-159e8b4
+ * @license Angular v6.1.9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.8+65.sha-159e8b4');
+const VERSION = new Version('6.1.9');
 
 /**
  * @license
