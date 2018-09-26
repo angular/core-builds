0fc412f8e4d7ec113db4142043ac5eb44504daab
Kara Erickson <karakara@google.com>
2018-09-26 23:07:41 +0000

    4cff5b2 release: cut the v7.0.0-beta.7 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+18.sha-a01acec
+ * @license Angular v7.0.0-beta.7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12032,7 +12032,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.7+18.sha-a01acec');
+    var VERSION = new Version('7.0.0-beta.7');
 
     /**
      * @license
