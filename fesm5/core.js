9bbf023928e2978e2942c43f3aa996e2b879e4cb
Kara Erickson <karakara@google.com>
2018-09-26 23:03:09 +0000

    a01acec fix(docs-infra): use correct parameters for paginated requests to GitHub (#25671)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+89.sha-4597582
+ * @license Angular v7.0.0-beta.7+18.sha-a01acec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12099,7 +12099,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.6+89.sha-4597582');
+var VERSION = new Version('7.0.0-beta.7+18.sha-a01acec');
 
 /**
  * @license
