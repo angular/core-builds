c0452b50620a654dd587b988a30a16d007572687
Kara Erickson <karakara@google.com>
2018-09-26 03:22:43 +0000

    559c647 docs: fix issues related to tutorial. (#24445)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+59.sha-c8817f3
+ * @license Angular v6.1.8+41.sha-559c647
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18557,7 +18557,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.8+59.sha-c8817f3');
+const VERSION = new Version('6.1.8+41.sha-559c647');
 
 /**
  * @fileoverview added by tsickle
