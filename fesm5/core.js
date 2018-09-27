cd7ad7a680f1275e2fb024af4e521a14e3df1ec7
Igor Minar <igor@angular.io>
2018-09-27 00:41:19 +0000

    ffc6e19 build: RxJS updated to 6.3 (#26087)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.7+1.sha-e1990a5
+ * @license Angular v7.0.0-beta.7+19.sha-ffc6e19
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read, __assign, __values } from 'tslib';
+import { __extends, __spread, __assign, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -11993,7 +11993,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-beta.7+1.sha-e1990a5');
+var VERSION = new Version('7.0.0-beta.7+19.sha-ffc6e19');
 
 /**
  * @license
