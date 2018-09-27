aa0119bd3941724d87c1b9aaab5af925a3c10ffc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 23:16:58 +0000

    f455518 docs: integrate cli doc from wiki into main doc (#25776)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+47.sha-7cf5807
+ * @license Angular v7.0.0-beta.7+48.sha-f455518
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14660,7 +14660,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.7+47.sha-7cf5807');
+const VERSION = new Version('7.0.0-beta.7+48.sha-f455518');
 
 /**
  * @fileoverview added by tsickle
