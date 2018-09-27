b20ca7c2ca40f4479b02ae42a43663ecbf5b0396
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 21:42:43 +0000

    9523991 refactor(router): cleanup to navigation stream for readability and documentation (#25740)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+35.sha-4d544bc
+ * @license Angular v7.0.0-beta.7+46.sha-9523991
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12220,7 +12220,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.7+35.sha-4d544bc');
+    var VERSION = new Version('7.0.0-beta.7+46.sha-9523991');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+35.sha-4d544bc
+ * @license Angular v7.0.0-beta.7+46.sha-9523991
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14818,7 +14818,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.7+35.sha-4d544bc');
+const VERSION = new Version('7.0.0-beta.7+46.sha-9523991');
 
 /**
  * @fileoverview added by tsickle
