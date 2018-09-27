82c85fb4ae80d6d998cc312bed2b55cb1246317a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 19:55:19 +0000

    63b795a refactor(ivy): make sure that test bed symbols are imported from ivy_switch (#26121)

--- src/ivy_switch/compiler/ivy_switch_on.d.ts
+++ src/ivy_switch/compiler/ivy_switch_on.d.ts
@@ -7,7 +7,7 @@
  */
 import { compileComponent, compileDirective } from '../../render3/jit/directive';
 import { compileInjectable } from '../../render3/jit/injectable';
-import { compileNgModule } from '../../render3/jit/module';
+import { compileNgModule, compileNgModuleDefs, patchComponentDefWithScope } from '../../render3/jit/module';
 import { compilePipe } from '../../render3/jit/pipe';
 export declare const ivyEnabled: boolean;
 export declare const R3_COMPILE_COMPONENT: typeof compileComponent;
@@ -15,3 +15,5 @@ export declare const R3_COMPILE_DIRECTIVE: typeof compileDirective;
 export declare const R3_COMPILE_INJECTABLE: typeof compileInjectable;
 export declare const R3_COMPILE_NGMODULE: typeof compileNgModule;
 export declare const R3_COMPILE_PIPE: typeof compilePipe;
+export declare const R3_COMPILE_NGMODULE_DEFS: typeof compileNgModuleDefs;
+export declare const R3_PATCH_COMPONENT_DEF_WTIH_SCOPE: typeof patchComponentDefWithScope;
