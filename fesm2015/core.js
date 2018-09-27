e3618abacfb2608d079c0001345d9c4252910c4d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 22:41:51 +0000

    9523991 refactor(router): cleanup to navigation stream for readability and documentation (#25740)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+30.sha-63b795a
+ * @license Angular v7.0.0-beta.7+46.sha-9523991
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14660,7 +14660,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.7+30.sha-63b795a');
+const VERSION = new Version('7.0.0-beta.7+46.sha-9523991');
 
 /**
  * @fileoverview added by tsickle
