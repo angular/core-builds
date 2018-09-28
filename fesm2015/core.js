d6f37e3c715825bc1162a55656817854413ddbb7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 23:18:27 +0000

    2ad1bb4 release: cut the v7.0.0-rc.0 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.0.0-beta.7+51.sha-b807106
+ * @license Angular v7.0.0-rc.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 import { share } from 'rxjs/operators';
 
 /**
@@ -11772,7 +11772,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-beta.7+51.sha-b807106');
+const VERSION = new Version('7.0.0-rc.0');
 
 /**
  * @license
