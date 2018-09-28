cc84f8ea29137b986e3c90341cbc4f1f385bfda1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 17:19:10 +0000

    b807106 build: use separate tags for ivy builds in `publish-build-artifacts.sh` (#26159)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+50.sha-86e6a20
+ * @license Angular v7.0.0-beta.7+51.sha-b807106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12114,7 +12114,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-beta.7+50.sha-86e6a20');
+    var VERSION = new Version('7.0.0-beta.7+51.sha-b807106');
 
     /**
      * @license
