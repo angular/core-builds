81fc1052eab2a75a0059f26f974145c90ad21777
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 21:23:02 +0000

    66c2d08 ci(docs-infra): run the  script in the correct folder

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+36.sha-c1bf82a
+ * @license Angular v6.1.9+37.sha-66c2d08
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.9+36.sha-c1bf82a');
+const VERSION = new Version('6.1.9+37.sha-66c2d08');
 
 /**
  * @license
