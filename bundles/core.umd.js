a69737370e9a81e7933f81afdb04e13e188ae29e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 17:22:16 +0000

    68fadd9 refactor(ivy): replace LNode.nodeInjector with TNode.injectorIndex (#26177)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+23.sha-a2878b0
+ * @license Angular v7.0.0-rc.0+1.sha-68fadd9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3505,30 +3505,6 @@
         return 0;
     }
 
-    var EMPTY_ARR = [];
-    var EMPTY_OBJ = {};
-    function createEmptyStylingContext(element, sanitizer, initialStylingValues) {
-        return [
-            element || null, null, sanitizer || null, initialStylingValues || [null], 0, 0, null, null
-        ];
-    }
-    function getOrCreatePlayerContext(target, context) {
-        context = context || getContext(target);
-        if (ngDevMode && !context) {
-            throw new Error('Only elements that exist in an Angular application can be used for player access');
-        }
-        var lViewData = context.lViewData, lNodeIndex = context.lNodeIndex;
-        var value = lViewData[lNodeIndex];
-        var stylingContext = value;
-        if (!Array.isArray(value)) {
-            stylingContext = lViewData[lNodeIndex] = createEmptyStylingContext(value);
-        }
-        return stylingContext[1 /* PlayerContext */] || allocPlayerContext(stylingContext);
-    }
-    function allocPlayerContext(data) {
-        return data[1 /* PlayerContext */] = [];
-    }
-
     /**
      * @license
      * Copyright Google Inc. All Rights Reserved.
@@ -3547,6 +3523,9 @@
         var context = templateStyleContext.slice();
         context[0 /* ElementPosition */] = lElement;
         return context;
+    }
+    function createEmptyStylingContext(element, sanitizer, initialStylingValues) {
+        return [element || null, null, sanitizer || null, initialStylingValues || [null], 0, 0, null];
     }
     /**
      * Creates a styling context template where styling information is stored.
@@ -3627,14 +3606,14 @@
         var classNamesIndexStart = styleProps.length;
         var totalProps = styleProps.length + classNames.length;
         // *2 because we are filling for both single and multi style spaces
-        var maxLength = totalProps * 3 /* Size */ * 2 + 8 /* SingleStylesStartPosition */;
+        var maxLength = totalProps * 3 /* Size */ * 2 + 7 /* SingleStylesStartPosition */;
         // we need to fill the array from the start so that we can access
         // both the multi and the single array positions in the same loop block
-        for (var i = 8 /* SingleStylesStartPosition */; i < maxLength; i++) {
+        for (var i = 7 /* SingleStylesStartPosition */; i < maxLength; i++) {
             context.push(null);
         }
-        var singleStart = 8 /* SingleStylesStartPosition */;
-        var multiStart = totalProps * 3 /* Size */ + 8 /* SingleStylesStartPosition */;
+        var singleStart = 7 /* SingleStylesStartPosition */;
+        var multiStart = totalProps * 3 /* Size */ + 7 /* SingleStylesStartPosition */;
         // fill single and multi-level styles
         for (var i = 0; i < totalProps; i++) {
             var isClassBased_1 = i >= classNamesIndexStart;
@@ -3658,6 +3637,8 @@
         setContextDirty(context, initialStylingValues.length > 1);
         return context;
     }
+    var EMPTY_ARR = [];
+    var EMPTY_OBJ = {};
     /**
      * Sets and resolves all `multi` styling on an `StylingContext` so that they can be
      * applied to the element once `renderStyling` is called.
@@ -3672,32 +3653,29 @@
      * @param styles The key/value map of CSS styles that will be used for the update.
      */
     function updateStylingMap(context, classes, styles) {
-        styles = styles || null;
-        // early exit (this is what's done to avoid using ctx.bind() to cache the value)
-        var ignoreAllClassUpdates = classes === context[6 /* PreviousMultiClassValue */];
-        var ignoreAllStyleUpdates = styles === context[7 /* PreviousMultiStyleValue */];
-        if (ignoreAllClassUpdates && ignoreAllStyleUpdates)
-            return;
         var classNames = EMPTY_ARR;
         var applyAllClasses = false;
+        var ignoreAllClassUpdates = false;
         // each time a string-based value pops up then it shouldn't require a deep
         // check of what's changed.
-        if (!ignoreAllClassUpdates) {
-            context[6 /* PreviousMultiClassValue */] = classes;
-            if (typeof classes == 'string') {
+        if (typeof classes == 'string') {
+            var cachedClassString = context[6 /* CachedCssClassString */];
+            if (cachedClassString && cachedClassString === classes) {
+                ignoreAllClassUpdates = true;
+            }
+            else {
+                context[6 /* CachedCssClassString */] = classes;
                 classNames = classes.split(/\s+/);
                 // this boolean is used to avoid having to create a key/value map of `true` values
                 // since a classname string implies that all those classes are added
                 applyAllClasses = true;
             }
-            else {
-                classNames = classes ? Object.keys(classes) : EMPTY_ARR;
-            }
+        }
+        else {
+            classNames = classes ? Object.keys(classes) : EMPTY_ARR;
+            context[6 /* CachedCssClassString */] = null;
         }
         classes = (classes || EMPTY_OBJ);
-        if (!ignoreAllStyleUpdates) {
-            context[7 /* PreviousMultiStyleValue */] = styles;
-        }
         var styleProps = styles ? Object.keys(styles) : EMPTY_ARR;
         styles = styles || EMPTY_OBJ;
         var classesStartIndex = styleProps.length;
@@ -3711,10 +3689,9 @@
         // are off-balance then they will be dealt in another loop after this one
         while (ctxIndex < context.length && propIndex < propLimit) {
             var isClassBased_2 = propIndex >= classesStartIndex;
-            var processValue = (!isClassBased_2 && !ignoreAllStyleUpdates) || (isClassBased_2 && !ignoreAllClassUpdates);
             // when there is a cache-hit for a string-based class then we should
             // avoid doing any work diffing any of the changes
-            if (processValue) {
+            if (!ignoreAllClassUpdates || !isClassBased_2) {
                 var adjustedPropIndex = isClassBased_2 ? propIndex - classesStartIndex : propIndex;
                 var newProp = isClassBased_2 ? classNames[adjustedPropIndex] : styleProps[adjustedPropIndex];
                 var newValue = isClassBased_2 ? (applyAllClasses ? true : classes[newProp]) : styles[newProp];
@@ -3727,7 +3704,7 @@
                         var initialValue = getInitialValue(context, flag);
                         // there is no point in setting this to dirty if the previously
                         // rendered value was being referenced by the initial style (or null)
-                        if (hasValueChanged(flag, initialValue, newValue)) {
+                        if (initialValue !== newValue) {
                             setDirty(context, ctxIndex, true);
                             dirty = true;
                         }
@@ -3740,10 +3717,10 @@
                         var valueToCompare = getValue(context, indexOfEntry);
                         var flagToCompare = getPointers(context, indexOfEntry);
                         swapMultiContextEntries(context, ctxIndex, indexOfEntry);
-                        if (hasValueChanged(flagToCompare, valueToCompare, newValue)) {
+                        if (valueToCompare !== newValue) {
                             var initialValue = getInitialValue(context, flagToCompare);
                             setValue(context, ctxIndex, newValue);
-                            if (hasValueChanged(flagToCompare, initialValue, newValue)) {
+                            if (initialValue !== newValue) {
                                 setDirty(context, ctxIndex, true);
                                 dirty = true;
                             }
@@ -3766,15 +3743,14 @@
         while (ctxIndex < context.length) {
             var flag = getPointers(context, ctxIndex);
             var isClassBased_3 = (flag & 2 /* Class */) === 2 /* Class */;
-            var processValue = (!isClassBased_3 && !ignoreAllStyleUpdates) || (isClassBased_3 && !ignoreAllClassUpdates);
-            if (processValue) {
-                var value = getValue(context, ctxIndex);
-                var doRemoveValue = valueExists(value, isClassBased_3);
-                if (doRemoveValue) {
-                    setDirty(context, ctxIndex, true);
-                    setValue(context, ctxIndex, null);
-                    dirty = true;
-                }
+            if (ignoreAllClassUpdates && isClassBased_3)
+                break;
+            var value = getValue(context, ctxIndex);
+            var doRemoveValue = valueExists(value, isClassBased_3);
+            if (doRemoveValue) {
+                setDirty(context, ctxIndex, true);
+                setValue(context, ctxIndex, null);
+                dirty = true;
             }
             ctxIndex += 3 /* Size */;
         }
@@ -3784,16 +3760,15 @@
         var sanitizer = getStyleSanitizer(context);
         while (propIndex < propLimit) {
             var isClassBased_4 = propIndex >= classesStartIndex;
-            var processValue = (!isClassBased_4 && !ignoreAllStyleUpdates) || (isClassBased_4 && !ignoreAllClassUpdates);
-            if (processValue) {
-                var adjustedPropIndex = isClassBased_4 ? propIndex - classesStartIndex : propIndex;
-                var prop = isClassBased_4 ? classNames[adjustedPropIndex] : styleProps[adjustedPropIndex];
-                var value = isClassBased_4 ? (applyAllClasses ? true : classes[prop]) : styles[prop];
-                var flag = prepareInitialFlag(prop, isClassBased_4, sanitizer) | 1 /* Dirty */;
-                context.push(flag, prop, value);
-                dirty = true;
-            }
+            if (ignoreAllClassUpdates && isClassBased_4)
+                break;
+            var adjustedPropIndex = isClassBased_4 ? propIndex - classesStartIndex : propIndex;
+            var prop = isClassBased_4 ? classNames[adjustedPropIndex] : styleProps[adjustedPropIndex];
+            var value = isClassBased_4 ? (applyAllClasses ? true : classes[prop]) : styles[prop];
+            var flag = prepareInitialFlag(prop, isClassBased_4, sanitizer) | 1 /* Dirty */;
+            context.push(flag, prop, value);
             propIndex++;
+            dirty = true;
         }
         if (dirty) {
             setContextDirty(context, true);
@@ -3814,7 +3789,7 @@
      * @param value The CSS style value that will be assigned
      */
     function updateStyleProp(context, index, value) {
-        var singleIndex = 8 /* SingleStylesStartPosition */ + index * 3 /* Size */;
+        var singleIndex = 7 /* SingleStylesStartPosition */ + index * 3 /* Size */;
         var currValue = getValue(context, singleIndex);
         var currFlag = getPointers(context, singleIndex);
         // didn't change ... nothing to make a note of
@@ -3824,7 +3799,7 @@
             var indexForMulti = getMultiOrSingleIndex(currFlag);
             // if the value is the same in the multi-area then there's no point in re-assembling
             var valueForMulti = getValue(context, indexForMulti);
-            if (!valueForMulti || hasValueChanged(currFlag, valueForMulti, value)) {
+            if (!valueForMulti || valueForMulti !== value) {
                 var multiDirty = false;
                 var singleDirty = true;
                 var isClassBased_5 = (currFlag & 2 /* Class */) === 2 /* Class */;
@@ -3875,7 +3850,7 @@
             var native = context[0 /* ElementPosition */].native;
             var multiStartIndex = getMultiStartIndex(context);
             var styleSanitizer = getStyleSanitizer(context);
-            for (var i = 8 /* SingleStylesStartPosition */; i < context.length; i += 3 /* Size */) {
+            for (var i = 7 /* SingleStylesStartPosition */; i < context.length; i += 3 /* Size */) {
                 // there is no point in rendering styles that have not changed on screen
                 if (isDirty(context, i)) {
                     var prop = getProp(context, i);
@@ -3972,7 +3947,7 @@
         }
     }
     function setDirty(context, index, isDirtyYes) {
-        var adjustedIndex = index >= 8 /* SingleStylesStartPosition */ ? (index + 0 /* FlagsOffset */) : index;
+        var adjustedIndex = index >= 7 /* SingleStylesStartPosition */ ? (index + 0 /* FlagsOffset */) : index;
         if (isDirtyYes) {
             context[adjustedIndex] |= 1 /* Dirty */;
         }
@@ -3981,15 +3956,15 @@
         }
     }
     function isDirty(context, index) {
-        var adjustedIndex = index >= 8 /* SingleStylesStartPosition */ ? (index + 0 /* FlagsOffset */) : index;
+        var adjustedIndex = index >= 7 /* SingleStylesStartPosition */ ? (index + 0 /* FlagsOffset */) : index;
         return (context[adjustedIndex] & 1 /* Dirty */) == 1 /* Dirty */;
     }
     function isClassBased(context, index) {
-        var adjustedIndex = index >= 8 /* SingleStylesStartPosition */ ? (index + 0 /* FlagsOffset */) : index;
+        var adjustedIndex = index >= 7 /* SingleStylesStartPosition */ ? (index + 0 /* FlagsOffset */) : index;
         return (context[adjustedIndex] & 2 /* Class */) == 2 /* Class */;
     }
     function isSanitizable(context, index) {
-        var adjustedIndex = index >= 8 /* SingleStylesStartPosition */ ? (index + 0 /* FlagsOffset */) : index;
+        var adjustedIndex = index >= 7 /* SingleStylesStartPosition */ ? (index + 0 /* FlagsOffset */) : index;
         return (context[adjustedIndex] & 4 /* Sanitize */) == 4 /* Sanitize */;
     }
     function pointers(configFlag, staticIndex, dynamicIndex) {
@@ -4005,7 +3980,7 @@
     }
     function getMultiOrSingleIndex(flag) {
         var index = (flag >> (14 /* BitCountSize */ + 3 /* BitCountSize */)) & 16383 /* BitMask */;
-        return index >= 8 /* SingleStylesStartPosition */ ? index : -1;
+        return index >= 7 /* SingleStylesStartPosition */ ? index : -1;
     }
     function getMultiStartIndex(context) {
         return getMultiOrSingleIndex(context[4 /* MasterFlagPosition */]);
@@ -11272,7 +11247,7 @@
         'ɵreference': reference,
         'ɵelementStyling': elementStyling,
         'ɵelementStylingMap': elementStylingMap,
-        'ɵelementStyleProp': elementStyleProp,
+        'ɵelementStylingProp': elementStyleProp,
         'ɵelementStylingApply': elementStylingApply,
         'ɵtemplate': template,
         'ɵtext': text,
@@ -12257,7 +12232,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.0+23.sha-a2878b0');
+    var VERSION = new Version('7.0.0-rc.0+1.sha-68fadd9');
 
     /**
      * @license
@@ -20774,7 +20749,7 @@
      */
     function addPlayer(ref, player) {
         var elementContext = getContext(ref);
-        var animationContext = getOrCreatePlayerContext(elementContext.native, elementContext);
+        var animationContext = getOrCreateAnimationContext(elementContext.native, elementContext);
         animationContext.push(player);
         player.addEventListener(200 /* Destroyed */, function () {
             var index = animationContext.indexOf(player);
@@ -20795,7 +20770,23 @@
         }
     }
     function getPlayers(ref) {
-        return getOrCreatePlayerContext(ref);
+        return getOrCreateAnimationContext(ref);
+    }
+    function getOrCreateAnimationContext(target, context) {
+        context = context || getContext(target);
+        if (ngDevMode && !context) {
+            throw new Error('Only elements that exist in an Angular application can be used for animations');
+        }
+        var lViewData = context.lViewData, lNodeIndex = context.lNodeIndex;
+        var value = lViewData[lNodeIndex];
+        var stylingContext = value;
+        if (!Array.isArray(value)) {
+            stylingContext = lViewData[lNodeIndex] = createEmptyStylingContext(value);
+        }
+        return stylingContext[1 /* AnimationContext */] || allocAnimationContext(stylingContext);
+    }
+    function allocAnimationContext(data) {
+        return data[1 /* AnimationContext */] = [];
     }
 
     /**
@@ -20860,20 +20851,20 @@
     exports.ɵangular_packages_core_core_r = endTimeRange;
     exports.ɵangular_packages_core_core_p = leave;
     exports.ɵangular_packages_core_core_q = startTimeRange;
+    exports.ɵangular_packages_core_core_ba = getOrCreateAnimationContext;
     exports.ɵangular_packages_core_core_w = getOrCreateInjectable;
     exports.ɵangular_packages_core_core_v = getOrCreateNodeInjector;
-    exports.ɵangular_packages_core_core_bd = NG_INJECTABLE_DEF;
+    exports.ɵangular_packages_core_core_be = NG_INJECTABLE_DEF;
     exports.ɵangular_packages_core_core_y = _getViewData;
     exports.ɵangular_packages_core_core_z = bindingUpdated;
     exports.ɵangular_packages_core_core_x = getPreviousOrParentTNode;
-    exports.ɵangular_packages_core_core_bh = getOrCreatePlayerContext;
-    exports.ɵangular_packages_core_core_bg = loadInternal;
-    exports.ɵangular_packages_core_core_ba = createElementRef;
-    exports.ɵangular_packages_core_core_bb = createTemplateRef;
-    exports.ɵangular_packages_core_core_bc = createViewRef;
+    exports.ɵangular_packages_core_core_bh = loadInternal;
+    exports.ɵangular_packages_core_core_bb = createElementRef;
+    exports.ɵangular_packages_core_core_bc = createTemplateRef;
+    exports.ɵangular_packages_core_core_bd = createViewRef;
     exports.ɵangular_packages_core_core_a = makeParamDecorator;
     exports.ɵangular_packages_core_core_b = makePropDecorator;
-    exports.ɵangular_packages_core_core_be = getClosureSafeProperty;
+    exports.ɵangular_packages_core_core_bf = getClosureSafeProperty;
     exports.ɵangular_packages_core_core_s = _def;
     exports.ɵangular_packages_core_core_t = DebugRendererFactory2;
     exports.ɵangular_packages_core_core_u = DebugContext;
@@ -21091,7 +21082,7 @@
     exports.ɵelementAttribute = elementAttribute;
     exports.ɵelementStyling = elementStyling;
     exports.ɵelementStylingMap = elementStylingMap;
-    exports.ɵelementStyleProp = elementStyleProp;
+    exports.ɵelementStylingProp = elementStyleProp;
     exports.ɵelementStylingApply = elementStylingApply;
     exports.ɵelementClassProp = elementClassProp;
     exports.ɵtextBinding = textBinding;
