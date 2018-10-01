75f0752e02644ce9ea223faecd58c380eb85492b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 18:05:36 +0000

    c1bf82a fix(docs-infra): remove unnecessary margin on short descriptions (#25768)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+21.sha-a48bf0b
+ * @license Angular v6.1.9+36.sha-c1bf82a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1950,7 +1950,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.9+21.sha-a48bf0b');
+const VERSION = new Version('6.1.9+36.sha-c1bf82a');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.9+21.sha-a48bf0b
+ * @license Angular v6.1.9+36.sha-c1bf82a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify } from '@angular/core';
-import { __spread, __values, __decorate, __extends } from 'tslib';
+import { __extends, __decorate, __spread, __values } from 'tslib';
 
 /**
  * @license
