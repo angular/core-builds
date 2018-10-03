356b8f2834e4005dd23fb6768cd85b09a53bd7a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 22:04:44 +0000

    d815e41 build(docs-infra): upgrade @angular/cli to 6.2.3 (#26145) (#25997)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+37.sha-66c2d08
+ * @license Angular v6.1.9+56.sha-d815e41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify, defineInjectable } from '@angular/core';
+import { RendererFactory2, getDebugNode, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify, Compiler, Injectable, defineInjectable } from '@angular/core';
 
 /**
  * @fileoverview added by tsickle
