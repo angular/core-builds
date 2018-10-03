8841e0d0d8dbd7a8dab759c3742c05106f90280a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 22:02:16 +0000

    d815e41 build(docs-infra): upgrade @angular/cli to 6.2.3 (#26145) (#25997)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+37.sha-66c2d08
+ * @license Angular v6.1.9+56.sha-d815e41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1950,7 +1950,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.9+37.sha-66c2d08');
+const VERSION = new Version('6.1.9+56.sha-d815e41');
 
 /**
  * @fileoverview added by tsickle
