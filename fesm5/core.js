455b0543d22541d22eab7f7bc0a803c841f98ab7
Jason Aden <jasonaden@google.com>
2018-10-05 23:23:00 +0000

    8c6c2fc docs: add fakeAsync test new feature document (#23117)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+70.sha-3b4d9dc
+ * @license Angular v6.1.9+69.sha-8c6c2fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __read, __extends, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.9+70.sha-3b4d9dc');
+var VERSION = new Version('6.1.9+69.sha-8c6c2fc');
 
 /**
  * @license
