70ddf6dc101f39daf4ee95b61afd0588eb4f83d2
Jason Aden <jasonaden@google.com>
2018-10-05 21:47:23 +0000

    5c5164b docs: typo fixes (#26247)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+60.sha-b8a081a
+ * @license Angular v6.1.9+61.sha-5c5164b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.9+60.sha-b8a081a');
+const VERSION = new Version('6.1.9+61.sha-5c5164b');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+60.sha-b8a081a
+ * @license Angular v6.1.9+61.sha-5c5164b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15084,7 +15084,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.9+60.sha-b8a081a');
+var VERSION = new Version('6.1.9+61.sha-5c5164b');
 
 /**
  * @license
