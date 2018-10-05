925b6ffc2fe45888eefbd79ccd860195eb2f7d23
Jason Aden <jasonaden@google.com>
2018-10-05 23:22:52 +0000

    3886bfa docs: Rename 'QuickStart' into 'Getting Started' (#25762)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+64.sha-2fe401d
+ * @license Angular v6.1.9+68.sha-3886bfa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18557,7 +18557,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.9+64.sha-2fe401d');
+const VERSION = new Version('6.1.9+68.sha-3886bfa');
 
 /**
  * @fileoverview added by tsickle
