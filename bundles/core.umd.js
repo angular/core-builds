da3e0c1ed0b52a50b7d41f95ac0763648cb4f775
Jason Aden <jasonaden@google.com>
2018-10-05 21:47:32 +0000

    ecb28bf docs: fix spelling errors (#26213)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+61.sha-5c5164b
+ * @license Angular v6.1.9+62.sha-ecb28bf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1714,7 +1714,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.9+61.sha-5c5164b');
+    var VERSION = new Version('6.1.9+62.sha-ecb28bf');
 
     /**
      * @license
