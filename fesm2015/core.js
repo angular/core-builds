2133f4790ea4ead0bd3958529f64cc0e625bd575
Jason Aden <jasonaden@google.com>
2018-10-05 23:23:14 +0000

    3b4d9dc fix(platform-browser): fix #22155, destroy hammer manager when off (#22156)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+68.sha-3886bfa
+ * @license Angular v6.1.9+70.sha-3b4d9dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.9+68.sha-3886bfa');
+const VERSION = new Version('6.1.9+70.sha-3b4d9dc');
 
 /**
  * @license
