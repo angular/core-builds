4e72f7a6041960301d1f22ca324a8b35b7000780
Jason Aden <jasonaden@google.com>
2018-10-05 21:49:44 +0000

    ecb28bf docs: fix spelling errors (#26213)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+61.sha-5c5164b
+ * @license Angular v6.1.9+62.sha-ecb28bf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify, defineInjectable } from '@angular/core';
+import { RendererFactory2, getDebugNode, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify, Compiler, Injectable, defineInjectable } from '@angular/core';
 
 /**
  * @fileoverview added by tsickle
