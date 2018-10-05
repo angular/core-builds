cd329641ee7362dc2655dd6620d7870d1c1309ae
Jason Aden <jasonaden@google.com>
2018-10-05 23:29:31 +0000

    39d979c docs: add package doc files (#26047)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+69.sha-8c6c2fc
+ * @license Angular v6.1.9+66.sha-39d979c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify, defineInjectable } from '@angular/core';
+import { RendererFactory2, getDebugNode, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify, Compiler, Injectable, defineInjectable } from '@angular/core';
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.9+69.sha-8c6c2fc
+ * @license Angular v6.1.9+66.sha-39d979c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { RendererFactory2, getDebugNode, defineInjectable, Compiler, InjectionToken, Injector, ɵclearOverrides, ɵstringify, ɵoverrideComponentView, NgZone, ApplicationInitStatus, NgModule, ɵAPP_ROOT, Optional, SkipSelf, ɵoverrideProvider, Component } from '@angular/core';
-import { __extends, __spread, __values, __decorate } from 'tslib';
+import { RendererFactory2, getDebugNode, InjectionToken, Injector, ɵclearOverrides, ɵstringify, ɵoverrideComponentView, NgZone, ApplicationInitStatus, NgModule, ɵAPP_ROOT, Optional, SkipSelf, ɵoverrideProvider, Component, defineInjectable, Compiler } from '@angular/core';
+import { __spread, __values, __decorate, __extends } from 'tslib';
 
 /**
  * @license
