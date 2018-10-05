661eeaca9009151109cda6572f9e724cd0f0620c
Jason Aden <jasonaden@google.com>
2018-10-05 21:26:15 +0000

    245b85f docs: typo fixes (#26247)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+53.sha-bfeed84
+ * @license Angular v7.0.0-rc.0+54.sha-245b85f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11827,7 +11827,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-rc.0+53.sha-bfeed84');
+const VERSION = new Version('7.0.0-rc.0+54.sha-245b85f');
 
 /**
  * @license
