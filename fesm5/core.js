25fe4667618535967a33e366f1136c5e2fb8949a
Jason Aden <jasonaden@google.com>
2018-10-05 23:27:23 +0000

    39d979c docs: add package doc files (#26047)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+69.sha-8c6c2fc
+ * @license Angular v6.1.9+66.sha-39d979c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.9+69.sha-8c6c2fc');
+var VERSION = new Version('6.1.9+66.sha-39d979c');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.9+69.sha-8c6c2fc
+ * @license Angular v6.1.9+66.sha-39d979c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify } from '@angular/core';
-import { __extends, __decorate, __spread, __values } from 'tslib';
+import { __spread, __values, __decorate, __extends } from 'tslib';
 
 /**
  * @license
