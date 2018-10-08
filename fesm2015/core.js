5637d72c84cc1ed8d3c836cec740396640d32447
Jason Aden <jasonaden@google.com>
2018-10-08 19:39:00 +0000

    623adbb fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+71.sha-3660ff8
+ * @license Angular v6.1.9+72.sha-623adbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.9+71.sha-3660ff8');
+const VERSION = new Version('6.1.9+72.sha-623adbb');
 
 /**
  * @license
