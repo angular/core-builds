5b5f58ad87b620ff5424fcda94000ad653b6126a
Jason Aden <jasonaden@google.com>
2018-10-08 21:16:12 +0000

    0bdea1f docs: update docs to reflect the changes in RxJS 6 (#26238)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+71.sha-9320ec0
+ * @license Angular v7.0.0-rc.0+66.sha-0bdea1f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14774,7 +14774,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+71.sha-9320ec0');
+const VERSION = new Version('7.0.0-rc.0+66.sha-0bdea1f');
 
 /**
  * @fileoverview added by tsickle
