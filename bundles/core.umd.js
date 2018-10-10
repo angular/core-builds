8ae41c90d619fbc4d3b0476c72503227b32218e2
Jason Aden <jasonaden@google.com>
2018-10-10 22:19:12 +0000

    65555fe Revert "fix(upgrade): properly destroy upgraded component elements and descendants (#26209)"

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10
+ * @license Angular v6.1.10+2.sha-65555fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15138,7 +15138,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.10');
+    var VERSION = new Version('6.1.10+2.sha-65555fe');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.10
+ * @license Angular v6.1.10+2.sha-65555fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify } from '@angular/core';
-import { __spread, __values, __decorate, __extends } from 'tslib';
+import { __extends, __decorate, __spread, __values } from 'tslib';
 
 /**
  * @license
