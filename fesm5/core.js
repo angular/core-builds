e226a7b1c72cc8689a8951c3f091d11cb5e73f86
Jason Aden <jasonaden@google.com>
2018-10-10 22:17:58 +0000

    65555fe Revert "fix(upgrade): properly destroy upgraded component elements and descendants (#26209)"

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10
+ * @license Angular v6.1.10+2.sha-65555fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.10');
+var VERSION = new Version('6.1.10+2.sha-65555fe');
 
 /**
  * @license
