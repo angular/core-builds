2fe37ce312d11c9fd7d5f633d1012b40e38328ab
Miško Hevery <misko@hevery.com>
2018-10-11 21:04:12 +0000

    7d0e175 ci(docs-infra): allow `aio_local` builds to fail on Travis (#26366)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+5.sha-053bf27
+ * @license Angular v7.0.0-rc.1+6.sha-7d0e175
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15006,7 +15006,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+5.sha-053bf27');
+const VERSION = new Version('7.0.0-rc.1+6.sha-7d0e175');
 
 /**
  * @fileoverview added by tsickle
