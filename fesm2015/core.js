3556f3a8fd901a33aaa2b9c12cbd29f02f67a124
Miško Hevery <misko@hevery.com>
2018-10-11 21:57:59 +0000

    50de03a docs: fix transpiles link in dependency injection (#26250)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+4.sha-f1e14a3
+ * @license Angular v6.1.10+3.sha-50de03a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.10+4.sha-f1e14a3');
+const VERSION = new Version('6.1.10+3.sha-50de03a');
 
 /**
  * @license
