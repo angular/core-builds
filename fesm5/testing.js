fa5d45da901dc996d0c57d7692abfc3e8eefc5ef
Miško Hevery <misko@hevery.com>
2018-10-11 21:51:42 +0000

    f1e14a3 docs: add angularmix to events page (#26374)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.10+2.sha-65555fe
+ * @license Angular v6.1.10+4.sha-f1e14a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { RendererFactory2, getDebugNode, InjectionToken, Injector, ɵclearOverrides, ɵstringify, ɵoverrideComponentView, NgZone, ApplicationInitStatus, NgModule, ɵAPP_ROOT, Optional, SkipSelf, ɵoverrideProvider, Component, defineInjectable, Compiler } from '@angular/core';
-import { __spread, __values, __decorate, __extends } from 'tslib';
+import { RendererFactory2, getDebugNode, defineInjectable, Compiler, InjectionToken, Injector, ɵclearOverrides, ɵstringify, ɵoverrideComponentView, NgZone, ApplicationInitStatus, NgModule, ɵAPP_ROOT, Optional, SkipSelf, ɵoverrideProvider, Component } from '@angular/core';
+import { __extends, __spread, __values, __decorate } from 'tslib';
 
 /**
  * @license
