eb36eaf8e95ff67741384ddf415bb7341a0b99b1
Miško Hevery <misko@hevery.com>
2018-10-11 21:49:12 +0000

    f1e14a3 docs: add angularmix to events page (#26374)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+2.sha-65555fe
+ * @license Angular v6.1.10+4.sha-f1e14a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1950,7 +1950,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.10+2.sha-65555fe');
+const VERSION = new Version('6.1.10+4.sha-f1e14a3');
 
 /**
  * @fileoverview added by tsickle
