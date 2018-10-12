7c5e0c94bf86ee331f9af1c3c642e8663c1389e0
Miško Hevery <misko@hevery.com>
2018-10-12 16:44:38 +0000

    ff15043 fix(core): allow null value for renderer setElement(…) (#17065)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+33.sha-ea20ae6
+ * @license Angular v7.0.0-rc.1+34.sha-ff15043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11901,7 +11901,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-rc.1+33.sha-ea20ae6');
+const VERSION = new Version('7.0.0-rc.1+34.sha-ff15043');
 
 /**
  * @license
