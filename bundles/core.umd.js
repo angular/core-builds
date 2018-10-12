919812331635e83178c8ce8bbe4f1a367fdcf4bd
Miško Hevery <misko@hevery.com>
2018-10-12 17:16:57 +0000

    ff15043 fix(core): allow null value for renderer setElement(…) (#17065)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+27.sha-071934e
+ * @license Angular v7.0.0-rc.1+34.sha-ff15043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12241,7 +12241,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+27.sha-071934e');
+    var VERSION = new Version('7.0.0-rc.1+34.sha-ff15043');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+27.sha-071934e
+ * @license Angular v7.0.0-rc.1+34.sha-ff15043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11901,7 +11901,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-rc.1+27.sha-071934e');
+const VERSION = new Version('7.0.0-rc.1+34.sha-ff15043');
 
 /**
  * @license
