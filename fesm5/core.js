c929ee529cf11705291dc1f7f81c9d0bfda85bda
Miško Hevery <misko@hevery.com>
2018-10-12 21:39:23 +0000

    9e32dc7 build: upgrade `@types/jasminewd2` to 2.0.4 (#26139)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+36.sha-bab5b68
+ * @license Angular v7.0.0-rc.1+38.sha-9e32dc7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12296,7 +12296,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+36.sha-bab5b68');
+var VERSION = new Version('7.0.0-rc.1+38.sha-9e32dc7');
 
 /**
  * @license
