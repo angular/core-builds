b826b8bbbed0e77717361cff538b1cc0e6b3046e
Miško Hevery <misko@hevery.com>
2018-10-12 21:50:59 +0000

    6790709 fix(docs-infra): prevent unnecessary SideNav scrollbar (#26416)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+8.sha-db49bea
+ * @license Angular v6.1.10+10.sha-6790709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.10+8.sha-db49bea');
+const VERSION = new Version('6.1.10+10.sha-6790709');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+8.sha-db49bea
+ * @license Angular v6.1.10+10.sha-6790709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
@@ -15084,7 +15084,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.10+8.sha-db49bea');
+var VERSION = new Version('6.1.10+10.sha-6790709');
 
 /**
  * @license
