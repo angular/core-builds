f27076f32130b7e26b4c45006a0c12246a0d8dfe
Miško Hevery <misko@hevery.com>
2018-10-12 21:40:42 +0000

    b2db32b docs: getting started updated for accuracy and style (#26093)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+37.sha-a19b690
+ * @license Angular v7.0.0-rc.1+39.sha-b2db32b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12296,7 +12296,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+37.sha-a19b690');
+var VERSION = new Version('7.0.0-rc.1+39.sha-b2db32b');
 
 /**
  * @license
