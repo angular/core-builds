1073168d7863f151384d84698f370fcfaf9dc592
Miško Hevery <misko@hevery.com>
2018-10-12 16:38:18 +0000

    db49bea build(docs-infra): upgrade @angular/material to 7.0.0-rc.1 (#26394)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+5.sha-4f09f7d
+ * @license Angular v6.1.10+8.sha-db49bea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15032,7 +15032,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.10+5.sha-4f09f7d');
+var VERSION = new Version('6.1.10+8.sha-db49bea');
 
 /**
  * @license
