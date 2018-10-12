c3be27d0d6594aa961ce3ee495a38d0f28d85008
Miško Hevery <misko@hevery.com>
2018-10-12 16:40:20 +0000

    ea20ae6 build(docs-infra): only show name in 'inherited from' section (#26387)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+32.sha-7777a99
+ * @license Angular v7.0.0-rc.1+33.sha-ea20ae6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -12187,7 +12187,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+32.sha-7777a99');
+var VERSION = new Version('7.0.0-rc.1+33.sha-ea20ae6');
 
 /**
  * @license
