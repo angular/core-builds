2d800525b0d8e006fbd0687f41b42533a618490f
Miško Hevery <misko@hevery.com>
2018-10-12 21:41:57 +0000

    a19b690 docs: move cli commands above api in nav (#26405)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+38.sha-9e32dc7
+ * @license Angular v7.0.0-rc.1+37.sha-a19b690
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -12187,7 +12187,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+38.sha-9e32dc7');
+var VERSION = new Version('7.0.0-rc.1+37.sha-a19b690');
 
 /**
  * @license
