e598c10e92152d9bc3fe00fd4e5ee1c07fcaa644
Miško Hevery <misko@hevery.com>
2018-10-14 05:06:17 +0000

    1f06b6c build: upgrade `@types/jasminewd2` to 2.0.4 (#26432)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+10.sha-6790709
+ * @license Angular v6.1.10+11.sha-1f06b6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14712,7 +14712,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('6.1.10+10.sha-6790709');
+const VERSION = new Version('6.1.10+11.sha-1f06b6c');
 
 /**
  * @license
