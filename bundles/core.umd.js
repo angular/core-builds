dda59a6a044039c6a25a0561cd6427beb3a40b3d
George Kalpakas <kalpakas.g@gmail.com>
2018-10-15 20:14:21 +0000

    a720d8a build(docs-infra): upgrade cli command docs sources to 9b72649b8

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+40.sha-b821218
+ * @license Angular v7.0.0-rc.1+40.sha-a720d8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12226,7 +12226,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+40.sha-b821218');
+    var VERSION = new Version('7.0.0-rc.1+40.sha-a720d8a');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+40.sha-b821218
+ * @license Angular v7.0.0-rc.1+40.sha-a720d8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14881,7 +14881,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+40.sha-b821218');
+const VERSION = new Version('7.0.0-rc.1+40.sha-a720d8a');
 
 /**
  * @fileoverview added by tsickle
