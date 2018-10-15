49af37f16111119b08253412ab23ef3329f02b7b
George Kalpakas <kalpakas.g@gmail.com>
2018-10-15 23:46:19 +0000

    24cf45f build(docs-infra): upgrade cli command docs sources to 301dfa6e5

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+45.sha-6ad85ae
+ * @license Angular v7.0.0-rc.1+45.sha-24cf45f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12312,7 +12312,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+45.sha-6ad85ae');
+    var VERSION = new Version('7.0.0-rc.1+45.sha-24cf45f');
 
     /**
      * @license
