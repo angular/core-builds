83febbc7b6051d59f6a226fab4ebca70bc93d570
Miško Hevery <misko@hevery.com>
2018-10-15 19:04:34 +0000

    a251374 docs: update process for cli tool and restructure doc (#25752)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+13.sha-2b00c17
+ * @license Angular v6.1.10+14.sha-a251374
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1950,7 +1950,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.10+13.sha-2b00c17');
+const VERSION = new Version('6.1.10+14.sha-a251374');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+13.sha-2b00c17
+ * @license Angular v6.1.10+14.sha-a251374
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1644,7 +1644,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.10+13.sha-2b00c17');
+var VERSION = new Version('6.1.10+14.sha-a251374');
 
 /**
  * @license
