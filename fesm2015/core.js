a326681be7741156ce7e4b088764ef7afbdaa565
George Kalpakas <kalpakas.g@gmail.com>
2018-10-15 23:48:38 +0000

    24cf45f build(docs-infra): upgrade cli command docs sources to 301dfa6e5

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+45.sha-6ad85ae
+ * @license Angular v7.0.0-rc.1+45.sha-24cf45f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14826,7 +14826,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+45.sha-6ad85ae');
+const VERSION = new Version('7.0.0-rc.1+45.sha-24cf45f');
 
 /**
  * @fileoverview added by tsickle
