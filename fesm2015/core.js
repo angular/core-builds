70cec3fe92ca4cefe2f4775fa24b6f1656d72326
Miško Hevery <misko@hevery.com>
2018-10-16 00:21:03 +0000

    1c9b065 fix(router): fix regression where navigateByUrl promise didn't resolve on CanLoad failure (#26455)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+49.sha-dd8a851
+ * @license Angular v7.0.0-rc.1+50.sha-1c9b065
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14826,7 +14826,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+49.sha-dd8a851');
+const VERSION = new Version('7.0.0-rc.1+50.sha-1c9b065');
 
 /**
  * @fileoverview added by tsickle
