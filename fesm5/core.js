aeaddfa0924fc70d6772c96998f3aec729be1057
Miško Hevery <misko@hevery.com>
2018-10-16 22:20:47 +0000

    81c9720 docs: fix links to setup and cli docs (#26463)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+56.sha-ea2cfbb
+ * @license Angular v7.0.0-rc.1+57.sha-81c9720
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12258,7 +12258,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+56.sha-ea2cfbb');
+var VERSION = new Version('7.0.0-rc.1+57.sha-81c9720');
 
 /**
  * @license
