078fa82a31ee2785fc336a869f35a1f3940d9994
Miško Hevery <misko@hevery.com>
2018-10-16 22:20:40 +0000

    7ac4b76 docs: remove setup for local dev and anatomy of setup docs from nav (#26380)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+55.sha-be0382b
+ * @license Angular v7.0.0-rc.1+54.sha-7ac4b76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14990,7 +14990,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+55.sha-be0382b');
+const VERSION = new Version('7.0.0-rc.1+54.sha-7ac4b76');
 
 /**
  * @fileoverview added by tsickle
