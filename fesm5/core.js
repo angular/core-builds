783a18d356491cb8b3510e6dcc56e230062e6006
Miško Hevery <misko@hevery.com>
2018-10-16 22:12:23 +0000

    41de0e0 docs: overview for cli reference section (#26043)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+53.sha-f6a2dbf
+ * @license Angular v7.0.0-rc.1+54.sha-41de0e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12258,7 +12258,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+53.sha-f6a2dbf');
+var VERSION = new Version('7.0.0-rc.1+54.sha-41de0e0');
 
 /**
  * @license
