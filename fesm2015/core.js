9fcddcf812e62565ec30c47d7cb3b0ff987c47f7
Miško Hevery <misko@hevery.com>
2018-10-16 00:20:31 +0000

    eeebe62 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+17.sha-05f279d
+ * @license Angular v6.1.10+18.sha-eeebe62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1950,7 +1950,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('6.1.10+17.sha-05f279d');
+const VERSION = new Version('6.1.10+18.sha-eeebe62');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.10+17.sha-05f279d
+ * @license Angular v6.1.10+18.sha-eeebe62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, Compiler, Injectable, ApplicationInitStatus, Component, InjectionToken, Injector, NgModule, NgZone, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵoverrideComponentView, ɵoverrideProvider, ɵstringify } from '@angular/core';
-import { __extends, __decorate, __spread, __values } from 'tslib';
+import { __spread, __values, __decorate, __extends } from 'tslib';
 
 /**
  * @license
