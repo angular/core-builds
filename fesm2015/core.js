7ce2c4523c57b447506324ffd9422f8d53d00dbf
Miško Hevery <misko@hevery.com>
2018-10-16 00:15:55 +0000

    dd8a851 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+48.sha-164f79a
+ * @license Angular v7.0.0-rc.1+49.sha-dd8a851
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14990,7 +14990,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+48.sha-164f79a');
+const VERSION = new Version('7.0.0-rc.1+49.sha-dd8a851');
 
 /**
  * @fileoverview added by tsickle
