39e0c2ba12f56210d4744ca2123f1254f28c44b4
Miško Hevery <misko@hevery.com>
2018-10-16 22:20:34 +0000

    be0382b docs: fix links to setup and cli docs (#26463)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+52.sha-96f3856
+ * @license Angular v7.0.0-rc.1+55.sha-be0382b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12188,7 +12188,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+52.sha-96f3856');
+    var VERSION = new Version('7.0.0-rc.1+55.sha-be0382b');
 
     /**
      * @license
