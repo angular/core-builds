993fbbc6ce0d20a2fcf9b7da6e6d7c5ec46df1b5
Miško Hevery <misko@hevery.com>
2018-10-16 22:13:27 +0000

    ea2cfbb docs: remove setup for local dev and anatomy of setup docs from nav (#26380)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+55.sha-2326b9c
+ * @license Angular v7.0.0-rc.1+56.sha-ea2cfbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12258,7 +12258,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+55.sha-2326b9c');
+var VERSION = new Version('7.0.0-rc.1+56.sha-ea2cfbb');
 
 /**
  * @license
