adfdf2040b097ce920d5ff644f521300faa65968
Miško Hevery <misko@hevery.com>
2018-10-16 22:12:44 +0000

    2326b9c fix(service-worker): clean up caches from old SW versions (#26319)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+54.sha-41de0e0
+ * @license Angular v7.0.0-rc.1+55.sha-2326b9c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14990,7 +14990,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+54.sha-41de0e0');
+const VERSION = new Version('7.0.0-rc.1+55.sha-2326b9c');
 
 /**
  * @fileoverview added by tsickle
