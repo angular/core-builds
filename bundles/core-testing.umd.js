bd0d3ca9d1057fc42adfee4505e187b73407ee70
Miško Hevery <misko@hevery.com>
2018-10-17 21:11:51 +0000

    f8195e5 fix(ivy): stub TestBed.compileComponents implementation (#26506)

--- bundles/core-testing.umd.js
+++ bundles/core-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+71.sha-8a3fd58
+ * @license Angular v7.0.0-rc.1+85.sha-f8195e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1146,9 +1146,10 @@
                 (_d = this._schemas).push.apply(_d, __spread(moduleDef.schemas));
             }
         };
-        // TODO(vicb): implement
         TestBedRender3.prototype.compileComponents = function () {
-            throw new Error('Render3TestBed.compileComponents is not implemented yet');
+            // assume for now that components don't use templateUrl / stylesUrl to unblock further testing
+            // TODO(pk): plug into the ivy's resource fetching pipeline
+            return Promise.resolve();
         };
         TestBedRender3.prototype.get = function (token, notFoundValue) {
             if (notFoundValue === void 0) { notFoundValue = i0.Injector.THROW_IF_NOT_FOUND; }
