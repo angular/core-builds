e4345aa2646337f8288dbd2dbff55357c1249d53
Misko Hevery <misko@hevery.com>
2018-10-17 19:24:50 +0000

    b29e709 fix(docs-infra): rename "title" by "header" to avoid unwanted tooltips (#26396)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-d769b44
+ * @license Angular v7.0.0-rc.1+62.sha-b29e709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12312,7 +12312,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+64.sha-d769b44');
+    var VERSION = new Version('7.0.0-rc.1+62.sha-b29e709');
 
     /**
      * @license
