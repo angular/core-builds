30afb0cbad2e5e3021592010e2092948e6267d6d
Misko Hevery <misko@hevery.com>
2018-10-17 19:19:03 +0000

    4b52257 fix(docs-infra): fix top menu logo position (#26473)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+58.sha-34aff0b
+ * @license Angular v7.0.0-rc.1+60.sha-4b52257
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12312,7 +12312,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+58.sha-34aff0b');
+    var VERSION = new Version('7.0.0-rc.1+60.sha-4b52257');
 
     /**
      * @license
