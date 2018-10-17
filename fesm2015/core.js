5dd994ef059e870a6b4915212129b294d4dbd9fa
Misko Hevery <misko@hevery.com>
2018-10-17 19:22:53 +0000

    0ef1f7e docs: change links to cli wiki to link to new aio docs files m to z (#26492)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+72.sha-ecd473b
+ * @license Angular v7.0.0-rc.1+74.sha-0ef1f7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12149,7 +12149,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-rc.1+72.sha-ecd473b');
+const VERSION = new Version('7.0.0-rc.1+74.sha-0ef1f7e');
 
 /**
  * @license
