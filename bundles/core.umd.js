876c731261a0d48354ac39cf60ebd07c7f4046b0
Misko Hevery <misko@hevery.com>
2018-10-17 19:25:50 +0000

    d769b44 docs: change links to cli wiki to link to new aio docs (#26489)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+61.sha-017a087
+ * @license Angular v7.0.0-rc.1+64.sha-d769b44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12188,7 +12188,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+61.sha-017a087');
+    var VERSION = new Version('7.0.0-rc.1+64.sha-d769b44');
 
     /**
      * @license
