a07f2500989ff05499f4f6ce8c22206c6829601c
Misko Hevery <misko@hevery.com>
2018-10-17 19:26:07 +0000

    b29e709 fix(docs-infra): rename "title" by "header" to avoid unwanted tooltips (#26396)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-d769b44
+ * @license Angular v7.0.0-rc.1+62.sha-b29e709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11863,7 +11863,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-rc.1+64.sha-d769b44');
+const VERSION = new Version('7.0.0-rc.1+62.sha-b29e709');
 
 /**
  * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-d769b44
+ * @license Angular v7.0.0-rc.1+62.sha-b29e709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compileInjectable, compilePipeFromMetadata } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -12149,7 +12149,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+64.sha-d769b44');
+var VERSION = new Version('7.0.0-rc.1+62.sha-b29e709');
 
 /**
  * @license
