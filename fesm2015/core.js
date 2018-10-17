82138ce7632126f642360c52448c5bf6ea8209fa
Misko Hevery <misko@hevery.com>
2018-10-17 19:31:26 +0000

    a43b80a ci: add triage PRs config for the ngbot (#26477)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+84.sha-65b2093
+ * @license Angular v7.0.0-rc.1+76.sha-a43b80a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12149,7 +12149,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-rc.1+84.sha-65b2093');
+const VERSION = new Version('7.0.0-rc.1+76.sha-a43b80a');
 
 /**
  * @license
