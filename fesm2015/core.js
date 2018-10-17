b73bc38b94691f6f9190011ae64103759f979f8e
Miško Hevery <misko@hevery.com>
2018-10-17 20:43:29 +0000

    a609bf5 docs: fix formatting of PULL_REQUEST_TEMPLATE.md (#26512)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+85.sha-f8195e5
+ * @license Angular v7.0.0-rc.1+86.sha-a609bf5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15260,7 +15260,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+85.sha-f8195e5');
+const VERSION = new Version('7.0.0-rc.1+86.sha-a609bf5');
 
 /**
  * @fileoverview added by tsickle
