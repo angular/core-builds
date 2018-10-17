eedf120d6522b8818d0fc5b185b7eef681a62a72
Misko Hevery <misko@hevery.com>
2018-10-17 20:39:10 +0000

    8a3fd58 feat(ivy): i18n compiler support for i18nStart and i18nEnd instructions (#26442)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-fa8e633
+ * @license Angular v7.0.0-rc.1+71.sha-8a3fd58
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12547,7 +12547,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0-rc.1+64.sha-fa8e633');
+var VERSION = new Version('7.0.0-rc.1+71.sha-8a3fd58');
 
 /**
  * @license
