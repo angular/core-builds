d12d753966c4f1ad8e4127758d7639ee41ddd554
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:39 +0000

    8d28fe9 docs: update http to https where possible (#26509)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+1.sha-a403c4f
+ * @license Angular v7.0.0+4.sha-8d28fe9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -11863,7 +11863,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0+1.sha-a403c4f');
+const VERSION = new Version('7.0.0+4.sha-8d28fe9');
 
 /**
  * @license
