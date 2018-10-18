689cd7ec3b87fd9398d619999d21d0667a94ef9a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:46:57 +0000

    d0a1e42 ci: add forms-overview guide to forms approval list (#26549)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+2.sha-32c018c
+ * @license Angular v7.0.0+6.sha-d0a1e42
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14826,7 +14826,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0+2.sha-32c018c');
+const VERSION = new Version('7.0.0+6.sha-d0a1e42');
 
 /**
  * @fileoverview added by tsickle
