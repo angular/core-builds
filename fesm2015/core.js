4de1adcbd4985857c13fd61a0aea5352cb578f0b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:43:32 +0000

    09cc458 docs: type fix (#26386)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+92.sha-efe0c5f
+ * @license Angular v7.0.0-rc.1+93.sha-09cc458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12149,7 +12149,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-rc.1+92.sha-efe0c5f');
+const VERSION = new Version('7.0.0-rc.1+93.sha-09cc458');
 
 /**
  * @license
