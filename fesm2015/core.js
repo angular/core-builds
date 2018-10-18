aaa21fd848f61416e8cd6261145db54bd06f881f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:45:19 +0000

    a403c4f docs: type fix (#26386)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0
+ * @license Angular v7.0.0+1.sha-a403c4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14826,7 +14826,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0');
+const VERSION = new Version('7.0.0+1.sha-a403c4f');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0
+ * @license Angular v7.0.0+1.sha-a403c4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12149,7 +12149,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0');
+var VERSION = new Version('7.0.0+1.sha-a403c4f');
 
 /**
  * @license
