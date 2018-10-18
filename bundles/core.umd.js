bccb84144c676e88a39d16a457451384ba8dfdd9
Miško Hevery <misko@hevery.com>
2018-10-18 17:30:55 +0000

    d9d2260 build(docs-infra): allow `""` as empty region in `{@example}` tags (#26514)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+88.sha-d557f1d
+ * @license Angular v7.0.0-rc.1+91.sha-d9d2260
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12477,7 +12477,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+88.sha-d557f1d');
+    var VERSION = new Version('7.0.0-rc.1+91.sha-d9d2260');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+88.sha-d557f1d
+ * @license Angular v7.0.0-rc.1+91.sha-d9d2260
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15260,7 +15260,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+88.sha-d557f1d');
+const VERSION = new Version('7.0.0-rc.1+91.sha-d9d2260');
 
 /**
  * @fileoverview added by tsickle
