bfade7e13ef2e340515f4c1bb89fb15dc44efd51
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:42:20 +0000

    09cc458 docs: type fix (#26386)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+92.sha-efe0c5f
+ * @license Angular v7.0.0-rc.1+93.sha-09cc458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12601,7 +12601,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+92.sha-efe0c5f');
+    var VERSION = new Version('7.0.0-rc.1+93.sha-09cc458');
 
     /**
      * @license
