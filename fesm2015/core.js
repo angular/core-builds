a0c22d1c6fe74c7d2a2c3ddad4d8e3a41a72ef0f
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:15:09 +0000

    aa55d88 docs: forms overview copy edit (#26450)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+106.sha-b9bd95b
+ * @license Angular v7.0.0-rc.1+108.sha-aa55d88
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15120,7 +15120,7 @@ class Version {
     }
 }
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+106.sha-b9bd95b');
+const VERSION = new Version('7.0.0-rc.1+108.sha-aa55d88');
 
 /**
  * @fileoverview added by tsickle
