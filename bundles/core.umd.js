bec9784da93181dc5a85035e1a9a8182f70bbec7
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:13:49 +0000

    aa55d88 docs: forms overview copy edit (#26450)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+106.sha-b9bd95b
+ * @license Angular v7.0.0-rc.1+108.sha-aa55d88
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12366,7 +12366,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+106.sha-b9bd95b');
+    var VERSION = new Version('7.0.0-rc.1+108.sha-aa55d88');
 
     /**
      * @license
