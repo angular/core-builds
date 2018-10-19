ae060cfbb0f061a13b7c5ace0a36e0f6c2bdad71
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:27:11 +0000

    3f94759 fix(docs-infra): correct the version of @angular-devkit/build-angular (#26555)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+33.sha-d972501
+ * @license Angular v7.0.0+32.sha-3f94759
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12438,7 +12438,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('7.0.0+33.sha-d972501');
+var VERSION = new Version('7.0.0+32.sha-3f94759');
 
 /**
  * @license
