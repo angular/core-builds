b2ca97bd705c28727f4928533180ea25d8b8ca1f
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:21:33 +0000

    d972501 docs: fix release info navigation (#26558)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+27.sha-078a928
+ * @license Angular v7.0.0+33.sha-d972501
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12477,7 +12477,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0+27.sha-078a928');
+    var VERSION = new Version('7.0.0+33.sha-d972501');
 
     /**
      * @license
