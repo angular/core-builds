6f37c3b8bbac4138dab87d68536674caae61ca78
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:15:16 +0000

    07fc4c2 docs(service-worker): updated browser support for Service Worker (#26408)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+108.sha-aa55d88
+ * @license Angular v7.0.0-rc.1+107.sha-07fc4c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12351,7 +12351,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('7.0.0-rc.1+108.sha-aa55d88');
+    var VERSION = new Version('7.0.0-rc.1+107.sha-07fc4c2');
 
     /**
      * @license
