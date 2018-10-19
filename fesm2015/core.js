a2893a9f8af26188cf487170cad1f2d41a9a0856
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 20:36:19 +0000

    5b4cf38 refactor(bazel): remove workaround for TS bug (#26516)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+102.sha-d4cee51
+ * @license Angular v7.0.0-rc.1+111.sha-5b4cf38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12030,7 +12030,7 @@ class Version {
         this.patch = full.split('.').slice(2).join('.');
     }
 }
-const VERSION = new Version('7.0.0-rc.1+102.sha-d4cee51');
+const VERSION = new Version('7.0.0-rc.1+111.sha-5b4cf38');
 
 /**
  * @license
