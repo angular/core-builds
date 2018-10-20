5c7acb088d1184a428d80d3ac81d928df8e5106f
Igor Minar <igor@angular.io>
2018-10-20 06:16:34 +0000

    c8adef2 build: remove manual tags from platform jasmine tests (#26606)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+52.sha-778e1c2
+ * @license Angular v7.0.0+53.sha-c8adef2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12588,7 +12588,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0+52.sha-778e1c2');
+var VERSION = new Version('7.0.0+53.sha-c8adef2');
 
 /**
  * @license
