2486c31bfee4851e09507fd0bdc882c1609cb13d
Igor Minar <igor@angular.io>
2018-10-21 04:28:18 +0000

    100c7ef build: fix ivy package-dist generation (#26629)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+137.sha-4c2ce4e
+ * @license Angular v7.0.0-rc.1+156.sha-100c7ef.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { LiteralExpr, R3ResolvedDependencyType, WrappedNodeExpr, compileInjector, compileNgModule, jitExpression, ConstantPool, compileComponentFromMetadata, compileDirectiveFromMetadata, makeBindingParser, parseHostBindings, parseTemplate, compilePipeFromMetadata, compileInjectable } from '@angular/compiler';
 import { share } from 'rxjs/operators';
@@ -12353,7 +12353,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0-rc.1+137.sha-4c2ce4e');
+var VERSION = new Version('7.0.0-rc.1+156.sha-100c7ef.with-local-changes');
 
 /**
  * @license
