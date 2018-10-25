97e25b4a8632f13a7ef1ff653fd2658758b9340f
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 00:15:54 +0000

    1880c95 ci: only publish builds if relevant `aio` jobs pass (#26722)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+2.sha-13a803d
+ * @license Angular v7.1.0-beta.0+3.sha-1880c95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12356,7 +12356,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+2.sha-13a803d');
+var VERSION = new Version('7.1.0-beta.0+3.sha-1880c95');
 
 /**
  * @license
