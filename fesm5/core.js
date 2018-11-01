d1785e61077a4517a089fadb298c63e41066bd6c
Kara Erickson <karakara@google.com>
2018-11-01 21:39:55 +0000

    7697650 fix(docs-infra): update overload rendering (#24976)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+1.sha-2810482
+ * @license Angular v7.0.2+6.sha-7697650
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12540,7 +12540,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2+1.sha-2810482');
+var VERSION = new Version('7.0.2+6.sha-7697650');
 
 /**
  * @license
