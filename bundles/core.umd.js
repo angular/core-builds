9235dc4f643e51e51d8072525844ef9af9916077
Kara Erickson <karakara@google.com>
2018-11-01 21:38:44 +0000

    bc68b59 test(ivy): ngcc - test compiling a CLI generated project (#26403)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+19.sha-eb5d308
+ * @license Angular v7.1.0-beta.1+31.sha-bc68b59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13473,7 +13473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.0-beta.1+19.sha-eb5d308');
+    var VERSION = new Version('7.1.0-beta.1+31.sha-bc68b59');
 
     /**
      * @license
@@ -22168,6 +22168,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
+    var ɵdefineNgModule = defineNgModule;
     // clang-format on
 
     /**
@@ -22380,11 +22381,11 @@
     exports.ɵoverrideComponentView = overrideComponentView;
     exports.ɵoverrideProvider = overrideProvider;
     exports.ɵNOT_FOUND_CHECK_ONLY_ELEMENT_INJECTOR = NOT_FOUND_CHECK_ONLY_ELEMENT_INJECTOR;
+    exports.ɵdefineNgModule = ɵdefineNgModule;
     exports.ɵdefineBase = defineBase;
     exports.ɵdefineComponent = defineComponent;
     exports.ɵdefineDirective = defineDirective;
     exports.ɵdefinePipe = definePipe;
-    exports.ɵdefineNgModule = defineNgModule;
     exports.ɵdetectChanges = detectChanges;
     exports.ɵrenderComponent = renderComponent;
     exports.ɵRender3ComponentFactory = ComponentFactory$1;
