c6d4e5f01b53baa30065a05b91b2aae70d28f36e
Kara Erickson <karakara@google.com>
2018-11-01 21:51:48 +0000

    5eb540c docs: Webcomponents activated as of firefox 63 (#26889) PR Close #26889

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+6.sha-7697650
+ * @license Angular v7.0.2+7.sha-5eb540c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12591,7 +12591,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.0.2+6.sha-7697650');
+    var VERSION = new Version('7.0.2+7.sha-5eb540c');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+6.sha-7697650
+ * @license Angular v7.0.2+7.sha-5eb540c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12540,7 +12540,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2+6.sha-7697650');
+var VERSION = new Version('7.0.2+7.sha-5eb540c');
 
 /**
  * @license
