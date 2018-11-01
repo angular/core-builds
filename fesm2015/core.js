985e8ce7fd75cc64862244965ef9b92bec0a5e18
Matias Niemelä <matias@yearofmoo.com>
2018-11-01 00:17:34 +0000

    4e9f2e5 feat(router): guard returning UrlTree cancels current navigation and redirects (#26521)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+79.sha-2c25d29
+ * @license Angular v7.1.0-beta.1+6.sha-4e9f2e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16333,7 +16333,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+79.sha-2c25d29');
+const VERSION = new Version('7.1.0-beta.1+6.sha-4e9f2e5');
 
 /**
  * @fileoverview added by tsickle
