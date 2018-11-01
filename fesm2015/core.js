29ed15c5518b8f67702ca28f6ca58dc5164fe3fd
Kara Erickson <karakara@google.com>
2018-11-01 22:57:25 +0000

    e9e804f feat(forms): add updateOn option to FormBuilder (#24599)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+50.sha-aed95fd
+ * @license Angular v7.1.0-beta.1+51.sha-e9e804f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16474,7 +16474,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+50.sha-aed95fd');
+const VERSION = new Version('7.1.0-beta.1+51.sha-e9e804f');
 
 /**
  * @fileoverview added by tsickle
