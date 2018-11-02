0ffd58a651804d1904bffe20efe3f83f04d27856
Kara Erickson <karakara@google.com>
2018-11-02 18:07:59 +0000

    c016066 fix(ivy): ngcc should not break lifecycle hooks (#26856)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+60.sha-beabfb7
+ * @license Angular v7.1.0-beta.1+61.sha-c016066
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16474,7 +16474,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+60.sha-beabfb7');
+const VERSION = new Version('7.1.0-beta.1+61.sha-c016066');
 
 /**
  * @fileoverview added by tsickle
