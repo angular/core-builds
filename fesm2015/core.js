1f3eac38e9557828c7450672e3ab044ca3d8942a
Kara Erickson <karakara@google.com>
2018-11-02 23:19:34 +0000

    7d2a746 build: remove ivy JIT mode (#26863)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+64.sha-516af6c
+ * @license Angular v7.1.0-beta.1+66.sha-7d2a746
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16474,7 +16474,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+64.sha-516af6c');
+const VERSION = new Version('7.1.0-beta.1+66.sha-7d2a746');
 
 /**
  * @fileoverview added by tsickle
