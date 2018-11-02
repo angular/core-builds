3a4515dd08677f9c6dc4e076eb48fd55c4121262
Kara Erickson <karakara@google.com>
2018-11-02 18:10:11 +0000

    affcbbd feat(docs-infra): add getting started widgets (#26059)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+61.sha-c016066
+ * @license Angular v7.1.0-beta.1+53.sha-affcbbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13341,7 +13341,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+61.sha-c016066');
+var VERSION = new Version('7.1.0-beta.1+53.sha-affcbbd');
 
 /**
  * @license
