078485c836e1452bc60dc8adfc109759e2009483
Kara Erickson <karakara@google.com>
2018-11-05 18:21:14 +0000

    d0037b2 fix(ivy): correct ngtsc path handling in Windows (#26703)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+69.sha-d568d7a
+ * @license Angular v7.1.0-beta.1+70.sha-d0037b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read, __assign, __values } from 'tslib';
+import { __extends, __spread, __assign, __read, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -13341,7 +13341,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+69.sha-d568d7a');
+var VERSION = new Version('7.1.0-beta.1+70.sha-d0037b2');
 
 /**
  * @license
