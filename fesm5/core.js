b21a955db6b55569d8b733d81cd548f52636feb7
Kara Erickson <karakara@google.com>
2018-11-05 19:42:01 +0000

    4e4bca6 Revert "fix(ivy): correct ngtsc path handling in Windows (#26703)"

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+70.sha-d0037b2
+ * @license Angular v7.1.0-beta.1+71.sha-4e4bca6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13423,7 +13423,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+70.sha-d0037b2');
+var VERSION = new Version('7.1.0-beta.1+71.sha-4e4bca6');
 
 /**
  * @license
