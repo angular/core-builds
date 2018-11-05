73a5119cfdac2027f33eb22a1cbdcd49c83f4cd2
Kara Erickson <karakara@google.com>
2018-11-05 18:12:33 +0000

    352219e docs: new GitHub Issue templates (#26918)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+21.sha-e8a8770
+ * @license Angular v7.0.2+22.sha-352219e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12540,7 +12540,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2+21.sha-e8a8770');
+var VERSION = new Version('7.0.2+22.sha-352219e');
 
 /**
  * @license
