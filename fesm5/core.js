618529a51bce72edd2bbbc88dcd6efffeba37970
Kara Erickson <karakara@google.com>
2018-11-05 20:01:44 +0000

    f9a4abb fix: missing semi-colon and empty line (#26945)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+71.sha-4e4bca6
+ * @license Angular v7.1.0-beta.1+72.sha-f9a4abb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13341,7 +13341,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+71.sha-4e4bca6');
+var VERSION = new Version('7.1.0-beta.1+72.sha-f9a4abb');
 
 /**
  * @license
