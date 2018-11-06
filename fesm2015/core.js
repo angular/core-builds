28a5047435e95613669560e861c3edb366e2a180
Kara Erickson <karakara@google.com>
2018-11-06 18:57:24 +0000

    7dbc103 fix(upgrade): improve downgrading-related error messages (#26217)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+62.sha-64647af
+ * @license Angular v7.1.0-beta.1+77.sha-7dbc103
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16474,7 +16474,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+62.sha-64647af');
+const VERSION = new Version('7.1.0-beta.1+77.sha-7dbc103');
 
 /**
  * @fileoverview added by tsickle
