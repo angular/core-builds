0a12928c282400ee70bf5f589fb8a597574fc472
Kara Erickson <karakara@google.com>
2018-11-06 00:56:49 +0000

    7dbc103 fix(upgrade): improve downgrading-related error messages (#26217)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+74.sha-099d1a6
+ * @license Angular v7.1.0-beta.1+77.sha-7dbc103
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __extends, __param, __read, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -13423,7 +13423,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+74.sha-099d1a6');
+var VERSION = new Version('7.1.0-beta.1+77.sha-7dbc103');
 
 /**
  * @license
