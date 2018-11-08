71e31a97091fc3d536d5de9be29160b318638074
Andrew Kushnir <akushnir@google.com>
2018-11-08 21:30:58 +0000

    0475b8d ci: exclude symbol golden files from build-and-ci pullapprove group (#27004)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+6.sha-6be6125
+ * @license Angular v7.0.3+7.sha-0475b8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12540,7 +12540,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+6.sha-6be6125');
+var VERSION = new Version('7.0.3+7.sha-0475b8d');
 
 /**
  * @license
