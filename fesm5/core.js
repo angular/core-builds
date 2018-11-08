407ec4211c5cb73aa79fa26bdc9b901c33d2beac
Andrew Kushnir <akushnir@google.com>
2018-11-08 21:29:57 +0000

    6be6125 docs: add Angular subreddit to resources.json (#26976)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+4.sha-d284fa6
+ * @license Angular v7.0.3+6.sha-6be6125
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12540,7 +12540,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+4.sha-d284fa6');
+var VERSION = new Version('7.0.3+6.sha-6be6125');
 
 /**
  * @license
