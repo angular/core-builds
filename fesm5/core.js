a154fae90922757ae8802e6684939b7023d32d51
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:25:22 +0000

    006a43f docs: correct https://angularconsole.com/ link (#26999) PR Close #26999

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3
+ * @license Angular v7.0.3+2.sha-006a43f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12540,7 +12540,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3');
+var VERSION = new Version('7.0.3+2.sha-006a43f');
 
 /**
  * @license
