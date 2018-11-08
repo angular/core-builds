1dfa90abf87773b66ffd8728f513e70b1eb45982
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:32:58 +0000

    19546c2 build: add 'yarn list-fixme-ivy-targets' to list all the fixme-ivy-* targest (#26834)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+3.sha-791d192
+ * @license Angular v7.1.0-beta.2+4.sha-19546c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16422,7 +16422,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+3.sha-791d192');
+const VERSION = new Version('7.1.0-beta.2+4.sha-19546c2');
 
 /**
  * @fileoverview added by tsickle
