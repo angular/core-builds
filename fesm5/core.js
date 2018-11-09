e8d8c2839393729a1226a5a62eeb3c38ac1cb827
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:14:55 +0000

    c89045f docs: update links to TC39 and observable proposal (#26715)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+11.sha-1258ec0
+ * @license Angular v7.1.0-beta.2+12.sha-c89045f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13523,7 +13523,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+11.sha-1258ec0');
+var VERSION = new Version('7.1.0-beta.2+12.sha-c89045f');
 
 /**
  * @license
