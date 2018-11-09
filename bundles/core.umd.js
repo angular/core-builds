4f97456f81d1b37a532654a7013ac550531b31d3
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:17:24 +0000

    4efb460 docs(router): add RxJS filter pipe operator to example (#25686)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+13.sha-edb50a2
+ * @license Angular v7.1.0-beta.2+15.sha-4efb460
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13574,7 +13574,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.0-beta.2+13.sha-edb50a2');
+    var VERSION = new Version('7.1.0-beta.2+15.sha-4efb460');
 
     /**
      * @license
