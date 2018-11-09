76bc147b9ee510bcdf0c9512201f72152203a510
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:10:50 +0000

    cbd4206 docs(core): cleanup todo notes in ContentChild documentation examples (#26543)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+12.sha-f33307b
+ * @license Angular v7.0.3+13.sha-cbd4206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12591,7 +12591,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.0.3+12.sha-f33307b');
+    var VERSION = new Version('7.0.3+13.sha-cbd4206');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+12.sha-f33307b
+ * @license Angular v7.0.3+13.sha-cbd4206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15383,7 +15383,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.3+12.sha-f33307b');
+const VERSION = new Version('7.0.3+13.sha-cbd4206');
 
 /**
  * @fileoverview added by tsickle
