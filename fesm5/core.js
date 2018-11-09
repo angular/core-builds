28536ae83a15f0c6e66e827bb091fbeec5d9e044
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:41:55 +0000

    7a1b876 docs: removed unused pull-left classes from hero form component (#26178)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+13.sha-cbd4206
+ * @license Angular v7.0.3+14.sha-7a1b876
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12540,7 +12540,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+13.sha-cbd4206');
+var VERSION = new Version('7.0.3+14.sha-7a1b876');
 
 /**
  * @license
