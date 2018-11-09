aa3580d9610551027848305818774fa49b00ddd5
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:07:25 +0000

    fb0d93d docs: override code formatting for CLI commands (#26619)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+8.sha-682034f
+ * @license Angular v7.0.3+10.sha-fb0d93d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12540,7 +12540,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+8.sha-682034f');
+var VERSION = new Version('7.0.3+10.sha-fb0d93d');
 
 /**
  * @license
