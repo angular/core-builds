3e9c0a33a5f623eb3170efac574f2c6dd3149eec
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:26:39 +0000

    1810cdf fix(ivy): compiler should generate restoreView() for local refs in listeners (#27034)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+21.sha-e5c9f7a
+ * @license Angular v7.1.0-beta.2+24.sha-1810cdf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13445,7 +13445,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+21.sha-e5c9f7a');
+var VERSION = new Version('7.1.0-beta.2+24.sha-1810cdf');
 
 /**
  * @license
