53e6ff37065052265c092b8fabdb455568c188ea
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:22:49 +0000

    f80c600 docs: Fix wrong quoting in SSR guide (#27057) PR Close #27057

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+22.sha-99c5db1
+ * @license Angular v7.1.0-beta.2+26.sha-f80c600
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13527,7 +13527,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+22.sha-99c5db1');
+var VERSION = new Version('7.1.0-beta.2+26.sha-f80c600');
 
 /**
  * @license
