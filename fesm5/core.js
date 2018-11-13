93d2fdcb9b7ba142129593cd2a565ed9477225f6
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:21:28 +0000

    6b1780d build: avoid writing `\r` in windows (#26888)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+30.sha-f5a0ec0
+ * @license Angular v7.1.0-beta.2+33.sha-6b1780d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13527,7 +13527,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+30.sha-f5a0ec0');
+var VERSION = new Version('7.1.0-beta.2+33.sha-6b1780d');
 
 /**
  * @license
