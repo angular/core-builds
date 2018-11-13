df71e1cbccc6eafa6110b7c1fb22dd5673773fc5
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:12:52 +0000

    bff5c24 docs: fix typo (#26878) PR Close #26878

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+17.sha-53197de
+ * @license Angular v7.0.3+18.sha-bff5c24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15383,7 +15383,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.3+17.sha-53197de');
+const VERSION = new Version('7.0.3+18.sha-bff5c24');
 
 /**
  * @fileoverview added by tsickle
