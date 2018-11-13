e7a1136378629dd159a55801b7454dabf5812330
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:23:34 +0000

    efa443b docs: fix typo (#26878) PR Close #26878

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+33.sha-6b1780d
+ * @license Angular v7.1.0-beta.2+31.sha-efa443b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13481,7 +13481,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.0-beta.2+33.sha-6b1780d');
+    var VERSION = new Version('7.1.0-beta.2+31.sha-efa443b');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+33.sha-6b1780d
+ * @license Angular v7.1.0-beta.2+31.sha-efa443b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -16512,7 +16512,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+33.sha-6b1780d');
+const VERSION = new Version('7.1.0-beta.2+31.sha-efa443b');
 
 /**
  * @fileoverview added by tsickle
