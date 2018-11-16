6516a6b3bb3513b19b2f169c2259ed5a963f57ac
Misko Hevery <misko@hevery.com>
2018-11-16 20:50:45 +0000

    a4934a7 docs: fix code examples style (#26573)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+10.sha-91bffa9
+ * @license Angular v7.1.0-rc.0+11.sha-a4934a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __extends, __param, __read, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -14247,7 +14247,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+10.sha-91bffa9');
+var VERSION = new Version('7.1.0-rc.0+11.sha-a4934a7');
 
 /**
  * @license
