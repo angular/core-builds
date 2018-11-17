7f71907683fb43830720cdf9867d5ac6b84096bf
Misko Hevery <misko@hevery.com>
2018-11-17 21:29:43 +0000

    81e975a build: update to latest Bazel rules_typescript (#27138)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+15.sha-20ea5b5
+ * @license Angular v7.1.0-rc.0+16.sha-81e975a.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17553,7 +17553,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-rc.0+15.sha-20ea5b5');
+const VERSION = new Version('7.1.0-rc.0+16.sha-81e975a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
