4bb830ad17c78b529ef83307bd15ae266bc48ea1
Miško Hevery <misko@hevery.com>
2018-11-21 17:45:11 +0000

    573fb78 docs: add formatting productivity hints to `DEVELOPER.md` (#27211)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+33.sha-d666370
+ * @license Angular v7.1.0-rc.0+34.sha-573fb78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __extends, __param, __read, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -9867,7 +9867,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+33.sha-d666370');
+var VERSION = new Version('7.1.0-rc.0+34.sha-573fb78');
 
 /**
  * @license
