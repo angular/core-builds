ec532bf70ab8a9acd8d3531bf223cbb517f66b0b
Filipe Silva <filipematossilva@gmail.com>
2018-11-23 11:02:09 +0000

    e201a67 ci: test ts-api-guardian on windows

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+17.sha-d3ddd31.with-local-changes
+ * @license Angular v7.1.0-rc.0+17.sha-e201a67.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __extends, __param, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -14337,7 +14337,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+17.sha-d3ddd31.with-local-changes');
+var VERSION = new Version('7.1.0-rc.0+17.sha-e201a67.with-local-changes');
 
 /**
  * @license
