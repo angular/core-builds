2b8138a55e48f53bb376bc33c16d1a45c0dd1c03
Jason Aden <jasonaden@google.com>
2018-11-28 18:58:31 +0000

    8282e15 release: cut the v7.1.1 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+5.sha-bdf5f3e
+ * @license Angular v7.1.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12373,7 +12373,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+5.sha-bdf5f3e');
+const VERSION = new Version('7.1.1');
 
 /**
  * @fileoverview added by tsickle
