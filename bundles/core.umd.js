acc0eb39229aaf311c7c0e0b13276db33a1814aa
Igor Minar <igor@angular.io>
2018-11-30 22:04:18 +0000

    1ddc34c build(bazel): update handling of rules_nodejs transitive deps (#27264)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+14.sha-fc2c23e
+ * @license Angular v7.1.1+13.sha-1ddc34c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.1+14.sha-fc2c23e');
+    var VERSION = new Version('7.1.1+13.sha-1ddc34c');
 
     /**
      * @license
