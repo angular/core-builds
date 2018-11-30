992b7865767df9cfd786aa8797e3bf3bca09c9d6
Igor Minar <igor@angular.io>
2018-11-30 06:10:07 +0000

    419fa17 docs: move past events on events page (#27342)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+49.sha-e024f2f
+ * @license Angular v7.1.0+51.sha-419fa17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9980,7 +9980,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+49.sha-e024f2f');
+var VERSION = new Version('7.1.0+51.sha-419fa17');
 
 /**
  * @license
