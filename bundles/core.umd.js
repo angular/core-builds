b2c0b435457dd73f121b9b6a1c5259841b01ac1e
Igor Minar <igor@angular.io>
2018-11-30 22:37:01 +0000

    c96dea2 test(ivy): turn ivy test selectors into real functions (#27372)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+82.sha-eb17502
+ * @license Angular v7.1.0+84.sha-c96dea2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9998,7 +9998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.0+82.sha-eb17502');
+    var VERSION = new Version('7.1.0+84.sha-c96dea2');
 
     /**
      * @license
