bdd4a64e6ad74471498ac0ef66c48dadd7735e10
Igor Minar <igor@angular.io>
2018-11-30 22:06:44 +0000

    c8a8dcf build(bazel): update handling of rules_nodejs transitive deps (#27264)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+83.sha-1fa5478
+ * @license Angular v7.1.0+81.sha-c8a8dcf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+83.sha-1fa5478');
+var VERSION = new Version('7.1.0+81.sha-c8a8dcf');
 
 /**
  * @license
