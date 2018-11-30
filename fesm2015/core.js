f6c66bad68dbf5b107f3df5d0061d095b0668611
Igor Minar <igor@angular.io>
2018-11-30 06:14:02 +0000

    23bc8ed test: move remaining fixmeIvy to test level (#27354)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+56.sha-36e7bf1
+ * @license Angular v7.1.0+57.sha-23bc8ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12444,7 +12444,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+56.sha-36e7bf1');
+const VERSION = new Version('7.1.0+57.sha-23bc8ed');
 
 /**
  * @fileoverview added by tsickle
