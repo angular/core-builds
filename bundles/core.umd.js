b8a63987507a3a0303079473fa373f946505864d
Igor Minar <igor@angular.io>
2018-11-30 06:34:56 +0000

    2cce8d4 docs: fix typo (#27294)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+9.sha-a149c3a
+ * @license Angular v7.1.1+10.sha-2cce8d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.1+9.sha-a149c3a');
+    var VERSION = new Version('7.1.1+10.sha-2cce8d4');
 
     /**
      * @license
