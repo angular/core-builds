572181dd82d3719aa0cb976361e83582ea19394b
Igor Minar <igor@angular.io>
2018-11-30 06:09:47 +0000

    2d51579 docs: move Minko to Angular (#27339)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+50.sha-8ceea0f
+ * @license Angular v7.1.0+52.sha-2d51579
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9990,7 +9990,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+50.sha-8ceea0f');
+var VERSION = new Version('7.1.0+52.sha-2d51579');
 
 /**
  * @license
