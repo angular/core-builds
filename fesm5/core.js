969920b0617fc307d0fe59944c577c68a06e5397
Igor Minar <igor@angular.io>
2018-11-30 06:10:14 +0000

    653eb59 build: add kara to approvers for packages needing ivy TestBed updates (#27337)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+51.sha-419fa17
+ * @license Angular v7.1.0+53.sha-653eb59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9980,7 +9980,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+51.sha-419fa17');
+var VERSION = new Version('7.1.0+53.sha-653eb59');
 
 /**
  * @license
