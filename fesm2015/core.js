b22385318036de5daf9ebfd706c6306bf9c1abcb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:07:57 +0000

    06d4a0c feat(bazel): ng-new schematics with Bazel (#27277)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+112.sha-7ec05b4
+ * @license Angular v7.1.0+113.sha-06d4a0c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12543,7 +12543,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+112.sha-7ec05b4');
+const VERSION = new Version('7.1.0+113.sha-06d4a0c');
 
 /**
  * @fileoverview added by tsickle
