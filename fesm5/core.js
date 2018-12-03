bfb5d0c1db7d81c3a1b56337b729f88e16aee060
Igor Minar <igor@angular.io>
2018-12-03 16:48:54 +0000

    1022db1 test(common): add PercentPipe round and trim tests (#27365)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+13.sha-1ddc34c
+ * @license Angular v7.1.1+17.sha-1022db1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9888,7 +9888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+13.sha-1ddc34c');
+var VERSION = new Version('7.1.1+17.sha-1022db1');
 
 /**
  * @license
