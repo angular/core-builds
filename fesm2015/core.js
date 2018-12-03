041caec6fa71a7c0ed82ae2321a26637940f2764
Filipe Silva <filipematossilva@gmail.com>
2018-12-03 19:56:07 +0000

    2f888b8 ci: test ts-api-guardian on windows

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+103.sha-4fd3402
+ * @license Angular v7.1.0+103.sha-2f888b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12487,7 +12487,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+103.sha-4fd3402');
+const VERSION = new Version('7.1.0+103.sha-2f888b8');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+103.sha-4fd3402
+ * @license Angular v7.1.0+103.sha-2f888b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10008,7 +10008,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+103.sha-4fd3402');
+var VERSION = new Version('7.1.0+103.sha-2f888b8');
 
 /**
  * @license
