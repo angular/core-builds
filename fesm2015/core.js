4a6852cf8e4ac634f8df099435cd0f3500e5c52b
Igor Minar <igor@angular.io>
2018-12-03 16:50:43 +0000

    0378792 docs: fix typo in toh-pt6.md (#27403)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+17.sha-1022db1
+ * @license Angular v7.1.1+18.sha-0378792
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12373,7 +12373,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.1+17.sha-1022db1');
+const VERSION = new Version('7.1.1+18.sha-0378792');
 
 /**
  * @fileoverview added by tsickle
