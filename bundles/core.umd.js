1b83e528472a58c081b23c8e5afd0203158d2eb5
Igor Minar <igor@angular.io>
2018-12-04 22:33:16 +0000

    8087b6b fix(compiler-cli): flatModuleIndex files not generated on windows with multiple input files (#27200)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+18.sha-0378792
+ * @license Angular v7.1.1+23.sha-8087b6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.1+18.sha-0378792');
+    var VERSION = new Version('7.1.1+23.sha-8087b6b');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+18.sha-0378792
+ * @license Angular v7.1.1+23.sha-8087b6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12373,7 +12373,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.1+18.sha-0378792');
+const VERSION = new Version('7.1.1+23.sha-8087b6b');
 
 /**
  * @fileoverview added by tsickle
