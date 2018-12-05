f11124d46e7b5d60a5289ece4578687ca397213b
Igor Minar <igor@angular.io>
2018-12-05 19:07:55 +0000

    20cef50 fix(router): process empty path segment when preceding a named outlet (#26243)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+150.sha-4b9948c
+ * @license Angular v7.1.0+153.sha-20cef50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12678,7 +12678,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+150.sha-4b9948c');
+const VERSION = new Version('7.1.0+153.sha-20cef50');
 
 /**
  * @fileoverview added by tsickle
