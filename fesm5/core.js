d557608e52ee5d4b1355ddefa33fbc8766abffb4
Igor Minar <igor@angular.io>
2018-12-05 04:49:44 +0000

    711ef2d build: update to newest ibazel (#27441)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+25.sha-5c57920
+ * @license Angular v7.1.1+26.sha-711ef2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9888,7 +9888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+25.sha-5c57920');
+var VERSION = new Version('7.1.1+26.sha-711ef2d');
 
 /**
  * @license
