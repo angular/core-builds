ccd6ee9d5dd7095da3812934664db28b46e79118
Igor Minar <igor@angular.io>
2018-12-05 19:09:07 +0000

    20cef50 fix(router): process empty path segment when preceding a named outlet (#26243)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+150.sha-4b9948c
+ * @license Angular v7.1.0+153.sha-20cef50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10216,7 +10216,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.0+150.sha-4b9948c');
+    var VERSION = new Version('7.1.0+153.sha-20cef50');
 
     /**
      * @license
