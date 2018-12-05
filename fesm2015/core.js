c9e2a9de6deac5abb80b4e727f48102e39a91f2e
Igor Minar <igor@angular.io>
2018-12-05 21:15:31 +0000

    24ebdbd ci: test ts-api-guardian on windows (#27205)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+163.sha-821fecb
+ * @license Angular v7.1.0+164.sha-24ebdbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12775,7 +12775,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+163.sha-821fecb');
+const VERSION = new Version('7.1.0+164.sha-24ebdbd');
 
 /**
  * @fileoverview added by tsickle
