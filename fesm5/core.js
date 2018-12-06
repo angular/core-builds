96e42917f6a21357893ddb203b790bfc9c099841
Igor Minar <igor@angular.io>
2018-12-06 19:49:45 +0000

    7f221d8 build: introduce remote bazel caching (#27358)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+200.sha-d63d53f
+ * @license Angular v7.1.0+201.sha-7f221d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10220,7 +10220,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+200.sha-d63d53f');
+var VERSION = new Version('7.1.0+201.sha-7f221d8');
 
 /**
  * @license
