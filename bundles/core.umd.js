dc3149fc2db2ecbc66bf5e9ed198920a90c30ac2
Igor Minar <igor@angular.io>
2018-12-06 17:47:09 +0000

    84f2928 fix(compiler-cli): ngtsc shim files not being generated on case-insensitive platforms (#27466)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+53.sha-d7c72fc
+ * @license Angular v7.1.1+56.sha-84f2928
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.1+53.sha-d7c72fc');
+    var VERSION = new Version('7.1.1+56.sha-84f2928');
 
     /**
      * @license
