3d1e6d7a66f015ad685c9a993cf0cc6cff72be46
Igor Minar <igor@angular.io>
2018-12-06 17:52:20 +0000

    ca1c430 fix(compiler-cli): ngtsc shim files not being generated on case-insensitive platforms (#27466)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+196.sha-091a504
+ * @license Angular v7.1.0+197.sha-ca1c430
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10203,7 +10203,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+196.sha-091a504');
+var VERSION = new Version('7.1.0+197.sha-ca1c430');
 
 /**
  * @license
