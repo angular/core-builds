e2a89c379d6150855cf2207fd9ed8ac0f055cd11
Igor Minar <igor@angular.io>
2018-12-06 22:10:38 +0000

    cad6714 fix(ivy): "select" attribute on <ng-content> should not be case-sensitive (FW-789) (#27500)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+209.sha-913563a
+ * @license Angular v7.1.0+207.sha-cad6714
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12715,7 +12715,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+209.sha-913563a');
+const VERSION = new Version('7.1.0+207.sha-cad6714');
 
 /**
  * @fileoverview added by tsickle
