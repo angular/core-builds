c77bace30c90e3a4bd66a0d46f604e1887c4e22b
Igor Minar <igor@angular.io>
2018-12-06 22:11:10 +0000

    f013c57 test(router): correct a wrong test (FW-766) (#27511)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+206.sha-c71d7b5
+ * @license Angular v7.1.0+210.sha-f013c57
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10217,7 +10217,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+206.sha-c71d7b5');
+var VERSION = new Version('7.1.0+210.sha-f013c57');
 
 /**
  * @license
