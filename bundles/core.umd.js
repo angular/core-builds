5102692d508cd7c0c4fc0c2365a7cd0e303b4369
Miško Hevery <misko@hevery.com>
2018-12-06 21:43:42 +0000

    3fa2a5f fix(ivy): add missing AttributeMarker export

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+163.sha-6fa5383
+ * @license Angular v7.1.0+202.sha-3fa2a5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10265,7 +10265,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.0+163.sha-6fa5383');
+    var VERSION = new Version('7.1.0+202.sha-3fa2a5f');
 
     /**
      * @license
