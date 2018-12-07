03726e01ce0ce0fad45e92c20c7f82ffdf971fa0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:16:22 +0000

    b0c7561 fix(forms): typed argument for FormBuilder group (#26985)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+209.sha-913563a
+ * @license Angular v7.2.0-beta.1+12.sha-b0c7561
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10234,7 +10234,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+209.sha-913563a');
+var VERSION = new Version('7.2.0-beta.1+12.sha-b0c7561');
 
 /**
  * @license
