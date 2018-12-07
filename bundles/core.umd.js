2709e28f2dce10c6434624a1053fc0aac9c2be0a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 17:56:04 +0000

    74346e9 style: run latest buildifier binary (#27489)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2
+ * @license Angular v7.1.2+3.sha-74346e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.2');
+    var VERSION = new Version('7.1.2+3.sha-74346e9');
 
     /**
      * @license
