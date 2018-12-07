2a0677d8516363cf9611cc11dec3157ad1e45b26
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:03:17 +0000

    2a35471 test(upgrade): add diagnosis to failing ivy tests (#27542)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+9.sha-15ca1bb
+ * @license Angular v7.2.0-beta.1+11.sha-2a35471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10247,7 +10247,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-beta.1+9.sha-15ca1bb');
+    var VERSION = new Version('7.2.0-beta.1+11.sha-2a35471');
 
     /**
      * @license
