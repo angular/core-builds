2f777e3d3b292b746d7421c22a47ed41e778789e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:01:17 +0000

    f6a67c9 ci: update ngbot config to add merge linked labels (#27512) PR Close #27512

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+6.sha-dcb44e6
+ * @license Angular v7.2.0-beta.1+7.sha-f6a67c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12715,7 +12715,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+6.sha-dcb44e6');
+const VERSION = new Version('7.2.0-beta.1+7.sha-f6a67c9');
 
 /**
  * @fileoverview added by tsickle
