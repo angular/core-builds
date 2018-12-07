49d5f8a27ac1586864fc30751edcc45641157757
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 22:56:29 +0000

    44dd764 build: update to Bazel 0.20 (#27394)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+14.sha-84ad0cc
+ * @license Angular v7.2.0-beta.1+16.sha-44dd764
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5793,7 +5793,7 @@ function getRenderFlags(view) {
 //////////////////////////
 var _currentNamespace = null;
 function namespaceSVG() {
-    _currentNamespace = 'http://www.w3.org/2000/svg/';
+    _currentNamespace = 'http://www.w3.org/2000/svg';
 }
 function namespaceMathML() {
     _currentNamespace = 'http://www.w3.org/1998/MathML/';
@@ -10234,7 +10234,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+14.sha-84ad0cc');
+var VERSION = new Version('7.2.0-beta.1+16.sha-44dd764');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.1+14.sha-84ad0cc
+ * @license Angular v7.2.0-beta.1+16.sha-44dd764
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, ɵstringify, Component, Directive, NgModule, Pipe, ɵReflectionCapabilities, InjectionToken, ApplicationInitStatus, Injector, NgZone, ɵRender3ComponentFactory, ɵRender3NgModuleRef, ɵcompileComponent, ɵcompileDirective, ɵcompileNgModuleDefs, ɵcompilePipe, ɵgetInjectableDef, ɵpatchComponentDefWithScope, Compiler, Injectable, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵivyEnabled, ɵoverrideComponentView, ɵoverrideProvider } from '@angular/core';
-import { __spread, __decorate, __values, __extends, __read } from 'tslib';
+import { __spread, __decorate, __values, __read, __extends } from 'tslib';
 
 /**
  * @license
