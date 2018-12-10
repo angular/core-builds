989551ac3ba3839ab57439bef95eee7becf839ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 22:48:12 +0000

    16d26e5 docs: add webucator to resources (#26895)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+20.sha-f5c0b30
+ * @license Angular v7.2.0-beta.1+21.sha-16d26e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10224,7 +10224,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+20.sha-f5c0b30');
+var VERSION = new Version('7.2.0-beta.1+21.sha-16d26e5');
 
 /**
  * @license
