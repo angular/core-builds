5c7e025edd05f66b39c383cc3de58163ef8ca041
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:54:06 +0000

    fb6a31c build: exclude ivy commit messages from the release notes (#27532)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+26.sha-3290fc3
+ * @license Angular v7.2.0-beta.1+27.sha-fb6a31c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12687,7 +12687,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+26.sha-3290fc3');
+const VERSION = new Version('7.2.0-beta.1+27.sha-fb6a31c');
 
 /**
  * @fileoverview added by tsickle
