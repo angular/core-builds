d5681467cbd511ec7d8aaf37024cf095f81cc61d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:58:21 +0000

    3cb6dad docs: update npm scripts to support angular 7 (#27334)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+29.sha-ceb14de
+ * @license Angular v7.2.0-beta.1+28.sha-3cb6dad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10224,7 +10224,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+29.sha-ceb14de');
+var VERSION = new Version('7.2.0-beta.1+28.sha-3cb6dad');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.1+29.sha-ceb14de
+ * @license Angular v7.2.0-beta.1+28.sha-3cb6dad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, ɵstringify, Component, Directive, NgModule, Pipe, ɵReflectionCapabilities, InjectionToken, ApplicationInitStatus, Injector, NgZone, ɵRender3ComponentFactory, ɵRender3NgModuleRef, ɵcompileComponent, ɵcompileDirective, ɵcompileNgModuleDefs, ɵcompilePipe, ɵgetInjectableDef, ɵpatchComponentDefWithScope, Compiler, Injectable, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵivyEnabled, ɵoverrideComponentView, ɵoverrideProvider } from '@angular/core';
-import { __spread, __decorate, __values, __read, __extends } from 'tslib';
+import { __spread, __values, __decorate, __extends, __read } from 'tslib';
 
 /**
  * @license
