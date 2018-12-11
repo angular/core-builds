b51a0dc1685bb6ce9344dc53193afd917fe80baa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:56:10 +0000

    ceb14de fix(ivy): proper slot allocation for pure functions in `hostBindings` function (#27587)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+27.sha-fb6a31c
+ * @license Angular v7.2.0-beta.1+29.sha-ceb14de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12687,7 +12687,7 @@ class Version {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+27.sha-fb6a31c');
+const VERSION = new Version('7.2.0-beta.1+29.sha-ceb14de');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+27.sha-fb6a31c
+ * @license Angular v7.2.0-beta.1+29.sha-ceb14de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10224,7 +10224,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+27.sha-fb6a31c');
+var VERSION = new Version('7.2.0-beta.1+29.sha-ceb14de');
 
 /**
  * @license
