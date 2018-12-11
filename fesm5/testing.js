a0e901d984f1316ad766a30ee46b53130862cc4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 20:49:32 +0000

    3290fc3 fix(bazel): fix TS errors in the `schematics/bazel-workspace` files (#27600)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.1+22.sha-05cdfb9
+ * @license Angular v7.2.0-beta.1+26.sha-3290fc3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, ɵstringify, Component, Directive, NgModule, Pipe, ɵReflectionCapabilities, InjectionToken, ApplicationInitStatus, Injector, NgZone, ɵRender3ComponentFactory, ɵRender3NgModuleRef, ɵcompileComponent, ɵcompileDirective, ɵcompileNgModuleDefs, ɵcompilePipe, ɵgetInjectableDef, ɵpatchComponentDefWithScope, Compiler, Injectable, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵivyEnabled, ɵoverrideComponentView, ɵoverrideProvider } from '@angular/core';
-import { __spread, __decorate, __values, __extends, __read } from 'tslib';
+import { __spread, __decorate, __values, __read, __extends } from 'tslib';
 
 /**
  * @license
