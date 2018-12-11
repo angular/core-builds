4f44f7b82810e5854aff8f3db238f046d112db92
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 22:31:51 +0000

    b69ec48 release: cut the v7.2.0-beta.2 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.1+33.sha-098a787
+ * @license Angular v7.2.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -10210,7 +10210,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+33.sha-098a787');
+var VERSION = new Version('7.2.0-beta.2');
 
 /**
  * @license
