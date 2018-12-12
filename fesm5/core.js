af0f23162858c45a22d9852099c6faa9eb0b181e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:18:46 +0000

    26c79ef build(bazel): fix postinstall-patches.js script run during yarn postinstall when running yarn_install (#27610)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+4.sha-0b8db92
+ * @license Angular v7.1.3+5.sha-26c79ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9888,7 +9888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+4.sha-0b8db92');
+var VERSION = new Version('7.1.3+5.sha-26c79ef');
 
 /**
  * @license
