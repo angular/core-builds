2b9081e6fff138c4a6631badb467cd550c5eab30
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 00:57:58 +0000

    93820b4 build: bump @bazel/ibazel to 0.9.0 (#27601)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2
+ * @license Angular v7.2.0-beta.2+2.sha-93820b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10210,7 +10210,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2');
+var VERSION = new Version('7.2.0-beta.2+2.sha-93820b4');
 
 /**
  * @license
