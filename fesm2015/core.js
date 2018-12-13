350899bb03bb1c2d4e3a16c5da0cc700a737bc08
Miško Hevery <misko@hevery.com>
2018-12-13 19:47:58 +0000

    e269274 build: ts-api-guardian tsconfig warning with bazel (#27583)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+18.sha-8313ffc
+ * @license Angular v7.2.0-beta.2+19.sha-e269274
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13038,7 +13038,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+18.sha-8313ffc');
+const VERSION = new Version('7.2.0-beta.2+19.sha-e269274');
 
 /**
  * @fileoverview added by tsickle
