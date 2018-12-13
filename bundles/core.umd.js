f28bf0a2ee8187d00b31d9bdebf90111b8919c21
Miško Hevery <misko@hevery.com>
2018-12-13 19:46:15 +0000

    1cc08b4 fix(bazel): fix major/minor semver check between @angular/bazel npm packager version and angular bazel repo version (#27635)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+12.sha-f00af3e
+ * @license Angular v7.2.0-beta.2+14.sha-1cc08b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10241,7 +10241,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-beta.2+12.sha-f00af3e');
+    var VERSION = new Version('7.2.0-beta.2+14.sha-1cc08b4');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+12.sha-f00af3e
+ * @license Angular v7.2.0-beta.2+14.sha-1cc08b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13117,7 +13117,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+12.sha-f00af3e');
+const VERSION = new Version('7.2.0-beta.2+14.sha-1cc08b4');
 
 /**
  * @fileoverview added by tsickle
