fcbf2b9f38f9f8af092944df3ee4030614973540
Miško Hevery <misko@hevery.com>
2018-12-13 19:28:55 +0000

    97eb858 build: update to latest karma-sauce-launcher version (#27634)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+9.sha-1059789
+ * @license Angular v7.1.3+10.sha-97eb858
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.3+9.sha-1059789');
+    var VERSION = new Version('7.1.3+10.sha-97eb858');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+9.sha-1059789
+ * @license Angular v7.1.3+10.sha-97eb858
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9888,7 +9888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+9.sha-1059789');
+var VERSION = new Version('7.1.3+10.sha-97eb858');
 
 /**
  * @license
