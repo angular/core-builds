38111d4fcd2f437e042aa814f20cf493a3985aca
Miško Hevery <misko@hevery.com>
2018-12-13 19:47:29 +0000

    8313ffc fix(bazel): Load http_archive and rules_nodejs dependencies (#27609)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+13.sha-e318081
+ * @license Angular v7.2.0-beta.2+18.sha-8313ffc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10228,7 +10228,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+13.sha-e318081');
+var VERSION = new Version('7.2.0-beta.2+18.sha-8313ffc');
 
 /**
  * @license
