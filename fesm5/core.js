f7caae34ebce9c69550a284ffb3fafbf05a45324
Miško Hevery <misko@hevery.com>
2018-12-13 19:48:22 +0000

    e318081 ci: remove packages/bazel from compiler-cli group (#27641)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+17.sha-ffe1b4d
+ * @license Angular v7.2.0-beta.2+13.sha-e318081
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10211,7 +10211,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+17.sha-ffe1b4d');
+var VERSION = new Version('7.2.0-beta.2+13.sha-e318081');
 
 /**
  * @license
