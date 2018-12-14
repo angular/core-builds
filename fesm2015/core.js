2287e99d56d81921d840d896f8440f638a7f9b0e
Miško Hevery <misko@hevery.com>
2018-12-14 23:26:10 +0000

    522919a fix(bazel): emit full node stack traces when Angular compilation crashes (#27678)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+30.sha-c6ae729
+ * @license Angular v7.2.0-beta.2+41.sha-522919a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12156,8 +12156,12 @@ class ViewRef {
         if (this._appRef) {
             this._appRef.detachView(this);
         }
-        else if (this._viewContainerRef && viewAttached(this._lView)) {
-            this._viewContainerRef.detach(this._viewContainerRef.indexOf(this));
+        else if (this._viewContainerRef) {
+            /** @type {?} */
+            const index = this._viewContainerRef.indexOf(this);
+            if (index > -1) {
+                this._viewContainerRef.detach(index);
+            }
             this._viewContainerRef = null;
         }
         destroyLView(this._lView);
@@ -13038,7 +13042,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+30.sha-c6ae729');
+const VERSION = new Version('7.2.0-beta.2+41.sha-522919a');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+30.sha-c6ae729
+ * @license Angular v7.2.0-beta.2+41.sha-522919a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9490,8 +9490,11 @@ var ViewRef = /** @class */ (function () {
         if (this._appRef) {
             this._appRef.detachView(this);
         }
-        else if (this._viewContainerRef && viewAttached(this._lView)) {
-            this._viewContainerRef.detach(this._viewContainerRef.indexOf(this));
+        else if (this._viewContainerRef) {
+            var index = this._viewContainerRef.indexOf(this);
+            if (index > -1) {
+                this._viewContainerRef.detach(index);
+            }
             this._viewContainerRef = null;
         }
         destroyLView(this._lView);
@@ -10228,7 +10231,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+30.sha-c6ae729');
+var VERSION = new Version('7.2.0-beta.2+41.sha-522919a');
 
 /**
  * @license
