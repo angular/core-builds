b3efa539bfb6c8ec9e8ede050d59cbc5b6e1821f
Miško Hevery <misko@hevery.com>
2018-12-14 18:49:47 +0000

    f5e7208 build: re-enable saucelabs non-verbose logging (#27657)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+15.sha-52fd4a2
+ * @license Angular v7.1.3+20.sha-f5e7208
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9888,7 +9888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+15.sha-52fd4a2');
+var VERSION = new Version('7.1.3+20.sha-f5e7208');
 
 /**
  * @license
