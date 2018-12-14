96617369bb325b0528b453856d9938bf895dbc7b
Miško Hevery <misko@hevery.com>
2018-12-14 18:55:31 +0000

    63d395c build: restrict visibility of npm_package targets (#27611)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+22.sha-fe8c6b0
+ * @license Angular v7.1.3+21.sha-63d395c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9888,7 +9888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+22.sha-fe8c6b0');
+var VERSION = new Version('7.1.3+21.sha-63d395c');
 
 /**
  * @license
