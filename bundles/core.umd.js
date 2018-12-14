40cabc6fec9df9b0ea3d55e8742c4fb4b2cb1071
Miško Hevery <misko@hevery.com>
2018-12-14 23:56:20 +0000

    453589f fix(animations): mark actual descendant node as disabled (#26180)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+28.sha-cd1e206
+ * @license Angular v7.1.3+27.sha-453589f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.1.3+28.sha-cd1e206');
+    var VERSION = new Version('7.1.3+27.sha-453589f');
 
     /**
      * @license
