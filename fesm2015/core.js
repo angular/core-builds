f056401595ebbcca7f868db919ed23b03f489838
Miško Hevery <misko@hevery.com>
2018-12-14 18:37:18 +0000

    ad26cd6 fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27523)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+10.sha-7fabe44
+ * @license Angular v7.2.0-beta.2+26.sha-ad26cd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13038,7 +13038,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+10.sha-7fabe44');
+const VERSION = new Version('7.2.0-beta.2+26.sha-ad26cd6');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+10.sha-7fabe44
+ * @license Angular v7.2.0-beta.2+26.sha-ad26cd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10228,7 +10228,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+10.sha-7fabe44');
+var VERSION = new Version('7.2.0-beta.2+26.sha-ad26cd6');
 
 /**
  * @license
