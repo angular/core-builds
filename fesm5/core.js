1320ec9640962ff1bd8e48eed489cabd4cc12180
Miško Hevery <misko@hevery.com>
2018-12-18 18:03:34 +0000

    8d0a3ce build: use https link to editorconfig.org in .editorconfig (#27664)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+76.sha-f57916c
+ * @license Angular v7.2.0-beta.2+80.sha-8d0a3ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10271,7 +10271,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+76.sha-f57916c');
+var VERSION = new Version('7.2.0-beta.2+80.sha-8d0a3ce');
 
 /**
  * @license
