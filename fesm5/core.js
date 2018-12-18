28a69116452ee3e332970bfb761745c05c9dcbe4
Miško Hevery <misko@hevery.com>
2018-12-18 18:02:10 +0000

    d766ad0 build: update gulp-clang-format dependency (#27712)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+75.sha-e98c57a
+ * @license Angular v7.2.0-beta.2+77.sha-d766ad0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10271,7 +10271,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+75.sha-e98c57a');
+var VERSION = new Version('7.2.0-beta.2+77.sha-d766ad0');
 
 /**
  * @license
