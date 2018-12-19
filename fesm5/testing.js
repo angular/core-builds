c2cc325f35e1270a6808f25823906eeeb41a929b
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:43:41 +0000

    0604527 Revert "build: update gulp-clang-format dependency (#27712)" (#27759)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-rc.0+3.sha-8f8572f
+ * @license Angular v7.2.0-rc.0+4.sha-0604527
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { RendererFactory2, getDebugNode, ɵstringify, Component, Directive, NgModule, Pipe, ɵReflectionCapabilities, InjectionToken, ApplicationInitStatus, Injector, NgZone, ɵRender3ComponentFactory, ɵRender3NgModuleRef, ɵcompileComponent, ɵcompileDirective, ɵcompileNgModuleDefs, ɵcompilePipe, ɵgetInjectableDef, ɵpatchComponentDefWithScope, ɵresetCompiledComponents, Compiler, Injectable, Optional, SkipSelf, ɵAPP_ROOT, ɵclearOverrides, ɵivyEnabled, ɵoverrideComponentView, ɵoverrideProvider } from '@angular/core';
-import { __spread, __decorate, __values, __read, __extends } from 'tslib';
+import { __spread, __values, __decorate, __extends, __read } from 'tslib';
 
 /**
  * @license
