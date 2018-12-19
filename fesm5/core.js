afed6d5d17c405541948a164978d636b08bfa796
Miško Hevery <misko@hevery.com>
2018-12-19 00:31:49 +0000

    f48a00f release: cut the v7.2.0-rc.0 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+92.sha-24fa0b7
+ * @license Angular v7.2.0-rc.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10299,7 +10299,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+92.sha-24fa0b7');
+var VERSION = new Version('7.2.0-rc.0');
 
 /**
  * @license
