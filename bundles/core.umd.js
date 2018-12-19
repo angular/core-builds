b728d20277cdaa2b0c856341ffe0f496d8024033
Igor Minar <igor@angular.io>
2018-12-19 18:24:37 +0000

    01a5c74 turns out that github silently converts '/ in team names to '-'

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+77.sha-4d71c4c
+ * @license Angular v7.2.0-beta.2+78.sha-01a5c74
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10333,7 +10333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-beta.2+77.sha-4d71c4c');
+    var VERSION = new Version('7.2.0-beta.2+78.sha-01a5c74');
 
     /**
      * @license
