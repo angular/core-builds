98cc98caf3f7972a10a6725331bcc9949eceee3f
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:41:20 +0000

    880e8a5 refactor: fix broken formatting

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+11.sha-a833b98
+ * @license Angular v7.2.0-rc.0+13.sha-880e8a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10352,7 +10352,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+11.sha-a833b98');
+    var VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
 
     /**
      * @license
