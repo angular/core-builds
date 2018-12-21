34586aad05152edf99cc4770d66b09eca985fb76
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 22:28:05 +0000

    70c184b build: update to latest karma version (#27735)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+10.sha-0ae1e93
+ * @license Angular v7.2.0-rc.0+12.sha-70c184b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10344,7 +10344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+10.sha-0ae1e93');
+    var VERSION = new Version('7.2.0-rc.0+12.sha-70c184b');
 
     /**
      * @license
