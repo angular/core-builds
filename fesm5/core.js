03596357f9bc7737211bdc1f414e8953314ea686
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 20:36:16 +0000

    5da55d6 test(ivy): add root cause analysis for failing router tests (#27792)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+19.sha-50a91ba
+ * @license Angular v7.2.0-rc.0+20.sha-5da55d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10290,7 +10290,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+19.sha-50a91ba');
+var VERSION = new Version('7.2.0-rc.0+20.sha-5da55d6');
 
 /**
  * @license
