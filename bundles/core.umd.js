46bbbecc049aa49565035027994b909bbf438a37
Ben Lesh <ben@benlesh.com>
2019-01-03 18:02:39 +0000

    061615c test(docs-infra): increase timeout for redirection to external URL (#27903)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+18.sha-e9de47c
+ * @license Angular v7.2.0-rc.0+17.sha-061615c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10344,7 +10344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+18.sha-e9de47c');
+    var VERSION = new Version('7.2.0-rc.0+17.sha-061615c');
 
     /**
      * @license
