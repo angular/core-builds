e87eba0e3789679ff06e583cde95c8de8e409c3b
Ben Lesh <ben@benlesh.com>
2019-01-03 18:15:43 +0000

    59f64dd docs(service-worker): update service worker configuration doc (#27868)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+38.sha-27431e0
+ * @license Angular v7.2.0-rc.0+39.sha-59f64dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13747,7 +13747,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+38.sha-27431e0');
+const VERSION = new Version('7.2.0-rc.0+39.sha-59f64dd');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+38.sha-27431e0
+ * @license Angular v7.2.0-rc.0+39.sha-59f64dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10698,7 +10698,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+38.sha-27431e0');
+var VERSION = new Version('7.2.0-rc.0+39.sha-59f64dd');
 
 /**
  * @license
