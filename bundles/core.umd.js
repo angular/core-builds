9d394ae4bab21b932d94ca37e0f7fa39bccbc94d
Ben Lesh <ben@benlesh.com>
2019-01-03 18:16:43 +0000

    791d865 fix(docs-infra): upgrade to latest dgeni-packages to fix linking problem (#27864)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+23.sha-5aecb2d
+ * @license Angular v7.2.0-rc.0+25.sha-791d865
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10344,7 +10344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+23.sha-5aecb2d');
+    var VERSION = new Version('7.2.0-rc.0+25.sha-791d865');
 
     /**
      * @license
