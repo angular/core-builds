1d180803f3e19ee99b6eb7c6161c18e962a9cc8c
Ben Lesh <ben@benlesh.com>
2019-01-03 18:19:54 +0000

    7df9040 docs: add missing comma to environment config in build guide (#27476)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+44.sha-1dc95c4
+ * @license Angular v7.2.0-rc.0+42.sha-7df9040
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10698,7 +10698,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+44.sha-1dc95c4');
+var VERSION = new Version('7.2.0-rc.0+42.sha-7df9040');
 
 /**
  * @license
