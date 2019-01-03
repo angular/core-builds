d20a2834b8e4710b01f6cb496fb12c03b6d03101
Ben Lesh <ben@benlesh.com>
2019-01-03 18:17:31 +0000

    a0a3648 docs: add more info on what is being cached (#27343)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+43.sha-a0a3648
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10760,7 +10760,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+40.sha-755c809');
+    var VERSION = new Version('7.2.0-rc.0+43.sha-a0a3648');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+43.sha-a0a3648
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13767,7 +13767,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+40.sha-755c809');
+const VERSION = new Version('7.2.0-rc.0+43.sha-a0a3648');
 
 /**
  * @fileoverview added by tsickle
