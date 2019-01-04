246963ed99b3af97ccfb486bdd37668c07258292
Kara Erickson <karakara@google.com>
2019-01-04 21:01:36 +0000

    13d23f3 fix(ivy): ngtsc program emit ignoring custom transformers (#27837)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+59.sha-48555f9
+ * @license Angular v7.2.0-rc.0+60.sha-13d23f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __spread, __extends, __param, __read, __assign, __values } from 'tslib';
+import { __decorate, __metadata, __spread, __param, __extends, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -10707,7 +10707,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+59.sha-48555f9');
+var VERSION = new Version('7.2.0-rc.0+60.sha-13d23f3');
 
 /**
  * @license
