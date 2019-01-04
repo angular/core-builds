531a1c668c71085504c740be2449a1870f5dad41
Kara Erickson <karakara@google.com>
2019-01-04 23:21:36 +0000

    fb7816f fix(ivy): element injector on the host of a component should see viewProviders of that component (#27928)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+62.sha-5d3dcfc
+ * @license Angular v7.2.0-rc.0+63.sha-fb7816f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2848,7 +2848,7 @@ function leaveView(newView) {
  * ```
  * @type {?}
  */
-let includeViewProviders = false;
+let includeViewProviders = true;
 /**
  * @param {?} v
  * @return {?}
@@ -13808,7 +13808,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+62.sha-5d3dcfc');
+const VERSION = new Version('7.2.0-rc.0+63.sha-fb7816f');
 
 /**
  * @fileoverview added by tsickle
