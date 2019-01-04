2ef8fbeaf4c18b88301166d5fd11c2cfbfe611bb
Kara Erickson <karakara@google.com>
2019-01-04 23:22:48 +0000

    fb7816f fix(ivy): element injector on the host of a component should see viewProviders of that component (#27928)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+62.sha-5d3dcfc
+ * @license Angular v7.2.0-rc.0+63.sha-fb7816f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2341,7 +2341,7 @@
      *
      * ```
      */
-    var includeViewProviders = false;
+    var includeViewProviders = true;
     function setIncludeViewProviders(v) {
         var oldValue = includeViewProviders;
         includeViewProviders = v;
@@ -10751,7 +10751,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+62.sha-5d3dcfc');
+    var VERSION = new Version('7.2.0-rc.0+63.sha-fb7816f');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+62.sha-5d3dcfc
+ * @license Angular v7.2.0-rc.0+63.sha-fb7816f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __assign, __read, __values } from 'tslib';
+import { __extends, __spread, __read, __assign, __values } from 'tslib';
 import { Subject, Subscription, Observable, merge } from 'rxjs';
 import { share } from 'rxjs/operators';
 
@@ -2296,7 +2296,7 @@ function leaveView(newView) {
  *
  * ```
  */
-var includeViewProviders = false;
+var includeViewProviders = true;
 function setIncludeViewProviders(v) {
     var oldValue = includeViewProviders;
     includeViewProviders = v;
@@ -10721,7 +10721,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+62.sha-5d3dcfc');
+var VERSION = new Version('7.2.0-rc.0+63.sha-fb7816f');
 
 /**
  * @license
