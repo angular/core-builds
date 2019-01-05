896db37a5b159e1b9c1b68a145712a5644d2a215
Filipe Silva <filipematossilva@gmail.com>
2019-01-05 22:41:41 +0000

    784453c ci: use image based cache

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-9576e48
+ * @license Angular v7.2.0-rc.0+53.sha-784453c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10761,7 +10761,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+53.sha-9576e48');
+    var VERSION = new Version('7.2.0-rc.0+53.sha-784453c');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-9576e48
+ * @license Angular v7.2.0-rc.0+53.sha-784453c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13769,7 +13769,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+53.sha-9576e48');
+const VERSION = new Version('7.2.0-rc.0+53.sha-784453c');
 
 /**
  * @fileoverview added by tsickle
