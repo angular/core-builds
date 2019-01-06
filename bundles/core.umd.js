f293d42098048cbece3e887b1825094a4d3e8ce8
Filipe Silva <filipematossilva@gmail.com>
2019-01-06 15:17:24 +0000

    1dd11c9 ci: add linux buildkite tests

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-b68bc8b
+ * @license Angular v7.2.0-rc.0+54.sha-1dd11c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10761,7 +10761,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+54.sha-b68bc8b');
+    var VERSION = new Version('7.2.0-rc.0+54.sha-1dd11c9');
 
     /**
      * @license
