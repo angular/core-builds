717ed58df028a06cdac92c704fe968ee178ace7c
Kara Erickson <karakara@google.com>
2019-01-06 02:04:59 +0000

    1ad88e0 ci: replace our pullapprove setup with GitHub CODEOWNERS (#27690)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+29.sha-6cc864a
+ * @license Angular v7.2.0-rc.0+30.sha-1ad88e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13241,7 +13241,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+29.sha-6cc864a');
+const VERSION = new Version('7.2.0-rc.0+30.sha-1ad88e0');
 
 /**
  * @fileoverview added by tsickle
