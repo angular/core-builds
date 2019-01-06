0dc556f736235dfdfa0863e6a3e449eed8d6a6a0
Kara Erickson <karakara@google.com>
2019-01-06 02:12:24 +0000

    6b0c1a7 test(ivy): turn on platform-browser-dynamic tests on CI (#27940)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+69.sha-224d787
+ * @license Angular v7.2.0-rc.0+71.sha-6b0c1a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+69.sha-224d787');
+var VERSION = new Version('7.2.0-rc.0+71.sha-6b0c1a7');
 
 /**
  * @license
