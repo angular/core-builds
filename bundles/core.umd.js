afcde9167d7f0355594b936564de936bcf953e0c
Kara Erickson <karakara@google.com>
2019-01-06 02:13:33 +0000

    4c9aff5 ci: fix typos in codeowners file (#27945)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+69.sha-224d787
+ * @license Angular v7.2.0-rc.0+70.sha-4c9aff5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10755,7 +10755,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+69.sha-224d787');
+    var VERSION = new Version('7.2.0-rc.0+70.sha-4c9aff5');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+69.sha-224d787
+ * @license Angular v7.2.0-rc.0+70.sha-4c9aff5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13792,7 +13792,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+69.sha-224d787');
+const VERSION = new Version('7.2.0-rc.0+70.sha-4c9aff5');
 
 /**
  * @fileoverview added by tsickle
