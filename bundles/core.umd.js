97615d9b7645774671fc626509d3027192401b9e
Kara Erickson <karakara@google.com>
2019-01-07 17:12:53 +0000

    4588053 test(ivy): update root causes for @angular/core tests (#27961)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+6.sha-e08feb7
+ * @license Angular v7.2.0+7.sha-4588053
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10762,7 +10762,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0+6.sha-e08feb7');
+    var VERSION = new Version('7.2.0+7.sha-4588053');
 
     /**
      * @license
