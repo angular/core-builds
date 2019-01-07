1c3e9209f05198b6cf80fa2145ba5c08ecd49de9
Kara Erickson <karakara@google.com>
2019-01-07 17:07:57 +0000

    e94e7ee docs: add Alyle UI to resources (#27954) PR Close #27954

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0
+ * @license Angular v7.2.0+2.sha-e94e7ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10755,7 +10755,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0');
+    var VERSION = new Version('7.2.0+2.sha-e94e7ee');
 
     /**
      * @license
