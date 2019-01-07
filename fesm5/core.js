e98cc44744e6e245bde5f5c990911bd189a4bfb4
Kara Erickson <karakara@google.com>
2019-01-07 16:25:55 +0000

    0efbb37 release: cut the v7.2.0 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+71.sha-6b0c1a7
+ * @license Angular v7.2.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10725,7 +10725,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+71.sha-6b0c1a7');
+var VERSION = new Version('7.2.0');
 
 /**
  * @license
