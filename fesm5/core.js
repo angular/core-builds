19a50cc084002284a606ff9bd452c7f5164ace3e
Kara Erickson <karakara@google.com>
2019-01-07 17:07:05 +0000

    654055b ci: add brandonroberts to @angular/framework-global-approvers-for-docs-only-changes (#27949)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+2.sha-e94e7ee
+ * @license Angular v7.2.0+1.sha-654055b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+2.sha-e94e7ee');
+var VERSION = new Version('7.2.0+1.sha-654055b');
 
 /**
  * @license
