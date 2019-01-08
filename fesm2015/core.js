211334d9805a63398e440439b12aee3571b26e54
Kara Erickson <karakara@google.com>
2019-01-08 19:45:58 +0000

    1edb8af docs: group fixes and features for 7.2.0 in changelog desc (#27974)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+52.sha-1cca9cc
+ * @license Angular v7.2.0-rc.0+54.sha-1edb8af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13241,7 +13241,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+52.sha-1cca9cc');
+const VERSION = new Version('7.2.0-rc.0+54.sha-1edb8af');
 
 /**
  * @fileoverview added by tsickle
