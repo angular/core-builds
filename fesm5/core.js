00361d850dc16fd5a8ecece8bc5305891195478b
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:55:42 +0000

    d0c3e25 docs: remove deprecated `fromPromise` from RxJS guide (#28015)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+78.sha-e1e4887
+ * @license Angular v7.2.0+79.sha-d0c3e25
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10645,7 +10645,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+78.sha-e1e4887');
+var VERSION = new Version('7.2.0+79.sha-d0c3e25');
 
 /**
  * @license
