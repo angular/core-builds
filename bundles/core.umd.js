92d2a32ef60eb9ffb10a9672c90d6ec6f99b2160
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:56:09 +0000

    11325ba test(ivy): mark jit_summaries_integration_spec as obsolete in Ivy (#28027)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+79.sha-d0c3e25
+ * @license Angular v7.2.0+80.sha-11325ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10675,7 +10675,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0+79.sha-d0c3e25');
+    var VERSION = new Version('7.2.0+80.sha-11325ba');
 
     /**
      * @license
