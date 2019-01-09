2344f70779fe5e7496ead8807ee3fa1b8aed247e
Kara Erickson <karakara@google.com>
2019-01-09 00:43:24 +0000

    5a7dd73 Revert "test(ivy): re-enable passing animation tests (#27997)"

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+55.sha-f1c3eab
+ * @license Angular v7.2.0-rc.0+60.sha-5a7dd73
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10344,7 +10344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+55.sha-f1c3eab');
+    var VERSION = new Version('7.2.0-rc.0+60.sha-5a7dd73');
 
     /**
      * @license
