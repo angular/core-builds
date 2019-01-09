4f20b6c2e619106a5983ae3ed6434baaf8943441
Kara Erickson <karakara@google.com>
2019-01-09 01:45:11 +0000

    7227b4a docs: use static zip function as an Observable creator. (#26790)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+60.sha-5a7dd73
+ * @license Angular v7.2.0-rc.0+61.sha-7227b4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10344,7 +10344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0-rc.0+60.sha-5a7dd73');
+    var VERSION = new Version('7.2.0-rc.0+61.sha-7227b4a');
 
     /**
      * @license
