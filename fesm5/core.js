0a9bc37506cdbbd92bd16ca6060344e564ef7134
Andrew Kushnir <akushnir@google.com>
2019-01-10 00:25:27 +0000

    94c0b7a fix(ivy): ensure animation @bindings work for {key:value} and empty bindings (#28026)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+81.sha-8934b73
+ * @license Angular v7.2.0+83.sha-94c0b7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10662,7 +10662,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+81.sha-8934b73');
+var VERSION = new Version('7.2.0+83.sha-94c0b7a');
 
 /**
  * @license
