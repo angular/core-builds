83ae87096edf6b30d2f095a25ab88367ca9bea60
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:32:00 +0000

    65e72e9 fix(bazel): protractor rule does not run spec files with underscore (#28022)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+90.sha-7374dfd
+ * @license Angular v7.2.0+92.sha-65e72e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10662,7 +10662,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+90.sha-7374dfd');
+var VERSION = new Version('7.2.0+92.sha-65e72e9');
 
 /**
  * @license
