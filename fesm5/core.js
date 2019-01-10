ea94d3e08877124d1992b36ff4a8a819a0b2183a
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:59:16 +0000

    5a0deb8 fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+84.sha-4694c93
+ * @license Angular v7.2.0+85.sha-5a0deb8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10662,7 +10662,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+84.sha-4694c93');
+var VERSION = new Version('7.2.0+85.sha-5a0deb8');
 
 /**
  * @license
