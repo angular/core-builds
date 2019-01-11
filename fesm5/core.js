85abc01c56b1e5b0d9d2042a82c13d98e3b47254
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:27:47 +0000

    2530c9d docs: fix typo (#27865) PR Close #27865

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+85.sha-7bafe18
+ * @license Angular v7.2.0-rc.0+83.sha-2530c9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10299,7 +10299,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+85.sha-7bafe18');
+var VERSION = new Version('7.2.0-rc.0+83.sha-2530c9d');
 
 /**
  * @license
