a75adc4b852c6cfa47054910f6ccd5f2a79292df
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:34:36 +0000

    bccf8da docs(router): fix misnamed path (#27879)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+110.sha-26a8c09
+ * @license Angular v7.2.0+109.sha-bccf8da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10763,7 +10763,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0+110.sha-26a8c09');
+    var VERSION = new Version('7.2.0+109.sha-bccf8da');
 
     /**
      * @license
