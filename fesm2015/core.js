feeb676d728f07190f51efed6279732938d5704a
Andrew Kushnir <akushnir@google.com>
2019-01-12 00:40:35 +0000

    31556fa docs: add library doc to guides in aio (#27581)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+96.sha-01d475d
+ * @license Angular v7.2.0-rc.0+97.sha-31556fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13241,7 +13241,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+96.sha-01d475d');
+const VERSION = new Version('7.2.0-rc.0+97.sha-31556fa');
 
 /**
  * @fileoverview added by tsickle
