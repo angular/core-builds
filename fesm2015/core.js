e22b70d95eeec3b9ee521fdba81b3fc8ec00d358
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:11:07 +0000

    9460218 docs(router): add clarification for Router config (#28159)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+184.sha-850b867
+ * @license Angular v7.2.0+185.sha-9460218
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16426,7 +16426,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+184.sha-850b867');
+const VERSION = new Version('7.2.0+185.sha-9460218');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+184.sha-850b867
+ * @license Angular v7.2.0+185.sha-9460218
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13207,7 +13207,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+184.sha-850b867');
+var VERSION = new Version('7.2.0+185.sha-9460218');
 
 /**
  * @license
