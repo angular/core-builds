03788a65ac05ee179ccc64ce42d13a9027b70402
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:34:13 +0000

    67e6b63 fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27680)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+121.sha-88d4155
+ * @license Angular v7.2.0-rc.0+122.sha-67e6b63
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10299,7 +10299,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+121.sha-88d4155');
+var VERSION = new Version('7.2.0-rc.0+122.sha-67e6b63');
 
 /**
  * @license
