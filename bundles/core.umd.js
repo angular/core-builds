8bd43fe04093d08d2142d6c5020ae1d5960c64c7
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:11:26 +0000

    850b867 docs: add api doc for switch directives (#27378)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+183.sha-a241200
+ * @license Angular v7.2.0+184.sha-850b867
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13166,7 +13166,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0+183.sha-a241200');
+    var VERSION = new Version('7.2.0+184.sha-850b867');
 
     /**
      * @license
