e45f73dbadeb1817bbdc925a39b92531de252b91
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:32:45 +0000

    fa53150 test(ivy): NodeInjector should know how to get itself (INJECTOR) (#28009)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+173.sha-b5c2ef2
+ * @license Angular v7.2.0+174.sha-fa53150
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16320,7 +16320,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+173.sha-b5c2ef2');
+const VERSION = new Version('7.2.0+174.sha-fa53150');
 
 /**
  * @fileoverview added by tsickle
