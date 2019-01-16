d631eac73e30245434b3a9493a46fa4e8ffc4e92
Andrew Kushnir <akushnir@google.com>
2019-01-16 21:58:37 +0000

    1e5e62e docs: release notes for the v7.2.1 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+198.sha-bac71ef
+ * @license Angular v7.2.0+199.sha-1e5e62e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13137,7 +13137,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+198.sha-bac71ef');
+var VERSION = new Version('7.2.0+199.sha-1e5e62e');
 
 /**
  * @license
