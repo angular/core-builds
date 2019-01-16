5c08e4d95585c51d171a570e5d26aadcd4f78101
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:02:11 +0000

    3b7a571 test(upgrade): clean up global state after test (#28181)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+192.sha-f983e99
+ * @license Angular v7.2.0+193.sha-3b7a571
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13253,7 +13253,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.0+192.sha-f983e99');
+    var VERSION = new Version('7.2.0+193.sha-3b7a571');
 
     /**
      * @license
