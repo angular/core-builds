3a9c397e7a20209fb5d8b96d8a98f492e8a443c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 22:06:59 +0000

    d8c75f1 docs: fix comments in services example (#26194)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+4.sha-6d40ef2
+ * @license Angular v7.2.1+5.sha-d8c75f1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+4.sha-6d40ef2');
+var VERSION = new Version('7.2.1+5.sha-d8c75f1');
 
 /**
  * @license
