6bbcebdb66bb85d1c0b5bba3b2d703c5cc1f455b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 01:57:35 +0000

    4ea6c27 build(bazel): Use @angular/cli from root package.json (#28139)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1
+ * @license Angular v7.2.1+3.sha-4ea6c27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13812,7 +13812,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.1');
+const VERSION = new Version('7.2.1+3.sha-4ea6c27');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1
+ * @license Angular v7.2.1+3.sha-4ea6c27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1');
+var VERSION = new Version('7.2.1+3.sha-4ea6c27');
 
 /**
  * @license
