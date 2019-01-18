e04e588aab494be4b2fb480cc144e307e1fe0ae7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:12:26 +0000

    2b9cc85 fix(ivy): Ensure proper namespace is used to create elements in JIT (#28144)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+18.sha-a58fd21
+ * @license Angular v8.0.0-beta.0+19.sha-2b9cc85
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16392,7 +16392,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+18.sha-a58fd21');
+const VERSION = new Version('8.0.0-beta.0+19.sha-2b9cc85');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+18.sha-a58fd21
+ * @license Angular v8.0.0-beta.0+19.sha-2b9cc85
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13169,7 +13169,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+18.sha-a58fd21');
+var VERSION = new Version('8.0.0-beta.0+19.sha-2b9cc85');
 
 /**
  * @license
