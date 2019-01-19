d3802ebca7ff1e9d5abc96989be5d1707cb142f1
Alex Eagle <alexeagle@angular.io>
2019-01-19 00:27:43 +0000

    218110a feat(compiler-cli): expose ngtsc as a TscPlugin

--- src/render3/instructions.d.ts
+++ src/render3/instructions.d.ts
@@ -184,6 +184,26 @@ export declare function locateHostElement(factory: RendererFactory3, elementOrSe
  */
 export declare function listener(eventName: string, listenerFn: (e?: any) => any, useCapture?: boolean, eventTargetResolver?: GlobalTargetResolver): void;
 /**
+ * Registers a synthetic host listener (e.g. `(@foo.start)`) on a component.
+ *
+ * This instruction is for compatibility purposes and is designed to ensure that a
+ * synthetic host listener (e.g. `@HostListener('@foo.start')`) properly gets rendered
+ * in the component's renderer. Normally all host listeners are evaluated with the
+ * parent component's renderer, but, in the case of animation @triggers, they need
+ * to be evaluated with the sub component's renderer (because that's where the
+ * animation triggers are defined).
+ *
+ * Do not use this instruction as a replacement for `listener`. This instruction
+ * only exists to ensure compatibility with the ViewEngine's host binding behavior.
+ *
+ * @param eventName Name of the event
+ * @param listenerFn The function to be called when event emits
+ * @param useCapture Whether or not to use capture in event listener
+ * @param eventTargetResolver Function that returns global target information in case this listener
+ * should be attached to a global object like window, document or body
+ */
+export declare function componentHostSyntheticListener<T>(eventName: string, listenerFn: (e?: any) => any, useCapture?: boolean, eventTargetResolver?: GlobalTargetResolver): void;
+/**
  * Saves context for this cleanup function in LView.cleanupInstances.
  *
  * On the first template pass, saves in TView:
@@ -235,7 +255,7 @@ export declare function elementProperty<T>(index: number, propName: string, valu
  * synthetic host binding (e.g. `@HostBinding('@foo')`) properly gets rendered in
  * the component's renderer. Normally all host bindings are evaluated with the parent
  * component's renderer, but, in the case of animation @triggers, they need to be
- * evaluated with the sub components renderer (because that's where the animation
+ * evaluated with the sub component's renderer (because that's where the animation
  * triggers are defined).
  *
  * Do not use this instruction as a replacement for `elementProperty`. This instruction
