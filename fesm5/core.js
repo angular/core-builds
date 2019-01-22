31a0660c04efa5875f9dbec82afd9cab7e828995
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:09:23 +0000

    607fd8e refactor: remove unused case in switch statement (#28253)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+12.sha-cb93027
+ * @license Angular v7.2.1+11.sha-607fd8e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+12.sha-cb93027');
+var VERSION = new Version('7.2.1+11.sha-607fd8e');
 
 /**
  * @license
