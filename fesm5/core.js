0dbd2fa43297be53e23d2284ac358351b2764126
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 17:42:18 +0000

    9dabeea ci: add a rebase check to the merge-pr script (#28250)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+24.sha-73dcd72
+ * @license Angular v8.0.0-beta.0+25.sha-9dabeea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13245,7 +13245,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+24.sha-73dcd72');
+var VERSION = new Version('8.0.0-beta.0+25.sha-9dabeea');
 
 /**
  * @license
