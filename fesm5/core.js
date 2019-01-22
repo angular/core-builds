2da63fc80e79c0e5268d67d354c6c4fdad26192a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:53:36 +0000

    f99082f build: hide vscode settings e.g. debug launch config (#28299)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+39.sha-197676a
+ * @license Angular v8.0.0-beta.0+41.sha-f99082f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13245,7 +13245,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+39.sha-197676a');
+var VERSION = new Version('8.0.0-beta.0+41.sha-f99082f');
 
 /**
  * @license
