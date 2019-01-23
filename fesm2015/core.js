06516394be8eacfbf5d9ce1bd076c58255488cd7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:26:42 +0000

    f72319c docs(router): removed additional to (#25989) PR Close #25989

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+3.sha-5877b3f
+ * @license Angular v7.2.2+4.sha-f72319c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13812,7 +13812,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.2+3.sha-5877b3f');
+const VERSION = new Version('7.2.2+4.sha-f72319c');
 
 /**
  * @fileoverview added by tsickle
