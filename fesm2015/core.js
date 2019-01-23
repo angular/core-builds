2e4f5766973b7589646ddd9d01bab393618744b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 00:06:52 +0000

    fe9b3ea docs: release notes for the v7.2.2 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+65.sha-50df897
+ * @license Angular v8.0.0-beta.0+66.sha-fe9b3ea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16473,7 +16473,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+65.sha-50df897');
+const VERSION = new Version('8.0.0-beta.0+66.sha-fe9b3ea');
 
 /**
  * @fileoverview added by tsickle
