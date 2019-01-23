b28e51641344020250e7473c4559d5e277d441c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:35:43 +0000

    d8f2318 fix(ivy): generating incorrect tag name with namespace (#28298)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+1.sha-1fd6735
+ * @license Angular v8.0.0-beta.1+2.sha-d8f2318
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13204,7 +13204,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.1+1.sha-1fd6735');
+    var VERSION = new Version('8.0.0-beta.1+2.sha-d8f2318');
 
     /**
      * @license
