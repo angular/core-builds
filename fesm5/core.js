9ee8df3ea2ffc34086edfc0246b9ef30826b3d5b
Jason Aden <jasonaden@google.com>
2019-01-24 23:56:01 +0000

    bc10328 fix(docs-infra): change the key used to find out the cli section (#28293)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+5.sha-beacbfc
+ * @license Angular v7.2.2+12.sha-bc10328
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2+5.sha-beacbfc');
+var VERSION = new Version('7.2.2+12.sha-bc10328');
 
 /**
  * @license
