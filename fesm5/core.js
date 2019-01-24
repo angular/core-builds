801de0ba707d885b61908b41fa7badd5fcad3f88
Jason Aden <jasonaden@google.com>
2019-01-24 23:58:33 +0000

    35f7ff0 docs: add api doc for ngif (#27376) (#28344)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+12.sha-bc10328
+ * @license Angular v7.2.2+15.sha-35f7ff0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2+12.sha-bc10328');
+var VERSION = new Version('7.2.2+15.sha-35f7ff0');
 
 /**
  * @license
