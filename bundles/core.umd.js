260b1fb680d81a9e89b58184579e2e53487e8c32
Jason Aden <jasonaden@google.com>
2019-01-24 18:49:54 +0000

    465abab docs(animations): fix group and sequence function usage examples (#28305)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+21.sha-c1fb9c2
+ * @license Angular v8.0.0-beta.1+22.sha-465abab
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13339,7 +13339,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.1+21.sha-c1fb9c2');
+    var VERSION = new Version('8.0.0-beta.1+22.sha-465abab');
 
     /**
      * @license
