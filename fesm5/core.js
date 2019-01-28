3ee6e655801bc9890273e3e9f16983628afa9e1c
Jason Aden <jasonaden@google.com>
2019-01-28 20:25:12 +0000

    2f19ad9 fix(docs-infra): upgrade codelyzer to 4.5.0 (#28389)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+16.sha-a1b9995
+ * @license Angular v7.2.2+25.sha-2f19ad9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2+16.sha-a1b9995');
+var VERSION = new Version('7.2.2+25.sha-2f19ad9');
 
 /**
  * @license
