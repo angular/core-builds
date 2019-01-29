2b1aa2c026a8b0a90f242a46db95d4bcb039ae28
Jason Aden <jasonaden@google.com>
2019-01-29 23:32:48 +0000

    dabcb3e docs: fix Redundant "See also:" link in ContentChild (#28334)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+89.sha-ec414b4
+ * @license Angular v8.0.0-beta.1+94.sha-dabcb3e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13462,7 +13462,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+89.sha-ec414b4');
+var VERSION = new Version('8.0.0-beta.1+94.sha-dabcb3e');
 
 /**
  * @license
