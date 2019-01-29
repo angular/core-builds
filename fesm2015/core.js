98e2648fae18093e692c3640d18980062cc7fff2
Jason Aden <jasonaden@google.com>
2019-01-29 18:01:31 +0000

    22f76df feat(compiler-cli): expose ngtsc as a TscPlugin (#28431)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+83.sha-e18a52e
+ * @license Angular v8.0.0-beta.1+84.sha-22f76df
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16650,7 +16650,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.1+83.sha-e18a52e');
+const VERSION = new Version('8.0.0-beta.1+84.sha-22f76df');
 
 /**
  * @fileoverview added by tsickle
