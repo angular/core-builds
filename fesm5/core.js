3852a7b132fa50b374889bc4c6e5d42521992e10
Jason Aden <jasonaden@google.com>
2019-01-30 01:11:34 +0000

    22bc6ef docs: fix typo for Browserslist in build guide (#28328)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+45.sha-2c63108
+ * @license Angular v7.2.2+49.sha-22bc6ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2+45.sha-2c63108');
+var VERSION = new Version('7.2.2+49.sha-22bc6ef');
 
 /**
  * @license
