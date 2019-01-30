e3806acd1d8bd28707f8308a84d049c6ec78344b
Jason Aden <jasonaden@google.com>
2019-01-30 01:07:34 +0000

    2c63108 docs(core): add relative path information to component metadata (#27962)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+47.sha-d11c2f9
+ * @license Angular v7.2.2+45.sha-2c63108
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13812,7 +13812,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.2+47.sha-d11c2f9');
+const VERSION = new Version('7.2.2+45.sha-2c63108');
 
 /**
  * @fileoverview added by tsickle
