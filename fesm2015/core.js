51ea54723838d2f29f5bf272bd97af3c5e2c453b
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:57:46 +0000

    7219639 fix(compiler-cli): base synthetic filepaths on input filepath (#28453)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+35.sha-f2621db
+ * @license Angular v8.0.0-beta.2+36.sha-7219639
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16809,7 +16809,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2+35.sha-f2621db');
+const VERSION = new Version('8.0.0-beta.2+36.sha-7219639');
 
 /**
  * @fileoverview added by tsickle
