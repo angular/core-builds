13eb4e14173ff06141ceb7b8b2dabf5219060703
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 07:47:54 +0000

    be8a403 feat(aio): show developer survey 2019 (#28552)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+10.sha-c378402
+ * @license Angular v7.2.4+11.sha-be8a403
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10787,7 +10787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.4+10.sha-c378402');
+    var VERSION = new Version('7.2.4+11.sha-be8a403');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+10.sha-c378402
+ * @license Angular v7.2.4+11.sha-be8a403
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13812,7 +13812,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4+10.sha-c378402');
+const VERSION = new Version('7.2.4+11.sha-be8a403');
 
 /**
  * @fileoverview added by tsickle
