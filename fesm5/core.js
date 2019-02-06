78254ac7f029c7d57579a5fc701720038c2d7f51
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 05:06:01 +0000

    c378402 build: fix failing compiler-cli tests on windows (#28550)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+1.sha-d491a20
+ * @license Angular v7.2.4+10.sha-c378402
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+1.sha-d491a20');
+var VERSION = new Version('7.2.4+10.sha-c378402');
 
 /**
  * @license
