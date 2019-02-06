2a0baab2c1c5aa2138afbb82571d8c65f87da2bf
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:09:23 +0000

    74c202a release: cut the v7.2.4 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+26.sha-483e8d2
+ * @license Angular v7.2.4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10787,7 +10787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.3+26.sha-483e8d2');
+    var VERSION = new Version('7.2.4');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+26.sha-483e8d2
+ * @license Angular v7.2.4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13812,7 +13812,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.3+26.sha-483e8d2');
+const VERSION = new Version('7.2.4');
 
 /**
  * @fileoverview added by tsickle
