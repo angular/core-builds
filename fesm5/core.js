55e435da0dd8a899a50d9fc3967126edf7705bc7
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 01:29:26 +0000

    979582a build: add some debug info for yarn installs (#28553)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+59.sha-63b744b
+ * @license Angular v8.0.0-beta.2+60.sha-979582a
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13560,7 +13560,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+59.sha-63b744b');
+var VERSION = new Version('8.0.0-beta.2+60.sha-979582a');
 
 /**
  * @license
