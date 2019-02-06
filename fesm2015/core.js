497f4ce7df71cf64907792782b1d2872d22241cb
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 07:36:53 +0000

    ae8b7b1 test(ivy): root cause analysis for mat-selection-list (#28525)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+16.sha-62a13e7
+ * @license Angular v8.0.0-beta.3+17.sha-ae8b7b1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16942,7 +16942,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+16.sha-62a13e7');
+const VERSION = new Version('8.0.0-beta.3+17.sha-ae8b7b1');
 
 /**
  * @fileoverview added by tsickle
