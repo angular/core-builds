8a0db073e46f024903b3ab47ef0fdcc0bf553d1d
Misko Hevery <misko@hevery.com>
2019-02-07 05:40:31 +0000

    1e58a21 fix(router): redirect to root url when returned as UrlTree from guard (#28271)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+28.sha-da81c9c
+ * @license Angular v7.2.4+29.sha-1e58a21
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13812,7 +13812,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4+28.sha-da81c9c');
+const VERSION = new Version('7.2.4+29.sha-1e58a21');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+28.sha-da81c9c
+ * @license Angular v7.2.4+29.sha-1e58a21
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10742,7 +10742,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+28.sha-da81c9c');
+var VERSION = new Version('7.2.4+29.sha-1e58a21');
 
 /**
  * @license
