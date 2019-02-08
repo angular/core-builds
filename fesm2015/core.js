9867c70ba5469e03fdf9107fdd88b62f813551c2
Misko Hevery <misko@hevery.com>
2019-02-08 06:19:56 +0000

    faf0b25 docs(aio): add Happy Angular Podcast to resources (#28407)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+54.sha-9067025
+ * @license Angular v8.0.0-beta.3+55.sha-faf0b25
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16971,7 +16971,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+54.sha-9067025');
+const VERSION = new Version('8.0.0-beta.3+55.sha-faf0b25');
 
 /**
  * @fileoverview added by tsickle
