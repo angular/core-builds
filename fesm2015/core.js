8ed3b83ff60d4e454d63fabb0484af068ccf828a
Miško Hevery <misko@hevery.com>
2019-02-08 17:47:06 +0000

    0e4705a test(ivy): run view_injector_integration tests on node (#28593)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+57.sha-ceada77
+ * @license Angular v8.0.0-beta.3+59.sha-0e4705a
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16892,7 +16892,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+57.sha-ceada77');
+const VERSION = new Version('8.0.0-beta.3+59.sha-0e4705a');
 
 /**
  * @fileoverview added by tsickle
