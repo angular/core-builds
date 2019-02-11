8c82f91911a11d41509113850339609b90463c19
Miško Hevery <misko@hevery.com>
2019-02-11 17:33:55 +0000

    570f735 docs: clarify library types integration (#28410)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+88.sha-9ec8aa5
+ * @license Angular v8.0.0-beta.3+90.sha-570f735
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13654,7 +13654,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+88.sha-9ec8aa5');
+var VERSION = new Version('8.0.0-beta.3+90.sha-570f735');
 
 /**
  * @license
