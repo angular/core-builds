6c2ec02c7281c9c477e847794cda61fa1c32ce0a
Miško Hevery <misko@hevery.com>
2019-02-12 18:48:11 +0000

    2bf526b docs: add a README for third_party usage (#28651)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+46.sha-8bd7d5b
+ * @license Angular v7.2.4+47.sha-2bf526b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13812,7 +13812,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4+46.sha-8bd7d5b');
+const VERSION = new Version('7.2.4+47.sha-2bf526b');
 
 /**
  * @fileoverview added by tsickle
