e45d7692f2a132ebac4f267a0909b4114f4dccad
Miško Hevery <misko@hevery.com>
2019-02-13 20:23:14 +0000

    644e7a2 docs: add di-related api doc (#27731)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+134.sha-f8b9e61
+ * @license Angular v8.0.0-beta.3+135.sha-644e7a2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2478,15 +2478,16 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /**
-     * This token can be used to create a virtual provider that will populate the
-     * `entryComponents` fields of components and ng modules based on its `useValue`.
+     * A DI token that you can use to create a virtual [provider](guide/glossary#provider)
+     * that will populate the `entryComponents` field of components and NgModules
+     * based on its `useValue` property value.
      * All components that are referenced in the `useValue` value (either directly
-     * or in a nested array or map) will be added to the `entryComponents` property.
+     * or in a nested array or map) are added to the `entryComponents` property.
      *
      * @usageNotes
-     * ### Example
+     *
      * The following example shows how the router can populate the `entryComponents`
-     * field of an NgModule based on the router configuration which refers
+     * field of an NgModule based on a router configuration that refers
      * to components.
      *
      * ```typescript
@@ -14103,7 +14104,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.3+134.sha-f8b9e61');
+    var VERSION = new Version('8.0.0-beta.3+135.sha-644e7a2');
 
     /**
      * @license
