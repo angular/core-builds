5d311ae79bfad7699e3234c951a6c7d64d8cf2c4
Miško Hevery <misko@hevery.com>
2019-02-15 22:42:06 +0000

    fcc8c56 docs: release notes for the v7.2.5 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+181.sha-8accc98
+ * @license Angular v8.0.0-beta.3+182.sha-fcc8c56
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17556,7 +17556,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+181.sha-8accc98');
+const VERSION = new Version('8.0.0-beta.3+182.sha-fcc8c56');
 
 /**
  * @fileoverview added by tsickle
