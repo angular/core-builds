f48a5485ac40f8539ad0c7f837fca48f60997b0f
Misko Hevery <misko@hevery.com>
2019-02-17 05:28:15 +0000

    fdca001 feat: upgrade domino to v2.1.2 (#28767)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+4.sha-04a1923
+ * @license Angular v8.0.0-beta.4+6.sha-fdca001
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14109,7 +14109,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+4.sha-04a1923');
+var VERSION = new Version('8.0.0-beta.4+6.sha-fdca001');
 
 /**
  * @license
