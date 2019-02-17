0e6df8577148c8258b66a2237f36e834d9d12798
Misko Hevery <misko@hevery.com>
2019-02-17 05:27:06 +0000

    04a1923 ci: add /packages/* to the @angular/fw-dev-infra CODEOWNERS group (#28745)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+2.sha-c1094cf
+ * @license Angular v8.0.0-beta.4+4.sha-04a1923
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14068,7 +14068,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.4+2.sha-c1094cf');
+    var VERSION = new Version('8.0.0-beta.4+4.sha-04a1923');
 
     /**
      * @license
