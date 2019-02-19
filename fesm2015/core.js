2f14689d362b254115101dbdd5b6e48705bc7d06
Igor Minar <igor@angular.io>
2019-02-19 21:18:32 +0000

    3b78f9e build(compiler-cli): upgrade chokidar to latest version (#28797)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+16.sha-f6ca619
+ * @license Angular v7.2.5+17.sha-3b78f9e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.5+16.sha-f6ca619');
+const VERSION = new Version('7.2.5+17.sha-3b78f9e');
 
 /**
  * @fileoverview added by tsickle
