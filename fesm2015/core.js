cbbe47313158b99a52d3fa4cb20943350d9efe85
Igor Minar <igor@angular.io>
2019-02-19 20:58:48 +0000

    caefc5d ci: add /aio/content/images/bios/** to @angular/fw-docs-marketing group (#28835)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+12.sha-a17fd43
+ * @license Angular v8.0.0-beta.4+14.sha-caefc5d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17453,7 +17453,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.4+12.sha-a17fd43');
+const VERSION = new Version('8.0.0-beta.4+14.sha-caefc5d');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+12.sha-a17fd43
+ * @license Angular v8.0.0-beta.4+14.sha-caefc5d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14038,7 +14038,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+12.sha-a17fd43');
+var VERSION = new Version('8.0.0-beta.4+14.sha-caefc5d');
 
 /**
  * @license
