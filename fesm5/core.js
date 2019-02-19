86b51bfb2d2791ee233142b62f90f6ae47ac2e1d
Igor Minar <igor@angular.io>
2019-02-19 20:43:52 +0000

    a17fd43 docs: display the beginning label in interpolation guide example (#28602)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+11.sha-0cc5804
+ * @license Angular v8.0.0-beta.4+12.sha-a17fd43
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14109,7 +14109,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+11.sha-0cc5804');
+var VERSION = new Version('8.0.0-beta.4+12.sha-a17fd43');
 
 /**
  * @license
