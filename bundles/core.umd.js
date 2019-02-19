dcfea20d5a8b0fae7b1e453e1588f7a832a33d2b
Igor Minar <igor@angular.io>
2019-02-19 21:23:23 +0000

    745c9c5 build(compiler-cli): upgrade chokidar to latest version (#28797)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+23.sha-692ddfc
+ * @license Angular v8.0.0-beta.4+26.sha-745c9c5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14068,7 +14068,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.4+23.sha-692ddfc');
+    var VERSION = new Version('8.0.0-beta.4+26.sha-745c9c5');
 
     /**
      * @license
