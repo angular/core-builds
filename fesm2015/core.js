21d42bf4e8706643cf6c4282ecff2a2f3923460a
Igor Minar <igor@angular.io>
2019-02-19 21:01:14 +0000

    ea1a3d3 build: fix `validate-commit-message` on Windows (#28780)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+9.sha-cdcbb53
+ * @license Angular v7.2.5+11.sha-ea1a3d3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.5+9.sha-cdcbb53');
+const VERSION = new Version('7.2.5+11.sha-ea1a3d3');
 
 /**
  * @fileoverview added by tsickle
