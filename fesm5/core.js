752c512612f5d6e887cbc5c1fd6e61d3730e90d3
Igor Minar <igor@angular.io>
2019-02-20 00:35:58 +0000

    fc895ba feat(router): add hash-based navigation option to setUpLocationSync (#28609)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+23.sha-9a51528
+ * @license Angular v7.2.5+24.sha-fc895ba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10743,7 +10743,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.5+23.sha-9a51528');
+var VERSION = new Version('7.2.5+24.sha-fc895ba');
 
 /**
  * @license
