f349a9d2926564de68e02144c10c90f17a49c554
Igor Minar <igor@angular.io>
2019-02-20 06:49:14 +0000

    96b597c fix(bazel): Add postinstall step to generate summaries (#28850)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+45.sha-669b441
+ * @license Angular v8.0.0-beta.4+46.sha-96b597c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14050,7 +14050,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+45.sha-669b441');
+var VERSION = new Version('8.0.0-beta.4+46.sha-96b597c');
 
 /**
  * @license
