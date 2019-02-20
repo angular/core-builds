8cf8210e596ba5297f875ac4d861449eba9d6c72
Igor Minar <igor@angular.io>
2019-02-20 17:39:14 +0000

    cfe4732 fix(docs-infra): prevent extra space at the bottom of the page in Chrome (#28864)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+25.sha-069146e
+ * @license Angular v7.2.5+28.sha-cfe4732
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.5+25.sha-069146e');
+const VERSION = new Version('7.2.5+28.sha-cfe4732');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+25.sha-069146e
+ * @license Angular v7.2.5+28.sha-cfe4732
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10743,7 +10743,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.5+25.sha-069146e');
+var VERSION = new Version('7.2.5+28.sha-cfe4732');
 
 /**
  * @license
