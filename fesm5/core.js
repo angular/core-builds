0145d30350b74b1fbeac03c623d52fa3c046bb1e
Igor Minar <igor@angular.io>
2019-02-20 21:30:49 +0000

    27eb8f2 build: update deps in ./scripts/release/post-check

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+29.sha-843fc7d
+ * @license Angular v7.2.5+31.sha-27eb8f2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10743,7 +10743,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.5+29.sha-843fc7d');
+var VERSION = new Version('7.2.5+31.sha-27eb8f2');
 
 /**
  * @license
