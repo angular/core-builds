3274fe382360c94e8ea18a8ce50d63da1e8e45ee
Igor Minar <igor@angular.io>
2019-02-20 17:43:23 +0000

    1832e0f build: use vendored yarn under Bazel (#28839)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+48.sha-bca0b44
+ * @license Angular v8.0.0-beta.4+50.sha-1832e0f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14166,7 +14166,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.4+48.sha-bca0b44');
+    var VERSION = new Version('8.0.0-beta.4+50.sha-1832e0f');
 
     /**
      * @license
