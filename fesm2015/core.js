f8feca804c9b60b7e38711de3be84f14d683cbcf
Igor Minar <igor@angular.io>
2019-02-22 02:08:19 +0000

    c6741bf build: no //package/bazel/src/api-extractor on the patch branch (#28901)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+9.sha-21e78ad.with-local-changes
+ * @license Angular v7.2.6+11.sha-c6741bf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.6+9.sha-21e78ad.with-local-changes');
+const VERSION = new Version('7.2.6+11.sha-c6741bf.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
