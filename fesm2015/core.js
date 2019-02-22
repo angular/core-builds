f17507a3fa375714be9bc89ababf4e61135b72c4
Ben Lesh <ben@benlesh.com>
2019-02-22 18:17:55 +0000

    7c20bf8 build(docs-infra): upgrade cli command docs sources to c91579a2e (#28922)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+39.sha-82820b0.with-local-changes
+ * @license Angular v8.0.0-beta.5+40.sha-7c20bf8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17538,7 +17538,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+39.sha-82820b0.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+40.sha-7c20bf8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
