b9d2ef26af86ef0b7a61a1108a1a235cb2df3fc7
Igor Minar <igor@angular.io>
2019-02-22 02:19:06 +0000

    32ae84d fixup! fix(ivy): incorrectly remapping certain properties that refer to inputs (#28765)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+24.sha-9defc00.with-local-changes
+ * @license Angular v8.0.0-beta.5+26.sha-32ae84d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10179,6 +10179,17 @@ function elementProperty(index, propName, value, sanitizer, nativeOnly) {
 function componentHostSyntheticProperty(index, propName, value, sanitizer, nativeOnly) {
     elementPropertyInternal(index, propName, value, sanitizer, nativeOnly, loadComponentRenderer);
 }
+/**
+ * Mapping between attributes names that don't correspond to their element property names.
+ */
+var ATTR_TO_PROP = {
+    'class': 'className',
+    'for': 'htmlFor',
+    'formaction': 'formAction',
+    'innerHtml': 'innerHTML',
+    'readonly': 'readOnly',
+    'tabindex': 'tabIndex',
+};
 function elementPropertyInternal(index, propName, value, sanitizer, nativeOnly, loadRendererFn) {
     if (value === NO_CHANGE)
         return;
@@ -10199,6 +10210,7 @@ function elementPropertyInternal(index, propName, value, sanitizer, nativeOnly, 
         }
     }
     else if (tNode.type === 3 /* Element */) {
+        propName = ATTR_TO_PROP[propName] || propName;
         if (ngDevMode) {
             validateAgainstEventProperties(propName);
             validateAgainstUnknownProperties(lView, element, propName, tNode);
@@ -14156,7 +14168,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+24.sha-9defc00.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+26.sha-32ae84d.with-local-changes');
 
 /**
  * @license
