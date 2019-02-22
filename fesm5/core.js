d237a7f768cd852e467d2fdfea9a51e8052fa2d8
Ben Lesh <ben@benlesh.com>
2019-02-22 22:58:59 +0000

    395fb18 docs(router): reword relativeLinkResolution docs to not mention version numbers (#26991)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+58.sha-f0989b7.with-local-changes
+ * @license Angular v8.0.0-beta.5+59.sha-395fb18.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14211,7 +14211,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+58.sha-f0989b7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+59.sha-395fb18.with-local-changes');
 
 /**
  * @license
