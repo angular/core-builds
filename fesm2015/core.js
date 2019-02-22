ea6dfbd3cabd50090f491ae9f787b7b59c718c1e
Igor Minar <igor@angular.io>
2019-02-22 06:39:21 +0000

    94223a0 ci: update circleci post checkout to v2 syntax (#28472)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+27.sha-65d839d.with-local-changes
+ * @license Angular v8.0.0-beta.5+28.sha-94223a0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17538,7 +17538,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+27.sha-65d839d.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+28.sha-94223a0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
