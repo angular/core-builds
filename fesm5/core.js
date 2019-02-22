51984b39824f06daabd5d77b40104d1c80f71b8a
Ben Lesh <ben@benlesh.com>
2019-02-22 19:13:12 +0000

    6ccf743 docs: clarify documentation about listening to progress events (#28892)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes
+ * @license Angular v8.0.0-beta.5+42.sha-6ccf743.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14160,7 +14160,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+42.sha-6ccf743.with-local-changes');
 
 /**
  * @license
