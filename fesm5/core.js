9c938a6eba73c9df9f609833bf46158a1e09abe6
Andrew Kushnir <akushnir@google.com>
2019-02-28 18:56:48 +0000

    6ee47d5 ci: speed up `publish_artifacts` CircleCI job for PRs (#29028)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+7.sha-9226b42.with-local-changes
+ * @license Angular v7.2.7+8.sha-6ee47d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10743,7 +10743,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.7+7.sha-9226b42.with-local-changes');
+var VERSION = new Version('7.2.7+8.sha-6ee47d5.with-local-changes');
 
 /**
  * @license
