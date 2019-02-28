fdfbae7e4ca64c5b92be6d4b44e167ffa507c7a7
Igor Minar <igor@angular.io>
2019-02-28 11:14:36 +0000

    9226b42 style: enforce buildifier lint rules (#29019)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+6.sha-b06847f.with-local-changes
+ * @license Angular v7.2.7+7.sha-9226b42.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10788,7 +10788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.7+6.sha-b06847f.with-local-changes');
+    var VERSION = new Version('7.2.7+7.sha-9226b42.with-local-changes');
 
     /**
      * @license
