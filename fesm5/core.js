43503cd6433b32d47a9a9324cc3b1dcb92685bfb
Andrew Kushnir <akushnir@google.com>
2019-02-28 21:32:40 +0000

    ea09430 build: rules_nodejs 0.26.0 & use @npm instead of @ngdeps now that downstream angular build uses angular bundles (#28871)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+25.sha-03d2e5c.with-local-changes
+ * @license Angular v8.0.0-beta.6+33.sha-ea09430.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14497,7 +14497,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+25.sha-03d2e5c.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+33.sha-ea09430.with-local-changes');
 
 /**
  * @license
