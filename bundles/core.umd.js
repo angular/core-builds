bd0b6bd7793e906b1433dd43d92372c6cdad3f4c
Andrew Kushnir <akushnir@google.com>
2019-02-28 19:04:58 +0000

    1d4dde2 ci(docs-infra): disable failing ivy jit systemjs examples (#28984)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+27.sha-d207c48.with-local-changes
+ * @license Angular v8.0.0-beta.6+31.sha-1d4dde2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14542,7 +14542,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.6+27.sha-d207c48.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.6+31.sha-1d4dde2.with-local-changes');
 
     /**
      * @license
