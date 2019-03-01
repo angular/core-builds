236a2fe0decb197ad7d03bdf5ba11f50c5f20222
Andrew Kushnir <akushnir@google.com>
2019-03-01 23:34:59 +0000

    ff8e4dd test(animations): fix unit-based delays within the animation DSL (#28993)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+43.sha-7ac58be.with-local-changes
+ * @license Angular v8.0.0-beta.6+42.sha-ff8e4dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3455,19 +3455,6 @@
             return Array.isArray(value) ? value : value.lView;
         }
         return null;
-    }
-    /**
-     * Returns a boolean for whether the view is attached to the change detection tree.
-     *
-     * Note: This determines whether a view should be checked, not whether it's inserted
-     * into a container. For that, you'll want `viewAttachedToContainer` below.
-     */
-    function viewAttachedToChangeDetector(view) {
-        return (view[FLAGS] & 128 /* Attached */) === 128 /* Attached */;
-    }
-    /** Returns a boolean for whether the view is attached to a container. */
-    function viewAttachedToContainer(view) {
-        return isLContainer(view[PARENT]);
     }
 
     /**
@@ -6940,7 +6927,6 @@
             viewToDetach[QUERIES].removeView();
         }
         viewToDetach[PARENT] = null;
-        viewToDetach[NEXT] = null;
         // Unsets the attached flag
         viewToDetach[FLAGS] &= ~128 /* Attached */;
         return viewToDetach;
@@ -11484,8 +11470,7 @@
         var hostView = getComponentViewByIndex(adjustedElementIndex, lView);
         ngDevMode && assertNodeType(lView[TVIEW].data[adjustedElementIndex], 3 /* Element */);
         // Only attached CheckAlways components or attached, dirty OnPush components should be checked
-        if (viewAttachedToChangeDetector(hostView) &&
-            hostView[FLAGS] & (16 /* CheckAlways */ | 64 /* Dirty */)) {
+        if (viewAttached(hostView) && hostView[FLAGS] & (16 /* CheckAlways */ | 64 /* Dirty */)) {
             syncViewWithBlueprint(hostView);
             checkView(hostView, hostView[CONTEXT]);
         }
@@ -11521,6 +11506,10 @@
         for (var i = componentView.length; i < componentTView.blueprint.length; i++) {
             componentView[i] = componentTView.blueprint[i];
         }
+    }
+    /** Returns a boolean for whether the view is attached */
+    function viewAttached(view) {
+        return (view[FLAGS] & 128 /* Attached */) === 128 /* Attached */;
     }
     /**
      * Instruction to distribute projectable nodes among <ng-content> occurrences in a given template.
@@ -14257,11 +14246,6 @@
                     }
                     var lView = viewRef._lView;
                     var adjustedIdx = this._adjustIndex(index);
-                    if (viewAttachedToContainer(lView)) {
-                        // If view is already attached, fall back to move() so we clean up
-                        // references appropriately.
-                        return this.move(viewRef, adjustedIdx);
-                    }
                     insertView(lView, this._lContainer, adjustedIdx);
                     var beforeNode = getBeforeNodeForView(adjustedIdx, this._lContainer[VIEWS], this._lContainer[NATIVE]);
                     addRemoveViewFromContainer(lView, true, beforeNode);
@@ -14274,9 +14258,8 @@
                         throw new Error('Cannot move a destroyed View in a ViewContainer!');
                     }
                     var index = this.indexOf(viewRef);
-                    if (index !== -1)
-                        this.detach(index);
-                    this.insert(viewRef, newIndex);
+                    this.detach(index);
+                    this.insert(viewRef, this._adjustIndex(newIndex));
                     return viewRef;
                 };
                 ViewContainerRef_.prototype.indexOf = function (viewRef) { return this._viewRefs.indexOf(viewRef); };
@@ -14559,7 +14542,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.6+43.sha-7ac58be.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.6+42.sha-ff8e4dd.with-local-changes');
 
     /**
      * @license
