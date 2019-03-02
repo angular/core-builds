8b18b70d80509710c6dcc20f4e55229416ef9d16
Andrew Kushnir <akushnir@google.com>
2019-03-02 00:03:20 +0000

    a06824a fix(ivy): correctly evaluate enum references in template expressions (#29062)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+48.sha-ba602db.with-local-changes
+ * @license Angular v8.0.0-beta.6+49.sha-a06824a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14473,7 +14473,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.6+48.sha-ba602db.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.6+49.sha-a06824a.with-local-changes');
 
     /**
      * @license
