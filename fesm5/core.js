729c832f65bbbf5cf92a376b693f406298717fa7
Andrew Kushnir <akushnir@google.com>
2019-03-05 20:07:02 +0000

    b446095 refactor: remove unused functions and classes in diagnostics (#28923)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+79.sha-1efad37.with-local-changes
+ * @license Angular v8.0.0-beta.6+80.sha-b446095.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14585,7 +14585,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+79.sha-1efad37.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+80.sha-b446095.with-local-changes');
 
 /**
  * @license
