d75b441dbf532c34aabf287b9a123d7e728a8cef
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:11:31 +0000

    36b6110 docs: order upcoming events in chronological order (#29115)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+30.sha-24a1e14.with-local-changes
+ * @license Angular v7.2.7+31.sha-36b6110.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.7+30.sha-24a1e14.with-local-changes');
+const VERSION = new Version('7.2.7+31.sha-36b6110.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
