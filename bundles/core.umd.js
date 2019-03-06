5ffae7ff0cc66f10a337351ac37c7a3c892523be
George Kalpakas <kalpakas.g@gmail.com>
2019-03-06 00:57:54 +0000

    36cbfb1 build(compiler-cli): upgrade chokidar to latest version

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+18.sha-eeebe62
+ * @license Angular v6.1.10+20.sha-36cbfb1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15138,7 +15138,7 @@
         }
         return Version;
     }());
-    var VERSION = new Version('6.1.10+18.sha-eeebe62');
+    var VERSION = new Version('6.1.10+20.sha-36cbfb1');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+18.sha-eeebe62
+ * @license Angular v6.1.10+20.sha-36cbfb1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15084,7 +15084,7 @@ var Version = /** @class */ (function () {
     }
     return Version;
 }());
-var VERSION = new Version('6.1.10+18.sha-eeebe62');
+var VERSION = new Version('6.1.10+20.sha-36cbfb1');
 
 /**
  * @license
