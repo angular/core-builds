cc3e874b93d38f5711e756ba787fa8c551e73477
Kara Erickson <karakara@google.com>
2019-03-07 18:56:38 +0000

    29f57e3 build: enable bundle_dts for router package (#28833)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+7.sha-22ddbf4.with-local-changes
+ * @license Angular v8.0.0-beta.7+16.sha-29f57e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18146,7 +18146,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+7.sha-22ddbf4.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+16.sha-29f57e3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
