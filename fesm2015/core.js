3de367850e905c99e01e160a0333ca0e9dd3b1b6
Kara Erickson <karakara@google.com>
2019-03-12 19:17:38 +0000

    efecf36 docs: fix failing specs in testing example (#29256)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+16.sha-b68d297.with-local-changes
+ * @license Angular v7.2.8+17.sha-efecf36.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.8+16.sha-b68d297.with-local-changes');
+const VERSION = new Version('7.2.8+17.sha-efecf36.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
