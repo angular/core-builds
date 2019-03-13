1bc5b9bba8227986648e0acacf1c0fe003325050
Kara Erickson <karakara@google.com>
2019-03-13 00:34:16 +0000

    ec01594 release: cut the v8.0.0-beta.8 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+89.sha-ac0553e.with-local-changes
+ * @license Angular v8.0.0-beta.8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14777,7 +14777,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+89.sha-ac0553e.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8.with-local-changes');
 
 /**
  * @license
