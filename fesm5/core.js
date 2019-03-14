b927987990b3e9ede838409aa465d3d1304fe629
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 20:22:33 +0000

    90df7de refactor(core): better failure message for explicit-query timing tslint rule (#29258)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+28.sha-6ab8c0b.with-local-changes
+ * @license Angular v8.0.0-beta.8+31.sha-90df7de.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14749,7 +14749,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+28.sha-6ab8c0b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+31.sha-90df7de.with-local-changes');
 
 /**
  * @license
