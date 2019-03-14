b8b7086ed586cdf8e89c3f40398296750a8490ba
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 15:09:11 +0000

    8c4c985 docs: fix redirect with secondary outlet in router guide example (#29267)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+3.sha-caf3a53.with-local-changes
+ * @license Angular v7.2.9+4.sha-8c4c985.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10743,7 +10743,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.9+3.sha-caf3a53.with-local-changes');
+var VERSION = new Version('7.2.9+4.sha-8c4c985.with-local-changes');
 
 /**
  * @license
