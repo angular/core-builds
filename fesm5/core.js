b306dc1408579f2242b5ff12c90a2e988fb65b2c
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:09:46 +0000

    a5cbfa2 docs: backport fix to event-binding example for production build (#28423)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+4.sha-8c4c985.with-local-changes
+ * @license Angular v7.2.9+13.sha-a5cbfa2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10743,7 +10743,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.9+4.sha-8c4c985.with-local-changes');
+var VERSION = new Version('7.2.9+13.sha-a5cbfa2.with-local-changes');
 
 /**
  * @license
