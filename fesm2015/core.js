5a7089aae93d2d96e7aa5440df0fc92bf686fe83
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:15:09 +0000

    f50928f docs: remove configuration instructions at the top of the i18n guide (#29313)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+36.sha-1fb670e.with-local-changes
+ * @license Angular v8.0.0-beta.8+35.sha-f50928f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18614,7 +18614,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.8+36.sha-1fb670e.with-local-changes');
+const VERSION = new Version('8.0.0-beta.8+35.sha-f50928f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
