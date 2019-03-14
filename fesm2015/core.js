52645abac809a0cb4dba189436b015eb5a2b9e45
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:33:41 +0000

    d787925 docs: update developer guide for use with yarn (#29044)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+16.sha-4e6efb3.with-local-changes
+ * @license Angular v7.2.9+17.sha-d787925.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.9+16.sha-4e6efb3.with-local-changes');
+const VERSION = new Version('7.2.9+17.sha-d787925.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
