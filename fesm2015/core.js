d098c5d90efd5d410309644fd33eb6a910c99767
Matias Niemelä <matias@yearofmoo.com>
2019-03-18 21:54:52 +0000

    f03d724 build: re-add bazel symlink prefix in patch branch (#29312)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+21.sha-67f4852.with-local-changes
+ * @license Angular v7.2.9+23.sha-f03d724.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.9+21.sha-67f4852.with-local-changes');
+const VERSION = new Version('7.2.9+23.sha-f03d724.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
