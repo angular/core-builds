15a5313d496e8b6136f45996b08b6f2f90087f46
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:30:31 +0000

    e769f9c docs(forms): fix small typo in forms documentation (#29370)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+74.sha-4a665ca.with-local-changes
+ * @license Angular v8.0.0-beta.8+75.sha-e769f9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14811,7 +14811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.8+74.sha-4a665ca.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.8+75.sha-e769f9c.with-local-changes');
 
     /**
      * @license
