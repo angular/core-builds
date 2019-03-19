99364cb3f4f334991d84f272776ce387eda57702
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 07:02:38 +0000

    761bb0a ci: add `.vscode/**` to the `@angular/fw-dev-infra` group (#29302)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+23.sha-f03d724.with-local-changes
+ * @license Angular v7.2.9+25.sha-761bb0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13813,7 +13813,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.9+23.sha-f03d724.with-local-changes');
+const VERSION = new Version('7.2.9+25.sha-761bb0a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
