ed88a4b90ecdb6e48ab0243a623971577b897291
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:51:10 +0000

    d6e27a4 test(ivy): remove passing snack bar test from blocklist (#29423)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes
+ * @license Angular v8.0.0-beta.9+15.sha-d6e27a4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15033,7 +15033,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+15.sha-d6e27a4.with-local-changes');
 
 /**
  * @license
