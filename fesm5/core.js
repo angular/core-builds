f81d4d03f7e09f4dd1096038d933b87516d1b856
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 01:44:26 +0000

    20bf4ca release: cut the v8.0.0-beta.9 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+89.sha-aae6f7b.with-local-changes
+ * @license Angular v8.0.0-beta.9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15033,7 +15033,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+89.sha-aae6f7b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9.with-local-changes');
 
 /**
  * @license
