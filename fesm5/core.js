b5fedfe5cd18c63252327db9fe179d5edd87081c
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:50:28 +0000

    1a4d4a0 docs: fix typo (culd -> could) (#29412)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+13.sha-3121409.with-local-changes
+ * @license Angular v8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14962,7 +14962,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+13.sha-3121409.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes');
 
 /**
  * @license
