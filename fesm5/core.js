abd368eac75f03df87ece531f76a2d668cc198d3
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:30:57 +0000

    fd122b0 ci: cache Material `node_modules` based on the lockfile checksum (#29417)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+11.sha-9a364a8.with-local-changes
+ * @license Angular v8.0.0-beta.9+12.sha-fd122b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14962,7 +14962,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+11.sha-9a364a8.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+12.sha-fd122b0.with-local-changes');
 
 /**
  * @license
