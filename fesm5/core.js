59cb8ef4865bba92ea29a46fce50ec3a65da35c4
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 23:20:21 +0000

    c75e16a docs: add Luis Aviles to GDE resources (#29405)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+6.sha-1affcc6.with-local-changes
+ * @license Angular v7.2.10+7.sha-c75e16a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10743,7 +10743,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.10+6.sha-1affcc6.with-local-changes');
+var VERSION = new Version('7.2.10+7.sha-c75e16a.with-local-changes');
 
 /**
  * @license
