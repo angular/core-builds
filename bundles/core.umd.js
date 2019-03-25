8a6af0ca3aa3d923373379fd695dc57dd0aa3105
Misko Hevery <misko@hevery.com>
2019-03-25 21:01:36 +0000

    6feef36 test(ivy): add tests for component factory projectable node corner cases (#27791)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+90.sha-c34c223.with-local-changes
+ * @license Angular v8.0.0-beta.9+91.sha-6feef36.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15127,7 +15127,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.9+90.sha-c34c223.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.9+91.sha-6feef36.with-local-changes');
 
     /**
      * @license
