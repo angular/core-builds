73da9cde6bcdacbc962a025d9e6cf75d731567c6
Miško Hevery <misko@hevery.com>
2019-03-26 23:28:31 +0000

    495a4c1 docs: release notes for the v7.2.11 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+99.sha-17f7bdb.with-local-changes
+ * @license Angular v8.0.0-beta.10.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18840,7 +18840,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.9+99.sha-17f7bdb.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
