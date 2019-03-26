6a04c916a336ad8a8fa8d533c153214255a758e4
Miško Hevery <misko@hevery.com>
2019-03-26 20:08:37 +0000

    17f7bdb build: update zone.js to 0.9.0 (#28219)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+97.sha-c412374.with-local-changes
+ * @license Angular v8.0.0-beta.9+99.sha-17f7bdb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -125,7 +125,7 @@ function runInTestZone(fn, context, finishCallback, failCallback) {
     const proxyZone = Zone.current.getZoneWith('ProxyZoneSpec');
     /** @type {?} */
     const previousDelegate = proxyZoneSpec.getDelegate();
-    proxyZone.parent.run((/**
+    (/** @type {?} */ ((/** @type {?} */ (proxyZone)).parent)).run((/**
      * @return {?}
      */
     () => {
