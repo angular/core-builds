d766777346036a07e7f2b446d12f9dbd566266b5
Miško Hevery <misko@hevery.com>
2019-03-27 17:18:00 +0000

    50fb629 docs: add Bonnie to GDE page (#29432)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+9.sha-309ffe7.with-local-changes
+ * @license Angular v8.0.0-beta.10+10.sha-50fb629.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15016,7 +15016,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+9.sha-309ffe7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+10.sha-50fb629.with-local-changes');
 
 /**
  * @license
