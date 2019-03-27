3e44b350887852c88cc6c05ec235091d80093bc5
Miško Hevery <misko@hevery.com>
2019-03-27 17:16:38 +0000

    3514543 build(bazel): update integration/bazel lock file (#29508)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+9.sha-309ffe7.with-local-changes
+ * @license Angular v8.0.0-beta.10+6.sha-3514543.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
