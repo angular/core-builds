8dfb6949bbe29c2af4a078ca45be0d008712de4f
Jason Aden <jasonaden@google.com>
2019-03-29 17:47:54 +0000

    d9b1136 docs: add note about ngModel usage in structural directives guide (#29522)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+16.sha-3ad67e5.with-local-changes
+ * @license Angular v7.2.11+18.sha-d9b1136.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10744,7 +10744,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+16.sha-3ad67e5.with-local-changes');
+var VERSION = new Version('7.2.11+18.sha-d9b1136.with-local-changes');
 
 /**
  * @license
