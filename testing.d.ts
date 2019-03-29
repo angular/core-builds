a045b0ec1565393d3143dc4358898582db485f32
Jason Aden <jasonaden@google.com>
2019-03-29 17:55:19 +0000

    6d7a4ff docs: move text that mention what is expected to fail up higher into the section (#27329)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+19.sha-72900ec.with-local-changes
+ * @license Angular v7.2.11+23.sha-6d7a4ff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
