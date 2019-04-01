61c7ebd3e8603d02fb271e26991120a495668a5a
Jason Aden <jasonaden@google.com>
2019-04-01 18:26:49 +0000

    e4eed73 docs(http): add information about body content type to flush method for testing (#29214)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+37.sha-a52123f.with-local-changes
+ * @license Angular v7.2.11+38.sha-e4eed73.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10744,7 +10744,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+37.sha-a52123f.with-local-changes');
+var VERSION = new Version('7.2.11+38.sha-e4eed73.with-local-changes');
 
 /**
  * @license
