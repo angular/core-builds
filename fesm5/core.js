df1c0bb9a61e5c370d59ab14915da4ee696a854b
Jason Aden <jasonaden@google.com>
2019-04-01 18:49:05 +0000

    e8b00ec docs: add missing window key shortcut to language service guide (#29613)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+41.sha-94b0e8f.with-local-changes
+ * @license Angular v7.2.11+42.sha-e8b00ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10744,7 +10744,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+41.sha-94b0e8f.with-local-changes');
+var VERSION = new Version('7.2.11+42.sha-e8b00ec.with-local-changes');
 
 /**
  * @license
