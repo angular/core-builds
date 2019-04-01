4651142e633b9b03f6f62f30a3a8370df23199cc
Jason Aden <jasonaden@google.com>
2019-04-01 19:27:30 +0000

    e884c0c fix(router): adjust setting navigationTransition when a new navigation cancels an existing one (#29636)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+44.sha-ab72e06.with-local-changes
+ * @license Angular v7.2.11+45.sha-e884c0c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10789,7 +10789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.11+44.sha-ab72e06.with-local-changes');
+    var VERSION = new Version('7.2.11+45.sha-e884c0c.with-local-changes');
 
     /**
      * @license
