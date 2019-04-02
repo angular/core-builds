026309e34a7ed9d0cbaeb897c350c80abe5b43f2
Jason Aden <jasonaden@google.com>
2019-04-02 18:01:17 +0000

    c7075fe refactor(compiler): rename INTERPOLATION_BLACKLIST_REGEXPS (#29593)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+53.sha-f6864cc.with-local-changes
+ * @license Angular v7.2.11+55.sha-c7075fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10789,7 +10789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.11+53.sha-f6864cc.with-local-changes');
+    var VERSION = new Version('7.2.11+55.sha-c7075fe.with-local-changes');
 
     /**
      * @license
