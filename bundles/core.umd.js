3a2d428e23d46ec83c95b1c5298b7e2360d6836f
Jason Aden <jasonaden@google.com>
2019-04-02 19:02:14 +0000

    98f8b0f fix(ivy): ngcc - properly handle aliases class expressions (#29119)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+93.sha-63013f1.with-local-changes
+ * @license Angular v8.0.0-beta.10+108.sha-98f8b0f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5984,7 +5984,7 @@
      * execution if used in URL context within a HTML document. Specifically, this
      * regular expression matches if (comment from here on and regex copied from
      * Soy's EscapingConventions):
-     * (1) Either a protocol in a whitelist (http, https, mailto or ftp).
+     * (1) Either an allowed protocol (http, https, mailto or ftp).
      * (2) or no protocol.  A protocol must be followed by a colon. The below
      *     allows that by allowing colons only after one of the characters [/?#].
      *     A colon after a hash (#) must be in the fragment.
@@ -15150,7 +15150,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.10+93.sha-63013f1.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.10+108.sha-98f8b0f.with-local-changes');
 
     /**
      * @license
