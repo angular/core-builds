e83b75cb273d7ff8b3047675530d5865aeb5a776
Jason Aden <jasonaden@google.com>
2019-04-02 18:02:31 +0000

    cbd5d28 docs: add info regarding injecting custom pipes (#28291)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+100.sha-1293da1.with-local-changes
+ * @license Angular v8.0.0-beta.10+106.sha-cbd5d28.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18955,7 +18955,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+100.sha-1293da1.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+106.sha-cbd5d28.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
