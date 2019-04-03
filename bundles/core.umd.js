56c45b7649feb04016e8f316edeb69bb8111bb78
Jason Aden <jasonaden@google.com>
2019-04-03 22:37:25 +0000

    0a530e6 release: cut the v8.0.0-beta.11 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+124.sha-dc5577d.with-local-changes
+ * @license Angular v8.0.0-beta.11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15195,7 +15195,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.10+124.sha-dc5577d.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.11.with-local-changes');
 
     /**
      * @license
