30277add10a297664f8d10d3d4d44e076da24ba2
Igor Minar <igor@angular.io>
2019-04-08 17:04:38 +0000

    8bbf481 refactor(docs-infra): rename properties (blacklisted --> ignored) (#29754)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+29.sha-ec56354.with-local-changes
+ * @license Angular v8.0.0-beta.11+32.sha-8bbf481.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15652,7 +15652,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.11+29.sha-ec56354.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.11+32.sha-8bbf481.with-local-changes');
 
     /**
      * @license
