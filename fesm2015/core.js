1e436ea2dde4a36c5ff6121794b309a52c997b82
Igor Minar <igor@angular.io>
2019-04-08 19:23:18 +0000

    98cf3e8 fix(ivy): creation mode not run on view detached from change detection (#29741)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+45.sha-84be7c5.with-local-changes
+ * @license Angular v8.0.0-beta.11+44.sha-98cf3e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19506,7 +19506,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.11+45.sha-84be7c5.with-local-changes');
+const VERSION = new Version('8.0.0-beta.11+44.sha-98cf3e8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
