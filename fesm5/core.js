d3c8087fe0fe8674e55508970c9ee06d370653bb
Igor Minar <igor@angular.io>
2019-04-09 00:38:55 +0000

    8c0c689 docs(router): use pipe in activatedRoute example (#29752)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+19.sha-77ee22e.with-local-changes
+ * @license Angular v7.2.12+18.sha-8c0c689.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10744,7 +10744,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.12+19.sha-77ee22e.with-local-changes');
+var VERSION = new Version('7.2.12+18.sha-8c0c689.with-local-changes');
 
 /**
  * @license
