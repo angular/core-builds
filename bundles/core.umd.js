e6006efbfa9309646b8edc482903638766dd6ca7
Igor Minar <igor@angular.io>
2019-04-10 19:42:30 +0000

    7f1c5f6 docs: fix grammatical errors in the style guide (#29769)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+26.sha-42b7f85.with-local-changes
+ * @license Angular v7.2.12+25.sha-7f1c5f6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10789,7 +10789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.12+26.sha-42b7f85.with-local-changes');
+    var VERSION = new Version('7.2.12+25.sha-7f1c5f6.with-local-changes');
 
     /**
      * @license
