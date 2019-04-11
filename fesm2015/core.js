cd2546a9b2f4b0be49df49e7d273c5960a0d5aee
Igor Minar <igor@angular.io>
2019-04-11 15:40:39 +0000

    387fbb8 fix(common): add `@Injectable()` to common pipes (#29834)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+81.sha-5b32f55.with-local-changes
+ * @license Angular v8.0.0-beta.11+82.sha-387fbb8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19830,7 +19830,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.11+81.sha-5b32f55.with-local-changes');
+const VERSION = new Version('8.0.0-beta.11+82.sha-387fbb8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
