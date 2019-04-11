8fda7a7e40438e163d670fb649e263063c6f7ddd
Igor Minar <igor@angular.io>
2019-04-11 15:23:05 +0000

    4dc066b docs(aio): add missing mentors for collaborators (#29142)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+28.sha-ca14509.with-local-changes
+ * @license Angular v7.2.12+30.sha-4dc066b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10789,7 +10789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.12+28.sha-ca14509.with-local-changes');
+    var VERSION = new Version('7.2.12+30.sha-4dc066b.with-local-changes');
 
     /**
      * @license
