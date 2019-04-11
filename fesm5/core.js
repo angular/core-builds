f6c1b9f52d8ffcc20c3bf512b43f7363f34151f4
Igor Minar <igor@angular.io>
2019-04-11 15:20:11 +0000

    f98093a docs(aio): add missing mentors for collaborators (#29142)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+73.sha-712d60e.with-local-changes
+ * @license Angular v8.0.0-beta.11+75.sha-f98093a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15946,7 +15946,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+73.sha-712d60e.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+75.sha-f98093a.with-local-changes');
 
 /**
  * @license
