2985158950a51433f39833548de00f0ac4f9f6fc
Ben Lesh <ben@benlesh.com>
2019-04-17 20:02:57 +0000

    725148a feat(ivy): let ngtsc statically evaluate `Array.concat` calls (#29887)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+8.sha-696e520.with-local-changes
+ * @license Angular v8.0.0-beta.13+9.sha-725148a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16076,7 +16076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.13+8.sha-696e520.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.13+9.sha-725148a.with-local-changes');
 
     /**
      * @license
