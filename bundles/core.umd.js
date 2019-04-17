2be9c59b727cbd3e0fa0c63b63a126285ba6aeae
Ben Lesh <ben@benlesh.com>
2019-04-17 20:08:41 +0000

    83291f0 fix(ivy): let ngtsc unwrap expressions when resolving `forwardRef` (#29886)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+9.sha-725148a.with-local-changes
+ * @license Angular v8.0.0-beta.13+10.sha-83291f0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16076,7 +16076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.13+9.sha-725148a.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.13+10.sha-83291f0.with-local-changes');
 
     /**
      * @license
