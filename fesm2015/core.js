4f6d82809483a4aabd715ded05b3a38f22ce4db4
Ben Lesh <ben@benlesh.com>
2019-04-22 16:08:28 +0000

    546299b docs: add ng-japan 2019 event (#30021) PR Close #30021

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+19.sha-a593b3f.with-local-changes
+ * @license Angular v7.2.13+20.sha-546299b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13814,7 +13814,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.13+19.sha-a593b3f.with-local-changes');
+const VERSION = new Version('7.2.13+20.sha-546299b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
