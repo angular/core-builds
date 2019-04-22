60bee9fc7df1e267051b57f9fadb9acf52ea853b
Ben Lesh <ben@benlesh.com>
2019-04-22 18:42:10 +0000

    ceea0b4 docs: fix typo in routing section of Tour of Heroes (#29961)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+69.sha-a9242c4.with-local-changes
+ * @license Angular v8.0.0-beta.13+72.sha-ceea0b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16044,7 +16044,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+69.sha-a9242c4.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+72.sha-ceea0b4.with-local-changes');
 
 /**
  * @license
