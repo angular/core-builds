57737fb4d275f82d7d6d5eb47b07f9b9233f95f8
Ben Lesh <ben@benlesh.com>
2019-04-22 18:36:06 +0000

    63523f7 fix(ivy): avoid generating instructions for empty style and class bindings (#30024)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+68.sha-94aeeec.with-local-changes
+ * @license Angular v8.0.0-beta.13+70.sha-63523f7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16044,7 +16044,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+68.sha-94aeeec.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+70.sha-63523f7.with-local-changes');
 
 /**
  * @license
