dba346c3d6f54d267e656aec82c9491f06d0ec8c
Ben Lesh <ben@benlesh.com>
2019-04-23 00:43:01 +0000

    0f9230d feat(ivy): generate ɵɵproperty in host bindings (#30009)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+78.sha-19dfadb.with-local-changes
+ * @license Angular v8.0.0-beta.13+79.sha-0f9230d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16506,7 +16506,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+78.sha-19dfadb.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+79.sha-0f9230d.with-local-changes');
 
 /**
  * @license
