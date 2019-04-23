a64233bf49bf5e90a7ae06dfa9a936e8e1b015a7
Ben Lesh <ben@benlesh.com>
2019-04-23 22:04:49 +0000

    aaf8145 fix(ivy): support module.id as @NgModule's "id" field value (#30040)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14.with-local-changes
+ * @license Angular v8.0.0-beta.14+2.sha-aaf8145.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16560,7 +16560,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-beta.14.with-local-changes');
+    var VERSION = new Version('8.0.0-beta.14+2.sha-aaf8145.with-local-changes');
 
     /**
      * @license
