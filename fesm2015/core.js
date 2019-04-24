3f08bbaa3554be7510b18b16994726c52d84ba01
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:19:55 +0000

    644f898 docs: add Andrew Kushnir to Angular team (#30045)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+2.sha-451d4bf.with-local-changes
+ * @license Angular v7.2.14+3.sha-644f898.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13814,7 +13814,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.14+2.sha-451d4bf.with-local-changes');
+const VERSION = new Version('7.2.14+3.sha-644f898.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
