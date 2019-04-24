777e09e40fb9b052d7772c8e86ef07be57185eb6
Ben Lesh <ben@benlesh.com>
2019-04-24 21:03:33 +0000

    071ee64 refactor(common): rename LocationUpgradeService and remove angular/upgrade dependency (#30055)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+19.sha-3938563.with-local-changes
+ * @license Angular v8.0.0-beta.14+31.sha-071ee64.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20459,7 +20459,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.14+19.sha-3938563.with-local-changes');
+const VERSION = new Version('8.0.0-beta.14+31.sha-071ee64.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
