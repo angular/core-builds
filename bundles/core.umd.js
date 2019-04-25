ef989ab587df49a582412e0cfd1ff234f856dc1f
Andrew Kushnir <akushnir@google.com>
2019-04-25 19:32:06 +0000

    07f0385 test(upgrade): rename `angular.module()` to `angular.module_()` (#30107)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+10.sha-d025159.with-local-changes
+ * @license Angular v7.2.14+13.sha-07f0385.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10789,7 +10789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('7.2.14+10.sha-d025159.with-local-changes');
+    var VERSION = new Version('7.2.14+13.sha-07f0385.with-local-changes');
 
     /**
      * @license
