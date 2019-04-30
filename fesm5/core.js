8d9e70fbd2616a162fbabc6b55c89dceb876a246
Kara Erickson <karakara@google.com>
2019-04-30 23:31:22 +0000

    b4a2cbb ci(docs-infra): use the tests from the stable branch in `aio_monitoring_stable` CircleCI job (#30110)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+13.sha-07f0385.with-local-changes
+ * @license Angular v7.2.14+17.sha-b4a2cbb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10744,7 +10744,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.14+13.sha-07f0385.with-local-changes');
+var VERSION = new Version('7.2.14+17.sha-b4a2cbb.with-local-changes');
 
 /**
  * @license
