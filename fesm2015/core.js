91d8d8e678749d666e4b5ca7e4439a124e3a805a
Kara Erickson <karakara@google.com>
2019-05-06 23:33:03 +0000

    b40f6f3 fix(router): ensure `history.state` is set in `eager` update mode (#30154)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+90.sha-3327bd8.with-local-changes
+ * @license Angular v8.0.0-rc.0+91.sha-b40f6f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20630,7 +20630,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+90.sha-3327bd8.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+91.sha-b40f6f3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
