a62f447ab5072cc1e91e724de94a5c83ec1418c8
Kara Erickson <karakara@google.com>
2019-05-06 23:27:59 +0000

    3327bd8 fix(router): fix a problem with router not responding to back button (#30160)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+89.sha-ea9a381.with-local-changes
+ * @license Angular v8.0.0-rc.0+90.sha-3327bd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16692,7 +16692,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-rc.0+89.sha-ea9a381.with-local-changes');
+    var VERSION = new Version('8.0.0-rc.0+90.sha-3327bd8.with-local-changes');
 
     /**
      * @license
