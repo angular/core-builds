2fdb54d57ad7581e68d34501509b28a7438e8440
Kara Erickson <karakara@google.com>
2019-05-06 23:26:50 +0000

    6a987f1 docs(forms): fix import line ending (#30290) PR Close #30290

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+21.sha-548b003.with-local-changes
+ * @license Angular v8.0.0-rc.2+22.sha-6a987f1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16576,7 +16576,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.2+21.sha-548b003.with-local-changes');
+var VERSION = new Version('8.0.0-rc.2+22.sha-6a987f1.with-local-changes');
 
 /**
  * @license
