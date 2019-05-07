38915ae6f6a8c57d1679fb0492669b53f88572a0
Kara Erickson <karakara@google.com>
2019-05-07 22:10:08 +0000

    bd37622 docs: release notes for the v8.0.0-rc.3 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+99.sha-48de6e8.with-local-changes
+ * @license Angular v8.0.0-rc.0+100.sha-bd37622.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16392,7 +16392,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+99.sha-48de6e8.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+100.sha-bd37622.with-local-changes');
 
 /**
  * @license
