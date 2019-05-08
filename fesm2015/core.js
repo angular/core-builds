075635da4898e3ab78101c705d8a6648d2ed4ef5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 21:51:49 +0000

    452f121 fix: ensure strict mode when evaluating in JIT (#30122)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+119.sha-728db88.with-local-changes
+ * @license Angular v8.0.0-rc.0+120.sha-452f121.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20267,7 +20267,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+119.sha-728db88.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+120.sha-452f121.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
