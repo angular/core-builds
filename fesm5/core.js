b644ae8e8ade7efae63ac9bb41da02516ed1ef52
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:52:35 +0000

    12fb639 fix(core): static-query migration should not prompt if no queries are used (#30254)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+6.sha-644925f.with-local-changes
+ * @license Angular v8.0.0-rc.3+7.sha-12fb639.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16576,7 +16576,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.3+6.sha-644925f.with-local-changes');
+var VERSION = new Version('8.0.0-rc.3+7.sha-12fb639.with-local-changes');
 
 /**
  * @license
