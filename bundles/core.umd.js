33c5f4abbc9edc1efa47b1fddc4fcd4dc55d88a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:45:59 +0000

    d9b9ed5 test(ivy): move host binding tests to acceptance (#30308)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+107.sha-7c8a62d.with-local-changes
+ * @license Angular v8.0.0-rc.0+108.sha-d9b9ed5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16425,7 +16425,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-rc.0+107.sha-7c8a62d.with-local-changes');
+    var VERSION = new Version('8.0.0-rc.0+108.sha-d9b9ed5.with-local-changes');
 
     /**
      * @license
