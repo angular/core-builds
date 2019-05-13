510c0276ad285e54cb59ec848bcb210f517e4698
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:09:39 +0000

    0cf09fc test(ivy): use `TestBed` for render3 i18n tests (#30352) PR Close #30352

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+151.sha-877b228.with-local-changes
+ * @license Angular v8.0.0-rc.0+152.sha-0cf09fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16603,7 +16603,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+151.sha-877b228.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+152.sha-0cf09fc.with-local-changes');
 
 /**
  * @license
