d5ff49aa05d1a33c462062bd51a99aaa86c277c7
Jason Aden <jasonaden@google.com>
2019-05-22 23:36:13 +0000

    62c936a docs: update node and typescript versions mentioned in docs (#30601)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+45.sha-77e1b34.with-local-changes
+ * @license Angular v8.0.0-rc.4+46.sha-62c936a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16583,7 +16583,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.4+45.sha-77e1b34.with-local-changes');
+var VERSION = new Version('8.0.0-rc.4+46.sha-62c936a.with-local-changes');
 
 /**
  * @license
