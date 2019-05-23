99f90f29c484067e584fa5dfcb3a4f299ecbeb8d
Jason Aden <jasonaden@google.com>
2019-05-23 00:00:33 +0000

    fa6cbb3 fix(ivy): ng-container with ViewContainerRef creates two comments (#30611)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+302.sha-e20b92b.with-local-changes
+ * @license Angular v8.0.0-rc.0+303.sha-fa6cbb3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16884,8 +16884,16 @@
             lContainer[ACTIVE_INDEX] = -1;
         }
         else {
-            var commentNode = hostView[RENDERER].createComment(ngDevMode ? 'container' : '');
-            ngDevMode && ngDevMode.rendererCreateComment++;
+            var commentNode = void 0;
+            // If the host is an element container, the native host element is guaranteed to be a
+            // comment and we can reuse that comment as anchor element for the new LContainer.
+            if (hostTNode.type === 4 /* ElementContainer */) {
+                commentNode = unwrapRNode(slotValue);
+            }
+            else {
+                ngDevMode && ngDevMode.rendererCreateComment++;
+                commentNode = hostView[RENDERER].createComment(ngDevMode ? 'container' : '');
+            }
             // A container can be created on the root (topmost / bootstrapped) component and in this case we
             // can't use LTree to insert container's marker node (both parent of a comment node and the
             // commend node itself is located outside of elements hold by LTree). In this specific case we
@@ -17120,7 +17128,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-rc.0+302.sha-e20b92b.with-local-changes');
+    var VERSION = new Version('8.0.0-rc.0+303.sha-fa6cbb3.with-local-changes');
 
     /**
      * @license
