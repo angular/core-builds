7f3d69ee842761b009a5660bd4d9bfd3ccb7ebac
Jason Aden <jasonaden@google.com>
2019-05-23 18:13:29 +0000

    faac51f test(core): update query-specific tests in core (#30626)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+320.sha-21e8ac1.with-local-changes
+ * @license Angular v8.0.0-rc.0+324.sha-faac51f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17471,7 +17471,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+320.sha-21e8ac1.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+324.sha-faac51f.with-local-changes');
 
 /**
  * @license
