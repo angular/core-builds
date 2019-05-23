f7c29c02c58c6cc411036ef01e255ccd145ebc7d
Jason Aden <jasonaden@google.com>
2019-05-23 20:53:59 +0000

    b5c8939 docs: add v8 deprecations (#30598)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+324.sha-faac51f.with-local-changes
+ * @license Angular v8.0.0-rc.0+327.sha-b5c8939.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17787,7 +17787,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+324.sha-faac51f.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+327.sha-b5c8939.with-local-changes');
 
 /**
  * @license
