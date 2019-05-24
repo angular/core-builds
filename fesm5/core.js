05dcd299d4581e1dd1612f52b49b9b783006fa8c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-24 10:45:46 +0000

    54ee5cd docs(core): static-query migration should use permalink for migration guide

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+336.sha-05479f8.with-local-changes
+ * @license Angular v8.0.0-rc.0+336.sha-54ee5cd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17787,7 +17787,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+336.sha-05479f8.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+336.sha-54ee5cd.with-local-changes');
 
 /**
  * @license
