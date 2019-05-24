7633f7e20608eeb129be77f7fbcbfc4e11929730
Igor Minar <igor@angular.io>
2019-05-24 15:27:51 +0000

    d5dd907 docs(core): static-query migration should use permalink for migration guide (#30649)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5.with-local-changes
+ * @license Angular v8.0.0-rc.5+2.sha-d5dd907.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16628,7 +16628,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-rc.5.with-local-changes');
+    var VERSION = new Version('8.0.0-rc.5+2.sha-d5dd907.with-local-changes');
 
     /**
      * @license
