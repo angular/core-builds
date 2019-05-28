121915a47ecac52837b6f22f9726f903f10891ad
Igor Minar <igor@angular.io>
2019-05-28 16:57:39 +0000

    c61c14a docs: remove core.module.ts from file tree in style guide (#30679)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+6.sha-45718b3.with-local-changes
+ * @license Angular v8.0.0-rc.5+11.sha-c61c14a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16628,7 +16628,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-rc.5+6.sha-45718b3.with-local-changes');
+    var VERSION = new Version('8.0.0-rc.5+11.sha-c61c14a.with-local-changes');
 
     /**
      * @license
