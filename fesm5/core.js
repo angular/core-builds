7169bffdb439d218b685babb69100e6b08a3d1df
Igor Minar <igor@angular.io>
2019-05-28 16:58:53 +0000

    002fbdd docs: add default AOT to migrating projects to Ivy (#30697)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+11.sha-c61c14a.with-local-changes
+ * @license Angular v8.0.0-rc.5+9.sha-002fbdd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16583,7 +16583,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.5+11.sha-c61c14a.with-local-changes');
+var VERSION = new Version('8.0.0-rc.5+9.sha-002fbdd.with-local-changes');
 
 /**
  * @license
