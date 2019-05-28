dbc04a48cff4761a964e636e4dc9aea6686b64b3
Rado Kirov <rkirov@gmail.com>
2019-05-28 20:54:16 +0000

    8a68ec0 docs: improve glossary for entry-point.

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+353.sha-1551920.with-local-changes
+ * @license Angular v8.0.0-rc.0+353.sha-8a68ec0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18321,7 +18321,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0-rc.0+353.sha-1551920.with-local-changes');
+    var VERSION = new Version('8.0.0-rc.0+353.sha-8a68ec0.with-local-changes');
 
     /**
      * @license
