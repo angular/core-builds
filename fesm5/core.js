77522dfb57928fcb8723c8eaa76274d3a9ae0083
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:30:27 +0000

    71be1da docs: fix deprecation information (#30717)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+373.sha-e3f3b10.with-local-changes
+ * @license Angular v8.0.0-rc.0+371.sha-71be1da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18358,7 +18358,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+373.sha-e3f3b10.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+371.sha-71be1da.with-local-changes');
 
 /**
  * @license
