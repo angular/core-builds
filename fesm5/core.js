01690f2ce88ac4abd325fea3455e43bd4098693c
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:12:20 +0000

    ae6fa92 docs: change wording for more clarity in TypeScript Configuration guide (#30748)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+5.sha-9a9ae60.with-local-changes
+ * @license Angular v8.0.0+4.sha-ae6fa92.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16583,7 +16583,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+5.sha-9a9ae60.with-local-changes');
+var VERSION = new Version('8.0.0+4.sha-ae6fa92.with-local-changes');
 
 /**
  * @license
