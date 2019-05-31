4d6aa49c345feef6a897aa5e7d5dac79cf8a102b
Misko Hevery <misko@hevery.com>
2019-05-31 04:10:32 +0000

    d82adbe fix(ivy): align DebugNode/DebugElement behavior with View Engine (#30756)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+4.sha-99c9bca.with-local-changes
+ * @license Angular v8.1.0-beta.0+2.sha-d82adbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18367,7 +18367,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-beta.0+4.sha-99c9bca.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+2.sha-d82adbe.with-local-changes');
 
 /**
  * @license
@@ -27015,7 +27015,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
