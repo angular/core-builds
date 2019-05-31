06072406bfa7b9e7fbe31e685c8b5b5c3676bd4d
Miško Hevery <misko@hevery.com>
2019-05-31 22:44:28 +0000

    8ce288a fix(docs-infra): add v7 in versions dropdown (#30775)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+12.sha-f936590.with-local-changes
+ * @license Angular v8.1.0-beta.0+13.sha-8ce288a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18402,7 +18402,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-beta.0+12.sha-f936590.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+13.sha-8ce288a.with-local-changes');
 
 /**
  * @license
@@ -27048,7 +27048,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
