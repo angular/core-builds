aef1815bd6287164dfec1278f0d19b0e536e7f90
Misko Hevery <misko@hevery.com>
2019-06-03 16:26:31 +0000

    8bedf50 docs: update example references from "my-app" to "app-root" (#30789)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+16.sha-32886cf.with-local-changes
+ * @license Angular v8.1.0-beta.0+17.sha-8bedf50.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18718,7 +18718,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-beta.0+16.sha-32886cf.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+17.sha-8bedf50.with-local-changes');
 
 /**
  * @license
