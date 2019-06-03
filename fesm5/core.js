461d2a7f1608464c925e5705ac004c7dedf7ba8f
Misko Hevery <misko@hevery.com>
2019-06-03 19:28:52 +0000

    06ffed5 docs: document UrlTree in guard return type in cheatsheet (#30822)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+28.sha-5a15126.with-local-changes
+ * @license Angular v8.0.0+29.sha-06ffed5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16583,7 +16583,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+28.sha-5a15126.with-local-changes');
+var VERSION = new Version('8.0.0+29.sha-06ffed5.with-local-changes');
 
 /**
  * @license
