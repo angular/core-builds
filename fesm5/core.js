0381012da122de42c093eb06569ec6461e160804
Misko Hevery <misko@hevery.com>
2019-06-04 19:37:16 +0000

    ccc1c27 refactor: Move away from index signature based "updateCache". (#30518)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+35.sha-19caace.with-local-changes
+ * @license Angular v8.0.0+34.sha-ccc1c27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+35.sha-19caace.with-local-changes');
+var VERSION = new Version('8.0.0+34.sha-ccc1c27.with-local-changes');
 
 /**
  * @license
