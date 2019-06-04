07f840eb9d19a8f0e0710af3c340500d353be3fd
Misko Hevery <misko@hevery.com>
2019-06-04 19:22:37 +0000

    7c41abe docs: rename component for constructor example (#30845) PR Close #30845

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+33.sha-97268b9.with-local-changes
+ * @license Angular v8.0.0+32.sha-7c41abe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+33.sha-97268b9.with-local-changes');
+var VERSION = new Version('8.0.0+32.sha-7c41abe.with-local-changes');
 
 /**
  * @license
