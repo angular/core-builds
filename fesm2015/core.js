49e4837d2643315640692b374ab54242e8fa81e8
Miško Hevery <misko@hevery.com>
2019-06-06 16:19:34 +0000

    4cef2c1 build(docs-infra): fix CLI command github links (#30889)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+40.sha-26e3615.with-local-changes
+ * @license Angular v8.0.0+41.sha-4cef2c1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20524,7 +20524,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+40.sha-26e3615.with-local-changes');
+const VERSION = new Version('8.0.0+41.sha-4cef2c1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
