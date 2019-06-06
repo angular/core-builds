ea26050a62e90733dbf6f4257fac8140d9c2002a
Misko Hevery <misko@hevery.com>
2019-06-06 04:48:27 +0000

    ea2d453 fix(ivy): ngcc - use spaces in overwritten `package.json` content for readability (#30831)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1.with-local-changes
+ * @license Angular v8.1.0-next.1+2.sha-ea2d453.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18563,7 +18563,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.1.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+2.sha-ea2d453.with-local-changes');
 
 /**
  * @license
