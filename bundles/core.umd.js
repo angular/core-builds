7884707098ccf4c64ee0fc682efe2d3c286e233e
Miško Hevery <misko@hevery.com>
2019-06-06 16:15:43 +0000

    26e3615 fix(core): TypeScript related migrations should cater for BOM (#30719)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+39.sha-65f2010.with-local-changes
+ * @license Angular v8.0.0+40.sha-26e3615.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16642,7 +16642,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0+39.sha-65f2010.with-local-changes');
+    var VERSION = new Version('8.0.0+40.sha-26e3615.with-local-changes');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+39.sha-65f2010.with-local-changes
+ * @license Angular v8.0.0+40.sha-26e3615.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+39.sha-65f2010.with-local-changes');
+var VERSION = new Version('8.0.0+40.sha-26e3615.with-local-changes');
 
 /**
  * @license
