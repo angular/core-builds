92ea8c6a068bcf4f64916b5c46c81d2af10bfb89
Misko Hevery <misko@hevery.com>
2019-06-06 04:49:59 +0000

    17bfedd fix(bazel): Load global stylesheet in dev and prod (#30879)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+3.sha-b51d8dd.with-local-changes
+ * @license Angular v8.1.0-next.1+1.sha-17bfedd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18563,7 +18563,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.1+3.sha-b51d8dd.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+1.sha-17bfedd.with-local-changes');
 
 /**
  * @license
