88902d28201c33b3b5d517e181fbd60824c31a52
Misko Hevery <misko@hevery.com>
2019-06-07 16:12:32 +0000

    8aef446 build(docs-infra): upgrade cli command docs sources to d09f130ce (#30909)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+46.sha-9aeef0a.with-local-changes
+ * @license Angular v8.0.0+45.sha-8aef446.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+46.sha-9aeef0a.with-local-changes');
+var VERSION = new Version('8.0.0+45.sha-8aef446.with-local-changes');
 
 /**
  * @license
