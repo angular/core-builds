7d2e5a469c7b78bbfdb44079a9252df16f8b6d4d
Misko Hevery <misko@hevery.com>
2019-06-07 17:04:33 +0000

    c038675 refactor(ivy): inherently call ɵɵselect(0) (#30830)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+10.sha-11a4454.with-local-changes
+ * @license Angular v8.1.0-next.1+18.sha-c038675.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11130,6 +11130,51 @@
         return I18nUpdateOpCodesDebug;
     }());
 
+    /**
+     * @license
+     * Copyright Google Inc. All Rights Reserved.
+     *
+     * Use of this source code is governed by an MIT-style license that can be
+     * found in the LICENSE file at https://angular.io/license
+     */
+    /**
+     * Selects an element for later binding instructions.
+     *
+     * Used in conjunction with instructions like {@link property} to act on elements with specified
+     * indices, for example those created with {@link element} or {@link elementStart}.
+     *
+     * ```ts
+     * (rf: RenderFlags, ctx: any) => {
+     *   if (rf & 1) {
+     *     element(0, 'div');
+     *   }
+     *   if (rf & 2) {
+     *     select(0); // Select the <div/> created above.
+     *     property('title', 'test');
+     *   }
+     *  }
+     * ```
+     * @param index the index of the item to act on with the following instructions
+     *
+     * @codeGenApi
+     */
+    function ɵɵselect(index) {
+        ngDevMode && assertGreaterThan(index, -1, 'Invalid index');
+        ngDevMode &&
+            assertLessThan(index, getLView().length - HEADER_OFFSET, 'Should be within range for the view data');
+        var lView = getLView();
+        selectInternal(lView, index);
+    }
+    function selectInternal(lView, index) {
+        // Flush the initial hooks for elements in the view that have been added up to this point.
+        executePreOrderHooks(lView, lView[TVIEW], getCheckNoChangesMode(), index);
+        // We must set the selected index *after* running the hooks, because hooks may have side-effects
+        // that cause other template functions to run, thus updating the selected index, which is global
+        // state. If we run `setSelectedIndex` *before* we run the hooks, in some cases the selected index
+        // will be altered by the time we leave the `ɵɵselect` instruction.
+        setSelectedIndex(index);
+    }
+
     var ɵ0$8 = function () { return Promise.resolve(null); };
     /**
      * A permanent marker promise which signifies that the current CD tree is
@@ -11401,7 +11446,7 @@
                 setPreviousOrParentTNode(null, true);
                 oldView = enterView(viewToRender, viewToRender[T_HOST]);
                 resetPreOrderHookFlags(viewToRender);
-                executeTemplate(tView.template, getRenderFlags(viewToRender), context);
+                executeTemplate(viewToRender, tView.template, getRenderFlags(viewToRender), context);
                 // This must be set to false immediately after the first creation run because in an
                 // ngFor loop, all the views will be created together before update mode runs and turns
                 // off firstTemplatePass. If we don't set it here, instances will perform directive
@@ -11426,13 +11471,13 @@
             }
             if (creationModeIsActive) {
                 // creation mode pass
-                templateFn && executeTemplate(templateFn, 1 /* Create */, context);
+                templateFn && executeTemplate(hostView, templateFn, 1 /* Create */, context);
                 refreshDescendantViews(hostView);
                 hostView[FLAGS] &= ~4 /* CreationMode */;
             }
             // update mode pass
             resetPreOrderHookFlags(hostView);
-            templateFn && executeTemplate(templateFn, 2 /* Update */, context);
+            templateFn && executeTemplate(hostView, templateFn, 2 /* Update */, context);
             refreshDescendantViews(hostView);
         }
         finally {
@@ -11442,11 +11487,16 @@
             leaveView(oldView);
         }
     }
-    function executeTemplate(templateFn, rf, context) {
+    function executeTemplate(lView, templateFn, rf, context) {
         ɵɵnamespaceHTML();
         var prevSelectedIndex = getSelectedIndex();
         try {
             setActiveHostElement(null);
+            if (rf & 2 /* Update */) {
+                // When we're updating, have an inherent ɵɵselect(0) so we don't have to generate that
+                // instruction for most update blocks
+                selectInternal(lView, 0);
+            }
             templateFn(rf, context);
         }
         finally {
@@ -12544,7 +12594,7 @@
         try {
             resetPreOrderHookFlags(hostView);
             creationMode && executeViewQueryFn(1 /* Create */, hostTView, component);
-            executeTemplate(templateFn, getRenderFlags(hostView), component);
+            executeTemplate(hostView, templateFn, getRenderFlags(hostView), component);
             refreshDescendantViews(hostView);
             // Only check view queries again in creation mode if there are static view queries
             if (!creationMode || hostTView.staticViewQueries) {
@@ -16553,48 +16603,6 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /**
-     * Selects an element for later binding instructions.
-     *
-     * Used in conjunction with instructions like {@link property} to act on elements with specified
-     * indices, for example those created with {@link element} or {@link elementStart}.
-     *
-     * ```ts
-     * (rf: RenderFlags, ctx: any) => {
-     *   if (rf & 1) {
-     *     element(0, 'div');
-     *   }
-     *   if (rf & 2) {
-     *     select(0); // Select the <div/> created above.
-     *     property('title', 'test');
-     *   }
-     *  }
-     * ```
-     * @param index the index of the item to act on with the following instructions
-     *
-     * @codeGenApi
-     */
-    function ɵɵselect(index) {
-        ngDevMode && assertGreaterThan(index, -1, 'Invalid index');
-        ngDevMode &&
-            assertLessThan(index, getLView().length - HEADER_OFFSET, 'Should be within range for the view data');
-        var lView = getLView();
-        // Flush the initial hooks for elements in the view that have been added up to this point.
-        executePreOrderHooks(lView, lView[TVIEW], getCheckNoChangesMode(), index);
-        // We must set the selected index *after* running the hooks, because hooks may have side-effects
-        // that cause other template functions to run, thus updating the selected index, which is global
-        // state. If we run `setSelectedIndex` *before* we run the hooks, in some cases the selected index
-        // will be altered by the time we leave the `ɵɵselect` instruction.
-        setSelectedIndex(index);
-    }
-
-    /**
-     * @license
-     * Copyright Google Inc. All Rights Reserved.
-     *
-     * Use of this source code is governed by an MIT-style license that can be
-     * found in the LICENSE file at https://angular.io/license
-     */
-    /**
      * Create static text node
      *
      * @param index Index of the node in the data array
@@ -18941,7 +18949,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.0-next.1+10.sha-11a4454.with-local-changes');
+    var VERSION = new Version('8.1.0-next.1+18.sha-c038675.with-local-changes');
 
     /**
      * @license
