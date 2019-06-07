4aa112f9bd57f2082b37075a3c075dd00d139b59
Misko Hevery <misko@hevery.com>
2019-06-07 16:17:26 +0000

    9ace748 docs: add dev.to link to the help section (#30873)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+45.sha-8aef446.with-local-changes
+ * @license Angular v8.0.0+48.sha-9ace748.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16642,7 +16642,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0+45.sha-8aef446.with-local-changes');
+    var VERSION = new Version('8.0.0+48.sha-9ace748.with-local-changes');
 
     /**
      * @license
