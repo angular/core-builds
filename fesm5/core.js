ca931f437c6fb8cc874bfeb5933086a039dd8f1f
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:41:33 +0000

    bb635c0 docs: use const in dynamic-component-loader example (#30888)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+39.sha-87bf179.with-local-changes
+ * @license Angular v8.1.0-next.1+41.sha-bb635c0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18907,7 +18907,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.1+39.sha-87bf179.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+41.sha-bb635c0.with-local-changes');
 
 /**
  * @license
