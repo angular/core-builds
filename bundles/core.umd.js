107764bc57555bc455d0b4cbdf200083736f88fa
Andrew Kushnir <akushnir@google.com>
2019-06-12 19:08:22 +0000

    77a5790 docs: add platform to glossary (#30731)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+66.sha-4ca401c.with-local-changes
+ * @license Angular v8.0.0+67.sha-77a5790.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16642,7 +16642,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.0+66.sha-4ca401c.with-local-changes');
+    var VERSION = new Version('8.0.0+67.sha-77a5790.with-local-changes');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+66.sha-4ca401c.with-local-changes
+ * @license Angular v8.0.0+67.sha-77a5790.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+66.sha-4ca401c.with-local-changes');
+var VERSION = new Version('8.0.0+67.sha-77a5790.with-local-changes');
 
 /**
  * @license
