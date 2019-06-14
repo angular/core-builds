dce0b41162b8d2ea8368ca588ab93ae3591c380f
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:13:57 +0000

    7e3a60a docs: add example and edit two-way-binding section of Template Syntax (#26278)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+15.sha-85d38ae.with-local-changes
+ * @license Angular v8.1.0-next.2+16.sha-7e3a60a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23189,7 +23189,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+15.sha-85d38ae.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+16.sha-7e3a60a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
@@ -34961,7 +34961,9 @@ class SystemJsNgModuleLoader {
      * @return {?}
      */
     load(path) {
-        return this.loadAndCompile(path);
+        /** @type {?} */
+        const legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     }
     /**
      * @private
