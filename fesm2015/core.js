244dbbbcdd74dfdf0240022e76ccdfed206c32fb
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:13:31 +0000

    85d38ae docs: rewrite property binding section and add example (#25770)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+14.sha-4ad323a.with-local-changes
+ * @license Angular v8.1.0-next.2+15.sha-85d38ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23189,7 +23189,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+14.sha-4ad323a.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+15.sha-85d38ae.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
@@ -34961,9 +34961,7 @@ class SystemJsNgModuleLoader {
      * @return {?}
      */
     load(path) {
-        /** @type {?} */
-        const legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+        return this.loadAndCompile(path);
     }
     /**
      * @private
