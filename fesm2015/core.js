892e85200171501ef1ddf0e53cb4a389730df5ff
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:22:55 +0000

    e76690b docs: edit codeowners for new Template Syntax examples (#31060)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+19.sha-f41242f.with-local-changes
+ * @license Angular v8.1.0-next.2+21.sha-e76690b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23189,7 +23189,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+19.sha-f41242f.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+21.sha-e76690b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
@@ -34961,9 +34961,7 @@ class SystemJsNgModuleLoader {
      * @return {?}
      */
     load(path) {
-        /** @type {?} */
-        const legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+        return this.loadAndCompile(path);
     }
     /**
      * @private
