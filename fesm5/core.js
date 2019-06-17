33d114ccefb56767278d050d103337a74db5fd45
Andrew Kushnir <akushnir@google.com>
2019-06-17 23:59:47 +0000

    6a01fa532 docs: add mock heroes to the code review tabs for TOH pt. 2 (#31080)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+24.sha-53c6425.with-local-changes
+ * @license Angular v8.0.1+26.sha-6a01fa5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.1+24.sha-53c6425.with-local-changes');
+var VERSION = new Version('8.0.1+26.sha-6a01fa5.with-local-changes');
 
 /**
  * @license
