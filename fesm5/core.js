ba2959c2c198d9ce00989f99343d7f33092b95c1
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:20:42 +0000

    87168acf3 refactor(ivy): move `bind` instruction into `i18nExp` (#31089)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+37.sha-beaab27.with-local-changes
+ * @license Angular v8.1.0-next.2+36.sha-87168ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18981,7 +18981,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.2+37.sha-beaab27.with-local-changes');
+var VERSION = new Version('8.1.0-next.2+36.sha-87168ac.with-local-changes');
 
 /**
  * @license
