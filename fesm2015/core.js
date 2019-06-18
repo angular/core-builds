a8f61067e52d453528512cf2939773a857872ef7
Andrew Kushnir <akushnir@google.com>
2019-06-18 19:35:55 +0000

    788d19c03 build: enable shard_count for some jasmine tests that have many specs (#31009)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+32.sha-8492499.with-local-changes
+ * @license Angular v8.0.1+33.sha-788d19c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20524,7 +20524,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.1+32.sha-8492499.with-local-changes');
+const VERSION = new Version('8.0.1+33.sha-788d19c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+32.sha-8492499.with-local-changes
+ * @license Angular v8.0.1+33.sha-788d19c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.1+32.sha-8492499.with-local-changes');
+var VERSION = new Version('8.0.1+33.sha-788d19c.with-local-changes');
 
 /**
  * @license
