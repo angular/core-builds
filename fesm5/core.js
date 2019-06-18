a080c7e10594604535600853362063e6ad24ae95
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:13:38 +0000

    eb970b462 docs: rewrite Observable examples with pipable operators (#31074)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+26.sha-6a01fa5.with-local-changes
+ * @license Angular v8.0.1+28.sha-eb970b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.1+26.sha-6a01fa5.with-local-changes');
+var VERSION = new Version('8.0.1+28.sha-eb970b4.with-local-changes');
 
 /**
  * @license
