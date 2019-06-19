c6dd7fb810c42caeb54171932f39d823b440e2aa
Kara Erickson <karakara@google.com>
2019-06-19 23:18:45 +0000

    c36edffe2 docs: fix bad tag (#31115)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+3.sha-b68f2d6.with-local-changes
+ * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16597,7 +16597,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.2+3.sha-b68f2d6.with-local-changes');
+var VERSION = new Version('8.0.2+7.sha-c36edff.with-local-changes');
 
 /**
  * @license
