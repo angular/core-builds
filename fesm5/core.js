a92d8974a119c478d22485684ba8c19a70cdc031
Kara Erickson <karakara@google.com>
2019-06-19 23:10:25 +0000

    91008bd97 fix(ivy): improve error message when NgModule properties are not arrays (#30796)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+6.sha-f039583.with-local-changes
+ * @license Angular v8.1.0-next.3+7.sha-91008bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18981,7 +18981,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+6.sha-f039583.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+7.sha-91008bd.with-local-changes');
 
 /**
  * @license
