05231c076050cee783bf6aba16acdbb7ecf66fe5
Kara Erickson <karakara@google.com>
2019-06-20 18:41:08 +0000

    70ad91ed8 refactor(compiler): recursive ast expression visitor not passing context (#31085)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+16.sha-f221908.with-local-changes
+ * @license Angular v8.1.0-next.3+15.sha-70ad91e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23277,7 +23277,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+16.sha-f221908.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+15.sha-70ad91e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
@@ -35052,7 +35052,9 @@ class SystemJsNgModuleLoader {
      * @return {?}
      */
     load(path) {
-        return this.loadAndCompile(path);
+        /** @type {?} */
+        const legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     }
     /**
      * @private
