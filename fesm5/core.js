a7c4c0d91d246f65f6f8ba7be3d22c6f9c039c70
Kara Erickson <karakara@google.com>
2019-06-20 18:55:25 +0000

    5eb742621 build: move zone.js to angular repo (#30962)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+18.sha-7b3bcc2.with-local-changes
+ * @license Angular v8.1.0-next.3+19.sha-5eb7426.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18981,7 +18981,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+18.sha-7b3bcc2.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+19.sha-5eb7426.with-local-changes');
 
 /**
  * @license
