2f997af2342aef25c17efd565cdbcc446a253043
Kara Erickson <karakara@google.com>
2019-06-25 18:00:52 +0000

    00242e8d6 docs(core): change from CSS Event to DOM event (#31229) PR Close #31229

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+33.sha-0a46b2a.with-local-changes
+ * @license Angular v8.0.2+30.sha-00242e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16642,7 +16642,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.0.2+33.sha-0a46b2a.with-local-changes');
+    var VERSION = new Version('8.0.2+30.sha-00242e8.with-local-changes');
 
     /**
      * @license
