fe47ed6cd43c94ef1099d8a1424e7d83093299c3
Kara Erickson <karakara@google.com>
2019-06-25 17:55:45 +0000

    2aba48511 refactor(ivy): use FatalDiagnosticError to throw more descriptive errors while extracting queries information (#31123)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+66.sha-e8d0265.with-local-changes
+ * @license Angular v8.1.0-next.3+62.sha-2aba485.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18717,7 +18717,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.0-next.3+66.sha-e8d0265.with-local-changes');
+    var VERSION = new Version('8.1.0-next.3+62.sha-2aba485.with-local-changes');
 
     /**
      * @license
@@ -27374,7 +27374,8 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+66.sha-e8d0265.with-local-changes
+ * @license Angular v8.1.0-next.3+62.sha-2aba485.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18687,7 +18687,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+66.sha-e8d0265.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+62.sha-2aba485.with-local-changes');
 
 /**
  * @license
@@ -27372,7 +27372,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
