d8a799498be3959c8da86904556b0ecc9ac7495d
Kara Erickson <karakara@google.com>
2019-06-25 23:45:57 +0000

    7186f9c01 refactor(ivy): implement a virtual file-system layer in ngtsc + ngcc (#30921)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+82.sha-1e7e065.with-local-changes
+ * @license Angular v8.1.0-next.3+83.sha-7186f9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18687,7 +18687,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+82.sha-1e7e065.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+83.sha-7186f9c.with-local-changes');
 
 /**
  * @license
@@ -27375,8 +27375,7 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+        return this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
