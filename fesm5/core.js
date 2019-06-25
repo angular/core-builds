78d42559db0f307177e24fb543d6416bdce34dee
Kara Erickson <karakara@google.com>
2019-06-25 17:54:23 +0000

    2aba48511 refactor(ivy): use FatalDiagnosticError to throw more descriptive errors while extracting queries information (#31123)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+66.sha-e8d0265.with-local-changes
+ * @license Angular v8.1.0-next.3+62.sha-2aba485.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19004,7 +19004,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+66.sha-e8d0265.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+62.sha-2aba485.with-local-changes');
 
 /**
  * @license
