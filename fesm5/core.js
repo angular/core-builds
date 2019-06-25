0d53ed20612c1416fdca3af06bb3665eaba75dd9
Kara Erickson <karakara@google.com>
2019-06-25 18:00:12 +0000

    00ecfc7f9 fix(docs-infra): separate vendor-specific CSS selectors (#31252)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+63.sha-1d5c445.with-local-changes
+ * @license Angular v8.1.0-next.3+65.sha-00ecfc7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19004,7 +19004,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+63.sha-1d5c445.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+65.sha-00ecfc7.with-local-changes');
 
 /**
  * @license
