34479f4ad8112bb66395d67a5c22b520aebd05dd
Kara Erickson <karakara@google.com>
2019-06-25 18:20:35 +0000

    a1d436e6a docs: fix formatting (#31120)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+65.sha-00ecfc7.with-local-changes
+ * @license Angular v8.1.0-next.3+68.sha-a1d436e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19004,7 +19004,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+65.sha-00ecfc7.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+68.sha-a1d436e.with-local-changes');
 
 /**
  * @license
