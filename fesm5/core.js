361d5fa2f0535395ce5df467ce2fd773602f4a54
Kara Erickson <karakara@google.com>
2019-06-26 16:43:53 +0000

    e01d697ee build: filter out changelog entries for zone.js from framework changelog (#31277)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+99.sha-a8f3b31.with-local-changes
+ * @license Angular v8.1.0-next.3+101.sha-e01d697.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18701,7 +18701,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+99.sha-a8f3b31.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+101.sha-e01d697.with-local-changes');
 
 /**
  * @license
@@ -27392,7 +27392,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
