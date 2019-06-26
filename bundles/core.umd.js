3062e80d0df9482d4c0c74f740eb9b4cc129196c
Kara Erickson <karakara@google.com>
2019-06-26 15:35:54 +0000

    a8f3b317f build(docs-infra): upgrade cli command docs sources to 01a7186bb (#31281)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+97.sha-32c760f.with-local-changes
+ * @license Angular v8.1.0-next.3+99.sha-a8f3b31.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18731,7 +18731,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.0-next.3+97.sha-32c760f.with-local-changes');
+    var VERSION = new Version('8.1.0-next.3+99.sha-a8f3b31.with-local-changes');
 
     /**
      * @license
@@ -27394,7 +27394,8 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
