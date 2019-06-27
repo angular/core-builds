bcf13e00d014c20088b93f13d38ae249810b456e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 23:14:52 +0000

    15e397816 fix(router): adjust UrlTree redirect to replace URL if in eager update (#31168)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+14.sha-4f38419.with-local-changes
+ * @license Angular v8.1.0-rc.0+16.sha-15e3978.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18739,7 +18739,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.0-rc.0+14.sha-4f38419.with-local-changes');
+    var VERSION = new Version('8.1.0-rc.0+16.sha-15e3978.with-local-changes');
 
     /**
      * @license
@@ -27405,8 +27405,7 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+            return this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
