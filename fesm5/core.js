14854f733af75705bf3ca7afadbebf772f4249e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 23:12:48 +0000

    c8875f2db docs: added svg example (#30559)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+11.sha-579f129.with-local-changes
+ * @license Angular v8.1.0-rc.0+15.sha-c8875f2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19018,7 +19018,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+11.sha-579f129.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+15.sha-c8875f2.with-local-changes');
 
 /**
  * @license
