dd13e728f9b6a28260e2a06b9044d406634e5589
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 23:14:16 +0000

    8b034188b docs: fix example misprints (#31284)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+15.sha-c8875f2.with-local-changes
+ * @license Angular v8.1.0-rc.0+12.sha-8b03418.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23795,7 +23795,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-rc.0+15.sha-c8875f2.with-local-changes');
+const VERSION = new Version('8.1.0-rc.0+12.sha-8b03418.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
