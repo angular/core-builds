6f0ed083e51937524efe9cb460f03e6e4b6a2eb3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 00:29:14 +0000

    c6b29f4c6 docs: added svg example (#30559)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+15.sha-f96a81a.with-local-changes
+ * @license Angular v8.1.0-rc.0+18.sha-c6b29f4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18739,7 +18739,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.0-rc.0+15.sha-f96a81a.with-local-changes');
+    var VERSION = new Version('8.1.0-rc.0+18.sha-c6b29f4.with-local-changes');
 
     /**
      * @license
@@ -27405,7 +27405,8 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
