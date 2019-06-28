a06788036ceae3c51d9ff37c5321b0ae7683732c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:49:01 +0000

    2b5d52fbd docs: add tests for lazy loading angularjs example (#30622)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+21.sha-f57e77e.with-local-changes
+ * @license Angular v8.1.0-rc.0+20.sha-2b5d52f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18710,7 +18710,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+21.sha-f57e77e.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+20.sha-2b5d52f.with-local-changes');
 
 /**
  * @license
@@ -27404,8 +27404,7 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+        return this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
