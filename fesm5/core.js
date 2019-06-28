a075fee6d2eec2cde5852ff4c87c7139f788ac3a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:49:27 +0000

    81332150a perf(ivy): chain host binding instructions (#31296)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+20.sha-2b5d52f.with-local-changes
+ * @license Angular v8.1.0-rc.0+19.sha-8133215.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18710,7 +18710,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+20.sha-2b5d52f.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+19.sha-8133215.with-local-changes');
 
 /**
  * @license
@@ -27404,7 +27404,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
