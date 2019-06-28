573bd33337dcf0070529a78421d1018ca978326a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 19:44:00 +0000

    4bbf60ed0 docs: change order of Angular Elements article (#30521)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+17.sha-299a43c.with-local-changes
+ * @license Angular v8.1.0-rc.0+18.sha-4bbf60e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19018,7 +19018,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+17.sha-299a43c.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+18.sha-4bbf60e.with-local-changes');
 
 /**
  * @license
