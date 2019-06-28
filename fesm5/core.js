938394071c70c671b61ad3855cf92b611d9aca4b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 19:37:46 +0000

    a29dc961a feat(bazel): allow passing a custom bazel compiler host to ngc compile (#31341)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+22.sha-0de5d79.with-local-changes
+ * @license Angular v8.1.0-rc.0+23.sha-a29dc96.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18710,7 +18710,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+22.sha-0de5d79.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+23.sha-a29dc96.with-local-changes');
 
 /**
  * @license
