680dec206d11ae902df436272d0a85ee2046f854
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 17:29:12 +0000

    0d6fd134d docs(core): update link to external i18n doc (#31353)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+26.sha-bbb27b5.with-local-changes
+ * @license Angular v8.1.0-rc.0+27.sha-0d6fd13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18743,7 +18743,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.0-rc.0+26.sha-bbb27b5.with-local-changes');
+    var VERSION = new Version('8.1.0-rc.0+27.sha-0d6fd13.with-local-changes');
 
     /**
      * @license
@@ -27410,8 +27410,7 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+            return this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
