1e59db6846ba56bc705ddfd568f914e4fb976987
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 21:43:00 +0000

    a4a423a08 build: fix build failures with worker mode cache and @types/events (#31325)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+29.sha-f83dfd6.with-local-changes
+ * @license Angular v8.1.0-rc.0+37.sha-a4a423a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+29.sha-f83dfd6.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+37.sha-a4a423a.with-local-changes');
 
 /**
  * @license
