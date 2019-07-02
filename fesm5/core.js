59b77e72fa9d5ed7f4dddf614028683d82c1d853
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:56:28 +0000

    5039faff8 docs: add doc for complex config values (#31010)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+43.sha-fde016b.with-local-changes
+ * @license Angular v8.1.0-rc.0+47.sha-5039faf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19739,7 +19739,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+43.sha-fde016b.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+47.sha-5039faf.with-local-changes');
 
 /**
  * @license
