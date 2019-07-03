473d3a8be812ce7f9d67f74ff0df1acd70692b5e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-03 16:14:41 +0000

    9515f171b ci: update gcp_token (#31405)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0.with-local-changes
+ * @license Angular v8.2.0-next.0+1.sha-9515f17.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19452,7 +19452,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-next.0.with-local-changes');
+    var VERSION = new Version('8.2.0-next.0+1.sha-9515f17.with-local-changes');
 
     /**
      * @license
@@ -28137,8 +28137,7 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+            return this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0.with-local-changes
+ * @license Angular v8.2.0-next.0+1.sha-9515f17.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19422,7 +19422,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.0.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+1.sha-9515f17.with-local-changes');
 
 /**
  * @license
@@ -28135,8 +28135,7 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+        return this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
