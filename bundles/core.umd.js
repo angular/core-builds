562284b123a0c2012f3c119456730648def9fe9c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-04 00:11:13 +0000

    1d3e22766 fix(bazel): revert location of xi18n outputs to bazel-genfiles (#31410)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+2.sha-c095597.with-local-changes
+ * @license Angular v8.2.0-next.0+3.sha-1d3e227.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19452,7 +19452,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-next.0+2.sha-c095597.with-local-changes');
+    var VERSION = new Version('8.2.0-next.0+3.sha-1d3e227.with-local-changes');
 
     /**
      * @license
@@ -28137,7 +28137,8 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+2.sha-c095597.with-local-changes
+ * @license Angular v8.2.0-next.0+3.sha-1d3e227.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19422,7 +19422,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.0+2.sha-c095597.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+3.sha-1d3e227.with-local-changes');
 
 /**
  * @license
