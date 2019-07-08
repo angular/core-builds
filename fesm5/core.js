73a4a387374bd70588140046f177bf66bf2f8263
Jason Aden <jasonaden@google.com>
2019-07-08 17:48:35 +0000

    6aaca21c2 fix(compiler): give ASTWithSource its own visit method (#31347)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+9.sha-9a2d1fa.with-local-changes
+ * @license Angular v8.2.0-next.0+11.sha-6aaca21.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19705,7 +19705,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.0+9.sha-9a2d1fa.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+11.sha-6aaca21.with-local-changes');
 
 /**
  * @license
