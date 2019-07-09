bfdfc63fe1448746bd419b1168de28d7bdb1a407
Jason Aden <jasonaden@google.com>
2019-07-09 16:58:28 +0000

    e6f1b04cd fix(zone.js): restore definition of global (#31453)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+11.sha-6aaca21.with-local-changes
+ * @license Angular v8.2.0-next.0+12.sha-e6f1b04.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19418,7 +19418,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-next.0+11.sha-6aaca21.with-local-changes');
+    var VERSION = new Version('8.2.0-next.0+12.sha-e6f1b04.with-local-changes');
 
     /**
      * @license
@@ -28088,8 +28088,7 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+            return this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
