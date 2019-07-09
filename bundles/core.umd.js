ce2a8847f51e45f4a2017a1cb375536cf1b48e18
Jason Aden <jasonaden@google.com>
2019-07-09 17:02:28 +0000

    24fd1a18a fix(ivy): handle namespaced imports correctly (#31367)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+7.sha-bdfbb92.with-local-changes
+ * @license Angular v8.1.0+9.sha-24fd1a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19075,7 +19075,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.0+7.sha-bdfbb92.with-local-changes');
+    var VERSION = new Version('8.1.0+9.sha-24fd1a1.with-local-changes');
 
     /**
      * @license
