89b5538647e5d724677ff00fe67611e364bacd10
Jason Aden <jasonaden@google.com>
2019-07-09 21:16:10 +0000

    067d9015c build: remove usage of deprecated experimental_ui bazel flag (#31457)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+9.sha-24fd1a1.with-local-changes
+ * @license Angular v8.1.0+11.sha-067d901.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+9.sha-24fd1a1.with-local-changes');
+var VERSION = new Version('8.1.0+11.sha-067d901.with-local-changes');
 
 /**
  * @license
