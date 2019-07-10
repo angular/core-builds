d757ae9e9fde3c89bab5a1c4c003eaefb47d0e4a
Jason Aden <jasonaden@google.com>
2019-07-10 17:45:48 +0000

    c69e552a8 docs: release notes for the v8.1.1 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+24.sha-2b44be9.with-local-changes
+ * @license Angular v8.2.0-next.0+26.sha-c69e552.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19725,7 +19725,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.0+24.sha-2b44be9.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+26.sha-c69e552.with-local-changes');
 
 /**
  * @license
