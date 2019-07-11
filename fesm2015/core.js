6a0048ed470666ecc648adcc336aa7331ef32b17
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 05:54:52 +0000

    a395cb1a5 docs: add Renderer to deprecations doc (#31419)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+12.sha-1863254.with-local-changes
+ * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23807,7 +23807,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+12.sha-1863254.with-local-changes');
+const VERSION = new Version('8.1.0+14.sha-a395cb1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+12.sha-1863254.with-local-changes
+ * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+12.sha-1863254.with-local-changes');
+var VERSION = new Version('8.1.0+14.sha-a395cb1.with-local-changes');
 
 /**
  * @license
