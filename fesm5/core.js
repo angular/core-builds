99fb18af3f0228b8e31d50bcf89ffa8aa2a9906c
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 17:17:34 +0000

    af418b33e fix(docs-infra): fix get started button rendering (#31470)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+24.sha-96f2d78.with-local-changes
+ * @license Angular v8.1.0+25.sha-af418b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+24.sha-96f2d78.with-local-changes');
+var VERSION = new Version('8.1.0+25.sha-af418b3.with-local-changes');
 
 /**
  * @license
