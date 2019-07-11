f47c4cd5576504d4f30a0f762731e4590db2edd2
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:39:51 +0000

    4bb283cbb build: remove redundant `@types/source-map` dependency (#31468)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+27.sha-565a58e.with-local-changes
+ * @license Angular v8.2.0-next.1+34.sha-4bb283c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19417,7 +19417,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.1+27.sha-565a58e.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+34.sha-4bb283c.with-local-changes');
 
 /**
  * @license
@@ -28137,7 +28137,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
