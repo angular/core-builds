2e9e997ebc467b8dc972469c680546c5db83b073
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:37:28 +0000

    3ade93f6f ci(docs-infra): run a11y audits on certain pages on CI (#31414)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+29.sha-4aed480.with-local-changes
+ * @license Angular v8.1.0+34.sha-3ade93f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23807,7 +23807,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+29.sha-4aed480.with-local-changes');
+const VERSION = new Version('8.1.0+34.sha-3ade93f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
