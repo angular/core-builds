7b464af6858fbc42f205a2dd2ee969758926a092
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:06:06 +0000

    fe1793844 docs: fix typo in Template Syntax headers (#31474)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+5.sha-68940f0.with-local-changes
+ * @license Angular v8.2.0-next.1+6.sha-fe17938.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19408,7 +19408,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.1+5.sha-68940f0.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+6.sha-fe17938.with-local-changes');
 
 /**
  * @license
@@ -28128,7 +28128,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
