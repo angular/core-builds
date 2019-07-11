31352ca0a303ec4bbcfd52bcdb0d12fc0d8032f5
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 19:30:40 +0000

    4aed480c6 fix: use the correct WTF array to iterate over (#31208) PR Close #31208

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+28.sha-ddb210c.with-local-changes
+ * @license Angular v8.1.0+29.sha-4aed480.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+28.sha-ddb210c.with-local-changes');
+var VERSION = new Version('8.1.0+29.sha-4aed480.with-local-changes');
 
 /**
  * @license
