e48c0958c0ceb5b3ccb0bfc1c9fb2c16b44c9c5f
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 15:57:42 +0000

    fa69e99bd build: add tsconfig-test to dependency for tsconfig in core/test/strict_types (#31471)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+36.sha-58260fc.with-local-changes
+ * @license Angular v8.1.0+41.sha-fa69e99.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+36.sha-58260fc.with-local-changes');
+var VERSION = new Version('8.1.0+41.sha-fa69e99.with-local-changes');
 
 /**
  * @license
