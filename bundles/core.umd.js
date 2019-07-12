75721e7e5e4ab30ebce0c6e3474d0f17ca63d449
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:18:11 +0000

    098659554 fix(docs-infra): fix API list search color and styles (#31272)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+44.sha-2ed8127.with-local-changes
+ * @license Angular v8.1.0+42.sha-0986595.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19075,7 +19075,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.0+44.sha-2ed8127.with-local-changes');
+    var VERSION = new Version('8.1.0+42.sha-0986595.with-local-changes');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+44.sha-2ed8127.with-local-changes
+ * @license Angular v8.1.0+42.sha-0986595.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23807,7 +23807,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+44.sha-2ed8127.with-local-changes');
+const VERSION = new Version('8.1.0+42.sha-0986595.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
