3dd146afe4740d5d95a95656c59926138abccb48
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:17:34 +0000

    b65e11e3c build(docs-infra): display CLI positional option enum values (#31529)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+45.sha-604d906.with-local-changes
+ * @license Angular v8.2.0-next.1+47.sha-b65e11e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19447,7 +19447,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-next.1+45.sha-604d906.with-local-changes');
+    var VERSION = new Version('8.2.0-next.1+47.sha-b65e11e.with-local-changes');
 
     /**
      * @license
@@ -28139,7 +28139,8 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
