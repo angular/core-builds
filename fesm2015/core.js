be6bc88834d876c5fe4f78a3a0e795914fe71a6c
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 16:59:27 +0000

    7c7774ff2 docs: remove and update resource list (#31469)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+42.sha-0986595.with-local-changes
+ * @license Angular v8.1.0+46.sha-7c7774f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23807,7 +23807,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+42.sha-0986595.with-local-changes');
+const VERSION = new Version('8.1.0+46.sha-7c7774f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
