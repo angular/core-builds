c899c2f565571e0840eb992519c37bae6233dc82
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 22:58:45 +0000

    3166cffd2 fix(compiler-cli): Return original sourceFile instead of redirected sourceFile from getSourceFile (#26036)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+54.sha-40705f3.with-local-changes
+ * @license Angular v8.2.0-next.1+55.sha-3166cff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19417,7 +19417,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.1+54.sha-40705f3.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+55.sha-3166cff.with-local-changes');
 
 /**
  * @license
@@ -28162,7 +28162,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
