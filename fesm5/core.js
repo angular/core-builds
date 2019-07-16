04a8290d6d66169262fc77bbfad6fb11313c734f
Matias Niemelä <matias@yearofmoo.com>
2019-07-16 18:46:26 +0000

    0110de266 fix(language-service): Eagarly initialize data members (#31577)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+61.sha-f166b6d.with-local-changes
+ * @license Angular v8.2.0-next.1+62.sha-0110de2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19445,7 +19445,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.1+61.sha-f166b6d.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+62.sha-0110de2.with-local-changes');
 
 /**
  * @license
@@ -28191,8 +28191,7 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+        return this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
