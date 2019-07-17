474e0347135001c29102c4a7fe3a4d163b68d32a
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 17:52:18 +0000

    f025fe6c2 docs: remove prerequisite section in NgModules docs (#31169)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+58.sha-e1cfa41.with-local-changes
+ * @license Angular v8.1.0+56.sha-f025fe6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+58.sha-e1cfa41.with-local-changes');
+var VERSION = new Version('8.1.0+56.sha-f025fe6.with-local-changes');
 
 /**
  * @license
