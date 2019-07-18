41512f6dc68486bab98884fb1a86a64d21beaef3
Miško Hevery <misko@hevery.com>
2019-07-18 18:22:38 +0000

    1e9eeafa9 docs: clean up router api doc (#31476)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2.with-local-changes
+ * @license Angular v8.2.0-next.2+7.sha-1e9eeaf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19805,7 +19805,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-next.2.with-local-changes');
+    var VERSION = new Version('8.2.0-next.2+7.sha-1e9eeaf.with-local-changes');
 
     /**
      * @license
