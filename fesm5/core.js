e11d69d004f6e07dc6aa6f7f08bfc7a9747648cf
Miško Hevery <misko@hevery.com>
2019-07-22 17:07:39 +0000

    8f084d721 docs: clarify toh (#28571)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+35.sha-cb848b9.with-local-changes
+ * @license Angular v8.2.0-next.2+36.sha-8f084d7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18117,7 +18117,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.2+35.sha-cb848b9.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+36.sha-8f084d7.with-local-changes');
 
 /**
  * @license
