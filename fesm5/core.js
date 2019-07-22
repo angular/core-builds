dec583709d0fa4c14f1f3ebe20a9a868941bab15
Kara Erickson <karakara@google.com>
2019-07-22 19:35:07 +0000

    5a8eb924b fix(bazel): increase memory limit of ngc under bazel from 2 to 4 GB (#31784)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+41.sha-8b26447.with-local-changes
+ * @license Angular v8.2.0-next.2+43.sha-5a8eb92.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18135,7 +18135,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.2+41.sha-8b26447.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+43.sha-5a8eb92.with-local-changes');
 
 /**
  * @license
@@ -26915,8 +26915,7 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+        return this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
