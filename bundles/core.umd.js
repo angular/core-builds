b4db6cccd3de60a12754be7cc75893b9ec5c7888
Miško Hevery <misko@hevery.com>
2019-07-22 17:14:52 +0000

    03050deca fix(docs-infra): improve small screen experience for docs cards layout (#31576)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+13.sha-157456b.with-local-changes
+ * @license Angular v8.1.2+16.sha-03050de.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19075,7 +19075,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.1.2+13.sha-157456b.with-local-changes');
+    var VERSION = new Version('8.1.2+16.sha-03050de.with-local-changes');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+13.sha-157456b.with-local-changes
+ * @license Angular v8.1.2+16.sha-03050de.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.2+13.sha-157456b.with-local-changes');
+var VERSION = new Version('8.1.2+16.sha-03050de.with-local-changes');
 
 /**
  * @license
