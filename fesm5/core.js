125a53b9b8f1762f1ce092a01bb213be6dc231a5
Miško Hevery <misko@hevery.com>
2019-07-24 21:55:52 +0000

    0440dc948 fix(zone.js): don't wrap uncaught promise error. (#31443)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+22.sha-5407b43.with-local-changes
+ * @license Angular v8.1.2+25.sha-0440dc9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19030,7 +19030,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.2+22.sha-5407b43.with-local-changes');
+var VERSION = new Version('8.1.2+25.sha-0440dc9.with-local-changes');
 
 /**
  * @license
