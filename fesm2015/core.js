4027fae83d1004ba39e038a34c6b00d83fd346a6
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:21:11 +0000

    155f40c17 docs: fix header mistake (#31811)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+3.sha-8c446b0.with-local-changes
+ * @license Angular v8.2.0-rc.0+4.sha-155f40c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22392,7 +22392,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-rc.0+3.sha-8c446b0.with-local-changes');
+const VERSION = new Version('8.2.0-rc.0+4.sha-155f40c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
