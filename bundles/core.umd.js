89467dcc74d8f38af756c63e7f8bf121550cb24d
Andrew Kushnir <akushnir@google.com>
2019-07-26 22:24:02 +0000

    5f0d5e9cc build: update to nodejs rules 0.34.0 and bazel 0.28.1 (#31824)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+103.sha-5296c04.with-local-changes
+ * @license Angular v8.2.0-next.2+104.sha-5f0d5e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17926,7 +17926,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-next.2+103.sha-5296c04.with-local-changes');
+    var VERSION = new Version('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
 
     /**
      * @license
@@ -26635,7 +26635,8 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
