77bc43a816ae6c375e5c1d74c8a832a017b6927f
Miško Hevery <misko@hevery.com>
2019-07-26 00:40:01 +0000

    3a2b195a5 feat(ivy): translate type-check diagnostics to their original source (#30181)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+89.sha-6b67cd5.with-local-changes
+ * @license Angular v8.2.0-next.2+93.sha-3a2b195.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18209,7 +18209,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.2+89.sha-6b67cd5.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+93.sha-3a2b195.with-local-changes');
 
 /**
  * @license
