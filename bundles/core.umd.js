25485946ec63564aac8d4b0c4613796d9dcd8cf5
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:22:48 +0000

    859ebdd83 fix(ivy): correctly bind `targetToIdentifier` to the TemplateVisitor (#31861)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+97.sha-3067309.with-local-changes
+ * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18254,7 +18254,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-next.2+97.sha-3067309.with-local-changes');
+    var VERSION = new Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
 
     /**
      * @license
