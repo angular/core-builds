c6943a46089517074f96e3d7ce4fce0aacb9ee0e
Andrew Kushnir <akushnir@google.com>
2019-07-29 23:30:42 +0000

    d269b111d fix(zone.js): should expose some other internal intefaces (#31866)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+9.sha-06c49b4.with-local-changes
+ * @license Angular v8.2.0-rc.0+8.sha-d269b11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18254,7 +18254,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-rc.0+9.sha-06c49b4.with-local-changes');
+    var VERSION = new Version('8.2.0-rc.0+8.sha-d269b11.with-local-changes');
 
     /**
      * @license
