2cbee51d86b38f82445339257f8fd164c11caa46
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-31 23:40:55 +0000

    b8f269414 fix(ivy): speed up ngtsc if project has no templates to check (#31922)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+2.sha-8ed83ca.with-local-changes
+ * @license Angular v8.2.0+3.sha-b8f2694.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18254,7 +18254,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0+2.sha-8ed83ca.with-local-changes');
+    var VERSION = new Version('8.2.0+3.sha-b8f2694.with-local-changes');
 
     /**
      * @license
