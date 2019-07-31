9f9258c0058a80f7ee00f9c19e2d7c19446aa263
Andrew Kushnir <akushnir@google.com>
2019-07-31 18:56:09 +0000

    aebf65d0e docs: add ReactiveConf 2019 to events (#31895) PR Close #31895

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+13.sha-03e8a31.with-local-changes
+ * @license Angular v8.2.0+1.sha-aebf65d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18254,7 +18254,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.0-rc.0+13.sha-03e8a31.with-local-changes');
+    var VERSION = new Version('8.2.0+1.sha-aebf65d.with-local-changes');
 
     /**
      * @license
