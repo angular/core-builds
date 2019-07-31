42ed3357ed45cbc44fcdb873534beab331d51eac
Andrew Kushnir <akushnir@google.com>
2019-07-31 19:13:51 +0000

    584b42343 build: Do not generate *.umd.min.js for language service (#31917)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+5.sha-ecffbda.with-local-changes
+ * @license Angular v9.0.0-next.0+3.sha-584b423.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17903,7 +17903,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+5.sha-ecffbda.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+3.sha-584b423.with-local-changes');
 
 /**
  * @license
@@ -26640,8 +26640,7 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
-        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
+        return this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
