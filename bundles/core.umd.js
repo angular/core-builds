5c4fa4030902a3e7c385c233b90ad4f3e456c0f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:31:38 +0000

    d0d875a3f fix(ivy): pass `schemas` field to nested views (#31913)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+10.sha-e8b8f6d.with-local-changes
+ * @license Angular v9.0.0-next.0+11.sha-d0d875a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13685,7 +13685,7 @@
         if (tView.firstTemplatePass) {
             ngDevMode && ngDevMode.firstTemplatePass++;
             resolveDirectives(tView, lView, tContainerNode, localRefs || null);
-            var embeddedTView = tContainerNode.tViews = createTView(-1, templateFn, consts, vars, tView.directiveRegistry, tView.pipeRegistry, null, null);
+            var embeddedTView = tContainerNode.tViews = createTView(-1, templateFn, consts, vars, tView.directiveRegistry, tView.pipeRegistry, null, tView.schemas);
             if (tView.queries !== null) {
                 tView.queries.template(tView, tContainerNode);
                 embeddedTView.queries = tView.queries.embeddedTView(tContainerNode);
@@ -17933,7 +17933,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.0+10.sha-e8b8f6d.with-local-changes');
+    var VERSION = new Version('9.0.0-next.0+11.sha-d0d875a.with-local-changes');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+10.sha-e8b8f6d.with-local-changes
+ * @license Angular v9.0.0-next.0+11.sha-d0d875a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13650,7 +13650,7 @@ function ɵɵtemplate(index, templateFn, consts, vars, tagName, attrs, localRefs
     if (tView.firstTemplatePass) {
         ngDevMode && ngDevMode.firstTemplatePass++;
         resolveDirectives(tView, lView, tContainerNode, localRefs || null);
-        var embeddedTView = tContainerNode.tViews = createTView(-1, templateFn, consts, vars, tView.directiveRegistry, tView.pipeRegistry, null, null);
+        var embeddedTView = tContainerNode.tViews = createTView(-1, templateFn, consts, vars, tView.directiveRegistry, tView.pipeRegistry, null, tView.schemas);
         if (tView.queries !== null) {
             tView.queries.template(tView, tContainerNode);
             embeddedTView.queries = tView.queries.embeddedTView(tContainerNode);
@@ -17903,7 +17903,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+10.sha-e8b8f6d.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+11.sha-d0d875a.with-local-changes');
 
 /**
  * @license
