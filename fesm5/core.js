2024587020d999c0f8a2317e0893199bca4d0155
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 16:55:24 +0000

    ebb27727e docs: add `anyComponentStyle` budget description (#31955)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+19.sha-a2183dd.with-local-changes
+ * @license Angular v9.0.0-next.0+22.sha-ebb2772.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18225,7 +18225,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+19.sha-a2183dd.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+22.sha-ebb2772.with-local-changes');
 
 /**
  * @license
