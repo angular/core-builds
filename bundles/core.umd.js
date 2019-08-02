8d36c3ac72e2776d668e51f7f573c62bd6b81bdb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 16:57:19 +0000

    ebb27727e docs: add `anyComponentStyle` budget description (#31955)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+19.sha-a2183dd.with-local-changes
+ * @license Angular v9.0.0-next.0+22.sha-ebb2772.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17932,7 +17932,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.0+19.sha-a2183dd.with-local-changes');
+    var VERSION = new Version('9.0.0-next.0+22.sha-ebb2772.with-local-changes');
 
     /**
      * @license
@@ -26640,7 +26640,8 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+19.sha-a2183dd.with-local-changes
+ * @license Angular v9.0.0-next.0+22.sha-ebb2772.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17902,7 +17902,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+19.sha-a2183dd.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+22.sha-ebb2772.with-local-changes');
 
 /**
  * @license
@@ -26638,7 +26638,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
