f84a0978d59741cd60911a3101d25effa93a2b6f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:23:26 +0000

    a8e2ee134 fix(language-service): Make Definition and QuickInfo compatible with TS LS (#31972)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+62.sha-e906a4f.with-local-changes
+ * @license Angular v9.0.0-next.0+63.sha-a8e2ee1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21969,7 +21969,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0+62.sha-e906a4f.with-local-changes');
+const VERSION = new Version('9.0.0-next.0+63.sha-a8e2ee1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
@@ -33810,7 +33810,9 @@ class SystemJsNgModuleLoader {
      * @return {?}
      */
     load(path) {
-        return this.loadAndCompile(path);
+        /** @type {?} */
+        const legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     }
     /**
      * @private
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+62.sha-e906a4f.with-local-changes
+ * @license Angular v9.0.0-next.0+63.sha-a8e2ee1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17901,7 +17901,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+62.sha-e906a4f.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+63.sha-a8e2ee1.with-local-changes');
 
 /**
  * @license
@@ -26637,7 +26637,8 @@ var SystemJsNgModuleLoader = /** @class */ (function () {
         this._config = config || DEFAULT_CONFIG;
     }
     SystemJsNgModuleLoader.prototype.load = function (path) {
-        return this.loadAndCompile(path);
+        var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+        return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
     };
     SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
         var _this = this;
