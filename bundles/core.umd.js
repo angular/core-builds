4fce7828316de2ebe502c7482de782bf905dd73f
Kara Erickson <karakara@google.com>
2019-08-09 18:06:14 +0000

    695f322dc docs: clarify pipe naming (#31806)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+7.sha-9896d43.with-local-changes
+ * @license Angular v9.0.0-next.1+5.sha-695f322.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18253,7 +18253,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.1+7.sha-9896d43.with-local-changes');
+    var VERSION = new Version('9.0.0-next.1+5.sha-695f322.with-local-changes');
 
     /**
      * @license
