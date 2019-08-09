2507c98861c30252a1fd94244a7f094c00778c3a
Kara Erickson <karakara@google.com>
2019-08-09 18:14:22 +0000

    eb5412d76 fix(ivy): reuse compilation scope for incremental template changes. (#31932)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+10.sha-fd6ed17.with-local-changes
+ * @license Angular v9.0.0-next.1+9.sha-eb5412d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17915,7 +17915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.1+10.sha-fd6ed17.with-local-changes');
+    var VERSION = new Version('9.0.0-next.1+9.sha-eb5412d.with-local-changes');
 
     /**
      * @license
@@ -26628,7 +26628,8 @@
             this._config = config || DEFAULT_CONFIG;
         }
         SystemJsNgModuleLoader.prototype.load = function (path) {
-            return this.loadAndCompile(path);
+            var legacyOfflineMode = !ivyEnabled && this._compiler instanceof Compiler;
+            return legacyOfflineMode ? this.loadFactory(path) : this.loadAndCompile(path);
         };
         SystemJsNgModuleLoader.prototype.loadAndCompile = function (path) {
             var _this = this;
