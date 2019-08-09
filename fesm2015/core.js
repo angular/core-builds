63cbb55e1881c84e0ec25bc309244bbba15aec79
Kara Erickson <karakara@google.com>
2019-08-09 18:08:46 +0000

    753333836 build: update tslint to version ~5.18.0 (#31879)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+5.sha-695f322.with-local-changes
+ * @license Angular v9.0.0-next.1+8.sha-7533338.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22396,7 +22396,7 @@ class Version {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.1+5.sha-695f322.with-local-changes');
+const VERSION = new Version('9.0.0-next.1+8.sha-7533338.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+5.sha-695f322.with-local-changes
+ * @license Angular v9.0.0-next.1+8.sha-7533338.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18208,7 +18208,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.1+5.sha-695f322.with-local-changes');
+var VERSION = new Version('9.0.0-next.1+8.sha-7533338.with-local-changes');
 
 /**
  * @license
