a5a86bef47639a2d63b22b28b7c01801abcbf189
Kara Erickson <karakara@google.com>
2019-08-09 22:32:36 +0000

    c67f490d2 build: ensure fixup commits match an earlier, unmerged commit (#32023)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.1+6.sha-eccb60c.with-local-changes
+ * @license Angular v8.2.1+10.sha-c67f490.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.1+6.sha-eccb60c.with-local-changes');
+const VERSION = new Version('8.2.1+10.sha-c67f490.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
