78027a2808c137cf644334f9b2c1fe3e096c7797
Kara Erickson <karakara@google.com>
2019-08-12 20:37:57 +0000

    76854287e Revert "fix(compiler): do not remove whitespace wrapping i18n expansions (#31962)" (#32110)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.1+11.sha-972550e.with-local-changes
+ * @license Angular v8.2.1+12.sha-7685428.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.1+11.sha-972550e.with-local-changes');
+const VERSION = new Version('8.2.1+12.sha-7685428.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.1+11.sha-972550e.with-local-changes
+ * @license Angular v8.2.1+12.sha-7685428.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.1+11.sha-972550e.with-local-changes');
+var VERSION = new Version('8.2.1+12.sha-7685428.with-local-changes');
 
 /**
  * @license
