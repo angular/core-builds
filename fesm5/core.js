9bb7264ca8809e273809b97e6fa39acb4ebbca49
Kara Erickson <karakara@google.com>
2019-08-12 22:38:26 +0000

    c68371ed1 docs: format currency api (#32107)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2.with-local-changes
+ * @license Angular v9.0.0-next.2+1.sha-c68371e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18345,7 +18345,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+1.sha-c68371e.with-local-changes');
 
 /**
  * @license
