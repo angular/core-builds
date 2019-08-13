593a0d41cf6e0a507cb46dc8c6459f99f9303865
Kara Erickson <karakara@google.com>
2019-08-13 17:13:56 +0000

    ebcf79d7f docs(zone.js): update DEVELOPER.md for changelog instruction (#32016)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+2.sha-48487cb.with-local-changes
+ * @license Angular v9.0.0-next.2+4.sha-ebcf79d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18345,7 +18345,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+2.sha-48487cb.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+4.sha-ebcf79d.with-local-changes');
 
 /**
  * @license
