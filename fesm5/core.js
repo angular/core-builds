bcfdd2a2d53204e25811554aaf4d948cd54904ab
Kara Erickson <karakara@google.com>
2019-08-13 18:42:50 +0000

    a91ab1552 fix(language-service): Remove 'context' used for module resolution (#32015)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+7.sha-a95f860.with-local-changes
+ * @license Angular v9.0.0-next.2+8.sha-a91ab15.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18345,7 +18345,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+7.sha-a95f860.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+8.sha-a91ab15.with-local-changes');
 
 /**
  * @license
