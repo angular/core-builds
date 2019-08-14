6cc6eb2cb0c27416dcb257d25f901d2a5a31863a
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:14:45 +0000

    c88cdea9a build(core): add missing tsconfig-build.json dependency (#31943)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+5.sha-3d1b82b.with-local-changes
+ * @license Angular v8.2.2+9.sha-c88cdea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.2+5.sha-3d1b82b.with-local-changes');
+var VERSION = new Version('8.2.2+9.sha-c88cdea.with-local-changes');
 
 /**
  * @license
