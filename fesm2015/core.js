27242e84add44f9655159601697d3b59919a9aab
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:24:18 +0000

    0757702d6 docs: add guide to reproduce material-unit-test failures locally (#32138)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+11.sha-da44498.with-local-changes
+ * @license Angular v8.2.2+14.sha-0757702.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.2+11.sha-da44498.with-local-changes');
+const VERSION = new Version('8.2.2+14.sha-0757702.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
