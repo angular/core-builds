07138dd3417a7e1018de2db291e737be9abe11ec
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:55:25 +0000

    2e4d17f3a perf(core): make sanitization tree-shakable in Ivy mode (#31934)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+40.sha-3cf2005.with-local-changes
+ * @license Angular v9.0.0-next.2+39.sha-2e4d17f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18415,7 +18415,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+40.sha-3cf2005.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+39.sha-2e4d17f.with-local-changes');
 
 /**
  * @license
