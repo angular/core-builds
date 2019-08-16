335c5eba70256b1551ef94f1c9ac2812e2a2060c
Andrew Kushnir <akushnir@google.com>
2019-08-16 17:22:10 +0000

    b8f86ad7d ci: move bazel saucelabs execution to script to be used across all Angular repos (#32141)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+18.sha-699c705.with-local-changes
+ * @license Angular v8.2.2+20.sha-b8f86ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.2+18.sha-699c705.with-local-changes');
+const VERSION = new Version('8.2.2+20.sha-b8f86ad.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
