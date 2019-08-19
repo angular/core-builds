d9c964ff74e5efaf31d24aec1b9e2be93e9b4c71
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:27:43 +0000

    adb4f7c7c build(docs-infra): upgrade cli command docs sources to 6aae7f563 (#32174)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+52.sha-5f76de1.with-local-changes
+ * @license Angular v9.0.0-next.2+53.sha-adb4f7c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18541,7 +18541,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.2+52.sha-5f76de1.with-local-changes');
+    var VERSION = new Version('9.0.0-next.2+53.sha-adb4f7c.with-local-changes');
 
     /**
      * @license
