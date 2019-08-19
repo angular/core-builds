78b6213bab16d8a6952a76c0a2b82b39f04a1ba5
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:32:45 +0000

    789dd6a0d docs(service-worker): mention that dataGroups only cache non-mutating requests (#32142)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+21.sha-e5b18e8.with-local-changes
+ * @license Angular v8.2.2+22.sha-789dd6a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.2+21.sha-e5b18e8.with-local-changes');
+var VERSION = new Version('8.2.2+22.sha-789dd6a.with-local-changes');
 
 /**
  * @license
