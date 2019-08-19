8d362a674b7b673e24e7da9312a42a30a25fd4db
Andrew Kushnir <akushnir@google.com>
2019-08-19 23:11:34 +0000

    a7b94783b fix(bazel): pin `@microsoft/api-extractor` (#32187)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+22.sha-789dd6a.with-local-changes
+ * @license Angular v8.2.2+23.sha-a7b9478.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.2+22.sha-789dd6a.with-local-changes');
+var VERSION = new Version('8.2.2+23.sha-a7b9478.with-local-changes');
 
 /**
  * @license
