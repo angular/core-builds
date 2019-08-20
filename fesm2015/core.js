00bbbc2e337bb459b4b7896cd85ab9249cf2167a
Andrew Kushnir <akushnir@google.com>
2019-08-20 17:07:53 +0000

    a01ed0d7b refactor(core): remove disabled injectable-pipe migration (#32184) (#32206)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+23.sha-a7b9478.with-local-changes
+ * @license Angular v8.2.2+24.sha-a01ed0d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.2+23.sha-a7b9478.with-local-changes');
+const VERSION = new Version('8.2.2+24.sha-a01ed0d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
