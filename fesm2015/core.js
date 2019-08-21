4336253660863e22cad5d787c387a8ca7274c37f
atscott <atscott@google.com>
2019-08-21 22:36:42 +0000

    44f233c02 docs: edit and organize aot doc (#32028)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+29.sha-e3c9f9d.with-local-changes
+ * @license Angular v8.2.3+1.sha-44f233c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.2+29.sha-e3c9f9d.with-local-changes');
+const VERSION = new Version('8.2.3+1.sha-44f233c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+29.sha-e3c9f9d.with-local-changes
+ * @license Angular v8.2.3+1.sha-44f233c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.2+29.sha-e3c9f9d.with-local-changes');
+var VERSION = new Version('8.2.3+1.sha-44f233c.with-local-changes');
 
 /**
  * @license
