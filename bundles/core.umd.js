c4ddafdead604f56fd71bd9e0c56bd47fa7ffd96
Andrew Kushnir <akushnir@google.com>
2019-08-21 18:16:25 +0000

    daac386f4 ci: update material-unit-tests commit (#32243)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+83.sha-0287b23.with-local-changes
+ * @license Angular v9.0.0-next.2+81.sha-daac386.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18512,7 +18512,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.2+83.sha-0287b23.with-local-changes');
+    var VERSION = new Version('9.0.0-next.2+81.sha-daac386.with-local-changes');
 
     /**
      * @license
