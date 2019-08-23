c8e915bf9067152a650fa53225f585ec20063c87
Andrew Scott <atscott01@gmail.com>
2019-08-23 16:28:57 +0000

    43bac2130 docs: fix http example to use correct property (#32288)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+10.sha-4df4150.with-local-changes
+ * @license Angular v8.2.3+11.sha-43bac21.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.3+10.sha-4df4150.with-local-changes');
+var VERSION = new Version('8.2.3+11.sha-43bac21.with-local-changes');
 
 /**
  * @license
