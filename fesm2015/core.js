98e39d7854eebd050bcc5ecf0bec165c928da9dd
atscott <atscott@google.com>
2019-08-23 18:31:56 +0000

    5e0890b3a build(docs-infra): upgrade cli command docs sources to 166cd1501 (#32286)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+11.sha-43bac21.with-local-changes
+ * @license Angular v8.2.3+12.sha-5e0890b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.3+11.sha-43bac21.with-local-changes');
+const VERSION = new Version('8.2.3+12.sha-5e0890b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
