5f00741d71a7629b0d5442fe3cab3f4ef17e4fed
atscott <atscott@google.com>
2019-08-26 17:39:51 +0000

    b5cb120db docs: Update the aria-valuemax attr of the sample (#32261)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+14.sha-a80a863.with-local-changes
+ * @license Angular v8.2.3+16.sha-b5cb120.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.3+14.sha-a80a863.with-local-changes');
+const VERSION = new Version('8.2.3+16.sha-b5cb120.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
