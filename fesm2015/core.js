135ceed5c644c83d21db55166d3fe7f98e0f61e5
atscott <atscott@google.com>
2019-08-26 22:24:24 +0000

    514271ba9 docs: reference articles specifically (#32221)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+18.sha-6b0b7d0.with-local-changes
+ * @license Angular v8.2.3+20.sha-514271b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.3+18.sha-6b0b7d0.with-local-changes');
+const VERSION = new Version('8.2.3+20.sha-514271b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
