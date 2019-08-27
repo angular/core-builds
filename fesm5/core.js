52580f0d2e36774e11df0d8297d6d983db7cef1f
atscott <atscott@google.com>
2019-08-27 23:38:35 +0000

    e3f42818e docs: add detail to router event doc (#32140)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+55.sha-7c7fcd7.with-local-changes
+ * @license Angular v9.0.0-next.3+58.sha-e3f4281.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18457,7 +18457,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.3+55.sha-7c7fcd7.with-local-changes');
+var VERSION = new Version('9.0.0-next.3+58.sha-e3f4281.with-local-changes');
 
 /**
  * @license
