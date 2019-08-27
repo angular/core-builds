763ac1015dd7bdd347d1dd7345a1f2f5c52ae133
atscott <atscott@google.com>
2019-08-27 16:28:07 +0000

    7033e7eb2 docs: Use ngOnInit to fetch data from services in the getting started guide (#32273)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+24.sha-8931239.with-local-changes
+ * @license Angular v8.2.3+25.sha-7033e7e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.3+24.sha-8931239.with-local-changes');
+var VERSION = new Version('8.2.3+25.sha-7033e7e.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+24.sha-8931239.with-local-changes
+ * @license Angular v8.2.3+25.sha-7033e7e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
