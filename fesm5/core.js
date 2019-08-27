2f09ce0b5ab8a4b0420965625c7339636f0044bf
atscott <atscott@google.com>
2019-08-27 16:24:49 +0000

    73dd43170 build: remove the deprecated `aio` commit message scope (#32341)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+20.sha-514271b.with-local-changes
+ * @license Angular v8.2.3+21.sha-73dd431.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.3+20.sha-514271b.with-local-changes');
+var VERSION = new Version('8.2.3+21.sha-73dd431.with-local-changes');
 
 /**
  * @license
