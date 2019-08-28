2e7e0e6bf21b81d045b3ad776dcaf2c1ec08f1d7
atscott <atscott@google.com>
2019-08-28 16:25:27 +0000

    6aaf4eac8 docs(docs-infra): add note about setting up examples on Windows (#32355)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+28.sha-aa8dd74.with-local-changes
+ * @license Angular v8.2.3+32.sha-6aaf4ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.3+28.sha-aa8dd74.with-local-changes');
+var VERSION = new Version('8.2.3+32.sha-6aaf4ea.with-local-changes');
 
 /**
  * @license
