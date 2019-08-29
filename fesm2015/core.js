741813495a889ea3a316107306a24631683f6bc0
Miško Hevery <misko@hevery.com>
2019-08-29 00:30:12 +0000

    46caf88b2 feat(language-service): add definitions for templateUrl (#32238)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+7.sha-b094936.with-local-changes
+ * @license Angular v9.0.0-next.4+1.sha-46caf88.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26527,7 +26527,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+7.sha-b094936.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+1.sha-46caf88.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
