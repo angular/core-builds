82e22404922c9716df46ca40faf219d8526ed41b
Miško Hevery <misko@hevery.com>
2019-08-29 19:56:12 +0000

    266c267b6 test: export TableModule in benchmarks (#32404)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+33.sha-b4d3468.with-local-changes
+ * @license Angular v9.0.0-next.4+32.sha-266c267.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18441,7 +18441,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.4+33.sha-b4d3468.with-local-changes');
+var VERSION = new Version('9.0.0-next.4+32.sha-266c267.with-local-changes');
 
 /**
  * @license
