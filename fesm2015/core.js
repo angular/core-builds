ad3fdfc03b6055d4c8f4b77a5d1415eccc7c211c
Miško Hevery <misko@hevery.com>
2019-08-29 00:32:45 +0000

    d0f3539e6 test(core): cleanup bazel target names for schematic tests (#32318)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+1.sha-46caf88.with-local-changes
+ * @license Angular v9.0.0-next.4+9.sha-d0f3539.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26508,7 +26508,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+1.sha-46caf88.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+9.sha-d0f3539.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
