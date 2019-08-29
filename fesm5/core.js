2ac216b302ff96a01fc64d468855834565694287
Miško Hevery <misko@hevery.com>
2019-08-29 00:37:34 +0000

    5b76b939a build: bump yarn requirement to 1.17.3 (#32344)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+1.sha-261593a.with-local-changes
+ * @license Angular v8.2.4+2.sha-5b76b93.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.4+1.sha-261593a.with-local-changes');
+var VERSION = new Version('8.2.4+2.sha-5b76b93.with-local-changes');
 
 /**
  * @license
