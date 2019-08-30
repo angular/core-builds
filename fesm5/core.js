551b140d645cdda29dabdc1cafabaf8483155ee4
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:15:35 +0000

    ba5e07efc perf(ivy): add a micro benchmark for map-based style and class bindings (#32401)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+48.sha-9bbe67f.with-local-changes
+ * @license Angular v9.0.0-next.4+46.sha-ba5e07e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18438,7 +18438,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.4+48.sha-9bbe67f.with-local-changes');
+var VERSION = new Version('9.0.0-next.4+46.sha-ba5e07e.with-local-changes');
 
 /**
  * @license
