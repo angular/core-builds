030ec98740cbb0621395e3b072002e4e6d96c82f
Misko Hevery <misko@hevery.com>
2019-08-30 20:10:41 +0000

    60f963986 ci: update codeowners for docs guides/examples/images (#32360)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+56.sha-c56c241.with-local-changes
+ * @license Angular v9.0.0-next.4+59.sha-60f9639.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26545,7 +26545,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+56.sha-c56c241.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+59.sha-60f9639.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
