74848982d2baea18cc12252690e97feb5e067acb
Misko Hevery <misko@hevery.com>
2019-08-30 20:04:17 +0000

    b5f85638f fix(zone.js): a path traversal attack in test (#32392)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+9.sha-efd13d3.with-local-changes
+ * @license Angular v8.2.4+10.sha-b5f8563.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.4+9.sha-efd13d3.with-local-changes');
+const VERSION = new Version('8.2.4+10.sha-b5f8563.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
