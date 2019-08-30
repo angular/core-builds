a4752f3b1d73c036602601d0595bff4344567d42
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:15:01 +0000

    9bbe67f28 docs: add clarification regarding development in older browsers in Deployment guide (#32210)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+49.sha-8c388e3.with-local-changes
+ * @license Angular v9.0.0-next.4+48.sha-9bbe67f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18581,7 +18581,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.4+49.sha-8c388e3.with-local-changes');
+    var VERSION = new Version('9.0.0-next.4+48.sha-9bbe67f.with-local-changes');
 
     /**
      * @license
