e9d79b56880079e1f241b022ff7a5df582bfe80e
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:54:45 +0000

    ad13520b2 docs: create Issue and Pull Request markdown doc, explaining automatic locking policy (#32405)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+50.sha-350ea47.with-local-changes
+ * @license Angular v9.0.0-next.4+51.sha-ad13520.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18614,7 +18614,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.4+50.sha-350ea47.with-local-changes');
+    var VERSION = new Version('9.0.0-next.4+51.sha-ad13520.with-local-changes');
 
     /**
      * @license
