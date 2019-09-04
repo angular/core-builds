34076afb62e5731c84b3cf7c4c6ae9df30ad7664
Miško Hevery <misko@hevery.com>
2019-09-04 18:53:44 +0000

    32a6972cd docs: add app-shell production reference (#32471)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+16.sha-2cd5d4c.with-local-changes
+ * @license Angular v8.2.4+17.sha-32a6972.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.4+16.sha-2cd5d4c.with-local-changes');
+var VERSION = new Version('8.2.4+17.sha-32a6972.with-local-changes');
 
 /**
  * @license
