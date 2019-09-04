2389c784009e04d8906c6ad254e20d7299daf17b
Miško Hevery <misko@hevery.com>
2019-09-04 19:08:01 +0000

    5112669d2 refactor(benchpress): remove two mutable exports from largetable/util.ts (#32425)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+18.sha-3d453fe.with-local-changes
+ * @license Angular v8.2.4+19.sha-5112669.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.4+18.sha-3d453fe.with-local-changes');
+var VERSION = new Version('8.2.4+19.sha-5112669.with-local-changes');
 
 /**
  * @license
