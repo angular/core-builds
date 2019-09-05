b799a9ff48aefa116b5979e2c2316dbff9e5bc1f
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:53:02 +0000

    be30f25da docs: update default path for xi18n (#32480)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+20.sha-8be20f3.with-local-changes
+ * @license Angular v8.2.5+5.sha-be30f25.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.4+20.sha-8be20f3.with-local-changes');
+const VERSION = new Version('8.2.5+5.sha-be30f25.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
