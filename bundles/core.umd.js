77caf91dc45f22f58d7ca0f4ea7486842aa19549
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 16:42:56 +0000

    aaea3878d ci: work around `CIRCLE_COMPARE_URL` not being available wih CircleCI Pipelines (#32537)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+9.sha-8838a1b.with-local-changes
+ * @license Angular v8.2.5+10.sha-aaea387.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18562,7 +18562,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.5+9.sha-8838a1b.with-local-changes');
+    var VERSION = new Version('8.2.5+10.sha-aaea387.with-local-changes');
 
     /**
      * @license
