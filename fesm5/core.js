d4f28a36ba6348b26dbe0560dee9eee1675f0c7b
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 20:14:11 +0000

    8296f6b68 test(ngcc): print an informative message when an integration test fails (#32427)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+29.sha-bf15d3e.with-local-changes
+ * @license Angular v9.0.0-next.5+41.sha-8296f6b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18425,7 +18425,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.5+29.sha-bf15d3e.with-local-changes');
+var VERSION = new Version('9.0.0-next.5+41.sha-8296f6b.with-local-changes');
 
 /**
  * @license
