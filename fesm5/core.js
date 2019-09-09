f8358172217bcfac726fd5f1f410f32dede567e3
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 20:17:25 +0000

    2895edc9c perf(ivy): introduce a node-based micro-benchmarks harness (#32510)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+43.sha-66df745.with-local-changes
+ * @license Angular v9.0.0-next.5+44.sha-2895edc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18392,7 +18392,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.5+43.sha-66df745.with-local-changes');
+var VERSION = new Version('9.0.0-next.5+44.sha-2895edc.with-local-changes');
 
 /**
  * @license
