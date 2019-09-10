13f51ed7f6716a22f80431e91f6aef323190c0cd
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 10:49:42 +0000

    1c71db846 docs: describe your change... (#32552)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+12.sha-48e4b0e.with-local-changes
+ * @license Angular v8.2.5+13.sha-1c71db8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.5+12.sha-48e4b0e.with-local-changes');
+var VERSION = new Version('8.2.5+13.sha-1c71db8.with-local-changes');
 
 /**
  * @license
