c94aa5df44165b1016111e14a841b0e80abe4b1b
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 18:37:30 +0000

    72807101e perf(ivy): check for animation synthetic props in dev mode only (#32578)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+61.sha-25d90da.with-local-changes
+ * @license Angular v9.0.0-next.5+62.sha-7280710.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26513,7 +26513,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+61.sha-25d90da.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+62.sha-7280710.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
