896f00805f1012a287c23d3a86fed818eb7f9c76
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 17:21:53 +0000

    f00bb85b5 refactor(ivy): migrate content spec from render3 (#32474)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+56.sha-bbd4a33.with-local-changes
+ * @license Angular v9.0.0-next.5+58.sha-f00bb85.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26513,7 +26513,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+56.sha-bbd4a33.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+58.sha-f00bb85.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
