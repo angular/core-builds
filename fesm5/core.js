d4d695b102d2c505af6586612cea86fe4fc2feea
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 17:24:37 +0000

    76a84706b docs: fixed typo for toh-pt5.md (#32362)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+14.sha-39302ba.with-local-changes
+ * @license Angular v8.2.5+15.sha-76a8470.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.5+14.sha-39302ba.with-local-changes');
+var VERSION = new Version('8.2.5+15.sha-76a8470.with-local-changes');
 
 /**
  * @license
