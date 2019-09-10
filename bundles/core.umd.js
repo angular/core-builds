b32665449508be860d3fd756551fec95e8f726c7
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 21:46:48 +0000

    2565f6795 docs: add mdb angular boilerplate to docs resources (#30448)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+17.sha-38f4dcd.with-local-changes
+ * @license Angular v8.2.5+18.sha-2565f67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18562,7 +18562,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('8.2.5+17.sha-38f4dcd.with-local-changes');
+    var VERSION = new Version('8.2.5+18.sha-2565f67.with-local-changes');
 
     /**
      * @license
