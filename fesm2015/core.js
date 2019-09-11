fdc4c5e0080d168b91faf99cd25706f85a35a721
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 23:46:46 +0000

    8a911773b docs: add header to code examples (#32563)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+9.sha-d30cd33.with-local-changes
+ * @license Angular v9.0.0-next.6+7.sha-8a91177.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26514,7 +26514,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.6+9.sha-d30cd33.with-local-changes');
+const VERSION = new Version('9.0.0-next.6+7.sha-8a91177.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- src/r3_symbols.d.ts
+++ src/r3_symbols.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+9.sha-d30cd33.with-local-changes
+ * @license Angular v9.0.0-next.6+7.sha-8a91177.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
