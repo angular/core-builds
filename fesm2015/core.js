4439af82da4cfea017e76029ba412380e37a0949
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 21:03:52 +0000

    bb9e61202 docs: remove duplicate entry in changelog for 8.2.6

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+2.sha-c1ac21d.with-local-changes
+ * @license Angular v9.0.0-next.6+3.sha-bb9e612.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26673,7 +26673,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.6+2.sha-c1ac21d.with-local-changes');
+const VERSION = new Version('9.0.0-next.6+3.sha-bb9e612.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
