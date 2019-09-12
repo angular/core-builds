f9586a35ab4e26afca1af07ebc27712aeab4a835
Kara Erickson <karakara@google.com>
2019-09-12 20:26:00 +0000

    5a830c49c refactor(ivy): improve micro-benchmark profiling (#32647)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+36.sha-bd67958.with-local-changes
+ * @license Angular v9.0.0-next.6+37.sha-5a830c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18441,7 +18441,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.6+36.sha-bd67958.with-local-changes');
+var VERSION = new Version('9.0.0-next.6+37.sha-5a830c4.with-local-changes');
 
 /**
  * @license
