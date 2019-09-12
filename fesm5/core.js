679514e80407f09b6f2ee60d0f50f772d1868292
Kara Erickson <karakara@google.com>
2019-09-12 17:43:07 +0000

    eab959aa1 build(docs-infra): upgrade cli command docs sources to 5d72355fc (#32634)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+18.sha-0a0489d.with-local-changes
+ * @license Angular v9.0.0-next.6+22.sha-eab959a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18486,7 +18486,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.6+18.sha-0a0489d.with-local-changes');
+var VERSION = new Version('9.0.0-next.6+22.sha-eab959a.with-local-changes');
 
 /**
  * @license
