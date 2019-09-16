ab2d31b17489a68e24b3940f1e918a695a398218
Andrew Kushnir <akushnir@google.com>
2019-09-16 21:40:19 +0000

    67d08b15e ci: fix name of lock-closed workflow (#32698)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+60.sha-a2e890e.with-local-changes
+ * @license Angular v9.0.0-next.6+62.sha-67d08b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18685,7 +18685,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.6+60.sha-a2e890e.with-local-changes');
+    var VERSION = new Version('9.0.0-next.6+62.sha-67d08b1.with-local-changes');
 
     /**
      * @license
