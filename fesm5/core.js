fdbd70eb12851671ced19d433b06c50dc16c4845
Andrew Kushnir <akushnir@google.com>
2019-09-17 21:04:38 +0000

    e367aa2ca docs: Typo fixes in hierarchical DI guide (#32536)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+15.sha-0ee2b75.with-local-changes
+ * @license Angular v8.2.6+16.sha-e367aa2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.6+15.sha-0ee2b75.with-local-changes');
+var VERSION = new Version('8.2.6+16.sha-e367aa2.with-local-changes');
 
 /**
  * @license
