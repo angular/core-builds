7027fbe9643f9b68645b2b126465e2d62fef7a92
Andrew Kushnir <akushnir@google.com>
2019-09-18 20:59:44 +0000

    8398f9d54 docs: edit copy on first step of getting started (#32751)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+18.sha-ba1ef6b.with-local-changes
+ * @license Angular v8.2.7+1.sha-8398f9d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.6+18.sha-ba1ef6b.with-local-changes');
+var VERSION = new Version('8.2.7+1.sha-8398f9d.with-local-changes');
 
 /**
  * @license
