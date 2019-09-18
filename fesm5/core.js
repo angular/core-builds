ce268453e49f3c90118d75527e0877e649b26cf4
Andrew Kushnir <akushnir@google.com>
2019-09-18 20:29:40 +0000

    9d8dc793d test(language-service): Create proper test project (#32653)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+1.sha-2846505.with-local-changes
+ * @license Angular v9.0.0-next.7+2.sha-9d8dc79.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18596,7 +18596,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.7+1.sha-2846505.with-local-changes');
+var VERSION = new Version('9.0.0-next.7+2.sha-9d8dc79.with-local-changes');
 
 /**
  * @license
