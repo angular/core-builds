545d1a9ca435013fafedfb01f920ec352e2f4d56
Andrew Kushnir <akushnir@google.com>
2019-09-23 21:46:06 +0000

    e63390daf docs: correct default for router extra option (#32702)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.7+2.sha-af99f9e.with-local-changes
+ * @license Angular v8.2.7+3.sha-e63390d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.7+2.sha-af99f9e.with-local-changes');
+const VERSION = new Version('8.2.7+3.sha-e63390d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
