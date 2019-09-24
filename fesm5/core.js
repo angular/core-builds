97c359299f805b7daac92a1b69fe4fe551c353ba
Andrew Kushnir <akushnir@google.com>
2019-09-24 21:20:29 +0000

    c093390c2 docs: fix typo in upgrade guide (#32807)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+20.sha-32b6c22.with-local-changes
+ * @license Angular v9.0.0-next.7+21.sha-c093390.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18748,7 +18748,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.7+20.sha-32b6c22.with-local-changes');
+var VERSION = new Version('9.0.0-next.7+21.sha-c093390.with-local-changes');
 
 /**
  * @license
