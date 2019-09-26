9266b42001a954fb5d0ffdddd2703b6478eb4d24
atscott <atscott@google.com>
2019-09-26 15:00:35 +0000

    fbad4ff65 build(docs-infra): upgrade cli command docs sources to c6184bf31 (#32864)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+7.sha-c07b6fe.with-local-changes
+ * @license Angular v9.0.0-next.8+8.sha-fbad4ff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18777,7 +18777,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.8+7.sha-c07b6fe.with-local-changes');
+    var VERSION = new Version('9.0.0-next.8+8.sha-fbad4ff.with-local-changes');
 
     /**
      * @license
