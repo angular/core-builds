4acfafb97b357db990b6b6229a1ba28c086e29c5
atscott <atscott@google.com>
2019-09-27 22:29:25 +0000

    a23ee2b0a docs(router): make `InitialNavigation` part of the public API (#32707)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+18.sha-e41cbfb.with-local-changes
+ * @license Angular v9.0.0-next.8+19.sha-a23ee2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18603,7 +18603,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.8+18.sha-e41cbfb.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+19.sha-a23ee2b.with-local-changes');
 
 /**
  * @license
