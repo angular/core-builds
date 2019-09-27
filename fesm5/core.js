6fce28570a5edbe56db3bf45101af0ad288d9184
atscott <atscott@google.com>
2019-09-27 23:18:26 +0000

    bdea243fe docs: add missing space between "Component" and "will" in guide (#32885)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+8.sha-2397144.with-local-changes
+ * @license Angular v8.2.8+9.sha-bdea243.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.8+8.sha-2397144.with-local-changes');
+var VERSION = new Version('8.2.8+9.sha-bdea243.with-local-changes');
 
 /**
  * @license
