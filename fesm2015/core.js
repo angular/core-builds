2116d77988050097ff8b9c81ebf9e2703fe03510
atscott <atscott@google.com>
2019-09-30 21:33:36 +0000

    0818e2c79 docs(service-worker): add info about recovering from degraded Driver state (#32682)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+13.sha-7b8b2d4.with-local-changes
+ * @license Angular v8.2.8+14.sha-0818e2c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.8+13.sha-7b8b2d4.with-local-changes');
+const VERSION = new Version('8.2.8+14.sha-0818e2c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
