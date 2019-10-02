e4789c67fc77d28f0a2e0bdadf5623473598ac55
atscott <atscott01@gmail.com>
2019-10-02 18:17:34 +0000

    d96167fc5 docs: add lib build note (#32940)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+22.sha-57457fb.with-local-changes
+ * @license Angular v8.2.8+24.sha-d96167f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18419,7 +18419,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.8+22.sha-57457fb.with-local-changes');
+var VERSION = new Version('8.2.8+24.sha-d96167f.with-local-changes');
 
 /**
  * @license
