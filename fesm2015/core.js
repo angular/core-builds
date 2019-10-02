a4df2c711e61e474397bc07d310d47fba369d041
atscott <atscott01@gmail.com>
2019-10-02 21:06:19 +0000

    9ba898d58 docs: remove Renderer2 section (#32972)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+24.sha-d96167f.with-local-changes
+ * @license Angular v8.2.9+9.sha-9ba898d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.8+24.sha-d96167f.with-local-changes');
+const VERSION = new Version('8.2.9+9.sha-9ba898d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
