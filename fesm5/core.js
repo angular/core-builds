835762b9ab7164e572da5fb3520cf299cea09fce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:19:28 +0000

    fee28e20b feat(forms): formGroupName and formArrayName also accepts a number (#32607)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+57.sha-c61e4d7.with-local-changes
+ * @license Angular v9.0.0-next.9+59.sha-fee28e2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6408,7 +6408,8 @@ function setStylingMapsSyncFn(fn) {
  */
 var setStyle = function (renderer, native, prop, value) {
     if (renderer !== null) {
-        if (value) {
+        // Use `isStylingValueDefined` to account for falsy values that should be bound like 0.
+        if (isStylingValueDefined(value)) {
             // opacity, z-index and flexbox all have number values
             // and these need to be converted into strings so that
             // they can be assigned properly.
@@ -18679,7 +18680,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.9+57.sha-c61e4d7.with-local-changes');
+var VERSION = new Version('9.0.0-next.9+59.sha-fee28e2.with-local-changes');
 
 /**
  * @license
