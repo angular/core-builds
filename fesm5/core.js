8600808d528c41a07e8a42218cdaef18cefe12d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 18:07:34 +0000

    ed711418f style(bazel): fix 2 unformatted .bzl files

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+72.sha-c653a16.with-local-changes
+ * @license Angular v9.0.0-next.9+73.sha-ed71141.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18628,7 +18628,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.9+72.sha-c653a16.with-local-changes');
+var VERSION = new Version('9.0.0-next.9+73.sha-ed71141.with-local-changes');
 
 /**
  * @license
