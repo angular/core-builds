be523d846d7dbf4e51229f938e5c8d7c869769f6
Miško Hevery <misko@hevery.com>
2019-10-10 21:35:32 +0000

    5ede5b780 ci: run bazel for package-builder via yarn rather than binary directly (#32983)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+20.sha-f640a4a.with-local-changes
+ * @license Angular v9.0.0-next.10+19.sha-5ede5b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18745,7 +18745,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10+20.sha-f640a4a.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+19.sha-5ede5b7.with-local-changes');
 
 /**
  * @license
