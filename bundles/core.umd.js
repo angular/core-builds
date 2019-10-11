05dcd6f428e6681d553cb5a19dc9bc96653ddf9e
Miško Hevery <misko@hevery.com>
2019-10-11 23:04:37 +0000

    64fd0d6db refactor(core): rename ngComponentDef to ɵcmp (#33088)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+43.sha-c88305d.with-local-changes
+ * @license Angular v9.0.0-next.10+42.sha-64fd0d6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18893,7 +18893,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.10+43.sha-c88305d.with-local-changes');
+    var VERSION = new Version('9.0.0-next.10+42.sha-64fd0d6.with-local-changes');
 
     /**
      * @license
