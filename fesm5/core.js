2152e9f838c21e8a9b7ea7fa1e641e4aabc4f222
Miško Hevery <misko@hevery.com>
2019-10-14 20:56:46 +0000

    cd7b19921 feat(ivy): check regular attributes that correspond with directive inputs (#33066)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+62.sha-39587ad.with-local-changes
+ * @license Angular v9.0.0-next.10+66.sha-cd7b199.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18719,7 +18719,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10+62.sha-39587ad.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+66.sha-cd7b199.with-local-changes');
 
 /**
  * @license
