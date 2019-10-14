8b01382db9367bb7f76118a0a3512f8573ba9871
Miško Hevery <misko@hevery.com>
2019-10-14 16:45:57 +0000

    25af147a8 refactor(ngcc): fix formatting of missing dependencies error (#33139)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+47.sha-4acf0a0.with-local-changes
+ * @license Angular v9.0.0-next.10+49.sha-25af147.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18750,7 +18750,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10+47.sha-4acf0a0.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+49.sha-25af147.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+47.sha-4acf0a0.with-local-changes
+ * @license Angular v9.0.0-next.10+49.sha-25af147.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
