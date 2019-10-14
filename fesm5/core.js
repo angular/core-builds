b204a2e7d918af2011440edba107aedd9a1a1cb1
Miško Hevery <misko@hevery.com>
2019-10-14 20:50:12 +0000

    39587ad12 fix(compiler-cli): resolve type of exported *ngIf variable. (#33016)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+61.sha-5557dec.with-local-changes
+ * @license Angular v9.0.0-next.10+62.sha-39587ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18719,7 +18719,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10+61.sha-5557dec.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+62.sha-39587ad.with-local-changes');
 
 /**
  * @license
