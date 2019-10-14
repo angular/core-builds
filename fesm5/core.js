c7554261b783136e3c72138a4bc4b15d2b5753d5
Miško Hevery <misko@hevery.com>
2019-10-14 16:43:57 +0000

    d8249d123 feat(ivy): better error messages for unknown components (#33064)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+42.sha-64fd0d6.with-local-changes
+ * @license Angular v9.0.0-next.10+44.sha-d8249d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18719,7 +18719,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10+42.sha-64fd0d6.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+44.sha-d8249d1.with-local-changes');
 
 /**
  * @license
