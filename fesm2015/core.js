0437b3fdcaff30d225a209a13abff39de71126fd
Miško Hevery <misko@hevery.com>
2019-10-15 21:28:33 +0000

    ad72c9044 fix(ivy): i18n - add XLIFF aliases for legacy message id support (#33160)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+94.sha-11e04b1.with-local-changes
+ * @license Angular v9.0.0-next.10+95.sha-ad72c90.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27199,7 +27199,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.10+94.sha-11e04b1.with-local-changes');
+const VERSION = new Version('9.0.0-next.10+95.sha-ad72c90.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
