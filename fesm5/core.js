e05a7ec0414fe552ac3632d3735927e026157db3
Miško Hevery <misko@hevery.com>
2019-10-15 00:09:03 +0000

    8b0cb2f0c docs: correct matcher description (#32970)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+83.sha-ac745c8.with-local-changes
+ * @license Angular v9.0.0-next.10+84.sha-8b0cb2f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18751,7 +18751,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10+83.sha-ac745c8.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+84.sha-8b0cb2f.with-local-changes');
 
 /**
  * @license
