19a52021fd337af2be8bdf4f29853a647453c2a2
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 21:12:18 +0000

    d5b59009d ci: fix dev-infra incorrectly matching all ".bzl" files as codeowner (#32956)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+44.sha-f45c431.with-local-changes
+ * @license Angular v9.0.0-next.11+45.sha-d5b5900.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18847,7 +18847,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.11+44.sha-f45c431.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+45.sha-d5b5900.with-local-changes');
 
 /**
  * @license
