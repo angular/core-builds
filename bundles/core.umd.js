e460817be738e7cc2b8917ecc97865811cb610b1
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 23:09:22 +0000

    fd4fed14d fix(compiler): absolute source span for template attribute expressions (#33189)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+47.sha-422eb14.with-local-changes
+ * @license Angular v9.0.0-next.11+48.sha-fd4fed1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18967,7 +18967,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.11+47.sha-422eb14.with-local-changes');
+    var VERSION = new Version('9.0.0-next.11+48.sha-fd4fed1.with-local-changes');
 
     /**
      * @license
