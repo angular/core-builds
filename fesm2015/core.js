c3432da255b9264bcb0117c73168764e46e281d1
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 06:58:52 +0000

    d192a7b47 revert: fix(docs-infra): consider all formats when compiling docs examples with `ngcc` (#33216)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+19.sha-724707c.with-local-changes
+ * @license Angular v9.0.0-next.11+23.sha-d192a7b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27393,7 +27393,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.11+19.sha-724707c.with-local-changes');
+const VERSION = new Version('9.0.0-next.11+23.sha-d192a7b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
