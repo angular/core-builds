a9f12a08bc6ad9bb1612b2a9f9607a4f487e9004
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 15:52:45 +0000

    f13e40db1 docs: change "patchMatch" to "pathMatch" (#32935)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+5.sha-fab6ea0.with-local-changes
+ * @license Angular v8.2.11+6.sha-f13e40d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26487,7 +26487,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+5.sha-fab6ea0.with-local-changes');
+const VERSION = new Version('8.2.11+6.sha-f13e40d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
