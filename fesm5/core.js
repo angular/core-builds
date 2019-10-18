0a5a9830e64939f69d8916af8e8001845a01dea3
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 00:01:36 +0000

    5dfbcd563 fix(core): add CLI instructions when localize polyfill is missing (#33199)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+53.sha-de44570.with-local-changes
+ * @license Angular v9.0.0-next.11+54.sha-5dfbcd5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18855,7 +18855,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.11+53.sha-de44570.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+54.sha-5dfbcd5.with-local-changes');
 
 /**
  * @license
@@ -30923,7 +30923,8 @@ if (ngDevMode) {
     _global.$localize = _global.$localize || function () {
         throw new Error('It looks like your application or one of its dependencies is using i18n.\n' +
             'Angular 9 introduced a global `$localize()` function that needs to be loaded.\n' +
-            'Please add `import \'@angular/localize/init\';` to your polyfills.ts file.');
+            'Please run `ng add @angular/localize` from the Angular CLI.\n' +
+            '(For non-CLI projects, add `import \'@angular/localize/init\';` to your polyfills.ts file)');
     };
 }
 
