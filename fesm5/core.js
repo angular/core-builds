2740ba1f0a4cdee4bd2b5939eb2fd894f6873819
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:03:00 +0000

    c44a1a78a9 docs: add ayaz hafiz as a contributor (#32975)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+66.sha-f6667f8.with-local-changes
+ * @license Angular v9.0.0-next.11+67.sha-c44a1a7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18807,7 +18807,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.11+66.sha-f6667f8.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+67.sha-c44a1a7.with-local-changes');
 
 /**
  * @license
