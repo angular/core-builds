489f7874dfa7148b4dfbd1ccf45cb44a05b674d1
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:00:52 +0000

    f6667f8281 fix(router): adjust UrlTree redirect to replace URL if in eager update (#32988)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+63.sha-01d3599.with-local-changes
+ * @license Angular v9.0.0-next.11+66.sha-f6667f8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27108,7 +27108,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.11+63.sha-01d3599.with-local-changes');
+const VERSION = new Version('9.0.0-next.11+66.sha-f6667f8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
