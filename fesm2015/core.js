49b80b1b14c162832271a53a1d8194c59b646298
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 17:10:06 +0000

    e5081bcf25 build: update the integration/payloads limit

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+11.sha-d7dc6cb.with-local-changes
+ * @license Angular v9.0.0-next.12+17.sha-e5081bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27099,7 +27099,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.12+11.sha-d7dc6cb.with-local-changes');
+const VERSION = new Version('9.0.0-next.12+17.sha-e5081bc.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+11.sha-d7dc6cb.with-local-changes
+ * @license Angular v9.0.0-next.12+17.sha-e5081bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18814,7 +18814,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.12+11.sha-d7dc6cb.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+17.sha-e5081bc.with-local-changes');
 
 /**
  * @license
