1e47093094b3d8d4218acfd9d1814bcc41a88c6f
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 20:15:00 +0000

    418e9cf3c4 build: address comments, restructure setup-rbe.sh script (#33109)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+24.sha-9de4b1c.with-local-changes
+ * @license Angular v9.0.0-next.12+31.sha-418e9cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27099,7 +27099,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.12+24.sha-9de4b1c.with-local-changes');
+const VERSION = new Version('9.0.0-next.12+31.sha-418e9cf.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
