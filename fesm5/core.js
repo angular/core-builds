39fcafe3bf060e0969f6df7c844bd83424d2fe37
Kara Erickson <karakara@google.com>
2019-10-22 19:29:14 +0000

    5632424d04 refactor(ivy): ViewRef needs embededViewRef declaration (#33074)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+49.sha-1b8b04c.with-local-changes
+ * @license Angular v9.0.0-next.12+48.sha-5632424.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18835,7 +18835,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.12+49.sha-1b8b04c.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+48.sha-5632424.with-local-changes');
 
 /**
  * @license
