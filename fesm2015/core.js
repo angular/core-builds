776fdcdb26be5aa63cf0c0eb51f0b233957cd9e7
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:21:54 +0000

    2d465ee37b feat(docs-infra): implement figure styles (#33259)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+21.sha-70238f7.with-local-changes
+ * @license Angular v8.2.11+22.sha-2d465ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26488,7 +26488,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+21.sha-70238f7.with-local-changes');
+const VERSION = new Version('8.2.11+22.sha-2d465ee.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
