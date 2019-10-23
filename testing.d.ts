516b9f186bac19ee26aed1593eab0d8e80d18ef9
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:30:08 +0000

    43ac02e566 fix(docs-infra): scroll to top when navigating to new page via address bar (#33344)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+55.sha-ed4d96f.with-local-changes
+ * @license Angular v9.0.0-next.12+56.sha-43ac02e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
