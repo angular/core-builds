e9abcef95ff9d53918e2ded5309100728e42d824
Andrew Kushnir <akushnir@google.com>
2019-10-23 23:33:37 +0000

    0e9140c738 release: cut the v9.0.0-next.13 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+69.sha-2f541a9.with-local-changes
+ * @license Angular v9.0.0-next.13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27120,7 +27120,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.12+69.sha-2f541a9.with-local-changes');
+const VERSION = new Version('9.0.0-next.13.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
