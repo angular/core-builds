8501ffc81d528879e3581ef7ad5abc2661bbdc2b
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:39:04 +0000

    66519de2dd build: fix gulp setup not working with node v12 (#33348)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+53.sha-49eec5d.with-local-changes
+ * @license Angular v9.0.0-next.12+58.sha-66519de.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18988,7 +18988,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.12+53.sha-49eec5d.with-local-changes');
+    var VERSION = new Version('9.0.0-next.12+58.sha-66519de.with-local-changes');
 
     /**
      * @license
