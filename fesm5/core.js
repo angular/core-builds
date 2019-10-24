bb742c52f110f2c523172065f8569c2b0e42e735
Andrew Kushnir <akushnir@google.com>
2019-10-24 23:34:56 +0000

    0d9be22023 feat(ivy): strictness flags for template type checking (#33365)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+28.sha-4aa51b7.with-local-changes
+ * @license Angular v9.0.0-next.13+31.sha-0d9be22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18890,7 +18890,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.13+28.sha-4aa51b7.with-local-changes');
+var VERSION = new Version('9.0.0-next.13+31.sha-0d9be22.with-local-changes');
 
 /**
  * @license
