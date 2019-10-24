43156a636bb6670c86780144fac14d8be43028f3
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:08:08 +0000

    2805af900f docs: fix typo in getting started guide (#33372)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+3.sha-a1d7b6b.with-local-changes
+ * @license Angular v9.0.0-next.13+6.sha-2805af9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19045,7 +19045,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.13+3.sha-a1d7b6b.with-local-changes');
+    var VERSION = new Version('9.0.0-next.13+6.sha-2805af9.with-local-changes');
 
     /**
      * @license
