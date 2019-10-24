2d751fadd41283ae9d39ec76d162a5d99787a206
Andrew Kushnir <akushnir@google.com>
2019-10-24 23:08:42 +0000

    4aa51b751b feat(ivy): verify whether TypeScript version is supported (#33377)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+27.sha-09a2bb8.with-local-changes
+ * @license Angular v9.0.0-next.13+28.sha-4aa51b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18890,7 +18890,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.13+27.sha-09a2bb8.with-local-changes');
+var VERSION = new Version('9.0.0-next.13+28.sha-4aa51b7.with-local-changes');
 
 /**
  * @license
