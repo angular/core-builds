3f3eecbe9cbb254bb94f70f4f136567c8c55e35b
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:13:19 +0000

    e0428fe83c docs: fix typo in getting started guide (#33372)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+23.sha-663aba5.with-local-changes
+ * @license Angular v8.2.12+2.sha-e0428fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26488,7 +26488,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+23.sha-663aba5.with-local-changes');
+const VERSION = new Version('8.2.12+2.sha-e0428fe.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
