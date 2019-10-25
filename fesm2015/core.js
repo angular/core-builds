c24af8a96c38e6e3f0ba7416b307f50f29560af8
Andrew Kushnir <akushnir@google.com>
2019-10-25 00:18:02 +0000

    3f195fefa9 test(ivy): improve microbenchmark reporting progress (#33386) PR Close #33386

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+32.sha-a17cc9b.with-local-changes
+ * @license Angular v9.0.0-next.13+33.sha-3f195fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27212,7 +27212,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.13+32.sha-a17cc9b.with-local-changes');
+const VERSION = new Version('9.0.0-next.13+33.sha-3f195fe.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
