b3cfeddf34b35b4533b5d8894a6707d0855f4d9f
Andrew Kushnir <akushnir@google.com>
2019-10-25 20:17:38 +0000

    3505692f75 docs: add @next to update instructions (#33410)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+52.sha-8d15bfa.with-local-changes
+ * @license Angular v9.0.0-next.13+53.sha-3505692.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19235,7 +19235,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.13+52.sha-8d15bfa.with-local-changes');
+var VERSION = new Version('9.0.0-next.13+53.sha-3505692.with-local-changes');
 
 /**
  * @license
