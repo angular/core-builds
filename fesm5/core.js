de51266f7fee11a587a21efee250fb6346d4ab3b
Andrew Kushnir <akushnir@google.com>
2019-10-29 21:57:57 +0000

    5ed6abe3df build: update to nodejs rules 0.39.1 (#33458)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+27.sha-bf913cc.with-local-changes
+ * @license Angular v9.0.0-next.14+29.sha-5ed6abe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19210,7 +19210,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.14+27.sha-bf913cc.with-local-changes');
+var VERSION = new Version('9.0.0-next.14+29.sha-5ed6abe.with-local-changes');
 
 /**
  * @license
