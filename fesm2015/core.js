0b41b8365d3f8073fffa6de0b3ed9d618c815e96
atscott <atscott01@gmail.com>
2019-10-31 22:46:47 +0000

    1d141a8ab1 fix(compiler-cli): attach the correct `viaModule` to namespace imports (#33495)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+6.sha-d5ae854.with-local-changes
+ * @license Angular v9.0.0-rc.0+7.sha-1d141a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27633,7 +27633,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0+6.sha-d5ae854.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+7.sha-1d141a8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
