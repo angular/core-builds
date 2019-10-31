387a8d41f372b2f1ead5e1d73597f8004beb1867
Andrew Kushnir <akushnir@google.com>
2019-10-31 19:14:53 +0000

    12703d5601 docs: update to use latest 8.3.x CLI (#33518)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.15+3.sha-e4ebffa.with-local-changes
+ * @license Angular v9.0.0-next.15+4.sha-12703d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.15+3.sha-e4ebffa.with-local-changes');
+    var VERSION = new Version('9.0.0-next.15+4.sha-12703d5.with-local-changes');
 
     /**
      * @license
