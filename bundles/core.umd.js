4a6fde6c822a6668508a461c8bb91ba1c724f296
Andrew Kushnir <akushnir@google.com>
2019-10-31 17:40:01 +0000

    c4695bb387 build: ensure that version stamping is applied when the npm_package publish action is called (#33512)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.15+1.sha-7caed51.with-local-changes
+ * @license Angular v9.0.0-next.15+2.sha-c4695bb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-next.15+1.sha-7caed51.with-local-changes');
+    var VERSION = new Version('9.0.0-next.15+2.sha-c4695bb.with-local-changes');
 
     /**
      * @license
