a826815835ef45f536cf3f79d95f0c729f56a134
atscott <atscott01@gmail.com>
2019-10-31 22:41:21 +0000

    d863839ecc docs: fix selector in getting started guide (#33333) PR Close #33333

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+2.sha-4099822.with-local-changes
+ * @license Angular v9.0.0-rc.0+5.sha-d863839.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27633,7 +27633,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0+2.sha-4099822.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+5.sha-d863839.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
