241fb91a6e73263e05443a4d3af6beb771e2a581
atscott <atscott01@gmail.com>
2019-11-04 20:29:44 +0000

    e3189f97ff docs: Update link to angular + bazel example app (#33409)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+36.sha-4ff43e1.with-local-changes
+ * @license Angular v9.0.0-rc.0+37.sha-e3189f9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19242,7 +19242,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+36.sha-4ff43e1.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+37.sha-e3189f9.with-local-changes');
 
 /**
  * @license
