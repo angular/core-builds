6d4392da5d993b61666b8a5760a1cf23343ff187
atscott <atscott01@gmail.com>
2019-11-05 01:12:02 +0000

    25aaff2ee1 docs: Adding Doug Parker to contributors list. (#33516)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+40.sha-4abf15c.with-local-changes
+ * @license Angular v9.0.0-rc.0+42.sha-25aaff2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
