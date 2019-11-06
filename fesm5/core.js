092bc3caaa22e1b131074717e6cf15ea762b66e8
atscott <atscott01@gmail.com>
2019-11-06 23:25:13 +0000

    55b1b7745b release: cut the v9.0.0-rc.1 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+78.sha-a20979d.with-local-changes
+ * @license Angular v9.0.0-rc.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19287,7 +19287,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+78.sha-a20979d.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1.with-local-changes');
 
 /**
  * @license
