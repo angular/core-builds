03c5afc7d2a499e2519db80f22215c9674a6b6a2
atscott <atscott01@gmail.com>
2019-11-06 20:20:58 +0000

    41560b47c4 refactor(ivy): move all styling configurations into `TNodeFlags` (#33540)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+74.sha-fe12d0d.with-local-changes
+ * @license Angular v9.0.0-rc.0+69.sha-41560b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19318,7 +19318,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+74.sha-fe12d0d.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+69.sha-41560b4.with-local-changes');
 
 /**
  * @license
