6838f084bd2e0589e9708348f957f34ed08a8af2
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:15:52 +0000

    cd8333cf0d fix(ivy): recompile component when template changes in ngc watch mode (#33551)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+7.sha-99ead47.with-local-changes
+ * @license Angular v9.0.0-rc.1+13.sha-cd8333c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19527,7 +19527,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+7.sha-99ead47.with-local-changes');
+    var VERSION = new Version('9.0.0-rc.1+13.sha-cd8333c.with-local-changes');
 
     /**
      * @license
