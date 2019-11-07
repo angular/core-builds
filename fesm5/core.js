2b67e869d7cc71f800090b05b5e00d66d25156ec
Andrew Scott <atscott01@gmail.com>
2019-11-07 22:58:26 +0000

    0ca2a390d5 docs: app-name not rendering on browser (#33613)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+20.sha-c77faf5.with-local-changes
+ * @license Angular v9.0.0-rc.1+17.sha-0ca2a39.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19343,7 +19343,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+20.sha-c77faf5.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+17.sha-0ca2a39.with-local-changes');
 
 /**
  * @license
