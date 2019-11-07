4317ddcff22dc2f4bc599d0c47644ad109b6a341
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:52:50 +0000

    71b400eb7b test(ivy): introduce a benchmark for duplicate map-based style/class bindings (#33608)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+17.sha-0ca2a39.with-local-changes
+ * @license Angular v9.0.0-rc.1+18.sha-71b400e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19374,7 +19374,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+17.sha-0ca2a39.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+18.sha-71b400e.with-local-changes');
 
 /**
  * @license
