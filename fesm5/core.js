2b588185d5d42bab02a163738fe8766009048b53
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:49:12 +0000

    81828ae7f4 fix(ngcc): add reexports only once (#33658)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+14.sha-1aa3053.with-local-changes
+ * @license Angular v9.0.0-rc.1+15.sha-81828ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19343,7 +19343,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+14.sha-1aa3053.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+15.sha-81828ae.with-local-changes');
 
 /**
  * @license
