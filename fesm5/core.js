c4799c225865211e248464d5e9c80a2cbd57760e
Kara Erickson <karakara@google.com>
2019-11-11 18:04:54 +0000

    39712bcdb2 test(ivy): get ViewRef.rootNodes should get all root nodes from projectable nodes (#33647)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+41.sha-b197e90.with-local-changes
+ * @license Angular v9.0.0-rc.1+37.sha-39712bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19403,7 +19403,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+41.sha-b197e90.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+37.sha-39712bc.with-local-changes');
 
 /**
  * @license
