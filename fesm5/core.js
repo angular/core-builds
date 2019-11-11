db5125071b6dde5623cd574e7a3e4d3c5d622235
Kara Erickson <karakara@google.com>
2019-11-11 18:13:31 +0000

    a8f9ff52f3 build: add @angular/localize to ng-update packageGroup (#33721)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+44.sha-a1e9956.with-local-changes
+ * @license Angular v9.0.0-rc.1+45.sha-a8f9ff5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19403,7 +19403,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+44.sha-a1e9956.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+45.sha-a8f9ff5.with-local-changes');
 
 /**
  * @license
