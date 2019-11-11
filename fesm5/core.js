be369a3761870e074a946a67657b4b8c7996d0aa
Kara Erickson <karakara@google.com>
2019-11-11 21:06:46 +0000

    c6ebcd1eb9 docs: fix numbering (#33736)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+47.sha-eece138.with-local-changes
+ * @license Angular v9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19372,7 +19372,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+47.sha-eece138.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes');
 
 /**
  * @license
