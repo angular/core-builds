9a845a12d54a2b1dd3d28e9d144b4e1cd4165702
Kara Erickson <karakara@google.com>
2019-11-12 18:24:42 +0000

    f1b0547f0a revert: fix(ivy): R3TestBed should clean up registered modules after each test (#32872) (#33663)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+58.sha-fcde671.with-local-changes
+ * @license Angular v9.0.0-rc.1+61.sha-f1b0547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1537,6 +1537,7 @@
         }
         return def;
     }
+    var autoRegisterModuleById = {};
     /**
      * @codeGenApi
      */
@@ -1551,6 +1552,9 @@
             schemas: def.schemas || null,
             id: def.id || null,
         };
+        if (def.id != null) {
+            noSideEffects(function () { autoRegisterModuleById[def.id] = def.type; });
+        }
         return res;
     }
     /**
@@ -19611,7 +19615,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+58.sha-fcde671.with-local-changes');
+    var VERSION = new Version('9.0.0-rc.1+61.sha-f1b0547.with-local-changes');
 
     /**
      * @license
@@ -24125,17 +24129,11 @@
             imports.forEach(function (i) { return registerNgModuleType(i); });
         }
     }
-    function clearRegisteredModuleState() {
+    function clearModulesForTest() {
         modules.clear();
     }
-    function getRegisteredModulesState() {
-        return new Map(modules);
-    }
-    function restoreRegisteredModulesState(moduleMap) {
-        modules = new Map(moduleMap);
-    }
     function getRegisteredNgModuleType(id) {
-        return modules.get(id);
+        return modules.get(id) || autoRegisterModuleById[id];
     }
 
     /**
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+58.sha-fcde671.with-local-changes
+ * @license Angular v9.0.0-rc.1+61.sha-f1b0547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1369,6 +1369,7 @@ function extractPipeDef(type) {
     }
     return def;
 }
+var autoRegisterModuleById = {};
 /**
  * @codeGenApi
  */
@@ -1383,6 +1384,9 @@ function ɵɵdefineNgModule(def) {
         schemas: def.schemas || null,
         id: def.id || null,
     };
+    if (def.id != null) {
+        noSideEffects(function () { autoRegisterModuleById[def.id] = def.type; });
+    }
     return res;
 }
 /**
@@ -19458,7 +19462,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+58.sha-fcde671.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+61.sha-f1b0547.with-local-changes');
 
 /**
  * @license
@@ -23976,17 +23980,11 @@ function registerNgModuleType(ngModuleType) {
         imports.forEach(function (i) { return registerNgModuleType(i); });
     }
 }
-function clearRegisteredModuleState() {
+function clearModulesForTest() {
     modules.clear();
 }
-function getRegisteredModulesState() {
-    return new Map(modules);
-}
-function restoreRegisteredModulesState(moduleMap) {
-    modules = new Map(moduleMap);
-}
 function getRegisteredNgModuleType(id) {
-    return modules.get(id);
+    return modules.get(id) || autoRegisterModuleById[id];
 }
 
 /**
