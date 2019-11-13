58d2778249d4dfcd389e7c2d6ee96f752a0f211e
Kara Erickson <karakara@google.com>
2019-11-13 19:30:24 +0000

    1e1e242570 fix(ngcc): support minified ES5 scenarios (#33777)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+79.sha-cf10b33.with-local-changes
+ * @license Angular v9.0.0-rc.1+86.sha-1e1e242.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19518,7 +19518,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+79.sha-cf10b33.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+86.sha-1e1e242.with-local-changes');
 
 /**
  * @license
