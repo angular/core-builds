e9780f6ce23dfeca20bdfba2c4c4d8f80bdbb32e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-18 17:35:24 +0000

    1218ce4f5f docs: add xrefs to update app and deprecations policy (#33767)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+45.sha-7567f2b.with-local-changes
+ * @license Angular v9.0.0-rc.2+61.sha-1218ce4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28206,7 +28206,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.2+45.sha-7567f2b.with-local-changes');
+const VERSION = new Version('9.0.0-rc.2+61.sha-1218ce4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
