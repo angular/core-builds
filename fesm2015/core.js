74b4d688520f58395865c366622123142f151041
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 20:15:56 +0000

    595375f7a3 fix(ivy): don't infer template context types when in full mode (#33537)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+92.sha-a7e2327.with-local-changes
+ * @license Angular v9.0.0-rc.2+96.sha-595375f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28250,7 +28250,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.2+92.sha-a7e2327.with-local-changes');
+const VERSION = new Version('9.0.0-rc.2+96.sha-595375f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
