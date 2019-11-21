73f590f09533e96dcc8fe45672e19e86535ee388
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 20:09:23 +0000

    acd8734937 fix(service-worker): throw when using the unsupported `versionedFiles` option in config (#33903)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes
+ * @license Angular v9.0.0-rc.1+212.sha-acd8734.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28254,7 +28254,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+212.sha-acd8734.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes
+ * @license Angular v9.0.0-rc.1+212.sha-acd8734.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19589,7 +19589,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+212.sha-acd8734.with-local-changes');
 
 /**
  * @license
