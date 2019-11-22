7fe879e5f951b648c2ae7e4fdee6c3f9592df342
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 01:05:38 +0000

    7d579b913c build: create bazel define flag, `angular_ivy_enabled`, to build using ivy (#33975)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+217.sha-3d69693.with-local-changes
+ * @license Angular v9.0.0-rc.1+218.sha-7d579b9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19741,7 +19741,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+217.sha-3d69693.with-local-changes');
+    var VERSION = new Version('9.0.0-rc.1+218.sha-7d579b9.with-local-changes');
 
     /**
      * @license
