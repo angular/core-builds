93e7d8d4c5e26c9b849c058b32f1ee42124219d8
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 18:43:12 +0000

    dca6ad87fe fix(docs-infra): update manifest short_name (#30194)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+229.sha-621b659.with-local-changes
+ * @license Angular v9.0.0-rc.1+231.sha-dca6ad8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19710,7 +19710,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+229.sha-621b659.with-local-changes');
+    var VERSION = new Version('9.0.0-rc.1+231.sha-dca6ad8.with-local-changes');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+229.sha-621b659.with-local-changes
+ * @license Angular v9.0.0-rc.1+231.sha-dca6ad8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19557,7 +19557,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+229.sha-621b659.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+231.sha-dca6ad8.with-local-changes');
 
 /**
  * @license
