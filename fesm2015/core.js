a8a778e1f1eae6d4635a1d6e8452b8941158997a
Matias Niemelä <matias@yearofmoo.com>
2019-11-23 00:00:21 +0000

    7b77ead88f ci: add JoostK to @angular/fw-compiler (#33994)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+52.sha-9248dba.with-local-changes
+ * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28365,7 +28365,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.3+52.sha-9248dba.with-local-changes');
+const VERSION = new Version('9.0.0-rc.3+55.sha-7b77ead.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+52.sha-9248dba.with-local-changes
+ * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19584,7 +19584,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.3+52.sha-9248dba.with-local-changes');
+var VERSION = new Version('9.0.0-rc.3+55.sha-7b77ead.with-local-changes');
 
 /**
  * @license
