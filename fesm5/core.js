5cff8a61941ee54cb755ddc829df427f956c2147
Miško Hevery <misko@hevery.com>
2019-11-27 21:28:13 +0000

    6108962291 docs: add ngIndia to event page (#33656)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+301.sha-85b551a.with-local-changes
+ * @license Angular v9.0.0-rc.1+302.sha-6108962.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19579,7 +19579,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+301.sha-85b551a.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+302.sha-6108962.with-local-changes');
 
 /**
  * @license
