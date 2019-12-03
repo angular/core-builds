fe6da9e2d28791c8ff431295461d180135edd644
Miško Hevery <misko@hevery.com>
2019-12-03 00:50:54 +0000

    716fc845f5 fix(bazel): don't rely on @angular/core being as a depedency to install @angular/bazel (#34181)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.4.with-local-changes
+ * @license Angular v9.0.0-rc.4+28.sha-716fc84.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19606,7 +19606,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.4.with-local-changes');
+var VERSION = new Version('9.0.0-rc.4+28.sha-716fc84.with-local-changes');
 
 /**
  * @license
