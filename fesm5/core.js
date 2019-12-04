3c5d4787d731be0e97b3a8fb0b661ce76b2fdf99
Miško Hevery <misko@hevery.com>
2019-12-04 19:38:56 +0000

    848018f5d3 release: cut the v9.0.0-rc.5 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.4+67.sha-9555731.with-local-changes
+ * @license Angular v9.0.0-rc.5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19608,7 +19608,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.4+67.sha-9555731.with-local-changes');
+var VERSION = new Version('9.0.0-rc.5.with-local-changes');
 
 /**
  * @license
