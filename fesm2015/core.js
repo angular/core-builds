d22bf1f72da6a52dbcb0794236759a798aa77965
Miško Hevery <misko@hevery.com>
2019-12-04 00:48:15 +0000

    60b13d9948 fix(ivy): support ICUs with pipes (#34198)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+361.sha-a295255.with-local-changes
+ * @license Angular v9.0.0-rc.1+363.sha-60b13d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17433,7 +17433,7 @@ function createViewRef(tNode, lView, isPipe) {
         return new ViewRef(componentView, componentView);
     }
     else if (tNode.type === 3 /* Element */ || tNode.type === 0 /* Container */ ||
-        tNode.type === 4 /* ElementContainer */) {
+        tNode.type === 4 /* ElementContainer */ || tNode.type === 5 /* IcuContainer */) {
         // The LView represents the location where the injection is requested from.
         // We need to locate the containing LView (in case where the `lView` is an embedded view)
         /** @type {?} */
@@ -28395,7 +28395,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+361.sha-a295255.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+363.sha-60b13d9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
