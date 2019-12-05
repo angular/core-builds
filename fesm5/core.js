c261e288c7cbad53b2f67dd2bbf2a5dcf17b258c
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:43:55 +0000

    023c9bebe5 ci: update saucelabs to use angular-framework account (#34233)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+378.sha-37bb901.with-local-changes
+ * @license Angular v9.0.0-rc.1+381.sha-023c9be.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19612,7 +19612,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+378.sha-37bb901.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+381.sha-023c9be.with-local-changes');
 
 /**
  * @license
