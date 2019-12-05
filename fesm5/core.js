9eba4e955acb0734026d6a58c2483f7a01d29ec8
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:22:35 +0000

    421dba0184 fix(docs-infra): make header full-width and panels full-width on mobile screens (#34188)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+382.sha-0213417.with-local-changes
+ * @license Angular v9.0.0-rc.1+384.sha-421dba0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19612,7 +19612,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+382.sha-0213417.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+384.sha-421dba0.with-local-changes');
 
 /**
  * @license
