bf2052bc273f1b72ec5c64f699279dfa103193b3
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:30:47 +0000

    c0869ecfb3 fix(docs-infra): add deprecated-api-item class to remaining deprecated items (#34192)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+373.sha-28ef1af.with-local-changes
+ * @license Angular v9.0.0-rc.1+374.sha-c0869ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19612,7 +19612,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+373.sha-28ef1af.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+374.sha-c0869ec.with-local-changes');
 
 /**
  * @license
