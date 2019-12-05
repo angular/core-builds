8ea324adcc954f2a82b5b800cf326c1c7bf40b21
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:30:46 +0000

    0d95c08cda perf: rename index to index_aot (#34258)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+386.sha-d6f2788.with-local-changes
+ * @license Angular v9.0.0-rc.1+387.sha-0d95c08.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19612,7 +19612,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+386.sha-d6f2788.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+387.sha-0d95c08.with-local-changes');
 
 /**
  * @license
