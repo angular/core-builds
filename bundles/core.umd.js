b2f354198f61bf8ff38fdd1743cfe92579be1748
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:28:01 +0000

    a12b5f930a fix(compiler): ensure localized strings are ES5 compatible for JIT mode (#34265)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+400.sha-b342a69.with-local-changes
+ * @license Angular v9.0.0-rc.1+401.sha-a12b5f9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19736,7 +19736,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+400.sha-b342a69.with-local-changes');
+    var VERSION = new Version('9.0.0-rc.1+401.sha-a12b5f9.with-local-changes');
 
     /**
      * @license
