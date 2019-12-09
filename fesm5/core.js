2566102c6ec211e5395fe1a12bcdf50d79fc43fb
Andrew Kushnir <akushnir@google.com>
2019-12-09 20:35:03 +0000

    60051ebb59 fix(compiler): switch to modern diagnostic formatting (#34234)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+35.sha-1347064.with-local-changes
+ * @license Angular v9.0.0-rc.5+37.sha-60051eb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19612,7 +19612,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.5+35.sha-1347064.with-local-changes');
+var VERSION = new Version('9.0.0-rc.5+37.sha-60051eb.with-local-changes');
 
 /**
  * @license
