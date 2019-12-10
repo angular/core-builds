08310b5175f8ec2205cadf7c37adac99846c6531
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:36:42 +0000

    659356a97b build(docs-infra): upgrade cli command docs sources to 94d07909c (#34325)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+413.sha-f95e2c7.with-local-changes
+ * @license Angular v9.0.0-rc.1+412.sha-659356a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19637,7 +19637,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+413.sha-f95e2c7.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+412.sha-659356a.with-local-changes');
 
 /**
  * @license
