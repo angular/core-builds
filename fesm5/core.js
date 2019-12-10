e7746e8013e0b53731d29df23558ef0bb75536ab
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:36:57 +0000

    7c2bb37e4e refactor(docs-infra): give more descriptive variable name (#34284)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+40.sha-24d1f9e.with-local-changes
+ * @license Angular v9.0.0-rc.5+44.sha-7c2bb37.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19612,7 +19612,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.5+40.sha-24d1f9e.with-local-changes');
+var VERSION = new Version('9.0.0-rc.5+44.sha-7c2bb37.with-local-changes');
 
 /**
  * @license
