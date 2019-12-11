48edf420914488405d9adb4f8955662dc8b77609
Andrew Kushnir <akushnir@google.com>
2019-12-11 18:13:29 +0000

    08ea38f197 fix(bazel): improve performance of tsHost.writeFile() (#34331)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+425.sha-b72c7a8.with-local-changes
+ * @license Angular v9.0.0-rc.1+428.sha-08ea38f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+425.sha-b72c7a8.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+428.sha-08ea38f.with-local-changes');
 
 /**
  * @license
