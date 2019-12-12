c33eac3bbee10ecf3bffc4974cabfbf8d4c9abf8
Kara Erickson <karakara@google.com>
2019-12-12 22:28:14 +0000

    ff0a91422a perf(compiler): speed up i18n digest computations (#34332)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes
+ * @license Angular v9.0.0-rc.1+457.sha-ff0a914.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19610,7 +19610,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+457.sha-ff0a914.with-local-changes');
 
 /**
  * @license
