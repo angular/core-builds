e89a55afc646e1ac0220e6a36a549f3a21d9fac0
Kara Erickson <karakara@google.com>
2019-12-12 00:17:22 +0000

    ad987021ce Revert "build: update integration/bazel to rules_nodejs 0.42.1" (#34360)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+435.sha-6ce9403.with-local-changes
+ * @license Angular v9.0.0-rc.1+442.sha-ad98702.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19794,7 +19794,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+435.sha-6ce9403.with-local-changes');
+    var VERSION = new Version('9.0.0-rc.1+442.sha-ad98702.with-local-changes');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+435.sha-6ce9403.with-local-changes
+ * @license Angular v9.0.0-rc.1+442.sha-ad98702.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19641,7 +19641,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+435.sha-6ce9403.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+442.sha-ad98702.with-local-changes');
 
 /**
  * @license
