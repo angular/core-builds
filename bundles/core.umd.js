138972a38cbcc02219cef85ede1fad6f1cd4be7e
Kara Erickson <karakara@google.com>
2019-12-12 17:28:17 +0000

    05c1398b4d fix(ngcc): render UMD imports even if no prior imports (#34353)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+442.sha-ad98702.with-local-changes
+ * @license Angular v9.0.0-rc.1+444.sha-05c1398.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19794,7 +19794,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+442.sha-ad98702.with-local-changes');
+    var VERSION = new Version('9.0.0-rc.1+444.sha-05c1398.with-local-changes');
 
     /**
      * @license
