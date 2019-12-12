afe0c1e00284430e2b6139b343dee56fd6647db9
Kara Erickson <karakara@google.com>
2019-12-12 17:28:18 +0000

    c26738df12 fix(ngcc): render UMD imports even if no prior imports (#34353)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+13.sha-de71f91.with-local-changes
+ * @license Angular v9.0.0-rc.6+15.sha-c26738d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19641,7 +19641,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.6+13.sha-de71f91.with-local-changes');
+var VERSION = new Version('9.0.0-rc.6+15.sha-c26738d.with-local-changes');
 
 /**
  * @license
