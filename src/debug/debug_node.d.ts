37f1af1f33dce9f2b6bc300d5c9877c97dfc4d3e
Kara Erickson <karakara@google.com>
2019-12-13 19:13:22 +0000

    ea57587b7c docs(ivy): document breaking changes for DebugElement classes and attributes (#34328)

--- src/debug/debug_node.d.ts
+++ src/debug/debug_node.d.ts
@@ -138,7 +138,6 @@ declare class DebugElement__POST_R3__ extends DebugNode__POST_R3__ implements De
     readonly styles: {
         [key: string]: string | null;
     };
-    private _classesProxy;
     readonly classes: {
         [key: string]: boolean;
     };
