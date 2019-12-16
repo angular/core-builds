cc231ded77810c242a38fec45574a50bc3adf89c
Kara Erickson <karakara@google.com>
2019-12-16 16:32:53 +0000

    ddc229b69b fix(ivy): record correct absolute source span for ngForOf expressions (#31813)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+480.sha-12444a8
+ * @license Angular v9.0.0-rc.1+481.sha-ddc229b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19633,7 +19633,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+480.sha-12444a8');
+var VERSION = new Version('9.0.0-rc.1+481.sha-ddc229b');
 
 /**
  * @license
