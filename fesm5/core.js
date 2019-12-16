ed3438435150695d6d54065fbdd6990dd2ce7bed
Kara Erickson <karakara@google.com>
2019-12-16 22:32:01 +0000

    5df8a3ba95 fix(language-service): HTML path should include last node before cursor (#34440)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+488.sha-28b4f4a
+ * @license Angular v9.0.0-rc.1+489.sha-5df8a3b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19633,7 +19633,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+488.sha-28b4f4a');
+var VERSION = new Version('9.0.0-rc.1+489.sha-5df8a3b');
 
 /**
  * @license
