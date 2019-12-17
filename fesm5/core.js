c8d7d7c652ba62de5a46215c45459d3cd0ea18aa
Kara Erickson <karakara@google.com>
2019-12-17 22:59:42 +0000

    7938ff34b1 refactor(compiler-cli): avoid unnecessarily calling `getSourceFile()` twice in `PartialEvaluator` (#34441)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+496.sha-b637b93
+ * @license Angular v9.0.0-rc.1+497.sha-7938ff3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19633,7 +19633,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+496.sha-b637b93');
+var VERSION = new Version('9.0.0-rc.1+497.sha-7938ff3');
 
 /**
  * @license
