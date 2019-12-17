b0e323aed408aefc99cd2d11cdae798451d531df
Kara Erickson <karakara@google.com>
2019-12-17 21:53:24 +0000

    b637b9322e ci(docs-infra): fix failure in `aio_monitoring_stable` due to yarn version mismatch (#34451)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+490.sha-5cecd97
+ * @license Angular v9.0.0-rc.1+496.sha-b637b93
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19633,7 +19633,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+490.sha-5cecd97');
+var VERSION = new Version('9.0.0-rc.1+496.sha-b637b93');
 
 /**
  * @license
