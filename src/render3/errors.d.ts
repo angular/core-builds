bc49dd5105326773782d02da64a1eac0fc11c560
Kara Erickson <karakara@google.com>
2019-12-18 17:34:40 +0000

    1eae7c81e9 refactor(language-service): Append missing AttrAst to AstPath (#34459)

--- src/render3/errors.d.ts
+++ src/render3/errors.d.ts
@@ -7,11 +7,25 @@
  */
 import { InjectorType } from '../di/interface/defs';
 import { TNode } from './interfaces/node';
+import { LView } from './interfaces/view';
 /** Called when directives inject each other (creating a circular dependency) */
 export declare function throwCyclicDependencyError(token: any): never;
 /** Called when there are multiple component selectors that match a given node */
 export declare function throwMultipleComponentError(tNode: TNode): never;
-/** Throws an ExpressionChangedAfterChecked error if checkNoChanges mode is on. */
-export declare function throwErrorIfNoChangesMode(creationMode: boolean, oldValue: any, currValue: any): never | void;
 export declare function throwMixedMultiProviderError(): void;
 export declare function throwInvalidProviderError(ngModuleType?: InjectorType<any>, providers?: any[], provider?: any): void;
+/** Throws an ExpressionChangedAfterChecked error if checkNoChanges mode is on. */
+export declare function throwErrorIfNoChangesMode(creationMode: boolean, oldValue: any, currValue: any, propName?: string): never | void;
+/**
+ * Constructs an object that contains details for the ExpressionChangedAfterItHasBeenCheckedError:
+ * - property name (for property bindings or interpolations)
+ * - old and new values, enriched using information from metadata
+ *
+ * More information on the metadata storage format can be found in `storePropertyBindingMetadata`
+ * function description.
+ */
+export declare function getExpressionChangedErrorDetails(lView: LView, bindingIndex: number, oldValue: any, newValue: any): {
+    propName?: string;
+    oldValue: any;
+    newValue: any;
+};
