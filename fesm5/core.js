dfa862e42d93874b389ca04848077b487af2ddd7
Kara Erickson <karakara@google.com>
2019-12-18 17:33:35 +0000

    a04f7c0d5f fix(language-service): Proper completions for properties and events (#34445)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+501.sha-1eae7c8
+ * @license Angular v9.0.0-rc.1+500.sha-a04f7c0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19690,7 +19690,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+501.sha-1eae7c8');
+var VERSION = new Version('9.0.0-rc.1+500.sha-a04f7c0');
 
 /**
  * @license
