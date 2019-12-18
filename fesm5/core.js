44b37fae5877eb6f40322d6fff926e4afa0832c6
Kara Erickson <karakara@google.com>
2019-12-18 18:42:39 +0000

    dfecca29da refactor: TypeScript 3.7 fixes. (#34372)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+499.sha-9d1175e
+ * @license Angular v9.0.0-rc.1+502.sha-dfecca2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19690,7 +19690,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+499.sha-9d1175e');
+var VERSION = new Version('9.0.0-rc.1+502.sha-dfecca2');
 
 /**
  * @license
