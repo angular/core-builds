b85499af087cbb28f8e91aefdf0aa26637c42be2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-19 19:53:27 +0000

    ca8b584097 fix(language-service): completions after "let x of |" in ngFor (#34473)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.7
+ * @license Angular v9.0.0-rc.7+2.sha-ca8b584
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19843,7 +19843,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.7');
+    var VERSION = new Version('9.0.0-rc.7+2.sha-ca8b584');
 
     /**
      * @license
