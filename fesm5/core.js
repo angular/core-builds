7660a3634a68cd015a4ea201515c1a9e9bec3b7e
George Kalpakas <kalpakas.g@gmail.com>
2019-12-29 10:23:31 +0000

    0fae3d0395 build(docs-infra): bump handlebars from 4.0.12 to 4.5.3 in /aio/tools/examples/shared

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-1aed00d
+ * @license Angular v9.0.0-rc.1+522.sha-0fae3d0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19690,7 +19690,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+522.sha-1aed00d');
+var VERSION = new Version('9.0.0-rc.1+522.sha-0fae3d0');
 
 /**
  * @license
