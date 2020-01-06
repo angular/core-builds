d21da1c7b88e97f522c7f293ab1f8dc8257f81b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:31:44 +0000

    19944c2424 feat(compiler): record end of expression Token (#33549)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-8410278
+ * @license Angular v9.0.0-rc.1+523.sha-19944c2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19659,7 +19659,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+522.sha-8410278');
+var VERSION = new Version('9.0.0-rc.1+523.sha-19944c2');
 
 /**
  * @license
