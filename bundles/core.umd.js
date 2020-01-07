dc201e6934fdd4b59a9cd662951daca370b230c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 19:20:39 +0000

    b9b8920227 docs: typo in Deployment section of getting started (#34490)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+535.sha-0254cba
+ * @license Angular v9.0.0-rc.1+537.sha-b9b8920
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19851,7 +19851,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+535.sha-0254cba');
+    var VERSION = new Version('9.0.0-rc.1+537.sha-b9b8920');
 
     /**
      * @license
