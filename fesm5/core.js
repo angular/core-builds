94e45acaec3dcfafe02ec13acfe0ce9ba797bd3e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 22:48:35 +0000

    186d310f6b release: cut the v9.0.0-rc.8 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.7+38.sha-10acac0
+ * @license Angular v9.0.0-rc.7+39.sha-186d310
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19698,7 +19698,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.7+38.sha-10acac0');
+var VERSION = new Version('9.0.0-rc.7+39.sha-186d310');
 
 /**
  * @license
