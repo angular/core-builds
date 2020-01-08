be024900d03bcddf52b6b8224dd434b8085a6ae6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 21:44:11 +0000

    39bf104a03 build: update `firebase-tools` to the latest version (#34682)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+555.sha-b3398ee
+ * @license Angular v9.0.0-rc.1+556.sha-39bf104
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19667,7 +19667,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+555.sha-b3398ee');
+var VERSION = new Version('9.0.0-rc.1+556.sha-39bf104');
 
 /**
  * @license
