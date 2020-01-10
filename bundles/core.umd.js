ccbc1a0a78ea78a968715f5ffdaf5521f0878077
atscott <atscott@google.com>
2020-01-10 00:30:05 +0000

    f312848c2e refactor(core): remove duplicate InjectableType declaration (#34679)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+29.sha-f7a2ed7
+ * @license Angular v9.0.0-rc.8+30.sha-f312848
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19879,7 +19879,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.8+29.sha-f7a2ed7');
+    var VERSION = new Version('9.0.0-rc.8+30.sha-f312848');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+29.sha-f7a2ed7
+ * @license Angular v9.0.0-rc.8+30.sha-f312848
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18300,18 +18300,6 @@ const ɵ0$8 = /**
  */
 const Injectable = makeDecorator('Injectable', undefined, undefined, undefined, (ɵ0$8));
 /**
- * Type representing injectable service.
- *
- * \@publicApi
- * @record
- * @template T
- */
-function InjectableType() { }
-if (false) {
-    /** @type {?} */
-    InjectableType.prototype.ɵprov;
-}
-/**
  * Supports \@Injectable() in JIT mode for Render2.
  * @param {?} injectableType
  * @param {?=} options
@@ -28548,7 +28536,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+29.sha-f7a2ed7');
+const VERSION = new Version('9.0.0-rc.8+30.sha-f312848');
 
 /**
  * @fileoverview added by tsickle
