4d5dffc4e3fc6a7c1236d5fd73da578d5199ecd7
atscott <atscott@google.com>
2020-01-14 23:14:46 +0000

    3113bb7ad2 fix(forms): accept number length in length validators (#32057)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+115.sha-e100bbc
+ * @license Angular v9.0.0-rc.8+116.sha-3113bb7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28635,7 +28635,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+115.sha-e100bbc');
+const VERSION = new Version('9.0.0-rc.8+116.sha-3113bb7');
 
 /**
  * @fileoverview added by tsickle
