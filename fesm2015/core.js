913b1214274548fb31a43eed8c3bcecfd2d1a4c4
Matias Niemelä <matias@yearofmoo.com>
2020-01-15 20:54:36 +0000

    436e7388c3 build: update to rules_nodejs 1.1.0 (#34736)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+692.sha-3d5bcd5
+ * @license Angular v9.0.0-rc.1+710.sha-436e738
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28635,7 +28635,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+692.sha-3d5bcd5');
+const VERSION = new Version('0.0.0');
 
 /**
  * @fileoverview added by tsickle
