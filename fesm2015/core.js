fac956b8ef2d031282976341233ec10244cb2491
GitHub <noreply@github.com>
2020-01-15 21:12:47 +0000

    966cb355db fixup! docs: fix public api tool name

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+691.sha-50e52c8
+ * @license Angular v9.0.0-rc.1+692.sha-966cb35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28635,7 +28635,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+691.sha-50e52c8');
+const VERSION = new Version('9.0.0-rc.1+692.sha-966cb35');
 
 /**
  * @fileoverview added by tsickle
