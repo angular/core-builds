5c88cfbbd12e4feeb1eab46373484d3283db0ff5
atscott <atscott@google.com>
2020-01-15 00:12:06 +0000

    fa39a8c026 feat(language-service): provide completion for $event variable (#34566)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+118.sha-c4d035e
+ * @license Angular v9.0.0-rc.8+119.sha-fa39a8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28635,7 +28635,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+118.sha-c4d035e');
+const VERSION = new Version('9.0.0-rc.8+119.sha-fa39a8c');
 
 /**
  * @fileoverview added by tsickle
