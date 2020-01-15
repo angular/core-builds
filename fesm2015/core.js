f44c3cf9ed7d2aac9729106e88c9ee652f72dd48
atscott <atscott@google.com>
2020-01-15 18:03:12 +0000

    b7385a77ad docs: release notes for the v9.0.0-rc.9 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+688.sha-bf8ba89
+ * @license Angular v9.0.0-rc.1+689.sha-b7385a7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28618,7 +28618,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+688.sha-bf8ba89');
+const VERSION = new Version('9.0.0-rc.1+689.sha-b7385a7');
 
 /**
  * @fileoverview added by tsickle
