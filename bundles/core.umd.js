b078c2c1fbe3678a356f5d7bb43ce64bc6aba39a
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:51:18 +0000

    a4cbb86360 docs: fix typo on router guide (#34911)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+14.sha-34c30ae
+ * @license Angular v9.0.0-rc.10+15.sha-a4cbb86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19963,7 +19963,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.10+14.sha-34c30ae');
+    var VERSION = new Version('9.0.0-rc.10+15.sha-a4cbb86');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+14.sha-34c30ae
+ * @license Angular v9.0.0-rc.10+15.sha-a4cbb86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28638,7 +28638,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.10+14.sha-34c30ae');
+const VERSION = new Version('9.0.0-rc.10+15.sha-a4cbb86');
 
 /**
  * @fileoverview added by tsickle
