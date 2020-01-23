d5f4abc55d1beade5c87503fba74cb78fe052cf0
Andrew Kushnir <akushnir@google.com>
2020-01-23 19:59:24 +0000

    970b22f98e test: setup circular dependency tests for all entry points (#34774)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+773.sha-0c8d085
+ * @license Angular v9.0.0-rc.1+775.sha-970b22f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19932,7 +19932,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+773.sha-0c8d085');
+    var VERSION = new Version('9.0.0-rc.1+775.sha-970b22f');
 
     /**
      * @license
