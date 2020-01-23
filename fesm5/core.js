a87ee39283acdec0d2cf7c58d48b4a6066b9a89e
Andrew Kushnir <akushnir@google.com>
2020-01-23 19:07:05 +0000

    0c8d085666 fix(ivy): use any for generic context checks when !strictTemplates (#34649)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+767.sha-6b468f9
+ * @license Angular v9.0.0-rc.1+773.sha-0c8d085
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19810,7 +19810,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+767.sha-6b468f9');
+var VERSION = new Version('9.0.0-rc.1+773.sha-0c8d085');
 
 /**
  * @license
