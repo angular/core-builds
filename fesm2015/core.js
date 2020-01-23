b5c62b331238747fd78a334d158fa263cac0f2c1
Andrew Kushnir <akushnir@google.com>
2020-01-23 23:11:51 +0000

    7659f2e24b fix(ngcc): do not attempt compilation when analysis fails (#34889)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+785.sha-5b42084
+ * @license Angular v9.0.0-rc.1+787.sha-7659f2e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28638,7 +28638,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+785.sha-5b42084');
+const VERSION = new Version('9.0.0-rc.1+787.sha-7659f2e');
 
 /**
  * @fileoverview added by tsickle
