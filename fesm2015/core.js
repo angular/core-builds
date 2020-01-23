06b3e7ee3ed52067619ff30882f7a26eab43dfd9
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:37:48 +0000

    93ffc67bfb fix(ngcc): update `package.json` deterministically (#34870)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+764.sha-68025ce
+ * @license Angular v9.0.0-rc.1+766.sha-93ffc67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28638,7 +28638,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+764.sha-68025ce');
+const VERSION = new Version('9.0.0-rc.1+766.sha-93ffc67');
 
 /**
  * @fileoverview added by tsickle
