1c2dd35b7f737dc131c61a512ff1c4273cd96f9e
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:25:35 +0000

    48f8ca5483 build: upgrade to bazel 2.0.0 (#34877)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+761.sha-32b72f3
+ * @license Angular v9.0.0-rc.1+762.sha-48f8ca5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19963,7 +19963,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+761.sha-32b72f3');
+    var VERSION = new Version('9.0.0-rc.1+762.sha-48f8ca5');
 
     /**
      * @license
