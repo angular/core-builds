58cc983ed2138cfc463d2c52fa744c5beebea619
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:35:16 +0000

    68025ce09a test(language-service): recognize inputs/ouputs declared in decorator (#34875)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
+ * @license Angular v9.0.0-rc.1+764.sha-68025ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19779,7 +19779,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+763.sha-1ea04ff');
+var VERSION = new Version('9.0.0-rc.1+764.sha-68025ce');
 
 /**
  * @license
