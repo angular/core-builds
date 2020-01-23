784aa98eac853d7464242de70b82c1fb0504cb99
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:45:07 +0000

    3049878aa9 ci: bump release version of ts-api-guardian (#34933)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+778.sha-055533b
+ * @license Angular v9.0.0-rc.1+779.sha-3049878
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28638,7 +28638,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+778.sha-055533b');
+const VERSION = new Version('9.0.0-rc.1+779.sha-3049878');
 
 /**
  * @fileoverview added by tsickle
