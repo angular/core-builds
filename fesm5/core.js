1b5eb2d07392804ac2d292a4b7993d8d92cd1e5f
Andrew Kushnir <akushnir@google.com>
2020-01-24 17:26:23 +0000

    bca432c60f refactor(ivy): introduce the 'core' package and split apart NgtscProgram (#34887)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+49.sha-c9db7bd
+ * @license Angular v9.0.0-rc.10+51.sha-bca432c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19810,7 +19810,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.10+49.sha-c9db7bd');
+var VERSION = new Version('9.0.0-rc.10+51.sha-bca432c');
 
 /**
  * @license
