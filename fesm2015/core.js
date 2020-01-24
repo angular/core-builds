a289fa1e735f106b3f409aade9a1c0da256eaca2
Andrew Kushnir <akushnir@google.com>
2020-01-24 00:16:34 +0000

    0223aa6121 fix(language-service): Diagnostic span should point to class name (#34932)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+788.sha-b50ed5c
+ * @license Angular v9.0.0-rc.1+789.sha-0223aa6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28638,7 +28638,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+788.sha-b50ed5c');
+const VERSION = new Version('9.0.0-rc.1+789.sha-0223aa6');
 
 /**
  * @fileoverview added by tsickle
