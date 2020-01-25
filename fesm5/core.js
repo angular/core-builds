c7ffaf561d95d9d8aa99e037254f65ec7e5d599e
Igor Minar <igor@angular.io>
2020-01-25 06:47:44 +0000

    bdd3865314 build(docs-infra): update CLI to 9.0.0-rc.10

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+4.sha-8fca9b6
+ * @license Angular v9.0.0-rc.11+5.sha-bdd3865
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19108,7 +19108,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+4.sha-8fca9b6');
+var VERSION = new Version('9.0.0-rc.11+5.sha-bdd3865');
 
 /**
  * @license
