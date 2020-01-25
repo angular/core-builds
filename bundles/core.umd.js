aae4a7214f55700a3867111273e73453d94913e5
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:15:25 +0000

    3414ef276e refactor(language-service): Avoid leaking host outside of LanguageService (#34941)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+807.sha-8926f76
+ * @license Angular v9.0.0-rc.1+808.sha-3414ef2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19230,7 +19230,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+807.sha-8926f76');
+    var VERSION = new Version('9.0.0-rc.1+808.sha-3414ef2');
 
     /**
      * @license
