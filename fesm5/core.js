127d564a6114cbf3d136c8acce9e0d20530e8059
Andrew Kushnir <akushnir@google.com>
2020-01-27 18:01:56 +0000

    b3bd6ca925 build: update API Golden files after node 12 update (#34955)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+815.sha-c349cb2
+ * @license Angular v9.0.0-rc.1+819.sha-b3bd6ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19077,7 +19077,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+815.sha-c349cb2');
+var VERSION = new Version('9.0.0-rc.1+819.sha-b3bd6ca');
 
 /**
  * @license
