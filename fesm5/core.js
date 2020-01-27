7133562a0bbc4ad86d6cd95be8dedb678bebb387
Andrew Kushnir <akushnir@google.com>
2020-01-27 17:53:05 +0000

    c349cb2d16 build: update CLI to 9.0.0-rc.10 (#34965)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+817.sha-724ce8c
+ * @license Angular v9.0.0-rc.1+815.sha-c349cb2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19077,7 +19077,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+817.sha-724ce8c');
+var VERSION = new Version('9.0.0-rc.1+815.sha-c349cb2');
 
 /**
  * @license
