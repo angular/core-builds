2e8a620ebf4efd89b177543ba6cac6e5743bc07a
Andrew Kushnir <akushnir@google.com>
2020-01-28 17:28:10 +0000

    e7c74cbd69 feat(language-service): completions for output $event properties in (#34570)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+830.sha-90b303f
+ * @license Angular v9.0.0-rc.1+832.sha-e7c74cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27028,7 +27028,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+830.sha-90b303f');
+const VERSION = new Version('9.0.0-rc.1+832.sha-e7c74cb');
 
 /**
  * @fileoverview added by tsickle
