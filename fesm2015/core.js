9e96c2e39986bee11d35cb04265e2f2d60dd6aa4
George Kalpakas <kalpakas.g@gmail.com>
2020-01-29 20:30:56 +0000

    8b88269ae1 docs: update events page

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.14+3.sha-015f833.with-local-changes
+ * @license Angular v8.2.14+6.sha-8b88269.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26488,7 +26488,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.14+3.sha-015f833.with-local-changes');
+const VERSION = new Version('8.2.14+6.sha-8b88269.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
