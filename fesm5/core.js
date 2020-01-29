b3b03cd0ced422e249335b66f01e36f5a8d4d7ed
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:37:32 +0000

    a3de110b52 build(docs-infra): update aio to @angular/cli@9.0.0-rc.11 (#35043)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+861.sha-95383ee
+ * @license Angular v9.0.0-rc.1+859.sha-a3de110
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19080,7 +19080,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+861.sha-95383ee');
+var VERSION = new Version('9.0.0-rc.1+859.sha-a3de110');
 
 /**
  * @license
