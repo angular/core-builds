7ba2a39f622d8445f621f8ea4b1a75868caf2dbe
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:55:23 +0000

    9b7d703b0b ci: don't run benchmark_tests on remote executors (#34996)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+54.sha-7e0a031
+ * @license Angular v9.0.0-rc.11+58.sha-9b7d703
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19264,7 +19264,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.11+54.sha-7e0a031');
+    var VERSION = new Version('9.0.0-rc.11+58.sha-9b7d703');
 
     /**
      * @license
