c8e04b8ae9ad509f363c412239a687a8bc556365
Andrew Kushnir <akushnir@google.com>
2020-01-29 00:11:33 +0000

    bd61cd0267 ci: filter out bad commit from the commit message check to green up master (#35017)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+838.sha-dff4e1e
+ * @license Angular v9.0.0-rc.1+840.sha-bd61cd0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19075,7 +19075,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+838.sha-dff4e1e');
+var VERSION = new Version('9.0.0-rc.1+840.sha-bd61cd0');
 
 /**
  * @license
