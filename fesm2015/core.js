c3f9ce6a4c42a8c4e13a762cf4822849ba5c33ed
Andrew Kushnir <akushnir@google.com>
2020-01-29 21:29:10 +0000

    7e0a031543 docs: remove named node version (#35014)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+53.sha-eaa4a5a
+ * @license Angular v9.0.0-rc.11+54.sha-7e0a031
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27033,7 +27033,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+53.sha-eaa4a5a');
+const VERSION = new Version('9.0.0-rc.11+54.sha-7e0a031');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+53.sha-eaa4a5a
+ * @license Angular v9.0.0-rc.11+54.sha-7e0a031
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19111,7 +19111,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+53.sha-eaa4a5a');
+var VERSION = new Version('9.0.0-rc.11+54.sha-7e0a031');
 
 /**
  * @license
