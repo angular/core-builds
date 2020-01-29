c6830d8bd6916f311eaabb3d1d2f59556ba826f4
Andrew Kushnir <akushnir@google.com>
2020-01-29 19:13:23 +0000

    8fd2a97c44 docs: ng-conf 2020 date change (#35019)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+849.sha-d43187f
+ * @license Angular v9.0.0-rc.1+850.sha-8fd2a97
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27011,7 +27011,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+849.sha-d43187f');
+const VERSION = new Version('9.0.0-rc.1+850.sha-8fd2a97');
 
 /**
  * @fileoverview added by tsickle
