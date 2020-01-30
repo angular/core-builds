6250ef63ae03ee6cedadf5481e0ddc1cd08be2cf
Andrew Kushnir <akushnir@google.com>
2020-01-30 18:44:37 +0000

    d72cfc9d57 docs: remove deployment section on golang because of errors (#34099)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+866.sha-37f39d6
+ * @license Angular v9.0.0-rc.1+867.sha-d72cfc9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27435,7 +27435,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+866.sha-37f39d6');
+const VERSION = new Version('9.0.0-rc.1+867.sha-d72cfc9');
 
 /**
  * @fileoverview added by tsickle
