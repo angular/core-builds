a7356d9b360d2f38570630a7e8dc9bab63a5f1a0
Andrew Kushnir <akushnir@google.com>
2020-01-30 17:28:38 +0000

    3b612afff3 build(compiler-cli): update to chokidar 3.x (#35047)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+62.sha-36b8c03
+ * @license Angular v9.0.0-rc.11+63.sha-3b612af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19382,7 +19382,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+62.sha-36b8c03');
+var VERSION = new Version('9.0.0-rc.11+63.sha-3b612af');
 
 /**
  * @license
