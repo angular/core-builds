85ede904dae668bc9837b628deefc96a2a1e9d3f
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:43:57 +0000

    a25cb2d1e0 docs: release notes for the v9.0.0-rc.12 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+872.sha-374a8ec
+ * @license Angular v9.0.0-rc.1+871.sha-a25cb2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19397,7 +19397,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+872.sha-374a8ec');
+var VERSION = new Version('9.0.0-rc.1+871.sha-a25cb2d');
 
 /**
  * @license
