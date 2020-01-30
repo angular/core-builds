7a1cfdbe365f526faf662d06a668efd3dcbd6a12
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:21:05 +0000

    3bdf1164ce ci: fix StephenFluin's casing in entries in PullApprove (#35062)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+67.sha-36dfca9
+ * @license Angular v9.0.0-rc.12+1.sha-3bdf116
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27462,7 +27462,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+67.sha-36dfca9');
+const VERSION = new Version('9.0.0-rc.12+1.sha-3bdf116');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+67.sha-36dfca9
+ * @license Angular v9.0.0-rc.12+1.sha-3bdf116
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19397,7 +19397,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+67.sha-36dfca9');
+var VERSION = new Version('9.0.0-rc.12+1.sha-3bdf116');
 
 /**
  * @license
