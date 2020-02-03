3369f1c750c7a047509174020260216f9ca84e65
Miško Hevery <misko@hevery.com>
2020-02-03 22:45:03 +0000

    523c785e8f fix(ngcc): correctly invalidate cache when moving/removing files/directories (#35106)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+905.sha-3c30474
+ * @license Angular v9.0.0-rc.1+906.sha-523c785
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27520,7 +27520,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+905.sha-3c30474');
+const VERSION = new Version('9.0.0-rc.1+906.sha-523c785');
 
 /**
  * @fileoverview added by tsickle
