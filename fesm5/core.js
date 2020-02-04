858e67751bcf351283b2cfa825997cd98abd5874
Miško Hevery <misko@hevery.com>
2020-02-04 17:21:23 +0000

    7369dd6f24 docs: fix animations example/remove 1st person (#35046)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+907.sha-128d4ac
+ * @license Angular v9.0.0-rc.1+909.sha-7369dd6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19440,7 +19440,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+907.sha-128d4ac');
+var VERSION = new Version('9.0.0-rc.1+909.sha-7369dd6');
 
 /**
  * @license
