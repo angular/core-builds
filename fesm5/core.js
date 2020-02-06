e90f5662b0a3ada1780a5315a1815c99d5f4536b
Kara Erickson <karakara@google.com>
2020-02-06 22:51:32 +0000

    532ccb647c docs: remove the release schedule from docs (#34474)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+940.sha-ba0cdd3
+ * @license Angular v9.0.0-rc.1+941.sha-532ccb6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19501,7 +19501,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+940.sha-ba0cdd3');
+var VERSION = new Version('9.0.0-rc.1+941.sha-532ccb6');
 
 /**
  * @license
