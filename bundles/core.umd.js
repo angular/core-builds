22fe2b2c5573446841f41051609af9a926dbdb17
Misko Hevery <misko@hevery.com>
2020-02-06 17:55:05 +0000

    c286fbb9dc docs(ivy): add size debugging section (#35178)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+933.sha-7526ef7
+ * @license Angular v9.0.0-rc.1+934.sha-c286fbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19654,7 +19654,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+933.sha-7526ef7');
+    var VERSION = new Version('9.0.0-rc.1+934.sha-c286fbb');
 
     /**
      * @license
