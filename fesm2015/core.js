81335e58ccc3f6771364d7e878d8bf4db7fc96e7
Misko Hevery <misko@hevery.com>
2020-02-06 22:12:30 +0000

    f4b9d664f2 docs(ivy): add a not about compile-time constants to the ivy compatibility guide (#35194)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+5.sha-becab76
+ * @license Angular v9.0.0+7.sha-f4b9d66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27598,7 +27598,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+5.sha-becab76');
+const VERSION = new Version('9.0.0+7.sha-f4b9d66');
 
 /**
  * @fileoverview added by tsickle
