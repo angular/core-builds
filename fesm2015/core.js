d935081c0006055007a3a6af6c171ff2d7e33414
Misko Hevery <misko@hevery.com>
2020-02-06 23:17:08 +0000

    49dd7b6a0a docs: consilidate release notes for the v8.0.0 release (#35197)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+941.sha-532ccb6
+ * @license Angular v9.0.0-rc.1+942.sha-49dd7b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27598,7 +27598,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+941.sha-532ccb6');
+const VERSION = new Version('9.0.0-rc.1+942.sha-49dd7b6');
 
 /**
  * @fileoverview added by tsickle
