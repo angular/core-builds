b59a3e7f7eb76a6e1d1aa9048bc52e775f5bc542
Kara Erickson <karakara@google.com>
2020-02-06 22:50:48 +0000

    ba0cdd3010 feat(docs-infra): add v8 to the version picker in the navbar (#35196)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+939.sha-86db592
+ * @license Angular v9.0.0-rc.1+940.sha-ba0cdd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27598,7 +27598,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+939.sha-86db592');
+const VERSION = new Version('9.0.0-rc.1+940.sha-ba0cdd3');
 
 /**
  * @fileoverview added by tsickle
