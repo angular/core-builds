5fd01fd4cd4683052235a5bb753d47639a095799
Misko Hevery <misko@hevery.com>
2020-02-06 17:56:13 +0000

    722251da40 build: update scripts/release/post-check with the latest versions (#35151)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+934.sha-c286fbb
+ * @license Angular v9.0.0-rc.1+935.sha-722251d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27598,7 +27598,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+934.sha-c286fbb');
+const VERSION = new Version('9.0.0-rc.1+935.sha-722251d');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+934.sha-c286fbb
+ * @license Angular v9.0.0-rc.1+935.sha-722251d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19501,7 +19501,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+934.sha-c286fbb');
+var VERSION = new Version('9.0.0-rc.1+935.sha-722251d');
 
 /**
  * @license
