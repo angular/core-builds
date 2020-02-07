8ed77451b2a28ed20109a86e36bb895f49ace90b
Misko Hevery <misko@hevery.com>
2020-02-07 00:38:51 +0000

    14fc1812f3 ci: re-enable android 7 saucelabs tests (#35171)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+10.sha-60b8870
+ * @license Angular v9.0.0+19.sha-14fc181
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19654,7 +19654,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0+10.sha-60b8870');
+    var VERSION = new Version('9.0.0+19.sha-14fc181');
 
     /**
      * @license
