f469a1eae4624c4249fe7e495f46829b0d125e28
Misko Hevery <misko@hevery.com>
2020-02-07 18:24:16 +0000

    79742a397f fix(docs-infra): fix parameters with @Optional() decorator do not match declared, optional type (#35150)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+956.sha-53193c9
+ * @license Angular v9.0.0-rc.1+958.sha-79742a3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19470,7 +19470,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+956.sha-53193c9');
+var VERSION = new Version('9.0.0-rc.1+958.sha-79742a3');
 
 /**
  * @license
