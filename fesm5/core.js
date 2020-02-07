a55b81b0d69b45c77a270517259473ad18bc2dc6
Kara Erickson <karakara@google.com>
2020-02-07 21:27:44 +0000

    5de5b52beb fix(ivy): repeat template guards to narrow types in event handlers (#35193)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+966.sha-daac33c
+ * @license Angular v9.0.0-rc.1+967.sha-5de5b52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19502,7 +19502,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+966.sha-daac33c');
+var VERSION = new Version('9.0.0-rc.1+967.sha-5de5b52');
 
 /**
  * @license
