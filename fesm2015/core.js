b482003fd355cdd4a9976060759ca6426c3146c8
Kara Erickson <karakara@google.com>
2020-02-08 00:38:06 +0000

    18ed9dcd83 refactor(benchpress): delete outdated/unused folder (#35147)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+32.sha-5b5dc81
+ * @license Angular v9.0.0+36.sha-18ed9dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27599,7 +27599,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+32.sha-5b5dc81');
+const VERSION = new Version('9.0.0+36.sha-18ed9dc');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+32.sha-5b5dc81
+ * @license Angular v9.0.0+36.sha-18ed9dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19502,7 +19502,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0+32.sha-5b5dc81');
+var VERSION = new Version('9.0.0+36.sha-18ed9dc');
 
 /**
  * @license
