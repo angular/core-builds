1ad8b53fd05aec94468556d23dddf041665801f8
Kara Erickson <karakara@google.com>
2020-02-10 17:46:43 +0000

    2d5bb26b02 fix(docs-infra): lighthouse reporting links to cross-origin destinations are unsafe (#35253)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+975.sha-13be065
+ * @license Angular v9.0.0-rc.1+976.sha-2d5bb26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19481,7 +19481,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+975.sha-13be065');
+var VERSION = new Version('9.0.0-rc.1+976.sha-2d5bb26');
 
 /**
  * @license
