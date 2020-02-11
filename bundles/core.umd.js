d69efe76c6dbde78617bbe227a162bd1316e2372
Kara Erickson <karakara@google.com>
2020-02-11 21:49:36 +0000

    b9b38f0ad2 docs: Repetition on getting started tutorial (#35290)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+988.sha-afdd405
+ * @license Angular v9.0.0-rc.1+989.sha-b9b38f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19665,7 +19665,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0-rc.1+988.sha-afdd405');
+    var VERSION = new Version('9.0.0-rc.1+989.sha-b9b38f0');
 
     /**
      * @license
