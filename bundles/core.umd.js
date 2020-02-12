fe98fc1111c1732bc7a9e9633d9daba80fbecb4d
Kara Erickson <karakara@google.com>
2020-02-12 01:24:17 +0000

    554c2cbd5c fix(forms): change Array.reduce usage to Array.forEach (#35349)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+47.sha-94d002b
+ * @license Angular v9.0.0+53.sha-554c2cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19665,7 +19665,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.0+47.sha-94d002b');
+    var VERSION = new Version('9.0.0+53.sha-554c2cb');
 
     /**
      * @license
