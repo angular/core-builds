e0bb8ab5e138f65fa667ab689c40e92ec01c2544
Kara Erickson <karakara@google.com>
2020-02-13 18:23:21 +0000

    dc63ceb5a3 docs: update release.md and rename 'beta' releases to 'next' (#35276)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+7.sha-d6bc63f
+ * @license Angular v9.1.0-next.0+8.sha-dc63ceb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19481,7 +19481,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+7.sha-d6bc63f');
+var VERSION = new Version('9.1.0-next.0+8.sha-dc63ceb');
 
 /**
  * @license
