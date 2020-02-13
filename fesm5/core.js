676245b5f9a51433006f80dc421d36f144702c20
Kara Erickson <karakara@google.com>
2020-02-13 18:38:14 +0000

    b2222e7e02 docs: minor typo fix (#35423) PR Close #35423

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+8.sha-dc63ceb
+ * @license Angular v9.1.0-next.0+16.sha-b2222e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19481,7 +19481,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+8.sha-dc63ceb');
+var VERSION = new Version('9.1.0-next.0+16.sha-b2222e7');
 
 /**
  * @license
