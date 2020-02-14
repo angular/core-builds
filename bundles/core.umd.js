e1f43473a91d090c2609c2cc76ab6718a4f9b662
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 00:56:09 +0000

    4f8d30361a build: update to bazel_toolchains 2.1.0 (#35430)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+17.sha-db693f4
+ * @license Angular v9.0.1+20.sha-4f8d303
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19665,7 +19665,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.0.1+17.sha-db693f4');
+    var VERSION = new Version('9.0.1+20.sha-4f8d303');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+17.sha-db693f4
+ * @license Angular v9.0.1+20.sha-4f8d303
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19512,7 +19512,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.1+17.sha-db693f4');
+var VERSION = new Version('9.0.1+20.sha-4f8d303');
 
 /**
  * @license
