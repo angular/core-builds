f912ee850eb0942d0022b834cc36b0d637f89553
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-15 00:04:31 +0000

    d05629be3b build: enable network for docker on remote executors (#35432)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+27.sha-939e2f7
+ * @license Angular v9.1.0-next.0+26.sha-d05629b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19685,7 +19685,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-next.0+27.sha-939e2f7');
+    var VERSION = new Version('9.1.0-next.0+26.sha-d05629b');
 
     /**
      * @license
