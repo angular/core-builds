7ded38ddda9fa0673cf71df5b848013c167f220f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:20:50 +0000

    2f18e8565e style(docs-infra): properly format files (#35318)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+34.sha-c013dd4
+ * @license Angular v9.1.0-next.0+36.sha-2f18e85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27610,7 +27610,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+34.sha-c013dd4');
+const VERSION = new Version('9.1.0-next.0+36.sha-2f18e85');
 
 /**
  * @fileoverview added by tsickle
