00709fcd9302e7db04033ef83f4048e825bb05ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 22:18:23 +0000

    183a8629a4 fix(docs-infra): set `th`/`td` to proper width (#35437)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+36.sha-2f18e85
+ * @license Angular v9.1.0-next.0+37.sha-183a862
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19654,7 +19654,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-next.0+36.sha-2f18e85');
+    var VERSION = new Version('9.1.0-next.0+37.sha-183a862');
 
     /**
      * @license
