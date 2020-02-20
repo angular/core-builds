24fe69847b5f218524884f16f56ded8dde9d61e5
Miško Hevery <misko@hevery.com>
2020-02-20 21:04:37 +0000

    72664cac19 fix(compiler): use FatalDiagnosticError to generate better error messages (#35244)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+3.sha-3a97972
+ * @license Angular v9.0.2+10.sha-72664ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -27689,7 +27689,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.2+3.sha-3a97972');
+const VERSION = new Version('9.0.2+10.sha-72664ca');
 
 /**
  * @fileoverview added by tsickle
