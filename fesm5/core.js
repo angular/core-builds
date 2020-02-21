664e3ad3341206be66fe4542e1f1434262913983
Miško Hevery <misko@hevery.com>
2020-02-21 21:02:20 +0000

    049f118c1b fix(language-service): provide hover for interpolation in attribute value (#35494)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+27.sha-54fd33f
+ * @license Angular v9.1.0-next.1+28.sha-049f118
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19869,7 +19869,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+27.sha-54fd33f');
+var VERSION = new Version('9.1.0-next.1+28.sha-049f118');
 
 /**
  * @license
