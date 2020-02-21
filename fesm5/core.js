ebdd0ca4513b211f4e3438a5270b08b3969ccb37
Miško Hevery <misko@hevery.com>
2020-02-21 22:49:17 +0000

    e084835fb1 Revert "feat: support passive event options by defining global variables in zone.js config file (#34503)"

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+31.sha-e17bde9
+ * @license Angular v9.1.0-next.1+32.sha-e084835
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19869,7 +19869,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+31.sha-e17bde9');
+var VERSION = new Version('9.1.0-next.1+32.sha-e084835');
 
 /**
  * @license
