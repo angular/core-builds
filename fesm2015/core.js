3c7394cef506532e364e54e2cd6280ab8b12486a
Miško Hevery <misko@hevery.com>
2020-02-21 17:38:11 +0000

    54fd33fb80 fix(language-service): infer context type of structural directives (#35537) (#35561)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+22.sha-8cb1f65
+ * @license Angular v9.1.0-next.1+27.sha-54fd33f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28010,7 +28010,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+22.sha-8cb1f65');
+const VERSION = new Version('9.1.0-next.1+27.sha-54fd33f');
 
 /**
  * @fileoverview added by tsickle
