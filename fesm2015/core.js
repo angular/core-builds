f921f535ad0a7ed393de88b2bef4e46dfc4e5fb8
Miško Hevery <misko@hevery.com>
2020-02-24 17:33:05 +0000

    3893799183 docs: add amazon s3 builder package (#34783)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+44.sha-53f059e
+ * @license Angular v9.1.0-next.1+46.sha-3893799
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28027,7 +28027,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+44.sha-53f059e');
+const VERSION = new Version('9.1.0-next.1+46.sha-3893799');
 
 /**
  * @fileoverview added by tsickle
