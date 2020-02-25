3f65579053a909c9e060fdaab9944d98d3c49535
Miško Hevery <misko@hevery.com>
2020-02-25 01:52:23 +0000

    f9d483e76e feat: add a temp solution to support passive event listeners. (#34503)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+53.sha-af76651
+ * @license Angular v9.1.0-next.1+54.sha-f9d483e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19869,7 +19869,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+53.sha-af76651');
+var VERSION = new Version('9.1.0-next.1+54.sha-f9d483e');
 
 /**
  * @license
