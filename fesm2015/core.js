e7a9b1f54a0ac2467f1d2b803eab7e3e0b074120
Miško Hevery <misko@hevery.com>
2020-02-25 21:40:31 +0000

    8e354dae00 fix(language-service): get the right 'ElementAst' in the nested HTML tag (#35317)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+61.sha-1e20b2c
+ * @license Angular v9.1.0-next.1+59.sha-8e354da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28010,7 +28010,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+61.sha-1e20b2c');
+const VERSION = new Version('9.1.0-next.1+59.sha-8e354da');
 
 /**
  * @fileoverview added by tsickle
