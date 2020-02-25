06c07b90dfba7bc8aa767504c63db1b6ffb48883
Miško Hevery <misko@hevery.com>
2020-02-25 01:50:08 +0000

    af76651ccc refactor: update tscplugin api to match google3 (#35455)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+47.sha-737ec52
+ * @license Angular v9.1.0-next.1+53.sha-af76651
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20053,7 +20053,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-next.1+47.sha-737ec52');
+    var VERSION = new Version('9.1.0-next.1+53.sha-af76651');
 
     /**
      * @license
