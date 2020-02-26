22b1aacbbe156e59dc8807701ed7550c6d2b2b8a
Miško Hevery <misko@hevery.com>
2020-02-26 21:04:05 +0000

    6f68eae9dd docs: more precise docs for template statement syntax (#34348)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+63.sha-bde4cd7
+ * @license Angular v9.1.0-next.1+64.sha-6f68eae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28010,7 +28010,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+63.sha-bde4cd7');
+const VERSION = new Version('9.1.0-next.1+64.sha-6f68eae');
 
 /**
  * @fileoverview added by tsickle
