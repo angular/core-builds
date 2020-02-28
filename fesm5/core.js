bae276c93b93b8420f51f8dbe1935d6ea2762e69
Joey Perrott <josephperrott.github@gmail.com>
2020-02-28 20:32:26 +0000

    3b32bc7b41 ci: move public-api goldens to goldens directory

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+22.sha-7bfcc35
+ * @license Angular v9.1.0-next.2+22.sha-3b32bc7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19903,7 +19903,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+22.sha-7bfcc35');
+var VERSION = new Version('9.1.0-next.2+22.sha-3b32bc7');
 
 /**
  * @license
