cd5d3468bf6c5faa50bd121f610510adbfd73b14
atscott <atscott@google.com>
2020-03-02 17:53:19 +0000

    44e47da4cf docs: reword explanation for using singleton services decorative to be more understandable (#35002)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+35.sha-a153b61
+ * @license Angular v9.1.0-next.2+36.sha-44e47da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19867,7 +19867,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+35.sha-a153b61');
+var VERSION = new Version('9.1.0-next.2+36.sha-44e47da');
 
 /**
  * @license
