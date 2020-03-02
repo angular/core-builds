53a38b7d87d97a389832bfa1f35d3446935ab2aa
atscott <atscott@google.com>
2020-03-02 16:42:40 +0000

    0a5a841994 test(docs-infra): add proxy to fake images in tests (#35741)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+31.sha-d7efc45
+ * @license Angular v9.1.0-next.2+29.sha-0a5a841
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20021,7 +20021,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-next.2+31.sha-d7efc45');
+    var VERSION = new Version('9.1.0-next.2+29.sha-0a5a841');
 
     /**
      * @license
