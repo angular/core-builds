fea3721839ed5643c189a6670f1cf0552c49c504
atscott <atscott@google.com>
2020-03-02 19:52:49 +0000

    4ba4d9076d style: Remove `ivy` scope, and add `ve` scope (#35799)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+37.sha-5652fb1
+ * @license Angular v9.1.0-next.2+38.sha-4ba4d90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19867,7 +19867,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+37.sha-5652fb1');
+var VERSION = new Version('9.1.0-next.2+38.sha-4ba4d90');
 
 /**
  * @license
