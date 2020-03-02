8864fece6bde6c39b425f14438967b18521c3b93
atscott <atscott@google.com>
2020-03-02 16:43:10 +0000

    746e313a76 docs: Fix typo in template syntax (#35767)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+29.sha-0a5a841
+ * @license Angular v9.1.0-next.2+28.sha-746e313
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19868,7 +19868,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+29.sha-0a5a841');
+var VERSION = new Version('9.1.0-next.2+28.sha-746e313');
 
 /**
  * @license
