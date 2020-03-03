8a2a2a6d5090a13ea262a17adececc6c4babd037
atscott <atscott@google.com>
2020-03-03 17:45:32 +0000

    93fc392549 build: fix flakiness in integration/bazel-schematics by disabling symlinked_node_modules (#35808)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+50.sha-3b0b905
+ * @license Angular v9.1.0-next.2+52.sha-93fc392
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19874,7 +19874,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+50.sha-3b0b905');
+var VERSION = new Version('9.1.0-next.2+52.sha-93fc392');
 
 /**
  * @license
