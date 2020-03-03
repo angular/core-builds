688aaafb1276c9d0a675319d83e8d49ffd56b6fa
atscott <atscott@google.com>
2020-03-03 18:25:04 +0000

    a07917bd22 perf(core): add micro benchmark for destroy hook invocation (#35784)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+27.sha-5428840
+ * @license Angular v9.0.4+30.sha-a07917b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28026,7 +28026,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.4+27.sha-5428840');
+const VERSION = new Version('9.0.4+30.sha-a07917b');
 
 /**
  * @fileoverview added by tsickle
