7582548ffe722214f54ef7fcb837d9a4db2a0069
atscott <atscott@google.com>
2020-03-03 17:20:17 +0000

    bf42807aa6 docs(common): switch HTTP guide examples to `TestBed.inject()` (#35777)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+46.sha-c195d22
+ * @license Angular v9.1.0-next.2+47.sha-bf42807
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19905,7 +19905,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+46.sha-c195d22');
+var VERSION = new Version('9.1.0-next.2+47.sha-bf42807');
 
 /**
  * @license
