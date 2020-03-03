f1daa6e643009d3f70e6125df9498d48f8d19a36
atscott <atscott@google.com>
2020-03-03 17:12:31 +0000

    47a1811e0b feat(language-service): modularize error messages (#35678)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+39.sha-ef2721b
+ * @license Angular v9.1.0-next.2+41.sha-47a1811
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28026,7 +28026,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+39.sha-ef2721b');
+const VERSION = new Version('9.1.0-next.2+41.sha-47a1811');
 
 /**
  * @fileoverview added by tsickle
