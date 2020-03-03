80d3e37953cfd30c17065d21475bd15501673c33
atscott <atscott@google.com>
2020-03-03 22:16:57 +0000

    ed44073a58 build(zone.js): sets rollup_bundle to use the --silent flag (#35835)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+58.sha-2c41bb8
+ * @license Angular v9.1.0-next.2+59.sha-ed44073
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28050,7 +28050,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+58.sha-2c41bb8');
+const VERSION = new Version('9.1.0-next.2+59.sha-ed44073');
 
 /**
  * @fileoverview added by tsickle
