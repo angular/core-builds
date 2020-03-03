cf2889458a5ddd24d46ecb3f564c6ab7068a5721
atscott <atscott@google.com>
2020-03-03 22:15:44 +0000

    2c41bb8490 fix(compiler): type-checking error for duplicate variables in templates (#35674)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+57.sha-1f8a243
+ * @license Angular v9.1.0-next.2+58.sha-2c41bb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19919,7 +19919,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+57.sha-1f8a243');
+var VERSION = new Version('9.1.0-next.2+58.sha-2c41bb8');
 
 /**
  * @license
