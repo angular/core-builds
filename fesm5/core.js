e1837616ddb7ccc39bb953b855e6fb9b5f9dd175
atscott <atscott@google.com>
2020-03-04 20:14:42 +0000

    b57be3bb41 docs: release notes for the v9.1.0-next.3 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+72.sha-dcf28bf
+ * @license Angular v9.1.0-next.2+73.sha-b57be3b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19888,7 +19888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+72.sha-dcf28bf');
+var VERSION = new Version('9.1.0-next.2+73.sha-b57be3b');
 
 /**
  * @license
