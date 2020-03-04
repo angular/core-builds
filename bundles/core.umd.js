5d887e9d5b700eb95d196c6daeb5771cefb5afe2
atscott <atscott@google.com>
2020-03-04 00:24:02 +0000

    d543b13e5c Revert "fix(core): log error instead of warning for unknown properties and elements (#35798)" (#35845)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+60.sha-00f3c58
+ * @license Angular v9.1.0-next.2+61.sha-d543b13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8099,7 +8099,7 @@
                 validateAgainstEventProperties(propName);
                 if (!validateProperty(tView, lView, element, propName, tNode)) {
                     // Return here since we only log warnings for unknown properties.
-                    logUnknownPropertyError(propName, tNode);
+                    warnAboutUnknownProperty(propName, tNode);
                     return;
                 }
                 ngDevMode.rendererSetProperty++;
@@ -8120,7 +8120,7 @@
             // If the node is a container and the property didn't
             // match any of the inputs or schemas we should throw.
             if (ngDevMode && !matchingSchemas(tView, lView, tNode.tagName)) {
-                logUnknownPropertyError(propName, tNode);
+                warnAboutUnknownProperty(propName, tNode);
             }
         }
     }
@@ -8198,12 +8198,12 @@
         return false;
     }
     /**
-     * Logs an error that a property is not supported on an element.
+     * Logs a warning that a property is not supported on an element.
      * @param propName Name of the invalid property.
      * @param tNode Node on which we encountered the property.
      */
-    function logUnknownPropertyError(propName, tNode) {
-        console.error("Can't bind to '" + propName + "' since it isn't a known property of '" + tNode.tagName + "'.");
+    function warnAboutUnknownProperty(propName, tNode) {
+        console.warn("Can't bind to '" + propName + "' since it isn't a known property of '" + tNode.tagName + "'.");
     }
     /**
      * Instantiate a root component.
@@ -14358,7 +14358,7 @@
         var attrs = getConstant(tViewConsts, attrsIndex);
         var tNode = getOrCreateTNode(tView, lView[T_HOST], index, 3 /* Element */, name, attrs);
         var hasDirectives = resolveDirectives(tView, lView, tNode, getConstant(tViewConsts, localRefsIndex));
-        ngDevMode && logUnknownElementError(tView, lView, native, tNode, hasDirectives);
+        ngDevMode && warnAboutUnknownElement(tView, lView, native, tNode, hasDirectives);
         if (tNode.mergedAttrs !== null) {
             computeStaticStyling(tNode, tNode.mergedAttrs);
         }
@@ -14469,7 +14469,7 @@
         ɵɵelementStart(index, name, attrsIndex, localRefsIndex);
         ɵɵelementEnd();
     }
-    function logUnknownElementError(tView, lView, element, tNode, hasDirectives) {
+    function warnAboutUnknownElement(tView, lView, element, tNode, hasDirectives) {
         var schemas = tView.schemas;
         // If `schemas` is set to `null`, that's an indication that this Component was compiled in AOT
         // mode where this check happens at compile time. In JIT mode, `schemas` is always present and
@@ -14491,18 +14491,18 @@
                 (typeof customElements !== 'undefined' && tagName.indexOf('-') > -1 &&
                     !customElements.get(tagName));
             if (isUnknown && !matchingSchemas(tView, lView, tagName)) {
-                var message = "'" + tagName + "' is not a known element:\n";
-                message +=
+                var warning = "'" + tagName + "' is not a known element:\n";
+                warning +=
                     "1. If '" + tagName + "' is an Angular component, then verify that it is part of this module.\n";
                 if (tagName && tagName.indexOf('-') > -1) {
-                    message +=
+                    warning +=
                         "2. If '" + tagName + "' is a Web Component then add 'CUSTOM_ELEMENTS_SCHEMA' to the '@NgModule.schemas' of this component to suppress this message.";
                 }
                 else {
-                    message +=
+                    warning +=
                         "2. To allow any element add 'NO_ERRORS_SCHEMA' to the '@NgModule.schemas' of this component.";
                 }
-                console.error(message);
+                console.warn(warning);
             }
         }
     }
@@ -20041,7 +20041,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-next.2+60.sha-00f3c58');
+    var VERSION = new Version('9.1.0-next.2+61.sha-d543b13');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+60.sha-00f3c58
+ * @license Angular v9.1.0-next.2+61.sha-d543b13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7941,7 +7941,7 @@ function elementPropertyInternal(tView, lView, index, propName, value, sanitizer
             validateAgainstEventProperties(propName);
             if (!validateProperty(tView, lView, element, propName, tNode)) {
                 // Return here since we only log warnings for unknown properties.
-                logUnknownPropertyError(propName, tNode);
+                warnAboutUnknownProperty(propName, tNode);
                 return;
             }
             ngDevMode.rendererSetProperty++;
@@ -7962,7 +7962,7 @@ function elementPropertyInternal(tView, lView, index, propName, value, sanitizer
         // If the node is a container and the property didn't
         // match any of the inputs or schemas we should throw.
         if (ngDevMode && !matchingSchemas(tView, lView, tNode.tagName)) {
-            logUnknownPropertyError(propName, tNode);
+            warnAboutUnknownProperty(propName, tNode);
         }
     }
 }
@@ -8040,12 +8040,12 @@ function matchingSchemas(tView, lView, tagName) {
     return false;
 }
 /**
- * Logs an error that a property is not supported on an element.
+ * Logs a warning that a property is not supported on an element.
  * @param propName Name of the invalid property.
  * @param tNode Node on which we encountered the property.
  */
-function logUnknownPropertyError(propName, tNode) {
-    console.error("Can't bind to '" + propName + "' since it isn't a known property of '" + tNode.tagName + "'.");
+function warnAboutUnknownProperty(propName, tNode) {
+    console.warn("Can't bind to '" + propName + "' since it isn't a known property of '" + tNode.tagName + "'.");
 }
 /**
  * Instantiate a root component.
@@ -14200,7 +14200,7 @@ function elementStartFirstCreatePass(index, tView, lView, native, name, attrsInd
     var attrs = getConstant(tViewConsts, attrsIndex);
     var tNode = getOrCreateTNode(tView, lView[T_HOST], index, 3 /* Element */, name, attrs);
     var hasDirectives = resolveDirectives(tView, lView, tNode, getConstant(tViewConsts, localRefsIndex));
-    ngDevMode && logUnknownElementError(tView, lView, native, tNode, hasDirectives);
+    ngDevMode && warnAboutUnknownElement(tView, lView, native, tNode, hasDirectives);
     if (tNode.mergedAttrs !== null) {
         computeStaticStyling(tNode, tNode.mergedAttrs);
     }
@@ -14311,7 +14311,7 @@ function ɵɵelement(index, name, attrsIndex, localRefsIndex) {
     ɵɵelementStart(index, name, attrsIndex, localRefsIndex);
     ɵɵelementEnd();
 }
-function logUnknownElementError(tView, lView, element, tNode, hasDirectives) {
+function warnAboutUnknownElement(tView, lView, element, tNode, hasDirectives) {
     var schemas = tView.schemas;
     // If `schemas` is set to `null`, that's an indication that this Component was compiled in AOT
     // mode where this check happens at compile time. In JIT mode, `schemas` is always present and
@@ -14333,18 +14333,18 @@ function logUnknownElementError(tView, lView, element, tNode, hasDirectives) {
             (typeof customElements !== 'undefined' && tagName.indexOf('-') > -1 &&
                 !customElements.get(tagName));
         if (isUnknown && !matchingSchemas(tView, lView, tagName)) {
-            var message = "'" + tagName + "' is not a known element:\n";
-            message +=
+            var warning = "'" + tagName + "' is not a known element:\n";
+            warning +=
                 "1. If '" + tagName + "' is an Angular component, then verify that it is part of this module.\n";
             if (tagName && tagName.indexOf('-') > -1) {
-                message +=
+                warning +=
                     "2. If '" + tagName + "' is a Web Component then add 'CUSTOM_ELEMENTS_SCHEMA' to the '@NgModule.schemas' of this component to suppress this message.";
             }
             else {
-                message +=
+                warning +=
                     "2. To allow any element add 'NO_ERRORS_SCHEMA' to the '@NgModule.schemas' of this component.";
             }
-            console.error(message);
+            console.warn(warning);
         }
     }
 }
@@ -19888,7 +19888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+60.sha-00f3c58');
+var VERSION = new Version('9.1.0-next.2+61.sha-d543b13');
 
 /**
  * @license
