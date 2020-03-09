d69a07564e09c4ef686d2c40e8036fe76b03cc33
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 21:26:44 +0000

    15482e7367 Revert "feat(bazel): transform generated shims (in Ivy) with tsickle (#35848)" (#35970)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+110.sha-fc4c3c3
+ * @license Angular v9.1.0-next.2+112.sha-15482e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19919,7 +19919,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+110.sha-fc4c3c3');
+var VERSION = new Version('9.1.0-next.2+112.sha-15482e7');
 
 /**
  * @license
