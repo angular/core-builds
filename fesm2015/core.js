32e71c7f65b4987bde071966cdb7e5acc8a00d4e
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 17:29:48 +0000

    83d7819bfc fix(localize): merge translation from all  XLIFF `<file>` elements (#35936)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+29.sha-4cc519e
+ * @license Angular v9.0.5+30.sha-83d7819
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28050,7 +28050,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.5+29.sha-4cc519e');
+const VERSION = new Version('9.0.5+30.sha-83d7819');
 
 /**
  * @fileoverview added by tsickle
