cc8e87a7573b939bce9fa0d2376c679ebc0eeb35
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:00:25 +0000

    983f48136a test(compiler): add a public API guard for the public compiler options (#35885)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+119.sha-64d6f13
+ * @license Angular v9.1.0-next.2+122.sha-983f481
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19888,7 +19888,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+119.sha-64d6f13');
+var VERSION = new Version('9.1.0-next.2+122.sha-983f481');
 
 /**
  * @license
