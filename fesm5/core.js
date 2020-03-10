159ae8029f246916bcc7518a65880035c4bdca0a
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:13:00 +0000

    fc71032dc4 test(router): use pageYOffset in testing when scrollY is not available (#35976)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+117.sha-81cb54f
+ * @license Angular v9.1.0-next.2+118.sha-fc71032
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19919,7 +19919,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+117.sha-81cb54f');
+var VERSION = new Version('9.1.0-next.2+118.sha-fc71032');
 
 /**
  * @license
