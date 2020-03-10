4613e7b61e374128851890f52ffe6fce71ddfd98
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 22:06:01 +0000

    c98c6e80c8 build: adding a script to compare commits in master and stable branches (#35130)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+124.sha-191e4d1
+ * @license Angular v9.1.0-next.2+128.sha-c98c6e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19919,7 +19919,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+124.sha-191e4d1');
+var VERSION = new Version('9.1.0-next.2+128.sha-c98c6e8');
 
 /**
  * @license
@@ -26535,6 +26535,17 @@ function transitiveScopesFor(moduleType) {
             pipes: new Set(),
         },
     };
+    maybeUnwrapFn(def.imports).forEach(function (imported) {
+        var importedType = imported;
+        if (!isNgModule(importedType)) {
+            throw new Error("Importing " + importedType.name + " which does not have a \u0275mod property");
+        }
+        // When this module imports another, the imported module's exported directives and pipes are
+        // added to the compilation scope of this module.
+        var importedScope = transitiveScopesFor(importedType);
+        importedScope.exported.directives.forEach(function (entry) { return scopes.compilation.directives.add(entry); });
+        importedScope.exported.pipes.forEach(function (entry) { return scopes.compilation.pipes.add(entry); });
+    });
     maybeUnwrapFn(def.declarations).forEach(function (declared) {
         var declaredWithDefs = declared;
         if (getPipeDef(declaredWithDefs)) {
@@ -26546,17 +26557,6 @@ function transitiveScopesFor(moduleType) {
             // directives.
             scopes.compilation.directives.add(declared);
         }
-    });
-    maybeUnwrapFn(def.imports).forEach(function (imported) {
-        var importedType = imported;
-        if (!isNgModule(importedType)) {
-            throw new Error("Importing " + importedType.name + " which does not have a \u0275mod property");
-        }
-        // When this module imports another, the imported module's exported directives and pipes are
-        // added to the compilation scope of this module.
-        var importedScope = transitiveScopesFor(importedType);
-        importedScope.exported.directives.forEach(function (entry) { return scopes.compilation.directives.add(entry); });
-        importedScope.exported.pipes.forEach(function (entry) { return scopes.compilation.pipes.add(entry); });
     });
     maybeUnwrapFn(def.exports).forEach(function (exported) {
         var exportedType = exported;
