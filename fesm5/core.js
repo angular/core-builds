b713fee904ac16faaded5382bc355e4a936c69e9
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:31:46 +0000

    18b1bd4415 fix(language-service): infer $implicit value for ngIf template contexts (#35941)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+26.sha-06779cf
+ * @license Angular v9.1.0-next.4+27.sha-18b1bd4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19921,7 +19921,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+26.sha-06779cf');
+var VERSION = new Version('9.1.0-next.4+27.sha-18b1bd4');
 
 /**
  * @license
