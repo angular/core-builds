f1d82618f5d5754ad40efb809aa4d5c6620121b5
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:10:33 +0000

    1f89c6130e fix(ngcc): show helpful error when providing an invalid option (#36010)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+8.sha-42375c4
+ * @license Angular v9.1.0-next.4+11.sha-1f89c61
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19921,7 +19921,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+8.sha-42375c4');
+var VERSION = new Version('9.1.0-next.4+11.sha-1f89c61');
 
 /**
  * @license
