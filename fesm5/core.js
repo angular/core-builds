048bf24066289c88ec6b12375e11e74a7a514941
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:21:12 +0000

    32f099aa36 fix(bazel): update several packages for better windows support (#35991)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+23.sha-20f0116
+ * @license Angular v9.1.0-next.4+25.sha-32f099a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+23.sha-20f0116');
+var VERSION = new Version('9.1.0-next.4+25.sha-32f099a');
 
 /**
  * @license
