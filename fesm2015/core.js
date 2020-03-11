2a64d51b6648203573efe0e689379ccc1da9a8ba
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:26:53 +0000

    e262962b12 fix(docs-infra): add boilerplate path and project type for elements example project (#33484)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+6.sha-3112b1b
+ * @license Angular v9.0.6+17.sha-e262962
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28053,7 +28053,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.6+6.sha-3112b1b');
+const VERSION = new Version('9.0.6+17.sha-e262962');
 
 /**
  * @fileoverview added by tsickle
