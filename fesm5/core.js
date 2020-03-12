16b995047ed97fd136a88b714d3e948acbe7ec34
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:13:03 +0000

    43635a6c54 build: update ibazel to `0.12.2` (#36033)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+37.sha-37a4839
+ * @license Angular v9.1.0-next.4+38.sha-43635a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19921,7 +19921,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+37.sha-37a4839');
+var VERSION = new Version('9.1.0-next.4+38.sha-43635a6');
 
 /**
  * @license
