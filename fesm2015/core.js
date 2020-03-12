3c65079ad7a896d6d4e52e45e06a0d51461e86b4
Andrew Kushnir <akushnir@google.com>
2020-03-12 19:50:26 +0000

    f699c962bb test(core): Add benchpress test for transplanted view change detection (#36001)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+27.sha-dec80b0
+ * @license Angular v9.0.6+28.sha-f699c96
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28053,7 +28053,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.6+27.sha-dec80b0');
+const VERSION = new Version('9.0.6+28.sha-f699c96');
 
 /**
  * @fileoverview added by tsickle
