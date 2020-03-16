0ab80499f0d8e33c447b36340cca431043c4a918
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:20:42 +0000

    31bec8ce61 feat(compiler): Propagate source span and value span to Variable AST (#36047)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+58.sha-e179c58
+ * @license Angular v9.1.0-next.4+59.sha-31bec8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28034,7 +28034,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+58.sha-e179c58');
+const VERSION = new Version('9.1.0-next.4+59.sha-31bec8c');
 
 /**
  * @fileoverview added by tsickle
