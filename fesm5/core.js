1da6df8c8ba3772397f767048b5981098fa631a4
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:14:14 +0000

    79659ee5aa fix(compiler): support directive inputs with interpolations on `<ng-template>`s (#35984)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+56.sha-3fa8952
+ * @license Angular v9.1.0-next.4+57.sha-79659ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+56.sha-3fa8952');
+var VERSION = new Version('9.1.0-next.4+57.sha-79659ee');
 
 /**
  * @license
