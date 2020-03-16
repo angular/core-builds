c8a2ab5f0af2f6345f62f9ad57e2e9965724f416
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:25:06 +0000

    3fa895298d fix(zone.js): zone.js patches rxjs should check null for unsubscribe (#35990)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+53.sha-0f8e710
+ * @license Angular v9.1.0-next.4+56.sha-3fa8952
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+53.sha-0f8e710');
+var VERSION = new Version('9.1.0-next.4+56.sha-3fa8952');
 
 /**
  * @license
