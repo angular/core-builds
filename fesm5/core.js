9accff01251aff5f44bd5614f6472e6fd40c237d
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:22:51 +0000

    46fe8fb8b4 build: depend on bazelisk rather than directly on Bazel (#36078)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+59.sha-31bec8c
+ * @license Angular v9.1.0-next.4+60.sha-46fe8fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+59.sha-31bec8c');
+var VERSION = new Version('9.1.0-next.4+60.sha-46fe8fb');
 
 /**
  * @license
