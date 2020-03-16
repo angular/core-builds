c78035574579400b6d8df9100588152078bd6a3c
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:17:39 +0000

    e179c5827f fix(compiler): do not recurse to find static symbols of same module (#35262)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+57.sha-79659ee
+ * @license Angular v9.1.0-next.4+58.sha-e179c58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+57.sha-79659ee');
+var VERSION = new Version('9.1.0-next.4+58.sha-e179c58');
 
 /**
  * @license
