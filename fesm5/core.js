1bfff37b5af2b41b614b09f4387b6ba3e7a8282c
Andrew Kushnir <akushnir@google.com>
2020-03-17 19:45:39 +0000

    22be967104 ci: run ts-circular-deps check in lint job on ci (#35647)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+70.sha-34a17f3
+ * @license Angular v9.1.0-next.4+75.sha-22be967
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+70.sha-34a17f3');
+var VERSION = new Version('9.1.0-next.4+75.sha-22be967');
 
 /**
  * @license
