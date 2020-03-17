bcd1b3529fe692cf99f5de50e2c4ef3b7afbcd9e
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:52:07 +0000

    acc483e2eb feat(language-service): improve non-callable error message (#35271)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+64.sha-61c3795
+ * @license Angular v9.1.0-next.4+63.sha-acc483e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20038,7 +20038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-next.4+64.sha-61c3795');
+    var VERSION = new Version('9.1.0-next.4+63.sha-acc483e');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+64.sha-61c3795
+ * @license Angular v9.1.0-next.4+63.sha-acc483e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19885,7 +19885,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+64.sha-61c3795');
+var VERSION = new Version('9.1.0-next.4+63.sha-acc483e');
 
 /**
  * @license
