3fca684838f3e9966c75506df3620f9ec5777102
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:19:51 +0000

    d50881a86e docs: move asset management section (#35665)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+6.sha-c9f554c
+ * @license Angular v9.1.0-next.5+1.sha-d50881a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19885,7 +19885,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.5+6.sha-c9f554c');
+var VERSION = new Version('9.1.0-next.5+1.sha-d50881a');
 
 /**
  * @license
