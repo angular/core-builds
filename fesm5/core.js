c29d0d9a51b5ad1c76adab1e2fcd14a90179931a
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:26:53 +0000

    1aa5783a45 docs: adding a note on extra text interpolations and HTML tags in translations to Ivy guide (#36029)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+101.sha-67b9adf
+ * @license Angular v9.1.0-next.4+102.sha-1aa5783
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+101.sha-67b9adf');
+var VERSION = new Version('9.1.0-next.4+102.sha-1aa5783');
 
 /**
  * @license
