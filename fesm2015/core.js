5baf81caa2944579aec5073d91e812e6cffbc92e
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:20:06 +0000

    c0143cb2ab build: ng_rollup_bundle internal rule should support ngcc (#36044)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+1.sha-d50881a
+ * @license Angular v9.1.0-next.5+7.sha-c0143cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28018,7 +28018,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.5+1.sha-d50881a');
+const VERSION = new Version('9.1.0-next.5+7.sha-c0143cb');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+1.sha-d50881a
+ * @license Angular v9.1.0-next.5+7.sha-c0143cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19885,7 +19885,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.5+1.sha-d50881a');
+var VERSION = new Version('9.1.0-next.5+7.sha-c0143cb');
 
 /**
  * @license
