302a00446e03f2f01bdddb3430148d6c8da0e668
Misko Hevery <misko@hevery.com>
2020-03-20 21:17:20 +0000

    c21c46a8e8 fix(docs-infra): include correct dependencies in StackBlitz examples (#36071)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+8.sha-ae3eaf8
+ * @license Angular v9.1.0-rc.0+10.sha-c21c46a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28035,7 +28035,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+8.sha-ae3eaf8');
+const VERSION = new Version('9.1.0-rc.0+10.sha-c21c46a');
 
 /**
  * @fileoverview added by tsickle
