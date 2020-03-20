7a7a1557a496f1ce877708fd8be772eff84b3300
Misko Hevery <misko@hevery.com>
2020-03-20 17:44:31 +0000

    8968b206b4 feat(benchpress): create component_benchmark macro (#35692)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+1.sha-47bfec4
+ * @license Angular v9.1.0-rc.0+3.sha-8968b20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+1.sha-47bfec4');
+var VERSION = new Version('9.1.0-rc.0+3.sha-8968b20');
 
 /**
  * @license
