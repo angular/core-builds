a3337255b11af887eb2fa7fe1d436df51cccb960
Misko Hevery <misko@hevery.com>
2020-03-23 18:56:03 +0000

    e81ad3a1bc docs: Add asterisk info in template syntax guide (#36176)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+13.sha-0ce8ad3
+ * @license Angular v9.1.0-rc.0+16.sha-e81ad3a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28035,7 +28035,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+13.sha-0ce8ad3');
+const VERSION = new Version('9.1.0-rc.0+16.sha-e81ad3a');
 
 /**
  * @fileoverview added by tsickle
