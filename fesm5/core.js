7be6a29cdd315071fdace68b48440961774735cc
Misko Hevery <misko@hevery.com>
2020-03-23 19:03:34 +0000

    9d415f9c3f fix(docs-infra): change `app-list-item` to `app-item-list` (#35601)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+18.sha-1cb7b88
+ * @license Angular v9.1.0-rc.0+20.sha-9d415f9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+18.sha-1cb7b88');
+var VERSION = new Version('9.1.0-rc.0+20.sha-9d415f9');
 
 /**
  * @license
