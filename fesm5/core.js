7df30c4df7d1d7f5bf7f6b85809b95b392d2db05
Misko Hevery <misko@hevery.com>
2020-03-24 17:45:39 +0000

    55dac05cf2 docs: fix typo in testing component with dependencies (#36219)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+25.sha-4f97173
+ * @license Angular v9.1.0-rc.0+28.sha-55dac05
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+25.sha-4f97173');
+var VERSION = new Version('9.1.0-rc.0+28.sha-55dac05');
 
 /**
  * @license
