3fd292fb1138894cbb1a898bcb9b9f16b20d219e
Misko Hevery <misko@hevery.com>
2020-03-25 21:43:06 +0000

    294c6297d7 docs: coalesce release notes for the v9.1.0 release (#36247)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+1.sha-58fc65d
+ * @license Angular v9.1.0+2.sha-294c629
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+1.sha-58fc65d');
+var VERSION = new Version('9.1.0+2.sha-294c629');
 
 /**
  * @license
