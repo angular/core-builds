27fc206292fcdc40b77a78c523c64e6fff55bd95
Misko Hevery <misko@hevery.com>
2020-03-25 17:01:43 +0000

    b6bd8d7572 release: cut the v9.1.0 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.2
+ * @license Angular v9.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.2');
+var VERSION = new Version('9.1.0');
 
 /**
  * @license
