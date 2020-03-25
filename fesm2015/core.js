9f23eac8d1d835293bc9629e7e6a319fac28b5c6
Misko Hevery <misko@hevery.com>
2020-03-25 19:11:08 +0000

    58fc65d198 build: ensure that refs and shas for PRs only need to be requested once (#36207)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0
+ * @license Angular v9.1.0+1.sha-58fc65d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28035,7 +28035,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0');
+const VERSION = new Version('9.1.0+1.sha-58fc65d');
 
 /**
  * @fileoverview added by tsickle
