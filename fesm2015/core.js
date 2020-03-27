427f56df55d9ed15d7290133b2282a5300bfd9a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:22:23 +0000

    1c385a1c22 build(docs-infra): do not include CI-specific config in docs examples ZIP archives (#36018)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+38.sha-fc3e5cb
+ * @license Angular v9.1.0-rc.0+44.sha-1c385a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28035,7 +28035,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+38.sha-fc3e5cb');
+const VERSION = new Version('9.1.0-rc.0+44.sha-1c385a1');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+38.sha-fc3e5cb
+ * @license Angular v9.1.0-rc.0+44.sha-1c385a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+38.sha-fc3e5cb');
+var VERSION = new Version('9.1.0-rc.0+44.sha-1c385a1');
 
 /**
  * @license
