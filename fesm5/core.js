03ba94d66387a6ab2bf60a5a294e329f4d05d9ac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:54:55 +0000

    0ab244f39e docs: fix typo in Schematics guide (#36328)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+20.sha-15cffa2
+ * @license Angular v9.1.0+22.sha-0ab244f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+20.sha-15cffa2');
+var VERSION = new Version('9.1.0+22.sha-0ab244f');
 
 /**
  * @license
