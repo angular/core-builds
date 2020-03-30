ff67eca8b5dce6f120b4fc9d678c9cfc6d5d5cca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 19:06:41 +0000

    a383116b43 docs: fix typo in Dependency Injection guide (#36304)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+57.sha-5ac3080
+ * @license Angular v9.1.0-rc.0+59.sha-a383116
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+57.sha-5ac3080');
+var VERSION = new Version('9.1.0-rc.0+59.sha-a383116');
 
 /**
  * @license
