8d254c6164732c0c068e29f8c4360ae307ac0d8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:53:55 +0000

    80c68583d1 refactor(core): use more narrow `QueryList` import to avoid circular deps issue (#36286)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+62.sha-2510e7d
+ * @license Angular v9.1.0-rc.0+61.sha-80c6858
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+62.sha-2510e7d');
+var VERSION = new Version('9.1.0-rc.0+61.sha-80c6858');
 
 /**
  * @license
