81c043202407c1257e9202b5909b1998b00e399b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:22:45 +0000

    63fbc71439 build: don't use deprecated $(location) pre-declared variable (#36308)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+63.sha-c5df9ce
+ * @license Angular v9.1.0-rc.0+65.sha-63fbc71
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28035,7 +28035,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+63.sha-c5df9ce');
+const VERSION = new Version('9.1.0-rc.0+65.sha-63fbc71');
 
 /**
  * @fileoverview added by tsickle
