1a50acf9b84af5bf250581f55416c856fe54ef31
Kara Erickson <karakara@google.com>
2020-04-01 18:32:32 +0000

    beb3f19dc0 docs: update end date of survey to middle of April (#36339)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+26.sha-e8f3c47
+ * @license Angular v9.1.0+27.sha-beb3f19
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+26.sha-e8f3c47');
+var VERSION = new Version('9.1.0+27.sha-beb3f19');
 
 /**
  * @license
