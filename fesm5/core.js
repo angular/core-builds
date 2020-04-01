14431666e35b030f23bcaf721ade72b3c45a5a06
Kara Erickson <karakara@google.com>
2020-04-01 22:44:58 +0000

    4d458db1b5 docs: update and edit web-worker page (#36383)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+84.sha-326240e
+ * @license Angular v9.1.0-rc.0+87.sha-4d458db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+84.sha-326240e');
+var VERSION = new Version('9.1.0-rc.0+87.sha-4d458db');
 
 /**
  * @license
