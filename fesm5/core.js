0c6fecf307549659504aae328910984fd7c74173
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:39:22 +0000

    c5c57f6737 build: update to clang 1.4.0 and only run clang format on changed files (#36203)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+68.sha-4d1d0fa
+ * @license Angular v9.1.0-rc.0+72.sha-c5c57f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+68.sha-4d1d0fa');
+var VERSION = new Version('9.1.0-rc.0+72.sha-c5c57f6');
 
 /**
  * @license
