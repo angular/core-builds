aea1b30422900383502ba9951d837dccc8f52405
Kara Erickson <karakara@google.com>
2020-04-01 22:47:35 +0000

    24a92472bf ci: use dev-infra cli from local sources (#36326)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+87.sha-4d458db
+ * @license Angular v9.1.0-rc.0+86.sha-24a9247
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+87.sha-4d458db');
+var VERSION = new Version('9.1.0-rc.0+86.sha-24a9247');
 
 /**
  * @license
