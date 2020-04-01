0dd7c9f04d29e8acb911971a288efae0fb7a602d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:52:12 +0000

    5516802142 fix(compiler): avoid undefined expressions in holey array (#36343)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+73.sha-9d8bb63
+ * @license Angular v9.1.0-rc.0+82.sha-5516802
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20038,7 +20038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-rc.0+73.sha-9d8bb63');
+    var VERSION = new Version('9.1.0-rc.0+82.sha-5516802');
 
     /**
      * @license
