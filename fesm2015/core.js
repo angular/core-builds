56ba7f4a333974e62f48e1e21e3d8971ad9fa835
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:54:14 +0000

    326240eb91 fix(ngcc): allow ngcc configuration to match pre-release versions of packages (#36370)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+81.sha-cc4b813
+ * @license Angular v9.1.0-rc.0+84.sha-326240e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28035,7 +28035,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+81.sha-cc4b813');
+const VERSION = new Version('9.1.0-rc.0+84.sha-326240e');
 
 /**
  * @fileoverview added by tsickle
