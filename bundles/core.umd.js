6237e3fa600ee68c6bbe5a9a16059e090c964f7b
Kara Erickson <karakara@google.com>
2020-04-01 22:46:50 +0000

    4d458db1b5 docs: update and edit web-worker page (#36383)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+84.sha-326240e
+ * @license Angular v9.1.0-rc.0+87.sha-4d458db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20038,7 +20038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-rc.0+84.sha-326240e');
+    var VERSION = new Version('9.1.0-rc.0+87.sha-4d458db');
 
     /**
      * @license
