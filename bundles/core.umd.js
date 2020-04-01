1e20c52e18bfa03a2107489cf8f67dbe39419bed
Kara Erickson <karakara@google.com>
2020-04-01 20:35:28 +0000

    4d1d0fa03b docs: update ng-conf announcement to remove livestream link (#36382)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+67.sha-ec8bae1
+ * @license Angular v9.1.0-rc.0+68.sha-4d1d0fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20038,7 +20038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.0-rc.0+67.sha-ec8bae1');
+    var VERSION = new Version('9.1.0-rc.0+68.sha-4d1d0fa');
 
     /**
      * @license
