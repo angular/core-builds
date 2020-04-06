57243abd34ee26ef15af497c4b5d753c341b4c2a
Kara Erickson <karakara@google.com>
2020-04-06 16:56:17 +0000

    1b4df6484e docs: add missing command in `WebWorker` guide (#36397)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+115.sha-1140bbc
+ * @license Angular v9.1.0-rc.0+114.sha-1b4df64
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+115.sha-1140bbc');
+var VERSION = new Version('9.1.0-rc.0+114.sha-1b4df64');
 
 /**
  * @license
