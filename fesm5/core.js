bf4b4754661e207ddd924edfdb5ef1ac9917c5b9
Kara Erickson <karakara@google.com>
2020-04-06 18:56:21 +0000

    f9fb8338f5 fix(ngcc): support ignoring deep-imports via package config (#36423)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+114.sha-1b4df64
+ * @license Angular v9.1.0-rc.0+118.sha-f9fb833
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+114.sha-1b4df64');
+var VERSION = new Version('9.1.0-rc.0+118.sha-f9fb833');
 
 /**
  * @license
