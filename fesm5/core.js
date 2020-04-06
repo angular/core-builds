9b2690538e7d46c753353443618f0d66290d0fe3
Kara Erickson <karakara@google.com>
2020-04-06 21:15:20 +0000

    068e28381d docs: fix broken guide link (tutorial/index --> tutorial) (#36453)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+73.sha-e2b221b
+ * @license Angular v9.1.0+81.sha-068e283
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+73.sha-e2b221b');
+var VERSION = new Version('9.1.0+81.sha-068e283');
 
 /**
  * @license
