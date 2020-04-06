dd1ddc8afed0ced32d7cd7cf2614731d7664adb1
Kara Erickson <karakara@google.com>
2020-04-06 20:44:30 +0000

    d43c30688a fix(core): avoid migration error when non-existent symbol is imported (#36367)

--- schematics/utils/typescript/compiler_host.d.ts
+++ schematics/utils/typescript/compiler_host.d.ts
@@ -8,4 +8,20 @@
  */
 import { Tree } from '@angular-devkit/schematics';
 import * as ts from 'typescript';
-export declare function createMigrationCompilerHost(tree: Tree, options: ts.CompilerOptions, basePath: string, fakeRead?: (fileName: string) => string | null): ts.CompilerHost;
+export declare type FakeReadFileFn = (fileName: string) => string | null;
+/**
+ * Creates a TypeScript program instance for a TypeScript project within
+ * the virtual file system tree.
+ * @param tree Virtual file system tree that contains the source files.
+ * @param tsconfigPath Virtual file system path that resolves to the TypeScript project.
+ * @param basePath Base path for the virtual file system tree.
+ * @param fakeFileRead Optional file reader function. Can be used to overwrite files in
+ *   the TypeScript program, or to add in-memory files (e.g. to add global types).
+ * @param additionalFiles Additional file paths that should be added to the program.
+ */
+export declare function createMigrationProgram(tree: Tree, tsconfigPath: string, basePath: string, fakeFileRead?: FakeReadFileFn, additionalFiles?: string[]): {
+    parsed: ts.ParsedCommandLine;
+    host: ts.CompilerHost;
+    program: ts.Program;
+};
+export declare function createMigrationCompilerHost(tree: Tree, options: ts.CompilerOptions, basePath: string, fakeRead?: FakeReadFileFn): ts.CompilerHost;
