2aaa8f552199399ac1aebd2fc4bfce471b629974
Kara Erickson <karakara@google.com>
2020-04-06 22:56:43 +0000

    aeb6d0d0f7 fix(zone.js): should not try to patch fetch if it is not writable (#36311)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+81.sha-068e283
+ * @license Angular v9.1.0+88.sha-aeb6d0d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+81.sha-068e283');
+var VERSION = new Version('9.1.0+88.sha-aeb6d0d');
 
 /**
  * @license
