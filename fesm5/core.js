58626f00d9535698702b3b2d9d29f3f0f8ea1254
Kara Erickson <karakara@google.com>
2020-04-07 17:09:28 +0000

    f48a065db0 style(forms): reformat of the `forms` package after clang update (#36466)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+88.sha-aeb6d0d
+ * @license Angular v9.1.0+89.sha-f48a065
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+88.sha-aeb6d0d');
+var VERSION = new Version('9.1.0+89.sha-f48a065');
 
 /**
  * @license
