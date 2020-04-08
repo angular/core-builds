b7bddb7132f377cea6acac7d4d666e0884445ac6
atscott <atscott@google.com>
2020-04-08 22:48:21 +0000

    5da621d3dd fix(language-service): remove circular dependency instance (#36463)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+11.sha-cbed582
+ * @license Angular v9.1.1+12.sha-5da621d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20177,7 +20177,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.1+11.sha-cbed582');
+    var VERSION = new Version('9.1.1+12.sha-5da621d');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+11.sha-cbed582
+ * @license Angular v9.1.1+12.sha-5da621d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20007,7 +20007,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+11.sha-cbed582');
+var VERSION = new Version('9.1.1+12.sha-5da621d');
 
 /**
  * @license
