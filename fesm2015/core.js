3fca10b2c57bf82cd8af47285d2f5475eedeccf7
atscott <atscott@google.com>
2020-04-08 21:50:08 +0000

    33eee43263 fix(ngcc): do not warn if `paths` mapping does not exist (#36525)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+8.sha-72053b0
+ * @license Angular v9.1.1+9.sha-33eee43
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28133,7 +28133,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.1+8.sha-72053b0');
+const VERSION = new Version('9.1.1+9.sha-33eee43');
 
 /**
  * @fileoverview added by tsickle
