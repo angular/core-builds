cef35b3f0b0a472001e0dfe0e2d894349668084d
atscott <atscott@google.com>
2020-04-13 15:38:30 +0000

    9181b73e5a docs: fix typo in Tests guide (#36592)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+21.sha-ca67748
+ * @license Angular v10.0.0-next.1+23.sha-9181b73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20007,7 +20007,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+21.sha-ca67748');
+var VERSION = new Version('10.0.0-next.1+23.sha-9181b73');
 
 /**
  * @license
