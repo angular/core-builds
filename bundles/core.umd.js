5c0bf12dda3e5a594246749b4b944298bddecdf8
atscott <atscott@google.com>
2020-04-13 15:39:41 +0000

    fd7698253e docs: move ng-conf 2020 to the already presented section (#36413)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+29.sha-3992341
+ * @license Angular v9.1.1+34.sha-fd76982
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20177,7 +20177,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.1+29.sha-3992341');
+    var VERSION = new Version('9.1.1+34.sha-fd76982');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+29.sha-3992341
+ * @license Angular v9.1.1+34.sha-fd76982
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20007,7 +20007,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+29.sha-3992341');
+var VERSION = new Version('9.1.1+34.sha-fd76982');
 
 /**
  * @license
