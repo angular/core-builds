889ffe5ed9d9b6a1693f4e05a404e857c630b054
atscott <atscott@google.com>
2020-04-14 21:19:06 +0000

    0147de9044 build: fix circular deps failure (#36629)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+34.sha-b010849
+ * @license Angular v10.0.0-next.1+35.sha-0147de9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20124,7 +20124,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+34.sha-b010849');
+var VERSION = new Version('10.0.0-next.1+35.sha-0147de9');
 
 /**
  * @license
