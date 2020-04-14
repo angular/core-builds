0411f9b85762358df84fa7a81bca5c54f40786da
atscott <atscott@google.com>
2020-04-14 00:55:20 +0000

    c8f2ca2349 docs: edit to setup-local (#36168)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+34.sha-fd76982
+ * @license Angular v9.1.1+36.sha-c8f2ca2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20007,7 +20007,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+34.sha-fd76982');
+var VERSION = new Version('9.1.1+36.sha-c8f2ca2');
 
 /**
  * @license
