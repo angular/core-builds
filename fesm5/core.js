678da3cdf6b1a2a25057e2270afcc5637224e0b5
atscott <atscott@google.com>
2020-04-14 00:59:33 +0000

    0cc53fb398 fix(upgrade): update $locationShim to handle Location changes before initialization (#36498)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+28.sha-f88e635
+ * @license Angular v10.0.0-next.1+27.sha-0cc53fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20007,7 +20007,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+28.sha-f88e635');
+var VERSION = new Version('10.0.0-next.1+27.sha-0cc53fb');
 
 /**
  * @license
