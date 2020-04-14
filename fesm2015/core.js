9d438f46958f791a60697ea3c061799a9415eb77
Joey Perrott <josephperrott@gmail.com>
2020-04-14 01:04:32 +0000

    870657ca93 build: reformat repo to new clang@1.4.0

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+35.sha-97c1f02
+ * @license Angular v9.1.1+35.sha-870657c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28261,7 +28261,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.1+35.sha-97c1f02');
+const VERSION = new Version('9.1.1+35.sha-870657c');
 
 /**
  * @fileoverview added by tsickle
