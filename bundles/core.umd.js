e900cffb3d6ea2b42faa13a5ff9b01acd85cb6a6
atscott <atscott@google.com>
2020-04-15 23:36:37 +0000

    074266b896 release: cut the v10.0.0-next.2 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+39.sha-c4dc642
+ * @license Angular v10.0.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20294,7 +20294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.1+39.sha-c4dc642');
+    var VERSION = new Version('10.0.0-next.2');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+39.sha-c4dc642
+ * @license Angular v10.0.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20124,7 +20124,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+39.sha-c4dc642');
+var VERSION = new Version('10.0.0-next.2');
 
 /**
  * @license
