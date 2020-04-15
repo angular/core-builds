3279c946b6011c3ef7f544dc77afeb3c27892c40
atscott <atscott@google.com>
2020-04-15 23:38:28 +0000

    074266b896 release: cut the v10.0.0-next.2 release

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+39.sha-c4dc642
+ * @license Angular v10.0.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20093,7 +20093,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+39.sha-c4dc642');
+var VERSION = new Version('10.0.0-next.2');
 
 /**
  * @license
