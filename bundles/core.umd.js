d43aba1128766bebca970d47a8f0b5bd76acdc5c
atscott <atscott@google.com>
2020-04-16 00:09:30 +0000

    793a001d7c release: cut the v9.1.2 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+46.sha-5c3774c
+ * @license Angular v9.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20294,7 +20294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.1+46.sha-5c3774c');
+    var VERSION = new Version('9.1.2');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+46.sha-5c3774c
+ * @license Angular v9.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28261,7 +28261,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.1+46.sha-5c3774c');
+const VERSION = new Version('9.1.2');
 
 /**
  * @fileoverview added by tsickle
