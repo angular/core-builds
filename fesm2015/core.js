064905b84f642a190a4074e4b3b2a3ff05a3edde
atscott <atscott@google.com>
2020-04-16 18:29:58 +0000

    bfda7ca5bd docs(changelog): Add new line between changes (#36653)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+18.sha-305b059
+ * @license Angular v10.0.0-next.2+19.sha-bfda7ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28273,7 +28273,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+18.sha-305b059');
+const VERSION = new Version('10.0.0-next.2+19.sha-bfda7ca');
 
 /**
  * @fileoverview added by tsickle
