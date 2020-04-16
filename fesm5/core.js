5aaeb09322f3a84ad2c826d88301423993f84b91
atscott <atscott@google.com>
2020-04-16 17:08:23 +0000

    4a9f0bebc3 fix(core): prevent unknown property check for AOT-compiled components (#36072)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+3.sha-b1f1d3f
+ * @license Angular v10.0.0-next.2+14.sha-4a9f0be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5991,7 +5991,12 @@ function matchTemplateAttribute(attrs, name) {
     if (i > -1) {
         i++;
         while (i < attrs.length) {
-            if (attrs[i] === name)
+            var attr = attrs[i];
+            // Return in case we checked all template attrs and are switching to the next section in the
+            // attrs array (that starts with a number that represents an attribute marker).
+            if (typeof attr === 'number')
+                return -1;
+            if (attr === name)
                 return i;
             i++;
         }
@@ -8113,6 +8118,12 @@ function setNgReflectProperties(lView, element, type, dataValue, value) {
     }
 }
 function validateProperty(tView, lView, element, propName, tNode) {
+    // If `schemas` is set to `null`, that's an indication that this Component was compiled in AOT
+    // mode where this check happens at compile time. In JIT mode, `schemas` is always present and
+    // defined as an array (as an empty array in case `schemas` field is not defined) and we should
+    // execute the check below.
+    if (tView.schemas === null)
+        return true;
     // The property is considered valid if the element matches the schema, it exists on the element
     // or it is synthetic, and we are in a browser context (web worker nodes should be skipped).
     if (matchingSchemas(tView, lView, tNode.tagName) || propName in element ||
@@ -20093,7 +20104,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+3.sha-b1f1d3f');
+var VERSION = new Version('10.0.0-next.2+14.sha-4a9f0be');
 
 /**
  * @license
