e12d5ea134c573a6cf5cf1886ea18feb94a63ed7
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 17:57:03 +0000

    28ff02b116 docs: remove version ^7.0.0 from LTS support (#36708)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+46.sha-f4ef8a9
+ * @license Angular v10.0.0-next.2+47.sha-28ff02b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20305,7 +20305,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.2+46.sha-f4ef8a9');
+    var VERSION = new Version('10.0.0-next.2+47.sha-28ff02b');
 
     /**
      * @license
