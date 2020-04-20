e248ffaad3354751316a36c40447c262c3d33f31
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 20:43:30 +0000

    254b9ea44c docs: place download section in accessibility to the top (#36561)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+46.sha-2a53f47
+ * @license Angular v9.1.2+47.sha-254b9ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28273,7 +28273,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.2+46.sha-2a53f47');
+const VERSION = new Version('9.1.2+47.sha-254b9ea');
 
 /**
  * @fileoverview added by tsickle
