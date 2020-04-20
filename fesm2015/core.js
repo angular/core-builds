3389a7cea32896d91050f545052682146fd69b03
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:02:48 +0000

    b5e92b9a5d Revert "fix(router): pass correct component to canDeactivate checks when using two or more sibling router-outlets (#36302)" (#36697)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+47.sha-28ff02b
+ * @license Angular v10.0.0-next.2+48.sha-b5e92b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28273,7 +28273,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+47.sha-28ff02b');
+const VERSION = new Version('10.0.0-next.2+48.sha-b5e92b9');
 
 /**
  * @fileoverview added by tsickle
