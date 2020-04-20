3461d660995fa43eade57e1fbfca78984cef9939
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 20:44:32 +0000

    458dc10ddc docs: place download section in accessibility to the top (#36561)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+49.sha-4341743
+ * @license Angular v10.0.0-next.2+50.sha-458dc10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20274,7 +20274,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.2+49.sha-4341743');
+    var VERSION = new Version('10.0.0-next.2+50.sha-458dc10');
 
     /**
      * @license
