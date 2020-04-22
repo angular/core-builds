5c52a3f7f5259ff1e454fe3304f63b57148a5bb0
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:23:58 +0000

    535c249f3a build: update `REQUIRED_BASE_SHA` in merge script to `commit-message` script update commit (#36750)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+56.sha-942b986
+ * @license Angular v10.0.0-next.2+57.sha-535c249
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20118,7 +20118,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+56.sha-942b986');
+var VERSION = new Version('10.0.0-next.2+57.sha-535c249');
 
 /**
  * @license
