a73270365efd361ff03393e2e8806e0eca241e00
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 19:16:22 +0000

    e1071615c6 release: cut the v9.1.3 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+56.sha-8bd5374
+ * @license Angular v9.1.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28288,7 +28288,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.2+56.sha-8bd5374');
+const VERSION = new Version('9.1.3');
 
 /**
  * @fileoverview added by tsickle
