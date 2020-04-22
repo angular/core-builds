47f9cfa277bf7b906b6ea908cc39fda79bf209ce
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:33:22 +0000

    d40bcce84e build: add AndrewKushnir to the `fw-testing` group (#36744)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+2.sha-aa94cd5
+ * @license Angular v9.1.3+6.sha-d40bcce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20319,7 +20319,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.3+2.sha-aa94cd5');
+    var VERSION = new Version('9.1.3+6.sha-d40bcce');
 
     /**
      * @license
