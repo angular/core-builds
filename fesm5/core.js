0a7aef78cb0006e134877184f58d2a8140c50378
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:37:03 +0000

    e5d2853070 docs: correct ngSwitch definition (#35489)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+13.sha-dd806b0
+ * @license Angular v10.0.0-next.3+16.sha-e5d2853
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20118,7 +20118,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+13.sha-dd806b0');
+var VERSION = new Version('10.0.0-next.3+16.sha-e5d2853');
 
 /**
  * @license
