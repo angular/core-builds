c102489ff8fa9c93a8c34c4ae1797d81ca8fa878
Andrew Kushnir <akushnir@google.com>
2020-04-23 20:54:04 +0000

    e6161ca307 build(core): use dev-infra's component_benchmark to show PoC (#36434)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+21.sha-e37f266
+ * @license Angular v10.0.0-next.3+24.sha-e6161ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28288,7 +28288,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+21.sha-e37f266');
+const VERSION = new Version('10.0.0-next.3+24.sha-e6161ca');
 
 /**
  * @fileoverview added by tsickle
