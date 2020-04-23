7ae9c2694e2eca1a55c6df3d8a3e0226f66ff359
Andrew Kushnir <akushnir@google.com>
2020-04-23 20:56:58 +0000

    d840503d35 build(core): use dev-infra's component_benchmark to show PoC (#36434)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+15.sha-f898c9a
+ * @license Angular v9.1.3+18.sha-d840503
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20319,7 +20319,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.3+15.sha-f898c9a');
+    var VERSION = new Version('9.1.3+18.sha-d840503');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+15.sha-f898c9a
+ * @license Angular v9.1.3+18.sha-d840503
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28288,7 +28288,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+15.sha-f898c9a');
+const VERSION = new Version('9.1.3+18.sha-d840503');
 
 /**
  * @fileoverview added by tsickle
