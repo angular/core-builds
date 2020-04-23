4886a2c059452319b5b4effc9fb81abc31564a62
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:38:20 +0000

    f0ec4ae941 ci: fix pullapprove incorrectly skipping fw-compiler as owner (#36661)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+16.sha-e5d2853
+ * @license Angular v10.0.0-next.3+19.sha-f0ec4ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20118,7 +20118,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+16.sha-e5d2853');
+var VERSION = new Version('10.0.0-next.3+19.sha-f0ec4ae');
 
 /**
  * @license
