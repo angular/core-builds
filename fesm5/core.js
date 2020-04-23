1aa790f0712fcc16878805031d49da1847c9d528
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:27:34 +0000

    ceb61d10c1 test(router): add canDeactivate test for forChild route (#36699)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+12.sha-113c75d
+ * @license Angular v10.0.0-next.3+14.sha-ceb61d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20149,7 +20149,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+12.sha-113c75d');
+var VERSION = new Version('10.0.0-next.3+14.sha-ceb61d1');
 
 /**
  * @license
