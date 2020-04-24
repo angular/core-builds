7bc3523701c75ab6533f2be556e1243b5270275b
Andrew Kushnir <akushnir@google.com>
2020-04-24 18:26:17 +0000

    ad8c4cdd75 revert: "feat(dev-infra): exposed new rule 'component_benchmark' via dev_infra (#36434)" (#36798)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+27.sha-76190c7
+ * @license Angular v10.0.0-next.3+30.sha-ad8c4cd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20118,7 +20118,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+27.sha-76190c7');
+var VERSION = new Version('10.0.0-next.3+30.sha-ad8c4cd');
 
 /**
  * @license
