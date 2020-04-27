3f333c545d571a91838d2c4e233f2e312ca69ea2
Andrew Kushnir <akushnir@google.com>
2020-04-27 21:07:44 +0000

    00e6cb1d62 feat(router): allow CanLoad guard to return UrlTree (#36610)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+33.sha-b34fb04
+ * @license Angular v10.0.0-next.3+35.sha-00e6cb1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20118,7 +20118,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+33.sha-b34fb04');
+var VERSION = new Version('10.0.0-next.3+35.sha-00e6cb1');
 
 /**
  * @license
