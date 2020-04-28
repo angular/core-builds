3b3509d00c62f5481faa2b69f080abc243962f91
Andrew Kushnir <akushnir@google.com>
2020-04-28 16:42:03 +0000

    0ce96f1d78 refactor(localize): remove unused code (#36834)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+28.sha-0f389fa
+ * @license Angular v9.1.3+34.sha-0ce96f1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20170,7 +20170,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.3+28.sha-0f389fa');
+var VERSION = new Version('9.1.3+34.sha-0ce96f1');
 
 /**
  * @license
