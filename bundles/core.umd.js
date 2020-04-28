00dca9ad560c242184bc555f9680db00325d54b1
Andrew Kushnir <akushnir@google.com>
2020-04-28 19:48:20 +0000

    70dd27ffd8 fix(compiler): normalize line endings in ICU expansions (#36741)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+38.sha-7c8c413
+ * @license Angular v10.0.0-next.3+43.sha-70dd27f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20309,7 +20309,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.3+38.sha-7c8c413');
+    var VERSION = new Version('10.0.0-next.3+43.sha-70dd27f');
 
     /**
      * @license
