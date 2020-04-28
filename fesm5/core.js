6b1d057dad44560e7b7f440633942b31915c52c0
Andrew Kushnir <akushnir@google.com>
2020-04-28 23:19:32 +0000

    c440165384 fix(ngcc): recognize enum declarations emitted in JavaScript (#36550)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+34.sha-0ce96f1
+ * @license Angular v9.1.3+35.sha-c440165
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20170,7 +20170,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.3+34.sha-0ce96f1');
+var VERSION = new Version('9.1.3+35.sha-c440165');
 
 /**
  * @license
