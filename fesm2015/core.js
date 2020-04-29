902c3f41185478485b56b49828cb4593928aab99
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:38:45 +0000

    52d2e46700 release: cut the v10.0.0-next.4 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+48.sha-c89421b
+ * @license Angular v10.0.0-next.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28340,7 +28340,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+48.sha-c89421b');
+const VERSION = new Version('10.0.0-next.4');
 
 /**
  * @fileoverview added by tsickle
