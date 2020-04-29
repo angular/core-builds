6b51aaeddef264062a34a46c94e7f4c16892ef5e
Andrew Kushnir <akushnir@google.com>
2020-04-29 20:24:12 +0000

    420c73c722 build: use sh instead of exec for release scripts (#36862)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+37.sha-62930aa
+ * @license Angular v9.1.3+38.sha-420c73c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28340,7 +28340,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+37.sha-62930aa');
+const VERSION = new Version('9.1.3+38.sha-420c73c');
 
 /**
  * @fileoverview added by tsickle
