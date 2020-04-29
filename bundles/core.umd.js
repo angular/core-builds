af7b56cafd4cd45a3ece580c83632045e6a9d68b
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:53:57 +0000

    45c09416ed refactor(ngcc): move `PathMappings` to separate file to avoid circular dependency (#36626)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+1.sha-62ba0ac
+ * @license Angular v10.0.0-next.4+13.sha-45c0941
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20328,7 +20328,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.4+1.sha-62ba0ac');
+    var VERSION = new Version('10.0.0-next.4+13.sha-45c0941');
 
     /**
      * @license
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+1.sha-62ba0ac
+ * @license Angular v10.0.0-next.4+13.sha-45c0941
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20158,7 +20158,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.4+1.sha-62ba0ac');
+var VERSION = new Version('10.0.0-next.4+13.sha-45c0941');
 
 /**
  * @license
