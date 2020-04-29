11baec78c772ad03c97b5581f5755bc170b1bdc7
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:21:00 +0000

    9b961a410a release: cut the v9.1.4 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+38.sha-420c73c
+ * @license Angular v9.1.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28340,7 +28340,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+38.sha-420c73c');
+const VERSION = new Version('9.1.4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+38.sha-420c73c
+ * @license Angular v9.1.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20189,7 +20189,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.3+38.sha-420c73c');
+var VERSION = new Version('9.1.4');
 
 /**
  * @license
