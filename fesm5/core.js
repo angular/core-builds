3d68ba12b8deaaf91ba734020de7b0be38f6e068
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:23:59 +0000

    12fcc7cafe build(language-service): make test project a filegroup (#36865)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+15.sha-49be32c
+ * @license Angular v10.0.0-next.4+31.sha-12fcc7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20267,7 +20267,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.4+15.sha-49be32c');
+var VERSION = new Version('10.0.0-next.4+31.sha-12fcc7c');
 
 /**
  * @license
