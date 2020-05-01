338dfbfb64a211f21c590cfd747ae51528132796
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:25:04 +0000

    e9300c979e build: use @angular/dev-infra-private for bazel stamping (#36844)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+30.sha-95a407d
+ * @license Angular v10.0.0-next.4+29.sha-e9300c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20267,7 +20267,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.4+30.sha-95a407d');
+var VERSION = new Version('10.0.0-next.4+29.sha-e9300c9');
 
 /**
  * @license
