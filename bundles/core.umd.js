ea688594ca5b419b9e6123e1ec1f0cecd4d0e1de
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:07:25 +0000

    297dab845d build: migrate bazel related formatting/linting to ng-dev format (#36842)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20437,7 +20437,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.4+34.sha-0f3831b');
+    var VERSION = new Version('10.0.0-next.4+36.sha-297dab8');
 
     /**
      * @license
