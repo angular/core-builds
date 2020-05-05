04b22ba970acfd90daa365d3041e2bc763bd8f96
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:17:17 +0000

    306f46ce92 build(docs-infra): upgrade docs examples to latest Angular CLI and framework (#36145)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+9.sha-70b25a3
+ * @license Angular v10.0.0-next.5+7.sha-306f46c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20267,7 +20267,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+9.sha-70b25a3');
+var VERSION = new Version('10.0.0-next.5+7.sha-306f46c');
 
 /**
  * @license
