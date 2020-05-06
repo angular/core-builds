1e4b2c23343a9de05d99be75f5d797bda599d668
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:50:07 +0000

    f7815cf96d test(compiler-cli): ensure reflection tests are not brittle to case-sensitivity (#36859)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+61.sha-f930e75
+ * @license Angular v10.0.0-next.5+71.sha-f7815cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28311,7 +28311,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+61.sha-f930e75');
+const VERSION = new Version('10.0.0-next.5+71.sha-f7815cf');
 
 /**
  * @fileoverview added by tsickle
