62ade9e26904930db0f1bebd63b5d09a56d051be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 01:12:15 +0000

    388dc93cee feat: remove @angular/http (#27038)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+38.sha-da79e04
+ * @license Angular v10.0.0-next.5+25.sha-388dc93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28432,7 +28432,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+38.sha-da79e04');
+const VERSION = new Version('10.0.0-next.5+25.sha-388dc93');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+38.sha-da79e04
+ * @license Angular v10.0.0-next.5+25.sha-388dc93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20244,7 +20244,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+38.sha-da79e04');
+var VERSION = new Version('10.0.0-next.5+25.sha-388dc93');
 
 /**
  * @license
