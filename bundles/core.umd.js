80a4c79cb560d60a229d9d7ed3199965f99d6ea4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:08:48 +0000

    42d1091d6a fix(compiler-cli): don't try to tag non-ts files as shims (#36987)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+73.sha-d0280a0
+ * @license Angular v10.0.0-next.5+74.sha-42d1091
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20325,7 +20325,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.5+73.sha-d0280a0');
+    var VERSION = new Version('10.0.0-next.5+74.sha-42d1091');
 
     /**
      * @license
