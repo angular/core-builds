417ddb7432121b4360c70f60c0fe4f574ca1431c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 19:01:10 +0000

    de0c71c467 docs: release notes for the v9.1.6 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+6.sha-178e750
+ * @license Angular v10.0.0-next.6+8.sha-de0c71c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20325,7 +20325,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.6+6.sha-178e750');
+    var VERSION = new Version('10.0.0-next.6+8.sha-de0c71c');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+6.sha-178e750
+ * @license Angular v10.0.0-next.6+8.sha-de0c71c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28311,7 +28311,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+6.sha-178e750');
+const VERSION = new Version('10.0.0-next.6+8.sha-de0c71c');
 
 /**
  * @fileoverview added by tsickle
