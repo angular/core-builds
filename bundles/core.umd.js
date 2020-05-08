ac8e4c76ee837de8397b9422586441df50a2fc83
Misko Hevery <misko@hevery.com>
2020-05-08 22:04:10 +0000

    bd30c37040 fix(docs-infra): add visual signal for external links (#36601)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+16.sha-eb34aa5
+ * @license Angular v10.0.0-next.6+17.sha-bd30c37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20356,7 +20356,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.6+16.sha-eb34aa5');
+    var VERSION = new Version('10.0.0-next.6+17.sha-bd30c37');
 
     /**
      * @license
