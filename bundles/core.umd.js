51890fd0b8ccb90cd8c5d93fd2b97255ab011761
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:02:40 +0000

    fdd61152c5 docs: replace into with in (#36778)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+3.sha-7cc332ef
+ * @license Angular v10.0.0-next.6+4.sha-fdd6115
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20325,7 +20325,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.6+3.sha-7cc332ef');
+    var VERSION = new Version('10.0.0-next.6+4.sha-fdd6115');
 
     /**
      * @license
