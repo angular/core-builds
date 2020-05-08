860638744ebe41b75c4292d45d4ac7f77cbffe8c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:01:35 +0000

    7cc332efe6 docs: update aot compiler docs (#35487)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6
+ * @license Angular v10.0.0-next.6+3.sha-7cc332ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20325,7 +20325,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.6');
+    var VERSION = new Version('10.0.0-next.6+3.sha-7cc332ef');
 
     /**
      * @license
