a81e568b66719e370befe3f93ff1181b60b97495
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 00:29:27 +0000

    b6bc0aeb61 release: cut the v10.0.0-next.6 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+74.sha-42d1091
+ * @license Angular v10.0.0-next.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28311,7 +28311,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+74.sha-42d1091');
+const VERSION = new Version('10.0.0-next.6');
 
 /**
  * @fileoverview added by tsickle
