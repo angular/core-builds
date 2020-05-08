1de16cab2c01c28595d6776bfc6e60b1de7a166c
Misko Hevery <misko@hevery.com>
2020-05-08 22:01:52 +0000

    1142c378fd feat(language-service): [ivy] wrap ngtsc to handle typecheck files (#36930)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+14.sha-82a3bd5
+ * @license Angular v10.0.0-next.6+15.sha-1142c37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20356,7 +20356,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.6+14.sha-82a3bd5');
+    var VERSION = new Version('10.0.0-next.6+15.sha-1142c37');
 
     /**
      * @license
