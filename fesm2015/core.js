8035b03b30eb767168b00069889fb72ed108dc35
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:03:34 +0000

    14be55c9fa docs(common): remove mention of the deprecated (#36953) web worker platform. Minor grammar/stylistic changes.

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+7.sha-39e066a
+ * @license Angular v10.0.0-next.6+5.sha-14be55c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28328,7 +28328,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+7.sha-39e066a');
+const VERSION = new Version('10.0.0-next.6+5.sha-14be55c');
 
 /**
  * @fileoverview added by tsickle
