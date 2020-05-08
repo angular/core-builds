19f45b469209fe90392615e060b78ad16b49850d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 18:57:11 +0000

    451e7e2364 release: cut the v9.1.6 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.5+16.sha-99b63b6
+ * @license Angular v9.1.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28449,7 +28449,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.5+16.sha-99b63b6');
+const VERSION = new Version('9.1.6');
 
 /**
  * @fileoverview added by tsickle
