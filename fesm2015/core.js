e148c00096ca089c00bdb89c58d48d703babd098
Misko Hevery <misko@hevery.com>
2020-05-11 18:18:35 +0000

    7eb1a58b26 docs: refactor form validation doc to be task-oriented (#36374)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+20.sha-28f3c1c
+ * @license Angular v10.0.0-next.6+21.sha-7eb1a58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28057,7 +28057,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+20.sha-28f3c1c');
+const VERSION = new Version('10.0.0-next.6+21.sha-7eb1a58');
 
 /**
  * @fileoverview added by tsickle
