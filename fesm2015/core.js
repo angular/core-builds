8e006299d9ffc7d633c04280af05bf7dde401fa1
Misko Hevery <misko@hevery.com>
2020-05-11 18:30:29 +0000

    40025f55ad refactor(language-service): move TS utils to separate file (#36984)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+23.sha-286fbf4
+ * @license Angular v10.0.0-next.6+22.sha-40025f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28040,7 +28040,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+23.sha-286fbf4');
+const VERSION = new Version('10.0.0-next.6+22.sha-40025f5');
 
 /**
  * @fileoverview added by tsickle
