666b6faa4c5c07ee18308ff2c873a273f9193729
Misko Hevery <misko@hevery.com>
2020-05-12 22:00:42 +0000

    7addc9f1a9 docs: add doc for template type guards to stuctural-directives.md (#34549)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+29.sha-cb6ddfc
+ * @license Angular v10.0.0-next.6+30.sha-7addc9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28040,7 +28040,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+29.sha-cb6ddfc');
+const VERSION = new Version('10.0.0-next.6+30.sha-7addc9f');
 
 /**
  * @fileoverview added by tsickle
