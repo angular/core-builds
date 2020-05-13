09dc5231aac80708d0b9c4bb12fb15cd2a725fff
Misko Hevery <misko@hevery.com>
2020-05-13 23:15:01 +0000

    141fcb95a4 refactor(localize): use the `FileSystem` from ngtsc (#36843)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+2.sha-019835c
+ * @license Angular v10.0.0-next.7+6.sha-141fcb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20107,7 +20107,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.7+2.sha-019835c');
+    var VERSION = new Version('10.0.0-next.7+6.sha-141fcb9');
 
     /**
      * @license
