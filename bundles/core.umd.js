6298e274d86b4fc78b9cafcf2c25034cee8a1878
Misko Hevery <misko@hevery.com>
2020-05-13 23:26:06 +0000

    2cacab92e6 docs: correct typos in doc for template type guards (#37090)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+15.sha-57a8686
+ * @license Angular v10.0.0-next.7+14.sha-2cacab9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19994,7 +19994,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.7+15.sha-57a8686');
+    var VERSION = new Version('10.0.0-next.7+14.sha-2cacab9');
 
     /**
      * @license
