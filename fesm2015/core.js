697773a820c21b637172b1c143fe3510cd62e086
Misko Hevery <misko@hevery.com>
2020-05-13 23:09:54 +0000

    255c2b2ff0 docs: refactor lifecycle hooks doc topic for task orientation (#36353)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7
+ * @license Angular v10.0.0-next.7+1.sha-255c2b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28040,7 +28040,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7');
+const VERSION = new Version('10.0.0-next.7+1.sha-255c2b2');
 
 /**
  * @fileoverview added by tsickle
