8c28a9a5ccef89df1ccf15f2bb4a791c750dd5a0
Kara Erickson <karakara@google.com>
2020-05-14 18:21:46 +0000

    ef9da2f619 refactor(dev-infra): add atscott for fw-core approvers (#37020)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+31.sha-92b97f7
+ * @license Angular v9.1.6+33.sha-ef9da2f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20209,7 +20209,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.6+31.sha-92b97f7');
+    var VERSION = new Version('9.1.6+33.sha-ef9da2f');
 
     /**
      * @license
