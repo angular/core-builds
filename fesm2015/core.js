b156e0b4403ee41ef1fada83228bd671ac6977ac
Kara Erickson <karakara@google.com>
2020-05-14 19:46:54 +0000

    8a56c99f87 docs(router): Update router guide to use UrlTree for guard redirects (#37100)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+47.sha-77c0ef3
+ * @license Angular v10.0.0-next.7+48.sha-8a56c99
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28021,7 +28021,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+47.sha-77c0ef3');
+const VERSION = new Version('10.0.0-next.7+48.sha-8a56c99');
 
 /**
  * @fileoverview added by tsickle
