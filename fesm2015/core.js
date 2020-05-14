1b28fe31ef66f2b2eb8555221e935ab36f866a39
Kara Erickson <karakara@google.com>
2020-05-14 19:24:00 +0000

    77c0ef38be docs(router): filter the event to subscribe (#37027)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+46.sha-5f64e52
+ * @license Angular v10.0.0-next.7+47.sha-77c0ef3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28004,7 +28004,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+46.sha-5f64e52');
+const VERSION = new Version('10.0.0-next.7+47.sha-77c0ef3');
 
 /**
  * @fileoverview added by tsickle
