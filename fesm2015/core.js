c2c90c73f7c73d41e91347c2ce16be411e61c7fa
Kara Erickson <karakara@google.com>
2020-05-15 17:43:53 +0000

    55c2433171 docs(changelog): remove duplicate features and bug fixes (#37101)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+61.sha-03afa36
+ * @license Angular v10.0.0-next.7+62.sha-55c2433
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28061,7 +28061,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+61.sha-03afa36');
+const VERSION = new Version('10.0.0-next.7+62.sha-55c2433');
 
 /**
  * @fileoverview added by tsickle
