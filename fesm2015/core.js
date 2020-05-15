6abd16293e3dbb10268de80b75890518bd505da2
Kara Erickson <karakara@google.com>
2020-05-15 18:51:53 +0000

    0912be47ff feat(dev-infra): create tool to determine conflicts created by a PR (#37051)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+49.sha-9548c7c
+ * @license Angular v9.1.6+50.sha-0912be4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28181,7 +28181,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.6+49.sha-9548c7c');
+const VERSION = new Version('9.1.6+50.sha-0912be4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+49.sha-9548c7c
+ * @license Angular v9.1.6+50.sha-0912be4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20055,7 +20055,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.6+49.sha-9548c7c');
+var VERSION = new Version('9.1.6+50.sha-0912be4');
 
 /**
  * @license
