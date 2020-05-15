5a1f962d6cd53a78df77d6c5e6de387b135cdf53
Kara Erickson <karakara@google.com>
2020-05-15 20:45:59 +0000

    7f58c579f3 docs(changelog): add breaking change note for ModuleWithProviders (#37121)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+63.sha-3d7c85b2
+ * @license Angular v10.0.0-next.7+64.sha-7f58c57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20004,7 +20004,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.7+63.sha-3d7c85b2');
+    var VERSION = new Version('10.0.0-next.7+64.sha-7f58c57');
 
     /**
      * @license
