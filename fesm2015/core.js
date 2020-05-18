d9c6ec6cf57de33408a5870d29bc5bea0918424a
Kara Erickson <karakara@google.com>
2020-05-18 17:45:32 +0000

    2ca2c8fe0f build: complete removal of gulp format (#37147)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8
+ * @license Angular v10.0.0-next.8+7.sha-2ca2c8f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28061,7 +28061,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8');
+const VERSION = new Version('10.0.0-next.8+7.sha-2ca2c8f');
 
 /**
  * @fileoverview added by tsickle
