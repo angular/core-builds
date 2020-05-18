82f613d25a241cee940581f4a91d0efcc3d6c091
Kara Erickson <karakara@google.com>
2020-05-18 17:01:39 +0000

    1754beb59c release: cut the v10.0.0-next.8 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+70.sha-844208f
+ * @license Angular v10.0.0-next.8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20035,7 +20035,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.7+70.sha-844208f');
+    var VERSION = new Version('10.0.0-next.8');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+70.sha-844208f
+ * @license Angular v10.0.0-next.8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28061,7 +28061,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+70.sha-844208f');
+const VERSION = new Version('10.0.0-next.8');
 
 /**
  * @fileoverview added by tsickle
