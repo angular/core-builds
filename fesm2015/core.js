c2b1787393fb8e396b2a81229a5f336ed572194a
Kara Erickson <karakara@google.com>
2020-05-18 17:48:51 +0000

    593e07bd21 build(docs-infra): upgrade cli command docs sources to dbde51375 (#37154)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+7.sha-2ca2c8f
+ * @license Angular v10.0.0-next.8+9.sha-593e07b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28061,7 +28061,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+7.sha-2ca2c8f');
+const VERSION = new Version('10.0.0-next.8+9.sha-593e07b');
 
 /**
  * @fileoverview added by tsickle
