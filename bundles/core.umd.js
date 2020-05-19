7b18de3e9ed5488bca4c6e8eaf88caa8016afdc6
Kara Erickson <karakara@google.com>
2020-05-19 22:26:28 +0000

    772c5b8f64 refactor: update to tslib 2.0 and move to direct dependencies (#37198)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+28.sha-4e96cdc
+ * @license Angular v10.0.0-next.8+27.sha-772c5b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20049,7 +20049,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.8+28.sha-4e96cdc');
+    var VERSION = new Version('10.0.0-next.8+27.sha-772c5b8');
 
     /**
      * @license
