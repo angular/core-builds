6a7106530e79cd5f8112160d57f54e92f64eae1b
Kara Erickson <karakara@google.com>
2020-05-19 22:42:02 +0000

    383f04b96d feat(dev-infra): register ts-node when reading configuration (#37196)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+27.sha-772c5b8
+ * @license Angular v10.0.0-next.8+29.sha-383f04b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28061,7 +28061,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+27.sha-772c5b8');
+const VERSION = new Version('10.0.0-next.8+29.sha-383f04b');
 
 /**
  * @fileoverview added by tsickle
