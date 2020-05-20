4dd1f4dd0918ba936cddff63a730d0e0e72b332c
Kara Erickson <karakara@google.com>
2020-05-20 22:11:21 +0000

    1465372a0e fix(elements): do not break when the constructor of an Angular Element is not called (#36114) (#37226)

--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.8+12.sha-3eee536
+ * @license Angular v9.1.8+19.sha-1465372
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20049,7 +20049,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.8+12.sha-3eee536');
+var VERSION = new Version('9.1.8+19.sha-1465372');
 
 /**
  * @license
