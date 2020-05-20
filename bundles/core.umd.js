4303a1db9644b8f588616c872bb27c3b600f8586
Kara Erickson <karakara@google.com>
2020-05-20 20:59:02 +0000

    3eee53603c docs: update docs for microbenchmarks (#37140)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.8+7.sha-19313f7
+ * @license Angular v9.1.8+12.sha-3eee536
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20219,7 +20219,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.8+7.sha-19313f7');
+    var VERSION = new Version('9.1.8+12.sha-3eee536');
 
     /**
      * @license
