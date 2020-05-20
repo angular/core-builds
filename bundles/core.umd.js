3febb2685a1a7d01577b6998d36cd3e8a9b2eb28
Kara Erickson <karakara@google.com>
2020-05-20 20:58:43 +0000

    59e7203a40 build: complete removal of bazel format yarn commands (#37148)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+13.sha-78b8d0e
+ * @license Angular v10.0.0-next.9+20.sha-59e7203
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20018,7 +20018,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.9+13.sha-78b8d0e');
+    var VERSION = new Version('10.0.0-next.9+20.sha-59e7203');
 
     /**
      * @license
