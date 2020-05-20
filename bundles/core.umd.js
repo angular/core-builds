798e1526b0bb746029d12820acfacb8a087a376a
Kara Erickson <karakara@google.com>
2020-05-20 17:05:53 +0000

    89b44d1900 fix(elements): do not break when the constructor of an Angular Element is not called (#36114)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+4.sha-4c30aa8
+ * @license Angular v10.0.0-next.9+11.sha-89b44d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20018,7 +20018,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-next.9+4.sha-4c30aa8');
+    var VERSION = new Version('10.0.0-next.9+11.sha-89b44d1');
 
     /**
      * @license
