06a4756f5f1033e86537e1b1778f6b5af82a5ac7
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 20:37:36 +0000

    f001aef2dc build: update requiredBaseCommit for patch branch merges (#37316)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+22.sha-98d8200
+ * @license Angular v10.0.0-rc.0+21.sha-f001aef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20018,7 +20018,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+22.sha-98d8200');
+    var VERSION = new Version('10.0.0-rc.0+21.sha-f001aef');
 
     /**
      * @license
