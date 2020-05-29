30117fdcbf08dcc2d5ca3e3f4acc55b722885778
Matias Niemelä <matias@yearofmoo.com>
2020-05-29 01:52:57 +0000

    82761ec50e docs: Mention Bazel builder and schematics in Deprecations section (#37190)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+11.sha-270da1f
+ * @license Angular v10.0.0-rc.0+22.sha-82761ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20049,7 +20049,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+11.sha-270da1f');
+    var VERSION = new Version('10.0.0-rc.0+22.sha-82761ec');
 
     /**
      * @license
