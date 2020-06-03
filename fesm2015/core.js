9c7c1f494cd253f41f7b00aad13a80f411f6deb6
atscott <atscott@google.com>
2020-06-03 22:49:51 +0000

    e35269dd87 docs: update file header to be correct (#37425)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+21.sha-60a03b7
+ * @license Angular v10.0.0-rc.2+22.sha-e35269d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19358,7 +19358,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+21.sha-60a03b7');
+const VERSION = new Version('10.0.0-rc.2+22.sha-e35269d');
 
 /**
  * @license
