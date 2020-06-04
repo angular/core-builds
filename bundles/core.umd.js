e9a597ba86d0d3cbdc3b03bbd63f0e3842faeeb9
atscott <atscott@google.com>
2020-06-04 20:03:03 +0000

    d713e33cc4 style(dev-infra): correct tslint failures in dev-infra directory (#37233)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+45.sha-9ec25ea
+ * @license Angular v10.0.0-rc.2+48.sha-d713e33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19981,7 +19981,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.2+45.sha-9ec25ea');
+    var VERSION = new Version('10.0.0-rc.2+48.sha-d713e33');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+45.sha-9ec25ea
+ * @license Angular v10.0.0-rc.2+48.sha-d713e33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19358,7 +19358,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+45.sha-9ec25ea');
+const VERSION = new Version('10.0.0-rc.2+48.sha-d713e33');
 
 /**
  * @license
