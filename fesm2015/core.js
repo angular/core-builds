17916ba2929e0d9abb8a5e9336862adbae9dbdfe
atscott <atscott@google.com>
2020-06-04 18:04:31 +0000

    9ec25ea036 refactor(dev-infra): change required base commit sha (#37424)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+43.sha-5f0be3c
+ * @license Angular v10.0.0-rc.2+45.sha-9ec25ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19358,7 +19358,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+43.sha-5f0be3c');
+const VERSION = new Version('10.0.0-rc.2+45.sha-9ec25ea');
 
 /**
  * @license
