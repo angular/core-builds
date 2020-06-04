04396a984f1ebe8456fcb85671c7dfa14cde93b7
atscott <atscott@google.com>
2020-06-04 23:25:09 +0000

    eb6ba9ac80 docs: fix various typos (#37443)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+73.sha-4e7a454
+ * @license Angular v10.0.0-rc.0+74.sha-eb6ba9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19950,7 +19950,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+73.sha-4e7a454');
+    var VERSION = new Version('10.0.0-rc.0+74.sha-eb6ba9a');
 
     /**
      * @license
