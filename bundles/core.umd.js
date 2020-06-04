56a883c2890f96d9986089328547021f13d5858d
atscott <atscott@google.com>
2020-06-04 18:05:21 +0000

    c8f7fc22c7 refactor(dev-infra): change required base commit sha (#37424)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+68.sha-c025357
+ * @license Angular v10.0.0-rc.0+70.sha-c8f7fc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19981,7 +19981,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+68.sha-c025357');
+    var VERSION = new Version('10.0.0-rc.0+70.sha-c8f7fc2');
 
     /**
      * @license
