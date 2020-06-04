581e5cc267524e6f73433292ad529b02fded2f07
atscott <atscott@google.com>
2020-06-04 20:04:48 +0000

    dffcca73e4 fix(dev-infra): clean up usages within pullapprove tooling (#37338)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+69.sha-b2bd386
+ * @license Angular v10.0.0-rc.0+71.sha-dffcca7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19358,7 +19358,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+69.sha-b2bd386');
+const VERSION = new Version('10.0.0-rc.0+71.sha-dffcca7');
 
 /**
  * @license
