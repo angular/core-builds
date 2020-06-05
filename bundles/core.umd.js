450d837df55c05d0d29618abafb511ba1fd6fc36
atscott <atscott@google.com>
2020-06-05 16:42:09 +0000

    2cb3b66640 fix(ngcc): find decorated constructor params on IIFE wrapped classes (#37436)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+50.sha-fff424a
+ * @license Angular v10.0.0-rc.2+53.sha-2cb3b66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19981,7 +19981,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.2+50.sha-fff424a');
+    var VERSION = new Version('10.0.0-rc.2+53.sha-2cb3b66');
 
     /**
      * @license
