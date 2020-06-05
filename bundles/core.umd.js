a6bfea533d7ae8b35e6afc92183339bea3f0ae1a
atscott <atscott@google.com>
2020-06-05 18:26:27 +0000

    17996bf719 fix(dev-infra): properly determine oauth scopes for git client token (#37462)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+79.sha-85b6c94
+ * @license Angular v10.0.0-rc.0+82.sha-17996bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19981,7 +19981,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+79.sha-85b6c94');
+    var VERSION = new Version('10.0.0-rc.0+82.sha-17996bf');
 
     /**
      * @license
