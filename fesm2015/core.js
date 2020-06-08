e3b66b56dd1f5f8af342c2a009484dd00d62ec39
atscott <atscott@google.com>
2020-06-08 16:52:30 +0000

    31b3888a2f style(ngcc): post-merge review tidy up (#37461)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+59.sha-75294e7
+ * @license Angular v10.0.0-rc.2+64.sha-31b3888
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19358,7 +19358,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+59.sha-75294e7');
+const VERSION = new Version('10.0.0-rc.2+64.sha-31b3888');
 
 /**
  * @license
