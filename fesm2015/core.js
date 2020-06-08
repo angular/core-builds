b1b8456a294165835fa7495fe638ddcd62b85989
atscott <atscott@google.com>
2020-06-08 17:46:44 +0000

    8bead6bfdd test(language-service): Remove all markers from test project (#37475)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+65.sha-52dda73
+ * @license Angular v10.0.0-rc.2+66.sha-8bead6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19358,7 +19358,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+65.sha-52dda73');
+const VERSION = new Version('10.0.0-rc.2+66.sha-8bead6b');
 
 /**
  * @license
