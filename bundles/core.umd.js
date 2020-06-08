0e945710b568738df3ebe47196790f7f83b8994c
atscott <atscott@google.com>
2020-06-08 16:41:04 +0000

    77efddcac3 fix(dev-infra): await setup in runBenchmark (#37428)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+85.sha-0c75a06
+ * @license Angular v10.0.0-rc.0+87.sha-77efddc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19981,7 +19981,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+85.sha-0c75a06');
+    var VERSION = new Version('10.0.0-rc.0+87.sha-77efddc');
 
     /**
      * @license
