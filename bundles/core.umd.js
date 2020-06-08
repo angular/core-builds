347fabbfdd4ff79a841bd72f1f2854dcbc08d45a
Pete Bacon Darwin <pete@bacondarwin.com>
2020-06-08 18:10:32 +0000

    1aae94a421 perf(ngcc): cache parsed tsconfig between runs

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.9+17.sha-f0f319b
+ * @license Angular v9.1.9+18.sha-1aae94a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20219,7 +20219,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.9+17.sha-f0f319b');
+    var VERSION = new Version('9.1.9+18.sha-1aae94a');
 
     /**
      * @license
