e177afb1d4e3112f73b7eaddea4862fde47843e6
atscott <atscott@google.com>
2020-06-08 22:25:08 +0000

    a1616ce181 docs: add note on publishing libraries in ivy (#36556)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+70.sha-1c22dff
+ * @license Angular v10.0.0-rc.2+71.sha-a1616ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19990,7 +19990,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.2+70.sha-1c22dff');
+    var VERSION = new Version('10.0.0-rc.2+71.sha-a1616ce');
 
     /**
      * @license
