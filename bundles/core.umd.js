fa0d8b6fb8e37e141ce1fbc05d9aec0f38a42b22
atscott <atscott@google.com>
2020-06-09 15:52:27 +0000

    ad6d2b4619 docs(core): fix path referenced in comments of both compiler facade interface files (#37370)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+3.sha-bf2cb6f
+ * @license Angular v10.0.0-rc.3+10.sha-ad6d2b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19990,7 +19990,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.3+3.sha-bf2cb6f');
+    var VERSION = new Version('10.0.0-rc.3+10.sha-ad6d2b4');
 
     /**
      * @license
