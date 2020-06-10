64384d3b547dbc439184eb658dfac3352411d066
atscott <atscott@google.com>
2020-06-10 15:43:10 +0000

    9d397eb5a1 Revert "build: remove wombot proxy registry from package.jsons for release (#37378)" (#37495)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+13.sha-6114cd2
+ * @license Angular v10.0.0-rc.3+14.sha-9d397eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19990,7 +19990,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.3+13.sha-6114cd2');
+    var VERSION = new Version('10.0.0-rc.3+14.sha-9d397eb');
 
     /**
      * @license
