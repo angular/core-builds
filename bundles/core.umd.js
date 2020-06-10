ef45da2aef6242430761153adc8898e874c94421
atscott <atscott@google.com>
2020-06-10 19:14:21 +0000

    e836eaf448 fix(docs-infra): preconnect to analytics origins (#37400)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+121.sha-a398d6a
+ * @license Angular v10.0.0-rc.0+120.sha-e836eaf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19990,7 +19990,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+121.sha-a398d6a');
+    var VERSION = new Version('10.0.0-rc.0+120.sha-e836eaf');
 
     /**
      * @license
