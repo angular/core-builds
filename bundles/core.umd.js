f5b134168d81153c36a2bf76a926c97af903012a
atscott <atscott@google.com>
2020-06-10 16:49:17 +0000

    401ef71ae5 fix(compiler-cli): downlevel angular decorators to static properties (#37382)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+113.sha-0a1d078
+ * @license Angular v10.0.0-rc.0+114.sha-401ef71
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19959,7 +19959,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+113.sha-0a1d078');
+    var VERSION = new Version('10.0.0-rc.0+114.sha-401ef71');
 
     /**
      * @license
