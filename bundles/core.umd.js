e343129eebbe4d6c740ecbcaf7ba5c3cfc7b96cd
atscott <atscott@google.com>
2020-06-10 19:15:07 +0000

    a398d6af45 docs: remove redundant web-worker nav (#37289)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+119.sha-ae36486
+ * @license Angular v10.0.0-rc.0+121.sha-a398d6a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19959,7 +19959,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+119.sha-ae36486');
+    var VERSION = new Version('10.0.0-rc.0+121.sha-a398d6a');
 
     /**
      * @license
