e6d573d74e8cca9aa4713adaaa7e003f99492a80
atscott <atscott@google.com>
2020-06-10 19:14:08 +0000

    d76a872efb docs: add Mike Huang as a GDE (#36472)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+117.sha-021dd33
+ * @license Angular v10.0.0-rc.0+122.sha-d76a872
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19959,7 +19959,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+117.sha-021dd33');
+    var VERSION = new Version('10.0.0-rc.0+122.sha-d76a872');
 
     /**
      * @license
