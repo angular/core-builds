7ee32c442b96b86d63b5d341e29b66d90fc694d2
Misko Hevery <misko@hevery.com>
2020-06-11 20:30:16 +0000

    a937889c3b refactor(elements): add accessor workaround for build-optimizer (#37456)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+130.sha-8c682c5
+ * @license Angular v10.0.0-rc.0+134.sha-a937889
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19990,7 +19990,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+130.sha-8c682c5');
+    var VERSION = new Version('10.0.0-rc.0+134.sha-a937889');
 
     /**
      * @license
