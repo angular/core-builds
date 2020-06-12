c206dd1589d0e8f2b648002ab78411566722c204
Misko Hevery <misko@hevery.com>
2020-06-12 02:18:36 +0000

    284123c6ba fix(core): should fake a top event task when coalescing events to prevent draining microTaskQueue too early. (#36841)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+136.sha-7e5f6c2
+ * @license Angular v10.0.0-rc.0+138.sha-284123c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19315,7 +19315,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+136.sha-7e5f6c2');
+const VERSION = new Version('10.0.0-rc.0+138.sha-284123c');
 
 /**
  * @license
@@ -27095,9 +27095,23 @@ function delayChangeDetectionForEvents(zone) {
         return;
     }
     zone.lastRequestAnimationFrameId = zone.nativeRequestAnimationFrame.call(_global, () => {
-        zone.lastRequestAnimationFrameId = -1;
-        updateMicroTaskStatus(zone);
-        checkStable(zone);
+        // This is a work around for https://github.com/angular/angular/issues/36839.
+        // The core issue is that when event coalescing is enabled it is possible for microtasks
+        // to get flushed too early (As is the case with `Promise.then`) between the
+        // coalescing eventTasks.
+        //
+        // To workaround this we schedule a "fake" eventTask before we process the
+        // coalescing eventTasks. The benefit of this is that the "fake" container eventTask
+        //  will prevent the microtasks queue from getting drained in between the coalescing
+        // eventTask execution.
+        if (!zone.fakeTopEventTask) {
+            zone.fakeTopEventTask = Zone.root.scheduleEventTask('fakeTopEventTask', () => {
+                zone.lastRequestAnimationFrameId = -1;
+                updateMicroTaskStatus(zone);
+                checkStable(zone);
+            }, undefined, () => { }, () => { });
+        }
+        zone.fakeTopEventTask.invoke();
     });
     updateMicroTaskStatus(zone);
 }
