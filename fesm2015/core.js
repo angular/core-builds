763cef3edbcb906b057ae1254b0f340e8e5253a0
Misko Hevery <misko@hevery.com>
2020-06-12 22:25:40 +0000

    1bc807c81a style(common): enforce format on newly included files (#36940)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+161.sha-8dcf618
+ * @license Angular v10.0.0-rc.0+164.sha-1bc807c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+161.sha-8dcf618');
+const VERSION = new Version('10.0.0-rc.0+164.sha-1bc807c');
 
 /**
  * @license
