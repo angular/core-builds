b74263aed53215a0427bd3279fa9063504d976ba
Misko Hevery <misko@hevery.com>
2020-06-12 16:10:10 +0000

    aba33de5f5 build: fix outdated payload size (#37551)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+159.sha-a075260
+ * @license Angular v10.0.0-rc.0+158.sha-aba33de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+159.sha-a075260');
+const VERSION = new Version('10.0.0-rc.0+158.sha-aba33de');
 
 /**
  * @license
