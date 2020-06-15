eaa1564b60300342d9b9f3783afe06eb4260538e
Misko Hevery <misko@hevery.com>
2020-06-15 17:12:24 +0000

    a7faa6bb65 docs(service-worker): minor fixes/improvements in the SW Communication guide (#37555)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+174.sha-adc9d5c
+ * @license Angular v10.0.0-rc.0+177.sha-a7faa6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+174.sha-adc9d5c');
+const VERSION = new Version('10.0.0-rc.0+177.sha-a7faa6b');
 
 /**
  * @license
