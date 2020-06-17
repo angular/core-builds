8c92185886fe335e321e62a27dc016cd3eb3465b
Misko Hevery <misko@hevery.com>
2020-06-17 18:43:29 +0000

    0d29259d9b docs: move ng-vikings 2020 to the already presented section (#37466)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+7.sha-26b0f3d
+ * @license Angular v10.0.0-rc.6+8.sha-0d29259
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19990,7 +19990,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.6+7.sha-26b0f3d');
+    var VERSION = new Version('10.0.0-rc.6+8.sha-0d29259');
 
     /**
      * @license
