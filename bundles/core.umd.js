18c832d7cc3e98b4d0277f2f02fb974e31ed944d
Misko Hevery <misko@hevery.com>
2020-06-18 23:29:34 +0000

    1d844b9bd8 docs(core): correct type for opts.read (#37626)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+195.sha-53e95233
+ * @license Angular v10.0.0-rc.0+196.sha-1d844b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19959,7 +19959,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+195.sha-53e95233');
+    var VERSION = new Version('10.0.0-rc.0+196.sha-1d844b9');
 
     /**
      * @license
