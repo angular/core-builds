11fcd769edb15853adc42e122e33a976a0158363
Misko Hevery <misko@hevery.com>
2020-06-18 23:26:20 +0000

    53e95233c3 refactor(dev-infra): cleanup shared package dependencies (#37594)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+198.sha-bd7f440
+ * @license Angular v10.0.0-rc.0+195.sha-53e95233
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+198.sha-bd7f440');
+const VERSION = new Version('10.0.0-rc.0+195.sha-53e95233');
 
 /**
  * @license
