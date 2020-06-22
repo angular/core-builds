ca39c568d2882e101b0eef4302a2efeb7e8bd0d5
Misko Hevery <misko@hevery.com>
2020-06-22 18:18:31 +0000

    d512e27979 docs: fix invalid anchor for CLI flags in deprecation guide (#37662)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+8.sha-0d29259
+ * @license Angular v10.0.0-rc.6+13.sha-d512e27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19990,7 +19990,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.6+8.sha-0d29259');
+    var VERSION = new Version('10.0.0-rc.6+13.sha-d512e27');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+8.sha-0d29259
+ * @license Angular v10.0.0-rc.6+13.sha-d512e27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.6+8.sha-0d29259');
+const VERSION = new Version('10.0.0-rc.6+13.sha-d512e27');
 
 /**
  * @license
