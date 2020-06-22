2f2b15665ca07f2111b42055c3fde826c1dbc078
Misko Hevery <misko@hevery.com>
2020-06-22 23:52:16 +0000

    4c1edd52c5 docs: Add content for new `strict` mode for Angular CLI (#37486)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+13.sha-d512e27
+ * @license Angular v10.0.0-rc.6+14.sha-4c1edd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19990,7 +19990,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.6+13.sha-d512e27');
+    var VERSION = new Version('10.0.0-rc.6+14.sha-4c1edd5');
 
     /**
      * @license
