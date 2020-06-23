a659437d2c0c7284dbd7455d26de1c2ab249e09c
Misko Hevery <misko@hevery.com>
2020-06-23 20:34:27 +0000

    36a00a255b ci(docs-infra): disable flaky tests (#37673)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+17.sha-82c8b44
+ * @license Angular v10.0.0-rc.6+20.sha-36a00a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.6+17.sha-82c8b44');
+const VERSION = new Version('10.0.0-rc.6+20.sha-36a00a2');
 
 /**
  * @license
