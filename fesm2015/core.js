a528fbd7c98ce6c4295e8aed5e3fa06208485410
Misko Hevery <misko@hevery.com>
2020-06-23 20:10:05 +0000

    e03adaf9b0 docs: add module/target compiler option migration docs (#37429)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+221.sha-38dfbc7
+ * @license Angular v10.0.0-rc.0+222.sha-e03adaf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19315,7 +19315,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+221.sha-38dfbc7');
+const VERSION = new Version('10.0.0-rc.0+222.sha-e03adaf');
 
 /**
  * @license
