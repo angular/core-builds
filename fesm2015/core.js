7bb402756ea1f0680ed3b1280b142cfdfd7f8da6
Andrew Kushnir <akushnir@google.com>
2020-06-23 19:15:21 +0000

    82c8b44db7 docs: Initial commit of update guide for v10 release. (#37152)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+14.sha-4c1edd5
+ * @license Angular v10.0.0-rc.6+17.sha-82c8b44
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.6+14.sha-4c1edd5');
+const VERSION = new Version('10.0.0-rc.6+17.sha-82c8b44');
 
 /**
  * @license
