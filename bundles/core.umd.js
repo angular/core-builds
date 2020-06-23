451f2f11d7d3b043478c3c7a7d745ee7a3377069
Andrew Kushnir <akushnir@google.com>
2020-06-23 19:18:49 +0000

    5229809adb docs: Initial commit of update guide for v10 release. (#37152)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+216.sha-5c40fd6
+ * @license Angular v10.0.0-rc.0+220.sha-5229809
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19956,7 +19956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+216.sha-5c40fd6');
+    var VERSION = new Version('10.0.0-rc.0+220.sha-5229809');
 
     /**
      * @license
