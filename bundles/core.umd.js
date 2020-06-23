2a58766086fc5516349c5d55b80f74b5ef4d6c2e
Misko Hevery <misko@hevery.com>
2020-06-23 20:36:36 +0000

    71c0363bb5 docs: add tslib update migration docs (#37402)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+222.sha-e03adaf
+ * @license Angular v10.0.0-rc.0+223.sha-71c0363
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19956,7 +19956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+222.sha-e03adaf');
+    var VERSION = new Version('10.0.0-rc.0+223.sha-71c0363');
 
     /**
      * @license
