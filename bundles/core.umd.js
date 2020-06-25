1ef222f6c07fae494ecb72f5181625f3ddb9bd27
Andrew Kushnir <akushnir@google.com>
2020-06-25 18:59:19 +0000

    8924ec1474 test(docs-infra): add end to end tests for api reference (#37612)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+231.sha-bedc045
+ * @license Angular v10.0.0-rc.0+229.sha-8924ec1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19956,7 +19956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+231.sha-bedc045');
+    var VERSION = new Version('10.0.0-rc.0+229.sha-8924ec1');
 
     /**
      * @license
