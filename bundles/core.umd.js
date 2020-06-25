7bb02cd59a49389e583ad854e2b182d26fe26106
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:02:30 +0000

    a0595f4b4f fix(forms): change error message (#37643)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+229.sha-8924ec1
+ * @license Angular v10.0.0-rc.0+230.sha-a0595f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19932,7 +19932,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+229.sha-8924ec1');
+    var VERSION = new Version('10.0.0-rc.0+230.sha-a0595f4');
 
     /**
      * @license
