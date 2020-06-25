a103c5bf78d8d072b44ee47b83f0e0a24eaf1e64
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:35:02 +0000

    e36d5b201a fix(forms): correct usage of `selectedOptions` (#37620)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+245.sha-8a74508
+ * @license Angular v10.0.0-rc.0+243.sha-e36d5b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19956,7 +19956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+245.sha-8a74508');
+    var VERSION = new Version('10.0.0-rc.0+243.sha-e36d5b2');
 
     /**
      * @license
