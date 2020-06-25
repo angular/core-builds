e989bfef0567ab502b534a7dfea61ac36c281952
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:33:10 +0000

    9318e23e64 perf(ngcc): use `EntryPointManifest` to speed up noop `ProgramBaseEntryPointFinder` (#37665)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+247.sha-2b2146b
+ * @license Angular v10.0.0-rc.0+255.sha-9318e23
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+247.sha-2b2146b');
+const VERSION = new Version('10.0.0-rc.0+255.sha-9318e23');
 
 /**
  * @license
