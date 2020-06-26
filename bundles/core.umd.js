bce6e88b8abea5866a1e4ad22850dd80bf1dcc03
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:24:29 +0000

    6341a837c1 docs: correct outdated dev instructions for public api golds (#37026)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+271.sha-dbc2364
+ * @license Angular v10.0.0-rc.0+274.sha-6341a83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19956,7 +19956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+271.sha-dbc2364');
+    var VERSION = new Version('10.0.0-rc.0+274.sha-6341a83');
 
     /**
      * @license
