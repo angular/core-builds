d0a8daa61aceb6ce3247bb7b156fd132749a41be
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:21:24 +0000

    c942662d79 fix(language-service): reinstate getExternalFiles() (#37750)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+273.sha-f954ab6
+ * @license Angular v10.0.0-rc.0+275.sha-c942662
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19956,7 +19956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+273.sha-f954ab6');
+    var VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+273.sha-f954ab6
+ * @license Angular v10.0.0-rc.0+275.sha-c942662
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+273.sha-f954ab6');
+const VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
 
 /**
  * @license
