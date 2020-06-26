6f42c74f275d102de64aacf768aefac395f96301
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:21:47 +0000

    dbc2364d16 fix(dev-infra): support running scripts from within a detached head (#37737)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+275.sha-c942662
+ * @license Angular v10.0.0-rc.0+271.sha-dbc2364
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19956,7 +19956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
+    var VERSION = new Version('10.0.0-rc.0+271.sha-dbc2364');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+275.sha-c942662
+ * @license Angular v10.0.0-rc.0+271.sha-dbc2364
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19346,7 +19346,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
+const VERSION = new Version('10.0.0-rc.0+271.sha-dbc2364');
 
 /**
  * @license
