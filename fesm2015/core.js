dce8c23c3f45f924734c7e77a21c9ef67aa0a243
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:27:42 +0000

    b2816a1536 docs: typo fixes for schematics-for-libraries.md (#37753)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+264.sha-8572e88
+ * @license Angular v10.0.0-rc.0+267.sha-b2816a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19315,7 +19315,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+264.sha-8572e88');
+const VERSION = new Version('10.0.0-rc.0+267.sha-b2816a1');
 
 /**
  * @license
