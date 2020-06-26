7427c15259fc6455f5a400d789ffb0574ca50738
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:30:29 +0000

    9c668e091f docs(elements): fixed command that adds the package @angular/elements (#37681)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+265.sha-4c7f32f
+ * @license Angular v10.0.0-rc.0+268.sha-9c668e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19932,7 +19932,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+265.sha-4c7f32f');
+    var VERSION = new Version('10.0.0-rc.0+268.sha-9c668e0');
 
     /**
      * @license
