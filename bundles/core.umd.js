ba6b180dba3b86614d55e3ef1f6fa5c1b8e82b72
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:18:41 +0000

    f954ab6f10 docs: add note about the month being zero-based in the Date constructor (#37770)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+272.sha-3ee6665
+ * @license Angular v10.0.0-rc.0+273.sha-f954ab6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19956,7 +19956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+272.sha-3ee6665');
+    var VERSION = new Version('10.0.0-rc.0+273.sha-f954ab6');
 
     /**
      * @license
