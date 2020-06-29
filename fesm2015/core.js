40c617afd37b9550bb3565f208e7e0cad418cc4e
Andrew Kushnir <akushnir@google.com>
2020-06-29 18:01:00 +0000

    eba9dd364e fix(elements): fire custom element output events during component initialization (#37570)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+291.sha-78a4476
+ * @license Angular v10.0.0-rc.0+292.sha-eba9dd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19306,7 +19306,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+291.sha-78a4476');
+const VERSION = new Version('10.0.0-rc.0+292.sha-eba9dd3');
 
 /**
  * @license
