13b0b0fd830e7758d0206339ae8a1f177bb0f989
Andrew Kushnir <akushnir@google.com>
2020-06-29 17:49:06 +0000

    78a44768e0 fix(ngcc): ensure lockfile is removed when analyzeFn fails (#37739)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+290.sha-98c047b
+ * @license Angular v10.0.0-rc.0+291.sha-78a4476
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19915,7 +19915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+290.sha-98c047b');
+    var VERSION = new Version('10.0.0-rc.0+291.sha-78a4476');
 
     /**
      * @license
