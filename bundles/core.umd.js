e2b2d9e0279c72c1c4d40cef73867c34306bd839
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:42:38 +0000

    cde4653c62 docs: Changing typo Stacblitz into Stackblitz in the Tour of Hereos tutorial docs page (#37794)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+294.sha-e07d940
+ * @license Angular v10.0.0-rc.0+295.sha-cde4653
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+294.sha-e07d940');
+    var VERSION = new Version('10.0.0-rc.0+295.sha-cde4653');
 
     /**
      * @license
