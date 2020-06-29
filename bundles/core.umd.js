0f9aca84e52a193884178d3662874e13f2328e69
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:24:20 +0000

    a177b1b7b1 docs: change definition of providedIn any (#35292)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+303.sha-e84539f
+ * @license Angular v10.0.0-rc.0+301.sha-a177b1b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19915,7 +19915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+303.sha-e84539f');
+    var VERSION = new Version('10.0.0-rc.0+301.sha-a177b1b');
 
     /**
      * @license
