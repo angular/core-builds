f7cbf2d6ad3e9e1e812cc76d2462817247e32b44
Andrew Kushnir <akushnir@google.com>
2020-06-30 19:19:52 +0000

    6fc80690ae docs: release notes for the v10.0.2 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+306.sha-f6d2705
+ * @license Angular v10.0.0-rc.0+307.sha-6fc8069
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19915,7 +19915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+306.sha-f6d2705');
+    var VERSION = new Version('10.0.0-rc.0+307.sha-6fc8069');
 
     /**
      * @license
