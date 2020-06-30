d643b3a925dcd05a2a3748c49bafde68fad6a21a
Andrew Kushnir <akushnir@google.com>
2020-06-30 18:09:33 +0000

    f6d2705942 docs: add Sonu Kapoor to the collaborator list (#37777)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+305.sha-79dc141
+ * @license Angular v10.0.0-rc.0+306.sha-f6d2705
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19306,7 +19306,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+305.sha-79dc141');
+const VERSION = new Version('10.0.0-rc.0+306.sha-f6d2705');
 
 /**
  * @license
