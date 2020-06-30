7e80f20b284f8cfe6fcd4e35f40ccf9d915e35eb
Andrew Kushnir <akushnir@google.com>
2020-06-30 18:05:36 +0000

    79dc14199b docs: update `/config/app-package-json` redirect (#37774)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+304.sha-7195625
+ * @license Angular v10.0.0-rc.0+305.sha-79dc141
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19306,7 +19306,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+304.sha-7195625');
+const VERSION = new Version('10.0.0-rc.0+305.sha-79dc141');
 
 /**
  * @license
