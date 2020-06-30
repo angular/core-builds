c70643fda42beb69f41499fe79e1c113bf157569
Andrew Kushnir <akushnir@google.com>
2020-06-30 19:34:19 +0000

    11dd2bef2d docs: add missing single quote (#37854)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+307.sha-6fc8069
+ * @license Angular v10.0.0-rc.0+308.sha-11dd2be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19275,7 +19275,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+307.sha-6fc8069');
+const VERSION = new Version('10.0.0-rc.0+308.sha-11dd2be');
 
 /**
  * @license
