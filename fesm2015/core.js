20c76d747f111f1b3cef72e767ee2960acbf21bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 17:00:55 +0000

    a5ffca0576 fix(router): fix navigation ignoring logic to compare to the browser url (#37716)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+312.sha-196bfa8
+ * @license Angular v10.0.0-rc.0+313.sha-a5ffca0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19278,7 +19278,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+312.sha-196bfa8');
+const VERSION = new Version('10.0.0-rc.0+313.sha-a5ffca0');
 
 /**
  * @license
