08211d0de7a77d9bdb20f39ad08817c9fa5ca6f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:20:33 +0000

    ec32eba02c feat(localize): expose `canParse()` diagnostics (#37909)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+318.sha-aed6b13
+ * @license Angular v10.0.0-rc.0+320.sha-ec32eba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+318.sha-aed6b13');
+    var VERSION = new Version('10.0.0-rc.0+320.sha-ec32eba');
 
     /**
      * @license
