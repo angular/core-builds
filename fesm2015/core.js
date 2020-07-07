121287edc1f38adefd581a79d097e0f5d05954c2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:42:47 +0000

    f296fea112 docs: minor fixes to docs related to updating to v10 (#37897)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.2+9.sha-9d54b3a
+ * @license Angular v10.0.2+11.sha-f296fea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.2+9.sha-9d54b3a');
+const VERSION = new Version('10.0.2+11.sha-f296fea');
 
 /**
  * @license
