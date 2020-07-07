e677c74eb0537c9288cc97cdae8c9370b4bd3472
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:40:31 +0000

    289a96b078 docs: minor fixes to docs related to updating to v10 (#37897)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
+ * @license Angular v10.0.0-rc.0+325.sha-289a96b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.0-rc.0+324.sha-cb2ab26');
+    var VERSION = new Version('10.0.0-rc.0+325.sha-289a96b');
 
     /**
      * @license
