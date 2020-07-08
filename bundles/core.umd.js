c9e89da9d1d9c96417a7c77321fc39cebc26be83
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 21:24:09 +0000

    1f0c1f3ff2 release: cut the v9.1.12 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+5.sha-a86a412
+ * @license Angular v9.1.12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20229,7 +20229,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('9.1.11+5.sha-a86a412');
+    var VERSION = new Version('9.1.12');
 
     /**
      * @license
