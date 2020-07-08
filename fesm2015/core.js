b843de164dece256dc1e297415757974a6ec3d62
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 19:33:34 +0000

    d2886b3bb4 build: filter out duplicate cherry-picked commits in changelog (#37956)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.2+11.sha-f296fea
+ * @license Angular v10.0.2+12.sha-d2886b3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.2+11.sha-f296fea');
+const VERSION = new Version('10.0.2+12.sha-d2886b3');
 
 /**
  * @license
