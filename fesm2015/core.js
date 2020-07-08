49e6b8ba5ed6a80406ab33fd70af09f57dcc5bbe
atscott <atscott@google.com>
2020-07-08 23:30:02 +0000

    bdbbff6a1a docs: break testing doc into multiple docs (#37957)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+2.sha-f81d3f6
+ * @license Angular v10.1.0-next.0+3.sha-bdbbff6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19278,7 +19278,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+2.sha-f81d3f6');
+const VERSION = new Version('10.1.0-next.0+3.sha-bdbbff6');
 
 /**
  * @license
