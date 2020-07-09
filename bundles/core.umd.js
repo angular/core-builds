ac44019e48fb1fb55acb1f60e15ea611bd1a386d
atscott <atscott@google.com>
2020-07-09 17:10:06 +0000

    fb735d625b refactor(service-worker): use nominal type for normalized URLs (#37922)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+11.sha-b98314e
+ * @license Angular v10.1.0-next.0+16.sha-fb735d6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.0+11.sha-b98314e');
+    var VERSION = new Version('10.1.0-next.0+16.sha-fb735d6');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+11.sha-b98314e
+ * @license Angular v10.1.0-next.0+16.sha-fb735d6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+11.sha-b98314e');
+const VERSION = new Version('10.1.0-next.0+16.sha-fb735d6');
 
 /**
  * @license
