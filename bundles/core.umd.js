52b3edc96e3ee44e0adbf24557ff427ac5ab167b
atscott <atscott01@gmail.com>
2020-07-09 22:29:28 +0000

    c40cbecdd1 docs(animations): Added consistency in code examples (#37081)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+16.sha-fb735d6
+ * @license Angular v10.1.0-next.0+18.sha-c40cbec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.0+16.sha-fb735d6');
+    var VERSION = new Version('10.1.0-next.0+18.sha-c40cbec');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+16.sha-fb735d6
+ * @license Angular v10.1.0-next.0+18.sha-c40cbec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+16.sha-fb735d6');
+const VERSION = new Version('10.1.0-next.0+18.sha-c40cbec');
 
 /**
  * @license
