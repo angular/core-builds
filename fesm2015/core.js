f74b84337d0fa78ff87842d16084abd5e87137b0
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:24:27 +0000

    a94383f168 fix(compiler): check more cases for pipe usage inside host bindings (#37883)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+33.sha-c776825
+ * @license Angular v10.0.3+36.sha-a94383f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.3+33.sha-c776825');
+const VERSION = new Version('10.0.3+36.sha-a94383f');
 
 /**
  * @license
