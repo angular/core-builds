79df30be162583e659769bfb828b972b2377760f
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:19:09 +0000

    9bd4b74b06 feat(dev-infra): commit message validation should skip lines consisting of URLs (#37890)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+20.sha-4b4b745
+ * @license Angular v10.1.0-next.0+22.sha-9bd4b74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+20.sha-4b4b745');
+const VERSION = new Version('10.1.0-next.0+22.sha-9bd4b74');
 
 /**
  * @license
