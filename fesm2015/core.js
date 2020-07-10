6b5bbf9b8c87f2a7eaf6316bf208b1326fdf75c4
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:52:33 +0000

    2e9b953e9d docs(router): fix routerLink docs (#37997) The current content for the routerLink commands input does not make it to aio. fixes #35414

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+36.sha-a94383f
+ * @license Angular v10.0.3+37.sha-2e9b953
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.3+36.sha-a94383f');
+const VERSION = new Version('10.0.3+37.sha-2e9b953');
 
 /**
  * @license
