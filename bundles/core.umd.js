31d219e2caaaf65a8f7d7dc7fc3787ec29a41cf0
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:29:29 +0000

    36dd817913 docs: fixed typo in https://angular.io/guide/glossary (#36220)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+33.sha-6b31155
+ * @license Angular v10.1.0-next.0+37.sha-36dd817
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.0+33.sha-6b31155');
+    var VERSION = new Version('10.1.0-next.0+37.sha-36dd817');
 
     /**
      * @license
