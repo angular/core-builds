95f8c1cde4f9a379502db7316b5152c8c0b6537a
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:39:10 +0000

    69472a1ed0 refactor(dev-infra): allow for consumption with rules_nodejs v2.0.0 (#37968)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+38.sha-10aba15
+ * @license Angular v10.1.0-next.0+39.sha-69472a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.0+38.sha-10aba15');
+    var VERSION = new Version('10.1.0-next.0+39.sha-69472a1');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+38.sha-10aba15
+ * @license Angular v10.1.0-next.0+39.sha-69472a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+38.sha-10aba15');
+const VERSION = new Version('10.1.0-next.0+39.sha-69472a1');
 
 /**
  * @license
