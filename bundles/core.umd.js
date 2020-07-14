3c4bce895ee2ae5a4bc8d3c0ab9229c60cbbd9fb
Sonu Kapoor <sonukapoor@gmail.com>
2020-07-14 18:40:19 +0000

    e52ac490c8 docs(core): fixes minor typo in initNgDevMode function docs

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-2dd22b7
+ * @license Angular v10.1.0-next.0+42.sha-e52ac49
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.0+42.sha-2dd22b7');
+    var VERSION = new Version('10.1.0-next.0+42.sha-e52ac49');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-2dd22b7
+ * @license Angular v10.1.0-next.0+42.sha-e52ac49
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+42.sha-2dd22b7');
+const VERSION = new Version('10.1.0-next.0+42.sha-e52ac49');
 
 /**
  * @license
