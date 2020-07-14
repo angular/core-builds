421cac941aeb93bc44a9cab55eadda7d50d692ff
Andrew Scott <atscott01@gmail.com>
2020-07-14 20:46:45 +0000

    b358495a6c fix(ngcc): report a warning if ngcc tries to use a solution-style tsconfig (#38003)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+48.sha-f4fac40
+ * @license Angular v10.1.0-next.0+49.sha-b358495
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.0+48.sha-f4fac40');
+    var VERSION = new Version('10.1.0-next.0+49.sha-b358495');
 
     /**
      * @license
