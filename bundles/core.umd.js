8ac45c15a2196b28613af4cf4be7ced6afa4d6df
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:00:37 +0000

    45d79efc3f docs: remove all references to Angular Console (#37608)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+6.sha-09eee5b
+ * @license Angular v10.1.0-next.1+2.sha-45d79ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.1+6.sha-09eee5b');
+    var VERSION = new Version('10.1.0-next.1+2.sha-45d79ef');
 
     /**
      * @license
