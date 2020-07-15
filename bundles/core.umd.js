4671381b5d9ce390dca83d4f124f02c97318d832
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:35:05 +0000

    739bf5c325 docs: fix typo in "Creating libraries" guide (`by publishing...ensures` --> `publishing...ensures`) (#38032)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+9.sha-17ddab9
+ * @license Angular v10.0.4+11.sha-739bf5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19915,7 +19915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.4+9.sha-17ddab9');
+    var VERSION = new Version('10.0.4+11.sha-739bf5c');
 
     /**
      * @license
