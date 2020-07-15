5260d6e99ec65325a4851a75abf40904dc233133
Andrew Kushnir <akushnir@google.com>
2020-07-15 19:58:29 +0000

    b00121c9a6 docs: update reference doc for router guards and resolvers (#38079)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+1.sha-1d9e56e
+ * @license Angular v10.1.0-next.1+3.sha-b00121c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19916,7 +19916,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.1+1.sha-1d9e56e');
+    var VERSION = new Version('10.1.0-next.1+3.sha-b00121c');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+1.sha-1d9e56e
+ * @license Angular v10.1.0-next.1+3.sha-b00121c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19309,7 +19309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+1.sha-1d9e56e');
+const VERSION = new Version('10.1.0-next.1+3.sha-b00121c');
 
 /**
  * @license
