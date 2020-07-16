d95f358eb983408e752795a7c022d4ba7734bdc9
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:30:36 +0000

    9ac3383d01 build(language-service): remove typescript from ivy bundle (#38088)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+12.sha-6e32972
+ * @license Angular v10.0.4+15.sha-9ac3383
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19308,7 +19308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+12.sha-6e32972');
+const VERSION = new Version('10.0.4+15.sha-9ac3383');
 
 /**
  * @license
