3bdedf78468f6c1cd55813573ff0074efc600a97
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:25:21 +0000

    99960a98d2 docs: update router api documentation (#37980)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+15.sha-9ac3383
+ * @license Angular v10.0.4+16.sha-99960a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19915,7 +19915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.4+15.sha-9ac3383');
+    var VERSION = new Version('10.0.4+16.sha-99960a9');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+15.sha-9ac3383
+ * @license Angular v10.0.4+16.sha-99960a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19308,7 +19308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+15.sha-9ac3383');
+const VERSION = new Version('10.0.4+16.sha-99960a9');
 
 /**
  * @license
