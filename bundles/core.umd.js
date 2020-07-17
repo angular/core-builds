4a7d0c28445ba896830aa2a7ddb11e4d08236482
Andrew Kushnir <akushnir@google.com>
2020-07-17 19:47:59 +0000

    f2ca4634e2 fix(docs-infra): correctly display SVG icons in IE11 (#38046)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+17.sha-e9cb6fb
+ * @license Angular v10.0.4+19.sha-f2ca463
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19915,7 +19915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.4+17.sha-e9cb6fb');
+    var VERSION = new Version('10.0.4+19.sha-f2ca463');
 
     /**
      * @license
