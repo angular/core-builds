74c4fddc6284be0dd6ba5d4ba1e67cf749d50ba5
Andrew Kushnir <akushnir@google.com>
2020-07-20 17:32:42 +0000

    72f1eec3ad docs(core): add note about not mutating multi provider arrays (#37645)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+32.sha-1a7a736
+ * @license Angular v10.1.0-next.1+30.sha-72f1eec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.1+32.sha-1a7a736');
+    var VERSION = new Version('10.1.0-next.1+30.sha-72f1eec');
 
     /**
      * @license
