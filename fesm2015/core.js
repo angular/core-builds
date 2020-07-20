120ce2c64411e3863d1b481d31e3a343652bd08b
Andrew Kushnir <akushnir@google.com>
2020-07-20 20:59:04 +0000

    d6d3984524 docs(common): fix selector field in NgIfAs example component (#35854)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+31.sha-9cb318f
+ * @license Angular v10.0.4+33.sha-d6d3984
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19307,7 +19307,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+31.sha-9cb318f');
+const VERSION = new Version('10.0.4+33.sha-d6d3984');
 
 /**
  * @license
