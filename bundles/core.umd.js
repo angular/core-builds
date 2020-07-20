1d1cef662fcc75a913d128b5d19b58a861b4440f
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:38:21 +0000

    c8c74153b1 refactor(dev-infra): Update triage labels documentation (#38081)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+30.sha-72f1eec
+ * @license Angular v10.1.0-next.1+34.sha-c8c7415
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.1+30.sha-72f1eec');
+    var VERSION = new Version('10.1.0-next.1+34.sha-c8c7415');
 
     /**
      * @license
