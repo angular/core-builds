fbdce357cf31501ad62e5ff816f4d9db278ba8d2
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:28:54 +0000

    970d10c671 release: cut the v10.1.0-next.2 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+46.sha-8851ba9
+ * @license Angular v10.1.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19914,7 +19914,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.1+46.sha-8851ba9');
+    var VERSION = new Version('10.1.0-next.2');
 
     /**
      * @license
