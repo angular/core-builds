cf59421782723bd483bae575111f1cf7d69970ce
Misko Hevery <misko@hevery.com>
2020-07-23 18:31:51 +0000

    dd09fb5348 build(docs-infra): upgrade cli command docs sources to b0b27361d (#38182)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+19.sha-ee22aa5
+ * @license Angular v10.1.0-next.2+18.sha-dd09fb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.2+19.sha-ee22aa5');
+    var VERSION = new Version('10.1.0-next.2+18.sha-dd09fb5');
 
     /**
      * @license
