d6303a6b5e3531bc1d7b1d0518fabefd08bb274b
Misko Hevery <misko@hevery.com>
2020-07-23 18:24:39 +0000

    51aa4aa1c3 docs: update dynamic-component loading guide (#36959)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+13.sha-8bb8f98
+ * @license Angular v10.1.0-next.2+14.sha-51aa4aa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19914,7 +19914,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.2+13.sha-8bb8f98');
+    var VERSION = new Version('10.1.0-next.2+14.sha-51aa4aa');
 
     /**
      * @license
