e51b7020f824b243f49fa2338a79b891fb86ec45
Misko Hevery <misko@hevery.com>
2020-07-24 17:15:54 +0000

    6e2db228af docs: Fix link by removing a space (#38214) PR Close #38214

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+22.sha-cce5583
+ * @license Angular v10.1.0-next.2+23.sha-6e2db22
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19276,7 +19276,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+22.sha-cce5583');
+const VERSION = new Version('10.1.0-next.2+23.sha-6e2db22');
 
 /**
  * @license
