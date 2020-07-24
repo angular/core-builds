b3a97779c847fdf3c89c2c17316c7d20e757cf31
Misko Hevery <misko@hevery.com>
2020-07-24 23:14:04 +0000

    6a3e68b606 docs(zone.js): update zone.js bundle doc since the APF change (#37919)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+26.sha-1822cbc
+ * @license Angular v10.1.0-next.2+27.sha-6a3e68b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19914,7 +19914,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.2+26.sha-1822cbc');
+    var VERSION = new Version('10.1.0-next.2+27.sha-6a3e68b');
 
     /**
      * @license
