3ffa4f40335852b95acf7d8e5b60b816bd45be16
Misko Hevery <misko@hevery.com>
2020-07-24 22:41:16 +0000

    7467fd36b9 fix(zone.js): clearTimeout/clearInterval should call on object global (#37858)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+18.sha-aca0198
+ * @license Angular v10.0.5+19.sha-7467fd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19914,7 +19914,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.5+18.sha-aca0198');
+    var VERSION = new Version('10.0.5+19.sha-7467fd3');
 
     /**
      * @license
