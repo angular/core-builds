3eaf6b2a0f71d333b8035f39ba6331dbaf772c33
Misko Hevery <misko@hevery.com>
2020-07-24 17:12:39 +0000

    cce5583e75 docs(core): Fix incorrectly rendered code example in structural directives guide (#38207)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+21.sha-307db74
+ * @license Angular v10.1.0-next.2+22.sha-cce5583
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19914,7 +19914,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.2+21.sha-307db74');
+    var VERSION = new Version('10.1.0-next.2+22.sha-cce5583');
 
     /**
      * @license
