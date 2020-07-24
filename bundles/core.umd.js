30dec39bd3422c77b952604f8f5dc08f64c370de
Misko Hevery <misko@hevery.com>
2020-07-24 23:07:03 +0000

    1822cbcd46 fix(zone.js): patch nodejs EventEmtter.prototype.off (#37863)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+25.sha-a71f114
+ * @license Angular v10.1.0-next.2+26.sha-1822cbc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.2+25.sha-a71f114');
+    var VERSION = new Version('10.1.0-next.2+26.sha-1822cbc');
 
     /**
      * @license
