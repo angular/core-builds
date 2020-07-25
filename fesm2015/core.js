9fc86f944eda7065d682b970c510edbe7811b485
Misko Hevery <misko@hevery.com>
2020-07-25 01:22:42 +0000

    a123ef58b1 fix(dev-infra): Ensure conditions with groups do not fail verification (#37798)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+20.sha-c4e6f58
+ * @license Angular v10.0.5+23.sha-a123ef5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19307,7 +19307,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+20.sha-c4e6f58');
+const VERSION = new Version('10.0.5+23.sha-a123ef5');
 
 /**
  * @license
