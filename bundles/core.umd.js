cf36619032fb4333db5f9637d4639b7bcd354501
Misko Hevery <misko@hevery.com>
2020-07-27 17:10:55 +0000

    1296003445 docs: add ng-add save option (#38198)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+30.sha-67e3ecc
+ * @license Angular v10.1.0-next.2+32.sha-1296003
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19914,7 +19914,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.2+30.sha-67e3ecc');
+    var VERSION = new Version('10.1.0-next.2+32.sha-1296003');
 
     /**
      * @license
