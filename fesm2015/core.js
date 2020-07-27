3989ae216e0cba1dd6c2a2b27a4f9dc3d0fb46a7
Misko Hevery <misko@hevery.com>
2020-07-27 17:12:44 +0000

    4dfc3fe6a2 refactor(router): extract Router config utils to a separate file (#38229)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+32.sha-1296003
+ * @license Angular v10.1.0-next.2+31.sha-4dfc3fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19307,7 +19307,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+32.sha-1296003');
+const VERSION = new Version('10.1.0-next.2+31.sha-4dfc3fe');
 
 /**
  * @license
