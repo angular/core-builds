7d9b45667af29567f6c0c403747e728502e43043
Misko Hevery <misko@hevery.com>
2020-07-27 22:49:01 +0000

    174aac68f7 refactor(platform-browser): specify return type of parseEventName (#38089)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+42.sha-5d3ba8d
+ * @license Angular v10.1.0-next.2+43.sha-174aac6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.2+42.sha-5d3ba8d');
+    var VERSION = new Version('10.1.0-next.2+43.sha-174aac6');
 
     /**
      * @license
