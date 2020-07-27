f305f1709996d9013bfbff9ceda1c029c6edca63
Misko Hevery <misko@hevery.com>
2020-07-27 17:15:22 +0000

    4dfc3fe6a2 refactor(router): extract Router config utils to a separate file (#38229)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+32.sha-1296003
+ * @license Angular v10.1.0-next.2+31.sha-4dfc3fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19890,7 +19890,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.2+32.sha-1296003');
+    var VERSION = new Version('10.1.0-next.2+31.sha-4dfc3fe');
 
     /**
      * @license
