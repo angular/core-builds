997957e157e9103d7c09235496c50322cd37651c
Misko Hevery <misko@hevery.com>
2020-07-28 23:02:59 +0000

    57575e379f release: cut the v10.1.0-next.3 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+59.sha-28c3e2d
+ * @license Angular v10.1.0-next.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19277,7 +19277,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+59.sha-28c3e2d');
+const VERSION = new Version('10.1.0-next.3');
 
 /**
  * @license
