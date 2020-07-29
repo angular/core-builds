d59082d072fe788afef10b45c1201d5ae389f2e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 20:55:52 +0000

    887c350f9d refactor(compiler): wrap large strings in function (#38253)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+14.sha-103a95c
+ * @license Angular v10.1.0-next.3+15.sha-887c350
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.3+14.sha-103a95c');
+    var VERSION = new Version('10.1.0-next.3+15.sha-887c350');
 
     /**
      * @license
