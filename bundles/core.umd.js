3e04d4c829a6f33bfb5c5c161a036680c1f0569b
Misko Hevery <misko@hevery.com>
2020-07-29 16:54:26 +0000

    0469d9240a Revert "refactor(platform-browser): specify return type of parseEventName (#38089)"

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6
+ * @license Angular v10.0.6+1.sha-0469d92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19915,7 +19915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.6');
+    var VERSION = new Version('10.0.6+1.sha-0469d92');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6
+ * @license Angular v10.0.6+1.sha-0469d92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19308,7 +19308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6');
+const VERSION = new Version('10.0.6+1.sha-0469d92');
 
 /**
  * @license
