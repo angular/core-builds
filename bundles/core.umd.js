0d604be7adaa0512df7092a4bb47692168c09a26
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 21:00:27 +0000

    8effc83b92 docs(router): clarify how base href is used to construct targets (#38123)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+15.sha-887c350
+ * @license Angular v10.1.0-next.3+17.sha-8effc83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.3+15.sha-887c350');
+    var VERSION = new Version('10.1.0-next.3+17.sha-8effc83');
 
     /**
      * @license
@@ -32515,6 +32515,7 @@
     exports.ɵmod = moduleDef;
     exports.ɵmpd = moduleProvideDef;
     exports.ɵncd = ngContentDef;
+    exports.ɵnoSideEffects = noSideEffects;
     exports.ɵnov = nodeValue;
     exports.ɵoverrideComponentView = overrideComponentView;
     exports.ɵoverrideProvider = overrideProvider;
