b596764972383a53dc85c3f993806d108ffa8a6b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 19:46:56 +0000

    3a525d196b Revert "fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)" (#38303)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+18.sha-1eebb7f
+ * @license Angular v10.1.0-next.3+19.sha-3a525d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.3+18.sha-1eebb7f');
+    var VERSION = new Version('10.1.0-next.3+19.sha-3a525d1');
 
     /**
      * @license
@@ -32515,7 +32515,6 @@
     exports.ɵmod = moduleDef;
     exports.ɵmpd = moduleProvideDef;
     exports.ɵncd = ngContentDef;
-    exports.ɵnoSideEffects = noSideEffects;
     exports.ɵnov = nodeValue;
     exports.ɵoverrideComponentView = overrideComponentView;
     exports.ɵoverrideProvider = overrideProvider;
--- src/r3_symbols.d.ts
+++ src/r3_symbols.d.ts
@@ -12,7 +12,6 @@ export { ɵɵdefineNgModule } from './render3/definition';
 export { ɵɵFactoryDef } from './render3/interfaces/definition';
 export { setClassMetadata } from './render3/metadata';
 export { NgModuleFactory } from './render3/ng_module_ref';
-export { noSideEffects as ɵnoSideEffects } from './util/closure';
 /**
  * The existence of this constant (in this particular file) informs the Angular compiler that the
  * current program is actually @angular/core, which needs to be compiled specially.
