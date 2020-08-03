cc72c7c3be76655a281ab4beedd665c379243fbc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:17:36 +0000

    e49b053dac build: cleanup .bazelrc file to no longer set unused flags (#38124)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+39.sha-ff9f4de
+ * @license Angular v10.1.0-next.3+41.sha-e49b053
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.3+39.sha-ff9f4de');
+    var VERSION = new Version('10.1.0-next.3+41.sha-e49b053');
 
     /**
      * @license
