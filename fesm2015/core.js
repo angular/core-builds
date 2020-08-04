0e9d5ee17a4efc2e02e7494bf2d9eaa55bb01e89
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-04 23:31:30 +0000

    192ef42304 docs: release notes for the v10.0.8 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+43.sha-8fbf40b
+ * @license Angular v10.1.0-next.3+44.sha-192ef42
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19277,7 +19277,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+43.sha-8fbf40b');
+const VERSION = new Version('10.1.0-next.3+44.sha-192ef42');
 
 /**
  * @license
