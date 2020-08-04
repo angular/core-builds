72a0b8ccc47caff97a02b9a26d231f6bdb113f62
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-04 23:29:21 +0000

    2ab8e88924 release: cut the v10.0.8 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+20.sha-a91dd2e
+ * @license Angular v10.0.8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19308,7 +19308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.7+20.sha-a91dd2e');
+const VERSION = new Version('10.0.8');
 
 /**
  * @license
