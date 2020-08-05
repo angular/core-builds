8b03932ecb37a133298def4b2def6fbf3cdd7870
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:13:45 +0000

    585ef2bdd7 docs: fix typo in glossary (#38318)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4
+ * @license Angular v10.1.0-next.4+1.sha-585ef2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.4');
+    var VERSION = new Version('10.1.0-next.4+1.sha-585ef2b');
 
     /**
      * @license
