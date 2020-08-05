0e6d47f74208e9242c0c9bbbd69e39852001683d
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:24:34 +0000

    ba175be41f fix(compiler-cli): match wrapHost parameter types within plugin interface (#38004)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+6.sha-7630234
+ * @license Angular v10.1.0-next.4+5.sha-ba175be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.4+6.sha-7630234');
+    var VERSION = new Version('10.1.0-next.4+5.sha-ba175be');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+6.sha-7630234
+ * @license Angular v10.1.0-next.4+5.sha-ba175be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19277,7 +19277,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+6.sha-7630234');
+const VERSION = new Version('10.1.0-next.4+5.sha-ba175be');
 
 /**
  * @license
