686e9047132cddaf50771f0e6ff0ebc0c7202450
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:36:50 +0000

    db897f4f7a docs: add a page with the Angular roadmap (#38356)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+6.sha-4151314
+ * @license Angular v10.0.8+8.sha-db897f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19915,7 +19915,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.8+6.sha-4151314');
+    var VERSION = new Version('10.0.8+8.sha-db897f4');
 
     /**
      * @license
