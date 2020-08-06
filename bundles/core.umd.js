c6f655647e8b3a6d424e90a13563d64b665a05aa
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:24:20 +0000

    dca4443a8e fix(compiler-cli): mark eager `NgModuleFactory` construction as not side effectful (#38320)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+11.sha-d7c043b
+ * @license Angular v10.1.0-next.4+15.sha-dca4443
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19891,7 +19891,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.4+11.sha-d7c043b');
+    var VERSION = new Version('10.1.0-next.4+15.sha-dca4443');
 
     /**
      * @license
@@ -32515,6 +32515,7 @@
     exports.ɵmod = moduleDef;
     exports.ɵmpd = moduleProvideDef;
     exports.ɵncd = ngContentDef;
+    exports.ɵnoSideEffects = noSideEffects;
     exports.ɵnov = nodeValue;
     exports.ɵoverrideComponentView = overrideComponentView;
     exports.ɵoverrideProvider = overrideProvider;
