3a55cbac0875752187fa2aaeae05d3d25185022b
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:27:24 +0000

    016a41b14d fix(compiler-cli): mark eager `NgModuleFactory` construction as not side effectful (#38320)

--- src/r3_symbols.d.ts
+++ src/r3_symbols.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+10.sha-58f4b3a
+ * @license Angular v10.0.8+14.sha-016a41b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -913,6 +913,18 @@ declare abstract class ViewRef extends ChangeDetectorRef {
      */
     abstract onDestroy(callback: Function): any /** TODO #9100 */;
 }
+
+
+/**
+ * Convince closure compiler that the wrapped function has no side-effects.
+ *
+ * Closure compiler always assumes that `toString` has no side-effects. We use this quirk to
+ * allow us to execute a function but have closure compiler mark the call as no-side-effects.
+ * It is important that the return value for the `noSideEffects` function be assigned
+ * to something which is retained otherwise the call to `noSideEffects` will be removed by closure
+ * compiler.
+ */
+export declare function ɵnoSideEffects<T>(fn: () => T): T;
 
 /**
  * Construct an `InjectableDef` which defines how a token will be constructed by the DI system, and
