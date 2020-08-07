f5ebb8cfda06685680117e94347255b93701cdad
Andrew Kushnir <akushnir@google.com>
2020-08-07 19:05:30 +0000

    856db56cca refactor(forms): get rid of duplicate functions (#38371)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+24.sha-354e66e
+ * @license Angular v10.1.0-next.4+25.sha-856db56
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19751,7 +19751,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.4+24.sha-354e66e');
+    var VERSION = new Version('10.1.0-next.4+25.sha-856db56');
 
     /**
      * @license
