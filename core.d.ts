3e67335ea006f94733c9188c472693d7f9675e51
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:27:31 +0000

    250e299dc3 refactor(core): break `i18n.ts` into smaller files (#38368)

--- core.d.ts
+++ core.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+31.sha-8f708b5
+ * @license Angular v10.1.0-next.4+32.sha-250e299
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,6 +9124,31 @@ export declare interface ɵangular_packages_core_core_bp extends Array<any> {
  */
 export declare function ɵangular_packages_core_core_bq(viewOrComponent: ɵangular_packages_core_core_bp | {}): RootContext;
 
+
+/**
+ * Handles message string post-processing for internationalization.
+ *
+ * Handles message string post-processing by transforming it from intermediate
+ * format (that might contain some markers that we need to replace) to the final
+ * form, consumable by i18nStart instruction. Post processing steps include:
+ *
+ * 1. Resolve all multi-value cases (like [�*1:1��#2:1�|�#4:1�|�5�])
+ * 2. Replace all ICU vars (like "VAR_PLURAL")
+ * 3. Replace all placeholders used inside ICUs in a form of {PLACEHOLDER}
+ * 4. Replace all ICU references with corresponding values (like �ICU_EXP_ICU_1�)
+ *    in case multiple ICUs have the same placeholder name
+ *
+ * @param message Raw translation string for post processing
+ * @param replacements Set of replacements that should be applied
+ *
+ * @returns Transformed string that can be consumed by i18nStart instruction
+ *
+ * @codeGenApi
+ */
+export declare function ɵangular_packages_core_core_br(message: string, replacements?: {
+    [key: string]: (string | string[]);
+}): string;
+
 export declare class ɵangular_packages_core_core_c implements Injector {
     get(token: any, notFoundValue?: any): any;
 }
@@ -10837,6 +10862,7 @@ export declare function ɵsetCurrentInjector(injector: Injector | null | undefin
  * @param document The object representing the global `document` in this environment.
  */
 export declare function ɵsetDocument(document: Document | undefined): void;
+
 
 /**
  * Sets the locale id that will be used for translations and ICU expressions.
