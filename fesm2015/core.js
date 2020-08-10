810273dd09669383a7339cbd671ce978a6744af7
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:49:52 +0000

    6da9e5851a fix(compiler-cli): preserve quotes in class member names (#38387)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+32.sha-250e299
+ * @license Angular v10.1.0-next.4+33.sha-6da9e58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19179,7 +19179,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+32.sha-250e299');
+const VERSION = new Version('10.1.0-next.4+33.sha-6da9e58');
 
 /**
  * @license
