ada9f0d88fa16028c8c6a532904c45410a7965d4
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:38:15 +0000

    e34c33cd46 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+29.sha-6d8c73a
+ * @license Angular v10.1.0-next.4+30.sha-e34c33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19763,7 +19763,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.4+29.sha-6d8c73a');
+    var VERSION = new Version('10.1.0-next.4+30.sha-e34c33c');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+29.sha-6d8c73a
+ * @license Angular v10.1.0-next.4+30.sha-e34c33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19148,7 +19148,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+29.sha-6d8c73a');
+const VERSION = new Version('10.1.0-next.4+30.sha-e34c33c');
 
 /**
  * @license
