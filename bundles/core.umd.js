b599d8f4d6bdf0bc270775cc6c78c5926d972d15
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:52:26 +0000

    6da9e5851a fix(compiler-cli): preserve quotes in class member names (#38387)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+32.sha-250e299
+ * @license Angular v10.1.0-next.4+33.sha-6da9e58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19763,7 +19763,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.4+32.sha-250e299');
+    var VERSION = new Version('10.1.0-next.4+33.sha-6da9e58');
 
     /**
      * @license
