3e954a5762dc50dc591f7e7e6372ce74da9deea7
Andrew Kushnir <akushnir@google.com>
2020-08-11 20:26:44 +0000

    ca2b4bcd4b fix(compiler-cli): avoid creating value expressions for symbols from type-only imports (#38415)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+36.sha-f6cfc92
+ * @license Angular v10.0.8+37.sha-ca2b4bc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19179,7 +19179,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+36.sha-f6cfc92');
+const VERSION = new Version('10.0.8+37.sha-ca2b4bc');
 
 /**
  * @license
