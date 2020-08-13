48360d46cf4c89ddc44e8480778c229d21ade5dd
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:56:55 +0000

    1388c1761f perf(compiler-cli): don't emit template guards when child scope is empty (#38418)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+13.sha-175c79d
+ * @license Angular v10.1.0-next.5+16.sha-1388c17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19149,7 +19149,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+13.sha-175c79d');
+const VERSION = new Version('10.1.0-next.5+16.sha-1388c17');
 
 /**
  * @license
