320cf975497c52b56b11148b58d8d057e5a9c4fe
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:18:17 +0000

    175c79d1d8 test(docs-infra): remove deprecated `ReflectiveInjector` (#38408)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+12.sha-945751e
+ * @license Angular v10.1.0-next.5+13.sha-175c79d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19149,7 +19149,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+12.sha-945751e');
+const VERSION = new Version('10.1.0-next.5+13.sha-175c79d');
 
 /**
  * @license
