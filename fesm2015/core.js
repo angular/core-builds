9b1e0e5a0bc713de790713ea6ff44f6dc7ff7d35
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:35:22 +0000

    8763d8201c build: update ng-dev config file for new commit message configuration (#38430)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+6.sha-773f790
+ * @license Angular v10.1.0-next.5+8.sha-8763d82
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19148,7 +19148,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+6.sha-773f790');
+const VERSION = new Version('10.1.0-next.5+8.sha-8763d82');
 
 /**
  * @license
