19cd562448c1c5d9cbaf531bf6031ae323c5ecba
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:03:33 +0000

    aa816d3887 ci: disable closure size tracking test (#38449)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+9.sha-cb83b8a
+ * @license Angular v10.0.9+11.sha-aa816d3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19180,7 +19180,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.9+9.sha-cb83b8a');
+const VERSION = new Version('10.0.9+11.sha-aa816d3');
 
 /**
  * @license
