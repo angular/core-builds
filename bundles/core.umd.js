12130eb22e25dfd17fe4e14413939d0b8bc8926c
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:16:46 +0000

    692e34d4a2 test(docs-infra): remove deprecated `ReflectiveInjector` (#38408)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+11.sha-aa816d3
+ * @license Angular v10.0.9+14.sha-692e34d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19788,7 +19788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.9+11.sha-aa816d3');
+    var VERSION = new Version('10.0.9+14.sha-692e34d');
 
     /**
      * @license
