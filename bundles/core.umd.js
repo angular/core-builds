38bf0b2233d0ae45a89a139641aa67ea373e2905
atscott <atscott01@gmail.com>
2020-08-17 20:40:54 +0000

    3e80f0e526 release: cut the v10.0.10 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+21.sha-f3dd6c2
+ * @license Angular v10.0.10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19817,7 +19817,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.9+21.sha-f3dd6c2');
+    var VERSION = new Version('10.0.10');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+21.sha-f3dd6c2
+ * @license Angular v10.0.10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19209,7 +19209,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.9+21.sha-f3dd6c2');
+const VERSION = new Version('10.0.10');
 
 /**
  * @license
