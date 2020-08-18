bd695cbcd03e54ccddd22a1394dd70c98c8a2fbb
Andrew Scott <atscott@google.com>
2020-08-18 17:43:52 +0000

    dbfb50e9f4 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38511)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+4.sha-723a9ff
+ * @license Angular v10.1.0-next.6+6.sha-dbfb50e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19210,7 +19210,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+4.sha-723a9ff');
+const VERSION = new Version('10.1.0-next.6+6.sha-dbfb50e');
 
 /**
  * @license
