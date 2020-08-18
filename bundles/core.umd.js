ba6547051ee9970f36f8af573b3a86a23cb69f44
Andrew Scott <atscott@google.com>
2020-08-18 17:44:49 +0000

    0af95332e9 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38511)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10
+ * @license Angular v10.0.10+6.sha-0af9533
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19817,7 +19817,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.10');
+    var VERSION = new Version('10.0.10+6.sha-0af9533');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10
+ * @license Angular v10.0.10+6.sha-0af9533
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19209,7 +19209,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10');
+const VERSION = new Version('10.0.10+6.sha-0af9533');
 
 /**
  * @license
