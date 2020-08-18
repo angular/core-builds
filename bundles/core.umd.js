200ac759e47cd84eeec00b20a05614912c5cd1f1
Andrew Scott <atscott@google.com>
2020-08-18 17:34:30 +0000

    723a9ff095 docs(common): Wrong parameter description on TrackBy (#38495)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+3.sha-e472f5f
+ * @license Angular v10.1.0-next.6+4.sha-723a9ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19794,7 +19794,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.6+3.sha-e472f5f');
+    var VERSION = new Version('10.1.0-next.6+4.sha-723a9ff');
 
     /**
      * @license
