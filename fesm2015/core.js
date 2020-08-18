13aff7f3e4b0479d573ba42f49f3d77111321d77
Andrew Scott <atscott@google.com>
2020-08-18 23:47:59 +0000

    dcc3f6d74d feat(dev-infra): tooling to check out pending PR (#38474)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+6.sha-0af9533
+ * @license Angular v10.0.10+7.sha-dcc3f6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19209,7 +19209,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10+6.sha-0af9533');
+const VERSION = new Version('10.0.10+7.sha-dcc3f6d');
 
 /**
  * @license
