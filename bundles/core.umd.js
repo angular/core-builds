530aa74c0f1cacb2ea26d563195b48af017decfa
Misko Hevery <misko@hevery.com>
2020-08-19 18:59:46 +0000

    7ad32649c0 fix(router): support lazy loading for empty path named outlets (#38379)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+12.sha-0270020
+ * @license Angular v10.1.0-next.7+2.sha-7ad3264
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19818,7 +19818,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.6+12.sha-0270020');
+    var VERSION = new Version('10.1.0-next.7+2.sha-7ad3264');
 
     /**
      * @license
