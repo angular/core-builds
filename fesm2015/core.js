e3209ad1e142278e6e59e86f94ec4aa08d758357
Misko Hevery <misko@hevery.com>
2020-08-19 19:02:28 +0000

    7ad32649c0 fix(router): support lazy loading for empty path named outlets (#38379)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+12.sha-0270020
+ * @license Angular v10.1.0-next.7+2.sha-7ad3264
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19179,7 +19179,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+12.sha-0270020');
+const VERSION = new Version('10.1.0-next.7+2.sha-7ad3264');
 
 /**
  * @license
