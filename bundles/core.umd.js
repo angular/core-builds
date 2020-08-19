ea34bc155214803ad28547aee728f28da4779f15
Andrew Scott <atscott@google.com>
2020-08-19 15:49:06 +0000

    0570c240e4 docs: Fix typo in the inputs and outputs guide (#38524)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+8.sha-398118f
+ * @license Angular v10.0.10+9.sha-0570c24
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19817,7 +19817,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.10+8.sha-398118f');
+    var VERSION = new Version('10.0.10+9.sha-0570c24');
 
     /**
      * @license
