f014a66c329a0922d541991588e8967861fcc594
Misko Hevery <misko@hevery.com>
2020-08-19 21:36:29 +0000

    0b54c0c6b4 refactor(compiler-cli): add getTemplateOfComponent to TemplateTypeChecker (#38355)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+3.sha-1ec6099
+ * @license Angular v10.1.0-next.7+4.sha-0b54c0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19794,7 +19794,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.7+3.sha-1ec6099');
+    var VERSION = new Version('10.1.0-next.7+4.sha-0b54c0c');
 
     /**
      * @license
