1484c7b6f22b87e24b0cc92ccfd2d3f0e731e912
Andrew Scott <atscott@google.com>
2020-08-19 16:28:58 +0000

    6b662d10c1 fix(zone.js): zone.js package.json should not include files/directories field (#38528)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+10.sha-55fd725
+ * @license Angular v10.1.0-next.6+11.sha-6b662d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19818,7 +19818,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.6+10.sha-55fd725');
+    var VERSION = new Version('10.1.0-next.6+11.sha-6b662d1');
 
     /**
      * @license
