25beb32989c38c42fb5c3ad59774e4aee53ebbfc
Misko Hevery <misko@hevery.com>
2020-08-20 17:04:42 +0000

    22f1ac3e37 docs: udpate the details (#37967)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+13.sha-8017ca4
+ * @license Angular v10.1.0-next.7+12.sha-22f1ac3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19795,7 +19795,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.7+13.sha-8017ca4');
+    var VERSION = new Version('10.1.0-next.7+12.sha-22f1ac3');
 
     /**
      * @license
