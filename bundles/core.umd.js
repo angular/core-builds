07e60a7487118d58fac1480dcf96cb979a67dc46
Misko Hevery <misko@hevery.com>
2020-08-24 22:53:30 +0000

    81db6a3171 release: cut the v10.0.12 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+16.sha-8224e65
+ * @license Angular v10.0.12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19818,7 +19818,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.11+16.sha-8224e65');
+    var VERSION = new Version('10.0.12');
 
     /**
      * @license
