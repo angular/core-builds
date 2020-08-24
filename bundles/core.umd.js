cd0d57dbdfd03674472b105575e278d8ad12d5a5
Misko Hevery <misko@hevery.com>
2020-08-24 16:47:35 +0000

    6e643d9874 docs(localize): fix angular.json syntax (#38553)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+18.sha-874792d
+ * @license Angular v10.1.0-next.7+21.sha-6e643d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19819,7 +19819,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.7+18.sha-874792d');
+    var VERSION = new Version('10.1.0-next.7+21.sha-6e643d9');
 
     /**
      * @license
