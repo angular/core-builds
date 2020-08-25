3c3759b69fa396a3b59acb36918ddb09470a0812
Misko Hevery <misko@hevery.com>
2020-08-25 22:41:36 +0000

    f0af387f6c fix(localize): ensure required XLIFF parameters are serialized (#38575)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.8+12.sha-841dfa6
+ * @license Angular v10.1.0-next.8+11.sha-f0af387
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19795,7 +19795,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.8+12.sha-841dfa6');
+    var VERSION = new Version('10.1.0-next.8+11.sha-f0af387');
 
     /**
      * @license
