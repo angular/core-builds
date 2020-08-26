a3945c2e9dab921cb15e4d2f8097acb6aaa540b2
Misko Hevery <misko@hevery.com>
2020-08-26 19:16:09 +0000

    ecc6fd0d28 docs: release notes for the v10.0.14 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.8+13.sha-80cab26
+ * @license Angular v10.1.0-next.8+14.sha-ecc6fd0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19795,7 +19795,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-next.8+13.sha-80cab26');
+    var VERSION = new Version('10.1.0-next.8+14.sha-ecc6fd0');
 
     /**
      * @license
