481b310574472966e8b6f597bdd56d0d53efa26f
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:17:28 +0000

    89ff4467fd docs: add Sam Vloeberghs to GDE list (#37970)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14
+ * @license Angular v10.0.14+3.sha-89ff446
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19210,7 +19210,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14');
+const VERSION = new Version('10.0.14+3.sha-89ff446');
 
 /**
  * @license
