e9d7209b6b4d324056d90f49da13f5575b3b1054
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:13:19 +0000

    0681a20d28 fix(docs-infra): add guard to not render anchor link for private api (#38589)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0
+ * @license Angular v10.1.0-rc.0+1.sha-0681a20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19819,7 +19819,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-rc.0');
+    var VERSION = new Version('10.1.0-rc.0+1.sha-0681a20');
 
     /**
      * @license
