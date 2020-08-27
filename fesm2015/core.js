6e0978ed17c7e6dad397b94bd05b406b56bc02bd
Joey Perrott <josephperrott@gmail.com>
2020-08-27 18:21:48 +0000

    43ddfbaf31 build: bump to lastest sha of angular/dev-infra's lock-closed action (#38615)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+5.sha-94fd5c7
+ * @license Angular v10.0.14+6.sha-43ddfba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19210,7 +19210,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14+5.sha-94fd5c7');
+const VERSION = new Version('10.0.14+6.sha-43ddfba');
 
 /**
  * @license
