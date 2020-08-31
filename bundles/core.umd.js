8069877c419d8f7b47da2d38498f5461708be54e
Joey Perrott <josephperrott@gmail.com>
2020-08-31 23:44:51 +0000

    71acf9dd49 docs: Restructure table of contents to provide a more streamlined experience. (#38353)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+31.sha-f5a148b
+ * @license Angular v10.1.0-rc.0+32.sha-71acf9d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19819,7 +19819,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-rc.0+31.sha-f5a148b');
+    var VERSION = new Version('10.1.0-rc.0+32.sha-71acf9d');
 
     /**
      * @license
