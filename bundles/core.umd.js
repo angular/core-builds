5e958081f5b7c368f2e8b3beafdf1201b6c744c4
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:08:37 +0000

    827ba05914 docs: remove first person and space in CircleCI in the testing guide. (#38631)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+17.sha-5d5caf2
+ * @license Angular v10.1.0-rc.0+19.sha-827ba05
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19795,7 +19795,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-rc.0+17.sha-5d5caf2');
+    var VERSION = new Version('10.1.0-rc.0+19.sha-827ba05');
 
     /**
      * @license
