f67ae13acf679ac8dcbc309baf4fcae37fde962d
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:51:56 +0000

    b060837e70 docs: Remove confusion between do/avoid templates (#38647) PR Close #38647

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+21.sha-9a64980
+ * @license Angular v10.0.14+22.sha-b060837
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19818,7 +19818,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.14+21.sha-9a64980');
+    var VERSION = new Version('10.0.14+22.sha-b060837');
 
     /**
      * @license
