870fdf97d48dfcc5077d5ad1d41e4e114a5025b1
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:57:47 +0000

    9a649809ca refactor(forms): remove extra space in error message (#38637)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+19.sha-4e8592e
+ * @license Angular v10.0.14+21.sha-9a64980
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19818,7 +19818,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.0.14+19.sha-4e8592e');
+    var VERSION = new Version('10.0.14+21.sha-9a64980');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+19.sha-4e8592e
+ * @license Angular v10.0.14+21.sha-9a64980
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19210,7 +19210,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14+19.sha-4e8592e');
+const VERSION = new Version('10.0.14+21.sha-9a64980');
 
 /**
  * @license
