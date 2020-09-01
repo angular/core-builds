984b01331083dedbd472319b3176e9e9714c87d8
Joey Perrott <josephperrott@gmail.com>
2020-09-01 20:31:09 +0000

    59c234cfb4 build: add configuration for the caretaker command (#38601)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+35.sha-d9fea85
+ * @license Angular v10.1.0-rc.0+37.sha-59c234c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19819,7 +19819,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-rc.0+35.sha-d9fea85');
+    var VERSION = new Version('10.1.0-rc.0+37.sha-59c234c');
 
     /**
      * @license
