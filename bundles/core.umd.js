52d4ccf2e2f92f9a16d14576c90873b244202ca9
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:20:50 +0000

    c01bd0fe8e release: cut the v10.1.0 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+19.sha-5588324
+ * @license Angular v10.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19819,7 +19819,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-rc.0+19.sha-5588324');
+    var VERSION = new Version('10.1.0');
 
     /**
      * @license
