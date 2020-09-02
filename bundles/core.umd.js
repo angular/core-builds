6a17fa94901851553d408e6ba35d50a61f6e24e5
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:36:20 +0000

    e6ee7c2aeb release: cut the v11.0.0-next.0 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+37.sha-59c234c
+ * @license Angular v11.0.0-next.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19819,7 +19819,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0-rc.0+37.sha-59c234c');
+    var VERSION = new Version('11.0.0-next.0');
 
     /**
      * @license
