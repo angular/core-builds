53d6d774e649b853e7d9aab81d91d50f3cbac48e
atscott <atscott01@gmail.com>
2020-09-03 19:22:29 +0000

    25afbcc459 docs: add dayjs date adapter to resources page (#38031)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+6.sha-29c89c9
+ * @license Angular v10.1.0+7.sha-25afbcc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19211,7 +19211,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+6.sha-29c89c9');
+const VERSION = new Version('10.1.0+7.sha-25afbcc');
 
 /**
  * @license
