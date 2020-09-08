cb1cb26a4c396d250869f253c25ebb63e3378511
atscott <atscott01@gmail.com>
2020-09-08 17:30:07 +0000

    c2d017de83 docs: word correction (#38729) PR Close #38729

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+23.sha-a29f9a9
+ * @license Angular v11.0.0-next.0+16.sha-c2d017d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19795,7 +19795,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.0+23.sha-a29f9a9');
+    var VERSION = new Version('11.0.0-next.0+16.sha-c2d017d');
 
     /**
      * @license
