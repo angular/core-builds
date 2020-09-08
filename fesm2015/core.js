bfefcc6ca5d17275bb003f18c6e8a7ce35864fe0
atscott <atscott01@gmail.com>
2020-09-08 22:15:31 +0000

    ba95b79a21 perf(compiler-cli): optimize computation of type-check scope information (#38539)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+43.sha-4360eed
+ * @license Angular v11.0.0-next.0+45.sha-ba95b79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19172,7 +19172,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+43.sha-4360eed');
+const VERSION = new Version('11.0.0-next.0+45.sha-ba95b79');
 
 /**
  * @license
