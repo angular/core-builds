4d7d593bd1549baca459417fd05449d3f1f1dcc9
atscott <atscott01@gmail.com>
2020-09-08 21:49:03 +0000

    190dca0fdc fix(localize): enable whitespace preservation marker in XLIFF files (#38737)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+26.sha-a1c34c6
+ * @license Angular v10.1.0+34.sha-190dca0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19811,7 +19811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0+26.sha-a1c34c6');
+    var VERSION = new Version('10.1.0+34.sha-190dca0');
 
     /**
      * @license
