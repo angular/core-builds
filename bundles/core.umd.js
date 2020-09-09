3b5f8122ba456a9b15e1b88ba333811cf358d4ed
atscott <atscott01@gmail.com>
2020-09-09 18:05:44 +0000

    f1b355b54f docs: Restructure table of contents to provide a more streamlined experience (#38763)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+43.sha-b1398d1
+ * @license Angular v10.1.0+46.sha-f1b355b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19811,7 +19811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.0+43.sha-b1398d1');
+    var VERSION = new Version('10.1.0+46.sha-f1b355b');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+43.sha-b1398d1
+ * @license Angular v10.1.0+46.sha-f1b355b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19203,7 +19203,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+43.sha-b1398d1');
+const VERSION = new Version('10.1.0+46.sha-f1b355b');
 
 /**
  * @license
