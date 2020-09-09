b4bef3b5245928359969e99f17c5b7b7607d0c5e
atscott <atscott01@gmail.com>
2020-09-09 23:55:11 +0000

    1373a98e25 feat(dev-infra): Allow local user ng-dev configuration to disable commit message wizard (#38701)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+3.sha-3c4b8b9
+ * @license Angular v11.0.0-next.1+5.sha-1373a98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19787,7 +19787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.1+3.sha-3c4b8b9');
+    var VERSION = new Version('11.0.0-next.1+5.sha-1373a98');
 
     /**
      * @license
