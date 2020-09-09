0eb1db2bd5f0ad5edd9d6adef221645df5a58a84
atscott <atscott01@gmail.com>
2020-09-09 16:31:02 +0000

    3cb2a79399 docs: update v9 support status to LTS (#38744)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+45.sha-ba95b79
+ * @license Angular v11.0.0-next.0+49.sha-3cb2a79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19172,7 +19172,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+45.sha-ba95b79');
+const VERSION = new Version('11.0.0-next.0+49.sha-3cb2a79');
 
 /**
  * @license
