66351bbf9ded7dca12929e4552c59ceb34213dd6
Andrew Kushnir <akushnir@google.com>
2020-09-10 22:57:45 +0000

    6acea54f62 fix(common): mark locale data arrays as readonly (#30397)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+22.sha-2d52c80
+ * @license Angular v11.0.0-next.1+23.sha-6acea54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.1+22.sha-2d52c80');
+    var VERSION = new Version('11.0.0-next.1+23.sha-6acea54');
 
     /**
      * @license
