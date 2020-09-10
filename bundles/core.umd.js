50d1c09a13c015bf34a9e112c9237baaa5517481
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:57:15 +0000

    2d52c80332 test(compiler): Add back tests for renamed inputs and outputs (#38798)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+20.sha-8f349b2
+ * @license Angular v11.0.0-next.1+22.sha-2d52c80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.1+20.sha-8f349b2');
+    var VERSION = new Version('11.0.0-next.1+22.sha-2d52c80');
 
     /**
      * @license
