962d3264c59666a3e905df178dc3ffa703cf020a
Andrew Kushnir <akushnir@google.com>
2020-09-11 16:05:39 +0000

    8096c63c66 docs: clarify what fixes are merged to LTS versions (#38788)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+13.sha-31d0ee4
+ * @license Angular v10.1.1+15.sha-8096c63
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.1+13.sha-31d0ee4');
+    var VERSION = new Version('10.1.1+15.sha-8096c63');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+13.sha-31d0ee4
+ * @license Angular v10.1.1+15.sha-8096c63
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19204,7 +19204,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+13.sha-31d0ee4');
+const VERSION = new Version('10.1.1+15.sha-8096c63');
 
 /**
  * @license
