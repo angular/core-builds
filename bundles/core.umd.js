eedb1843cdbee20038c9ce7fd47eaecd924e3aa6
Andrew Kushnir <akushnir@google.com>
2020-09-11 20:44:26 +0000

    b05d79d14a build(router): update symbols for routing app (#38817)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+15.sha-8096c63
+ * @license Angular v10.1.1+18.sha-b05d79d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.1+15.sha-8096c63');
+    var VERSION = new Version('10.1.1+18.sha-b05d79d');
 
     /**
      * @license
