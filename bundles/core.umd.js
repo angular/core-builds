0a9e280abbf927ccbb470d4f8c31948730ec00c6
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:22:45 +0000

    6768fe9927 fix(dev-infra): correct build setup for dev-infra (#38815)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+29.sha-57c442f
+ * @license Angular v11.0.0-next.1+30.sha-6768fe9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.1+29.sha-57c442f');
+    var VERSION = new Version('11.0.0-next.1+30.sha-6768fe9');
 
     /**
      * @license
