3bf4921a78c0d33f36bcb8099d1b8a37a653ca36
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:21:43 +0000

    aa43cbf8c5 fix(dev-infra): correct build setup for dev-infra (#38815)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+18.sha-b05d79d
+ * @license Angular v10.1.1+19.sha-aa43cbf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.1+18.sha-b05d79d');
+    var VERSION = new Version('10.1.1+19.sha-aa43cbf');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+18.sha-b05d79d
+ * @license Angular v10.1.1+19.sha-aa43cbf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19204,7 +19204,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+18.sha-b05d79d');
+const VERSION = new Version('10.1.1+19.sha-aa43cbf');
 
 /**
  * @license
