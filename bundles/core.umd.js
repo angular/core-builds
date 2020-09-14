f9eacdcdcdf32e252783b90ea5226787a280fc7b
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:55:50 +0000

    26deef2d3e feat(dev-infra): allow local ng-dev configuration to error on invalid commit messages (#38784)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+22.sha-ebede67
+ * @license Angular v10.1.1+24.sha-26deef2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.1+22.sha-ebede67');
+    var VERSION = new Version('10.1.1+24.sha-26deef2');
 
     /**
      * @license
