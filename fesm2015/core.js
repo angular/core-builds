602d65482922a5442bd4f4ec732ec904006a1fe6
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:55:06 +0000

    3934f0a833 docs(elements): convert the `ng add` command to code-example (#38834)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+34.sha-297c060
+ * @license Angular v11.0.0-next.1+35.sha-3934f0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19173,7 +19173,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+34.sha-297c060');
+const VERSION = new Version('11.0.0-next.1+35.sha-3934f0a');
 
 /**
  * @license
