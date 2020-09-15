907271b3435a117dc8452868eb769550468f9bd1
Andrew Kushnir <akushnir@google.com>
2020-09-15 16:09:18 +0000

    b9ca6d20cc feat(dev-infra): include CI status check in the caretaker check (#38779)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+39.sha-e00535a
+ * @license Angular v11.0.0-next.1+40.sha-b9ca6d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.1+39.sha-e00535a');
+    var VERSION = new Version('11.0.0-next.1+40.sha-b9ca6d2');
 
     /**
      * @license
