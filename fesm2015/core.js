ab2530bb0118dce5965e998d39b3c90cf44a1fe8
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:40:31 +0000

    97adc27207 docs(bazel): fix typo in BUILD.bazel comments (#38054)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+2.sha-4407449
+ * @license Angular v11.0.0-next.2+6.sha-97adc27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19173,7 +19173,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+2.sha-4407449');
+const VERSION = new Version('11.0.0-next.2+6.sha-97adc27');
 
 /**
  * @license
