42009167fd78953c22a2f01689c569b35f3b6d7f
mgechev <mgechev@gmail.com>
2020-09-16 23:03:37 +0000

    4a21eb84a7 docs: add upcoming angular conferences

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+47.sha-956e4f2
+ * @license Angular v11.0.0-next.2+12.sha-4a21eb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19204,7 +19204,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+47.sha-956e4f2');
+const VERSION = new Version('11.0.0-next.2+12.sha-4a21eb8');
 
 /**
  * @license
