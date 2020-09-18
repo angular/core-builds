a72b2dd6939b3db91f125f9e2d0adae5221385a9
Misko Hevery <misko@hevery.com>
2020-09-18 18:48:54 +0000

    d92a0dd72f fix(zone.js): should invoke xhr send task when no response error occurs (#38836)

--- src/errors.d.ts
+++ src/errors.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { DebugContext } from './view/types';
+import { DebugContext } from './view';
 export declare function getType(error: Error): Function;
 export declare function getDebugContext(error: Error): DebugContext;
 export declare function getOriginalError(error: Error): Error;
