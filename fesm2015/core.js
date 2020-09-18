3d45bce40878fd7a8a2ea6f85777705cf246baf8
Misko Hevery <misko@hevery.com>
2020-09-18 15:26:19 +0000

    e4424863c2 fix(ngcc): fix compilation of `ChangeDetectorRef` in pipe constructors (#38892)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+14.sha-c4b8964
+ * @license Angular v11.0.0-next.2+20.sha-e442486
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19204,7 +19204,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+14.sha-c4b8964');
+const VERSION = new Version('11.0.0-next.2+20.sha-e442486');
 
 /**
  * @license
