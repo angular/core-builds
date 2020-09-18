bcab2855a00d404ec3379fbb5934f6460484541c
Misko Hevery <misko@hevery.com>
2020-09-18 15:27:45 +0000

    d3169c533e refactor(core): remove unused imports (#38818)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+17.sha-02c6bff
+ * @license Angular v11.0.0-next.2+21.sha-d3169c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.2+17.sha-02c6bff');
+    var VERSION = new Version('11.0.0-next.2+21.sha-d3169c5');
 
     /**
      * @license
