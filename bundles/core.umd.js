7ed7425f8933fc06f25821ec3f67a2823f9c8e18
Misko Hevery <misko@hevery.com>
2020-09-19 00:17:42 +0000

    7d5b5153cf docs: Grammar fixes (#38900)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+21.sha-3902ec0
+ * @license Angular v10.1.2+24.sha-7d5b515
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.2+21.sha-3902ec0');
+    var VERSION = new Version('10.1.2+24.sha-7d5b515');
 
     /**
      * @license
