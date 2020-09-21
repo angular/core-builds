6f4b5f3e764d921c78050e4135854335f422cf9b
Misko Hevery <misko@hevery.com>
2020-09-21 19:59:34 +0000

    297b123151 refactor(compiler-cli): make the output AST translator generic (#38775)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+40.sha-6ae3b68
+ * @license Angular v11.0.0-next.2+47.sha-297b123
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19788,7 +19788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.2+40.sha-6ae3b68');
+    var VERSION = new Version('11.0.0-next.2+47.sha-297b123');
 
     /**
      * @license
