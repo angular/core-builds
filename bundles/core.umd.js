be2a679ae102e7da4785c4191aea1e07bcf27d36
Misko Hevery <misko@hevery.com>
2020-09-21 16:58:18 +0000

    e498ea9b5a revert: feat(router): better warning message when a router outlet has not been instantiated (#38920)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+29.sha-d5ddb9f
+ * @license Angular v11.0.0-next.2+31.sha-e498ea9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19788,7 +19788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.2+29.sha-d5ddb9f');
+    var VERSION = new Version('11.0.0-next.2+31.sha-e498ea9');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+29.sha-d5ddb9f
+ * @license Angular v11.0.0-next.2+31.sha-e498ea9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19173,7 +19173,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+29.sha-d5ddb9f');
+const VERSION = new Version('11.0.0-next.2+31.sha-e498ea9');
 
 /**
  * @license
