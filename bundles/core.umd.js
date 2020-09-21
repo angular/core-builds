09a88e8c42b6ad94f1c7894a9d4ebe26784da6bf
Misko Hevery <misko@hevery.com>
2020-09-21 19:45:02 +0000

    f3f6a42342 test(docs-infra): replace deprecated `ReflectiveInjector` with `Injector` (#38897)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+31.sha-e498ea9
+ * @license Angular v11.0.0-next.2+32.sha-f3f6a42
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.2+31.sha-e498ea9');
+    var VERSION = new Version('11.0.0-next.2+32.sha-f3f6a42');
 
     /**
      * @license
