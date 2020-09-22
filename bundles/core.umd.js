c51787675261a31fef141bc175f5e9e8eca0ee56
Misko Hevery <misko@hevery.com>
2020-09-22 22:32:33 +0000

    32e32e5bdc refactor(dev-infra): simplify runBenchmark (#38941)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+24.sha-7d5b515
+ * @license Angular v10.1.2+33.sha-32e32e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.2+24.sha-7d5b515');
+    var VERSION = new Version('10.1.2+33.sha-32e32e5');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+24.sha-7d5b515
+ * @license Angular v10.1.2+33.sha-32e32e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19204,7 +19204,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.2+24.sha-7d5b515');
+const VERSION = new Version('10.1.2+33.sha-32e32e5');
 
 /**
  * @license
