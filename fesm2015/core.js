ca4cc728aa125c5b7893a72b1461d93e88196589
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:04:18 +0000

    4baabf9cd3 release: cut the v11.0.0-next.3 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+55.sha-7244e1b
+ * @license Angular v11.0.0-next.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19204,7 +19204,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+55.sha-7244e1b');
+const VERSION = new Version('11.0.0-next.3');
 
 /**
  * @license
