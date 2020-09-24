e8e62381667159d9335875e15e23b8ea5d2e94d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 20:43:36 +0000

    837889f0a4 fix(router): make relativeLinkResolution corrected by default (#25609)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+8.sha-4c87665
+ * @license Angular v11.0.0-next.3+10.sha-837889f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19173,7 +19173,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+8.sha-4c87665');
+const VERSION = new Version('11.0.0-next.3+10.sha-837889f');
 
 /**
  * @license
