a96c12f063dc93e66aa836b7bb1bc9b164907669
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:57:44 +0000

    e790c8547e test(compiler-cli): load test files into memory only once (#38909)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+17.sha-4ca1c73
+ * @license Angular v11.0.0-next.3+14.sha-e790c85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.3+17.sha-4ca1c73');
+    var VERSION = new Version('11.0.0-next.3+14.sha-e790c85');
 
     /**
      * @license
