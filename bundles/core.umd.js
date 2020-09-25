ccb19bebee26ecbf5df250c1fa4328ff771d5cc9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:52:21 +0000

    2f6777df18 docs(docs-infra): add upcoming conference (#38956)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+12.sha-7f6f8bb
+ * @license Angular v11.0.0-next.3+18.sha-2f6777d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19812,7 +19812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.3+12.sha-7f6f8bb');
+    var VERSION = new Version('11.0.0-next.3+18.sha-2f6777d');
 
     /**
      * @license
