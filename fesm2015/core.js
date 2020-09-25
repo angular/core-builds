16aa144687aa2a1151e6090812a5595ea63bad02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 15:04:47 +0000

    e4f4d18e7e refactor(router): Adjust type of parameter in navigateByUrl and createUrlTree to be more accurate (#38227)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+10.sha-837889f
+ * @license Angular v11.0.0-next.3+11.sha-e4f4d18
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19173,7 +19173,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+10.sha-837889f');
+const VERSION = new Version('11.0.0-next.3+11.sha-e4f4d18');
 
 /**
  * @license
