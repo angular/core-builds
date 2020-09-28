139e408215307f7929b7674d7badca69647e8a0e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:52:04 +0000

    c5e178997c build(docs-infra): update docs examples to Angular v10 (#38993)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+69.sha-4fd635a
+ * @license Angular v11.0.0-next.3+65.sha-c5e1789
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+69.sha-4fd635a');
+const VERSION = new Version('11.0.0-next.3+65.sha-c5e1789');
 
 /**
  * @license
