d99f0c713587f94ce082c9cad0e9808ae208d862
Joey Perrott <josephperrott@gmail.com>
2020-09-30 20:11:32 +0000

    8e3710349d build(docs-infra): upgrade cli command docs sources to e526da605 (#39059)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+3.sha-eec9f4a
+ * @license Angular v11.0.0-next.4+2.sha-8e37103
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.4+3.sha-eec9f4a');
+    var VERSION = new Version('11.0.0-next.4+2.sha-8e37103');
 
     /**
      * @license
