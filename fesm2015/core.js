668380e5954e077eb01ed20dfb98a20250585516
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:57:07 +0000

    1e3f810f8f fix(bazel): clean up outstanding failure message for usages of --define=compile (#39046)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+89.sha-2aeaedc
+ * @license Angular v11.0.0-next.3+94.sha-1e3f810
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16240,7 +16240,7 @@ function markDuplicates(tData, tStylingKey, index, isPrevDir, isClassBinding) {
 /**
  * Determines if two `TStylingKey`s are a match.
  *
- * When computing weather a binding contains a duplicate, we need to compare if the instruction
+ * When computing whether a binding contains a duplicate, we need to compare if the instruction
  * `TStylingKey` has a match.
  *
  * Here are examples of `TStylingKey`s which match given `tStylingKeyCursor` is:
@@ -19221,7 +19221,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+89.sha-2aeaedc');
+const VERSION = new Version('11.0.0-next.3+94.sha-1e3f810');
 
 /**
  * @license
