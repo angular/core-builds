5799ab0cad7d46dd582b97ad44b918eaa38913cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:55:46 +0000

    e8084ff906 build(docs-infra): upgrade cli command docs sources to 568d34cdb (#39045)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+94.sha-1e3f810
+ * @license Angular v11.0.0-next.3+93.sha-e8084ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.3+94.sha-1e3f810');
+    var VERSION = new Version('11.0.0-next.3+93.sha-e8084ff');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+94.sha-1e3f810
+ * @license Angular v11.0.0-next.3+93.sha-e8084ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+94.sha-1e3f810');
+const VERSION = new Version('11.0.0-next.3+93.sha-e8084ff');
 
 /**
  * @license
