612fffaf626152116acfb50114dc1ac2fe5b66ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:37:52 +0000

    a6971ba89a release: cut the zone.js-0.11.2 release (#38911)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+76.sha-e2997ed
+ * @license Angular v11.0.0-next.3+80.sha-a6971ba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+76.sha-e2997ed');
+const VERSION = new Version('11.0.0-next.3+80.sha-a6971ba');
 
 /**
  * @license
