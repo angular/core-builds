6694962dc43f6b9dc82697438c697309ef3c312a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:15:19 +0000

    a1bf0de711 release: cut the zone.js-0.11.2 release (#39032)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+60.sha-69302ad
+ * @license Angular v10.1.3+72.sha-a1bf0de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16271,7 +16271,7 @@ function markDuplicates(tData, tStylingKey, index, isPrevDir, isClassBinding) {
 /**
  * Determines if two `TStylingKey`s are a match.
  *
- * When computing weather a binding contains a duplicate, we need to compare if the instruction
+ * When computing whether a binding contains a duplicate, we need to compare if the instruction
  * `TStylingKey` has a match.
  *
  * Here are examples of `TStylingKey`s which match given `tStylingKeyCursor` is:
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.3+60.sha-69302ad');
+const VERSION = new Version('10.1.3+72.sha-a1bf0de');
 
 /**
  * @license
