f533494ffd3620ea259877cb12b4b2b5bae0c53f
Joey Perrott <josephperrott@gmail.com>
2020-09-30 22:22:25 +0000

    ea1968317e fix(router): update getRouteGuards to check if the context outlet is activated (#39049)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+10.sha-5b15e5e
+ * @license Angular v11.0.0-next.4+11.sha-ea19683
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.4+10.sha-5b15e5e');
+    var VERSION = new Version('11.0.0-next.4+11.sha-ea19683');
 
     /**
      * @license
