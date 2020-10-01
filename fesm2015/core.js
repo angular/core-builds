f6e747ace3fc32dea91402e8b2bc62fd74266eb5
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:58:59 +0000

    ac54019315 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+23.sha-8894706
+ * @license Angular v11.0.0-next.4+22.sha-ac54019
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+23.sha-8894706');
+const VERSION = new Version('11.0.0-next.4+22.sha-ac54019');
 
 /**
  * @license
