dfcde9f8bd6b0eb0c6b3cc8c81386bc196dc7689
Joey Perrott <josephperrott@gmail.com>
2020-10-02 19:04:43 +0000

    f50313f54d feat(compiler): Recover on malformed keyed reads and keyed writes (#39004)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+20.sha-30433a0
+ * @license Angular v10.1.4+21.sha-f50313f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.4+20.sha-30433a0');
+    var VERSION = new Version('10.1.4+21.sha-f50313f');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+20.sha-30433a0
+ * @license Angular v10.1.4+21.sha-f50313f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+20.sha-30433a0');
+const VERSION = new Version('10.1.4+21.sha-f50313f');
 
 /**
  * @license
