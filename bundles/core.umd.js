954d7e0001abefb4fc3a9ff802caa624db0da02b
Joey Perrott <josephperrott@gmail.com>
2020-10-02 22:39:11 +0000

    89c5255b8c refactor(compiler): iteratively parse interpolations (#38977)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+35.sha-5dbf357
+ * @license Angular v11.0.0-next.4+36.sha-89c5255
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.4+35.sha-5dbf357');
+    var VERSION = new Version('11.0.0-next.4+36.sha-89c5255');
 
     /**
      * @license
