3e5df60d5ab202ecc6a7d545cab72ecccbd32547
Joey Perrott <josephperrott@gmail.com>
2020-10-02 18:21:48 +0000

    30433a0710 fix(docs-infra): better distinguish wrapped headings from other entries in TOC (#39092)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+18.sha-42f9679
+ * @license Angular v10.1.4+20.sha-30433a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.4+18.sha-42f9679');
+    var VERSION = new Version('10.1.4+20.sha-30433a0');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+18.sha-42f9679
+ * @license Angular v10.1.4+20.sha-30433a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+18.sha-42f9679');
+const VERSION = new Version('10.1.4+20.sha-30433a0');
 
 /**
  * @license
