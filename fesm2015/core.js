5df6c398eb05269dfdb3b575e7265a1cc73ed8bc
Joey Perrott <josephperrott@gmail.com>
2020-10-02 18:24:15 +0000

    904adb72d2 feat(language-service): add quick info for inline templates in ivy (#39060)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+31.sha-d3bd00b
+ * @license Angular v11.0.0-next.4+33.sha-904adb7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19221,7 +19221,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+31.sha-d3bd00b');
+const VERSION = new Version('11.0.0-next.4+33.sha-904adb7');
 
 /**
  * @license
