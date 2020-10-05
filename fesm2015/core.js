943cdf04b500fa2e5644f15540ca162e67b9d092
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:50:15 +0000

    bc69182bdd docs: Migrate section, view encapsulation, from Component Styles topic into its own topic. (#38986)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+43.sha-82d54fe
+ * @license Angular v11.0.0-next.4+44.sha-bc69182
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+43.sha-82d54fe');
+const VERSION = new Version('11.0.0-next.4+44.sha-bc69182');
 
 /**
  * @license
