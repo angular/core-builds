202c1937fe117008933b677531954cd987dd4ecb
Joey Perrott <josephperrott@gmail.com>
2020-10-05 15:31:07 +0000

    4c30f5135b docs(upgrade): expand upon change detection implications for downgraded components (#39100)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+22.sha-6791cd7
+ * @license Angular v10.1.4+23.sha-4c30f51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+22.sha-6791cd7');
+const VERSION = new Version('10.1.4+23.sha-4c30f51');
 
 /**
  * @license
