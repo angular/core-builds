04725ad77500dd34409dd7fa060ce8c1cbd85377
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:05:56 +0000

    26afcdd86c docs: Made a minor change in the documentation (#38917) PR Close #38917

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+41.sha-75e90fa
+ * @license Angular v11.0.0-next.4+40.sha-26afcdd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+41.sha-75e90fa');
+const VERSION = new Version('11.0.0-next.4+40.sha-26afcdd');
 
 /**
  * @license
