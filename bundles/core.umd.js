2ec2b57a4ea82f18aec028b26f8b6bf6bdac2d56
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:43:43 +0000

    82d54fe8c3 feat(zone.js): add jest fakeTimers support (#39016)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+40.sha-26afcdd
+ * @license Angular v11.0.0-next.4+43.sha-82d54fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.4+40.sha-26afcdd');
+    var VERSION = new Version('11.0.0-next.4+43.sha-82d54fe');
 
     /**
      * @license
