2457007dd1d0ad9a1f3263854842f8d53ea92919
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:07:55 +0000

    75e90fa456 docs(common): update docs for HttpClient methods (#38878)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+39.sha-f315022
+ * @license Angular v11.0.0-next.4+41.sha-75e90fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19221,7 +19221,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+39.sha-f315022');
+const VERSION = new Version('11.0.0-next.4+41.sha-75e90fa');
 
 /**
  * @license
