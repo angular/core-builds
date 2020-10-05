f789ab0e5c46792b433ce45064023fdba70041e7
Joey Perrott <josephperrott@gmail.com>
2020-10-05 23:36:40 +0000

    faa81dcdd1 refactor(language-service): Extract common methods from VE and Ivy to new package (#39098)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+46.sha-e10b3e2
+ * @license Angular v11.0.0-next.4+47.sha-faa81dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+46.sha-e10b3e2');
+const VERSION = new Version('11.0.0-next.4+47.sha-faa81dc');
 
 /**
  * @license
