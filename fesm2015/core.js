895043d07221a91dcbd30df678be1b15fa9a9cc3
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:20:05 +0000

    072b707b38 ci: update to latest version of sauce-connect (#39073)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+24.sha-878e2f0
+ * @license Angular v10.1.4+27.sha-072b707
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+24.sha-878e2f0');
+const VERSION = new Version('10.1.4+27.sha-072b707');
 
 /**
  * @license
