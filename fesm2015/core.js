5f1ce2c57cdba7050c9ad510774cbbff5cc54014
Joey Perrott <josephperrott@gmail.com>
2020-10-06 21:18:34 +0000

    f2fca6d58e refactor(compiler-cli): add a global autocompletion API (#39048)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+60.sha-aeec223
+ * @license Angular v11.0.0-next.4+59.sha-f2fca6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+60.sha-aeec223');
+const VERSION = new Version('11.0.0-next.4+59.sha-f2fca6d');
 
 /**
  * @license
