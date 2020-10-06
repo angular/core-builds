fae564de236c32232d8b45414b4d8897311fe823
Joey Perrott <josephperrott@gmail.com>
2020-10-06 21:18:25 +0000

    aeec223db1 feat(forms): add migration for AbstractControl.parent accesses (#39009)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+57.sha-3975dd9
+ * @license Angular v11.0.0-next.4+60.sha-aeec223
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19221,7 +19221,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+57.sha-3975dd9');
+const VERSION = new Version('11.0.0-next.4+60.sha-aeec223');
 
 /**
  * @license
