7d8c10e1e7d7b1adba1891849b206db44120a0a1
Joey Perrott <josephperrott@gmail.com>
2020-10-06 17:35:40 +0000

    246de9aaad fix(forms): improve types of directive constructor arguments (#38944)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+53.sha-07a66fc
+ * @license Angular v11.0.0-next.4+54.sha-246de9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.4+53.sha-07a66fc');
+    var VERSION = new Version('11.0.0-next.4+54.sha-246de9a');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+53.sha-07a66fc
+ * @license Angular v11.0.0-next.4+54.sha-246de9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+53.sha-07a66fc');
+const VERSION = new Version('11.0.0-next.4+54.sha-246de9a');
 
 /**
  * @license
