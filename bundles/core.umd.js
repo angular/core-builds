0c73a0affbb185e0f31fbf922c9001a2115e9c7b
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:00:34 +0000

    d8c05347c3 fix(router): properly assign ExtraOptions to Router in RouterTestingModule (#39096)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+47.sha-faa81dc
+ * @license Angular v11.0.0-next.4+48.sha-d8c0534
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.4+47.sha-faa81dc');
+    var VERSION = new Version('11.0.0-next.4+48.sha-d8c0534');
 
     /**
      * @license
