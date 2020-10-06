663ceedaeca2716d631ce9edbd40611fe2192ea5
Joey Perrott <josephperrott@gmail.com>
2020-10-06 16:00:17 +0000

    07a66fc4c2 docs: remove explicit boolean type in examples as TS infers it automatically (#39081)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+52.sha-6e0a8eb
+ * @license Angular v11.0.0-next.4+53.sha-07a66fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.4+52.sha-6e0a8eb');
+    var VERSION = new Version('11.0.0-next.4+53.sha-07a66fc');
 
     /**
      * @license
