03e1b8b194cf142ab1f1f39511d7bed76657817f
Joey Perrott <josephperrott@gmail.com>
2020-10-06 21:20:57 +0000

    f2fca6d58e refactor(compiler-cli): add a global autocompletion API (#39048)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+60.sha-aeec223
+ * @license Angular v11.0.0-next.4+59.sha-f2fca6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19852,7 +19852,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.4+60.sha-aeec223');
+    var VERSION = new Version('11.0.0-next.4+59.sha-f2fca6d');
 
     /**
      * @license
