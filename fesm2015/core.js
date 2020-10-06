f11044751958f5555fc7a70b05f14df3d9d4986a
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:32:21 +0000

    3c474ecf56 fix(common): add boolean to valid json for testing (#37893)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+49.sha-ee03408
+ * @license Angular v11.0.0-next.4+50.sha-3c474ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+49.sha-ee03408');
+const VERSION = new Version('11.0.0-next.4+50.sha-3c474ec');
 
 /**
  * @license
