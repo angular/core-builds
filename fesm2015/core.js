ccea35eade4fc77de4bd3cd61ae4346be2412a3e
Joey Perrott <josephperrott@gmail.com>
2020-10-06 15:59:38 +0000

    6e0a8eb268 docs: add info about working with fixup commits (#39110)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+51.sha-21dfb33
+ * @license Angular v11.0.0-next.4+52.sha-6e0a8eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+51.sha-21dfb33');
+const VERSION = new Version('11.0.0-next.4+52.sha-6e0a8eb');
 
 /**
  * @license
