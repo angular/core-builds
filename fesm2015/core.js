f3f3ac60abfb67571ff45a75e642f4cd2d1477da
Joey Perrott <josephperrott@gmail.com>
2020-10-07 20:54:22 +0000

    293270696b build(docs-infra): render optional decorator options with a ? (#39167)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+59.sha-f2fca6d
+ * @license Angular v11.0.0-next.4+63.sha-2932706
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+59.sha-f2fca6d');
+const VERSION = new Version('11.0.0-next.4+63.sha-2932706');
 
 /**
  * @license
