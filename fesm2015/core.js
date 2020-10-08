80c1317375b067c3124e01e043356c4387b8909d
atscott <atscott01@gmail.com>
2020-10-08 19:22:32 +0000

    7628c36f49 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0 (#39182)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+5.sha-b417dd7
+ * @license Angular v10.1.5+6.sha-7628c36
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+5.sha-b417dd7');
+const VERSION = new Version('10.1.5+6.sha-7628c36');
 
 /**
  * @license
