5dedad411146acfd31ec890693260fd84e60cdcf
atscott <atscott01@gmail.com>
2020-10-09 15:58:51 +0000

    52a0c6b36e fix(compiler): incorrectly encapsulating @import containing colons and semicolons (#38716)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+7.sha-8913aee
+ * @license Angular v10.1.5+8.sha-52a0c6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.5+7.sha-8913aee');
+    var VERSION = new Version('10.1.5+8.sha-52a0c6b');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+7.sha-8913aee
+ * @license Angular v10.1.5+8.sha-52a0c6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+7.sha-8913aee');
+const VERSION = new Version('10.1.5+8.sha-52a0c6b');
 
 /**
  * @license
