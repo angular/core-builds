dc5493d26310e41b591734b4e33d45a6f7450aef
atscott <atscott01@gmail.com>
2020-10-12 16:16:08 +0000

    952710b43b refactor(compiler-cli): ensure `isNamed....()` helpers check name is identity (#38959)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+22.sha-e930b0c
+ * @license Angular v11.0.0-next.5+40.sha-952710b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19844,7 +19844,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.5+22.sha-e930b0c');
+    var VERSION = new Version('11.0.0-next.5+40.sha-952710b');
 
     /**
      * @license
