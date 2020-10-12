2aa50074b1373d1295a992bd4b1f66d4af812e82
atscott <atscott01@gmail.com>
2020-10-12 20:21:55 +0000

    b0b4953fd6 fix(router): Allow undefined inputs on routerLink (#39151)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+46.sha-72f4497
+ * @license Angular v11.0.0-next.5+47.sha-b0b4953
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19213,7 +19213,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+46.sha-72f4497');
+const VERSION = new Version('11.0.0-next.5+47.sha-b0b4953');
 
 /**
  * @license
