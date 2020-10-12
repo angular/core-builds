e18525568cf57e50806fad9c3417fe0775f97803
atscott <atscott01@gmail.com>
2020-10-12 18:11:42 +0000

    5d98834585 fix(dev-infra): correctly check for commit that closes PR (#39135)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+12.sha-2d37e47
+ * @license Angular v10.1.5+15.sha-5d98834
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.5+12.sha-2d37e47');
+    var VERSION = new Version('10.1.5+15.sha-5d98834');
 
     /**
      * @license
