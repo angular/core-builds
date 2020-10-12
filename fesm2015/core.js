02e9a7610a38811ae93deddc4f49003e0863b639
atscott <atscott01@gmail.com>
2020-10-12 16:04:20 +0000

    2d37e47e95 build(docs-infra): support passing args to `ng serve` via `serve-and-sync` (#39201)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+10.sha-952fd86
+ * @license Angular v10.1.5+12.sha-2d37e47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+10.sha-952fd86');
+const VERSION = new Version('10.1.5+12.sha-2d37e47');
 
 /**
  * @license
