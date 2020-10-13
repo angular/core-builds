bdf553d4bde47f43c5fe14b515b5c79b595866a2
atscott <atscott01@gmail.com>
2020-10-13 22:21:48 +0000

    b642f0bf45 docs(zone.js): update changelog to add jest useFakeTimers feature (#39126)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+51.sha-0875fd2
+ * @license Angular v11.0.0-next.5+53.sha-b642f0b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19844,7 +19844,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.5+51.sha-0875fd2');
+    var VERSION = new Version('11.0.0-next.5+53.sha-b642f0b');
 
     /**
      * @license
