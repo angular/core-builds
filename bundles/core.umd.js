14f04d3c71572ae14f125fd9807be38f1dbed53a
atscott <atscott01@gmail.com>
2020-10-14 20:22:18 +0000

    e08d02145e fix(ngcc): ensure that "inline exports" can be interpreted correctly (#39272)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+23.sha-df09415
+ * @license Angular v10.1.5+42.sha-e08d021
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19876,7 +19876,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.5+23.sha-df09415');
+    var VERSION = new Version('10.1.5+42.sha-e08d021');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+23.sha-df09415
+ * @license Angular v10.1.5+42.sha-e08d021
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+23.sha-df09415');
+const VERSION = new Version('10.1.5+42.sha-e08d021');
 
 /**
  * @license
