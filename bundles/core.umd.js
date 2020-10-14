2de4385b7141744794066a72ead41a4187c45b8c
atscott <atscott01@gmail.com>
2020-10-14 21:26:30 +0000

    a5d8d03edb release: cut the v11.0.0-next.6 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+74.sha-0ec7043
+ * @license Angular v11.0.0-next.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19931,7 +19931,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.5+74.sha-0ec7043');
+    var VERSION = new Version('11.0.0-next.6');
 
     /**
      * @license
