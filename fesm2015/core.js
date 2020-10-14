6a4f0a14b78d3f35533aba346bd3de0a816a4b8d
atscott <atscott01@gmail.com>
2020-10-14 21:04:05 +0000

    5db7b4c354 release: cut the v10.1.6 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+42.sha-e08d021
+ * @license Angular v10.1.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19252,7 +19252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+42.sha-e08d021');
+const VERSION = new Version('10.1.6');
 
 /**
  * @license
