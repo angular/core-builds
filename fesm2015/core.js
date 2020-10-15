a1a7a5048c5e4b32a2ba8e5fa0f88d7491286f7a
Andrew Kushnir <akushnir@google.com>
2020-10-15 21:40:00 +0000

    6acddb9518 refactor(language-service): Return directive defs when input name is part of selector (#39243)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+15.sha-14f15ec
+ * @license Angular v11.0.0-next.6+22.sha-6acddb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21183,7 +21183,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+15.sha-14f15ec');
+const VERSION = new Version('11.0.0-next.6+22.sha-6acddb9');
 
 /**
  * @license
