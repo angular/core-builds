1d4d0bac38a748921b3806d08dd2cc3efa14ad06
Andrew Kushnir <akushnir@google.com>
2020-10-15 18:43:10 +0000

    14f15ecd38 ci: add jessicajaniuk to require-minimum-review reviewers (#39268)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+14.sha-56ab6d5
+ * @license Angular v11.0.0-next.6+15.sha-14f15ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21183,7 +21183,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+14.sha-56ab6d5');
+const VERSION = new Version('11.0.0-next.6+15.sha-14f15ec');
 
 /**
  * @license
