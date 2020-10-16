9544528a3cf2e5274c316a4d3aeebe14a0989933
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:58:51 +0000

    1b21350e17 test(language-service): Make project service a singleton (#39308)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+40.sha-0c01c4a
+ * @license Angular v11.0.0-next.6+41.sha-1b21350
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21229,7 +21229,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+40.sha-0c01c4a');
+const VERSION = new Version('11.0.0-next.6+41.sha-1b21350');
 
 /**
  * @license
