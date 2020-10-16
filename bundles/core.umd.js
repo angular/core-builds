a494943f34c316c782355b991845371f6b44dc24
Andrew Kushnir <akushnir@google.com>
2020-10-16 22:10:21 +0000

    8fd59dabba ci: separate the windows CI tests into build and test (#39289)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+20.sha-d8e313b
+ * @license Angular v10.1.6+21.sha-8fd59da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21677,7 +21677,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.1.6+20.sha-d8e313b');
+    var VERSION = new Version('10.1.6+21.sha-8fd59da');
 
     /**
      * @license
