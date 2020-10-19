30d7782e827afe01ec43618ecc3096e0ca8e3252
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:56:44 +0000

    41c2228d49 docs: clarify grammatical error (#39279)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+53.sha-f5a3e44
+ * @license Angular v11.0.0-next.6+52.sha-41c2228
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21873,7 +21873,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.6+53.sha-f5a3e44');
+    var VERSION = new Version('11.0.0-next.6+52.sha-41c2228');
 
     /**
      * @license
