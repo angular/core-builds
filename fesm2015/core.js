ba1528185e8efce433286179b4ac454a8c68a1e3
Andrew Kushnir <akushnir@google.com>
2020-10-20 20:49:13 +0000

    a7964f4eca fix(common): update locales using new CLDR data (#39343)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+57.sha-a67895c
+ * @license Angular v11.0.0-next.6+59.sha-a7964f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21229,7 +21229,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+57.sha-a67895c');
+const VERSION = new Version('11.0.0-next.6+59.sha-a7964f4');
 
 /**
  * @license
