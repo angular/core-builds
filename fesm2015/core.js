d84e064c056b58f9e5ba8f103f3833d4b03e7e7e
Andrew Kushnir <akushnir@google.com>
2020-10-20 21:00:46 +0000

    9fb7bdea89 fix(router): incorrect signature for createUrlTree (#39347)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+59.sha-a7964f4
+ * @license Angular v11.0.0-next.6+60.sha-9fb7bde
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21229,7 +21229,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+59.sha-a7964f4');
+const VERSION = new Version('11.0.0-next.6+60.sha-9fb7bde');
 
 /**
  * @license
