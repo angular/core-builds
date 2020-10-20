b523a6fc01dd77c47b0cec2b5a17ebc654e634bf
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:43:54 +0000

    a67895cee3 docs: Add Component Overview topic to angular.io (#39186)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+54.sha-7040f17
+ * @license Angular v11.0.0-next.6+57.sha-a67895c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21260,7 +21260,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+54.sha-7040f17');
+const VERSION = new Version('11.0.0-next.6+57.sha-a67895c');
 
 /**
  * @license
