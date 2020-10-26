eb71beca5361b5822d966a98be9f93182edaf710
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:19:35 +0000

    1db4f212e5 docs: improve DefaultValueAccessor directive docs (#39404)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+104.sha-57ead7a
+ * @license Angular v11.0.0-next.6+101.sha-1db4f21
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21708,7 +21708,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+104.sha-57ead7a');
+const VERSION = new Version('11.0.0-next.6+101.sha-1db4f21');
 
 /**
  * @license
