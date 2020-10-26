382ac853c29f675d0b9564ed9613c0e5e46ff181
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:15:50 +0000

    57ead7aa53 fix(localize): serialize all the message locations to XLIFF (#39411)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+105.sha-3236ae0
+ * @license Angular v11.0.0-next.6+104.sha-57ead7a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21739,7 +21739,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+105.sha-3236ae0');
+const VERSION = new Version('11.0.0-next.6+104.sha-57ead7a');
 
 /**
  * @license
