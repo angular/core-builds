1100e7e3a87ab989a82294c53a5859ba5db845c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:17:10 +0000

    1db4f212e5 docs: improve DefaultValueAccessor directive docs (#39404)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+104.sha-57ead7a
+ * @license Angular v11.0.0-next.6+101.sha-1db4f21
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22360,7 +22360,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.6+104.sha-57ead7a');
+    var VERSION = new Version('11.0.0-next.6+101.sha-1db4f21');
 
     /**
      * @license
