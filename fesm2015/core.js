c534cdc6ce49a29aff200b224d5a97fe2da7f700
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:24:47 +0000

    fdb6b98d80 docs: add `enableI18nLegacyMessageIdFormat` option to Angular compiler options guide (#39453)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+136.sha-371fb9a
+ * @license Angular v11.0.0-next.6+142.sha-fdb6b98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21727,7 +21727,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+136.sha-371fb9a');
+const VERSION = new Version('11.0.0-next.6+142.sha-fdb6b98');
 
 /**
  * @license
