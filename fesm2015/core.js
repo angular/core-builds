9978e2a86b8b9bb7cd94b5968d611e9e7210f0ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:43:40 +0000

    1fbe217670 docs(core): add jessicajaniuk to pullapprove list (#39473)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.0+31.sha-055f7eb
+ * @license Angular v10.2.0+40.sha-1fbe217
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21066,7 +21066,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.0+31.sha-055f7eb');
+const VERSION = new Version('10.2.0+40.sha-1fbe217');
 
 /**
  * @license
