67ed84390c627ab67d5c9f19b6605455829c9537
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:20:43 +0000

    e42d3ecfcc docs: release notes for the v10.2.1 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+145.sha-6d51ae2
+ * @license Angular v11.0.0-next.6+146.sha-e42d3ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22326,7 +22326,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.6+145.sha-6d51ae2');
+    var VERSION = new Version('11.0.0-next.6+146.sha-e42d3ec');
 
     /**
      * @license
