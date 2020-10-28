7760f0f768990fef65d80de2e1d934fe2ae0b3ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:18:03 +0000

    b9358c2ad5 docs: add carlos caballero to GDE resources (#37976)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+132.sha-e6ca3d3
+ * @license Angular v11.0.0-next.6+133.sha-b9358c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21696,7 +21696,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+132.sha-e6ca3d3');
+const VERSION = new Version('11.0.0-next.6+133.sha-b9358c2');
 
 /**
  * @license
