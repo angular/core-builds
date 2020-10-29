2af6ad0b5e9fc76c62ed0a275d01c35eb5813ff3
Joey Perrott <josephperrott@gmail.com>
2020-10-29 18:44:16 +0000

    f9d35884a3 docs(forms): update ngModel documentation (#39481)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+8.sha-cff61b2
+ * @license Angular v10.2.1+9.sha-f9d3588
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21066,7 +21066,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1+8.sha-cff61b2');
+const VERSION = new Version('10.2.1+9.sha-f9d3588');
 
 /**
  * @license
