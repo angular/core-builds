978fcea716874c716fb714d8c1dbde58826ef327
Joey Perrott <josephperrott@gmail.com>
2020-10-29 21:48:32 +0000

    fb96c3d624 ci: update NgBot config to include more labels that indicate that ticket is triaged (#39494)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+9.sha-f9d3588
+ * @license Angular v10.2.1+10.sha-fb96c3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21066,7 +21066,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1+9.sha-f9d3588');
+const VERSION = new Version('10.2.1+10.sha-fb96c3d');
 
 /**
  * @license
