b95211c859d242bdf146b2163d66ea0f0881ef53
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:06:17 +0000

    3241d922fc refactor(compiler): parse bindings "by hand" rather than via regex (#39375)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+165.sha-c83b2ad
+ * @license Angular v11.0.0-next.6+166.sha-3241d92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21698,7 +21698,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+165.sha-c83b2ad');
+const VERSION = new Version('11.0.0-next.6+166.sha-3241d92');
 
 /**
  * @license
