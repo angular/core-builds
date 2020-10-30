6247011c1821fc26de706932ceeadf2774db0e63
Joey Perrott <josephperrott@gmail.com>
2020-10-30 20:04:00 +0000

    eb625b993e docs: update CLI workspace config documentation (#39427)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+18.sha-a8e0db7
+ * @license Angular v11.0.0-rc.1+19.sha-eb625b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22277,7 +22277,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-rc.1+18.sha-a8e0db7');
+    var VERSION = new Version('11.0.0-rc.1+19.sha-eb625b9');
 
     /**
      * @license
