891e58c9bab0dcba48c098bb4fcf28151cdde541
Joey Perrott <josephperrott@gmail.com>
2020-10-30 18:15:03 +0000

    cbc0907bfd fix(compiler): preserve this.$event and this.$any accesses in expressions (#39323)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+168.sha-4e68254
+ * @license Angular v11.0.0-next.6+169.sha-cbc0907
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21729,7 +21729,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+168.sha-4e68254');
+const VERSION = new Version('11.0.0-next.6+169.sha-cbc0907');
 
 /**
  * @license
