3e13b36977b17c67c47ffbcf68fc8180f3050734
Joey Perrott <josephperrott@gmail.com>
2020-10-30 23:46:25 +0000

    3af585c004 fix(router): Ensure all outlets are used when commands have a prefix (#39456)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+170.sha-f12157c
+ * @license Angular v11.0.0-next.6+172.sha-3af585c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22328,7 +22328,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.6+170.sha-f12157c');
+    var VERSION = new Version('11.0.0-next.6+172.sha-3af585c');
 
     /**
      * @license
