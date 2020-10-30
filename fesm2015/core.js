da3a3e3c396f071a810084a7b1732dee7dcca680
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:49:32 +0000

    4e68254514 fix(common): change the week-numbering year format from `r` -> `Y` (#39495)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+167.sha-beb9356
+ * @license Angular v11.0.0-next.6+168.sha-4e68254
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21698,7 +21698,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+167.sha-beb9356');
+const VERSION = new Version('11.0.0-next.6+168.sha-4e68254');
 
 /**
  * @license
