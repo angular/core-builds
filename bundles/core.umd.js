213919aecafd1deb6ee19de15872ba82c38be723
Joey Perrott <josephperrott@gmail.com>
2020-11-02 20:28:16 +0000

    ad62edac9c docs: update forwardRef example (#39519)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+186.sha-eaace44
+ * @license Angular v11.0.0-next.6+187.sha-ad62eda
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22352,7 +22352,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.6+186.sha-eaace44');
+    var VERSION = new Version('11.0.0-next.6+187.sha-ad62eda');
 
     /**
      * @license
