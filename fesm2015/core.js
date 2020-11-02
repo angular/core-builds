4f40e5c3f727f45c39bd157dcdd71dc67263bb5c
Joey Perrott <josephperrott@gmail.com>
2020-11-02 20:59:43 +0000

    a0b6548a94 build(docs-infra): ensure that deployment works on CI (#39535)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+33.sha-d386fb3
+ * @license Angular v11.0.0-rc.1+35.sha-a0b6548
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21654,7 +21654,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+33.sha-d386fb3');
+const VERSION = new Version('11.0.0-rc.1+35.sha-a0b6548');
 
 /**
  * @license
