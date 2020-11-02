92ea8603ceb39840ae43441445f0903db1963076
Joey Perrott <josephperrott@gmail.com>
2020-11-02 22:03:53 +0000

    3cab823222 docs(bazel): Mention Alex Eagle's article on migration doc (#39507)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+188.sha-5f1e975
+ * @license Angular v11.0.0-next.6+189.sha-3cab823
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21698,7 +21698,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+188.sha-5f1e975');
+const VERSION = new Version('11.0.0-next.6+189.sha-3cab823');
 
 /**
  * @license
