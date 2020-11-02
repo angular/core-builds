547f99fe5b2269e9a134f6bfe4ac8efd74be8998
Joey Perrott <josephperrott@gmail.com>
2020-11-02 20:30:25 +0000

    ad62edac9c docs: update forwardRef example (#39519)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+186.sha-eaace44
+ * @license Angular v11.0.0-next.6+187.sha-ad62eda
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21698,7 +21698,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+186.sha-eaace44');
+const VERSION = new Version('11.0.0-next.6+187.sha-ad62eda');
 
 /**
  * @license
