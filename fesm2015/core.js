7beb7d0ed0b8cb9ce0287e9069fa431e08fc1961
Joey Perrott <josephperrott@gmail.com>
2020-11-03 23:18:58 +0000

    d0dd0e80f6 feat(dev-infra): provide tooling to check what branches a pr targets (#39504)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+192.sha-7e33cb9
+ * @license Angular v11.0.0-next.6+193.sha-d0dd0e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21729,7 +21729,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+192.sha-7e33cb9');
+const VERSION = new Version('11.0.0-next.6+193.sha-d0dd0e8');
 
 /**
  * @license
