6fe4db3cdfc0a06ed7b307d0d34f61cb540dd136
Joey Perrott <josephperrott@gmail.com>
2020-11-03 23:16:22 +0000

    7e33cb9626 fix(compiler-cli): generating invalid setClassMetadata call in ES5 for class with custom decorator (#39527)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+190.sha-db1c48f
+ * @license Angular v11.0.0-next.6+192.sha-7e33cb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22328,7 +22328,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.6+190.sha-db1c48f');
+    var VERSION = new Version('11.0.0-next.6+192.sha-7e33cb9');
 
     /**
      * @license
