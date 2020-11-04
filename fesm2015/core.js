be1f784d0a7b1037937c3836349442148ba5a528
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:06:36 +0000

    3701376894 docs: release notes for the v10.2.2 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+205.sha-27ae060
+ * @license Angular v11.0.0-next.6+206.sha-3701376
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21698,7 +21698,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+205.sha-27ae060');
+const VERSION = new Version('11.0.0-next.6+206.sha-3701376');
 
 /**
  * @license
