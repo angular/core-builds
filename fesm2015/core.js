56e9f47bd0469903f98c00aee379945f3eaac60b
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:11:46 +0000

    d34e36cc95 docs: fix typo in the "Property Binding" guide (#39569) PR Close #39569

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+46.sha-96ad439
+ * @license Angular v11.0.0-rc.2+1.sha-d34e36c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21654,7 +21654,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+46.sha-96ad439');
+const VERSION = new Version('11.0.0-rc.2+1.sha-d34e36c');
 
 /**
  * @license
