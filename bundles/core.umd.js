888841b0e6974e08bda524c691e184a1664c1c60
Joey Perrott <josephperrott@gmail.com>
2020-11-04 20:30:22 +0000

    96ad439d48 test(compiler-cli): resolve test failure (#39568)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+44.sha-43b4940
+ * @license Angular v11.0.0-rc.1+46.sha-96ad439
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22277,7 +22277,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-rc.1+44.sha-43b4940');
+    var VERSION = new Version('11.0.0-rc.1+46.sha-96ad439');
 
     /**
      * @license
