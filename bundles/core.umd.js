caba24b9a6323d1c1cc8c46f6b6da24b06f593c9
Misko Hevery <misko@hevery.com>
2020-11-09 21:28:28 +0000

    0786c59835 release: cut the v10.2.3 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+10.sha-ae71b76
+ * @license Angular v10.2.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21686,7 +21686,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.2.2+10.sha-ae71b76');
+    var VERSION = new Version('10.2.3');
 
     /**
      * @license
