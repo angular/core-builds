de1903d0f5720ede10e3b66c40e59ad91562ecac
atscott <atscott01@gmail.com>
2020-11-12 22:13:53 +0000

    a87236292e docs: edit two-way binding doc (#38952)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+240.sha-b015d3e
+ * @license Angular v11.0.0-next.6+241.sha-a872362
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22451,7 +22451,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.6+240.sha-b015d3e');
+    var VERSION = new Version('11.0.0-next.6+241.sha-a872362');
 
     /**
      * @license
