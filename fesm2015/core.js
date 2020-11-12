85f6ea4f80829ae1d085b315a75c7b3741e91055
atscott <atscott01@gmail.com>
2020-11-12 17:27:10 +0000

    3be344cdc2 docs: correct appearance of numbering (#39638)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+234.sha-6d13473
+ * @license Angular v11.0.0-next.6+235.sha-3be344c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21834,7 +21834,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+234.sha-6d13473');
+const VERSION = new Version('11.0.0-next.6+235.sha-3be344c');
 
 /**
  * @license
