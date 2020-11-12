727614b32c0b3201421da21057e64f75f902420e
atscott <atscott01@gmail.com>
2020-11-12 23:43:41 +0000

    8a1c98c5e8 refactor(compiler-cli): add keySpan to reference nodes (#39616)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+249.sha-c33326c
+ * @license Angular v11.0.0-next.6+250.sha-8a1c98c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22427,7 +22427,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0-next.6+249.sha-c33326c');
+    var VERSION = new Version('11.0.0-next.6+250.sha-8a1c98c');
 
     /**
      * @license
