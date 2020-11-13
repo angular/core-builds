2725320633b37344abcccb025ded3c4a552d54b6
atscott <atscott01@gmail.com>
2020-11-13 22:04:24 +0000

    d68cac69ed ci: update angular-robot config for L2 triage of PRs (#39655)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+261.sha-24e13e0
+ * @license Angular v11.0.0-next.6+262.sha-d68cac6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21803,7 +21803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+261.sha-24e13e0');
+const VERSION = new Version('11.0.0-next.6+262.sha-d68cac6');
 
 /**
  * @license
