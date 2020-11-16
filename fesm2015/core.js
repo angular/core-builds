2e365edb05e907b0c45065e714bc8e12c192f34b
atscott <atscott01@gmail.com>
2020-11-16 17:40:48 +0000

    b0c4ecf563 docs: improve commit message format documentation (#39705)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+263.sha-5e92d64
+ * @license Angular v11.0.0-next.6+265.sha-b0c4ecf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21834,7 +21834,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+263.sha-5e92d64');
+const VERSION = new Version('11.0.0-next.6+265.sha-b0c4ecf');
 
 /**
  * @license
