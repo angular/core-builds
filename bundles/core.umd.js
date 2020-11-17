5548643567086ae06239a9a0994e7aea1eba9a35
atscott <atscott01@gmail.com>
2020-11-17 18:39:19 +0000

    882ff8fc37 perf(compiler): use raw bytes to represent utf-8 encoded strings (#39694)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+35.sha-9e87fc8
+ * @license Angular v11.0.0+37.sha-882ff8f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21637,7 +21637,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.0+35.sha-9e87fc8');
+    var VERSION = new Version('11.0.0+37.sha-882ff8f');
 
     /**
      * @license
