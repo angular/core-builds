a2996cb303329af31130fd828db299a018f5ca88
atscott <atscott01@gmail.com>
2020-11-18 19:31:26 +0000

    ea2312ea85 release: cut the v11.1.0-next.0 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+307.sha-79bd1bd
+ * @license Angular v11.1.0-next.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21134,7 +21134,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+307.sha-79bd1bd');
+const VERSION = new Version('11.1.0-next.0');
 
 /**
  * @license
