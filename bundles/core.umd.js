0152158025cda31df92bc94c5fe51c6fa7af2a93
atscott <atscott01@gmail.com>
2020-11-18 20:33:54 +0000

    26533a0954 docs: fixes on paragraph (#39664)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1
+ * @license Angular v11.0.1+3.sha-26533a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21637,7 +21637,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.1');
+    var VERSION = new Version('11.0.1+3.sha-26533a0');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1
+ * @license Angular v11.0.1+3.sha-26533a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21064,7 +21064,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.1');
+const VERSION = new Version('11.0.1+3.sha-26533a0');
 
 /**
  * @license
