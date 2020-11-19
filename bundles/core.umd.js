8d421bf39d90980ad9a6e2ea0c0508afebd3c89d
Andrew Kushnir <akushnir@google.com>
2020-11-19 21:07:08 +0000

    17d5266cdb docs: add ngx-skeleton-loader module into resources page (#39352)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+33.sha-be998e8
+ * @license Angular v11.1.0-next.0+35.sha-17d5266
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21759,7 +21759,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.0+33.sha-be998e8');
+    var VERSION = new Version('11.1.0-next.0+35.sha-17d5266');
 
     /**
      * @license
