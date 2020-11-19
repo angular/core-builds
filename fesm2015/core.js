465e6deb33af22666aa74bff2e23768e226dab80
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:37:30 +0000

    066126ae2f fix(core): remove duplicated noop function (#39761)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+18.sha-0e85938
+ * @license Angular v11.1.0-next.0+22.sha-066126a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21134,7 +21134,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+18.sha-0e85938');
+const VERSION = new Version('11.1.0-next.0+22.sha-066126a');
 
 /**
  * @license
@@ -28177,7 +28177,7 @@ class NgZone {
      */
     runTask(fn, applyThis, applyArgs, name) {
         const zone = this._inner;
-        const task = zone.scheduleEventTask('NgZoneEvent: ' + name, fn, EMPTY_PAYLOAD, noop$1, noop$1);
+        const task = zone.scheduleEventTask('NgZoneEvent: ' + name, fn, EMPTY_PAYLOAD, noop, noop);
         try {
             return zone.runTask(task, applyThis, applyArgs);
         }
@@ -28209,7 +28209,6 @@ class NgZone {
         return this._outer.run(fn);
     }
 }
-function noop$1() { }
 const EMPTY_PAYLOAD = {};
 function checkStable(zone) {
     if (zone._nesting == 0 && !zone.hasPendingMicrotasks && !zone.isStable) {
