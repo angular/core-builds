490b1adf96c9863c8d5834957783579afdda6ec0
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:48:56 +0000

    9aaa86967d build(language-service): include sources for Ivy LS (#39748)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+45.sha-156f9f3
+ * @license Angular v11.1.0-next.0+44.sha-9aaa869
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21157,7 +21157,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+45.sha-156f9f3');
+const VERSION = new Version('11.1.0-next.0+44.sha-9aaa869');
 
 /**
  * @license
