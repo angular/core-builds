9e4a85fc0a8074d7185ff8391b6f757e0074d281
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:31:52 +0000

    75b53690f6 docs: explain foreign function/value constraints in Ivy compatibility examples (#36092)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+49.sha-9cb12ca
+ * @license Angular v11.0.2+51.sha-75b5369
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21685,7 +21685,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.2+49.sha-9cb12ca');
+    var VERSION = new Version('11.0.2+51.sha-75b5369');
 
     /**
      * @license
