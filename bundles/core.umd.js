ce713667c1c48d0bd8e2a27f724d099dceecf35e
Andrew Kushnir <akushnir@google.com>
2020-11-24 19:38:56 +0000

    9cb12ca62c docs(core): update links to schema.json of @schematics/angular (#39827)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+48.sha-8634611
+ * @license Angular v11.0.2+49.sha-9cb12ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21685,7 +21685,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.2+48.sha-8634611');
+    var VERSION = new Version('11.0.2+49.sha-9cb12ca');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+48.sha-8634611
+ * @license Angular v11.0.2+49.sha-9cb12ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21119,7 +21119,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+48.sha-8634611');
+const VERSION = new Version('11.0.2+49.sha-9cb12ca');
 
 /**
  * @license
