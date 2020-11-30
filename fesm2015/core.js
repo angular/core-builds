bb9d8f0467a4664444bacf7d0190aa2b5ecd6cc9
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:57:18 +0000

    9a2f83b826 docs: edit Getting Started start/index.md copy and headers (#39583)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+111.sha-c5619ca
+ * @license Angular v11.1.0-next.0+106.sha-9a2f83b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21153,7 +21153,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+111.sha-c5619ca');
+const VERSION = new Version('11.1.0-next.0+106.sha-9a2f83b');
 
 /**
  * @license
