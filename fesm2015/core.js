9a2bbea70aa15bce32fce5d118b27c908076c538
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:29:23 +0000

    93d689ffa8 docs: edit dependency-injection.md (#39255)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+104.sha-68d4a74
+ * @license Angular v11.1.0-next.0+105.sha-93d689f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21189,7 +21189,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+104.sha-68d4a74');
+const VERSION = new Version('11.1.0-next.0+105.sha-93d689f');
 
 /**
  * @license
