8acf892d139173109e3a62bc7a92573771793b8d
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:39:51 +0000

    df97fbc077 docs: added a missing ` in inputs-outputs.md (#39856)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+105.sha-93d689f
+ * @license Angular v11.1.0-next.0+107.sha-df97fbc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21153,7 +21153,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+105.sha-93d689f');
+const VERSION = new Version('11.1.0-next.0+107.sha-df97fbc');
 
 /**
  * @license
