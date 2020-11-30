7ec4bedc3921a9dd3826cef84d95d6bb0b13b82e
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:29:26 +0000

    1539c64fb0 docs(common): change HTTPResponse to HttpResponse (#39860) PR Close #39860

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+110.sha-c622b17
+ * @license Angular v11.1.0-next.0+112.sha-1539c64
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21726,7 +21726,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.0+110.sha-c622b17');
+    var VERSION = new Version('11.1.0-next.0+112.sha-1539c64');
 
     /**
      * @license
