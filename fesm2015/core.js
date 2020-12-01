47d0008186742b4901a1e46b9b98d32dd38ca984
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 23:23:20 +0000

    7695671cd6 build(docs-infra): upgrade cli command docs sources to 548c6ed3e (#39906)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+124.sha-50962c1
+ * @license Angular v11.1.0-next.0+125.sha-7695671
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21192,7 +21192,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+124.sha-50962c1');
+const VERSION = new Version('11.1.0-next.0+125.sha-7695671');
 
 /**
  * @license
