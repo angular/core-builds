7600a7aa8268cedf76c7e77fff271e018ef51cb8
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:00:45 +0000

    00d56e4ead ci: enable the Selenium Promise Manager in AIO e2e tests to avoid flakiness on CI (#39905)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+117.sha-0b5b24a
+ * @license Angular v11.1.0-next.0+119.sha-00d56e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21758,7 +21758,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.0+117.sha-0b5b24a');
+    var VERSION = new Version('11.1.0-next.0+119.sha-00d56e4');
 
     /**
      * @license
