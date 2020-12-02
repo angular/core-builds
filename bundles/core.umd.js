e359171972682cb35b5726a9f33d4e828bc36f58
Joey Perrott <josephperrott@gmail.com>
2020-12-02 18:36:46 +0000

    7001ef6429 release: cut the v11.0.3 release (#39931)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+77.sha-3d19645
+ * @license Angular v11.0.2+78.sha-7001ef6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21693,7 +21693,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.2+77.sha-3d19645');
+    var VERSION = new Version('11.0.2+78.sha-7001ef6');
 
     /**
      * @license
