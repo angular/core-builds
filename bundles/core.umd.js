6de8fa0d3ec8a61ef1bea694b497519d9211998b
Misko Hevery <misko@hevery.com>
2020-12-02 23:24:34 +0000

    8d613c1d42 feat(compiler): allow trailing comma in array literal (#22277)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+22.sha-744f46c
+ * @license Angular v11.1.0-next.1+25.sha-8d613c1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21758,7 +21758,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.1+22.sha-744f46c');
+    var VERSION = new Version('11.1.0-next.1+25.sha-8d613c1');
 
     /**
      * @license
