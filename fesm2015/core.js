e781cf808e237c381aeb6170b9d25a87e214e7e3
Joey Perrott <josephperrott@gmail.com>
2020-12-02 19:03:19 +0000

    784667ca12 release: cut the v11.1.0-next.1 release (#39932)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+126.sha-eac85b4
+ * @license Angular v11.1.0-next.0+127.sha-784667c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21156,7 +21156,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+126.sha-eac85b4');
+const VERSION = new Version('11.1.0-next.0+127.sha-784667c');
 
 /**
  * @license
