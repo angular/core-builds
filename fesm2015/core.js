761fe030b170465c172263034142c4147ef93543
Misko Hevery <misko@hevery.com>
2020-12-03 15:45:20 +0000

    2b84882ab0 fix(language-service): do not return external template that does not exist (#39898)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+24.sha-efd0d33
+ * @license Angular v11.1.0-next.1+26.sha-2b84882
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21185,7 +21185,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+24.sha-efd0d33');
+const VERSION = new Version('11.1.0-next.1+26.sha-2b84882');
 
 /**
  * @license
