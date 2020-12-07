b665e04325f7713c6e73cf0d851922ecdf5c3262
Misko Hevery <misko@hevery.com>
2020-12-07 18:15:13 +0000

    d9356f2842 feat(dev-infra): close pull requests on merge which do not target master branch (#39979)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+50.sha-a8aeb74
+ * @license Angular v11.1.0-next.1+51.sha-d9356f2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21714,7 +21714,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.1+50.sha-a8aeb74');
+    var VERSION = new Version('11.1.0-next.1+51.sha-d9356f2');
 
     /**
      * @license
