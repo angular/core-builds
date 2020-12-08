3f18934c327920bbd210fafa1004abc6115e5f4a
Misko Hevery <misko@hevery.com>
2020-12-08 17:48:42 +0000

    8ab1231908 docs(animations): a small typo (#37164)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+42.sha-5631daa
+ * @license Angular v11.0.3+54.sha-8ab1231
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21678,7 +21678,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.3+42.sha-5631daa');
+    var VERSION = new Version('11.0.3+54.sha-8ab1231');
 
     /**
      * @license
