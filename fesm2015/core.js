4ac39dbc01e2fc9bd19c6812c2cd2b1177e6fe45
Misko Hevery <misko@hevery.com>
2020-12-08 17:19:01 +0000

    e8d2348e21 docs:  how to create a custom Attribute decorator in a child component and use it from a parent component (#38721)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+66.sha-f408af3
+ * @license Angular v11.1.0-next.1+65.sha-e8d2348
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21141,7 +21141,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+66.sha-f408af3');
+const VERSION = new Version('11.1.0-next.1+65.sha-e8d2348');
 
 /**
  * @license
