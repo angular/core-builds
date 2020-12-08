25e82b8cbb550eabe350ba0d19d7cd8f2d817f90
Misko Hevery <misko@hevery.com>
2020-12-08 19:16:00 +0000

    a694838c41 refactor(compiler-cli): TemplateTypeChecker with checkTypeOfAttributes=false should still work (#39537)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+76.sha-5a2a907
+ * @license Angular v11.1.0-next.1+73.sha-a694838
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21714,7 +21714,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.1+76.sha-5a2a907');
+    var VERSION = new Version('11.1.0-next.1+73.sha-a694838');
 
     /**
      * @license
