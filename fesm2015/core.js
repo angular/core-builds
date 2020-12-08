7f89d3110c2f1ca3d815578ab00cc405b935cd8a
Misko Hevery <misko@hevery.com>
2020-12-08 20:33:58 +0000

    74e42cf7d5 fix(service-worker): handle error with ErrorHandler (#39990)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+85.sha-6dc43a4
+ * @license Angular v11.1.0-next.1+86.sha-74e42cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21141,7 +21141,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+85.sha-6dc43a4');
+const VERSION = new Version('11.1.0-next.1+86.sha-74e42cf');
 
 /**
  * @license
