f80d7bafb1aa485e34ac3d3572f800b82e7ce297
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:10:09 +0000

    b4b21bdff4 fix(upgrade): fix HMR for hybrid applications (#40045)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+10.sha-76e3de2
+ * @license Angular v11.1.0-next.2+13.sha-b4b21bd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21141,7 +21141,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+10.sha-76e3de2');
+const VERSION = new Version('11.1.0-next.2+13.sha-b4b21bd');
 
 /**
  * @license
