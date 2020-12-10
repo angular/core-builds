a6722913863e266b287acfc84e14bd3164c6c96c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:38:07 +0000

    f5aab2b972 fix(compiler): handle strings inside bindings that contain binding characters (#39826)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+1.sha-eba7de6
+ * @license Angular v11.0.4+3.sha-f5aab2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21678,7 +21678,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.4+1.sha-eba7de6');
+    var VERSION = new Version('11.0.4+3.sha-f5aab2b');
 
     /**
      * @license
