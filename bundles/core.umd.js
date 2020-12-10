e0cc07635f8722853de7d7ffdcfc30f579f0b70f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:56:37 +0000

    b08fd0c18c docs: remove unused CSS rules (#40069)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+23.sha-91fb830
+ * @license Angular v11.1.0-next.2+24.sha-b08fd0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21714,7 +21714,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.2+23.sha-91fb830');
+    var VERSION = new Version('11.1.0-next.2+24.sha-b08fd0c');
 
     /**
      * @license
