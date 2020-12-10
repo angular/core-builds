957a4c8fd6dd12045f90b8d54156075173c890e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:28:44 +0000

    29298f4f3d docs: fix the code sample displayed in the "reusable animations" guide (#37262)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+1.sha-d39d64c
+ * @license Angular v11.1.0-next.2+2.sha-29298f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21714,7 +21714,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.2+1.sha-d39d64c');
+    var VERSION = new Version('11.1.0-next.2+2.sha-29298f4');
 
     /**
      * @license
