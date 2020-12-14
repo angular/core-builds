1a85b9d9fe0ede21732dc629fe495abc03c0b1e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:14:36 +0000

    7d9ac37ce0 docs: fix typo (#40106)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+15.sha-e90a7f7
+ * @license Angular v11.0.4+22.sha-7d9ac37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21112,7 +21112,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.4+15.sha-e90a7f7');
+const VERSION = new Version('11.0.4+22.sha-7d9ac37');
 
 /**
  * @license
