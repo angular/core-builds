8a93b00bbd90fbdca47f4f9d64b54ebacdf7d157
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:16:22 +0000

    524d581dfd docs: fix typo (#40106)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+36.sha-c18a9d5
+ * @license Angular v11.1.0-next.2+38.sha-524d581
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21714,7 +21714,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.2+36.sha-c18a9d5');
+    var VERSION = new Version('11.1.0-next.2+38.sha-524d581');
 
     /**
      * @license
