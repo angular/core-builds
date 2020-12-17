c4425ea4f6ad8d8f93ea50c9e1cd43a89c45c53b
Joey Perrott <josephperrott@gmail.com>
2020-12-17 00:49:36 +0000

    31448197e4 release: cut the v9.1.13 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+13.sha-9ae216c
+ * @license Angular v9.1.13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -28223,7 +28223,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.12+13.sha-9ae216c');
+const VERSION = new Version('9.1.13');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/core.js
+++ fesm5/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+13.sha-9ae216c
+ * @license Angular v9.1.13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20100,7 +20100,7 @@ var Version = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.12+13.sha-9ae216c');
+var VERSION = new Version('9.1.13');
 
 /**
  * @license
