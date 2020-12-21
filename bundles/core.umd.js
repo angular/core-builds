2e52e574d9ff2db921a8356ef5dba8465f1b1930
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:45:13 +0000

    df7ed4160f docs: remove -webkit-transition-timing-function CSS property (#40153)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+10.sha-eae07e1
+ * @license Angular v11.1.0-next.3+11.sha-df7ed41
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21779,7 +21779,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.3+10.sha-eae07e1');
+    var VERSION = new Version('11.1.0-next.3+11.sha-df7ed41');
 
     /**
      * @license
