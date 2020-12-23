2277045cdfe41463b579e33ccf4aeb6b0dca2ce9
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:31:28 +0000

    c22d00547a docs(router): fix typo (#40244)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+31.sha-9186f1f
+ * @license Angular v11.1.0-next.3+32.sha-c22d005
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21177,7 +21177,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+31.sha-9186f1f');
+const VERSION = new Version('11.1.0-next.3+32.sha-c22d005');
 
 /**
  * @license
