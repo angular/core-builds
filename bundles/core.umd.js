097ec441cf1246ec9164d7eaefe9dcf59e3710af
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:26:03 +0000

    c83163ecfe docs(router): fix typo (#40244)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+24.sha-9a149d4
+ * @license Angular v11.0.5+26.sha-c83163e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21714,7 +21714,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.5+24.sha-9a149d4');
+    var VERSION = new Version('11.0.5+26.sha-c83163e');
 
     /**
      * @license
