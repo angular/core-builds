46a22bf7f7caacbd3ea86d496485eec7df2ce5c8
Joey Perrott <josephperrott@gmail.com>
2021-01-05 21:12:03 +0000

    e43f7e26fe fix(router): apply redirects should match named outlets with empty path parents (#40029)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+44.sha-b473bc2
+ * @license Angular v11.1.0-next.3+50.sha-e43f7e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21753,7 +21753,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.3+44.sha-b473bc2');
+    var VERSION = new Version('11.1.0-next.3+50.sha-e43f7e2');
 
     /**
      * @license
