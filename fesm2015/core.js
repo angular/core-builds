43776bb96726fb0f5d70b1936c5936a2bf1cc8fa
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:19:56 +0000

    c1d7805acc docs(core): Fix spelling (#40296) PR Close #40296

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+50.sha-e43f7e2
+ * @license Angular v11.1.0-next.3+52.sha-c1d7805
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21180,7 +21180,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+50.sha-e43f7e2');
+const VERSION = new Version('11.1.0-next.3+52.sha-c1d7805');
 
 /**
  * @license
@@ -28775,7 +28775,7 @@ function createPlatform(injector) {
 }
 /**
  * Creates a factory for a platform. Can be used to provide or override `Providers` specific to
- * your applciation's runtime needs, such as `PLATFORM_INITIALIZER` and `PLATFORM_ID`.
+ * your application's runtime needs, such as `PLATFORM_INITIALIZER` and `PLATFORM_ID`.
  * @param parentPlatformFactory Another platform factory to modify. Allows you to compose factories
  * to build up configurations that might be required by different libraries or parts of the
  * application.
