ca1fa5594ac3e947ac7cddff49bd10f4c976d8d1
Joey Perrott <josephperrott@gmail.com>
2021-01-05 20:53:29 +0000

    b473bc226c ci: remove autolabeling from pullapprove config (#40314)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+43.sha-a384961
+ * @license Angular v11.1.0-next.3+44.sha-b473bc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.3+43.sha-a384961');
+    var VERSION = new Version('11.1.0-next.3+44.sha-b473bc2');
 
     /**
      * @license
