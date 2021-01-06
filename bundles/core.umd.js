33afa256c01caeee7540c9e691399c70335dbd4f
atscott <atscott01@gmail.com>
2021-01-06 22:16:16 +0000

    48526ccdf1 docs: fix instructions for setting up `ProductDetailsComponent` in `start-routing.md` (#40197)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+56.sha-5b9c981
+ * @license Angular v11.0.6+5.sha-48526cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21725,7 +21725,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.5+56.sha-5b9c981');
+    var VERSION = new Version('11.0.6+5.sha-48526cc');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+56.sha-5b9c981
+ * @license Angular v11.0.6+5.sha-48526cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21161,7 +21161,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+56.sha-5b9c981');
+const VERSION = new Version('11.0.6+5.sha-48526cc');
 
 /**
  * @license
