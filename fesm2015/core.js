1eeb84e2b2ae73eb04417248d6dcf5e094abe809
Joey Perrott <josephperrott@gmail.com>
2021-01-06 19:11:24 +0000

    335d6c8c00 fix(compiler): incorrectly encapsulating selectors with escape sequences (#40264)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+67.sha-8ebac24
+ * @license Angular v11.1.0-next.3+68.sha-335d6c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21212,7 +21212,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+67.sha-8ebac24');
+const VERSION = new Version('11.1.0-next.3+68.sha-335d6c8');
 
 /**
  * @license
