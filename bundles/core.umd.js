869be62045d8be5e038d6718fe790c6e58ba33f3
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:29:30 +0000

    0a317f3bb4 docs: add Aristeidis Bampakos to contributor list (#40165)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+56.sha-9e6b3cf
+ * @license Angular v11.1.0-next.3+58.sha-0a317f3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21775,7 +21775,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.3+56.sha-9e6b3cf');
+    var VERSION = new Version('11.1.0-next.3+58.sha-0a317f3');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+56.sha-9e6b3cf
+ * @license Angular v11.1.0-next.3+58.sha-0a317f3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21202,7 +21202,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+56.sha-9e6b3cf');
+const VERSION = new Version('11.1.0-next.3+58.sha-0a317f3');
 
 /**
  * @license
