8309fa229a05f4f65947f6ae87fc8397e2c5fa6a
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:59:10 +0000

    c130812afe docs: fix `fullTemplateTypeCheck` compiler option description (#40253)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+62.sha-7eadf2e
+ * @license Angular v11.1.0-next.3+61.sha-c130812
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21202,7 +21202,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+62.sha-7eadf2e');
+const VERSION = new Version('11.1.0-next.3+61.sha-c130812');
 
 /**
  * @license
