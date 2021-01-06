c0f865cfbce7fb916a550f5f34796c602bf8feb7
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:34:07 +0000

    d5a7a268cf ci: add jessicajaniuk to pullapprove groups (#40176)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+58.sha-0a317f3
+ * @license Angular v11.1.0-next.3+57.sha-d5a7a26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21202,7 +21202,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+58.sha-0a317f3');
+const VERSION = new Version('11.1.0-next.3+57.sha-d5a7a26');
 
 /**
  * @license
