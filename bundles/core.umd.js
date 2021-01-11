6e8594afb0508e22ec4ea30b0b1615c3199eec17
atscott <atscott01@gmail.com>
2021-01-11 23:59:25 +0000

    184d0e568f docs: edit getting started start-forms.md (#39681)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+75.sha-957332e
+ * @license Angular v11.1.0-next.4+77.sha-184d0e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21812,7 +21812,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.4+75.sha-957332e');
+    var VERSION = new Version('11.1.0-next.4+77.sha-184d0e5');
 
     /**
      * @license
