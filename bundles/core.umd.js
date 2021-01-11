e585e928e592a8ce5db0c0968f1adc3e441ea495
atscott <atscott01@gmail.com>
2021-01-11 19:35:17 +0000

    adf42daed6 fix(router): better ngZone checking for warning (#25839)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+6.sha-6438a29
+ * @license Angular v11.0.8+19.sha-adf42da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21725,7 +21725,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.8+6.sha-6438a29');
+    var VERSION = new Version('11.0.8+19.sha-adf42da');
 
     /**
      * @license
