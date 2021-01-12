232b2e43def1bfc5034e6c85237b963cdbf53c34
atscott <atscott01@gmail.com>
2021-01-12 00:04:29 +0000

    642c45bbf1 fix(compiler): incorrectly inferring content type of SVG-specific title tag (#40259)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+20.sha-f8495ca
+ * @license Angular v11.0.8+30.sha-642c45b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21725,7 +21725,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.8+20.sha-f8495ca');
+    var VERSION = new Version('11.0.8+30.sha-642c45b');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+20.sha-f8495ca
+ * @license Angular v11.0.8+30.sha-642c45b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21161,7 +21161,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+20.sha-f8495ca');
+const VERSION = new Version('11.0.8+30.sha-642c45b');
 
 /**
  * @license
