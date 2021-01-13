9acc5b3db0b77a470d65014447a8c9e334f5fee8
atscott <atscott01@gmail.com>
2021-01-13 23:09:01 +0000

    49dcb06dca build: remove pre-check script from publish scripts as it no longer performs its check (#40422)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+92.sha-6cff877
+ * @license Angular v11.1.0-next.4+93.sha-49dcb06
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21852,7 +21852,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.4+92.sha-6cff877');
+    var VERSION = new Version('11.1.0-next.4+93.sha-49dcb06');
 
     /**
      * @license
