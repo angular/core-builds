015c820bd7633e142e95e083de36c8b40597facf
atscott <atscott01@gmail.com>
2021-01-14 18:53:03 +0000

    72e8899d17 docs(core): add Ivy note to entryComponents page (#35479)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+11.sha-f3a96a5
+ * @license Angular v11.0.9+12.sha-72e8899
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21725,7 +21725,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.9+11.sha-f3a96a5');
+    var VERSION = new Version('11.0.9+12.sha-72e8899');
 
     /**
      * @license
