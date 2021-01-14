99a0774f0b006c5fada766bf5035901012757218
Andrew Scott <atscott@google.com>
2021-01-14 17:45:19 +0000

    f3a96a50b6 build(docs-infra): upgrade cli command docs sources to f67800842 (#40426)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+9.sha-5aa264f
+ * @license Angular v11.0.9+11.sha-f3a96a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21725,7 +21725,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.0.9+9.sha-5aa264f');
+    var VERSION = new Version('11.0.9+11.sha-f3a96a5');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+9.sha-5aa264f
+ * @license Angular v11.0.9+11.sha-f3a96a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21161,7 +21161,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.9+9.sha-5aa264f');
+const VERSION = new Version('11.0.9+11.sha-f3a96a5');
 
 /**
  * @license
