8500921c96aa90c8b2c728fa20ad0ac8be5644bd
Andrew Kushnir <akushnir@google.com>
2021-01-15 20:20:41 +0000

    524415ef8a fix(compiler): correct the `KeySpan` for animation events and properties (#40347)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+21.sha-d5f696c
+ * @license Angular v11.1.0-rc.0+28.sha-524415e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21889,7 +21889,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-rc.0+21.sha-d5f696c');
+    var VERSION = new Version('11.1.0-rc.0+28.sha-524415e');
 
     /**
      * @license
