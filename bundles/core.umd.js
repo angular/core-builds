a8ee42b29d6716ff6349478c4e1ee45f269f5d90
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 01:00:59 +0000

    a87e90071c docs: fix the docs because the interface does not exist (#40308)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+41.sha-33e0f2b
+ * @license Angular v11.1.0+43.sha-a87e900
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0+41.sha-33e0f2b');
+    var VERSION = new Version('11.1.0+43.sha-a87e900');
 
     /**
      * @license
