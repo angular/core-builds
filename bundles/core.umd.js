cb6d4f8f82a3655b89513951ba1821a28c4a2e27
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 20:02:50 +0000

    67c4ab616d build: update .bazelversion (#40579)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+47.sha-a01a9d2
+ * @license Angular v11.1.0+48.sha-67c4ab6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21932,7 +21932,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0+47.sha-a01a9d2');
+    var VERSION = new Version('11.1.0+48.sha-67c4ab6');
 
     /**
      * @license
