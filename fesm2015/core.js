b3b75523e07a295ae75f352196ac1813ff252f89
Misko Hevery <misko@hevery.com>
2021-02-01 17:37:39 +0000

    caafac2248 fix(compiler-cli): preserve user line endings in diagnostic template parse (#40597)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+15.sha-4c5f18d
+ * @license Angular v11.2.0-next.0+18.sha-caafac2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21369,7 +21369,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0-next.0+15.sha-4c5f18d');
+const VERSION = new Version('11.2.0-next.0+18.sha-caafac2');
 
 /**
  * @license
