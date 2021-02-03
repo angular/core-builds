e3eed842e3eddc65c954a59f303b860b797c4457
Misko Hevery <misko@hevery.com>
2021-02-03 23:19:19 +0000

    6ac05c05e9 release: cut the v11.2.0-next.1 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+37.sha-7c38a4a
+ * @license Angular v11.2.0-next.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21933,7 +21933,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.0-next.0+37.sha-7c38a4a');
+    var VERSION = new Version('11.2.0-next.1');
 
     /**
      * @license
