de8097d39fb029fdc569768acb02702c100f19e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:24:59 +0000

    d067dc0cb9 docs: add Angular Global Summit to event pages (#40697)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+241.sha-5324a62
+ * @license Angular v11.1.0-next.4+245.sha-d067dc0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21904,7 +21904,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.4+241.sha-5324a62');
+    var VERSION = new Version('11.1.0-next.4+245.sha-d067dc0');
 
     /**
      * @license
