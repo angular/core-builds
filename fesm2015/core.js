ecd34da49b8ce8349df46d20cdfd99c8fd513ee1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-08 17:58:34 +0000

    8fb83ea1b5 feat(forms): introduce min and max validators (#39063)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+244.sha-c9fa59a
+ * @license Angular v11.1.0-next.4+246.sha-8fb83ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21369,7 +21369,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+244.sha-c9fa59a');
+const VERSION = new Version('11.1.0-next.4+246.sha-8fb83ea');
 
 /**
  * @license
