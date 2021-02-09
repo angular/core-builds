cb09dc0f2918eb5fff3716dff2e5cc851b2acdcf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:04:14 +0000

    2a851c0924 docs: edit example to fetch hero before sending message (#40419)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+310.sha-820d95b
+ * @license Angular v11.1.0-next.4+311.sha-2a851c0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21933,7 +21933,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.4+310.sha-820d95b');
+    var VERSION = new Version('11.1.0-next.4+311.sha-2a851c0');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+310.sha-820d95b
+ * @license Angular v11.1.0-next.4+311.sha-2a851c0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21369,7 +21369,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+310.sha-820d95b');
+const VERSION = new Version('11.1.0-next.4+311.sha-2a851c0');
 
 /**
  * @license
