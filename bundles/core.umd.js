546012ed78756d5af375b50d8940b78f22166915
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 22:13:45 +0000

    46ab85996c build(docs-infra): upgrade cli command docs sources to c61df5d30 (#40758)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+305.sha-4e4fcac
+ * @license Angular v11.1.0-next.4+307.sha-46ab859
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21904,7 +21904,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.1.0-next.4+305.sha-4e4fcac');
+    var VERSION = new Version('11.1.0-next.4+307.sha-46ab859');
 
     /**
      * @license
