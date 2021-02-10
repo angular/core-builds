ef3bcc5f4376be4477dfc272d5371bcd33ab7096
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:42:14 +0000

    fface518f1 docs: add Angular + NgRx book by the asian BPB Publications (#39680)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+320.sha-bbf61fc
+ * @license Angular v11.1.0-next.4+324.sha-fface51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21389,7 +21389,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+320.sha-bbf61fc');
+const VERSION = new Version('11.1.0-next.4+324.sha-fface51');
 
 /**
  * @license
