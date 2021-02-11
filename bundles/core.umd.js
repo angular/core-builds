c699f0f7938c9d26b0b977acde23026a2e06b447
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:33:37 +0000

    dfc9f36dd8 build: update browserstacktunnel-wrapper (#40810)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+13.sha-e27787a
+ * @license Angular v12.0.0-next.0+16.sha-dfc9f36
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21953,7 +21953,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.0+13.sha-e27787a');
+    var VERSION = new Version('12.0.0-next.0+16.sha-dfc9f36');
 
     /**
      * @license
