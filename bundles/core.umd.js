af682a7fcf8cbd747890e76bdefcee19ea49dd7d
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:48:47 +0000

    e9a19a6152 fix(router): fix load interaction of navigation and preload strategies (#40389)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+8.sha-c22ae5d
+ * @license Angular v12.0.0-next.0+9.sha-e9a19a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21924,7 +21924,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.0+8.sha-c22ae5d');
+    var VERSION = new Version('12.0.0-next.0+9.sha-e9a19a6');
 
     /**
      * @license
