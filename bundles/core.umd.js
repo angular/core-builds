382af795caa0832c268c8f83261c6fe8c7164c78
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:33:44 +0000

    9cbd9bed55 Revert "ci: Remove language-service from testing group (#40787)" (#40794)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-rc.0+80.sha-e3f09ce
+ * @license Angular v11.2.0+3.sha-9cbd9be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21953,7 +21953,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.0-rc.0+80.sha-e3f09ce');
+    var VERSION = new Version('11.2.0+3.sha-9cbd9be');
 
     /**
      * @license
