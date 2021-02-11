6dbd098e8dea97bbccfdc216d418e3b355d1e139
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:46:54 +0000

    e9a19a6152 fix(router): fix load interaction of navigation and preload strategies (#40389)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+8.sha-c22ae5d
+ * @license Angular v12.0.0-next.0+9.sha-e9a19a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21389,7 +21389,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+8.sha-c22ae5d');
+const VERSION = new Version('12.0.0-next.0+9.sha-e9a19a6');
 
 /**
  * @license
