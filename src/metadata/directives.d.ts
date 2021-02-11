87d91311337ffb8e8da11c41dfcaa3c4ebc6205e
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:21:52 +0000

    7ca244a38b docs(core): add missing dot at the end of the sentence (#40800)

--- src/metadata/directives.d.ts
+++ src/metadata/directives.d.ts
@@ -508,7 +508,7 @@ export interface Component extends Directive {
      */
     encapsulation?: ViewEncapsulation;
     /**
-     * Overrides the default encapsulation start and end delimiters (`{{` and `}}`)
+     * Overrides the default interpolation start and end delimiters (`{{` and `}}`).
      */
     interpolation?: [string, string];
     /**
