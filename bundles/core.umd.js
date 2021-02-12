12a381d541093d2fcc2b4a8b32c697cb88de4d3b
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:48:33 +0000

    b4c8143cc2 docs: Update start-deployment.md (#40817)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+27.sha-9cb43fb
+ * @license Angular v12.0.0-next.0+28.sha-b4c8143
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21924,7 +21924,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.0+27.sha-9cb43fb');
+    var VERSION = new Version('12.0.0-next.0+28.sha-b4c8143');
 
     /**
      * @license
