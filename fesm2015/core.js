1a3e8cf371befa7cd3e3ad94b024101f0f67c3bc
Joey Perrott <josephperrott@gmail.com>
2021-02-16 21:22:04 +0000

    75db0c7445 docs: edit builtin-directives doc (#39816)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+66.sha-95ad452
+ * @license Angular v12.0.0-next.0+67.sha-75db0c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21396,7 +21396,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+66.sha-95ad452');
+const VERSION = new Version('12.0.0-next.0+67.sha-75db0c7');
 
 /**
  * @license
