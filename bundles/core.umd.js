9b072b33f8ebb1297e2566ca694f4ef894af8d02
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:19:02 +0000

    19114dc11e docs: remove unnecessary `<hr />` tags in Getting Started sections (#40693)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+41.sha-72db3e8
+ * @license Angular v12.0.0-next.0+42.sha-19114dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21960,7 +21960,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.0+41.sha-72db3e8');
+    var VERSION = new Version('12.0.0-next.0+42.sha-19114dc');
 
     /**
      * @license
