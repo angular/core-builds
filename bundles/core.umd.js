48ebb6d9d15319445e8aa41f5f743505a58ab3ab
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:22:10 +0000

    322951af49 refactor(compiler-cli): error on cyclic imports in partial compilation (#40782)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+75.sha-0807d13
+ * @license Angular v12.0.0-next.0+73.sha-322951a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21960,7 +21960,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.0+75.sha-0807d13');
+    var VERSION = new Version('12.0.0-next.0+73.sha-322951a');
 
     /**
      * @license
