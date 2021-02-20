0fb8d8d36d6db34cb92778cb6dac4c40355f231a
JoostK <joost.koehoorn@gmail.com>
2021-02-20 23:58:39 +0000

    e6bf7c20f4 wip: explicit reference tracking

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+38.sha-44ffa8c
+ * @license Angular v12.0.0-next.1+41.sha-e6bf7c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21962,7 +21962,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.1+38.sha-44ffa8c');
+    var VERSION = new Version('12.0.0-next.1+41.sha-e6bf7c2');
 
     /**
      * @license
