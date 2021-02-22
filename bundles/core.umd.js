a7cb976ba4904d2a2970daf0ceb6cfc1325a427a
atscott <atscott01@gmail.com>
2021-02-22 17:05:53 +0000

    0d5fbea5dc refactor(zone.js): update Object.create params to match web platform (#34287)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+33.sha-ff86f91
+ * @license Angular v11.2.1+36.sha-0d5fbea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21960,7 +21960,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.1+33.sha-ff86f91');
+    var VERSION = new Version('11.2.1+36.sha-0d5fbea');
 
     /**
      * @license
