51c1881a57b35c988c14b30e1388fdc4ca52af8b
Andrew Scott <atscott@google.com>
2021-02-22 20:55:01 +0000

    b84f719747 docs: update year (#40925)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v12.0.0-next.1+41.sha-dc9fd1a
- * (c) 2010-2020 Google LLC. https://angular.io/
+ * @license Angular v12.0.0-next.1+42.sha-b84f719
+ * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -21962,7 +21962,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.1+41.sha-dc9fd1a');
+    var VERSION = new Version('12.0.0-next.1+42.sha-b84f719');
 
     /**
      * @license
