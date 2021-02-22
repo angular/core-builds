23d544463d1629cc8faf11c6f74cf6fccb16b1d5
atscott <atscott01@gmail.com>
2021-02-22 17:00:19 +0000

    53c65f468f test(language-service): update compiler_spec to use the new testing env (#40679)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+35.sha-d1d1dad
+ * @license Angular v12.0.0-next.1+37.sha-53c65f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21933,7 +21933,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.1+35.sha-d1d1dad');
+    var VERSION = new Version('12.0.0-next.1+37.sha-53c65f4');
 
     /**
      * @license
