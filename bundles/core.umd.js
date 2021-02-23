3b148e3fb98e90ff093cca08be2f4dd586ac6727
atscott <atscott01@gmail.com>
2021-02-23 19:02:45 +0000

    596dfb88a6 test(bazel): use bazel runfiles helper for resolving file names (#40955)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+47.sha-d3705b3
+ * @license Angular v12.0.0-next.1+50.sha-596dfb8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21962,7 +21962,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.1+47.sha-d3705b3');
+    var VERSION = new Version('12.0.0-next.1+50.sha-596dfb8');
 
     /**
      * @license
