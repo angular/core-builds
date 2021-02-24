027f59266652cf61f47d73a95237aabd15688c40
Zach Arend <zachzach@google.com>
2021-02-24 22:44:41 +0000

    ca271b84fc test(localize): ensure tests pass on Windows (#40952)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+26.sha-d7133a8
+ * @license Angular v11.2.3+3.sha-ca271b8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21960,7 +21960,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.2+26.sha-d7133a8');
+    var VERSION = new Version('11.2.3+3.sha-ca271b8');
 
     /**
      * @license
