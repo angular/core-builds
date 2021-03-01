ea26110ff2e50bb9b75d852d214a1ac1b9792310
Zach Arend <zachzach@google.com>
2021-03-01 23:47:25 +0000

    a31da48507 fix(animations): cleanup DOM elements when the root view is removed (#41001)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+37.sha-8110cf0
+ * @license Angular v12.0.0-next.2+38.sha-a31da48
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21357,7 +21357,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+37.sha-8110cf0');
+const VERSION = new Version('12.0.0-next.2+38.sha-a31da48');
 
 /**
  * @license
