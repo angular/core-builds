d8fe5e81e3cd16d25bb64d45205eab963aa9ce13
Zach Arend <zachzach@google.com>
2021-03-03 23:44:19 +0000

    a10fe450d9 release: cut the v12.0.0-next.3 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+59.sha-4606a60
+ * @license Angular v12.0.0-next.3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21956,7 +21956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.2+59.sha-4606a60');
+    var VERSION = new Version('12.0.0-next.3');
 
     /**
      * @license
