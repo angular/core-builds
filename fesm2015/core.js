8a99b42b8821be1d2f341fa319f5e64e1774f200
Zach Arend <zachzach@google.com>
2021-03-03 23:12:37 +0000

    2028a77dc8 release: cut the v11.2.4 release

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+49.sha-9322e6a
+ * @license Angular v11.2.4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21395,7 +21395,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+49.sha-9322e6a');
+const VERSION = new Version('11.2.4');
 
 /**
  * @license
