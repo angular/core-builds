1c72c18497f7b0645e10c60f63cc60669bad2caa
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:16:42 +0000

    9ae0faa00a docs(router): fix typo for the equivalent `exactMatchOptions` value (#41075)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3
+ * @license Angular v12.0.0-next.3+2.sha-9ae0faa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21956,7 +21956,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.3');
+    var VERSION = new Version('12.0.0-next.3+2.sha-9ae0faa');
 
     /**
      * @license
