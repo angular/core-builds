87ac93ad6ff375fbf51346eb2e57f4870f3c8077
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:20:41 +0000

    d9acaa8547 docs: remove Codelyzer recommendation from style guide (#40992)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+3.sha-ad40fca
+ * @license Angular v12.0.0-next.3+4.sha-d9acaa8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21927,7 +21927,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.3+3.sha-ad40fca');
+    var VERSION = new Version('12.0.0-next.3+4.sha-d9acaa8');
 
     /**
      * @license
