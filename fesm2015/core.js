07f77c3b9367dcecbd8ec7bef94bf9b2d11014ae
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:52:54 +0000

    153e3a8960 test(dev-infra): fix test order failure in tests for the release command (#41023)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+36.sha-03d47d5
+ * @license Angular v12.0.0-next.3+37.sha-153e3a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21308,7 +21308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+36.sha-03d47d5');
+const VERSION = new Version('12.0.0-next.3+37.sha-153e3a8');
 
 /**
  * @license
