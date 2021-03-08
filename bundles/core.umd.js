bbfa2dec447115cd827d12f3b967d85381cb415f
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:50:18 +0000

    03d47d5701 feat(core): manually annotate de-sugarred core tree-shakable providers with @pureOrBreakMyCode (#41096)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+33.sha-1735430
+ * @license Angular v12.0.0-next.3+36.sha-03d47d5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21908,7 +21908,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.3+33.sha-1735430');
+    var VERSION = new Version('12.0.0-next.3+36.sha-03d47d5');
 
     /**
      * @license
