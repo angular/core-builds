75a6189b5d5bb1a82d9aaed248f70ffe5c557fbf
Andrew Kushnir <akushnir@google.com>
2021-03-08 23:17:21 +0000

    c5ead386b8 docs: remove augury resource from docs, partially addresses #41030 (#41120)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+30.sha-35935b4
+ * @license Angular v11.2.4+31.sha-c5ead38
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21940,7 +21940,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.4+30.sha-35935b4');
+    var VERSION = new Version('11.2.4+31.sha-c5ead38');
 
     /**
      * @license
