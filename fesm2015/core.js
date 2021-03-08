570e5168211b744569c3a79d02d6588d9f349664
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:25:01 +0000

    5c4914deff fix(bazel): fix incorrect rollup plugin method signature (#41101)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+31.sha-c1a93fc
+ * @license Angular v12.0.0-next.3+34.sha-5c4914d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21308,7 +21308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+31.sha-c1a93fc');
+const VERSION = new Version('12.0.0-next.3+34.sha-5c4914d');
 
 /**
  * @license
