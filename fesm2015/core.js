424341f49a50ca9443257c561cd16adb9ea94e81
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:14:39 +0000

    2d69f0c9ca docs: extend :host selector documentation (#41055)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+23.sha-7854c4d
+ * @license Angular v12.0.0-next.3+26.sha-2d69f0c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21351,7 +21351,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+23.sha-7854c4d');
+const VERSION = new Version('12.0.0-next.3+26.sha-2d69f0c');
 
 /**
  * @license
