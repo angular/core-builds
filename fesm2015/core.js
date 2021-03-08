6ce226fa7e8924e17d2313d71a1fff36456b143f
Andrew Kushnir <akushnir@google.com>
2021-03-08 20:41:26 +0000

    bdf13fe376 docs(compiler-cli): add README.md for template type checking system (#41004)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+37.sha-153e3a8
+ * @license Angular v12.0.0-next.3+40.sha-bdf13fe
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21308,7 +21308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+37.sha-153e3a8');
+const VERSION = new Version('12.0.0-next.3+40.sha-bdf13fe');
 
 /**
  * @license
