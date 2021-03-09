f472b69084ed4740afc77753ff5e075b2d9994b6
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:02:05 +0000

    16f90cac4b perf(compiler-cli): ensure module resolution cache is reused for type-check program (#39693)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+50.sha-e7ce857
+ * @license Angular v11.2.4+51.sha-16f90ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21922,7 +21922,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.4+50.sha-e7ce857');
+    var VERSION = new Version('11.2.4+51.sha-16f90ca');
 
     /**
      * @license
