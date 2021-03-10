0514ff5f5aed0d50a3432ce96e5afa3a4fe5bafa
Andrew Kushnir <akushnir@google.com>
2021-03-10 20:16:37 +0000

    3c66b100dd perf(common): remove unused methods from DomAdapter (#41102)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+3.sha-4c79b8a
+ * @license Angular v12.0.0-next.4+4.sha-3c66b10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21861,7 +21861,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.4+3.sha-4c79b8a');
+    var VERSION = new Version('12.0.0-next.4+4.sha-3c66b10');
 
     /**
      * @license
