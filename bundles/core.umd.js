834fe01539aad5a51e979c1a8104f066f98d014d
Andrew Kushnir <akushnir@google.com>
2021-03-10 19:25:28 +0000

    ba81e8cc94 build: remove commit message max line length limit (#41157)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+64.sha-1fb0dbb
+ * @license Angular v12.0.0-next.4+2.sha-ba81e8c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21890,7 +21890,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.3+64.sha-1fb0dbb');
+    var VERSION = new Version('12.0.0-next.4+2.sha-ba81e8c');
 
     /**
      * @license
