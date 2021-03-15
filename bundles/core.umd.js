2707f2cb23a2d72fdf13a0576673b94631e200f3
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 17:52:28 +0000

    b4484125d6 docs: document `help wanted` and `good first issue` labels (#41195)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+18.sha-5908eda
+ * @license Angular v11.2.5+19.sha-b448412
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.5+18.sha-5908eda');
+    var VERSION = new Version('11.2.5+19.sha-b448412');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+18.sha-5908eda
+ * @license Angular v11.2.5+19.sha-b448412
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21355,7 +21355,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+18.sha-5908eda');
+const VERSION = new Version('11.2.5+19.sha-b448412');
 
 /**
  * @license
