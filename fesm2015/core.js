c05bc7211c37972e060a9448aad51b27d5f1a83e
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:25:57 +0000

    6bc22a7557 build(docs-infra): upgrade cli command docs sources to 5f5448301 (#41239)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+43.sha-7d5b60e
+ * @license Angular v12.0.0-next.4+46.sha-6bc22a7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21289,7 +21289,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.4+43.sha-7d5b60e');
+const VERSION = new Version('12.0.0-next.4+46.sha-6bc22a7');
 
 /**
  * @license
