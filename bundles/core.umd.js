f9fc5103658563a85886ee6492a0d5602cb2ea3e
GitHub <noreply@github.com>
2021-03-17 18:05:42 +0000

    cbba7152fd release: cut the v11.2.6 release (#41247)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+30.sha-7803802
+ * @license Angular v11.2.5+31.sha-cbba715
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.5+30.sha-7803802');
+    var VERSION = new Version('11.2.5+31.sha-cbba715');
 
     /**
      * @license
