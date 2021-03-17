282bcde1e234d8921edf6d9135aee827848f9427
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:24:31 +0000

    78038027fc docs: update assets configuration to include `followSymlinks` option (#41238)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+28.sha-18cd7bb
+ * @license Angular v11.2.5+30.sha-7803802
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.5+28.sha-18cd7bb');
+    var VERSION = new Version('11.2.5+30.sha-7803802');
 
     /**
      * @license
