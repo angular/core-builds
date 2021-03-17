1ad9f422d8078621bca939f1fd56813f675d8b03
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:25:12 +0000

    0f0f34fb8a docs: minor update to what is Angular topic (#41123)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+46.sha-6bc22a7
+ * @license Angular v12.0.0-next.4+44.sha-0f0f34f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21889,7 +21889,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.4+46.sha-6bc22a7');
+    var VERSION = new Version('12.0.0-next.4+44.sha-0f0f34f');
 
     /**
      * @license
