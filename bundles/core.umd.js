24930d7ff79673e15ee3af1afdf01167a8b10311
Misko Hevery <misko@hevery.com>
2021-03-22 16:18:53 +0000

    17c5dc7bc5 docs: update contributors.json (#41284)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+12.sha-72c9565
+ * @license Angular v11.2.6+17.sha-17c5dc7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.6+12.sha-72c9565');
+    var VERSION = new Version('11.2.6+17.sha-17c5dc7');
 
     /**
      * @license
