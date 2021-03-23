96be01bb44532b0082254902d0195f3a11944e7a
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:41:42 +0000

    9cb11758c1 docs: add link to fork repo how-to (#41266)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+22.sha-18cd7a0
+ * @license Angular v11.2.6+24.sha-9cb1175
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.6+22.sha-18cd7a0');
+    var VERSION = new Version('11.2.6+24.sha-9cb1175');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+22.sha-18cd7a0
+ * @license Angular v11.2.6+24.sha-9cb1175
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21355,7 +21355,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+22.sha-18cd7a0');
+const VERSION = new Version('11.2.6+24.sha-9cb1175');
 
 /**
  * @license
