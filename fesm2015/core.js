fabc02ee1b8a4edbffc4de00942bf77c931e6cff
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:45:59 +0000

    274dc15452 fix(core): detect synthesized constructors that have been downleveled using TS 4.2 (#41305)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+33.sha-65f7d53
+ * @license Angular v12.0.0-next.5+35.sha-274dc15
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4372,14 +4372,21 @@ function _arrayIndexOfSorted(array, value, shift) {
  *     var _this = _super.apply(this, arguments) || this;
  * ```
  *
+ * downleveled to ES5 with `downlevelIteration` for TypeScript < 4.2:
  * ```
  *   function MyClass() {
  *     var _this = _super.apply(this, __spread(arguments)) || this;
  * ```
  *
+ * or downleveled to ES5 with `downlevelIteration` for TypeScript >= 4.2:
+ * ```
+ *   function MyClass() {
+ *     var _this = _super.apply(this, __spreadArray([], __read(arguments))) || this;
+ * ```
+ *
  * More details can be found in: https://github.com/angular/angular/issues/38453.
  */
-const ES5_DELEGATE_CTOR = /^function\s+\S+\(\)\s*{[\s\S]+\.apply\(this,\s*(arguments|[^()]+\(arguments\))\)/;
+const ES5_DELEGATE_CTOR = /^function\s+\S+\(\)\s*{[\s\S]+\.apply\(this,\s*(arguments|(?:[^()]+\(\[\],)?[^()]+\(arguments\))\)/;
 /** Regular expression that detects ES2015 classes which extend from other classes. */
 const ES2015_INHERITED_CLASS = /^class\s+[A-Za-z\d$_]*\s*extends\s+[^{]+{/;
 /**
@@ -21327,7 +21334,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+33.sha-65f7d53');
+const VERSION = new Version('12.0.0-next.5+35.sha-274dc15');
 
 /**
  * @license
