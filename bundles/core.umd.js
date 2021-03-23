708b773bba4ea31fcb7fdeb5b0622b335bc3a2aa
Joey Perrott <josephperrott@gmail.com>
2021-03-23 22:41:52 +0000

    cdbec67032 docs: spelling changes in app.e2e-spec and validate-spec files (#41331)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+26.sha-d8d77ed
+ * @license Angular v11.2.6+27.sha-cdbec67
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.6+26.sha-d8d77ed');
+    var VERSION = new Version('11.2.6+27.sha-cdbec67');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+26.sha-d8d77ed
+ * @license Angular v11.2.6+27.sha-cdbec67
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21355,7 +21355,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+26.sha-d8d77ed');
+const VERSION = new Version('11.2.6+27.sha-cdbec67');
 
 /**
  * @license
