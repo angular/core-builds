2b1ae1c31d274b69962a31b4d7b53b802a24aea5
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:43:28 +0000

    65f7d5380d docs: add link to fork repo how-to (#41266)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+31.sha-190fa07
+ * @license Angular v12.0.0-next.5+33.sha-65f7d53
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21291,7 +21291,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+31.sha-190fa07');
+const VERSION = new Version('12.0.0-next.5+33.sha-65f7d53');
 
 /**
  * @license
