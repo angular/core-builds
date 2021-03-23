ba65deaa52f80ca166f0371d9cfd5e9a8f28681c
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:10:48 +0000

    1eba57eb00 fix(language-service): show suggestion when type inference is suboptimal (#41072)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+29.sha-80f11a9
+ * @license Angular v12.0.0-next.5+30.sha-1eba57e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21327,7 +21327,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+29.sha-80f11a9');
+const VERSION = new Version('12.0.0-next.5+30.sha-1eba57e');
 
 /**
  * @license
