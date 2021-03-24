20eee99da762c44091cd8634d06e7b38bfe40da9
Joey Perrott <josephperrott@gmail.com>
2021-03-24 15:51:39 +0000

    2d183f63c6 ci: fix `elements` test on older browsers (#41324)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+27.sha-cdbec67
+ * @license Angular v11.2.6+28.sha-2d183f6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.6+27.sha-cdbec67');
+    var VERSION = new Version('11.2.6+28.sha-2d183f6');
 
     /**
      * @license
