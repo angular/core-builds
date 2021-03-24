2fb5d077f2eaf1e048c5d04c6f2e334c95519b13
Joey Perrott <josephperrott@gmail.com>
2021-03-24 16:45:00 +0000

    3177d6375e docs: update changelog for the v11.2.7 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+39.sha-81a0e71
+ * @license Angular v12.0.0-next.5+40.sha-3177d63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21906,7 +21906,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.5+39.sha-81a0e71');
+    var VERSION = new Version('12.0.0-next.5+40.sha-3177d63');
 
     /**
      * @license
