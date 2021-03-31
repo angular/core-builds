2297ed0053a94cc51d491f989fcc7ca89cae9885
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:24:38 +0000

    a371646a37 build: update yargs dependency to 16.2.0. (#41351)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+24.sha-5167030
+ * @license Angular v12.0.0-next.6+25.sha-a371646
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21886,7 +21886,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.6+24.sha-5167030');
+    var VERSION = new Version('12.0.0-next.6+25.sha-a371646');
 
     /**
      * @license
