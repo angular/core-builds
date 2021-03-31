a63ec9ecfc7efcd26291eb7082209724dc3bcd86
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:19:19 +0000

    c4628f29be refactor(common): remove `XhrFactory` re-export from `@angular/common/http` (#41370)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+21.sha-31370f4
+ * @license Angular v12.0.0-next.6+22.sha-c4628f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21315,7 +21315,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+21.sha-31370f4');
+const VERSION = new Version('12.0.0-next.6+22.sha-c4628f2');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+21.sha-31370f4
+ * @license Angular v12.0.0-next.6+22.sha-c4628f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
