95d90b9ecf11d56804ee470ab527f48fa6085b94
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 20:17:15 +0000

    bef92f97d7 docs(forms): fix the short description for `DefaultValueAccessor` (#41409)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.7+13.sha-723bfc7
+ * @license Angular v11.2.7+31.sha-bef92f9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21919,7 +21919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.7+13.sha-723bfc7');
+    var VERSION = new Version('11.2.7+31.sha-bef92f9');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.7+13.sha-723bfc7
+ * @license Angular v11.2.7+31.sha-bef92f9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21355,7 +21355,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.7+13.sha-723bfc7');
+const VERSION = new Version('11.2.7+31.sha-bef92f9');
 
 /**
  * @license
