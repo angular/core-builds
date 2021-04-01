94fb50600c2b3fe9c47846b9d9df13551fc5928c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:40:23 +0000

    ed77bfea26 test(core): fix JIT factory declaration test for IE11 (#41416)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+48.sha-c4af493
+ * @license Angular v12.0.0-next.6+50.sha-ed77bfe
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21857,7 +21857,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.6+48.sha-c4af493');
+    var VERSION = new Version('12.0.0-next.6+50.sha-ed77bfe');
 
     /**
      * @license
