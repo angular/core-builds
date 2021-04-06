827099fe6af616c94ac571790a7feb080bd70816
atscott <atscott01@gmail.com>
2021-04-06 17:30:06 +0000

    f7c294ee0f feat(core): support `forwardRef` in `providedIn` of `Injectable` declaration (#41426)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+25.sha-575f537
+ * @license Angular v12.0.0-next.7+22.sha-f7c294e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21922,7 +21922,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.7+25.sha-575f537');
+    var VERSION = new Version('12.0.0-next.7+22.sha-f7c294e');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+25.sha-575f537
+ * @license Angular v12.0.0-next.7+22.sha-f7c294e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21344,7 +21344,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+25.sha-575f537');
+const VERSION = new Version('12.0.0-next.7+22.sha-f7c294e');
 
 /**
  * @license
