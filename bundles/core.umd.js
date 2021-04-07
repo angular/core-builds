7ec6f6cda0b4a228115745e2e68f21da03f95a95
atscott <atscott01@gmail.com>
2021-04-07 19:03:20 +0000

    4b8bbe2cf5 docs: cherry-pick the v11.2.9 release changelog

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+41.sha-eda27c9
+ * @license Angular v12.0.0-next.7+40.sha-4b8bbe2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21922,7 +21922,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.7+41.sha-eda27c9');
+    var VERSION = new Version('12.0.0-next.7+40.sha-4b8bbe2');
 
     /**
      * @license
