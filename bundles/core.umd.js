7e676bb9978c4901a82f26207df362abbbc328b1
Zach Arend <zachzach@google.com>
2021-04-07 21:18:31 +0000

    d28a391385 fix(router): Remove information about attached component when deactivating route (#41381)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+12.sha-0a4308f
+ * @license Angular v12.0.0-next.8+13.sha-d28a391
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21951,7 +21951,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+12.sha-0a4308f');
+    var VERSION = new Version('12.0.0-next.8+13.sha-d28a391');
 
     /**
      * @license
