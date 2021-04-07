a33f5b32cfbe29b2e7963923049fc5bce9af1fdf
atscott <atscott01@gmail.com>
2021-04-07 19:13:08 +0000

    18d4446dc6 docs: add Zack DeRose to GDE resources (#41464)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+1.sha-5a81490
+ * @license Angular v12.0.0-next.8+2.sha-18d4446
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21922,7 +21922,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+1.sha-5a81490');
+    var VERSION = new Version('12.0.0-next.8+2.sha-18d4446');
 
     /**
      * @license
