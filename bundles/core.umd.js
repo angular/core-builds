c06578df121d83a3ff7c8f80a50e11673b8b77cf
Zach Arend <zachzach@google.com>
2021-04-09 14:53:04 +0000

    e34299af25 fix(router): handle new navigations from a NavigationEnd event (#41262) (#41511)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+13.sha-74d1769
+ * @license Angular v11.2.9+14.sha-e34299a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.9+13.sha-74d1769');
+    var VERSION = new Version('11.2.9+14.sha-e34299a');
 
     /**
      * @license
