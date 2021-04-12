b7f5abf2dd78a07119b1567b6971b680e06325c1
Zach Arend <zachzach@google.com>
2021-04-12 17:54:48 +0000

    42e3a5241d test(compiler-cli): add integration test for relative rootDir (#41359)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+32.sha-aa0e54f
+ * @license Angular v12.0.0-next.8+35.sha-42e3a52
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21947,7 +21947,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+32.sha-aa0e54f');
+    var VERSION = new Version('12.0.0-next.8+35.sha-42e3a52');
 
     /**
      * @license
