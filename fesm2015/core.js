41fa92f957d47dab1978786921a25fe2015f1c5c
Zach Arend <zachzach@google.com>
2021-04-13 21:16:57 +0000

    1d12c50f63 fix(compiler-cli): autocomplete literal types in templates. (#41456)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+71.sha-ba84fa6
+ * @license Angular v12.0.0-next.8+72.sha-1d12c50
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21376,7 +21376,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+71.sha-ba84fa6');
+const VERSION = new Version('12.0.0-next.8+72.sha-1d12c50');
 
 /**
  * @license
