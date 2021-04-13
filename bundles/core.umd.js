3cc59655041a7f8c77c858bd5389af0c87f724e0
Zach Arend <zachzach@google.com>
2021-04-13 19:11:46 +0000

    4847c146dc fix(dev-infra): only create authenticated github instance once in yargs (#41603)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+35.sha-cfd67c1
+ * @license Angular v11.2.9+36.sha-4847c14
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.9+35.sha-cfd67c1');
+    var VERSION = new Version('11.2.9+36.sha-4847c14');
 
     /**
      * @license
