295e1b43edda20aa1851594a44a98bdeb985fdda
Zach Arend <zachzach@google.com>
2021-04-14 23:18:41 +0000

    b649273f6c docs: remove `entryComponent` from deprecated list (#41408)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+104.sha-1b43158
+ * @license Angular v12.0.0-next.8+107.sha-b649273
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21388,7 +21388,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+104.sha-1b43158');
+const VERSION = new Version('12.0.0-next.8+107.sha-b649273');
 
 /**
  * @license
