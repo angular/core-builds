eafd7391df050af7657b768b25860863dcd39b8f
Zach Arend <zachzach@google.com>
2021-04-14 16:46:26 +0000

    0bc539af29 Revert "fix(compiler-cli): autocomplete literal types in templates. (#41456)" (#41623)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+77.sha-de93a7a
+ * @license Angular v12.0.0-next.8+78.sha-0bc539a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21969,7 +21969,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+77.sha-de93a7a');
+    var VERSION = new Version('12.0.0-next.8+78.sha-0bc539a');
 
     /**
      * @license
