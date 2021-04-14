8924cf04fce97965f3e3bafece04c3381a017b33
Zach Arend <zachzach@google.com>
2021-04-14 21:58:51 +0000

    74eed46477 release: cut the v10.2.5 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.4+2.sha-f630f33
+ * @license Angular v10.2.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21740,7 +21740,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('10.2.4+2.sha-f630f33');
+    var VERSION = new Version('10.2.5');
 
     /**
      * @license
