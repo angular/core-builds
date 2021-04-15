022b59330ec6be460365ea78788c6819eca732b4
Andrew Kushnir <akushnir@google.com>
2021-04-15 23:59:42 +0000

    21d3084512 docs: edits to remove jargon and abbreviations (#41530)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+8.sha-6c43a94
+ * @license Angular v11.2.10+9.sha-21d3084
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.10+8.sha-6c43a94');
+    var VERSION = new Version('11.2.10+9.sha-21d3084');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+8.sha-6c43a94
+ * @license Angular v11.2.10+9.sha-21d3084
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21419,7 +21419,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+8.sha-6c43a94');
+const VERSION = new Version('11.2.10+9.sha-21d3084');
 
 /**
  * @license
