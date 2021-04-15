a4d22080fbcaffcd79acdf82183d2c0f9d676a8c
Andrew Kushnir <akushnir@google.com>
2021-04-15 20:06:37 +0000

    1aebf165db fix(common): viewport scroller not finding elements inside the shadow DOM (#41644)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+114.sha-6bceb70
+ * @license Angular v12.0.0-next.8+116.sha-1aebf16
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21969,7 +21969,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+114.sha-6bceb70');
+    var VERSION = new Version('12.0.0-next.8+116.sha-1aebf16');
 
     /**
      * @license
