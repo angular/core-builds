ca87f60eda5ec401e29772376184820006715a76
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:18:22 +0000

    fe5bf7f53f fix(compiler-cli): autocomplete literal types in templates. (#41456) (#41645)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+123.sha-5d73b33
+ * @license Angular v12.0.0-next.8+126.sha-fe5bf7f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21969,7 +21969,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+123.sha-5d73b33');
+    var VERSION = new Version('12.0.0-next.8+126.sha-fe5bf7f');
 
     /**
      * @license
