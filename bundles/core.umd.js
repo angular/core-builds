73e0823416cee9ea52530a436583ad69a474cbec
Andrew Kushnir <akushnir@google.com>
2021-04-16 20:30:27 +0000

    d99e8f1c85 feat(dev-infra): prevent pr merges of features or breaking changes on rc (#41660)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+10.sha-296f887
+ * @license Angular v11.2.10+11.sha-d99e8f1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.10+10.sha-296f887');
+    var VERSION = new Version('11.2.10+11.sha-d99e8f1');
 
     /**
      * @license
