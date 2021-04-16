bf4e9123b7603266e48fb9a5c75449e4fee046dd
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:17:28 +0000

    2bd4363f2a build: update bazel packages (#41654)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+126.sha-fe5bf7f
+ * @license Angular v12.0.0-next.8+125.sha-2bd4363
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+126.sha-fe5bf7f');
+    var VERSION = new Version('12.0.0-next.8+125.sha-2bd4363');
 
     /**
      * @license
