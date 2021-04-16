b96b098c1c1ec5f68970785cbe7330cf12cc1115
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:55:16 +0000

    ab257b3701 fix(compiler): non-literal inline templates incorrectly processed in partial compilation (#41583)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+26.sha-8b2b5ef
+ * @license Angular v12.0.0-next.9+32.sha-ab257b3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.9+26.sha-8b2b5ef');
+    var VERSION = new Version('12.0.0-next.9+32.sha-ab257b3');
 
     /**
      * @license
