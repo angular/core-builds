1f3a9eb4c063ba91c6ae4ab0b16112cc216b1e63
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:12:48 +0000

    5d73b33ba2 build: update angular packages (#41633)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+121.sha-391d08f
+ * @license Angular v12.0.0-next.8+123.sha-5d73b33
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+121.sha-391d08f');
+    var VERSION = new Version('12.0.0-next.8+123.sha-5d73b33');
 
     /**
      * @license
