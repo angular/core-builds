9bec415087c176aea422bfbe6ed86cd89acef78c
GitHub <noreply@github.com>
2021-04-17 10:24:38 +0000

    aa35a1a082 ci: disable automatic updates of rollup

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+133.sha-d5b13ce
+ * @license Angular v12.0.0-next.8+139.sha-aa35a1a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+133.sha-d5b13ce');
+    var VERSION = new Version('12.0.0-next.8+139.sha-aa35a1a');
 
     /**
      * @license
