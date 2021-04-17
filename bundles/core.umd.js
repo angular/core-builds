0fd3440adcffdfc73ba73a2b90d5e9883b962db9
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:44:36 +0000

    88dd08c020 docs: move lazy loading section to the best practices section (#41378)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+33.sha-e251ece
+ * @license Angular v12.0.0-next.9+38.sha-88dd08c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.9+33.sha-e251ece');
+    var VERSION = new Version('12.0.0-next.9+38.sha-88dd08c');
 
     /**
      * @license
