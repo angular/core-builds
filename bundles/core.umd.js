49800914d0cec54e646609b809fa495a64e20daa
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:30:02 +0000

    343b9d0ddc build: update jasmine-spec-reporter to version 7.0.0 (#41692)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+159.sha-322db17
+ * @license Angular v12.0.0-next.8+155.sha-343b9d0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21969,7 +21969,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+159.sha-322db17');
+    var VERSION = new Version('12.0.0-next.8+155.sha-343b9d0');
 
     /**
      * @license
