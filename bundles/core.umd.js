9e854f5e484f08b894158ed711efc3f6e1496ef0
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:29:11 +0000

    b64b6b3944 build: update mocha to version 8.3.2 (#41699)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+154.sha-0c23c8b
+ * @license Angular v12.0.0-next.8+160.sha-b64b6b3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21969,7 +21969,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+154.sha-0c23c8b');
+    var VERSION = new Version('12.0.0-next.8+160.sha-b64b6b3');
 
     /**
      * @license
