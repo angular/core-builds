50f2336350f21cc1bb5f38aa0b8670104bb1fdad
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:39:48 +0000

    6cdb8fb0aa build: remove entities dependency from root package.json (#41705)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+171.sha-c617f1f
+ * @license Angular v12.0.0-next.8+172.sha-6cdb8fb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+171.sha-c617f1f');
+    var VERSION = new Version('12.0.0-next.8+172.sha-6cdb8fb');
 
     /**
      * @license
