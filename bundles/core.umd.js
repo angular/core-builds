d0c7012c7f20c7f99d52d9b3f200c49aeacf055f
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:20:17 +0000

    75168a2a70 build: update graceful-fs to version 4.2.6 (#41668)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+145.sha-3738dbb
+ * @license Angular v12.0.0-next.8+144.sha-75168a2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21969,7 +21969,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+145.sha-3738dbb');
+    var VERSION = new Version('12.0.0-next.8+144.sha-75168a2');
 
     /**
      * @license
