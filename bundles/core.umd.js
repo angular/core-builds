bf4e1fbd389d8b0b702757cfb4b1c9f8eeeb8881
Andrew Kushnir <akushnir@google.com>
2021-04-20 20:32:48 +0000

    8f3e5a5982 build(docs-infra): update docs examples to Angular v11.2.10 (#41689) (#41716)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+39.sha-20e0917
+ * @license Angular v11.2.10+41.sha-8f3e5a5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.10+39.sha-20e0917');
+    var VERSION = new Version('11.2.10+41.sha-8f3e5a5');
 
     /**
      * @license
