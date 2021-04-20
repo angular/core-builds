27d9a57a67275f1f88829415a3d49f073b3d4323
Andrew Kushnir <akushnir@google.com>
2021-04-20 23:19:22 +0000

    7bd8741109 build: update io_bazel_rules_sass to version 1.32.11 (#41713)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+87.sha-87b5026
+ * @license Angular v12.0.0-next.9+88.sha-7bd8741
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21424,7 +21424,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.9+87.sha-87b5026');
+const VERSION = new Version('12.0.0-next.9+88.sha-7bd8741');
 
 /**
  * @license
