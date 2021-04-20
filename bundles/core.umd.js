749bb4c7e38d23cc0595d4cf65ccee7b2fbf1777
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:01:43 +0000

    7459f6d705 docs: rewrites animation query () method use (#41710)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+35.sha-a76f07b
+ * @license Angular v11.2.10+38.sha-7459f6d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.10+35.sha-a76f07b');
+    var VERSION = new Version('11.2.10+38.sha-7459f6d');
 
     /**
      * @license
