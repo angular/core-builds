621a9005fa1dc4a828aa1b9241b748a8bc77915a
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:06:23 +0000

    73824d5337 fix(compiler): not generating update instructions for ng-template inside alternate namespaces (#41669)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+185.sha-364ff96
+ * @license Angular v12.0.0-next.8+186.sha-73824d5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+185.sha-364ff96');
+    var VERSION = new Version('12.0.0-next.8+186.sha-73824d5');
 
     /**
      * @license
