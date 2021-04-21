6c6492b048d4ee8b9d05ced7940c2554a46b20b2
GitHub <noreply@github.com>
2021-04-21 17:41:49 +0000

    897d5ad76e release: cut the v12.0.0-rc.0 release (#41752)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+88.sha-7bd8741
+ * @license Angular v12.0.0-next.9+89.sha-897d5ad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.9+88.sha-7bd8741');
+    var VERSION = new Version('12.0.0-next.9+89.sha-897d5ad');
 
     /**
      * @license
