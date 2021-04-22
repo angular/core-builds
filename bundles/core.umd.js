94a80ee1514154f0a06e867f8bb65d041886a755
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:52:02 +0000

    a4a55f0687 fix(dev-infra): verify python3 is available before release (#41753)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+195.sha-8bfd9e8
+ * @license Angular v12.0.0-next.8+196.sha-a4a55f0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21969,7 +21969,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+195.sha-8bfd9e8');
+    var VERSION = new Version('12.0.0-next.8+196.sha-a4a55f0');
 
     /**
      * @license
