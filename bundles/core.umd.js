38108a9557b2a65428f0bd16650092b8df56ce17
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:47:53 +0000

    8bfd9e886b build: update typed-graphqlify to version 3.1.1 (#41738)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+193.sha-6611d5c
+ * @license Angular v12.0.0-next.8+195.sha-8bfd9e8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+193.sha-6611d5c');
+    var VERSION = new Version('12.0.0-next.8+195.sha-8bfd9e8');
 
     /**
      * @license
