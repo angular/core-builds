5b23d9bf0bf7a94c72762aad30a5a9b662aaf33c
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:03:41 +0000

    e0fb18a133 build: update @babel/cli to version 7.13.16 (#41734)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+202.sha-c3a512a
+ * @license Angular v12.0.0-next.8+203.sha-e0fb18a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21388,7 +21388,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+202.sha-c3a512a');
+const VERSION = new Version('12.0.0-next.8+203.sha-e0fb18a');
 
 /**
  * @license
