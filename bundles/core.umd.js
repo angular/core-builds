82be344a2ea5b4aafdce77be512f4dc15e85938c
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:03:07 +0000

    64567d3d39 docs: Remove Protractor test from CI examples (#41733)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+203.sha-e0fb18a
+ * @license Angular v12.0.0-next.8+204.sha-64567d3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21998,7 +21998,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+203.sha-e0fb18a');
+    var VERSION = new Version('12.0.0-next.8+204.sha-64567d3');
 
     /**
      * @license
