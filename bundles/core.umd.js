2d5a52cecd45da48cb7868bfac74b6ad398054bc
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 17:06:19 +0000

    dd46b87bc7 fix(upgrade): preserve $interval.flush when ngMocks is being used (#30229)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+8.sha-42c64aa
+ * @license Angular v11.2.11+9.sha-dd46b87
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.11+8.sha-42c64aa');
+    var VERSION = new Version('11.2.11+9.sha-dd46b87');
 
     /**
      * @license
