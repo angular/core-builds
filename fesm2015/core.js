d0b66362fd91fad7675192ef746e4f6c099b8751
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 18:57:56 +0000

    163c076f97 docs: Indicate `HttpClientModule` being imported (#41749)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+9.sha-dd46b87
+ * @license Angular v11.2.11+10.sha-163c076
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21419,7 +21419,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+9.sha-dd46b87');
+const VERSION = new Version('11.2.11+10.sha-163c076');
 
 /**
  * @license
