5b2783b56ae0aaa0117f61c12bddfb7afef7e140
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:43:04 +0000

    b1ae31c712 fix(dev-infra): add github token requirement for discover-new-conflicts (#41825)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+255.sha-3783976
+ * @license Angular v12.0.0-next.8+256.sha-b1ae31c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22052,7 +22052,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+255.sha-3783976');
+    var VERSION = new Version('12.0.0-next.8+256.sha-b1ae31c');
 
     /**
      * @license
