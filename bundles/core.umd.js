f716709a595b119f76ccfdde4f806d9302f99cd2
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 21:00:06 +0000

    30db14b5db ci: several updates to renovate configuration (#41834)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+68.sha-947b451
+ * @license Angular v12.0.0-rc.0+69.sha-30db14b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22081,7 +22081,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-rc.0+68.sha-947b451');
+    var VERSION = new Version('12.0.0-rc.0+69.sha-30db14b');
 
     /**
      * @license
