a01bb00d774548748f9d2d15c0b5c6cc8f0d770c
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 19:43:39 +0000

    947b4516e8 feat(dev-infra): create the prettier formatter for ng-dev format tooling (#41824)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+67.sha-0e09009
+ * @license Angular v12.0.0-rc.0+68.sha-947b451
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22081,7 +22081,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-rc.0+67.sha-0e09009');
+    var VERSION = new Version('12.0.0-rc.0+68.sha-947b451');
 
     /**
      * @license
