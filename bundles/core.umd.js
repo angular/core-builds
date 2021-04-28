c7599f4bef035a7952fbc9412409f553db5e7fe7
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:46:23 +0000

    044b380e5a build: update remark packages (#41721)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+268.sha-1aed2f3
+ * @license Angular v12.0.0-next.8+269.sha-044b380
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22052,7 +22052,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+268.sha-1aed2f3');
+    var VERSION = new Version('12.0.0-next.8+269.sha-044b380');
 
     /**
      * @license
