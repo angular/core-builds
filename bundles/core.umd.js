b609d9bf30674397c672b1d6c3ccdcc894457cc0
Alan Agius <alan.agius4@gmail.com>
2021-04-28 09:20:47 +0000

    ab88957d68 fix(dev-infra): replace `chalk` require with namespace import

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+266.sha-c31d3c0
+ * @license Angular v12.0.0-next.8+267.sha-ab88957
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22081,7 +22081,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+266.sha-c31d3c0');
+    var VERSION = new Version('12.0.0-next.8+267.sha-ab88957');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+266.sha-c31d3c0
+ * @license Angular v12.0.0-next.8+267.sha-ab88957
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21499,7 +21499,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+266.sha-c31d3c0');
+const VERSION = new Version('12.0.0-next.8+267.sha-ab88957');
 
 /**
  * @license
