49bce908a9c4da20a04105f2f60bd452095eaa2b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 22:31:12 +0000

    ecf9a12ce4 docs: Edits to remove jargon (#41536)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12+3.sha-fca8d7a
+ * @license Angular v11.2.12+4.sha-ecf9a12
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.12+3.sha-fca8d7a');
+    var VERSION = new Version('11.2.12+4.sha-ecf9a12');
 
     /**
      * @license
