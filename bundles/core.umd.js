064f60304179a0fc0b989b4d826910e5ac8233d5
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 18:45:53 +0000

    f550742020 release: cut the v11.2.12 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+38.sha-336fe91
+ * @license Angular v11.2.12
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.11+38.sha-336fe91');
+    var VERSION = new Version('11.2.12');
 
     /**
      * @license
