212ed2431f4ee9644f80c5ba8d4be4eb182dbea5
Misko Hevery <misko@hevery.com>
2021-04-29 17:44:19 +0000

    f3331a8350 build(docs-infra): upgrade cli command docs sources to ac6fc449e (#41878)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+284.sha-d80ad95
+ * @license Angular v12.0.0-next.8+288.sha-f3331a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21499,7 +21499,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+284.sha-d80ad95');
+const VERSION = new Version('12.0.0-next.8+288.sha-f3331a8');
 
 /**
  * @license
