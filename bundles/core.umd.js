b0f735aece9ba44071c3c9f4a05a83b944ffc5d7
Misko Hevery <misko@hevery.com>
2021-04-29 21:16:51 +0000

    5bae16da48 docs(docs-infra): fix handling of client-side errors in networking (#36608)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12+7.sha-6ba5921
+ * @license Angular v11.2.12+9.sha-5bae16d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21983,7 +21983,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.12+7.sha-6ba5921');
+    var VERSION = new Version('11.2.12+9.sha-5bae16d');
 
     /**
      * @license
