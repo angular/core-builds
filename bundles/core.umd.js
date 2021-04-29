06483ef15df2bfb9bff1fa6103a0a3bd890b1fb8
Misko Hevery <misko@hevery.com>
2021-04-29 21:09:10 +0000

    e53c80c7ca ci: changes to `packages/common/http` should not require `fw-common` approval (#41887)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+289.sha-ca0fcd6
+ * @license Angular v12.0.0-next.8+290.sha-e53c80c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22052,7 +22052,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+289.sha-ca0fcd6');
+    var VERSION = new Version('12.0.0-next.8+290.sha-e53c80c');
 
     /**
      * @license
