db5a872aaf16de060ba5d2b71d052b664419a37f
Misko Hevery <misko@hevery.com>
2021-04-29 17:25:46 +0000

    595a75cda4 build: update dependency karma to v6 (#41696)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+282.sha-55d9713
+ * @license Angular v12.0.0-next.8+283.sha-595a75c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21499,7 +21499,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+282.sha-55d9713');
+const VERSION = new Version('12.0.0-next.8+283.sha-595a75c');
 
 /**
  * @license
