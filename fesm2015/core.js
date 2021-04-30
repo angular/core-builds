84f89ba01b34003a4eb80790e7dc066370fc38d1
Misko Hevery <misko@hevery.com>
2021-04-30 21:57:55 +0000

    74da3724e4 docs: Update link to Testing Angular online book (#41906)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+303.sha-b5134e2
+ * @license Angular v12.0.0-next.8+307.sha-74da372
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21439,7 +21439,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+303.sha-b5134e2');
+const VERSION = new Version('12.0.0-next.8+307.sha-74da372');
 
 /**
  * @license
