d6c32679ae008c3a8e27c7ef398dd543e994d875
Misko Hevery <misko@hevery.com>
2021-04-30 21:37:37 +0000

    72384a3357 fix(docs-infra): switch over to img[loading=lazy] for front page images (#34900)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+296.sha-6581a1b
+ * @license Angular v12.0.0-next.8+300.sha-72384a3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21982,7 +21982,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+296.sha-6581a1b');
+    var VERSION = new Version('12.0.0-next.8+300.sha-72384a3');
 
     /**
      * @license
