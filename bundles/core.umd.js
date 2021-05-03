f8d67c19d6c050a549857d5f28f4cf7c6149c899
Misko Hevery <misko@hevery.com>
2021-05-03 21:47:56 +0000

    3777cf55aa Revert "fix(language-service): only provide template results on reference requests (#41041)" (#41930)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+313.sha-3b58903
+ * @license Angular v12.0.0-next.8+316.sha-3777cf5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22008,7 +22008,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+313.sha-3b58903');
+    var VERSION = new Version('12.0.0-next.8+316.sha-3777cf5');
 
     /**
      * @license
