21f946b593c642bff29066c936608c05cae67944
Misko Hevery <misko@hevery.com>
2021-05-04 15:36:42 +0000

    b7db98fd92 build: remove emojis from github issue templates (#41902)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+314.sha-741c295
+ * @license Angular v12.0.0-next.8+318.sha-b7db98f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21979,7 +21979,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+314.sha-741c295');
+    var VERSION = new Version('12.0.0-next.8+318.sha-b7db98f');
 
     /**
      * @license
