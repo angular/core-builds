c81fe8a8058a3000fc11c829871830ebf5c517dd
Misko Hevery <misko@hevery.com>
2021-05-05 04:37:44 +0000

    19b12d7286 refactor(dev-infra): switch away from deprecated platform execution properties (#41941)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+329.sha-5947239
+ * @license Angular v12.0.0-next.8+331.sha-19b12d7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21439,7 +21439,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+329.sha-5947239');
+const VERSION = new Version('12.0.0-next.8+331.sha-19b12d7');
 
 /**
  * @license
