c1865555bd163e658f8024e0c103b6813c5f3ee7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:12:20 +0000

    4fac102966 build: update release configuration to use new release notes system (#41905)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13
+ * @license Angular v11.2.13+3.sha-4fac102
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21446,7 +21446,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.13');
+const VERSION = new Version('11.2.13+3.sha-4fac102');
 
 /**
  * @license
