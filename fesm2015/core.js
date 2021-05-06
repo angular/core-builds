81e99a52c3ea9ae541fc8a5bb048e4affcbc6783
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 13:59:17 +0000

    35fbd4f411 build: update dependency fs-extra to v10 (#41937)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+344.sha-abcd4bb
+ * @license Angular v12.0.0-next.8+345.sha-35fbd4f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21439,7 +21439,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+344.sha-abcd4bb');
+const VERSION = new Version('12.0.0-next.8+345.sha-35fbd4f');
 
 /**
  * @license
