fc47a7cd76499eb9038a9ed44d62e84a3fb55689
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 13:52:45 +0000

    abcd4bbfaa fix(compiler): preserve @page rules in encapsulated styles (#41915)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+343.sha-efe8566
+ * @license Angular v12.0.0-next.8+344.sha-abcd4bb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22008,7 +22008,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+343.sha-efe8566');
+    var VERSION = new Version('12.0.0-next.8+344.sha-abcd4bb');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+343.sha-efe8566
+ * @license Angular v12.0.0-next.8+344.sha-abcd4bb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21439,7 +21439,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+343.sha-efe8566');
+const VERSION = new Version('12.0.0-next.8+344.sha-abcd4bb');
 
 /**
  * @license
