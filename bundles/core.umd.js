bba24d8c27c19e7de28a519c906d03621bc19ed8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 20:06:32 +0000

    cd252b99fe fix(compiler-cli): use '' for the source map URL of indirect templates (#41973)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+372.sha-2843f15
+ * @license Angular v12.0.0-next.8+373.sha-cd252b9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21993,7 +21993,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+372.sha-2843f15');
+    var VERSION = new Version('12.0.0-next.8+373.sha-cd252b9');
 
     /**
      * @license
