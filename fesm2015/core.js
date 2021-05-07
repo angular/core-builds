c8f90d03457f2bce613f34bc46de661679bd9356
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 18:30:40 +0000

    c580fbb60c fix(dev-infra): merge tool should ensure that token has `workflow` oauth scope (#41989)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+22.sha-89c0298
+ * @license Angular v12.0.0-rc.2+23.sha-c580fbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21453,7 +21453,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.2+22.sha-89c0298');
+const VERSION = new Version('12.0.0-rc.2+23.sha-c580fbb');
 
 /**
  * @license
