e9b3da62860c3ece4afea8d0ca6249735ad16526
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 17:31:38 +0000

    89c02980ae build(docs-infra): ensure that terminal code snippets render correctly (#41986)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+21.sha-d6c217b
+ * @license Angular v12.0.0-rc.2+22.sha-89c0298
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22022,7 +22022,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-rc.2+21.sha-d6c217b');
+    var VERSION = new Version('12.0.0-rc.2+22.sha-89c0298');
 
     /**
      * @license
