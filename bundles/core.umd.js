3fab1e1a9cdd87821305024901114316312c0a4e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 22:18:51 +0000

    eac42e9889 docs: release notes for the v12.0.0-rc.3 release (#42032)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+394.sha-96624b7
+ * @license Angular v12.0.0-next.8+395.sha-eac42e9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21994,7 +21994,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+394.sha-96624b7');
+    var VERSION = new Version('12.0.0-next.8+395.sha-eac42e9');
 
     /**
      * @license
