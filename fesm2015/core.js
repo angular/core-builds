2645385ce41b81cf805e7ccf8d689038693349f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 23:55:49 +0000

    d34ce6b76a build(docs-infra): update docs examples to Angular v12.0.0 (#42071)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+398.sha-65ee1c3
+ * @license Angular v12.0.0-next.8+399.sha-d34ce6b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21453,7 +21453,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+398.sha-65ee1c3');
+const VERSION = new Version('12.0.0-next.8+399.sha-d34ce6b');
 
 /**
  * @license
