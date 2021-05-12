18baf061ace32d9ddaec5e6cb72002bc482d7337
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 23:30:07 +0000

    5f7a401fef release: cut the v11.2.14 release (#42070)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13+29.sha-5f7a401
+ * @license Angular v11.2.14
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21460,7 +21460,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.13+29.sha-5f7a401');
+const VERSION = new Version('11.2.14');
 
 /**
  * @license
