552b4fb078a608380f767d5ffbc246d16756bfad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 18:25:59 +0000

    04f41554dc build(docs-infra): show a dist-tag when installing the CLI on non-stable versions (#41991) (#42041)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.14
+ * @license Angular v11.2.14+1.sha-04f4155
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22023,7 +22023,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('11.2.14');
+    var VERSION = new Version('11.2.14+1.sha-04f4155');
 
     /**
      * @license
