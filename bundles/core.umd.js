691e9e66775ea794b9c91fd7f5abfe3227ae8d68
Zach Arend <zachzach@google.com>
2021-05-14 19:34:21 +0000

    3aec55b626 ci: update to latest feature request triage bot commit (#42084)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+411.sha-4bc5b4d
+ * @license Angular v12.0.0-next.8+412.sha-3aec55b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21996,7 +21996,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+411.sha-4bc5b4d');
+    var VERSION = new Version('12.0.0-next.8+412.sha-3aec55b');
 
     /**
      * @license
