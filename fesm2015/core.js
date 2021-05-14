de2b8ba26b15d7fb0bfc6c873bbfd89160ea86c9
Zach Arend <zachzach@google.com>
2021-05-14 21:32:03 +0000

    5960d4c513 docs(benchpress): reword a comment (#42096)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+415.sha-6d87895
+ * @license Angular v12.0.0-next.8+416.sha-5960d4c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21427,7 +21427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+415.sha-6d87895');
+const VERSION = new Version('12.0.0-next.8+416.sha-5960d4c');
 
 /**
  * @license
