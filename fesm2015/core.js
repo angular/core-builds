71e962bafe6fcfdc8c8ed1d79d08b3227eb08f20
Andrew Scott <atscott@google.com>
2021-05-14 00:21:46 +0000

    2e7eb270df fix(benchpress): update the check for start and end events (#42085)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+409.sha-fdb3d30
+ * @license Angular v12.0.0-next.8+410.sha-2e7eb27
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21453,7 +21453,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+409.sha-fdb3d30');
+const VERSION = new Version('12.0.0-next.8+410.sha-2e7eb27');
 
 /**
  * @license
