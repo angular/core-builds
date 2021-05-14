26a531c6c1b876fd2349bcc4c8a31b28b2394567
Andrew Scott <atscott@google.com>
2021-05-14 00:23:10 +0000

    2e7eb270df fix(benchpress): update the check for start and end events (#42085)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+409.sha-fdb3d30
+ * @license Angular v12.0.0-next.8+410.sha-2e7eb27
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21993,7 +21993,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0-next.8+409.sha-fdb3d30');
+    var VERSION = new Version('12.0.0-next.8+410.sha-2e7eb27');
 
     /**
      * @license
