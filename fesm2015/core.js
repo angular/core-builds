29af03b14e46f59ab0f21a20112a11b0b95de7b9
atscott <atscott01@gmail.com>
2021-05-17 18:18:27 +0000

    edfcbb43c5 docs: change `project` to `projected` for past tense (#42118)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+13.sha-678bce2
+ * @license Angular v12.0.0+22.sha-edfcbb4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+13.sha-678bce2');
+const VERSION = new Version('12.0.0+22.sha-edfcbb4');
 
 /**
  * @license
