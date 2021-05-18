d14c93acf7edac8a3dd9152d56ef833e11c541ca
atscott <atscott01@gmail.com>
2021-05-18 16:00:25 +0000

    fd4dbcb0c7 docs: decorate input properties with @Input (#42128) PR Close #42128

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+425.sha-2dfa010
+ * @license Angular v12.0.0-next.8+423.sha-fd4dbcb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+425.sha-2dfa010');
+const VERSION = new Version('12.0.0-next.8+423.sha-fd4dbcb');
 
 /**
  * @license
