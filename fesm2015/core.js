c71776f349558a184617838e3d9ba79560f78515
Andrew Scott <atscott@google.com>
2021-05-18 22:59:16 +0000

    aa9a86cfd5 build: remove publishConfig entry from package.json entries (#42104)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+34.sha-a20ef16
+ * @license Angular v12.0.0+37.sha-aa9a86c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+34.sha-a20ef16');
+const VERSION = new Version('12.0.0+37.sha-aa9a86c');
 
 /**
  * @license
