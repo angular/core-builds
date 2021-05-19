80622144e3d8ec4c005d321c5412fdcd7e80d051
GitHub <noreply@github.com>
2021-05-19 18:30:25 +0000

    4259945128 release: cut the v12.1.0-next.2 release (#42178)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+442.sha-8d20bb9
+ * @license Angular v12.0.0-next.8+445.sha-4259945
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+442.sha-8d20bb9');
+const VERSION = new Version('12.0.0-next.8+445.sha-4259945');
 
 /**
  * @license
