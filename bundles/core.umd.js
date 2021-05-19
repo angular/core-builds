f0b3bef255fc5b055f70f0abc57ab94e81f6c428
GitHub <noreply@github.com>
2021-05-19 18:10:53 +0000

    43525adab1 release: cut the v12.0.1 release (#42175)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+41.sha-d0214e7
+ * @license Angular v12.0.0+42.sha-43525ad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22025,7 +22025,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.0+41.sha-d0214e7');
+    var VERSION = new Version('12.0.0+42.sha-43525ad');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+41.sha-d0214e7
+ * @license Angular v12.0.0+42.sha-43525ad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+41.sha-d0214e7');
+const VERSION = new Version('12.0.0+42.sha-43525ad');
 
 /**
  * @license
