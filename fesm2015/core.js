ccb65371d679313eec88535cc3361e90ffb8c41b
atscott <atscott01@gmail.com>
2021-05-19 18:39:23 +0000

    ef676bf946 build: reformat yml files (#42136)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+445.sha-4259945
+ * @license Angular v12.1.0-next.2+2.sha-ef676bf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21427,7 +21427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+445.sha-4259945');
+const VERSION = new Version('12.1.0-next.2+2.sha-ef676bf');
 
 /**
  * @license
