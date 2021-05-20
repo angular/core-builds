3988f1cd8d1511b53e9a7762ff092eebec16f764
Zach Arend <zachzach@google.com>
2021-05-20 23:24:28 +0000

    d2a89dbbd9 docs: edits to remove jargon (#41282)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+8.sha-d6bc61a
+ * @license Angular v12.1.0-next.2+11.sha-d2a89db
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21427,7 +21427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+8.sha-d6bc61a');
+const VERSION = new Version('12.1.0-next.2+11.sha-d2a89db');
 
 /**
  * @license
