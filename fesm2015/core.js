89be7ff54a20d7b0ca672dad541471adb3c86248
Zach Arend <zachzach@google.com>
2021-05-20 16:26:46 +0000

    390d24b75f test(docs-infra): correctly test URL redirects when the destination URL is also redirected (#42018)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+42.sha-43525ad
+ * @license Angular v12.0.1+3.sha-390d24b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+42.sha-43525ad');
+const VERSION = new Version('12.0.1+3.sha-390d24b');
 
 /**
  * @license
