5795b5bccd09fbb609c0cbd79420e1608b123d3b
Zach Arend <zachzach@google.com>
2021-05-21 22:29:19 +0000

    4d56c10584 docs: move helpful alert regarding built-in directives to more logical place (#42226)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+18.sha-30c461d
+ * @license Angular v12.1.0-next.2+19.sha-4d56c10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+18.sha-30c461d');
+const VERSION = new Version('12.1.0-next.2+19.sha-4d56c10');
 
 /**
  * @license
