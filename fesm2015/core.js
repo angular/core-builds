eb28bcb3ae92f11702f6517db246fe425b5b7618
Zach Arend <zachzach@google.com>
2021-05-24 22:45:04 +0000

    ad12b31c27 docs: unsubscribe from observables (#42284)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+34.sha-47bb841
+ * @license Angular v12.1.0-next.2+33.sha-ad12b31
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21427,7 +21427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+34.sha-47bb841');
+const VERSION = new Version('12.1.0-next.2+33.sha-ad12b31');
 
 /**
  * @license
