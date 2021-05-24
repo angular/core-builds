67cc3165ac48424a08b556e10d290584db703d4a
Zach Arend <zachzach@google.com>
2021-05-24 17:28:39 +0000

    bf505ad707 docs(core): tighten up PipeTransform example types (#42240)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+24.sha-4ac55ca
+ * @license Angular v12.1.0-next.2+25.sha-bf505ad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21427,7 +21427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+24.sha-4ac55ca');
+const VERSION = new Version('12.1.0-next.2+25.sha-bf505ad');
 
 /**
  * @license
