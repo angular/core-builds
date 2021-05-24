4b3eb33fdd6fb948934d0be2c0031b53ad972d93
Zach Arend <zachzach@google.com>
2021-05-24 22:43:42 +0000

    ad12b31c27 docs: unsubscribe from observables (#42284)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+34.sha-47bb841
+ * @license Angular v12.1.0-next.2+33.sha-ad12b31
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22025,7 +22025,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.2+34.sha-47bb841');
+    var VERSION = new Version('12.1.0-next.2+33.sha-ad12b31');
 
     /**
      * @license
