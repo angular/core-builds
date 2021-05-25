02037754f46e3f932b72256f3318d7e85578ed16
Zach Arend <zachzach@google.com>
2021-05-25 18:20:47 +0000

    c3cbfc22e0 fix(dev-infra): format command incorrectly prints outdated files (#42252)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+65.sha-f74fd64
+ * @license Angular v12.1.0-next.2+63.sha-c3cbfc2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21427,7 +21427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+65.sha-f74fd64');
+const VERSION = new Version('12.1.0-next.2+63.sha-c3cbfc2');
 
 /**
  * @license
