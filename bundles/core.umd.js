9d09ec4a37f5fadbb2d51d282b6f35153fd85172
Zach Arend <zachzach@google.com>
2021-05-25 22:30:50 +0000

    063d1789c2 docs: move "Browser Support" page in navigation tree (#42326)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+72.sha-bdd3cca
+ * @license Angular v12.1.0-next.2+71.sha-063d178
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22025,7 +22025,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.2+72.sha-bdd3cca');
+    var VERSION = new Version('12.1.0-next.2+71.sha-063d178');
 
     /**
      * @license
