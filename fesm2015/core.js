4a059b96dc59948f6a274e128cd765d8bd471c14
Zach Arend <zachzach@google.com>
2021-05-25 18:22:31 +0000

    594e63315e feat(dev-infra): add release notes generation to ng-dev (#42225)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+63.sha-c3cbfc2
+ * @license Angular v12.1.0-next.2+67.sha-594e633
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+63.sha-c3cbfc2');
+const VERSION = new Version('12.1.0-next.2+67.sha-594e633');
 
 /**
  * @license
