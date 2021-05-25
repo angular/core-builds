252dea0cce343704f4f92b7b5387c0c3ca5c6acc
Zach Arend <zachzach@google.com>
2021-05-25 22:33:07 +0000

    b0581f3117 docs: add link to the style precedence guide in ivy compatibility (#42317)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+73.sha-9f50495
+ * @license Angular v12.1.0-next.2+78.sha-b0581f3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22025,7 +22025,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.2+73.sha-9f50495');
+    var VERSION = new Version('12.1.0-next.2+78.sha-b0581f3');
 
     /**
      * @license
