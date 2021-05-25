27285d35a5b1853c3ed6cff325d6e320516865f9
Zach Arend <zachzach@google.com>
2021-05-25 18:10:07 +0000

    9f09c3b9b1 build(core): update supported range of node versions to be less restrictive (#42205)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+40.sha-1c3fe69
+ * @license Angular v12.1.0-next.2+56.sha-9f09c3b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21427,7 +21427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+40.sha-1c3fe69');
+const VERSION = new Version('12.1.0-next.2+56.sha-9f09c3b');
 
 /**
  * @license
