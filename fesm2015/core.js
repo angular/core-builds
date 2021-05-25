7e6079a39d886ebeb01d10954810d74ce82882a5
Zach Arend <zachzach@google.com>
2021-05-25 18:14:42 +0000

    2ff5bfe139 docs: fix documentation of guard execution order (#42288)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+60.sha-dc6bff1
+ * @license Angular v12.1.0-next.2+61.sha-2ff5bfe
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21456,7 +21456,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+60.sha-dc6bff1');
+const VERSION = new Version('12.1.0-next.2+61.sha-2ff5bfe');
 
 /**
  * @license
