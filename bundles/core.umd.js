e565f281d9655bca4e20f11507783644ed44c072
Zach Arend <zachzach@google.com>
2021-05-25 18:16:07 +0000

    2ff5bfe139 docs: fix documentation of guard execution order (#42288)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+60.sha-dc6bff1
+ * @license Angular v12.1.0-next.2+61.sha-2ff5bfe
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21996,7 +21996,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.2+60.sha-dc6bff1');
+    var VERSION = new Version('12.1.0-next.2+61.sha-2ff5bfe');
 
     /**
      * @license
