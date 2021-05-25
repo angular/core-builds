0f79329799beb53942ce9725edc163cc0890fcb2
Zach Arend <zachzach@google.com>
2021-05-25 18:15:08 +0000

    d15a6a3a71 docs: update web-worker syntax (#42306)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+59.sha-1955dbb
+ * @license Angular v12.1.0-next.2+58.sha-d15a6a3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21427,7 +21427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+59.sha-1955dbb');
+const VERSION = new Version('12.1.0-next.2+58.sha-d15a6a3');
 
 /**
  * @license
