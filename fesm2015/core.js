841d57360fd339a75652d4e8d9cbbadcf01bf2c1
Zach Arend <zachzach@google.com>
2021-05-26 20:32:25 +0000

    bd51762d6e docs: add section on binding to passive events (#42292)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+9.sha-de8a6ae
+ * @license Angular v12.1.0-next.3+10.sha-bd51762
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21433,7 +21433,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+9.sha-de8a6ae');
+const VERSION = new Version('12.1.0-next.3+10.sha-bd51762');
 
 /**
  * @license
