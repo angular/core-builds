316cfb7b969025e11b44b4da2cacc77dd9342bb9
Zach Arend <zachzach@google.com>
2021-05-26 20:30:57 +0000

    33e1a18530 build(docs-infra): allow only Node.js v14 and above (#42093)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+9.sha-de8a6ae
+ * @license Angular v12.1.0-next.3+12.sha-33e1a18
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -204,11 +204,6 @@ export declare const getTestBed: () => TestBed;
  *   expect(...);
  * })
  * ```
- *
- * Notes:
- * - inject is currently a function because of some Traceur limitation the syntax should
- * eventually
- *   becomes `it('...', @Inject (object: AClass, async: AsyncTestCompleter) => { ... });`
  *
  * @publicApi
  */
