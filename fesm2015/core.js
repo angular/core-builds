19fc134fbc5640d85d7263e2a5d85e27c98b0ec5
Zach Arend <zachzach@google.com>
2021-05-26 20:23:55 +0000

    74ebdf6fdc docs: refactor `HeroDetailComponent` and unit test (#42349)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3
+ * @license Angular v12.1.0-next.3+1.sha-74ebdf6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21433,7 +21433,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3');
+const VERSION = new Version('12.1.0-next.3+1.sha-74ebdf6');
 
 /**
  * @license
