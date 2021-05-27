c9a355fa58e41b74ff326b5c84fd315b0ae3d5a9
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:11:53 +0000

    d74d1306e2 docs: improve preloading component data chapter (#42340)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+22.sha-141791e
+ * @license Angular v12.1.0-next.3+24.sha-d74d130
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22031,7 +22031,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.3+22.sha-141791e');
+    var VERSION = new Version('12.1.0-next.3+24.sha-d74d130');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+22.sha-141791e
+ * @license Angular v12.1.0-next.3+24.sha-d74d130
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21462,7 +21462,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+22.sha-141791e');
+const VERSION = new Version('12.1.0-next.3+24.sha-d74d130');
 
 /**
  * @license
