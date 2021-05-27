28de80299540e414758b96f259938bbca292834e
Andrew Kushnir <akushnir@google.com>
2021-05-27 22:54:08 +0000

    3a959152f9 docs: Edited to remove jargon, fixed issue (#42026)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+41.sha-d33fdea
+ * @license Angular v12.1.0-next.3+44.sha-3a95915
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21462,7 +21462,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+41.sha-d33fdea');
+const VERSION = new Version('12.1.0-next.3+44.sha-3a95915');
 
 /**
  * @license
