1c611f10f7332a2e33fe01c5169b218f9fc9e958
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:36:24 +0000

    4c39869566 docs: add a note about javascript mime-types to the deployment guide (#42339)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+4.sha-f3f1014
+ * @license Angular v12.1.0-next.4+5.sha-4c39869
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21433,7 +21433,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+4.sha-f3f1014');
+const VERSION = new Version('12.1.0-next.4+5.sha-4c39869');
 
 /**
  * @license
