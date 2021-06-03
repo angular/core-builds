4a57f6828014abc5a8b75af90839e79cbcf0fa03
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 21:03:33 +0000

    a6dec05e82 refactor(dev-infra): always use en-US locale for printing date in merge tool (#42470)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+22.sha-50c87e8
+ * @license Angular v12.0.3+26.sha-a6dec05
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22031,7 +22031,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.3+22.sha-50c87e8');
+    var VERSION = new Version('12.0.3+26.sha-a6dec05');
 
     /**
      * @license
