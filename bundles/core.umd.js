722792bfe964112bd5633cf022254fffc4e5b9cb
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:54:24 +0000

    50c87e86b6 fix(forms): the `min` and `max` validators should work correctly with `0` as a value (#42412)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+17.sha-0dad375
+ * @license Angular v12.0.3+22.sha-50c87e8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22031,7 +22031,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.3+17.sha-0dad375');
+    var VERSION = new Version('12.0.3+22.sha-50c87e8');
 
     /**
      * @license
--- core.d.ts
+++ core.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+17.sha-0dad375
+ * @license Angular v12.0.3+22.sha-50c87e8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8008,7 +8008,7 @@ export declare interface TrackByFunction<T> {
      * @param index The index of the item within the iterable.
      * @param item The item in the iterable.
      */
-    (index: number, item: T): any;
+    <U extends T>(index: number, item: U): any;
 }
 
 /**
