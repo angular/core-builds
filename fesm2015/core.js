b8040f3a199f4e8072d242ea3fceeddf86f05aac
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:56:49 +0000

    751f68dcf8 docs: updated collaborators and add angular dev's link (#42516)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+70.sha-66f49c2
+ * @license Angular v12.1.0-next.4+71.sha-751f68d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21466,7 +21466,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+70.sha-66f49c2');
+const VERSION = new Version('12.1.0-next.4+71.sha-751f68d');
 
 /**
  * @license
