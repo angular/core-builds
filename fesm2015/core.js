946cf962afcf288d93404d4b20dd5db045a0ec47
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:02:12 +0000

    5c0541f351 docs: fix issue 24571 in ngOnChanges (#42517)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+82.sha-386550c
+ * @license Angular v12.1.0-next.4+84.sha-5c0541f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21469,7 +21469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+82.sha-386550c');
+const VERSION = new Version('12.1.0-next.4+84.sha-5c0541f');
 
 /**
  * @license
