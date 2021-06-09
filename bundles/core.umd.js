f5d28a65abe7a30a3af85804f13a680aee2001ea
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 17:04:10 +0000

    6203f5e3a7 docs: clarify onSameUrlNavigation behavior (#42275)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+64.sha-3abf365
+ * @license Angular v12.0.3+65.sha-6203f5e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22038,7 +22038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.3+64.sha-3abf365');
+    var VERSION = new Version('12.0.3+65.sha-6203f5e');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+64.sha-3abf365
+ * @license Angular v12.0.3+65.sha-6203f5e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21469,7 +21469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+64.sha-3abf365');
+const VERSION = new Version('12.0.3+65.sha-6203f5e');
 
 /**
  * @license
