2b36b30bed8f4cbff166fca07e2dee5d9398b3ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:35:33 +0000

    665b986896 docs: CLI users polyfills (#42263)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+3.sha-22bda22
+ * @license Angular v12.1.0-next.5+4.sha-665b986
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22038,7 +22038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.5+3.sha-22bda22');
+    var VERSION = new Version('12.1.0-next.5+4.sha-665b986');
 
     /**
      * @license
