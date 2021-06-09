834e0c73bdfa6ae71bae4a93fbd1f52f71a5ef5d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:36:58 +0000

    665b986896 docs: CLI users polyfills (#42263)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+3.sha-22bda22
+ * @license Angular v12.1.0-next.5+4.sha-665b986
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21440,7 +21440,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+3.sha-22bda22');
+const VERSION = new Version('12.1.0-next.5+4.sha-665b986');
 
 /**
  * @license
