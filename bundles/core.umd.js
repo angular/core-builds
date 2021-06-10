ef36feec26cd82dba314d945617e48332916b0c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:17:19 +0000

    aef489f139 docs: add note for naming libraries (#42335)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+6.sha-e299683
+ * @license Angular v12.1.0-next.5+7.sha-aef489f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22038,7 +22038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.5+6.sha-e299683');
+    var VERSION = new Version('12.1.0-next.5+7.sha-aef489f');
 
     /**
      * @license
