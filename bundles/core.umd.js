5e6d0e14d9af924fdfff051fc0061a464350095f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:23:48 +0000

    ec90d8d5e5 docs: change field name from id to $id in schema.json file (#42528)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+9.sha-cddb3a8
+ * @license Angular v12.1.0-next.5+8.sha-ec90d8d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22009,7 +22009,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.5+9.sha-cddb3a8');
+    var VERSION = new Version('12.1.0-next.5+8.sha-ec90d8d');
 
     /**
      * @license
