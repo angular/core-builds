9a3aa42d51721dcae5230f3bbfd1b0fc7bc4c314
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-11 19:06:39 +0000

    5161084917 docs: remove section on tables from docs style guide (#42330)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+13.sha-0c13e2b
+ * @license Angular v12.1.0-next.5+14.sha-5161084
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22009,7 +22009,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.5+13.sha-0c13e2b');
+    var VERSION = new Version('12.1.0-next.5+14.sha-5161084');
 
     /**
      * @license
