6ac44e39b917d7f4154c7d6394b0ab0ea46bd9ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:18:45 +0000

    56a0582d79 fix(docs-infra): correctly serve `index.html` with a query string (#42547)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+21.sha-e7c69b8
+ * @license Angular v12.1.0-next.5+19.sha-56a0582
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22038,7 +22038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.5+21.sha-e7c69b8');
+    var VERSION = new Version('12.1.0-next.5+19.sha-56a0582');
 
     /**
      * @license
