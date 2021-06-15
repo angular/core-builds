d0d46d1352d01ed954ee1fd9e6dfa39519ebb6cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 20:13:11 +0000

    b52e93543f docs: add Daniel Trevino to contributors file (#42573)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+32.sha-a66dd88
+ * @license Angular v12.1.0-next.5+33.sha-b52e935
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21975,7 +21975,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.5+32.sha-a66dd88');
+    var VERSION = new Version('12.1.0-next.5+33.sha-b52e935');
 
     /**
      * @license
