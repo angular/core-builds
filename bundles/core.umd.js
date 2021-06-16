89f3dbc4584e1bc7a3fb0ce338b8193761d20d0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:03:15 +0000

    7db8e1f82e release: cut the v12.0.5 release (#42585)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+32.sha-f9a5d25
+ * @license Angular v12.0.4+33.sha-7db8e1f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22038,7 +22038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.4+32.sha-f9a5d25');
+    var VERSION = new Version('12.0.4+33.sha-7db8e1f');
 
     /**
      * @license
