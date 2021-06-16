43275d0bf899100fe27ce893d5005219b03b2954
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:21:36 +0000

    f9a5d25d72 fix(docs-infra): make anchor in list item inherit line-height (#42572)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+25.sha-6bf4cb6
+ * @license Angular v12.0.4+32.sha-f9a5d25
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22038,7 +22038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.4+25.sha-6bf4cb6');
+    var VERSION = new Version('12.0.4+32.sha-f9a5d25');
 
     /**
      * @license
