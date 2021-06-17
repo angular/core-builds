e3cce7fba29d4bb68b21b60be5a96beae0f8609a
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:31:27 +0000

    07c1ddc487 fix(router): error if module is destroyed before location is initialized (#42560)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+3.sha-166e98a
+ * @license Angular v12.1.0-next.6+4.sha-07c1ddc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22009,7 +22009,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.0-next.6+3.sha-166e98a');
+    var VERSION = new Version('12.1.0-next.6+4.sha-07c1ddc');
 
     /**
      * @license
