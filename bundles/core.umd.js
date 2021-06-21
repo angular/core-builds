f384c4632697ac704ec1ab563b50a69c1cb722da
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 17:12:12 +0000

    c5d2681893 refactor(compiler-cli): support xi18n in ngtsc (#42485)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.5+20.sha-4112323
+ * @license Angular v12.0.5+22.sha-c5d2681
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22038,7 +22038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.5+20.sha-4112323');
+    var VERSION = new Version('12.0.5+22.sha-c5d2681');
 
     /**
      * @license
