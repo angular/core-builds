030fc4ae39567281212b7121a4825a8481f04d3c
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 19:06:29 +0000

    1ce5c20656 docs: add new i18n formats to i18n guide (#42597)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.5+23.sha-1cb85ba
+ * @license Angular v12.0.5+28.sha-1ce5c20
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22038,7 +22038,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.0.5+23.sha-1cb85ba');
+    var VERSION = new Version('12.0.5+28.sha-1ce5c20');
 
     /**
      * @license
