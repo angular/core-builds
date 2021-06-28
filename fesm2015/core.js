4283e28c0e95fb68d5ae088fd4d271c27c692e42
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:54:00 +0000

    2a260dc249 docs: update Roadmap section header from "Done" to "Completed" (#42620)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+4.sha-56fb4eb
+ * @license Angular v12.2.0-next.0+9.sha-2a260dc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21440,7 +21440,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+4.sha-56fb4eb');
+const VERSION = new Version('12.2.0-next.0+9.sha-2a260dc');
 
 /**
  * @license
