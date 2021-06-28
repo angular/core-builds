d423d72509ab63eae7161dfe9d590cdbe88d2834
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 19:28:03 +0000

    41823ff277 feat(dev-infra): add command for printing release train information (#42644)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+12.sha-c93df7d
+ * @license Angular v12.2.0-next.0+14.sha-41823ff
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21440,7 +21440,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+12.sha-c93df7d');
+const VERSION = new Version('12.2.0-next.0+14.sha-41823ff');
 
 /**
  * @license
