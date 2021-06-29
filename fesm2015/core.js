366e6e9a51328284929c2ad43be5aa6c07baf931
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 16:46:10 +0000

    2c490d5c6c docs: remove duplicate link to `guide/inputs-outputs.md` (#42654)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+14.sha-41823ff
+ * @license Angular v12.2.0-next.0+15.sha-2c490d5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21469,7 +21469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+14.sha-41823ff');
+const VERSION = new Version('12.2.0-next.0+15.sha-2c490d5');
 
 /**
  * @license
