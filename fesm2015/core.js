99cd09889215395215100c137edb1a9abca79421
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 17:53:08 +0000

    02742552f3 refactor(docs-infra): make angular.io and docs examples compatible with RxJS v7 (#42660)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+15.sha-2c490d5
+ * @license Angular v12.2.0-next.0+17.sha-0274255
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21440,7 +21440,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+15.sha-2c490d5');
+const VERSION = new Version('12.2.0-next.0+17.sha-0274255');
 
 /**
  * @license
