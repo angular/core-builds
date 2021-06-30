a7aacc38f6c0112d276686c6b56f3d79132af40f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 16:33:59 +0000

    f3965ff834 docs: setting context value inside an interceptor (#42699)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+20.sha-8528073
+ * @license Angular v12.2.0-next.0+21.sha-f3965ff
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21469,7 +21469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+20.sha-8528073');
+const VERSION = new Version('12.2.0-next.0+21.sha-f3965ff');
 
 /**
  * @license
