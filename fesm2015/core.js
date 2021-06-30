16f5d9d4f8e5e19381a954c58a1369a5ce7ba49a
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:54:14 +0000

    234b5edcc7 fix(platform-browser): in `Meta.addTag()` do not add duplicate meta tags (#42703)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+21.sha-f3965ff
+ * @license Angular v12.2.0-next.0+22.sha-234b5ed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21469,7 +21469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+21.sha-f3965ff');
+const VERSION = new Version('12.2.0-next.0+22.sha-234b5ed');
 
 /**
  * @license
