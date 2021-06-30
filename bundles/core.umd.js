537d8751ea15ce548fb461dc46ce3f3c74fab55c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:58:33 +0000

    9f5cc7c808 feat(compiler): support number separators in templates (#42672)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+22.sha-234b5ed
+ * @license Angular v12.2.0-next.0+23.sha-9f5cc7c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22044,7 +22044,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0-next.0+22.sha-234b5ed');
+    var VERSION = new Version('12.2.0-next.0+23.sha-9f5cc7c');
 
     /**
      * @license
