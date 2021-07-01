bf0668d4864bac1e32f411a221a15e72978bfc41
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:57:20 +0000

    df5cc1fbbf fix(compiler-cli): return directives for an element on a microsyntax template (#42640)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+1.sha-41c6877
+ * @license Angular v12.1.1+4.sha-df5cc1f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22044,7 +22044,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.1+1.sha-41c6877');
+    var VERSION = new Version('12.1.1+4.sha-df5cc1f');
 
     /**
      * @license
