6f1c7aa1217ab660d52491d15b807ca19a6889c5
atscott <atscott01@gmail.com>
2021-07-02 19:51:07 +0000

    6da24e6451 build: update API goldens after api-extractor update (#42749)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+7.sha-aa261e7
+ * @license Angular v12.1.1+12.sha-6da24e6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22044,7 +22044,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.1+7.sha-aa261e7');
+    var VERSION = new Version('12.1.1+12.sha-6da24e6');
 
     /**
      * @license
