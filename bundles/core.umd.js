83624845cef620455a063d452f1c03042f2ccd06
atscott <atscott01@gmail.com>
2021-07-03 00:35:27 +0000

    9557e87ad8 fix(dev-infra): missing `pkg_npm` substitution for top-level targets being accessed (#42754)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+12.sha-6da24e6
+ * @license Angular v12.1.1+16.sha-9557e87
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22044,7 +22044,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.1+12.sha-6da24e6');
+    var VERSION = new Version('12.1.1+16.sha-9557e87');
 
     /**
      * @license
