1aa76018e85e5afddb5d7982efa732fedf86ca05
Andrew Scott <atscott@google.com>
2021-07-07 17:30:59 +0000

    0c0c32d539 docs: change misspelled function (#42742)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+16.sha-c9b47f2
+ * @license Angular v12.2.0-next.1+17.sha-0c0c32d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22044,7 +22044,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0-next.1+16.sha-c9b47f2');
+    var VERSION = new Version('12.2.0-next.1+17.sha-0c0c32d');
 
     /**
      * @license
