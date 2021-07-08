0d1b8ded7d7827c996276f7245d4c8a2208f20e5
atscott <atscott01@gmail.com>
2021-07-08 21:26:39 +0000

    e26bfc1131 refactor(core): remove no longer needed types workaround (#42798)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+28.sha-f592a12
+ * @license Angular v12.2.0-next.1+40.sha-e26bfc1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21440,7 +21440,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+28.sha-f592a12');
+const VERSION = new Version('12.2.0-next.1+40.sha-e26bfc1');
 
 /**
  * @license
