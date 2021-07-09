3ff0bcd28a9321661553e5a47ca0fa5197656c42
Andrew Kushnir <akushnir@google.com>
2021-07-09 17:43:42 +0000

    1c31828620 refactor(router): Adjust behavior for computed navigation restoration (#42751)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+43.sha-a8d5a31
+ * @license Angular v12.1.1+46.sha-1c31828
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22044,7 +22044,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.1+43.sha-a8d5a31');
+    var VERSION = new Version('12.1.1+46.sha-1c31828');
 
     /**
      * @license
