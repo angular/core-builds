b8fbeb605c35214df3ef4c8a753aad51883f7250
Andrew Kushnir <akushnir@google.com>
2021-07-12 20:34:07 +0000

    b5ab7aff43 refactor: add override keyword to members implementing abstract declarations (#42512)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+67.sha-e8be045
+ * @license Angular v12.2.0-next.1+89.sha-b5ab7af
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21465,7 +21465,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+67.sha-e8be045');
+const VERSION = new Version('12.2.0-next.1+89.sha-b5ab7af');
 
 /**
  * @license
