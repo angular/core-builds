863984a2dc31b6c6114077acebf8c1a4ad3d64c5
Andrew Kushnir <akushnir@google.com>
2021-07-12 16:57:58 +0000

    ef15b979cf docs(core): edit and add docs for ng-template (#42704)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+58.sha-762e057
+ * @license Angular v12.2.0-next.1+59.sha-ef15b97
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22044,7 +22044,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0-next.1+58.sha-762e057');
+    var VERSION = new Version('12.2.0-next.1+59.sha-ef15b97');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+58.sha-762e057
+ * @license Angular v12.2.0-next.1+59.sha-ef15b97
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21469,7 +21469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+58.sha-762e057');
+const VERSION = new Version('12.2.0-next.1+59.sha-ef15b97');
 
 /**
  * @license
