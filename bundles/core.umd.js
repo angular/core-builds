fcd754c8b8a94a12383123443cc396f91e95bb93
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:33:30 +0000

    e6f01d777d revert: docs: remove references to Trusted Types due to incompatibility with Webpack 5 (#42796)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+97.sha-2a61728
+ * @license Angular v12.1.2+3.sha-e6f01d7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22040,7 +22040,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.1+97.sha-2a61728');
+    var VERSION = new Version('12.1.2+3.sha-e6f01d7');
 
     /**
      * @license
