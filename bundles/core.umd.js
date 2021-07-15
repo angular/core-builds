2e4eb1ef7108bc8555253e0fbae5eea12f21d622
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:44:05 +0000

    f5baa55b81 docs: remove reference to an obsolete design doc (#42842)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+11.sha-4e97514
+ * @license Angular v12.2.0-next.2+12.sha-f5baa55
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22064,7 +22064,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0-next.2+11.sha-4e97514');
+    var VERSION = new Version('12.2.0-next.2+12.sha-f5baa55');
 
     /**
      * @license
