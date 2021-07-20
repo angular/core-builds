130c285fe5fd98070d3078a60695edfad671c1e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 19:18:55 +0000

    70c3461be3 fix(compiler-cli): use correct module import for types behind a `forwardRef` (#42887)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+41.sha-445fee4
+ * @license Angular v12.2.0-next.2+44.sha-70c3461
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22003,7 +22003,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0-next.2+41.sha-445fee4');
+    var VERSION = new Version('12.2.0-next.2+44.sha-70c3461');
 
     /**
      * @license
