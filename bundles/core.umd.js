39548f83b66d4e93a12fbd8c2fd789d736bd97c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-21 22:26:13 +0000

    23d6133ea8 docs: release notes for the v12.1.3 release

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+45.sha-e2aa765
+ * @license Angular v12.2.0-next.2+46.sha-23d6133
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22003,7 +22003,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0-next.2+45.sha-e2aa765');
+    var VERSION = new Version('12.2.0-next.2+46.sha-23d6133');
 
     /**
      * @license
