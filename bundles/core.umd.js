f0436b8b87321397982f72c1c1da5e2a61c67a42
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 19:27:13 +0000

    a6c256fcd6 docs: Add import line to router event example for clarification (#42935)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+70.sha-ccbb913.with-local-changes
+ * @license Angular v12.2.0-next.1+72.sha-7313756.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22003,7 +22003,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0-next.1+70.sha-ccbb913.with-local-changes');
+    var VERSION = new Version('12.2.0-next.1+72.sha-7313756.with-local-changes');
 
     /**
      * @license
