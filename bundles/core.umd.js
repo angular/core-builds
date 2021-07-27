4635fab600845b8716358da06093e472b9f091e6
Dylan Hunn <dylhunn@gmail.com>
2021-07-27 16:41:54 +0000

    b93c42eff3 docs: updated input-output page documentation content (#42964) PR Close #42964

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+82.sha-01e869a.with-local-changes
+ * @license Angular v12.2.0-next.1+83.sha-388496c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22032,7 +22032,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0-next.1+82.sha-01e869a.with-local-changes');
+    var VERSION = new Version('12.2.0-next.1+83.sha-388496c.with-local-changes');
 
     /**
      * @license
