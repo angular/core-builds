a493d1c515f8071fedbb85cc9275a302a7548a11
atscott <atscott01@gmail.com>
2021-08-03 22:58:29 +0000

    6875d9463f Revert "refactor(ngcc): remove unused import (#42062)" (#43033)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+83.sha-4f8a728.with-local-changes
+ * @license Angular v12.1.2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22066,7 +22066,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.1.1+83.sha-4f8a728.with-local-changes');
+    var VERSION = new Version('12.1.2.with-local-changes');
 
     /**
      * @license
