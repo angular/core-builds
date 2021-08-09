2eb77e850eef6725fe7cf867085d70d8ca6c3ef6
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:47:25 +0000

    dd3abdb9d9 fix(router): ensure check for match options is compatible with property renaming (#43086)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0+7.sha-d93427b.with-local-changes
+ * @license Angular v12.2.0+8.sha-dd3abdb.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22033,7 +22033,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.0+7.sha-d93427b.with-local-changes');
+    var VERSION = new Version('12.2.0+8.sha-dd3abdb.with-local-changes');
 
     /**
      * @license
