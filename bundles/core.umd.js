4f763affb360be3c8a4453d35b3ae924da99245b
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 18:11:32 +0000

    513b17043e docs: fix typo (#43190)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+32.sha-c501e86.with-local-changes
+ * @license Angular v12.2.2+1.sha-513b170.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22033,7 +22033,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.1+32.sha-c501e86.with-local-changes');
+    var VERSION = new Version('12.2.2+1.sha-513b170.with-local-changes');
 
     /**
      * @license
