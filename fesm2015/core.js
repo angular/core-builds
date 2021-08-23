5c186b802cdbeb4b0681ec9328af199067cdded7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-23 20:49:17 +0000

    6e924313c3 fix(service-worker): NPE if onActionClick is undefined (#43210)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.2+9.sha-ccb09b4.with-local-changes
+ * @license Angular v13.0.0-next.2+11.sha-6e92431.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.2+9.sha-ccb09b4.with-local-changes');
+const VERSION = new Version('13.0.0-next.2+11.sha-6e92431.with-local-changes');
 
 /**
  * @license
