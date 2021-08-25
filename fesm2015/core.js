23367c829425e6c11b6e46153373b408c795cf8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:10:28 +0000

    b46ce7d42f build(docs-infra): upgrade cli command docs sources to 56c5962df (#43239)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.2+9.sha-d7b6416.with-local-changes
+ * @license Angular v12.2.2+16.sha-b46ce7d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.2+9.sha-d7b6416.with-local-changes');
+const VERSION = new Version('12.2.2+16.sha-b46ce7d.with-local-changes');
 
 /**
  * @license
