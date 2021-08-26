5884163cd6ef5fba1c0204df5aaa2ec7297b33cc
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 20:02:04 +0000

    30d959a48a build: update issue template to include core package (#43272)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.3+5.sha-fdcb420.with-local-changes
+ * @license Angular v12.2.3+6.sha-30d959a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.3+5.sha-fdcb420.with-local-changes');
+const VERSION = new Version('12.2.3+6.sha-30d959a.with-local-changes');
 
 /**
  * @license
