e12afc5d7fba0a979d5bf53bd5328afe17e70fa1
GitHub <noreply@github.com>
2021-09-01 18:56:56 +0000

    caecd1889b release: cut the v12.2.4 release (#43308)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.3+16.sha-d8f637a.with-local-changes
+ * @license Angular v12.2.3+17.sha-caecd18.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22033,7 +22033,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('12.2.3+16.sha-d8f637a.with-local-changes');
+    var VERSION = new Version('12.2.3+17.sha-caecd18.with-local-changes');
 
     /**
      * @license
