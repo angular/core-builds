aac44719aabbc523676a4bcf8de00e893395bb26
Andrew Scott <atscott01@gmail.com>
2021-09-01 21:41:34 +0000

    95f3aecc29 fix(router): add more context to `Unhandled Navigation Error` (#43291)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.3+22.sha-478131c.with-local-changes
+ * @license Angular v13.0.0-next.4+1.sha-95f3aec.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.3+22.sha-478131c.with-local-changes');
+const VERSION = new Version('13.0.0-next.4+1.sha-95f3aec.with-local-changes');
 
 /**
  * @license
