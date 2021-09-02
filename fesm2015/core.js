d2e628b8946eca9a6074b8388fae668cc33de80d
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:05:01 +0000

    3c863c5c4e build(docs-infra): update dgeni-packages to improve checkLinks messages (#43317)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.4+10.sha-c7a93e6.with-local-changes
+ * @license Angular v13.0.0-next.4+2.sha-3c863c5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.4+10.sha-c7a93e6.with-local-changes');
+const VERSION = new Version('13.0.0-next.4+2.sha-3c863c5.with-local-changes');
 
 /**
  * @license
