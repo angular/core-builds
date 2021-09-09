0857b7898bcfb1037e00efc5d426f1daefda3d31
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:53:43 +0000

    8cc93b723c docs: add Angular Projects book in resources (#43130)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.5+4.sha-2658219.with-local-changes
+ * @license Angular v12.2.5+8.sha-8cc93b7.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.5+4.sha-2658219.with-local-changes');
+const VERSION = new Version('12.2.5+8.sha-8cc93b7.with-local-changes');
 
 /**
  * @license
