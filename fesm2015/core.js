3ac05c7bc5804ba2a98824894f40baffbdf2440c
Andrew Kushnir <akushnir@google.com>
2021-09-14 00:15:06 +0000

    4034f252c9 fix(router): Allow renavigating to failed URLs (#43424)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.5+25.sha-52c320f.with-local-changes
+ * @license Angular v12.2.5+28.sha-4034f25.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.5+25.sha-52c320f.with-local-changes');
+const VERSION = new Version('12.2.5+28.sha-4034f25.with-local-changes');
 
 /**
  * @license
