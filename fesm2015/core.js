35e260151a32c2c510393deb41cd9666721ebc45
Andrew Kushnir <akushnir@google.com>
2021-09-15 18:07:59 +0000

    66c6dbdc79 refactor(docs-infra): introduce max-len 120 eslint rule (#43439)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+11.sha-1986407.with-local-changes
+ * @license Angular v13.0.0-next.6+13.sha-66c6dbd.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.6+11.sha-1986407.with-local-changes');
+const VERSION = new Version('13.0.0-next.6+13.sha-66c6dbd.with-local-changes');
 
 /**
  * @license
