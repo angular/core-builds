9a94b17fd405600ffcf3a6e7b502bd7ee31e0ed0
GitHub <noreply@github.com>
2021-09-15 17:55:22 +0000

    bab963fd7f docs: release notes for the v12.2.6 release (#43463)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+54.sha-02ccceb.with-local-changes
+ * @license Angular v13.0.0-next.5+55.sha-bab963f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21458,7 +21458,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.5+54.sha-02ccceb.with-local-changes');
+const VERSION = new Version('13.0.0-next.5+55.sha-bab963f.with-local-changes');
 
 /**
  * @license
