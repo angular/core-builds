f4fdf3e85cb0e42d155455d6c2e3b27dc2caad78
Andrew Kushnir <akushnir@google.com>
2021-09-15 18:10:18 +0000

    e5d08c1902 docs: update reviewed tag (#43452)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+13.sha-66c6dbd.with-local-changes
+ * @license Angular v13.0.0-next.6+14.sha-e5d08c1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21429,7 +21429,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.6+13.sha-66c6dbd.with-local-changes');
+const VERSION = new Version('13.0.0-next.6+14.sha-e5d08c1.with-local-changes');
 
 /**
  * @license
