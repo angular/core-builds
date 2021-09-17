5076fb8c39db968a5a93e8d7cb6a5f2ae7ff259f
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 17:00:44 +0000

    312e421ff3 docs: Edits to remove jargon (#43045)

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+30.sha-313ea92.with-local-changes
+ * @license Angular v13.0.0-next.6+31.sha-312e421.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22004,7 +22004,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new Version('13.0.0-next.6+30.sha-313ea92.with-local-changes');
+    var VERSION = new Version('13.0.0-next.6+31.sha-312e421.with-local-changes');
 
     /**
      * @license
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+30.sha-313ea92.with-local-changes
+ * @license Angular v13.0.0-next.6+31.sha-312e421.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21429,7 +21429,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.6+30.sha-313ea92.with-local-changes');
+const VERSION = new Version('13.0.0-next.6+31.sha-312e421.with-local-changes');
 
 /**
  * @license
