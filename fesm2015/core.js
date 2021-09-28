63b571d0bba7e19e0c46a03798eba8ae4744bd82
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:37:16 +0000

    4f3beffdbf feat(router): emit activate/deactivate events when an outlet gets attached/detached (#43333)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+13.sha-8d2b6af.with-local-changes
+ * @license Angular v13.0.0-next.8+14.sha-4f3beff.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21459,7 +21459,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.8+13.sha-8d2b6af.with-local-changes');
+const VERSION = new Version('13.0.0-next.8+14.sha-4f3beff.with-local-changes');
 
 /**
  * @license
