ea97ab8f93388a44795e672a251b2d4bc945c1ef
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 20:57:30 +0000

    d585fbc3d8 ci: change ownership of `goldens/public-api/manage.js` (#43664)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.11+9.sha-0dc4544.with-local-changes
+ * @license Angular v13.0.0-next.11+11.sha-d585fbc.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21382,7 +21382,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.11+9.sha-0dc4544.with-local-changes');
+const VERSION = new Version('13.0.0-next.11+11.sha-d585fbc.with-local-changes');
 
 /**
  * @license
