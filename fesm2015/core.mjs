f9fe37e401cf5f64de8c8c263fae2ce659fde914
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 00:48:14 +0000

    c14085e434 feat(core): drop support for TypeScript 4.2 and 4.3 (#43642)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.11+10.sha-fddb50b.with-local-changes
+ * @license Angular v13.0.0-next.11+14.sha-c14085e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21382,7 +21382,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.11+10.sha-fddb50b.with-local-changes');
+const VERSION = new Version('13.0.0-next.11+14.sha-c14085e.with-local-changes');
 
 /**
  * @license
