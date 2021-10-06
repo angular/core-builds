7e270be830715bb1ad8c00e5920bd06ba7e579cd
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 18:14:23 +0000

    e0a0d05d45 feat(core): update node version support range to support v16 (#43740)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.12+14.sha-841c5ae.with-local-changes
+ * @license Angular v13.0.0-next.12+16.sha-e0a0d05.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21382,7 +21382,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.12+14.sha-841c5ae.with-local-changes');
+const VERSION = new Version('13.0.0-next.12+16.sha-e0a0d05.with-local-changes');
 
 /**
  * @license
