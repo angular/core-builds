91fdbf076c8b8575ea22bc690fa637554b2c830f
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 22:45:27 +0000

    04fec60a61 ci: ignore "remark*" packages from automatic updates (#43735)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.12+17.sha-5d7229e.with-local-changes
+ * @license Angular v13.0.0-next.13+3.sha-04fec60.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21382,7 +21382,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.12+17.sha-5d7229e.with-local-changes');
+const VERSION = new Version('13.0.0-next.13+3.sha-04fec60.with-local-changes');
 
 /**
  * @license
