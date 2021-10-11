4747e4ee2b8196319e2b9f92e08762adc9586628
Andrew Scott <atscott01@gmail.com>
2021-10-11 16:59:45 +0000

    8b2971417a docs: add missing declaration of `CartComponent` in tutorial (#43774)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+17.sha-4c00e92.with-local-changes
+ * @license Angular v13.0.0-next.14+19.sha-8b29714.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21397,7 +21397,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+17.sha-4c00e92.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+19.sha-8b29714.with-local-changes');
 
 /**
  * @license
