ac9c2622f719114f2a43e8fe257009a8e9e9e606
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:07:51 +0000

    fced3e88cf docs(animations): fix wrong code links present in the animations guide (#43787)

--- core.d.ts
+++ core.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.7+49.sha-d6679e2.with-local-changes
+ * @license Angular v12.2.9+13.sha-fced3e8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ declare interface R3DeclareDirectiveFacade {
 
 declare interface R3DeclareFactoryFacade {
     type: Type_2;
-    deps: R3DeclareDependencyMetadataFacade[] | null;
+    deps: R3DeclareDependencyMetadataFacade[] | 'invalid' | null;
     target: ɵɵFactoryTarget;
 }
 
