ed29b68f191a87702e2db373fef0212246125c48
Andrew Kushnir <akushnir@google.com>
2021-10-18 16:54:59 +0000

    db57e391cc docs: update getting started links to provide a more consistent user experience (#43872)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.0+16.sha-ee2678c.with-local-changes
+ * @license Angular v13.0.0-rc.0+20.sha-db57e39.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21404,7 +21404,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-rc.0+16.sha-ee2678c.with-local-changes');
+const VERSION = new Version('13.0.0-rc.0+20.sha-db57e39.with-local-changes');
 
 /**
  * @license
--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.0+16.sha-ee2678c.with-local-changes
+ * @license Angular v13.0.0-rc.0+20.sha-db57e39.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1927,9 +1927,9 @@ class TestingCompiler extends Compiler {
         throw unimplemented();
     }
 }
-TestingCompiler.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.0.0-rc.0+16.sha-ee2678c.with-local-changes", ngImport: i0, type: TestingCompiler, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
-TestingCompiler.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.0.0-rc.0+16.sha-ee2678c.with-local-changes", ngImport: i0, type: TestingCompiler });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.0-rc.0+16.sha-ee2678c.with-local-changes", ngImport: i0, type: TestingCompiler, decorators: [{
+TestingCompiler.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.0.0-rc.0+20.sha-db57e39.with-local-changes", ngImport: i0, type: TestingCompiler, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
+TestingCompiler.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.0.0-rc.0+20.sha-db57e39.with-local-changes", ngImport: i0, type: TestingCompiler });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.0-rc.0+20.sha-db57e39.with-local-changes", ngImport: i0, type: TestingCompiler, decorators: [{
             type: Injectable
         }] });
 /**
