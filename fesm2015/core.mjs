c0e72333ed82dd2a51d50ea4a2f3aa68ff8a3ed0
Andrew Kushnir <akushnir@google.com>
2021-10-18 16:39:37 +0000

    67a7c48683 fix(docs-infra): move security anchors outside callout elements (#43865)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+64.sha-e79e3ca.with-local-changes
+ * @license Angular v13.0.0-next.14+66.sha-67a7c48.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21389,7 +21389,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+64.sha-e79e3ca.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+66.sha-67a7c48.with-local-changes');
 
 /**
  * @license
