f837a45cb8766df1b7863882326406ae7279e215
Andrew Kushnir <akushnir@google.com>
2021-10-18 22:36:26 +0000

    9f40d2a0ea ci: remove browserstack configuration (#43881)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+76.sha-60f3b33.with-local-changes
+ * @license Angular v13.0.0-next.14+78.sha-9f40d2a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21389,7 +21389,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+76.sha-60f3b33.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+78.sha-9f40d2a.with-local-changes');
 
 /**
  * @license
