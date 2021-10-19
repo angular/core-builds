60a346ab1b671f7bf4ed29d966666f6be13489b5
Andrew Kushnir <akushnir@google.com>
2021-10-19 23:42:42 +0000

    7d33af0400 test(compiler-cli): run i18n extraction tests using Ivy compiler (#43893)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.0+40.sha-9d9346c.with-local-changes
+ * @license Angular v13.0.0-rc.0+42.sha-7d33af0.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21403,7 +21403,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-rc.0+40.sha-9d9346c.with-local-changes');
+const VERSION = new Version('13.0.0-rc.0+42.sha-7d33af0.with-local-changes');
 
 /**
  * @license
