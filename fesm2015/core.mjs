8ae9a0536f2bd66f3b4106e60033c8d0e9bfcf9f
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 14:39:33 +0000

    c5d0b76e29 refactor(service-worker): avoid unnecessary writes to cache when version fails (#43680)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+98.sha-c78f1cd.with-local-changes
+ * @license Angular v13.0.0-next.14+99.sha-c5d0b76.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21388,7 +21388,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+98.sha-c78f1cd.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+99.sha-c5d0b76.with-local-changes');
 
 /**
  * @license
