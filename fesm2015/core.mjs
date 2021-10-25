cf93e886323029c318a42fc372a6784e6e4a3f69
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:16:26 +0000

    8277c38bd8 test(platform-browser): complete component bootstrap before switching to the next test (#43933)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.1+11.sha-97fc9de.with-local-changes
+ * @license Angular v13.0.0-rc.1+17.sha-8277c38.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21388,7 +21388,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-rc.1+11.sha-97fc9de.with-local-changes');
+const VERSION = new Version('13.0.0-rc.1+17.sha-8277c38.with-local-changes');
 
 /**
  * @license
