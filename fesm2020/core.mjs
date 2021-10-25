1c23a5d7a96014f9868e3fe863e15531db90bf3f
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 19:49:04 +0000

    087fae5ea9 refactor(migrations): remove no longer accessible migrations (#43942)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.1+17.sha-8277c38.with-local-changes
+ * @license Angular v13.0.0-rc.1+18.sha-087fae5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21403,7 +21403,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-rc.1+17.sha-8277c38.with-local-changes');
+const VERSION = new Version('13.0.0-rc.1+18.sha-087fae5.with-local-changes');
 
 /**
  * @license
