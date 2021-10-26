6a7e472111529ce84eb427b13b8551cc650bc06b
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 22:23:39 +0000

    6c471396af build: update all non-major dependencies (#43917)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+124.sha-5c22dfe.with-local-changes
+ * @license Angular v13.0.0-next.14+125.sha-6c47139.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21415,7 +21415,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+124.sha-5c22dfe.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+125.sha-6c47139.with-local-changes');
 
 /**
  * @license
