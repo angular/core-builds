5343be8032f75080503265f986a5875221b9fb6d
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 01:47:39 +0000

    5f32863a74 docs(docs-infra): change deprecated keyboardEvent.keyCode example to use keyboardEvent.code (#43934)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+8.sha-c831c67.with-local-changes
+ * @license Angular v12.2.11+10.sha-5f32863.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21466,7 +21466,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.11+8.sha-c831c67.with-local-changes');
+const VERSION = new Version('12.2.11+10.sha-5f32863.with-local-changes');
 
 /**
  * @license
