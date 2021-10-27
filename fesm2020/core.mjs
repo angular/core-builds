0bbbfcddcfbe8d010770d33b5decb4f352916093
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 16:30:28 +0000

    b393188899 fix(docs-infra): fix heritage links (#43956)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+131.sha-57cbb4f.with-local-changes
+ * @license Angular v13.0.0-next.14+133.sha-b393188.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21415,7 +21415,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+131.sha-57cbb4f.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+133.sha-b393188.with-local-changes');
 
 /**
  * @license
