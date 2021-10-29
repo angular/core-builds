14cf5673493d17c21fdcc3c48efb28fae254212d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 20:30:20 +0000

    7b35a3426f refactor(docs-infra): align docs examples with default CLI TS config (#43880)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+139.sha-65cb2c5.with-local-changes
+ * @license Angular v13.0.0-next.14+144.sha-7b35a34.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21396,7 +21396,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+139.sha-65cb2c5.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+144.sha-7b35a34.with-local-changes');
 
 /**
  * @license
