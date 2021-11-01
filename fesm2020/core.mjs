da8c09862c725db19370fc07fdded0a839ed8d8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-01 16:45:36 +0000

    d015ac214f Revert "refactor(router): restore history in a consistent way on canceled navigations (#43651)" (#44006)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+185.sha-3d1ac6a.with-local-changes
+ * @license Angular v13.0.0-next.14+186.sha-d015ac2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21411,7 +21411,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.14+185.sha-3d1ac6a.with-local-changes');
+const VERSION = new Version('13.0.0-next.14+186.sha-d015ac2.with-local-changes');
 
 /**
  * @license
