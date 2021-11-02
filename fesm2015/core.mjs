aad06847b618bddc75794602008ae33967d983c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-02 02:10:13 +0000

    d24cbcea70 release: cut the v13.0.0-rc.3 release (#44011)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.2+54.sha-4fd388b.with-local-changes
+ * @license Angular v13.0.0-rc.2+55.sha-d24cbce.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21396,7 +21396,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-rc.2+54.sha-4fd388b.with-local-changes');
+const VERSION = new Version('13.0.0-rc.2+55.sha-d24cbce.with-local-changes');
 
 /**
  * @license
