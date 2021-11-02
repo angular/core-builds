2fe03f04e2cd13f90edbca82624976f63ab322a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-02 00:57:35 +0000

    74c497289f docs: update per feedback (#44009)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.2+52.sha-892ded5.with-local-changes
+ * @license Angular v13.0.0-rc.2+53.sha-74c4972.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21396,7 +21396,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-rc.2+52.sha-892ded5.with-local-changes');
+const VERSION = new Version('13.0.0-rc.2+53.sha-74c4972.with-local-changes');
 
 /**
  * @license
