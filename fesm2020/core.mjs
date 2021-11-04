b41411ff5c1d9b98efc25284055c8fd2f1e50bed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-04 00:43:11 +0000

    ef15db0e80 docs: add blog post link to CHANGELOG (#44050)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0+6.sha-0744075.with-local-changes
+ * @license Angular v13.0.0+7.sha-ef15db0.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21411,7 +21411,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0+6.sha-0744075.with-local-changes');
+const VERSION = new Version('13.0.0+7.sha-ef15db0.with-local-changes');
 
 /**
  * @license
