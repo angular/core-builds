0040c815dd82445e1228df23d49df052d25fdd2c
Andrew Scott <atscott01@gmail.com>
2021-11-09 18:36:25 +0000

    28ef5af03f refactor(ngcc): improve logging of progress (#43996)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+57.sha-4f8eaac.with-local-changes
+ * @license Angular v13.1.0-next.0+62.sha-28ef5af.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21435,7 +21435,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.0-next.0+57.sha-4f8eaac.with-local-changes');
+const VERSION = new Version('13.1.0-next.0+62.sha-28ef5af.with-local-changes');
 
 /**
  * @license
