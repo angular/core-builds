325b68b9039c5c24030e2285e54e1d8a7fbdd47c
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 17:52:58 +0000

    2c954bb75d docs: fix filename of the file containing the 'routes'-array in lazy loading guide (#44297)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+55.sha-594bd12.with-local-changes
+ * @license Angular v13.1.0-next.2+54.sha-2c954bb.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21061,7 +21061,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.0-next.2+55.sha-594bd12.with-local-changes');
+const VERSION = new Version('13.1.0-next.2+54.sha-2c954bb.with-local-changes');
 
 /**
  * @license
