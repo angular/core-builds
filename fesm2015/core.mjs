f77ff468526f6de1d64edfdde9e4189d47250541
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:40:30 +0000

    24baa5e61d docs(forms): add an example for async validator token (#44301) to be consistent with the validator token, have the example code for the async validator token.

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+82.sha-96dfc7a.with-local-changes
+ * @license Angular v13.1.0-next.2+83.sha-24baa5e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.0-next.2+82.sha-96dfc7a.with-local-changes');
+const VERSION = new Version('13.1.0-next.2+83.sha-24baa5e.with-local-changes');
 
 /**
  * @license
