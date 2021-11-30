d4923c71d09de03565c4a70ff1c8f927670bc566
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 21:48:51 +0000

    a532040d97 Revert "feat(core): add migration to remove entryComponents (#44308)" (#44318)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+83.sha-24baa5e.with-local-changes
+ * @license Angular v13.1.0-next.2+84.sha-a532040.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21064,7 +21064,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.0-next.2+83.sha-24baa5e.with-local-changes');
+const VERSION = new Version('13.1.0-next.2+84.sha-a532040.with-local-changes');
 
 /**
  * @license
