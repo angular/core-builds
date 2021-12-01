623714ff4534ccd2072a87d402611f6d1e5d2a7e
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 23:38:13 +0000

    f3898f8d84 fix(docs-infra): prevent automatic linking of 'json' (#44294)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+95.sha-6769df7.with-local-changes
+ * @license Angular v13.1.0-next.3+1.sha-f3898f8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.0-next.2+95.sha-6769df7.with-local-changes');
+const VERSION = new Version('13.1.0-next.3+1.sha-f3898f8.with-local-changes');
 
 /**
  * @license
