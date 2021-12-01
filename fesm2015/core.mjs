335a676c915c6882c6cb28a1f504c22c166d7af9
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 21:21:50 +0000

    257a30d7b3 release: cut the v13.0.3 release (#44332)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.2+81.sha-0c9d33c.with-local-changes
+ * @license Angular v13.0.2+82.sha-257a30d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.2+81.sha-0c9d33c.with-local-changes');
+const VERSION = new Version('13.0.2+82.sha-257a30d.with-local-changes');
 
 /**
  * @license
