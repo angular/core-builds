78ac46a9c38b09b1676c7ce444744f66e7cdd4ab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-06 21:21:04 +0000

    d06e54678e refactor(compiler): delete View Engine components of @angular/compiler (#44387)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.3+7.sha-e8e21ac.with-local-changes
+ * @license Angular v13.0.3+11.sha-d06e546.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21064,7 +21064,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.3+7.sha-e8e21ac.with-local-changes');
+const VERSION = new Version('13.0.3+11.sha-d06e546.with-local-changes');
 
 /**
  * @license
