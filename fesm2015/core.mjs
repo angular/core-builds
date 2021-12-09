dce6d50d2e930b4b2e1046e703013b60c22e89aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 16:38:51 +0000

    cab7a84c70 release: bump the next branch to v13.2.0-next.0

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+23.sha-2664bc2.with-local-changes
+ * @license Angular v13.1.0-next.3+24.sha-cab7a84.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.0-next.3+23.sha-2664bc2.with-local-changes');
+const VERSION = new Version('13.1.0-next.3+24.sha-cab7a84.with-local-changes');
 
 /**
  * @license
