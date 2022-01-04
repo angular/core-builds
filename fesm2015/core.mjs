50a3c0280ee409bad0da7ce2dc36cbb0f426b163
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 18:55:10 +0000

    e6beeab7dd ci: remove atscott for unavailable users (#44539)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+1.sha-beb1828.with-local-changes
+ * @license Angular v13.1.1+2.sha-e6beeab.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.1+1.sha-beb1828.with-local-changes');
+const VERSION = new Version('13.1.1+2.sha-e6beeab.with-local-changes');
 
 /**
  * @license
