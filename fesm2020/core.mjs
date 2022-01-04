70278585035f40a17860410cfebc103c4a5e709e
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:33:10 +0000

    3a29c57f60 test: update size goldens to reflect latest CLI devkit update (#44490)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+26.sha-f0b4c70.with-local-changes
+ * @license Angular v13.2.0-next.1+23.sha-3a29c57.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21064,7 +21064,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-next.1+26.sha-f0b4c70.with-local-changes');
+const VERSION = new Version('13.2.0-next.1+23.sha-3a29c57.with-local-changes');
 
 /**
  * @license
