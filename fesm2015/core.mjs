de320c3521c185d42cd13f32b1cacf2cafa9415e
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:36:22 +0000

    98592b03bd docs(common): url link format updated (#44638)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+49.sha-0daa9bf.with-local-changes
+ * @license Angular v13.1.1+57.sha-98592b0.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.1+49.sha-0daa9bf.with-local-changes');
+const VERSION = new Version('13.1.1+57.sha-98592b0.with-local-changes');
 
 /**
  * @license
