1b81281c9de2bb5eb28a0369071017e9e6a963a2
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:50:29 +0000

    d5ced77d31 refactor(docs-infra): fix eslint issues for aio ugrade-* examples (#44534)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+49.sha-c7ac2df.with-local-changes
+ * @license Angular v13.2.0-next.1+50.sha-d5ced77.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-next.1+49.sha-c7ac2df.with-local-changes');
+const VERSION = new Version('13.2.0-next.1+50.sha-d5ced77.with-local-changes');
 
 /**
  * @license
