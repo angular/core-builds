8c90c44c313df8dcf672ec947cd9f43cb799a57a
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:38:15 +0000

    e39f804f1e test(upgrade): fix flaky tests (#44597)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+61.sha-2d14724.with-local-changes
+ * @license Angular v13.2.0-next.1+60.sha-e39f804.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-next.1+61.sha-2d14724.with-local-changes');
+const VERSION = new Version('13.2.0-next.1+60.sha-e39f804.with-local-changes');
 
 /**
  * @license
