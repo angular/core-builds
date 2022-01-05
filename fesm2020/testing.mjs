9394b62d35bd26ad45939940fc7f580da730b2c8
Andrew Scott <atscott01@gmail.com>
2022-01-05 18:03:10 +0000

    6a55affacd docs(router): fix code block for IsActiveMatchOptions (#44635)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+50.sha-d5ced77.with-local-changes
+ * @license Angular v13.2.0-next.1+52.sha-6a55aff.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
