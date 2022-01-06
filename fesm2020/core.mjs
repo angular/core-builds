23c5b5040c27a2eb388b8c21955f96bfb5715022
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:58:14 +0000

    7b9169e5e1 docs(animations): fix grammar (#44607)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+64.sha-3591032.with-local-changes
+ * @license Angular v13.2.0-next.1+66.sha-7b9169e.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21064,7 +21064,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-next.1+64.sha-3591032.with-local-changes');
+const VERSION = new Version('13.2.0-next.1+66.sha-7b9169e.with-local-changes');
 
 /**
  * @license
