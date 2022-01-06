746624da22b3235e76f879d8f2d5b78778427c68
Andrew Scott <atscott01@gmail.com>
2022-01-06 00:40:12 +0000

    9b9042c8d1 docs(animations): fix links to the AnimationBuilder.build method (#44594)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+57.sha-98592b0.with-local-changes
+ * @license Angular v13.1.1+58.sha-9b9042c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21050,7 +21050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.1+57.sha-98592b0.with-local-changes');
+const VERSION = new Version('13.1.1+58.sha-9b9042c.with-local-changes');
 
 /**
  * @license
