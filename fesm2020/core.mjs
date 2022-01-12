49f0c563c2834fdf68860eb3ee00ed669eb17ccb
GitHub <noreply@github.com>
2022-01-12 19:11:54 +0000

    555a57d505 docs: release notes for the v13.1.2 release (#44699)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+107.sha-e98b8f5.with-local-changes
+ * @license Angular v13.2.0-next.1+108.sha-555a57d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21042,7 +21042,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-next.1+107.sha-e98b8f5.with-local-changes');
+const VERSION = new Version('13.2.0-next.1+108.sha-555a57d.with-local-changes');
 
 /**
  * @license
