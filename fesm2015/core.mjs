7990a1e297b0b8cf33def5a6509f9445ff040849
GitHub <noreply@github.com>
2022-01-12 19:03:15 +0000

    d1762c78af release: cut the v13.1.2 release (#44698)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+96.sha-92784ca.with-local-changes
+ * @license Angular v13.1.1+97.sha-d1762c7.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21028,7 +21028,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.1+96.sha-92784ca.with-local-changes');
+const VERSION = new Version('13.1.1+97.sha-d1762c7.with-local-changes');
 
 /**
  * @license
