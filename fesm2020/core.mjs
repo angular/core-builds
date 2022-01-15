91a210f80e6ca1998f8d19544f6d46ad3e4ee07b
Dylan Hunn <dylhunn@gmail.com>
2022-01-15 01:59:45 +0000

    f9ca4d8499 fix(ngcc): support element accesses for export declarations (#44669)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.2+12.sha-af0a152.with-local-changes
+ * @license Angular v13.1.2+13.sha-f9ca4d8.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21042,7 +21042,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.2+12.sha-af0a152.with-local-changes');
+const VERSION = new Version('13.1.2+13.sha-f9ca4d8.with-local-changes');
 
 /**
  * @license
