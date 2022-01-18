5dbfefc49ee8a352d85d8a0ff7184df2803d68db
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 18:49:25 +0000

    cab81ba13a refactor(common): make the error messages tree shakable (#44663)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.2+15.sha-617610e.with-local-changes
+ * @license Angular v13.1.2+16.sha-cab81ba.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21042,7 +21042,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.2+15.sha-617610e.with-local-changes');
+const VERSION = new Version('13.1.2+16.sha-cab81ba.with-local-changes');
 
 /**
  * @license
