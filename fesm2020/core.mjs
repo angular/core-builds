77595d674146a3e89da4b652c0655afd92a0fb89
Andrew Kushnir <akushnir@google.com>
2022-01-20 22:20:41 +0000

    197f3f4a26 fix(compiler-cli): remove leftover `_extendedTemplateDiagnostics` requirements (#44777)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+39.sha-4e180cb.with-local-changes
+ * @license Angular v13.2.0-next.2+40.sha-197f3f4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21075,7 +21075,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-next.2+39.sha-4e180cb.with-local-changes');
+const VERSION = new Version('13.2.0-next.2+40.sha-197f3f4.with-local-changes');
 
 /**
  * @license
