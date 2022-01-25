efc398f7613b7ef1c85b507e8b80931eb5aa06dd
Andrew Kushnir <akushnir@google.com>
2022-01-25 17:41:36 +0000

    6cb7c3ebe3 build(docs-infra): update `dgeni-packages` to v0.29.3 (#44810)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-rc.1+22.sha-7e93513.with-local-changes
+ * @license Angular v13.2.0-rc.1+24.sha-6cb7c3e.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21069,7 +21069,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-rc.1+22.sha-7e93513.with-local-changes');
+const VERSION = new Version('13.2.0-rc.1+24.sha-6cb7c3e.with-local-changes');
 
 /**
  * @license
