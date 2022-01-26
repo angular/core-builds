1c5ef75d28fd8b168dcc3c1d9235093b3e2f524b
Andrew Kushnir <akushnir@google.com>
2022-01-26 20:51:50 +0000

    e46ac81f54 docs: cleanup CHANGELOG.md file (#44841)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+81.sha-04eec90.with-local-changes
+ * @license Angular v14.0.0-next.0+1.sha-e46ac81.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21069,7 +21069,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-next.2+81.sha-04eec90.with-local-changes');
+const VERSION = new Version('14.0.0-next.0+1.sha-e46ac81.with-local-changes');
 
 /**
  * @license
