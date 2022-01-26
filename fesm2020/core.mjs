98495dcae081baeea61b1517fc8a79b1f35e7653
Andrew Kushnir <akushnir@google.com>
2022-01-26 21:10:21 +0000

    fcc6528159 test(animations): Add bundling size regression test for animations package (#44826)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+1.sha-dc0cac2.with-local-changes
+ * @license Angular v13.2.0+2.sha-fcc6528.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21083,7 +21083,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0+1.sha-dc0cac2.with-local-changes');
+const VERSION = new Version('13.2.0+2.sha-fcc6528.with-local-changes');
 
 /**
  * @license
