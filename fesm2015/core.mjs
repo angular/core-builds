1fdb045e3965753976a2d7affa8c8b5636fa74cd
Andrew Kushnir <akushnir@google.com>
2022-01-27 01:53:34 +0000

    4a91d9b0a0 build: update dependency rules_pkg to v0.6.0 (#44818)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+2.sha-fcc6528.with-local-changes
+ * @license Angular v13.2.0+3.sha-4a91d9b.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21069,7 +21069,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0+2.sha-fcc6528.with-local-changes');
+const VERSION = new Version('13.2.0+3.sha-4a91d9b.with-local-changes');
 
 /**
  * @license
