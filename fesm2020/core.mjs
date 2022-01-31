a5aee2af16a30d0d2e019dcc63f7d43509025d43
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:55:31 +0000

    9eff8908af refactor(core): strict templates type-checking compatibility for perf tests (#44905)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1065.sha-889de82.with-local-changes
+ * @license Angular v14.0.0-next.0+1066.sha-9eff890.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21083,7 +21083,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.0+1065.sha-889de82.with-local-changes');
+const VERSION = new Version('14.0.0-next.0+1066.sha-9eff890.with-local-changes');
 
 /**
  * @license
