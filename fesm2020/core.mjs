f600914c89117f8e0e04ad4de7ff66a4b200f790
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 23:02:10 +0000

    4644886aaf perf(animations): remove no longer needed CssKeyframes classes (#44903) (#44919)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+39.sha-f5ce6cf.with-local-changes
+ * @license Angular v13.2.0+40.sha-4644886.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21083,7 +21083,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0+39.sha-f5ce6cf.with-local-changes');
+const VERSION = new Version('13.2.0+40.sha-4644886.with-local-changes');
 
 /**
  * @license
