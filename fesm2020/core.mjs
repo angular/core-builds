5bbfc4f9f7de212d16cd0d3704417665a00b50ad
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:56:31 +0000

    f5ce6cfa1e refactor(compiler-cli): remove unused `canonical-path` dependency (#44918)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+34.sha-8fb2e3b.with-local-changes
+ * @license Angular v13.2.0+39.sha-f5ce6cf.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21083,7 +21083,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0+34.sha-8fb2e3b.with-local-changes');
+const VERSION = new Version('13.2.0+39.sha-f5ce6cf.with-local-changes');
 
 /**
  * @license
