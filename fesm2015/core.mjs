fb567fa62dbe534fb2ab67f43bb47205f20d7c07
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 02:12:17 +0000

    165aa4f164 docs: update the survey expiration date (#44938)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+55.sha-4a5ad17.with-local-changes
+ * @license Angular v13.2.0+56.sha-165aa4f.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0+55.sha-4a5ad17.with-local-changes');
+const VERSION = new Version('13.2.0+56.sha-165aa4f.with-local-changes');
 
 /**
  * @license
