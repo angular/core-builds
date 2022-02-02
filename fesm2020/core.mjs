a09e3f4af94d722c409042442994264cb3811418
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 20:21:14 +0000

    a01bcb8e7e fix(platform-browser): do not run change detection when loading Hammer (#44921)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+1.sha-39b90b4.with-local-changes
+ * @license Angular v14.0.0-next.1+2.sha-a01bcb8.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21090,7 +21090,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.1+1.sha-39b90b4.with-local-changes');
+const VERSION = new Version('14.0.0-next.1+2.sha-a01bcb8.with-local-changes');
 
 /**
  * @license
