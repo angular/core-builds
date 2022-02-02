09e534f0461251f30475f7e95e06b26006990333
GitHub <noreply@github.com>
2022-02-02 20:08:30 +0000

    0c36f82134 release: cut the v14.0.0-next.1 release (#44951)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1092.sha-38bb2be.with-local-changes
+ * @license Angular v14.0.0-next.0+1093.sha-0c36f82.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.0+1092.sha-38bb2be.with-local-changes');
+const VERSION = new Version('14.0.0-next.0+1093.sha-0c36f82.with-local-changes');
 
 /**
  * @license
