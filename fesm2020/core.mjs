57046b5cfa5cafb2a3d93c90ba92f67d48b002f6
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 20:58:15 +0000

    226fac67bd docs: describe your change... (#42899)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+18.sha-f7ea275.with-local-changes
+ * @license Angular v14.0.0-next.1+19.sha-226fac6.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21090,7 +21090,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.1+18.sha-f7ea275.with-local-changes');
+const VERSION = new Version('14.0.0-next.1+19.sha-226fac6.with-local-changes');
 
 /**
  * @license
