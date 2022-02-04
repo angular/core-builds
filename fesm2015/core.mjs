55883a39ade86f28b3854b9066435fe926869de7
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:58:48 +0000

    d183e7d736 docs: correct typo (#44793)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+31.sha-459cbed.with-local-changes
+ * @license Angular v14.0.0-next.1+32.sha-d183e7d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.1+31.sha-459cbed.with-local-changes');
+const VERSION = new Version('14.0.0-next.1+32.sha-d183e7d.with-local-changes');
 
 /**
  * @license
