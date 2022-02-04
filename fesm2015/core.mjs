182cf26790c4fcff513aac9645961e1ef85e0333
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 20:11:53 +0000

    cd97d8793f build: update all non-major dependencies (#44974)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+20.sha-3fae663.with-local-changes
+ * @license Angular v13.2.1+21.sha-cd97d87.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.1+20.sha-3fae663.with-local-changes');
+const VERSION = new Version('13.2.1+21.sha-cd97d87.with-local-changes');
 
 /**
  * @license
