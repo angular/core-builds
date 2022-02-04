625a2c19aa8096fe242d7b831d3b4ade4db353db
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 07:32:21 +0000

    8dd3f82f94 fix(forms): Correct empty validator to handle objects with a property `length: 0`. (#33729)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+29.sha-b32647d.with-local-changes
+ * @license Angular v14.0.0-next.1+30.sha-8dd3f82.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.1+29.sha-b32647d.with-local-changes');
+const VERSION = new Version('14.0.0-next.1+30.sha-8dd3f82.with-local-changes');
 
 /**
  * @license
