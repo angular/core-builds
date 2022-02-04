05f7e1ad6155c04cced01e3e1c8abedb77aca824
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 01:24:41 +0000

    64f837d2c0 fix(router): Update `Navigation#initialUrl` to match documentation and reality (#43863)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+27.sha-e46b379.with-local-changes
+ * @license Angular v14.0.0-next.1+28.sha-64f837d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.1+27.sha-e46b379.with-local-changes');
+const VERSION = new Version('14.0.0-next.1+28.sha-64f837d.with-local-changes');
 
 /**
  * @license
