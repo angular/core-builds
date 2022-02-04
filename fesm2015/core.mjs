ba6f5b5c9c87f0bed6cbc6f32351ef146e8da69d
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 00:10:57 +0000

    e46b379204 fix(animations): implement missing transition delay (#44799)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+26.sha-6a5f2da.with-local-changes
+ * @license Angular v14.0.0-next.1+27.sha-e46b379.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.1+26.sha-6a5f2da.with-local-changes');
+const VERSION = new Version('14.0.0-next.1+27.sha-e46b379.with-local-changes');
 
 /**
  * @license
