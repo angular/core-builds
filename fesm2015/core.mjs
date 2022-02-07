45529c70a4f5b4c26c97aae0d9b6eff8c009da97
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 21:36:02 +0000

    1368f58b49 docs: add Cost-Effective Angular Testing (#44969)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+25.sha-3919ee3.with-local-changes
+ * @license Angular v13.2.1+27.sha-1368f58.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.1+25.sha-3919ee3.with-local-changes');
+const VERSION = new Version('13.2.1+27.sha-1368f58.with-local-changes');
 
 /**
  * @license
