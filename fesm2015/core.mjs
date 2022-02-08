07c3c3fde6f8a352fb77891c834504555092e170
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 01:35:26 +0000

    a3de44c7f6 release: cut the v14.0.0-next.2 release (#45008)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+46.sha-64acbc4.with-local-changes
+ * @license Angular v14.0.0-next.1+47.sha-a3de44c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21076,7 +21076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.1+46.sha-64acbc4.with-local-changes');
+const VERSION = new Version('14.0.0-next.1+47.sha-a3de44c.with-local-changes');
 
 /**
  * @license
