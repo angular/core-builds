d059dff393b793b550a9bbf1f41508165bb9bdda
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 17:56:27 +0000

    7a47add255 release: cut the v13.2.2 release (#45018)

--- core.d.ts
+++ core.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+29.sha-7bc4dbd.with-local-changes
+ * @license Angular v13.2.1+30.sha-7a47add.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
