690966b5ceeca011f5295ed5f2aed6073450b1b1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-11 22:49:20 +0000

    b4157449c4 refactor(docs-infra): replace deprecated subscribe usages (#45046)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+22.sha-8793dbd.with-local-changes
+ * @license Angular v14.0.0-next.2+24.sha-b415744.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21102,7 +21102,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.2+22.sha-8793dbd.with-local-changes');
+const VERSION = new Version('14.0.0-next.2+24.sha-b415744.with-local-changes');
 
 /**
  * @license
