177740628e9a4f21fa3323493fdf8fea2c464061
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:45:57 +0000

    13172421f9 docs: fix grammatical error (#45092)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+34.sha-787eaa7.with-local-changes
+ * @license Angular v14.0.0-next.2+35.sha-1317242.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21102,7 +21102,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.2+34.sha-787eaa7.with-local-changes');
+const VERSION = new Version('14.0.0-next.2+35.sha-1317242.with-local-changes');
 
 /**
  * @license
