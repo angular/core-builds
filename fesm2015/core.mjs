fc412e067f8ca4bb5f42913895c7f1e3ccbeae5b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:05:37 +0000

    5c7c56bc85 perf(animations): improve algorithm to balance animation namespaces (#45057)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+43.sha-1eae65e.with-local-changes
+ * @license Angular v14.0.0-next.2+44.sha-5c7c56b.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21088,7 +21088,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.2+43.sha-1eae65e.with-local-changes');
+const VERSION = new Version('14.0.0-next.2+44.sha-5c7c56b.with-local-changes');
 
 /**
  * @license
