058032d83abaaf1600d6951d998e79bda9b3e8b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:02:05 +0000

    1eae65ead7 build: update .pullapprove.yml (#45061)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+40.sha-80585f4.with-local-changes
+ * @license Angular v14.0.0-next.2+43.sha-1eae65e.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21088,7 +21088,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.2+40.sha-80585f4.with-local-changes');
+const VERSION = new Version('14.0.0-next.2+43.sha-1eae65e.with-local-changes');
 
 /**
  * @license
