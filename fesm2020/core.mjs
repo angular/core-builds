89eba34634b1e87afc6a79f954d0f8ffe06b3b34
Andrew Scott <atscott01@gmail.com>
2022-02-22 18:37:25 +0000

    c02e4d72bc ci: update SHA for components-repo-unit-tests job to fix flakiness (#45166)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.3+9.sha-4d2ef0f.with-local-changes
+ * @license Angular v13.2.3+10.sha-c02e4d7.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21102,7 +21102,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.3+9.sha-4d2ef0f.with-local-changes');
+const VERSION = new Version('13.2.3+10.sha-c02e4d7.with-local-changes');
 
 /**
  * @license
