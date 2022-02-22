398f88f558dec53c40372f2ebedd49e104b6a7ce
Andrew Scott <atscott01@gmail.com>
2022-02-22 21:04:42 +0000

    bd168fc0c3 build: update ossf/scorecard-action commit hash to 3662744 (#45162)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+14.sha-c0778b4.with-local-changes
+ * @license Angular v14.0.0-next.3+16.sha-bd168fc.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21186,7 +21186,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.3+14.sha-c0778b4.with-local-changes');
+const VERSION = new Version('14.0.0-next.3+16.sha-bd168fc.with-local-changes');
 
 /**
  * @license
