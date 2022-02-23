14d5c43a01587a4e154a35ea6e7a303bb5f90896
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:15:03 +0000

    d65706a3b2 feat(zone.js): update electron patch to support electron/remote 14 (#45073)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+20.sha-f8dc660.with-local-changes
+ * @license Angular v14.0.0-next.3+19.sha-d65706a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21186,7 +21186,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.3+20.sha-f8dc660.with-local-changes');
+const VERSION = new Version('14.0.0-next.3+19.sha-d65706a.with-local-changes');
 
 /**
  * @license
