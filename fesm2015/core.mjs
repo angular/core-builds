b7a8ba960a7e566017b3e9b16a91661fe213e50a
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 19:32:04 +0000

    b39db80f91 test(animations): add route animation delay to pages tests (#45144)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+6.sha-3b86dda.with-local-changes
+ * @license Angular v13.2.4+7.sha-b39db80.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21088,7 +21088,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.4+6.sha-3b86dda.with-local-changes');
+const VERSION = new Version('13.2.4+7.sha-b39db80.with-local-changes');
 
 /**
  * @license
