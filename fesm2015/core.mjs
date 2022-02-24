c023ed4d29a6bc0d0b94c5b770ea30515c63a88b
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 17:29:00 +0000

    8eb82884bb build: update github/codeql-action action to v1.1.3 (#45188)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+2.sha-bad5096.with-local-changes
+ * @license Angular v14.0.0-next.4+3.sha-8eb8288.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21172,7 +21172,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.4+2.sha-bad5096.with-local-changes');
+const VERSION = new Version('14.0.0-next.4+3.sha-8eb8288.with-local-changes');
 
 /**
  * @license
