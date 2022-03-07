01e6daa69797192db827d908377eb5282aeffe93
Andrew Kushnir <akushnir@google.com>
2022-03-07 23:58:30 +0000

    0b31c8e2da docs: update `flush` description to include microtasks (#45237)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.5+21.sha-a659d15
+ * @license Angular v14.0.0-next.5+20.sha-0b31c8e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21266,7 +21266,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.5+21.sha-a659d15');
+const VERSION = new Version('14.0.0-next.5+20.sha-0b31c8e');
 
 /**
  * @license
