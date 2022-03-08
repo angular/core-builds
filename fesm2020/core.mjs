4d2679083ad394d4af29866ec7c0512767783189
Andrew Kushnir <akushnir@google.com>
2022-03-08 18:44:47 +0000

    9e56e400c5 fix(docs-infra): remove anchor tags from heritage docs (#45287)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.5+23.sha-2b7553d
+ * @license Angular v14.0.0-next.5+26.sha-9e56e40
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21286,7 +21286,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.5+23.sha-2b7553d');
+const VERSION = new Version('14.0.0-next.5+26.sha-9e56e40');
 
 /**
  * @license
