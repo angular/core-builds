d2fa0bc656112f75d9f3803661e2f3fea388a867
Andrew Kushnir <akushnir@google.com>
2022-03-08 20:44:01 +0000

    6eaaefd22e feat(core): drop support for Node.js 12 (#45286)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.5+31.sha-6cec8aa
+ * @license Angular v14.0.0-next.5+30.sha-6eaaefd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21286,7 +21286,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.5+31.sha-6cec8aa');
+const VERSION = new Version('14.0.0-next.5+30.sha-6eaaefd');
 
 /**
  * @license
