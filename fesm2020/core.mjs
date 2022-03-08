a851c1da2f23dc3d1521cd2dfd1130dcce536c7e
Andrew Kushnir <akushnir@google.com>
2022-03-08 18:52:13 +0000

    f58b8a94d4 build: update all non-major dependencies (#45250)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.5+28.sha-f25c8be
+ * @license Angular v14.0.0-next.5+29.sha-f58b8a9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21286,7 +21286,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.5+28.sha-f25c8be');
+const VERSION = new Version('14.0.0-next.5+29.sha-f58b8a9');
 
 /**
  * @license
