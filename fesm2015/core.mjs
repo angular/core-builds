e9ba25115564be6203cedb5cca7840e3fdb8ec8b
Andrew Scott <atscott01@gmail.com>
2022-03-09 22:09:58 +0000

    2e2c80acd5 docs: update <div> to <p> in lifecycle-hooks.md (#45063)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.6+1.sha-89d71b5
+ * @license Angular v14.0.0-next.6+4.sha-2e2c80a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21272,7 +21272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.6+1.sha-89d71b5');
+const VERSION = new Version('14.0.0-next.6+4.sha-2e2c80a');
 
 /**
  * @license
