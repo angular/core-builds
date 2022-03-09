1a7369fdf7b6f2d288f520c6bd44396256759e88
Andrew Scott <atscott01@gmail.com>
2022-03-09 22:08:49 +0000

    89d71b545e docs: update CHANGELOG to fix broken markdown (#45305)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.5+37.sha-51b67eb
+ * @license Angular v14.0.0-next.6+1.sha-89d71b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21286,7 +21286,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.5+37.sha-51b67eb');
+const VERSION = new Version('14.0.0-next.6+1.sha-89d71b5');
 
 /**
  * @license
