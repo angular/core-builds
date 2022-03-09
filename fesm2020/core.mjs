0b9f6fadf9a23312746eba6b5905a42d6e539290
GitHub <noreply@github.com>
2022-03-09 17:13:36 +0000

    6f1c941dfe release: cut the v13.2.6 release (#45299)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.5+23.sha-9bce9ce
+ * @license Angular v13.2.5+24.sha-6f1c941
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21102,7 +21102,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.5+23.sha-9bce9ce');
+const VERSION = new Version('13.2.5+24.sha-6f1c941');
 
 /**
  * @license
