89dfd91d174f2545d0a93e7001d87265dfb76d1d
Andrew Scott <atscott01@gmail.com>
2022-03-14 16:42:03 +0000

    0dbd50bea5 build: update dependency eslint-plugin-jsdoc to v38 (#45332)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.6+11.sha-7f043c9
+ * @license Angular v14.0.0-next.6+12.sha-0dbd50b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21272,7 +21272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.6+11.sha-7f043c9');
+const VERSION = new Version('14.0.0-next.6+12.sha-0dbd50b');
 
 /**
  * @license
