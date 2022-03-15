7edc1cd1fbbdc381b802dd30064a920ab3cad030
Andrew Scott <atscott01@gmail.com>
2022-03-15 20:44:39 +0000

    7ff4c0bf39 fix(forms): improve error message for invalid value accessors (#45192)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.0-rc.0+5.sha-bb9b330
+ * @license Angular v13.3.0-rc.0+7.sha-7ff4c0b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21088,7 +21088,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.0-rc.0+5.sha-bb9b330');
+const VERSION = new Version('13.3.0-rc.0+7.sha-7ff4c0b');
 
 /**
  * @license
