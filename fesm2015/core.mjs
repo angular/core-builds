ba8c3e6ca427862b94adf8b0371f6ce35ee17dc0
Andrew Scott <atscott01@gmail.com>
2022-03-15 23:46:21 +0000

    e47ba49b5f docs: add ngx-gantt to "EXPLORE ANGULAR RESOURCES" page (#45351)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.6+17.sha-9db9091
+ * @license Angular v14.0.0-next.6+18.sha-e47ba49
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21272,7 +21272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.6+17.sha-9db9091');
+const VERSION = new Version('14.0.0-next.6+18.sha-e47ba49');
 
 /**
  * @license
