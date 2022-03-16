429ae263a24f77ac709ebdd834af6ee2374fdbd8
Andrew Scott <atscott01@gmail.com>
2022-03-16 20:12:17 +0000

    3700323e1c docs: changelog for v13.3.0 release (#45367)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.0-rc.0+9.sha-f00f8c4
+ * @license Angular v13.3.0+2.sha-3700323
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21088,7 +21088,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.0-rc.0+9.sha-f00f8c4');
+const VERSION = new Version('13.3.0+2.sha-3700323');
 
 /**
  * @license
