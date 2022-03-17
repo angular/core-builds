27bbaaa0a608ea75a522408bcff7f73dea1ffcf6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-17 21:21:14 +0000

    e5300d6d75 build(docs-infra): move to circleci v2 api for aio-builds (#45349)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.0+2.sha-3700323
+ * @license Angular v13.3.0+3.sha-e5300d6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21088,7 +21088,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.0+2.sha-3700323');
+const VERSION = new Version('13.3.0+3.sha-e5300d6');
 
 /**
  * @license
