c2e33c6fcad1490dd8a0950f61226156b4fbf70f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 21:59:01 +0000

    c26915c8cf refactor(core): Make the typed forms migration apply to all usages of the symbols. (#45311)

--- schematics/migrations/typed-forms/util.d.ts
+++ schematics/migrations/typed-forms/util.d.ts
@@ -8,10 +8,11 @@
 /// <amd-module name="@angular/core/schematics/migrations/typed-forms/util" />
 import ts from 'typescript';
 export declare const classes: Set<string>;
+export declare const formControl = "FormControl";
 export declare const untypedPrefix = "Untyped";
 export declare const forms = "@angular/forms";
 export interface MigratableNode {
-    node: ts.Expression;
+    node: ts.Node;
     importName: string;
 }
 export declare type rewriteFn = (startPos: number, origLength: number, text: string) => void;
