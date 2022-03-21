4a211361ad41430e5db7db4d5befe6ceec542656
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 22:07:34 +0000

    aec9abd374 fix(docs-infra): don't wrap subcommands in code element (#45343)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.7+6.sha-c26915c
+ * @license Angular v14.0.0-next.7+7.sha-aec9abd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21286,7 +21286,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.7+6.sha-c26915c');
+const VERSION = new Version('14.0.0-next.7+7.sha-aec9abd');
 
 /**
  * @license
