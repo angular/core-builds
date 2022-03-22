f370517883d07c6e196d16f431b96ed677c3edfb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-22 00:13:33 +0000

    e80df0d149 build: update to bazel v5 for new runfiles API used in dev-infra (#45407)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.7+8.sha-fffa023
+ * @license Angular v14.0.0-next.7+9.sha-e80df0d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21286,7 +21286,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.7+8.sha-fffa023');
+const VERSION = new Version('14.0.0-next.7+9.sha-e80df0d');
 
 /**
  * @license
