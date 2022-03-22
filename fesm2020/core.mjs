73bf626cb783d58a0fa2005fc342ba5db2cb3384
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-22 00:14:33 +0000

    b716a81e30 docs: replace `defaultCollection` with `schematicCollections` (#45406)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.7+11.sha-15dbb4d
+ * @license Angular v14.0.0-next.7+10.sha-b716a81
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21286,7 +21286,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.7+11.sha-15dbb4d');
+const VERSION = new Version('14.0.0-next.7+10.sha-b716a81');
 
 /**
  * @license
