1ee4890d7f742ae7b61d96334f64803bcca8637d
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 21:47:39 +0000

    dc0e39a767 docs: update for 2022 angular events (#45386)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.7+35.sha-4ddcf81
+ * @license Angular v14.0.0-next.7+36.sha-dc0e39a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21234,7 +21234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.7+35.sha-4ddcf81');
+const VERSION = new Version('14.0.0-next.7+36.sha-dc0e39a');
 
 /**
  * @license
