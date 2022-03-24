a3352cb8b1c5e158d4111a47c362ab4f615ed451
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:14:06 +0000

    f671dd5815 fix(docs-infra): update (and unpin) dependency versions for preview server (#45390)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.7+26.sha-dc72f30
+ * @license Angular v14.0.0-next.7+29.sha-f671dd5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21340,7 +21340,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.7+26.sha-dc72f30');
+const VERSION = new Version('14.0.0-next.7+29.sha-f671dd5');
 
 /**
  * @license
