2454cf28e00d48a312dded248604d6bfc0dafd1f
Dylan Hunn <dylhunn@gmail.com>
2022-03-28 16:42:01 +0000

    c996b477a9 build: replace bazel devserver with shared dev-infra implementation (#45452)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.9+5.sha-d36fa11
+ * @license Angular v14.0.0-next.9+7.sha-c996b47
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21250,7 +21250,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.9+5.sha-d36fa11');
+const VERSION = new Version('14.0.0-next.9+7.sha-c996b47');
 
 /**
  * @license
