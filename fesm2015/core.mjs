4fa77bd73219a4ed589b636e7aac143b5ca519ee
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 00:28:06 +0000

    82956dcbd3 Update aio/content/marketing/resources.json (#45076)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.9+35.sha-7d94923
+ * @license Angular v14.0.0-next.9+37.sha-82956dc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21238,7 +21238,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.9+35.sha-7d94923');
+const VERSION = new Version('14.0.0-next.9+37.sha-82956dc');
 
 /**
  * @license
