5e8da1c96ef9a6df89371c6455ea1d352d8b5d7f
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:59:06 +0000

    b5f39c5da1 docs(devtools): updating firefox documentation (#44952)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.11+34.sha-222d866
+ * @license Angular v14.0.0-next.11+45.sha-b5f39c5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21287,7 +21287,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.11+34.sha-222d866');
+const VERSION = new Version('14.0.0-next.11+45.sha-b5f39c5');
 
 /**
  * @license
