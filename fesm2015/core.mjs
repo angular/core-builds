58e15ed3d4653b4d989c3dcccfb70e954686bfec
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 19:56:24 +0000

    c7bf75dd5e fix(platform-browser): remove obsolete shim for Map comparison in Jasmine (#45521)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.11+45.sha-b5f39c5
+ * @license Angular v14.0.0-next.11+46.sha-c7bf75d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21273,7 +21273,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.11+45.sha-b5f39c5');
+const VERSION = new Version('14.0.0-next.11+46.sha-c7bf75d');
 
 /**
  * @license
