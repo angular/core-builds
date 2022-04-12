bd190a0a020e5b748188e0d1f41b427c738447e0
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:44:35 +0000

    ee3cfc8815 docs: fix path typo (#45604) PR Close #45604

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.12+1.sha-79de026
+ * @license Angular v14.0.0-next.12+3.sha-ee3cfc8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21287,7 +21287,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.12+1.sha-79de026');
+const VERSION = new Version('14.0.0-next.12+3.sha-ee3cfc8');
 
 /**
  * @license
