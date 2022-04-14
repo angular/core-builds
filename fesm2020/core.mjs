344bce299f5e3c0fb98968332c292b975d2c1023
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 23:46:51 +0000

    b5b0a50294 docs: fix typo (#45637)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.13+8.sha-7d2a619
+ * @license Angular v14.0.0-next.13+9.sha-b5b0a50
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21298,7 +21298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.13+8.sha-7d2a619');
+const VERSION = new Version('14.0.0-next.13+9.sha-b5b0a50');
 
 /**
  * @license
