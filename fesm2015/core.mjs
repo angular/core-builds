2516a8a5b139af68da665d232aff837cba258a11
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:17:14 +0000

    94fba9b047 docs(core): fix typo in View Encapsulation code example (#45629)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.13+3.sha-f8a1ea0
+ * @license Angular v14.0.0-next.13+5.sha-94fba9b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21284,7 +21284,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.13+3.sha-f8a1ea0');
+const VERSION = new Version('14.0.0-next.13+5.sha-94fba9b');
 
 /**
  * @license
