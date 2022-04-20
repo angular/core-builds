a2f0520a3af3c4c122780ca5538906a39596d6d7
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 18:50:18 +0000

    4e0957a4e1 feat(router): Add ability to specify providers on a Route (#45673)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.13+59.sha-ba45428
+ * @license Angular v14.0.0-next.13+60.sha-4e0957a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21407,7 +21407,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.13+59.sha-ba45428');
+const VERSION = new Version('14.0.0-next.13+60.sha-4e0957a');
 
 /**
  * @license
