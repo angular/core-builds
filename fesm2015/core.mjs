8caf29d2db3e08505dcfddfdb7df63dd4a4eba7e
Andrew Scott <atscott01@gmail.com>
2022-04-26 17:26:13 +0000

    4e0784c2f0 refactor(compiler-cli): optimize InjectorDef imports generation (#45701)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.14+23.sha-7992132
+ * @license Angular v14.0.0-next.14+27.sha-4e0784c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21495,7 +21495,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.14+23.sha-7992132');
+const VERSION = new Version('14.0.0-next.14+27.sha-4e0784c');
 
 /**
  * @license
