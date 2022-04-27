de3c2caa62b484e29238fab0d9cf20d57a310b3a
Andrew Scott <atscott01@gmail.com>
2022-04-27 16:26:09 +0000

    2eb39c0878 fix(devtools): inline loading svg (#45762)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.14+37.sha-882f595
+ * @license Angular v14.0.0-next.14+41.sha-2eb39c0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21509,7 +21509,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.14+37.sha-882f595');
+const VERSION = new Version('14.0.0-next.14+41.sha-2eb39c0');
 
 /**
  * @license
