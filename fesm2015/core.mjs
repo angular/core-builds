fbd81013bea27aa736cbe36944378ee03505057e
Andrew Scott <atscott01@gmail.com>
2022-04-27 17:48:58 +0000

    4d6a1d6722 fix(compiler): scope css keyframes in emulated view encapsulation (#42608)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.14+42.sha-3d45484
+ * @license Angular v14.0.0-next.14+43.sha-4d6a1d6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21509,7 +21509,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.14+42.sha-3d45484');
+const VERSION = new Version('14.0.0-next.14+43.sha-4d6a1d6');
 
 /**
  * @license
