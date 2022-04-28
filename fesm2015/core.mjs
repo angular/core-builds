70517e85535a5663fc3c97d08cfc49bfb4ed1f51
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 21:57:17 +0000

    d4fc12fa19 fix(router): Remove deprecated initialNavigation option (#45729)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-3a60063
+ * @license Angular v14.0.0-next.15+sha-d4fc12f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21509,7 +21509,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-3a60063');
+const VERSION = new Version('14.0.0-next.15+sha-d4fc12f');
 
 /**
  * @license
