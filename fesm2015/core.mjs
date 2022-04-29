ec577434d27a5869388dcf8fcc73954db759921f
Dylan Hunn <dylhunn@gmail.com>
2022-04-29 21:38:48 +0000

    a88bf2090e refactor(compiler-cli): increase minimum version of partial declarations (#45782)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-dff5586
+ * @license Angular v14.0.0-next.15+sha-a88bf20
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21509,7 +21509,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-dff5586');
+const VERSION = new Version('14.0.0-next.15+sha-a88bf20');
 
 /**
  * @license
