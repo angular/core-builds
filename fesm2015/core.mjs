1a9d26894c4839a805482eb5554025b584f46e40
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 18:59:51 +0000

    26c7b5ec0c docs(docs-infra): improve clarity of toh-pt2 click code snippet (#45829)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.5+2.sha-ab011b7
+ * @license Angular v13.3.5+3.sha-26c7b5e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21128,7 +21128,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.5+2.sha-ab011b7');
+const VERSION = new Version('13.3.5+3.sha-26c7b5e');
 
 /**
  * @license
