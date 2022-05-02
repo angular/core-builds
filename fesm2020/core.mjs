a98524d77dde848fadc66fc601d896889e5a3a67
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 22:54:17 +0000

    a521571e85 docs(devtools): create documentation on installing Angular DevTools in Safari (#45763)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-509cab9
+ * @license Angular v14.0.0-next.15+sha-a521571
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21543,7 +21543,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-509cab9');
+const VERSION = new Version('14.0.0-next.15+sha-a521571');
 
 /**
  * @license
