fc766ba914729b7ebdf6b4ec5d05e00a19e10cc7
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 23:49:54 +0000

    9e6538c4ba fix(dev-infra): Fix pullapprove by commenting out the empty availability (#45846)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-d322052
+ * @license Angular v14.0.0-next.15+sha-9e6538c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21567,7 +21567,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-d322052');
+const VERSION = new Version('14.0.0-next.15+sha-9e6538c');
 
 /**
  * @license
