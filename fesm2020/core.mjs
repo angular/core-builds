8a978f6a24c2e989faebeaef2ad7a3b23c1bc295
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:30:31 +0000

    e79260e445 release: cut the v14.0.0-next.16 release (#45875)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-c988487
+ * @license Angular v14.0.0-next.16+sha-e79260e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21596,7 +21596,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-c988487');
+const VERSION = new Version('14.0.0-next.16+sha-e79260e');
 
 /**
  * @license
