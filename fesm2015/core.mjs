c071f5f3df2c309dc653379bbcfcafde9147fae1
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:01:18 +0000

    ea2b3a9dfa docs: update angular package format guide to reflect recent changes (#45725)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.15+sha-a2d5358
+ * @license Angular v14.0.0-next.15+sha-ea2b3a9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21582,7 +21582,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.15+sha-a2d5358');
+const VERSION = new Version('14.0.0-next.15+sha-ea2b3a9');
 
 /**
  * @license
