d1994058504b37505dd037432e4d1478bdd0e0a3
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:54:15 +0000

    8786c609f8 refactor(docs-infra): Redirect old document title guide to new router feature (#45857)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.16+sha-967e934
+ * @license Angular v14.0.0-next.16+sha-8786c60
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21596,7 +21596,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.16+sha-967e934');
+const VERSION = new Version('14.0.0-next.16+sha-8786c60');
 
 /**
  * @license
