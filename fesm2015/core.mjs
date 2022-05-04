80f8c887b3f8c4a64097f0dae92f671a16a010d8
Joey Perrott <josephperrott.github@gmail.com>
2022-05-04 22:35:28 +0000

    ecae55d279 release: bump the next branch to v14.1.0-next.0

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.16+sha-e79260e
+ * @license Angular v14.1.0-next.0+sha-ecae55d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21582,7 +21582,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.16+sha-e79260e');
+const VERSION = new Version('14.1.0-next.0+sha-ecae55d');
 
 /**
  * @license
