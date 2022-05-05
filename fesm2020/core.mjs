b68aba22b5c04f01f2a9e6eff943501be8175998
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:06:54 +0000

    58a8bd3df2 build(docs-infra): remove unused Docker artifacts when updating the preview server (#45895)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.16+sha-f6543c2
+ * @license Angular v14.0.0-next.16+sha-58a8bd3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21596,7 +21596,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.16+sha-f6543c2');
+const VERSION = new Version('14.0.0-next.16+sha-58a8bd3');
 
 /**
  * @license
