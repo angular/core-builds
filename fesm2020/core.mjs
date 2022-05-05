037b2acc1dc0db052f39a5062cf0bf37157244b1
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:47:30 +0000

    a212fb4dde docs: update the Angular DevTools auto-generated READMEs (#45884)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-1368390
+ * @license Angular v14.1.0-next.0+sha-a212fb4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21596,7 +21596,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-1368390');
+const VERSION = new Version('14.1.0-next.0+sha-a212fb4');
 
 /**
  * @license
