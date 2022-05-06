ccac6d0d4d46f507e9d24a0adb870d4301b7da25
Andrew Kushnir <akushnir@google.com>
2022-05-06 00:16:15 +0000

    3f3812e20a docs(docs-infra): improve clarity of toh-pt2 li>button code (#45858)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-1a233c2
+ * @license Angular v14.1.0-next.0+sha-3f3812e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21582,7 +21582,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-1a233c2');
+const VERSION = new Version('14.1.0-next.0+sha-3f3812e');
 
 /**
  * @license
