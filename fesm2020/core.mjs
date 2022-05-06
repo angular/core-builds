4e36774aa30209f30ce7f33a9555bffc8410dbfb
Andrew Kushnir <akushnir@google.com>
2022-05-06 00:15:11 +0000

    1a233c2c55 docs: update the Angular DevTools supported version in docs and README (#45902)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-0ffb19a
+ * @license Angular v14.1.0-next.0+sha-1a233c2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21596,7 +21596,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-0ffb19a');
+const VERSION = new Version('14.1.0-next.0+sha-1a233c2');
 
 /**
  * @license
