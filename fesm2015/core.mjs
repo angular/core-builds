3f2c09baea8a83d7d78d52cc12756fdb3f48efb5
Andrew Kushnir <akushnir@google.com>
2022-05-06 00:14:10 +0000

    0ffb19abed test(platform-browser): refactor flaky test (#45906)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-3e7d299
+ * @license Angular v14.1.0-next.0+sha-0ffb19a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21582,7 +21582,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-3e7d299');
+const VERSION = new Version('14.1.0-next.0+sha-0ffb19a');
 
 /**
  * @license
