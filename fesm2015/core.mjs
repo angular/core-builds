bf60d70fdd60eed7c3adcd4f8c8f7ff11ad2ec8a
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:26:41 +0000

    0f0d01b023 docs: remove unnecesarry backslash from pipes documentation (#45916)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-75b3d0f
+ * @license Angular v14.1.0-next.0+sha-0f0d01b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21594,7 +21594,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-75b3d0f');
+const VERSION = new Version('14.1.0-next.0+sha-0f0d01b');
 
 /**
  * @license
