257b9369674c2cfcb70591f3ad6ab533cce56c5c
Andrew Kushnir <akushnir@google.com>
2022-05-09 18:42:14 +0000

    b6156f41a6 docs(docs-infra): clarify toh-2 error message (#45878)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-e83775c
+ * @license Angular v14.1.0-next.0+sha-b6156f4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21582,7 +21582,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-e83775c');
+const VERSION = new Version('14.1.0-next.0+sha-b6156f4');
 
 /**
  * @license
