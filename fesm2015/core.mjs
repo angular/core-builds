c7a608e4facba283d3ca2855425321602a40c17e
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:12:11 +0000

    c4340970c7 fix(docs-infra): handle CircleCI API v2 responses in preview server (#45934)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-af253cc
+ * @license Angular v14.1.0-next.0+sha-c434097
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21582,7 +21582,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-af253cc');
+const VERSION = new Version('14.1.0-next.0+sha-c434097');
 
 /**
  * @license
