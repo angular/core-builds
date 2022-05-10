82ce86809d56a052db22fd61c2c283db3aca032d
Andrew Kushnir <akushnir@google.com>
2022-05-10 01:19:58 +0000

    43ba4ab9da fix(forms): Allow NonNullableFormBuilder to be injected. (#45904)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-216a966
+ * @license Angular v14.1.0-next.0+sha-43ba4ab
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21608,7 +21608,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-216a966');
+const VERSION = new Version('14.1.0-next.0+sha-43ba4ab');
 
 /**
  * @license
