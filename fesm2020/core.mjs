f0f3ec0e92ce7259a7297d21eaf565dd66b94528
Andrew Kushnir <akushnir@google.com>
2022-05-11 18:40:20 +0000

    6375fa7987 refactor(bazel): add support for starlark's unused_inputs_list (#45946)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-6238f78
+ * @license Angular v14.1.0-next.0+sha-6375fa7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21605,7 +21605,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-6238f78');
+const VERSION = new Version('14.1.0-next.0+sha-6375fa7');
 
 /**
  * @license
