68a0a014decc6b055757a81587f29764c3204f66
Andrew Kushnir <akushnir@google.com>
2022-05-11 18:33:26 +0000

    6238f7895f docs: Add a guide for typed forms. (#45939)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-b882d03
+ * @license Angular v14.1.0-next.0+sha-6238f78
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21605,7 +21605,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-b882d03');
+const VERSION = new Version('14.1.0-next.0+sha-6238f78');
 
 /**
  * @license
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-b882d03
+ * @license Angular v14.1.0-next.0+sha-6238f78
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
