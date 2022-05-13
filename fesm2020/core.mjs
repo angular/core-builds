124f58696d2f315a4eb03cd84fe8da4795ee762d
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 16:54:40 +0000

    4f309fed02 build: update to TypeScript 4.7 RC (#45966)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-e1a93e4
+ * @license Angular v14.1.0-next.0+sha-4f309fe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21605,7 +21605,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-e1a93e4');
+const VERSION = new Version('14.1.0-next.0+sha-4f309fe');
 
 /**
  * @license
