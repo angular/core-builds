b7c02eb2a0815673e85c08041dd4767534197e23
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 22:22:22 +0000

    19018ca61e docs: add selin tunc to contributors (#45989)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-rc.0+sha-bcef46d
+ * @license Angular v14.0.0-rc.0+sha-19018ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21594,7 +21594,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-rc.0+sha-bcef46d');
+const VERSION = new Version('14.0.0-rc.0+sha-19018ca');
 
 /**
  * @license
