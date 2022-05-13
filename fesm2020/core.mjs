c676f3fc31ebba3729f8b7e011d658382d9cdc73
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 20:24:56 +0000

    a4deac3fda docs: adds ngx-cookie-service library to resources list (#45955) PR Close #45955

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-4d2e006
+ * @license Angular v14.1.0-next.0+sha-a4deac3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21608,7 +21608,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-4d2e006');
+const VERSION = new Version('14.1.0-next.0+sha-a4deac3');
 
 /**
  * @license
