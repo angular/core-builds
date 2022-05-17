bec21d9376d5517ab9519f4dc0c06dc15fe0392d
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-17 18:05:12 +0000

    338de1b4cf docs: remove Narco from hero list in code, text, and images (#46008)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-rc.0+sha-79025a6
+ * @license Angular v14.0.0-rc.0+sha-338de1b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21595,7 +21595,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-rc.0+sha-79025a6');
+const VERSION = new Version('14.0.0-rc.0+sha-338de1b');
 
 /**
  * @license
