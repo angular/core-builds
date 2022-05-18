c0c5dbe146d481b5ed123fc616e5cdf6b32abd10
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-18 16:14:59 +0000

    e92799c97e test(router): Add tests for initialNavigation: enabledBlocking (#45733)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-1e07a11
+ * @license Angular v14.1.0-next.0+sha-e92799c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21609,7 +21609,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-1e07a11');
+const VERSION = new Version('14.1.0-next.0+sha-e92799c');
 
 /**
  * @license
