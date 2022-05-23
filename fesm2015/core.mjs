c5b3267c351ee79e4e8f10555fe1c3045162a07a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 17:11:24 +0000

    1bd0532e87 docs: Remove outdated note (#46022)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-23a9990
+ * @license Angular v14.1.0-next.0+sha-1bd0532
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21617,7 +21617,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-23a9990');
+const VERSION = new Version('14.1.0-next.0+sha-1bd0532');
 
 /**
  * @license
