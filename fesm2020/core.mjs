4ee3f3277aa167e001fbf714fddf280000dbbdfa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 16:32:41 +0000

    ae18f00023 docs: add guide to standalone components preview (#46058)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-76f5a55
+ * @license Angular v14.1.0-next.0+sha-ae18f00
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21609,7 +21609,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-76f5a55');
+const VERSION = new Version('14.1.0-next.0+sha-ae18f00');
 
 /**
  * @license
