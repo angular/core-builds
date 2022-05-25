f9a68d69be741a9bab359ec23ae2567294c7172f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-25 18:42:46 +0000

    a12372a66d docs: update the Angular roadmap as of May 2022 (#46117)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.9+17.sha-4a7e724
+ * @license Angular v13.3.9+18.sha-a12372a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21154,7 +21154,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.9+17.sha-4a7e724');
+const VERSION = new Version('13.3.9+18.sha-a12372a');
 
 /**
  * @license
