98b11f8d05a11bb5db77c47d2015161e9604f9f7
Andrew Scott <atscott01@gmail.com>
2022-05-27 16:47:25 +0000

    2863dff7fa docs: change wrong alert to callout (#46145)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-bb16b19
+ * @license Angular v14.1.0-next.0+sha-2863dff
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21675,7 +21675,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-bb16b19');
+const VERSION = new Version('14.1.0-next.0+sha-2863dff');
 
 /**
  * @license
