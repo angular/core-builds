99030a6c453593e4ce3fa8f8099aa61fdbdc2ed7
Andrew Scott <atscott01@gmail.com>
2022-05-27 01:01:08 +0000

    bb16b1919a docs: fix incorrect title in the roadmap (#46140)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-6a4353c
+ * @license Angular v14.1.0-next.0+sha-bb16b19
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21675,7 +21675,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-6a4353c');
+const VERSION = new Version('14.1.0-next.0+sha-bb16b19');
 
 /**
  * @license
