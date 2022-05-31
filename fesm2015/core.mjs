e99179353aebba37414ce1045fda85678da8989a
Andrew Scott <atscott01@gmail.com>
2022-05-31 19:24:16 +0000

    a623c4f622 test: drop no longer needed postinstall step from integration test apps (#46182)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-253ac15
+ * @license Angular v14.1.0-next.0+sha-a623c4f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21700,7 +21700,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-253ac15');
+const VERSION = new Version('14.1.0-next.0+sha-a623c4f');
 
 /**
  * @license
