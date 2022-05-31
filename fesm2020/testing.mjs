bd6636e6d0d98091aecd48b2a1d00bb70422b20d
Andrew Scott <atscott01@gmail.com>
2022-05-31 19:18:00 +0000

    efed24eb70 docs: rename stage to staging (#46164)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-3af2cd5
+ * @license Angular v14.1.0-next.0+sha-efed24e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22013,7 +22013,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-3af2cd5');
+const VERSION = new Version('14.1.0-next.0+sha-efed24e');
 
 /**
  * @license
