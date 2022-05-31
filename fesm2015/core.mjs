b41d5d7f04629ce58ba08c66f9fd9a25fa98317f
Andrew Scott <atscott01@gmail.com>
2022-05-31 19:47:33 +0000

    d74a6ba3d6 docs: remove router title docs from 13.3.x (#46198)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.10+8.sha-00e54e3
+ * @license Angular v13.3.10+9.sha-d74a6ba
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21140,7 +21140,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.10+8.sha-00e54e3');
+const VERSION = new Version('13.3.10+9.sha-d74a6ba');
 
 /**
  * @license
