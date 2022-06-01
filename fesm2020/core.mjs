3181bda061f3de61a7a6f9714b5373b8e1e77b5c
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:21:07 +0000

    f22f451310 docs: replace double dashes with m-dashes, fix typos (#46174)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-0f89753
+ * @license Angular v14.1.0-next.0+sha-f22f451
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21755,7 +21755,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-0f89753');
+const VERSION = new Version('14.1.0-next.0+sha-f22f451');
 
 /**
  * @license
