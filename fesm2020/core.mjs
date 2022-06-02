0c5ad8983fe41878eaef010b47529ffb405c716e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 17:22:41 +0000

    7985f492cd docs: release notes for the v14.0.0 release (#46224)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-0259035
+ * @license Angular v14.1.0-next.0+sha-7985f49
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21787,7 +21787,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-0259035');
+const VERSION = new Version('14.1.0-next.0+sha-7985f49');
 
 /**
  * @license
