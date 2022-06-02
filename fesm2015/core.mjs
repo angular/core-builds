2cfca0b84aed5f9644763da464aaa9a0af307049
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-06-02 23:51:39 +0000

    7b9569dae2 feat(common): add built-in Imgix loader

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-9e3369c
+ * @license Angular v14.1.0-next.0+sha-7b9569d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21627,7 +21627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-9e3369c');
+const VERSION = new Version('14.1.0-next.0+sha-7b9569d');
 
 /**
  * @license
