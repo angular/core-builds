d02f8d9b2aa5e9c1eacde0d3e613935926e59faf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 20:56:28 +0000

    a006edefd1 refactor(compiler-cli): introduce `onlyPublishPublicTypingsForNgModules` (#45894)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-2444f36
+ * @license Angular v14.1.0-next.0+sha-a006ede
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22087,7 +22087,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-2444f36');
+const VERSION = new Version('14.1.0-next.0+sha-a006ede');
 
 /**
  * @license
