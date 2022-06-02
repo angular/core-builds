6e74f1afc6acf22b808623a0f6238cc1a915dc45
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 19:53:26 +0000

    2444f36cbf fix(docs-infra): correct developer preview link (#46225)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-917f421
+ * @license Angular v14.1.0-next.0+sha-2444f36
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22089,7 +22089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-917f421');
+const VERSION = new Version('14.1.0-next.0+sha-2444f36');
 
 /**
  * @license
