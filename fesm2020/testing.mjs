b59fe8e0fdf61e673ee96a9ddc7bee597de7412d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-03 17:26:48 +0000

    ee98f4eaf8 docs: update outdated link title for structural directives guide (#46228)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-243c679
+ * @license Angular v14.1.0-next.0+sha-ee98f4e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22076,7 +22076,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-243c679');
+const VERSION = new Version('14.1.0-next.0+sha-ee98f4e');
 
 /**
  * @license
