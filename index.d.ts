eb11f50ff7b8a48438f6d9f870df2f82604f3c6c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-03 17:25:28 +0000

    243c6797f4 test(router): illustrate canceling of intermediate results in canLoad (#46231)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-07b4084
+ * @license Angular v14.1.0-next.0+sha-243c679
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10240,7 +10240,7 @@ export declare class ɵLContext {
  * renderComponent(AppComponent, {hostFeatures: [LifecycleHooksFeature]});
  * ```
  */
-export declare function ɵLifecycleHooksFeature(component: any, def: ɵComponentDef<any>): void;
+export declare function ɵLifecycleHooksFeature(): void;
 
 /**
  * Index of each type of locale data from the locale data array
