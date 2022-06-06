0a24cc5230d58a29076be729bd878589da521e4e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 19:05:43 +0000

    a9e8a212f8 docs: mark version 13 as LTS in releases guide (#46268)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-c083f6d
+ * @license Angular v14.1.0-next.0+sha-a9e8a21
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21772,7 +21772,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-c083f6d');
+const VERSION = new Version('14.1.0-next.0+sha-a9e8a21');
 
 /**
  * @license
