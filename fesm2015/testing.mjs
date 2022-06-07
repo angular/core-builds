b9c9850b04a7f7f540682ba0918cd3e1933cdca0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-07 17:35:16 +0000

    b280b97d5a refactor(common): include locale extra data in closure locale file (#46167)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-627cdf3
+ * @license Angular v14.1.0-next.0+sha-b280b97
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22087,7 +22087,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-627cdf3');
+const VERSION = new Version('14.1.0-next.0+sha-b280b97');
 
 /**
  * @license
