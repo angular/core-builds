78b20c8277020938624555e71fca4000bba858fb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-08 19:58:58 +0000

    7391143dd3 docs: remove azure builder reference (#46285)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-34890fb
+ * @license Angular v14.1.0-next.0+sha-7391143
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22087,7 +22087,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-34890fb');
+const VERSION = new Version('14.1.0-next.0+sha-7391143');
 
 /**
  * @license
