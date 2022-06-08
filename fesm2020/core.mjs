904e904e9a0d6c76f5a764f9e00700e4a7921c93
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-06-08 23:07:31 +0000

    0201016bc7 fix(common): sanitize `rawSrc` and `rawSrcset` values in NgOptimizedImage directive

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-850d972
+ * @license Angular v14.1.0-next.0+sha-0201016
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21785,7 +21785,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-850d972');
+const VERSION = new Version('14.1.0-next.0+sha-0201016');
 
 /**
  * @license
