2e1cb95365df04a8ac0be0f5603eb6d02ce1d630
GitHub <noreply@github.com>
2022-06-08 09:35:16 +0000

    850d9723fa refactor(common): move LCP image observer to a separate file (#46295)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-7e8be93
+ * @license Angular v14.1.0-next.0+sha-850d972
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22085,7 +22085,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-7e8be93');
+const VERSION = new Version('14.1.0-next.0+sha-850d972');
 
 /**
  * @license
