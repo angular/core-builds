f1ff9ff26f85bb14f53fe43fde970553abd5ac07
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-06-09 09:54:32 +0000

    40ea24aa0b feat(common): add cloudflare loader

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-fbe005e
+ * @license Angular v14.1.0-next.0+sha-40ea24a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21772,7 +21772,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-fbe005e');
+const VERSION = new Version('14.1.0-next.0+sha-40ea24a');
 
 /**
  * @license
