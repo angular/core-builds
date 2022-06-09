bc7edfb7906c3bbe6268c8c87423e64d0d3f29d5
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-09 16:24:46 +0000

    ef5cba3df7 fix(common): allow null in ngComponentOutlet (#46280)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.1+sha-16c71fc
+ * @license Angular v14.0.1+sha-ef5cba3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21772,7 +21772,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.1+sha-16c71fc');
+const VERSION = new Version('14.0.1+sha-ef5cba3');
 
 /**
  * @license
