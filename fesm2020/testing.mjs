74c951178e2e7a51a3aab86602bb38ef8e4b549f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-06-10 20:38:19 +0000

    b00b52d9e8 feat(common): add warnings re: image distortion

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-68fd8cb
+ * @license Angular v14.1.0-next.0+sha-b00b52d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22085,7 +22085,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-68fd8cb');
+const VERSION = new Version('14.1.0-next.0+sha-b00b52d');
 
 /**
  * @license
