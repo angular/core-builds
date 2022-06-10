6774c8b64d2eab775e52e5b8e64fcd4310bdb2ec
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 21:42:18 +0000

    131d029da1 feat(compiler-cli): detect missing control flow directive imports in standalone components (#46146)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-f35f475
+ * @license Angular v14.1.0-next.0+sha-131d029
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21785,7 +21785,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-f35f475');
+const VERSION = new Version('14.1.0-next.0+sha-131d029');
 
 /**
  * @license
