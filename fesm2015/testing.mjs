97118aa80a454bb39b5c2d9f7c2a4c440077b713
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 16:27:09 +0000

    5e20f60efa fix(docs-infra): add missing aria-labels to contributors page (#46324)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-caa2f35
+ * @license Angular v14.1.0-next.0+sha-5e20f60
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22087,7 +22087,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-caa2f35');
+const VERSION = new Version('14.1.0-next.0+sha-5e20f60');
 
 /**
  * @license
