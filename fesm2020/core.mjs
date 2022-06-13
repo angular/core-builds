76ef07016f06892cfeb79b55839ba8be4ba34721
Andrew Kushnir <akushnir@google.com>
2022-06-13 22:19:00 +0000

    5af8e46501 refactor(common): update peer deps version of `@angular/core`

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.11+sha-9dd43f2
+ * @license Angular v13.3.11+sha-5af8e46
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21154,7 +21154,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.3.11+sha-9dd43f2');
+const VERSION = new Version('13.3.11+sha-5af8e46');
 
 /**
  * @license
