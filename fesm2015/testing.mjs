2944057d610c068c7f7eeea45474f91b3f8131e6
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 21:17:21 +0000

    9ab32616aa docs:  Adding a book "Angular from Scratch" (#46352)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-d6880c9
+ * @license Angular v14.1.0-next.0+sha-9ab3261
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22087,7 +22087,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-d6880c9');
+const VERSION = new Version('14.1.0-next.0+sha-9ab3261');
 
 /**
  * @license
