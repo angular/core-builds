e703584130bc5171755c2a5c5c3be53105f35a7a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-06-13 09:22:38 +0000

    374f345ce8 refactor(common): add missing format error call in NgOptimizedImage

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-bf8bc1e
+ * @license Angular v14.1.0-next.0+sha-374f345
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21785,7 +21785,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-bf8bc1e');
+const VERSION = new Version('14.1.0-next.0+sha-374f345');
 
 /**
  * @license
