b23168d82083170e9dbc42f84a337103f40ee2e9
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-14 16:17:18 +0000

    86303f73c9 docs(core): fix broken link <BLANK LINE> Dead link is updated with new, working, and updated link. (#46359) PR Close #46359

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-714183d
+ * @license Angular v14.1.0-next.0+sha-86303f7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21772,7 +21772,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-714183d');
+const VERSION = new Version('14.1.0-next.0+sha-86303f7');
 
 /**
  * @license
