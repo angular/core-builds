f7c5f44c6ecdd4882c43350cdc61bded14d7b0da
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-14 20:14:54 +0000

    9ce733aacb docs: fix code style (#45527)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-1120d54
+ * @license Angular v14.1.0-next.0+sha-9ce733a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22085,7 +22085,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-1120d54');
+const VERSION = new Version('14.1.0-next.0+sha-9ce733a');
 
 /**
  * @license
