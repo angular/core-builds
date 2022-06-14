9fcf17d1520277609a23a1a36f748a81e4b87fe8
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-14 17:23:46 +0000

    1278d2af6a docs: fix multicast observable example output (#46127)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-2c8fd2b
+ * @license Angular v14.1.0-next.0+sha-1278d2a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22087,7 +22087,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-2c8fd2b');
+const VERSION = new Version('14.1.0-next.0+sha-1278d2a');
 
 /**
  * @license
