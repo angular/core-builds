5b439fc809a398ad0fcf5602865fc5304e75b1e0
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-15 16:16:48 +0000

    7a376466b3 feat(docs-infra): show dark scrollbars in dark mode (#46363)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.0+sha-736b545
+ * @license Angular v14.1.0-next.0+sha-7a37646
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22087,7 +22087,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.0+sha-736b545');
+const VERSION = new Version('14.1.0-next.0+sha-7a37646');
 
 /**
  * @license
