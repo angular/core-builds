1754ff1dcf7d0fdd68c96f4947b6c52a948d91b2
Andrew Kushnir <akushnir@google.com>
2022-06-15 21:23:40 +0000

    d1e5ef51b6 docs: fix link to unpkged locales (#46379)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-2509d76
+ * @license Angular v14.1.0-next.1+sha-d1e5ef5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22085,7 +22085,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-2509d76');
+const VERSION = new Version('14.1.0-next.1+sha-d1e5ef5');
 
 /**
  * @license
