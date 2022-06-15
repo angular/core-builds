4e137e257ef4bff5b80e1bf308e720cc9e04e857
Andrew Kushnir <akushnir@google.com>
2022-06-15 21:23:18 +0000

    2509d7681b docs: Add missing line in *zone-flags.ts* file for event-binding wiki. (#46369)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-463feb6
+ * @license Angular v14.1.0-next.1+sha-2509d76
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21785,7 +21785,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-463feb6');
+const VERSION = new Version('14.1.0-next.1+sha-2509d76');
 
 /**
  * @license
