ed0501908874e6df474d364837ba83a69f995fd3
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:13:13 +0000

    3d8787c961 fix(core): handle NgModules with standalone pipes in TestBed correctly (#46407)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-08d3db2
+ * @license Angular v14.1.0-next.1+sha-3d8787c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22075,7 +22075,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-08d3db2');
+const VERSION = new Version('14.1.0-next.1+sha-3d8787c');
 
 /**
  * @license
