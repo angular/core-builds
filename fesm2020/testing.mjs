118134b91b8b0ad0b2c12071c9b6cdace3045121
Andrew Kushnir <akushnir@google.com>
2022-06-21 21:39:39 +0000

    7a084c8134 docs: add dany paredes to GDE resources (#46411)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.2+sha-c086653
+ * @license Angular v14.0.2+sha-7a084c8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22073,7 +22073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.2+sha-c086653');
+const VERSION = new Version('14.0.2+sha-7a084c8');
 
 /**
  * @license
