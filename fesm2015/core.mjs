c07f1d204d5f78d418bfc485c1bc4e4de59d710b
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:13:00 +0000

    08d3db232c fix(platform-server): invalid style attribute being generated for null values (#46433)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-96c7074
+ * @license Angular v14.1.0-next.1+sha-08d3db2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21760,7 +21760,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-96c7074');
+const VERSION = new Version('14.1.0-next.1+sha-08d3db2');
 
 /**
  * @license
