3208af346fa3dbb94cb086513a11a3f24237f135
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:06:59 +0000

    0c9af71f80 build: update scorecard action dependencies (#46440) PR Close #46440

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-ffedcde
+ * @license Angular v14.1.0-next.1+sha-0c9af71
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22073,7 +22073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-ffedcde');
+const VERSION = new Version('14.1.0-next.1+sha-0c9af71');
 
 /**
  * @license
