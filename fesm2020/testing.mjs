cd5e3e6bdf23ce24bf9715cce081c4dede3d1adf
Andrew Kushnir <akushnir@google.com>
2022-06-22 20:44:05 +0000

    abe3759e24 fix(router): allow to return `UrlTree` from `CanMatchFn` (#46455)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-cb57cdb
+ * @license Angular v14.1.0-next.1+sha-abe3759
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22073,7 +22073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-cb57cdb');
+const VERSION = new Version('14.1.0-next.1+sha-abe3759');
 
 /**
  * @license
