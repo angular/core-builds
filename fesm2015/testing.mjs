d320b33e1f049b2b469e34f6750516b28a39f622
Andrew Kushnir <akushnir@google.com>
2022-06-22 16:33:37 +0000

    fac6944ff7 docs: fix typos in several guides (#46453)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-cce395a
+ * @license Angular v14.1.0-next.1+sha-fac6944
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22075,7 +22075,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-cce395a');
+const VERSION = new Version('14.1.0-next.1+sha-fac6944');
 
 /**
  * @license
