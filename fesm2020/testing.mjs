f87000f80474e8363db3e21d3a313bcbf957675e
Andrew Kushnir <akushnir@google.com>
2022-06-22 22:11:31 +0000

    63396a1ea7 build: avoid use of `ts-node` wrapper script as it breaks child process forking (#46456)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-2d713f5
+ * @license Angular v14.1.0-next.1+sha-63396a1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22073,7 +22073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-2d713f5');
+const VERSION = new Version('14.1.0-next.1+sha-63396a1');
 
 /**
  * @license
