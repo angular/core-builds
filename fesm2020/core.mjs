63d2a28351d425366aff9bdc1a609a81b5cbf18e
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:10:11 +0000

    d24d54bf69 refactor(platform-browser):remove unused code in dom sanitization service (#46383)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-b417370
+ * @license Angular v14.1.0-next.2+sha-d24d54b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21773,7 +21773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-b417370');
+const VERSION = new Version('14.1.0-next.2+sha-d24d54b');
 
 /**
  * @license
