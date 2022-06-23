ba90843268f094a032f6caea06bda0ed185d820e
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:10:21 +0000

    4a2e7335b1 feat(common): make the `CommonModule` pipes standalone (#46401)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-d24d54b
+ * @license Angular v14.1.0-next.2+sha-4a2e733
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21773,7 +21773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-d24d54b');
+const VERSION = new Version('14.1.0-next.2+sha-4a2e733');
 
 /**
  * @license
