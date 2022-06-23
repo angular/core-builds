ff85b12544ee806b3e2eda0811b8f84ca0b8b856
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-06-23 00:21:05 +0000

    8d485491a9 release: cut the v14.1.0-next.2 release

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.1+sha-b1414ed
+ * @license Angular v14.1.0-next.2+sha-8d48549
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22073,7 +22073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.1+sha-b1414ed');
+const VERSION = new Version('14.1.0-next.2+sha-8d48549');
 
 /**
  * @license
