67218544f4a0e16c73b5b60dd4c1fa172c9f0648
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 20:34:40 +0000

    1c39aace0e fix(common): handle CSS custom properties in NgStyle (#46451)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-0782852
+ * @license Angular v14.1.0-next.2+sha-1c39aac
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22068,7 +22068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-0782852');
+const VERSION = new Version('14.1.0-next.2+sha-1c39aac');
 
 /**
  * @license
