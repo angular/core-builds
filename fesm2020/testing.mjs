e2ee70afbac816e321e9bbf5d22e1609226836e4
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 22:45:44 +0000

    599870f121 refactor(docs-infra): create external-link-with-icon mixin (#46482)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-1c39aac
+ * @license Angular v14.1.0-next.2+sha-599870f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22068,7 +22068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-1c39aac');
+const VERSION = new Version('14.1.0-next.2+sha-599870f');
 
 /**
  * @license
