a13bfbb6406d109a7ffcf558bb57fd28334c4036
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 16:24:46 +0000

    05254f53ff build: replace `extract_typings` rule with shared rule from dev-infra (#46508)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-e42db01
+ * @license Angular v14.1.0-next.2+sha-05254f5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22068,7 +22068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-e42db01');
+const VERSION = new Version('14.1.0-next.2+sha-05254f5');
 
 /**
  * @license
