c9ae0a9e2c141f55e20108eb28051eaf7caf78e0
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 16:22:32 +0000

    0a84898749 test: remove unused intl polyfill from tests and `third_party` vendoring (#46512)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-bdf57ab
+ * @license Angular v14.1.0-next.2+sha-0a84898
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22068,7 +22068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-bdf57ab');
+const VERSION = new Version('14.1.0-next.2+sha-0a84898');
 
 /**
  * @license
