0c36de7ae35439c69e3aa37ed35b2fa353c364fc
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 18:33:02 +0000

    879ac84862 test(core): convert remaining query tests to TestBed (#46549)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-4e8957e
+ * @license Angular v14.1.0-next.2+sha-879ac84
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22070,7 +22070,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-4e8957e');
+const VERSION = new Version('14.1.0-next.2+sha-879ac84');
 
 /**
  * @license
