c074b4b5db2d3f3de0d400709c636a272b59c2a3
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 20:14:53 +0000

    a7597dd080 feat(common): make the CommonModule directives standalone (#46469)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-ee39cf6
+ * @license Angular v14.1.0-next.2+sha-a7597dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22080,7 +22080,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-ee39cf6');
+const VERSION = new Version('14.1.0-next.2+sha-a7597dd');
 
 /**
  * @license
