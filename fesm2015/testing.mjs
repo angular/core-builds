21aec354e3938619ff51b4f3df88a11737b4d79c
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 16:53:36 +0000

    196b64dc11 docs: document text update for web workers (#46540)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-f86e094
+ * @license Angular v14.1.0-next.2+sha-196b64d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22070,7 +22070,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-f86e094');
+const VERSION = new Version('14.1.0-next.2+sha-196b64d');
 
 /**
  * @license
