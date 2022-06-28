f5ebaed94ad3294f339e1bbf39022fdccb655020
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 16:57:44 +0000

    10036d1722 test: replace `--prod` with `--configuration production` (#46548)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.3+sha-57e8fc0
+ * @license Angular v14.0.3+sha-10036d1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21768,7 +21768,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.3+sha-57e8fc0');
+const VERSION = new Version('14.0.3+sha-10036d1');
 
 /**
  * @license
