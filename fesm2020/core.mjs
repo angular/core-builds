a967be286e858ff497b69cab83557a4a8adef241
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:29:52 +0000

    d429eca654 build: update dependency jsdom to v20 (#46576) PR Close #46576

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-2758a30
+ * @license Angular v14.1.0-next.2+sha-d429eca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21778,7 +21778,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-2758a30');
+const VERSION = new Version('14.1.0-next.2+sha-d429eca');
 
 /**
  * @license
