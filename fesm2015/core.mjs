4cba1dbcaac9001959c4b6e15cd01dba0b8f68e3
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:25:59 +0000

    b1ac3d9968 refactor: clean-up pre-CLDR 39 safety check (#46606)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-957744f
+ * @license Angular v14.1.0-next.2+sha-b1ac3d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21765,7 +21765,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-957744f');
+const VERSION = new Version('14.1.0-next.2+sha-b1ac3d9');
 
 /**
  * @license
