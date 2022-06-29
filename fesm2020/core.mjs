f709343d11a317829a9cf7b300ea2c2cf3311651
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:27:43 +0000

    2758a30a34 build: update dependency @rollup/plugin-commonjs to v22 (#46556) PR Close #46556

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-a33d4ee
+ * @license Angular v14.1.0-next.2+sha-2758a30
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21778,7 +21778,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-a33d4ee');
+const VERSION = new Version('14.1.0-next.2+sha-2758a30');
 
 /**
  * @license
