dbe035535bd55bed5c612ba73a61b87f9572681f
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 08:12:58 +0000

    1289425cf8 test: update golden symbols (#46582)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-6a53034
+ * @license Angular v14.1.0-next.2+sha-1289425
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22080,7 +22080,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-6a53034');
+const VERSION = new Version('14.1.0-next.2+sha-1289425');
 
 /**
  * @license
