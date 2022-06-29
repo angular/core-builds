633ebb7c7652d41d1670b616de27db2fcf554f6a
Andrew Scott <atscott01@gmail.com>
2022-06-29 21:13:24 +0000

    598239c35d build: update dependency rimraf to v3 (#46625)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.3+sha-daa6c24
+ * @license Angular v14.1.0-next.3+sha-598239c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21778,7 +21778,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.3+sha-daa6c24');
+const VERSION = new Version('14.1.0-next.3+sha-598239c');
 
 /**
  * @license
