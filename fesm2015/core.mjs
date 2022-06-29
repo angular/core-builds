cb8941b7d70c05ec26feab1f62b3458a17fe3fba
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 04:46:56 +0000

    6a530349e0 refactor(core): remove no longer used helper methods in the `renderer3` folder (#46561)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-8b6f421
+ * @license Angular v14.1.0-next.2+sha-6a53034
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21765,7 +21765,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-8b6f421');
+const VERSION = new Version('14.1.0-next.2+sha-6a53034');
 
 /**
  * @license
