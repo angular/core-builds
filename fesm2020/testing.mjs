9fbc9bb8461944ea8818a65049f5e6fa0c43849e
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:20:20 +0000

    957744f215 refactor(core): remove no longer needed utils (#46571)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-1289425
+ * @license Angular v14.1.0-next.2+sha-957744f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22078,7 +22078,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-1289425');
+const VERSION = new Version('14.1.0-next.2+sha-957744f');
 
 /**
  * @license
