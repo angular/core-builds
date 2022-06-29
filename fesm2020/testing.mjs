28f7409809da94c97a1d2d9b63bf70b1ddd7b089
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:34:28 +0000

    d478e60c4f ci: enable renovate commit message body table to fix minimum commit message (#46611)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.2+sha-47c65ac
+ * @license Angular v14.1.0-next.2+sha-d478e60
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22078,7 +22078,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.2+sha-47c65ac');
+const VERSION = new Version('14.1.0-next.2+sha-d478e60');
 
 /**
  * @license
