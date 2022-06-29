ad79db8c6c47ca9112b4137ac842ba38f5fb2236
Andrew Scott <atscott01@gmail.com>
2022-06-29 22:38:32 +0000

    a3bd65e2b8 fix(router): Ensure `APP_INITIALIZER` of `enabledBlocking` option completes (#46634)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.4+sha-88298ca
+ * @license Angular v14.0.4+sha-a3bd65e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22080,7 +22080,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.4+sha-88298ca');
+const VERSION = new Version('14.0.4+sha-a3bd65e');
 
 /**
  * @license
