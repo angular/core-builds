c85a91e898d0015f7f2fdddc6f01aa551bbbd988
Andrew Scott <atscott01@gmail.com>
2022-06-30 16:14:53 +0000

    f05ed1203d build: update dependency husky to v8 (#46616)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.3+sha-e8c7dd1
+ * @license Angular v14.1.0-next.3+sha-f05ed12
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21778,7 +21778,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.3+sha-e8c7dd1');
+const VERSION = new Version('14.1.0-next.3+sha-f05ed12');
 
 /**
  * @license
