3355183fc21cadfc116b288cb651628fc7459fd7
Andrew Scott <atscott01@gmail.com>
2022-07-06 15:00:23 +0000

    6c1357dd7d feat(router): Add stable cancelation code to `NavigationCancel` event (#46675)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.3+sha-e96ece9
+ * @license Angular v14.1.0-next.3+sha-6c1357d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21402,7 +21402,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.3+sha-e96ece9');
+const VERSION = new Version('14.1.0-next.3+sha-6c1357d');
 
 /**
  * @license
