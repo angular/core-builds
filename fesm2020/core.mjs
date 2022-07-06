1c4f371eecd969f8dc3ea82149348345f38439c8
GitHub <noreply@github.com>
2022-07-06 16:47:14 +0000

    054de6dc34 release: cut the v14.0.5 release (#46724)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.4+sha-aee02b7
+ * @license Angular v14.0.5+sha-054de6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21075,7 +21075,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.4+sha-aee02b7');
+const VERSION = new Version('14.0.5+sha-054de6d');
 
 /**
  * @license
