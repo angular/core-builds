4bec5f48e9eea404f6627cce8524f8ae56fea0da
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-08 16:36:43 +0000

    a4ce273e50 feat(router): Add the target `RouterStateSnapshot` to `NavigationError` (#46731)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.4+sha-bd92723
+ * @license Angular v14.1.0-next.4+sha-a4ce273
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21072,7 +21072,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.4+sha-bd92723');
+const VERSION = new Version('14.1.0-next.4+sha-a4ce273');
 
 /**
  * @license
