3cb9fffaf3d74422e21dffa71f5b510d71e179d6
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 17:52:11 +0000

    4190e9a776 build: update dependency jasmine to v4 (#46617)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.4+sha-b9f5829
+ * @license Angular v14.1.0-next.4+sha-4190e9a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21385,7 +21385,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.4+sha-b9f5829');
+const VERSION = new Version('14.1.0-next.4+sha-4190e9a');
 
 /**
  * @license
