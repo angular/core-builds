51711f35f808454bf2398cfa0622e206b33cd66d
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 22:08:21 +0000

    99697dae66 fix(compiler-cli): only consider used pipes for inline type-check requirement (#46807)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.5+sha-4a2c6e3
+ * @license Angular v14.0.5+sha-99697da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21358,7 +21358,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.5+sha-4a2c6e3');
+const VERSION = new Version('14.0.5+sha-99697da');
 
 /**
  * @license
