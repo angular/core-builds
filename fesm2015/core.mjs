e2a3ffd74466e17d17c577ba71142ba8bddfa556
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 19:25:39 +0000

    f6859e8da2 build: replace size-tracking test with rule from shared dev-infra (#46802)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.4+sha-55308f2
+ * @license Angular v14.1.0-next.4+sha-f6859e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21072,7 +21072,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.4+sha-55308f2');
+const VERSION = new Version('14.1.0-next.4+sha-f6859e8');
 
 /**
  * @license
