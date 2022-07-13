a4c1b88e161e83f8b45d56b15602988cb93ed072
GitHub <noreply@github.com>
2022-07-13 19:27:07 +0000

    5308634900 docs: release notes for the v14.0.6 release (#46828)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.4+sha-dbb0dfe
+ * @license Angular v14.1.0-next.4+sha-5308634
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21387,7 +21387,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0-next.4+sha-dbb0dfe');
+const VERSION = new Version('14.1.0-next.4+sha-5308634');
 
 /**
  * @license
