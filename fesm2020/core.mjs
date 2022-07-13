943d056faa7a9a16e86949041223f8f2862aed57
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 21:43:42 +0000

    f67d2cabaf fix(compiler): inputs/outputs incorrectly parsed in jit mode (#46813)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-7e03fc9
+ * @license Angular v14.2.0-next.0+sha-f67d2ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21085,7 +21085,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-7e03fc9');
+const VERSION = new Version('14.2.0-next.0+sha-f67d2ca');
 
 /**
  * @license
