2ebe8c2a0ad921d92eddc7dc768c8b468382eecc
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-14 22:45:31 +0000

    c0f023f602 fix(router): Ensure target `RouterStateSnapshot` is defined in `NavigationError` (#46842)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-26ea976
+ * @license Angular v14.2.0-next.0+sha-c0f023f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-26ea976');
+const VERSION = new Version('14.2.0-next.0+sha-c0f023f');
 
 /**
  * @license
