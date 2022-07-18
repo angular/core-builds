2b0e829dc66c93392f322d69c90fdcb7574b7514
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 22:19:05 +0000

    91b3ad1672 refactor(router): Update TitleStrategy to useFactory (#46876)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.6+sha-76badad
+ * @license Angular v14.0.6+sha-91b3ad1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21364,7 +21364,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.6+sha-76badad');
+const VERSION = new Version('14.0.6+sha-91b3ad1');
 
 /**
  * @license
