93de46f467b0f18aebf21a17893b277ef56624f6
Andrew Kushnir <akushnir@google.com>
2022-07-19 16:58:00 +0000

    0dc754a726 test: convert `source-map` core tests to use `async/await` instead of `fakeAsync` (#46888)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-14081dc
+ * @license Angular v14.2.0-next.0+sha-0dc754a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7649,7 +7649,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-14081dc');
+const VERSION = new Version('14.2.0-next.0+sha-0dc754a');
 
 /**
  * @license
