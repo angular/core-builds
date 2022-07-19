f3d83aa1e936c9ea554044d88a056cafd90dc557
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-19 16:21:01 +0000

    6479cdba43 build: update babel dependencies to v7.18.9 (#46883)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-bde925b
+ * @license Angular v14.2.0-next.0+sha-6479cdb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7649,7 +7649,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-bde925b');
+const VERSION = new Version('14.2.0-next.0+sha-6479cdb');
 
 /**
  * @license
