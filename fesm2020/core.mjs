a599007b65c53b0cf5dbde0668f7738f57aedad0
Andrew Kushnir <akushnir@google.com>
2022-07-20 16:07:40 +0000

    3562eb107d refactor(compiler-cli): improve error for non-exported non-standalone (#46114)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-4e328c4
+ * @license Angular v14.2.0-next.0+sha-3562eb1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-4e328c4');
+const VERSION = new Version('14.2.0-next.0+sha-3562eb1');
 
 /**
  * @license
