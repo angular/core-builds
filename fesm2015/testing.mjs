d877f89e0f099ae96575aac1926ae66e2a055f02
Andrew Kushnir <akushnir@google.com>
2022-07-20 18:23:06 +0000

    a8e9247e6a refactor(platform-server): make `TransferState` standalone-friendly (#46879)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0+sha-793326d
+ * @license Angular v14.1.0+sha-a8e9247
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7618,7 +7618,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0+sha-793326d');
+const VERSION = new Version('14.1.0+sha-a8e9247');
 
 /**
  * @license
