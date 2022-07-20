101b7cfff6c2bc80c2c3c4ab0fe3af1afa493cdb
Andrew Kushnir <akushnir@google.com>
2022-07-20 16:05:43 +0000

    0d192e8d0b docs: add info about request timeouts (#46882)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-0dc754a
+ * @license Angular v14.2.0-next.0+sha-0d192e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-0dc754a');
+const VERSION = new Version('14.2.0-next.0+sha-0d192e8');
 
 /**
  * @license
