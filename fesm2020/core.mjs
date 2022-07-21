e9f721970aa799ac618e0c50f30a9f8044e96b63
Pawel Kozlowski <pkozlowski@google.com>
2022-07-21 09:41:59 +0000

    10289f1f6e feat(router): expose resolved route title (#46826)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-76790a6
+ * @license Angular v14.2.0-next.0+sha-10289f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-76790a6');
+const VERSION = new Version('14.2.0-next.0+sha-10289f1');
 
 /**
  * @license
