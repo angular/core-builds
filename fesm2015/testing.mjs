08538e97dda4ad605d47db0b1f94dc3c1340d246
Pawel Kozlowski <pkozlowski@google.com>
2022-07-22 09:34:35 +0000

    07606e3181 feat(platform-browser): add `isEmpty` method to the `TransferState` class (#46915)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-afa6050
+ * @license Angular v14.2.0-next.0+sha-07606e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7649,7 +7649,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-afa6050');
+const VERSION = new Version('14.2.0-next.0+sha-07606e3');
 
 /**
  * @license
