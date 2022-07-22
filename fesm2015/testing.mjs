dccc31b4efb621be1b44dfb6d2ac091b9a015ef4
GitHub <noreply@github.com>
2022-07-22 18:52:12 +0000

    3ca985269d build: update `SECURITY.md` to match the content of `angular/.github`

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-d651e73
+ * @license Angular v14.2.0-next.0+sha-3ca9852
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7649,7 +7649,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-d651e73');
+const VERSION = new Version('14.2.0-next.0+sha-3ca9852');
 
 /**
  * @license
