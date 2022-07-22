187386828167011e9670f63677f65af5e2790aa7
Pawel Kozlowski <pkozlowski@google.com>
2022-07-22 08:43:29 +0000

    1eb77a2f4f docs: add simona cotin to contributors (#46922)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0+sha-55f485b
+ * @license Angular v14.1.0+sha-1eb77a2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0+sha-55f485b');
+const VERSION = new Version('14.1.0+sha-1eb77a2');
 
 /**
  * @license
