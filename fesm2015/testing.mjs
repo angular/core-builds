38f92cc9440f35f2f33ef3bad60fb91673989bd5
Andrew Kushnir <akushnir@google.com>
2022-08-01 18:41:40 +0000

    55febc1691 refactor(router): Remove unused ANALYZE_FOR_ENTRY_COMPONENTS (#46942)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-6fc52c5
+ * @license Angular v14.2.0-next.0+sha-55febc1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7649,7 +7649,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-6fc52c5');
+const VERSION = new Version('14.2.0-next.0+sha-55febc1');
 
 /**
  * @license
