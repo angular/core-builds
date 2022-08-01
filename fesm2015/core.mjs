5c81f7ea103935a6b2ec17bc67d63fbdcf6a6d8e
Andrew Kushnir <akushnir@google.com>
2022-08-01 20:12:47 +0000

    e2ab99b95e fix(common): allow null/undefined to be passed to ngClass input (#39280) (#46906)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-8600732
+ * @license Angular v14.2.0-next.0+sha-e2ab99b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-8600732');
+const VERSION = new Version('14.2.0-next.0+sha-e2ab99b');
 
 /**
  * @license
