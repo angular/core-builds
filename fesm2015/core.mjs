67aa0274610706218af3ac4becc7a66b47fbe25e
Andrew Kushnir <akushnir@google.com>
2022-08-01 20:15:43 +0000

    f47c93ba3f build: update bazel setup to v5.5.3 (#47000)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-e2ab99b
+ * @license Angular v14.2.0-next.0+sha-f47c93b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-e2ab99b');
+const VERSION = new Version('14.2.0-next.0+sha-f47c93b');
 
 /**
  * @license
