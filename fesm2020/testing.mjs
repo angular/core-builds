803623fa8c1689b48c5bf9a4ea67a9699bde4d00
Andrew Kushnir <akushnir@google.com>
2022-08-01 17:10:13 +0000

    8cd95d4901 refactor(zone.js): remove leftover debugging code using `Error.stack` (#46989)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0+sha-ff7c716
+ * @license Angular v14.1.0+sha-8cd95d4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7618,7 +7618,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.0+sha-ff7c716');
+const VERSION = new Version('14.1.0+sha-8cd95d4');
 
 /**
  * @license
