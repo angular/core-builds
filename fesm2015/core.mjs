87fb518feb66ba3ea6ac8c23f86f28a5a4193d54
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-08-01 20:08:07 +0000

    65fd757e90 refactor(common): make NgOptimizedImage directive standalone

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-ec83595
+ * @license Angular v14.2.0-next.0+sha-65fd757
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-ec83595');
+const VERSION = new Version('14.2.0-next.0+sha-65fd757');
 
 /**
  * @license
