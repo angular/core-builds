240b63c7f309d2aa1ad21ba98693585de4780f44
Andrew Kushnir <akushnir@google.com>
2022-08-02 16:58:53 +0000

    d8cf78ba5e fix(router): Do not call preload method when not necessary (#47007)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-b757b13
+ * @license Angular v14.2.0-next.0+sha-d8cf78b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-b757b13');
+const VERSION = new Version('14.2.0-next.0+sha-d8cf78b');
 
 /**
  * @license
