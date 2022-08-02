e71b7c9d64ea65dae118bf39637f216d6c393974
Andrew Kushnir <akushnir@google.com>
2022-08-02 19:17:55 +0000

    7243564499 build(docs-infra): ensure all boilerplate dependencies are in sync (#47009)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-cb42788
+ * @license Angular v14.2.0-next.0+sha-7243564
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7649,7 +7649,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-cb42788');
+const VERSION = new Version('14.2.0-next.0+sha-7243564');
 
 /**
  * @license
