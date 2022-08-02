5fc9869a963880e04fd740632ed39af1a9deedab
Andrew Kushnir <akushnir@google.com>
2022-08-02 16:58:32 +0000

    b757b1387c build: update dev-infra packages and account for build-tooling split from `ng-dev` (#46976)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-1fe759d
+ * @license Angular v14.2.0-next.0+sha-b757b13
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-1fe759d');
+const VERSION = new Version('14.2.0-next.0+sha-b757b13');
 
 /**
  * @license
