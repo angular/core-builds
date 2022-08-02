82ee7a2382da6bfc04501e4e9dd2760a05c385c3
Andrew Kushnir <akushnir@google.com>
2022-08-02 19:17:52 +0000

    598b72bd05 feat(language-service): support fix the component missing member (#46764)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-cb42788
+ * @license Angular v14.2.0-next.0+sha-598b72b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-cb42788');
+const VERSION = new Version('14.2.0-next.0+sha-598b72b');
 
 /**
  * @license
