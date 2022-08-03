6c32a9da05ececac84f450f087a9b20740fe4a1b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-08-03 18:08:23 +0000

    71b4ccd70c docs: release notes for the v14.1.1 release

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-9e3994c
+ * @license Angular v14.2.0-next.0+sha-71b4ccd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7649,7 +7649,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-9e3994c');
+const VERSION = new Version('14.2.0-next.0+sha-71b4ccd');
 
 /**
  * @license
