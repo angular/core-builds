b9a2e32bc6df07d2e123bc235885f4854af5e666
Dylan Hunn <dylhunn@gmail.com>
2022-08-04 20:24:46 +0000

    c360b43add docs: add yarn build fail workaround on windows (#47037)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-c01ccc5
+ * @license Angular v14.2.0-next.0+sha-c360b43
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-c01ccc5');
+const VERSION = new Version('14.2.0-next.0+sha-c360b43');
 
 /**
  * @license
