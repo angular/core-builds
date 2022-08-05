15a388dbb2c04733c60d5db392515cf2e1f5b14e
Andrew Scott <atscott01@gmail.com>
2022-08-05 17:22:34 +0000

    387f4a79f8 docs: update dependency injection content (#44466)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.1+sha-c7fed38
+ * @license Angular v14.1.1+sha-387f4a7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.1+sha-c7fed38');
+const VERSION = new Version('14.1.1+sha-387f4a7');
 
 /**
  * @license
