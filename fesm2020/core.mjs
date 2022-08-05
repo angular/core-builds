4a3a60bb63bf899f78fe533c0e5b6ea40f685bee
Andrew Scott <atscott01@gmail.com>
2022-08-05 17:14:28 +0000

    0b392afb69 build: update `ngsw-config.json` after redirect changes (#47048)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-f23232f
+ * @license Angular v14.2.0-next.0+sha-0b392af
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-f23232f');
+const VERSION = new Version('14.2.0-next.0+sha-0b392af');
 
 /**
  * @license
