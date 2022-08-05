abd3083561b4fba51543e3a9d24be3f0a9d16a25
Andrew Scott <atscott01@gmail.com>
2022-08-05 16:52:40 +0000

    c7fed38cf0 refactor(router): Cherry-pick Router services to be providedIn: 'root' (#47052)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.1+sha-1d39ab6
+ * @license Angular v14.1.1+sha-c7fed38
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.1+sha-1d39ab6');
+const VERSION = new Version('14.1.1+sha-c7fed38');
 
 /**
  * @license
