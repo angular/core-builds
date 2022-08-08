748302b513b863d8f43c61a4ce86eb6096bbd8bf
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 18:48:36 +0000

    7f992326e4 docs: update link to the TypeScript handbook (#47075)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-2548214
+ * @license Angular v14.2.0-next.0+sha-7f99232
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-2548214');
+const VERSION = new Version('14.2.0-next.0+sha-7f99232');
 
 /**
  * @license
