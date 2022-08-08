bb901ad9d518aff86fa536670029a924c2582771
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 18:50:53 +0000

    621c38813f refactor: improve disabled attribute warning (#47041)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-dbed2cf
+ * @license Angular v14.2.0-next.0+sha-621c388
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7261,7 +7261,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-dbed2cf');
+const VERSION = new Version('14.2.0-next.0+sha-621c388');
 
 /**
  * @license
