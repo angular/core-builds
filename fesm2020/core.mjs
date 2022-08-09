a0c825ea379c5797c4ff90760209047135da90f7
Dylan Hunn <dylhunn@gmail.com>
2022-08-09 17:20:46 +0000

    e4aee2b83d docs: add antoniocardenas to GDE resources (#47033)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-5e15e4f
+ * @license Angular v14.2.0-next.0+sha-e4aee2b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7261,7 +7261,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-5e15e4f');
+const VERSION = new Version('14.2.0-next.0+sha-e4aee2b');
 
 /**
  * @license
