b82c4a37cdee75116c31361f7691b69182c6d543
Dylan Hunn <dylhunn@gmail.com>
2022-08-10 16:41:45 +0000

    2a43beec15 fix(router): Fix route recognition behavior with some versions of rxjs (#47098)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-91954cf
+ * @license Angular v14.2.0-next.0+sha-2a43bee
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7261,7 +7261,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-91954cf');
+const VERSION = new Version('14.2.0-next.0+sha-2a43bee');
 
 /**
  * @license
