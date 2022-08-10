909fe034c7cf2fd2c1bd901dd4bb01952e01969f
Dylan Hunn <dylhunn@gmail.com>
2022-08-10 18:02:11 +0000

    2548712aba build: add atscott to unavailable users (#47102)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.1+sha-e10aa88
+ * @license Angular v14.1.1+sha-2548712
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7614,7 +7614,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.1+sha-e10aa88');
+const VERSION = new Version('14.1.1+sha-2548712');
 
 /**
  * @license
