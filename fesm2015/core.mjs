3f53f2568e68dae953801da3fa3feb46aa0ab396
Pawel Kozlowski <pkozlowski@google.com>
2022-08-11 16:25:40 +0000

    8d0d6d941d docs: rename duplicate header (#47110)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.1+sha-01f5598
+ * @license Angular v14.2.0-next.1+sha-8d0d6d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7246,7 +7246,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.1+sha-01f5598');
+const VERSION = new Version('14.2.0-next.1+sha-8d0d6d9');
 
 /**
  * @license
