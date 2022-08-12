2c5b136b9d823f1733565d2004414c71408bcbb7
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 18:35:31 +0000

    d4678316b4 docs: fix code samples formatting in tutorial (#47144)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.1+sha-d4f44bd
+ * @license Angular v14.2.0-next.1+sha-d467831
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7246,7 +7246,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.1+sha-d4f44bd');
+const VERSION = new Version('14.2.0-next.1+sha-d467831');
 
 /**
  * @license
--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.1+sha-d4f44bd
+ * @license Angular v14.2.0-next.1+sha-d467831
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7647,7 +7647,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.1+sha-d4f44bd');
+const VERSION = new Version('14.2.0-next.1+sha-d467831');
 
 /**
  * @license
