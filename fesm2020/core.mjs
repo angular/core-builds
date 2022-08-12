26e417245c1f9055063edddebdf45a5f0c142a7c
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 13:44:22 +0000

    93289f9c76 refactor(router): Update error message when Router is provided twice (#47130)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.1+sha-1329707
+ * @license Angular v14.2.0-next.1+sha-93289f9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7261,7 +7261,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.1+sha-1329707');
+const VERSION = new Version('14.2.0-next.1+sha-93289f9');
 
 /**
  * @license
