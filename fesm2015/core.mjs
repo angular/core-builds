29fe706daa7282d9419ebf8e4836b5c7c1ac0031
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-08-12 00:26:51 +0000

    0a9601b964 refactor(common): clean up util.ts and preconnect_link_checker files

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.0+sha-30ed35f
+ * @license Angular v14.2.0-next.0+sha-0a9601b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.0+sha-30ed35f');
+const VERSION = new Version('14.2.0-next.0+sha-0a9601b');
 
 /**
  * @license
