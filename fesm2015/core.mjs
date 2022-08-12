79234cfd1788238faa43c961c9a687d7cc376a5e
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 10:14:54 +0000

    1329707327 docs: clarify use of unsubscribe with HttpClient (#47127)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.1+sha-5509e35
+ * @license Angular v14.2.0-next.1+sha-1329707
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7246,7 +7246,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.1+sha-5509e35');
+const VERSION = new Version('14.2.0-next.1+sha-1329707');
 
 /**
  * @license
