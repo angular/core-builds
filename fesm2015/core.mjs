47fb1e3d6df9d30eddc8279782f562a5c99f3cb6
Joey Perrott <josephperrott@gmail.com>
2022-08-15 15:35:46 +0000

    3d9455ee8b build(docs-infra): upgrade cli command docs sources to 07471d230 (#47149)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.2+sha-970e68a
+ * @license Angular v14.1.2+sha-3d9455e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7239,7 +7239,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.2+sha-970e68a');
+const VERSION = new Version('14.1.2+sha-3d9455e');
 
 /**
  * @license
