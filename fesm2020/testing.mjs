7b704d4a3bc03e1e3c3eb85a1c8f87715b391a7e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-08-17 15:33:44 +0000

    debd254b27 docs: release notes for the v14.2.0-rc.0 release

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-next.1+sha-f2b9956
+ * @license Angular v14.3.0-next.0+sha-debd254
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7647,7 +7647,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-next.1+sha-f2b9956');
+const VERSION = new Version('14.3.0-next.0+sha-debd254');
 
 /**
  * @license
