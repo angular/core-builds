b4922071e83a30139f005f77d2690a6a118e3c1c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-08-17 15:13:34 +0000

    dacac7eb62 release: cut the v14.1.3 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.2+sha-0583227
+ * @license Angular v14.1.3+sha-dacac7e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7239,7 +7239,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.1.2+sha-0583227');
+const VERSION = new Version('14.1.3+sha-dacac7e');
 
 /**
  * @license
