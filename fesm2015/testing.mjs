a83809e44b395d877b0d32c4dea8bd25d5d3b2a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:12:32 +0000

    2095a08781 docs: fix grammar issues in various markdown files (#47200)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-05f5e8a
+ * @license Angular v14.3.0-next.0+sha-2095a08
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7643,7 +7643,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-05f5e8a');
+const VERSION = new Version('14.3.0-next.0+sha-2095a08');
 
 /**
  * @license
