32a8a617f3568ddb0d10aabf9a3ec5f4c8809163
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:18:05 +0000

    b8562eaf01 refactor(compiler): Add a new helper method `getOwningNgModule`. (#47166)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-rc.0+sha-7c5ede6
+ * @license Angular v14.2.0-rc.0+sha-b8562ea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7257,7 +7257,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-rc.0+sha-7c5ede6');
+const VERSION = new Version('14.2.0-rc.0+sha-b8562ea');
 
 /**
  * @license
