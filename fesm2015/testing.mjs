f1fa68c6490ba7b83fea6876afcad53a69751420
Joey Perrott <josephperrott@gmail.com>
2022-08-22 13:22:19 +0000

    b13c991ce7 fix(docs-infra): avoid distorting contributor images (#47215)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-a360309
+ * @license Angular v14.3.0-next.0+sha-b13c991
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7643,7 +7643,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-a360309');
+const VERSION = new Version('14.3.0-next.0+sha-b13c991');
 
 /**
  * @license
