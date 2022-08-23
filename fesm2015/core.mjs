e675295fe4581408215cc56d28a3ea98c7c6a1c0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 16:40:18 +0000

    4780f1bf26 ci: do not match dev-infra group for pullapprove owner changes (#47216)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-c5b3f1c
+ * @license Angular v14.3.0-next.0+sha-4780f1b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-c5b3f1c');
+const VERSION = new Version('14.3.0-next.0+sha-4780f1b');
 
 /**
  * @license
