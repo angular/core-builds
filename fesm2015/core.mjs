33ffbe8a26fc28849de243f9a962a05ab616070a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 16:42:32 +0000

    0dd6102c79 build: update io_bazel_rules_sass digest to 6802532 (#47104)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0-rc.0+sha-e2be526
+ * @license Angular v14.2.0-rc.0+sha-0dd6102
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7242,7 +7242,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0-rc.0+sha-e2be526');
+const VERSION = new Version('14.2.0-rc.0+sha-0dd6102');
 
 /**
  * @license
