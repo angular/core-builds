ade2944264083620a976caa528244f61d70e3330
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 23:38:13 +0000

    4fdbd85d26 fix(common): consider density descriptors with multiple digits as valid (#47230)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-d34ae84
+ * @license Angular v14.3.0-next.0+sha-4fdbd85
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7644,7 +7644,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-d34ae84');
+const VERSION = new Version('14.3.0-next.0+sha-4fdbd85');
 
 /**
  * @license
