3a009263c591461014f71a2643ef06d349215135
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-24 19:37:48 +0000

    a946ca99c6 fix(common): avoid interacting with a destroyed injector (#47243)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-353a8ea
+ * @license Angular v14.3.0-next.0+sha-a946ca9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-353a8ea');
+const VERSION = new Version('14.3.0-next.0+sha-a946ca9');
 
 /**
  * @license
