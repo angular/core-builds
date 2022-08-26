0cd4978f30e068d67be0edf8906ce473d2460e3c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-26 16:38:40 +0000

    32e3081b5d docs(common): fix code examples in image directive guide (#47269)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-46f5021
+ * @license Angular v15.0.0-next.0+sha-32e3081
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-46f5021');
+const VERSION = new Version('15.0.0-next.0+sha-32e3081');
 
 /**
  * @license
