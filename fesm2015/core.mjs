c50740a689e594f968c7879f31ecbc140cd2e157
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-26 00:50:50 +0000

    9dcfeb2dff release: bump the next branch to v15.0.0-next.0 (#47267)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-d037423
+ * @license Angular v15.0.0-next.0+sha-9dcfeb2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.3.0-next.0+sha-d037423');
+const VERSION = new Version('15.0.0-next.0+sha-9dcfeb2');
 
 /**
  * @license
