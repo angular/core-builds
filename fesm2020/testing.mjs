284e422530a098f6d4336164b91ce6c94cabbe65
Joey Perrott <josephperrott@gmail.com>
2022-08-30 18:22:36 +0000

    4ac5ea3ee8 docs: update security guide (#47296)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0+sha-20e1a24
+ * @license Angular v14.2.0+sha-4ac5ea3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7643,7 +7643,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0+sha-20e1a24');
+const VERSION = new Version('14.2.0+sha-4ac5ea3');
 
 /**
  * @license
