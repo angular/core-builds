26a473564ca284f7d3679e3937f6e0f9f75a9ae0
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:50:13 +0000

    9efe24af40 docs: fixed a "grammatical opportunity" (#47328)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-d3c2ca1
+ * @license Angular v15.0.0-next.0+sha-9efe24a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-d3c2ca1');
+const VERSION = new Version('15.0.0-next.0+sha-9efe24a');
 
 /**
  * @license
