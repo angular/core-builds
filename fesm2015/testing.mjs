a324e451c8b73ea2d158a6d2de8afd425cb796ad
Andrew Kushnir <akushnir@google.com>
2022-09-06 19:15:57 +0000

    0e35829580 fix(compiler): avoid errors for inputs with Object-builtin names (#47220)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.0+sha-6091786
+ * @license Angular v14.2.0+sha-0e35829
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7643,7 +7643,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.0+sha-6091786');
+const VERSION = new Version('14.2.0+sha-0e35829');
 
 /**
  * @license
