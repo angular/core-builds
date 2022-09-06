d64ffff8d789b91da949252f8c77068f23782ea5
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:07:06 +0000

    9fbf810744 build: add zone.js to bug-report template for github (#47213)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-385471d
+ * @license Angular v15.0.0-next.0+sha-9fbf810
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-385471d');
+const VERSION = new Version('15.0.0-next.0+sha-9fbf810');
 
 /**
  * @license
