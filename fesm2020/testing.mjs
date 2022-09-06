e33e8bd80b75a0c2dd2f2f11fb45703703e7c886
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:31:31 +0000

    28f277e037 docs: typo (#47246)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-17d66d9
+ * @license Angular v15.0.0-next.0+sha-28f277e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7644,7 +7644,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-17d66d9');
+const VERSION = new Version('15.0.0-next.0+sha-28f277e');
 
 /**
  * @license
