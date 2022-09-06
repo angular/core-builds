f988acebbaf80c23f5998eab7918410b77409461
Andrew Kushnir <akushnir@google.com>
2022-09-06 18:49:06 +0000

    ab936dd407 build: update cross-repo angular dependencies to f2a04c8 (#47231)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-e1bb638
+ * @license Angular v15.0.0-next.0+sha-ab936dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-e1bb638');
+const VERSION = new Version('15.0.0-next.0+sha-ab936dd');
 
 /**
  * @license
