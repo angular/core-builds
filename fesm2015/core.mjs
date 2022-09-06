f68d19866a54352a41bf4448d44ece2d47d1095a
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:17:16 +0000

    b9f80d277d build: update io_bazel_rules_sass digest to f3874ae (#47292)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-93d7d91
+ * @license Angular v15.0.0-next.0+sha-b9f80d2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-93d7d91');
+const VERSION = new Version('15.0.0-next.0+sha-b9f80d2');
 
 /**
  * @license
--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-93d7d91
+ * @license Angular v15.0.0-next.0+sha-b9f80d2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7258,7 +7258,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-93d7d91');
+const VERSION = new Version('15.0.0-next.0+sha-b9f80d2');
 
 /**
  * @license
