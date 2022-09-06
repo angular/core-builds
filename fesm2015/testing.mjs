c58a7583347f36d7a89c6b8b2277d8c8a79d40f9
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:33:27 +0000

    33bb34b64e refactor(service-worker): refactored code to remove useless code (#47255)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-0d13cc7
+ * @license Angular v15.0.0-next.0+sha-33bb34b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7644,7 +7644,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-0d13cc7');
+const VERSION = new Version('15.0.0-next.0+sha-33bb34b');
 
 /**
  * @license
