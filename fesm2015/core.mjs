d7905a0c5b2d27daf5155efdf8c0066981b9573d
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:15:55 +0000

    35f93bd409 refactor(router): remove unnecesary `null` (#47238)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-d36fd3d
+ * @license Angular v15.0.0-next.0+sha-35f93bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-d36fd3d');
+const VERSION = new Version('15.0.0-next.0+sha-35f93bd');
 
 /**
  * @license
