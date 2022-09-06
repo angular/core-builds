f3d14d14835713d25bdd753c2d6bf6ce83ef6292
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:57:00 +0000

    99c12c92a8 docs: apply suggestions from code review (#47350)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-e4f6eed
+ * @license Angular v15.0.0-next.0+sha-99c12c9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7644,7 +7644,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-e4f6eed');
+const VERSION = new Version('15.0.0-next.0+sha-99c12c9');
 
 /**
  * @license
