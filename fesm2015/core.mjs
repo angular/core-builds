dc38c3b4dca7dccc6103e95a0c63a42d2034699e
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:56:00 +0000

    e4f6eed653 docs: formatting issue (#47349)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.0+sha-99be607
+ * @license Angular v15.0.0-next.0+sha-e4f6eed
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7243,7 +7243,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.0+sha-99be607');
+const VERSION = new Version('15.0.0-next.0+sha-e4f6eed');
 
 /**
  * @license
