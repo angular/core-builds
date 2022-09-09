11964e91392f02128e34a6d76ed0454dcb88a4c1
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:18:47 +0000

    c377f5713c docs: increase aio-preview size limit (#47395)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-4e10a74
+ * @license Angular v15.0.0-next.1+sha-c377f57
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7644,7 +7644,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.1+sha-4e10a74');
+const VERSION = new Version('15.0.0-next.1+sha-c377f57');
 
 /**
  * @license
