df9e26a985b9cb387a8c7f1261dc3c3966dbba6e
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 08:38:22 +0000

    c9bdf9bab1 fix(common): rename `rawSrc` -> `ngSrc` in NgOptimizedImage directive (#47362) (#47396)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.1+sha-a61d553
+ * @license Angular v14.2.1+sha-c9bdf9b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.1+sha-a61d553');
+const VERSION = new Version('14.2.1+sha-c9bdf9b');
 
 /**
  * @license
