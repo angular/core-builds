54e3add9b51433356409b0b4b942eb96da960a5b
Dylan Hunn <dylhunn@gmail.com>
2022-09-13 02:23:25 +0000

    6c7cd6ba64 refactor(platform-server): add a marker to specify how a page was rendered (#47103)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-ec9ee8e
+ * @license Angular v15.0.0-next.1+sha-6c7cd6b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7636,7 +7636,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.1+sha-ec9ee8e');
+const VERSION = new Version('15.0.0-next.1+sha-6c7cd6b');
 
 /**
  * @license
