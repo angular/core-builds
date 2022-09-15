fc820a09ed9dea61ac09b15eaffc64f4214028af
GitHub <noreply@github.com>
2022-09-15 17:44:26 +0000

    1f88df0f8f build: update `CODE_OF_CONDUCT.md` to match the content of `angular/.github`

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-deaa05a
+ * @license Angular v15.0.0-next.2+sha-1f88df0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7636,7 +7636,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-deaa05a');
+const VERSION = new Version('15.0.0-next.2+sha-1f88df0');
 
 /**
  * @license
