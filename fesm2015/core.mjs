cc018f61c74fab9dba144c1257c93e6ae70276c6
Dylan Hunn <dylhunn@gmail.com>
2022-09-15 16:37:41 +0000

    19f189fa41 docs(docs-infra): add documentation on debugging the events generation process (#47449)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-5039275
+ * @license Angular v15.0.0-next.2+sha-19f189f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7235,7 +7235,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-5039275');
+const VERSION = new Version('15.0.0-next.2+sha-19f189f');
 
 /**
  * @license
