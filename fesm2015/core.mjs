91fdc3fc3260c0e2aa287f8d9dd8f4860110b7d3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-16 09:36:18 +0000

    db3b532950 fix(docs-infra): fix `Update AIO events` GitHub Action (#47452)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.2+sha-d961f1f
+ * @license Angular v14.2.2+sha-db3b532
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.2+sha-d961f1f');
+const VERSION = new Version('14.2.2+sha-db3b532');
 
 /**
  * @license
