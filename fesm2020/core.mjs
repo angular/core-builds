712b10797c2f8405f832829f1d1c6d9c873af949
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 17:16:46 +0000

    32cad55f0d feat(devtools): implement inspect functionality for directives (#47334)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-150dd30
+ * @license Angular v15.0.0-next.2+sha-32cad55
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-150dd30');
+const VERSION = new Version('15.0.0-next.2+sha-32cad55');
 
 /**
  * @license
