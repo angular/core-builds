dd7d1f09105d795150f0a9a95fc6511dff130acc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 08:00:47 +0000

    3f4cbe4ca7 docs: update contribution docs for serving a local library/project (#47421)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.2+sha-db3b532
+ * @license Angular v14.2.2+sha-3f4cbe4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.2+sha-db3b532');
+const VERSION = new Version('14.2.2+sha-3f4cbe4');
 
 /**
  * @license
