b37e37813bdd54d28ceec2f60b17bfe88a609064
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 15:26:48 +0000

    247181f67d docs: update events (#47462)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-b6c90e8
+ * @license Angular v15.0.0-next.2+sha-247181f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7637,7 +7637,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-b6c90e8');
+const VERSION = new Version('15.0.0-next.2+sha-247181f');
 
 /**
  * @license
