503b0fecd8cddb2f69d8e534f365dc34b1239ce1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 16:35:00 +0000

    8a3054c954 ci: remove atscott from unavailable list (#47482)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-7814da6
+ * @license Angular v15.0.0-next.2+sha-8a3054c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7637,7 +7637,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-7814da6');
+const VERSION = new Version('15.0.0-next.2+sha-8a3054c');
 
 /**
  * @license
