1c8480b5114261808eeb8fd210915a495536130c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 07:56:12 +0000

    7f637e1873 docs: move Image Directive guide link to the Feature Preview section (#47466)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-4eff4d3
+ * @license Angular v15.0.0-next.2+sha-7f637e1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-4eff4d3');
+const VERSION = new Version('15.0.0-next.2+sha-7f637e1');
 
 /**
  * @license
