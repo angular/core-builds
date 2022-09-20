85cd971e188be57eadfcb40e1287e0acf462f6a3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-20 08:28:42 +0000

    d11b1d99b0 fix(animations): make sure that the useAnimation function delay is applied (#47468)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-32cad55
+ * @license Angular v15.0.0-next.2+sha-d11b1d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7236,7 +7236,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-32cad55');
+const VERSION = new Version('15.0.0-next.2+sha-d11b1d9');
 
 /**
  * @license
