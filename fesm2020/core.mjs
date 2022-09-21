325a5903f0766b3aa8caa6ab649d94c8f372e8a0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-21 14:21:41 +0000

    8cef5dd7c1 build: update all non-major dependencies (#47487)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-06ca0dc
+ * @license Angular v15.0.0-next.2+sha-8cef5dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-06ca0dc');
+const VERSION = new Version('15.0.0-next.2+sha-8cef5dd');
 
 /**
  * @license
--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-06ca0dc
+ * @license Angular v15.0.0-next.2+sha-8cef5dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7637,7 +7637,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-06ca0dc');
+const VERSION = new Version('15.0.0-next.2+sha-8cef5dd');
 
 /**
  * @license
