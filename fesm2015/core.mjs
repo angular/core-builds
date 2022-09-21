2789fa2f300aa0a2c51ec032814bd49cbd383c51
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-21 16:42:02 +0000

    8fcadaad48 perf(compiler-cli): cache source file for reporting type-checking diagnostics (#47471)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-8cef5dd
+ * @license Angular v15.0.0-next.2+sha-8fcadaa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7236,7 +7236,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-8cef5dd');
+const VERSION = new Version('15.0.0-next.2+sha-8fcadaa');
 
 /**
  * @license
--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.2+sha-8cef5dd
+ * @license Angular v15.0.0-next.2+sha-8fcadaa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.2+sha-8cef5dd');
+const VERSION = new Version('15.0.0-next.2+sha-8fcadaa');
 
 /**
  * @license
