6121ffb93f39ed846408b592ebf2e85f42c4f885
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:19:04 +0000

    478db892c2 docs: Add new PR response templates (#47515)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.3+sha-aef8d0b
+ * @license Angular v15.0.0-next.3+sha-478db89
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7637,7 +7637,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.3+sha-aef8d0b');
+const VERSION = new Version('15.0.0-next.3+sha-478db89');
 
 /**
  * @license
