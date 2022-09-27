02ff845bb701009448e9699f8f6d83c9fcfa4056
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:36:09 +0000

    a4167c8854 docs: update what what the CLI collects in analytics (#47529)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.3+sha-551c641
+ * @license Angular v15.0.0-next.3+sha-a4167c8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7256,7 +7256,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.3+sha-551c641');
+const VERSION = new Version('15.0.0-next.3+sha-a4167c8');
 
 /**
  * @license
