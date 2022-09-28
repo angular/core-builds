9db5eafb70b41994e8220e315e2a98729c9e7ab2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-28 21:35:52 +0000

    2d35395b69 docs: release notes for the v14.2.4 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.3+sha-002ee32
+ * @license Angular v15.0.0-next.3+sha-2d35395
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7241,7 +7241,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.3+sha-002ee32');
+const VERSION = new Version('15.0.0-next.3+sha-2d35395');
 
 /**
  * @license
