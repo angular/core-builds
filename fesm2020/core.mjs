4dca965c8e04615c1b136b0e3405e503d4f784a8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-28 21:40:51 +0000

    bcc4a95c44 release: cut the v15.0.0-next.4 release

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.3+sha-2d35395
+ * @license Angular v15.0.0-next.4+sha-bcc4a95
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7256,7 +7256,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.3+sha-2d35395');
+const VERSION = new Version('15.0.0-next.4+sha-bcc4a95');
 
 /**
  * @license
