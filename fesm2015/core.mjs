bc98129c7cd81ba4af5829b5303d76c391c53723
Andrew Kushnir <akushnir@google.com>
2022-09-29 23:11:39 +0000

    ec1105dd71 build: use shared circleci dev-infra for common circleci code (#47467)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-d90e760
+ * @license Angular v15.0.0-next.4+sha-ec1105d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7241,7 +7241,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-d90e760');
+const VERSION = new Version('15.0.0-next.4+sha-ec1105d');
 
 /**
  * @license
