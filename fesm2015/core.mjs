89c5fa0bc3983bd3888fa511d954b15876add979
Andrew Kushnir <akushnir@google.com>
2022-09-29 18:07:43 +0000

    ee0c7161dd docs: add new files (#47391)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-5a17858
+ * @license Angular v15.0.0-next.4+sha-ee0c716
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7241,7 +7241,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-5a17858');
+const VERSION = new Version('15.0.0-next.4+sha-ee0c716');
 
 /**
  * @license
