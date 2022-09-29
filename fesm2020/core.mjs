f8f33ac1db4a587097e56118133b30490a14371a
Andrew Kushnir <akushnir@google.com>
2022-09-29 18:13:35 +0000

    26ad19e81a docs(common): update reference url to ICU docs (#47518) PR Close #47518

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-c39a3a1
+ * @license Angular v15.0.0-next.4+sha-26ad19e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7256,7 +7256,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-c39a3a1');
+const VERSION = new Version('15.0.0-next.4+sha-26ad19e');
 
 /**
  * @license
