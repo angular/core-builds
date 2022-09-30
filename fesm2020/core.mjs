59b728552f4bc219355358a1f05483825dd99cb5
Andrew Kushnir <akushnir@google.com>
2022-09-30 18:12:25 +0000

    ce59f1a14d test: delete bazel_ngtsc_plugin integration test (#47584)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-382330c
+ * @license Angular v15.0.0-next.4+sha-ce59f1a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7256,7 +7256,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-382330c');
+const VERSION = new Version('15.0.0-next.4+sha-ce59f1a');
 
 /**
  * @license
