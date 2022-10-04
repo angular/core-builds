e2bf89907eb2013fcfa49d6646d8b91d1cb6876d
Andrew Kushnir <akushnir@google.com>
2022-10-04 15:15:58 +0000

    c3f857975d feat(router): make RouterOutlet name an Input so it can be set dynamically (#46569)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-07d9a27
+ * @license Angular v15.0.0-next.4+sha-c3f8579
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7643,7 +7643,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-07d9a27');
+const VERSION = new Version('15.0.0-next.4+sha-c3f8579');
 
 /**
  * @license
