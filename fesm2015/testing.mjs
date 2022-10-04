24f5123f8ecdd94b90988d9a7ecd5899f68874a8
Andrew Kushnir <akushnir@google.com>
2022-10-04 21:57:29 +0000

    29194638c1 refactor(compiler): Rename `PipeInScope` -> `PotentialPipe` (#47561)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-e3cef4a
+ * @license Angular v15.0.0-next.4+sha-2919463
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7646,7 +7646,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-e3cef4a');
+const VERSION = new Version('15.0.0-next.4+sha-2919463');
 
 /**
  * @license
--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-e3cef4a
+ * @license Angular v15.0.0-next.4+sha-2919463
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7260,7 +7260,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-e3cef4a');
+const VERSION = new Version('15.0.0-next.4+sha-2919463');
 
 /**
  * @license
