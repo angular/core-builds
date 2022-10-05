fa73ced964df89dfe56204df9bc29b97e0500a41
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-10-05 16:07:59 +0000

    2cdf1286e3 release: cut the v15.0.0-next.5 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-8352cf7
+ * @license Angular v15.0.0-next.5+sha-2cdf128
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7245,7 +7245,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-8352cf7');
+const VERSION = new Version('15.0.0-next.5+sha-2cdf128');
 
 /**
  * @license
--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.4+sha-8352cf7
+ * @license Angular v15.0.0-next.5+sha-2cdf128
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7646,7 +7646,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.4+sha-8352cf7');
+const VERSION = new Version('15.0.0-next.5+sha-2cdf128');
 
 /**
  * @license
