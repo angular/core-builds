52d1a5544724110eb4b22c830725dd7469a14127
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-05 22:34:39 +0000

    f73ef21442 feat(router): merge `RouterLinkWithHref` into `RouterLink` (#47630)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-8df8c77
+ * @license Angular v15.0.0-next.5+sha-f73ef21
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7646,7 +7646,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-8df8c77');
+const VERSION = new Version('15.0.0-next.5+sha-f73ef21');
 
 /**
  * @license
