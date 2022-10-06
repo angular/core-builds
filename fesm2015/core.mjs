76406d12f599e660fbb4d546e254251334020667
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 21:42:20 +0000

    e3ef401c25 docs: add Marko to GDE resources (#47595)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.5+sha-197e8b4
+ * @license Angular v14.2.5+sha-e3ef401
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.5+sha-197e8b4');
+const VERSION = new Version('14.2.5+sha-e3ef401');
 
 /**
  * @license
