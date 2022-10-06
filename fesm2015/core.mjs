3304f5d4562471f6aa6b5d85a80255d3b850fdca
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 20:48:48 +0000

    1095dc5fd1 docs: fix documentation linting errors in ajs-quick-reference.md (#47526)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.5+sha-5782d63
+ * @license Angular v14.2.5+sha-1095dc5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.5+sha-5782d63');
+const VERSION = new Version('14.2.5+sha-1095dc5');
 
 /**
  * @license
