eb99315c7b1ec210869d0f1360e23ae0589f2176
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 20:46:34 +0000

    bf6679a579 docs(forms): setErrors emitEvent default value (#47546) PR Close #47546

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-5377e35
+ * @license Angular v15.0.0-next.5+sha-bf6679a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7260,7 +7260,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-5377e35');
+const VERSION = new Version('15.0.0-next.5+sha-bf6679a');
 
 /**
  * @license
--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-5377e35
+ * @license Angular v15.0.0-next.5+sha-bf6679a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7646,7 +7646,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-5377e35');
+const VERSION = new Version('15.0.0-next.5+sha-bf6679a');
 
 /**
  * @license
