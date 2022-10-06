29185deb168b81c30674a10a78db0d54ce9431f8
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 19:22:06 +0000

    c09c1bb033 docs(http): fix copy-paste error in HttpClientJsonpModule docs (#47502)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-16c8f55
+ * @license Angular v15.0.0-next.5+sha-c09c1bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7260,7 +7260,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-16c8f55');
+const VERSION = new Version('15.0.0-next.5+sha-c09c1bb');
 
 /**
  * @license
