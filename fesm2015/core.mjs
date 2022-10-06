2b09599f799a2a41a275ca747f7efd5afcaa8cae
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 21:19:54 +0000

    214baeae8b docs: fixed typo in AsyncValidatorFn (#47465)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-bf6679a
+ * @license Angular v15.0.0-next.5+sha-214baea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7245,7 +7245,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-bf6679a');
+const VERSION = new Version('15.0.0-next.5+sha-214baea');
 
 /**
  * @license
