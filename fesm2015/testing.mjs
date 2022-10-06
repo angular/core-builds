2abee2183dd495e31ba6dd43d65eeebba1d86e97
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 23:43:34 +0000

    4620fbdf04 refactor(docs-infra): add new Input for better description of close button in notification component (#47681)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.5+sha-e3ef401
+ * @license Angular v14.2.5+sha-4620fbd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7635,7 +7635,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.5+sha-e3ef401');
+const VERSION = new Version('14.2.5+sha-4620fbd');
 
 /**
  * @license
