7e3fc25d7f4c0f42eddc87629061109b5b080577
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-07 18:00:40 +0000

    cfd0cdbd9b docs: remove assorted lint errors (#47693)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.5+sha-9e14658
+ * @license Angular v14.2.5+sha-cfd0cdb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7249,7 +7249,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.5+sha-9e14658');
+const VERSION = new Version('14.2.5+sha-cfd0cdb');
 
 /**
  * @license
