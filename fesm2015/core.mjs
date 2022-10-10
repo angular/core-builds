709040a49b744302ed46d2993c1e5088d6faab54
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 16:27:57 +0000

    370aa345b1 build: update dependency google-closure-compiler to v20221004 (#47696)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-6bdba61
+ * @license Angular v15.0.0-next.5+sha-370aa34
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-6bdba61');
+const VERSION = new Version('15.0.0-next.5+sha-370aa34');
 
 /**
  * @license
