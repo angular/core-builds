c78ceee0927ffbdaf3338fecb618d01b98506ab8
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 22:06:52 +0000

    bc54687c7b fix(compiler-cli): exclude abstract classes from `strictInjectionParameters` requirement (#44615)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-48b354a
+ * @license Angular v15.0.0-next.5+sha-bc54687
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7684,7 +7684,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-48b354a');
+const VERSION = new Version('15.0.0-next.5+sha-bc54687');
 
 /**
  * @license
