babc423f455dbc2c021e49a5d1d05b114e0df438
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 21:51:35 +0000

    bebef5fb43 feat(language-service): Quick fix to import a component when its selector is used (#47088)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-75e6297
+ * @license Angular v15.0.0-next.5+sha-bebef5f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-75e6297');
+const VERSION = new Version('15.0.0-next.5+sha-bebef5f');
 
 /**
  * @license
