79ea8d3abc97e85be0513e6d9732df25e68f018b
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 16:39:56 +0000

    4fde292bb5 feat(common): Add automatic srcset generation to ngOptimizedImage (#47547)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-ed11a13
+ * @license Angular v15.0.0-next.5+sha-4fde292
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-ed11a13');
+const VERSION = new Version('15.0.0-next.5+sha-4fde292');
 
 /**
  * @license
