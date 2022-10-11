a27dbc71b81c32ea2d60910a95233f103a572d7f
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 17:41:23 +0000

    b618b5aa86 fix(zone.js): cancel tasks only when they are scheduled or running (#46435)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-96b7fe9
+ * @license Angular v15.0.0-next.5+sha-b618b5a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7684,7 +7684,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-96b7fe9');
+const VERSION = new Version('15.0.0-next.5+sha-b618b5a');
 
 /**
  * @license
