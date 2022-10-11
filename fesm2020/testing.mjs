c0407ac8b63ab38c9f6d07d07420819713cf6bdc
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 17:42:36 +0000

    39b72e208b fix(compiler): update element schema (#47552)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-b618b5a
+ * @license Angular v15.0.0-next.5+sha-39b72e2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7684,7 +7684,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-b618b5a');
+const VERSION = new Version('15.0.0-next.5+sha-39b72e2');
 
 /**
  * @license
