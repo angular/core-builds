fd9c54b2f00266122b5df78164576d95b79cd1f4
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 16:17:47 +0000

    1818c54b62 docs: fix linter errors in prerendering.md (#47724)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-a41ff94
+ * @license Angular v15.0.0-next.5+sha-1818c54
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7684,7 +7684,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-a41ff94');
+const VERSION = new Version('15.0.0-next.5+sha-1818c54');
 
 /**
  * @license
