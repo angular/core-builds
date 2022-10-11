70bbc0a27bc21113662f598533716fbecb7abcf9
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 23:27:45 +0000

    e3fbeab3bd build: update dependency @rollup/plugin-commonjs to v23 (#47711)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-a910c83
+ * @license Angular v15.0.0-next.5+sha-e3fbeab
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7298,7 +7298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-a910c83');
+const VERSION = new Version('15.0.0-next.5+sha-e3fbeab');
 
 /**
  * @license
