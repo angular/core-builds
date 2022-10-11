466527f28b566159f5466783151723e046b298c5
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 17:43:47 +0000

    1b9fd46d14 feat(core): add support for Node.js version 18 (#47730)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-39b72e2
+ * @license Angular v15.0.0-next.5+sha-1b9fd46
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7684,7 +7684,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-39b72e2');
+const VERSION = new Version('15.0.0-next.5+sha-1b9fd46');
 
 /**
  * @license
