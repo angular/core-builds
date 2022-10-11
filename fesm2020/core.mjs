425a6b1a9818268016dcb74bef9715e30abb3087
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 22:24:01 +0000

    bbbd4c3ac8 docs: fix linter errors for security.md (#47688)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-23c06ee
+ * @license Angular v15.0.0-next.5+sha-bbbd4c3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7298,7 +7298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-23c06ee');
+const VERSION = new Version('15.0.0-next.5+sha-bbbd4c3');
 
 /**
  * @license
