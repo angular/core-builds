465bcc098e26f305f9c2d9d4408e7dc6ea2377fb
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-10-12 18:57:14 +0000

    93cc5b2210 release: cut the v15.0.0-next.6 release

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-7ab0a26
+ * @license Angular v15.0.0-next.6+sha-93cc5b2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7298,7 +7298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-7ab0a26');
+const VERSION = new Version('15.0.0-next.6+sha-93cc5b2');
 
 /**
  * @license
