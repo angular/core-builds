7a5e386911bc38f4498d7118eb373b655af1b3a8
Joey Perrott <josephperrott.github@gmail.com>
2022-10-12 17:53:07 +0000

    510a295ed1 build: update to the latest version of the feature triage action

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-c36f500
+ * @license Angular v15.0.0-next.5+sha-510a295
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7298,7 +7298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-c36f500');
+const VERSION = new Version('15.0.0-next.5+sha-510a295');
 
 /**
  * @license
