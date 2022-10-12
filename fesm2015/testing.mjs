76db879f4a94d0b576e36b75b8594f91258267b5
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-12 16:15:51 +0000

    9483343ebf feat(common): Add fill mode to NgOptimizedImage (#47738)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-3a9c452
+ * @license Angular v15.0.0-next.5+sha-9483343
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7684,7 +7684,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-3a9c452');
+const VERSION = new Version('15.0.0-next.5+sha-9483343');
 
 /**
  * @license
