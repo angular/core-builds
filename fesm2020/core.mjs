691d261c18aa8d9acf6ce7efd250e1a75bc6a70c
Joey Perrott <josephperrott@gmail.com>
2022-10-12 18:29:54 +0000

    7ab0a269ec build: update comp labels to be area instead (#47750)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.5+sha-59dfb54
+ * @license Angular v15.0.0-next.5+sha-7ab0a26
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7298,7 +7298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.5+sha-59dfb54');
+const VERSION = new Version('15.0.0-next.5+sha-7ab0a26');
 
 /**
  * @license
