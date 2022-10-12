53ea63d2a25c6cb8103f2a3a094febaf5a843818
Joey Perrott <josephperrott@gmail.com>
2022-10-12 17:22:16 +0000

    09d5f51764 build: update labels to match standardized labels (#47747)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.5+sha-3fd176a
+ * @license Angular v14.2.5+sha-09d5f51
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.5+sha-3fd176a');
+const VERSION = new Version('14.2.5+sha-09d5f51');
 
 /**
  * @license
