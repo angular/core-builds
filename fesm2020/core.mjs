8cef0c561260c6268fb01a9426652ee916429a99
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 17:02:48 +0000

    e13d97cf7e refactor(common): make `provideHttpClient` return `EnvironmentProviders` (#47758)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.6+sha-1b48e65
+ * @license Angular v15.0.0-next.6+sha-e13d97c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7298,7 +7298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.6+sha-1b48e65');
+const VERSION = new Version('15.0.0-next.6+sha-e13d97c');
 
 /**
  * @license
