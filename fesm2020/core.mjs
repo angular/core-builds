a8e40007ec5d14a1534cbbbaa2cc3d93fbae5706
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 21:13:00 +0000

    d684148f93 refactor(compiler-cli): use mkdirSync recursive option instead of custom implementation (#47678)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-59f3324
+ * @license Angular v15.1.0-next.0+sha-d684148
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7298,7 +7298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-59f3324');
+const VERSION = new Version('15.1.0-next.0+sha-d684148');
 
 /**
  * @license
