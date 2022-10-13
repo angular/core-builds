a3879e8acbf25f9720b441e6c57246a309b44c31
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 19:30:20 +0000

    0457b58dfe docs: Separated HTML elements with blank lines (#47331)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.6+sha-e13d97c
+ * @license Angular v15.0.0-next.6+sha-0457b58
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7684,7 +7684,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-next.6+sha-e13d97c');
+const VERSION = new Version('15.0.0-next.6+sha-0457b58');
 
 /**
  * @license
