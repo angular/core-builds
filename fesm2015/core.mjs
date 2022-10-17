4ed92bd4db5af6f0646c8a66428fd385d95d64f1
Dylan Hunn <dylhunn@gmail.com>
2022-10-17 08:16:56 +0000

    c9541f4c49 fix(localize): update ng add schematic to support Angular CLI version 15 (#47763)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-e0bda23
+ * @license Angular v15.1.0-next.0+sha-c9541f4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-e0bda23');
+const VERSION = new Version('15.1.0-next.0+sha-c9541f4');
 
 /**
  * @license
