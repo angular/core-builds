ab019698bc1720df65ab1d804d22645fce91ae1d
Dylan Hunn <dylhunn@gmail.com>
2022-10-17 10:32:55 +0000

    c62246c038 docs: fix linter errors for zone.md (#47707)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.6+sha-46d33dd
+ * @license Angular v14.2.6+sha-c62246c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.6+sha-46d33dd');
+const VERSION = new Version('14.2.6+sha-c62246c');
 
 /**
  * @license
