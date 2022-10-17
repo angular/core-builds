3028f4332ce547ff9f6653ab1e1b3c55def0aba5
Dylan Hunn <dylhunn@gmail.com>
2022-10-17 10:29:47 +0000

    cd25cb2f83 docs: fix linter errors for view-encapsulation.md (#47772)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-c9541f4
+ * @license Angular v15.1.0-next.0+sha-cd25cb2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-c9541f4');
+const VERSION = new Version('15.1.0-next.0+sha-cd25cb2');
 
 /**
  * @license
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-c9541f4
+ * @license Angular v15.1.0-next.0+sha-cd25cb2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
