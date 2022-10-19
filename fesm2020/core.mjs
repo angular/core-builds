2373e537a0775e52902375a4f1e5d5068f4db5bb
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:41:34 +0000

    bdd4d14db9 refactor(common): drop deprecated selector from the NgOptimizedImage directive (#47798)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-ce5880f
+ * @license Angular v15.1.0-next.0+sha-bdd4d14
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-ce5880f');
+const VERSION = new Version('15.1.0-next.0+sha-bdd4d14');
 
 /**
  * @license
