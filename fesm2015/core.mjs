f233348b577da9d63fb497f34306eab5e285dcd0
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 18:30:28 +0000

    c5ba762bfa docs(router): fix misleading description of `BaseRouteReuseStrategy` (#47786)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-80da664
+ * @license Angular v15.1.0-next.0+sha-c5ba762
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-80da664');
+const VERSION = new Version('15.1.0-next.0+sha-c5ba762');
 
 /**
  * @license
