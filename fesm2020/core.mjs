98b0e1c5d44fa606cedc6ca24c885663ec75908c
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:44:20 +0000

    b31091b222 docs(common): update image directive docs (#47796)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-a471684
+ * @license Angular v15.1.0-next.0+sha-b31091b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-a471684');
+const VERSION = new Version('15.1.0-next.0+sha-b31091b');
 
 /**
  * @license
