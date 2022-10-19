7d9b9f87e4e32a29c223cffe787e35d1dcb550e8
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:26:43 +0000

    38ec1565ad fix(common): don't generate srcset if noopImageLoader is used (#47804)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-0f726bd
+ * @license Angular v15.1.0-next.0+sha-38ec156
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7681,7 +7681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-0f726bd');
+const VERSION = new Version('15.1.0-next.0+sha-38ec156');
 
 /**
  * @license
