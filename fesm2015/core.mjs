eaa139a86b19b5988e44a0e835c40dd2b6010818
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:41:49 +0000

    a4716840fc docs: Linting suggested corrections made. Restructuring or rewording sentences to be shorter and clearer. Inserted an introductory paragraph under level 1 heading. (#47788)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-bdd4d14
+ * @license Angular v15.1.0-next.0+sha-a471684
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-bdd4d14');
+const VERSION = new Version('15.1.0-next.0+sha-a471684');
 
 /**
  * @license
