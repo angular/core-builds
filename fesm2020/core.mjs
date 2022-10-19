3d020a1e016a7dfc709077806c7456904f369df0
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 22:15:53 +0000

    883b32ccf2 docs: release notes for the v15.0.0-rc.0 release

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-f1ed796
+ * @license Angular v15.1.0-next.0+sha-883b32c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-f1ed796');
+const VERSION = new Version('15.1.0-next.0+sha-883b32c');
 
 /**
  * @license
