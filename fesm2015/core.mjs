e79a10f4b45fffb7df0f7a77375cbd0f80267701
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:44:52 +0000

    f1ed796a68 docs: release notes for the v14.2.7 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-af8afee
+ * @license Angular v15.1.0-next.0+sha-f1ed796
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-af8afee');
+const VERSION = new Version('15.1.0-next.0+sha-f1ed796');
 
 /**
  * @license
