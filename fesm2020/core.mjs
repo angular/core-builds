1ecc4d4ce9ffc36592942b1139b66fed43d5aa19
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:26:40 +0000

    177c55686d docs: exclude lifecycle hooks of the NgOptimizedImage from docs (#47814)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-0f726bd
+ * @license Angular v15.1.0-next.0+sha-177c556
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-0f726bd');
+const VERSION = new Version('15.1.0-next.0+sha-177c556');
 
 /**
  * @license
