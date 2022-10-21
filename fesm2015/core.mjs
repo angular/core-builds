83587d8c1eeeb474b3089fe595b5d18f026f2996
Dylan Hunn <dylhunn@gmail.com>
2022-10-21 00:08:16 +0000

    3a18398d83 fix(common): Don't warn about image distortion is fill mode is enabled (#47824)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.0+sha-44c1770
+ * @license Angular v15.0.0-rc.0+sha-3a18398
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.0+sha-44c1770');
+const VERSION = new Version('15.0.0-rc.0+sha-3a18398');
 
 /**
  * @license
--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.0+sha-44c1770
+ * @license Angular v15.0.0-rc.0+sha-3a18398
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.0+sha-44c1770');
+const VERSION = new Version('15.0.0-rc.0+sha-3a18398');
 
 /**
  * @license
