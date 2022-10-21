eb029c6460746a4e1c0552221805ff1cfb4924b9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-21 15:52:55 +0000

    a42c8cfc67 docs: fix minor typo in hierarchical-dependency-injection.md (#47812)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.7+sha-4d18ba1
+ * @license Angular v14.2.7+sha-a42c8cf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7635,7 +7635,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.7+sha-4d18ba1');
+const VERSION = new Version('14.2.7+sha-a42c8cf');
 
 /**
  * @license
