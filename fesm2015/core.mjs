30485b27178f1f66a3d419cd403d6f5387c1c7a9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-21 17:31:23 +0000

    a014ca1760 docs: fix linter errors in user-input.md (#47832)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.0+sha-fb273f6
+ * @license Angular v15.0.0-rc.0+sha-a014ca1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.0+sha-fb273f6');
+const VERSION = new Version('15.0.0-rc.0+sha-a014ca1');
 
 /**
  * @license
