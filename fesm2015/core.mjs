a340ac42170d00e48e4c6093f0247bb3b7cc5ed4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 14:00:52 +0000

    41709d8aff docs: removing http-server section in Service worker tour (#47613)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-e1bf693
+ * @license Angular v15.1.0-next.0+sha-41709d8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-e1bf693');
+const VERSION = new Version('15.1.0-next.0+sha-41709d8');
 
 /**
  * @license
