77ea287d62ecef23beb4cc97710ae31a29d0ca93
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 13:08:53 +0000

    54e32dad3e fix(platform-server): align server renderer interface with base renderer (#47868)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-48853be
+ * @license Angular v15.1.0-next.0+sha-54e32da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-48853be');
+const VERSION = new Version('15.1.0-next.0+sha-54e32da');
 
 /**
  * @license
