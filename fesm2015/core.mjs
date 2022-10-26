49111354a13a985eb0ea563eb669ddbc7171a985
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:52:24 +0000

    f24da6baad release: cut the v15.0.0-rc.1 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.0+sha-ea7ff35
+ * @license Angular v15.0.0-rc.1+sha-f24da6b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.0+sha-ea7ff35');
+const VERSION = new Version('15.0.0-rc.1+sha-f24da6b');
 
 /**
  * @license
