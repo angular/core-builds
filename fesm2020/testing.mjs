ccbd8d0dfa3b6c7ed55e8eafa3e812c5d9002234
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 14:52:10 +0000

    f7a1c92bbc build: update cross-repo angular dependencies (#47874)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-0984039
+ * @license Angular v15.1.0-next.0+sha-f7a1c92
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7681,7 +7681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-0984039');
+const VERSION = new Version('15.1.0-next.0+sha-f7a1c92');
 
 /**
  * @license
