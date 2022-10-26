beac9927c83b2e5fed7564cbb52b9b641b960d82
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 17:18:37 +0000

    ea7ff35b30 Revert "build: add atscott to unavailable list for pullapprove (#47658)" (#47877)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.0+sha-603194e
+ * @license Angular v15.0.0-rc.0+sha-ea7ff35
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7681,7 +7681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.0+sha-603194e');
+const VERSION = new Version('15.0.0-rc.0+sha-ea7ff35');
 
 /**
  * @license
