af3dd74fd151b608b5c7b967adb0e63957ad481b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:44:47 +0000

    e78885ee8e docs: release notes for the v14.2.8 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-2acdb08
+ * @license Angular v15.1.0-next.0+sha-e78885e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-2acdb08');
+const VERSION = new Version('15.1.0-next.0+sha-e78885e');
 
 /**
  * @license
