43f016e4bd2de43839849098ce3e1c5653568af9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-27 16:21:19 +0000

    c7bc47c509 docs: update CLI related deprecation information (#47885)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.1+sha-96c0e42
+ * @license Angular v15.0.0-rc.1+sha-c7bc47c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.1+sha-96c0e42');
+const VERSION = new Version('15.0.0-rc.1+sha-c7bc47c');
 
 /**
  * @license
