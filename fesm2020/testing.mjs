a721229bc2bce42c83e00a1a1079593f2ebe2521
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-10-28 09:46:50 +0000

    73693be516 docs: improve grammer in CLI command template (#47891)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-790ee17
+ * @license Angular v15.1.0-next.0+sha-73693be
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7681,7 +7681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-790ee17');
+const VERSION = new Version('15.1.0-next.0+sha-73693be');
 
 /**
  * @license
