1e386df01651c77b7ac43442871c20fceb2d7c45
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-10-28 09:53:26 +0000

    24c93bbd87 release: cut the zone.js-0.12.0 release (#47895)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-73693be
+ * @license Angular v15.1.0-next.0+sha-24c93bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-73693be');
+const VERSION = new Version('15.1.0-next.0+sha-24c93bb');
 
 /**
  * @license
--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-73693be
+ * @license Angular v15.1.0-next.0+sha-24c93bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7681,7 +7681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-73693be');
+const VERSION = new Version('15.1.0-next.0+sha-24c93bb');
 
 /**
  * @license
