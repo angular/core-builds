add2bc16f0e5a6ce66b1e0d3c4ce59356eaf45c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-10-28 09:54:31 +0000

    37ba610449 docs: Linting corrections.  Restructuring or rewording sentences to be shorter and clearer. (#47897)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-24c93bb
+ * @license Angular v15.1.0-next.0+sha-37ba610
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7681,7 +7681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-24c93bb');
+const VERSION = new Version('15.1.0-next.0+sha-37ba610');
 
 /**
  * @license
