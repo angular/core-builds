5a4892fb3793bfbe2560c16fc4a4aa14c2602b75
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-01 12:02:12 +0000

    0d49fe01ea docs(core): update standalone docs for `provideRouter` (#47902)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-11288f2
+ * @license Angular v15.1.0-next.0+sha-0d49fe0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-11288f2');
+const VERSION = new Version('15.1.0-next.0+sha-0d49fe0');
 
 /**
  * @license
--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-11288f2
+ * @license Angular v15.1.0-next.0+sha-0d49fe0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7681,7 +7681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-11288f2');
+const VERSION = new Version('15.1.0-next.0+sha-0d49fe0');
 
 /**
  * @license
