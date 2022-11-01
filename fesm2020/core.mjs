2b43368a0e9566212920a2bcd8d825d0b5821128
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-01 12:02:27 +0000

    925dd73c7e perf(platform-browser): resolve memory leak when using animations with shadow DOM (#47903)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-0d49fe0
+ * @license Angular v15.1.0-next.0+sha-925dd73
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-0d49fe0');
+const VERSION = new Version('15.1.0-next.0+sha-925dd73');
 
 /**
  * @license
