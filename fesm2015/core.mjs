b373e5b62bf297bbe9bddc57831d8bde7715a11c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-11-04 00:03:57 +0000

    bab5f9a18f release: cut the v14.2.9 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.8+sha-f7f354a
+ * @license Angular v14.2.9+sha-bab5f9a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.8+sha-f7f354a');
+const VERSION = new Version('14.2.9+sha-bab5f9a');
 
 /**
  * @license
