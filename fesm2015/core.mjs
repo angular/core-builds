3f3d40e1029939822d80970288ac99dcdad30f62
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:08:35 +0000

    f01ea3c9dd docs: fix lint errors for change detection guides (#47952)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-ce8160e
+ * @license Angular v15.1.0-next.0+sha-f01ea3c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-ce8160e');
+const VERSION = new Version('15.1.0-next.0+sha-f01ea3c');
 
 /**
  * @license
