bce11012ea457db1f2b446ca22c21a268eb5d8ea
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:10:02 +0000

    665af467d8 docs: remove CI testing setups (#47940)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-f01ea3c
+ * @license Angular v15.1.0-next.0+sha-665af46
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-f01ea3c');
+const VERSION = new Version('15.1.0-next.0+sha-665af46');
 
 /**
  * @license
--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-f01ea3c
+ * @license Angular v15.1.0-next.0+sha-665af46
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-f01ea3c');
+const VERSION = new Version('15.1.0-next.0+sha-665af46');
 
 /**
  * @license
