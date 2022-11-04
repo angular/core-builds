3e9b3d8367c5d6ebc522757fd22cb456c30f09e9
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:13:10 +0000

    ad8ccedeca build: enable the branch manager action in the repo (#47942)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-febf29d
+ * @license Angular v15.1.0-next.0+sha-ad8cced
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-febf29d');
+const VERSION = new Version('15.1.0-next.0+sha-ad8cced');
 
 /**
  * @license
