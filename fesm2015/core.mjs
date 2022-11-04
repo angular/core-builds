98c130d765deccb656b5ef3ed42678267cc732ce
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:13:59 +0000

    e7b48da713 fix(http): rename `withLegacyInterceptors` to `withInterceptorsFromDi` (#47901)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.1+sha-173dde7
+ * @license Angular v15.0.0-rc.2+sha-e7b48da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.1+sha-173dde7');
+const VERSION = new Version('15.0.0-rc.2+sha-e7b48da');
 
 /**
  * @license
--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.1+sha-173dde7
+ * @license Angular v15.0.0-rc.2+sha-e7b48da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7295,7 +7295,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.1+sha-173dde7');
+const VERSION = new Version('15.0.0-rc.2+sha-e7b48da');
 
 /**
  * @license
