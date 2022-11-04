72640700dbdd9d963bf7f2d39278e2057f739788
Andrew Kushnir <akushnir@google.com>
2022-11-04 16:46:32 +0000

    bc0fc02016 refactor(router): Warn when provideRoutes is used without provideRouter (#47896)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-549af69
+ * @license Angular v15.1.0-next.0+sha-bc0fc02
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7280,7 +7280,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-549af69');
+const VERSION = new Version('15.1.0-next.0+sha-bc0fc02');
 
 /**
  * @license
