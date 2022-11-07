4ffd6222d10400125edc7f64373a903263f232b8
Andrew Kushnir <akushnir@google.com>
2022-11-07 20:50:14 +0000

    ea3374c16b refactor(router): Simplify routerLink selector (#47941)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-64d6db6
+ * @license Angular v15.1.0-next.0+sha-ea3374c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7681,7 +7681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-64d6db6');
+const VERSION = new Version('15.1.0-next.0+sha-ea3374c');
 
 /**
  * @license
