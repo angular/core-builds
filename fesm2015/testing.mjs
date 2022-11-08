df76ee0227deb0fe497d3f72525053c170ec11cd
Andrew Kushnir <akushnir@google.com>
2022-11-08 18:54:06 +0000

    6438f69f28 docs(common): Merge two ngOptimizedImage docs into one (#47990)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-8e6ec72
+ * @license Angular v15.1.0-next.0+sha-6438f69
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-8e6ec72');
+const VERSION = new Version('15.1.0-next.0+sha-6438f69');
 
 /**
  * @license
