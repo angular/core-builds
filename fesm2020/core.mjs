227370909eb1cb4e6a9335ed07294d6aa88d7d5b
Andrew Kushnir <akushnir@google.com>
2022-11-09 18:13:19 +0000

    a4312e1be5 fix(core): add` zone.js` version `0.12.x` as a valid peer dependency (#48002)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.9+sha-28efb06
+ * @license Angular v14.2.9+sha-a4312e1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7249,7 +7249,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.9+sha-28efb06');
+const VERSION = new Version('14.2.9+sha-a4312e1');
 
 /**
  * @license
