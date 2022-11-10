6d5a6a2f4e5978bb892cbd14120c12e22ae754ee
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-10 17:54:17 +0000

    1d1e33e8d0 fix(common): Add fetchpriority to ngOptimizedImage preloads (#48010)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-4a05cfd
+ * @license Angular v15.1.0-next.0+sha-1d1e33e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9131,7 +9131,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-4a05cfd');
+const VERSION = new Version('15.1.0-next.0+sha-1d1e33e');
 
 /**
  * @license
