40c5586101a7a0ea91b4a3589295b43533d52677
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-12 00:03:55 +0000

    954f700ab4 docs(common): Refine NgOptimizedImage docs (#48035)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-ec8b52a
+ * @license Angular v15.1.0-next.0+sha-954f700
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9131,7 +9131,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-ec8b52a');
+const VERSION = new Version('15.1.0-next.0+sha-954f700');
 
 /**
  * @license
