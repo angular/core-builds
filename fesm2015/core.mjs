14922450f3e91c5019b464a6134acada84dbbd46
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 19:48:24 +0000

    11809badb2 docs: remove version 12 from actively supported versions (#48041)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.10+sha-1248992
+ * @license Angular v14.2.10+sha-11809ba
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7234,7 +7234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.2.10+sha-1248992');
+const VERSION = new Version('14.2.10+sha-11809ba');
 
 /**
  * @license
