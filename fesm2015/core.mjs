1baf5ada46aacd62465e9a3ed93452873bf299ee
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 17:40:18 +0000

    d6dc6b6349 test(common): add e2e tests for the fill mode checks in the NgOptimizedImage directive (#48036)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-954f700
+ * @license Angular v15.1.0-next.0+sha-d6dc6b6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9131,7 +9131,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-954f700');
+const VERSION = new Version('15.1.0-next.0+sha-d6dc6b6');
 
 /**
  * @license
