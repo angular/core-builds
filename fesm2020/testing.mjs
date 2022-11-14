b530d354222bc1bcf17743172219e08b248776d7
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 19:45:43 +0000

    804b85554c fix(common): export the IMAGE_CONFIG token (#48051)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-8602f16
+ * @license Angular v15.1.0-next.0+sha-804b855
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9531,7 +9531,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-8602f16');
+const VERSION = new Version('15.1.0-next.0+sha-804b855');
 
 /**
  * @license
