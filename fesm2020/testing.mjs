bf50219624c702ea4879941568d6916d314edb12
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-11-14 20:26:17 +0000

    38b2ed314d docs: release notes for the v15.0.0-rc.4 release

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-804b855
+ * @license Angular v15.1.0-next.0+sha-38b2ed3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9531,7 +9531,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-804b855');
+const VERSION = new Version('15.1.0-next.0+sha-38b2ed3');
 
 /**
  * @license
