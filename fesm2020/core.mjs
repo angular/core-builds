623a8f221cbf7c274f846d296341917f84d89e5a
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-16 22:53:50 +0000

    c9cb0c014f docs: Add v15 release blog post link to changelog (#48094)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-893ec32
+ * @license Angular v15.1.0-next.0+sha-c9cb0c0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9145,7 +9145,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-893ec32');
+const VERSION = new Version('15.1.0-next.0+sha-c9cb0c0');
 
 /**
  * @license
