fce21a61cce49c14ceb54928b00d06b01164ca60
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-11-16 18:01:20 +0000

    b79ad226b2 release: cut the v15.0.0 release

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-rc.4+sha-bc97322
+ * @license Angular v15.0.0+sha-b79ad22
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9531,7 +9531,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0-rc.4+sha-bc97322');
+const VERSION = new Version('15.0.0+sha-b79ad22');
 
 /**
  * @license
