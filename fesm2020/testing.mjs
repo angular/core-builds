98f197499860a54032c8f89cd14c40d833a8c033
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:54:12 +0000

    f8f8928210 fix(devtools): prevent devTools to load when not text/html document (#48021)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-779a76f
+ * @license Angular v15.1.0-next.0+sha-f8f8928
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9522,7 +9522,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-779a76f');
+const VERSION = new Version('15.1.0-next.0+sha-f8f8928');
 
 /**
  * @license
