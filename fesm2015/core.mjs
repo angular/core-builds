6fd7e2f826ecfd1be079ff90e482711f744be707
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:48:00 +0000

    80e3c28995 build: update dependency @nguniversal/builders to v15 (#48093)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-0a8b8a6
+ * @license Angular v15.1.0-next.0+sha-80e3c28
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9131,7 +9131,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-0a8b8a6');
+const VERSION = new Version('15.1.0-next.0+sha-80e3c28');
 
 /**
  * @license
