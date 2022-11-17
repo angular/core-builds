eda9a3fdd98bff62513e8810074605595a063e71
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 19:20:04 +0000

    ef6dbc8850 refactor(compiler): Add `getPotentialPipes` API method. (#48090)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-1df0ed7
+ * @license Angular v15.1.0-next.0+sha-ef6dbc8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9136,7 +9136,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-1df0ed7');
+const VERSION = new Version('15.1.0-next.0+sha-ef6dbc8');
 
 /**
  * @license
