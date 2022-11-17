20d3ecf3e10a35e3fb3bbe4c472f344e0e8bf0e8
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:49:13 +0000

    79bd0f1e96 build(docs-infra): upgrade cli command docs sources to 6ccbc6b4c (#48076)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-80e3c28
+ * @license Angular v15.1.0-next.0+sha-79bd0f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9131,7 +9131,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-80e3c28');
+const VERSION = new Version('15.1.0-next.0+sha-79bd0f1');
 
 /**
  * @license
