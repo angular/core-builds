23def6a8f592277aac22c48a205e4a0884dd42f2
Dylan Hunn <dylhunn@gmail.com>
2022-11-18 20:45:20 +0000

    b653ee340e docs: fix typo in image-directive.md (#48115)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-8d81fcb
+ * @license Angular v15.1.0-next.0+sha-b653ee3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9525,7 +9525,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-8d81fcb');
+const VERSION = new Version('15.1.0-next.0+sha-b653ee3');
 
 /**
  * @license
