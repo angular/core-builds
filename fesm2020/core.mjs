a5752a901cb212a0b8fb010e2bf1decb2d7ed11a
Dylan Hunn <dylhunn@gmail.com>
2022-11-18 20:33:47 +0000

    57d9296802 Revert "refactor(router): Simplify router defaults by making the defaults Injectable" (#48121)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-764fa3d
+ * @license Angular v15.1.0-next.0+sha-57d9296
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9139,7 +9139,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-764fa3d');
+const VERSION = new Version('15.1.0-next.0+sha-57d9296');
 
 /**
  * @license
