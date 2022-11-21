a2f9003132c6ffabeeba0557c4c9d4ad4d5a3e4e
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 21:34:17 +0000

    ed50032316 docs: release notes for the v13.3.12 release

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-6618b25
+ * @license Angular v15.1.0-next.0+sha-ed50032
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9526,7 +9526,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-6618b25');
+const VERSION = new Version('15.1.0-next.0+sha-ed50032');
 
 /**
  * @license
