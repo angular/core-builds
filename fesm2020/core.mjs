e0e1ccdd2405077b5baefbbdb9a681b923b63c8f
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 17:40:49 +0000

    890feb9332 Revert "refactor(router): Remove promise catch and re-reject (#48067)" (#48135)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0+sha-bf724a2
+ * @license Angular v15.0.0+sha-890feb9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9139,7 +9139,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.0+sha-bf724a2');
+const VERSION = new Version('15.0.0+sha-890feb9');
 
 /**
  * @license
