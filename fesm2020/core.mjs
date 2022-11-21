56f160637e179faf87dcafcf8ad2c20f793c3cd7
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 22:01:49 +0000

    80d0cb5b5b docs: add version 15 to active version (#48148)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-29b198f
+ * @license Angular v15.1.0-next.0+sha-80d0cb5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9139,7 +9139,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-29b198f');
+const VERSION = new Version('15.1.0-next.0+sha-80d0cb5');
 
 /**
  * @license
