be3c394d265ac46d73451b21f0bf52424c289c4e
GitHub <noreply@github.com>
2022-11-22 22:32:50 +0000

    eccda3203c docs: fix errors in 12.2.17 changelog

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-903917b
+ * @license Angular v15.1.0-next.0+sha-eccda32
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9125,7 +9125,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-903917b');
+const VERSION = new Version('15.1.0-next.0+sha-eccda32');
 
 /**
  * @license
