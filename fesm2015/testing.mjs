ac3f618d099ffdd6b3fb056c7eff65ea2ff4028c
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:54:55 +0000

    7fbb53f52d docs(forms): fix typos in removeValidators and hasValidator usage notes (#48144)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-bc148c3
+ * @license Angular v15.1.0-next.0+sha-7fbb53f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9526,7 +9526,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-bc148c3');
+const VERSION = new Version('15.1.0-next.0+sha-7fbb53f');
 
 /**
  * @license
--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-bc148c3
+ * @license Angular v15.1.0-next.0+sha-7fbb53f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9139,7 +9139,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-bc148c3');
+const VERSION = new Version('15.1.0-next.0+sha-7fbb53f');
 
 /**
  * @license
