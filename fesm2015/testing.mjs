7da4f672b930a0b27599ad6d55df77815170480a
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:51:28 +0000

    73f03ad2d2 feat(router): Add new NavigationSkipped event for ignored navigations (#48024)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-ffc427b
+ * @license Angular v15.1.0-next.0+sha-73f03ad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9526,7 +9526,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-ffc427b');
+const VERSION = new Version('15.1.0-next.0+sha-73f03ad');
 
 /**
  * @license
