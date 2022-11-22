510c2f9a9315b2d68db6397337684e2393b8d9bc
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:57:13 +0000

    4f03a9c327 docs: fix broken link in v15 deprecations table (#48157)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-83f0b7b
+ * @license Angular v15.1.0-next.0+sha-4f03a9c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9526,7 +9526,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-83f0b7b');
+const VERSION = new Version('15.1.0-next.0+sha-4f03a9c');
 
 /**
  * @license
