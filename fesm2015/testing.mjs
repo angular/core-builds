3b5010c2c7b1fcbbdff46bdaaca0418b1bced2a8
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:56:24 +0000

    83f0b7b7b2 refactor(router): Update RouterOutlet to not read internal route property (#48112)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-5127a4d
+ * @license Angular v15.1.0-next.0+sha-83f0b7b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9526,7 +9526,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-5127a4d');
+const VERSION = new Version('15.1.0-next.0+sha-83f0b7b');
 
 /**
  * @license
