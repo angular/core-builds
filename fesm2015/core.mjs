65269f03c629c8f8f0b2d4052f4589aee37f6b88
Joey Perrott <josephperrott.github@gmail.com>
2022-11-23 19:55:16 +0000

    2e38af1f48 build(bazel): fix aio deployment unit tests

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-73ad3bb
+ * @license Angular v15.1.0-next.0+sha-2e38af1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9125,7 +9125,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-73ad3bb');
+const VERSION = new Version('15.1.0-next.0+sha-2e38af1');
 
 /**
  * @license
