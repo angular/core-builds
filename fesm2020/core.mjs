3965a4832744686d0a13cac2a234e74b8ab008ae
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 19:12:02 +0000

    73ad3bb9c8 build: update scorecard action dependencies (#47866)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-a5c4e3d
+ * @license Angular v15.1.0-next.0+sha-73ad3bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9139,7 +9139,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-a5c4e3d');
+const VERSION = new Version('15.1.0-next.0+sha-73ad3bb');
 
 /**
  * @license
--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-a5c4e3d
+ * @license Angular v15.1.0-next.0+sha-73ad3bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9525,7 +9525,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-a5c4e3d');
+const VERSION = new Version('15.1.0-next.0+sha-73ad3bb');
 
 /**
  * @license
