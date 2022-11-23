3c9eb28a2905eea5d8611f796b7b5c939433237d
Joey Perrott <josephperrott.github@gmail.com>
2022-11-23 21:59:01 +0000

    b258b32f55 build(bazel): copy dist folder to aio for firebase deployment

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-e864254
+ * @license Angular v15.1.0-next.0+sha-b258b32
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9125,7 +9125,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-e864254');
+const VERSION = new Version('15.1.0-next.0+sha-b258b32');
 
 /**
  * @license
