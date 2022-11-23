0e363c861e0022f7a29b5f0fb2d0d96ec4891e46
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 03:37:05 +0000

    332461bd0c feat(router): Add ability to override `onSameUrlNavigation` default per-navigation (#48050)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-6a8ab30
+ * @license Angular v15.1.0-next.0+sha-332461b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9139,7 +9139,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-6a8ab30');
+const VERSION = new Version('15.1.0-next.0+sha-332461b');
 
 /**
  * @license
