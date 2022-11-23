d74f959689189921516ff16c0783c901f49214a1
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 03:14:00 +0000

    cadc011f63 release: cut the v15.1.0-next.0 release

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-6acae14
+ * @license Angular v15.1.0-next.0+sha-cadc011
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9526,7 +9526,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-6acae14');
+const VERSION = new Version('15.1.0-next.0+sha-cadc011');
 
 /**
  * @license
