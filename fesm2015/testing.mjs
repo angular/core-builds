ac5f9deb503759f7ddc95e669e325c4592523c91
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 17:44:31 +0000

    0138d75335 refactor(language-service): make selector nullable (#48193)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-6948f0f
+ * @license Angular v15.1.0-next.0+sha-0138d75
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9526,7 +9526,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-6948f0f');
+const VERSION = new Version('15.1.0-next.0+sha-0138d75');
 
 /**
  * @license
