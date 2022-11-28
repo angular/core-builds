9f7dc9ce11e0ae0f56580be915d69b34e9706e34
Andrew Scott <atscott01@gmail.com>
2022-11-28 21:26:44 +0000

    c0601db72c test(core): remove some ViewEngine related test code (#48238)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.1+sha-6eb2f76
+ * @license Angular v15.0.1+sha-c0601db
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9520,7 +9520,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.1+sha-6eb2f76');
+const VERSION = new Version('15.0.1+sha-c0601db');
 
 /**
  * @license
