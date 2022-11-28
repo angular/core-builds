a9618e9e713c75a4529d1286eb1d6350281cd300
Andrew Scott <atscott@google.com>
2022-11-28 17:30:22 +0000

    ad483007b4 refactor(platform-server): remove `Injector` dependency for `initDominoAdapter` factory (#48231)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-7368ee1
+ * @license Angular v15.1.0-next.0+sha-ad48300
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9134,7 +9134,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-7368ee1');
+const VERSION = new Version('15.1.0-next.0+sha-ad48300');
 
 /**
  * @license
