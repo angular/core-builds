84a73cfec688b3ffc98bd14cd5985009e5a26942
Andrew Scott <atscott@google.com>
2022-11-28 17:31:40 +0000

    fcf857209f docs: removed empty methods (#48230)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-ad48300
+ * @license Angular v15.1.0-next.0+sha-fcf8572
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9520,7 +9520,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-ad48300');
+const VERSION = new Version('15.1.0-next.0+sha-fcf8572');
 
 /**
  * @license
