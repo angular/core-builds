20e3788760825875ddd8be61e9bb4a02fac58ac3
Andrew Scott <atscott01@gmail.com>
2022-11-29 20:46:13 +0000

    d4c3584d88 build: update github/codeql-action action to v2.1.34 (#48272)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-535838d
+ * @license Angular v15.1.0-next.0+sha-d4c3584
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9521,7 +9521,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-535838d');
+const VERSION = new Version('15.1.0-next.0+sha-d4c3584');
 
 /**
  * @license
