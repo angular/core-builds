5cf251a569df8a62b8bb273888629bf3fe3af22d
Andrew Scott <atscott01@gmail.com>
2022-11-29 20:46:27 +0000

    208f52209b build: update eslint dependencies (#48216)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-d4c3584
+ * @license Angular v15.1.0-next.0+sha-208f522
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9521,7 +9521,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-d4c3584');
+const VERSION = new Version('15.1.0-next.0+sha-208f522');
 
 /**
  * @license
