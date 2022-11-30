db01464df76d5a060feae1305cc643407dbd50b6
Andrew Scott <atscott01@gmail.com>
2022-11-30 20:41:07 +0000

    33fcb0f171 docs: release notes for the v15.0.2 release

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-b935255
+ * @license Angular v15.1.0-next.0+sha-33fcb0f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9511,7 +9511,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-b935255');
+const VERSION = new Version('15.1.0-next.0+sha-33fcb0f');
 
 /**
  * @license
