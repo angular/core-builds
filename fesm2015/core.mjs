659f3a62a124199f4439a921558affc9c5e8ac10
Andrew Scott <atscott01@gmail.com>
2022-11-30 17:43:31 +0000

    e777588b05 build: update dependency @types/uuid to v9 (#48285)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-d5aaf55
+ * @license Angular v15.1.0-next.0+sha-e777588
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9120,7 +9120,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.0+sha-d5aaf55');
+const VERSION = new Version('15.1.0-next.0+sha-e777588');
 
 /**
  * @license
