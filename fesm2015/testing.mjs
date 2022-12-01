183cefc22e169cc83aaa31ef17a47b86b6dc7efe
Andrew Kushnir <akushnir@google.com>
2022-12-01 22:01:28 +0000

    27eaded62d fix(compiler-cli): Produce diagnostic rather than crash when using invalid hostDirective (#48314)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-3842157
+ * @license Angular v15.1.0-next.1+sha-27eaded
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9520,7 +9520,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-3842157');
+const VERSION = new Version('15.1.0-next.1+sha-27eaded');
 
 /**
  * @license
