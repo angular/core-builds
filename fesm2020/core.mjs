1f0b0f2fe83a8e2a49ca3351729e87f1b5354215
Andrew Kushnir <akushnir@google.com>
2022-12-01 17:56:17 +0000

    b620d9b316 docs: removed empty method (#48236)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.2+sha-73ec0c6
+ * @license Angular v15.0.2+sha-b620d9b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9125,7 +9125,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.2+sha-73ec0c6');
+const VERSION = new Version('15.0.2+sha-b620d9b');
 
 /**
  * @license
