711cbbac6f5677a17c63b8af4074399fb903059f
Andrew Kushnir <akushnir@google.com>
2022-12-02 20:08:46 +0000

    2131c551cd docs: added `missingControlFlowDirective` extended diagnostics details page (#48319)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.2+sha-2eccfd4
+ * @license Angular v15.0.2+sha-2131c55
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9511,7 +9511,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.2+sha-2eccfd4');
+const VERSION = new Version('15.0.2+sha-2131c55');
 
 /**
  * @license
