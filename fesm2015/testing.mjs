804ba9639a4470a53aaa7d255fc1ded835e6d666
Andrew Kushnir <akushnir@google.com>
2022-12-02 18:06:15 +0000

    2eccfd412e build: fix unnecessary large runfiles in AIO when composing boilerplate (#48329)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.2+sha-c0fe820
+ * @license Angular v15.0.2+sha-2eccfd4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9512,7 +9512,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.2+sha-c0fe820');
+const VERSION = new Version('15.0.2+sha-2eccfd4');
 
 /**
  * @license
