d81a58a92dbdaa7354d02c3d24f4213cfe911d62
Andrew Kushnir <akushnir@google.com>
2022-12-02 23:13:49 +0000

    7b7d5e1199 docs: added `missingNgForOfLet` extended diagnostics details page (#48320)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-4bff346
+ * @license Angular v15.1.0-next.1+sha-7b7d5e1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9520,7 +9520,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-4bff346');
+const VERSION = new Version('15.1.0-next.1+sha-7b7d5e1');
 
 /**
  * @license
