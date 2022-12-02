8c25e834c6b9ee1deb17b0234ae016f562206e49
Andrew Kushnir <akushnir@google.com>
2022-12-02 17:17:46 +0000

    21952bbd0f docs: Add page for 'suffixNotSupported' diagnostic (#48315)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-b53e1ed
+ * @license Angular v15.1.0-next.1+sha-21952bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9133,7 +9133,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-b53e1ed');
+const VERSION = new Version('15.1.0-next.1+sha-21952bb');
 
 /**
  * @license
--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-b53e1ed
+ * @license Angular v15.1.0-next.1+sha-21952bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9519,7 +9519,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-b53e1ed');
+const VERSION = new Version('15.1.0-next.1+sha-21952bb');
 
 /**
  * @license
