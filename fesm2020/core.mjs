4774ec1d93c61f6f2dd45f3b8877f3b02f286092
Andrew Kushnir <akushnir@google.com>
2022-12-02 17:17:18 +0000

    b53e1ed4ef build: enable progress messages for Bazel in CircleCI (#48316)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-7f36221
+ * @license Angular v15.1.0-next.1+sha-b53e1ed
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9133,7 +9133,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-7f36221');
+const VERSION = new Version('15.1.0-next.1+sha-b53e1ed');
 
 /**
  * @license
