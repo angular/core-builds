23b9d6667ae3f983afe43183835d54809f2a2d83
Andrew Kushnir <akushnir@google.com>
2022-12-02 23:14:32 +0000

    dd4fd1f4f4 build: attempt to avoid nodejs version flakiness in `test_win` job (#48326)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.2+sha-2131c55
+ * @license Angular v15.0.2+sha-dd4fd1f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9125,7 +9125,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.2+sha-2131c55');
+const VERSION = new Version('15.0.2+sha-dd4fd1f');
 
 /**
  * @license
