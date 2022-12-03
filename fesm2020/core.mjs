83efc5aa422def066c1e4e27da8b24a307b3e659
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-03 16:38:30 +0000

    09fe75d0e9 build: update actions/checkout action to v3 (#48337)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-d1a501e
+ * @license Angular v15.1.0-next.1+sha-09fe75d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9133,7 +9133,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-d1a501e');
+const VERSION = new Version('15.1.0-next.1+sha-09fe75d');
 
 /**
  * @license
