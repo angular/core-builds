f74424035e80898fb083af7919032a0629c5b521
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 11:38:48 +0000

    011ef66a04 build: set firebase deploy target when deploying AIO preview (#48338)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-09fe75d
+ * @license Angular v15.1.0-next.1+sha-011ef66
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9133,7 +9133,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-09fe75d');
+const VERSION = new Version('15.1.0-next.1+sha-011ef66');
 
 /**
  * @license
