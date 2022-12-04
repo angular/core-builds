0a2b1498c8e0fada36a18fc196e3057d4087de3e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 16:33:02 +0000

    38701ce07b build: check out project before setting AIO preview targets (#48347)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.2+sha-09388eb
+ * @license Angular v15.0.2+sha-38701ce
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9512,7 +9512,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.2+sha-09388eb');
+const VERSION = new Version('15.0.2+sha-38701ce');
 
 /**
  * @license
