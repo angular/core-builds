e861589058d6b8829215899379231fcf25e45634
Andrew Kushnir <akushnir@google.com>
2022-12-05 23:12:19 +0000

    ecddcbc269 build: switch new AIO preview action to use `aio: preview` label (#48345)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-65d8803
+ * @license Angular v15.1.0-next.1+sha-ecddcbc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9440,7 +9440,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-65d8803');
+const VERSION = new Version('15.1.0-next.1+sha-ecddcbc');
 
 /**
  * @license
