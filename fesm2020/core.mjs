3a37a9d407d93343a54576891a6cd3a8d6817748
Andrew Kushnir <akushnir@google.com>
2022-12-05 20:31:06 +0000

    408aed7a75 build: enable the ng-dev auth service (#48361)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-c0ddaf8
+ * @license Angular v15.1.0-next.1+sha-408aed7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9133,7 +9133,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-c0ddaf8');
+const VERSION = new Version('15.1.0-next.1+sha-408aed7');
 
 /**
  * @license
