3f9054b40db19a4fea90e509604a6a90b9799438
Andrew Kushnir <akushnir@google.com>
2022-12-06 00:19:59 +0000

    281c614113 build: update dependency jasmine-core to v4 (#48366)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-ecddcbc
+ * @license Angular v15.1.0-next.1+sha-281c614
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9440,7 +9440,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-ecddcbc');
+const VERSION = new Version('15.1.0-next.1+sha-281c614');
 
 /**
  * @license
