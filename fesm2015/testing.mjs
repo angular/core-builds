25ffdab2672f89121fd168213be0c15b6e69e4ec
Andrew Kushnir <akushnir@google.com>
2022-12-06 19:25:44 +0000

    126573d76b build: update all non-major dependencies (#48372)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-57fdc99
+ * @license Angular v15.1.0-next.1+sha-126573d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9417,7 +9417,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-57fdc99');
+const VERSION = new Version('15.1.0-next.1+sha-126573d');
 
 /**
  * @license
