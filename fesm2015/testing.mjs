4a61be8e665d94ba4362c157963615fe143ba2de
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-12-07 22:15:44 +0000

    9e9b90cc39 docs: release notes for the v15.0.3 release

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.1+sha-f8ecc19
+ * @license Angular v15.1.0-next.1+sha-9e9b90c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9400,7 +9400,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.1+sha-f8ecc19');
+const VERSION = new Version('15.1.0-next.1+sha-9e9b90c');
 
 /**
  * @license
