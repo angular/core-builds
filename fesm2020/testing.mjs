711605e7bccbac7f5b4e3acefa24ac19ea4a54c6
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-08 20:51:58 +0000

    77ba4aac00 build: update cross-repo angular dependencies (#48398)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-b55d2da
+ * @license Angular v15.0.3+sha-77ba4aa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9391,7 +9391,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-b55d2da');
+const VERSION = new Version('15.0.3+sha-77ba4aa');
 
 /**
  * @license
