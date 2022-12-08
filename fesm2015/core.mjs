811ca7414028a8045e45876abee5d39fa6e7d1f0
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-08 23:58:45 +0000

    e7707dbf56 docs: move marketing demo out of tested examples (#48404)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-5af7ec2
+ * @license Angular v15.0.3+sha-e7707db
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8991,7 +8991,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-5af7ec2');
+const VERSION = new Version('15.0.3+sha-e7707db');
 
 /**
  * @license
