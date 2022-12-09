f8137f209f91ce6479a1b4273e1821e0523cbcd4
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 18:21:10 +0000

    0fa70dfb6d refactor(router): code cleaning (#48259)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-bbaa850
+ * @license Angular v15.1.0-next.2+sha-0fa70df
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8999,7 +8999,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-bbaa850');
+const VERSION = new Version('15.1.0-next.2+sha-0fa70df');
 
 /**
  * @license
