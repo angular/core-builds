6ae551dd58fb05260e7c909d3b0e3711e92ba396
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 18:04:06 +0000

    d99638f796 build: update gitignore to ignore files which should not be commited. (#48399)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-d300297
+ * @license Angular v15.1.0-next.2+sha-d99638f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9400,7 +9400,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-d300297');
+const VERSION = new Version('15.1.0-next.2+sha-d99638f');
 
 /**
  * @license
