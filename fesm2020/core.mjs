e4a93c8349223b5a6d0233e1bf757d121e0c445a
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 20:59:26 +0000

    dcfbe22434 docs(common): Add note to location about investigating base href handling (#48422)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-04d77f5
+ * @license Angular v15.1.0-next.2+sha-dcfbe22
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9013,7 +9013,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-04d77f5');
+const VERSION = new Version('15.1.0-next.2+sha-dcfbe22');
 
 /**
  * @license
