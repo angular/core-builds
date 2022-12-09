7495e82596a081e78db1681e01ad7c13f45f0582
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 23:19:00 +0000

    528cc457c6 docs: Document extracted from the original Introduction document, which is to be retired. Linting suggested corrections made.  Restructuring or rewording sentences to be shorter and clearer. (#47909)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-7ec97f3
+ * @license Angular v15.1.0-next.2+sha-528cc45
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8999,7 +8999,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-7ec97f3');
+const VERSION = new Version('15.1.0-next.2+sha-528cc45');
 
 /**
  * @license
