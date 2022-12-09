b9900b6a7d9b4e6f5d2b6b24b9b18df174ef0218
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 18:47:52 +0000

    1f3e9d904a refactor(migrations): remove unused migrations (#48414)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-e7707db
+ * @license Angular v15.0.3+sha-1f3e9d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9392,7 +9392,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-e7707db');
+const VERSION = new Version('15.0.3+sha-1f3e9d9');
 
 /**
  * @license
