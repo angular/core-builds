93509a0e2719557f14fd86a4684b9ef45f24e927
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-10 00:23:08 +0000

    6279fecd52 docs: update of landing page and backup pages (#47047)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-c4d17b9
+ * @license Angular v15.0.3+sha-6279fec
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8991,7 +8991,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-c4d17b9');
+const VERSION = new Version('15.0.3+sha-6279fec');
 
 /**
  * @license
--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-c4d17b9
+ * @license Angular v15.0.3+sha-6279fec
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9392,7 +9392,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-c4d17b9');
+const VERSION = new Version('15.0.3+sha-6279fec');
 
 /**
  * @license
