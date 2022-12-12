85808acc0d5ab3e9093c2e53f823300df9f9ebf8
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 23:17:25 +0000

    5bc502fa2c docs: modify directive composition API guide (#48439)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-6dd7575
+ * @license Angular v15.1.0-next.2+sha-5bc502f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9399,7 +9399,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-6dd7575');
+const VERSION = new Version('15.1.0-next.2+sha-5bc502f');
 
 /**
  * @license
