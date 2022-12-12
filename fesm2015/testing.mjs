6dcba668734dce6b7eb327c9f86c1d7d37cde040
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 22:39:52 +0000

    6dd7575d74 docs: fix typo in search keywords guide (#48442) PR Close #48442

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-cc5b044
+ * @license Angular v15.1.0-next.2+sha-6dd7575
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9400,7 +9400,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-cc5b044');
+const VERSION = new Version('15.1.0-next.2+sha-6dd7575');
 
 /**
  * @license
