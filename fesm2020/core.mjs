c566567987f6456f4263dec400b37711938bc356
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 23:40:10 +0000

    6c1064c72f fix(animations): fix incorrect handling of camel-case css properties (#48436)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-8ca95b8
+ * @license Angular v15.0.3+sha-6c1064c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9005,7 +9005,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-8ca95b8');
+const VERSION = new Version('15.0.3+sha-6c1064c');
 
 /**
  * @license
