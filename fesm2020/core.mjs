69ebff63906370cb9fef2e977ee7ce8a6c18849b
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 00:45:41 +0000

    abd11a6ae9 docs: add docgeni to tooling of resources list (#48061)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-e0d62cb
+ * @license Angular v15.1.0-next.2+sha-abd11a6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9013,7 +9013,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-e0d62cb');
+const VERSION = new Version('15.1.0-next.2+sha-abd11a6');
 
 /**
  * @license
