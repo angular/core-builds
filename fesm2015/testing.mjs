670abee36105cdeacb2b2eb849f456a80bdc51e7
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 00:38:11 +0000

    e0d62cb734 Revert "fix(common): Update `Location` to get a normalized URL valid in case a represented URL starts with the substring equals `APP_BASE_HREF`" (#48461)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.2+sha-c864845
+ * @license Angular v15.1.0-next.2+sha-e0d62cb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9400,7 +9400,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.2+sha-c864845');
+const VERSION = new Version('15.1.0-next.2+sha-e0d62cb');
 
 /**
  * @license
