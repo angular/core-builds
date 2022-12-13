4188f8b5ff34a1a0db393482af42974d67aada23
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 23:30:41 +0000

    aabf8be033 docs: edit suggestion to meet style guide (#47979) PR Close #47979

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-d181b93
+ * @license Angular v15.0.3+sha-aabf8be
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9005,7 +9005,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-d181b93');
+const VERSION = new Version('15.0.3+sha-aabf8be');
 
 /**
  * @license
