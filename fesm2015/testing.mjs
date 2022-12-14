d7b51336a4f15f0022e5b454fa944abed17b18a2
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-14 01:16:11 +0000

    9291bd950f refactor(router): Add opt-in provider for upcoming router change (#47988)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.3+sha-aabf8be
+ * @license Angular v15.0.3+sha-9291bd9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9392,7 +9392,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.3+sha-aabf8be');
+const VERSION = new Version('15.0.3+sha-9291bd9');
 
 /**
  * @license
