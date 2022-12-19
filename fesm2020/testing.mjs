af67f895facab1e31a2a5001e1d640a5117fc712
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-19 20:37:26 +0000

    a3958a2a76 build: update google sync config to match internal changes (#48538)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.4+sha-470738c
+ * @license Angular v15.0.4+sha-a3958a2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9391,7 +9391,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.4+sha-470738c');
+const VERSION = new Version('15.0.4+sha-a3958a2');
 
 /**
  * @license
