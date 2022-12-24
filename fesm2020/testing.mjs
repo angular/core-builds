138f8d70c00fe7385c6265fd41380df92fccced8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-24 11:50:22 +0000

    8c9f067125 build(bazel): prevent remote cache misses on dgeni build (#48585)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-27da733
+ * @license Angular v15.1.0-next.3+sha-8c9f067
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9399,7 +9399,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-27da733');
+const VERSION = new Version('15.1.0-next.3+sha-8c9f067');
 
 /**
  * @license
