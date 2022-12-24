ee330b85588807478dc45250d712afe320718068
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-24 11:45:22 +0000

    125b3df884 ci: remove redundant aio dependencies installation step (#48574)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.4+sha-ab13c96
+ * @license Angular v15.0.4+sha-125b3df
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9392,7 +9392,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.4+sha-ab13c96');
+const VERSION = new Version('15.0.4+sha-125b3df');
 
 /**
  * @license
