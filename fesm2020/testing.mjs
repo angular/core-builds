f3930d6f635e38120270fbd17ba8f84658be0557
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-02 12:36:06 +0000

    cc284afbbc fix(migrations): combine newly-added imports in import manager (#48620)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-0ae80a7
+ * @license Angular v15.1.0-next.3+sha-cc284af
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9399,7 +9399,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-0ae80a7');
+const VERSION = new Version('15.1.0-next.3+sha-cc284af');
 
 /**
  * @license
