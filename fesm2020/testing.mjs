8f8d176f6da0940e7a44c9dcb13e3509be136315
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-03 16:48:38 +0000

    caedef0f5b fix(compiler-cli): update `@babel/core` dependency and lock version (#48634)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-e38bc6b
+ * @license Angular v15.1.0-next.3+sha-caedef0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-e38bc6b');
+const VERSION = new Version('15.1.0-next.3+sha-caedef0');
 
 // This default value is when checking the hierarchy for a token.
 //
