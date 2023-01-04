140d99274253e3c68258eba82e888f0f1a3bbff3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:36:32 +0000

    c51e3e33ce build: update eslint dependencies to v5.47.0 (#48548)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-a532d71
+ * @license Angular v15.1.0-next.3+sha-c51e3e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-a532d71');
+const VERSION = new Version('15.1.0-next.3+sha-c51e3e3');
 
 // This default value is when checking the hierarchy for a token.
 //
