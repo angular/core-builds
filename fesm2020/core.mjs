f3c285154d7b12f13cacffd39d201f16daadcb09
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:25:27 +0000

    b3fca32a79 refactor(common): remove unnecessary parseFloat from ng_optimized_image directive. (#48527)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-687ab04
+ * @license Angular v15.1.0-next.3+sha-b3fca32
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-687ab04');
+const VERSION = new Version('15.1.0-next.3+sha-b3fca32');
 
 // This default value is when checking the hierarchy for a token.
 //
