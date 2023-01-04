f22ccb31bd79c3508f7dc08052aefe41e77c02a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 19:50:40 +0000

    aa51e9b217 build: update all non-major dependencies (#48462)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-c5a8485
+ * @license Angular v15.1.0-next.3+sha-aa51e9b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-c5a8485');
+const VERSION = new Version('15.1.0-next.3+sha-aa51e9b');
 
 // This default value is when checking the hierarchy for a token.
 //
