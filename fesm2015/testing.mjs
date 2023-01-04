c8fd1259aeb0e4cce049d59cf28a43be127b25f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:34:30 +0000

    c8310a842d refactor(core): cleanup type any (#48623)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.3+sha-29efedf
+ * @license Angular v15.1.0-next.3+sha-c8310a8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-next.3+sha-29efedf');
+const VERSION = new Version('15.1.0-next.3+sha-c8310a8');
 
 // This default value is when checking the hierarchy for a token.
 //
