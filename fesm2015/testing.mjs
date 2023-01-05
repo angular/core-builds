0713731d8b829b5b5c801a2c7546f833782b6864
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:06:39 +0000

    43ced455d6 docs: remove empty constructors & ngOnInit() (#48576)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-d887d69
+ * @license Angular v15.2.0-next.0+sha-43ced45
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9400,7 +9400,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-d887d69');
+const VERSION = new Version('15.2.0-next.0+sha-43ced45');
 
 /**
  * @license
