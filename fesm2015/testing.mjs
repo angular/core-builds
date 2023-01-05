26713b79ae72f2610c6fe350ee932c8b140f65c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:06:54 +0000

    15eccef4eb docs(router): Clean up error handler deprecations (#48551)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-43ced45
+ * @license Angular v15.2.0-next.0+sha-15eccef
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9400,7 +9400,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-43ced45');
+const VERSION = new Version('15.2.0-next.0+sha-15eccef');
 
 /**
  * @license
