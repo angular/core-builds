e53073892c688d97530219a0fea10ffe6c4f788e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:07:44 +0000

    10de92c68a refactor(forms): cleanup type any in forms tests (#48624)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.4+sha-c001528
+ * @license Angular v15.0.4+sha-10de92c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.4+sha-c001528');
+const VERSION = new Version('15.0.4+sha-10de92c');
 
 // This default value is when checking the hierarchy for a token.
 //
