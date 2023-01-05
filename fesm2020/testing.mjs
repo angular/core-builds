9509d1335e966662b5646c3961941bafd19b8c8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:17:19 +0000

    61fc47475d refactor(forms): cleanup type any in forms tests (#48624)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-rc.0+sha-28ea114
+ * @license Angular v15.1.0-rc.0+sha-61fc474
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9399,7 +9399,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-rc.0+sha-28ea114');
+const VERSION = new Version('15.1.0-rc.0+sha-61fc474');
 
 /**
  * @license
