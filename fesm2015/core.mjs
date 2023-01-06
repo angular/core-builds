a446faf24944198018ae84fdb7a4dd72ee88d458
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-06 00:44:43 +0000

    bdf288dcbf fix(forms): Form provider FormsModule.withConfig return a FormsModule (#48526)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-b9577ad
+ * @license Angular v15.2.0-next.0+sha-bdf288d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-b9577ad');
+const VERSION = new Version('15.2.0-next.0+sha-bdf288d');
 
 // This default value is when checking the hierarchy for a token.
 //
