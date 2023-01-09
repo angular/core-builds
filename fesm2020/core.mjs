28016ec5ec00725933f99dc66368a75086585048
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-09 18:25:42 +0000

    db8b3b7ffa fix(language-service): update packages/language-service/build.sh script to work with vscode-ng-language-service's new Bazel build (#48663)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-9fc950d
+ * @license Angular v15.2.0-next.0+sha-db8b3b7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-9fc950d');
+const VERSION = new Version('15.2.0-next.0+sha-db8b3b7');
 
 // This default value is when checking the hierarchy for a token.
 //
