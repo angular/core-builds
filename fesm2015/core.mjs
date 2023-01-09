be42244ab84801a668091fccfc6adbe184f73c62
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-09 18:25:37 +0000

    9fc950daaf fix(language-service): ship `/api` entry-point (#48670)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-b6cc5f7
+ * @license Angular v15.2.0-next.0+sha-9fc950d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-b6cc5f7');
+const VERSION = new Version('15.2.0-next.0+sha-9fc950d');
 
 // This default value is when checking the hierarchy for a token.
 //
