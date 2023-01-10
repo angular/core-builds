ce777179bf22749f9f6f8e118d25e9af6f161fce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:26:58 +0000

    111e2c7876 test(docs-infra): increase jasmine default timeout for e2e specs (#48671)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-579cbb2
+ * @license Angular v15.2.0-next.0+sha-111e2c7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-579cbb2');
+const VERSION = new Version('15.2.0-next.0+sha-111e2c7');
 
 // This default value is when checking the hierarchy for a token.
 //
