f56e594c05d064aa72eacd127042c845aef47fb3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 22:42:10 +0000

    4bd926edd9 ci: expose `ANGULAR_CLI_BUILDS_READONLY_GITHUB_TOKEN` secret as env (#48687)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-d4a5672
+ * @license Angular v15.2.0-next.0+sha-4bd926e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8999,7 +8999,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-d4a5672');
+const VERSION = new Version('15.2.0-next.0+sha-4bd926e');
 
 /**
  * @license
