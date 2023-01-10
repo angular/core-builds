7d2342bcc79e0778cab041adf9c9369157967783
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:33:50 +0000

    caf7228f8a fix(compiler): resolve deprecation warning (#48652)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-rc.0+sha-e615b59
+ * @license Angular v15.1.0-rc.0+sha-caf7228
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0-rc.0+sha-e615b59');
+const VERSION = new Version('15.1.0-rc.0+sha-caf7228');
 
 // This default value is when checking the hierarchy for a token.
 //
