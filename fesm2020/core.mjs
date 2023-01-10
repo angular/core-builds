d27500ea6ed682cf484198f6ed687d1cba4a34e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 19:25:09 +0000

    d4a56721a4 ci: use readonly github token for querying CLI snapshot repo (#48681)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-45029f1
+ * @license Angular v15.2.0-next.0+sha-d4a5672
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-45029f1');
+const VERSION = new Version('15.2.0-next.0+sha-d4a5672');
 
 // This default value is when checking the hierarchy for a token.
 //
