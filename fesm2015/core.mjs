c0dc1889dd0edc296757c1ff60ef88f2dd865533
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:34:57 +0000

    d4eaaefe30 fix(docs-infra): correctly read example type (#48665)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-111e2c7
+ * @license Angular v15.2.0-next.0+sha-d4eaaef
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-111e2c7');
+const VERSION = new Version('15.2.0-next.0+sha-d4eaaef');
 
 // This default value is when checking the hierarchy for a token.
 //
