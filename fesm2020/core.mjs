cac16d052d5a7650df8fd583f71012d10985e2b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:14:30 +0000

    123c95209f docs(compiler-cli): fix commands to run compliance unit test (#48559)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-a3a4ca0
+ * @license Angular v15.2.0-next.0+sha-123c952
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-a3a4ca0');
+const VERSION = new Version('15.2.0-next.0+sha-123c952');
 
 // This default value is when checking the hierarchy for a token.
 //
