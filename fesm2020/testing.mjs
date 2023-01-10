7c178d7a3ae49680f3e6f91bb3bf307726c73078
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:11:13 +0000

    edd7982d51 refactor(bazel): disables the Tsickle decorator transform (#47934)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-db8b3b7
+ * @license Angular v15.2.0-next.0+sha-edd7982
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-db8b3b7');
+const VERSION = new Version('15.2.0-next.0+sha-edd7982');
 
 // This default value is when checking the hierarchy for a token.
 //
