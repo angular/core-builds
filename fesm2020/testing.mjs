f3edb93e361bfd4ad02850f2634287f4a7cd8208
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 17:29:07 +0000

    ae34e6cabd fix(language-service): expose `package.json` for vscode extension resolution (#48678)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.4+sha-aa31d61
+ * @license Angular v15.0.4+sha-ae34e6c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9391,7 +9391,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.0.4+sha-aa31d61');
+const VERSION = new Version('15.0.4+sha-ae34e6c');
 
 /**
  * @license
