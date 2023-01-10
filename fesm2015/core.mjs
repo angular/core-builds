b502e856b23970c6a5c30c4452d91faeacb91a64
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:25:27 +0000

    579cbb20d9 refactor(platform-browser): prepare the code to use `MockPlatformLocation` by default (#48651)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-123c952
+ * @license Angular v15.2.0-next.0+sha-579cbb2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-123c952');
+const VERSION = new Version('15.2.0-next.0+sha-579cbb2');
 
 // This default value is when checking the hierarchy for a token.
 //
