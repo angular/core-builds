263df4252ebf7d550747ce709a8002f06a473633
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 19:04:53 +0000

    45029f111a docs: release notes for the v15.1.0 release

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-489243b
+ * @license Angular v15.2.0-next.0+sha-45029f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-489243b');
+const VERSION = new Version('15.2.0-next.0+sha-45029f1');
 
 // This default value is when checking the hierarchy for a token.
 //
