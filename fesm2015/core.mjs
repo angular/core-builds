af3c1a37c9ad312d92e1ed7fc1d3cde7274e4691
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:13:46 +0000

    a3a4ca0b00 refactor(router): move internal router properties to transition handler where appropriate (#48475)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-edd7982
+ * @license Angular v15.2.0-next.0+sha-a3a4ca0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-edd7982');
+const VERSION = new Version('15.2.0-next.0+sha-a3a4ca0');
 
 // This default value is when checking the hierarchy for a token.
 //
