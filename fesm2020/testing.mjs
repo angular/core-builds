82323d3ee5cd49d5e373a68b69e115473717c246
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-11 15:08:36 +0000

    2b83986a1a ci: correctly interpolate `ANGULAR_CLI_BUILDS_READONLY_GITHUB_TOKEN` (#48690)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-4bd926e
+ * @license Angular v15.2.0-next.0+sha-2b83986
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-4bd926e');
+const VERSION = new Version('15.2.0-next.0+sha-2b83986');
 
 // This default value is when checking the hierarchy for a token.
 //
