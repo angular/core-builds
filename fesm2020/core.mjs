dacdcb3fe2c4947948d61fdbb02c1d24f87f1324
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:17:48 +0000

    926c35f4ac docs: Deprecate class and InjectionToken and resolvers (#47924)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-7f68a70
+ * @license Angular v15.2.0-next.0+sha-926c35f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-7f68a70');
+const VERSION = new Version('15.2.0-next.0+sha-926c35f');
 
 // This default value is when checking the hierarchy for a token.
 //
