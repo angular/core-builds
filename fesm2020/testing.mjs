7213f5bec401b894a10a83dc1414a7162fe60789
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:10:26 +0000

    b0318f8b39 docs(docs-infra): fix wording inconsistency of the i18n common prepare section references (#47582)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-fbcddb0
+ * @license Angular v15.2.0-next.0+sha-b0318f8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9399,7 +9399,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-fbcddb0');
+const VERSION = new Version('15.2.0-next.0+sha-b0318f8');
 
 /**
  * @license
