4b6e0faf1525883a70528dd515228546748cb349
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:12:11 +0000

    61023b563d refactor(compiler): refactor the shadow css specs (#48443)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-fda96a6
+ * @license Angular v15.2.0-next.0+sha-61023b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-fda96a6');
+const VERSION = new Version('15.2.0-next.0+sha-61023b5');
 
 // This default value is when checking the hierarchy for a token.
 //
