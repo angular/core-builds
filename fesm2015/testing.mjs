553bdda6550464807eaf5ef6a4f31ec98e96b2b4
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:15:31 +0000

    8375a8c9a8 docs: Add heading to the router writable properties deprecation (#48684)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-f09b6e4
+ * @license Angular v15.2.0-next.0+sha-8375a8c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9400,7 +9400,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-f09b6e4');
+const VERSION = new Version('15.2.0-next.0+sha-8375a8c');
 
 /**
  * @license
