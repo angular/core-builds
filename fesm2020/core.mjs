ceb9673647bb60ee5239d21eb75ce67cedc7fbc0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-11 15:57:47 +0000

    7cfe97ca31 ci: set `fetch-depth: 0` in `update-events` and `update-cli-help` actions (#48698)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-2b83986
+ * @license Angular v15.2.0-next.0+sha-7cfe97c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9013,7 +9013,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-2b83986');
+const VERSION = new Version('15.2.0-next.0+sha-7cfe97c');
 
 /**
  * @license
