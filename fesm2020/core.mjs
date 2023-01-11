fb4b2f00b68a9373bddc01e71f172abcc4afcf0a
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:19:29 +0000

    cc392e532b refactor(forms): replace type any for the providers (#48647)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0+sha-c3ce295
+ * @license Angular v15.1.0+sha-cc392e5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0+sha-c3ce295');
+const VERSION = new Version('15.1.0+sha-cc392e5');
 
 // This default value is when checking the hierarchy for a token.
 //
