483354ee3f99c20e07dd1f2622d6fc86d0ae3b28
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:14:30 +0000

    f09b6e4507 docs: add angular devsat q1 2023 (#48683)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-01be0e2
+ * @license Angular v15.2.0-next.0+sha-f09b6e4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-01be0e2');
+const VERSION = new Version('15.2.0-next.0+sha-f09b6e4');
 
 // This default value is when checking the hierarchy for a token.
 //
