8c84f55eb1399958b9880346eff0c27d18d290e2
Andrew Kushnir <akushnir@google.com>
2023-01-12 22:07:17 +0000

    dd54f6bd96 fix(core): makeEnvironmentProviders should accept EnvironmentProviders (#48720)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0+sha-68ce4f6
+ * @license Angular v15.1.0+sha-dd54f6b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0+sha-68ce4f6');
+const VERSION = new Version('15.1.0+sha-dd54f6b');
 
 // This default value is when checking the hierarchy for a token.
 //
--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0+sha-68ce4f6
+ * @license Angular v15.1.0+sha-dd54f6b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0+sha-68ce4f6');
+const VERSION = new Version('15.1.0+sha-dd54f6b');
 
 // This default value is when checking the hierarchy for a token.
 //
