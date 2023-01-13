89c11dec0a7cb871fd9d13751469aaba996a849b
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:58:02 +0000

    5f21c6d627 perf(common): avoid excessive DOM mutation in NgClass (#48433)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-5bb1223
+ * @license Angular v15.2.0-next.0+sha-5f21c6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-5bb1223');
+const VERSION = new Version('15.2.0-next.0+sha-5f21c6d');
 
 // This default value is when checking the hierarchy for a token.
 //
