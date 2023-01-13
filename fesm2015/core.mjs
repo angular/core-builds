adf8304e9a0372242d4bf5ce7570f986a003cca4
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:47:10 +0000

    5b1c7c10a0 build: update dependency @rollup/plugin-commonjs to v24 (#48530)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-d47bd48
+ * @license Angular v15.2.0-next.0+sha-5b1c7c1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-d47bd48');
+const VERSION = new Version('15.2.0-next.0+sha-5b1c7c1');
 
 // This default value is when checking the hierarchy for a token.
 //
