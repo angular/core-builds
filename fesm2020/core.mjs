7bc7e4ca2e183272fce381900df11f1b63d5e9b2
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:46:36 +0000

    d47bd480c9 build: update scorecard action dependencies (#48465)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-f03f20c
+ * @license Angular v15.2.0-next.0+sha-d47bd48
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-f03f20c');
+const VERSION = new Version('15.2.0-next.0+sha-d47bd48');
 
 // This default value is when checking the hierarchy for a token.
 //
