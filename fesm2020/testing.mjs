db68af632144ae6cfc53dec0284a75d65d48fa89
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-13 14:22:27 +0000

    13f10de40e test: update code fixes spec to work with esm-cjs interop lint (#48731)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-4ae384f
+ * @license Angular v15.2.0-next.0+sha-13f10de
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-4ae384f');
+const VERSION = new Version('15.2.0-next.0+sha-13f10de');
 
 // This default value is when checking the hierarchy for a token.
 //
