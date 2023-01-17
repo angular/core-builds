70c0a82b266b37d0b77d21b6e81f83e4c2e55c1e
Andrew Kushnir <akushnir@google.com>
2023-01-17 19:02:36 +0000

    9130635eac test: update size golden to reflect Angular cross dependency updates (#48747)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-fb7b8a9
+ * @license Angular v15.2.0-next.0+sha-9130635
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-fb7b8a9');
+const VERSION = new Version('15.2.0-next.0+sha-9130635');
 
 // This default value is when checking the hierarchy for a token.
 //
