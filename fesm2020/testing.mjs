d2c4a51537f9c290237eba9627d009ee5ced26ce
Andrew Kushnir <akushnir@google.com>
2023-01-17 17:27:06 +0000

    1e30baaaa7 test: remove usages of deprecated runSchematicAsync function (#48745)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-6beff5e
+ * @license Angular v15.2.0-next.0+sha-1e30baa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-6beff5e');
+const VERSION = new Version('15.2.0-next.0+sha-1e30baa');
 
 // This default value is when checking the hierarchy for a token.
 //
