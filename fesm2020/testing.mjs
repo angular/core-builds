5c9643475d65cc2a1f0a33e644c55b5e94538710
Andrew Kushnir <akushnir@google.com>
2023-01-18 21:46:13 +0000

    97f6535219 ci: increase payload size for an integration test app (#48772)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.1+sha-e3a8a5c
+ * @license Angular v15.1.1+sha-97f6535
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.1+sha-e3a8a5c');
+const VERSION = new Version('15.1.1+sha-97f6535');
 
 // This default value is when checking the hierarchy for a token.
 //
