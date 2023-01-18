35d20ee3afad8ebf117df19739a5f38cdd77197b
Andrew Kushnir <akushnir@google.com>
2023-01-18 19:05:00 +0000

    fe6a2bd9e2 build: update dependency rimraf to v4 (#48726)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-3a688cb
+ * @license Angular v15.2.0-next.1+sha-fe6a2bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-3a688cb');
+const VERSION = new Version('15.2.0-next.1+sha-fe6a2bd');
 
 // This default value is when checking the hierarchy for a token.
 //
