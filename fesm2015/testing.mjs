6680102dfb1064affc20ea03a38b5f3ba9d742e4
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-01-18 18:29:15 +0000

    318d94d4e1 docs: release notes for the v15.1.1 release

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.0+sha-9130635
+ * @license Angular v15.2.0-next.0+sha-318d94d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.0+sha-9130635');
+const VERSION = new Version('15.2.0-next.0+sha-318d94d');
 
 // This default value is when checking the hierarchy for a token.
 //
