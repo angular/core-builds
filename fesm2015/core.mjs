7a642f3ba47988337be09d1678d193d9e450d844
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-01-18 18:28:46 +0000

    e3a8a5cc42 release: cut the v15.1.1 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0+sha-0f8a4f9
+ * @license Angular v15.1.1+sha-e3a8a5c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.0+sha-0f8a4f9');
+const VERSION = new Version('15.1.1+sha-e3a8a5c');
 
 // This default value is when checking the hierarchy for a token.
 //
