27b418c6f80608b6d2138d6afe70d2cad852be09
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-19 17:53:59 +0000

    60a7214ab2 refactor(bazel): Extract the helper patchNgHostWithFileNameToModuleName to its own file for 1P use (#48739)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.1+sha-97f6535
+ * @license Angular v15.1.1+sha-60a7214
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.1+sha-97f6535');
+const VERSION = new Version('15.1.1+sha-60a7214');
 
 // This default value is when checking the hierarchy for a token.
 //
