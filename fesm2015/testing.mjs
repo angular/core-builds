f4f556e409bcb80f04aa1c61ae574034512b0e64
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 18:55:35 +0000

    b6e3840ed4 refactor(bazel): remove unnecessary banner stamping code (#48798)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-840530f
+ * @license Angular v15.2.0-next.1+sha-b6e3840
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-840530f');
+const VERSION = new Version('15.2.0-next.1+sha-b6e3840');
 
 // This default value is when checking the hierarchy for a token.
 //
