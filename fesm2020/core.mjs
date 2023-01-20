3fd9c9b398b8173592460dbcb50e67d07b93ccd0
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 17:25:41 +0000

    840530ffd8 docs: update supported Chrome version description (#48782)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-2d0ca73
+ * @license Angular v15.2.0-next.1+sha-840530f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-2d0ca73');
+const VERSION = new Version('15.2.0-next.1+sha-840530f');
 
 // This default value is when checking the hierarchy for a token.
 //
