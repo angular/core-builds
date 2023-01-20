0c359c32e72a4211c10c7b89049bdef53c06107e
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 01:34:51 +0000

    2d0ca73d73 docs: Document extracted from the original Introduction document, which is to be retired. Linting suggested corrections made.  Restructuring or rewording sentences to be shorter and clearer. (#47907)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-bca76ed
+ * @license Angular v15.2.0-next.1+sha-2d0ca73
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-bca76ed');
+const VERSION = new Version('15.2.0-next.1+sha-2d0ca73');
 
 // This default value is when checking the hierarchy for a token.
 //
