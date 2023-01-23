950bdd201a8ec099c801b1ac5ff059fadc316503
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-23 13:51:44 +0000

    2f83366f58 build: remove unused build optimizer dependency (#48797)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.1+sha-92ab80d
+ * @license Angular v15.1.1+sha-2f83366
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.1+sha-92ab80d');
+const VERSION = new Version('15.1.1+sha-2f83366');
 
 // This default value is when checking the hierarchy for a token.
 //
