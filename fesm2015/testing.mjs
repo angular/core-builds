b14f7a2edfcb6bc42ee1976d43f9f68e7ef2e4d4
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 21:26:13 +0000

    87fe3161d4 refactor: remove unnecessary test support check utilities (#47543)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-402fcc5
+ * @license Angular v15.2.0-next.1+sha-87fe316
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-402fcc5');
+const VERSION = new Version('15.2.0-next.1+sha-87fe316');
 
 // This default value is when checking the hierarchy for a token.
 //
