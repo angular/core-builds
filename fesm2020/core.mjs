c7bdf4df6c535e8ddca24762ba96201a0ead7168
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 18:04:09 +0000

    bc8cfa2552 fix(compiler): handle css selectors with space after an escaped character. (#48558)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-4da1f29
+ * @license Angular v15.2.0-next.1+sha-bc8cfa2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-4da1f29');
+const VERSION = new Version('15.2.0-next.1+sha-bc8cfa2');
 
 // This default value is when checking the hierarchy for a token.
 //
