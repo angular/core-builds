7e745705266aaeacbadf824d9ed5ad7cd16a9616
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 17:04:41 +0000

    a154db8a81 feat(core): add ng generate schematic to convert declarations to standalone (#48790)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.1+sha-28da74c
+ * @license Angular v15.2.0-next.1+sha-a154db8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.1+sha-28da74c');
+const VERSION = new Version('15.2.0-next.1+sha-a154db8');
 
 // This default value is when checking the hierarchy for a token.
 //
