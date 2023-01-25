66f9ddd2006a98948d62d646c8ce4ac71f26f7f4
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 18:52:28 +0000

    6e197520dd refactor(upgrade): rewrite the downgrade component adapter (#48715)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.2+sha-0861471
+ * @license Angular v15.1.2+sha-6e19752
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.2+sha-0861471');
+const VERSION = new Version('15.1.2+sha-6e19752');
 
 // This default value is when checking the hierarchy for a token.
 //
