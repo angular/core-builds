97aa551150145047675c0ca65845ad0044ae0a02
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 18:47:05 +0000

    14aefa1b03 docs: add new files for HTTP content (#47684)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-c91606b
+ * @license Angular v15.2.0-next.2+sha-14aefa1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-c91606b');
+const VERSION = new Version('15.2.0-next.2+sha-14aefa1');
 
 // This default value is when checking the hierarchy for a token.
 //
