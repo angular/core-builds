98b362f0bb6c0d005dc36a91176b4c302bbc2f2c
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 18:51:46 +0000

    06e161f2dd fix(compiler): incorrect code when non-null assertion is used after a safe access (#48801)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-5915c72
+ * @license Angular v15.2.0-next.2+sha-06e161f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-5915c72');
+const VERSION = new Version('15.2.0-next.2+sha-06e161f');
 
 // This default value is when checking the hierarchy for a token.
 //
