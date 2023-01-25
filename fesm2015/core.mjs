a73b00d71967b28c54944023e41bae7767b66a77
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 19:51:29 +0000

    dedac8d3f7 feat(router): Add test helper for trigger navigations in tests (#48552)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-a055196
+ * @license Angular v15.2.0-next.2+sha-dedac8d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-a055196');
+const VERSION = new Version('15.2.0-next.2+sha-dedac8d');
 
 // This default value is when checking the hierarchy for a token.
 //
