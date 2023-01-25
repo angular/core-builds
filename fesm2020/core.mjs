9f972c963e0375c2522333fd6633abf14b5ff905
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 19:45:07 +0000

    a055196c55 fix(common): warn if using ngSrcset without a configured image loader (#48804)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-06e161f
+ * @license Angular v15.2.0-next.2+sha-a055196
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-06e161f');
+const VERSION = new Version('15.2.0-next.2+sha-a055196');
 
 // This default value is when checking the hierarchy for a token.
 //
