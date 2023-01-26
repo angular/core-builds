3462d00279eb39ea1189f6ec2b0f73023b9386bb
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 16:12:26 +0000

    04e0ac3d7c fix(migrations): migration host incorrectly reading empty files (#48849)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-65c74ed
+ * @license Angular v15.2.0-next.2+sha-04e0ac3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-65c74ed');
+const VERSION = new Version('15.2.0-next.2+sha-04e0ac3');
 
 // This default value is when checking the hierarchy for a token.
 //
