19caed18556e09e3ec61d9559c8c84d6b9706dca
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 22:23:49 +0000

    b4187548ca fix(animations): fix non-animatable warnings for easing (#48583)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-33a636a
+ * @license Angular v15.2.0-next.2+sha-b418754
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-33a636a');
+const VERSION = new Version('15.2.0-next.2+sha-b418754');
 
 // This default value is when checking the hierarchy for a token.
 //
