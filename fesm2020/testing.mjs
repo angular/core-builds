46af621513cb34334e638c47a606345e6ab32328
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 16:41:17 +0000

    ef6e0b8f65 docs: add this is angular to Community Curations (#48618)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.2+sha-d36dfd4
+ * @license Angular v15.1.2+sha-ef6e0b8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.2+sha-d36dfd4');
+const VERSION = new Version('15.1.2+sha-ef6e0b8');
 
 // This default value is when checking the hierarchy for a token.
 //
