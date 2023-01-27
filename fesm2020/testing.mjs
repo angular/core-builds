29d81e2aa35fa7e207267787b7a19be45ececdbf
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 17:04:10 +0000

    869c9ac1a9 docs: add angular getting started course (#48619)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-c2dc51a
+ * @license Angular v15.2.0-next.2+sha-869c9ac
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-c2dc51a');
+const VERSION = new Version('15.2.0-next.2+sha-869c9ac');
 
 // This default value is when checking the hierarchy for a token.
 //
