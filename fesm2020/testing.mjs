06cf6e469d8b66dce4539f591895bd6a45e36082
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 17:24:40 +0000

    d3a14d94a1 docs(docs-infra): update strongbrew resource to new website (#48340)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-f82bdc4
+ * @license Angular v15.2.0-next.2+sha-d3a14d9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8795,7 +8795,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-f82bdc4');
+const VERSION = new Version('15.2.0-next.2+sha-d3a14d9');
 
 // This default value is when checking the hierarchy for a token.
 //
