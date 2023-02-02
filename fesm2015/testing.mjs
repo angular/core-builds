5f4c16c509814e62a1ff1aeae46cfc816e2229f4
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:50:41 +0000

    617a01073d docs: release notes for the v15.1.3 release

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-3cce18e
+ * @license Angular v15.2.0-next.3+sha-617a010
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-3cce18e');
+const VERSION = new Version('15.2.0-next.3+sha-617a010');
 
 // This default value is when checking the hierarchy for a token.
 //
