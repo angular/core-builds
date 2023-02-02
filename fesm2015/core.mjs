035b9d1d33052b8b582221bc87f7f4a67c8cba75
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:57:55 +0000

    c2bcf0b475 refactor(compiler): Remove strictStyling option for ShadowCss (#48824)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-3a9bbad
+ * @license Angular v15.2.0-next.3+sha-c2bcf0b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-3a9bbad');
+const VERSION = new Version('15.2.0-next.3+sha-c2bcf0b');
 
 // This default value is when checking the hierarchy for a token.
 //
