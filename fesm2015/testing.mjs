ca63cae5b64ce7ca0424e9480b1bc2866464ecc6
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 17:47:23 +0000

    584976e6c8 fix(migrations): support --defaults in standalone migration (#48921)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-8a9907c
+ * @license Angular v15.2.0-next.2+sha-584976e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-8a9907c');
+const VERSION = new Version('15.2.0-next.2+sha-584976e');
 
 // This default value is when checking the hierarchy for a token.
 //
