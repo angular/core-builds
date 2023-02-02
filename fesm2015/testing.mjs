84ee6eb1252b7f8cd53a84682e14a8f2bd433d05
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 22:00:42 +0000

    21b15e8b86 docs: making footer copyright year dynamic (#48811)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.2+sha-7f5d035
+ * @license Angular v15.1.3+sha-21b15e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.2+sha-7f5d035');
+const VERSION = new Version('15.1.3+sha-21b15e8');
 
 // This default value is when checking the hierarchy for a token.
 //
