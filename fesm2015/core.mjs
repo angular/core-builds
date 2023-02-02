a2597f6208afb0432bed9368d9f02145f3a74c70
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:36:46 +0000

    3cce18e434 release: cut the v15.2.0-next.3 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.2+sha-65329a9
+ * @license Angular v15.2.0-next.3+sha-3cce18e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.2+sha-65329a9');
+const VERSION = new Version('15.2.0-next.3+sha-3cce18e');
 
 // This default value is when checking the hierarchy for a token.
 //
