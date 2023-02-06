7f2412f7cefafc6c497cb73e5b4a3a483b6c045a
Andrew Scott <atscott@google.com>
2023-02-06 18:27:58 +0000

    54b24eb40f feat(common): Add loaderParams attribute to NgOptimizedImage (#48907)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-759db12
+ * @license Angular v15.2.0-next.3+sha-54b24eb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8385,7 +8385,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-759db12');
+const VERSION = new Version('15.2.0-next.3+sha-54b24eb');
 
 // This default value is when checking the hierarchy for a token.
 //
