a3cd5aeabfc05250fc4244a8e62fa123fffbaee2
Andrew Scott <atscott@google.com>
2023-02-07 20:03:16 +0000

    4b8b5e3f3c build: update eslint dependencies to v5.51.0 (#48983)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-57d0c03
+ * @license Angular v15.2.0-next.3+sha-4b8b5e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8385,7 +8385,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-57d0c03');
+const VERSION = new Version('15.2.0-next.3+sha-4b8b5e3');
 
 // This default value is when checking the hierarchy for a token.
 //
