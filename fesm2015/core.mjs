0122b2dce47fe15aca467f17f465e0a5be95f7e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 14:36:51 +0000

    bdbf21d04b fix(migrations): avoid generating imports with forward slashes (#48993)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-c7926b5
+ * @license Angular v15.2.0-next.3+sha-bdbf21d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8378,7 +8378,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-c7926b5');
+const VERSION = new Version('15.2.0-next.3+sha-bdbf21d');
 
 // This default value is when checking the hierarchy for a token.
 //
