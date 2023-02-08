38b3c7a6dde51fb6396f80437628fc0877cabcf3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 18:44:01 +0000

    642cc1c349 release: cut the v15.2.0-next.4 release

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.3+sha-47387d4
+ * @license Angular v15.2.0-next.4+sha-642cc1c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8751,7 +8751,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.3+sha-47387d4');
+const VERSION = new Version('15.2.0-next.4+sha-642cc1c');
 
 // This default value is when checking the hierarchy for a token.
 //
