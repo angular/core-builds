5355cbd225b72f506ea954c7197c0bb15750aab2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 10:46:17 +0000

    5f2a3edcf2 fix(forms): Make radio buttons respect `[attr.disabled]` (#48864)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.4+sha-d814de2
+ * @license Angular v15.1.4+sha-5f2a3ed
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8803,7 +8803,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.4+sha-d814de2');
+const VERSION = new Version('15.1.4+sha-5f2a3ed');
 
 // This default value is when checking the hierarchy for a token.
 //
