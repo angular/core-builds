4c011e7e2aeba957f5627c34246701cc48cff44e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 08:49:58 +0000

    0331473e3a docs(platform-browser): Sanitize method has more explicit documentation (#48765)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-84ad4d0
+ * @license Angular v15.2.0-next.4+sha-0331473
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8743,7 +8743,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-84ad4d0');
+const VERSION = new Version('15.2.0-next.4+sha-0331473');
 
 // This default value is when checking the hierarchy for a token.
 //
