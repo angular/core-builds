84755e06c6a27b97a09d8afc4cbe2af1457644ba
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:38:03 +0000

    7af4ea62e6 build: update actions/cache digest to 6998d13 (#49020)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-40c976c
+ * @license Angular v15.2.0-next.4+sha-7af4ea6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8378,7 +8378,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-40c976c');
+const VERSION = new Version('15.2.0-next.4+sha-7af4ea6');
 
 // This default value is when checking the hierarchy for a token.
 //
