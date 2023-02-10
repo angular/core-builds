3a92b83f0645037be635a4eae995e28c56ad040f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:49:46 +0000

    993a43f87c test: update size goldens to reflect devkit update and terser (#48996)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-7af4ea6
+ * @license Angular v15.2.0-next.4+sha-993a43f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8751,7 +8751,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-7af4ea6');
+const VERSION = new Version('15.2.0-next.4+sha-993a43f');
 
 // This default value is when checking the hierarchy for a token.
 //
