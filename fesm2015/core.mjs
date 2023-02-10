794fbf1cd9f1a91cc44f9801270c7880fddf43a0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 07:42:56 +0000

    b24cf027d3 build: remove unused webdriver-manager code (#49009)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.4+sha-38257a3
+ * @license Angular v15.1.4+sha-b24cf02
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8430,7 +8430,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.4+sha-38257a3');
+const VERSION = new Version('15.1.4+sha-b24cf02');
 
 // This default value is when checking the hierarchy for a token.
 //
