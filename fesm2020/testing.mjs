dffd21bf8870608fb2598bd21300d98272b37383
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 07:55:15 +0000

    a28cf2438b docs(router): fix error handler deprecation (#49015)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-865e67f
+ * @license Angular v15.2.0-next.4+sha-a28cf24
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8743,7 +8743,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-865e67f');
+const VERSION = new Version('15.2.0-next.4+sha-a28cf24');
 
 // This default value is when checking the hierarchy for a token.
 //
