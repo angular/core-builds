835412c3bacbd99f8d62c2ea86c6046060b5f73c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 08:25:53 +0000

    b97d591ff4 build: improve debugging of symbol extractor tests (#49000)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-42619bd
+ * @license Angular v15.2.0-next.4+sha-b97d591
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8385,7 +8385,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-42619bd');
+const VERSION = new Version('15.2.0-next.4+sha-b97d591');
 
 // This default value is when checking the hierarchy for a token.
 //
