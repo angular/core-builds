bf1ef7bae20495c332edcf56deffab44494b4325
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 19:16:29 +0000

    c120fe5eab build: never cache monitoring bazel test target (#49039)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.4+sha-b022d07
+ * @license Angular v15.1.4+sha-c120fe5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8437,7 +8437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.1.4+sha-b022d07');
+const VERSION = new Version('15.1.4+sha-c120fe5');
 
 // This default value is when checking the hierarchy for a token.
 //
