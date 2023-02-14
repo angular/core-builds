e010d0a85fa9f2b289511294c99c87d771e118ce
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 19:37:42 +0000

    2ca49726ca refactor(devtools): consolidate devtools global styles into 1 file (#49001)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-7321154
+ * @license Angular v15.2.0-next.4+sha-2ca4972
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8385,7 +8385,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-7321154');
+const VERSION = new Version('15.2.0-next.4+sha-2ca4972');
 
 // This default value is when checking the hierarchy for a token.
 //
