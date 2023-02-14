c40f1045820de9e79c683c963d8b49fb6808911b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 18:03:24 +0000

    73211543b3 docs: remove former team members from bios (#48999)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-1f1a312
+ * @license Angular v15.2.0-next.4+sha-7321154
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8385,7 +8385,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-1f1a312');
+const VERSION = new Version('15.2.0-next.4+sha-7321154');
 
 // This default value is when checking the hierarchy for a token.
 //
