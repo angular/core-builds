1dc1fe046082ce013ec29acf3c109e3840350dc4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 14:14:26 +0000

    62453bfe7a build: update github/codeql-action action to v2.2.4 (#49055)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-1c5ae63
+ * @license Angular v15.2.0-next.4+sha-62453bf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8378,7 +8378,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-1c5ae63');
+const VERSION = new Version('15.2.0-next.4+sha-62453bf');
 
 // This default value is when checking the hierarchy for a token.
 //
