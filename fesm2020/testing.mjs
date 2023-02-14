195bab8f2b465a7c2288b8038f0a5ad8b9124f87
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 13:52:39 +0000

    64f3684f8a build: update io_bazel_rules_sass digest to b603e6f (#49053)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-3ec2e2d
+ * @license Angular v15.2.0-next.4+sha-64f3684
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8743,7 +8743,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-3ec2e2d');
+const VERSION = new Version('15.2.0-next.4+sha-64f3684');
 
 // This default value is when checking the hierarchy for a token.
 //
