60683bf61278007ee409b11c668673f4275ca0e3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 17:15:56 +0000

    1f1a312e2e build: update dependency json5 to v2.2.2 [security] (#48989)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-a4fdc8f
+ * @license Angular v15.2.0-next.4+sha-1f1a312
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8378,7 +8378,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-a4fdc8f');
+const VERSION = new Version('15.2.0-next.4+sha-1f1a312');
 
 // This default value is when checking the hierarchy for a token.
 //
