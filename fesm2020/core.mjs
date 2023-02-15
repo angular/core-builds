99a305076177dec2d912be2e5ca9b81cc171f3b7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-15 17:49:38 +0000

    44f13e2f2b docs: release notes for the v15.1.5 release

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-next.4+sha-2ca4972
+ * @license Angular v15.2.0-next.4+sha-44f13e2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8385,7 +8385,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-next.4+sha-2ca4972');
+const VERSION = new Version('15.2.0-next.4+sha-44f13e2');
 
 // This default value is when checking the hierarchy for a token.
 //
