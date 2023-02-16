449d81165f2515d8e778587ffb293f323904cbcb
Andrew Kushnir <akushnir@google.com>
2023-02-16 19:04:58 +0000

    6977403a9b release: bump version to 16.0.0-next.0 (#49084) PR Close #49084

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.3.0-next.0+sha-be21eaa
+ * @license Angular v16.0.0-next.0+sha-6977403
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8751,7 +8751,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.3.0-next.0+sha-be21eaa');
+const VERSION = new Version('16.0.0-next.0+sha-6977403');
 
 // This default value is when checking the hierarchy for a token.
 //
