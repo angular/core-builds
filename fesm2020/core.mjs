8768233822da39a9094a717a83f800011c18f0a7
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:21:57 +0000

    e6a622df38 docs: New doc HTTP client - Security: Cross-Site Request Forgery (XSRF) protection (#48916)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-4adc790
+ * @license Angular v16.0.0-next.0+sha-e6a622d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8384,7 +8384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-4adc790');
+const VERSION = new Version('16.0.0-next.0+sha-e6a622d');
 
 // This default value is when checking the hierarchy for a token.
 //
