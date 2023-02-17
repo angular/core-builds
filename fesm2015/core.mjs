0c3b5c6cc2c1dd942890a47bd8b4b7cd7340f7dd
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:20:18 +0000

    e58f814c20 docs: new doc extract from original HTTP doc which is to be retired. (#48905)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-1f055b9
+ * @license Angular v16.0.0-next.0+sha-e58f814
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8377,7 +8377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-1f055b9');
+const VERSION = new Version('16.0.0-next.0+sha-e58f814');
 
 // This default value is when checking the hierarchy for a token.
 //
