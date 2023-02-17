dc97f5e531594e3883d1cf91a0a176755d4e2f57
Andrew Kushnir <akushnir@google.com>
2023-02-17 20:06:15 +0000

    8ed653b93f ci: update AIO payload size tracking (#49130)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-rc.0+sha-25e220a
+ * @license Angular v15.2.0-rc.0+sha-8ed653b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8377,7 +8377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-rc.0+sha-25e220a');
+const VERSION = new Version('15.2.0-rc.0+sha-8ed653b');
 
 // This default value is when checking the hierarchy for a token.
 //
