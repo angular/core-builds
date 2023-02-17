cec313df4916d824ec0724e8195d68b6341e0af4
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:24:09 +0000

    ca53adeceb docs: New doc HTTP client - Pass metadata to interceptors. (#48918)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-09e06fb
+ * @license Angular v16.0.0-next.0+sha-ca53ade
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8750,7 +8750,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-09e06fb');
+const VERSION = new Version('16.0.0-next.0+sha-ca53ade');
 
 // This default value is when checking the hierarchy for a token.
 //
