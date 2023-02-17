c7dec32a5d4690799431069b379b5fd7a7dbe343
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:09:42 +0000

    320f36f217 docs: fix typo in toh-pt5.md (#49108)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-d2a7245
+ * @license Angular v16.0.0-next.0+sha-320f36f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8751,7 +8751,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-d2a7245');
+const VERSION = new Version('16.0.0-next.0+sha-320f36f');
 
 // This default value is when checking the hierarchy for a token.
 //
