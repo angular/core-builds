154833ecb7f9ef48edee1e28d52b40b4f54ee924
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:21:45 +0000

    9105c41f44 fix(platform-server): avoid duplicate TransferState info after renderApplication call (#49094)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-07db67b
+ * @license Angular v16.0.0-next.0+sha-9105c41
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8742,7 +8742,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-07db67b');
+const VERSION = new Version('16.0.0-next.0+sha-9105c41');
 
 // This default value is when checking the hierarchy for a token.
 //
