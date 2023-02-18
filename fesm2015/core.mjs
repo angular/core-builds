59bdb1c4ca32c3a0c8ad560c6d8409a17d546042
Joey Perrott <josephperrott@gmail.com>
2023-02-18 02:53:49 +0000

    8ba8e0dcff build: update to latest remote build execution setup (#49133)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-c872426
+ * @license Angular v16.0.0-next.0+sha-8ba8e0d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8377,7 +8377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-c872426');
+const VERSION = new Version('16.0.0-next.0+sha-8ba8e0d');
 
 // This default value is when checking the hierarchy for a token.
 //
