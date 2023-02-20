0b041975db4aed0dfe55fb89aca5fff56d7d69f2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-02-20 15:27:55 +0000

    f9b6a3fecc ci: attempt to fix node windows job flakiness (#49142)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-8ba8e0d
+ * @license Angular v16.0.0-next.0+sha-f9b6a3f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8750,7 +8750,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-8ba8e0d');
+const VERSION = new Version('16.0.0-next.0+sha-f9b6a3f');
 
 // This default value is when checking the hierarchy for a token.
 //
