73ccc5351d3a3b0361affd7994045977b5d5f65c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-21 21:32:05 +0000

    637102d992 docs: add NG Poland 2023 and JS Poland 2023 to events (#48972)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-83a6e20
+ * @license Angular v16.0.0-next.0+sha-637102d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8384,7 +8384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-83a6e20');
+const VERSION = new Version('16.0.0-next.0+sha-637102d');
 
 // This default value is when checking the hierarchy for a token.
 //
