5507e78888f8695c7f8d6ca617c20e5d69bdf9dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-22 19:42:55 +0000

    1e32709e0e fix(router): remove RouterEvent from Event union type (#46061)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-5dce2a5
+ * @license Angular v16.0.0-next.0+sha-1e32709
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8742,7 +8742,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-5dce2a5');
+const VERSION = new Version('16.0.0-next.0+sha-1e32709');
 
 // This default value is when checking the hierarchy for a token.
 //
