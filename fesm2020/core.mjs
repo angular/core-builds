608cae5f303e154465f0dd242f473c839dc8a781
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-22 19:47:00 +0000

    323ffc98eb docs: escape script tag to prevent rendering problems (#49152)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-71d5cda
+ * @license Angular v16.0.0-next.0+sha-323ffc9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8384,7 +8384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-71d5cda');
+const VERSION = new Version('16.0.0-next.0+sha-323ffc9');
 
 // This default value is when checking the hierarchy for a token.
 //
