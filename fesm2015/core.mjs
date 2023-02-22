1f97d588f10c1a3085dac9c41da8a581cf7d3047
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-22 19:40:05 +0000

    5dce2a5a3a feat(common): Provide MockPlatformLocation by default in BrowserTestingModule (#49137)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-02cd490
+ * @license Angular v16.0.0-next.0+sha-5dce2a5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8377,7 +8377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-02cd490');
+const VERSION = new Version('16.0.0-next.0+sha-5dce2a5');
 
 // This default value is when checking the hierarchy for a token.
 //
