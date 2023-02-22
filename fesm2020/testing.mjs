2727ab8d8e8e250ba2255d68394e232d01fbacd9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-22 19:50:35 +0000

    03fcb36cfd fix(migrations): migrate HttpClientModule to provideHttpClient() (#48949)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-rc.0+sha-7b4d3c0
+ * @license Angular v15.2.0-rc.0+sha-03fcb36
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8742,7 +8742,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0-rc.0+sha-7b4d3c0');
+const VERSION = new Version('15.2.0-rc.0+sha-03fcb36');
 
 // This default value is when checking the hierarchy for a token.
 //
