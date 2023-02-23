a12d1f246939c473c71e1e19b2d94fa148b4bde3
Andrew Kushnir <akushnir@google.com>
2023-02-23 18:46:41 +0000

    760cf4ca9f build: update io_bazel_rules_sass digest to 1f27790 (#49132)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-daae4d8
+ * @license Angular v16.0.0-next.0+sha-760cf4c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8384,7 +8384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-daae4d8');
+const VERSION = new Version('16.0.0-next.0+sha-760cf4c');
 
 // This default value is when checking the hierarchy for a token.
 //
