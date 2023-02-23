e5aff49080970e99cfab299622f8770417e1a698
Andrew Kushnir <akushnir@google.com>
2023-02-23 19:01:29 +0000

    d7835e4f1a docs(router): fix code block closing ticks (#49185) PR Close #49185

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0+sha-7677963
+ * @license Angular v15.2.0+sha-d7835e4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8377,7 +8377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0+sha-7677963');
+const VERSION = new Version('15.2.0+sha-d7835e4');
 
 // This default value is when checking the hierarchy for a token.
 //
