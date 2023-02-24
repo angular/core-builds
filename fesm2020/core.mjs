b0afab122c182f01fa6870efe0f865e34645535c
Andrew Kushnir <akushnir@google.com>
2023-02-24 19:02:02 +0000

    8542593c80 refactor(platform-browser): move TransferState init logic into its constructor (#49191)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0+sha-d0fa598
+ * @license Angular v15.2.0+sha-8542593
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8384,7 +8384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0+sha-d0fa598');
+const VERSION = new Version('15.2.0+sha-8542593');
 
 // This default value is when checking the hierarchy for a token.
 //
