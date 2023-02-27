e844f54740d66f37f113dcbd84b534f5e75ca8f0
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:21:46 +0000

    47ef9e83c7 build: cleanup unused dependencies (#49227)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-d1ac3aa
+ * @license Angular v16.0.0-next.0+sha-47ef9e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8384,7 +8384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-d1ac3aa');
+const VERSION = new Version('16.0.0-next.0+sha-47ef9e8');
 
 // This default value is when checking the hierarchy for a token.
 //
