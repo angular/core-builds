03ad7aee99a95fe7b34e00872ce75f3be54bba7b
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:21:06 +0000

    d1ac3aa14e fix(zone.js): zone-node only patch Promise.prototype.then (#49144)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-74258f3
+ * @license Angular v16.0.0-next.0+sha-d1ac3aa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8377,7 +8377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-74258f3');
+const VERSION = new Version('16.0.0-next.0+sha-d1ac3aa');
 
 // This default value is when checking the hierarchy for a token.
 //
