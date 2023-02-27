b8a632b66e6714665b64f9f7cb2802fa1d5c8e1e
Andrew Kushnir <akushnir@google.com>
2023-02-27 18:23:25 +0000

    3c079a74c9 release: bump DevTools version (#49121)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-3c9d49a
+ * @license Angular v16.0.0-next.0+sha-3c079a7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8750,7 +8750,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-3c9d49a');
+const VERSION = new Version('16.0.0-next.0+sha-3c079a7');
 
 // This default value is when checking the hierarchy for a token.
 //
