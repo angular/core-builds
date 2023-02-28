f7afdad591020ad5b74da624274d3a410ef21657
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:43:08 +0000

    6d30f607a0 docs(router): Improving linking (#49203)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-31f210b
+ * @license Angular v16.0.0-next.0+sha-6d30f60
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8384,7 +8384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-31f210b');
+const VERSION = new Version('16.0.0-next.0+sha-6d30f60');
 
 // This default value is when checking the hierarchy for a token.
 //
