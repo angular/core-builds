4a9d01f454c901fc1f3f97aa9888557473c25781
Andrew Kushnir <akushnir@google.com>
2023-02-28 21:03:01 +0000

    377d0c9c1e build: update cross-repo angular dependencies (#49268)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0+sha-d60ea6a
+ * @license Angular v15.2.0+sha-377d0c9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8377,7 +8377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0+sha-d60ea6a');
+const VERSION = new Version('15.2.0+sha-377d0c9');
 
 // This default value is when checking the hierarchy for a token.
 //
