7a6bd8f723955fd16f7ecd8ccd22fd6c55bbcc3e
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:31:49 +0000

    1c64e2b5ec build: update github/codeql-action action to v2.2.5 (#49242)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-0544b86
+ * @license Angular v16.0.0-next.0+sha-1c64e2b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8742,7 +8742,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-0544b86');
+const VERSION = new Version('16.0.0-next.0+sha-1c64e2b');
 
 // This default value is when checking the hierarchy for a token.
 //
