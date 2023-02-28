d4506d8282cd7eeb8c0583307c0fb9667c5d108d
Andrew Kushnir <akushnir@google.com>
2023-02-28 19:12:49 +0000

    0c805f128e build: update bazel setup (#48240)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-7b9a082
+ * @license Angular v16.0.0-next.0+sha-0c805f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8378,7 +8378,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-7b9a082');
+const VERSION = new Version('16.0.0-next.0+sha-0c805f1');
 
 // This default value is when checking the hierarchy for a token.
 //
