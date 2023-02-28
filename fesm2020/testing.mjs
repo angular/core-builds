3d9f8b5f9eae285d38e2f0e8cf002821ea588aba
Andrew Kushnir <akushnir@google.com>
2023-02-28 20:22:59 +0000

    d60ea6ab5a fix(core): update zone.js peerDependencies ranges (#49244)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0+sha-dafb765
+ * @license Angular v15.2.0+sha-d60ea6a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8742,7 +8742,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0+sha-dafb765');
+const VERSION = new Version('15.2.0+sha-d60ea6a');
 
 // This default value is when checking the hierarchy for a token.
 //
