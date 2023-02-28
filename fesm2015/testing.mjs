673476507a7ac79da0114b3585ca85c60c6c5bd2
Andrew Kushnir <akushnir@google.com>
2023-02-28 20:13:06 +0000

    7fd650a898 build: update lint path for @angular/build-tooling rules (#49266)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.0+sha-e9edea3
+ * @license Angular v16.0.0-next.0+sha-7fd650a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8773,7 +8773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.0+sha-e9edea3');
+const VERSION = new Version('16.0.0-next.0+sha-7fd650a');
 
 // This default value is when checking the hierarchy for a token.
 //
