517824f9a2d2d3501920af14e431573fa8e2ff41
Andrew Kushnir <akushnir@google.com>
2023-03-01 20:51:05 +0000

    17abe6dc96 refactor(platform-server): remove deprecated `renderModuleFactory` (#49247)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-030060e
+ * @license Angular v16.0.0-next.1+sha-17abe6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8773,7 +8773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-030060e');
+const VERSION = new Version('16.0.0-next.1+sha-17abe6d');
 
 // This default value is when checking the hierarchy for a token.
 //
