bbdf813dca9a768aca225d6acc5388a17bd5c308
Andrew Kushnir <akushnir@google.com>
2023-03-01 01:29:37 +0000

    439b4d3a89 docs: clarify ActivatedRouter injection location (#49270)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0+sha-377d0c9
+ * @license Angular v15.2.0+sha-439b4d3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8750,7 +8750,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.0+sha-377d0c9');
+const VERSION = new Version('15.2.0+sha-439b4d3');
 
 // This default value is when checking the hierarchy for a token.
 //
