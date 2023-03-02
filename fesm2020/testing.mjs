b641675a7a48f5dfbad96e40ff8e7f970a57656c
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 20:13:02 +0000

    ffc684381f docs: Add NG8107 entry : Optional chain not nullable extended diagnostic (#49287)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.1+sha-8f7fbdd
+ * @license Angular v15.2.1+sha-ffc6843
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8742,7 +8742,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.1+sha-8f7fbdd');
+const VERSION = new Version('15.2.1+sha-ffc6843');
 
 // This default value is when checking the hierarchy for a token.
 //
