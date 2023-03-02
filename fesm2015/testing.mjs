0739e4698d396976181162286397a5b51afd2d7c
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 21:32:51 +0000

    e7948d30c5 docs(router): Update component testing to user `RouterTestingHarness` (#48553)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-e9bda8a
+ * @license Angular v16.0.0-next.1+sha-e7948d3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8773,7 +8773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-e9bda8a');
+const VERSION = new Version('16.0.0-next.1+sha-e7948d3');
 
 // This default value is when checking the hierarchy for a token.
 //
