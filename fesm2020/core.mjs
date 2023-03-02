ee69d3b1cdf1adb072ff50e711285805cf391256
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 22:07:06 +0000

    de48c99afb refactor(migrations): expose current file in import remapper (#49288)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.1+sha-81e32f4
+ * @license Angular v15.2.1+sha-de48c99
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8384,7 +8384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.1+sha-81e32f4');
+const VERSION = new Version('15.2.1+sha-de48c99');
 
 // This default value is when checking the hierarchy for a token.
 //
