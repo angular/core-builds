2db9c6932e9ce7b58397df7d7d75eb09f3cfd399
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 22:08:54 +0000

    ea32c3289a feat(router): Expose information about the last successful `Navigation` (#49235)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-10c8eae
+ * @license Angular v16.0.0-next.1+sha-ea32c32
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8765,7 +8765,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-10c8eae');
+const VERSION = new Version('16.0.0-next.1+sha-ea32c32');
 
 // This default value is when checking the hierarchy for a token.
 //
