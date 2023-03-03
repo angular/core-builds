2ed2ad109658e6b626b539d786ba58d903d7c90a
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 18:47:25 +0000

    1ba551b888 Revert "refactor(router): Remove zone code from router scroller (#49238)" (#49311)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-fce0602
+ * @license Angular v16.0.0-next.1+sha-1ba551b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8765,7 +8765,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-fce0602');
+const VERSION = new Version('16.0.0-next.1+sha-1ba551b');
 
 // This default value is when checking the hierarchy for a token.
 //
