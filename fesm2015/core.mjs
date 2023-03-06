b251e837bcdcd72c34f8076f4ec12846c1bbe22d
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-06 19:04:38 +0000

    2312eb53ef fix(platform-browser): KeyEventsPlugin should keep the same behavior (#49330)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-10ffd03
+ * @license Angular v16.0.0-next.1+sha-2312eb5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8642,7 +8642,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-10ffd03');
+const VERSION = new Version('16.0.0-next.1+sha-2312eb5');
 
 // This default value is when checking the hierarchy for a token.
 //
