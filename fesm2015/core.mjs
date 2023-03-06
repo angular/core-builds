74969bd5698441ddda8ea7f2ec6633ff16dbf2a6
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-06 19:32:14 +0000

    fc5100986b release: cut the zone.js-0.13 release (#49240)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-477237a
+ * @license Angular v16.0.0-next.1+sha-fc51009
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8642,7 +8642,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-477237a');
+const VERSION = new Version('16.0.0-next.1+sha-fc51009');
 
 // This default value is when checking the hierarchy for a token.
 //
