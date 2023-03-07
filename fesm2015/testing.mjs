1dd44e2b1733cb6b4e8c0d46ea804b1fb3a77b3c
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-07 19:34:41 +0000

    fdf61974d1 feat(core): drop support for `zone.js` versions `<=0.12.0` (#49331)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-436050f
+ * @license Angular v16.0.0-next.1+sha-fdf6197
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9056,7 +9056,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-436050f');
+const VERSION = new Version('16.0.0-next.1+sha-fdf6197');
 
 // This default value is when checking the hierarchy for a token.
 //
