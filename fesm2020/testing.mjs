619248028a97f01525a71db5a968d8fccdcb8419
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-07 19:33:42 +0000

    88a0ff74ad docs(docs-infra): Split Error by categories & sort by code. (#49343)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.1+sha-e0be553
+ * @license Angular v15.2.1+sha-88a0ff7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8694,7 +8694,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.1+sha-e0be553');
+const VERSION = new Version('15.2.1+sha-88a0ff7');
 
 // This default value is when checking the hierarchy for a token.
 //
