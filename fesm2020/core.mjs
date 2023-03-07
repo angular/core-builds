76441dca90e817e7c4f3329926107215df7b0331
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-07 17:56:21 +0000

    e0be553abf docs(forms): change error code in filename to match enum value. (#49344)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.1+sha-ffccb8e
+ * @license Angular v15.2.1+sha-e0be553
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8336,7 +8336,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.1+sha-ffccb8e');
+const VERSION = new Version('15.2.1+sha-e0be553');
 
 // This default value is when checking the hierarchy for a token.
 //
