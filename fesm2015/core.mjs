f497eb2b270c7526304f71db09b816a65af5716b
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 18:18:22 +0000

    daaf0fd2f6 refactor(core): Remove `isObservable()` in favor `isSubscribable()`. (#49295)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-8e76107
+ * @license Angular v16.0.0-next.1+sha-daaf0fd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8735,7 +8735,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.1+sha-8e76107');
+const VERSION = new Version('16.0.0-next.1+sha-daaf0fd');
 
 // This default value is when checking the hierarchy for a token.
 //
