492aea4de26cdfa96fb809df21d9e2f3f8fcd3c7
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-09 17:29:09 +0000

    0a3ac9d6ea docs: delete outdated link. (#48909)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-addd7f6
+ * @license Angular v16.0.0-next.2+sha-0a3ac9d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8728,7 +8728,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-addd7f6');
+const VERSION = new Version('16.0.0-next.2+sha-0a3ac9d');
 
 // This default value is when checking the hierarchy for a token.
 //
