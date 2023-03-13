9b3f0592815939de1bb894401ec9c7f217a59c98
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-03-13 10:09:13 +0000

    7dbe328b28 docs: release notes for the v14.3.0 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-c10962b
+ * @license Angular v16.0.0-next.2+sha-7dbe328
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9220,7 +9220,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-c10962b');
+const VERSION = new Version('16.0.0-next.2+sha-7dbe328');
 
 // This default value is when checking the hierarchy for a token.
 //
