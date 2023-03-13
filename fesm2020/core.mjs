373f7df36e06bf248f86b865d26305489718cf8b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-03-13 14:03:39 +0000

    2df92f7633 build: update cross-repo angular dependencies (#49410)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.2+sha-1535ab9
+ * @license Angular v15.2.2+sha-2df92f7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8347,7 +8347,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.2+sha-1535ab9');
+const VERSION = new Version('15.2.2+sha-2df92f7');
 
 // This default value is when checking the hierarchy for a token.
 //
