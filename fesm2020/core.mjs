e4e25a744534b215b7417aa21b8166e97640b449
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:41:57 +0000

    b20ee9d47c refactor(core): remove duplicate call to `getComponentId` (#49390)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-3d2351c
+ * @license Angular v16.0.0-next.2+sha-b20ee9d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9241,7 +9241,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-3d2351c');
+const VERSION = new Version('16.0.0-next.2+sha-b20ee9d');
 
 // This default value is when checking the hierarchy for a token.
 //
