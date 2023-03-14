6d55c0330d3bb2273e6347316b27ddcd1eb875b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:30:29 +0000

    5a2b6227b3 fix(zone.js): revert Mocha it.skip, describe.skip method patch (#49329)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-daaed89
+ * @license Angular v16.0.0-next.2+sha-5a2b622
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9220,7 +9220,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-daaed89');
+const VERSION = new Version('16.0.0-next.2+sha-5a2b622');
 
 // This default value is when checking the hierarchy for a token.
 //
