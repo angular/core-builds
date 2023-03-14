6eda8f1f607470fcdc9e8273030118ad7d948f16
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 17:03:15 +0000

    e4516c75d2 docs(upgrade): mark lifecycle methods as nodoc (#49416) PR Close #49416

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.2+sha-6c189ed
+ * @license Angular v15.2.2+sha-e4516c7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8347,7 +8347,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.2+sha-6c189ed');
+const VERSION = new Version('15.2.2+sha-e4516c7');
 
 // This default value is when checking the hierarchy for a token.
 //
