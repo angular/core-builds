12ccfa2f11817ff2d78ae6f27c09e16fc663bfd1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:26:16 +0000

    bf2a5d5e04 build: update io_bazel_rules_sass digest to 3d2ad47 (#49428)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.2+sha-cf98777
+ * @license Angular v16.0.0-next.2+sha-bf2a5d5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9271,7 +9271,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.2+sha-cf98777');
+const VERSION = new Version('16.0.0-next.2+sha-bf2a5d5');
 
 // This default value is when checking the hierarchy for a token.
 //
