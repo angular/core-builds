56c5a616e1696290394d703dd5489cdc57d3f30e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:43:17 +0000

    c9816448ed build: update actions/cache digest to 940f3d7 (#49377)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.2+sha-e4516c7
+ * @license Angular v15.2.2+sha-c981644
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8695,7 +8695,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.2+sha-e4516c7');
+const VERSION = new Version('15.2.2+sha-c981644');
 
 // This default value is when checking the hierarchy for a token.
 //
