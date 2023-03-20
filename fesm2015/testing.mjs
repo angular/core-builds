736eace342e4cf7c75002f41ad392aa748b9440d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 12:28:29 +0000

    8f539c11f4 feat(compiler): add support for compile-time required inputs (#49468)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-d5946bd
+ * @license Angular v16.0.0-next.3+sha-8f539c1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9717,7 +9717,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.3+sha-d5946bd');
+const VERSION = new Version('16.0.0-next.3+sha-8f539c1');
 
 // This default value is when checking the hierarchy for a token.
 //
