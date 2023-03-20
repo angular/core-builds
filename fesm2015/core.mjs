9930d7c58549e519aa2d484f6a8f6fab8eed7c63
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 08:05:16 +0000

    b4e1bfb9b0 docs: remove bazel builder section (#49423)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-686499a
+ * @license Angular v16.0.0-next.3+sha-b4e1bfb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9344,7 +9344,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.3+sha-686499a');
+const VERSION = new Version('16.0.0-next.3+sha-b4e1bfb');
 
 // This default value is when checking the hierarchy for a token.
 //
