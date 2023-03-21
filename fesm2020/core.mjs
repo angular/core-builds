987ea3bd09dccde97de2492e13f26dfa38d2169a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 15:52:24 +0000

    24b3e8f042 build: exclude draft PRs from triage queue (#49515)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-f0f1e7d
+ * @license Angular v16.0.0-next.3+sha-24b3e8f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9373,7 +9373,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.3+sha-f0f1e7d');
+const VERSION = new Version('16.0.0-next.3+sha-24b3e8f');
 
 // This default value is when checking the hierarchy for a token.
 //
