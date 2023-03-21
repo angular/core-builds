3e4469296e77d81b98fb74537c1c83cb0670e575
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 13:02:21 +0000

    81b9e858a8 Revert "refactor(core): adding hydration for content projection (#49454)" (#49511)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-7885f35
+ * @license Angular v16.0.0-next.3+sha-81b9e85
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6017,8 +6017,6 @@ export declare class NgZone {
  */
 export declare const NO_ERRORS_SCHEMA: SchemaMetadata;
 
-declare const NODES = "n";
-
 declare const NUM_ROOT_NODES = "r";
 
 declare const ON_DESTROY_HOOKS = 22;
@@ -7948,13 +7946,6 @@ declare interface SerializedView {
      * of serialized information about views within this container.
      */
     [CONTAINERS]?: Record<number, SerializedContainerView[]>;
-    /**
-     * Serialized information about nodes in a template.
-     * Key-value pairs where a key is an index of the corresponding
-     * DOM node in an LView and the value is a path that describes
-     * the location of this node (as a set of navigation instructions).
-     */
-    [NODES]?: Record<number, string>;
 }
 
 /**
