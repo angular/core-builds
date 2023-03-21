16e4544dfa14399189c5254e8b156e36a986999f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 07:12:06 +0000

    4b4e8619b7 docs: fix link ngFor documentation link (#49479)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-84a2ad7
+ * @license Angular v16.0.0-next.3+sha-4b4e861
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9729,7 +9729,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.3+sha-84a2ad7');
+const VERSION = new Version('16.0.0-next.3+sha-4b4e861');
 
 // This default value is when checking the hierarchy for a token.
 //
