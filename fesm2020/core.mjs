9d0e10b5735ff060312590eb44d02a5f77c58cbf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 15:19:41 +0000

    3bb76768a6 docs: remove ticks in code sample to generate components (#49525)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.3+sha-be2db59
+ * @license Angular v15.2.3+sha-3bb7676
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8347,7 +8347,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.3+sha-be2db59');
+const VERSION = new Version('15.2.3+sha-3bb7676');
 
 // This default value is when checking the hierarchy for a token.
 //
