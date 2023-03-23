58adc6c041afe0001a2c061b785ae077bf3c395a
Andrew Scott <atscott01@gmail.com>
2023-03-23 19:03:01 +0000

    3f8e4fb724 docs: add missing word (#49565)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.4+sha-a844435
+ * @license Angular v15.2.4+sha-3f8e4fb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8347,7 +8347,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.4+sha-a844435');
+const VERSION = new Version('15.2.4+sha-3f8e4fb');
 
 // This default value is when checking the hierarchy for a token.
 //
