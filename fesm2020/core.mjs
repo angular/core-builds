f87dc30fc658605ef9b89865186f2596f357b778
Andrew Scott <atscott01@gmail.com>
2023-03-24 18:00:25 +0000

    978d37f324 fix(router): Ensure Router preloading works with lazy component and static children (#49571)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.4+sha-656a388
+ * @license Angular v15.2.4+sha-978d37f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8347,7 +8347,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.4+sha-656a388');
+const VERSION = new Version('15.2.4+sha-978d37f');
 
 // This default value is when checking the hierarchy for a token.
 //
