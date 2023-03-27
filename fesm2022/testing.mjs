cb3ba62952c6e89a7b8cf4de25fa171424b1832c
Andrew Scott <atscott01@gmail.com>
2023-03-27 15:50:58 +0000

    bc412fd537 feat(zone.js): jest 29 should ignore uncaught error console log (#49325)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-a1578de
+ * @license Angular v16.0.0-next.4+sha-bc412fd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9783,7 +9783,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-a1578de');
+const VERSION = new Version('16.0.0-next.4+sha-bc412fd');
 
 // This default value is when checking the hierarchy for a token.
 //
