7341f76bb8704aaea8b1acca18cf95174ca15231
Andrew Scott <atscott01@gmail.com>
2023-03-27 15:54:13 +0000

    1d63183bec ci: update pullapprove config to reflect current availability  (#49592)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-6499f5a
+ * @license Angular v16.0.0-next.4+sha-1d63183
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9386,7 +9386,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-6499f5a');
+const VERSION = new Version('16.0.0-next.4+sha-1d63183');
 
 // This default value is when checking the hierarchy for a token.
 //
