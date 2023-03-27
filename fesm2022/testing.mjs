f9e707162efb0c94f1fec46b2ca438970730f870
Andrew Scott <atscott01@gmail.com>
2023-03-27 15:52:48 +0000

    6499f5ae28 fix(common): invalid ImageKit transformation (#49201)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-bc412fd
+ * @license Angular v16.0.0-next.4+sha-6499f5a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9783,7 +9783,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-bc412fd');
+const VERSION = new Version('16.0.0-next.4+sha-6499f5a');
 
 // This default value is when checking the hierarchy for a token.
 //
