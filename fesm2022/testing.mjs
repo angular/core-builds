97987462c2aa03e2f52572e05f81cb02acd282d3
Andrew Scott <atscott01@gmail.com>
2023-03-27 15:48:33 +0000

    a1578de8fa build(zone.js): remove legacy browsers support (#49327)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-2dbf3e0
+ * @license Angular v16.0.0-next.4+sha-a1578de
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9783,7 +9783,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-2dbf3e0');
+const VERSION = new Version('16.0.0-next.4+sha-a1578de');
 
 // This default value is when checking the hierarchy for a token.
 //
