ce255c5cf1dd32d907c784849f61d74a4a50033f
GitHub <noreply@github.com>
2023-03-29 19:49:00 +0000

    e78a2a4a47 release: cut the v15.2.5 release (#49635)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.4+sha-077f6b4
+ * @license Angular v15.2.5+sha-e78a2a4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8695,7 +8695,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.4+sha-077f6b4');
+const VERSION = new Version('15.2.5+sha-e78a2a4');
 
 // This default value is when checking the hierarchy for a token.
 //
