38de6517af8e16b3f68d057b2a4a30db005bdab5
Andrew Scott <atscott01@gmail.com>
2023-03-29 22:56:54 +0000

    f2ba19253d release: cut the v16.0.0-next.5 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-cbe6807
+ * @license Angular v16.0.0-next.5+sha-f2ba192
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9390,7 +9390,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-cbe6807');
+const VERSION = new Version('16.0.0-next.5+sha-f2ba192');
 
 // This default value is when checking the hierarchy for a token.
 //
