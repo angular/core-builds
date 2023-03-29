8a815df72af6ce12d7b150e68112a8fc45016caa
Andrew Scott <atscott01@gmail.com>
2023-03-29 17:36:42 +0000

    6bcdb68e69 refactor(router): clear pending navigation task in a microtask (#49619)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-c024574
+ * @license Angular v16.0.0-next.4+sha-6bcdb68
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9390,7 +9390,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-c024574');
+const VERSION = new Version('16.0.0-next.4+sha-6bcdb68');
 
 // This default value is when checking the hierarchy for a token.
 //
