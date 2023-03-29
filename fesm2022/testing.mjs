eb0eadc81758963e47f35056d6a09ef0647bf19d
Andrew Scott <atscott01@gmail.com>
2023-03-29 16:43:32 +0000

    e8e36811d5 fix(platform-browser): set nonce attribute in a platform compatible way (#49624)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.4+sha-e994608
+ * @license Angular v16.0.0-next.4+sha-e8e3681
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9787,7 +9787,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.4+sha-e994608');
+const VERSION = new Version('16.0.0-next.4+sha-e8e3681');
 
 // This default value is when checking the hierarchy for a token.
 //
