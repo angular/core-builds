8532fa3584e0e9671e5a5f3f0d085a10515a2e0f
Dylan Hunn <dylhunn@gmail.com>
2023-03-30 23:29:17 +0000

    1dc8480bf4 refactor(router): Remove dead code (#49652)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-e9dd7f0
+ * @license Angular v16.0.0-next.5+sha-1dc8480
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9391,7 +9391,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-e9dd7f0');
+const VERSION = new Version('16.0.0-next.5+sha-1dc8480');
 
 // This default value is when checking the hierarchy for a token.
 //
