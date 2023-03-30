0cb3cf4dc6507ed51d66aff28438a6ad5080c248
Andrew Scott <atscott01@gmail.com>
2023-03-30 16:54:01 +0000

    1c3891cf50 docs: fixed typo error (#49637)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-06e74a5
+ * @license Angular v16.0.0-next.5+sha-1c3891c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9786,7 +9786,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-06e74a5');
+const VERSION = new Version('16.0.0-next.5+sha-1c3891c');
 
 // This default value is when checking the hierarchy for a token.
 //
