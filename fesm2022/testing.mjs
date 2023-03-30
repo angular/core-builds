d030ac47ce3efdbece9a6ba3057e89ed8d6abf6a
Dylan Hunn <dylhunn@gmail.com>
2023-03-30 18:54:10 +0000

    39a648f31c refactor(core): Use (where applicable), `ViewContainer.createComponent` instead of relying on the `ComponentFactoryResolver`. (#49614)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-9d96487
+ * @license Angular v16.0.0-next.5+sha-39a648f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9786,7 +9786,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-9d96487');
+const VERSION = new Version('16.0.0-next.5+sha-39a648f');
 
 // This default value is when checking the hierarchy for a token.
 //
