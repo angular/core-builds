654fcdd3ee89e4c15bdf1b1d938fcd2caa34604b
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:09:55 +0000

    78a3298f65 refactor(router): Remove internal use of guard and resolve interfaces (#49548)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-01d4a13
+ * @license Angular v16.0.0-next.5+sha-78a3298
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9391,7 +9391,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-01d4a13');
+const VERSION = new Version('16.0.0-next.5+sha-78a3298');
 
 // This default value is when checking the hierarchy for a token.
 //
