f58d9f83817e87504630c74f14ec0125377132f1
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:09:44 +0000

    01d4a136ed build: update eslint dependencies to v5.57.0 (#49608)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-12b2caa
+ * @license Angular v16.0.0-next.5+sha-01d4a13
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9787,7 +9787,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-12b2caa');
+const VERSION = new Version('16.0.0-next.5+sha-01d4a13');
 
 // This default value is when checking the hierarchy for a token.
 //
