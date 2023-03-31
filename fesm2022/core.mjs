a52f9a87fe663cca58e79bca5ca8b91df23f2bb7
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:15:31 +0000

    fda3f677d1 refactor(elements): `ComponentNgElementStrategyFactory` and `ComponentNgElementStrategy` are not part of the public API. (#49642)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.5+sha-d7d6514
+ * @license Angular v16.0.0-next.5+sha-fda3f67
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9391,7 +9391,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.5+sha-d7d6514');
+const VERSION = new Version('16.0.0-next.5+sha-fda3f67');
 
 // This default value is when checking the hierarchy for a token.
 //
