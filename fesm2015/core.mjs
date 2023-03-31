577578c8794de863fdb918014b0741bd7f6766eb
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:18:44 +0000

    08979e4589 docs: fix typo in toh-pg6.md (#49141)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.5+sha-35f4918
+ * @license Angular v15.2.5+sha-08979e4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8322,7 +8322,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.5+sha-35f4918');
+const VERSION = new Version('15.2.5+sha-08979e4');
 
 // This default value is when checking the hierarchy for a token.
 //
