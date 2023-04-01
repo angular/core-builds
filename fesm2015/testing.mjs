653f48e70dece3d24ee30dc27010a6c1149cb329
Dylan Hunn <dylhunn@gmail.com>
2023-04-01 00:24:35 +0000

    9b61379096 fix(router): Ensure initial navigation clears current navigation when blocking (#49572)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.5+sha-08979e4
+ * @license Angular v15.2.5+sha-9b61379
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8695,7 +8695,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.5+sha-08979e4');
+const VERSION = new Version('15.2.5+sha-9b61379');
 
 // This default value is when checking the hierarchy for a token.
 //
