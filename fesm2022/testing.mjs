5e8f18832a778d6f2607ddfdb599351f4c7cb39a
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:30:37 +0000

    8019fed1f7 docs: fix typo in quick reference (#49655)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-d11f5d7
+ * @license Angular v16.0.0-next.6+sha-8019fed
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9787,7 +9787,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-d11f5d7');
+const VERSION = new Version('16.0.0-next.6+sha-8019fed');
 
 // This default value is when checking the hierarchy for a token.
 //
