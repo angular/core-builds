1b8b72a2028403ba47b1b61b4bbbc8b3ed23c906
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:37:53 +0000

    61bedaf930 refactor(core): handle a case when ngSkipHydration is applied to the root node of an application (#49675)

--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-4b673d5
+ * @license Angular v16.0.0-next.6+sha-61bedaf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -142,7 +142,7 @@ export declare function discardPeriodicTasks(): void;
  *
  * Can be used to wrap `inject()` calls.
  *
- * @param fn The function that you want to wrap in the `fakeAsync` zone.
+ * @param fn The function that you want to wrap in the `fakeAysnc` zone.
  *
  * @usageNotes
  * ### Example
