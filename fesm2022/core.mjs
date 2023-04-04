5a41b4cc19b5322dd1696193c2f3ebd315019ced
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 18:39:27 +0000

    edc3bb180f Revert "feat(forms): Improve typings form (async)Validators" (#49706)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-761e02d
+ * @license Angular v16.0.0-next.6+sha-edc3bb1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9391,7 +9391,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-761e02d');
+const VERSION = new Version('16.0.0-next.6+sha-edc3bb1');
 
 // This default value is when checking the hierarchy for a token.
 //
