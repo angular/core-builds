3f8811d6860ccd3409734feda6c90b70b8395beb
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:33:35 +0000

    31f4efe979 docs: add hydration guide (#49665)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-aff1512
+ * @license Angular v16.0.0-next.6+sha-31f4efe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9787,7 +9787,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-aff1512');
+const VERSION = new Version('16.0.0-next.6+sha-31f4efe');
 
 // This default value is when checking the hierarchy for a token.
 //
