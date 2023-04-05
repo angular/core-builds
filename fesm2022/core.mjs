f0ea9fefe5461fbbd670a72a4084426f4661500a
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 18:58:52 +0000

    dc9651b3a5 refactor(platform-browser): remove empty `begin` and `end` methods from renderer (#49630)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-a6fc8b3
+ * @license Angular v16.0.0-next.6+sha-dc9651b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9954,7 +9954,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-a6fc8b3');
+const VERSION = new Version('16.0.0-next.6+sha-dc9651b');
 
 // This default value is when checking the hierarchy for a token.
 //
