2f5b88552b76ac5b3efa470b1dae61ea2bd0863f
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 16:21:22 +0000

    136ffbca8e refactor(forms): Add a test that uses ControlConfig with a non-static validator. (#49693)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-3b863dd
+ * @license Angular v16.0.0-next.6+sha-136ffbc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9392,7 +9392,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-3b863dd');
+const VERSION = new Version('16.0.0-next.6+sha-136ffbc');
 
 // This default value is when checking the hierarchy for a token.
 //
