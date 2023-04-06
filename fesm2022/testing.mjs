103373ab067b9d0b0a7cdb827a725e532c81e352
Dylan Hunn <github@dylanhunn.com>
2023-04-06 03:16:40 +0000

    726f09c01e docs: release notes for the v16.0.0-next.7 release

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-a747954
+ * @license Angular v16.1.0-next.0+sha-726f09c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10350,7 +10350,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-a747954');
+const VERSION = new Version('16.1.0-next.0+sha-726f09c');
 
 // This default value is when checking the hierarchy for a token.
 //
