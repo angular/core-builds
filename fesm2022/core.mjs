845f19231c1ccd652ad4c9a7d48c2474ee5c21ba
Joey Perrott <josephperrott.github@gmail.com>
2023-04-06 17:57:27 +0000

    ec0b44da8c docs: release notes for the v13.4.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-726f09c
+ * @license Angular v16.1.0-next.0+sha-ec0b44d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9954,7 +9954,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-726f09c');
+const VERSION = new Version('16.1.0-next.0+sha-ec0b44d');
 
 // This default value is when checking the hierarchy for a token.
 //
