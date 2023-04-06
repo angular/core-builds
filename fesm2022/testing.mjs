ddc46a1be609a3a3e80fccefd242cb1cd58a602e
Dylan Hunn <dylhunn@gmail.com>
2023-04-06 02:52:28 +0000

    40ed152d21 refactor(animations): remove #9100 todos. (#49407)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-9bd9a11
+ * @license Angular v16.0.0-next.6+sha-40ed152
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10350,7 +10350,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.6+sha-9bd9a11');
+const VERSION = new Version('16.0.0-next.6+sha-40ed152');
 
 // This default value is when checking the hierarchy for a token.
 //
