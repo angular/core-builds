461a233e69f6a07405b4fa25958d720daab3677a
Andrew Kushnir <akushnir@google.com>
2023-04-11 18:57:36 +0000

    4e41d127db build(compiler): add a flag to conditionally build with the template pipeline (#48580)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-cf46dfb
+ * @license Angular v16.1.0-next.0+sha-4e41d12
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9954,7 +9954,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-cf46dfb');
+const VERSION = new Version('16.1.0-next.0+sha-4e41d12');
 
 // This default value is when checking the hierarchy for a token.
 //
