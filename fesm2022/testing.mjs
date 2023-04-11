1c8a8efba93f8486537e0ed9d3c8756659fa0455
Andrew Kushnir <akushnir@google.com>
2023-04-11 21:30:02 +0000

    f9b821f07d fix(http): delay accessing `pendingTasks.whenAllTasksComplete` (#49784)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-e949548
+ * @license Angular v16.1.0-next.0+sha-f9b821f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10364,7 +10364,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-e949548');
+const VERSION = new Version('16.1.0-next.0+sha-f9b821f');
 
 // This default value is when checking the hierarchy for a token.
 //
