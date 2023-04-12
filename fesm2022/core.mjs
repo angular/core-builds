785df724f72c0787593c1bfdccca8eb7e8ec3d54
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:51:47 +0000

    d966fdd438 refactor(router): Ensure data is bound to components in change detection following navigation (#49741)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-c190fbb
+ * @license Angular v16.1.0-next.0+sha-d966fdd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9968,7 +9968,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-c190fbb');
+const VERSION = new Version('16.1.0-next.0+sha-d966fdd');
 
 // This default value is when checking the hierarchy for a token.
 //
