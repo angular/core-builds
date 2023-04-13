a9a30105cc4f38f268aac494996e7f09a0c4b979
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:21:46 +0000

    294fae02eb refactor(core): fix privately imported symbol from signals package (#49812)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-7fd0b67
+ * @license Angular v16.1.0-next.0+sha-294fae0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10348,7 +10348,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-7fd0b67');
+const VERSION = new Version('16.1.0-next.0+sha-294fae0');
 
 // This default value is when checking the hierarchy for a token.
 //
