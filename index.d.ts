b5c7d77701306f7c55ed45d444b6ca74a560f0bb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:28:23 +0000

    38fe1b91fc refactor(core): drop IE workarounds (#49763)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-ddf0d4e
+ * @license Angular v16.1.0-next.0+sha-38fe1b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10879,10 +10879,9 @@ export declare interface ɵInternalEnvironmentProviders extends EnvironmentProvi
 }
 
 /**
- * Internal token that specifies whether DOM reuse logic
- * during hydration is enabled.
+ * Internal token that specifies whether hydration is enabled.
  */
-export declare const ɵIS_HYDRATION_DOM_REUSE_ENABLED: InjectionToken<boolean>;
+export declare const ɵIS_HYDRATION_FEATURE_ENABLED: InjectionToken<boolean>;
 
 export declare function ɵisBoundToModule<C>(cf: ComponentFactory<C>): boolean;
 
