452829e6313185cc5e744c14b3412cad0024b666
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 19:39:12 +0000

    8c3b92cfb3 refactor(platform-server): simplify `_render` method (#49840)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-9e9b424
+ * @license Angular v16.1.0-next.0+sha-8c3b92c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-9e9b424');
+const VERSION = new Version('16.1.0-next.0+sha-8c3b92c');
 
 // This default value is when checking the hierarchy for a token.
 //
