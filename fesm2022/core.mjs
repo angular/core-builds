c843396ce9401bca3424c4249b193257f12b1ebe
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:16:38 +0000

    f9886e1e28 refactor(docs-infra): remove dart support & mentions (#49857)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-ea4d9e8
+ * @license Angular v16.1.0-next.0+sha-f9886e1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-ea4d9e8');
+const VERSION = new Version('16.1.0-next.0+sha-f9886e1');
 
 // This default value is when checking the hierarchy for a token.
 //
