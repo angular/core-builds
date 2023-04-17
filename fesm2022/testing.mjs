290f1fd2df0dc2baea75ce276532888815d36037
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:24:49 +0000

    3659e035f7 test: refactor platform-server tests to use async/await (#49855)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-288f2c8
+ * @license Angular v16.1.0-next.0+sha-3659e03
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-288f2c8');
+const VERSION = new Version('16.1.0-next.0+sha-3659e03');
 
 // This default value is when checking the hierarchy for a token.
 //
