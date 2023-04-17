1324f324a3c00110b3d3b0c09a4ccdb0cd88ac4c
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 17:47:23 +0000

    49386de19f docs: Specify when an app is debuggable by the DevTools. (#48970)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-2aa6d6d
+ * @license Angular v16.1.0-next.0+sha-49386de
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-2aa6d6d');
+const VERSION = new Version('16.1.0-next.0+sha-49386de');
 
 // This default value is when checking the hierarchy for a token.
 //
