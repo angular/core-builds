4c6ad9e227a55b858b1c2f98d5c6ef8c0a3e9dff
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 17:52:41 +0000

    f6b9cf3f66 refactor(compiler): Remove unnecessary assertion in jit_compiler_facade. (#49852)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-a49279d
+ * @license Angular v16.1.0-next.0+sha-f6b9cf3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-a49279d');
+const VERSION = new Version('16.1.0-next.0+sha-f6b9cf3');
 
 // This default value is when checking the hierarchy for a token.
 //
