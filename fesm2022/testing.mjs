c5bc7604dd7289b94bef4f47755eef752bebba07
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:14:56 +0000

    ea4d9e8ef8 docs: fix typo in Hierarchical injectors doc (#49824)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-fd0c26e
+ * @license Angular v16.1.0-next.0+sha-ea4d9e8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-fd0c26e');
+const VERSION = new Version('16.1.0-next.0+sha-ea4d9e8');
 
 // This default value is when checking the hierarchy for a token.
 //
