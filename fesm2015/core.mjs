810caea544c715847772a3e2f095c6cbca0110d1
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:22:32 +0000

    1017630d04 refactor(docs-infra): cleanup deprecated code (#49671)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-a63d9a3
+ * @license Angular v15.2.7+sha-1017630
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8308,7 +8308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-a63d9a3');
+const VERSION = new Version('15.2.7+sha-1017630');
 
 // This default value is when checking the hierarchy for a token.
 //
