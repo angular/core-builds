5efb601a7b20313b2d6fc60a25035fef119906ee
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 17:39:14 +0000

    2aa6d6d616 refactor(compiler-cli): cleanup inferences (#49863)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-8f28f19
+ * @license Angular v16.1.0-next.0+sha-2aa6d6d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-8f28f19');
+const VERSION = new Version('16.1.0-next.0+sha-2aa6d6d');
 
 // This default value is when checking the hierarchy for a token.
 //
