370f476bea9df75c890b143a83ea73141a356af6
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 15:58:44 +0000

    8f28f1999b build: update dependency eslint-plugin-jsdoc to v41 (#49885)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-f023020
+ * @license Angular v16.1.0-next.0+sha-8f28f19
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-f023020');
+const VERSION = new Version('16.1.0-next.0+sha-8f28f19');
 
 // This default value is when checking the hierarchy for a token.
 //
