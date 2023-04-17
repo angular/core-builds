64b2b2cfc1b3899e59f5912f765115eb5fe17595
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 18:01:04 +0000

    3c65305b10 refactor(router): remove targetPageId from transition (#49799)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-f6b9cf3
+ * @license Angular v16.1.0-next.0+sha-3c65305
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-f6b9cf3');
+const VERSION = new Version('16.1.0-next.0+sha-3c65305');
 
 // This default value is when checking the hierarchy for a token.
 //
