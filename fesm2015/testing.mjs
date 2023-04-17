2c2c3141d65796347d85ccd06d4675566c911667
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 18:47:34 +0000

    1d4cf95a94 docs(docs-infra): add lara newsom to GDE resources (#49620)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-79a8b3c
+ * @license Angular v15.2.7+sha-1d4cf95
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8681,7 +8681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-79a8b3c');
+const VERSION = new Version('15.2.7+sha-1d4cf95');
 
 // This default value is when checking the hierarchy for a token.
 //
--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-79a8b3c
+ * @license Angular v15.2.7+sha-1d4cf95
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8331,7 +8331,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-79a8b3c');
+const VERSION = new Version('15.2.7+sha-1d4cf95');
 
 // This default value is when checking the hierarchy for a token.
 //
