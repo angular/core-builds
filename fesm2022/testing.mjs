2cb60e93ee237b68e159e332a596ad1c3222d5d6
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 14:07:02 +0000

    79292c9b65 docs: include a note on blocking initial navigation when destructive hydration is used (#49895)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-2845017
+ * @license Angular v16.1.0-next.0+sha-79292c9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-2845017');
+const VERSION = new Version('16.1.0-next.0+sha-79292c9');
 
 // This default value is when checking the hierarchy for a token.
 //
