06ae3adb36e77c10dd46379b5ce45dfe520667fb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:51:04 +0000

    27c624e8e7 refactor(compiler): handle #24571 todos. (#49220)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-8219857
+ * @license Angular v16.0.0-rc.1+sha-27c624e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-8219857');
+const VERSION = new Version('16.0.0-rc.1+sha-27c624e');
 
 // This default value is when checking the hierarchy for a token.
 //
