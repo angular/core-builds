53ddc237668e7a45c573110b2378bcabf1b2451c
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:57:03 +0000

    ded7ede1d4 test: update platform-server integration tests to use the Angular CLI (#49900)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-5e4797c
+ * @license Angular v16.0.0-rc.1+sha-ded7ede
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-5e4797c');
+const VERSION = new Version('16.0.0-rc.1+sha-ded7ede');
 
 // This default value is when checking the hierarchy for a token.
 //
