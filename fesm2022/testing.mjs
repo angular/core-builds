21b15a147890e313b98f0ffdb86e18d218766546
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:32:09 +0000

    36ed47fdf4 docs: fix errors in toh-pt6.md (#49129)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-09332e7
+ * @license Angular v16.1.0-next.0+sha-36ed47f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-09332e7');
+const VERSION = new Version('16.1.0-next.0+sha-36ed47f');
 
 // This default value is when checking the hierarchy for a token.
 //
