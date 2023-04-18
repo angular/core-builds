4a158cb28c7df1dc56587580f2379761e7f6f1ab
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 14:19:25 +0000

    bce7f6bfd9 refactor(core): Remove ununsed Zone mock from testing internals. (#49873)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-1d4cf95
+ * @license Angular v15.2.7+sha-bce7f6b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8331,7 +8331,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-1d4cf95');
+const VERSION = new Version('15.2.7+sha-bce7f6b');
 
 // This default value is when checking the hierarchy for a token.
 //
