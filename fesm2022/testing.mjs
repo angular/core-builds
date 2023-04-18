f0636b4b79656ff1eb124c1ffb7405ba9c22a98e
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 17:18:08 +0000

    ead10aff41 refactor(compiler): next context merging in the template pipeline (#49797)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-27c624e
+ * @license Angular v16.0.0-rc.1+sha-ead10af
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-27c624e');
+const VERSION = new Version('16.0.0-rc.1+sha-ead10af');
 
 // This default value is when checking the hierarchy for a token.
 //
