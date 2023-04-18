b4730533bab3c54d8bdcfab46c196f0c6a051a01
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 17:16:54 +0000

    cab701d0ee docs: update README.md (#48423)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-5a13eb8
+ * @license Angular v15.2.7+sha-cab701d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8308,7 +8308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-5a13eb8');
+const VERSION = new Version('15.2.7+sha-cab701d');
 
 // This default value is when checking the hierarchy for a token.
 //
