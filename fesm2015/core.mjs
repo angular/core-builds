e631fae3fcc72f7eb6f9a3908f4990c80ed47e50
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 21:07:16 +0000

    279e9e7fdb build: update dependency build_bazel_rules_nodejs to v5.8.2 (#49910)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-6a93956
+ * @license Angular v15.2.7+sha-279e9e7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8308,7 +8308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-6a93956');
+const VERSION = new Version('15.2.7+sha-279e9e7');
 
 // This default value is when checking the hierarchy for a token.
 //
