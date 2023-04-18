bf847fc921f0bd1e984342d43f605e6ccc089956
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:20:14 +0000

    fb1161f2ec docs: type clearTimer function as VoidFunction or undefined (#48480)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-0d9705b
+ * @license Angular v16.1.0-next.0+sha-fb1161f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-0d9705b');
+const VERSION = new Version('16.1.0-next.0+sha-fb1161f');
 
 // This default value is when checking the hierarchy for a token.
 //
