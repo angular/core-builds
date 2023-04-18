a5a62fbc348ee14bdb59abdd5e36552baa573efb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:57:06 +0000

    ce4a57288c build: update actions/cache digest to 88522ab (#49437)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-03ae094
+ * @license Angular v16.1.0-next.0+sha-ce4a572
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-03ae094');
+const VERSION = new Version('16.1.0-next.0+sha-ce4a572');
 
 // This default value is when checking the hierarchy for a token.
 //
