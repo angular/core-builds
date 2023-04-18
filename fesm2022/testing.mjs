48a390fabd923e348f1f4e8a02d9d278c7ba5bc8
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:21:53 +0000

    80907b2d23 docs: Update hydration guide constraints section (#49908)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-fb1161f
+ * @license Angular v16.1.0-next.0+sha-80907b2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-fb1161f');
+const VERSION = new Version('16.1.0-next.0+sha-80907b2');
 
 // This default value is when checking the hierarchy for a token.
 //
