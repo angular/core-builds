95f583adc9faab7a5ca345b0928979b40ef97d82
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-04-19 19:26:20 +0000

    4b49bfc4f0 docs: release notes for the v15.2.8 release

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-a29c51b
+ * @license Angular v16.1.0-next.0+sha-4b49bfc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-a29c51b');
+const VERSION = new Version('16.1.0-next.0+sha-4b49bfc');
 
 // This default value is when checking the hierarchy for a token.
 //
