fb176710888c40034d706e193b0bda54c77f86f2
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 15:07:06 +0000

    a175ac5441 test: update platform-server tests to test both ngmodule and standalone application (#49927)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-21b834f
+ * @license Angular v16.0.0-rc.1+sha-a175ac5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.1+sha-21b834f');
+const VERSION = new Version('16.0.0-rc.1+sha-a175ac5');
 
 // This default value is when checking the hierarchy for a token.
 //
