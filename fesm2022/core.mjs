0238a5406e41865dc738341747375365079b6964
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 20:10:45 +0000

    9180f98f0e fix(core): ensure takeUntilDestroyed unregisters onDestroy listener on unsubscribe (#49901)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.2+sha-2f4b61e
+ * @license Angular v16.0.0-rc.2+sha-9180f98
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.2+sha-2f4b61e');
+const VERSION = new Version('16.0.0-rc.2+sha-9180f98');
 
 // This default value is when checking the hierarchy for a token.
 //
--- fesm2022/rxjs-interop.mjs
+++ fesm2022/rxjs-interop.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.2+sha-2f4b61e
+ * @license Angular v16.0.0-rc.2+sha-9180f98
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,8 @@ function takeUntilDestroyed(destroyRef) {
         destroyRef = inject(DestroyRef);
     }
     const destroyed$ = new Observable(observer => {
-        destroyRef.onDestroy(observer.next.bind(observer));
+        const unregisterFn = destroyRef.onDestroy(observer.next.bind(observer));
+        return unregisterFn;
     });
     return (source) => {
         return source.pipe(takeUntil(destroyed$));
