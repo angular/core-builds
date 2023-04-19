5cf8e9232c0fe18e7f926484c90079ae85984956
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 18:56:30 +0000

    a29c51bca4 docs: add information on how to use index option in configuration (#49813)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-1dc919a
+ * @license Angular v16.1.0-next.0+sha-a29c51b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-1dc919a');
+const VERSION = new Version('16.1.0-next.0+sha-a29c51b');
 
 // This default value is when checking the hierarchy for a token.
 //
