b8fdc2d47f29269a5de0668ef4bc8f67e0742eaa
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-04-19 19:29:19 +0000

    35690fdf6d release: cut the v15.2.8 release

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.7+sha-3d7b8cd
+ * @license Angular v15.2.8+sha-35690fd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8331,7 +8331,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.7+sha-3d7b8cd');
+const VERSION = new Version('15.2.8+sha-35690fd');
 
 // This default value is when checking the hierarchy for a token.
 //
