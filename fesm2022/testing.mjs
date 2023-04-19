7d53121884d60623c2485292d59de610fddffdad
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 14:21:35 +0000

    c7aa10f2f5 docs: update events (#49569)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-bf8041f
+ * @license Angular v16.1.0-next.0+sha-c7aa10f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-bf8041f');
+const VERSION = new Version('16.1.0-next.0+sha-c7aa10f');
 
 // This default value is when checking the hierarchy for a token.
 //
