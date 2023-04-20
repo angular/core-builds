81fc484398db482c4303183000e788dac8f8cc06
Dylan Hunn <dylhunn@gmail.com>
2023-04-20 22:00:51 +0000

    da1ed98dfd docs: replaces "comp: docs" label to "area: docs" (#49952)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-c029c67
+ * @license Angular v16.1.0-next.0+sha-da1ed98
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-c029c67');
+const VERSION = new Version('16.1.0-next.0+sha-da1ed98');
 
 // This default value is when checking the hierarchy for a token.
 //
