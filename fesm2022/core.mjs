a07ffeb224c481745bbba1a5973b1144e11853d4
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 19:09:42 +0000

    5f7d929d9e docs(core): correct description of actively supported versions (#49945)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-a5428a7
+ * @license Angular v16.1.0-next.0+sha-5f7d929
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-a5428a7');
+const VERSION = new Version('16.1.0-next.0+sha-5f7d929');
 
 // This default value is when checking the hierarchy for a token.
 //
