e44786bbeaedf9cc98fb9b2ae3b83b80c68856db
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 19:07:51 +0000

    b9c53658a4 refactor(compiler-cli): remove unecessary type assertion (#49971)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-92897b4
+ * @license Angular v16.1.0-next.0+sha-b9c5365
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-92897b4');
+const VERSION = new Version('16.1.0-next.0+sha-b9c5365');
 
 // This default value is when checking the hierarchy for a token.
 //
