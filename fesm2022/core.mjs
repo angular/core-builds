01fbb91a75cf936a16b183b2a3b4e8af7ea534e7
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 17:08:25 +0000

    1752af1cf5 docs: applicability of NgModule providers (#49966)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-bb6a3e8
+ * @license Angular v16.1.0-next.0+sha-1752af1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-bb6a3e8');
+const VERSION = new Version('16.1.0-next.0+sha-1752af1');
 
 // This default value is when checking the hierarchy for a token.
 //
