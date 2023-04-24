02258c1b11f5310883b3985c7a05abacd3d73aad
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 01:38:39 +0000

    eb5bc95dbf docs: remove the warning about shadowDom support (#49961)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-345dd6d
+ * @license Angular v16.1.0-next.0+sha-eb5bc95
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-345dd6d');
+const VERSION = new Version('16.1.0-next.0+sha-eb5bc95');
 
 // This default value is when checking the hierarchy for a token.
 //
