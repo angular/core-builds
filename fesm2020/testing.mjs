b3222f1b31f9dffa3c12658a13751fee2023d075
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:26 +0000

    bdf2aa1957 build: bump license year (#49984)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-1ef6205
+ * @license Angular v15.2.8+sha-bdf2aa1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8689,7 +8689,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.8+sha-1ef6205');
+const VERSION = new Version('15.2.8+sha-bdf2aa1');
 
 // This default value is when checking the hierarchy for a token.
 //
