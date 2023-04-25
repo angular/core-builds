8faf6111d7421afc25279c5c84d359f49dc28c9a
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:50 +0000

    f5dd3b4ede refactor(docs-infra): remove `semi` rule from eslintrc. (#49970)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-1090bf8
+ * @license Angular v16.1.0-next.0+sha-f5dd3b4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-1090bf8');
+const VERSION = new Version('16.1.0-next.0+sha-f5dd3b4');
 
 // This default value is when checking the hierarchy for a token.
 //
