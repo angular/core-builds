31a8e289781ca75cca782152b027db5589d50591
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:12 +0000

    1090bf870f refactor(core): improve error message when dom node is not found (#49977)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-8a2739f
+ * @license Angular v16.1.0-next.0+sha-1090bf8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-8a2739f');
+const VERSION = new Version('16.1.0-next.0+sha-1090bf8');
 
 // This default value is when checking the hierarchy for a token.
 //
