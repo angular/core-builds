03528f0cf5dc79b1823586c9c277b43a02130d20
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 18:40:25 +0000

    3599214526 build: update dependency patch-package to v7 (#50002)

--- fesm2020/testing.mjs
+++ fesm2020/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-dd4fb18
+ * @license Angular v15.2.8+sha-3599214
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8689,7 +8689,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.8+sha-dd4fb18');
+const VERSION = new Version('15.2.8+sha-3599214');
 
 // This default value is when checking the hierarchy for a token.
 //
