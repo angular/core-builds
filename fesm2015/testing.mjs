f6c645e18d136f89b4b2a65b06273a3ac3bde387
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 19:51:49 +0000

    1584b32f09 build: update io_bazel_rules_sass digest to 236d2fc (#50006)

--- fesm2015/testing.mjs
+++ fesm2015/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-3599214
+ * @license Angular v15.2.8+sha-1584b32
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8681,7 +8681,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.8+sha-3599214');
+const VERSION = new Version('15.2.8+sha-1584b32');
 
 // This default value is when checking the hierarchy for a token.
 //
