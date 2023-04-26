568892d177eeabb1d59f119bc778459733455434
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:35:22 +0000

    2d1691aa4f docs: update universal guide to include standalone methods (#49993)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-355fa72
+ * @license Angular v16.1.0-next.0+sha-2d1691a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9965,7 +9965,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-355fa72');
+const VERSION = new Version('16.1.0-next.0+sha-2d1691a');
 
 // This default value is when checking the hierarchy for a token.
 //
