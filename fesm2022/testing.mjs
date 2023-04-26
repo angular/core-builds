570dab31f5af717124a2050f4fb0dc7a781eda8c
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:33:41 +0000

    355fa72c5e docs: update links to reference the version compatibility guide (#49995)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-8fdc116
+ * @license Angular v16.1.0-next.0+sha-355fa72
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10361,7 +10361,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-8fdc116');
+const VERSION = new Version('16.1.0-next.0+sha-355fa72');
 
 // This default value is when checking the hierarchy for a token.
 //
