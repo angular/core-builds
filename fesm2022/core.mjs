3f6aacf5eec917cfc389b32da89ba4e661bb8931
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:42:22 +0000

    744f7ffb61 refactor(platform-server): avoid using `platformDynamicServer` when `ngJitMode` is disabled (#50007)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-8a324c5
+ * @license Angular v16.1.0-next.0+sha-744f7ff
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-8a324c5');
+const VERSION = new Version('16.1.0-next.0+sha-744f7ff');
 
 // This default value is when checking the hierarchy for a token.
 //
