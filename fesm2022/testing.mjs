5ee34efac6df3a9cfe518d0b44bd8757cb909bff
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:40:31 +0000

    74194de6f9 fix(platform-browser): export deprecated `TransferState` as type (#50015)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-08949c7
+ * @license Angular v16.1.0-next.0+sha-74194de
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10359,7 +10359,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-08949c7');
+const VERSION = new Version('16.1.0-next.0+sha-74194de');
 
 // This default value is when checking the hierarchy for a token.
 //
