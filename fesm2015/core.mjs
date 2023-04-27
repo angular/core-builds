d61e9c1edd3a463ed9764185961d674d6b15d0f6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-27 15:37:04 +0000

    9107e931ca fix(common): fix incorrectly reported distortion for padded images (#49889)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-1584b32
+ * @license Angular v15.2.8+sha-9107e93
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8308,7 +8308,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.8+sha-1584b32');
+const VERSION = new Version('15.2.8+sha-9107e93');
 
 // This default value is when checking the hierarchy for a token.
 //
