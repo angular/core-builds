9f0eb96bfa8ae3bcd3d197681c0cd07d8f240437
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-04-28 23:52:20 +0000

    6dc3add921 docs(core): primary Angular Signals documentation (#50053)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.3+sha-d5fb3ad
+ * @license Angular v16.0.0-rc.3+sha-6dc3add
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10377,7 +10377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.3+sha-d5fb3ad');
+const VERSION = new Version('16.0.0-rc.3+sha-6dc3add');
 
 // This default value is when checking the hierarchy for a token.
 //
