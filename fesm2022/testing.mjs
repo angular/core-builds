32facdcb5f321fbec71a97e57798235b03475fae
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 09:52:10 +0000

    aba7f03920 ci: update link to start google presubmit (#50060)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-5a37928
+ * @license Angular v16.1.0-next.0+sha-aba7f03
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10379,7 +10379,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-5a37928');
+const VERSION = new Version('16.1.0-next.0+sha-aba7f03');
 
 // This default value is when checking the hierarchy for a token.
 //
