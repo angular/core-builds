580e8b93ef1a3a35ff91d08a3e462c3de21372e8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 16:36:16 +0000

    cb5c4e0bff build: update cross-repo angular dependencies (#50067)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.3+sha-6da1054
+ * @license Angular v16.0.0-rc.3+sha-cb5c4e0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10377,7 +10377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.3+sha-6da1054');
+const VERSION = new Version('16.0.0-rc.3+sha-cb5c4e0');
 
 // This default value is when checking the hierarchy for a token.
 //
