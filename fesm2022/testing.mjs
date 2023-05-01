e74cd143c2a77ae5138726b1e953a6a313c90cfe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 14:56:02 +0000

    f49c6d0a2f refactor(common): Deprecate useless isPlatform functions (#50055)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-109eb88
+ * @license Angular v16.1.0-next.0+sha-f49c6d0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10379,7 +10379,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-109eb88');
+const VERSION = new Version('16.1.0-next.0+sha-f49c6d0');
 
 // This default value is when checking the hierarchy for a token.
 //
