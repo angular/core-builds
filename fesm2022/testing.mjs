a996b8bc0b23f440c5ff63633d3d95d0eeaaa239
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 15:12:43 +0000

    d6d347fec5 docs: add developer preview guide for esbuild-based build system (#50068)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.3+sha-6dc3add
+ * @license Angular v16.0.0-rc.3+sha-d6d347f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10377,7 +10377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.3+sha-6dc3add');
+const VERSION = new Version('16.0.0-rc.3+sha-d6d347f');
 
 // This default value is when checking the hierarchy for a token.
 //
