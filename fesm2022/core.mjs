48710b2c5d99b56d52c47cb7aead498d56be93a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 14:52:42 +0000

    08b664d3c8 docs: Introduce RxJS and Signals interop guide (#50039)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-50a4a7c
+ * @license Angular v16.1.0-next.0+sha-08b664d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9983,7 +9983,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-50a4a7c');
+const VERSION = new Version('16.1.0-next.0+sha-08b664d');
 
 // This default value is when checking the hierarchy for a token.
 //
