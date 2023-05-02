ff9e71f5478ba720ca1e6a3abee1818745eb7096
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 10:06:37 +0000

    90192c80fa docs: fix a typo (#50092) PR Close #50092

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-1bc3546
+ * @license Angular v16.1.0-next.0+sha-90192c8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10379,7 +10379,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-1bc3546');
+const VERSION = new Version('16.1.0-next.0+sha-90192c8');
 
 // This default value is when checking the hierarchy for a token.
 //
