4deff0c12d6fe562e9ee9d78edffc9e10e664766
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 10:20:52 +0000

    f0bef4c438 docs: archiving older changelog entries (#49957)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-c68545e
+ * @license Angular v16.1.0-next.0+sha-f0bef4c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10379,7 +10379,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-c68545e');
+const VERSION = new Version('16.1.0-next.0+sha-f0bef4c');
 
 // This default value is when checking the hierarchy for a token.
 //
