c3e31412b4404e7c78c6d956e64a171e06dad692
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 14:30:41 +0000

    37443d8680 test: update payload golden for AIO to reflect reduction in size (#50107)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.4+sha-8080bf0
+ * @license Angular v16.0.0-rc.4+sha-37443d8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10377,7 +10377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-rc.4+sha-8080bf0');
+const VERSION = new Version('16.0.0-rc.4+sha-37443d8');
 
 // This default value is when checking the hierarchy for a token.
 //
