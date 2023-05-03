0aa50db0138747da4ac98b0bf89d04bd4ea235d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-03 20:16:29 +0000

    b3730081c3 docs: add the v16 blog post link to the changelog (#50132)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-612771e
+ * @license Angular v16.1.0-next.0+sha-b373008
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9980,7 +9980,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-612771e');
+const VERSION = new Version('16.1.0-next.0+sha-b373008');
 
 // This default value is when checking the hierarchy for a token.
 //
--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-612771e
+ * @license Angular v16.1.0-next.0+sha-b373008
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10377,7 +10377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-612771e');
+const VERSION = new Version('16.1.0-next.0+sha-b373008');
 
 // This default value is when checking the hierarchy for a token.
 //
