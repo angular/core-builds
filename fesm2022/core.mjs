60d6fbc266f877333e32e053c0ec7f75fa7f5c04
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-03 20:12:23 +0000

    612771ed54 build: update aio to 16.0.0 (#50131)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-f97ec15
+ * @license Angular v16.1.0-next.0+sha-612771e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9980,7 +9980,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-f97ec15');
+const VERSION = new Version('16.1.0-next.0+sha-612771e');
 
 // This default value is when checking the hierarchy for a token.
 //
