73bcc117cf67d913869c092be3eefc6b514ea91f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:13:06 +0000

    cb8cdadd3b refactor(compiler): reflect arrow function definition (#50084)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-2180a69
+ * @license Angular v16.1.0-next.0+sha-cb8cdad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10377,7 +10377,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-2180a69');
+const VERSION = new Version('16.1.0-next.0+sha-cb8cdad');
 
 // This default value is when checking the hierarchy for a token.
 //
