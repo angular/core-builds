8e0de7231c16c46dd6213fd591721c4f9791172e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 10:40:48 +0000

    2f03eefd5c refactor(platform-browser): Throw tree-shakable errors. (#49341)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0+sha-f28580a
+ * @license Angular v16.0.0+sha-2f03eef
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10375,7 +10375,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0+sha-f28580a');
+const VERSION = new Version('16.0.0+sha-2f03eef');
 
 // This default value is when checking the hierarchy for a token.
 //
