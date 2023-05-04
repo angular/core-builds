866bc8c0b7d0b365acb1501588de8c0ccd4a028e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:02:51 +0000

    6024b6669c build: update dependency puppeteer-core to v20 (#50112)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-d78678e
+ * @license Angular v16.1.0-next.0+sha-6024b66
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9980,7 +9980,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-d78678e');
+const VERSION = new Version('16.1.0-next.0+sha-6024b66');
 
 // This default value is when checking the hierarchy for a token.
 //
