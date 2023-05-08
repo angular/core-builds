7f69e30b714feb7f68148f38667c0790d9d633f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 22:02:05 +0000

    fc1d777c69 docs: Fixed Spelling 'servivce' to 'service' (#50204)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-4655fd5
+ * @license Angular v16.1.0-next.0+sha-fc1d777
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9982,7 +9982,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-4655fd5');
+const VERSION = new Version('16.1.0-next.0+sha-fc1d777');
 
 // This default value is when checking the hierarchy for a token.
 //
