27c725d0ddfc0031455b22e572a98f2875ee37ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 22:03:41 +0000

    a648e30449 docs: fix filename in first-app (#50207)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-fc1d777
+ * @license Angular v16.1.0-next.0+sha-a648e30
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10378,7 +10378,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-fc1d777');
+const VERSION = new Version('16.1.0-next.0+sha-a648e30');
 
 // This default value is when checking the hierarchy for a token.
 //
