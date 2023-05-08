d710e3df785e967cbc24b0c98226e77a28d9910d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 21:59:54 +0000

    4655fd5ebc docs: fix obs variable name to obs$ (#50196)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-46a3692
+ * @license Angular v16.1.0-next.0+sha-4655fd5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9982,7 +9982,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-46a3692');
+const VERSION = new Version('16.1.0-next.0+sha-4655fd5');
 
 // This default value is when checking the hierarchy for a token.
 //
--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-46a3692
+ * @license Angular v16.1.0-next.0+sha-4655fd5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10378,7 +10378,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-46a3692');
+const VERSION = new Version('16.1.0-next.0+sha-4655fd5');
 
 // This default value is when checking the hierarchy for a token.
 //
