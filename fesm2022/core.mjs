84b644a87c9552142343b1c7274513a556b0b16c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:00:08 +0000

    a1dc3bb503 build(devtools): target es2020 explicitly (#50086)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-0f40756
+ * @license Angular v16.1.0-next.0+sha-a1dc3bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9982,7 +9982,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-0f40756');
+const VERSION = new Version('16.1.0-next.0+sha-a1dc3bb');
 
 // This default value is when checking the hierarchy for a token.
 //
--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-0f40756
+ * @license Angular v16.1.0-next.0+sha-a1dc3bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10378,7 +10378,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-0f40756');
+const VERSION = new Version('16.1.0-next.0+sha-a1dc3bb');
 
 // This default value is when checking the hierarchy for a token.
 //
