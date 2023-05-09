4860c55346069e61c5763d4d57a2ca39299a3276
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:06:30 +0000

    2ef473c524 docs: fix typos on first-app tutorial (#50211)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-6636e83
+ * @license Angular v16.1.0-next.0+sha-2ef473c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10446,7 +10446,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-6636e83');
+const VERSION = new Version('16.1.0-next.0+sha-2ef473c');
 
 // This default value is when checking the hierarchy for a token.
 //
