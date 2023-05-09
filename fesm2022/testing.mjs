a90d3276fe2d1b39dceb32501cdb0d5e2f487f87
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:13:50 +0000

    69dadd2502 feat(core): support TypeScript 5.1 (#50156)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-4e99439
+ * @license Angular v16.1.0-next.0+sha-69dadd2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10384,7 +10384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-4e99439');
+const VERSION = new Version('16.1.0-next.0+sha-69dadd2');
 
 // This default value is when checking the hierarchy for a token.
 //
