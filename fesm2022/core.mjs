16fad9a9a8dab5b6e9926fe5ec9648b32c74a0b7
Andrew Kushnir <akushnir@google.com>
2023-05-10 21:49:03 +0000

    8e821ca41e refactor(elements): remove unnecessary polyfill deps (#50115)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-5db170c
+ * @license Angular v16.1.0-next.0+sha-8e821ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10034,7 +10034,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-5db170c');
+const VERSION = new Version('16.1.0-next.0+sha-8e821ca');
 
 // This default value is when checking the hierarchy for a token.
 //
