19fc44b25dc8ca565ed9b362e7ac9df4d687943a
Andrew Kushnir <akushnir@google.com>
2023-05-10 21:40:06 +0000

    143b85184d refactor: remove benchpres tests for other frameworks (#50108)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-cd5946b
+ * @license Angular v16.1.0-next.0+sha-143b851
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10034,7 +10034,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-cd5946b');
+const VERSION = new Version('16.1.0-next.0+sha-143b851');
 
 // This default value is when checking the hierarchy for a token.
 //
