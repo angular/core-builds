24ac48f6cbc1125786ba602fa6b6c6cd8c422ff8
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-05-10 18:27:12 +0000

    62545ea288 docs: release notes for the v16.0.1 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-6945aff
+ * @license Angular v16.1.0-next.0+sha-62545ea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10050,7 +10050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-6945aff');
+const VERSION = new Version('16.1.0-next.0+sha-62545ea');
 
 // This default value is when checking the hierarchy for a token.
 //
