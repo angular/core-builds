5a6d8a979addcdff44510ffaaefc7ddab2914c06
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-05-10 18:34:15 +0000

    ea3b5f1cde release: cut the v16.1.0-next.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-62545ea
+ * @license Angular v16.1.0-next.0+sha-ea3b5f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10050,7 +10050,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-62545ea');
+const VERSION = new Version('16.1.0-next.0+sha-ea3b5f1');
 
 // This default value is when checking the hierarchy for a token.
 //
