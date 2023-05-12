af85dad1bb9395ae196d931dee9fb66a6e78d53a
Andrew Kushnir <akushnir@google.com>
2023-05-12 17:30:19 +0000

    06f19ce582 docs: fix mistake in tutorial (#50261)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-839074b
+ * @license Angular v16.1.0-next.0+sha-06f19ce
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10039,7 +10039,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-839074b');
+const VERSION = new Version('16.1.0-next.0+sha-06f19ce');
 
 // This default value is when checking the hierarchy for a token.
 //
