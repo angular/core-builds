c184dcbd394b4bd96b7b9ff4126e8080cf0f4eb1
Andrew Kushnir <akushnir@google.com>
2023-05-12 17:32:54 +0000

    aad267ac07 docs: add missing "when" (#50262) PR Close #50262

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-06f19ce
+ * @license Angular v16.1.0-next.0+sha-aad267a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10039,7 +10039,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-06f19ce');
+const VERSION = new Version('16.1.0-next.0+sha-aad267a');
 
 // This default value is when checking the hierarchy for a token.
 //
