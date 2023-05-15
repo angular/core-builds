2c330d80d8892ebfbcbc59a173a3bdd5c87343e3
Andrew Kushnir <akushnir@google.com>
2023-05-15 16:32:22 +0000

    d0262cd0e4 build: don't run saucelabs tests yet on PRs... that will happen in a followup (#49200)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-85b4941
+ * @license Angular v16.1.0-next.0+sha-d0262cd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10039,7 +10039,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-85b4941');
+const VERSION = new Version('16.1.0-next.0+sha-d0262cd');
 
 // This default value is when checking the hierarchy for a token.
 //
