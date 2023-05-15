29a637343afab32c03524fa3e7b18085d0826ff0
Andrew Kushnir <akushnir@google.com>
2023-05-15 16:49:47 +0000

    37acd37777 docs(docs-infra): Add a deprecated label to APIs (#50287)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.1+sha-ee08121
+ * @license Angular v16.0.1+sha-37acd37
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10432,7 +10432,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.1+sha-ee08121');
+const VERSION = new Version('16.0.1+sha-37acd37');
 
 // This default value is when checking the hierarchy for a token.
 //
@@ -16037,8 +16037,6 @@ function wrapListener(tNode, lView, context, listenerFn, wrapWithPreventDefault)
         }
         if (wrapWithPreventDefault && result === false) {
             e.preventDefault();
-            // Necessary for legacy browsers that don't support preventDefault (e.g. IE)
-            e.returnValue = false;
         }
         return result;
     };
