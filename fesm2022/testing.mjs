a70b18c6054c64e2281592e49c15c10779b7fbce
Andrew Kushnir <akushnir@google.com>
2023-05-15 16:41:01 +0000

    d0f6d31228 docs(docs-infra): Remove internal constructors from the doc. (#50282)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-8885bf4
+ * @license Angular v16.1.0-next.0+sha-d0f6d31
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10435,7 +10435,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-8885bf4');
+const VERSION = new Version('16.1.0-next.0+sha-d0f6d31');
 
 // This default value is when checking the hierarchy for a token.
 //
