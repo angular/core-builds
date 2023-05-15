323ec60fb957432dfc75844dd24bfb96b94fc7da
Andrew Kushnir <akushnir@google.com>
2023-05-15 16:40:31 +0000

    239e7e6de3 refactor(core): remove legacy way of preventing default actions (#50257)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-d0262cd
+ * @license Angular v16.1.0-next.0+sha-239e7e6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10039,7 +10039,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-d0262cd');
+const VERSION = new Version('16.1.0-next.0+sha-239e7e6');
 
 // This default value is when checking the hierarchy for a token.
 //
@@ -15694,8 +15694,6 @@ function wrapListener(tNode, lView, context, listenerFn, wrapWithPreventDefault)
         }
         if (wrapWithPreventDefault && result === false) {
             e.preventDefault();
-            // Necessary for legacy browsers that don't support preventDefault (e.g. IE)
-            e.returnValue = false;
         }
         return result;
     };
