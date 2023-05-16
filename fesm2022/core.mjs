ced56b399c165570472d4b1ee94f869b0d4daf91
Andrew Kushnir <akushnir@google.com>
2023-05-16 16:41:31 +0000

    cd86eb4606 refactor(common): Reduce the precision to 2 digits in the ngOptimizedImage distortion warning message (#50276)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-165b8b6
+ * @license Angular v16.1.0-next.0+sha-cd86eb4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10041,7 +10041,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.0+sha-165b8b6');
+const VERSION = new Version('16.1.0-next.0+sha-cd86eb4');
 
 // This default value is when checking the hierarchy for a token.
 //
