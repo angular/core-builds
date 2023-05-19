6cee2e798441ccff7dad7ea5c1e8b484dcf9b015
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 14:36:19 +0000

    3239160ec9 build: update babel dependencies (#48273)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-00b4924
+ * @license Angular v16.1.0-next.1+sha-3239160
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10437,7 +10437,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-00b4924');
+const VERSION = new Version('16.1.0-next.1+sha-3239160');
 
 // This default value is when checking the hierarchy for a token.
 //
