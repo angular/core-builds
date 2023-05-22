03676d485a7373cafcdad4430d28c2f54e308778
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 14:59:51 +0000

    97f90e0e0c docs: fix typo in housing.service.ts first app lesson 13 (#50387) PR Close #50387

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-6a51704
+ * @license Angular v16.1.0-next.1+sha-97f90e0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-6a51704');
+const VERSION = new Version('16.1.0-next.1+sha-97f90e0');
 
 // This default value is when checking the hierarchy for a token.
 //
