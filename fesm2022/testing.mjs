6f7ded7944d22d5b36973469bd2f72ac8af947e0
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 17:51:40 +0000

    203b55a8cd docs: replaced the html tags with the updated ones (#50403)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-6b02242
+ * @license Angular v16.1.0-next.1+sha-203b55a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10464,7 +10464,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-6b02242');
+const VERSION = new Version('16.1.0-next.1+sha-203b55a');
 
 // This default value is when checking the hierarchy for a token.
 //
