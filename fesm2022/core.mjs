84d10ae966da6be65859344244c0d165373369bd
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 17:50:46 +0000

    6b022428ad docs: improve AOT docs. (#50398)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-f6da091
+ * @license Angular v16.1.0-next.1+sha-6b02242
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-f6da091');
+const VERSION = new Version('16.1.0-next.1+sha-6b02242');
 
 // This default value is when checking the hierarchy for a token.
 //
