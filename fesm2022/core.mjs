933b73266df11976c9ccd3fa88a237c72e307490
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:34:17 +0000

    8933ca3275 refactor(compiler): remove convertIndexImportShorthand (#50343)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-ea560a7
+ * @license Angular v16.1.0-next.1+sha-8933ca3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-ea560a7');
+const VERSION = new Version('16.1.0-next.1+sha-8933ca3');
 
 // This default value is when checking the hierarchy for a token.
 //
