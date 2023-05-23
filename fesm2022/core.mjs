08f20e6a853e815733a7697bf8423435ea472e4a
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 17:46:51 +0000

    4dc528d402 build: remove unused bash utilities from legacy build (#50427)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-f7b8fb3
+ * @license Angular v16.1.0-next.1+sha-4dc528d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-f7b8fb3');
+const VERSION = new Version('16.1.0-next.1+sha-4dc528d');
 
 // This default value is when checking the hierarchy for a token.
 //
