011b7b3d8a220a5df3f6ad1f165f3b900adebbb5
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:27:53 +0000

    3152c4de92 fix(docs-infra): remove extra slash from JSON-LD data (#50140)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-3c093d7
+ * @license Angular v16.1.0-next.1+sha-3152c4d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10464,7 +10464,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-3c093d7');
+const VERSION = new Version('16.1.0-next.1+sha-3152c4d');
 
 // This default value is when checking the hierarchy for a token.
 //
