906540e373d0af26932593b37794863633698689
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 18:38:36 +0000

    6ced5c8779 Revert "build: update babel dependencies to v7.21.9 (#50416)" (#50433)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-4dc528d
+ * @license Angular v16.1.0-next.1+sha-6ced5c8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10464,7 +10464,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-4dc528d');
+const VERSION = new Version('16.1.0-next.1+sha-6ced5c8');
 
 // This default value is when checking the hierarchy for a token.
 //
