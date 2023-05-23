ccfcee1f8cd9a97abdaf09ddf55c8c73ed662a03
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 17:33:39 +0000

    f7b8fb3c97 build: remove unused gulp helper script (#50428)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-f4393f7
+ * @license Angular v16.1.0-next.1+sha-f7b8fb3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10464,7 +10464,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-f4393f7');
+const VERSION = new Version('16.1.0-next.1+sha-f7b8fb3');
 
 // This default value is when checking the hierarchy for a token.
 //
