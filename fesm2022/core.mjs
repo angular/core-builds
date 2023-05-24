2cd91186415a8f538f0e734ceb9bc2298311d53d
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-05-24 17:56:16 +0000

    cbe27990c2 docs: release notes for the v16.0.3 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-2cdb4c5
+ * @license Angular v16.1.0-next.1+sha-cbe2799
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10071,7 +10071,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-2cdb4c5');
+const VERSION = new Version('16.1.0-next.1+sha-cbe2799');
 
 // This default value is when checking the hierarchy for a token.
 //
