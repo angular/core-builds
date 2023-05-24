f5bf30e6cb35e01846e02a80be9c6724fa2fa6ff
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-24 14:18:39 +0000

    98e8fdf40e fix(core): fix `Self` flag inside embedded views with custom injectors (#50270)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.2+sha-ac0150f
+ * @license Angular v16.0.2+sha-98e8fdf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4446,7 +4446,10 @@ function getOrCreateInjectable(tNode, lView, token, flags = InjectFlags.Default,
     if (tNode !== null) {
         // If the view or any of its ancestors have an embedded
         // view injector, we have to look it up there first.
-        if (lView[FLAGS] & 2048 /* LViewFlags.HasEmbeddedViewInjector */) {
+        if (lView[FLAGS] & 2048 /* LViewFlags.HasEmbeddedViewInjector */ &&
+            // The token must be present on the current node injector when the `Self`
+            // flag is set, so the lookup on embedded view injector(s) can be skipped.
+            !(flags & InjectFlags.Self)) {
             const embeddedInjectorValue = lookupTokenUsingEmbeddedInjector(tNode, lView, token, flags, NOT_FOUND);
             if (embeddedInjectorValue !== NOT_FOUND) {
                 return embeddedInjectorValue;
@@ -10065,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.2+sha-ac0150f');
+const VERSION = new Version('16.0.2+sha-98e8fdf');
 
 // This default value is when checking the hierarchy for a token.
 //
