f7594dca8847aa935b59ca9e99929154f108d058
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-24 14:06:57 +0000

    24f7c1dc61 refactor(devtools): run spellcheck on devtools dir (#50445)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-4c01107
+ * @license Angular v16.1.0-next.1+sha-24f7c1d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.1+sha-4c01107');
+const VERSION = new Version('16.1.0-next.1+sha-24f7c1d');
 
 // This default value is when checking the hierarchy for a token.
 //
