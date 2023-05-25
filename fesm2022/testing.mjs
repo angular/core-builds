2cbe4b1326303e909432e1bbf1b957878a50b62a
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:54:30 +0000

    258c773188 docs: remove unused imports and improved the code (#50424)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-06b498f
+ * @license Angular v16.1.0-next.2+sha-258c773
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10467,7 +10467,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-06b498f');
+const VERSION = new Version('16.1.0-next.2+sha-258c773');
 
 // This default value is when checking the hierarchy for a token.
 //
