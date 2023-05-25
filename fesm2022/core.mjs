1bda376213adc8b50787c8601c03f233e43ac31b
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:56:53 +0000

    04553b9016 build: update dependency https-proxy-agent to v7 (#50456)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-258c773
+ * @license Angular v16.1.0-next.2+sha-04553b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10071,7 +10071,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-258c773');
+const VERSION = new Version('16.1.0-next.2+sha-04553b9');
 
 // This default value is when checking the hierarchy for a token.
 //
