0e25ebc36b5096e2707b86279dab22c9f90e181c
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:56:58 +0000

    721bc72649 fix(compiler): resolve deprecation warning with TypeScript 5.1 (#50460)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-04553b9
+ * @license Angular v16.1.0-next.2+sha-721bc72
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10071,7 +10071,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-04553b9');
+const VERSION = new Version('16.1.0-next.2+sha-721bc72');
 
 // This default value is when checking the hierarchy for a token.
 //
