f56a11ca47a6ac318d413c87bcb0607ca8fd0141
Dylan Hunn <dylhunn@gmail.com>
2023-05-25 20:41:53 +0000

    1c37a83e1b Revert "fix(http): create macrotask during request handling instead of load start (#50406)" (#50475)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.3+sha-df65c4f
+ * @license Angular v16.0.3+sha-1c37a83
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10464,7 +10464,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.3+sha-df65c4f');
+const VERSION = new Version('16.0.3+sha-1c37a83');
 
 // This default value is when checking the hierarchy for a token.
 //
