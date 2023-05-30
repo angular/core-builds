7dd583069901510fb73189cbf537013356fe89d3
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 20:14:08 +0000

    1ca6362c06 docs(docs-infra): rimraf requires the globflag. (#50493)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-0c80349
+ * @license Angular v16.1.0-next.2+sha-1ca6362
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10467,7 +10467,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-0c80349');
+const VERSION = new Version('16.1.0-next.2+sha-1ca6362');
 
 // This default value is when checking the hierarchy for a token.
 //
