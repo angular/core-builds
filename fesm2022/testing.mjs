994ff9a669b74c2f1de5d49faeed6ec3b62a19f2
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 20:11:33 +0000

    381cb98226 fix(zone.js): enable monkey patching of the `queueMicrotask()` API in node.js (#50467)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-019eaf6
+ * @license Angular v16.1.0-next.2+sha-381cb98
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10467,7 +10467,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-019eaf6');
+const VERSION = new Version('16.1.0-next.2+sha-381cb98');
 
 // This default value is when checking the hierarchy for a token.
 //
