286968e1945f18e719d1cc87e6088acff146b088
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 20:14:26 +0000

    25b6b9788d docs: improved code quality (#50443)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-1ca6362
+ * @license Angular v16.1.0-next.2+sha-25b6b97
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10467,7 +10467,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-1ca6362');
+const VERSION = new Version('16.1.0-next.2+sha-25b6b97');
 
 // This default value is when checking the hierarchy for a token.
 //
