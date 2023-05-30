7f513468fc00bdc4c6332c25589ef210779ff039
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 16:37:43 +0000

    f517d18dd1 docs: removed the unused import and update the obsolete tag of width in table tag (#50474)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.3+sha-1c37a83
+ * @license Angular v16.0.3+sha-f517d18
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.3+sha-1c37a83');
+const VERSION = new Version('16.0.3+sha-f517d18');
 
 // This default value is when checking the hierarchy for a token.
 //
