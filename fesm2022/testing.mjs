f24a8e5600160be0887d26ea388729d0b45d9377
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-31 14:53:55 +0000

    277a473e53 build: Fix size tracking test (#50531)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.3+sha-f26c9a2
+ * @license Angular v16.0.3+sha-277a473
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10464,7 +10464,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.3+sha-f26c9a2');
+const VERSION = new Version('16.0.3+sha-277a473');
 
 // This default value is when checking the hierarchy for a token.
 //
