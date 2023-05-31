88c8330af7a59335e23217bbe5a5ed2d7e10fd1f
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 19:54:19 +0000

    ac447ede22 refactor(common): fix variable name (#50521)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-915abc6
+ * @license Angular v16.1.0-next.2+sha-ac447ed
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-915abc6');
+const VERSION = new Version('16.1.0-next.2+sha-ac447ed');
 
 // This default value is when checking the hierarchy for a token.
 //
