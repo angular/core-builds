4b374566c2b6aba7bd8e0656451fe9fdc0f452b1
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 19:48:32 +0000

    7ca301acd5 build: update dependency eslint-plugin-jsdoc to v45 (#50523)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.2+sha-7567348
+ * @license Angular v16.1.0-next.2+sha-7ca301a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.2+sha-7567348');
+const VERSION = new Version('16.1.0-next.2+sha-7ca301a');
 
 // This default value is when checking the hierarchy for a token.
 //
