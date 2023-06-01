9716a41069560d7cad7be8dbfa0a94b68ff4e3a6
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 16:04:12 +0000

    540e643347 fix(compiler): do not remove comments in component styles (#50346)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.3+sha-9847085
+ * @license Angular v16.1.0-next.3+sha-540e643
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10469,7 +10469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.3+sha-9847085');
+const VERSION = new Version('16.1.0-next.3+sha-540e643');
 
 // This default value is when checking the hierarchy for a token.
 //
