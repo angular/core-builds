8182e11464d1ab83a0dcf8d416e4e36b60863787
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 16:02:53 +0000

    9847085448 refactor(compiler): support property interpolation in the template pipeline (#50118)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.3+sha-57d47b3
+ * @license Angular v16.1.0-next.3+sha-9847085
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10469,7 +10469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.3+sha-57d47b3');
+const VERSION = new Version('16.1.0-next.3+sha-9847085');
 
 // This default value is when checking the hierarchy for a token.
 //
