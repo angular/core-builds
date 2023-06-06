3144463e05865866c9c46dc726a8d09c4f955e6c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-06 12:34:34 +0000

    7106b2f2ce fix(docs-infra): replace use of turbo on StackBlitz with npm (#50576)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.4+sha-9d7991a
+ * @license Angular v16.0.4+sha-7106b2f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.4+sha-9d7991a');
+const VERSION = new Version('16.0.4+sha-7106b2f');
 
 // This default value is when checking the hierarchy for a token.
 //
