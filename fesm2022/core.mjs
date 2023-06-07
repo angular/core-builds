1e7a5687922663e6eb1f7b2fc95bb767fc2d7ca5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-07 20:04:20 +0000

    4e663297c5 fix(compiler): error when reading compiled input transforms metadata in JIT mode (#50600)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.3+sha-3044822
+ * @license Angular v16.1.0-next.3+sha-4e66329
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.3+sha-3044822');
+const VERSION = new Version('16.1.0-next.3+sha-4e66329');
 
 // This default value is when checking the hierarchy for a token.
 //
