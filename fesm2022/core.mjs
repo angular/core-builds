5ebd26cc2da8f9f369733c881fd908d8b8eefce8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-08 21:03:56 +0000

    e1a715da73 docs: remove reverted commit from the changelog

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-024d6a8
+ * @license Angular v16.2.0-next.0+sha-e1a715d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-024d6a8');
+const VERSION = new Version('16.2.0-next.0+sha-e1a715d');
 
 // This default value is when checking the hierarchy for a token.
 //
