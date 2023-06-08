083b37b4a75af928d09a3adc206d29eececdabc9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-08 20:59:11 +0000

    024d6a81fd docs: release notes for the v16.1.0-rc.0 release

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.3+sha-55d6147
+ * @license Angular v16.2.0-next.0+sha-024d6a8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10469,7 +10469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-next.3+sha-55d6147');
+const VERSION = new Version('16.2.0-next.0+sha-024d6a8');
 
 // This default value is when checking the hierarchy for a token.
 //
