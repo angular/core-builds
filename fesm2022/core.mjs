7e3e81214a3b6ce9873c27607263ebc08400bc2b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-09 15:56:50 +0000

    a163d35c31 docs: removed unused methods (#50631)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-a1973a4
+ * @license Angular v16.2.0-next.0+sha-a163d35
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-a1973a4');
+const VERSION = new Version('16.2.0-next.0+sha-a163d35');
 
 // This default value is when checking the hierarchy for a token.
 //
