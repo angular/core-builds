8c12332265cf4542735892ebfd42f2db47f7a508
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-09 15:54:13 +0000

    a1973a4216 docs: remove repeated word (#50614)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-b7c710e
+ * @license Angular v16.2.0-next.0+sha-a1973a4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10469,7 +10469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-b7c710e');
+const VERSION = new Version('16.2.0-next.0+sha-a1973a4');
 
 // This default value is when checking the hierarchy for a token.
 //
