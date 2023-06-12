ffa12f39b43f28768d82bc94adfb30777f00ea20
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 16:35:52 +0000

    9429f96f62 test(compiler): Prevent back-sliding on already passing template pipeline tests (#50582)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-d773589
+ * @license Angular v16.2.0-next.0+sha-9429f96
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-d773589');
+const VERSION = new Version('16.2.0-next.0+sha-9429f96');
 
 // This default value is when checking the hierarchy for a token.
 //
