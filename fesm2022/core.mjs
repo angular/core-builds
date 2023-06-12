0384b2aa6171b6c4df7db4aa8679663346a1fc1d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 12:41:29 +0000

    9a172a9345 docs: update first-app-lesson-11.md to include info about routerLink (#50348)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-dbadfea
+ * @license Angular v16.2.0-next.0+sha-9a172a9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-dbadfea');
+const VERSION = new Version('16.2.0-next.0+sha-9a172a9');
 
 // This default value is when checking the hierarchy for a token.
 //
