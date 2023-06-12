eee234c0c2bf4a17a41f4e07310102181bce926c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 10:54:20 +0000

    89a089bdeb docs: add missing navigation item (#50113)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.5+sha-8e9f920
+ * @license Angular v16.0.5+sha-89a089b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.5+sha-8e9f920');
+const VERSION = new Version('16.0.5+sha-89a089b');
 
 // This default value is when checking the hierarchy for a token.
 //
