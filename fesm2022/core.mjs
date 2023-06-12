03d0d5f292cbce10315b73efba67b05ce2b089af
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 11:01:26 +0000

    dbadfea67f feat(devtools): create demo application that uses standalone APIs and standalone components/directives/pipes (#48533)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-b1aed48
+ * @license Angular v16.2.0-next.0+sha-dbadfea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-b1aed48');
+const VERSION = new Version('16.2.0-next.0+sha-dbadfea');
 
 // This default value is when checking the hierarchy for a token.
 //
