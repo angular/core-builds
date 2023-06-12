88db4afa5c136945a72faf5e1711e9ddfb9b1259
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 09:55:36 +0000

    33db5f934c docs: removed duplicated words (#50648)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.5+sha-080d62a
+ * @license Angular v16.0.5+sha-33db5f9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.5+sha-080d62a');
+const VERSION = new Version('16.0.5+sha-33db5f9');
 
 // This default value is when checking the hierarchy for a token.
 //
