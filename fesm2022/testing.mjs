a89f88e059480b5ae94e003fea98dc68852f6b57
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 16:01:10 +0000

    d773589ed2 build: bump up zone.js version for the release (#50680)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-8eecc0f
+ * @license Angular v16.2.0-next.0+sha-d773589
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10469,7 +10469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-8eecc0f');
+const VERSION = new Version('16.2.0-next.0+sha-d773589');
 
 // This default value is when checking the hierarchy for a token.
 //
