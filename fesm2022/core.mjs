5dba72c0ba3d685ce57d0e7fc46bf6eece0186be
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 09:39:51 +0000

    080d62a6b6 docs: added title attribute to the iframe tag (#50569)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.5+sha-89c6592
+ * @license Angular v16.0.5+sha-080d62a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10068,7 +10068,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.5+sha-89c6592');
+const VERSION = new Version('16.0.5+sha-080d62a');
 
 // This default value is when checking the hierarchy for a token.
 //
