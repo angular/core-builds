8d95c5196d795171392b921132d1139d24c4e878
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 10:24:03 +0000

    8e9f920ed5 docs: added lang attributes in html files (#50588)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.5+sha-33db5f9
+ * @license Angular v16.0.5+sha-8e9f920
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10464,7 +10464,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.5+sha-33db5f9');
+const VERSION = new Version('16.0.5+sha-8e9f920');
 
 // This default value is when checking the hierarchy for a token.
 //
