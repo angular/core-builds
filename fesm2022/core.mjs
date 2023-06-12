61760a2cc6cf63e9899dcc026e00db6a0cba493d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 17:32:51 +0000

    57f9efe200 docs: update schematic authoring guide to use new utilities (#50667)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-rc.0+sha-ecbdb24
+ * @license Angular v16.1.0-rc.0+sha-57f9efe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-rc.0+sha-ecbdb24');
+const VERSION = new Version('16.1.0-rc.0+sha-57f9efe');
 
 // This default value is when checking the hierarchy for a token.
 //
