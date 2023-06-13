b13acede8ca9882324efcfe997152c50fe6ed4a4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 10:02:39 +0000

    7abb71609b build: lock file maintenance (#50532)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-rc.0+sha-57f9efe
+ * @license Angular v16.1.0-rc.0+sha-7abb716
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10073,7 +10073,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-rc.0+sha-57f9efe');
+const VERSION = new Version('16.1.0-rc.0+sha-7abb716');
 
 // This default value is when checking the hierarchy for a token.
 //
