0c68a75143cc35199e3c96b921bea059c1b4e5bb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 11:02:39 +0000

    f13ecd5fad build: update all non-major dependencies (#50583)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-rc.0+sha-7abb716
+ * @license Angular v16.1.0-rc.0+sha-f13ecd5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10469,7 +10469,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-rc.0+sha-7abb716');
+const VERSION = new Version('16.1.0-rc.0+sha-f13ecd5');
 
 // This default value is when checking the hierarchy for a token.
 //
