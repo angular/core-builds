b4f2d0d101b91a371491cd6e1c29371231024071
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 15:24:10 +0000

    01d45df6d9 docs: release notes for the v16.1.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-2782fc2
+ * @license Angular v16.2.0-next.0+sha-01d45df
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10082,7 +10082,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-2782fc2');
+const VERSION = new Version('16.2.0-next.0+sha-01d45df');
 
 // This default value is when checking the hierarchy for a token.
 //
