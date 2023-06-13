f4f57904f4cb9d546fc9eb61513ec2cb1855f54d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 15:23:45 +0000

    f48c0bc4e3 release: cut the v16.1.0 release

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-rc.0+sha-fa3b733
+ * @license Angular v16.1.0+sha-f48c0bc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10478,7 +10478,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.0-rc.0+sha-fa3b733');
+const VERSION = new Version('16.1.0+sha-f48c0bc');
 
 // This default value is when checking the hierarchy for a token.
 //
