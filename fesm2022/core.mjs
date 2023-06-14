89073faedb65e239820fbd84a8e4e8e8fcc409e0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 13:32:04 +0000

    6883a7ebf9 docs: update Angular CLI help [main] (#50711)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-8d42747
+ * @license Angular v16.2.0-next.0+sha-6883a7e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-8d42747');
+const VERSION = new Version('16.2.0-next.0+sha-6883a7e');
 
 // This default value is when checking the hierarchy for a token.
 //
