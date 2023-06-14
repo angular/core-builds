daba936b1139002b0336f4c06950744a0ccac902
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 09:39:01 +0000

    c31fe761cf docs: remove `allowEmptyCodegenFiles`. (#50379)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-946df15
+ * @license Angular v16.2.0-next.0+sha-c31fe76
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10082,7 +10082,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-946df15');
+const VERSION = new Version('16.2.0-next.0+sha-c31fe76');
 
 // This default value is when checking the hierarchy for a token.
 //
