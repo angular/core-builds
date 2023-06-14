501a1b4209c73465a38df30608660af248156b6e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 09:16:10 +0000

    946df15bc8 refactor(platform-browser): remove unused polyfill (#50661)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-f199305
+ * @license Angular v16.2.0-next.0+sha-946df15
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10082,7 +10082,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-f199305');
+const VERSION = new Version('16.2.0-next.0+sha-946df15');
 
 // This default value is when checking the hierarchy for a token.
 //
