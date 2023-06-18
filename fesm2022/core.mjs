c89deaefb79d39e570ced2b787a2949f8e85258a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-18 19:43:58 +0000

    d5736d7d50 build: create github action to run benchmark comparisons (#50745)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-9588b11
+ * @license Angular v16.2.0-next.0+sha-d5736d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-9588b11');
+const VERSION = new Version('16.2.0-next.0+sha-d5736d7');
 
 // This default value is when checking the hierarchy for a token.
 //
