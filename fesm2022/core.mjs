0f92b20df5071e719824bc1b1ba7da03079452eb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-19 12:20:43 +0000

    864bd72cb2 build: resolve benchmark compare ref to improve results text

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-ac13b65
+ * @license Angular v16.2.0-next.0+sha-864bd72
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-ac13b65');
+const VERSION = new Version('16.2.0-next.0+sha-864bd72');
 
 // This default value is when checking the hierarchy for a token.
 //
