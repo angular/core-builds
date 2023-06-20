240d4085a94545a0f8f2dfc03ff509f27423e32e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 09:18:56 +0000

    19913ccbb7 feat(docs-infra): add option to filter docs with developer preview status (#50142)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-076d449
+ * @license Angular v16.2.0-next.0+sha-19913cc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-076d449');
+const VERSION = new Version('16.2.0-next.0+sha-19913cc');
 
 // This default value is when checking the hierarchy for a token.
 //
