86fc89eb75622da6dad296c3b0c2c9f24afbcd02
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 09:18:32 +0000

    076d4499af refactor(common): do not run CD when verifying image distortion (#50723)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-43a80ca
+ * @license Angular v16.2.0-next.0+sha-076d449
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-43a80ca');
+const VERSION = new Version('16.2.0-next.0+sha-076d449');
 
 // This default value is when checking the hierarchy for a token.
 //
