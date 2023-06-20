2082c6e233c86dbecb88af1f34ea3892f3fb219a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 13:07:51 +0000

    a88ae41504 build: remove AngularJS benchmarks (#50730)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-7f4a7b4
+ * @license Angular v16.2.0-next.0+sha-a88ae41
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-7f4a7b4');
+const VERSION = new Version('16.2.0-next.0+sha-a88ae41');
 
 // This default value is when checking the hierarchy for a token.
 //
