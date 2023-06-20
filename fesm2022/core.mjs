765138d34ae8c32651ca4c982807c7295e59a4b5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 09:08:29 +0000

    43a80ca8f8 build: update eslint dependencies to v5.60.0 (#50690)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-254023a
+ * @license Angular v16.2.0-next.0+sha-43a80ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-254023a');
+const VERSION = new Version('16.2.0-next.0+sha-43a80ca');
 
 // This default value is when checking the hierarchy for a token.
 //
