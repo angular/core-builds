f366f25b5709cd042f782705a8257cfe777d5290
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 16:16:20 +0000

    4af49106aa build: speed up remote execution by allowing more concurrent jobs (#50734)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-f77bd0a
+ * @license Angular v16.2.0-next.0+sha-4af4910
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-f77bd0a');
+const VERSION = new Version('16.2.0-next.0+sha-4af4910');
 
 // This default value is when checking the hierarchy for a token.
 //
