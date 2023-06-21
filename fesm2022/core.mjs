7f8f81647c4942910c928175288a0887416a0623
Andrew Kushnir <akushnir@google.com>
2023-06-21 19:00:37 +0000

    bb617f580c build: update babel dependencies (#50509)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-e7b218c
+ * @license Angular v16.2.0-next.0+sha-bb617f5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-e7b218c');
+const VERSION = new Version('16.2.0-next.0+sha-bb617f5');
 
 // This default value is when checking the hierarchy for a token.
 //
