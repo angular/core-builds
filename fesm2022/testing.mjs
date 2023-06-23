1545e9968a73e998ad9772ac3ff6a7b1c46de9f8
Andrew Kushnir <akushnir@google.com>
2023-06-23 16:51:53 +0000

    f9891c0a47 ci: re-enable RBE for http tests (#50741)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.2+sha-ea38aee
+ * @license Angular v16.1.2+sha-f9891c0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.2+sha-ea38aee');
+const VERSION = new Version('16.1.2+sha-f9891c0');
 
 // This default value is when checking the hierarchy for a token.
 //
