bea016a2e88df94b5458491847c418e7ebbb4b4f
Andrew Kushnir <akushnir@google.com>
2023-06-23 00:23:53 +0000

    9f2fb7c6dc docs: update invalid links to the new http guides (#49456)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-d64864e
+ * @license Angular v16.2.0-next.0+sha-9f2fb7c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-d64864e');
+const VERSION = new Version('16.2.0-next.0+sha-9f2fb7c');
 
 // This default value is when checking the hierarchy for a token.
 //
