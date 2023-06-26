fa19136f342d33bf65f153f24f05d4f8268a2fa3
Andrew Kushnir <akushnir@google.com>
2023-06-26 20:50:29 +0000

    b09119b14b docs: Updating index.md to remove alert message which no longer applicable after version 15 (#50832)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-d6b1fed
+ * @license Angular v16.2.0-next.0+sha-b09119b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-d6b1fed');
+const VERSION = new Version('16.2.0-next.0+sha-b09119b');
 
 // This default value is when checking the hierarchy for a token.
 //
