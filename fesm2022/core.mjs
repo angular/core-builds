7d15e11d665e10c36a996728a774846c6df216ee
Andrew Kushnir <akushnir@google.com>
2023-06-26 19:28:22 +0000

    9b7bc801f6 Revert "fix(platform-browser): wait until animation completion before destroying renderer (#50677)" (#50857)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.2+sha-21ced23
+ * @license Angular v16.1.2+sha-9b7bc80
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.2+sha-21ced23');
+const VERSION = new Version('16.1.2+sha-9b7bc80');
 
 // This default value is when checking the hierarchy for a token.
 //
