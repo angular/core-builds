d6bfed5485f7acebf2e27d4e98ee8f3ffa0a4a22
Andrew Kushnir <akushnir@google.com>
2023-06-26 20:52:35 +0000

    ec1f6e3a5e build: do not attempt deflaking locally (#50852)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.2+sha-a844249
+ * @license Angular v16.1.2+sha-ec1f6e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.2+sha-a844249');
+const VERSION = new Version('16.1.2+sha-ec1f6e3');
 
 // This default value is when checking the hierarchy for a token.
 //
