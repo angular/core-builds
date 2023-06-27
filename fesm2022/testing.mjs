459f46d28cb1b452893d1729db63c168c256e872
Andrew Kushnir <akushnir@google.com>
2023-06-27 13:41:47 +0000

    52f1dd9aaa refactor(animations): remove redundant castings (#50860)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.2+sha-ec1f6e3
+ * @license Angular v16.1.2+sha-52f1dd9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.2+sha-ec1f6e3');
+const VERSION = new Version('16.1.2+sha-52f1dd9');
 
 // This default value is when checking the hierarchy for a token.
 //
