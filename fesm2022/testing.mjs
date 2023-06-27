d2b8004afca510d27a7305b15dd10b58f938c092
Andrew Kushnir <akushnir@google.com>
2023-06-27 23:07:57 +0000

    7aa913e512 build: update actions/checkout digest to c85c95e (#50640)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-0522f1b
+ * @license Angular v16.2.0-next.0+sha-7aa913e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.0+sha-0522f1b');
+const VERSION = new Version('16.2.0-next.0+sha-7aa913e');
 
 // This default value is when checking the hierarchy for a token.
 //
