ec06b8a8a6e54b90d50e84b18207c46e95e023db
Andrew Kushnir <akushnir@google.com>
2023-06-27 23:23:41 +0000

    520a1b5fe1 build: update eslint dependencies to v5.60.1 (#50859)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.2+sha-52f1dd9
+ * @license Angular v16.1.2+sha-520a1b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.2+sha-52f1dd9');
+const VERSION = new Version('16.1.2+sha-520a1b5');
 
 // This default value is when checking the hierarchy for a token.
 //
