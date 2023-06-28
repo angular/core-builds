328c1d007c37830584a771d8c4db416dcc73047c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-28 20:44:00 +0000

    a5bd5ddbaa build: lock file maintenance (#50877)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-fc9da5e
+ * @license Angular v16.2.0-next.1+sha-a5bd5dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-fc9da5e');
+const VERSION = new Version('16.2.0-next.1+sha-a5bd5dd');
 
 // This default value is when checking the hierarchy for a token.
 //
