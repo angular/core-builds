328b1e4f59b7d711254147e2a73463a47525bb3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 18:55:19 +0000

    471cdff082 build: update scorecard action dependencies (#50880)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-a19a87d
+ * @license Angular v16.2.0-next.1+sha-471cdff
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-a19a87d');
+const VERSION = new Version('16.2.0-next.1+sha-471cdff');
 
 // This default value is when checking the hierarchy for a token.
 //
