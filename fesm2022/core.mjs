31794a61b1670cbe504196bb536e8f3455272f4d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 19:01:50 +0000

    0bfd3f537c docs: Use id instead of name for anchor targets (#50885)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-4782336
+ * @license Angular v16.2.0-next.1+sha-0bfd3f5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10089,7 +10089,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-4782336');
+const VERSION = new Version('16.2.0-next.1+sha-0bfd3f5');
 
 // This default value is when checking the hierarchy for a token.
 //
