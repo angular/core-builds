cfc17f68789195c2476403458862fe91934ae7db
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-07-05 12:09:55 +0000

    f9175dd3b3 docs: updated the b tag with the strong tag (#50921)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-6538e67
+ * @license Angular v16.2.0-next.1+sha-f9175dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-6538e67');
+const VERSION = new Version('16.2.0-next.1+sha-f9175dd');
 
 // This default value is when checking the hierarchy for a token.
 //
