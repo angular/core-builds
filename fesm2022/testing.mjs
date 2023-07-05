9022b5da58da0097acca1fee761ee272f6be602c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-07-05 12:13:46 +0000

    7aa6e580c9 docs: update link to the TS config. (#50933)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.3+sha-b9c28ca
+ * @license Angular v16.1.3+sha-7aa6e58
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10482,7 +10482,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.3+sha-b9c28ca');
+const VERSION = new Version('16.1.3+sha-7aa6e58');
 
 // This default value is when checking the hierarchy for a token.
 //
