11fbf948734bcb1ec3212673a0d36bbf6c8c2e81
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 17:27:22 +0000

    31419f6a3b perf(platform-browser): do not remove renderer from cache when `REMOVE_STYLES_ON_COMPONENT_DESTROY` is enabled. (#51005)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.4+sha-de01f75
+ * @license Angular v16.1.4+sha-31419f6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10493,7 +10493,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.4+sha-de01f75');
+const VERSION = new Version('16.1.4+sha-31419f6');
 
 // This default value is when checking the hierarchy for a token.
 //
