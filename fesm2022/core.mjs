04b91a53e7946bb066365ec12a5649eb1979b063
Joey Perrott <josephperrott@gmail.com>
2023-07-11 17:17:17 +0000

    6c76a83e31 ci: migrate lint job from CircleCI to GHA (#50891)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-c27a1e6
+ * @license Angular v16.2.0-next.1+sha-6c76a83
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10100,7 +10100,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-c27a1e6');
+const VERSION = new Version('16.2.0-next.1+sha-6c76a83');
 
 // This default value is when checking the hierarchy for a token.
 //
