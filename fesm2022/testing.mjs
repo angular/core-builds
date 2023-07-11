bb2ced77c39cca43052571752fcc00f2f659ea4a
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:41:52 +0000

    19fb3e1701 docs: fix styling in tutorial lesson 5 (#51001)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-ef45fa8
+ * @license Angular v16.2.0-next.1+sha-19fb3e1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10493,7 +10493,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-ef45fa8');
+const VERSION = new Version('16.2.0-next.1+sha-19fb3e1');
 
 // This default value is when checking the hierarchy for a token.
 //
