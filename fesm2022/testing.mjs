79fb4d25ddf6ddd651667f1216bff07cdc0a5e46
Dylan Hunn <dylhunn@gmail.com>
2023-07-13 16:59:06 +0000

    b8107b1914 refactor(common): remove `supportScrollRestoration` (#50056)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-8f1bbd7
+ * @license Angular v16.2.0-next.1+sha-b8107b1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10493,7 +10493,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-8f1bbd7');
+const VERSION = new Version('16.2.0-next.1+sha-b8107b1');
 
 // This default value is when checking the hierarchy for a token.
 //
