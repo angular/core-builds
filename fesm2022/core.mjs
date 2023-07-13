eeb88f2515ff54ef61d458d3028298431a181f25
Dylan Hunn <dylhunn@gmail.com>
2023-07-13 16:52:31 +0000

    36c5a57a3c docs(docs-infra): typo fix in aot-metadata-errors (#51017)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.1+sha-3b78d06
+ * @license Angular v16.2.0-next.1+sha-36c5a57
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10100,7 +10100,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.1+sha-3b78d06');
+const VERSION = new Version('16.2.0-next.1+sha-36c5a57');
 
 // This default value is when checking the hierarchy for a token.
 //
