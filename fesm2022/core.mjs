4e14434c46707fdfbb516b27e8383fdf28d87ed8
Dylan Hunn <dylhunn@gmail.com>
2023-07-14 17:03:37 +0000

    451d6c0c44 docs(docs-infra): http-server-communication typo fix (#51024)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.2+sha-8d636ad
+ * @license Angular v16.2.0-next.2+sha-451d6c0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10100,7 +10100,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.2+sha-8d636ad');
+const VERSION = new Version('16.2.0-next.2+sha-451d6c0');
 
 // This default value is when checking the hierarchy for a token.
 //
