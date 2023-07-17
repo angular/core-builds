e022a4847d948de4091046b7981fd92e2b02c448
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:51:39 +0000

    7f71d89c9a refactor: add dylhunn to pullapprove (#51069)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.2+sha-76d22ae
+ * @license Angular v16.2.0-next.2+sha-7f71d89
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.2+sha-76d22ae');
+const VERSION = new Version('16.2.0-next.2+sha-7f71d89');
 
 // This default value is when checking the hierarchy for a token.
 //
