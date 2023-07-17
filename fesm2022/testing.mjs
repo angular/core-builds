3c6c355f47cc74eb4555a112ac4832ac26b42b85
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 15:03:10 +0000

    57ea8871a8 ci: mark devversion as unavailable for reviews (#51058)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.2+sha-743be79
+ * @license Angular v16.2.0-next.2+sha-57ea887
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10485,7 +10485,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.2+sha-743be79');
+const VERSION = new Version('16.2.0-next.2+sha-57ea887');
 
 // This default value is when checking the hierarchy for a token.
 //
