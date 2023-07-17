ce57fa91a2a67c56e87c0ea2ee43b073d17eec8d
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:18:05 +0000

    3da03cacae docs: add mention of paramsInheritanceStrategy in the router doc (#51051)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.5+sha-dea8dc0
+ * @license Angular v16.1.5+sha-3da03ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10493,7 +10493,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.5+sha-dea8dc0');
+const VERSION = new Version('16.1.5+sha-3da03ca');
 
 // This default value is when checking the hierarchy for a token.
 //
