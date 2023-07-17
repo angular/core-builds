4e298f86d652050714f27cbece38d32c546c723d
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 15:12:38 +0000

    dea8dc0378 fix(http): Run fetch request out the angular zone (#50981)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.5+sha-78663e6
+ * @license Angular v16.1.5+sha-dea8dc0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10493,7 +10493,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.5+sha-78663e6');
+const VERSION = new Version('16.1.5+sha-dea8dc0');
 
 // This default value is when checking the hierarchy for a token.
 //
