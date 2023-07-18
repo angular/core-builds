5926ab03eb4227f5f24eb7d1714b932751090f9f
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 14:18:48 +0000

    d2b0677688 docs: fix typos and styling in lessons 7-14 (#51004)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.5+sha-ed1900e
+ * @license Angular v16.1.5+sha-d2b0677
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10493,7 +10493,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.5+sha-ed1900e');
+const VERSION = new Version('16.1.5+sha-d2b0677');
 
 // This default value is when checking the hierarchy for a token.
 //
