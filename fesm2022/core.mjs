c8f5e0d933ef98e3a3cc06ee70d5a91e3481efac
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 14:11:57 +0000

    b76088df9a build: update dependency lighthouse-logger to v2 (#51075)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.2+sha-7f71d89
+ * @license Angular v16.2.0-next.2+sha-b76088d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.2+sha-7f71d89');
+const VERSION = new Version('16.2.0-next.2+sha-b76088d');
 
 // This default value is when checking the hierarchy for a token.
 //
