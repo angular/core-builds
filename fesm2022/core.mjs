9e63b49fcbef8ec36a0f4337d0a98c1d6ba06640
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 19:22:38 +0000

    20c85f1ffb docs(common): Add NgOptimizedImage FAQ (#51036) Make minor updatees to NgOptimizedImage docs, and add an FAQ.

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.5+sha-d2b0677
+ * @license Angular v16.1.5+sha-20c85f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10100,7 +10100,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.5+sha-d2b0677');
+const VERSION = new Version('16.1.5+sha-20c85f1');
 
 // This default value is when checking the hierarchy for a token.
 //
