1257daa364bf2ed323661288cd59adb450aa8765
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 21:13:05 +0000

    5a5ecf78bd build: update all non-major dependencies (#51086)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.5+sha-20c85f1
+ * @license Angular v16.1.5+sha-5a5ecf7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10493,7 +10493,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.5+sha-20c85f1');
+const VERSION = new Version('16.1.5+sha-5a5ecf7');
 
 // This default value is when checking the hierarchy for a token.
 //
