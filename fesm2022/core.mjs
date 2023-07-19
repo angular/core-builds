43a4bbb438949cdf5cf04bceea78a15a495a651d
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-19 18:58:35 +0000

    386cb2f18b Revert "docs: added the alt attribute in the img tag (#51102)" (#51107)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.2+sha-385adbb
+ * @license Angular v16.2.0-next.2+sha-386cb2f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.2+sha-385adbb');
+const VERSION = new Version('16.2.0-next.2+sha-386cb2f');
 
 // This default value is when checking the hierarchy for a token.
 //
