dc553ea8adaf9244ac8919a242e4be3f7ccb31ba
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-20 20:35:49 +0000

    0413e5568b docs: remove the links to docs.angular.lat (#51117)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.3+sha-33acf4f
+ * @license Angular v16.2.0-next.3+sha-0413e55
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10485,7 +10485,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.3+sha-33acf4f');
+const VERSION = new Version('16.2.0-next.3+sha-0413e55');
 
 // This default value is when checking the hierarchy for a token.
 //
