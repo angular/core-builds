a66a89766f8a7be7fe86582f3e25955c36e46a6a
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-21 14:23:50 +0000

    528fea7da1 build: update cross-repo angular dependencies (#51127)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.3+sha-39899fe
+ * @license Angular v16.2.0-next.3+sha-528fea7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.3+sha-39899fe');
+const VERSION = new Version('16.2.0-next.3+sha-528fea7');
 
 // This default value is when checking the hierarchy for a token.
 //
