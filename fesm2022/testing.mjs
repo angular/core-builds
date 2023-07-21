a360a50f7b2fae90d345bea6928e28d9072d48fa
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-21 17:45:42 +0000

    7cfc36567f docs: fix typos in first app tutorial (#51115) PR Close #51115

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.6+sha-bfc6e48
+ * @license Angular v16.1.6+sha-7cfc365
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10493,7 +10493,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.6+sha-bfc6e48');
+const VERSION = new Version('16.1.6+sha-7cfc365');
 
 // This default value is when checking the hierarchy for a token.
 //
