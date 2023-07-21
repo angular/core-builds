c21112bcf9734755966419047e6488e3bb1261e1
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-21 17:03:19 +0000

    57e8412e53 fix(http): check whether `Zone` is defined (#51119)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.3+sha-189d601
+ * @license Angular v16.2.0-next.3+sha-57e8412
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10485,7 +10485,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.3+sha-189d601');
+const VERSION = new Version('16.2.0-next.3+sha-57e8412');
 
 // This default value is when checking the hierarchy for a token.
 //
