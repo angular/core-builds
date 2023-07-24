f55803ea0bb32ffdc376e6fd6328a7da2084bb51
Andrew Kushnir <akushnir@google.com>
2023-07-24 15:29:26 +0000

    92ebfd1ca7 refactor(compiler): handle braces in block parameters (#51143)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.3+sha-27d43a4
+ * @license Angular v16.2.0-next.3+sha-92ebfd1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.3+sha-27d43a4');
+const VERSION = new Version('16.2.0-next.3+sha-92ebfd1');
 
 // This default value is when checking the hierarchy for a token.
 //
