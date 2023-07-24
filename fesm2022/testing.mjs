c2d095030918ffc83a8467644812ffb0a4943f3c
Andrew Kushnir <akushnir@google.com>
2023-07-24 15:23:08 +0000

    27d43a4a7f build: lock file maintenance (#51134)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.3+sha-4120a03
+ * @license Angular v16.2.0-next.3+sha-27d43a4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10485,7 +10485,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.3+sha-4120a03');
+const VERSION = new Version('16.2.0-next.3+sha-27d43a4');
 
 // This default value is when checking the hierarchy for a token.
 //
