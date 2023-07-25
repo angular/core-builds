20cefa34e45d35790714f7dc0f40a7b61ed575b9
Andrew Kushnir <akushnir@google.com>
2023-07-25 17:39:41 +0000

    2c5e5e4970 docs: updated the tour of heroes doc file (#51151)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.3+sha-d886887
+ * @license Angular v16.2.0-next.3+sha-2c5e5e4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10485,7 +10485,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.3+sha-d886887');
+const VERSION = new Version('16.2.0-next.3+sha-2c5e5e4');
 
 // This default value is when checking the hierarchy for a token.
 //
