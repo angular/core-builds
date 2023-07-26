9f0c16c9e985c76e0c8586c7544562cd16ac0e51
Andrew Kushnir <akushnir@google.com>
2023-07-26 17:19:00 +0000

    bb0f3bc125 docs(docs-infra): Adding required and Transform Args into the usage and documentation of input (#51173)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-3a59de6
+ * @license Angular v16.2.0-next.4+sha-bb0f3bc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-3a59de6');
+const VERSION = new Version('16.2.0-next.4+sha-bb0f3bc');
 
 // This default value is when checking the hierarchy for a token.
 //
