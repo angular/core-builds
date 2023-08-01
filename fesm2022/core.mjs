ba28970573862bd80692a1c10261ba4837a93a69
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:08:48 +0000

    6691c497e2 docs: add lang attributes to the files (#51200)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-63b7f52
+ * @license Angular v16.2.0-next.4+sha-6691c49
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10252,7 +10252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-63b7f52');
+const VERSION = new Version('16.2.0-next.4+sha-6691c49');
 
 // This default value is when checking the hierarchy for a token.
 //
