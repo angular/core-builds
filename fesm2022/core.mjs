dbaf11139106a5f3086c0915e94bfe843887d9a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:07:36 +0000

    71e803a868 docs: add new edition of Angular Projects book (#51184) PR Close #51184

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-5c6d1bb
+ * @license Angular v16.2.0-next.4+sha-71e803a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10252,7 +10252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-5c6d1bb');
+const VERSION = new Version('16.2.0-next.4+sha-71e803a');
 
 // This default value is when checking the hierarchy for a token.
 //
