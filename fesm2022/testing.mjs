4674998abdf65298de25d3d0c3ad57b84657d16e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:22:03 +0000

    2274d156dc docs: updated the doc files (#51204)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-a248307
+ * @license Angular v16.2.0-next.4+sha-2274d15
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10645,7 +10645,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-a248307');
+const VERSION = new Version('16.2.0-next.4+sha-2274d15');
 
 // This default value is when checking the hierarchy for a token.
 //
