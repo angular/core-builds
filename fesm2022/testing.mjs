3e67c2b09fa86eab0d008700cf100dcf10200890
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:07:27 +0000

    5c6d1bb6e3 docs: fix typos in component overview guide (#51197) PR Close #51197

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-efb486e
+ * @license Angular v16.2.0-next.4+sha-5c6d1bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10645,7 +10645,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-efb486e');
+const VERSION = new Version('16.2.0-next.4+sha-5c6d1bb');
 
 // This default value is when checking the hierarchy for a token.
 //
