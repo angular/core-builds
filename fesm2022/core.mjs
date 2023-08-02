d55b8f176ef1cdc6ac2fcd2d9fff188d8e88e15f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-02 20:05:02 +0000

    9d88cd8e0e docs: release notes for the v16.1.8 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.4+sha-2d52d5e
+ * @license Angular v16.2.0-next.4+sha-9d88cd8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10252,7 +10252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-next.4+sha-2d52d5e');
+const VERSION = new Version('16.2.0-next.4+sha-9d88cd8');
 
 // This default value is when checking the hierarchy for a token.
 //
