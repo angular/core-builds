85a09e49d42c1df606a04f992ca83a08bb1e70f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-02 19:55:23 +0000

    938d7a65f6 release: cut the v16.1.8 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.7+sha-8ffa85f
+ * @license Angular v16.1.8+sha-938d7a6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10100,7 +10100,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.7+sha-8ffa85f');
+const VERSION = new Version('16.1.8+sha-938d7a6');
 
 // This default value is when checking the hierarchy for a token.
 //
