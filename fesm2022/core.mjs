6c2e5dead3c98da531ded81a357e2cbcc1f24250
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 15:44:54 +0000

    526e215dcd docs: fix missing space after full stop (#51252)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-6f506cd
+ * @license Angular v16.3.0-next.0+sha-526e215
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10252,7 +10252,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-6f506cd');
+const VERSION = new Version('16.3.0-next.0+sha-526e215');
 
 // This default value is when checking the hierarchy for a token.
 //
