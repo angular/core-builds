38e58223726d7babd974ae8d3d968dfcee10a39f
Dylan Hunn <dylhunn@gmail.com>
2023-08-08 17:58:38 +0000

    b7dc169cf4 docs: Instead of CanActivateFn, CanActivateChildFn appears in CanActivateFn docs (#51283)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.8+sha-4a8efbe
+ * @license Angular v16.1.8+sha-b7dc169
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10100,7 +10100,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.1.8+sha-4a8efbe');
+const VERSION = new Version('16.1.8+sha-b7dc169');
 
 // This default value is when checking the hierarchy for a token.
 //
