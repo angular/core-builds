6d20a03324f5b3d6f294ea69c90d1b2a11ade4ad
Dylan Hunn <dylhunn@gmail.com>
2023-08-08 21:08:21 +0000

    91ea1bade6 build: update @angular/ng-dev (#51253)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-3a647a5
+ * @license Angular v16.3.0-next.0+sha-91ea1ba
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10887,7 +10887,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-3a647a5');
+const VERSION = new Version('16.3.0-next.0+sha-91ea1ba');
 
 // This default value is when checking the hierarchy for a token.
 //
