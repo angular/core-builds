e7bcfffda49c048334083280b732328c2e0f85b0
Dylan Hunn <dylhunn@gmail.com>
2023-08-09 17:34:32 +0000

    9998bee5f4 refactor: add `dgp1130` to DevTools PullApprove config (#51301)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-rc.0+sha-899e51a
+ * @license Angular v16.2.0-rc.0+sha-9998bee
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10291,7 +10291,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0-rc.0+sha-899e51a');
+const VERSION = new Version('16.2.0-rc.0+sha-9998bee');
 
 // This default value is when checking the hierarchy for a token.
 //
