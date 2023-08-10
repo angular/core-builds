dfcefd2a511b6fb2826050931a1f880264b4b5b2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 00:33:05 +0000

    af0c8b8fa8 docs: add legend tag to fieldset (#51259)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0+sha-096fdd0
+ * @license Angular v16.2.0+sha-af0c8b8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10291,7 +10291,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0+sha-096fdd0');
+const VERSION = new Version('16.2.0+sha-af0c8b8');
 
 // This default value is when checking the hierarchy for a token.
 //
