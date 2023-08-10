20939dff8544d84b60100925767b1e70e3bc09e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 00:46:21 +0000

    b07266d6a9 ci: migrate framework tests to GHA (#51101)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-1a56763
+ * @license Angular v16.3.0-next.0+sha-b07266d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10887,7 +10887,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-1a56763');
+const VERSION = new Version('16.3.0-next.0+sha-b07266d');
 
 // This default value is when checking the hierarchy for a token.
 //
