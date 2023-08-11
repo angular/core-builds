aaf1acafd84044f57d3a9e05dbf74a36859830e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-11 13:33:15 +0000

    f67812c220 release: switch the next branch to v17.0.0-next.0

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-b2661fe
+ * @license Angular v17.0.0-next.0+sha-f67812c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11232,7 +11232,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-b2661fe');
+const VERSION = new Version('17.0.0-next.0+sha-f67812c');
 
 // This default value is when checking the hierarchy for a token.
 //
