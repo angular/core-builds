4cea7976c9aa327385a14911d82de3717b042d4e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-11 13:17:53 +0000

    b2661fe2a0 docs(docs-infra): Hide decorator option row if empty. (#51327)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.3.0-next.0+sha-464b458
+ * @license Angular v16.3.0-next.0+sha-b2661fe
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10887,7 +10887,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.3.0-next.0+sha-464b458');
+const VERSION = new Version('16.3.0-next.0+sha-b2661fe');
 
 // This default value is when checking the hierarchy for a token.
 //
