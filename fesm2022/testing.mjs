533aee203dfef99ed3fe8f35db825e73edd8ac02
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 22:22:43 +0000

    074fd4a5ec build: update pullapprove (#51360)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0+sha-094d733
+ * @license Angular v16.2.0+sha-074fd4a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10939,7 +10939,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0+sha-094d733');
+const VERSION = new Version('16.2.0+sha-074fd4a');
 
 // This default value is when checking the hierarchy for a token.
 //
