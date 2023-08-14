b82fc07d6a7aadf5f5e6d3c211bec18ab93750a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 21:41:19 +0000

    5a2b7683bc docs: Add CDN Optimization section to hydration guide (#51355)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-ebd217d
+ * @license Angular v17.0.0-next.0+sha-5a2b768
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10887,7 +10887,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-ebd217d');
+const VERSION = new Version('17.0.0-next.0+sha-5a2b768');
 
 // This default value is when checking the hierarchy for a token.
 //
