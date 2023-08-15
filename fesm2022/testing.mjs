ee4fe1342d97ed18123aa8e778d7ada82df6e9b1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:23:00 +0000

    328b3613b8 docs(common): revert format date_pipe (#51359)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-422d0d5
+ * @license Angular v17.0.0-next.0+sha-328b361
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11232,7 +11232,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-422d0d5');
+const VERSION = new Version('17.0.0-next.0+sha-328b361');
 
 // This default value is when checking the hierarchy for a token.
 //
