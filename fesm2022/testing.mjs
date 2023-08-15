dfc4924d965e00813fb47c9f0259a52503c47389
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 22:53:10 +0000

    da68bead56 docs(router): Clarify how to clear secondary routes (#51376)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0+sha-543b98e
+ * @license Angular v16.2.0+sha-da68bea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10939,7 +10939,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0+sha-543b98e');
+const VERSION = new Version('16.2.0+sha-da68bea');
 
 // This default value is when checking the hierarchy for a token.
 //
