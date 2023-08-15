907cbffec5a75a041f3ab79e73d58d1c495c0c43
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 16:47:48 +0000

    9f977e0f34 docs(docs-infra): fix minor typo in standalone-migration.md (#51370) PR Close #51370

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0+sha-3e19890
+ * @license Angular v16.2.0+sha-9f977e0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10291,7 +10291,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.0+sha-3e19890');
+const VERSION = new Version('16.2.0+sha-9f977e0');
 
 // This default value is when checking the hierarchy for a token.
 //
