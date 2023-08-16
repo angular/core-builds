5b11d8be6ca3b5963567f73e45feb05c20b00426
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-16 17:53:58 +0000

    77504f18af release: cut the v17.0.0-next.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-72beab0
+ * @license Angular v17.0.0-next.0+sha-77504f1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10887,7 +10887,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-72beab0');
+const VERSION = new Version('17.0.0-next.0+sha-77504f1');
 
 // This default value is when checking the hierarchy for a token.
 //
