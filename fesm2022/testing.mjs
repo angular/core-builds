b722591691c359f2f26504432f7fed1596be5e0e
Andrew Kushnir <akushnir@google.com>
2023-08-17 17:35:41 +0000

    552ea77854 refactor(compiler-cli): drop `tsickle` code paths (#50602)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-d910bf8
+ * @license Angular v17.0.0-next.0+sha-552ea77
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11307,7 +11307,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-d910bf8');
+const VERSION = new Version('17.0.0-next.0+sha-552ea77');
 
 // This default value is when checking the hierarchy for a token.
 //
