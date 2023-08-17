1f9d791ca63619bd87728b0b97cfa26238e6cd19
Andrew Kushnir <akushnir@google.com>
2023-08-17 21:15:24 +0000

    8798abb744 refactor(core): implement runtime logic to compute component dependencies in local compilation mode (#51377)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-0b901a8
+ * @license Angular v17.0.0-next.0+sha-8798abb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10852,7 +10852,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-0b901a8');
+const VERSION = new Version('17.0.0-next.0+sha-8798abb');
 
 // This default value is when checking the hierarchy for a token.
 //
