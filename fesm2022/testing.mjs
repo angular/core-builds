348c0ee604d0e629c89c93ca22849bb00521bbae
Andrew Kushnir <akushnir@google.com>
2023-08-21 15:24:05 +0000

    53434864cb ci: remove myself from pullapprove unavailability list (#51437)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-f724b8d
+ * @license Angular v17.0.0-next.0+sha-5343486
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11272,7 +11272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-f724b8d');
+const VERSION = new Version('17.0.0-next.0+sha-5343486');
 
 // This default value is when checking the hierarchy for a token.
 //
