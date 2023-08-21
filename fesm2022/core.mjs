484654403e5c68a0a81aa27ed37c139e222a18b0
Andrew Kushnir <akushnir@google.com>
2023-08-21 20:11:14 +0000

    38671f7c14 docs(animations): update animation docs to be standalone first (#51390)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-3a68647
+ * @license Angular v17.0.0-next.0+sha-38671f7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10852,7 +10852,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-3a68647');
+const VERSION = new Version('17.0.0-next.0+sha-38671f7');
 
 // This default value is when checking the hierarchy for a token.
 //
