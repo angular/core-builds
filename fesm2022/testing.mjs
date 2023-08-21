c2704096760b8a0afe1dfd5f135411ad45e49ef1
Andrew Kushnir <akushnir@google.com>
2023-08-21 20:13:08 +0000

    b0396e7164 fix(router): Ensure `canceledNavigationResolution: 'computed'` works on first page (#51441)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.1+sha-6dc2b22
+ * @license Angular v16.2.1+sha-b0396e7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10939,7 +10939,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.1+sha-6dc2b22');
+const VERSION = new Version('16.2.1+sha-b0396e7');
 
 // This default value is when checking the hierarchy for a token.
 //
