7d78778b75f0f0754c734c1f0c9c4113f7572e58
Andrew Kushnir <akushnir@google.com>
2023-08-22 15:12:32 +0000

    bd150a2550 docs: remove update reference (#51449)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-d2346a6
+ * @license Angular v17.0.0-next.0+sha-bd150a2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11272,7 +11272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-d2346a6');
+const VERSION = new Version('17.0.0-next.0+sha-bd150a2');
 
 // This default value is when checking the hierarchy for a token.
 //
