41465263aa8b59669861f7683ebfe35b180127cd
Andrew Kushnir <akushnir@google.com>
2023-08-22 15:16:15 +0000

    157415831c refactor(router): Have router subscribe to transition events to update state (#48427)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.1+sha-4fe88cf
+ * @license Angular v16.2.1+sha-1574158
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10939,7 +10939,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.1+sha-4fe88cf');
+const VERSION = new Version('16.2.1+sha-1574158');
 
 // This default value is when checking the hierarchy for a token.
 //
