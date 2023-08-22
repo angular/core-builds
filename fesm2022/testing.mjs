31f34f52be2b867b84326a11d4c7adeee42a9e80
Andrew Kushnir <akushnir@google.com>
2023-08-22 18:52:06 +0000

    76860b54b9 ci: correctly check aio job results (#51464)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.1+sha-fb296e4
+ * @license Angular v16.2.1+sha-76860b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10939,7 +10939,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.1+sha-fb296e4');
+const VERSION = new Version('16.2.1+sha-76860b5');
 
 // This default value is when checking the hierarchy for a token.
 //
