b636d06e342ba5233c99d4704fbed411134744c9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-08-23 17:53:00 +0000

    1a8aa9ddfa release: cut the v17.0.0-next.1 release

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-189bb74
+ * @license Angular v17.0.0-next.1+sha-1a8aa9d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11272,7 +11272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.0+sha-189bb74');
+const VERSION = new Version('17.0.0-next.1+sha-1a8aa9d');
 
 // This default value is when checking the hierarchy for a token.
 //
