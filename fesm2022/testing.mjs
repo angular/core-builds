33cd6e8d93d578d7fecc4de2f328451fd16df0eb
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-23 21:54:46 +0000

    5cea2b52f6 ci: properly provide environment variables for aio deployment (#51484)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-574a759
+ * @license Angular v17.0.0-next.1+sha-5cea2b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11272,7 +11272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-574a759');
+const VERSION = new Version('17.0.0-next.1+sha-5cea2b5');
 
 // This default value is when checking the hierarchy for a token.
 //
