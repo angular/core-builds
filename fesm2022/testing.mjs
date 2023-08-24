fa349c8f79c6cd4722c3b0b116769a77b83ff8fa
Joey Perrott <josephperrott@gmail.com>
2023-08-24 20:45:30 +0000

    c3349fb2a1 ci: fix escaping for determing current stable version (#51496)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-6d29ab7
+ * @license Angular v17.0.0-next.1+sha-c3349fb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11272,7 +11272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-6d29ab7');
+const VERSION = new Version('17.0.0-next.1+sha-c3349fb');
 
 // This default value is when checking the hierarchy for a token.
 //
