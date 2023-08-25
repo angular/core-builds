599356b7eb960493ecfad98eef4bf8b9df25103d
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-25 16:53:09 +0000

    440684ddb4 docs(router): Update the url matcher guide to use standalone (#51450)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-c3349fb
+ * @license Angular v17.0.0-next.1+sha-440684d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11272,7 +11272,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-c3349fb');
+const VERSION = new Version('17.0.0-next.1+sha-440684d');
 
 // This default value is when checking the hierarchy for a token.
 //
