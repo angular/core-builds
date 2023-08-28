158c521911b45c3377725926257245ea0c69824e
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 18:46:31 +0000

    a28864d0f7 docs: migrate template guide and its code examples to standalone. (#51364)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-dc4db80
+ * @license Angular v17.0.0-next.1+sha-a28864d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10854,7 +10854,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-dc4db80');
+const VERSION = new Version('17.0.0-next.1+sha-a28864d');
 
 // This default value is when checking the hierarchy for a token.
 //
