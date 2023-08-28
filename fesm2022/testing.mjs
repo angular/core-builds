eb7d28003521615474b0e8d919da28c0e7379912
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 18:46:14 +0000

    14941b9b79 docs(docs-infra): remove inactive resource from community page (#51418)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.2+sha-0ad7386
+ * @license Angular v16.2.2+sha-14941b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10939,7 +10939,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.2+sha-0ad7386');
+const VERSION = new Version('16.2.2+sha-14941b9');
 
 // This default value is when checking the hierarchy for a token.
 //
