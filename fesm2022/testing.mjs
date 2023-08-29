1adacca2fdbcd1e30f40a86450a2e27ecee1c56a
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 00:06:14 +0000

    f7364ece87 docs: migrate binding-syntax code to standalone (#51479)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-ba32d6f
+ * @license Angular v17.0.0-next.1+sha-f7364ec
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11275,7 +11275,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-ba32d6f');
+const VERSION = new Version('17.0.0-next.1+sha-f7364ec');
 
 // This default value is when checking the hierarchy for a token.
 //
