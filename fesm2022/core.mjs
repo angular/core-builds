9a41bbd56698d4ca200837631bcbec77083e949e
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:42:40 +0000

    fa29eb84a3 docs: fix casing of a11y labels in main page (#51431) PR Close #51431

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-8c77240
+ * @license Angular v17.0.0-next.1+sha-fa29eb8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10854,7 +10854,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-8c77240');
+const VERSION = new Version('17.0.0-next.1+sha-fa29eb8');
 
 // This default value is when checking the hierarchy for a token.
 //
