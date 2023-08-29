cbda23fc64cc18a96976807436c0f107d5e9c67f
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 20:00:38 +0000

    c62e680098 fix(router): Remove deprecated Router properties (#51502)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-6fff65c
+ * @license Angular v17.0.0-next.1+sha-c62e680
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10854,7 +10854,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-6fff65c');
+const VERSION = new Version('17.0.0-next.1+sha-c62e680');
 
 // This default value is when checking the hierarchy for a token.
 //
