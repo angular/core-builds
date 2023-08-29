c8f32c6bcd47f31f545271dbf04bd3d544f0dbd7
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:08:44 +0000

    e027a59be0 docs: migrate built-in-template-functions to standalone (#51486)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-7d4550b
+ * @license Angular v17.0.0-next.1+sha-e027a59
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10854,7 +10854,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-7d4550b');
+const VERSION = new Version('17.0.0-next.1+sha-e027a59');
 
 // This default value is when checking the hierarchy for a token.
 //
