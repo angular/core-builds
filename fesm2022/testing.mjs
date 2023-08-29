b6352d2906dea1be71a9cb5b90d295e9221c79d4
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 19:18:39 +0000

    6e5b865710 build: use pin for devcontainer base image (#51556)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.2+sha-0c7c852
+ * @license Angular v16.2.2+sha-6e5b865
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10940,7 +10940,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.2+sha-0c7c852');
+const VERSION = new Version('16.2.2+sha-6e5b865');
 
 // This default value is when checking the hierarchy for a token.
 //
