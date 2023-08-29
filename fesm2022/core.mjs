8dbc16e246c7cb5cf1df3aa1e1674e06541d4932
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 18:10:11 +0000

    7ed4c56c24 docs: migrate event-binding code to standalone (#51538)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-685d01e
+ * @license Angular v17.0.0-next.1+sha-7ed4c56
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10854,7 +10854,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-685d01e');
+const VERSION = new Version('17.0.0-next.1+sha-7ed4c56');
 
 // This default value is when checking the hierarchy for a token.
 //
