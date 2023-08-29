7cec09e7c2219a3694e598c849d6b071666f8c41
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 20:37:05 +0000

    97a40958b5 build: update security related package version for aio (#51562)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.2+sha-dbd761f
+ * @license Angular v16.2.2+sha-97a4095
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10940,7 +10940,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.2+sha-dbd761f');
+const VERSION = new Version('16.2.2+sha-97a4095');
 
 // This default value is when checking the hierarchy for a token.
 //
