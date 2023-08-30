fe6066bcf07134f144772cff3f269ab5f9c4b231
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 16:01:35 +0000

    dad47bb720 docs: migrate security guide and example to standalone (#51446)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-c340d6e
+ * @license Angular v17.0.0-next.1+sha-dad47bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10854,7 +10854,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.1+sha-c340d6e');
+const VERSION = new Version('17.0.0-next.1+sha-dad47bb');
 
 // This default value is when checking the hierarchy for a token.
 //
