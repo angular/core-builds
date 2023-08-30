bccaaacbe651df32b998f482af35875527d01b11
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 16:06:00 +0000

    6b5c812b6b docs(core): clarify InjectionToken usage (#51386)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.1+sha-dad47bb
+ * @license Angular v17.0.0-next.1+sha-6b5c812
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4711,11 +4711,17 @@ export declare enum InjectFlags {
  * `InjectionToken` is parameterized on `T` which is the type of object which will be returned by
  * the `Injector`. This provides an additional level of type safety.
  *
- * ```
- * interface MyInterface {...}
- * const myInterface = injector.get(new InjectionToken<MyInterface>('SomeToken'));
- * // myInterface is inferred to be MyInterface.
- * ```
+ * <div class="alert is-helpful">
+ *
+ * **Important Note**: Ensure that you use the same instance of the `InjectionToken` in both the
+ * provider and the injection call. Creating a new instance of `InjectionToken` in different places,
+ * even with the same description, will be treated as different tokens by Angular's DI system,
+ * leading to a `NullInjectorError`.
+ *
+ * </div>
+ *
+ * <code-example format="typescript" language="typescript" path="injection-token/src/main.ts"
+ * region="InjectionToken"></code-example>
  *
  * When creating an `InjectionToken`, you can optionally specify a factory function which returns
  * (possibly by creating) a default value of the parameterized type `T`. This sets up the
@@ -4741,7 +4747,6 @@ export declare enum InjectFlags {
  * ### Tree-shakable InjectionToken
  *
  * {@example core/di/ts/injector_spec.ts region='ShakableInjectionToken'}
- *
  *
  * @publicApi
  */
