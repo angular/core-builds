5a07f382f6d14fca4e0af60de2cd1237ab10693a
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:42:07 +0000

    240b7cd9c2 build: update dependency archiver to v6 (#51605)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-525acbb
+ * @license Angular v17.0.0-next.2+sha-240b7cd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10859,7 +10859,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-525acbb');
+const VERSION = new Version('17.0.0-next.2+sha-240b7cd');
 
 // This default value is when checking the hierarchy for a token.
 //
