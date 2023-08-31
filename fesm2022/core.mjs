4e504dbe0c51d4ecd294d399e2f0a6edc81c7059
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 16:44:51 +0000

    2b518bb899 build: update actions/checkout digest to f43a0e5 (#51576)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-544c778
+ * @license Angular v17.0.0-next.2+sha-2b518bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10859,7 +10859,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-544c778');
+const VERSION = new Version('17.0.0-next.2+sha-2b518bb');
 
 // This default value is when checking the hierarchy for a token.
 //
