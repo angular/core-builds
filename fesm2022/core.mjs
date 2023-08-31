67776d3df78ca9d129cf073aa46518c92883ffed
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 16:36:22 +0000

    544c778b98 ci: fix permissions issue in zonejs ci test (#51600)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-eab9216
+ * @license Angular v17.0.0-next.2+sha-544c778
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10859,7 +10859,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-eab9216');
+const VERSION = new Version('17.0.0-next.2+sha-544c778');
 
 // This default value is when checking the hierarchy for a token.
 //
