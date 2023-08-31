b90cdf2b9603bf73c2ecd073ad869e28fe6e33d4
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:11:04 +0000

    acd59ad037 docs: Migrate Observables guides & code examples to standalone (#51516)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-92f478e
+ * @license Angular v17.0.0-next.2+sha-acd59ad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10859,7 +10859,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-92f478e');
+const VERSION = new Version('17.0.0-next.2+sha-acd59ad');
 
 // This default value is when checking the hierarchy for a token.
 //
