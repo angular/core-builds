320c41c580ebc0a31df436174df9a0421ba5cafe
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:42:24 +0000

    78afe889fd refactor(devtools): migrate material legacy components to mdc (#51590)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-240b7cd
+ * @license Angular v17.0.0-next.2+sha-78afe88
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10859,7 +10859,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-240b7cd');
+const VERSION = new Version('17.0.0-next.2+sha-78afe88');
 
 // This default value is when checking the hierarchy for a token.
 //
