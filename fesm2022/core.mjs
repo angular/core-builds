22421b1b5fc22e0dda4b1c56a5439671b250937b
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 16:47:10 +0000

    cfcd2e3aeb refactor(docs-infra): correct incomplete string replacement (#51585)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-c439a40
+ * @license Angular v17.0.0-next.2+sha-cfcd2e3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10859,7 +10859,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-c439a40');
+const VERSION = new Version('17.0.0-next.2+sha-cfcd2e3');
 
 // This default value is when checking the hierarchy for a token.
 //
