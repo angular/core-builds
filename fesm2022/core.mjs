756187921a28052a76d1ebc3d6368cfba1661817
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:25:36 +0000

    8a3479b9e4 fix(docs-infra): fix incomplete escaping (#51604)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-acd59ad
+ * @license Angular v17.0.0-next.2+sha-8a3479b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10859,7 +10859,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-acd59ad');
+const VERSION = new Version('17.0.0-next.2+sha-8a3479b');
 
 // This default value is when checking the hierarchy for a token.
 //
