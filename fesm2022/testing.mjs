9047ed9e62ed157b548f6b383cb8f1a7473d0051
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 15:31:11 +0000

    af9cd84c13 build: migrate to node 18 (#51609)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.3+sha-f56b655
+ * @license Angular v16.2.3+sha-af9cd84
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10905,7 +10905,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.3+sha-f56b655');
+const VERSION = new Version('16.2.3+sha-af9cd84');
 
 // This default value is when checking the hierarchy for a token.
 //
