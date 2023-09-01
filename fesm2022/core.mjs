a8d77c2cc3d1db0bdf61db40e0ba39f5bbc1f405
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 14:50:54 +0000

    dc4b4aa57e feat(devtools): Display getters and setters in devtools property viewer (#49695)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-201ab9d
+ * @license Angular v17.0.0-next.2+sha-dc4b4aa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10819,7 +10819,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-201ab9d');
+const VERSION = new Version('17.0.0-next.2+sha-dc4b4aa');
 
 // This default value is when checking the hierarchy for a token.
 //
