4f8439c8e707ac6e5100a00d585e9be59f4c2300
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 20:53:48 +0000

    690c8325cd build: lock file maintenance (#51628)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-1aff106
+ * @license Angular v17.0.0-next.2+sha-690c832
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10834,7 +10834,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-1aff106');
+const VERSION = new Version('17.0.0-next.2+sha-690c832');
 
 // This default value is when checking the hierarchy for a token.
 //
