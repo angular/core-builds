8934d903287c5994f5129ec5173b61ed00152c81
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 14:26:30 +0000

    fcfa173270 docs: add Connie Leung to GDE contributors list (#51476)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.3+sha-dcf6b01
+ * @license Angular v16.2.3+sha-fcfa173
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10256,7 +10256,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.3+sha-dcf6b01');
+const VERSION = new Version('16.2.3+sha-fcfa173');
 
 // This default value is when checking the hierarchy for a token.
 //
