486b80d5bcd015662bb6ceb6391679120a31c3ab
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 20:18:08 +0000

    ca1dac2ada docs: update Angular CLI help [main] (#51661)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-df54b1c
+ * @license Angular v17.0.0-next.2+sha-ca1dac2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10834,7 +10834,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-df54b1c');
+const VERSION = new Version('17.0.0-next.2+sha-ca1dac2');
 
 // This default value is when checking the hierarchy for a token.
 //
