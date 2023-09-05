d1551b84cdf8c1515702a50bd8658b52db0c7287
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 18:28:24 +0000

    df54b1ce1a ci: disable all of the angular-robot settings (#51663)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-7c06886
+ * @license Angular v17.0.0-next.2+sha-df54b1c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10834,7 +10834,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-7c06886');
+const VERSION = new Version('17.0.0-next.2+sha-df54b1c');
 
 // This default value is when checking the hierarchy for a token.
 //
