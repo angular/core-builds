180ec6841764c95f4ecd2f4a6b6047816f407d19
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 18:17:53 +0000

    2d96dc27b7 ci: remove test job from circleci in favor of GHA (#51660)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-e804075
+ * @license Angular v17.0.0-next.2+sha-2d96dc2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10834,7 +10834,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-e804075');
+const VERSION = new Version('17.0.0-next.2+sha-2d96dc2');
 
 // This default value is when checking the hierarchy for a token.
 //
