e1c8a9efd0594665f0ead6892e639c51906be194
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:43:19 +0000

    fe0b793fdc refactor(router): Share more code in the route matching utilities (#51543)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.2+sha-88a0af6
+ * @license Angular v17.0.0-next.2+sha-fe0b793
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10834,7 +10834,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.2+sha-88a0af6');
+const VERSION = new Version('17.0.0-next.2+sha-fe0b793');
 
 // This default value is when checking the hierarchy for a token.
 //
