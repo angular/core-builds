8ca042a0626beec098e00d3879fa01ba974622b2
Andrew Kushnir <akushnir@google.com>
2023-09-08 16:38:55 +0000

    00bf05251b docs: update Angular CLI help [main] (#51704)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-b3edcda
+ * @license Angular v17.0.0-next.3+sha-00bf052
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10834,7 +10834,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-b3edcda');
+const VERSION = new Version('17.0.0-next.3+sha-00bf052');
 
 // This default value is when checking the hierarchy for a token.
 //
