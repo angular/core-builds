9710ee79fcbe9f3f224a379e4fd5b8753e6ec8e6
Andrew Kushnir <akushnir@google.com>
2023-09-12 20:06:36 +0000

    f9939757d3 build: skip simulated file system tests on Windows (#51738)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-635318f
+ * @license Angular v17.0.0-next.3+sha-f993975
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10836,7 +10836,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-635318f');
+const VERSION = new Version('17.0.0-next.3+sha-f993975');
 
 // This default value is when checking the hierarchy for a token.
 //
