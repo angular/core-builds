30d84d0b4b30f418b646d96b94077b5210fba3c2
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:31:48 +0000

    f6ccb163aa docs: Migrate i18n guide pages and code to Standalone (#51589)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-44c7469
+ * @license Angular v17.0.0-next.3+sha-f6ccb16
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10837,7 +10837,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-44c7469');
+const VERSION = new Version('17.0.0-next.3+sha-f6ccb16');
 
 // This default value is when checking the hierarchy for a token.
 //
