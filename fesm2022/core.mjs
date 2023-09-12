5ba74ab28b344bf7a475fe30229e3b5191304395
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:30:01 +0000

    44c7469495 docs: Migrate Universal guide and code to Standalone (#51564)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-52cc7f8
+ * @license Angular v17.0.0-next.3+sha-44c7469
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10837,7 +10837,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-52cc7f8');
+const VERSION = new Version('17.0.0-next.3+sha-44c7469');
 
 // This default value is when checking the hierarchy for a token.
 //
