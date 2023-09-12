05501331db860972e8b02e5ca6888f6d04e2a659
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:31:57 +0000

    b08323747e docs: Migrate 2nd set of template guide pages and code to Standalone (#51632)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-f6ccb16
+ * @license Angular v17.0.0-next.3+sha-b083237
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10837,7 +10837,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-f6ccb16');
+const VERSION = new Version('17.0.0-next.3+sha-b083237');
 
 // This default value is when checking the hierarchy for a token.
 //
