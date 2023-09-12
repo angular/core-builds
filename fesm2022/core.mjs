bb36ac98bfe9bc4692d50d92d85695c6a84b2583
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:32:57 +0000

    7b0bfc1dd8 docs: migrate testing to standalone (#51684)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-b083237
+ * @license Angular v17.0.0-next.3+sha-7b0bfc1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10837,7 +10837,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-b083237');
+const VERSION = new Version('17.0.0-next.3+sha-7b0bfc1');
 
 // This default value is when checking the hierarchy for a token.
 //
