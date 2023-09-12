9ec8d348901a5f20186fcd0372fbb28a741ef45d
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:34:23 +0000

    cbeef95c29 docs: migrate form-validation to standalone (#51709)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-4e0a575
+ * @license Angular v17.0.0-next.3+sha-cbeef95
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10837,7 +10837,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-4e0a575');
+const VERSION = new Version('17.0.0-next.3+sha-cbeef95');
 
 // This default value is when checking the hierarchy for a token.
 //
