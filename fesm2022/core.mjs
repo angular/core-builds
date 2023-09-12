cd83b9676cd85b0481bedc8dc6530b7a0a82c869
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:34:43 +0000

    65f7686769 docs: migrate reactive-forms to standalone (#51707)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.3+sha-cbeef95
+ * @license Angular v17.0.0-next.3+sha-65f7686
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10837,7 +10837,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.3+sha-cbeef95');
+const VERSION = new Version('17.0.0-next.3+sha-65f7686');
 
 // This default value is when checking the hierarchy for a token.
 //
