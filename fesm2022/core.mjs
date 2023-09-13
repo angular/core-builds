7dd18ec2f2f124eab4a77479202d529b7d9d56f4
Andrew Kushnir <akushnir@google.com>
2023-09-13 17:58:33 +0000

    2a1723c945 test(compiler): fix broken integrity check (#51751)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-06bbc2f
+ * @license Angular v17.0.0-next.4+sha-2a1723c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10849,7 +10849,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-06bbc2f');
+const VERSION = new Version('17.0.0-next.4+sha-2a1723c');
 
 // This default value is when checking the hierarchy for a token.
 //
