bc205f8b4fae660423deac29274c6b24ddf22914
Andrew Kushnir <akushnir@google.com>
2023-09-13 17:59:23 +0000

    59aa0634f4 build: remove support for Node.js v16 (#51755)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-2a1723c
+ * @license Angular v17.0.0-next.4+sha-59aa063
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10849,7 +10849,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-2a1723c');
+const VERSION = new Version('17.0.0-next.4+sha-59aa063');
 
 // This default value is when checking the hierarchy for a token.
 //
