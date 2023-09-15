6740fed69d3555955cf5050bd6e697a7ed3a6087
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 07:14:10 +0000

    25b9b86373 build: update babel dependencies (#50932)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-06bf80d
+ * @license Angular v17.0.0-next.4+sha-25b9b86
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10854,7 +10854,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-06bf80d');
+const VERSION = new Version('17.0.0-next.4+sha-25b9b86');
 
 // This default value is when checking the hierarchy for a token.
 //
