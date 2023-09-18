4604feccf4ebcd76b87c7cb9198921e8307ba9a9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 08:57:05 +0000

    75380bf220 test(compiler): attempt to deflake windows tests (#51804)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-fe2fd7e
+ * @license Angular v17.0.0-next.4+sha-75380bf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10877,7 +10877,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-fe2fd7e');
+const VERSION = new Version('17.0.0-next.4+sha-75380bf');
 
 // This default value is when checking the hierarchy for a token.
 //
