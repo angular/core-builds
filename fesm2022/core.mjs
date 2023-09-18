4b91f76aff5276e9d76f2d7ea766c92291ce1f4b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 10:07:02 +0000

    9bd1551c3f release: cut the zone.js-0.14.0 release (#51774)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-75380bf
+ * @license Angular v17.0.0-next.4+sha-9bd1551
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10877,7 +10877,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-75380bf');
+const VERSION = new Version('17.0.0-next.4+sha-9bd1551');
 
 // This default value is when checking the hierarchy for a token.
 //
