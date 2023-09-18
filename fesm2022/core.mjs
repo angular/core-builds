63e5b4b3545bd283d6e7d3ab7b78e3a21e631369
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 08:48:10 +0000

    e2416a284f feat(language-service): Enable go to definition of styleUrl (#51746)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-0c5b34b
+ * @license Angular v17.0.0-next.4+sha-e2416a2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10877,7 +10877,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-0c5b34b');
+const VERSION = new Version('17.0.0-next.4+sha-e2416a2');
 
 // This default value is when checking the hierarchy for a token.
 //
