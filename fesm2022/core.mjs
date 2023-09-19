f83c967a9aa063d2221cfb65d4a2b049bcaac004
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 10:11:57 +0000

    9f3b549cf6 docs: Migrate Service Worker to Standalone (#51687)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.4+sha-0b3e6a4
+ * @license Angular v17.0.0-next.4+sha-9f3b549
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10898,7 +10898,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.4+sha-0b3e6a4');
+const VERSION = new Version('17.0.0-next.4+sha-9f3b549');
 
 // This default value is when checking the hierarchy for a token.
 //
