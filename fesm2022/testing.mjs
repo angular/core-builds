58064a9a9f7f4092f31ea74b4c7db9939141e9e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 16:43:27 +0000

    4ccd845081 refactor(router): Refactor loadChildren callback execution and privately export for SSR (#51818)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.5+sha-6e4d8f6
+ * @license Angular v16.2.5+sha-4ccd845
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.5+sha-6e4d8f6');
+const VERSION = new Version('16.2.5+sha-4ccd845');
 
 // This default value is when checking the hierarchy for a token.
 //
