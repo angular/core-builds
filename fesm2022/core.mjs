5b0daaf6be54daf29aadb27f0c314a2e7c376be8
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 19:24:54 +0000

    0598613950 refactor(animations): deprecation of `AnimationDriver.NOOP` (#51843)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-e5d327d
+ * @license Angular v17.0.0-next.5+sha-0598613
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10897,7 +10897,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-e5d327d');
+const VERSION = new Version('17.0.0-next.5+sha-0598613');
 
 // This default value is when checking the hierarchy for a token.
 //
