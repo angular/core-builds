14330a31da6a84c8dc284a92d32b7d64ea0231ec
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 16:17:26 +0000

    18e34ad91f ci: use pull request head sha instead of the sha provided in the github context (#51908)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-487b9e1
+ * @license Angular v17.0.0-next.5+sha-18e34ad
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-487b9e1');
+const VERSION = new Version('17.0.0-next.5+sha-18e34ad');
 
 // This default value is when checking the hierarchy for a token.
 //
