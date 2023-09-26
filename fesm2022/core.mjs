78594545902c86667cf22df2f6a655742d011241
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 16:19:28 +0000

    b771539eb3 build: improve results logging in perf script (#51902)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-5f6cc7c
+ * @license Angular v17.0.0-next.5+sha-b771539
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-5f6cc7c');
+const VERSION = new Version('17.0.0-next.5+sha-b771539');
 
 // This default value is when checking the hierarchy for a token.
 //
