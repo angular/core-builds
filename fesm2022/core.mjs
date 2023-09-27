1151ac2e4801f6de1ebd1bb773e2348180925799
Dylan Hunn <dylhunn@gmail.com>
2023-09-27 16:32:35 +0000

    a39010f6ce refactor(core): avoid an empty array allocation during hydration (#51917)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.5+sha-9b40b64
+ * @license Angular v17.0.0-next.5+sha-a39010f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.5+sha-9b40b64');
+const VERSION = new Version('17.0.0-next.5+sha-a39010f');
 
 // This default value is when checking the hierarchy for a token.
 //
@@ -19930,8 +19930,8 @@ let _findMatchingDehydratedViewImpl = (lContainer, template) => null;
  * in this container.
  */
 function findMatchingDehydratedViewImpl(lContainer, template) {
-    const views = lContainer[DEHYDRATED_VIEWS] ?? [];
-    if (!template || views.length === 0) {
+    const views = lContainer[DEHYDRATED_VIEWS];
+    if (!template || views === null || views.length === 0) {
         return null;
     }
     const view = views[0];
