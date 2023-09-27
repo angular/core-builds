7c8a421e6cda4091bff9c8667da95838ad0cc4a6
Dylan Hunn <github@dylanhunn.com>
2023-09-27 20:37:55 +0000

    a16e3b8fe1 release: cut the v16.2.7 release

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.6+sha-39a3e34
+ * @license Angular v16.2.7+sha-a16e3b8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.6+sha-39a3e34');
+const VERSION = new Version('16.2.7+sha-a16e3b8');
 
 // This default value is when checking the hierarchy for a token.
 //
