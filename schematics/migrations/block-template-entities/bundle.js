5696fa68ba84e592d77ad6158a644dc2575e37cf
Dylan Hunn <dylhunn@gmail.com>
2023-09-27 16:32:31 +0000

    69ec3493bb docs: update Angular CLI help [main] (#51916)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23047,7 +23047,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.5+sha-9b40b64");
+var VERSION2 = new Version("17.0.0-next.5+sha-69ec349");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
