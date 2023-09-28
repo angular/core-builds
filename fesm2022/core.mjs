8ebe83024023405aa9e77c3bcf0ed4258a07eac6
Joey Perrott <josephperrott@gmail.com>
2023-09-28 19:52:17 +0000

    ee0d04b630 WIP

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.6+sha-e1728a2
+ * @license Angular v17.0.0-next.6+sha-ee0d04b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.6+sha-e1728a2');
+const VERSION = new Version('17.0.0-next.6+sha-ee0d04b');
 
 // This default value is when checking the hierarchy for a token.
 //
