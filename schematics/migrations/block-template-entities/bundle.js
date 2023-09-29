0ea325395e9b6f31101182d7862b04e5361e4fee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:01:36 +0000

    47e841c834 ci: move permissions for google-internal-tests into job definition (#51847)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23059,7 +23059,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.6+sha-81e7f5b");
+var VERSION2 = new Version("17.0.0-next.6+sha-47e841c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
