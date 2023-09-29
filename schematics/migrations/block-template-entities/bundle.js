6f631e672e62f12a4b6793c383bbef7b6c693260
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:00:09 +0000

    81e7f5be03 test(animations): update the integration test (#50738)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23059,7 +23059,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.6+sha-e1728a2");
+var VERSION2 = new Version("17.0.0-next.6+sha-81e7f5b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
