34d8f7554a6435700ee8d1a020ec85695c523109
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:01:10 +0000

    82e8e5e043 ci: move permissions for google-internal-tests into job definition (#51847)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.7+sha-a16e3b8
+ * @license Angular v16.2.7+sha-82e8e5e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.7+sha-a16e3b8');
+const VERSION = new Version('16.2.7+sha-82e8e5e');
 
 // This default value is when checking the hierarchy for a token.
 //
