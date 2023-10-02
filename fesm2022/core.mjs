232cf9b7d2b986c76f75fe1aa7f11f51996f6ee4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-02 15:45:46 +0000

    367b075d15 test(core): enable previously failing @defer tests (#51965)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.6+sha-e368d81
+ * @license Angular v17.0.0-next.6+sha-367b075
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.6+sha-e368d81');
+const VERSION = new Version('17.0.0-next.6+sha-367b075');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23274,7 +23274,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.6+sha-e368d81");
+var VERSION2 = new Version("17.0.0-next.6+sha-367b075");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
