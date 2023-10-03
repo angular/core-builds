d5eb0f6d31f9e5534363d2043a4e5a6a570cd82c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-03 15:18:16 +0000

    e9c3790b4e docs: update to latest version of material (#51996)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23276,7 +23276,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.6+sha-32cfbb4");
+var VERSION2 = new Version("17.0.0-next.6+sha-e9c3790");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
