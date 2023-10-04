9b68f2353b8986b5985db2957bf0cbac36576c74
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 18:37:58 +0000

    08482f2c7d fix(language-service): Retain correct language service when `ts.Project` reloads (#51912)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.6+sha-422a3db
+ * @license Angular v17.0.0-next.6+sha-08482f2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.6+sha-422a3db');
+const VERSION = new Version('17.0.0-next.6+sha-08482f2');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23446,7 +23446,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.6+sha-422a3db");
+var VERSION2 = new Version("17.0.0-next.6+sha-08482f2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
