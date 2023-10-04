a020370cbe45b4d7846006c43f14d730f872c181
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 14:36:35 +0000

    3bf9b96ef5 build: update dependency css-selector-parser to v3 (#51983)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.6+sha-e5bca43
+ * @license Angular v17.0.0-next.6+sha-3bf9b96
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.6+sha-e5bca43');
+const VERSION = new Version('17.0.0-next.6+sha-3bf9b96');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23431,7 +23431,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.6+sha-e5bca43");
+var VERSION2 = new Version("17.0.0-next.6+sha-3bf9b96");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
