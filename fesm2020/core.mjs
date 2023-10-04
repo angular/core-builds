384a7bffe23fb45a50de4f1373d349246bcae84a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 16:12:30 +0000

    9fe08968b8 fix(service-worker): throw a critical error when handleFetch fail (#51989)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.9+sha-21eb5ee
+ * @license Angular v15.2.9+sha-9fe0896
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8331,7 +8331,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.9+sha-21eb5ee');
+const VERSION = new Version('15.2.9+sha-9fe0896');
 
 // This default value is when checking the hierarchy for a token.
 //
