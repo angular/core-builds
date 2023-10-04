4b448c1665ef47ce967bc51f0011c9ee21f14054
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 16:02:19 +0000

    966ce9790a fix(service-worker): throw a critical error when handleFetch fails (#51960)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.7+sha-334fe60
+ * @license Angular v16.2.7+sha-966ce97
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.7+sha-334fe60');
+const VERSION = new Version('16.2.7+sha-966ce97');
 
 // This default value is when checking the hierarchy for a token.
 //
