fa2c2441fee46d02b7f51d2f2d51868629d7c7ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 14:38:35 +0000

    650ab4f218 refactor(core): add support for `on timer` trigger in `@defer` blocks (#51974)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23431,7 +23431,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.6+sha-3bf9b96");
+var VERSION2 = new Version("17.0.0-next.6+sha-650ab4f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
