076cb17a71710bbfe80fa76171a68467cb59441b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 18:47:51 +0000

    dde3fdabbd feat(common): upgrade warning to logged error for lazy-loaded LCP images using NgOptimizedImage (#52004)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23446,7 +23446,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.6+sha-4611b70");
+var VERSION2 = new Version("17.0.0-next.6+sha-dde3fda");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
