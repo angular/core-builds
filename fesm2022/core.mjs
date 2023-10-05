f737882e06ba049c09b7483f8ce5b002c11ce433
Joey Perrott <josephperrott@gmail.com>
2023-10-05 16:50:33 +0000

    2668180cc7 fixup! ci: migrate snapshot publishing from CircleCI to GHA

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-ceaf814
+ * @license Angular v17.0.0-next.7+sha-2668180
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-ceaf814');
+const VERSION = new Version('17.0.0-next.7+sha-2668180');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23446,7 +23446,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-ceaf814");
+var VERSION2 = new Version("17.0.0-next.7+sha-2668180");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
