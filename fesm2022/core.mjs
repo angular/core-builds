092c93e90415c4bdd0be2acc8c41d800293e376c
Joey Perrott <josephperrott@gmail.com>
2023-10-05 16:07:56 +0000

    ceaf814ba1 fixup! ci: migrate snapshot publishing from CircleCI to GHA

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-2a1464e
+ * @license Angular v17.0.0-next.7+sha-ceaf814
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-2a1464e');
+const VERSION = new Version('17.0.0-next.7+sha-ceaf814');
 
 // This default value is when checking the hierarchy for a token.
 //
