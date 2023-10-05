5cf6426bea1004919646828566fca473dc1cd7c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-05 16:58:37 +0000

    5122729c95 ci: migrate saucelabs bazel job to Github Actions (#51945)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.8+sha-481198a
+ * @license Angular v16.2.8+sha-5122729
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.8+sha-481198a');
+const VERSION = new Version('16.2.8+sha-5122729');
 
 // This default value is when checking the hierarchy for a token.
 //
