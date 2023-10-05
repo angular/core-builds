9899f5ff940e8c8f21cd30ba9f8012196979ea8c
Andrew Scott <atscott01@gmail.com>
2023-10-05 17:59:39 +0000

    a3b0891031 docs: update Angular CLI help [main] (#52022)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-f933524
+ * @license Angular v17.0.0-next.7+sha-a3b0891
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10907,7 +10907,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-f933524');
+const VERSION = new Version('17.0.0-next.7+sha-a3b0891');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23446,7 +23446,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-f933524");
+var VERSION2 = new Version("17.0.0-next.7+sha-a3b0891");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
