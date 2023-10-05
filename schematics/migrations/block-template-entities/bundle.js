695275730ef6461a6132a337f2642731b480c46c
Joey Perrott <josephperrott@gmail.com>
2023-10-05 15:35:31 +0000

    2a1464e811 fixup! ci: migrate snapshot publishing from CircleCI to GHA

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23446,7 +23446,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-e9aeaec");
+var VERSION2 = new Version("17.0.0-next.7+sha-2a1464e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
