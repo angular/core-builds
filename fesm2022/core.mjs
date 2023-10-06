51478f4ee9e85c817259ccee37231d8125f5716e
Andrew Scott <atscott01@gmail.com>
2023-10-06 15:40:28 +0000

    f06d97e86d test(core): await all async work to be completed before checking test results (#52064)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-22fa9fe
+ * @license Angular v17.0.0-next.7+sha-f06d97e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10882,7 +10882,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-22fa9fe');
+const VERSION = new Version('17.0.0-next.7+sha-f06d97e');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23506,7 +23506,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-22fa9fe");
+var VERSION2 = new Version("17.0.0-next.7+sha-f06d97e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
