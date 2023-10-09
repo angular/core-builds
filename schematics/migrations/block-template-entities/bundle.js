3813d0b260da9ea90c78214c998dbe6d6d8fd0de
Andrew Scott <atscott01@gmail.com>
2023-10-09 15:13:09 +0000

    753eb2459e build: fix broken symbols test (#52092)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23506,7 +23506,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-00128e3");
+var VERSION2 = new Version("17.0.0-next.7+sha-753eb24");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
