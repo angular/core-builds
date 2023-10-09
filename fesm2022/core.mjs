97a073eae50a5ac30721b8d4defc6af8931aa637
Andrew Scott <atscott01@gmail.com>
2023-10-09 20:33:21 +0000

    aeb7d0395e build: update io_bazel_rules_sass digest to 6725d86 (#51844)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.8+sha-e63e3db
+ * @license Angular v16.2.8+sha-aeb7d03
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10276,7 +10276,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.8+sha-e63e3db');
+const VERSION = new Version('16.2.8+sha-aeb7d03');
 
 // This default value is when checking the hierarchy for a token.
 //
