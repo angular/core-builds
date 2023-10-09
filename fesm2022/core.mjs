98d2978fed9897ebd62292ad4f609b724e6f4e51
Andrew Scott <atscott01@gmail.com>
2023-10-09 17:16:07 +0000

    28a5925f53 fix(common): use === operator to match NgSwitch cases (#51504)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-e25006b
+ * @license Angular v17.0.0-next.7+sha-28a5925
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10361,7 +10361,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-e25006b');
+const VERSION = new Version('17.0.0-next.7+sha-28a5925');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23506,7 +23506,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-e25006b");
+var VERSION2 = new Version("17.0.0-next.7+sha-28a5925");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
