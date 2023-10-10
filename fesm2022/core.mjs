1ef2243f2ed71a3949a0657d57ddcd7ecbf4d67e
Andrew Scott <atscott01@gmail.com>
2023-10-10 17:14:27 +0000

    4da08dc2ef fix(core): remove unnecessary migration (#52141)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-d5dad3e
+ * @license Angular v17.0.0-next.7+sha-4da08dc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10418,7 +10418,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-d5dad3e');
+const VERSION = new Version('17.0.0-next.7+sha-4da08dc');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23637,7 +23637,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-d5dad3e");
+var VERSION2 = new Version("17.0.0-next.7+sha-4da08dc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
