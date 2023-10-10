d0deb24d0dc69576ebe98219158fee94cf51ff9a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-10-10 07:17:59 +0000

    229331e11b build: fix broken symbols test

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-b9a4941
+ * @license Angular v17.0.0-next.7+sha-229331e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10418,7 +10418,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-b9a4941');
+const VERSION = new Version('17.0.0-next.7+sha-229331e');
 
 // This default value is when checking the hierarchy for a token.
 //
