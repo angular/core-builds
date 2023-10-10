549adfbfe83d4030db3c9a0c1212ba337de27e0c
Andrew Scott <atscott01@gmail.com>
2023-10-10 19:48:40 +0000

    1934524 feat(compiler): add docs extraction for type aliases (#52118)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-75d610d
+ * @license Angular v17.0.0-next.7+sha-1934524
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10418,7 +10418,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-75d610d');
+const VERSION = new Version('17.0.0-next.7+sha-1934524');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/standalone-migration/bundle.js
+++ schematics/ng-generate/standalone-migration/bundle.js
@@ -24462,7 +24462,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-75d610d");
+var VERSION2 = new Version("17.0.0-next.7+sha-1934524");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _I18N_ATTR = "i18n";
@@ -25496,7 +25496,7 @@ var MINIMUM_PARTIAL_LINKER_VERSION = "12.0.0";
 function compileDeclareClassMetadata(metadata) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION));
-  definitionMap.set("version", literal("17.0.0-next.7+sha-75d610d"));
+  definitionMap.set("version", literal("17.0.0-next.7+sha-1934524"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", metadata.type);
   definitionMap.set("decorators", metadata.decorators);
@@ -25567,7 +25567,7 @@ function createDirectiveDefinitionMap(meta) {
   const hasTransformFunctions = Object.values(meta.inputs).some((input) => input.transformFunction !== null);
   const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION2 : "14.0.0";
   definitionMap.set("minVersion", literal(minVersion));
-  definitionMap.set("version", literal("17.0.0-next.7+sha-75d610d"));
+  definitionMap.set("version", literal("17.0.0-next.7+sha-1934524"));
   definitionMap.set("type", meta.type.value);
   if (meta.isStandalone) {
     definitionMap.set("isStandalone", literal(meta.isStandalone));
@@ -25799,7 +25799,7 @@ var MINIMUM_PARTIAL_LINKER_VERSION3 = "12.0.0";
 function compileDeclareFactoryFunction(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION3));
-  definitionMap.set("version", literal("17.0.0-next.7+sha-75d610d"));
+  definitionMap.set("version", literal("17.0.0-next.7+sha-1934524"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   definitionMap.set("deps", compileDependencies(meta.deps));
@@ -25822,7 +25822,7 @@ function compileDeclareInjectableFromMetadata(meta) {
 function createInjectableDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION4));
-  definitionMap.set("version", literal("17.0.0-next.7+sha-75d610d"));
+  definitionMap.set("version", literal("17.0.0-next.7+sha-1934524"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.providedIn !== void 0) {
@@ -25860,7 +25860,7 @@ function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION5));
-  definitionMap.set("version", literal("17.0.0-next.7+sha-75d610d"));
+  definitionMap.set("version", literal("17.0.0-next.7+sha-1934524"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   definitionMap.set("providers", meta.providers);
@@ -25884,7 +25884,7 @@ function createNgModuleDefinitionMap(meta) {
     throw new Error("Invalid path! Local compilation mode should not get into the partial compilation path");
   }
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION6));
-  definitionMap.set("version", literal("17.0.0-next.7+sha-75d610d"));
+  definitionMap.set("version", literal("17.0.0-next.7+sha-1934524"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.bootstrap.length > 0) {
@@ -25919,7 +25919,7 @@ function compileDeclarePipeFromMetadata(meta) {
 function createPipeDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION7));
-  definitionMap.set("version", literal("17.0.0-next.7+sha-75d610d"));
+  definitionMap.set("version", literal("17.0.0-next.7+sha-1934524"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.isStandalone) {
@@ -25936,7 +25936,7 @@ function createPipeDefinitionMap(meta) {
 publishFacade(_global);
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
-var VERSION3 = new Version("17.0.0-next.7+sha-75d610d");
+var VERSION3 = new Version("17.0.0-next.7+sha-1934524");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/transformers/api.mjs
 var EmitFlags;
@@ -36476,6 +36476,18 @@ function isSyntheticAngularConstant(declaration) {
   return declaration.name.getText() === "USED_FOR_NG_TYPE_CHECKING";
 }
 
+// bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/docs/src/type_alias_extractor.mjs
+function extractTypeAlias(declaration) {
+  return {
+    name: declaration.name.getText(),
+    type: declaration.type.getText(),
+    entryType: EntryType.TypeAlias,
+    rawComment: extractRawJsDoc(declaration),
+    description: extractJsDocDescription(declaration),
+    jsdocTags: extractJsDocTags(declaration)
+  };
+}
+
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/docs/src/extractor.mjs
 var DocsExtractor = class {
   constructor(typeChecker, metadataReader) {
@@ -36508,6 +36520,9 @@ var DocsExtractor = class {
     }
     if (import_typescript62.default.isVariableDeclaration(node) && !isSyntheticAngularConstant(node)) {
       return extractConstant(node, this.typeChecker);
+    }
+    if (import_typescript62.default.isTypeAliasDeclaration(node)) {
+      return extractTypeAlias(node);
     }
     if (import_typescript62.default.isEnumDeclaration(node)) {
       return extractEnum(node, this.typeChecker);
