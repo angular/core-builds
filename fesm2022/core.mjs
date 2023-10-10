f4cdcb0f6bc060651a5869fdf63d45ec43422973
Andrew Scott <atscott01@gmail.com>
2023-10-10 21:38:59 +0000

    da056a1 fix(common): add missing types field for @angular/common/locales of exports in package.json (#52080)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-11588a1
+ * @license Angular v17.0.0-next.7+sha-da056a1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10418,7 +10418,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-11588a1');
+const VERSION = new Version('17.0.0-next.7+sha-da056a1');
 
 // This default value is when checking the hierarchy for a token.
 //
