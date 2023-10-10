c455c4ea07a12498f23a63d3b9e99ffcf699ece7
Andrew Scott <atscott01@gmail.com>
2023-10-10 19:43:18 +0000

    427705e6c4 docs: remove the developer survey 2023 banner (#52148)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.8+sha-2bfd28c
+ * @license Angular v16.2.8+sha-427705e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10276,7 +10276,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.8+sha-2bfd28c');
+const VERSION = new Version('16.2.8+sha-427705e');
 
 // This default value is when checking the hierarchy for a token.
 //
