227d3b68f1d9754fa6487583a4e5a0645d44e022
Andrew Scott <atscott01@gmail.com>
2023-10-10 17:35:39 +0000

    7c052bb6ef feat(language-service): Support autocompletion for blocks (#52121)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-4da08dc
+ * @license Angular v17.0.0-next.7+sha-7c052bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10418,7 +10418,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-4da08dc');
+const VERSION = new Version('17.0.0-next.7+sha-7c052bb');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23637,7 +23637,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.7+sha-4da08dc");
+var VERSION2 = new Version("17.0.0-next.7+sha-7c052bb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
