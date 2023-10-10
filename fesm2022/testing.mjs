6de265fa2849bef1c3fb639f56c036777227140e
Andrew Scott <atscott01@gmail.com>
2023-10-10 21:07:32 +0000

    7d8b3f4968 build: update payload sizes to fix pre-existing failures (#52149)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.8+sha-427705e
+ * @license Angular v16.2.8+sha-7d8b3f4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.8+sha-427705e');
+const VERSION = new Version('16.2.8+sha-7d8b3f4');
 
 // This default value is when checking the hierarchy for a token.
 //
