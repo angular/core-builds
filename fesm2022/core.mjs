4c929eee73586159d26e4ab1d45a5227e3309844
Andrew Scott <atscott01@gmail.com>
2023-10-11 19:42:42 +0000

    c2b1a24 refactor(compiler-cli): remove `MethodIdentifier` type (#49611)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-221f882
+ * @license Angular v17.1.0-next.0+sha-c2b1a24
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10429,7 +10429,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-221f882');
+const VERSION = new Version('17.1.0-next.0+sha-c2b1a24');
 
 // This default value is when checking the hierarchy for a token.
 //
