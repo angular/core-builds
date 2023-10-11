2314eb975bde88dc54e6fa755d19d4bfa4d3a39f
Andrew Scott <atscott01@gmail.com>
2023-10-11 18:39:15 +0000

    0198d21 fix(compiler): apply style on :host attributes in prod builds. (#49118)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-12aeed8
+ * @license Angular v17.1.0-next.0+sha-0198d21
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10429,7 +10429,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-12aeed8');
+const VERSION = new Version('17.1.0-next.0+sha-0198d21');
 
 // This default value is when checking the hierarchy for a token.
 //
