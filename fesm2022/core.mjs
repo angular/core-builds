616cd47657b3a964de3f0bad7b62b201d18aa985
Andrew Scott <atscott01@gmail.com>
2023-10-11 17:12:57 +0000

    44a75aa docs: release notes for the v16.2.9 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.7+sha-87340a4
+ * @license Angular v17.0.0-next.7+sha-44a75aa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10429,7 +10429,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.7+sha-87340a4');
+const VERSION = new Version('17.0.0-next.7+sha-44a75aa');
 
 // This default value is when checking the hierarchy for a token.
 //
