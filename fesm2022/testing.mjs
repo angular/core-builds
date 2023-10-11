7033b4de0a77e34014f21b34eaadb9c89a8420e4
Andrew Scott <atscott01@gmail.com>
2023-10-11 16:21:54 +0000

    fcdae578fe docs(animations): fix typo in state name of keyframes example (#52155)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.8+sha-51a5baa
+ * @license Angular v16.2.8+sha-fcdae57
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.8+sha-51a5baa');
+const VERSION = new Version('16.2.8+sha-fcdae57');
 
 // This default value is when checking the hierarchy for a token.
 //
