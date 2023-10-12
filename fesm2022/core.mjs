bc0ece48ce17eb157c7ed5cf24569101b9c66ea3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 15:51:03 +0000

    5c70148 docs: add Nicolas Frizzarin to GDE resources (#52003)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-65b4604
+ * @license Angular v17.0.0-next.8+sha-5c70148
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10429,7 +10429,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-65b4604');
+const VERSION = new Version('17.0.0-next.8+sha-5c70148');
 
 // This default value is when checking the hierarchy for a token.
 //
