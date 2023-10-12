1417a1e9e2cf8e141227290af5f8cdcd9e853ca1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 17:34:17 +0000

    99e7629 fix(core): do not remove used ng-template nodes in control flow migration (#52186)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-ffe9b1f
+ * @license Angular v17.0.0-next.8+sha-99e7629
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10429,7 +10429,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-ffe9b1f');
+const VERSION = new Version('17.0.0-next.8+sha-99e7629');
 
 // This default value is when checking the hierarchy for a token.
 //
