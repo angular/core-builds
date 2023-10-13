1adbb23d9542ab3d3b0d20c066bbe8f79b42dab3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 11:53:18 +0000

    030a68b refactor(router): Introduce `StateManager` interface (#52171)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-6fd863d
+ * @license Angular v17.0.0-next.8+sha-030a68b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10429,7 +10429,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-6fd863d');
+const VERSION = new Version('17.0.0-next.8+sha-030a68b');
 
 // This default value is when checking the hierarchy for a token.
 //
