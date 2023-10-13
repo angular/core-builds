f640c04d85917256934cf9462819285e80fbb737
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 09:20:59 +0000

    630e906747 docs(router): add clarification for behavior of paramsInheritanceStrategy: 'emptyOnly' (#52170)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.9+sha-25788b2
+ * @license Angular v16.2.9+sha-630e906
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10276,7 +10276,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.9+sha-25788b2');
+const VERSION = new Version('16.2.9+sha-630e906');
 
 // This default value is when checking the hierarchy for a token.
 //
