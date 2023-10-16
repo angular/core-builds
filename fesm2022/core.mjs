d6eda5da6cd9091155920279b4df37c44e0ce2c0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 16:41:14 +0000

    2003caf fix(core): handle if alias in control flow migration (#52181)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-302ab34
+ * @license Angular v17.1.0-next.0+sha-2003caf
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10417,7 +10417,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-302ab34');
+const VERSION = new Version('17.1.0-next.0+sha-2003caf');
 
 // This default value is when checking the hierarchy for a token.
 //
