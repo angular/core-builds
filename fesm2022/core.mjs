3dbfab35c0c28e15b19959e416dcd5818f100708
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-17 10:35:07 +0000

    a318e6a refactor(compiler): extract generic info for api reference (#52204)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-c9cde3a
+ * @license Angular v17.0.0-next.8+sha-a318e6a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10417,7 +10417,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-next.8+sha-c9cde3a');
+const VERSION = new Version('17.0.0-next.8+sha-a318e6a');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23674,7 +23674,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-next.8+sha-c9cde3a");
+var VERSION2 = new Version("17.0.0-next.8+sha-a318e6a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
