6ce86764a4a0f05e499af97db1953eeeece8136b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 09:36:16 +0000

    06ce49aeeb docs: fix typo (#52247)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.9+sha-d6070af
+ * @license Angular v16.2.9+sha-06ce49a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.9+sha-d6070af');
+const VERSION = new Version('16.2.9+sha-06ce49a');
 
 // This default value is when checking the hierarchy for a token.
 //
