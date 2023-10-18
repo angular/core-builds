22747f94a742b025d7feaf857035206a08084fce
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 16:21:42 +0000

    b4c4b42222 docs: Update BAZEL.md with correct bazelrc documentation link (#52090) PR Close #52090

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.9+sha-55e41d8
+ * @license Angular v16.2.9+sha-b4c4b42
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10276,7 +10276,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.9+sha-55e41d8');
+const VERSION = new Version('16.2.9+sha-b4c4b42');
 
 // This default value is when checking the hierarchy for a token.
 //
