73c1e4ad09e1ebc9693a7bf0820102dce11a030a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 09:40:57 +0000

    1640743 Revert "perf(platform-browser): disable styles of removed components instead of removing (#51808)" (#52238)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-6fefbe8
+ * @license Angular v17.1.0-next.0+sha-1640743
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10417,7 +10417,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-6fefbe8');
+const VERSION = new Version('17.1.0-next.0+sha-1640743');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23674,7 +23674,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-6fefbe8");
+var VERSION2 = new Version("17.1.0-next.0+sha-1640743");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
