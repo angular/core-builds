c5e402121560b4c7874797868ea681ce5095295c
Dylan Hunn <dylhunn@gmail.com>
2023-10-18 18:58:30 +0000

    866d0f4 refactor(core): remove test.ts files in integration tests (#52109)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-edd060e
+ * @license Angular v17.0.0-rc.0+sha-866d0f4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10417,7 +10417,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-edd060e');
+const VERSION = new Version('17.0.0-rc.0+sha-866d0f4');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -23797,7 +23797,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-rc.0+sha-edd060e");
+var VERSION2 = new Version("17.0.0-rc.0+sha-866d0f4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
