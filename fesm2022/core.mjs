13f6249c1784133f84aef3389cf74327a7ab6d1a
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 16:45:18 +0000

    0b4c013 refactor(common): use performance API for NgOptimizedImage feature logging (#52205)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-a207421
+ * @license Angular v17.0.0-rc.0+sha-0b4c013
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10417,7 +10417,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-a207421');
+const VERSION = new Version('17.0.0-rc.0+sha-0b4c013');
 
 // This default value is when checking the hierarchy for a token.
 //
