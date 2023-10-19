bb4c5138ee39b765a01508400ff6ce2f7ba3d1c3
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 17:32:16 +0000

    3278966 fix(router): Ensure newly resolved data is inherited by child routes (#52167)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-8fff07c
+ * @license Angular v17.1.0-next.0+sha-3278966
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10417,7 +10417,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-8fff07c');
+const VERSION = new Version('17.1.0-next.0+sha-3278966');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23797,7 +23797,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-8fff07c");
+var VERSION2 = new Version("17.1.0-next.0+sha-3278966");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
