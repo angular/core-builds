4de095fe2b5de96a481a88714ee384e95d237272
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 16:29:55 +0000

    36873596be docs: remove test.ts file in aio (#52327)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.10+sha-8239a8f
+ * @license Angular v16.2.10+sha-3687359
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10925,7 +10925,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.10+sha-8239a8f');
+const VERSION = new Version('16.2.10+sha-3687359');
 
 // This default value is when checking the hierarchy for a token.
 //
