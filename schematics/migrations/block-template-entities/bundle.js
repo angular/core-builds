e6f9a7971e57c415f5524179c7708926275d5cf3
Dylan Hunn <github@dylanhunn.com>
2023-10-25 21:35:15 +0000

    4ca2cea docs: release notes for the v16.2.11 release

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24202,7 +24202,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-0037c21");
+var VERSION2 = new Version("17.1.0-next.0+sha-4ca2cea");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
