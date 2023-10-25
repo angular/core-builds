6f11d3619490551526d56ed95e98792e39d297e8
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 21:47:04 +0000

    16bf04b release: cut the zone.js-0.14.1 release (#52374)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-4ca2cea
+ * @license Angular v17.1.0-next.0+sha-16bf04b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10426,7 +10426,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-4ca2cea');
+const VERSION = new Version('17.1.0-next.0+sha-16bf04b');
 
 // This default value is when checking the hierarchy for a token.
 //
