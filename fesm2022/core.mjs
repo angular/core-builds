d8325e66802d3246182a4222b2a84c13b2cc6099
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 23:44:47 +0000

    57630c3 docs: fixed typos (#52297)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.1+sha-40f7536
+ * @license Angular v17.0.0-rc.1+sha-57630c3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10426,7 +10426,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.1+sha-40f7536');
+const VERSION = new Version('17.0.0-rc.1+sha-57630c3');
 
 // This default value is when checking the hierarchy for a token.
 //
