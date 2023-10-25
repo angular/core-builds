42d8d428e95c13b5a84df0946023544ebaae9da5
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 21:46:12 +0000

    40f7536 release: cut the zone.js-0.14.1 release (#52374)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24202,7 +24202,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-rc.0+sha-6858149");
+var VERSION2 = new Version("17.0.0-rc.1+sha-40f7536");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
