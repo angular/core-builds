0ab72d98fae87083c312ea2e7c7b0b926e0e0a78
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 18:09:56 +0000

    6858149 fix(router): `RouterTestingHarness` should throw if a component is expected but navigation fails (#52357)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.0+sha-a803c89
+ * @license Angular v17.0.0-rc.0+sha-6858149
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10426,7 +10426,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.0+sha-a803c89');
+const VERSION = new Version('17.0.0-rc.0+sha-6858149');
 
 // This default value is when checking the hierarchy for a token.
 //
