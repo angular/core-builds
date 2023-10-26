32ca8a79acb6adebe7573af59798bf6a3a4c5134
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-26 19:37:08 +0000

    cb8a741 docs: update SSR doc to reflect version 17 (#52398)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-f1a020b
+ * @license Angular v17.1.0-next.0+sha-cb8a741
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10426,7 +10426,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-f1a020b');
+const VERSION = new Version('17.1.0-next.0+sha-cb8a741');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24202,7 +24202,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-f1a020b");
+var VERSION2 = new Version("17.1.0-next.0+sha-cb8a741");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
