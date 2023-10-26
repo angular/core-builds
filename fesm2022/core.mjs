314c24f3bec6bcfeee1eb019e708bde5f787e3a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-26 21:18:11 +0000

    7888819 build: update cross-repo angular dependencies (#52191)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-cb8a741
+ * @license Angular v17.1.0-next.0+sha-7888819
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10426,7 +10426,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-cb8a741');
+const VERSION = new Version('17.1.0-next.0+sha-7888819');
 
 // This default value is when checking the hierarchy for a token.
 //
