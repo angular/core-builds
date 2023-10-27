321f46c5a2406f7b842de6c5a08c2abede2fed93
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 23:12:44 +0000

    b0bc25d refactor(localize): mark `TargetMessage` and `MessageId` as public. (#52418)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-07623ca
+ * @license Angular v17.1.0-next.0+sha-b0bc25d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10426,7 +10426,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-07623ca');
+const VERSION = new Version('17.1.0-next.0+sha-b0bc25d');
 
 // This default value is when checking the hierarchy for a token.
 //
