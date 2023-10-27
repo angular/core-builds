66c39c0ef297c87f6d2b166b980f63aafb1921e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 23:12:04 +0000

    8e17dd7 refactor(localize): mark `TargetMessage` and `MessageId` as public. (#52418)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24202,7 +24202,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-rc.1+sha-57b9798");
+var VERSION2 = new Version("17.0.0-rc.1+sha-8e17dd7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
