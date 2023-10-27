40736e597b7f3bf3d3bf9a45cf8aaaf4b0035301
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:55:54 +0000

    a51bb6927d docs(docs-infra): fix typo in tap into the observable tutorial example (#52359)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.11+sha-5ff99f2
+ * @license Angular v16.2.11+sha-a51bb69
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10947,7 +10947,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.11+sha-5ff99f2');
+const VERSION = new Version('16.2.11+sha-a51bb69');
 
 // This default value is when checking the hierarchy for a token.
 //
