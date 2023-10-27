a32ebc6d2c76d8688b52d0a571c8fec94f17e19e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 23:24:00 +0000

    6070c9d fix(core): handle trackBy and aliased index in control flow migration (#52423)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-b0bc25d
+ * @license Angular v17.1.0-next.0+sha-6070c9d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10426,7 +10426,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-b0bc25d');
+const VERSION = new Version('17.1.0-next.0+sha-6070c9d');
 
 // This default value is when checking the hierarchy for a token.
 //
