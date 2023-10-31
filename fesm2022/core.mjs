90c4ebc37394a27e9e1468b621b9ca96ef657af7
Andrew Scott <atscott01@gmail.com>
2023-10-31 19:04:30 +0000

    df0902d212 docs: update home page for live stream event (#52456)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.11+sha-a51bb69
+ * @license Angular v16.2.11+sha-df0902d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10298,7 +10298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.11+sha-a51bb69');
+const VERSION = new Version('16.2.11+sha-df0902d');
 
 // This default value is when checking the hierarchy for a token.
 //
