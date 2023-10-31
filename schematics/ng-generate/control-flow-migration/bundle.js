07300e288072625f3fcb8b213b7b9da017ffbc1c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 21:26:41 +0000

    696f003 fix(core): mutation bug in `getDependenciesFromInjectable` (#52450)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24491,7 +24491,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-rc.1+sha-b9ea2d6");
+var VERSION2 = new Version("17.0.0-rc.1+sha-696f003");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
