9ce8117b87f3a49df3406beb5f204a09c159bdc7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 20:05:25 +0000

    e355f05 refactor(language-service): Add quick info for built in control flow/blocks (#52386)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.1+sha-595009e
+ * @license Angular v17.0.0-rc.1+sha-e355f05
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10444,7 +10444,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.1+sha-595009e');
+const VERSION = new Version('17.0.0-rc.1+sha-e355f05');
 
 // This default value is when checking the hierarchy for a token.
 //
