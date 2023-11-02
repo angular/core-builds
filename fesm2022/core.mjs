180153779905929282ec824f29f657a89075a9df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 17:38:22 +0000

    0045f02 build: update actions/checkout digest to b4ffde6 (#52252)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-664099b
+ * @license Angular v17.1.0-next.0+sha-0045f02
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10445,7 +10445,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-664099b');
+const VERSION = new Version('17.1.0-next.0+sha-0045f02');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24538,7 +24538,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-664099b");
+var VERSION2 = new Version("17.1.0-next.0+sha-0045f02");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
