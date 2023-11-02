34da26f47d0d16dc2a1bdcd90993507f6873dcbc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 18:10:31 +0000

    e092184a5c fix(common): apply fixed_srcset_width values only to fixed srcsets (#52486)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.11+sha-64e4835
+ * @license Angular v16.2.11+sha-e092184
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10947,7 +10947,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.11+sha-64e4835');
+const VERSION = new Version('16.2.11+sha-e092184');
 
 // This default value is when checking the hierarchy for a token.
 //
