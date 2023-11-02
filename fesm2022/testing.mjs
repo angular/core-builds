71c101c570bd40d25928c2a2928ecb580403ad32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 17:35:56 +0000

    03f4050636 fix(animations): remove `finish` listener once player is destroyed (#51136)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.11+sha-07efb49
+ * @license Angular v16.2.11+sha-03f4050
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10947,7 +10947,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.2.11+sha-07efb49');
+const VERSION = new Version('16.2.11+sha-03f4050');
 
 // This default value is when checking the hierarchy for a token.
 //
