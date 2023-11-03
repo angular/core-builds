536ae040f087c4a664de0f075f6f683be51fb4ac
Andrew Scott <atscott01@gmail.com>
2023-11-03 17:59:34 +0000

    1a33a2c release: cut the zone.js-0.14.2 release (#52508)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-6033504
+ * @license Angular v17.1.0-next.0+sha-1a33a2c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10425,7 +10425,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-6033504');
+const VERSION = new Version('17.1.0-next.0+sha-1a33a2c');
 
 // This default value is when checking the hierarchy for a token.
 //
