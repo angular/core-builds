a55f148c5f92ae821be04ece27e02209055210a1
Andrew Scott <atscott01@gmail.com>
2023-11-04 00:43:46 +0000

    1c92609 test(devtools): fix transformInjectorResolutionPathsIntoTree tests (#52489)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-rc.2+sha-de13610
+ * @license Angular v17.0.0-rc.2+sha-1c92609
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10425,7 +10425,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0-rc.2+sha-de13610');
+const VERSION = new Version('17.0.0-rc.2+sha-1c92609');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24548,7 +24548,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-rc.2+sha-de13610");
+var VERSION2 = new Version("17.0.0-rc.2+sha-1c92609");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
