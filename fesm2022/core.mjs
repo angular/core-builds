51aca645c3fa3466d0096ec5a94940c927ae8fbf
Andrew Scott <atscott01@gmail.com>
2023-11-06 20:38:40 +0000

    d01f371 refactor(devtools): update Angular logo for extension and loading animation (#52546)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-699ae60
+ * @license Angular v17.1.0-next.0+sha-d01f371
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10425,7 +10425,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-699ae60');
+const VERSION = new Version('17.1.0-next.0+sha-d01f371');
 
 // This default value is when checking the hierarchy for a token.
 //
