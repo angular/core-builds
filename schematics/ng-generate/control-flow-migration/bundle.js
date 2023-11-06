11a74c70f4a175c23e3bfba269e114acc45dbb8c
Andrew Scott <atscott@google.com>
2023-11-06 19:20:34 +0000

    651c347 docs: add angular.dev (#52536)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24663,7 +24663,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-8bbec83");
+var VERSION2 = new Version("17.1.0-next.0+sha-651c347");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
