a01d213d347e95b38ec2ec27d0e2b568d6c4e241
Andrew Scott <atscott01@gmail.com>
2023-11-06 19:09:20 +0000

    2c3daeb docs: add angular.dev banner (#52535) PR Close #52535

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24663,7 +24663,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0-rc.2+sha-91dbcb5");
+var VERSION2 = new Version("17.0.0-rc.2+sha-2c3daeb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
