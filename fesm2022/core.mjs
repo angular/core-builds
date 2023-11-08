9bec8af34f7b9c6d2f55112839137f3a37ba9c01
Andrew Scott <atscott01@gmail.com>
2023-11-08 17:28:56 +0000

    a46c8cb fix(docs-infra): fix menu icon size (#52689)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0+sha-1d31a0c
+ * @license Angular v17.0.0+sha-a46c8cb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10425,7 +10425,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.0+sha-1d31a0c');
+const VERSION = new Version('17.0.0+sha-a46c8cb');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24750,7 +24750,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0+sha-1d31a0c");
+var VERSION2 = new Version("17.0.0+sha-a46c8cb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
