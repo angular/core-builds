9ad8e04793fccf633876f273086b5ce04e1dabb8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 15:06:58 +0000

    33ae659 build: update aio to 17.0.0 (#52635)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-5ebb361
+ * @license Angular v17.1.0-next.0+sha-33ae659
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10425,7 +10425,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-5ebb361');
+const VERSION = new Version('17.1.0-next.0+sha-33ae659');
 
 // This default value is when checking the hierarchy for a token.
 //
