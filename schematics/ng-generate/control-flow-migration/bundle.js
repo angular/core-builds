ff9694f22773514183772bb3137cfafd403408a0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 16:10:33 +0000

    c30dde2 build: bump in-memory-web-api to v17 (#52676)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24761,7 +24761,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.0+sha-da429c5");
+var VERSION2 = new Version("17.0.0+sha-c30dde2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
