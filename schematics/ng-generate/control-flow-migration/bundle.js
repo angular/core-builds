f8dd903d820900e8c405ed90b386f924a83f174b
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-08 18:47:55 +0000

    291ba38 fix(http): Don't override the backend when using the InMemoryWebAPI (#52425)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24761,7 +24761,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-12f979d");
+var VERSION2 = new Version("17.1.0-next.0+sha-291ba38");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
