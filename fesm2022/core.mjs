8fb58156c98aec51bc7a6e639f9ad26ad262b6b7
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:37:06 +0000

    90018f9 docs: add missing backtick on title (#52711)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.1+sha-158b198
+ * @license Angular v17.0.1+sha-90018f9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10423,7 +10423,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.1+sha-158b198');
+const VERSION = new Version('17.0.1+sha-90018f9');
 
 // This default value is when checking the hierarchy for a token.
 //
