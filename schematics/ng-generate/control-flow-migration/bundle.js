80fa8cdb8ab96289e5bf8bf32ef38e033e1b62fe
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:45:54 +0000

    cfb42ee docs: add extended diagnostic NG8109 (#52721)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24761,7 +24761,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.1+sha-90018f9");
+var VERSION2 = new Version("17.0.1+sha-cfb42ee");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
