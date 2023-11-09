b2f17b8d182c06f531e7de864646e0b55fbe5f54
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-11-09 20:15:05 +0000

    01efb1b release: cut the v17.0.2 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.1+sha-b16fc26
+ * @license Angular v17.0.2+sha-01efb1b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10423,7 +10423,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.1+sha-b16fc26');
+const VERSION = new Version('17.0.2+sha-01efb1b');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24750,7 +24750,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.1+sha-b16fc26");
+var VERSION2 = new Version("17.0.2+sha-01efb1b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
