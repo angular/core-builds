f3373f9c4e3dfe8ea9e7bed8f0255a9d6efa38ef
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:44:40 +0000

    154f286 docs: add extended diagnostic NG8109 (#52721)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-867161f
+ * @license Angular v17.1.0-next.0+sha-154f286
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10423,7 +10423,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-867161f');
+const VERSION = new Version('17.1.0-next.0+sha-154f286');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24750,7 +24750,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-867161f");
+var VERSION2 = new Version("17.1.0-next.0+sha-154f286");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
