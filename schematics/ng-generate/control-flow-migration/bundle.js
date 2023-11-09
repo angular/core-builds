aa8d70ecc547c72740ce5bbe3314ab5c8b992abe
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 15:07:07 +0000

    3d98a44 docs: fix incorrect tag reference (#52586)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24761,7 +24761,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-9cac4ee");
+var VERSION2 = new Version("17.1.0-next.0+sha-3d98a44");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
