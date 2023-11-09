25c2ee66cd4bbb89d92a26c38dcad804e0fc98bf
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:10:42 +0000

    27bf371 docs: removed the broken link (#52743)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24750,7 +24750,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.1+sha-56ec37d");
+var VERSION2 = new Version("17.0.1+sha-27bf371");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
