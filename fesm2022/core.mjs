60e8e735373bf5dc37dde52af2234c678c482828
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 15:43:08 +0000

    b1cc092 docs(docs-infra): improve decorator deprecated property display in documentation (#52041)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-3d98a44
+ * @license Angular v17.1.0-next.0+sha-b1cc092
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10425,7 +10425,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-3d98a44');
+const VERSION = new Version('17.1.0-next.0+sha-b1cc092');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24750,7 +24750,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-3d98a44");
+var VERSION2 = new Version("17.1.0-next.0+sha-b1cc092");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
