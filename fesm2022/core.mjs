a9aeca34444bf481b032a202a40f1e8ac75bd2f3
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 23:32:07 +0000

    561cb1d docs: fix provideRouter typo (#52798)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-d033540
+ * @license Angular v17.0.2+sha-561cb1d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10428,7 +10428,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-d033540');
+const VERSION = new Version('17.0.2+sha-561cb1d');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24770,7 +24770,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.2+sha-d033540");
+var VERSION2 = new Version("17.0.2+sha-561cb1d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
