b1150b876e46c2f0ba049899de11180d37880ea3
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 16:35:59 +0000

    f5872c9 fix(animations): prevent the AsyncAnimationRenderer from calling the delegate when there is no element. (#52570)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-7c066a4
+ * @license Angular v17.0.2+sha-f5872c9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10423,7 +10423,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-7c066a4');
+const VERSION = new Version('17.0.2+sha-f5872c9');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24770,7 +24770,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.2+sha-7c066a4");
+var VERSION2 = new Version("17.0.2+sha-f5872c9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
