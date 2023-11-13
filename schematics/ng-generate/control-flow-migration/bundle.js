0d6decbfc189153f1f7c014fe2ef2a5305be71e4
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 19:03:34 +0000

    a00beec docs(forms): replace fb with formBuilder (#52795)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24770,7 +24770,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.2+sha-76aba9e");
+var VERSION2 = new Version("17.0.2+sha-a00beec");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
