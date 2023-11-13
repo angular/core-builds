d30e995949124f55c361d0d3a2b40b19c63683b7
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 16:00:49 +0000

    e46162d docs: update readme links to target angular.dev documentation (#52772)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-e1f84a3
+ * @license Angular v17.0.2+sha-e46162d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10423,7 +10423,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-e1f84a3');
+const VERSION = new Version('17.0.2+sha-e46162d');
 
 // This default value is when checking the hierarchy for a token.
 //
