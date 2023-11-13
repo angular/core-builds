63c8dfe824494b7360123490e5d7db9875a1b211
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 16:35:24 +0000

    5ee11a7 fix(animations): prevent the AsyncAnimationRenderer from calling the delegate when there is no element. (#52570)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24770,7 +24770,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-cf86ae5");
+var VERSION2 = new Version("17.1.0-next.0+sha-5ee11a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
