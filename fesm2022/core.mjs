25aa338bf577269d807eec9e6bbf1a6757394ea1
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 15:28:00 +0000

    e1f84a3 fix(migrations): passed in paths will be respected in nx workspaces (#52796)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-54a7b33
+ * @license Angular v17.0.2+sha-e1f84a3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10423,7 +10423,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-54a7b33');
+const VERSION = new Version('17.0.2+sha-e1f84a3');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24839,7 +24839,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.2+sha-54a7b33");
+var VERSION2 = new Version("17.0.2+sha-e1f84a3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
