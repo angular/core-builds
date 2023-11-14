87e9de6965228d5af8157414c99f87884306168f
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 19:21:54 +0000

    faaac05 docs: fix and improve @defer testing docs for adev (#52874)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-7d00175
+ * @license Angular v17.1.0-next.0+sha-faaac05
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10428,7 +10428,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.0+sha-7d00175');
+const VERSION = new Version('17.1.0-next.0+sha-faaac05');
 
 // This default value is when checking the hierarchy for a token.
 //
