abaa99a1f912e5b29b1d715f82ca7f6a3833875c
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 18:38:58 +0000

    7d00175 docs: fix grammar in guide/defer (#52899)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24770,7 +24770,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-7f9ab9d");
+var VERSION2 = new Version("17.1.0-next.0+sha-7d00175");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
