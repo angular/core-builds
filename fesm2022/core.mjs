f2ab623ba87d6fde018cbdb8e0e1efb18c1b1603
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 22:06:17 +0000

    a523801 docs: fixed unexpected closing tags for Receipt (#52914)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-290e26a
+ * @license Angular v17.0.2+sha-a523801
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10428,7 +10428,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-290e26a');
+const VERSION = new Version('17.0.2+sha-a523801');
 
 // This default value is when checking the hierarchy for a token.
 //
