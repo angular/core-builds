ec65ef160b8c157d579dd98baa1f8a7944cd6150
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-11-15 20:31:02 +0000

    179304b docs: release notes for the v17.0.3 release

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24770,7 +24770,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-e639bf4");
+var VERSION2 = new Version("17.1.0-next.0+sha-179304b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
