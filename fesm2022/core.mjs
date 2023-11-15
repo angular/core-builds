7403589ede8a41b3763e25ec3d99d091dd5f4038
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-15 20:22:56 +0000

    151a544 release: bump DevTools version to 1.0.9 (#52938)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.2+sha-4e200bf
+ * @license Angular v17.0.2+sha-151a544
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10428,7 +10428,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.2+sha-4e200bf');
+const VERSION = new Version('17.0.2+sha-151a544');
 
 // This default value is when checking the hierarchy for a token.
 //
