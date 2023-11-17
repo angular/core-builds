84b80de6657010a69f42e33fcc92d3746040a137
Andrew Kushnir <akushnir@google.com>
2023-11-17 20:23:11 +0000

    6c68f4c docs: fix broken link for view encapsulation (#52995)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24863,7 +24863,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.3+sha-6df5f0e");
+var VERSION2 = new Version("17.0.3+sha-6c68f4c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
