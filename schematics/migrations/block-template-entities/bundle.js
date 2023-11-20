a9cbab334dd1f9f76573cd8fd587c46097467f9d
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-11-20 19:52:19 +0000

    2ecda9f docs: release notes for the v17.0.4 release

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24976,7 +24976,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.0+sha-e090b48");
+var VERSION2 = new Version("17.1.0-next.0+sha-2ecda9f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
