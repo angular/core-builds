fbe1ca11cd32c53973eca4d1b8f66e61a57abe4e
Andrew Kushnir <akushnir@google.com>
2023-11-20 20:13:02 +0000

    29e0834 fix(router): Resolvers in different parts of the route tree should be able to execute together (#52934)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24907,7 +24907,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.1+sha-33959f4");
+var VERSION2 = new Version("17.1.0-next.1+sha-29e0834");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
