359e9a8c47031b11937c955f20dcf276eff44a9c
Andrew Kushnir <akushnir@google.com>
2023-11-21 18:49:53 +0000

    c62b2da build: update to TypeScript 5.3 final (#53087)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.1+sha-cb2db54
+ * @license Angular v17.1.0-next.1+sha-c62b2da
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.1+sha-cb2db54');
+const VERSION = new Version('17.1.0-next.1+sha-c62b2da');
 
 // This default value is when checking the hierarchy for a token.
 //
