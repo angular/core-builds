5d5474c9db3b4f6d3a963b85d3ae7d88a90a0b0e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 09:53:18 +0000

    6d61f2d build: update dependency karma-jasmine-html-reporter to v2 (#52544)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-935fa8f
+ * @license Angular v17.0.4+sha-6d61f2d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-935fa8f');
+const VERSION = new Version('17.0.4+sha-6d61f2d');
 
 // This default value is when checking the hierarchy for a token.
 //
