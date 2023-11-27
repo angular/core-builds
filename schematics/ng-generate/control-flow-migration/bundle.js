b1fdcbf986800e40444436677eab59c5b1cbbb83
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 14:54:20 +0000

    115dc23 build: upgrade node version for .devcontainer example file (#53157)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24907,7 +24907,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.4+sha-4604be2");
+var VERSION2 = new Version("17.0.4+sha-115dc23");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
