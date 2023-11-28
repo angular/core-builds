192165360b7aa7b16fce82effb2e8c2d82a6685e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 18:33:35 +0000

    df01a5c test: refactor platform-server integration tests to use application builder (#53219)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-ba6d7fe
+ * @license Angular v17.0.4+sha-df01a5c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-ba6d7fe');
+const VERSION = new Version('17.0.4+sha-df01a5c');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24907,7 +24907,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.4+sha-ba6d7fe");
+var VERSION2 = new Version("17.0.4+sha-df01a5c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
