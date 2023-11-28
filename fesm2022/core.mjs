b9805500786c1ad649557e37cf9807f7bd8b10ef
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 10:11:36 +0000

    92a28a7 build: update dependency jsdom to v23 (#53194)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-cf3de83
+ * @license Angular v17.0.4+sha-92a28a7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.4+sha-cf3de83');
+const VERSION = new Version('17.0.4+sha-92a28a7');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24907,7 +24907,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.4+sha-cf3de83");
+var VERSION2 = new Version("17.0.4+sha-92a28a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
