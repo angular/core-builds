1e74c60b018c0f8c2d039747a67186001a21fc94
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 10:04:56 +0000

    cf3de83 docs: fix recurring typo in adev (#53172)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24907,7 +24907,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.4+sha-115dc23");
+var VERSION2 = new Version("17.0.4+sha-cf3de83");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
