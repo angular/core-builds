79c5c4bd5a1b000fe15b9514f2c96cdb2b902cc2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 13:07:54 +0000

    53912fd fix(migrations): allows colons in ngIf else cases to migrate (#53076)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.1+sha-4c1d69e
+ * @license Angular v17.1.0-next.1+sha-53912fd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.1+sha-4c1d69e');
+const VERSION = new Version('17.1.0-next.1+sha-53912fd');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -24978,7 +24978,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.1+sha-4c1d69e");
+var VERSION2 = new Version("17.1.0-next.1+sha-53912fd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
