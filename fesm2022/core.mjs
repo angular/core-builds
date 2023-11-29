bd4618cd315801ba79ca9d6d70d775b9aaca132b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 09:36:22 +0000

    c9f8e75 test(compiler): Update golden partial file (#53209)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.1+sha-58cf389
+ * @license Angular v17.1.0-next.1+sha-c9f8e75
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.1+sha-58cf389');
+const VERSION = new Version('17.1.0-next.1+sha-c9f8e75');
 
 // This default value is when checking the hierarchy for a token.
 //
