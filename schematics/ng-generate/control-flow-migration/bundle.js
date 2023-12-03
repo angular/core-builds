febeeb1dab214b45df1ec971412105c29170cf7a
Dylan Hunn <dylhunn@gmail.com>
2023-12-03 23:01:55 +0000

    d4b4236 Revert "fix(router): Ensure canMatch guards run on wildcard routes (#53239)" (#53339)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -24944,7 +24944,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.2+sha-acd6100");
+var VERSION2 = new Version("17.1.0-next.2+sha-d4b4236");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
