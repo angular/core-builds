cb934397fbf61ec7ff060eaede5fb6f5893dd84c
Dylan Hunn <dylhunn@gmail.com>
2023-12-04 22:32:07 +0000

    dc27bea test(compiler): Update golden partial file (#53327)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.5+sha-ac8eb5b
+ * @license Angular v17.0.5+sha-dc27bea
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.5+sha-ac8eb5b');
+const VERSION = new Version('17.0.5+sha-dc27bea');
 
 // This default value is when checking the hierarchy for a token.
 //
