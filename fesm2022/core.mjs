4f1df5bdf13a0e26db81607fd78f67e069d6b2ff
Dylan Hunn <dylhunn@gmail.com>
2023-12-06 01:18:28 +0000

    05f9c7b refactor(compiler): Initial support for i18n attributes (#53341)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.2+sha-6cd91c6
+ * @license Angular v17.1.0-next.2+sha-05f9c7b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.2+sha-6cd91c6');
+const VERSION = new Version('17.1.0-next.2+sha-05f9c7b');
 
 // This default value is when checking the hierarchy for a token.
 //
