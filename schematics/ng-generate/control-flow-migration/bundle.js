bd3f2bb9449e8d5d400c821daf368745266ff931
Dylan Hunn <github@dylanhunn.com>
2023-12-06 18:22:25 +0000

    423692a docs: release notes for the v17.0.6 release

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25262,7 +25262,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.2+sha-a1e3485");
+var VERSION2 = new Version("17.1.0-next.2+sha-423692a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
