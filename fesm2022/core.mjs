207c7a03e69980b1eb8062ca8870ad74e65d882d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-07 21:26:06 +0000

    299eae4 Revert "refactor: reduce direct babel dependencies (#53374)" (#53432)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-2565121
+ * @license Angular v17.1.0-next.3+sha-299eae4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-2565121');
+const VERSION = new Version('17.1.0-next.3+sha-299eae4');
 
 // This default value is when checking the hierarchy for a token.
 //
