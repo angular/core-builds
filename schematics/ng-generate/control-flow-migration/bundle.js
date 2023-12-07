6e0cebd8d7d0874f57be4f3e5b20c43878cc72be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-07 20:09:45 +0000

    2565121 fix(core): Avoid refreshing a host view twice when using transplanted views (#53021)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25262,7 +25262,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.3+sha-1f5c8bf");
+var VERSION2 = new Version("17.1.0-next.3+sha-2565121");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
