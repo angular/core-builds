919e2f279ecba6d395322ddf19c3e3cc76f94b75
Joey Perrott <josephperrott@gmail.com>
2023-12-11 19:27:16 +0000

    1ea8a14 refactor(docs-infra): move versions information into the application (#53501)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.6+sha-24d8aa6
+ * @license Angular v17.0.6+sha-1ea8a14
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.6+sha-24d8aa6');
+const VERSION = new Version('17.0.6+sha-1ea8a14');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25349,7 +25349,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.6+sha-24d8aa6");
+var VERSION2 = new Version("17.0.6+sha-1ea8a14");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
