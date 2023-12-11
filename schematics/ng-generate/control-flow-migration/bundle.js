bc7e0a4be2ec2d79038991976f675882074c49d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-11 19:01:57 +0000

    7c8f026 build: replace multimatch dependency with glob ignore option (#53463)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25349,7 +25349,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.3+sha-636b0d0");
+var VERSION2 = new Version("17.1.0-next.3+sha-7c8f026");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
