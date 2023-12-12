55af6cd38f4577c50fbfadfb2090cd5f61fe056e
Definitely not me <something@something.com>
2023-12-12 23:53:21 +0000

    d9348be feat(docs-infra): put the editor back in angular/angular (#53540)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.3+sha-63fd649
+ * @license Angular v17.1.0-next.3+sha-d9348be
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.3+sha-63fd649');
+const VERSION = new Version('17.1.0-next.3+sha-d9348be');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25406,7 +25406,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.3+sha-63fd649");
+var VERSION2 = new Version("17.1.0-next.3+sha-d9348be");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
