9c3bdffcf0699277d76acee2b87d7d7bec96eba2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-14 00:39:52 +0000

    203a3d5 release: cut the v17.0.7 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.6+sha-c16b5e8
+ * @license Angular v17.0.7+sha-203a3d5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10427,7 +10427,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.6+sha-c16b5e8');
+const VERSION = new Version('17.0.7+sha-203a3d5');
 
 // This default value is when checking the hierarchy for a token.
 //
