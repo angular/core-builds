40c472cacece30a61ad5b36532cdba162e843fb8
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-15 20:03:12 +0000

    658c73d refactor(migrations): update cf migration to handle module imports better (#53575)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.7+sha-e73205f
+ * @license Angular v17.0.7+sha-658c73d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10441,7 +10441,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.7+sha-e73205f');
+const VERSION = new Version('17.0.7+sha-658c73d');
 
 // This default value is when checking the hierarchy for a token.
 //
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25522,7 +25522,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.7+sha-e73205f");
+var VERSION2 = new Version("17.0.7+sha-658c73d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
