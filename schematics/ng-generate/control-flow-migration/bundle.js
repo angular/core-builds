43aa5669fb2d3f850803bbaa0661e78266e4429a
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-18 15:39:58 +0000

    b06b24b fix(zone.js): handle fetch with AbortSignal (#49595)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25476,7 +25476,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0-next.4+sha-1892904");
+var VERSION2 = new Version("17.1.0-next.4+sha-b06b24b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
