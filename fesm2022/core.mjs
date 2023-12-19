fb095a7b6feee38adb90babf14d27c664c47dae9
Andrew Scott <atscott01@gmail.com>
2023-12-19 17:26:31 +0000

    502f300 fix(router): Should not freeze original object used for route data (#53635)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.4+sha-00cbdc9
+ * @license Angular v17.1.0-next.4+sha-502f300
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15749,7 +15749,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.0-next.4+sha-00cbdc9']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.0-next.4+sha-502f300']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30025,7 +30025,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-next.4+sha-00cbdc9');
+const VERSION = new Version('17.1.0-next.4+sha-502f300');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
