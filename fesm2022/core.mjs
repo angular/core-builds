b917e2ab31533e8d425b5a7a70fa9e92e93557a8
Andrew Scott <atscott@google.com>
2024-01-04 22:03:23 +0000

    8fa5ebb refactor(router): replace `last` helper with native `Array.at(-1)` (#53749)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.8+sha-91cb16f
+ * @license Angular v17.0.8+sha-8fa5ebb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15661,7 +15661,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.0.8+sha-91cb16f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.0.8+sha-8fa5ebb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29937,7 +29937,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.8+sha-91cb16f');
+const VERSION = new Version('17.0.8+sha-8fa5ebb');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25486,7 +25486,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.8+sha-91cb16f");
+var VERSION2 = new Version("17.0.8+sha-8fa5ebb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
