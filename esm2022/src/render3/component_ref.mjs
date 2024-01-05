8639821a12aea61cf00b6cd20df3e0e12a284f49
Andrew Scott <atscott@google.com>
2024-01-05 19:32:36 +0000

    15a973f build: configure cross-pkg resolution for api extraction (#52499)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -380,7 +380,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.0.8+sha-8fa5ebb']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.0.8+sha-15a973f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25417,7 +25417,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.8+sha-8fa5ebb");
+var VERSION2 = new Version("17.0.8+sha-15a973f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
