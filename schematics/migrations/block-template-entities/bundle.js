14dee1effa65acb008a116f487dc81956abb7012
Andrew Scott <atscott01@gmail.com>
2024-01-10 22:13:01 +0000

    37e2769 release: cut the v17.0.9 release

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25484,7 +25484,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.8+sha-74094e1");
+var VERSION2 = new Version("17.0.9+sha-37e2769");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
