9d876105afeeabeadd43b96a39bbcc8abd9352dd
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:40:27 +0000

    8ef626e refactor(router): Use Nullish coalescing assignment when possible. (#53930)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.9+sha-4373889
+ * @license Angular v17.0.9+sha-8ef626e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15621,7 +15621,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.0.9+sha-4373889']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.0.9+sha-8ef626e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29894,7 +29894,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.0.9+sha-4373889');
+const VERSION = new Version('17.0.9+sha-8ef626e');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25484,7 +25484,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.9+sha-4373889");
+var VERSION2 = new Version("17.0.9+sha-8ef626e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
