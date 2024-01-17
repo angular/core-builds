51472cd096b2440d8c8a98ab423819a46808386b
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:46:44 +0000

    6616019 refactor: migrate modules to prettier formatting (#53954)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-rc.0+sha-4d4858f
+ * @license Angular v17.1.0-rc.0+sha-6616019
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15717,7 +15717,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.0-rc.0+sha-4d4858f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.0-rc.0+sha-6616019']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30045,7 +30045,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.0-rc.0+sha-4d4858f');
+const VERSION = new Version('17.1.0-rc.0+sha-6616019');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
