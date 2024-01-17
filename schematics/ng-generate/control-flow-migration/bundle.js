b06751259168e402bd4b5c56eec1c932cab34521
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 03:23:31 +0000

    e1b9b66 ci: set up previews of adev (#53941)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25573,7 +25573,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-6581ac9");
+var VERSION2 = new Version("17.2.0-next.0+sha-e1b9b66");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
