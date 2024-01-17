1c6171ffa2974fac2a801dc534d2aa9a89c44af6
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:41:46 +0000

    a7f8d9d refactor(compiler-cli): interpolatedSignalNotInvoked diagnostic for input signals (#53883)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-455b645']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-a7f8d9d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
