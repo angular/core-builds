71f508ebc93aaed7a6a23502dc4a8ac7b9935b9f
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:46:53 +0000

    f9781f9 refactor: migrate modules to prettier formatting (#53954)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25643,7 +25643,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-616df43");
+var VERSION2 = new Version("17.2.0-next.0+sha-f9781f9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
