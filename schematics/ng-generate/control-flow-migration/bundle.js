7c4412f5619b8e12bc17d219ad3553585567696b
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:46:09 +0000

    3363295 refactor: migrate modules to prettier formatting (#53954)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25415,7 +25415,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.0.9+sha-23f19f8");
+var VERSION2 = new Version("17.0.9+sha-3363295");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
