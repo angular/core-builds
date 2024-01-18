0b5d5d399027474e5a49aefa788eddec4aaa51ff
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 09:27:33 +0000

    8737544 build(devtools): prevent underscores from being added at build time (#53921)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25645,7 +25645,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-a566c5c");
+var VERSION2 = new Version("17.2.0-next.0+sha-8737544");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
