b8ac8304d4db49f4aeebb6216e5bf5a0b1e3fa35
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 13:33:49 +0000

    4e03410 docs: add ankita (#52319)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25644,7 +25644,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0+sha-0616dc2");
+var VERSION2 = new Version("17.1.0+sha-4e03410");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
