e1245e908e937b408956f6da92bba0a284d7d2a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 09:21:39 +0000

    a566c5c docs: fix broken code example (#53531)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25576,7 +25576,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-e227275");
+var VERSION2 = new Version("17.2.0-next.0+sha-a566c5c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
