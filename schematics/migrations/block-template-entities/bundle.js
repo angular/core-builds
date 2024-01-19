43d37c5cfc2f0cef68bc6c16a84c8aa948f252cb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:16:44 +0000

    ee68df9 build: update all non-major dependencies (#53979)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25645,7 +25645,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-a057b61");
+var VERSION2 = new Version("17.2.0-next.0+sha-ee68df9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
