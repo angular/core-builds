0fe9234bf5202ab53649147f77194c1c40445514
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 16:23:05 +0000

    0fe9331 build: update g3 sync config to match internal one (#53987)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25575,7 +25575,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0+sha-7620f50");
+var VERSION2 = new Version("17.1.0+sha-0fe9331");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
