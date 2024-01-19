be75e06f42dbfcd31a7d01d194d139e1f3046949
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 19:13:25 +0000

    bbbe477 refactor: migrate animations to prettier formatting (#53977)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25645,7 +25645,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-711cb41");
+var VERSION2 = new Version("17.2.0-next.0+sha-bbbe477");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
