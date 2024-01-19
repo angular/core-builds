73219b3a0b2bcf15e54df97d8913bcf040c19703
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 16:22:44 +0000

    c8ee528 build: update g3 sync config to match internal one (#53987)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25645,7 +25645,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-5ae85e4");
+var VERSION2 = new Version("17.2.0-next.0+sha-c8ee528");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
