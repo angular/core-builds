59fb31d06b8c026395b78080e2a2f2ec16180a55
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:26:57 +0000

    cc34e5f refactor: migrate scripts to prettier formatting (#53976)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-1d1c413
+ * @license Angular v17.2.0-next.0+sha-cc34e5f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15717,7 +15717,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-1d1c413']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-cc34e5f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30046,7 +30046,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-1d1c413');
+const VERSION = new Version('17.2.0-next.0+sha-cc34e5f');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
