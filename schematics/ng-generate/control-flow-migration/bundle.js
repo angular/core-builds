3ffe282e715367980bb76cb9c5ce79fdf42cd2cd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-22 08:12:26 +0000

    ec03e46 refactor: migrate bazel, benchpress, elements and misc to prettier formatting (#53995)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25576,7 +25576,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-bbbe477");
+var VERSION2 = new Version("17.2.0-next.0+sha-ec03e46");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
