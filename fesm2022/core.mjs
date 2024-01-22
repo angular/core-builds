c1749d681809e0f98d3f53c7d9d38524a437a44e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-22 18:48:36 +0000

    6186e52 docs: move import statement of ngmodule from standalone component to module code #53952 (#54016)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-3e13840
+ * @license Angular v17.2.0-next.0+sha-6186e52
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15714,7 +15714,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-3e13840']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-6186e52']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30043,7 +30043,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-3e13840');
+const VERSION = new Version('17.2.0-next.0+sha-6186e52');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
