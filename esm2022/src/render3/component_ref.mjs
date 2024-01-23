8e3018b7993d20b18af7072412eedccb95d8b0dd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 18:01:15 +0000

    9a9a00e docs: update docs to reflect changes in Angular CLI version 17.1 (#53949)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.0+sha-5f9cac0']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.0+sha-9a9a00e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
