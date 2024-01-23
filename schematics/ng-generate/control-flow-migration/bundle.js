95b6fddbef2645f7bad9b0a9a61dfd028bfb094a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 08:58:39 +0000

    9384537 refactor(devtools): migrate to standalone (#53998)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25576,7 +25576,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-6186e52");
+var VERSION2 = new Version("17.2.0-next.0+sha-9384537");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
