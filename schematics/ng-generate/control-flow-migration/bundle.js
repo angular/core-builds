b41db4e209a87583e7610549277ba6c1eaf08d2a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 09:59:42 +0000

    bc85551 fix(router): revert commit that replaced `last` helper with native `Array.at(-1)` (#54021)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25613,7 +25613,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-aeaec86");
+var VERSION2 = new Version("17.2.0-next.0+sha-bc85551");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
