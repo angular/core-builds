bd4b3493828df4db260643ecc7c7a097b8477485
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 14:36:51 +0000

    e194fbf docs: fix typo in Custom directives section (#54044)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25575,7 +25575,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.0+sha-8ad1ee7");
+var VERSION2 = new Version("17.1.0+sha-e194fbf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
