5e6b77a2bff43490f77d32a1db55b7482e9ad540
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-01-24 21:05:17 +0000

    fad1354 release: cut the v17.2.0-next.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-9e10af4
+ * @license Angular v17.2.0-next.0+sha-fad1354
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15714,7 +15714,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-9e10af4']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-fad1354']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30062,7 +30062,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-9e10af4');
+const VERSION = new Version('17.2.0-next.0+sha-fad1354');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
