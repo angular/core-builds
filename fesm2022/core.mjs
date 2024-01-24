382257deffafa3931f4f33d827cd615d027ca3d2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 20:29:28 +0000

    9e10af4 docs: typo Chat to chart fix (#53892)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-a2a0b33
+ * @license Angular v17.2.0-next.0+sha-9e10af4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15714,7 +15714,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-a2a0b33']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-9e10af4']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30062,7 +30062,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-a2a0b33');
+const VERSION = new Version('17.2.0-next.0+sha-9e10af4');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
