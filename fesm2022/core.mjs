0b43b8581fd46cbc67d9b8c4d65d7a0e4aa7e25b
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 18:09:05 +0000

    58d4abc docs: update Angular CLI help [main] (#54075)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-656bc28
+ * @license Angular v17.2.0-next.0+sha-58d4abc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15725,7 +15725,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-656bc28']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-58d4abc']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30225,7 +30225,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-656bc28');
+const VERSION = new Version('17.2.0-next.0+sha-58d4abc');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
