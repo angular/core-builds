38bd57a0417e2eee1933416ca9f855aa1214ba42
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:24:05 +0000

    d5c0743 ci: add ben to reviewers for adev files (#54078)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25739,7 +25739,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-58d4abc");
+var VERSION2 = new Version("17.2.0-next.0+sha-d5c0743");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
