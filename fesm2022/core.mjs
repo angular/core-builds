629c04779e07c7bf0632dd86744ae3955959e140
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:39:38 +0000

    d8d6478 docs: testing-components-basics.md and components-basics.md links to css selector documentation are dead (#53586)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-293dc3b
+ * @license Angular v17.1.1+sha-d8d6478
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15722,7 +15722,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-293dc3b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-d8d6478']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30051,7 +30051,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-293dc3b');
+const VERSION = new Version('17.1.1+sha-d8d6478');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
