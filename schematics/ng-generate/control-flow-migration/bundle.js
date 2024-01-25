17f018ccc1b3424512c4b664d07065b8b01a0fd0
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:44:58 +0000

    a6dad59 ci: update adev approvers list (#54081)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25670,7 +25670,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-d5c0743");
+var VERSION2 = new Version("17.2.0-next.0+sha-a6dad59");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
