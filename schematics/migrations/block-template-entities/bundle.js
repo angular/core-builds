291d02492ec4aca6d29419902b0e49f2db8b2d76
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 21:31:38 +0000

    9bb1884 docs: change the file of the provideHttpClient standalone example to match cli generated project (#52806)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25739,7 +25739,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-35ec260");
+var VERSION2 = new Version("17.2.0-next.0+sha-9bb1884");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
