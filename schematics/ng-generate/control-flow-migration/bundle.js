ca9801b06850d4b4467a95bf498a99ea73a9a934
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:51:11 +0000

    259707e docs: fix type at change detection with pipes section (#53940)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25604,7 +25604,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.1+sha-d17ba9b");
+var VERSION2 = new Version("17.1.1+sha-259707e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
