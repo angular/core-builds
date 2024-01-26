82ae8bcf6ff8b544fe48a75a4a6668838754d372
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 16:08:30 +0000

    f240c70 docs(docs-infra): cleanup repeated scss from typography (#51579)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25604,7 +25604,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.1+sha-0172477");
+var VERSION2 = new Version("17.1.1+sha-f240c70");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
