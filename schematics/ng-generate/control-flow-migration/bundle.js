8193bec6e0dd0be3785c5a4f7444ccdb0048bdb9
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 20:44:14 +0000

    5cd00f1 InMemoryBackendConfigArgs.passThruUnknownUrl: fix tsdoc (#52273)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25604,7 +25604,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.1+sha-9f9dd90");
+var VERSION2 = new Version("17.1.1+sha-5cd00f1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
