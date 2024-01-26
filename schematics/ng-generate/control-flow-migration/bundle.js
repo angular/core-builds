0244d34f7de991669c6eaeb5bfe4f0ea62dd07e5
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:03:00 +0000

    89ab1c1 refactor(common): Use Nullish coalescing assignment when possible. (#53931)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25670,7 +25670,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-271755d");
+var VERSION2 = new Version("17.2.0-next.0+sha-89ab1c1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
