5b4757aa13f188d70cdd224b70fc0aab7da60fb5
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:03:03 +0000

    8109013 refactor(common): Use Nullish coalescing assignment when possible. (#53931)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25673,7 +25673,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.1+sha-f240c70");
+var VERSION2 = new Version("17.1.1+sha-8109013");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
