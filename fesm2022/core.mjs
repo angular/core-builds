af02c5cd178a6c6c982541cef8ddafdfe2ca7d13
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:43:16 +0000

    4105ab5 docs: update i18n-common-merge.md (#52400)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-1a2b569
+ * @license Angular v17.2.0-next.0+sha-4105ab5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15731,7 +15731,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-1a2b569']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-4105ab5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30231,7 +30231,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-1a2b569');
+const VERSION = new Version('17.2.0-next.0+sha-4105ab5');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
