d1ce2a89eda1bf692d1c7fa1ad911b028436897e
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 19:07:36 +0000

    16fbe85 docs(router): fix variable reference in text (#54099) PR Close #54099

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25739,7 +25739,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-4105ab5");
+var VERSION2 = new Version("17.2.0-next.0+sha-16fbe85");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
