939549c57861e3f36eaf4d03323411ebb35a54ac
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:46:17 +0000

    9571287 docs: correct grammar error on start page (#53993)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-f5c520b
+ * @license Angular v17.2.0-next.0+sha-9571287
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15732,7 +15732,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-f5c520b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-9571287']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30234,7 +30234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-f5c520b');
+const VERSION = new Version('17.2.0-next.0+sha-9571287');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
