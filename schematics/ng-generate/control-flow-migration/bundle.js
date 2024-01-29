6fb4c9b9c0c4b3e838bd5c66a836aa63f6aaccf0
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:02:29 +0000

    030d770 docs: update TypeScript version support details for Angular v15 and v16 (#54131)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25604,7 +25604,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.1+sha-96ca49d");
+var VERSION2 = new Version("17.1.1+sha-030d770");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
