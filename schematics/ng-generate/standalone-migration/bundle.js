03c6baab8352a56b0dd10e37e998270101235f62
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 21:16:54 +0000

    7b4d275 fix(compiler): Fix the template pipeline option (#54148)

--- schematics/ng-generate/standalone-migration/bundle.js
+++ schematics/ng-generate/standalone-migration/bundle.js
@@ -26584,7 +26584,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-3ca34e6");
+var VERSION2 = new Version("17.2.0-next.0+sha-7b4d275");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _I18N_ATTR = "i18n";
@@ -27650,7 +27650,7 @@ var MINIMUM_PARTIAL_LINKER_VERSION = "12.0.0";
 function compileDeclareClassMetadata(metadata) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION));
-  definitionMap.set("version", literal("17.2.0-next.0+sha-3ca34e6"));
+  definitionMap.set("version", literal("17.2.0-next.0+sha-7b4d275"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", metadata.type);
   definitionMap.set("decorators", metadata.decorators);
@@ -27719,7 +27719,7 @@ function createDirectiveDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   const minVersion = getMinimumVersionForPartialOutput(meta);
   definitionMap.set("minVersion", literal(minVersion));
-  definitionMap.set("version", literal("17.2.0-next.0+sha-3ca34e6"));
+  definitionMap.set("version", literal("17.2.0-next.0+sha-7b4d275"));
   definitionMap.set("type", meta.type.value);
   if (meta.isStandalone) {
     definitionMap.set("isStandalone", literal(meta.isStandalone));
@@ -28018,7 +28018,7 @@ var MINIMUM_PARTIAL_LINKER_VERSION2 = "12.0.0";
 function compileDeclareFactoryFunction(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION2));
-  definitionMap.set("version", literal("17.2.0-next.0+sha-3ca34e6"));
+  definitionMap.set("version", literal("17.2.0-next.0+sha-7b4d275"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   definitionMap.set("deps", compileDependencies(meta.deps));
@@ -28041,7 +28041,7 @@ function compileDeclareInjectableFromMetadata(meta) {
 function createInjectableDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION3));
-  definitionMap.set("version", literal("17.2.0-next.0+sha-3ca34e6"));
+  definitionMap.set("version", literal("17.2.0-next.0+sha-7b4d275"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.providedIn !== void 0) {
@@ -28079,7 +28079,7 @@ function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION4));
-  definitionMap.set("version", literal("17.2.0-next.0+sha-3ca34e6"));
+  definitionMap.set("version", literal("17.2.0-next.0+sha-7b4d275"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   definitionMap.set("providers", meta.providers);
@@ -28103,7 +28103,7 @@ function createNgModuleDefinitionMap(meta) {
     throw new Error("Invalid path! Local compilation mode should not get into the partial compilation path");
   }
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION5));
-  definitionMap.set("version", literal("17.2.0-next.0+sha-3ca34e6"));
+  definitionMap.set("version", literal("17.2.0-next.0+sha-7b4d275"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.bootstrap.length > 0) {
@@ -28138,7 +28138,7 @@ function compileDeclarePipeFromMetadata(meta) {
 function createPipeDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION6));
-  definitionMap.set("version", literal("17.2.0-next.0+sha-3ca34e6"));
+  definitionMap.set("version", literal("17.2.0-next.0+sha-7b4d275"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.isStandalone) {
@@ -28155,7 +28155,7 @@ function createPipeDefinitionMap(meta) {
 publishFacade(_global);
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
-var VERSION3 = new Version("17.2.0-next.0+sha-3ca34e6");
+var VERSION3 = new Version("17.2.0-next.0+sha-7b4d275");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/transformers/api.mjs
 var EmitFlags;
@@ -38209,8 +38209,10 @@ var ComponentDecoratorHandler = class {
       return [];
     }
     const deferrableTypes = this.collectDeferredSymbols(resolution);
+    const useTemplatePipeline = this.useTemplatePipeline;
     const meta = __spreadProps(__spreadValues(__spreadValues({}, analysis.meta), resolution), {
-      deferrableTypes
+      deferrableTypes,
+      useTemplatePipeline
     });
     const fac = compileNgFactoryDefField(toFactoryMetadata(meta, FactoryTarget.Component));
     removeDeferrableTypesFromComponentDecorator(analysis, deferrableTypes);
@@ -38231,7 +38233,10 @@ var ComponentDecoratorHandler = class {
       isInline: analysis.template.declaration.isInline,
       inlineTemplateLiteralExpression: analysis.template.sourceMapping.type === "direct" ? new WrappedNodeExpr(analysis.template.sourceMapping.node) : null
     };
-    const meta = __spreadValues(__spreadValues({}, analysis.meta), resolution);
+    const useTemplatePipeline = this.useTemplatePipeline;
+    const meta = __spreadProps(__spreadValues(__spreadValues({}, analysis.meta), resolution), {
+      useTemplatePipeline
+    });
     const fac = compileDeclareFactory(toFactoryMetadata(meta, FactoryTarget.Component));
     const inputTransformFields = compileInputTransformFields(analysis.inputs);
     const def = compileDeclareComponentFromMetadata(meta, analysis.template, templateInfo);
@@ -38243,8 +38248,10 @@ var ComponentDecoratorHandler = class {
       return [];
     }
     const deferrableTypes = analysis.explicitlyDeferredTypes;
+    const useTemplatePipeline = this.useTemplatePipeline;
     const meta = __spreadProps(__spreadValues(__spreadValues({}, analysis.meta), resolution), {
-      deferrableTypes: deferrableTypes != null ? deferrableTypes : /* @__PURE__ */ new Map()
+      deferrableTypes: deferrableTypes != null ? deferrableTypes : /* @__PURE__ */ new Map(),
+      useTemplatePipeline
     });
     if (analysis.explicitlyDeferredTypes !== null) {
       removeDeferrableTypesFromComponentDecorator(analysis, analysis.explicitlyDeferredTypes);
