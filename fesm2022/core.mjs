b40cebb7139d4419092198b763fa47724e8b0a12
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 17:54:39 +0000

    274a489 docs: fix path to housing location css file in lesson 03 (#54106)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.0+sha-0460a9d
+ * @license Angular v17.2.0-next.0+sha-274a489
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15732,7 +15732,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-0460a9d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-274a489']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30234,7 +30234,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.0+sha-0460a9d');
+const VERSION = new Version('17.2.0-next.0+sha-274a489');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
