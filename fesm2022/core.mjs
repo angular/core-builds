08be45e61a7e09dfa10da5624c87f772dcfe3aa8
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 15:10:57 +0000

    57174bb refactor(common): simplify i18n plural and select pipe metadata (#53939)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-c5a0e90
+ * @license Angular v17.1.1+sha-57174bb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15722,7 +15722,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-c5a0e90']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-57174bb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30053,7 +30053,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-c5a0e90');
+const VERSION = new Version('17.1.1+sha-57174bb');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
