4296bce9aff6f8955fbd7c06f9c27074e213b164
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 21:32:10 +0000

    752ebd6 docs: focus on the respective card of the current active line while scrolling (#53733)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-7001b5d
+ * @license Angular v17.1.1+sha-752ebd6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15768,7 +15768,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-7001b5d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-752ebd6']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30108,7 +30108,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-7001b5d');
+const VERSION = new Version('17.1.1+sha-752ebd6');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
