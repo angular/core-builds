2375f477bd137896aa9b9913aa485631668e6b62
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:13:04 +0000

    4367c44 docs: Update docs about equality. (#54158)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.1+sha-b69bf37
+ * @license Angular v17.1.1+sha-4367c44
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15768,7 +15768,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-b69bf37']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-4367c44']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30108,7 +30108,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.1+sha-b69bf37');
+const VERSION = new Version('17.1.1+sha-4367c44');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
