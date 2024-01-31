2c96116a0957ceb102ea6ad5f2e0414033912334
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 17:41:32 +0000

    4f7edaa refactor(router): RouterLink read urlTree only once (#53817)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-f5ede2a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.0+sha-4f7edaa']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
