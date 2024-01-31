798302c267be373262810de8128dd8e0ef935184
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:35:31 +0000

    a297012 docs(docs-infra): add tab titles to home, playground and tutorial (#53026)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25670,7 +25670,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-2e2e8aa");
+var VERSION2 = new Version("17.2.0-next.0+sha-a297012");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
