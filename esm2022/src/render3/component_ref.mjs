3b11aabd6464f323440d4008622cf01763149a80
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:49:37 +0000

    b353ad5 fix(docs-infra): sort api reference items (#54116)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-bd6d18b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.1+sha-b353ad5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
