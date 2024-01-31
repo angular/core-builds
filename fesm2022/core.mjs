727c3bd222f01b3028dd5d24dbf823d51e2443ce
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 21:54:12 +0000

    924a276 refactor(docs-infra): migrate to using `docs-` prefix for class names rather than `adev-` for shared pieces (#54189)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-7df133d
+ * @license Angular v17.1.2+sha-924a276
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15764,7 +15764,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-7df133d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-924a276']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30104,7 +30104,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-7df133d');
+const VERSION = new Version('17.1.2+sha-924a276');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
