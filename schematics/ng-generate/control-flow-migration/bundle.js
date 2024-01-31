fff33c837e6e3256c63d4fa26708c800936b6949
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:09:16 +0000

    34feb4f docs(docs-infra): @input getter setter syntax change (#51023)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25670,7 +25670,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.0+sha-b02cf14");
+var VERSION2 = new Version("17.2.0-next.0+sha-34feb4f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
