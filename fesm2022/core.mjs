33a2ae27d407345f6c1b7cc59fc184315ecdd1f5
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 20:17:06 +0000

    989d496 build: update cross-repo angular dependencies (#54178)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-0a67085
+ * @license Angular v17.2.0-next.1+sha-989d496
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15778,7 +15778,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.1+sha-0a67085']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.1+sha-989d496']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30289,7 +30289,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-0a67085');
+const VERSION = new Version('17.2.0-next.1+sha-989d496');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
