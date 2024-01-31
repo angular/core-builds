41e46edd7cf33bdb1c0381c9775f86d8693c5578
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 19:50:13 +0000

    0a67085 build: update actions/cache action to v4 (#54179)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25670,7 +25670,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.1+sha-2d20c4a");
+var VERSION2 = new Version("17.2.0-next.1+sha-0a67085");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
