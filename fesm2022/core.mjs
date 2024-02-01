0532519d30727d8d8e9fd8a57948e8badb8aa481
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:24:18 +0000

    8608616 [aio/src/styles/custom-themes/{dark,light}-theme.scss] Change theme definitions to resolve warning "Angular Material themes should be created from a map" (#53717)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-99a0b90
+ * @license Angular v17.1.2+sha-8608616
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15764,7 +15764,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-99a0b90']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-8608616']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30104,7 +30104,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-99a0b90');
+const VERSION = new Version('17.1.2+sha-8608616');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25610,7 +25610,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.2+sha-99a0b90");
+var VERSION2 = new Version("17.1.2+sha-8608616");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
