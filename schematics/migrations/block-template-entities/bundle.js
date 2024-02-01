2c24fced33be01ca536e8b0bda36478790645408
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:12:07 +0000

    99a0b90 docs: correct TypeScript version range for 17.0.x (#53213)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25679,7 +25679,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.2+sha-52a31d0");
+var VERSION2 = new Version("17.1.2+sha-99a0b90");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
