e6b841878b26aca7e7ab66d2d96c82aa1260476a
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 18:29:25 +0000

    241858d build: update dependency @octokit/graphql to v7 (#53554)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-f303838
+ * @license Angular v17.1.2+sha-241858d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15764,7 +15764,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-f303838']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-241858d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30177,7 +30177,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-f303838');
+const VERSION = new Version('17.1.2+sha-241858d');
 
 class Console {
     log(message) {
