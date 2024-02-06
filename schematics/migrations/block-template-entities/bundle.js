c94ebb8d8635abbd6248f62b0268e89774652d6d
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 15:27:30 +0000

    4851453 ci: add debug information for failing firebase deploy (#54275)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26053,7 +26053,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.0-next.1+sha-99bfbab");
+var VERSION2 = new Version("17.2.0-next.1+sha-4851453");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
