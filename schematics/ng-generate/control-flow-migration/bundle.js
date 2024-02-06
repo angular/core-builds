733efbfe91f0ec80661a9346cd579ede96fbcb44
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 18:43:43 +0000

    0cd9538 refactor: address prototype polution concern with better typing (#54286)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25622,7 +25622,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.2+sha-241858d");
+var VERSION2 = new Version("17.1.2+sha-0cd9538");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
