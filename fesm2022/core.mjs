e4aa2432de09375a53f576faeeedb75faef125f7
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 15:28:27 +0000

    ca45917 ci: add debug information for failing firebase deploy (#54275)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-04a1d50
+ * @license Angular v17.1.2+sha-ca45917
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15764,7 +15764,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-04a1d50']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-ca45917']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30177,7 +30177,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-04a1d50');
+const VERSION = new Version('17.1.2+sha-ca45917');
 
 class Console {
     log(message) {
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -25686,7 +25686,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.2+sha-04a1d50");
+var VERSION2 = new Version("17.1.2+sha-ca45917");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
