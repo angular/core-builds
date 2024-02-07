2498ff05a63f82262f91eaa502a36c93b9d15582
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-07 15:51:46 +0000

    d695398 Revert "fix(router): Clear internal transition when navigation finalizes (#54261)" (#54317)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.2+sha-f3f1f85
+ * @license Angular v17.1.2+sha-d695398
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15764,7 +15764,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-f3f1f85']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.2+sha-d695398']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30177,7 +30177,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.1.2+sha-f3f1f85');
+const VERSION = new Version('17.1.2+sha-d695398');
 
 class Console {
     log(message) {
