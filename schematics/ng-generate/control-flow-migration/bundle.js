70434ff13aa94e7baab72e83cf2e69f3c99919a9
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-07 22:26:12 +0000

    2e3fa28 docs: enable version picker (#54161)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25622,7 +25622,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.2+sha-eeaec92");
+var VERSION2 = new Version("17.1.2+sha-2e3fa28");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
