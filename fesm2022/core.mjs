e618ca45ecc5545a2528cadb4a6cff9cbfca0845
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-07 19:00:15 +0000

    38c0084 test: verify signal-based queries in CLI integration test (#54314)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-a08863e
+ * @license Angular v17.2.0-next.1+sha-38c0084
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16740,7 +16740,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.1+sha-a08863e']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.1+sha-38c0084']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30666,7 +30666,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-a08863e');
+const VERSION = new Version('17.2.0-next.1+sha-38c0084');
 
 class Console {
     log(message) {
