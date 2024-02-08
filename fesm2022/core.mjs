6684217bc4d578470ebb9d86fb9a270ce0cbfac3
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:54:03 +0000

    8dea3b5 test: tidy up signal signal input/query/model signature tests (#54334)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.0-next.1+sha-adfc3f0
+ * @license Angular v17.2.0-next.1+sha-8dea3b5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16740,7 +16740,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.1+sha-adfc3f0']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0-next.1+sha-8dea3b5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30670,7 +30670,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.0-next.1+sha-adfc3f0');
+const VERSION = new Version('17.2.0-next.1+sha-8dea3b5');
 
 class Console {
     log(message) {
@@ -32503,7 +32503,10 @@ function whenStable(applicationRef) {
     return whenStablePromise;
 }
 function shouldRecheckView(view) {
-    return requiresRefreshOrTraversal(view) || !!(view[FLAGS] & 64 /* LViewFlags.Dirty */);
+    return requiresRefreshOrTraversal(view);
+    // TODO(atscott): We need to support rechecking views marked dirty again in afterRender hooks
+    // in order to support the transition to zoneless. b/308152025
+    /* || !!(view[FLAGS] & LViewFlags.Dirty); */
 }
 
 /**
