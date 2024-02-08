19bb695b6a206b56b6d75a88de434fec67cf1653
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 19:23:59 +0000

    375e606 refactor(devtools): show tooltip for hydration icon on directive forest (#54326)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25622,7 +25622,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.1.3+sha-ca39926");
+var VERSION2 = new Version("17.1.3+sha-375e606");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
