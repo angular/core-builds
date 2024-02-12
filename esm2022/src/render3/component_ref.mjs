658a110267b1d8b58f9a2f15bf25b019ba13121a
Andrew Kushnir <akushnir@google.com>
2024-02-12 16:56:10 +0000

    017825b fix(zone.js): patch form-associated custom element callbacks (#50686)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.3+sha-58365e0']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.1.3+sha-017825b']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
