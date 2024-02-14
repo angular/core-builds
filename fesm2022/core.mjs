e606059da6bbed8991b2bd16a854bff0955f4965
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:27:11 +0000

    12d4b8e docs: add guide for signal inputs (#54316)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-ed0d5ec
+ * @license Angular v17.3.0-next.0+sha-12d4b8e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16739,7 +16739,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-ed0d5ec']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-12d4b8e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30671,7 +30671,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-ed0d5ec');
+const VERSION = new Version('17.3.0-next.0+sha-12d4b8e');
 
 class Console {
     log(message) {
