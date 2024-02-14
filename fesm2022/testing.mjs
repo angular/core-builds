28fd54ad51e5a2f0b5d41d263b2d9b4822a7c27c
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:34:03 +0000

    8521188 docs: fix typo (#54427) PR Close #54427

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-44a0ea4
+ * @license Angular v17.2.1+sha-8521188
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25992,7 +25992,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.1+sha-44a0ea4");
+var VERSION2 = new Version("17.2.1+sha-8521188");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
