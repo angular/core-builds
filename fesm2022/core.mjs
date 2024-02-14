f496325f4923375f33e8cffd51d601943530a3bc
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:38:29 +0000

    e5e8be4 ci: remove feature-request bot (#54442)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-8521188
+ * @license Angular v17.2.1+sha-e5e8be4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16739,7 +16739,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.1+sha-8521188']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.1+sha-e5e8be4']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30671,7 +30671,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-8521188');
+const VERSION = new Version('17.2.1+sha-e5e8be4');
 
 class Console {
     log(message) {
--- fesm2022/primitives/signals.mjs
+++ fesm2022/primitives/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-8521188
+ * @license Angular v17.2.1+sha-e5e8be4
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
