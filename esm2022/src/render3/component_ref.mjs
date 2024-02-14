d8d5a4b090eb2ed20907eaf35b65865af43f973c
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:49:14 +0000

    da7fbb4 fix(compiler-cli): detect when the linker is working in unpublished angular and widen supported versions (#54439)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-12d4b8e']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-da7fbb4']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
