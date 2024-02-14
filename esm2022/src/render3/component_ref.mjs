5266c8a0d07c0add396cdcac23415a36f6d3ce9b
Andrew Kushnir <akushnir@google.com>
2024-02-14 20:52:39 +0000

    7234824 fix(compiler-cli): fix broken version detection condition (#54443)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0+sha-0b95d14']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.0+sha-7234824']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
