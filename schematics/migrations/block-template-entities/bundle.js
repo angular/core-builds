4956b5135f99bb6f39296f6f69afa80b55952645
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:45:22 +0000

    fba7fe1 build: update all non-major dependencies (#54406)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26061,7 +26061,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.1+sha-e5e8be4");
+var VERSION2 = new Version("17.2.1+sha-fba7fe1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
