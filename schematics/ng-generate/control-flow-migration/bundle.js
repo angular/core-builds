6dcc5cd128806651ae09cdd38556548a41357bae
Andrew Kushnir <akushnir@google.com>
2024-02-15 01:19:52 +0000

    a7e4b99 refactor(devtools): implement multiframe support in devtools page (#53934)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25992,7 +25992,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.1+sha-fba7fe1");
+var VERSION2 = new Version("17.2.1+sha-a7e4b99");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
