8181f51a60a859503273767b00a9acd905cbc70c
Andrew Kushnir <akushnir@google.com>
2024-02-15 00:23:02 +0000

    e0d59f7 Revert "fix(docs-infra): process mermaid code blocks (#54434)" (#54448)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-da7fbb4
+ * @license Angular v17.3.0-next.0+sha-e0d59f7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16739,7 +16739,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-da7fbb4']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-e0d59f7']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30671,7 +30671,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-da7fbb4');
+const VERSION = new Version('17.3.0-next.0+sha-e0d59f7');
 
 class Console {
     log(message) {
