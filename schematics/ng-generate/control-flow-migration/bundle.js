819486ae50dddbc57c4b5a3db87cae711db69a3c
Andrew Kushnir <akushnir@google.com>
2024-02-15 18:38:30 +0000

    03acdbe fix(docs-infra): process mermaid code blocks (#54462)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25992,7 +25992,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0-next.0+sha-6c2d4b6");
+var VERSION2 = new Version("17.3.0-next.0+sha-03acdbe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
