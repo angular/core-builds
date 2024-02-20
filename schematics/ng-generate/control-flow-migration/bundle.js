1cea1388d42ea427d88a25fc4f0c1c9dd5379e21
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:34:46 +0000

    f8a5b40 docs: enhance Dependency injection description (#54464)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25992,7 +25992,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.1+sha-3bd5860");
+var VERSION2 = new Version("17.2.1+sha-f8a5b40");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
