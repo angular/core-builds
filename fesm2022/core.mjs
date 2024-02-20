20755394355ede157f062c536c49976137782948
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:37:56 +0000

    a9741e6 docs(router): deprecate RouterTestingModule (#54466)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-70345b1
+ * @license Angular v17.3.0-next.0+sha-a9741e6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16742,7 +16742,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-70345b1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-a9741e6']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30674,7 +30674,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-70345b1');
+const VERSION = new Version('17.3.0-next.0+sha-a9741e6');
 
 class Console {
     log(message) {
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26061,7 +26061,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0-next.0+sha-70345b1");
+var VERSION2 = new Version("17.3.0-next.0+sha-a9741e6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
