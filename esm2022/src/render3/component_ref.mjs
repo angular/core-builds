d031f15957f8684b5f009b6455801a63cff3bc00
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:55:41 +0000

    55fd7b3 docs(docs-infra): Add missing api entries (#54517)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-a9741e6']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-55fd7b3']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
