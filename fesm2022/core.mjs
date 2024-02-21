fb6b978e4151f598efb438a761e171306f051a5a
Joey Perrott <josephperrott@gmail.com>
2024-02-21 16:10:14 +0000

    d49558d docs: fix brokens links (#54507)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-55fd7b3
+ * @license Angular v17.3.0-next.0+sha-d49558d
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16742,7 +16742,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-55fd7b3']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-d49558d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30674,7 +30674,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-55fd7b3');
+const VERSION = new Version('17.3.0-next.0+sha-d49558d');
 
 class Console {
     log(message) {
