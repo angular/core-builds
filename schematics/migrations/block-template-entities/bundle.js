9e49371c0f4734446b0263982ba4ce9d238f9980
Joey Perrott <josephperrott@gmail.com>
2024-02-21 16:46:46 +0000

    554628e docs(docs-infra): Add missing API entries to ADEV (#54530)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26061,7 +26061,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0-next.0+sha-d49558d");
+var VERSION2 = new Version("17.3.0-next.0+sha-554628e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
