e54e956bb0d64d8d83e205ac15ce01eae1ea0973
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-21 23:33:03 +0000

    bb57d34 fix(migrations): Fix cf migration regular expression to include underscores (#54533)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-3e31f1a
+ * @license Angular v17.2.1+sha-bb57d34
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16742,7 +16742,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.1+sha-3e31f1a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.1+sha-bb57d34']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30674,7 +30674,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.1+sha-3e31f1a');
+const VERSION = new Version('17.2.1+sha-bb57d34');
 
 class Console {
     log(message) {
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.1+sha-3e31f1a
+ * @license Angular v17.2.1+sha-bb57d34
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10906,16 +10906,14 @@ export declare interface ViewChildDecorator {
      *   * Any provider defined through a string token (e.g. `{provide: 'token', useValue: 'val'}`)
      *   * `TemplateRef`, `ElementRef`, and `ViewContainerRef`
      *
-     * Difference between dynamic and static queries**:
-     *
-     * | Queries                             | Details |
-     * |:---                                 |:---     |
-     * | Dynamic queries \(`static: false`\) | The query resolves before the `ngAfterViewInit()`
+     * Difference between dynamic and static queries:
+     *   * Dynamic queries \(`static: false`\) - The query resolves before the `ngAfterViewInit()`
      * callback is called. The result will be updated for changes to your view, such as changes to
-     * `ngIf` and `ngFor` blocks. | | Static queries \(`static: true`\)   | The query resolves once
+     * `ngIf` and `ngFor` blocks.
+     *   * Static queries \(`static: true`\) - The query resolves once
      * the view has been created, but before change detection runs (before the `ngOnInit()` callback
      * is called). The result, though, will never be updated to reflect changes to your view, such as
-     * changes to `ngIf` and `ngFor` blocks. |
+     * changes to `ngIf` and `ngFor` blocks.
      *
      * @usageNotes
      *
