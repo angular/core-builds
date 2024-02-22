84282524d9e232886dbc9bba872eff586125e6eb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-22 01:58:54 +0000

    600a8cd release: cut the v17.2.2 release

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26070,7 +26070,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.1+sha-bb57d34");
+var VERSION2 = new Version("17.2.2+sha-600a8cd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
