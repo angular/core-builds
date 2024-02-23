ad84bf2d348528d093495039562b823029421df9
Joey Perrott <josephperrott@gmail.com>
2024-02-23 18:34:24 +0000

    812a972 docs(docs-infra): revert font-inlining (#54573)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -26001,7 +26001,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0-next.0+sha-3f95829");
+var VERSION2 = new Version("17.3.0-next.0+sha-812a972");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
