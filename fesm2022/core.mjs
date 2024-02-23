851ddceaa8af3f15a679fc579aa536b21618cda1
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 17:25:40 +0000

    1a526f2 perf(common): `AsyncPipe` should not call `markForCheck` on subscription (#54554)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.2+sha-600a8cd
+ * @license Angular v17.2.2+sha-1a526f2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16742,7 +16742,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.2+sha-600a8cd']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.2+sha-1a526f2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30674,7 +30674,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.2+sha-600a8cd');
+const VERSION = new Version('17.2.2+sha-1a526f2');
 
 class Console {
     log(message) {
