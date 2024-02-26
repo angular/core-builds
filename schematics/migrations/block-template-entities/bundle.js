93e86114b759e2933bb5496d3571da9c191bacdf
Joey Perrott <josephperrott@gmail.com>
2024-02-26 18:29:49 +0000

    54b5a2f ci: correct @jeanmeche to @JeanMeche in pullapprove (#54610)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26070,7 +26070,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0-next.0+sha-69948e1");
+var VERSION2 = new Version("17.3.0-next.0+sha-54b5a2f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
