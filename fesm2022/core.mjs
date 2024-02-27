d8495ac42b483f99ae3b635796f94232ab5ebf4a
Dylan Hunn <dylhunn@gmail.com>
2024-02-27 22:11:47 +0000

    6470e33 build: update io_bazel_rules_sass digest to 888dd9c (#54615)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.0+sha-3e0b3a1
+ * @license Angular v17.3.0-next.0+sha-6470e33
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16748,7 +16748,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-3e0b3a1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.0+sha-6470e33']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30744,7 +30744,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.0+sha-3e0b3a1');
+const VERSION = new Version('17.3.0-next.0+sha-6470e33');
 
 class Console {
     log(message) {
