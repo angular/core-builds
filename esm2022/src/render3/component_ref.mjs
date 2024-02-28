8d99d2adae452d7fd192f118c4d207a604a6260c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 13:43:40 +0000

    c05bef0 docs: update versioning and deprecation policies (#51423)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-3f0e892']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-c05bef0']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
