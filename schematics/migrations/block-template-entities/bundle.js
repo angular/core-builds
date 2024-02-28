da06b56c2723f527caeb37106b75b8e65c63fe10
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 13:25:02 +0000

    3f0e892 docs: add updating to v17 guide (#52410)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26070,7 +26070,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.3+sha-edc45d5");
+var VERSION2 = new Version("17.2.3+sha-3f0e892");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
