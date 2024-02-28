7f552b5378477fd02528ed143026b385a214d6f9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 11:53:13 +0000

    2e401c7 fix(docs-infra): include CLI reference docs in adev (#54591)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -26001,7 +26001,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0-next.1+sha-e923545");
+var VERSION2 = new Version("17.3.0-next.1+sha-2e401c7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
