9c3984055d57f9e322d9b3e436f71887ffd618c1
Dylan Hunn <dylhunn@gmail.com>
2024-02-28 00:16:56 +0000

    ccda258 release: cut the v17.3.0-next.1 release

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26070,7 +26070,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0-next.0+sha-df0e69b");
+var VERSION2 = new Version("17.3.0-next.1+sha-ccda258");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
