6277fdd03b9581785693b9410ad7c98ca90cbbcd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-29 13:59:21 +0000

    b322079 refactor(core): remove unused `EffectRef` import statement (#53863)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.1+sha-ffad7b8
+ * @license Angular v17.3.0-next.1+sha-b322079
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16835,7 +16835,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-ffad7b8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-b322079']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30833,7 +30833,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.1+sha-ffad7b8');
+const VERSION = new Version('17.3.0-next.1+sha-b322079');
 
 class Console {
     log(message) {
