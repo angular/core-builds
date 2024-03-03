3410d7bc32bbcd0ed44669129389d0097d33538c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-03 13:03:15 +0000

    3e703f5 docs: fix broken link on the tutorial page (#54669)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.1+sha-66d78a7
+ * @license Angular v17.3.0-next.1+sha-3e703f5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16835,7 +16835,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-66d78a7']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-3e703f5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30833,7 +30833,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.1+sha-66d78a7');
+const VERSION = new Version('17.3.0-next.1+sha-3e703f5');
 
 class Console {
     log(message) {
