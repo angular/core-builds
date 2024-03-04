45dc0b95ecc9e17857070551c2bf6f29036f2aa9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 09:07:51 +0000

    342737e docs(docs-infra): Update tutorial deps (#54677)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-0767adc']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-342737e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
