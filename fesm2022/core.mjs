c81c80c14a2d5d586e536985f300f61665b991f7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 10:16:24 +0000

    6ca8eca docs: queries as signals adev guide (#54505)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-e3bdab1
+ * @license Angular v17.2.3+sha-6ca8eca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15361,7 +15361,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-e3bdab1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-6ca8eca']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29472,7 +29472,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-e3bdab1');
+const VERSION = new Version('17.2.3+sha-6ca8eca');
 
 class Console {
     log(message) {
