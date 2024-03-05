cb80af0c73dea82430c4ad5f69257a0a2b0b64da
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-05 09:33:05 +0000

    b40f1e5 refactor(compiler): Remove deep imports in the language service (#54695)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-33a6fab']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-b40f1e5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.1+sha-33a6fab
+ * @license Angular v17.3.0-next.1+sha-b40f1e5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15402,7 +15402,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-33a6fab']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-b40f1e5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29513,7 +29513,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.1+sha-33a6fab');
+const VERSION = new Version('17.3.0-next.1+sha-b40f1e5');
 
 class Console {
     log(message) {
