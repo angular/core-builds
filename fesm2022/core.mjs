2b3a945ef0e1dbe59748d038441978306398bf2c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-05 17:21:18 +0000

    0ecf515 ci: fix ng-dev build for pr merging (#54707)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-next.1+sha-b40f1e5
+ * @license Angular v17.3.0-next.1+sha-0ecf515
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15402,7 +15402,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-b40f1e5']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-next.1+sha-0ecf515']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29513,7 +29513,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-next.1+sha-b40f1e5');
+const VERSION = new Version('17.3.0-next.1+sha-0ecf515');
 
 class Console {
     log(message) {
