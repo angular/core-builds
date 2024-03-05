d68a080f97b08420436ef92399f2e81b596ec650
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-05 18:16:20 +0000

    3b50885 docs: add process documentation to CARETAKER.md (#54664)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-6148d76']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-3b50885']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25986,7 +25986,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.3+sha-6148d76");
+var VERSION2 = new Version("17.2.3+sha-3b50885");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
