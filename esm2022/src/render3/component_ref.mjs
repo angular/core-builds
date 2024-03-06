a42b33cccb31485f1cd1f8051ff93510a362968c
Andrew Scott <atscott@google.com>
2024-03-06 21:40:08 +0000

    4d708b2 refactor(core): Fix internal test util types (#54736)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.4+sha-50623ea']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.4+sha-4d708b2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26055,7 +26055,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.4+sha-50623ea");
+var VERSION2 = new Version("17.2.4+sha-4d708b2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
