3bfb29f60adafa3a2af46169ecfe12637371061a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-06 09:23:02 +0000

    c75c168 fix(docs-infra): do not delete `BUILD.bazel` in help directory during updates (#54697)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.3+sha-3b50885
+ * @license Angular v17.2.3+sha-c75c168
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15361,7 +15361,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-3b50885']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.3+sha-c75c168']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29472,7 +29472,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.3+sha-3b50885');
+const VERSION = new Version('17.2.3+sha-c75c168');
 
 class Console {
     log(message) {
