98d35df42c3d55a61b71040e9e3747d6a09d8e3f
Andrew Scott <atscott@google.com>
2024-03-07 21:11:54 +0000

    115ee88 fix(router): Navigations triggered by cancellation events should cancel previous navigation (#54710)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-2787c50
+ * @license Angular v18.0.0-next.0+sha-115ee88
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15482,7 +15482,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-2787c50']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-115ee88']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29637,7 +29637,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-2787c50');
+const VERSION = new Version('18.0.0-next.0+sha-115ee88');
 
 class Console {
     log(message) {
