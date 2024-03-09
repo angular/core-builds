cd4a1e3d66952f95bf2a7aad2a546dde458c0d8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-09 00:55:32 +0000

    54340a9 refactor(compiler): Delete `TemplateDefinitionBuilder` acceptance test expectations (#54757)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-37d1f71
+ * @license Angular v18.0.0-next.0+sha-54340a9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15482,7 +15482,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-37d1f71']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-54340a9']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29637,7 +29637,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-37d1f71');
+const VERSION = new Version('18.0.0-next.0+sha-54340a9');
 
 class Console {
     log(message) {
