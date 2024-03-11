943c7e96ed44b1578b8a4c8fc2843e85b88eb84e
Andrew Scott <atscott@google.com>
2024-03-11 18:06:42 +0000

    4db321c docs: adding signal inputs and model to ADEV. (#54753)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.4+sha-4799a3b
+ * @license Angular v17.2.4+sha-4db321c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15361,7 +15361,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.4+sha-4799a3b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.4+sha-4db321c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29531,7 +29531,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.4+sha-4799a3b');
+const VERSION = new Version('17.2.4+sha-4db321c');
 
 class Console {
     log(message) {
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25989,7 +25989,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.4+sha-4799a3b");
+var VERSION2 = new Version("17.2.4+sha-4db321c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
