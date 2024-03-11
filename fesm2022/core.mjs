7bae82354db7718ed93d3e8eda20c8889cc85add
Andrew Scott <atscott@google.com>
2024-03-11 20:44:38 +0000

    57e54e2 docs: bump to 2024 for copyright text (#54822)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0-rc.0+sha-2909e98
+ * @license Angular v17.3.0-rc.0+sha-57e54e2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15557,7 +15557,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-rc.0+sha-2909e98']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0-rc.0+sha-57e54e2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29712,7 +29712,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0-rc.0+sha-2909e98');
+const VERSION = new Version('17.3.0-rc.0+sha-57e54e2');
 
 class Console {
     log(message) {
