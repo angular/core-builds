625b4f1c81d1bae46a52a7638752b98f1663cfe0
Andrew Scott <atscott@google.com>
2024-03-11 20:45:05 +0000

    e43b03f docs: bump to 2024 for copyright text (#54822)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25989,7 +25989,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.2.4+sha-e27b505");
+var VERSION2 = new Version("17.2.4+sha-e43b03f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
