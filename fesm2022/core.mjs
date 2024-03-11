ba8429f8fba7370bdd5a12086f16292578cfedee
Andrew Scott <atscott@google.com>
2024-03-11 16:04:20 +0000

    d269c88 refactor(core): avoid additional closure for queued microtask callback (#54801)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-484ae23
+ * @license Angular v18.0.0-next.0+sha-d269c88
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15482,7 +15482,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-484ae23']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-d269c88']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29637,7 +29637,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-484ae23');
+const VERSION = new Version('18.0.0-next.0+sha-d269c88');
 
 class Console {
     log(message) {
@@ -35733,9 +35733,7 @@ function queueStateUpdate(callback, options) {
         callback();
     };
     internalAfterNextRender(runCallbackOnce, { injector, runOnServer: true });
-    queueMicrotask(() => {
-        runCallbackOnce();
-    });
+    queueMicrotask(runCallbackOnce);
 }
 
 /**
--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -22730,7 +22730,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.0+sha-484ae23");
+var VERSION2 = new Version("18.0.0-next.0+sha-d269c88");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
