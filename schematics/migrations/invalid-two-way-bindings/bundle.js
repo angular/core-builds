50ed1ab64c302a19869621c338089dbb3fb275af
Andrew Scott <atscott@google.com>
2024-03-12 21:10:37 +0000

    edc1740 refactor(core): restructure logic in `isCssClassMatching` function (#54800)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -22681,7 +22681,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.0+sha-0f7bbd0");
+var VERSION2 = new Version("18.0.0-next.0+sha-edc1740");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
