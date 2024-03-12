624bebe9d5b82adc44179595fad6b292d31ec459
Andrew Scott <atscott@google.com>
2024-03-12 18:37:53 +0000

    db2f9a9 refactor(compiler-cli): cleanup unused code (#54775)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-4ff94aa
+ * @license Angular v18.0.0-next.0+sha-db2f9a9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15574,7 +15574,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-4ff94aa']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-db2f9a9']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29736,7 +29736,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-4ff94aa');
+const VERSION = new Version('18.0.0-next.0+sha-db2f9a9');
 
 class Console {
     log(message) {
