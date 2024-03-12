951946b228cdbb43c99d17a79d586a5e6c1f56a0
Andrew Scott <atscott@google.com>
2024-03-12 20:17:23 +0000

    77329b7 docs: fix missing security guide in navigation (#54830)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.2.4+sha-43ad12b
+ * @license Angular v17.2.4+sha-77329b7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15442,7 +15442,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.4+sha-43ad12b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.2.4+sha-77329b7']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29624,7 +29624,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.2.4+sha-43ad12b');
+const VERSION = new Version('17.2.4+sha-77329b7');
 
 class Console {
     log(message) {
