6abe4c8f085876ab0c122bd5aed157cb11aed1ee
Andrew Scott <atscott@google.com>
2024-03-13 18:40:57 +0000

    2d47329 build: update symbol goldens (#54843)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22523,7 +22523,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.0+sha-0dd441d");
+var VERSION2 = new Version("18.0.0-next.0+sha-2d47329");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
