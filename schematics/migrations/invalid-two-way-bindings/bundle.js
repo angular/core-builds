a327127f0809cc10fe422cf6579336b7145d33be
Andrew Scott <atscott@google.com>
2024-03-14 18:23:24 +0000

    2b80258 feat(router): Allow Route.redirectTo to be a function which returns a string or UrlTree (#52606)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -22681,7 +22681,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.0+sha-b71a323");
+var VERSION2 = new Version("18.0.0-next.0+sha-2b80258");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
