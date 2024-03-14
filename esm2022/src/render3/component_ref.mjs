3a2e68d4d02990da6a2f779c8fd011f0c11aeee2
Andrew Scott <atscott@google.com>
2024-03-14 19:37:46 +0000

    da906fd fix(router): Routed components never inherit `RouterOutlet` `EnvironmentInjector` (#54265)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-2b80258']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-da906fd']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
