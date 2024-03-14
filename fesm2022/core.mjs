cb656df7111e08726dd10df0f378b3649547eacc
Andrew Scott <atscott@google.com>
2024-03-14 16:00:27 +0000

    b71a323 docs: update Angular CLI help [main] (#54861)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-77a397b
+ * @license Angular v18.0.0-next.0+sha-b71a323
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15599,7 +15599,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-77a397b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-b71a323']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29773,7 +29773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-77a397b');
+const VERSION = new Version('18.0.0-next.0+sha-b71a323');
 
 class Console {
     log(message) {
