ba8a47b839c029a76a5a2e7496a853644fac1a2c
Joey Perrott <josephperrott@gmail.com>
2024-03-15 17:34:38 +0000

    e56c86f build: update actions/cache digest to ab5e6d0 (#54886)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -22681,7 +22681,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.0+sha-0472a92");
+var VERSION2 = new Version("18.0.0-next.0+sha-e56c86f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
