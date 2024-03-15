ddc0cad71b3c21fa7db94ace45068a3a1656d481
Joey Perrott <josephperrott@gmail.com>
2024-03-15 17:11:15 +0000

    0472a92 build: update cross-repo angular dependencies (#54883)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22523,7 +22523,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.0+sha-e473b70");
+var VERSION2 = new Version("18.0.0-next.0+sha-0472a92");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
