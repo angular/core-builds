1d02a1b7734076fc417ef533cfaedeac8a69935e
Joey Perrott <josephperrott@gmail.com>
2024-03-15 17:11:30 +0000

    90bbc68 build: update cross-repo angular dependencies (#54883)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26061,7 +26061,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0+sha-a5cc82e");
+var VERSION2 = new Version("17.3.0+sha-90bbc68");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
