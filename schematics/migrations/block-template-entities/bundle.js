b4d59cad61c95f7f0980730263bc34e61aaa9d25
Joey Perrott <josephperrott@gmail.com>
2024-03-15 18:37:15 +0000

    33dc072 build: update dependency google-closure-compiler to v20231112 (#54893)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26061,7 +26061,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0+sha-209632e");
+var VERSION2 = new Version("17.3.0+sha-33dc072");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
