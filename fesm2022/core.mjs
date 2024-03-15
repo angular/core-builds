3a9e06d52b494932246fbf9ee53f17bc8ad804dc
Joey Perrott <josephperrott@gmail.com>
2024-03-15 16:48:02 +0000

    e473b70 docs: update events (#54749)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-da906fd
+ * @license Angular v18.0.0-next.0+sha-e473b70
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15599,7 +15599,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-da906fd']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-e473b70']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29773,7 +29773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-da906fd');
+const VERSION = new Version('18.0.0-next.0+sha-e473b70');
 
 class Console {
     log(message) {
