f6427fc4bb78b8d9b809e349b060d4efbe22cc6d
Joey Perrott <josephperrott@gmail.com>
2024-03-15 18:23:25 +0000

    406448a build: update dependency archiver to v7 (#54892)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-9574eda
+ * @license Angular v18.0.0-next.0+sha-406448a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15599,7 +15599,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-9574eda']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-406448a']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29773,7 +29773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-9574eda');
+const VERSION = new Version('18.0.0-next.0+sha-406448a');
 
 class Console {
     log(message) {
