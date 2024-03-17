9ea0738fc4c3be2c762c47df15d74898837dcecd
Joey Perrott <josephperrott@gmail.com>
2024-03-17 00:46:51 +0000

    e61ccd2 build: update to latest angular/dev-infra/github-actions/create-pr-for-changes (#54904)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0+sha-80e5a0a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0+sha-e61ccd2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0+sha-80e5a0a
+ * @license Angular v17.3.0+sha-e61ccd2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15564,7 +15564,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0+sha-80e5a0a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0+sha-e61ccd2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29738,7 +29738,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0+sha-80e5a0a');
+const VERSION = new Version('17.3.0+sha-e61ccd2');
 
 class Console {
     log(message) {
