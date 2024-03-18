c88bfb71d8b92dc8735038a072f1eac2735c566d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-18 18:08:36 +0000

    860ecce ci: move saucelabs to regular CI job (#54926)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25998,7 +25998,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.0+sha-e61ccd2");
+var VERSION2 = new Version("17.3.0+sha-860ecce");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
