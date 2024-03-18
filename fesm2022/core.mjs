30dfb71182dba4d1ca9b3267400a6ca9ebdd78eb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-18 18:07:32 +0000

    0be64c1 ci: move saucelabs to regular CI job (#54926)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-f50afb1
+ * @license Angular v18.0.0-next.0+sha-0be64c1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15599,7 +15599,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-f50afb1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-0be64c1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29773,7 +29773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-f50afb1');
+const VERSION = new Version('18.0.0-next.0+sha-0be64c1');
 
 class Console {
     log(message) {
