cfac4f5359d7b5b90c18fac026d0ca5f4ab11eb4
Joey Perrott <josephperrott@gmail.com>
2024-03-18 19:10:59 +0000

    60dbc2b fix(docs-infra): remove part aio infra

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.0+sha-ce5bb97
+ * @license Angular v18.0.0-next.0+sha-60dbc2b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15599,7 +15599,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-ce5bb97']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-60dbc2b']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29773,7 +29773,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.0+sha-ce5bb97');
+const VERSION = new Version('18.0.0-next.0+sha-60dbc2b');
 
 class Console {
     log(message) {
