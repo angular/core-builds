8cf2a18dc35d3a40fc51ffe4b2c2b89032583105
Joey Perrott <josephperrott@gmail.com>
2024-03-19 17:07:14 +0000

    766bdf3 build: update dependency google-closure-compiler to v20240317 (#54931)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.0+sha-e70228a
+ * @license Angular v17.3.0+sha-766bdf3
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15977,7 +15977,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0+sha-e70228a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.0+sha-766bdf3']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29741,7 +29741,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.0+sha-e70228a');
+const VERSION = new Version('17.3.0+sha-766bdf3');
 
 class Console {
     log(message) {
