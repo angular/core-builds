3fe637bfb8fba51ce0f09e3d6686c8b5aa05de30
Andrew Scott <atscott01@gmail.com>
2024-03-20 22:05:43 +0000

    0461bff release: cut the v18.0.0-next.1 release

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.0+sha-a9f5a33']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.1+sha-0461bff']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
