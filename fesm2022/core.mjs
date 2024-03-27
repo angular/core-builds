743ada6f5a02559d01ac9e86d2a712f445ae9817
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-03-27 12:03:50 +0000

    adf3ee8 ci: fix build failures in patch branch (#55064)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-d15dca0
+ * @license Angular v18.0.0-next.1+sha-adf3ee8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16085,7 +16085,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.1+sha-d15dca0']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.1+sha-adf3ee8']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29929,7 +29929,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-d15dca0');
+const VERSION = new Version('18.0.0-next.1+sha-adf3ee8');
 
 class Console {
     log(message) {
