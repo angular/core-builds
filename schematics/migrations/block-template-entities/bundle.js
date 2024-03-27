c2f0c60ed54e070b12e3f1f02621b36c9463655a
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:24:41 +0000

    93ce4d0 refactor(compiler-cli): properly preserve file overview comments (#54983)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26068,7 +26068,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.1+sha-d1d9f55");
+var VERSION2 = new Version("17.3.1+sha-93ce4d0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
