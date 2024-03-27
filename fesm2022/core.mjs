c5ea1cb07a042f07795f02787c917ba50e8116a8
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 18:13:40 +0000

    9e60609 docs: explain difference between @placeholder and @loading (#54779)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-658cf8c
+ * @license Angular v18.0.0-next.1+sha-9e60609
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16085,7 +16085,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.1+sha-658cf8c']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.1+sha-9e60609']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29929,7 +29929,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-658cf8c');
+const VERSION = new Version('18.0.0-next.1+sha-9e60609');
 
 class Console {
     log(message) {
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -22656,7 +22656,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.1+sha-658cf8c");
+var VERSION2 = new Version("18.0.0-next.1+sha-9e60609");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
