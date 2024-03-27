b2349c143e715f75b27c1c813a4a9fd50f70af29
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-03-27 12:04:19 +0000

    9aff43b ci: fix build failures in patch branch (#55064)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25999,7 +25999,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.1+sha-fa46844");
+var VERSION2 = new Version("17.3.1+sha-9aff43b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
