a4ab7aa2082edb8cf4110c18e2fadba6034f1c9e
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:45:12 +0000

    a893f0b fix(zone.js): should not clear onhandler when remove capture listener (#54602)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.1+sha-93ce4d0
+ * @license Angular v17.3.1+sha-a893f0b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15977,7 +15977,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.1+sha-93ce4d0']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.1+sha-a893f0b']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29741,7 +29741,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.1+sha-93ce4d0');
+const VERSION = new Version('17.3.1+sha-a893f0b');
 
 class Console {
     log(message) {
