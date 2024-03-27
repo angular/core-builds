484af82a048b5f815ef01f21babdeb0b5946a17d
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 21:33:23 +0000

    e02bcf8 fix(core): Fix clearing of pending task in zoneless cleanup implementation (#55074)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.1+sha-80f96e7
+ * @license Angular v18.0.0-next.1+sha-e02bcf8
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16093,7 +16093,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.1+sha-80f96e7']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.1+sha-e02bcf8']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29937,7 +29937,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.1+sha-80f96e7');
+const VERSION = new Version('18.0.0-next.1+sha-e02bcf8');
 
 class Console {
     log(message) {
@@ -32040,7 +32040,7 @@ class ChangeDetectionSchedulerImpl {
         // before removing it from the pending tasks (or the tasks service should
         // not synchronously emit stable, similar to how Zone stableness only
         // happens if it's still stable after a microtask).
-        if (this.pendingRenderTaskId) {
+        if (this.pendingRenderTaskId !== null) {
             const taskId = this.pendingRenderTaskId;
             this.pendingRenderTaskId = null;
             this.taskService.remove(taskId);
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -22656,7 +22656,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.1+sha-80f96e7");
+var VERSION2 = new Version("18.0.0-next.1+sha-e02bcf8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
