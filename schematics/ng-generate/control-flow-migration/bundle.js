14fe0194dd3d75f8ff641dd99bea21288eb15280
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:02:35 +0000

    14235b7 docs: update compat table with 17.3 (#54882)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22587,7 +22587,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.1+sha-adf3ee8");
+var VERSION2 = new Version("18.0.0-next.1+sha-14235b7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
