4be3797781806beb9390f4110597d5f8b3ab3ec4
Dylan Hunn <dylhunn@gmail.com>
2024-03-28 17:09:51 +0000

    6d14fc5 release: cut the v18.0.0-next.2 release

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.1+sha-a64eca2']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-6d14fc5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
