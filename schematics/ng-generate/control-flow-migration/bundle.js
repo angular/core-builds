77f5f84cce172193565f29868aedb5aa779f2a28
Dylan Hunn <dylhunn@gmail.com>
2024-03-28 17:35:15 +0000

    ddbf6bb fix(zone.js): make sure fakeasync use the same id pool with native (#54600)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22587,7 +22587,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.2+sha-6d14fc5");
+var VERSION2 = new Version("18.0.0-next.2+sha-ddbf6bb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
