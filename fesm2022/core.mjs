50cc8c2a186c9a5a00ef98220a78b964d638b53d
Andrew Scott <atscott@google.com>
2024-03-28 22:11:49 +0000

    fac8d34 docs: remove broken link from CONTRIBUTING.md (#54953)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-22dd52f
+ * @license Angular v18.0.0-next.2+sha-fac8d34
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16093,7 +16093,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-22dd52f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-fac8d34']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29951,7 +29951,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-22dd52f');
+const VERSION = new Version('18.0.0-next.2+sha-fac8d34');
 
 class Console {
     log(message) {
