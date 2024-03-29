918584585329187e87f0f5923eb75e54a19f579f
Andrew Scott <atscott@google.com>
2024-03-29 16:44:59 +0000

    e53d02d docs(http): Update the Setup for testing in Http Client to include provideHttpClient() (#53716)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26068,7 +26068,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.2+sha-109df12");
+var VERSION2 = new Version("17.3.2+sha-e53d02d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
