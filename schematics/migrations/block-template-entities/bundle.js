a19fd0a93c0674f87b1ee09e36b333a5243c2ae0
Andrew Scott <atscott@google.com>
2024-03-29 23:42:24 +0000

    7d4dd08 test(core): Update hybrid mode scheduling test to wait for fixture stable (#55076)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -22656,7 +22656,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.2+sha-0c20c40");
+var VERSION2 = new Version("18.0.0-next.2+sha-7d4dd08");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
