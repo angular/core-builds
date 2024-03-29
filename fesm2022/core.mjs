691de71255deeb372fc97fc1dab3a73d29586dee
Andrew Scott <atscott@google.com>
2024-03-29 16:50:41 +0000

    218c5bd build: update dependency @babel/core to v7.24.3 (#55038)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-112fae5
+ * @license Angular v18.0.0-next.2+sha-218c5bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16735,7 +16735,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-112fae5']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-218c5bd']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30374,7 +30374,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-112fae5');
+const VERSION = new Version('18.0.0-next.2+sha-218c5bd');
 
 class Console {
     log(message) {
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-112fae5
+ * @license Angular v18.0.0-next.2+sha-218c5bd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
