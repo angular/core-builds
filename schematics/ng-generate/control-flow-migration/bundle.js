60f2d362c0e4ebae2e3c71942eb64b3c240b4424
Andrew Scott <atscott@google.com>
2024-03-29 20:16:45 +0000

    0c20c40 fix(migrations): avoid conflicts with some greek letters in control flow migration (#55113)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -10726,7 +10726,7 @@ var OBJECT = "object";
 var SCHEMA = [
   "[Element]|textContent,%ariaAtomic,%ariaAutoComplete,%ariaBusy,%ariaChecked,%ariaColCount,%ariaColIndex,%ariaColSpan,%ariaCurrent,%ariaDescription,%ariaDisabled,%ariaExpanded,%ariaHasPopup,%ariaHidden,%ariaKeyShortcuts,%ariaLabel,%ariaLevel,%ariaLive,%ariaModal,%ariaMultiLine,%ariaMultiSelectable,%ariaOrientation,%ariaPlaceholder,%ariaPosInSet,%ariaPressed,%ariaReadOnly,%ariaRelevant,%ariaRequired,%ariaRoleDescription,%ariaRowCount,%ariaRowIndex,%ariaRowSpan,%ariaSelected,%ariaSetSize,%ariaSort,%ariaValueMax,%ariaValueMin,%ariaValueNow,%ariaValueText,%classList,className,elementTiming,id,innerHTML,*beforecopy,*beforecut,*beforepaste,*fullscreenchange,*fullscreenerror,*search,*webkitfullscreenchange,*webkitfullscreenerror,outerHTML,%part,#scrollLeft,#scrollTop,slot,*message,*mozfullscreenchange,*mozfullscreenerror,*mozpointerlockchange,*mozpointerlockerror,*webglcontextcreationerror,*webglcontextlost,*webglcontextrestored",
   "[HTMLElement]^[Element]|accessKey,autocapitalize,!autofocus,contentEditable,dir,!draggable,enterKeyHint,!hidden,!inert,innerText,inputMode,lang,nonce,*abort,*animationend,*animationiteration,*animationstart,*auxclick,*beforexrselect,*blur,*cancel,*canplay,*canplaythrough,*change,*click,*close,*contextmenu,*copy,*cuechange,*cut,*dblclick,*drag,*dragend,*dragenter,*dragleave,*dragover,*dragstart,*drop,*durationchange,*emptied,*ended,*error,*focus,*formdata,*gotpointercapture,*input,*invalid,*keydown,*keypress,*keyup,*load,*loadeddata,*loadedmetadata,*loadstart,*lostpointercapture,*mousedown,*mouseenter,*mouseleave,*mousemove,*mouseout,*mouseover,*mouseup,*mousewheel,*paste,*pause,*play,*playing,*pointercancel,*pointerdown,*pointerenter,*pointerleave,*pointermove,*pointerout,*pointerover,*pointerrawupdate,*pointerup,*progress,*ratechange,*reset,*resize,*scroll,*securitypolicyviolation,*seeked,*seeking,*select,*selectionchange,*selectstart,*slotchange,*stalled,*submit,*suspend,*timeupdate,*toggle,*transitioncancel,*transitionend,*transitionrun,*transitionstart,*volumechange,*waiting,*webkitanimationend,*webkitanimationiteration,*webkitanimationstart,*webkittransitionend,*wheel,outerText,!spellcheck,%style,#tabIndex,title,!translate,virtualKeyboardPolicy",
-  "abbr,address,article,aside,b,bdi,bdo,cite,content,code,dd,dfn,dt,em,figcaption,figure,footer,header,hgroup,i,kbd,main,mark,nav,noscript,rb,rp,rt,rtc,ruby,s,samp,search,section,small,strong,sub,sup,u,var,wbr^[HTMLElement]|accessKey,autocapitalize,!autofocus,contentEditable,dir,!draggable,enterKeyHint,!hidden,innerText,inputMode,lang,nonce,*abort,*animationend,*animationiteration,*animationstart,*auxclick,*beforexrselect,*blur,*cancel,*canplay,*canplaythrough,*change,*click,*close,*contextmenu,*copy,*cuechange,*cut,*dblclick,*drag,*dragend,*dragenter,*dragleave,*dragover,*dragstart,*drop,*durationchange,*emptied,*ended,*error,*focus,*formdata,*gotpointercapture,*input,*invalid,*keydown,*keypress,*keyup,*load,*loadeddata,*loadedmetadata,*loadstart,*lostpointercapture,*mousedown,*mouseenter,*mouseleave,*mousemove,*mouseout,*mouseover,*mouseup,*mousewheel,*paste,*pause,*play,*playing,*pointercancel,*pointerdown,*pointerenter,*pointerleave,*pointermove,*pointerout,*pointerover,*pointerrawupdate,*pointerup,*progress,*ratechange,*reset,*resize,*scroll,*securitypolicyviolation,*seeked,*seeking,*select,*selectionchange,*selectstart,*slotchange,*stalled,*submit,*suspend,*timeupdate,*toggle,*transitioncancel,*transitionend,*transitionrun,*transitionstart,*volumechange,*waiting,*webkitanimationend,*webkitanimationiteration,*webkitanimationstart,*webkittransitionend,*wheel,outerText,!spellcheck,%style,#tabIndex,title,!translate,virtualKeyboardPolicy",
+  "abbr,address,article,aside,b,bdi,bdo,cite,content,code,dd,dfn,dt,em,figcaption,figure,footer,header,hgroup,i,kbd,main,mark,nav,noscript,rb,rp,rt,rtc,ruby,s,samp,section,small,strong,sub,sup,u,var,wbr^[HTMLElement]|accessKey,autocapitalize,!autofocus,contentEditable,dir,!draggable,enterKeyHint,!hidden,innerText,inputMode,lang,nonce,*abort,*animationend,*animationiteration,*animationstart,*auxclick,*beforexrselect,*blur,*cancel,*canplay,*canplaythrough,*change,*click,*close,*contextmenu,*copy,*cuechange,*cut,*dblclick,*drag,*dragend,*dragenter,*dragleave,*dragover,*dragstart,*drop,*durationchange,*emptied,*ended,*error,*focus,*formdata,*gotpointercapture,*input,*invalid,*keydown,*keypress,*keyup,*load,*loadeddata,*loadedmetadata,*loadstart,*lostpointercapture,*mousedown,*mouseenter,*mouseleave,*mousemove,*mouseout,*mouseover,*mouseup,*mousewheel,*paste,*pause,*play,*playing,*pointercancel,*pointerdown,*pointerenter,*pointerleave,*pointermove,*pointerout,*pointerover,*pointerrawupdate,*pointerup,*progress,*ratechange,*reset,*resize,*scroll,*securitypolicyviolation,*seeked,*seeking,*select,*selectionchange,*selectstart,*slotchange,*stalled,*submit,*suspend,*timeupdate,*toggle,*transitioncancel,*transitionend,*transitionrun,*transitionstart,*volumechange,*waiting,*webkitanimationend,*webkitanimationiteration,*webkitanimationstart,*webkittransitionend,*wheel,outerText,!spellcheck,%style,#tabIndex,title,!translate,virtualKeyboardPolicy",
   "media^[HTMLElement]|!autoplay,!controls,%controlsList,%crossOrigin,#currentTime,!defaultMuted,#defaultPlaybackRate,!disableRemotePlayback,!loop,!muted,*encrypted,*waitingforkey,#playbackRate,preload,!preservesPitch,src,%srcObject,#volume",
   ":svg:^[HTMLElement]|!autofocus,nonce,*abort,*animationend,*animationiteration,*animationstart,*auxclick,*beforexrselect,*blur,*cancel,*canplay,*canplaythrough,*change,*click,*close,*contextmenu,*copy,*cuechange,*cut,*dblclick,*drag,*dragend,*dragenter,*dragleave,*dragover,*dragstart,*drop,*durationchange,*emptied,*ended,*error,*focus,*formdata,*gotpointercapture,*input,*invalid,*keydown,*keypress,*keyup,*load,*loadeddata,*loadedmetadata,*loadstart,*lostpointercapture,*mousedown,*mouseenter,*mouseleave,*mousemove,*mouseout,*mouseover,*mouseup,*mousewheel,*paste,*pause,*play,*playing,*pointercancel,*pointerdown,*pointerenter,*pointerleave,*pointermove,*pointerout,*pointerover,*pointerrawupdate,*pointerup,*progress,*ratechange,*reset,*resize,*scroll,*securitypolicyviolation,*seeked,*seeking,*select,*selectionchange,*selectstart,*slotchange,*stalled,*submit,*suspend,*timeupdate,*toggle,*transitioncancel,*transitionend,*transitionrun,*transitionstart,*volumechange,*waiting,*webkitanimationend,*webkitanimationiteration,*webkitanimationstart,*webkittransitionend,*wheel,%style,#tabIndex",
   ":svg:graphics^:svg:|",
@@ -10792,7 +10792,6 @@ var SCHEMA = [
   "source^[HTMLElement]|#height,media,sizes,src,srcset,type,#width",
   "span^[HTMLElement]|",
   "style^[HTMLElement]|!disabled,media,type",
-  "search^[HTMLELement]|",
   "caption^[HTMLElement]|align",
   "th,td^[HTMLElement]|abbr,align,axis,bgColor,ch,chOff,#colSpan,headers,height,!noWrap,#rowSpan,scope,vAlign,width",
   "col,colgroup^[HTMLElement]|align,ch,chOff,#span,vAlign,width",
@@ -22588,7 +22587,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.2+sha-d3c75f8");
+var VERSION2 = new Version("18.0.0-next.2+sha-0c20c40");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
@@ -23165,6 +23164,15 @@ function validateI18nStructure(parsed, fileName) {
 function isChildOf(parent, el) {
   return parent.sourceSpan.start.offset < el.sourceSpan.start.offset && parent.sourceSpan.end.offset > el.sourceSpan.end.offset;
 }
+var PlaceholderKind;
+(function(PlaceholderKind2) {
+  PlaceholderKind2[PlaceholderKind2["Default"] = 0] = "Default";
+  PlaceholderKind2[PlaceholderKind2["Alternate"] = 1] = "Alternate";
+})(PlaceholderKind || (PlaceholderKind = {}));
+function getPlaceholder(value, kind = PlaceholderKind.Default) {
+  const name = `<<<\u0275\u0275ngControlFlowMigration_${kind}\u0275\u0275>>>`;
+  return `___${name}${value}${name}___`;
+}
 function calculateNesting(visitor, hasLineBreaks2) {
   let nestedQueue = [];
   for (let i = 0; i < visitor.elements.length; i++) {
@@ -23233,8 +23241,8 @@ function processNgTemplates(template2) {
   try {
     const templates = getTemplates(template2);
     for (const [name, t] of templates) {
-      const replaceRegex = new RegExp(`\u03B8${name.slice(1)}\\\u03B4`, "g");
-      const forRegex = new RegExp(`\u03B8${name.slice(1)}\\\u03C6`, "g");
+      const replaceRegex = new RegExp(getPlaceholder(name.slice(1)), "g");
+      const forRegex = new RegExp(getPlaceholder(name.slice(1), PlaceholderKind.Alternate), "g");
       const forMatches = [...template2.matchAll(forRegex)];
       const matches = [...forMatches, ...template2.matchAll(replaceRegex)];
       let safeToRemove = true;
@@ -23267,11 +23275,14 @@ function processNgTemplates(template2) {
   }
 }
 function replaceRemainingPlaceholders(template2) {
-  const replaceRegex = new RegExp(`\u03B8.*\u03B4`, "g");
+  const pattern = ".*";
+  const placeholderPattern = getPlaceholder(pattern);
+  const replaceRegex = new RegExp(placeholderPattern, "g");
+  const [placeholderStart, placeholderEnd] = placeholderPattern.split(pattern);
   const placeholders = [...template2.matchAll(replaceRegex)];
   for (let ph of placeholders) {
     const placeholder = ph[0];
-    const name = placeholder.slice(1, placeholder.length - 1);
+    const name = placeholder.slice(placeholderStart.length, placeholder.length - placeholderEnd.length);
     template2 = template2.replace(placeholder, `<ng-template [ngTemplateOutlet]="${name}"></ng-template>`);
   }
   return template2;
@@ -23634,7 +23645,7 @@ function migrateStandardNgFor(etm, tmpl, offset) {
       trackBy = `${trackByFn}($index, ${loopVar})`;
     }
     if (part.startsWith("template:")) {
-      tmplPlaceholder = `\u03B8${part.split(":")[1].trim()}\u03C6`;
+      tmplPlaceholder = getPlaceholder(part.split(":")[1].trim(), PlaceholderKind.Alternate);
     }
     if (part.match(aliasWithEqualRegexp)) {
       const aliasParts = part.split("=");
@@ -23811,7 +23822,7 @@ function buildIfBlock(etm, tmpl, offset) {
 }
 function buildStandardIfElseBlock(etm, tmpl, elseString, offset) {
   const condition = etm.getCondition().replace(" as ", "; as ").replace(/;\s*let/g, "; as");
-  const elsePlaceholder = `\u03B8${etm.getTemplateName(elseString)}\u03B4`;
+  const elsePlaceholder = getPlaceholder(etm.getTemplateName(elseString));
   return buildIfElseBlock(etm, tmpl, condition, elsePlaceholder, offset);
 }
 function buildBoundIfElseBlock(etm, tmpl, offset) {
@@ -23826,9 +23837,9 @@ function buildBoundIfElseBlock(etm, tmpl, offset) {
   } else if (aliases.length === 1) {
     condition += `; as ${aliases[0]}`;
   }
-  const elsePlaceholder = `\u03B8${etm.elseAttr.value.trim()}\u03B4`;
+  const elsePlaceholder = getPlaceholder(etm.elseAttr.value.trim());
   if (etm.thenAttr !== void 0) {
-    const thenPlaceholder = `\u03B8${etm.thenAttr.value.trim()}\u03B4`;
+    const thenPlaceholder = getPlaceholder(etm.thenAttr.value.trim());
     return buildIfThenElseBlock(etm, tmpl, condition, thenPlaceholder, elsePlaceholder, offset);
   }
   return buildIfElseBlock(etm, tmpl, condition, elsePlaceholder, offset);
@@ -23850,13 +23861,13 @@ function buildIfElseBlock(etm, tmpl, condition, elsePlaceholder, offset) {
 }
 function buildStandardIfThenElseBlock(etm, tmpl, thenString, elseString, offset) {
   const condition = etm.getCondition().replace(" as ", "; as ").replace(/;\s*let/g, "; as");
-  const thenPlaceholder = `\u03B8${etm.getTemplateName(thenString, elseString)}\u03B4`;
-  const elsePlaceholder = `\u03B8${etm.getTemplateName(elseString)}\u03B4`;
+  const thenPlaceholder = getPlaceholder(etm.getTemplateName(thenString, elseString));
+  const elsePlaceholder = getPlaceholder(etm.getTemplateName(elseString));
   return buildIfThenElseBlock(etm, tmpl, condition, thenPlaceholder, elsePlaceholder, offset);
 }
 function buildStandardIfThenBlock(etm, tmpl, thenString, offset) {
   const condition = etm.getCondition().replace(" as ", "; as ").replace(/;\s*let/g, "; as");
-  const thenPlaceholder = `\u03B8${etm.getTemplateName(thenString)}\u03B4`;
+  const thenPlaceholder = getPlaceholder(etm.getTemplateName(thenString));
   return buildIfThenBlock(etm, tmpl, condition, thenPlaceholder, offset);
 }
 function buildIfThenElseBlock(etm, tmpl, condition, thenPlaceholder, elsePlaceholder, offset) {
