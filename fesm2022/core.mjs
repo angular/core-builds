d04529658a604c65799483a044072746846bc11b
Andrew Scott <atscott@google.com>
2024-03-29 17:19:43 +0000

    a122bd1 build: update dependency gulp to v5 (#55108)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-367b3ee
+ * @license Angular v18.0.0-next.2+sha-a122bd1
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16735,7 +16735,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-367b3ee']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-a122bd1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30374,7 +30374,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-367b3ee');
+const VERSION = new Version('18.0.0-next.2+sha-a122bd1');
 
 class Console {
     log(message) {
