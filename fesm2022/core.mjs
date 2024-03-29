4d6d7c4f3d90c0c9ac168308d8099617b8f0d477
Andrew Scott <atscott@google.com>
2024-03-29 16:21:51 +0000

    860de66 refactor(router): Navigation promise should resolve `false` instead of `null` on skip (#55068)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-0b450ff
+ * @license Angular v18.0.0-next.2+sha-860de66
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16074,7 +16074,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-0b450ff']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-860de66']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29932,7 +29932,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-0b450ff');
+const VERSION = new Version('18.0.0-next.2+sha-860de66');
 
 class Console {
     log(message) {
