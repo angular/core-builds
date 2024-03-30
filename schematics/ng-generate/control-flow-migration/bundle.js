026d1219b31c950e45842bed849431b57d002d9d
Andrew Scott <atscott@google.com>
2024-03-30 02:10:19 +0000

    a84beaa fix(docs-infra): scrolling experience in API Reference (#55133)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25999,7 +25999,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.2+sha-fd45d03");
+var VERSION2 = new Version("17.3.2+sha-a84beaa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
