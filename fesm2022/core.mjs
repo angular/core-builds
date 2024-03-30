2fc1964a1b4df0dcc3a8e3ba0051dc0c9d582789
Andrew Scott <atscott@google.com>
2024-03-30 17:56:32 +0000

    87f3f27 feat(router): Allow resolvers to return `RedirectCommand` (#54556)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-8a8181a
+ * @license Angular v18.0.0-next.2+sha-87f3f27
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16735,7 +16735,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-8a8181a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-87f3f27']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30374,7 +30374,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-8a8181a');
+const VERSION = new Version('18.0.0-next.2+sha-87f3f27');
 
 class Console {
     log(message) {
