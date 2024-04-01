1ce9a8829cddff8b8237fce9260ba0ccd4b5b4a8
Andrew Scott <atscott@google.com>
2024-04-01 17:04:36 +0000

    bc429ba ci: Update workflows to use latest dev infra hash (#55128)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26068,7 +26068,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.2+sha-a84beaa");
+var VERSION2 = new Version("17.3.2+sha-bc429ba");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
