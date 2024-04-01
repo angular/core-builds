9919e69fb63afbd47f4466433aaf805b3d1413ab
Andrew Scott <atscott@google.com>
2024-04-01 16:23:21 +0000

    3839cfb fix(router): Routed components never inherit `RouterOutlet` `EnvironmentInjector` (#54265)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-87f3f27
+ * @license Angular v18.0.0-next.2+sha-3839cfb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16735,7 +16735,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-87f3f27']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-3839cfb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30374,7 +30374,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-87f3f27');
+const VERSION = new Version('18.0.0-next.2+sha-3839cfb');
 
 class Console {
     log(message) {
