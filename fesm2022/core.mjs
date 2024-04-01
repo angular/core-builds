4996e778bde8d486f5fadca3c2a2465684e56673
Andrew Scott <atscott@google.com>
2024-04-01 16:52:06 +0000

    940a358 refactor(router): prevent navigationErrorHandler from breaking transition subscription (#55152)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.2+sha-3839cfb
+ * @license Angular v18.0.0-next.2+sha-940a358
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16735,7 +16735,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-3839cfb']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.2+sha-940a358']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30374,7 +30374,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.2+sha-3839cfb');
+const VERSION = new Version('18.0.0-next.2+sha-940a358');
 
 class Console {
     log(message) {
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -22656,7 +22656,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.2+sha-3839cfb");
+var VERSION2 = new Version("18.0.0-next.2+sha-940a358");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
