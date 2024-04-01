f412b9a6c76a43e70864799dc8f90738eea3c89c
Andrew Scott <atscott@google.com>
2024-04-01 17:57:00 +0000

    e18a0ed fix(devtools): fix padding in property tree view (#54648)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22587,7 +22587,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.2+sha-0ceefae");
+var VERSION2 = new Version("18.0.0-next.2+sha-e18a0ed");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
