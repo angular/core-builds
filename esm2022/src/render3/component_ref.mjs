37098fb572a1d2bc5e86f2d5dc86290c3a717768
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 18:32:05 +0000

    5ba2a48 ci: fixes jsaction path name (#55199)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.2+sha-88f0545']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.2+sha-5ba2a48']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
