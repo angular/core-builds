2b473f706eed9c7405a54377dfd6f6c59bc34894
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 18:12:31 +0000

    2656fd9 refactor(core): remove unnecessary folder (#55197)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22587,7 +22587,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.2+sha-1c736dc");
+var VERSION2 = new Version("18.0.0-next.2+sha-2656fd9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
