d912ceb1571b0123df0db59689ac810094544faf
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-05 14:59:26 +0000

    58e2f47 test(common): update ImageKit low quality test (#55193)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.3+sha-8cd1b23
+ * @license Angular v18.0.0-next.3+sha-58e2f47
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16725,7 +16725,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.3+sha-8cd1b23']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.3+sha-58e2f47']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30363,7 +30363,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.3+sha-8cd1b23');
+const VERSION = new Version('18.0.0-next.3+sha-58e2f47');
 
 class Console {
     log(message) {
