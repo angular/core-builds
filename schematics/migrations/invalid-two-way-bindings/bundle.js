064e00ace796ac881551dbe743b96245b04dc818
Andrew Scott <atscott@google.com>
2024-04-05 18:17:07 +0000

    a84cb62 build: lock file maintenance (#55145)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -22730,7 +22730,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.3+sha-59c7538");
+var VERSION2 = new Version("18.0.0-next.3+sha-a84cb62");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
