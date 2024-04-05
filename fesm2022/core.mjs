11d3d811c41d91003770ffdef56686d64017a8bf
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-05 04:31:28 +0000

    8cd1b23 ci: pullapprove is case sensitive (#55220)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.3+sha-39624c6
+ * @license Angular v18.0.0-next.3+sha-8cd1b23
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16725,7 +16725,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.3+sha-39624c6']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.3+sha-8cd1b23']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30363,7 +30363,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.3+sha-39624c6');
+const VERSION = new Version('18.0.0-next.3+sha-8cd1b23');
 
 class Console {
     log(message) {
