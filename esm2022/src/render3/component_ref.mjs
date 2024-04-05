f2dd90d69b9571a3888250f3f57bc22b661aebaf
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-05 14:59:49 +0000

    80d1094 test(common): update ImageKit low quality test (#55193)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-9cb1d27']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-80d1094']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -26157,7 +26157,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.3+sha-9cb1d27");
+var VERSION2 = new Version("17.3.3+sha-80d1094");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
