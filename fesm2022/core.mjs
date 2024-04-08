7bb142096eb91da2e0ca8e19e84f939f55fa4773
Andrew Scott <atscott@google.com>
2024-04-08 17:48:19 +0000

    3583d67 build: update dependency madge to v7 (#55245)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.3+sha-a84cb62
+ * @license Angular v18.0.0-next.3+sha-3583d67
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16737,7 +16737,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.3+sha-a84cb62']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.3+sha-3583d67']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30375,7 +30375,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.3+sha-a84cb62');
+const VERSION = new Version('18.0.0-next.3+sha-3583d67');
 
 class Console {
     log(message) {
