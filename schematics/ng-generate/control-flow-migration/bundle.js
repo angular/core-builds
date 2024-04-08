98bc0f9a5f00d8b4ea5d1267f69e1bfed5a7b28e
Andrew Scott <atscott@google.com>
2024-04-08 18:54:29 +0000

    87cdfaf docs: replace `code-example` with `docs-code` (#55186)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22571,7 +22571,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.3+sha-f8d5921");
+var VERSION2 = new Version("18.0.0-next.3+sha-87cdfaf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
