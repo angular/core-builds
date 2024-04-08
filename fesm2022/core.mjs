e89ed8f5ff89d1ebf9297dbfb036c162fdaac81b
Andrew Scott <atscott@google.com>
2024-04-08 20:53:22 +0000

    5826d8c docs: fix typo in event-binding documentation (#55189) PR Close #55189

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.3+sha-a7c1115
+ * @license Angular v17.3.3+sha-5826d8c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16022,7 +16022,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-a7c1115']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-5826d8c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29830,7 +29830,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.3+sha-a7c1115');
+const VERSION = new Version('17.3.3+sha-5826d8c');
 
 class Console {
     log(message) {
