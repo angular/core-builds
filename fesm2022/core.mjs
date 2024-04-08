ebfbe6df747bff6130515a2f94eed7cb1b187db5
Andrew Scott <atscott@google.com>
2024-04-08 18:54:23 +0000

    9f06915 docs: replace `code-example` with `docs-code` (#55186)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.3+sha-4d2a800
+ * @license Angular v17.3.3+sha-9f06915
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16022,7 +16022,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-4d2a800']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-9f06915']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29830,7 +29830,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.3+sha-4d2a800');
+const VERSION = new Version('17.3.3+sha-9f06915');
 
 class Console {
     log(message) {
