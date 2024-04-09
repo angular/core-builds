c9471dfa13204158a521290360b4434bde687bc5
Andrew Scott <atscott@google.com>
2024-04-09 18:27:34 +0000

    bc394de docs: fix input name (#55141)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.3+sha-3236111']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.3+sha-bc394de']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
