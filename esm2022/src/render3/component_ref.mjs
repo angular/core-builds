77ea8d32c0c60187493886c3c3deaac106101535
Andrew Scott <atscott@google.com>
2024-04-10 18:04:10 +0000

    ce231fb ci: Merge primitives-shared and primitives-event-dispatch groups (#55273)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-888360a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-ce231fb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.3+sha-888360a
+ * @license Angular v17.3.3+sha-ce231fb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16022,7 +16022,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-888360a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.3+sha-ce231fb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29830,7 +29830,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.3+sha-888360a');
+const VERSION = new Version('17.3.3+sha-ce231fb');
 
 class Console {
     log(message) {
