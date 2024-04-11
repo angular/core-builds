60b1acf58de73c1c918c57d5d7ff001f5fc364d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-11 15:32:21 +0000

    1ee9f32 feat(core): Synchronize changes from internal JSAction codebase. (#55182)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.4+sha-0a8c48e
+ * @license Angular v18.0.0-next.4+sha-1ee9f32
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16736,7 +16736,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.4+sha-0a8c48e']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.4+sha-1ee9f32']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30370,7 +30370,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.4+sha-0a8c48e');
+const VERSION = new Version('18.0.0-next.4+sha-1ee9f32');
 
 class Console {
     log(message) {
