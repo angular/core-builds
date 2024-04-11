18ddc743346196616f0c66a38b789b9464b0c2c3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-11 09:25:53 +0000

    0a8c48e refactor(compiler-cli): move config initialization into constructor (#54975)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.4+sha-aae062e
+ * @license Angular v18.0.0-next.4+sha-0a8c48e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16736,7 +16736,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.4+sha-aae062e']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.4+sha-0a8c48e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30370,7 +30370,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.4+sha-aae062e');
+const VERSION = new Version('18.0.0-next.4+sha-0a8c48e');
 
 class Console {
     log(message) {
