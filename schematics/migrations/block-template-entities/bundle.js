81a4b6e1a2eb80aa2881296e40e49b2bb09c3260
Andrew Scott <atscott@google.com>
2024-04-12 17:47:43 +0000

    c9abe77 docs: Simply redirect function example for router (#55317)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -22640,7 +22640,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.4+sha-11523c1");
+var VERSION2 = new Version("18.0.0-next.4+sha-c9abe77");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
