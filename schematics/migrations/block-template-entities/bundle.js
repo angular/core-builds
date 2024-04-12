10762da7f7b011f1ddebba18f1dcec2a7e4ac73d
Andrew Scott <atscott@google.com>
2024-04-12 17:18:42 +0000

    a3b1dc0 docs: fix repeated note in "Add an Input to the component" section (#55306)

--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26068,7 +26068,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.4+sha-8a670e6");
+var VERSION2 = new Version("17.3.4+sha-a3b1dc0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
