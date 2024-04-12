2bf0a8e1320ed7fbf83c8d0b8421da26a94458d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-12 05:44:02 +0000

    f2f2c2a test(core): fix expectations that accidentally used incorrect matchers (#55294)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -22730,7 +22730,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.4+sha-ab910e2");
+var VERSION2 = new Version("18.0.0-next.4+sha-f2f2c2a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
