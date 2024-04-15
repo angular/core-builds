52abdb70fa646d3fd1d7f44533d6ad5f43efbddd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 09:13:54 +0000

    3af54d5 docs: fix links to output function (#55335)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.4+sha-571a61b
+ * @license Angular v18.0.0-next.4+sha-3af54d5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16736,7 +16736,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.4+sha-571a61b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.4+sha-3af54d5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30370,7 +30370,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.4+sha-571a61b');
+const VERSION = new Version('18.0.0-next.4+sha-3af54d5');
 
 class Console {
     log(message) {
