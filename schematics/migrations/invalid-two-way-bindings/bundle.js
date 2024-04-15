b1fef38726766efbcd7283614c648f8020d492aa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 14:41:30 +0000

    9bf9864 build(docs-infra): fix playground lezer parsing (#55349)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -26157,7 +26157,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.4+sha-a3b1dc0");
+var VERSION2 = new Version("17.3.4+sha-9bf9864");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
