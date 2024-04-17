9b82d1762dc33b114ed079b4dfbbb12d04ffc400
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 15:59:42 +0000

    011cc06 release: cut the v18.0.0-next.5 release

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -22685,7 +22685,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.4+sha-9d2704c");
+var VERSION2 = new Version("18.0.0-next.5+sha-011cc06");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
