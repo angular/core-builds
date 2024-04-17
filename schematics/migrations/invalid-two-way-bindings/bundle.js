c907ebed7d78ea939cc15297c73782e1194857d0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 16:07:22 +0000

    e1eae84 build: update github/codeql-action action to v3.25.0 (#55358)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -22685,7 +22685,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.5+sha-011cc06");
+var VERSION2 = new Version("18.0.0-next.5+sha-e1eae84");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
