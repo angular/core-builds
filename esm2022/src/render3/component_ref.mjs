47732151e265c2d4882827c423f2b6ea91e395a0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 09:33:59 +0000

    a34267b refactor(core): Switch stopPropagation default to false (#55351)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.4+sha-9f5e90e']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.4+sha-a34267b']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
