18ce6ddc87c64f73469f7bdfbba3ce2b44974b02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-22 18:45:09 +0000

    b0f1552 build: use installed deps from main repo (#55433)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.5+sha-ebfba77
+ * @license Angular v18.0.0-next.5+sha-b0f1552
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16822,7 +16822,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.5+sha-ebfba77']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.5+sha-b0f1552']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30479,7 +30479,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.5+sha-ebfba77');
+const VERSION = new Version('18.0.0-next.5+sha-b0f1552');
 
 class Console {
     log(message) {
