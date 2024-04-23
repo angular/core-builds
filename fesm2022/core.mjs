3250ffc28e9a0db7eb55b6bc571ca83f7b8c81dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 20:21:06 +0000

    eda03d4 docs: Fix typo in waitForAsync example (#55407)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.5+sha-8979fba
+ * @license Angular v17.3.5+sha-eda03d4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16022,7 +16022,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.5+sha-8979fba']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.5+sha-eda03d4']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29852,7 +29852,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.5+sha-8979fba');
+const VERSION = new Version('17.3.5+sha-eda03d4');
 
 class Console {
     log(message) {
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.5+sha-8979fba
+ * @license Angular v17.3.5+sha-eda03d4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -525,7 +525,7 @@ export declare function tick(millis?: number, tickOptions?: {
  *   object.doSomething.then(() => {
  *     expect(...);
  *   })
- * });
+ * })));
  * ```
  *
  * @publicApi
