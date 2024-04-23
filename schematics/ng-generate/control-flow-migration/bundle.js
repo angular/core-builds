9a91d678624a27a958a6c25602d510a9c7b4623e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 00:10:52 +0000

    f84d7dd test(core): Wait for zone stability before test completion (#55468)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22848,7 +22848,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.0-next.5+sha-9894278");
+var VERSION2 = new Version("18.0.0-next.5+sha-f84d7dd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
