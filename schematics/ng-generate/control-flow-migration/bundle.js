d83b6186c36fc95e313a91dd38c7889d5186c108
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 20:26:35 +0000

    79a29e4 docs(docs-infra): fix broken links (#55446)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -25999,7 +25999,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.5+sha-eda03d4");
+var VERSION2 = new Version("17.3.5+sha-79a29e4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
