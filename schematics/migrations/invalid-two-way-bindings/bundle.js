43866e6d31b236946be4427c477fb3063c23d099
Andrew Kushnir <akushnir@google.com>
2024-04-25 21:36:30 +0000

    356ec65 refactor(core): Do not duplicate change detection with run coalescing (part 2) (#55403)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23007,7 +23007,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.0+sha-bf8814c");
+var VERSION2 = new Version("18.1.0-next.0+sha-356ec65");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
