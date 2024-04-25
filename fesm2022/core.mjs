35599a53e55855a2b368e385ea3215cf6ec70e01
Andrew Kushnir <akushnir@google.com>
2024-04-25 22:03:21 +0000

    7a97678 refactor(platform-server): Update event_replay_spec to more match production code. (#55517)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-next.6+sha-9ec0136
+ * @license Angular v18.0.0-next.6+sha-7a97678
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16304,7 +16304,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.6+sha-9ec0136']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-next.6+sha-7a97678']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30566,7 +30566,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-next.6+sha-9ec0136');
+const VERSION = new Version('18.0.0-next.6+sha-7a97678');
 
 class Console {
     log(message) {
