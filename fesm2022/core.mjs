233f2e11319d094517dffce03bc420ee1c02d7b1
Andrew Kushnir <akushnir@google.com>
2024-04-26 16:46:35 +0000

    be17de5 refactor(core): Permit disabling autoDetect for zoneless fixture (#55494)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-91b1f24
+ * @license Angular v18.1.0-next.0+sha-be17de5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16305,7 +16305,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-91b1f24']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-be17de5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30567,7 +30567,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-91b1f24');
+const VERSION = new Version('18.1.0-next.0+sha-be17de5');
 
 class Console {
     log(message) {
