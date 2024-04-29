fa40a33dcfb4db9afb995cf7934e77a21dad36ad
Andrew Kushnir <akushnir@google.com>
2024-04-29 21:04:41 +0000

    fd54415 ci: complete migration to prettier formatting (#55580)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-49d3062']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-fd54415']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
