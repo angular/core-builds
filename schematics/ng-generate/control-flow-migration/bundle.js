947c5a5a679e5610cd7af45feb478fa85b10c49a
Andrew Kushnir <akushnir@google.com>
2024-04-29 20:47:06 +0000

    49d3062 docs(docs-infra): allow file renaming in code editor (#54989)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22848,7 +22848,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.0+sha-292c987");
+var VERSION2 = new Version("18.1.0-next.0+sha-49d3062");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
