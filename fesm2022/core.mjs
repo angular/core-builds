524e2b9a1f6ca9454bcfd6a48a077f3c5a343a7b
Andrew Kushnir <akushnir@google.com>
2024-05-01 00:06:17 +0000

    7cea897 release: bump DevTools to 1.0.14 (#55545)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-024e9bf
+ * @license Angular v18.1.0-next.0+sha-7cea897
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16366,7 +16366,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-024e9bf']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-7cea897']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30796,7 +30796,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-024e9bf');
+const VERSION = new Version('18.1.0-next.0+sha-7cea897');
 
 class Console {
     log(message) {
