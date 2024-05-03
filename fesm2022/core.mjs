6d2863b49e961acc35b7c84ae28108f851b5d328
Andrew Kushnir <akushnir@google.com>
2024-05-03 15:20:42 +0000

    aeb3fe2 build: remove tslint and codelyzer deps from integration tests (#55658)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-58a8f12
+ * @license Angular v18.1.0-next.0+sha-aeb3fe2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16875,7 +16875,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-58a8f12']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-aeb3fe2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30787,7 +30787,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-58a8f12');
+const VERSION = new Version('18.1.0-next.0+sha-aeb3fe2');
 
 class Console {
     log(message) {
--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23040,7 +23040,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.0+sha-58a8f12");
+var VERSION2 = new Version("18.1.0-next.0+sha-aeb3fe2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
