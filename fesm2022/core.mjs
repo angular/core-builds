292f56787a7e112082d3ee95ac8e59380b9a7d4b
Andrew Kushnir <akushnir@google.com>
2024-05-03 20:06:04 +0000

    f1dc74f build: update cross-repo angular dependencies (#55422)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.0+sha-0a8f6a7
+ * @license Angular v18.0.0-rc.0+sha-f1dc74f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16875,7 +16875,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.0+sha-0a8f6a7']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.0+sha-f1dc74f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30787,7 +30787,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.0+sha-0a8f6a7');
+const VERSION = new Version('18.0.0-rc.0+sha-f1dc74f');
 
 class Console {
     log(message) {
