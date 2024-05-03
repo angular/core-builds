7a8bc39d177991a1d8affbef70976c40346341a1
Andrew Kushnir <akushnir@google.com>
2024-05-03 16:22:47 +0000

    46acb55 build: update integration tests lock files (#55661)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23040,7 +23040,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.0+sha-aeb3fe2");
+var VERSION2 = new Version("18.1.0-next.0+sha-46acb55");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
