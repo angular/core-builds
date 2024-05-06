d742312c3ea474f251b22d4dee0097a130c210ad
Andrew Kushnir <akushnir@google.com>
2024-05-06 15:21:27 +0000

    8f273ce refactor(core): Allow the container and the listenable element to be configurable for early event contract. (#55586)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-1872fcd
+ * @license Angular v18.1.0-next.0+sha-8f273ce
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16875,7 +16875,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-1872fcd']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-8f273ce']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30787,7 +30787,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-1872fcd');
+const VERSION = new Version('18.1.0-next.0+sha-8f273ce');
 
 class Console {
     log(message) {
