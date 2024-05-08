94bcda71a2874e807056a5d939af25270ccb868f
Andrew Scott <atscott@google.com>
2024-05-08 18:18:01 +0000

    84f1639 release: bump Angular DevTools to version 1.0.16 (#55715)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.1+sha-a9dd754
+ * @license Angular v18.0.0-rc.1+sha-84f1639
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16888,7 +16888,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.1+sha-a9dd754']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.1+sha-84f1639']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30800,7 +30800,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.1+sha-a9dd754');
+const VERSION = new Version('18.0.0-rc.1+sha-84f1639');
 
 class Console {
     log(message) {
