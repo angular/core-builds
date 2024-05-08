69e36a1593f88454b538a68eecfacd1eeed6adba
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-05-08 15:54:37 +0000

    0b79089 docs: release notes for the v17.3.8 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-7287fef
+ * @license Angular v18.1.0-next.0+sha-0b79089
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16888,7 +16888,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-7287fef']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-0b79089']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30800,7 +30800,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-7287fef');
+const VERSION = new Version('18.1.0-next.0+sha-0b79089');
 
 class Console {
     log(message) {
