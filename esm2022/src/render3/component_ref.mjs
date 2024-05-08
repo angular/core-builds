c2654f99c84b14f812046ff214c4457160404fd0
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-05-08 15:51:21 +0000

    99bf2cd release: cut the v17.3.8 release

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -397,7 +397,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.7+sha-7725b70']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.8+sha-99bf2cd']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -26187,7 +26187,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.7+sha-7725b70");
+var VERSION2 = new Version("17.3.8+sha-99bf2cd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
