8354597041a5b250e6e70ac251c6f313e9f5128a
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-05-08 16:07:26 +0000

    a9dd754 release: cut the v18.0.0-rc.1 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.0+sha-e701672
+ * @license Angular v18.0.0-rc.1+sha-a9dd754
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16888,7 +16888,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.0+sha-e701672']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.1+sha-a9dd754']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30800,7 +30800,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.0+sha-e701672');
+const VERSION = new Version('18.0.0-rc.1+sha-a9dd754');
 
 class Console {
     log(message) {
