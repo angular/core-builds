c36d12748c2b01de574c54fc38d04ae14168d538
Andrew Scott <atscott@google.com>
2024-05-08 22:51:04 +0000

    c1915f1 docs: fix visible code snippets in first-app tutorial (#55742)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-c001286
+ * @license Angular v18.1.0-next.0+sha-c1915f1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16888,7 +16888,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-c001286']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-c1915f1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30800,7 +30800,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-c001286');
+const VERSION = new Version('18.1.0-next.0+sha-c1915f1');
 
 class Console {
     log(message) {
