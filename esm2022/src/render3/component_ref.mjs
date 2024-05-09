9e984817e32b018127a741d9361d2f25759d8fac
Andrew Scott <atscott@google.com>
2024-05-09 16:25:35 +0000

    fedeaac fix(forms): Add event for forms submitted & reset (#55667)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-c1915f1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-fedeaac']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
