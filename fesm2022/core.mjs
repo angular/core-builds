ffe014fbe72927c06428228b9efa19a0fb9e81d5
Andrew Scott <atscott@google.com>
2024-05-13 19:53:33 +0000

    9d1acd6 build: switch from rollup and terser to esbuild for creating contract bundle (#55705)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.1+sha-dc0c55c
+ * @license Angular v18.0.0-rc.1+sha-9d1acd6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16887,7 +16887,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.1+sha-dc0c55c']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.1+sha-9d1acd6']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30799,7 +30799,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.1+sha-dc0c55c');
+const VERSION = new Version('18.0.0-rc.1+sha-9d1acd6');
 
 class Console {
     log(message) {
