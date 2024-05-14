311eb4cca5e86da2a07c4980118dbb7f47f88700
Andrew Scott <atscott@google.com>
2024-05-14 18:53:02 +0000

    f736bea refactor(core): deprecate `@Component.interpolation` (#55778)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-1b8000d
+ * @license Angular v18.1.0-next.0+sha-f736bea
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1325,6 +1325,8 @@ export declare interface Component extends Directive {
     encapsulation?: ViewEncapsulation;
     /**
      * Overrides the default interpolation start and end delimiters (`{{` and `}}`).
+     *
+     * @deprecated use Angular's default interpolation delimiters instead.
      */
     interpolation?: [string, string];
     /**
