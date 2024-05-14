027df3434558c865412ccae6faeab329b727dd29
Andrew Scott <atscott@google.com>
2024-05-14 19:25:40 +0000

    55d6942 docs: fix production mode broken link (#55794)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.1+sha-1fd63e9']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.1+sha-55d6942']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
