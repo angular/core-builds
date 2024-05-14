797d400a9c064d26f0fe7351a2177ea0e5fc2825
Andrew Scott <atscott@google.com>
2024-05-14 16:42:49 +0000

    3ad120d refactor(core): Simplify event handler extraction logic. (#55747)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -22878,7 +22878,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.0+sha-ef029b8");
+var VERSION2 = new Version("18.1.0-next.0+sha-3ad120d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
