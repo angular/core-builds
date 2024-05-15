ccf327496f8c7e061c0645da100a2c30490d5242
Andrew Scott <atscott01@gmail.com>
2024-05-15 21:00:55 +0000

    7a172f7 docs: release notes for the v17.3.9 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-84b2351
+ * @license Angular v18.1.0-next.0+sha-7a172f7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16889,7 +16889,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-84b2351']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-7a172f7']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30801,7 +30801,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-84b2351');
+const VERSION = new Version('18.1.0-next.0+sha-7a172f7');
 
 class Console {
     log(message) {
