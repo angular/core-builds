110100d05948afa53627fb9999ba411a74b77985
Andrew Scott <atscott@google.com>
2024-05-16 16:26:49 +0000

    3055b92 fix(zone.js): correctly bundle `zone-patch-rxjs` (#55826)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.2+sha-a768c90
+ * @license Angular v18.0.0-rc.2+sha-3055b92
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16889,7 +16889,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.2+sha-a768c90']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.2+sha-3055b92']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30801,7 +30801,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.2+sha-a768c90');
+const VERSION = new Version('18.0.0-rc.2+sha-3055b92');
 
 class Console {
     log(message) {
