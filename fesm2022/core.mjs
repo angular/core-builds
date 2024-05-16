ef5886a07f03fa181cd43573f3b83ac5e8a73728
Andrew Scott <atscott@google.com>
2024-05-16 16:20:38 +0000

    a768c90 refactor(zone.js): remove `tslib` from `dependencies` (#55827)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.0-rc.2+sha-69085ea
+ * @license Angular v18.0.0-rc.2+sha-a768c90
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16889,7 +16889,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.2+sha-69085ea']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.0-rc.2+sha-a768c90']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30801,7 +30801,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.0-rc.2+sha-69085ea');
+const VERSION = new Version('18.0.0-rc.2+sha-a768c90');
 
 class Console {
     log(message) {
