420a14cf66bad0bf76a2341c94cd44020f58a1c2
Andrew Scott <atscott@google.com>
2024-05-16 16:20:48 +0000

    349f24a refactor(zone.js): remove `tslib` from `dependencies` (#55827)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.9+sha-45badde
+ * @license Angular v17.3.9+sha-349f24a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16022,7 +16022,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.9+sha-45badde']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '17.3.9+sha-349f24a']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -29861,7 +29861,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('17.3.9+sha-45badde');
+const VERSION = new Version('17.3.9+sha-349f24a');
 
 class Console {
     log(message) {
--- schematics/migrations/block-template-entities/bundle.js
+++ schematics/migrations/block-template-entities/bundle.js
@@ -26098,7 +26098,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("17.3.9+sha-45badde");
+var VERSION2 = new Version("17.3.9+sha-349f24a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
