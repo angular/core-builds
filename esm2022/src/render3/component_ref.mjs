ab75aee5d14616e30ce2c7c729e3d67713705446
Andrew Scott <atscott@google.com>
2024-05-16 16:20:24 +0000

    f3b0e2f refactor(zone.js): remove `tslib` from `dependencies` (#55827)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-3d5c3d9']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-f3b0e2f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
