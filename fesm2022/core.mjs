d03531677caedb697136752221d5f6b1f087ed46
Dylan Hunn <dylhunn@gmail.com>
2024-05-17 00:36:58 +0000

    1360110 release: cut the zone.js-0.14.6 release (#55845)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.0+sha-400911e
+ * @license Angular v18.1.0-next.0+sha-1360110
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16889,7 +16889,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-400911e']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.0+sha-1360110']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30801,7 +30801,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.0+sha-400911e');
+const VERSION = new Version('18.1.0-next.0+sha-1360110');
 
 class Console {
     log(message) {
