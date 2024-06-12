3fa9cf2749798119dfcb86ae0272daba76d262c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 20:41:19 +0000

    f4b86bd docs: fix configure routes link (#56415)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.1+sha-5cc9e80']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.1+sha-f4b86bd']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
