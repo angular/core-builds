c1ceba867975e82b4ac4ffc291c36e9412541abe
Andrew Kushnir <akushnir@google.com>
2024-06-12 00:38:51 +0000

    160171e docs(docs-infra): generate valid HTML for the update guide. (#56390)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.1+sha-b3780b8
+ * @license Angular v18.1.0-next.1+sha-160171e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16990,7 +16990,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.1+sha-b3780b8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.1+sha-160171e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30790,7 +30790,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.1+sha-b3780b8');
+const VERSION = new Version('18.1.0-next.1+sha-160171e');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- primitives/event-dispatch/index.d.ts
+++ primitives/event-dispatch/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.1+sha-b3780b8
+ * @license Angular v18.1.0-next.1+sha-160171e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
