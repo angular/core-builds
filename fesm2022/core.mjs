d09e0b8b93dea0f3cf051b24041a9e831d27c256
Andrew Kushnir <akushnir@google.com>
2024-06-12 00:51:41 +0000

    bf6f778 docs(docs-infra): generate valid HTML for the update guide. (#56390)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.2+sha-4204bee
+ * @license Angular v18.0.2+sha-bf6f778
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17055,7 +17055,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.2+sha-4204bee']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.2+sha-bf6f778']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30855,7 +30855,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.2+sha-4204bee');
+const VERSION = new Version('18.0.2+sha-bf6f778');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- rxjs-interop/index.d.ts
+++ rxjs-interop/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.2+sha-4204bee
+ * @license Angular v18.0.2+sha-bf6f778
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
