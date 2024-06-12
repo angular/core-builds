e900ab43b3248af6bbdcd05b54c45410ab91f562
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 19:06:02 +0000

    70157aa docs(service-worker): fix formatting of directory structure example (#56412)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.1+sha-96d3ed8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.1+sha-70157aa']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23053,7 +23053,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.1+sha-96d3ed8");
+var VERSION2 = new Version("18.1.0-next.1+sha-70157aa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
