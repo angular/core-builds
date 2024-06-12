9ed84941902208920bbdc236165e29ecacabf8bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 20:17:01 +0000

    995b7fc build: update babel dependencies to v7.24.7 (#56373)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.2+sha-e49a81b
+ * @license Angular v18.0.2+sha-995b7fc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7008,11 +7008,11 @@ class EventEmitter_ extends Subject {
         this.destroyRef = undefined;
         this.pendingTasks = undefined;
         this.__isAsync = isAsync;
-        // Attempt to retrieve a `DestroyRef` optionally.
-        // For backwards compatibility reasons, this cannot be required
+        // Attempt to retrieve a `DestroyRef` and `PendingTasks` optionally.
+        // For backwards compatibility reasons, this cannot be required.
         if (isInInjectionContext()) {
             this.destroyRef = inject(DestroyRef, { optional: true }) ?? undefined;
-            this.pendingTasks = inject(PendingTasks);
+            this.pendingTasks = inject(PendingTasks, { optional: true }) ?? undefined;
         }
     }
     emit(value) {
@@ -17055,7 +17055,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.2+sha-e49a81b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.2+sha-995b7fc']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30855,7 +30855,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.2+sha-e49a81b');
+const VERSION = new Version('18.0.2+sha-995b7fc');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
