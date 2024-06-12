4abcfb3b6bf8c9835354aeb70f71490ea45e13ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 18:50:39 +0000

    882cae1 ci: use depth 100 to allow for some history in artifact repos (#56414)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.2+sha-bf6f778']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.2+sha-882cae1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
