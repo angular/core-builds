8702aee7b204d48fa9f5a64c724bd1ec9b03207c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 20:41:18 +0000

    0a8d30e docs: fix configure routes link (#56415)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.2+sha-995b7fc
+ * @license Angular v18.0.2+sha-0a8d30e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17055,7 +17055,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.2+sha-995b7fc']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.2+sha-0a8d30e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30855,7 +30855,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.2+sha-995b7fc');
+const VERSION = new Version('18.0.2+sha-0a8d30e');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
