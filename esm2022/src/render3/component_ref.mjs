d2612ff4e404ecc85a76f951dbba2913babe7c56
Andrew Kushnir <akushnir@google.com>
2024-06-13 17:22:10 +0000

    ec0d1bf fix(compiler-cli): insert constant statements after the first group of imports (#56431)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.3+sha-34dc700']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.3+sha-ec0d1bf']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -10493,13 +10493,15 @@ var _ParseAST = class {
         const keyStart = this.inputIndex;
         const quoted = this.next.isString();
         const key = this.expectIdentifierOrKeywordOrString();
-        keys.push({ key, quoted });
+        const literalMapKey = { key, quoted };
+        keys.push(literalMapKey);
         if (quoted) {
           this.expectCharacter($COLON);
           values.push(this.parsePipe());
         } else if (this.consumeOptionalCharacter($COLON)) {
           values.push(this.parsePipe());
         } else {
+          literalMapKey.isShorthandInitialized = true;
           const span = this.span(keyStart);
           const sourceSpan = this.sourceSpan(keyStart);
           values.push(new PropertyRead(span, sourceSpan, sourceSpan, new ImplicitReceiver(span, sourceSpan), key));
@@ -23053,7 +23055,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.3+sha-34dc700");
+var VERSION2 = new Version("18.0.3+sha-ec0d1bf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
