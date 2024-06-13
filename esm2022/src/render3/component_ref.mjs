30ba64d6b4fc8393986a8ebf7426d101e7d6af27
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:28:27 +0000

    c698668 docs: language-service - Fix quick info URLs (#56416)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -399,7 +399,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.2+sha-ef2ad1b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.2+sha-c698668']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
