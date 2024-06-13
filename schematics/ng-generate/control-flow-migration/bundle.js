def78309f5dd7a8e1f26f1a30eda77cb256248cc
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:28:44 +0000

    34dc700 docs: language-service - Fix quick info URLs (#56416)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23053,7 +23053,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.3+sha-7c76eb5");
+var VERSION2 = new Version("18.0.3+sha-34dc700");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
