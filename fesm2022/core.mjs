46236d51ec630454615c5c06270dd640e91a33c8
Andrew Kushnir <akushnir@google.com>
2024-06-13 21:04:17 +0000

    1d3a752 feat(router): Set a different browser URL from the one for route matching (#53318)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.2+sha-fca5764
+ * @license Angular v18.1.0-next.2+sha-1d3a752
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17002,7 +17002,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.2+sha-fca5764']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.2+sha-1d3a752']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30802,7 +30802,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.2+sha-fca5764');
+const VERSION = new Version('18.1.0-next.2+sha-1d3a752');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23055,7 +23055,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.2+sha-fca5764");
+var VERSION2 = new Version("18.1.0-next.2+sha-1d3a752");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
