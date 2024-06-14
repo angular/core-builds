44d67775d6c9e2afed77f6e5dfb197d81d825831
Andrew Kushnir <akushnir@google.com>
2024-06-14 20:55:03 +0000

    ee8ecfa ci: begin checking licenses of upstream dependencies to ensure they are approved (#56460)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.2+sha-d64ee6c
+ * @license Angular v18.1.0-next.2+sha-ee8ecfa
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17002,7 +17002,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.2+sha-d64ee6c']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.2+sha-ee8ecfa']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30802,7 +30802,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.2+sha-d64ee6c');
+const VERSION = new Version('18.1.0-next.2+sha-ee8ecfa');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
