a6aeaa3be35d38f4483bac2b149df1a18da90ba5
Andrew Kushnir <akushnir@google.com>
2024-06-17 19:53:05 +0000

    841476d docs(docs-infra): Remove redundant API link (#56480)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -374,7 +374,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.3+sha-1d48e72']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.3+sha-841476d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.3+sha-1d48e72
+ * @license Angular v18.0.3+sha-841476d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17064,7 +17064,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.3+sha-1d48e72']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.3+sha-841476d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30856,7 +30856,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.3+sha-1d48e72');
+const VERSION = new Version('18.0.3+sha-841476d');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
