2e1e3a07496096eec3bd240d04413a22ce3481b6
Andrew Kushnir <akushnir@google.com>
2024-06-21 17:10:09 +0000

    75c3921 docs: fix file structure link (#56542)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.4+sha-be9e489
+ * @license Angular v18.0.4+sha-75c3921
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17237,7 +17237,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.4+sha-be9e489']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.4+sha-75c3921']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30998,7 +30998,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.4+sha-be9e489');
+const VERSION = new Version('18.0.4+sha-75c3921');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
