62e477d692f42844b67a6a08669a8382ac973468
Andrew Kushnir <akushnir@google.com>
2024-06-24 16:49:14 +0000

    553914d docs(docs-infra): add canonical link to each adev page (#56540)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.4+sha-75c3921
+ * @license Angular v18.0.4+sha-553914d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17237,7 +17237,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.4+sha-75c3921']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.4+sha-553914d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30998,7 +30998,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.4+sha-75c3921');
+const VERSION = new Version('18.0.4+sha-553914d');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
