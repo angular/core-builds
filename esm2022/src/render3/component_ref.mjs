f975ac816fc2f3f2bd7e716f03f837a150e075be
Andrew Kushnir <akushnir@google.com>
2024-06-24 16:49:34 +0000

    e81abdb docs(docs-infra): add canonical link to each adev page (#56540)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.3+sha-49b2e65']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.3+sha-e81abdb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.3+sha-49b2e65
+ * @license Angular v18.1.0-next.3+sha-e81abdb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17182,7 +17182,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.3+sha-49b2e65']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.3+sha-e81abdb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30943,7 +30943,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.3+sha-49b2e65');
+const VERSION = new Version('18.1.0-next.3+sha-e81abdb');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
