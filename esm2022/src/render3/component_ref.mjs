01fbd070fe3254f790ff8929316a986727795077
Dylan Hunn <dylhunn@gmail.com>
2024-06-26 19:44:50 +0000

    95f118b docs(docs-infra): fix asset path for learn angular #11 (#56587)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -374,7 +374,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.4+sha-f49a2a4']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.4+sha-95f118b']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23262,7 +23262,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.4+sha-f49a2a4");
+var VERSION2 = new Version("18.0.4+sha-95f118b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
