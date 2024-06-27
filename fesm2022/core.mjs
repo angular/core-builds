6a36fa9041d2e7b56c2939e02ece1b3b27183bcc
Dylan Hunn <dylhunn@gmail.com>
2024-06-27 00:20:00 +0000

    3bfba7e docs: release notes for the v18.0.5 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.4+sha-5dc0de1
+ * @license Angular v18.1.0-next.4+sha-3bfba7e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17201,7 +17201,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.4+sha-5dc0de1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.4+sha-3bfba7e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30981,7 +30981,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.4+sha-5dc0de1');
+const VERSION = new Version('18.1.0-next.4+sha-3bfba7e');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23422,7 +23422,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.4+sha-5dc0de1");
+var VERSION2 = new Version("18.1.0-next.4+sha-3bfba7e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
