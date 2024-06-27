afab775e3a3afb9cb201f4494c9f634f2db5f291
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 17:49:00 +0000

    3447753 Revert "fix(common): Don't run preconnect assertion on the server. (#56213)" (#56740)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23422,7 +23422,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.4+sha-2d8a96b");
+var VERSION2 = new Version("18.1.0-next.4+sha-3447753");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
