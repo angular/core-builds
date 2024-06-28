85ba165b97dcf8059d04f23a048f9d20f5c22caf
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-28 18:42:49 +0000

    f25653e fix(common): typo in NgOptimizedImage warning (#56756)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.4+sha-ceaaa98']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.4+sha-f25653e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.4+sha-ceaaa98
+ * @license Angular v18.1.0-next.4+sha-f25653e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17200,7 +17200,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.4+sha-ceaaa98']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-next.4+sha-f25653e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30980,7 +30980,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-next.4+sha-ceaaa98');
+const VERSION = new Version('18.1.0-next.4+sha-f25653e');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
