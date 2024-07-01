e844d7d4e730df9d559b09e705e7d6cf83fbbfe9
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 13:48:12 +0000

    5dc6dec build: update docs infra package (#56758)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23422,7 +23422,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.4+sha-ecd5e6b");
+var VERSION2 = new Version("18.1.0-next.4+sha-5dc6dec");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
