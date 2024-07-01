071a28d66a366483dfa20b6e0bb333e2603b6447
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 18:53:47 +0000

    b4cc8d4 ci: upload build artifacts on each pr run (#56787)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-next.4+sha-f407e3c");
+var VERSION2 = new Version("18.1.0-next.4+sha-b4cc8d4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
