88b6cd40e44d2e4e6e74d94f102a2803093a6c2f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 14:08:42 +0000

    5cb1dd7 docs: Update structural-directives.md (#56753)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -374,7 +374,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.5+sha-deedc47']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.5+sha-5cb1dd7']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
