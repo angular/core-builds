aca241101489727b1a19f60477f36919dd1b10fc
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:02:02 +0000

    0f612dc docs: update playground app to use hello world for easier onboarding (#56813)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-9acccb1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-0f612dc']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
