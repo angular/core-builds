7607dbbad64b0b79e715bfb8988638b3890e571f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:15:27 +0000

    b7d3ecc fix(router): routes should not get stale providers (#56798)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.5+sha-445dd96
+ * @license Angular v18.0.5+sha-b7d3ecc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17255,7 +17255,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.5+sha-445dd96']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.5+sha-b7d3ecc']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31035,7 +31035,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.5+sha-445dd96');
+const VERSION = new Version('18.0.5+sha-b7d3ecc');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23424,7 +23424,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.5+sha-445dd96");
+var VERSION2 = new Version("18.0.5+sha-b7d3ecc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
