7c2056dfa9c930f1de9e10879ca57f5226cd36c5
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 15:31:50 +0000

    99de3b7 build: update all non-major dependencies (#56801)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23424,7 +23424,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.5+sha-8de9aca");
+var VERSION2 = new Version("18.0.5+sha-99de3b7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
