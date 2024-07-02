8ac0839cfc4c6b2c9ab9c3e65b2765ccf9ad540a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 16:48:56 +0000

    9acccb1 docs: Update interceptors.md to replace `public class` with `export class` in DI section (#56807)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-rc.0+sha-58be8e6
+ * @license Angular v18.1.0-rc.0+sha-9acccb1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17200,7 +17200,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-58be8e6']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-9acccb1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30980,7 +30980,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-rc.0+sha-58be8e6');
+const VERSION = new Version('18.1.0-rc.0+sha-9acccb1');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
