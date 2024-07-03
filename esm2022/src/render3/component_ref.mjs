4373866c24958a391c3b8ff8e9ce81a2936b16f2
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-03 14:45:54 +0000

    1ee64a5 refactor(common): Fire priority check on stable. (#56776)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-3235d6d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-1ee64a5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-rc.0+sha-3235d6d
+ * @license Angular v18.1.0-rc.0+sha-1ee64a5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17200,7 +17200,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-3235d6d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-1ee64a5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30998,7 +30998,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.0-rc.0+sha-3235d6d');
+const VERSION = new Version('18.1.0-rc.0+sha-1ee64a5');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
