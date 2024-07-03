c9956c6d893a35857dc2ed8e51d9f5760a5a4b6b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-03 14:13:21 +0000

    00d9cd2 fix(core): establish proper defer injector hierarchy for components attached to ApplicationRef (#56763)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.0+sha-a1f6ff3");
+var VERSION2 = new Version("18.2.0-next.0+sha-00d9cd2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
