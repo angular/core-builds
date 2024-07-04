8e5aee6d7e79d4f60a60c438539a6bc4038b7ce7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 12:33:58 +0000

    833fa2d docs: add window title to reference pages (#56840)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -374,7 +374,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.6+sha-95d7076']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.6+sha-833fa2d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23262,7 +23262,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.6+sha-95d7076");
+var VERSION2 = new Version("18.0.6+sha-833fa2d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
