6ab69e01e69c26ae9966afe5edc279233f313d85
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 10:14:53 +0000

    ecc5644 perf(http): execute `fetch` outside of Angular zone (#56820)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-rc.0+sha-85da4b3");
+var VERSION2 = new Version("18.1.0-rc.0+sha-ecc5644");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
