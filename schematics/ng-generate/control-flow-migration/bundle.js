c90f518ba237c68d0296decb8b7052bc422fe1f0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-05 11:09:43 +0000

    9b35726 fix(common): typo in warning for NgOptimizedDirective (#56817)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23277,7 +23277,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-rc.0+sha-1efe907");
+var VERSION2 = new Version("18.1.0-rc.0+sha-9b35726");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
