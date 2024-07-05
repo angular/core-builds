d821cbd3caf12e5884673e45f1a0adec4031d366
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-05 10:32:24 +0000

    1efe907 docs: fix broken link (#56848)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23277,7 +23277,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-rc.0+sha-1074134");
+var VERSION2 = new Version("18.1.0-rc.0+sha-1efe907");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
