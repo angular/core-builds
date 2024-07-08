397d226e376b6703821d29b23b9e9ab391187791
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 07:58:51 +0000

    ba325c5 docs: remove private symbol from docs (#56851)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-9b35726']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-ba325c5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
