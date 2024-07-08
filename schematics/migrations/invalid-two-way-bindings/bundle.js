ddcfc6f342e30c55cdde4667bee47bb09ac1dc4e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 07:53:01 +0000

    cea3dfa docs: fix previous configuration link (#56880)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23424,7 +23424,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.6+sha-1517dde");
+var VERSION2 = new Version("18.0.6+sha-cea3dfa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
