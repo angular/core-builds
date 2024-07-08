688fbca208eefc7bfebe2f70ec8e9d7d60b5898c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 14:31:58 +0000

    56ec97d refactor(compiler-cli): do not truncate/reduce types in API docs (#56572)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-rc.0+sha-ba325c5");
+var VERSION2 = new Version("18.1.0-rc.0+sha-56ec97d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
--- schematics/ng-generate/standalone-migration/bundle.js
+++ schematics/ng-generate/standalone-migration/bundle.js
@@ -26071,7 +26071,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-rc.0+sha-ba325c5");
+var VERSION2 = new Version("18.1.0-rc.0+sha-56ec97d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _I18N_ATTR = "i18n";
@@ -27175,7 +27175,7 @@ var MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION = "18.0.0";
 function compileDeclareClassMetadata(metadata) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION));
-  definitionMap.set("version", literal("18.1.0-rc.0+sha-ba325c5"));
+  definitionMap.set("version", literal("18.1.0-rc.0+sha-56ec97d"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", metadata.type);
   definitionMap.set("decorators", metadata.decorators);
@@ -27194,7 +27194,7 @@ function compileComponentDeclareClassMetadata(metadata, dependencies) {
   callbackReturnDefinitionMap.set("ctorParameters", (_a2 = metadata.ctorParameters) != null ? _a2 : literal(null));
   callbackReturnDefinitionMap.set("propDecorators", (_b2 = metadata.propDecorators) != null ? _b2 : literal(null));
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION));
-  definitionMap.set("version", literal("18.1.0-rc.0+sha-ba325c5"));
+  definitionMap.set("version", literal("18.1.0-rc.0+sha-56ec97d"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", metadata.type);
   definitionMap.set("resolveDeferredDeps", compileComponentMetadataAsyncResolver(dependencies));
@@ -27262,7 +27262,7 @@ function createDirectiveDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   const minVersion = getMinimumVersionForPartialOutput(meta);
   definitionMap.set("minVersion", literal(minVersion));
-  definitionMap.set("version", literal("18.1.0-rc.0+sha-ba325c5"));
+  definitionMap.set("version", literal("18.1.0-rc.0+sha-56ec97d"));
   definitionMap.set("type", meta.type.value);
   if (meta.isStandalone) {
     definitionMap.set("isStandalone", literal(meta.isStandalone));
@@ -27580,7 +27580,7 @@ var MINIMUM_PARTIAL_LINKER_VERSION2 = "12.0.0";
 function compileDeclareFactoryFunction(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION2));
-  definitionMap.set("version", literal("18.1.0-rc.0+sha-ba325c5"));
+  definitionMap.set("version", literal("18.1.0-rc.0+sha-56ec97d"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   definitionMap.set("deps", compileDependencies(meta.deps));
@@ -27603,7 +27603,7 @@ function compileDeclareInjectableFromMetadata(meta) {
 function createInjectableDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION3));
-  definitionMap.set("version", literal("18.1.0-rc.0+sha-ba325c5"));
+  definitionMap.set("version", literal("18.1.0-rc.0+sha-56ec97d"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.providedIn !== void 0) {
@@ -27641,7 +27641,7 @@ function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION4));
-  definitionMap.set("version", literal("18.1.0-rc.0+sha-ba325c5"));
+  definitionMap.set("version", literal("18.1.0-rc.0+sha-56ec97d"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   definitionMap.set("providers", meta.providers);
@@ -27665,7 +27665,7 @@ function createNgModuleDefinitionMap(meta) {
     throw new Error("Invalid path! Local compilation mode should not get into the partial compilation path");
   }
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION5));
-  definitionMap.set("version", literal("18.1.0-rc.0+sha-ba325c5"));
+  definitionMap.set("version", literal("18.1.0-rc.0+sha-56ec97d"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.bootstrap.length > 0) {
@@ -27700,7 +27700,7 @@ function compileDeclarePipeFromMetadata(meta) {
 function createPipeDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION6));
-  definitionMap.set("version", literal("18.1.0-rc.0+sha-ba325c5"));
+  definitionMap.set("version", literal("18.1.0-rc.0+sha-56ec97d"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.isStandalone) {
@@ -27717,7 +27717,7 @@ function createPipeDefinitionMap(meta) {
 publishFacade(_global);
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
-var VERSION3 = new Version("18.1.0-rc.0+sha-ba325c5");
+var VERSION3 = new Version("18.1.0-rc.0+sha-56ec97d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/imports/src/emitter.mjs
 var import_typescript5 = __toESM(require("typescript"), 1);
@@ -45840,7 +45840,11 @@ var FunctionExtractor = class {
   extract() {
     var _a2;
     const signature = this.typeChecker.getSignatureFromDeclaration(this.declaration);
-    const returnType = signature ? this.typeChecker.typeToString(this.typeChecker.getReturnTypeOfSignature(signature)) : "unknown";
+    const returnType = signature ? this.typeChecker.typeToString(
+      this.typeChecker.getReturnTypeOfSignature(signature),
+      void 0,
+      import_typescript100.default.TypeFormatFlags.NoTypeReduction | import_typescript100.default.TypeFormatFlags.NoTruncation
+    ) : "unknown";
     const jsdocsTags = extractJsDocTags(this.declaration);
     return {
       params: extractAllParams(this.declaration.parameters, this.typeChecker),
@@ -46428,7 +46432,7 @@ function filterSignatureDeclarations(signatures) {
   for (const signature of signatures) {
     const decl = signature.getDeclaration();
     if (import_typescript106.default.isFunctionDeclaration(decl) || import_typescript106.default.isCallSignatureDeclaration(decl)) {
-      result.push(decl);
+      result.push({ signature, decl });
     }
   }
   return result;
@@ -46436,16 +46440,20 @@ function filterSignatureDeclarations(signatures) {
 function extractFunctionWithOverloads(name, signatures, typeChecker) {
   return {
     name,
-    signatures: filterSignatureDeclarations(signatures).map((s) => ({
+    signatures: filterSignatureDeclarations(signatures).map(({ decl, signature }) => ({
       name,
       entryType: EntryType.Function,
-      description: extractJsDocDescription(s),
-      generics: extractGenerics(s),
+      description: extractJsDocDescription(decl),
+      generics: extractGenerics(decl),
       isNewType: false,
-      jsdocTags: extractJsDocTags(s),
-      params: extractAllParams(s.parameters, typeChecker),
-      rawComment: extractRawJsDoc(s),
-      returnType: typeChecker.typeToString(typeChecker.getReturnTypeOfSignature(typeChecker.getSignatureFromDeclaration(s)))
+      jsdocTags: extractJsDocTags(decl),
+      params: extractAllParams(decl.parameters, typeChecker),
+      rawComment: extractRawJsDoc(decl),
+      returnType: typeChecker.typeToString(
+        typeChecker.getReturnTypeOfSignature(signature),
+        void 0,
+        import_typescript106.default.TypeFormatFlags.NoTypeReduction | import_typescript106.default.TypeFormatFlags.NoTruncation
+      )
     })),
     implementation: null
   };
