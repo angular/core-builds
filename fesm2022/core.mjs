873133eff5fb53d267a09738dc49ef6629b136bc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 14:31:41 +0000

    f370f64 refactor(compiler-cli): do not truncate/reduce types in API docs (#56572)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.0+sha-176b26f
+ * @license Angular v18.2.0-next.0+sha-f370f64
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17200,7 +17200,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.0+sha-176b26f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.0+sha-f370f64']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -30998,7 +30998,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.0+sha-176b26f');
+const VERSION = new Version('18.2.0-next.0+sha-f370f64');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
