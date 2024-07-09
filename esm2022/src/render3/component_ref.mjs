1fbd19ccd8ea0e732e1e1e637bc0be138326282e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 07:43:30 +0000

    02a31c5 docs: add documentation page for let declarations (#56886)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.0+sha-5dcdbfc']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.0+sha-02a31c5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
