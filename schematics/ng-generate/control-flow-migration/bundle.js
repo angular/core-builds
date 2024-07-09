57b3e3c05ae72e1af401f3340591760a10647d71
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:19:08 +0000

    260531d refactor(compiler-cli):  escape missing decorators. (#56794)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23277,7 +23277,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.0+sha-fe52d8f");
+var VERSION2 = new Version("18.2.0-next.0+sha-260531d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
