0b5a887b7cab81a3b0c3ff8a685b1cd0ee1712b3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 07:42:53 +0000

    02f5e73 docs: add documentation page for let declarations (#56886)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23277,7 +23277,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-rc.0+sha-35fa9b0");
+var VERSION2 = new Version("18.1.0-rc.0+sha-02f5e73");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
