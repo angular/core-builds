159f6cb95715bdcf7bff18b473e9f08c0f6651e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:18:56 +0000

    5d8cfc6 refactor(compiler-cli):  escape missing decorators. (#56794)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.6+sha-122573b
+ * @license Angular v18.0.6+sha-5d8cfc6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17255,7 +17255,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.6+sha-122573b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.0.6+sha-5d8cfc6']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31053,7 +31053,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.0.6+sha-122573b');
+const VERSION = new Version('18.0.6+sha-5d8cfc6');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
