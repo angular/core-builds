76dd6221b2603d576d4f162cd2fd7a8b32aa3890
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:10:38 +0000

    ae9505b docs: update dependency-injection-context.md (#56736)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0-rc.0+sha-0f4219c");
+var VERSION2 = new Version("18.1.0-rc.0+sha-ae9505b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
