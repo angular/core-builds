3ce1d8d0f749f12d0ff977fc546df671331dbaa4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 09:33:38 +0000

    1c2415e docs: Update components-scenarios.md (#56921)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23262,7 +23262,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.6+sha-a7d021f");
+var VERSION2 = new Version("18.0.6+sha-1c2415e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
