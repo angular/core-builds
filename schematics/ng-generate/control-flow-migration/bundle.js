c999d7f32c76cc172924cddb2bc4c38ed1976719
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 07:39:12 +0000

    a7d021f docs: update version table for 18.1 (#56920)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23262,7 +23262,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.6+sha-619e181");
+var VERSION2 = new Version("18.0.6+sha-a7d021f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
