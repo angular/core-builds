cec39358ba50a1f4881b034e31bab29c17a8a7ea
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 13:23:26 +0000

    8fc2605 docs: fix testing angular url (#56847)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.0+sha-85d1d22']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.0+sha-8fc2605']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
