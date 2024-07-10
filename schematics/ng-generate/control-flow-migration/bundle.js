62cb7be2d0a158a91d638797779d0ba30e22b0a3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 12:58:16 +0000

    543f636 docs: release notes for the v18.0.7 release

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23277,7 +23277,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.0+sha-9d94177");
+var VERSION2 = new Version("18.2.0-next.0+sha-543f636");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
