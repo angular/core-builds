f3e157972550e31d812b660bd0634aa8322679d1
Andrew Scott <atscott@google.com>
2024-07-10 20:58:19 +0000

    d011c9f refactor(core): Refactor `EarlyEventContract` to prepare for using it as a container. (#56900)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.0+sha-8df13e1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.0+sha-d011c9f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
