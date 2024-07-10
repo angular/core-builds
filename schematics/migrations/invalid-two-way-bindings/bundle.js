68897f312178da118ee72f92d9b7127e7acd1f84
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 13:23:07 +0000

    729bf5b docs: fix testing angular url (#56847)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0+sha-07a0b87");
+var VERSION2 = new Version("18.1.0+sha-729bf5b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
