91d0b208ce2bf2577e627295afc804ac0ab753cf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 13:04:56 +0000

    07a0b87 release: cut the v18.1.0 release

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0-rc.0+sha-551ffbf']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.0+sha-07a0b87']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
