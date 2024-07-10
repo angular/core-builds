84879a4e5da49e2300972868193ab453d8844239
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 07:16:15 +0000

    619e181 docs: Update component testing scenarios (#56388)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23262,7 +23262,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.0.6+sha-d6fff45");
+var VERSION2 = new Version("18.0.6+sha-619e181");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
