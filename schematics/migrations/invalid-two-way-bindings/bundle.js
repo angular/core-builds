5553411739bbb0490f65bd5b149db1031475c63c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 16:21:41 +0000

    8df13e1 docs: add overview page for performance section (#56828)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.0+sha-7f550ea");
+var VERSION2 = new Version("18.2.0-next.0+sha-8df13e1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
