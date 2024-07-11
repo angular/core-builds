8cc8aab838d3ab3ac40e96733197182f28e9b538
Andrew Scott <atscott@google.com>
2024-07-11 19:09:53 +0000

    60df722 docs(docs-infra): focus api search filter on non-touch devices (#56294)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23277,7 +23277,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0+sha-a531106");
+var VERSION2 = new Version("18.1.0+sha-60df722");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
