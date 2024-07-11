62915bc37c7a6c6482a08146e38ba7c6bc8e03c1
Andrew Scott <atscott@google.com>
2024-07-11 00:10:27 +0000

    dd6e548 docs: fixing links to guides on the Performance Overview page (#56942)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.0+sha-d011c9f");
+var VERSION2 = new Version("18.2.0-next.0+sha-dd6e548");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
