8cf737b06edaa013ab0991a6726e5dc92e11df44
Andrew Scott <atscott@google.com>
2024-07-15 18:54:54 +0000

    ac9d9db build: update dependency inquirer to v10 (#56883)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23439,7 +23439,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.0+sha-e958fa8");
+var VERSION2 = new Version("18.2.0-next.0+sha-ac9d9db");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
