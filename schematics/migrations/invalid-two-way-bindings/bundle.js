f7581f2806ecdfa678f3142fb7584e6165f02f9f
Andrew Scott <atscott@google.com>
2024-07-16 19:54:00 +0000

    4f11d6e fix(zone.js): support `Timeout.refresh` in Node.js (#56852)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23448,7 +23448,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.0+sha-a1cb9df");
+var VERSION2 = new Version("18.1.0+sha-4f11d6e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
