238cef71276fa4181ab3e0a4ae3a886b79b6f141
Andrew Scott <atscott@google.com>
2024-07-17 21:07:23 +0000

    d97aee6 release: cut the zone.js 0.14.8 release (#57030)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23448,7 +23448,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.1+sha-89550e7");
+var VERSION2 = new Version("18.2.0-next.1+sha-d97aee6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
