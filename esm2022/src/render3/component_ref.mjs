24c2696f8bee5a7af44c8b0fd4084fec8892bcf7
Andrew Scott <atscott@google.com>
2024-07-18 15:46:43 +0000

    edbb403 docs(docs-infra): add open in StackBlitz button to code editor (#57018)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.1+sha-4bfe707']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.1+sha-edbb403']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
