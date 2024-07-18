ff1997f2253ceb21813b16d49bdf2362e9b3f6f9
Andrew Scott <atscott@google.com>
2024-07-18 18:12:05 +0000

    bbabf54 docs: fix headings and code font in `@for` docs (#57007)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.1+sha-edbb403
+ * @license Angular v18.1.1+sha-bbabf54
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17205,7 +17205,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.1+sha-edbb403']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.1+sha-bbabf54']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31003,7 +31003,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.1+sha-edbb403');
+const VERSION = new Version('18.1.1+sha-bbabf54');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
