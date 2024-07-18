f22c175ca3f45fa1881bec04395b2a5527d19ff5
Andrew Scott <atscott@google.com>
2024-07-18 22:31:54 +0000

    f3a88ad docs(docs-infra): update tutorial deps to 18.1 (#57024)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-425f44c
+ * @license Angular v18.2.0-next.1+sha-f3a88ad
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17205,7 +17205,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.1+sha-425f44c']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.1+sha-f3a88ad']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31003,7 +31003,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-425f44c');
+const VERSION = new Version('18.2.0-next.1+sha-f3a88ad');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
