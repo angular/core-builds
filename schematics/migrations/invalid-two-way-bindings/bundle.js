26010c89a8fa490c96e353e286ac5d83ac9ef4af
Andrew Scott <atscott@google.com>
2024-07-19 01:09:17 +0000

    244c50d docs: fix pipes link (#52979)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23448,7 +23448,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.1+sha-f3a88ad");
+var VERSION2 = new Version("18.2.0-next.1+sha-244c50d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
