74d1012bd4c5ec6fa9d17cb0e4ffa1b8478f785b
Andrew Scott <atscott@google.com>
2024-07-19 01:09:23 +0000

    04c504a docs: fix pipes link (#52979)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.1+sha-e088c80']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.1+sha-04c504a']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
