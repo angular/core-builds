6e5fbe700c827d87c0e79d24ad79571d03b48c40
Andrew Scott <atscott@google.com>
2024-07-22 22:32:08 +0000

    4ac39ae Revert "fix(compiler-cli): add warning for unused let declarations (#57033)" (#57088)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.1+sha-fab673a
+ * @license Angular v18.2.0-next.1+sha-4ac39ae
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17205,7 +17205,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.1+sha-fab673a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.1+sha-4ac39ae']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31003,7 +31003,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.1+sha-fab673a');
+const VERSION = new Version('18.2.0-next.1+sha-4ac39ae');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
