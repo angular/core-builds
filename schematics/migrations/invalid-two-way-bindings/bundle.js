4fd78633f00e84659d66f049ce7eae13e08567df
Andrew Scott <atscott@google.com>
2024-07-22 17:57:15 +0000

    06ae226 docs: Update packages/router/src/router.ts (#57053)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23448,7 +23448,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.1+sha-ae15dd9");
+var VERSION2 = new Version("18.1.1+sha-06ae226");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
