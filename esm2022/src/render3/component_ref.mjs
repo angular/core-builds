de8705add409d1a94659c78a679727d66e5a364c
Andrew Scott <atscott@google.com>
2024-07-23 22:50:17 +0000

    43515a3 test(docs-infra): fix test with slight adjustment to error handling (#57104)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.1+sha-c578239']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.1+sha-43515a3']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
