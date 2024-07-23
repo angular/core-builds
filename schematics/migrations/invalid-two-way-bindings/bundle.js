d2cbe3eb69d8b99cb1a6e02c71a63f14d2be4329
Andrew Scott <atscott@google.com>
2024-07-23 17:33:48 +0000

    c979dfd build: update github/codeql-action action to v3.25.13 (#57091)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23451,7 +23451,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.1+sha-915b0e0");
+var VERSION2 = new Version("18.1.1+sha-c979dfd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
