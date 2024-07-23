a754ca5949dfa55073c00b3a953e845739d30fb7
Andrew Scott <atscott@google.com>
2024-07-23 22:58:38 +0000

    83117f1 refactor(core): Switch to using concat rather than spread operator for events. (#57103)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.1+sha-43515a3']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.1+sha-83117f1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
