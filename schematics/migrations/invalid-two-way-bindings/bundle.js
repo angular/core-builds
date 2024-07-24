30deb838e85e8a9ce10ed820af60c262f6c227b9
Andrew Scott <atscott@google.com>
2024-07-24 17:22:53 +0000

    d5fbe11 docs(docs-infra): disable experimental decorators (#56257)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23451,7 +23451,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.1+sha-0e439d7");
+var VERSION2 = new Version("18.2.0-next.1+sha-d5fbe11");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
