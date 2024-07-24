b8d964e5f908bb9e591518f0caa13283683841ba
Andrew Scott <atscott@google.com>
2024-07-24 17:32:37 +0000

    3d0ee73 docs(docs-infra): Use shiki for code highlighting (#57059)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.1+sha-8bb5937']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.1+sha-3d0ee73']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
