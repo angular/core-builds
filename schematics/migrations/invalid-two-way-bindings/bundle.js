c6e2558bda74ab6a1be356d106a1aa8e2625541b
Dylan Hunn <dylhunn@gmail.com>
2024-07-26 17:08:05 +0000

    481092d docs: add open in IDX support (#57099)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23451,7 +23451,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.2+sha-58ee039");
+var VERSION2 = new Version("18.1.2+sha-481092d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
