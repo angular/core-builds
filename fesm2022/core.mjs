beccb254b14b70dac7b82ed497b42d80fa5a3475
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 15:56:29 +0000

    2d8635d refactor(docs-infra): migrate @angular/docs from dev-infra into adev directory (#57132)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-next.2+sha-dd56270
+ * @license Angular v18.2.0-next.2+sha-2d8635d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17208,7 +17208,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.2+sha-dd56270']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.2+sha-2d8635d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31004,7 +31004,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-next.2+sha-dd56270');
+const VERSION = new Version('18.2.0-next.2+sha-2d8635d');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
