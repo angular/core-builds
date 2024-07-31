6e041884d1edf9a8c94c3dc8b231b026d0f38e42
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-31 19:01:36 +0000

    de7f5ac refactor(docs-infra): Use new APIs in search dialog (#57215)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.3+sha-be6c246
+ * @license Angular v18.1.3+sha-de7f5ac
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17218,7 +17218,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.3+sha-be6c246']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.3+sha-de7f5ac']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31014,7 +31014,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.3+sha-be6c246');
+const VERSION = new Version('18.1.3+sha-de7f5ac');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
