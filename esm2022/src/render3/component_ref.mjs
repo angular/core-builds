7c9b06cee4aab12759b0521ea4e23fee3d6b6eff
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 15:56:06 +0000

    6ac209c fix(language-service): avoid generating TS suggestion diagnostics for templates (#56241)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.3+sha-0c72976']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.3+sha-6ac209c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
