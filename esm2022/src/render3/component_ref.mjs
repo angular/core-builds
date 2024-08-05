5a1bc9a160050acd76a4f2ab42b48a5aa2661401
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 14:52:12 +0000

    afb05ff fix(compiler-cli): support JIT transforms before other transforms modifying classes (#57262)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.3+sha-523f639']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.3+sha-afb05ff']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
