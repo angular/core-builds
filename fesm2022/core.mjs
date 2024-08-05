721a1ad063f844ab184b3c90be42afb0a4f3413a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 17:13:40 +0000

    6eac1b3 refactor(docs-infra): migrate api-gen from dev-infra into the repo (#57241)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.3+sha-afb05ff
+ * @license Angular v18.1.3+sha-6eac1b3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17218,7 +17218,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.3+sha-afb05ff']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.3+sha-6eac1b3']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31014,7 +31014,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.3+sha-afb05ff');
+const VERSION = new Version('18.1.3+sha-6eac1b3');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
