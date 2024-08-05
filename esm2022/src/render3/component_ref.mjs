76ba05334051d714adfa12cf78504ab7c177fec5
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 16:03:21 +0000

    00a6c41 build: update dependency madge to v8 (#57257)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.3+sha-e2259c7']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-next.3+sha-00a6c41']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23193,7 +23193,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.3+sha-e2259c7");
+var VERSION2 = new Version("18.2.0-next.3+sha-00a6c41");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
