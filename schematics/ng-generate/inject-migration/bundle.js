26387da1d009fa7932627973d957a0c85e53b0bf
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 17:13:21 +0000

    3bdead1 refactor(docs-infra): migrate api-gen from dev-infra into the repo (#57241)

--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23193,7 +23193,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.3+sha-de99d3e");
+var VERSION2 = new Version("18.2.0-next.3+sha-3bdead1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
