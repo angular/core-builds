0324132cf77ede54e3fe2d6a33a1a997477ec278
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-08-07 19:33:40 +0000

    0761e9a docs: release notes for the v18.2.0-rc.0 release

--- schematics/ng-generate/route-lazy-loading/bundle.js
+++ schematics/ng-generate/route-lazy-loading/bundle.js
@@ -23195,7 +23195,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0-next.4+sha-ccbce8f");
+var VERSION2 = new Version("18.3.0-next.0+sha-0761e9a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
