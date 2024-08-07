f358819cccdf1652fd47bb923955d90c3afe12a2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-08-07 18:57:05 +0000

    bdf8450 release: cut the v18.1.4 release

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23452,7 +23452,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.1.3+sha-69540d3");
+var VERSION2 = new Version("18.1.4+sha-bdf8450");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
