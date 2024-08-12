db75db67fc15b8cf8a3e67b496ef04a2c299a5ef
Andrew Kushnir <akushnir@google.com>
2024-08-12 19:17:05 +0000

    5d16c28 refactor(migrations): handle safe property reads in signal input migration (#57318)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.0-rc.0+sha-cea3e4b
+ * @license Angular v18.2.0-rc.0+sha-5d16c28
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17234,7 +17234,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-rc.0+sha-cea3e4b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0-rc.0+sha-5d16c28']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31037,7 +31037,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.0-rc.0+sha-cea3e4b');
+const VERSION = new Version('18.2.0-rc.0+sha-5d16c28');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
