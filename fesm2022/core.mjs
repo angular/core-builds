9de70c84628422e1c5f5c60d7b1ca47f729f7a61
Andrew Kushnir <akushnir@google.com>
2024-08-12 16:32:42 +0000

    b279081 fix(router): Align RouterModule.forRoot errorHandler with provider error handler (#57050)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-37bf7de
+ * @license Angular v19.0.0-next.0+sha-b279081
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17234,7 +17234,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.0+sha-37bf7de']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.0+sha-b279081']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31037,7 +31037,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.0+sha-37bf7de');
+const VERSION = new Version('19.0.0-next.0+sha-b279081');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23452,7 +23452,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.0+sha-37bf7de");
+var VERSION2 = new Version("19.0.0-next.0+sha-b279081");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
