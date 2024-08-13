3cf8b92160b09c380ec3a09e4e5dfee126268735
Andrew Kushnir <akushnir@google.com>
2024-08-13 16:36:09 +0000

    0a808fb build: update scorecard action dependencies (#57358)

--- fesm2022/primitives/signals.mjs
+++ fesm2022/primitives/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.0+sha-d608b85
+ * @license Angular v19.0.0-next.0+sha-0a808fb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/migrations/after-render-phase/bundle.js
+++ schematics/migrations/after-render-phase/bundle.js
@@ -23331,7 +23331,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.0+sha-d608b85");
+var VERSION2 = new Version("19.0.0-next.0+sha-0a808fb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
