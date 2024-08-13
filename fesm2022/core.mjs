36b904f9a4d2bc391ec3d648104308ebbc691399
Andrew Kushnir <akushnir@google.com>
2024-08-13 22:58:28 +0000

    95af661 docs(docs-infra): fix markdown link rendering (#57377)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.4+sha-f99bb20
+ * @license Angular v18.1.4+sha-95af661
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17228,7 +17228,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.4+sha-f99bb20']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.1.4+sha-95af661']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31028,7 +31028,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.1.4+sha-f99bb20');
+const VERSION = new Version('18.1.4+sha-95af661');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
