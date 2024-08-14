e59a5a633f539a2a0e59ca997eb8e90fb701d6a6
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-08-14 16:48:11 +0000

    c1f1c02 docs: release notes for the v18.2.0 release

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23237,7 +23237,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.0+sha-c5b598e");
+var VERSION2 = new Version("19.0.0-next.0+sha-c1f1c02");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
