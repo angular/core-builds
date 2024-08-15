f8358bd4929b54b02c675f31afae5a4fc986f0d7
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 16:36:42 +0000

    3b0dca7 fix(core): Allow zoneless scheduler to run inside `fakeAsync` (#56932)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.0+sha-58a79b6']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.0+sha-3b0dca7']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/migrations/http-providers/bundle.js
+++ schematics/migrations/http-providers/bundle.js
@@ -23331,7 +23331,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.0+sha-58a79b6");
+var VERSION2 = new Version("19.0.0-next.0+sha-3b0dca7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
