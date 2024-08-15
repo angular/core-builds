bdae6a14e9710201e31c625eb4818f5ba48d126d
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 20:15:12 +0000

    0bbbd78 docs: add `withEventReplay` and `withI18nSupport` links to `provideClientHydration` docs (#57382)

--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23194,7 +23194,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.0+sha-9c73943");
+var VERSION2 = new Version("19.0.0-next.0+sha-0bbbd78");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
