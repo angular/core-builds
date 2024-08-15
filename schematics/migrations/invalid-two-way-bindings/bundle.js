4e713890d2439222882cb1cd958b84a0b61ef04d
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 15:26:19 +0000

    d4449fc fix(core): handle hydration of components that project content conditionally (#57383)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23452,7 +23452,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.0+sha-751880a");
+var VERSION2 = new Version("19.0.0-next.0+sha-d4449fc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
