94bac0f9807ec4c5d0df2c6bc877f01780034b86
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 14:50:57 +0000

    9155f75 docs(common): fix example code (#57406)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0+sha-d49e083']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0+sha-9155f75']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/migrations/after-render-phase/bundle.js
+++ schematics/migrations/after-render-phase/bundle.js
@@ -23331,7 +23331,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0+sha-d49e083");
+var VERSION2 = new Version("18.2.0+sha-9155f75");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
