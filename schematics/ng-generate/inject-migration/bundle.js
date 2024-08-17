c45d4b496b12ed5d4b131f56b3dd81d6a8c84d5b
Dylan Hunn <dylhunn@gmail.com>
2024-08-17 13:59:22 +0000

    57db366 refactor(migrations): framework to build batchable migrations (#57396)

--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23194,7 +23194,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0+sha-3e6ee7e");
+var VERSION2 = new Version("18.2.0+sha-57db366");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
