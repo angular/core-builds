811693a862c24e980e2efdef0656f278234dbd29
Dylan Hunn <dylhunn@gmail.com>
2024-08-21 18:31:47 +0000

    69b20d8 docs(docs-infra): fix regressions around members & deprecations (#57465)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23237,7 +23237,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0+sha-1a5f5ee");
+var VERSION2 = new Version("18.2.0+sha-69b20d8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
