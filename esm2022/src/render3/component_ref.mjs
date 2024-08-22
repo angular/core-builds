a8e17558d2aebdb435a05ba702acf214c4794a2c
Dylan Hunn <dylhunn@gmail.com>
2024-08-22 16:17:06 +0000

    09deb24 docs: fix indentation of file tree in app-shell (#57436)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -387,7 +387,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.0+sha-69b20d8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.1+sha-09deb24']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/route-lazy-loading/bundle.js
+++ schematics/ng-generate/route-lazy-loading/bundle.js
@@ -23195,7 +23195,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.0+sha-69b20d8");
+var VERSION2 = new Version("18.2.1+sha-09deb24");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
