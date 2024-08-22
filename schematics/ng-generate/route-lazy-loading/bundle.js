447d1b69be44f231973ff589481862b01800c53a
Dylan Hunn <dylhunn@gmail.com>
2024-08-22 15:55:27 +0000

    66da115 release: cut the v19.0.0-next.1 release

--- schematics/ng-generate/route-lazy-loading/bundle.js
+++ schematics/ng-generate/route-lazy-loading/bundle.js
@@ -23195,7 +23195,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.0+sha-5cc63c2");
+var VERSION2 = new Version("19.0.0-next.1+sha-66da115");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
