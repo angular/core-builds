d3ee8fb1657c4939fc383e3dff68aa1b14f9b525
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-23 20:54:22 +0000

    aacef07 refactor(migrations): replacements in tsurge are always serializable (#57501)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -384,7 +384,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.1+sha-17f7b25']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.1+sha-aacef07']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
