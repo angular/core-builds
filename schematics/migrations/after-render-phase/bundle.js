6765a9b1ed05ee494aaea4e3cc1d097957222b13
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-26 16:09:42 +0000

    03ec620 fix(upgrade): Address Trusted Types violations in @angular/upgrade (#57454)

--- schematics/migrations/after-render-phase/bundle.js
+++ schematics/migrations/after-render-phase/bundle.js
@@ -23331,7 +23331,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.1+sha-6d3a2af");
+var VERSION2 = new Version("18.2.1+sha-03ec620");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
