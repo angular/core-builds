e3df2c203f0d9faa3b4d82042167464d5203f047
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 20:02:51 +0000

    3067633 docs(docs-infra): Add query as an url query param to the api reference (#57062)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.1+sha-03b3527
+ * @license Angular v18.2.1+sha-3067633
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16933,7 +16933,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.1+sha-03b3527']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.1+sha-3067633']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31023,7 +31023,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.1+sha-03b3527');
+const VERSION = new Version('18.2.1+sha-3067633');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
