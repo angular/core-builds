f988f582f60ae10b54950fb12d39fa61699c4c91
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 19:47:31 +0000

    3ecf620 Revert "fix(http): Dynamicaly call the global fetch implementation (#57531)" (#57571)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.1+sha-36aa3af
+ * @license Angular v18.2.1+sha-3ecf620
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16943,7 +16943,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.1+sha-36aa3af']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.1+sha-3ecf620']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31033,7 +31033,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.1+sha-36aa3af');
+const VERSION = new Version('18.2.1+sha-3ecf620');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
