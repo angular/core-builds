a114df98f364ef7ad86dad5cdb358ba2a1b8ff5b
Andrew Kushnir <akushnir@google.com>
2024-08-29 22:24:48 +0000

    614c311 docs: update common-router-tasks to include sample dynamic fragment (#57590)

--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23314,7 +23314,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.2+sha-5007970");
+var VERSION2 = new Version("18.2.2+sha-614c311");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
