d51d1931d942c1c6475fca226269d0634be55a81
Andrew Kushnir <akushnir@google.com>
2024-08-29 17:25:02 +0000

    8bcc663 feat(core): drop support for TypeScript 5.4 (#57577)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23572,7 +23572,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.2+sha-6e28f96");
+var VERSION2 = new Version("19.0.0-next.2+sha-8bcc663");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
