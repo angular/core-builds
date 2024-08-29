2bc0e7617eaeacea6d29cd1b0ca4e9e6766b5bb9
Andrew Kushnir <akushnir@google.com>
2024-08-29 21:33:26 +0000

    2ad9609 refactor(migrations): pass project-relative paths for tsurge replacements (#57584)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.2+sha-01030d5");
+var VERSION2 = new Version("19.0.0-next.2+sha-2ad9609");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
