38a1fc5d244e14e98d7e4415d42d2ed41985c2a4
Andrew Kushnir <akushnir@google.com>
2024-08-30 18:16:58 +0000

    3bafda2 docs(docs-infra): fix navigation between API pages (#57492)

--- schematics/migrations/http-providers/bundle.js
+++ schematics/migrations/http-providers/bundle.js
@@ -23451,7 +23451,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("19.0.0-next.2+sha-5e42769");
+var VERSION2 = new Version("19.0.0-next.2+sha-3bafda2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
