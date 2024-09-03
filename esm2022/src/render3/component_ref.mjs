5ac6a7270a8ceb0fc64b330397b0e9c25caa375a
Andrew Kushnir <akushnir@google.com>
2024-09-03 17:47:46 +0000

    c91c377 release: bump Angular DevTools version to 1.0.18 (#57585)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.2+sha-de68e04']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.2+sha-c91c377']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
