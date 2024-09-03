4a3b6b4ada52d9b45cd3dc71dbf9535068bed2b5
Andrew Kushnir <akushnir@google.com>
2024-09-03 15:55:05 +0000

    2324d9b docs: Fix 'EnvironmentInjector' description (#57582)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.2+sha-b494e9b");
+var VERSION2 = new Version("18.2.2+sha-2324d9b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
