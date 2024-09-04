a1d7fdc1eea00556fa37d5e9ea3df57f48cdb711
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-04 21:04:42 +0000

    004110c refactor(devtools): use signal apis for directive forest (#56998)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.3+sha-69ca33f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.3+sha-004110c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
