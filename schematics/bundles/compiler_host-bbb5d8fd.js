09b8277c9cdd044b5535f1d80cccb01bc811fbeb
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-04 17:10:38 +0000

    9da21f7 feat(migrations): replace .next usage on outputs (#57654)

--- schematics/bundles/compiler_host-bbb5d8fd.js
+++ schematics/bundles/compiler_host-bbb5d8fd.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.3+sha-3c75684
+ * @license Angular v19.0.0-next.3+sha-9da21f7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.3+sha-3c75684');
+new Version('19.0.0-next.3+sha-9da21f7');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
