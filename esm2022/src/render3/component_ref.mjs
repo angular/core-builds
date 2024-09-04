555a91e1297bce36db299b4768f800a7bfbbf1fd
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-04 17:10:23 +0000

    69ca33f docs: update top level banner styles (#57503)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.3+sha-f75429d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.3+sha-69ca33f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
