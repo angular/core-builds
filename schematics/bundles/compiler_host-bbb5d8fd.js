96f78bc060335523887b705f6143545ca95b0733
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-05 19:37:37 +0000

    227d13b refactor(migrations): preserve brace spacing when applying imports (#57672)

--- schematics/bundles/compiler_host-bbb5d8fd.js
+++ schematics/bundles/compiler_host-bbb5d8fd.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.3+sha-4fa25cf
+ * @license Angular v19.0.0-next.3+sha-227d13b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.3+sha-4fa25cf');
+new Version('19.0.0-next.3+sha-227d13b');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/control-flow-migration.js
+++ schematics/bundles/control-flow-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.3+sha-4fa25cf
+ * @license Angular v19.0.0-next.3+sha-227d13b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
