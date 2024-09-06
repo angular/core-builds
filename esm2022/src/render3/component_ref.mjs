e0d3f9eb95850858d97091b4d6304e646faf76a7
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-06 15:14:50 +0000

    bb8e7ce docs(docs-infra): fix extraction tests (#57694)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.3+sha-9895e44']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.3+sha-bb8e7ce']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23314,7 +23314,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.3+sha-9895e44");
+var VERSION2 = new Version("18.2.3+sha-bb8e7ce");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
