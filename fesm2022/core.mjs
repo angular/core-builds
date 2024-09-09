11f6e3fd952acc1017239df791cbe267bd5e3cc0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-09-09 18:20:07 +0000

    d58ffd6 release: cut the v19.0.0-next.4 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.3+sha-6fa1b38
+ * @license Angular v19.0.0-next.4+sha-d58ffd6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16962,7 +16962,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.3+sha-6fa1b38']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.4+sha-d58ffd6']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31052,7 +31052,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.3+sha-6fa1b38');
+const VERSION = new Version('19.0.0-next.4+sha-d58ffd6');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.3+sha-6fa1b38
+ * @license Angular v19.0.0-next.4+sha-d58ffd6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.3+sha-6fa1b38');
+new Version('19.0.0-next.4+sha-d58ffd6');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
