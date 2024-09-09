da431ab39175827dd76838274fd53909f75bb3ca
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 18:27:47 +0000

    95b8105 docs(core): fix typo (#57741)

--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.4+sha-d58ffd6
+ * @license Angular v19.0.0-next.4+sha-95b8105
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.4+sha-d58ffd6');
+new Version('19.0.0-next.4+sha-95b8105');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
