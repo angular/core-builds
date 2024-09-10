e44cc21dac4b32e28eb7be9862e051679e26e152
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 17:22:32 +0000

    cc99297 docs: update Angular CLI help [main] (#57743)

--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.4+sha-685866d
+ * @license Angular v19.0.0-next.4+sha-cc99297
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.4+sha-685866d');
+new Version('19.0.0-next.4+sha-cc99297');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
