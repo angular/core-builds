51bed4d3761d974435dd5db6a5e0cc05bd31ce91
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 20:32:37 +0000

    c4b19c4 docs: fix link from zoneless to OnPush (#57760) PR Close #57760

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.3+sha-a919c6c");
+var VERSION2 = new Version("18.2.3+sha-c4b19c4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
