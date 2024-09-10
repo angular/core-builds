40c0cedf53556f1c60474e892cc64393c439f334
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 20:32:34 +0000

    45575b5 docs: fix link from zoneless to OnPush (#57760) PR Close #57760

--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.4+sha-cc99297
+ * @license Angular v19.0.0-next.4+sha-45575b5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.4+sha-cc99297');
+new Version('19.0.0-next.4+sha-45575b5');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
