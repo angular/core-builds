3151e9e54ffba718a998a98105f26c5edcbc0bd9
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 16:37:04 +0000

    a919c6c build: update all non-major dependencies to ~5.3.0 (#57754)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.3+sha-d0fe538']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.3+sha-a919c6c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/migrations/http-providers/bundle.js
+++ schematics/migrations/http-providers/bundle.js
@@ -23451,7 +23451,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.3+sha-d0fe538");
+var VERSION2 = new Version("18.2.3+sha-a919c6c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
