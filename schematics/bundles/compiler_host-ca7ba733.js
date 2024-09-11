cf2bbbfc931d71c5c862046944ffa3cbd8b9e622
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-11 21:29:59 +0000

    2b3e7f8 docs: use correct example (#57783) PR Close #57783

--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.5+sha-488621f
+ * @license Angular v19.0.0-next.5+sha-2b3e7f8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.5+sha-488621f');
+new Version('19.0.0-next.5+sha-2b3e7f8');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
