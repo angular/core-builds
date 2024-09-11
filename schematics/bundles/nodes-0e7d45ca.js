a700e57c0dcddd99d0f8f18b954f19f50df955e5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-09-11 20:32:09 +0000

    488621f release: cut the v19.0.0-next.5 release

--- schematics/bundles/nodes-0e7d45ca.js
+++ schematics/bundles/nodes-0e7d45ca.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.4+sha-784d4be
+ * @license Angular v19.0.0-next.5+sha-488621f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
