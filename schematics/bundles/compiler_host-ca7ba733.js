4e8be915a2351f2139cbc0139ff569103bd960b3
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-11 17:52:50 +0000

    73feb0e refactor(migrations): support reference migration in query migration (#57777)

--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.4+sha-c93b510
+ * @license Angular v19.0.0-next.4+sha-73feb0e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.4+sha-c93b510');
+new Version('19.0.0-next.4+sha-73feb0e');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
