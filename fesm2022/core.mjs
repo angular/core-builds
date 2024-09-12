4aa94215158e62f13b65ab653a67c23b58c756aa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-12 09:28:24 +0000

    55e3a3a docs: fix example for defer on hover on an alternative element (#57788)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.5+sha-6c8fd30
+ * @license Angular v19.0.0-next.5+sha-55e3a3a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16962,7 +16962,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.5+sha-6c8fd30']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.5+sha-55e3a3a']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31052,7 +31052,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.5+sha-6c8fd30');
+const VERSION = new Version('19.0.0-next.5+sha-55e3a3a');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.5+sha-6c8fd30
+ * @license Angular v19.0.0-next.5+sha-55e3a3a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.5+sha-6c8fd30');
+new Version('19.0.0-next.5+sha-55e3a3a');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
