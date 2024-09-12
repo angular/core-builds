076811f105c932ec2af0305faadae8126200d0af
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-12 08:58:31 +0000

    6c8fd30 docs: add section on removing Zone.js imports/dependency from the build (#57784)

--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.5+sha-2b3e7f8
+ * @license Angular v19.0.0-next.5+sha-6c8fd30
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.5+sha-2b3e7f8');
+new Version('19.0.0-next.5+sha-6c8fd30');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
