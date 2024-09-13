e52a2e54e562a3756aafdc1077922a8344789556
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-13 14:13:49 +0000

    8a5f319 build: update cross-repo angular dependencies (#57776)

--- schematics/bundles/compiler_host-ca7ba733.js
+++ schematics/bundles/compiler_host-ca7ba733.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.5+sha-fd081e8
+ * @license Angular v19.0.0-next.5+sha-8a5f319
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29070,7 +29070,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.5+sha-fd081e8');
+new Version('19.0.0-next.5+sha-8a5f319');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
