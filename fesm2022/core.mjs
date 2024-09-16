56b2c70a94ef719b3834eba3ccbd842463d83a0b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 09:30:46 +0000

    e16b8a3 docs: correct key usage in sectionStyles and update template binding (#57820) PR Close #57820

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.4+sha-d5a27b8
+ * @license Angular v18.2.4+sha-e16b8a3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16946,7 +16946,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.4+sha-d5a27b8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.4+sha-e16b8a3']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31036,7 +31036,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.4+sha-d5a27b8');
+const VERSION = new Version('18.2.4+sha-e16b8a3');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
