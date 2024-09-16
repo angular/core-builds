3b9ea8ec0a988cafcd96dcd61576597a7f9ca940
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 09:43:30 +0000

    6b066f3 build: update cross-repo angular dependencies (#57810)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.5+sha-7526687
+ * @license Angular v19.0.0-next.5+sha-6b066f3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16991,7 +16991,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.5+sha-7526687']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.5+sha-6b066f3']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31081,7 +31081,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.5+sha-7526687');
+const VERSION = new Version('19.0.0-next.5+sha-6b066f3');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
