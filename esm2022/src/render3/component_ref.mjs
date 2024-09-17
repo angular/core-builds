24c416bef02eeb998243434937b8ec4276e559b8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-17 13:28:18 +0000

    c0b4986 docs: add fork url in contribution guidelines for new contributors (#57837)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.4+sha-cd97054']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.4+sha-c0b4986']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23314,7 +23314,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.4+sha-cd97054");
+var VERSION2 = new Version("18.2.4+sha-c0b4986");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
