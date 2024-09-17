fd670c18e78ee1e63bf7e4542e90e92b2ae8317e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-17 13:52:42 +0000

    dbd697c build: update github/codeql-action action to v3.26.7 (#57843)

--- schematics/ng-generate/route-lazy-loading/bundle.js
+++ schematics/ng-generate/route-lazy-loading/bundle.js
@@ -23315,7 +23315,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.4+sha-c0b4986");
+var VERSION2 = new Version("18.2.4+sha-dbd697c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
