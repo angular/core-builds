7beb5488bccbbfbf13e72c2b71aca77fe861b5c9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-18 14:58:06 +0000

    029163c docs: adapt the NgModule to the new default : standalone (#57532)

--- schematics/bundles/checker-dcf9a14e.js
+++ schematics/bundles/checker-dcf9a14e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.5+sha-f611faa
+ * @license Angular v19.0.0-next.5+sha-029163c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29234,7 +29234,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.5+sha-f611faa');
+new Version('19.0.0-next.5+sha-029163c');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.5+sha-f611faa
+ * @license Angular v19.0.0-next.5+sha-029163c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29874,7 +29874,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.5+sha-f611faa');
+new Version('19.0.0-next.5+sha-029163c');
 
 var _VisitorMode;
 (function (_VisitorMode) {
