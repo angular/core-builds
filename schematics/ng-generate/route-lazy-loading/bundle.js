e4d794356d1d331edf96e1d60327ed7e0512f387
Andrew Scott <atscott@google.com>
2024-09-19 21:22:00 +0000

    07a485f docs(docs-infra): Add dedicated support for decorators. (#57595)

--- schematics/ng-generate/route-lazy-loading/bundle.js
+++ schematics/ng-generate/route-lazy-loading/bundle.js
@@ -23315,7 +23315,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.5+sha-88457ab");
+var VERSION2 = new Version("18.2.5+sha-07a485f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
