7f65026b1caeda46f5b885d9673e2e4528857a9e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 14:34:10 +0000

    cd24eaf build: update cross-repo angular dependencies (#57851)

--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-9020a50
+ * @license Angular v19.0.0-next.6+sha-cd24eaf
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29874,7 +29874,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.6+sha-9020a50');
+new Version('19.0.0-next.6+sha-cd24eaf');
 
 var _VisitorMode;
 (function (_VisitorMode) {
