66b4b27318f2da1e4da66aab015cc69e78851ee4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 12:22:54 +0000

    9020a50 docs: Update Making HTTP requests best practices example (#57876)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-e598b08
+ * @license Angular v19.0.0-next.6+sha-9020a50
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17031,7 +17031,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.6+sha-e598b08']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.6+sha-9020a50']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31138,7 +31138,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-e598b08');
+const VERSION = new Version('19.0.0-next.6+sha-9020a50');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
