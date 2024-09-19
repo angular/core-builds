2595ab93a6b32d8054d2f5554c94deb74bc5dc24
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 09:23:36 +0000

    ddbdf22 ci: update pullapprove config to enact changes for renovate (#57853)

--- schematics/bundles/explicit-standalone-flag.js
+++ schematics/bundles/explicit-standalone-flag.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-38de06b
+ * @license Angular v19.0.0-next.6+sha-ddbdf22
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-38de06b
+ * @license Angular v19.0.0-next.6+sha-ddbdf22
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.0-next.6+sha-38de06b');
+new checker.Version('19.0.0-next.6+sha-ddbdf22');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
