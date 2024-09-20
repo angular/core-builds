c826475137a46371dfb3f8243947879a764cd80e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 09:18:49 +0000

    4112e6f build: update io_bazel_rules_sass digest to 8dac332 (#57892)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-fc59e2a
+ * @license Angular v19.0.0-next.6+sha-4112e6f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17031,7 +17031,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.6+sha-fc59e2a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.6+sha-4112e6f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31138,7 +31138,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-fc59e2a');
+const VERSION = new Version('19.0.0-next.6+sha-4112e6f');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-fc59e2a
+ * @license Angular v19.0.0-next.6+sha-4112e6f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29874,7 +29874,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.6+sha-fc59e2a');
+new Version('19.0.0-next.6+sha-4112e6f');
 
 var _VisitorMode;
 (function (_VisitorMode) {
