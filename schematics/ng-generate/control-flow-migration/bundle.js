458a217dca1e59832d62622303c0e284cf980939
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 15:05:47 +0000

    fc2f936 ci: update ownership to include MarkTechson (#57885)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.5+sha-1ee7ec5");
+var VERSION2 = new Version("18.2.5+sha-fc2f936");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
