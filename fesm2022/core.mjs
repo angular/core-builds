68930e94c1ca68dd05a40ddc6beaa1e7891084d2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 15:05:05 +0000

    a448f5a ci: update ownership to include MarkTechson (#57885)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.6+sha-3ac0a78
+ * @license Angular v19.0.0-next.6+sha-a448f5a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17031,7 +17031,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.6+sha-3ac0a78']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.6+sha-a448f5a']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31138,7 +31138,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.6+sha-3ac0a78');
+const VERSION = new Version('19.0.0-next.6+sha-a448f5a');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
