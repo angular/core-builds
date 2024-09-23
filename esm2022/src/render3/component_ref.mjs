83bf2230c37881221994d0ff33711653484f6fb8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 15:26:01 +0000

    1825460 fix(docs-infra): resolve Mermaid from Bazel root path (#57924)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.5+sha-3cc40f3']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.5+sha-1825460']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
