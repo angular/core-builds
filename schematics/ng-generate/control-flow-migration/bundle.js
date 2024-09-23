2879ce2eebd000f7a66ecc73990e1cf74f06e70c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 14:13:50 +0000

    3cc40f3 refactor(devtools): provide user with suggestions on why their angular application is not being detected in dev mode (#57861)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.5+sha-6092015");
+var VERSION2 = new Version("18.2.5+sha-3cc40f3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
