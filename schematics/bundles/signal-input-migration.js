d03abd9528be4f31f1b047fa5cf7b9ca19e333a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 14:29:45 +0000

    6c9abd3 refactor(forms): lift circular deps (#55881)

--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-9f4063b
+ * @license Angular v19.0.0-next.6+sha-6c9abd3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29988,7 +29988,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.6+sha-9f4063b');
+new Version('19.0.0-next.6+sha-6c9abd3');
 
 var _VisitorMode;
 (function (_VisitorMode) {
