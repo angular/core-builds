3e1cda7359c81e2120be2e018dbcbef3a09d6190
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 09:47:36 +0000

    4ff695c build: update io_bazel_rules_sass digest to 1fc4bd9 (#57907)

--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-950a554
+ * @license Angular v19.0.0-next.6+sha-4ff695c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29886,7 +29886,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.6+sha-950a554');
+new Version('19.0.0-next.6+sha-4ff695c');
 
 var _VisitorMode;
 (function (_VisitorMode) {
