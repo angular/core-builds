242f944e4260d0ad2df1c8e5d624c0ffe1a191d3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 10:06:59 +0000

    6092015 docs: add signal example for "Binding to the host element" section (#57272)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.5+sha-cd6ecdb']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.5+sha-6092015']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.5+sha-cd6ecdb");
+var VERSION2 = new Version("18.2.5+sha-6092015");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
