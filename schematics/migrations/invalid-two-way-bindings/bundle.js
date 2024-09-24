e90b36f7dc9175176f97b4f248805b96e32431d4
Andrew Scott <atscott@google.com>
2024-09-24 20:07:54 +0000

    dbf15c8 fix(docs-infra): extend the timeout for jasmine tests of mermaid (#57948)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23572,7 +23572,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.5+sha-03ac3c2");
+var VERSION2 = new Version("18.2.5+sha-dbf15c8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
