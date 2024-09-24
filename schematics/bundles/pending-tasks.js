0ea5438341c4dc68b8ee6606796a973f0db182f0
Andrew Scott <atscott@google.com>
2024-09-24 20:07:43 +0000

    af66e24 fix(docs-infra): extend the timeout for jasmine tests of mermaid (#57948)

--- schematics/bundles/pending-tasks.js
+++ schematics/bundles/pending-tasks.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-9dbe6fc
+ * @license Angular v19.0.0-next.6+sha-af66e24
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-9dbe6fc
+ * @license Angular v19.0.0-next.6+sha-af66e24
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29999,7 +29999,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.6+sha-9dbe6fc');
+new Version('19.0.0-next.6+sha-af66e24');
 
 var _VisitorMode;
 (function (_VisitorMode) {
