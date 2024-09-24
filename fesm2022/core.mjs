9cc9bebafe6abd2ada177b4609c522c6eeec2a3b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 09:17:46 +0000

    613dd02 build: update github/codeql-action action to v3.26.8 (#57937)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.5+sha-1825460
+ * @license Angular v18.2.5+sha-613dd02
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16949,7 +16949,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.5+sha-1825460']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.5+sha-613dd02']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31039,7 +31039,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.5+sha-1825460');
+const VERSION = new Version('18.2.5+sha-613dd02');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/migrations/http-providers/bundle.js
+++ schematics/migrations/http-providers/bundle.js
@@ -23451,7 +23451,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.5+sha-1825460");
+var VERSION2 = new Version("18.2.5+sha-613dd02");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
