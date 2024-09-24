3334fce9b48a2a90816751d346af3fa8a97fc5a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 10:58:02 +0000

    b124e50 refactor(core): avoid producing zone-related warnings during hydration when in zoneless mode (#57911)

--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-030ffb2
+ * @license Angular v19.0.0-next.6+sha-b124e50
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29988,7 +29988,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.6+sha-030ffb2');
+new Version('19.0.0-next.6+sha-b124e50');
 
 var _VisitorMode;
 (function (_VisitorMode) {
