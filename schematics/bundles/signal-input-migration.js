04e72df83832b2f994ad88b4fa17303d55d335c0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 10:12:05 +0000

    48216a2 refactor(router): Move view transitions to developer preview (#55600)

--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.6+sha-5a04837
+ * @license Angular v19.0.0-next.6+sha-48216a2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29988,7 +29988,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.6+sha-5a04837');
+new Version('19.0.0-next.6+sha-48216a2');
 
 var _VisitorMode;
 (function (_VisitorMode) {
