1242ff4152915ed31fb23cc80dce76d1148063c3
Andrew Scott <atscott01@gmail.com>
2024-09-25 17:05:26 +0000

    1391928 release: cut the v18.2.6 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.5+sha-dbf15c8
+ * @license Angular v18.2.6+sha-1391928
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16949,7 +16949,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.5+sha-dbf15c8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.6+sha-1391928']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31039,7 +31039,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.5+sha-dbf15c8');
+const VERSION = new Version('18.2.6+sha-1391928');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
