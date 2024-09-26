ce2f2899e4db27166b43bf68d33a72a69e0148ca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-09-26 21:37:05 +0000

    69b7cf5 build: update @angular/build-tooling to latest version (#57980)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.6+sha-2f347ef");
+var VERSION2 = new Version("18.2.6+sha-69b7cf5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
