9effb1a7c664a5ad45c7ff12379ea93688850e20
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:37:00 +0000

    e77a163 docs: update the name of the sub-navigation item at error encyclopedia menu (#57997)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-78bc349
+ * @license Angular v19.0.0-next.7+sha-e77a163
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17029,7 +17029,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.7+sha-78bc349']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.7+sha-e77a163']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31314,7 +31314,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.7+sha-78bc349');
+const VERSION = new Version('19.0.0-next.7+sha-e77a163');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/bundles/checker-e68dd7ce.js
+++ schematics/bundles/checker-e68dd7ce.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.7+sha-78bc349
+ * @license Angular v19.0.0-next.7+sha-e77a163
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29246,7 +29246,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.7+sha-78bc349');
+new Version('19.0.0-next.7+sha-e77a163');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
