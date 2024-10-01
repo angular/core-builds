fe2662354f8c6135426fc9998d52edd08fc6bed2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 13:59:57 +0000

    3b35c4e fix(docs-infra): allow tutorial component to be GCed (#58028)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-9ce839f
+ * @license Angular v19.0.0-next.7+sha-3b35c4e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17029,7 +17029,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.7+sha-9ce839f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.7+sha-3b35c4e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31314,7 +31314,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.7+sha-9ce839f');
+const VERSION = new Version('19.0.0-next.7+sha-3b35c4e');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
