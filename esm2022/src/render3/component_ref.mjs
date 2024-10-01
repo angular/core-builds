009e72cb633e2f088c9bc06c4a3a62a79cb53de5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:37:31 +0000

    8d8fb44 docs: update the name of the sub-navigation item at error encyclopedia menu (#57997)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.6+sha-6b67cb5']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.6+sha-8d8fb44']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23314,7 +23314,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.6+sha-6b67cb5");
+var VERSION2 = new Version("18.2.6+sha-8d8fb44");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
