0b944ac6eed51e9b9175bd5db5782720b34875b7
Andrew Kushnir <akushnir@google.com>
2024-10-04 21:43:51 +0000

    ccf2c7f docs(common): Minor typo in code example (#58085)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.7+sha-39a851f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.7+sha-ccf2c7f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
