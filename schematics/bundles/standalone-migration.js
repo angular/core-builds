92b0ba0fe92bec33b4c365a8dee26c03dcc9bb2f
Andrew Kushnir <akushnir@google.com>
2024-10-04 18:03:33 +0000

    06600d2 refactor: change security issue redirect to angular.dev (#58070)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.8+sha-84b6896
+ * @license Angular v19.0.0-next.8+sha-06600d2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.0-next.8+sha-84b6896');
+new checker.Version('19.0.0-next.8+sha-06600d2');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
