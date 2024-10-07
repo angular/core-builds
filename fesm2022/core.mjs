9a6e770e4d52bf1b107a159a77560fc7d8cd61bc
Andrew Kushnir <akushnir@google.com>
2024-10-07 20:34:49 +0000

    5c63fc4 ci: update renovate configuration to automatically run generated file updates (#58109)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.8+sha-837af44
+ * @license Angular v19.0.0-next.8+sha-5c63fc4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17029,7 +17029,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.8+sha-837af44']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.8+sha-5c63fc4']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31314,7 +31314,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.8+sha-837af44');
+const VERSION = new Version('19.0.0-next.8+sha-5c63fc4');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
--- schematics/bundles/group_replacements-edfda3b8.js
+++ schematics/bundles/group_replacements-edfda3b8.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.8+sha-837af44
+ * @license Angular v19.0.0-next.8+sha-5c63fc4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29378,7 +29378,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.8+sha-837af44');
+new Version('19.0.0-next.8+sha-5c63fc4');
 
 var _VisitorMode;
 (function (_VisitorMode) {
