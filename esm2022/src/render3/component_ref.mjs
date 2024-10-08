73c52d3b6c3889c6e59ad318dcb6bbffdae13a64
Andrew Kushnir <akushnir@google.com>
2024-10-08 16:34:37 +0000

    9f2726b refactor(docs-infra): add tooltip to the download button in playground (#58065)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.7+sha-7b13d34']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.7+sha-9f2726b']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/route-lazy-loading/bundle.js
+++ schematics/ng-generate/route-lazy-loading/bundle.js
@@ -23315,7 +23315,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.7+sha-7b13d34");
+var VERSION2 = new Version("18.2.7+sha-9f2726b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
