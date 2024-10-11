f98ae43ce014b1e0dcc5163d358d43be0c813c6c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 10:43:23 +0000

    b198593 build: update io_bazel_rules_sass digest to 583fc8c (#58155)

--- schematics/bundles/group_replacements-8c11682b.js
+++ schematics/bundles/group_replacements-8c11682b.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.9+sha-fc6c76a
+ * @license Angular v19.0.0-next.9+sha-b198593
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29421,7 +29421,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.9+sha-fc6c76a');
+new Version('19.0.0-next.9+sha-b198593');
 
 var _VisitorMode;
 (function (_VisitorMode) {
