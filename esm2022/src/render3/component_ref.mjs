ec0b5b3c6b4b513c811528494101c4dda75e5e17
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-14 13:25:15 +0000

    c1f4a47 docs: remove word repetition (#58187) PR Close #58187

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.8+sha-6cd2028']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.8+sha-c1f4a47']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
