e5e87eb360fb3afb70d675bcab0a8152c22de504
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-15 06:42:48 +0000

    711e82e build: update scorecard action dependencies (#58202)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.9+sha-9224f9b
+ * @license Angular v19.0.0-next.9+sha-711e82e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17029,7 +17029,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.9+sha-9224f9b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.9+sha-711e82e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31323,7 +31323,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.9+sha-9224f9b');
+const VERSION = new Version('19.0.0-next.9+sha-711e82e');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
