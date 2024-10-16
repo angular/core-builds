7db8f9e25a0605f67eff973954021a55695f8234
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-16 14:53:25 +0000

    532bd11 docs(language-service): fix link URLs (#58143)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23399,7 +23399,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.8+sha-608a151");
+var VERSION2 = new Version("18.2.8+sha-532bd11");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
