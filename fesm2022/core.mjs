e6c04c615d97392d1a49c7fa927d84bf8a5fc078
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-16 07:37:11 +0000

    608a151 refactor(compiler-cli): Don't extract abstract overload multiple times (#57707)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-06f45c3
+ * @license Angular v18.2.8+sha-608a151
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16946,7 +16946,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.8+sha-06f45c3']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.8+sha-608a151']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31040,7 +31040,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-06f45c3');
+const VERSION = new Version('18.2.8+sha-608a151');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
