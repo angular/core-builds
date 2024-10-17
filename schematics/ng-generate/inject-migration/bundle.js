73f2dca81efbf64c808d9c71d97f05468451f715
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-17 10:18:52 +0000

    d330f94 build: update io_bazel_rules_sass digest to 9fa2d50 (#58235)

--- schematics/ng-generate/inject-migration/bundle.js
+++ schematics/ng-generate/inject-migration/bundle.js
@@ -23356,7 +23356,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.8+sha-e60d016");
+var VERSION2 = new Version("18.2.8+sha-d330f94");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
