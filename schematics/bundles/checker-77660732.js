b3e9271ded02ef7a357b213714d5261164a0b17b
Andrew Kushnir <akushnir@google.com>
2024-10-17 23:03:45 +0000

    e603e56 test(core): fix flaky hydration test (#58252)

--- schematics/bundles/checker-77660732.js
+++ schematics/bundles/checker-77660732.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-next.10+sha-dd407a7
+ * @license Angular v19.0.0-next.10+sha-e603e56
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29654,7 +29654,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-next.10+sha-dd407a7');
+new Version('19.0.0-next.10+sha-e603e56');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
