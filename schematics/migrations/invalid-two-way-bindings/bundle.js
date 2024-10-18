4fa1b525e9d514c72675c345f25b608a49953bda
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 09:32:24 +0000

    b0ab653 fix(compiler-cli): report when NgModule imports or exports itself (#58231)

--- schematics/migrations/invalid-two-way-bindings/bundle.js
+++ schematics/migrations/invalid-two-way-bindings/bundle.js
@@ -23614,7 +23614,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.8+sha-37b921d");
+var VERSION2 = new Version("18.2.8+sha-b0ab653");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
--- schematics/ng-generate/standalone-migration/bundle.js
+++ schematics/ng-generate/standalone-migration/bundle.js
@@ -26268,7 +26268,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.8+sha-37b921d");
+var VERSION2 = new Version("18.2.8+sha-b0ab653");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _I18N_ATTR = "i18n";
@@ -27385,7 +27385,7 @@ var MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION = "18.0.0";
 function compileDeclareClassMetadata(metadata) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION));
-  definitionMap.set("version", literal("18.2.8+sha-37b921d"));
+  definitionMap.set("version", literal("18.2.8+sha-b0ab653"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", metadata.type);
   definitionMap.set("decorators", metadata.decorators);
@@ -27404,7 +27404,7 @@ function compileComponentDeclareClassMetadata(metadata, dependencies) {
   callbackReturnDefinitionMap.set("ctorParameters", (_a2 = metadata.ctorParameters) != null ? _a2 : literal(null));
   callbackReturnDefinitionMap.set("propDecorators", (_b2 = metadata.propDecorators) != null ? _b2 : literal(null));
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION));
-  definitionMap.set("version", literal("18.2.8+sha-37b921d"));
+  definitionMap.set("version", literal("18.2.8+sha-b0ab653"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", metadata.type);
   definitionMap.set("resolveDeferredDeps", compileComponentMetadataAsyncResolver(dependencies));
@@ -27472,7 +27472,7 @@ function createDirectiveDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   const minVersion = getMinimumVersionForPartialOutput(meta);
   definitionMap.set("minVersion", literal(minVersion));
-  definitionMap.set("version", literal("18.2.8+sha-37b921d"));
+  definitionMap.set("version", literal("18.2.8+sha-b0ab653"));
   definitionMap.set("type", meta.type.value);
   if (meta.isStandalone) {
     definitionMap.set("isStandalone", literal(meta.isStandalone));
@@ -27790,7 +27790,7 @@ var MINIMUM_PARTIAL_LINKER_VERSION2 = "12.0.0";
 function compileDeclareFactoryFunction(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION2));
-  definitionMap.set("version", literal("18.2.8+sha-37b921d"));
+  definitionMap.set("version", literal("18.2.8+sha-b0ab653"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   definitionMap.set("deps", compileDependencies(meta.deps));
@@ -27813,7 +27813,7 @@ function compileDeclareInjectableFromMetadata(meta) {
 function createInjectableDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION3));
-  definitionMap.set("version", literal("18.2.8+sha-37b921d"));
+  definitionMap.set("version", literal("18.2.8+sha-b0ab653"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.providedIn !== void 0) {
@@ -27851,7 +27851,7 @@ function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION4));
-  definitionMap.set("version", literal("18.2.8+sha-37b921d"));
+  definitionMap.set("version", literal("18.2.8+sha-b0ab653"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   definitionMap.set("providers", meta.providers);
@@ -27875,7 +27875,7 @@ function createNgModuleDefinitionMap(meta) {
     throw new Error("Invalid path! Local compilation mode should not get into the partial compilation path");
   }
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION5));
-  definitionMap.set("version", literal("18.2.8+sha-37b921d"));
+  definitionMap.set("version", literal("18.2.8+sha-b0ab653"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.bootstrap.length > 0) {
@@ -27910,7 +27910,7 @@ function compileDeclarePipeFromMetadata(meta) {
 function createPipeDefinitionMap(meta) {
   const definitionMap = new DefinitionMap();
   definitionMap.set("minVersion", literal(MINIMUM_PARTIAL_LINKER_VERSION6));
-  definitionMap.set("version", literal("18.2.8+sha-37b921d"));
+  definitionMap.set("version", literal("18.2.8+sha-b0ab653"));
   definitionMap.set("ngImport", importExpr(Identifiers.core));
   definitionMap.set("type", meta.type.value);
   if (meta.isStandalone) {
@@ -27927,7 +27927,7 @@ function createPipeDefinitionMap(meta) {
 publishFacade(_global);
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
-var VERSION3 = new Version("18.2.8+sha-37b921d");
+var VERSION3 = new Version("18.2.8+sha-b0ab653");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/imports/src/emitter.mjs
 var import_typescript5 = __toESM(require("typescript"), 1);
@@ -35359,6 +35359,7 @@ function makeUnknownComponentDeferredImportDiagnostic(ref, rawExpr) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/scope/src/local.mjs
+var IN_PROGRESS_RESOLUTION = {};
 var LocalModuleScopeRegistry = class {
   constructor(localReader, fullReader, dependencyScopeReader, refEmitter, aliasingHost) {
     this.localReader = localReader;
@@ -35431,8 +35432,12 @@ var LocalModuleScopeRegistry = class {
   }
   getScopeOfModuleReference(ref) {
     if (this.cache.has(ref.node)) {
-      return this.cache.get(ref.node);
+      const cachedValue = this.cache.get(ref.node);
+      if (cachedValue !== IN_PROGRESS_RESOLUTION) {
+        return cachedValue;
+      }
     }
+    this.cache.set(ref.node, IN_PROGRESS_RESOLUTION);
     this.sealed = true;
     const ngModule = this.localReader.getNgModuleMetadata(ref);
     if (ngModule === null) {
@@ -35452,10 +35457,12 @@ var LocalModuleScopeRegistry = class {
     for (const decl of ngModule.imports) {
       const importScope = this.getExportedScope(decl, diagnostics, ref.node, "import");
       if (importScope !== null) {
-        if (importScope === "invalid" || importScope.exported.isPoisoned) {
-          diagnostics.push(invalidTransitiveNgModuleRef(decl, ngModule.rawImports, "import"));
+        if (importScope === "invalid" || importScope === "cycle" || importScope.exported.isPoisoned) {
           isPoisoned = true;
-          if (importScope === "invalid") {
+          if (importScope !== "cycle") {
+            diagnostics.push(invalidTransitiveNgModuleRef(decl, ngModule.rawImports, "import"));
+          }
+          if (importScope === "invalid" || importScope === "cycle") {
             continue;
           }
         }
@@ -35522,10 +35529,12 @@ var LocalModuleScopeRegistry = class {
     }
     for (const decl of ngModule.exports) {
       const exportScope = this.getExportedScope(decl, diagnostics, ref.node, "export");
-      if (exportScope === "invalid" || exportScope !== null && exportScope.exported.isPoisoned) {
-        diagnostics.push(invalidTransitiveNgModuleRef(decl, ngModule.rawExports, "export"));
+      if (exportScope === "invalid" || exportScope === "cycle" || exportScope !== null && exportScope.exported.isPoisoned) {
         isPoisoned = true;
-        if (exportScope === "invalid") {
+        if (exportScope !== "cycle") {
+          diagnostics.push(invalidTransitiveNgModuleRef(decl, ngModule.rawExports, "export"));
+        }
+        if (exportScope === "invalid" || exportScope === "cycle") {
           continue;
         }
       } else if (exportScope !== null) {
@@ -35593,6 +35602,10 @@ var LocalModuleScopeRegistry = class {
       }
       return this.dependencyScopeReader.resolve(ref);
     } else {
+      if (this.cache.get(ref.node) === IN_PROGRESS_RESOLUTION) {
+        diagnostics.push(makeDiagnostic(type === "import" ? ErrorCode.NGMODULE_INVALID_IMPORT : ErrorCode.NGMODULE_INVALID_EXPORT, identifierOfNode(ref.node) || ref.node, `NgModule "${type}" field contains a cycle`));
+        return "cycle";
+      }
       return this.getScopeOfModuleReference(ref);
     }
   }
