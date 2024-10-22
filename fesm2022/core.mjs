cfb33019d29b0c02fd16555ccca1a3ebab5fbd87
Andrew Kushnir <akushnir@google.com>
2024-10-22 16:49:53 +0000

    713a14f build: update actions/cache digest to 6849a64 (#58305)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-7e933d8
+ * @license Angular v18.2.8+sha-713a14f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16949,7 +16949,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.8+sha-7e933d8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.8+sha-713a14f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31043,7 +31043,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-7e933d8');
+const VERSION = new Version('18.2.8+sha-713a14f');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
