7ff214ddca5d8a114ba4e7f272cc434b0dd9b59d
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-10-23 20:16:32 +0000

    fae3c83 release: cut the v18.2.9 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.8+sha-47270d5
+ * @license Angular v18.2.9+sha-fae3c83
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16949,7 +16949,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.8+sha-47270d5']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.9+sha-fae3c83']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31043,7 +31043,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.8+sha-47270d5');
+const VERSION = new Version('18.2.9+sha-fae3c83');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
