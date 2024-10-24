9432e5d71fabf5ce81c03a49b76d96bdf0992c28
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 23:47:19 +0000

    3b989ac fix(localize): Adding  arb format to the list of valid formats in the localization extractor cli (#58287)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.9+sha-e638b29
+ * @license Angular v18.2.9+sha-3b989ac
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16949,7 +16949,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.9+sha-e638b29']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.9+sha-3b989ac']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31043,7 +31043,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.9+sha-e638b29');
+const VERSION = new Version('18.2.9+sha-3b989ac');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
