40d166986336c0e55afd97898ecf6bc6470c4024
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 23:47:07 +0000

    7ed5e6c docs(docs-infra): re-enable adev tests (#58276)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-2f1f525
+ * @license Angular v19.1.0-next.0+sha-7ed5e6c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16843,7 +16843,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-2f1f525']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-7ed5e6c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34235,7 +34235,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-2f1f525');
+const VERSION = new Version('19.1.0-next.0+sha-7ed5e6c');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-b90223bb.js
+++ schematics/bundles/index-b90223bb.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-2f1f525
+ * @license Angular v19.1.0-next.0+sha-7ed5e6c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -29682,7 +29682,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-2f1f525');
+new Version('19.1.0-next.0+sha-7ed5e6c');
 
 var _VisitorMode;
 (function (_VisitorMode) {
