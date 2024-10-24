a881f4ef8103dde447fa45dbde381713534b5608
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 19:46:53 +0000

    e638b29 docs(router): document segment consumption during route evaluation (#58069)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.9+sha-fae3c83
+ * @license Angular v18.2.9+sha-e638b29
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16949,7 +16949,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.9+sha-fae3c83']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.9+sha-e638b29']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31043,7 +31043,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.9+sha-fae3c83');
+const VERSION = new Version('18.2.9+sha-e638b29');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
