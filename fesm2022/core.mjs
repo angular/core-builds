8f8c72b7eca25a06bb114795594b41abad1400cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-28 19:57:13 +0000

    7d9b38e docs: update Angular CLI help [main] (#58396)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-db467e1
+ * @license Angular v19.1.0-next.0+sha-7d9b38e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16954,7 +16954,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-db467e1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-7d9b38e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34384,7 +34384,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-db467e1');
+const VERSION = new Version('19.1.0-next.0+sha-7d9b38e');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
