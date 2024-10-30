6296a5d3147d66f79d090f09ecff7f2d98a3021a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 20:27:23 +0000

    d8829fb docs: release notes for the v19.0.0-rc.0 release

--- schematics/bundles/combine_units-11b76063.js
+++ schematics/bundles/combine_units-11b76063.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-af5d776
+ * @license Angular v19.1.0-next.0+sha-d8829fb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30308,7 +30308,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-af5d776');
+new Version('19.1.0-next.0+sha-d8829fb');
 
 var _VisitorMode;
 (function (_VisitorMode) {
