713458623e6a6d76a18709df42639a433acf7baf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 17:19:20 +0000

    378284f refactor(core): introduce `ngServerMode` as global (#58386)

--- schematics/bundles/combine_units-11b76063.js
+++ schematics/bundles/combine_units-11b76063.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-3161360
+ * @license Angular v19.1.0-next.0+sha-378284f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30308,7 +30308,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-3161360');
+new Version('19.1.0-next.0+sha-378284f');
 
 var _VisitorMode;
 (function (_VisitorMode) {
