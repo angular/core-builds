4241a31694add38469bfd72412392b72dadd548e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 19:00:35 +0000

    dcd16b4 refactor(migrations): Make the explicit standalone migration idempotent (#58418)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.11+sha-3230d78
+ * @license Angular v19.0.0-next.11+sha-dcd16b4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16946,7 +16946,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.11+sha-3230d78']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-next.11+sha-dcd16b4']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34405,7 +34405,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-next.11+sha-3230d78');
+const VERSION = new Version('19.0.0-next.11+sha-dcd16b4');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
