59934de7518d8fe67e13a429e004214e68f1f548
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-01 14:40:10 +0000

    a249d0b docs: fix external link in NG0100 error page (#58462)

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.10+sha-127023f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.10+sha-a249d0b']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
