d5861baa44b0e88c6a2cfad68d05ddf912fa2d47
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-04 16:20:38 +0000

    31430a6 docs: fix grammatical error in template binding documentation (#58491)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-4ef11c9
+ * @license Angular v19.1.0-next.0+sha-31430a6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16943,7 +16943,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-4ef11c9']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-31430a6']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34240,7 +34240,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-4ef11c9');
+const VERSION = new Version('19.1.0-next.0+sha-31430a6');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
