c6d05b50fb4cfd311bc36559e7a045234063b1a6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-05 07:19:46 +0000

    9b8a2c3 build: update all non-major dependencies (#58506)

--- schematics/bundles/combine_units-c0063d0e.js
+++ schematics/bundles/combine_units-c0063d0e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-5fe3770
+ * @license Angular v19.1.0-next.0+sha-9b8a2c3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30307,7 +30307,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-5fe3770');
+new Version('19.1.0-next.0+sha-9b8a2c3');
 
 var _VisitorMode;
 (function (_VisitorMode) {
