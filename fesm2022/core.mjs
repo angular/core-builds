def5fcf34d799a4085353ee4931f0abbc25a5257
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-05 13:15:33 +0000

    e7ac965 docs: update Angular CLI help [19.0.x] (#58511)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.0+sha-c36a1c0
+ * @license Angular v19.0.0-rc.0+sha-e7ac965
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16943,7 +16943,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.0+sha-c36a1c0']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.0+sha-e7ac965']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34245,7 +34245,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.0+sha-c36a1c0');
+const VERSION = new Version('19.0.0-rc.0+sha-e7ac965');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/combine_units-c0063d0e.js
+++ schematics/bundles/combine_units-c0063d0e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.0+sha-c36a1c0
+ * @license Angular v19.0.0-rc.0+sha-e7ac965
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30307,7 +30307,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-rc.0+sha-c36a1c0');
+new Version('19.0.0-rc.0+sha-e7ac965');
 
 var _VisitorMode;
 (function (_VisitorMode) {
