dbb3596b0cc8203ec1aa53e8b46e901ba749d1b6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-06 18:25:33 +0000

    0642f45 docs: release notes for the v19.0.0-rc.1 release

--- schematics/bundles/checker-9ca42e51.js
+++ schematics/bundles/checker-9ca42e51.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-b469952
+ * @license Angular v19.1.0-next.0+sha-0642f45
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30274,7 +30274,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-b469952');
+new Version('19.1.0-next.0+sha-0642f45');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
