772e9cf57321d6ea3a2cd8bad46f1ffe6712e78b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-07 12:32:26 +0000

    10e6a25 docs: add component HMR information to application build system (#58528)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-f994539
+ * @license Angular v19.0.0-rc.1+sha-10e6a25
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16943,7 +16943,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-f994539']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-10e6a25']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34242,7 +34242,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.1+sha-f994539');
+const VERSION = new Version('19.0.0-rc.1+sha-10e6a25');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
