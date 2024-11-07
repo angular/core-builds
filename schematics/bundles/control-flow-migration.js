898de4107939cddabf8e935f45a18cf91b8eaaea
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-07 19:05:01 +0000

    3aa0263 docs: add v19 instructions in the update guide (#58268)

--- schematics/bundles/control-flow-migration.js
+++ schematics/bundles/control-flow-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.1+sha-ab701ad
+ * @license Angular v19.0.0-rc.1+sha-3aa0263
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.1+sha-ab701ad
+ * @license Angular v19.0.0-rc.1+sha-3aa0263
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.0-rc.1+sha-ab701ad');
+new checker.Version('19.0.0-rc.1+sha-3aa0263');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
