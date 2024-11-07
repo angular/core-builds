b034d4461f90c62e557ebeb76049852d200dd23b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-07 17:00:41 +0000

    c5ce302 docs(docs-infra): add support for `@remarks` (#58523)

--- schematics/bundles/checker-9ca42e51.js
+++ schematics/bundles/checker-9ca42e51.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-d076c32
+ * @license Angular v19.1.0-next.0+sha-c5ce302
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30274,7 +30274,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-d076c32');
+new Version('19.1.0-next.0+sha-c5ce302');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
