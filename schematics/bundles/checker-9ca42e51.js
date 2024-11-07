09a7329054fb4ff57dc4a98c1e446a2dc4637d6b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-07 22:49:05 +0000

    4c3e1a9 ci: add packages/ssr in pullappove (#58556)

--- schematics/bundles/checker-9ca42e51.js
+++ schematics/bundles/checker-9ca42e51.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-48be715
+ * @license Angular v19.1.0-next.0+sha-4c3e1a9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30274,7 +30274,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-48be715');
+new Version('19.1.0-next.0+sha-4c3e1a9');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
