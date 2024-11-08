667bd428db8ff8c5d4daa70ba0f5c1e0198ba884
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-08 19:01:47 +0000

    bc5aa3c docs: update hybrid rendering docs to remove `RenderMode.AppShell` (#58564)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-ff11551
+ * @license Angular v19.1.0-next.0+sha-bc5aa3c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.0+sha-ff11551');
+new checker.Version('19.1.0-next.0+sha-bc5aa3c');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
