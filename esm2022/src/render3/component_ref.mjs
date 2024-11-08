2497ad5a0a05d187deb1b5c2a2e730bc72ecc5ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-08 17:33:44 +0000

    2fbc7f0 docs: Fix menu label for ng cache clean, clear -> clean (#58560) PR Close #58560

--- esm2022/src/render3/component_ref.mjs
+++ esm2022/src/render3/component_ref.mjs
@@ -395,7 +395,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.11+sha-f3567cc']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.11+sha-2fbc7f0']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.11+sha-f3567cc");
+var VERSION2 = new Version("18.2.11+sha-2fbc7f0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
