249acaa3bef21045b861e1cf67c5664111888994
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 14:55:56 +0000

    64661dd ci: re-enable adev tests (#58574)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-b6a998b
+ * @license Angular v19.0.0-rc.1+sha-64661dd
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16621,7 +16621,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-b6a998b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-64661dd']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34298,7 +34298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.1+sha-b6a998b');
+const VERSION = new Version('19.0.0-rc.1+sha-64661dd');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
