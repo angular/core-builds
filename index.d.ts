145efd37b75e7ab0206fe78635e15f556167f28e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 19:16:30 +0000

    c77b53b refactor(core): reorganize defer codebase (#58598)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-b834d7f
+ * @license Angular v19.0.0-rc.1+sha-c77b53b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14263,6 +14263,7 @@ export declare class ɵRender3NgModuleRef<T> extends NgModuleRef<T> implements I
     onDestroy(callback: () => void): void;
 }
 
+/** Rendering Helpers */
 /**
  * Transitions a defer block to the new state. Updates the  necessary
  * data structures and renders corresponding block.
--- schematics/bundles/control-flow-migration.js
+++ schematics/bundles/control-flow-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.1+sha-b834d7f
+ * @license Angular v19.0.0-rc.1+sha-c77b53b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
