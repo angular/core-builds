34497922b7868057dfbdec28e711684ee8d1b407
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 17:58:24 +0000

    869df8e docs: remove "getting started" from headers on API ref page (#58595)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-33e62c4
+ * @license Angular v19.1.0-next.0+sha-869df8e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16621,7 +16621,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-33e62c4']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-869df8e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34298,7 +34298,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-33e62c4');
+const VERSION = new Version('19.1.0-next.0+sha-869df8e');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
