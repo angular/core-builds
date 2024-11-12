0c44ec736795b5333270d62ccf5908e782806647
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-12 22:30:25 +0000

    f3d9316 refactor(core): moves incremental hydration codebase to better locations (#58616)

--- schematics/bundles/checker-cd95ebda.js
+++ schematics/bundles/checker-cd95ebda.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-9fced64
+ * @license Angular v19.1.0-next.0+sha-f3d9316
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30278,7 +30278,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-9fced64');
+new Version('19.1.0-next.0+sha-f3d9316');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-9fced64
+ * @license Angular v19.1.0-next.0+sha-f3d9316
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.0+sha-9fced64');
+new checker.Version('19.1.0-next.0+sha-f3d9316');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
