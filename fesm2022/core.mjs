cbb67fbff2007a0c85f0982a5e0aa2490d0c36f3
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-12 18:44:33 +0000

    26602fc ci: remove circleci as we no longer rely on it (#58615)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-560282a
+ * @license Angular v19.0.0-rc.1+sha-26602fc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16621,7 +16621,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-560282a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-26602fc']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34309,7 +34309,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.1+sha-560282a');
+const VERSION = new Version('19.0.0-rc.1+sha-26602fc');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/pending-tasks.js
+++ schematics/bundles/pending-tasks.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.1+sha-560282a
+ * @license Angular v19.0.0-rc.1+sha-26602fc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
