c56b92feacffdfc999e272ea9b041e4a0db2d065
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 18:54:29 +0000

    e1e2e3f build: update io_bazel_rules_sass digest to b442a44 (#58617)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-4abb645
+ * @license Angular v19.0.0-rc.1+sha-e1e2e3f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-4abb645']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-e1e2e3f']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34472,7 +34472,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.1+sha-4abb645');
+const VERSION = new Version('19.0.0-rc.1+sha-e1e2e3f');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
