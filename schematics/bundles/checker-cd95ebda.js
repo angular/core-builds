b0419f57044622590019a3cbefb7920ee478289f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 18:09:58 +0000

    e26797b fix(migrations): replace removed NgModules in tests with their exports (#58627)

--- schematics/bundles/checker-cd95ebda.js
+++ schematics/bundles/checker-cd95ebda.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.1+sha-29207d8
+ * @license Angular v19.0.0-rc.1+sha-e26797b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30278,7 +30278,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-rc.1+sha-29207d8');
+new Version('19.0.0-rc.1+sha-e26797b');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
