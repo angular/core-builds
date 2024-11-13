4bb6a4018c8741fe0ce48474de0099adfb4e626c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 20:09:22 +0000

    84c543a docs: update NgModule overview for standalone default (#58606)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-4f7ad3a
+ * @license Angular v19.1.0-next.0+sha-84c543a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-4f7ad3a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-84c543a']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34472,7 +34472,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-4f7ad3a');
+const VERSION = new Version('19.1.0-next.0+sha-84c543a');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
