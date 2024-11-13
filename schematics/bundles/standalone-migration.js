2c0ef1b28200bde3332619ed7bb28d51ab377536
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 18:42:55 +0000

    4abb645 build: update cross-repo angular dependencies (#58639)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.1+sha-e26797b
+ * @license Angular v19.0.0-rc.1+sha-4abb645
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.0-rc.1+sha-e26797b');
+new checker.Version('19.0.0-rc.1+sha-4abb645');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
