21fad219834f3bd9db41a490e223c8bb99bc57dc
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 15:45:45 +0000

    8f558b6 docs: update components guide for default standalone (#58605)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-f3d9316
+ * @license Angular v19.1.0-next.0+sha-8f558b6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-f3d9316']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-8f558b6']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34472,7 +34472,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-f3d9316');
+const VERSION = new Version('19.1.0-next.0+sha-8f558b6');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
