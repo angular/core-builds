3e74e0f1e0f636eb860ec83aeb27f33ce135c7f8
Andrew Kushnir <akushnir@google.com>
2024-11-14 22:26:37 +0000

    91a616f refactor(core): add `REQUEST`, `RESPONSE_INIT` and `REQUEST_CONTEXT` tokens (#58669)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.2+sha-ee49947
+ * @license Angular v19.0.0-rc.2+sha-91a616f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9583,6 +9583,38 @@ export declare interface RendererType2 {
 }
 
 /**
+ * Injection token representing the current HTTP request object.
+ *
+ * Use this token to access the current request when handling server-side
+ * rendering (SSR).
+ *
+ * @remarks
+ * This token may be `null` in the following scenarios:
+ *
+ * * During the build processes.
+ * * When the application is rendered in the browser (client-side rendering).
+ * * When performing static site generation (SSG).
+ * * During route extraction in development (at the time of the request).
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Request | `Request` on MDN}
+ *
+ * @developerPreview
+ */
+export declare const REQUEST: InjectionToken<Request | null>;
+
+/**
+ * Injection token for additional request context.
+ *
+ * Use this token to pass custom metadata or context related to the current request in server-side rendering.
+ *
+ * @remarks
+ * This token is only available during server-side rendering and will be `null` in other contexts.
+ *
+ * @developerPreview
+ */
+export declare const REQUEST_CONTEXT: InjectionToken<unknown>;
+
+/**
  * Lazily retrieves the reference value from a forwardRef.
  *
  * Acts as the identity function when given a non-forward-ref value.
@@ -9754,6 +9786,26 @@ export declare enum ResourceStatus {
      */
     Local = 5
 }
+
+/**
+ * Injection token for response initialization options.
+ *
+ * Use this token to provide response options for configuring or initializing
+ * HTTP responses in server-side rendering or API endpoints.
+ *
+ * @remarks
+ * This token may be `null` in the following scenarios:
+ *
+ * * During the build processes.
+ * * When the application is rendered in the browser (client-side rendering).
+ * * When performing static site generation (SSG).
+ * * During route extraction in development (at the time of the request).
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Response/Response | `ResponseInit` on MDN}
+ *
+ * @developerPreview
+ */
+export declare const RESPONSE_INIT: InjectionToken<ResponseInit | null>;
 
 /**
  * The goal here is to make sure that the browser DOM API is the Renderer.
