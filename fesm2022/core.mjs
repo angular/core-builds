17cedf13f36305200472ba2b996047c84c3b7423
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-14 15:12:52 +0000

    fa1b021 refactor(core): add performance feature flag for incremental hydration (#58658)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.1+sha-5fe57d4
+ * @license Angular v19.0.0-rc.1+sha-fa1b021
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-5fe57d4']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.1+sha-fa1b021']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.1+sha-5fe57d4');
+const VERSION = new Version('19.0.0-rc.1+sha-fa1b021');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
@@ -39635,6 +39635,7 @@ function withIncrementalHydration() {
             provide: ENVIRONMENT_INITIALIZER,
             useValue: () => {
                 enableIncrementalHydrationRuntimeSupport();
+                performanceMarkFeature('NgIncrementalHydration');
             },
             multi: true,
         },
--- schematics/bundles/checker-cd95ebda.js
+++ schematics/bundles/checker-cd95ebda.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.1+sha-5fe57d4
+ * @license Angular v19.0.0-rc.1+sha-fa1b021
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30278,7 +30278,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-rc.1+sha-5fe57d4');
+new Version('19.0.0-rc.1+sha-fa1b021');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
