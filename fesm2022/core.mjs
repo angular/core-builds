b95e6fa63a504afb0993c6bbd46ab8d631c3abcb
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-14 15:12:56 +0000

    0dc8757 refactor(core): add performance feature flag for incremental hydration (#58658)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-292d819
+ * @license Angular v19.1.0-next.0+sha-0dc8757
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-292d819']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-0dc8757']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-292d819');
+const VERSION = new Version('19.1.0-next.0+sha-0dc8757');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
@@ -39635,6 +39635,7 @@ function withIncrementalHydration() {
             provide: ENVIRONMENT_INITIALIZER,
             useValue: () => {
                 enableIncrementalHydrationRuntimeSupport();
+                performanceMarkFeature('NgIncrementalHydration');
             },
             multi: true,
         },
