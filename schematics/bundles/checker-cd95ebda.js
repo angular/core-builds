e6eba79c39601c5e3e5366f8bb4882a16c9e5986
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-14 16:19:24 +0000

    666a2a4 docs: release notes for the v18.2.12 release

--- schematics/bundles/checker-cd95ebda.js
+++ schematics/bundles/checker-cd95ebda.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-0dc8757
+ * @license Angular v19.1.0-next.0+sha-666a2a4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30278,7 +30278,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-0dc8757');
+new Version('19.1.0-next.0+sha-666a2a4');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
