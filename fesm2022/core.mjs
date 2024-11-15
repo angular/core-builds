9484a4ff805788fae3767606ed40820b354f2cf7
Andrew Kushnir <akushnir@google.com>
2024-11-15 17:55:15 +0000

    497fe97 docs: add incremental hydration guide (#58671)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.3+sha-36a5712
+ * @license Angular v19.0.0-rc.3+sha-497fe97
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.3+sha-36a5712']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.3+sha-497fe97']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.3+sha-36a5712');
+const VERSION = new Version('19.0.0-rc.3+sha-497fe97');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
