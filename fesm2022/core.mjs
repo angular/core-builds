4899678f1b08fb50ac2e76f1afcf6c466396d408
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-15 13:21:12 +0000

    2a767a3 refactor(core): mark linkedSignal as developer preview (#58684)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-ff71af7
+ * @license Angular v19.1.0-next.0+sha-2a767a3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-ff71af7']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-2a767a3']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-ff71af7');
+const VERSION = new Version('19.1.0-next.0+sha-2a767a3');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/output-migration.js
+++ schematics/bundles/output-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-ff71af7
+ * @license Angular v19.1.0-next.0+sha-2a767a3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -268,7 +268,7 @@ class OutputMigration extends combine_units.TsurgeFunnelMigration {
         const knownFields = {
             // Note: We don't support cross-target migration of `Partial<T>` usages.
             // This is an acceptable limitation for performance reasons.
-            shouldTrackClassReference: (node) => false,
+            shouldTrackClassReference: () => false,
             attemptRetrieveDescriptorFromSymbol: (s) => {
                 const propDeclaration = getTargetPropertyDeclaration(s);
                 if (propDeclaration !== null) {
@@ -377,13 +377,17 @@ class OutputMigration extends combine_units.TsurgeFunnelMigration {
             // detect .next usages that should be migrated to .emit in template and host binding expressions
             if (ref.kind === combine_units.ReferenceKind.InTemplate) {
                 const callExpr = checkNonTsReferenceCallsField(ref, 'next');
-                if (callExpr !== null) {
+                // TODO: here and below for host bindings, we should ideally filter in the global meta stage
+                // (instead of using the `outputFieldReplacements` map)
+                //  as technically, the call expression could refer to an output
+                //  from a whole different compilation unit (e.g. tsconfig.json).
+                if (callExpr !== null && outputFieldReplacements[ref.target.key] !== undefined) {
                     addOutputReplacement(outputFieldReplacements, ref.target.key, ref.from.templateFile, calculateNextFnReplacementInTemplate(ref.from.templateFile, callExpr.nameSpan));
                 }
             }
             else if (ref.kind === combine_units.ReferenceKind.InHostBinding) {
                 const callExpr = checkNonTsReferenceCallsField(ref, 'next');
-                if (callExpr !== null) {
+                if (callExpr !== null && outputFieldReplacements[ref.target.key] !== undefined) {
                     addOutputReplacement(outputFieldReplacements, ref.target.key, ref.from.file, calculateNextFnReplacementInHostBinding(ref.from.file, ref.from.hostPropertyNode.getStart() + 1, callExpr.nameSpan));
                 }
             }
