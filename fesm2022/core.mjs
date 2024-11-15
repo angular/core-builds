184e8a807979fece0501a2058540efb170ec6b00
Andrew Kushnir <akushnir@google.com>
2024-11-15 22:41:37 +0000

    805d8f1 docs: reorganize NgModule and Animations guides (#58694)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.3+sha-392b992
+ * @license Angular v19.0.0-rc.3+sha-805d8f1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.3+sha-392b992']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.3+sha-805d8f1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.3+sha-392b992');
+const VERSION = new Version('19.0.0-rc.3+sha-805d8f1');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.3+sha-392b992
+ * @license Angular v19.0.0-rc.3+sha-805d8f1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.0-rc.3+sha-392b992');
+new checker.Version('19.0.0-rc.3+sha-805d8f1');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
