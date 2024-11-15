9f38067db4c6745112defcca9568969948260d8b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-15 11:58:52 +0000

    e33b195 fix(bazel): allow strictStandalone to be passed through tsconfig (#58683)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-rc.2+sha-806a61b
+ * @license Angular v19.0.0-rc.2+sha-e33b195
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.2+sha-806a61b']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0-rc.2+sha-e33b195']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0-rc.2+sha-806a61b');
+const VERSION = new Version('19.0.0-rc.2+sha-e33b195');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-e3da3b0a.js
+++ schematics/bundles/checker-e3da3b0a.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.2+sha-806a61b
+ * @license Angular v19.0.0-rc.2+sha-e33b195
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30310,7 +30310,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-rc.2+sha-806a61b');
+new Version('19.0.0-rc.2+sha-e33b195');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
