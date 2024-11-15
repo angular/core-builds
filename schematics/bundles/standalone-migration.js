09827b97020a8fee6821d6fb9f065954b90dd59f
Andrew Kushnir <akushnir@google.com>
2024-11-15 18:37:47 +0000

    0deb6f9 docs(docs-infra): remove Featured section from the API reference list page (#58659)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-ea02069
+ * @license Angular v19.1.0-next.0+sha-0deb6f9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.0+sha-ea02069');
+new checker.Version('19.1.0-next.0+sha-0deb6f9');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
