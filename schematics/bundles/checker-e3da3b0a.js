751dcf268a9a7c7b238134402d7fa0d91e9dbceb
Andrew Kushnir <akushnir@google.com>
2024-11-18 20:54:30 +0000

    2c05f4c docs: add v19 developer event banner (#58721)

--- schematics/bundles/checker-e3da3b0a.js
+++ schematics/bundles/checker-e3da3b0a.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.3+sha-805d8f1
+ * @license Angular v19.0.0-rc.3+sha-2c05f4c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30310,7 +30310,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0-rc.3+sha-805d8f1');
+new Version('19.0.0-rc.3+sha-2c05f4c');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
