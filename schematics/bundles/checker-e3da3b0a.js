c7b793d8c0a4484789d73940fa09ef3e0427daba
GitHub <noreply@github.com>
2024-11-19 14:33:16 +0000

    e29bd14 docs: release notes for the v19.0.0 release (#58735)

--- schematics/bundles/checker-e3da3b0a.js
+++ schematics/bundles/checker-e3da3b0a.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-0274e6c
+ * @license Angular v19.1.0-next.0+sha-e29bd14
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30310,7 +30310,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-0274e6c');
+new Version('19.1.0-next.0+sha-e29bd14');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
