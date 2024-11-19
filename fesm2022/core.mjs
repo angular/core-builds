465f97f4035b0a00cc248a5f134ab85f9d7aa15f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-11-19 17:43:54 +0000

    5838f43 docs: add link to v19 blog post (#58745)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0+sha-c4357eb
+ * @license Angular v19.0.0+sha-5838f43
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0+sha-c4357eb']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0+sha-5838f43']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0+sha-c4357eb');
+const VERSION = new Version('19.0.0+sha-5838f43');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0+sha-c4357eb
+ * @license Angular v19.0.0+sha-5838f43
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.0+sha-c4357eb');
+new checker.Version('19.0.0+sha-5838f43');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
