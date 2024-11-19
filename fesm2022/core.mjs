198c8d16bc25cce273c85d94ebe698e1d7a9ed92
Andrew Kushnir <akushnir@google.com>
2024-11-19 18:00:51 +0000

    b69b16c docs: update standalone migration guide (#58703)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-284771d
+ * @license Angular v19.1.0-next.0+sha-b69b16c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-284771d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-b69b16c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-284771d');
+const VERSION = new Version('19.1.0-next.0+sha-b69b16c');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
