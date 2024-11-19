c07d3d5c6a13f8abcb7277e53cdf3014afdbbef9
Andrew Kushnir <akushnir@google.com>
2024-11-19 00:07:59 +0000

    02b258a docs: update the angular roadmap for v19 (#58549)

--- schematics/bundles/provide-initializer.js
+++ schematics/bundles/provide-initializer.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0-rc.3+sha-f696ecb
+ * @license Angular v19.0.0-rc.3+sha-02b258a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
