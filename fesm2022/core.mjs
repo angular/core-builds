e8937adf9cc70e438efc429f2ee059fe394136a2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-11-19 17:44:00 +0000

    284771d docs: add link to v19 blog post (#58745)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-0ce15c6
+ * @license Angular v19.1.0-next.0+sha-284771d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16687,7 +16687,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-0ce15c6']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-284771d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34492,7 +34492,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-0ce15c6');
+const VERSION = new Version('19.1.0-next.0+sha-284771d');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
