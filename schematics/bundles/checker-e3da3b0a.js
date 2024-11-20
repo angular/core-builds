f60715ae710fd0034ccb5add394991c3d0b2e4b4
Andrew Kushnir <akushnir@google.com>
2024-11-20 00:39:41 +0000

    f4e2777 docs: add details on how `@defer` loads deps (#58763)

--- schematics/bundles/checker-e3da3b0a.js
+++ schematics/bundles/checker-e3da3b0a.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0+sha-a983865
+ * @license Angular v19.0.0+sha-f4e2777
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30310,7 +30310,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0+sha-a983865');
+new Version('19.0.0+sha-f4e2777');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
