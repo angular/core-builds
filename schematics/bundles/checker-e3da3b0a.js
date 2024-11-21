e20e5f41de54b4c9f2fb11db4adae6674cb4e454
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-21 21:45:34 +0000

    d3491c7 fix(core): Prevents race condition of cleanup for incremental hydration (#58722)

--- schematics/bundles/checker-e3da3b0a.js
+++ schematics/bundles/checker-e3da3b0a.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0+sha-5ce1026
+ * @license Angular v19.0.0+sha-d3491c7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30310,7 +30310,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0+sha-5ce1026');
+new Version('19.0.0+sha-d3491c7');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
