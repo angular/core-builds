ba72f958fdccb05c26383409101f926443df7d86
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 20:37:07 +0000

    1bdc3f1 docs(docs-infra): changed default type to object (#58289)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-834c7c3
+ * @license Angular v19.1.0-next.0+sha-1bdc3f1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18072,7 +18072,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-834c7c3']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-1bdc3f1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34568,7 +34568,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-834c7c3');
+const VERSION = new Version('19.1.0-next.0+sha-1bdc3f1');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
