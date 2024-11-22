a6ac347fed3b6c4721d0a07ee6af5cd3f55a54fa
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 16:19:43 +0000

    e294ca8 docs: fix RxJS interop typo (#58823) PR Close #58823

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-843c34f
+ * @license Angular v19.1.0-next.0+sha-e294ca8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18072,7 +18072,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-843c34f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-e294ca8']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34553,7 +34553,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-843c34f');
+const VERSION = new Version('19.1.0-next.0+sha-e294ca8');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
