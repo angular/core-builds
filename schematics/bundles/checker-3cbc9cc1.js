8c57624146ec0fd20418026735515c9825b9ce88
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-25 16:18:18 +0000

    03a14b5 refactor(core): invoke `setActiveConsumer` in `ɵɵdeferHydrateWhen` at the right time (#58864)

--- schematics/bundles/checker-3cbc9cc1.js
+++ schematics/bundles/checker-3cbc9cc1.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0+sha-08b9452
+ * @license Angular v19.0.0+sha-03a14b5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30323,7 +30323,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0+sha-08b9452');
+new Version('19.0.0+sha-03a14b5');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0+sha-08b9452
+ * @license Angular v19.0.0+sha-03a14b5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.0+sha-08b9452');
+new checker.Version('19.0.0+sha-03a14b5');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
