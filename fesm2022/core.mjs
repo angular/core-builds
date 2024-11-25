1a00992370c46432f8ca4ab25570a943d25aa54f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-25 18:06:06 +0000

    bae473d build: update cross-repo angular dependencies (#58877)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0+sha-83d2a09
+ * @license Angular v19.0.0+sha-bae473d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18078,7 +18078,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0+sha-83d2a09']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.0+sha-bae473d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34575,7 +34575,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.0+sha-83d2a09');
+const VERSION = new Version('19.0.0+sha-bae473d');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
