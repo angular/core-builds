159e64d2a8edaa5e1206f87ff821617d1489a944
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-26 14:55:04 +0000

    0c86d3e refactor(router): remove unused default error handler function (#58819)

--- schematics/bundles/checker-3cbc9cc1.js
+++ schematics/bundles/checker-3cbc9cc1.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0+sha-f0ecbe6
+ * @license Angular v19.0.0+sha-0c86d3e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30323,7 +30323,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.0+sha-f0ecbe6');
+new Version('19.0.0+sha-0c86d3e');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.0+sha-f0ecbe6
+ * @license Angular v19.0.0+sha-0c86d3e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.0+sha-f0ecbe6');
+new checker.Version('19.0.0+sha-0c86d3e');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
