342301356e1b5c4c5c96a3b8cdd8fd14d3dc251e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-26 18:09:44 +0000

    e1e419a release: cut the v18.2.13 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.12+sha-c6cec5e
+ * @license Angular v18.2.13+sha-e1e419a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16940,7 +16940,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.12+sha-c6cec5e']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '18.2.13+sha-e1e419a']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -31056,7 +31056,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('18.2.12+sha-c6cec5e');
+const VERSION = new Version('18.2.13+sha-e1e419a');
 
 /*
  * This file exists to support compilation of @angular/core in Ivy mode.
