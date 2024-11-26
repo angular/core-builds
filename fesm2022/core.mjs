e387183ecceaa08a65a32c77096a0c7dcc7fa648
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-26 20:13:20 +0000

    f6e4485 build: update cross-repo angular dependencies to ^19.1.0-next (#58910)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.1+sha-552092d
+ * @license Angular v19.0.1+sha-f6e4485
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18078,7 +18078,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.1+sha-552092d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.1+sha-f6e4485']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34558,7 +34558,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.1+sha-552092d');
+const VERSION = new Version('19.0.1+sha-f6e4485');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-552092d
+ * @license Angular v19.0.1+sha-f6e4485
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.1+sha-552092d');
+new checker.Version('19.0.1+sha-f6e4485');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
