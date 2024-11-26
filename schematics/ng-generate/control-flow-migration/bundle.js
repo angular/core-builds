74135ddd08b4b456ffdf74124f1435fba1697ff6
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-26 14:53:47 +0000

    c6cec5e docs: angular dev v18 page issue fix (#58848)

--- schematics/ng-generate/control-flow-migration/bundle.js
+++ schematics/ng-generate/control-flow-migration/bundle.js
@@ -23357,7 +23357,7 @@ function publishFacade(global) {
 }
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/version.mjs
-var VERSION2 = new Version("18.2.12+sha-06d70a2");
+var VERSION2 = new Version("18.2.12+sha-c6cec5e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler/src/i18n/extractor_merger.mjs
 var _VisitorMode;
