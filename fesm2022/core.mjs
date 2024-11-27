0c8940c094b81b47a44e7fd312aa67dbb50e0a84
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-27 10:04:26 +0000

    2e4e485 refactor(docs-infra): use interpolation instead of innerHTML for better perf (#58913)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.1+sha-4852e57
+ * @license Angular v19.0.1+sha-2e4e485
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18078,7 +18078,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.1+sha-4852e57']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.1+sha-2e4e485']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34558,7 +34558,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.1+sha-4852e57');
+const VERSION = new Version('19.0.1+sha-2e4e485');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
