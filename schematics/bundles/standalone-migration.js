9efb6dacd5eafcdafe01173aa60287197f82bf10
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-27 10:04:32 +0000

    0c00907 refactor(docs-infra): use interpolation instead of innerHTML for better perf (#58913)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-0f1c718
+ * @license Angular v19.1.0-next.0+sha-0c00907
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.0+sha-0f1c718');
+new checker.Version('19.1.0-next.0+sha-0c00907');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
