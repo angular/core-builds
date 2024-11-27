d0e6a2e41da44c4e32e2f4f93b21de2c43937b45
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-27 17:17:11 +0000

    4792db9 fix(core): Explicitly manage TracingSnapshot lifecycle and dispose of it once it's been used. (#58929)

--- schematics/bundles/checker-bef908ad.js
+++ schematics/bundles/checker-bef908ad.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-d23a5d5
+ * @license Angular v19.0.1+sha-4792db9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30332,7 +30332,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.1+sha-d23a5d5');
+new Version('19.0.1+sha-4792db9');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-d23a5d5
+ * @license Angular v19.0.1+sha-4792db9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.1+sha-d23a5d5');
+new checker.Version('19.0.1+sha-4792db9');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
