6e09c7fc9c2bfd520860ec584dfdc35cb422ae6e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 16:46:34 +0000

    3eedb98 fix(docs-infra): do not focus h1 from docs-top-level-banner (#57698)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-dabbeb6
+ * @license Angular v19.0.1+sha-3eedb98
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.1+sha-dabbeb6');
+new checker.Version('19.0.1+sha-3eedb98');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
