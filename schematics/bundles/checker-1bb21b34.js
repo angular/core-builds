48596dce51a51d64779cbc7035d2ae8ede7881ea
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 16:58:52 +0000

    9bee4a3 docs: fix `IMPORTANT` for "Animating the items of a reordering list" (#58938)

--- schematics/bundles/checker-1bb21b34.js
+++ schematics/bundles/checker-1bb21b34.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-cca70cb
+ * @license Angular v19.0.1+sha-9bee4a3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30332,7 +30332,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.1+sha-cca70cb');
+new Version('19.0.1+sha-9bee4a3');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/imports-4ac08251.js
+++ schematics/bundles/imports-4ac08251.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-cca70cb
+ * @license Angular v19.0.1+sha-9bee4a3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
