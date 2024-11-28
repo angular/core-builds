b183759853d7536a4a2a6303ef258683cb026605
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 09:19:57 +0000

    293dce9 docs(docs-infra): remove redundant nav menu items from the DOM in the reference (#58934)

--- schematics/bundles/checker-1bb21b34.js
+++ schematics/bundles/checker-1bb21b34.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-9f99196
+ * @license Angular v19.0.1+sha-293dce9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30332,7 +30332,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.1+sha-9f99196');
+new Version('19.0.1+sha-293dce9');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
