97271c7e30ba2adab96909766d7ee287e5fb8d2f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 08:48:49 +0000

    6ff53b7 docs(docs-infra): Drop standalone: true (#58914)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-3b76536
+ * @license Angular v19.1.0-next.0+sha-6ff53b7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18079,7 +18079,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-3b76536']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-6ff53b7']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-3b76536');
+const VERSION = new Version('19.1.0-next.0+sha-6ff53b7');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
