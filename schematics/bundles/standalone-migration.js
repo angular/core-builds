de7159907dee3f77107397a6c6a3b06b06fa9a85
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 15:48:09 +0000

    66abff6 docs(docs-infra): don't use `URL` to check for external links (#58955)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-a4b86b2
+ * @license Angular v19.1.0-next.0+sha-66abff6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.0+sha-a4b86b2');
+new checker.Version('19.1.0-next.0+sha-66abff6');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
