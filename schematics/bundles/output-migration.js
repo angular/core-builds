850d74224511c294b0930e61027725fd94e904df
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 09:35:16 +0000

    c639210 refactor(devtools): add missing imports (#59000)

--- schematics/bundles/output-migration.js
+++ schematics/bundles/output-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-4392cce
+ * @license Angular v19.0.1+sha-c639210
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-4392cce
+ * @license Angular v19.0.1+sha-c639210
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.1+sha-4392cce');
+new checker.Version('19.0.1+sha-c639210');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
