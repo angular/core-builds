a5acc2791086d480d7d31b0bc8b4a0e1088a81bb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 09:49:40 +0000

    e4b85d1 docs(docs-infra): add missing dot (#58993) PR Close #58993

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-c639210
+ * @license Angular v19.0.1+sha-e4b85d1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.1+sha-c639210');
+new checker.Version('19.0.1+sha-e4b85d1');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
