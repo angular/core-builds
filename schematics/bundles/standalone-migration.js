6a810b18c5904491f095392f5654e136ff643960
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 15:22:03 +0000

    d0ea622 docs(docs-infra): read jsdoctags from function overloads (#58994)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-7c628f9
+ * @license Angular v19.1.0-next.0+sha-d0ea622
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.0+sha-7c628f9');
+new checker.Version('19.1.0-next.0+sha-d0ea622');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
