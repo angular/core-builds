de5613839155c3449a0f2cb9975dc3ade0ab9e76
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 15:48:06 +0000

    10b3dcc build: update cross-repo angular dependencies (#59010)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-d0ea622
+ * @license Angular v19.1.0-next.0+sha-10b3dcc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.0+sha-d0ea622');
+new checker.Version('19.1.0-next.0+sha-10b3dcc');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
