010f6e3da971e6e976496b2af2bdb0f233531252
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 14:18:27 +0000

    f72a1b1 ci: mark devversion as unavailable (#59007)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-c453f5d
+ * @license Angular v19.0.1+sha-f72a1b1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.1+sha-c453f5d');
+new checker.Version('19.0.1+sha-f72a1b1');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
