d34d08d8cf6dcfdb5698f5a2ba300bab91037770
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 09:49:45 +0000

    16ab166 docs(docs-infra): add missing dot (#58993) PR Close #58993

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-35165d1
+ * @license Angular v19.1.0-next.0+sha-16ab166
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18079,7 +18079,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-35165d1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-16ab166']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-35165d1');
+const VERSION = new Version('19.1.0-next.0+sha-16ab166');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
