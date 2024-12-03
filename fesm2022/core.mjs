9456704bdc313d693f51af492dd658a1aebb83a9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 09:28:44 +0000

    55c3cd3 build: update io_bazel_rules_sass digest to fe48516 (#59014)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-42b1662
+ * @license Angular v19.1.0-next.0+sha-55c3cd3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18079,7 +18079,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-42b1662']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-55c3cd3']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-42b1662');
+const VERSION = new Version('19.1.0-next.0+sha-55c3cd3');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
