b20c0ab4354b34fb8bffbba6bc29474e3cd8fb84
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 08:21:32 +0000

    42b1662 refactor(devtools): removing unused styles and addressing PR comments (#58213)

--- schematics/bundles/checker-a00b735e.js
+++ schematics/bundles/checker-a00b735e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.0+sha-10b3dcc
+ * @license Angular v19.1.0-next.0+sha-42b1662
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.0+sha-10b3dcc');
+new Version('19.1.0-next.0+sha-42b1662');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
