cff810cdea3dd3459d1f193a05458155a916108c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 11:26:37 +0000

    a107bf8 build: update dependency mocha to v11 (#59012)

--- schematics/bundles/checker-a00b735e.js
+++ schematics/bundles/checker-a00b735e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-bc5ca99
+ * @license Angular v19.0.1+sha-a107bf8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.1+sha-bc5ca99');
+new Version('19.0.1+sha-a107bf8');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
