ed08b4152f31bd546eb84d24d3d52bd05f33cba8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 17:36:23 +0000

    f073bed ci: add kirjs to pullapprove groups (#59025)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-a37af17
+ * @license Angular v19.1.0-next.0+sha-f073bed
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18079,7 +18079,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-a37af17']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-f073bed']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-a37af17');
+const VERSION = new Version('19.1.0-next.0+sha-f073bed');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
