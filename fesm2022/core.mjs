9a74420ebe62804fca42a6fbf71dacd70409f3c5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 11:27:08 +0000

    392c036 build: update dependency mocha to v11 (#59012)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-992e64f
+ * @license Angular v19.1.0-next.0+sha-392c036
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18079,7 +18079,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-992e64f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-392c036']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-992e64f');
+const VERSION = new Version('19.1.0-next.0+sha-392c036');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
