7de0d34195b0f1d7b0cee971e0eac3916f4b0246
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 15:19:31 +0000

    5cd35cb docs: remove extra spacing from DI token note (#59023)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.1+sha-a107bf8
+ * @license Angular v19.0.1+sha-5cd35cb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18079,7 +18079,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.1+sha-a107bf8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.1+sha-5cd35cb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.1+sha-a107bf8');
+const VERSION = new Version('19.0.1+sha-5cd35cb');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
