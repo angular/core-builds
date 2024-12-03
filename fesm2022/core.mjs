feb95510845ff17bccbbb1fdc6726f389a59f2c5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 09:28:47 +0000

    b65eea5 build: update io_bazel_rules_sass digest to fe48516 (#59014)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.1+sha-bee3394
+ * @license Angular v19.0.1+sha-b65eea5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18079,7 +18079,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.1+sha-bee3394']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.1+sha-b65eea5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.1+sha-bee3394');
+const VERSION = new Version('19.0.1+sha-b65eea5');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-a00b735e.js
+++ schematics/bundles/checker-a00b735e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-bee3394
+ * @license Angular v19.0.1+sha-b65eea5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.1+sha-bee3394');
+new Version('19.0.1+sha-b65eea5');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
