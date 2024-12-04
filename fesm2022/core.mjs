e5c67c3331c778fd5825db7e10433ec77a06f306
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 14:31:11 +0000

    922ffdb feat(docs-infra): move members descriptions section to the center of the page (#58610)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-9658dd1
+ * @license Angular v19.1.0-next.0+sha-922ffdb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18081,7 +18081,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-9658dd1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-922ffdb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34575,7 +34575,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-9658dd1');
+const VERSION = new Version('19.1.0-next.0+sha-922ffdb');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
