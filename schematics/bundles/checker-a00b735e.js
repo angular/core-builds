6ca951fda503b2352c9aabd07c9075c69c5bbb04
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 11:47:21 +0000

    0df7b1e refactor(common): remove standalone: true (#58949)

--- schematics/bundles/checker-a00b735e.js
+++ schematics/bundles/checker-a00b735e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-b8a2ae0
+ * @license Angular v19.0.1+sha-0df7b1e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.1+sha-b8a2ae0');
+new Version('19.0.1+sha-0df7b1e');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-b8a2ae0
+ * @license Angular v19.0.1+sha-0df7b1e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.1+sha-b8a2ae0');
+new checker.Version('19.0.1+sha-0df7b1e');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
