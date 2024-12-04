98e8a407e43ab8736285f86ee2ff1ff4c7f25e92
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 15:18:03 +0000

    6ae3ce7 release: cut the v19.0.2 release

--- schematics/bundles/checker-a00b735e.js
+++ schematics/bundles/checker-a00b735e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-7d2308f
+ * @license Angular v19.0.2+sha-6ae3ce7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.1+sha-7d2308f');
+new Version('19.0.2+sha-6ae3ce7');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/explicit-standalone-flag.js
+++ schematics/bundles/explicit-standalone-flag.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.1+sha-7d2308f
+ * @license Angular v19.0.2+sha-6ae3ce7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
