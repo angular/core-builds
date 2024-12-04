80e29ffbbf27c6f90c4fb25f9f99e5f1747153fc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 11:47:46 +0000

    9658dd1 refactor(common): remove standalone: true (#58949)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.0+sha-e894a5d
+ * @license Angular v19.1.0-next.0+sha-9658dd1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18081,7 +18081,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-e894a5d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.0+sha-9658dd1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34575,7 +34575,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.0+sha-e894a5d');
+const VERSION = new Version('19.1.0-next.0+sha-9658dd1');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
