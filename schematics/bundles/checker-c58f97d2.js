356592dfaf6aa79ded3f61db15e33337278c4635
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 17:13:40 +0000

    60e5900 build: update cross-repo angular dependencies (#59011)

--- schematics/bundles/checker-c58f97d2.js
+++ schematics/bundles/checker-c58f97d2.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.1+sha-0513fbc
+ * @license Angular v19.1.0-next.1+sha-60e5900
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.1+sha-0513fbc');
+new Version('19.1.0-next.1+sha-60e5900');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
