07e134949a422d37e9ff3b9b88efe9c857e065f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-12-05 23:30:41 +0000

    e2c8c6f refactor(core): Remove event_contract_multi_container (#59059)

--- schematics/bundles/checker-c58f97d2.js
+++ schematics/bundles/checker-c58f97d2.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.3+sha-47e314f
+ * @license Angular v19.0.3+sha-e2c8c6f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.3+sha-47e314f');
+new Version('19.0.3+sha-e2c8c6f');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
