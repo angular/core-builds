e3304d1c567ed3806bf5d8cc1efb7bdb77802859
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-12-06 00:10:11 +0000

    f15ccb9 docs(docs-infra): enable tslint (#58961)

--- schematics/bundles/checker-c58f97d2.js
+++ schematics/bundles/checker-c58f97d2.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.2+sha-8780399
+ * @license Angular v19.1.0-next.2+sha-f15ccb9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.2+sha-8780399');
+new Version('19.1.0-next.2+sha-f15ccb9');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
