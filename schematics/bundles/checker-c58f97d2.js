20326ff3347a8e10e718cc6b05acbd5da518376d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-12-12 10:14:17 +0000

    ec91da0 release: cut the v19.0.4 release

--- schematics/bundles/checker-c58f97d2.js
+++ schematics/bundles/checker-c58f97d2.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.3+sha-c3cd227
+ * @license Angular v19.0.4+sha-ec91da0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.3+sha-c3cd227');
+new Version('19.0.4+sha-ec91da0');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.3+sha-c3cd227
+ * @license Angular v19.0.4+sha-ec91da0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.3+sha-c3cd227');
+new checker.Version('19.0.4+sha-ec91da0');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
