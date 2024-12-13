040df734b4db76118f4de0aefca2aacf5867d954
Andrew Kushnir <akushnir@google.com>
2024-12-13 00:25:16 +0000

    9203276 docs: update i18n deployment docs to use subPath option (#59166)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.3+sha-8230388
+ * @license Angular v19.1.0-next.3+sha-9203276
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18092,7 +18092,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.3+sha-8230388']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.3+sha-9203276']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34735,7 +34735,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.3+sha-8230388');
+const VERSION = new Version('19.1.0-next.3+sha-9203276');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
