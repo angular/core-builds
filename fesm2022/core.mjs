5678a94c406dce2f4c6b332be785f10a4bea27b5
Andrew Kushnir <akushnir@google.com>
2024-12-13 01:33:56 +0000

    4c2cdd2 docs(core): remove redundant reference-manifests handwritten entries (#59169)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.3+sha-9203276
+ * @license Angular v19.1.0-next.3+sha-4c2cdd2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18092,7 +18092,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.3+sha-9203276']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.3+sha-4c2cdd2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34735,7 +34735,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.3+sha-9203276');
+const VERSION = new Version('19.1.0-next.3+sha-4c2cdd2');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
