618a76c4d8f1584a73867925b0edf1d71c57cb74
Andrew Kushnir <akushnir@google.com>
2024-12-16 19:40:08 +0000

    6f66b04 docs: Fix Dependency Injection Readme guide link (#59193) PR Close #59193

--- schematics/bundles/checker-228cb8a8.js
+++ schematics/bundles/checker-228cb8a8.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.3+sha-9e0b50b
+ * @license Angular v19.1.0-next.3+sha-6f66b04
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30410,7 +30410,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.3+sha-9e0b50b');
+new Version('19.1.0-next.3+sha-6f66b04');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
