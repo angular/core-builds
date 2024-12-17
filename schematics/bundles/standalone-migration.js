81f995f5f77cd60c452b304ce27b9f833b531038
Andrew Kushnir <akushnir@google.com>
2024-12-17 20:59:31 +0000

    8333ef7 docs: Fix Code of Conduct Readme link (#59217) PR Close #59217

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.3+sha-9b0aab9
+ * @license Angular v19.1.0-next.3+sha-8333ef7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.3+sha-9b0aab9');
+new checker.Version('19.1.0-next.3+sha-8333ef7');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
