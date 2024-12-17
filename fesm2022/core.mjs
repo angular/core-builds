00c380de59d82322392078513e6a257c43a30592
Andrew Kushnir <akushnir@google.com>
2024-12-17 00:37:28 +0000

    f0df010 docs(docs-infra): replace newline with <br> (#59156)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.4+sha-b476537
+ * @license Angular v19.0.4+sha-f0df010
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.4+sha-b476537']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.4+sha-f0df010']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34572,7 +34572,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.4+sha-b476537');
+const VERSION = new Version('19.0.4+sha-f0df010');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
