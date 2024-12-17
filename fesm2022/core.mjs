86d4c6cb31e550582e5e18e66384550c46d4d426
Andrew Kushnir <akushnir@google.com>
2024-12-17 20:51:26 +0000

    9b0aab9 release: bump Angular DevTools version to 1.0.19 (#59225)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.3+sha-47212e0
+ * @license Angular v19.1.0-next.3+sha-9b0aab9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18110,7 +18110,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.3+sha-47212e0']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.3+sha-9b0aab9']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34981,7 +34981,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.3+sha-47212e0');
+const VERSION = new Version('19.1.0-next.3+sha-9b0aab9');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.3+sha-47212e0
+ * @license Angular v19.1.0-next.3+sha-9b0aab9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.3+sha-47212e0');
+new checker.Version('19.1.0-next.3+sha-9b0aab9');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
