63be11925ef496c6e3d392dbb5e2aaf779d28ddc
Andrew Kushnir <akushnir@google.com>
2024-12-18 17:43:01 +0000

    e0362b2 docs: fix typo in code example (#59235) PR Close #59235

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.4+sha-fd968e8
+ * @license Angular v19.0.4+sha-e0362b2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.4+sha-fd968e8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.4+sha-e0362b2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34567,7 +34567,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.4+sha-fd968e8');
+const VERSION = new Version('19.0.4+sha-e0362b2');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
