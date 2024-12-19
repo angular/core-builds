8b330f95e9bd3f6a870ef3e618cdbf29630617d9
Joey Perrott <josephperrott@gmail.com>
2024-12-19 16:05:30 +0000

    df7af21 docs: update example code formatting (#59242) PR Close #59242

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.5+sha-ae68df7
+ * @license Angular v19.0.5+sha-df7af21
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.5+sha-ae68df7']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.5+sha-df7af21']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34567,7 +34567,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.5+sha-ae68df7');
+const VERSION = new Version('19.0.5+sha-df7af21');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-eced36c5.js
+++ schematics/bundles/checker-eced36c5.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.5+sha-ae68df7
+ * @license Angular v19.0.5+sha-df7af21
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.5+sha-ae68df7');
+new Version('19.0.5+sha-df7af21');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
