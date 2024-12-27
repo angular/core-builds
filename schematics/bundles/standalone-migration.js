6e63df3c89551d4f62ad66f3ba05c149f68c9320
Joey Perrott <josephperrott@gmail.com>
2024-12-27 15:01:39 +0000

    f8d22a9 fix(bazel): allow `ng_package` to work with `rules_js` dependencies (#59316)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.4+sha-8c5db3c
+ * @license Angular v19.1.0-next.4+sha-f8d22a9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-next.4+sha-8c5db3c');
+new checker.Version('19.1.0-next.4+sha-f8d22a9');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
