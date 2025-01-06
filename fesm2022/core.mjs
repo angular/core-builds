7876e9eba9785d724e6473130950aff9b971f7d6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 22:35:53 +0000

    b22677d docs(changelog): removed repetitive point from CHANGELOG.md of 18.0.0 (#59374)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.4+sha-8eb8af5
+ * @license Angular v19.1.0-next.4+sha-b22677d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18110,7 +18110,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-8eb8af5']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-b22677d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34976,7 +34976,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.4+sha-8eb8af5');
+const VERSION = new Version('19.1.0-next.4+sha-b22677d');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-24b68d23.js
+++ schematics/bundles/checker-24b68d23.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.4+sha-8eb8af5
+ * @license Angular v19.1.0-next.4+sha-b22677d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.4+sha-8eb8af5');
+new Version('19.1.0-next.4+sha-b22677d');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
