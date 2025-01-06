00e94674710136656da84c67b2e5fee3490671bc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 22:30:09 +0000

    8eb8af5 build: lock file maintenance (#59291)

--- schematics/bundles/checker-24b68d23.js
+++ schematics/bundles/checker-24b68d23.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-next.4+sha-2563f39
+ * @license Angular v19.1.0-next.4+sha-8eb8af5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-next.4+sha-2563f39');
+new Version('19.1.0-next.4+sha-8eb8af5');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
