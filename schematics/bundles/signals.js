5fc82d2501dbacc4b459381c54b7efa0e8041373
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 19:24:31 +0000

    144bccb fix(router): avoid component ID collisions with user code (#59300)

--- schematics/bundles/signals.js
+++ schematics/bundles/signals.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.5+sha-d7a34dc
+ * @license Angular v19.0.5+sha-144bccb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.5+sha-d7a34dc
+ * @license Angular v19.0.5+sha-144bccb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.5+sha-d7a34dc');
+new checker.Version('19.0.5+sha-144bccb');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
