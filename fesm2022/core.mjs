ff2d19b0131c8df00348123bf146a2ce0817e7ba
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 15:50:08 +0000

    adc56b7 build: update dependency @types/diff to v7 (#59403)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.4+sha-97039e8
+ * @license Angular v19.1.0-next.4+sha-adc56b7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18110,7 +18110,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-97039e8']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-adc56b7']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34976,7 +34976,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.4+sha-97039e8');
+const VERSION = new Version('19.1.0-next.4+sha-adc56b7');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
