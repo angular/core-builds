eee2b8226460bdab8d5c8e8f8a7b68ca4fdec93b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 15:37:15 +0000

    5c010a5 ci: remove `devversion` from unavailable pullapprove list (#59391)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.5+sha-d3eff34
+ * @license Angular v19.0.5+sha-5c010a5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.5+sha-d3eff34']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.5+sha-5c010a5']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34567,7 +34567,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.5+sha-d3eff34');
+const VERSION = new Version('19.0.5+sha-5c010a5');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.5+sha-d3eff34
+ * @license Angular v19.0.5+sha-5c010a5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32,7 +32,7 @@ var ts__default = /*#__PURE__*/_interopDefaultLegacy(ts);
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.0.5+sha-d3eff34');
+new checker.Version('19.0.5+sha-5c010a5');
 
 function createProgram({ rootNames, options, host, oldProgram, }) {
     return new program.NgtscProgram(rootNames, options, host, oldProgram);
