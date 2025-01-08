f343af04c17a9213bed8cea4bb18e3a1f94141cb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-01-08 18:30:54 +0000

    e689aae docs: release notes for the v19.1.0-rc.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.4+sha-c69f12f
+ * @license Angular v19.2.0-next.0+sha-e689aae
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18167,7 +18167,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-c69f12f']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-e689aae']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -35027,7 +35027,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.4+sha-c69f12f');
+const VERSION = new Version('19.2.0-next.0+sha-e689aae');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
