3c9ca106f989cb730adbf6e1ab9c61f4d911a415
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-08 15:29:08 +0000

    a25396d fix(bazel): support setting `type: module` in source `package.json` (#59406)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.4+sha-d9707e1
+ * @license Angular v19.1.0-next.4+sha-a25396d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18112,7 +18112,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-d9707e1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-a25396d']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34975,7 +34975,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.4+sha-d9707e1');
+const VERSION = new Version('19.1.0-next.4+sha-a25396d');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
