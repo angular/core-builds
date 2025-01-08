af1bc72ef2f1f94a674817d90588f54b6749cb03
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-08 16:17:13 +0000

    dd1d69b refactor(common): drop `PRELOADED_IMAGES` name in production (#59425)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.4+sha-00cc923
+ * @license Angular v19.1.0-next.4+sha-dd1d69b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18167,7 +18167,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-00cc923']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-next.4+sha-dd1d69b']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -35018,7 +35018,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.4+sha-00cc923');
+const VERSION = new Version('19.1.0-next.4+sha-dd1d69b');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
