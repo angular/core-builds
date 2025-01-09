3cafeb6e424307d5cf7d48749c729f7d7e463831
kirjs <kirjs@users.noreply.github.com>
2025-01-09 17:43:03 +0000

    4491b5b refactor(platform-browser): drop Hammer token names in production (#59438)

--- schematics/bundles/checker-24b68d23.js
+++ schematics/bundles/checker-24b68d23.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-3502039
+ * @license Angular v19.1.0-rc.0+sha-4491b5b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-rc.0+sha-3502039');
+new Version('19.1.0-rc.0+sha-4491b5b');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
