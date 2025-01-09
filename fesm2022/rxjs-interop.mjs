7377798df9884abf1741c309899d16e67d98e149
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:38:02 +0000

    a55575d fix(devtools): fixing dark mode colors for devtools and router tree (#59329)

--- fesm2022/rxjs-interop.mjs
+++ fesm2022/rxjs-interop.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-adc6c09
+ * @license Angular v19.2.0-next.0+sha-a55575d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/checker-24b68d23.js
+++ schematics/bundles/checker-24b68d23.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-adc6c09
+ * @license Angular v19.2.0-next.0+sha-a55575d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-adc6c09');
+new Version('19.2.0-next.0+sha-a55575d');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
