2799502cee04c9d3d3d6f810931b522f8fc5cad1
kirjs <kirjs@users.noreply.github.com>
2025-01-09 18:20:16 +0000

    bfaeefe refactor(animations): drop warning functions in production (#59408)

--- schematics/bundles/index-09c31a2b.js
+++ schematics/bundles/index-09c31a2b.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-988a6b2
+ * @license Angular v19.1.0-rc.0+sha-bfaeefe
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-rc.0+sha-988a6b2');
+new checker.Version('19.1.0-rc.0+sha-bfaeefe');
 
 var LogLevel;
 (function (LogLevel) {
