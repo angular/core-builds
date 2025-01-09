677387e332476f0ec29b6d7f60752f2333206960
kirjs <kirjs@users.noreply.github.com>
2025-01-09 18:15:02 +0000

    988a6b2 refactor(platform-browser): remove `Console` from Hammer gestures (#59409)

--- schematics/bundles/checker-24b68d23.js
+++ schematics/bundles/checker-24b68d23.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-4491b5b
+ * @license Angular v19.1.0-rc.0+sha-988a6b2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-rc.0+sha-4491b5b');
+new Version('19.1.0-rc.0+sha-988a6b2');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
