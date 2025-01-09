ace27133e629091ea6d3a699f4c500373de5afc4
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:01:02 +0000

    adc6c09 build: clean up pipeline-specific tests (#59450)

--- schematics/bundles/index-b1033cf0.js
+++ schematics/bundles/index-b1033cf0.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-854a561
+ * @license Angular v19.2.0-next.0+sha-adc6c09
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-854a561');
+new checker.Version('19.2.0-next.0+sha-adc6c09');
 
 var LogLevel;
 (function (LogLevel) {
