86cabf90e70dc6a4a277347037ab3fc1ca01b3c5
kirjs <kirjs@users.noreply.github.com>
2025-01-09 17:37:56 +0000

    3502039 refactor(common): drop `NullViewportScroller` for client bundles (#59440)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-c281807
+ * @license Angular v19.1.0-rc.0+sha-3502039
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18155,7 +18155,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-c281807']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-3502039']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -35015,7 +35015,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-rc.0+sha-c281807');
+const VERSION = new Version('19.1.0-rc.0+sha-3502039');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
