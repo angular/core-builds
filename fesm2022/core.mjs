2085bb7590377b77a1b499c78568e55d1cec2ed3
kirjs <kirjs@users.noreply.github.com>
2025-01-09 18:20:11 +0000

    068f4fb refactor(animations): drop warning functions in production (#59408)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-57a98c3
+ * @license Angular v19.0.6+sha-068f4fb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-57a98c3']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-068f4fb']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-57a98c3');
+const VERSION = new Version('19.0.6+sha-068f4fb');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-99fcd356.js
+++ schematics/bundles/checker-99fcd356.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.6+sha-57a98c3
+ * @license Angular v19.0.6+sha-068f4fb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.6+sha-57a98c3');
+new Version('19.0.6+sha-068f4fb');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
