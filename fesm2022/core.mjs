016983e7fecdfaa0c45f5d3239d0d4cca4348024
kirjs <kirjs@users.noreply.github.com>
2025-01-09 14:26:26 +0000

    21a9f53 refactor(forms): drop `CALL_SET_DISABLED_STATE` name in production (#59430)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-f0d5243
+ * @license Angular v19.0.6+sha-21a9f53
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-f0d5243']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-21a9f53']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-f0d5243');
+const VERSION = new Version('19.0.6+sha-21a9f53');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
