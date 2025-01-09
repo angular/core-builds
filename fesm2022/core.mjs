35053fa57b611c70bca6a20c534fa307ad17ff65
kirjs <kirjs@users.noreply.github.com>
2025-01-09 20:46:43 +0000

    3dc359e release: bump Angular DevTools version to 1.0.20 (#59454)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-068f4fb
+ * @license Angular v19.0.6+sha-3dc359e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-068f4fb']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-3dc359e']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-068f4fb');
+const VERSION = new Version('19.0.6+sha-3dc359e');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/migrate_ts_type_references-6122b2ac.js
+++ schematics/bundles/migrate_ts_type_references-6122b2ac.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.6+sha-068f4fb
+ * @license Angular v19.0.6+sha-3dc359e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
