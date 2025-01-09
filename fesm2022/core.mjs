551a52928e63b136a38bb8aa29bc6251a6d9bc4d
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:01:10 +0000

    28a5a89 build: clean up pipeline-specific tests (#59450)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-21a9f53
+ * @license Angular v19.0.6+sha-28a5a89
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-21a9f53']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-28a5a89']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34573,7 +34573,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-21a9f53');
+const VERSION = new Version('19.0.6+sha-28a5a89');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-99fcd356.js
+++ schematics/bundles/checker-99fcd356.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.6+sha-21a9f53
+ * @license Angular v19.0.6+sha-28a5a89
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.6+sha-21a9f53');
+new Version('19.0.6+sha-28a5a89');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
