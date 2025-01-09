3e8aba36179e34577c277714324e061e496ded0a
kirjs <kirjs@users.noreply.github.com>
2025-01-09 18:14:55 +0000

    1723421 refactor(platform-browser): remove `Console` from Hammer gestures (#59409)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-f2e293d
+ * @license Angular v19.2.0-next.0+sha-1723421
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18155,7 +18155,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-f2e293d']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-1723421']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -35015,7 +35015,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-f2e293d');
+const VERSION = new Version('19.2.0-next.0+sha-1723421');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
