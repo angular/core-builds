faa10cb53521e0838d15e567741f372c9862974e
kirjs <kirjs@users.noreply.github.com>
2025-01-09 14:25:52 +0000

    854a561 refactor(forms): drop `CALL_SET_DISABLED_STATE` name in production (#59430)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-e689aae
+ * @license Angular v19.2.0-next.0+sha-854a561
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18167,7 +18167,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-e689aae']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-854a561']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -35027,7 +35027,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-e689aae');
+const VERSION = new Version('19.2.0-next.0+sha-854a561');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-b1033cf0.js
+++ schematics/bundles/index-b1033cf0.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-e689aae
+ * @license Angular v19.2.0-next.0+sha-854a561
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-e689aae');
+new checker.Version('19.2.0-next.0+sha-854a561');
 
 var LogLevel;
 (function (LogLevel) {
