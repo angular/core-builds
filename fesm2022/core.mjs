8ceddb35843b1045249808f2bc8304b0307a6edd
kirjs <kirjs@users.noreply.github.com>
2025-01-09 20:47:15 +0000

    c1bc06c release: bump Angular DevTools version to 1.0.20 (#59454)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-ac2dbe3
+ * @license Angular v19.2.0-next.0+sha-c1bc06c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18100,7 +18100,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-ac2dbe3']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-c1bc06c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34972,7 +34972,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-ac2dbe3');
+const VERSION = new Version('19.2.0-next.0+sha-c1bc06c');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
