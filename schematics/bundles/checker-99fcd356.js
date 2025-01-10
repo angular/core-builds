b592f277cd388fe89fabce9c41dc19cf47df68bd
kirjs <kirjs@users.noreply.github.com>
2025-01-10 19:08:36 +0000

    63ef2ac docs(docs-infra): prevent the host class from being replaced (#59460)

--- schematics/bundles/checker-99fcd356.js
+++ schematics/bundles/checker-99fcd356.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.6+sha-eb2fcd1
+ * @license Angular v19.0.6+sha-63ef2ac
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.6+sha-eb2fcd1');
+new Version('19.0.6+sha-63ef2ac');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
