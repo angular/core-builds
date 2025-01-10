a60c251b0de48deaf0b7d3cff8d15634c9074567
kirjs <kirjs@users.noreply.github.com>
2025-01-10 15:47:49 +0000

    53c8dc6 docs: update `linkedSignal`. (#59221)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-aed49dd
+ * @license Angular v19.1.0-rc.0+sha-53c8dc6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6890,6 +6890,8 @@ export declare function linkedSignal<D>(computation: () => D, options?: {
 /**
  * Creates a writable signals whose value is initialized and reset by the linked, reactive computation.
  * This is an advanced API form where the computation has access to the previous value of the signal and the computation result.
+ *
+ * Note: The computation is reactive, meaning the linked signal will automatically update whenever any of the signals used within the computation change.
  *
  * @developerPreview
  */
