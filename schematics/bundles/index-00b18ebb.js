822b8b4aadf8115e68ba9622fd5932eb0732770d
kirjs <kirjs@users.noreply.github.com>
2025-01-10 15:47:36 +0000

    676357d docs: update `linkedSignal`. (#59221)

--- schematics/bundles/index-00b18ebb.js
+++ schematics/bundles/index-00b18ebb.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-a416414
+ * @license Angular v19.2.0-next.0+sha-676357d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-a416414');
+new checker.Version('19.2.0-next.0+sha-676357d');
 
 var LogLevel;
 (function (LogLevel) {
