26e2f116124304922dce50667e4a8c26b883980d
kirjs <kirjs@users.noreply.github.com>
2025-01-10 19:08:26 +0000

    40e7904 docs(docs-infra): prevent the host class from being replaced (#59460)

--- schematics/bundles/checker-1928982e.js
+++ schematics/bundles/checker-1928982e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-f978cde
+ * @license Angular v19.2.0-next.0+sha-40e7904
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-f978cde');
+new Version('19.2.0-next.0+sha-40e7904');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-00b18ebb.js
+++ schematics/bundles/index-00b18ebb.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-f978cde
+ * @license Angular v19.2.0-next.0+sha-40e7904
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-f978cde');
+new checker.Version('19.2.0-next.0+sha-40e7904');
 
 var LogLevel;
 (function (LogLevel) {
