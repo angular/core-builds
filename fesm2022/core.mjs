9883a10891d5b90a97fd8d87ba1f5231e1b470ea
kirjs <kirjs@users.noreply.github.com>
2025-01-10 18:41:52 +0000

    f90dc4c build: update cross-repo angular dependencies (#59473)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-574cfaf
+ * @license Angular v19.1.0-rc.0+sha-f90dc4c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18100,7 +18100,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-574cfaf']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-f90dc4c']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34972,7 +34972,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-rc.0+sha-574cfaf');
+const VERSION = new Version('19.1.0-rc.0+sha-f90dc4c');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/nodes-a9f0b985.js
+++ schematics/bundles/nodes-a9f0b985.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-574cfaf
+ * @license Angular v19.1.0-rc.0+sha-f90dc4c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
