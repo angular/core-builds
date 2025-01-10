1a71aff20d936deef305ef3baf082e1f1d152760
kirjs <kirjs@users.noreply.github.com>
2025-01-10 16:52:33 +0000

    17510be refactor(platform-browser): drop `BROWSER_MODULE_PROVIDERS_MARKER` in production (#59412)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-e8df770
+ * @license Angular v19.2.0-next.0+sha-17510be
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18100,7 +18100,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-e8df770']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-17510be']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34972,7 +34972,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-e8df770');
+const VERSION = new Version('19.2.0-next.0+sha-17510be');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
