55c65f7f7046f4e37adec24b2110cc5b912ceb00
kirjs <kirjs@users.noreply.github.com>
2025-01-10 19:08:20 +0000

    c2b77b5 docs(docs-infra): prevent the host class from being replaced (#59460)

--- schematics/bundles/leading_space-d190b83b.js
+++ schematics/bundles/leading_space-d190b83b.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-f90dc4c
+ * @license Angular v19.1.0-rc.0+sha-c2b77b5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
