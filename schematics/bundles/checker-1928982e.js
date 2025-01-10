167e4dd8938e5aa7a0aba596852249c18119a674
kirjs <kirjs@users.noreply.github.com>
2025-01-10 18:33:45 +0000

    574cfaf fix(dev-infra): remove no longer necessary scope (#59472)

--- schematics/bundles/checker-1928982e.js
+++ schematics/bundles/checker-1928982e.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-c8bbade
+ * @license Angular v19.1.0-rc.0+sha-574cfaf
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.0-rc.0+sha-c8bbade');
+new Version('19.1.0-rc.0+sha-574cfaf');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-00b18ebb.js
+++ schematics/bundles/index-00b18ebb.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-c8bbade
+ * @license Angular v19.1.0-rc.0+sha-574cfaf
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-rc.0+sha-c8bbade');
+new checker.Version('19.1.0-rc.0+sha-574cfaf');
 
 var LogLevel;
 (function (LogLevel) {
