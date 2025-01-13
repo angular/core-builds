209e7e67aa25bc64a0b2418cc6a17be32df8c0a8
kirjs <kirjs@users.noreply.github.com>
2025-01-13 15:43:19 +0000

    fea5c27 docs: fix typo example code in pipes documentation (#59312)

--- schematics/bundles/checker-99fcd356.js
+++ schematics/bundles/checker-99fcd356.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.0.6+sha-63ef2ac
+ * @license Angular v19.0.6+sha-fea5c27
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30321,7 +30321,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.0.6+sha-63ef2ac');
+new Version('19.0.6+sha-fea5c27');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
