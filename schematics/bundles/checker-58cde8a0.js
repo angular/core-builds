eaed1b6da8f37c3d07193324c42f01495b35d35d
kirjs <kirjs@users.noreply.github.com>
2025-01-13 19:53:26 +0000

    065e118 fix(devtools): remove property tab css that is hiding directive metadata (#59493)

--- schematics/bundles/checker-58cde8a0.js
+++ schematics/bundles/checker-58cde8a0.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-368d69e
+ * @license Angular v19.2.0-next.0+sha-065e118
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-368d69e');
+new Version('19.2.0-next.0+sha-065e118');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-d18613d6.js
+++ schematics/bundles/index-d18613d6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-368d69e
+ * @license Angular v19.2.0-next.0+sha-065e118
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-368d69e');
+new checker.Version('19.2.0-next.0+sha-065e118');
 
 var LogLevel;
 (function (LogLevel) {
