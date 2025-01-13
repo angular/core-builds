eb032f994f2d6ac0b40a2d3a6c9f42a74c26d4c1
kirjs <kirjs@users.noreply.github.com>
2025-01-13 20:05:31 +0000

    47e48b4 refactor(docs-infra): lazy-load `EmbeddedTutorialManager` in code editor (#59505)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-065e118
+ * @license Angular v19.2.0-next.0+sha-47e48b4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18114,7 +18114,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-065e118']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-47e48b4']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34994,7 +34994,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-065e118');
+const VERSION = new Version('19.2.0-next.0+sha-47e48b4');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-58cde8a0.js
+++ schematics/bundles/checker-58cde8a0.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-065e118
+ * @license Angular v19.2.0-next.0+sha-47e48b4
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-065e118');
+new Version('19.2.0-next.0+sha-47e48b4');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
