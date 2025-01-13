bfbeeeacf0ed2929f97d79d1931501982aff77f5
kirjs <kirjs@users.noreply.github.com>
2025-01-13 17:43:43 +0000

    5d63581 refactor(common): prevent duplicating `Accept` header (#59467)

--- schematics/bundles/index-d18613d6.js
+++ schematics/bundles/index-d18613d6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-4491704
+ * @license Angular v19.2.0-next.0+sha-5d63581
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-4491704');
+new checker.Version('19.2.0-next.0+sha-5d63581');
 
 var LogLevel;
 (function (LogLevel) {
