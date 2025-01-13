1e361e30ae28a0c5c15d91a59162ce4542494d4c
kirjs <kirjs@users.noreply.github.com>
2025-01-13 15:43:16 +0000

    cb30fec docs: fix typo example code in pipes documentation (#59312)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-40e7904
+ * @license Angular v19.2.0-next.0+sha-cb30fec
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18100,7 +18100,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-40e7904']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-cb30fec']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34972,7 +34972,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-40e7904');
+const VERSION = new Version('19.2.0-next.0+sha-cb30fec');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
