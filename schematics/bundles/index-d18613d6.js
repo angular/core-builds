40f46ffd6f49cc7a15200cc90e9efc5a1c9f18b6
kirjs <kirjs@users.noreply.github.com>
2025-01-14 19:52:00 +0000

    19d2241 build: lock file maintenance (#59519)

--- schematics/bundles/index-d18613d6.js
+++ schematics/bundles/index-d18613d6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-815f1b1
+ * @license Angular v19.2.0-next.0+sha-19d2241
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-815f1b1');
+new checker.Version('19.2.0-next.0+sha-19d2241');
 
 var LogLevel;
 (function (LogLevel) {
