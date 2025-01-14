347b75576df13ffe46c1260595c50ad9f6f916ca
kirjs <kirjs@users.noreply.github.com>
2025-01-14 16:17:25 +0000

    2b85ddc docs(docs-infra): change API reference list items order to top-down (#58655)

--- schematics/bundles/index-d18613d6.js
+++ schematics/bundles/index-d18613d6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-c67af8b
+ * @license Angular v19.1.0-rc.0+sha-2b85ddc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-rc.0+sha-c67af8b');
+new checker.Version('19.1.0-rc.0+sha-2b85ddc');
 
 var LogLevel;
 (function (LogLevel) {
