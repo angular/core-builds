eba9e4fdecafe8de158d0e58856764f208146a08
kirjs <kirjs@users.noreply.github.com>
2025-01-14 16:28:12 +0000

    2ca2587 fix(core): destroy renderer when replacing styles during HMR (#59514)

--- schematics/bundles/index-d18613d6.js
+++ schematics/bundles/index-d18613d6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-803358f
+ * @license Angular v19.2.0-next.0+sha-2ca2587
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-803358f');
+new checker.Version('19.2.0-next.0+sha-2ca2587');
 
 var LogLevel;
 (function (LogLevel) {
