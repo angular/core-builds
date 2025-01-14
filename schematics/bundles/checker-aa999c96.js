12ff69a85c9b0e01550554e03e0e1db960e6213d
kirjs <kirjs@users.noreply.github.com>
2025-01-14 21:12:09 +0000

    8830537 build: update cross-repo angular dependencies (#59520)

--- schematics/bundles/checker-aa999c96.js
+++ schematics/bundles/checker-aa999c96.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-b9155b5
+ * @license Angular v19.2.0-next.0+sha-8830537
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-b9155b5');
+new Version('19.2.0-next.0+sha-8830537');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
