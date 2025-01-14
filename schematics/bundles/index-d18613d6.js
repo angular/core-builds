452df1976ec7939be3cdf6d5e5bf7b3efe8cdf5c
kirjs <kirjs@users.noreply.github.com>
2025-01-14 20:02:42 +0000

    99c542a docs(docs-infra): fix scrolling issues in the API reference (#59207)

--- schematics/bundles/index-d18613d6.js
+++ schematics/bundles/index-d18613d6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-3913893
+ * @license Angular v19.1.0-rc.0+sha-99c542a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-rc.0+sha-3913893');
+new checker.Version('19.1.0-rc.0+sha-99c542a');
 
 var LogLevel;
 (function (LogLevel) {
