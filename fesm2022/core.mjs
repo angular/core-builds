638ef69110990c749059b7a44e35aeadde56c958
kirjs <kirjs@users.noreply.github.com>
2025-01-14 21:33:11 +0000

    b44c020 docs: fix property wrong usage inside afterNextRender in lifecycle.md (#59521)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-0130298
+ * @license Angular v19.1.0-rc.0+sha-b44c020
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18093,7 +18093,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-0130298']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-b44c020']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34967,7 +34967,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-rc.0+sha-0130298');
+const VERSION = new Version('19.1.0-rc.0+sha-b44c020');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-02a11f43.js
+++ schematics/bundles/index-02a11f43.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-0130298
+ * @license Angular v19.1.0-rc.0+sha-b44c020
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-rc.0+sha-0130298');
+new checker.Version('19.1.0-rc.0+sha-b44c020');
 
 var LogLevel;
 (function (LogLevel) {
