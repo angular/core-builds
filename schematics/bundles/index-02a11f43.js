0a421cd126af5a1c7e95ada3a24b9a4bb584a705
kirjs <kirjs@users.noreply.github.com>
2025-01-14 21:33:18 +0000

    9db23f9 docs: fix property wrong usage inside afterNextRender in lifecycle.md (#59521)

--- schematics/bundles/index-02a11f43.js
+++ schematics/bundles/index-02a11f43.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-8830537
+ * @license Angular v19.2.0-next.0+sha-9db23f9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-8830537');
+new checker.Version('19.2.0-next.0+sha-9db23f9');
 
 var LogLevel;
 (function (LogLevel) {
