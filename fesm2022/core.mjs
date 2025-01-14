fcf154043625e7e28a63d1a65ab2ee15fceef2bd
kirjs <kirjs@users.noreply.github.com>
2025-01-14 19:53:30 +0000

    3913893 build: lock file maintenance (#59519)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-a7f824a
+ * @license Angular v19.1.0-rc.0+sha-3913893
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18093,7 +18093,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-a7f824a']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-3913893']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34967,7 +34967,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-rc.0+sha-a7f824a');
+const VERSION = new Version('19.1.0-rc.0+sha-3913893');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-d18613d6.js
+++ schematics/bundles/index-d18613d6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-a7f824a
+ * @license Angular v19.1.0-rc.0+sha-3913893
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-rc.0+sha-a7f824a');
+new checker.Version('19.1.0-rc.0+sha-3913893');
 
 var LogLevel;
 (function (LogLevel) {
