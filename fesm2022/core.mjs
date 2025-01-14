d497faa2711e13ddac3ac6f1d6ef24a2951e8391
kirjs <kirjs@users.noreply.github.com>
2025-01-14 22:53:34 +0000

    6a0dd96 Revert "refactor: initialize headers map directly in HttpHeaders class (#59268)" (#59523)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-b44c020
+ * @license Angular v19.1.0-rc.0+sha-6a0dd96
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18093,7 +18093,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-b44c020']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-6a0dd96']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34967,7 +34967,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-rc.0+sha-b44c020');
+const VERSION = new Version('19.1.0-rc.0+sha-6a0dd96');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
