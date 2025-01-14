7f1e6985866095a73a759a404145aa5d449b12aa
kirjs <kirjs@users.noreply.github.com>
2025-01-14 19:48:02 +0000

    fb3aeba refactor(common): tree-shake transfer cache interceptor stuff (#59439)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-f893d07
+ * @license Angular v19.0.6+sha-fb3aeba
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18088,7 +18088,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-f893d07']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.0.6+sha-fb3aeba']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34575,7 +34575,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.0.6+sha-f893d07');
+const VERSION = new Version('19.0.6+sha-fb3aeba');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
