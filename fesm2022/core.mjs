3e79c1dafac34453c2d7f495dd444d4dcc19822d
kirjs <kirjs@users.noreply.github.com>
2025-01-15 15:22:47 +0000

    d0fc0d2 docs(forms): escape inequality signs of input tags in flowchart (#59517)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-rc.0+sha-1fba8d1
+ * @license Angular v19.1.0-rc.0+sha-d0fc0d2
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18093,7 +18093,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-1fba8d1']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0-rc.0+sha-d0fc0d2']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34967,7 +34967,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-rc.0+sha-1fba8d1');
+const VERSION = new Version('19.1.0-rc.0+sha-d0fc0d2');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
