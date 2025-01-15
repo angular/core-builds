c34965e97597c92541e634e8721f85ec15b13beb
kirjs <kirjs@users.noreply.github.com>
2025-01-15 17:34:41 +0000

    3b3040d refactor(common): drop error message in production (#59471)

--- schematics/bundles/index-02a11f43.js
+++ schematics/bundles/index-02a11f43.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-c38ea07
+ * @license Angular v19.2.0-next.0+sha-3b3040d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-c38ea07');
+new checker.Version('19.2.0-next.0+sha-3b3040d');
 
 var LogLevel;
 (function (LogLevel) {
