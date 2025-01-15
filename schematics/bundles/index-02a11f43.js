3cdb5844d728bd214aef5859dc408e6d2c6c6869
kirjs <kirjs@users.noreply.github.com>
2025-01-15 17:23:11 +0000

    f8e1f85 docs: update Angular CLI help [19.1.x] (#59536)

--- schematics/bundles/index-02a11f43.js
+++ schematics/bundles/index-02a11f43.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0-rc.0+sha-cfdf5ee
+ * @license Angular v19.1.0-rc.0+sha-f8e1f85
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.0-rc.0+sha-cfdf5ee');
+new checker.Version('19.1.0-rc.0+sha-f8e1f85');
 
 var LogLevel;
 (function (LogLevel) {
