e02f18f1c4aadfc7ee3734297957b300442db8cc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-16 12:24:18 +0000

    aa45d44 docs: release notes for the v19.1.1 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-c9cd558
+ * @license Angular v19.2.0-next.0+sha-aa45d44
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18149,7 +18149,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-c9cd558']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-aa45d44']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34999,7 +34999,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-c9cd558');
+const VERSION = new Version('19.2.0-next.0+sha-aa45d44');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
