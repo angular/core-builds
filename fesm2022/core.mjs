deb0fad5a75ea0706d7640aeb98e752135039599
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-16 11:32:58 +0000

    2fe54a1 fix(platform-browser): roll back HMR fix (#59557)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-db745cd
+ * @license Angular v19.2.0-next.0+sha-2fe54a1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18149,7 +18149,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-db745cd']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.2.0-next.0+sha-2fe54a1']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34989,7 +34989,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-db745cd');
+const VERSION = new Version('19.2.0-next.0+sha-2fe54a1');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
