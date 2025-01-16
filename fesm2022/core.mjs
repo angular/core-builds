d74108af582ee8760b066bc54dd9c8807cae0d5a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-16 11:33:02 +0000

    eb0b185 fix(platform-browser): roll back HMR fix (#59557)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0+sha-326248c
+ * @license Angular v19.1.0+sha-eb0b185
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18132,7 +18132,7 @@ function createRootComponent(componentView, rootComponentDef, rootDirectives, ho
 function setRootNodeAttributes(hostRenderer, componentDef, hostRNode, rootSelectorOrNode) {
     if (rootSelectorOrNode) {
         // The placeholder will be replaced with the actual version at build time.
-        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0+sha-326248c']);
+        setUpAttributes(hostRenderer, hostRNode, ['ng-version', '19.1.0+sha-eb0b185']);
     }
     else {
         // If host element is created as a part of this function call (i.e. `rootSelectorOrNode`
@@ -34964,7 +34964,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0+sha-326248c');
+const VERSION = new Version('19.1.0+sha-eb0b185');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/output-migration.js
+++ schematics/bundles/output-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.0+sha-326248c
+ * @license Angular v19.1.0+sha-eb0b185
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
