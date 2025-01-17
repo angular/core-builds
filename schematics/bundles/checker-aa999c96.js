09abbbf98bf749d487baddd260ccc6310f06be36
Andrew Kushnir <akushnir@google.com>
2025-01-17 18:06:04 +0000

    14791f3 fix(core): cleanup stash listener when app is destroyed (#59598)

--- schematics/bundles/checker-aa999c96.js
+++ schematics/bundles/checker-aa999c96.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-4e6017a
+ * @license Angular v19.2.0-next.0+sha-14791f3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30418,7 +30418,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-4e6017a');
+new Version('19.2.0-next.0+sha-14791f3');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-02a11f43.js
+++ schematics/bundles/index-02a11f43.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-4e6017a
+ * @license Angular v19.2.0-next.0+sha-14791f3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-4e6017a');
+new checker.Version('19.2.0-next.0+sha-14791f3');
 
 var LogLevel;
 (function (LogLevel) {
