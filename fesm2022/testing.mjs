55fb556914b8983d0b97161c6f6e19f43082800f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-20 14:15:49 +0000

    2ec826d release: cut the v19.1.2 release

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.1+sha-266a8f2
+ * @license Angular v19.1.2+sha-2ec826d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -175,10 +175,10 @@ class TestBedApplicationErrorHandler {
             throw e;
         }
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.1.1+sha-266a8f2", ngImport: i0, type: TestBedApplicationErrorHandler, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.1.1+sha-266a8f2", ngImport: i0, type: TestBedApplicationErrorHandler });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.1.2+sha-2ec826d", ngImport: i0, type: TestBedApplicationErrorHandler, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.1.2+sha-2ec826d", ngImport: i0, type: TestBedApplicationErrorHandler });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.1.1+sha-266a8f2", ngImport: i0, type: TestBedApplicationErrorHandler, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.1.2+sha-2ec826d", ngImport: i0, type: TestBedApplicationErrorHandler, decorators: [{
             type: Injectable
         }] });
 
--- schematics/bundles/index-4830663b.js
+++ schematics/bundles/index-4830663b.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.1+sha-266a8f2
+ * @license Angular v19.1.2+sha-2ec826d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.1+sha-266a8f2');
+new checker.Version('19.1.2+sha-2ec826d');
 
 var LogLevel;
 (function (LogLevel) {
