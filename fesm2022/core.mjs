7206db3859fd0221f22b0f6222ee053cff9651d2
Andrew Kushnir <akushnir@google.com>
2025-01-21 21:25:22 +0000

    89b391c test(zone.js): reduce flakiness of a timer-related test (#59653)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.2+sha-0de5049
+ * @license Angular v19.1.2+sha-89b391c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17947,7 +17947,7 @@ class ComponentFactory extends ComponentFactory$1 {
                 }
                 // If host dom element is created (instead of being provided as part of the dynamic component creation), also apply attributes and classes extracted from component selector.
                 const tAttributes = rootSelectorOrNode
-                    ? ['ng-version', '19.1.2+sha-0de5049']
+                    ? ['ng-version', '19.1.2+sha-89b391c']
                     : // Extract attributes and classes from the first selector only to match VE behavior.
                         getRootTAttributesFromSelector(this.componentDef.selectors[0]);
                 // TODO: this logic is shared with the element instruction first create pass
@@ -34996,7 +34996,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.2+sha-0de5049');
+const VERSION = new Version('19.1.2+sha-89b391c');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
