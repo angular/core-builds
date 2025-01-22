36c12679d390b509c9c9de98862adb986594970d
Andrew Kushnir <akushnir@google.com>
2025-01-22 16:03:38 +0000

    61302cb docs: add Bluesky links (#59661)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-6c96d79
+ * @license Angular v19.2.0-next.0+sha-61302cb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17963,7 +17963,7 @@ class ComponentFactory extends ComponentFactory$1 {
                 }
                 // If host dom element is created (instead of being provided as part of the dynamic component creation), also apply attributes and classes extracted from component selector.
                 const tAttributes = rootSelectorOrNode
-                    ? ['ng-version', '19.2.0-next.0+sha-6c96d79']
+                    ? ['ng-version', '19.2.0-next.0+sha-61302cb']
                     : // Extract attributes and classes from the first selector only to match VE behavior.
                         getRootTAttributesFromSelector(this.componentDef.selectors[0]);
                 // TODO: this logic is shared with the element instruction first create pass
@@ -35021,7 +35021,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-6c96d79');
+const VERSION = new Version('19.2.0-next.0+sha-61302cb');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
