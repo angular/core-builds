439e9a1edc9c8f5f3278b1f90575446cb33cdde8
Andrew Kushnir <akushnir@google.com>
2025-01-22 21:40:21 +0000

    1de822c refactor(devtools): select the root element by default (#59626)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.3+sha-c243670
+ * @license Angular v19.1.3+sha-1de822c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17948,7 +17948,7 @@ class ComponentFactory extends ComponentFactory$1 {
                 }
                 // If host dom element is created (instead of being provided as part of the dynamic component creation), also apply attributes and classes extracted from component selector.
                 const tAttributes = rootSelectorOrNode
-                    ? ['ng-version', '19.1.3+sha-c243670']
+                    ? ['ng-version', '19.1.3+sha-1de822c']
                     : // Extract attributes and classes from the first selector only to match VE behavior.
                         getRootTAttributesFromSelector(this.componentDef.selectors[0]);
                 // TODO: this logic is shared with the element instruction first create pass
@@ -34997,7 +34997,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.3+sha-c243670');
+const VERSION = new Version('19.1.3+sha-1de822c');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-761b9f6f.js
+++ schematics/bundles/index-761b9f6f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-c243670
+ * @license Angular v19.1.3+sha-1de822c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.3+sha-c243670');
+new checker.Version('19.1.3+sha-1de822c');
 
 var LogLevel;
 (function (LogLevel) {
