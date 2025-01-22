0568d5fe74bb023b38933ca606a3d988cf540d28
Andrew Kushnir <akushnir@google.com>
2025-01-22 20:35:41 +0000

    ae1dfdf refactor(core): prevent duplicating `isRootView` checks (#59614)

--- schematics/bundles/checker-78667e44.js
+++ schematics/bundles/checker-78667e44.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-2ed633a
+ * @license Angular v19.2.0-next.0+sha-ae1dfdf
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30696,7 +30696,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-2ed633a');
+new Version('19.2.0-next.0+sha-ae1dfdf');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
