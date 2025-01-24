5c5e35d7102173ef1d0d892e57040317627ae476
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 19:47:37 +0000

    7a328ba docs: update Angular CLI help [19.1.x] (#59706)

--- schematics/bundles/checker-ca858016.js
+++ schematics/bundles/checker-ca858016.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-b4b36ed
+ * @license Angular v19.1.3+sha-7a328ba
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30451,7 +30451,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.3+sha-b4b36ed');
+new Version('19.1.3+sha-7a328ba');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-761b9f6f.js
+++ schematics/bundles/index-761b9f6f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-b4b36ed
+ * @license Angular v19.1.3+sha-7a328ba
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.3+sha-b4b36ed');
+new checker.Version('19.1.3+sha-7a328ba');
 
 var LogLevel;
 (function (LogLevel) {
