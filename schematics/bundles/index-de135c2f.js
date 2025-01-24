8822dba673a23a028ae5875bab8a312b4f2fa036
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 11:50:28 +0000

    edb8407 refactor(common): simplify null/undefined check in `keyvalue` pipe (#59696)

--- schematics/bundles/index-de135c2f.js
+++ schematics/bundles/index-de135c2f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-434568b
+ * @license Angular v19.2.0-next.0+sha-edb8407
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-434568b');
+new checker.Version('19.2.0-next.0+sha-edb8407');
 
 var LogLevel;
 (function (LogLevel) {
