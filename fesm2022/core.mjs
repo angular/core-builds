19831b621ed30844b4777b153cfe862a5a376723
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 11:36:20 +0000

    434568b docs: update Angular CLI help [main] (#59699)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-e483831
+ * @license Angular v19.2.0-next.0+sha-434568b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17967,7 +17967,7 @@ class ComponentFactory extends ComponentFactory$1 {
                 }
                 // If host dom element is created (instead of being provided as part of the dynamic component creation), also apply attributes and classes extracted from component selector.
                 const tAttributes = rootSelectorOrNode
-                    ? ['ng-version', '19.2.0-next.0+sha-e483831']
+                    ? ['ng-version', '19.2.0-next.0+sha-434568b']
                     : // Extract attributes and classes from the first selector only to match VE behavior.
                         getRootTAttributesFromSelector(this.componentDef.selectors[0]);
                 // TODO: this logic is shared with the element instruction first create pass
@@ -34969,7 +34969,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-e483831');
+const VERSION = new Version('19.2.0-next.0+sha-434568b');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
