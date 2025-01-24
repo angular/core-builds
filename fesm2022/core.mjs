74d36cea866561908a991fa573c38431c49d563f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 11:49:39 +0000

    b7b8da5 refactor(common): simplify null/undefined check in `keyvalue` pipe (#59696)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.3+sha-f8b8a3e
+ * @license Angular v19.1.3+sha-b7b8da5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17951,7 +17951,7 @@ class ComponentFactory extends ComponentFactory$1 {
                 }
                 // If host dom element is created (instead of being provided as part of the dynamic component creation), also apply attributes and classes extracted from component selector.
                 const tAttributes = rootSelectorOrNode
-                    ? ['ng-version', '19.1.3+sha-f8b8a3e']
+                    ? ['ng-version', '19.1.3+sha-b7b8da5']
                     : // Extract attributes and classes from the first selector only to match VE behavior.
                         getRootTAttributesFromSelector(this.componentDef.selectors[0]);
                 // TODO: this logic is shared with the element instruction first create pass
@@ -34944,7 +34944,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.3+sha-f8b8a3e');
+const VERSION = new Version('19.1.3+sha-b7b8da5');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-761b9f6f.js
+++ schematics/bundles/index-761b9f6f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-f8b8a3e
+ * @license Angular v19.1.3+sha-b7b8da5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.3+sha-f8b8a3e');
+new checker.Version('19.1.3+sha-b7b8da5');
 
 var LogLevel;
 (function (LogLevel) {
