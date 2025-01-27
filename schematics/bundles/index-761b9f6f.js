04d65e44e5cda3650091fe888036ef1410c8045b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 09:01:01 +0000

    170c052 docs: fix typo in signals-interop.md (#59724) PR Close #59724

--- schematics/bundles/index-761b9f6f.js
+++ schematics/bundles/index-761b9f6f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-7a328ba
+ * @license Angular v19.1.3+sha-170c052
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.3+sha-7a328ba');
+new checker.Version('19.1.3+sha-170c052');
 
 var LogLevel;
 (function (LogLevel) {
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-7a328ba
+ * @license Angular v19.1.3+sha-170c052
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
