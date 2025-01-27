a1f4cce05cd1bc333a931eb76b9a1a01a5b7797d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 15:46:20 +0000

    b409330 refactor(service-worker): drop platform checks (#59475)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.0+sha-df143b4
+ * @license Angular v19.2.0-next.0+sha-b409330
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17968,7 +17968,7 @@ class ComponentFactory extends ComponentFactory$1 {
                 }
                 // If host dom element is created (instead of being provided as part of the dynamic component creation), also apply attributes and classes extracted from component selector.
                 const tAttributes = rootSelectorOrNode
-                    ? ['ng-version', '19.2.0-next.0+sha-df143b4']
+                    ? ['ng-version', '19.2.0-next.0+sha-b409330']
                     : // Extract attributes and classes from the first selector only to match VE behavior.
                         extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
                 // TODO: this logic is shared with the element instruction first create pass
@@ -34945,7 +34945,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.0+sha-df143b4');
+const VERSION = new Version('19.2.0-next.0+sha-b409330');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
