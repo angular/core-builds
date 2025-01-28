6eee77553af4cd9e623a5e6c6061e014ea012a80
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 08:45:08 +0000

    873870f refactor(common): drop error messages in production (#59545)

--- schematics/bundles/checker-ca858016.js
+++ schematics/bundles/checker-ca858016.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-d0dc8f7
+ * @license Angular v19.1.3+sha-873870f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30451,7 +30451,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.3+sha-d0dc8f7');
+new Version('19.1.3+sha-873870f');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
