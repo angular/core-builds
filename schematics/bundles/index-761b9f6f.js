c5472dac266d1a6179b18c677d0092cb0d4953be
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 11:47:34 +0000

    3c28301 build: revert back to @bazel/ibazel v0.16 (#59748)

--- schematics/bundles/index-761b9f6f.js
+++ schematics/bundles/index-761b9f6f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-cb1cb0c
+ * @license Angular v19.1.3+sha-3c28301
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.3+sha-cb1cb0c');
+new checker.Version('19.1.3+sha-3c28301');
 
 var LogLevel;
 (function (LogLevel) {
