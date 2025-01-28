0e2d0f1c90d7a9081a668754ac2ca7a2176086cb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 19:40:42 +0000

    9ed9c40 docs(docs-infra): remove non-necessary files from playground zip. (#59728)

--- schematics/bundles/checker-78667e44.js
+++ schematics/bundles/checker-78667e44.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-ccebbba
+ * @license Angular v19.2.0-next.0+sha-9ed9c40
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30696,7 +30696,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-ccebbba');
+new Version('19.2.0-next.0+sha-9ed9c40');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-de135c2f.js
+++ schematics/bundles/index-de135c2f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-ccebbba
+ * @license Angular v19.2.0-next.0+sha-9ed9c40
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-ccebbba');
+new checker.Version('19.2.0-next.0+sha-9ed9c40');
 
 var LogLevel;
 (function (LogLevel) {
