22a5713f469551f1ab2775d28026a88e0b0668ac
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 08:13:35 +0000

    3373b4f build: update github/codeql-action action to v3.28.6 (#59744)

--- schematics/bundles/index-de135c2f.js
+++ schematics/bundles/index-de135c2f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-762e56e
+ * @license Angular v19.2.0-next.0+sha-3373b4f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-762e56e');
+new checker.Version('19.2.0-next.0+sha-3373b4f');
 
 var LogLevel;
 (function (LogLevel) {
