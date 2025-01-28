e5d5cbc933169fe1e0d1e955b26850fec958db6a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 11:59:00 +0000

    f3d6c49 ci: configure merge tool to enforce mandatory PR statuses (#59749)

--- schematics/bundles/checker-ca858016.js
+++ schematics/bundles/checker-ca858016.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.3+sha-3c28301
+ * @license Angular v19.1.3+sha-f3d6c49
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30451,7 +30451,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.3+sha-3c28301');
+new Version('19.1.3+sha-f3d6c49');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
