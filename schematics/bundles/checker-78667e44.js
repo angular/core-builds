3a436b0ccf5b695718114beaf9a7d7dc337c1926
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 18:59:19 +0000

    b5f9018 docs: fix programmatic-rendering example (#59768)

--- schematics/bundles/checker-78667e44.js
+++ schematics/bundles/checker-78667e44.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-0c1ecb4
+ * @license Angular v19.2.0-next.0+sha-b5f9018
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30696,7 +30696,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.0+sha-0c1ecb4');
+new Version('19.2.0-next.0+sha-b5f9018');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-de135c2f.js
+++ schematics/bundles/index-de135c2f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-0c1ecb4
+ * @license Angular v19.2.0-next.0+sha-b5f9018
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-0c1ecb4');
+new checker.Version('19.2.0-next.0+sha-b5f9018');
 
 var LogLevel;
 (function (LogLevel) {
