9bfc8f3bc553662f554cbe822f76a57e30292bae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 23:17:05 +0000

    eb9dca3 docs: release notes for the v19.1.4 release

--- schematics/bundles/index-de135c2f.js
+++ schematics/bundles/index-de135c2f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.0+sha-b5f9018
+ * @license Angular v19.2.0-next.0+sha-eb9dca3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.0+sha-b5f9018');
+new checker.Version('19.2.0-next.0+sha-eb9dca3');
 
 var LogLevel;
 (function (LogLevel) {
