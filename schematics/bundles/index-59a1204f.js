cf67b07592b3252fdb21a080207a089b635f1e51
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-02-06 20:39:14 +0000

    27c39f5 release: cut the v19.1.5 release

--- schematics/bundles/index-59a1204f.js
+++ schematics/bundles/index-59a1204f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.4+sha-6217e45
+ * @license Angular v19.1.5+sha-27c39f5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.4+sha-6217e45');
+new checker.Version('19.1.5+sha-27c39f5');
 
 var LogLevel;
 (function (LogLevel) {
