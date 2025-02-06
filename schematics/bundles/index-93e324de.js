3d44f83f13b131a5a47be301afcac16b0119c8df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-02-06 20:55:49 +0000

    a7f20eb release: cut the v19.2.0-next.2 release

--- schematics/bundles/index-93e324de.js
+++ schematics/bundles/index-93e324de.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.1+sha-7196725
+ * @license Angular v19.2.0-next.2+sha-a7f20eb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.1+sha-7196725');
+new checker.Version('19.2.0-next.2+sha-a7f20eb');
 
 var LogLevel;
 (function (LogLevel) {
