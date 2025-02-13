c077ab7cd5e1e35f86d6de02139fa51393fc0aa1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 15:09:10 +0000

    9f09214 docs(docs-infra): replace the WebGL home page animation with a CSS+JS version of it (#59865)

--- schematics/bundles/checker-32db85a6.js
+++ schematics/bundles/checker-32db85a6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.2+sha-adf2a0a
+ * @license Angular v19.2.0-next.2+sha-9f09214
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30733,7 +30733,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.2+sha-adf2a0a');
+new Version('19.2.0-next.2+sha-9f09214');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.2+sha-adf2a0a
+ * @license Angular v19.2.0-next.2+sha-9f09214
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
