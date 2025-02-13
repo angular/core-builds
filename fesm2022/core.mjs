c9946356a12d6cf11bce8c12247e02a44ba0b8d1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-02-13 16:35:30 +0000

    fb39fe9 release: cut the v19.2.0-next.3 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.2+sha-f34aeae
+ * @license Angular v19.2.0-next.3+sha-fb39fe9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18020,7 +18020,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.0-next.2+sha-f34aeae']
+                ? ['ng-version', '19.2.0-next.3+sha-fb39fe9']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34993,7 +34993,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.2+sha-f34aeae');
+const VERSION = new Version('19.2.0-next.3+sha-fb39fe9');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-d5020c9c.js
+++ schematics/bundles/index-d5020c9c.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.2+sha-f34aeae
+ * @license Angular v19.2.0-next.3+sha-fb39fe9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
