1733c30a7447b69b97b6a36a15a06a109969a9c9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 14:56:13 +0000

    adf2a0a release: bump Angular DevTools version to 1.0.22 (#59791)

--- schematics/bundles/checker-32db85a6.js
+++ schematics/bundles/checker-32db85a6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.2+sha-6789c7e
+ * @license Angular v19.2.0-next.2+sha-adf2a0a
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30733,7 +30733,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.2+sha-6789c7e');
+new Version('19.2.0-next.2+sha-adf2a0a');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
