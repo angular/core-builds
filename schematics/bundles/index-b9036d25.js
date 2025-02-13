aa5f6141bb680c6a0ad1b3b7a1afc1ab6080e0a7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 20:25:03 +0000

    3fd69d7 refactor(core): Check in some interfaces for the DI package (#59921)

--- schematics/bundles/index-b9036d25.js
+++ schematics/bundles/index-b9036d25.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.6+sha-defe672
+ * @license Angular v19.1.6+sha-3fd69d7
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.6+sha-defe672');
+new checker.Version('19.1.6+sha-3fd69d7');
 
 var LogLevel;
 (function (LogLevel) {
