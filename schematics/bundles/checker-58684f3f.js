d5292c17fa6ab3e6c83315cd7500359b752c68e9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 15:07:16 +0000

    adbf013 docs: link to Tailwind docs about Angular CLI integration (#59765)

--- schematics/bundles/checker-58684f3f.js
+++ schematics/bundles/checker-58684f3f.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.6+sha-22c2dfa
+ * @license Angular v19.1.6+sha-adbf013
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30488,7 +30488,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.6+sha-22c2dfa');
+new Version('19.1.6+sha-adbf013');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-4bb9c755.js
+++ schematics/bundles/index-4bb9c755.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.6+sha-22c2dfa
+ * @license Angular v19.1.6+sha-adbf013
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.6+sha-22c2dfa');
+new checker.Version('19.1.6+sha-adbf013');
 
 var LogLevel;
 (function (LogLevel) {
