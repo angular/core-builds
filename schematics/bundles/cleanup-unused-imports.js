76fc7a2d75594f6339290d23eddcfb30f2c681b1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-14 16:43:01 +0000

    650e1f9 build: update io_bazel_rules_sass digest to c01e884 (#59948)

--- schematics/bundles/cleanup-unused-imports.js
+++ schematics/bundles/cleanup-unused-imports.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.3+sha-1ebf7bf
+ * @license Angular v19.2.0-next.3+sha-650e1f9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/index-b0208365.js
+++ schematics/bundles/index-b0208365.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.3+sha-1ebf7bf
+ * @license Angular v19.2.0-next.3+sha-650e1f9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.3+sha-1ebf7bf');
+new checker.Version('19.2.0-next.3+sha-650e1f9');
 
 var LogLevel;
 (function (LogLevel) {
