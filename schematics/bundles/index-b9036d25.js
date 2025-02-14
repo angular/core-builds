e2c69301a618a94ebaa43a0de44ad12f4d7e92e6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-14 19:38:00 +0000

    8d40876 docs: update `zip` command and link to recent DevTools commits (#59792)

--- schematics/bundles/index-b9036d25.js
+++ schematics/bundles/index-b9036d25.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.6+sha-d46dd77
+ * @license Angular v19.1.6+sha-8d40876
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.6+sha-d46dd77');
+new checker.Version('19.1.6+sha-8d40876');
 
 var LogLevel;
 (function (LogLevel) {
