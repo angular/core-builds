c5abf56274a9c1349d4b79d2a78542d08f0591a2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-14 19:37:47 +0000

    be32d63 docs: update `zip` command and link to recent DevTools commits (#59792)

--- schematics/bundles/index-b0208365.js
+++ schematics/bundles/index-b0208365.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.3+sha-751dc4a
+ * @license Angular v19.2.0-next.3+sha-be32d63
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-next.3+sha-751dc4a');
+new checker.Version('19.2.0-next.3+sha-be32d63');
 
 var LogLevel;
 (function (LogLevel) {
