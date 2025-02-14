3b634cf85ab5f59a31e8106be42045af95b8ab6e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-14 18:07:01 +0000

    e141893 build: update dependency jsdom to v26 (#59958)

--- schematics/bundles/index-b9036d25.js
+++ schematics/bundles/index-b9036d25.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.6+sha-82eba1d
+ * @license Angular v19.1.6+sha-e141893
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.6+sha-82eba1d');
+new checker.Version('19.1.6+sha-e141893');
 
 var LogLevel;
 (function (LogLevel) {
