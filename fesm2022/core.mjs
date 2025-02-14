1b57721c44defae7b3882c3e7aedf79cac343d2a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-14 18:05:38 +0000

    47d5e1e build: update dependency jsdom to v26 (#59444)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.3+sha-5ca2fa5
+ * @license Angular v19.2.0-next.3+sha-47d5e1e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18020,7 +18020,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.0-next.3+sha-5ca2fa5']
+                ? ['ng-version', '19.2.0-next.3+sha-47d5e1e']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34993,7 +34993,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.3+sha-5ca2fa5');
+const VERSION = new Version('19.2.0-next.3+sha-47d5e1e');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
