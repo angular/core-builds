17f1ac8aa0492dd33a462302808ccf1a2f58cbcc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 16:56:27 +0000

    b6fa69f build: update dependency shiki to v3 (#59986)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.3+sha-c076b57
+ * @license Angular v19.2.0-next.3+sha-b6fa69f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18018,7 +18018,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.0-next.3+sha-c076b57']
+                ? ['ng-version', '19.2.0-next.3+sha-b6fa69f']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34991,7 +34991,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.3+sha-c076b57');
+const VERSION = new Version('19.2.0-next.3+sha-b6fa69f');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
