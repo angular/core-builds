4254b2d906c426171f378b9a497b76691194de96
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 15:04:21 +0000

    26c64a8 docs(docs-infra): fix card highlighting in the API reference details page (#59965)

--- schematics/bundles/checker-2760b2a4.js
+++ schematics/bundles/checker-2760b2a4.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.6+sha-8d40876
+ * @license Angular v19.1.6+sha-26c64a8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30488,7 +30488,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.6+sha-8d40876');
+new Version('19.1.6+sha-26c64a8');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-b9036d25.js
+++ schematics/bundles/index-b9036d25.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.6+sha-8d40876
+ * @license Angular v19.1.6+sha-26c64a8
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.6+sha-8d40876');
+new checker.Version('19.1.6+sha-26c64a8');
 
 var LogLevel;
 (function (LogLevel) {
