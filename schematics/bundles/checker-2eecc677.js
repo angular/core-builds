7e9374a972e21db8e5c543b38c2e5a5138b2271b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-02-19 19:08:44 +0000

    11ea0a0 release: cut the v19.2.0-rc.0 release

--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.3+sha-497028c
+ * @license Angular v19.2.0-rc.0+sha-11ea0a0
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.3+sha-497028c');
+new Version('19.2.0-rc.0+sha-11ea0a0');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
