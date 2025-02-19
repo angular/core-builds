208070b11670e238b87e500f25dec488e12bfffa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-19 21:06:53 +0000

    b7dd5a1 ci: update deploy doc site script (#60021)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.3.0-next.0+sha-8657a0e
+ * @license Angular v19.3.0-next.0+sha-b7dd5a1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17972,7 +17972,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.3.0-next.0+sha-8657a0e']
+                ? ['ng-version', '19.3.0-next.0+sha-b7dd5a1']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34978,7 +34978,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.3.0-next.0+sha-8657a0e');
+const VERSION = new Version('19.3.0-next.0+sha-b7dd5a1');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
