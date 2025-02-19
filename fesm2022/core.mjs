0e2a4b0b8f7d5b46eef5d7e0cb10b25d28d6a1f0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-02-19 18:50:32 +0000

    497028c docs: release notes for the v19.1.7 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-next.3+sha-40dc99d
+ * @license Angular v19.2.0-next.3+sha-497028c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17972,7 +17972,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.0-next.3+sha-40dc99d']
+                ? ['ng-version', '19.2.0-next.3+sha-497028c']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34978,7 +34978,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-next.3+sha-40dc99d');
+const VERSION = new Version('19.2.0-next.3+sha-497028c');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-next.3+sha-40dc99d
+ * @license Angular v19.2.0-next.3+sha-497028c
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-next.3+sha-40dc99d');
+new Version('19.2.0-next.3+sha-497028c');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
