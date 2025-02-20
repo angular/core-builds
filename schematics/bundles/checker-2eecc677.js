b78415b1f47fd70b0a0fdaf5b86e4b838644d5c8
kirjs <kirjs@users.noreply.github.com>
2025-02-20 17:41:02 +0000

    35e25f6 refactor(core): drop platform check in `ImagePerformanceWarning` (#59809)

--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.3.0-next.0+sha-c513e5d
+ * @license Angular v19.3.0-next.0+sha-35e25f6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.3.0-next.0+sha-c513e5d');
+new Version('19.3.0-next.0+sha-35e25f6');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
