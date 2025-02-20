efd8ec12fe3772b6771dd119fdc89e2a1517c7b6
kirjs <kirjs@users.noreply.github.com>
2025-02-20 16:27:32 +0000

    7a8e8f3 fix(benchpress): Ensure future-proof correct initialization order (#60025)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.3.0-next.0+sha-af5155f
+ * @license Angular v19.3.0-next.0+sha-7a8e8f3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17972,7 +17972,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.3.0-next.0+sha-af5155f']
+                ? ['ng-version', '19.3.0-next.0+sha-7a8e8f3']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34978,7 +34978,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.3.0-next.0+sha-af5155f');
+const VERSION = new Version('19.3.0-next.0+sha-7a8e8f3');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.3.0-next.0+sha-af5155f
+ * @license Angular v19.3.0-next.0+sha-7a8e8f3
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.3.0-next.0+sha-af5155f');
+new Version('19.3.0-next.0+sha-7a8e8f3');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
