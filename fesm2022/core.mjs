9fa09580e0a2832158b474716a61abecab93c0ee
kirjs <kirjs@users.noreply.github.com>
2025-02-20 16:09:42 +0000

    195d0b1 docs: Fix typo ')' in banner-initial.component.spec.ts (#60032)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-rc.0+sha-053593e
+ * @license Angular v19.2.0-rc.0+sha-195d0b1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17972,7 +17972,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.0-rc.0+sha-053593e']
+                ? ['ng-version', '19.2.0-rc.0+sha-195d0b1']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34978,7 +34978,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-rc.0+sha-053593e');
+const VERSION = new Version('19.2.0-rc.0+sha-195d0b1');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-rc.0+sha-053593e
+ * @license Angular v19.2.0-rc.0+sha-195d0b1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -175,10 +175,10 @@ class TestBedApplicationErrorHandler {
             throw e;
         }
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.2.0-rc.0+sha-053593e", ngImport: i0, type: TestBedApplicationErrorHandler, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.2.0-rc.0+sha-053593e", ngImport: i0, type: TestBedApplicationErrorHandler });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.2.0-rc.0+sha-195d0b1", ngImport: i0, type: TestBedApplicationErrorHandler, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.2.0-rc.0+sha-195d0b1", ngImport: i0, type: TestBedApplicationErrorHandler });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.2.0-rc.0+sha-053593e", ngImport: i0, type: TestBedApplicationErrorHandler, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.2.0-rc.0+sha-195d0b1", ngImport: i0, type: TestBedApplicationErrorHandler, decorators: [{
             type: Injectable
         }] });
 
