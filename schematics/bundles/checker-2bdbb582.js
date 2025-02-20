293e6ef3c8c3f2dd8edd22bda6a34317f396eaaf
kirjs <kirjs@users.noreply.github.com>
2025-02-20 16:28:17 +0000

    f0990c6 fix(benchpress): Ensure future-proof correct initialization order (#60025)

--- schematics/bundles/checker-2bdbb582.js
+++ schematics/bundles/checker-2bdbb582.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.7+sha-da25071
+ * @license Angular v19.1.7+sha-f0990c6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30477,7 +30477,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.7+sha-da25071');
+new Version('19.1.7+sha-f0990c6');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-4978a91a.js
+++ schematics/bundles/index-4978a91a.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.7+sha-da25071
+ * @license Angular v19.1.7+sha-f0990c6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.7+sha-da25071');
+new checker.Version('19.1.7+sha-f0990c6');
 
 var LogLevel;
 (function (LogLevel) {
