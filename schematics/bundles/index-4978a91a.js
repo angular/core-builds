3aed659957db510582ed1e2da3ce7f020c43744a
kirjs <kirjs@users.noreply.github.com>
2025-02-21 16:17:38 +0000

    91149b5 refactor(core): clean up when blocks fail to fetch or hydrate (#59740)

--- schematics/bundles/index-4978a91a.js
+++ schematics/bundles/index-4978a91a.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.7+sha-51caec8
+ * @license Angular v19.1.7+sha-91149b5
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.1.7+sha-51caec8');
+new checker.Version('19.1.7+sha-91149b5');
 
 var LogLevel;
 (function (LogLevel) {
