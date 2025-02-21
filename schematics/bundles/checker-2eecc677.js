8973881f1adbf6a4035a51946ce91ac24bd5e6ca
kirjs <kirjs@users.noreply.github.com>
2025-02-21 16:22:44 +0000

    3fd6551 docs: remove todo from jsdoc (#60042)

--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-rc.0+sha-2174084
+ * @license Angular v19.2.0-rc.0+sha-3fd6551
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-rc.0+sha-2174084');
+new Version('19.2.0-rc.0+sha-3fd6551');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
