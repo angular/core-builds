fc71859a24230fcfd1b366d31a12e1360f6844ee
kirjs <kirjs@users.noreply.github.com>
2025-02-24 17:01:43 +0000

    e601bdc fix(core): capture stack for HMR errors (#60067)

--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.3.0-next.0+sha-4853129
+ * @license Angular v19.3.0-next.0+sha-e601bdc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.3.0-next.0+sha-4853129');
+new Version('19.3.0-next.0+sha-e601bdc');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-3891dd55.js
+++ schematics/bundles/index-3891dd55.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.3.0-next.0+sha-4853129
+ * @license Angular v19.3.0-next.0+sha-e601bdc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.3.0-next.0+sha-4853129');
+new checker.Version('19.3.0-next.0+sha-e601bdc');
 
 var LogLevel;
 (function (LogLevel) {
