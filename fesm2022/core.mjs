06dee3f7672c7b62aebb41085bee3a5b144daa3c
kirjs <kirjs@users.noreply.github.com>
2025-02-25 19:23:03 +0000

    db2f2d9 fix(router): Scroller should scroll as soon as change detection completes (#60086)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-07338c0
+ * @license Angular v20.0.0-next.0+sha-db2f2d9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18118,7 +18118,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '20.0.0-next.0+sha-07338c0']
+                ? ['ng-version', '20.0.0-next.0+sha-db2f2d9']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -35166,7 +35166,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-07338c0');
+const VERSION = new Version('20.0.0-next.0+sha-db2f2d9');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-6675d6bc.js
+++ schematics/bundles/index-6675d6bc.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.0+sha-07338c0
+ * @license Angular v20.0.0-next.0+sha-db2f2d9
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('20.0.0-next.0+sha-07338c0');
+new checker.Version('20.0.0-next.0+sha-db2f2d9');
 
 var LogLevel;
 (function (LogLevel) {
