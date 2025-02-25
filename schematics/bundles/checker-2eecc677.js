9c9440af78b98a39ca9ecf6d2e6fa815b20d587d
kirjs <kirjs@users.noreply.github.com>
2025-02-25 18:07:16 +0000

    7d66360 fix(common): make types for HttpClient more readable (#59901)

--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-rc.0+sha-ce3a9a2
+ * @license Angular v19.2.0-rc.0+sha-7d66360
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-rc.0+sha-ce3a9a2');
+new Version('19.2.0-rc.0+sha-7d66360');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
