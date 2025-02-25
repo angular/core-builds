fc8f00f879b6bcc1105c58d8f7fa2ffa1b432ea3
kirjs <kirjs@users.noreply.github.com>
2025-02-25 18:07:22 +0000

    1fbaeab fix(common): make types for HttpClient more readable (#59901)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.7+sha-69b6d65
+ * @license Angular v19.1.7+sha-1fbaeab
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17912,7 +17912,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.1.7+sha-69b6d65']
+                ? ['ng-version', '19.1.7+sha-1fbaeab']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34959,7 +34959,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.7+sha-69b6d65');
+const VERSION = new Version('19.1.7+sha-1fbaeab');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-2bdbb582.js
+++ schematics/bundles/checker-2bdbb582.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.7+sha-69b6d65
+ * @license Angular v19.1.7+sha-1fbaeab
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30477,7 +30477,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.7+sha-69b6d65');
+new Version('19.1.7+sha-1fbaeab');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
