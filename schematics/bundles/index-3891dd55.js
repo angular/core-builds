e19c10dcf36c5c31bd7aa253fb24a902c40ee414
kirjs <kirjs@users.noreply.github.com>
2025-02-25 15:30:40 +0000

    93c1918 docs: update security guide (#59885) PR Close #59885

--- schematics/bundles/index-3891dd55.js
+++ schematics/bundles/index-3891dd55.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-rc.0+sha-ebfbaeb
+ * @license Angular v19.2.0-rc.0+sha-93c1918
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-rc.0+sha-ebfbaeb');
+new checker.Version('19.2.0-rc.0+sha-93c1918');
 
 var LogLevel;
 (function (LogLevel) {
--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-rc.0+sha-ebfbaeb
+ * @license Angular v19.2.0-rc.0+sha-93c1918
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
