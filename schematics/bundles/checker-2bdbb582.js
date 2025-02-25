7d1f9fba95e79c0d8f7d61927925c85b1aa7d460
kirjs <kirjs@users.noreply.github.com>
2025-02-25 15:55:33 +0000

    94117f1 build: update all non-major dependencies (#60093)

--- schematics/bundles/checker-2bdbb582.js
+++ schematics/bundles/checker-2bdbb582.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.1.7+sha-7a88ce5
+ * @license Angular v19.1.7+sha-94117f1
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30477,7 +30477,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.1.7+sha-7a88ce5');
+new Version('19.1.7+sha-94117f1');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
