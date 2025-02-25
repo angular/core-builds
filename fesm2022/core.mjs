a2c20ccf39f8b92bcc5cbc18ff9210e2a1961e6a
kirjs <kirjs@users.noreply.github.com>
2025-02-25 15:55:34 +0000

    d5f467e build: update all non-major dependencies (#60093)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0-rc.0+sha-93c1918
+ * @license Angular v19.2.0-rc.0+sha-d5f467e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18019,7 +18019,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.0-rc.0+sha-93c1918']
+                ? ['ng-version', '19.2.0-rc.0+sha-d5f467e']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -35067,7 +35067,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.0-rc.0+sha-93c1918');
+const VERSION = new Version('19.2.0-rc.0+sha-d5f467e');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/index-3891dd55.js
+++ schematics/bundles/index-3891dd55.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-rc.0+sha-93c1918
+ * @license Angular v19.2.0-rc.0+sha-d5f467e
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0-rc.0+sha-93c1918');
+new checker.Version('19.2.0-rc.0+sha-d5f467e');
 
 var LogLevel;
 (function (LogLevel) {
