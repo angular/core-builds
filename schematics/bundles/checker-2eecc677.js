d1d8472fe08a013b04f241da3ad17d6294b6d6db
kirjs <kirjs@users.noreply.github.com>
2025-02-26 17:07:30 +0000

    9b47275 docs(docs-infra): remove explicit `dectectChanges()` (#60098)

--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-rc.0+sha-ee2f07f
+ * @license Angular v19.2.0-rc.0+sha-9b47275
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-rc.0+sha-ee2f07f');
+new Version('19.2.0-rc.0+sha-9b47275');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
