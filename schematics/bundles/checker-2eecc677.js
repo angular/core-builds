a188b84144ed85cd1f5932945742b4379ee5e0e0
kirjs <kirjs@users.noreply.github.com>
2025-02-26 16:58:29 +0000

    ee2f07f refactor(http): rename `map` to `parse` on `HttpResourceOptions` (#60112)

--- schematics/bundles/checker-2eecc677.js
+++ schematics/bundles/checker-2eecc677.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0-rc.0+sha-f76fd48
+ * @license Angular v19.2.0-rc.0+sha-ee2f07f
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0-rc.0+sha-f76fd48');
+new Version('19.2.0-rc.0+sha-ee2f07f');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
