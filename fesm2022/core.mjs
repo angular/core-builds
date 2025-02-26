e93ad574833e81d40594d2f18e030b4f53183ca3
kirjs <kirjs@users.noreply.github.com>
2025-02-26 17:06:56 +0000

    41ef1cb docs(docs-infra): remove explicit `dectectChanges()` (#60098)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.7+sha-567d48f
+ * @license Angular v19.1.7+sha-41ef1cb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17913,7 +17913,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.1.7+sha-567d48f']
+                ? ['ng-version', '19.1.7+sha-41ef1cb']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34960,7 +34960,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.7+sha-567d48f');
+const VERSION = new Version('19.1.7+sha-41ef1cb');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
