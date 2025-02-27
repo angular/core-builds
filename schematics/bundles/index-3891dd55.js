22682425470e976e98f62e7d19b178544eb29d60
Miles Malerba <mmalerba@google.com>
2025-02-27 17:59:15 +0000

    e549bde refactor(animations): tree-shake regular expressions (#60095)

--- schematics/bundles/index-3891dd55.js
+++ schematics/bundles/index-3891dd55.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0+sha-16ca676
+ * @license Angular v19.2.0+sha-e549bde
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.0+sha-16ca676');
+new checker.Version('19.2.0+sha-e549bde');
 
 var LogLevel;
 (function (LogLevel) {
