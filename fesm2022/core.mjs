a91d0683e937611e2f161a1b3961f0ef467061e9
Miles Malerba <mmalerba@google.com>
2025-02-27 18:31:06 +0000

    a0dc0cb docs(docs-infra): add a tooltip to show the full text when nav item text is lengthy (#59038)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-25db666
+ * @license Angular v20.0.0-next.0+sha-a0dc0cb
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18112,7 +18112,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '20.0.0-next.0+sha-25db666']
+                ? ['ng-version', '20.0.0-next.0+sha-a0dc0cb']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -35160,7 +35160,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-25db666');
+const VERSION = new Version('20.0.0-next.0+sha-a0dc0cb');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
