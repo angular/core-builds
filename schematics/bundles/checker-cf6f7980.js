3a8e4d7e81c789f029c914a16acd88d16e9ba447
Miles Malerba <mmalerba@google.com>
2025-02-27 00:32:48 +0000

    4078895 docs: fix mermaid event node text visibility in light mode (#60124) PR Close #60124

--- schematics/bundles/checker-cf6f7980.js
+++ schematics/bundles/checker-cf6f7980.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.0+sha-ab2ee22
+ * @license Angular v20.0.0-next.0+sha-4078895
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31284,7 +31284,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.0+sha-ab2ee22');
+new Version('20.0.0-next.0+sha-4078895');
 
 //////////////////////////////////////
 // This file only reexports content of the `src` folder. Keep it that way.
--- schematics/bundles/index-6675d6bc.js
+++ schematics/bundles/index-6675d6bc.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.0+sha-ab2ee22
+ * @license Angular v20.0.0-next.0+sha-4078895
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('20.0.0-next.0+sha-ab2ee22');
+new checker.Version('20.0.0-next.0+sha-4078895');
 
 var LogLevel;
 (function (LogLevel) {
