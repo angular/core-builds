49a6636214e7a914c9f929eae802df6df28faa26
Miles Malerba <mmalerba@google.com>
2025-03-03 17:20:19 +0000

    c2de5f6 fix(common): clean up `onUrlChange` listener when root scope is destroyed (#60004)

--- schematics/bundles/checker-a9b8c723.js
+++ schematics/bundles/checker-a9b8c723.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.0+sha-1dd9447
+ * @license Angular v19.2.0+sha-c2de5f6
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30722,7 +30722,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.0+sha-1dd9447');
+new Version('19.2.0+sha-c2de5f6');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
