49b2b2e8b8e7c7323652a6270dce3cdaf79507cc
Miles Malerba <mmalerba@google.com>
2025-03-04 22:26:23 +0000

    30ede6b refactor(docs-infra): AppComponent cleanups (#58897)

--- schematics/bundles/checker-af521da6.js
+++ schematics/bundles/checker-af521da6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.0+sha-ff46280
+ * @license Angular v20.0.0-next.0+sha-30ede6b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31388,7 +31388,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.0+sha-ff46280');
+new Version('20.0.0-next.0+sha-30ede6b');
 
 //////////////////////////////////////
 // This file only reexports content of the `src` folder. Keep it that way.
--- schematics/bundles/index-ef1bffbb.js
+++ schematics/bundles/index-ef1bffbb.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.0+sha-ff46280
+ * @license Angular v20.0.0-next.0+sha-30ede6b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('20.0.0-next.0+sha-ff46280');
+new checker.Version('20.0.0-next.0+sha-30ede6b');
 
 var LogLevel;
 (function (LogLevel) {
