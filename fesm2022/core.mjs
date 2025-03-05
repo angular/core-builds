56b459b89a40bb6fc6b4fc260a1d3bdf10b4a6f3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-05 10:10:41 +0000

    4a8d0b7 docs(docs-infra): fix secondary menu (#60212)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.0+sha-30ede6b
+ * @license Angular v20.0.0-next.0+sha-4a8d0b7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18730,7 +18730,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.0+sha-30ede6b']
+        ? ['ng-version', '20.0.0-next.0+sha-4a8d0b7']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
@@ -35407,7 +35407,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.0+sha-30ede6b');
+const VERSION = new Version('20.0.0-next.0+sha-4a8d0b7');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
