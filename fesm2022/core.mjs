ba36a8bc7b4f1543320975aabe58bb702a49e4ca
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:29:15 +0000

    5e2f312 fix(docs-infra): terminal light mode text color (#60246)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-bde1b37
+ * @license Angular v19.2.1+sha-5e2f312
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17878,7 +17878,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.1+sha-bde1b37']
+                ? ['ng-version', '19.2.1+sha-5e2f312']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34583,7 +34583,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-bde1b37');
+const VERSION = new Version('19.2.1+sha-5e2f312');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
