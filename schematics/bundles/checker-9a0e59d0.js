2aae2b452893de419e6d5fd4d01fe11cd9c9b855
Andrew Kushnir <akushnir@google.com>
2025-03-10 22:14:22 +0000

    4e87b58 ci: fix timer test flakiness (#60310)

--- schematics/bundles/checker-9a0e59d0.js
+++ schematics/bundles/checker-9a0e59d0.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.1+sha-ec65169
+ * @license Angular v19.2.1+sha-4e87b58
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30720,7 +30720,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.1+sha-ec65169');
+new Version('19.2.1+sha-4e87b58');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-5e6b2505.js
+++ schematics/bundles/index-5e6b2505.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.1+sha-ec65169
+ * @license Angular v19.2.1+sha-4e87b58
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.1+sha-ec65169');
+new checker.Version('19.2.1+sha-4e87b58');
 
 var LogLevel;
 (function (LogLevel) {
