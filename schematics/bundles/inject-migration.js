548df88f90f82d6a32dc8031a5abb61a82a5b860
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:52:51 +0000

    3089ab4 build: update minimum development Node.js versions (#60080)

--- schematics/bundles/inject-migration.js
+++ schematics/bundles/inject-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.1+sha-07dc806
+ * @license Angular v20.0.0-next.1+sha-3089ab4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
