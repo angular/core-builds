160078d57c3a52f5ef9cc100fb2c5eef3f62f8a8
Andrew Kushnir <akushnir@google.com>
2025-03-11 16:33:26 +0000

    90165b1 refactor(devtools): refactors property view drag-and-drop behavior (#60286)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-c73520b
+ * @license Angular v20.0.0-next.1+sha-90165b1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18515,7 +18515,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.1+sha-c73520b']
+        ? ['ng-version', '20.0.0-next.1+sha-90165b1']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
@@ -34850,7 +34850,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-c73520b');
+const VERSION = new Version('20.0.0-next.1+sha-90165b1');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/inject-migration.js
+++ schematics/bundles/inject-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.1+sha-c73520b
+ * @license Angular v20.0.0-next.1+sha-90165b1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
