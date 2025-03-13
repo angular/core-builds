aa22657ead567feb1848f1864aebd971c9df12c8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 16:42:49 +0000

    cd7c170 fix(upgrade): handle output emitters when downgrading a component (#60369)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.2+sha-adecf4c
+ * @license Angular v20.0.0-next.2+sha-cd7c170
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18473,7 +18473,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.2+sha-adecf4c']
+        ? ['ng-version', '20.0.0-next.2+sha-cd7c170']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
@@ -34811,7 +34811,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.2+sha-adecf4c');
+const VERSION = new Version('20.0.0-next.2+sha-cd7c170');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-DF8ZaFW5.js
+++ schematics/bundles/checker-DF8ZaFW5.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.2+sha-adecf4c
+ * @license Angular v20.0.0-next.2+sha-cd7c170
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31437,7 +31437,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.2+sha-adecf4c');
+new Version('20.0.0-next.2+sha-cd7c170');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
