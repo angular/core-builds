04879b0669a3614d6521e819c46b5ef0a553cba4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 14:56:15 +0000

    7a39585 docs: note that CSS is also taken into account in the max bundle size (#60362)

--- schematics/bundles/checker-DF8ZaFW5.js
+++ schematics/bundles/checker-DF8ZaFW5.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.2+sha-f5f2549
+ * @license Angular v20.0.0-next.2+sha-7a39585
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31437,7 +31437,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.2+sha-f5f2549');
+new Version('20.0.0-next.2+sha-7a39585');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
