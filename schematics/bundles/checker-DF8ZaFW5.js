b5e9b016964eacbba94ddf50b9658bfc32587df5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-14 10:21:59 +0000

    997117b refactor(router): Clean up the transition subject (#60357)

--- schematics/bundles/checker-DF8ZaFW5.js
+++ schematics/bundles/checker-DF8ZaFW5.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.2+sha-d79c57d
+ * @license Angular v20.0.0-next.2+sha-997117b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31437,7 +31437,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.2+sha-d79c57d');
+new Version('20.0.0-next.2+sha-997117b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
