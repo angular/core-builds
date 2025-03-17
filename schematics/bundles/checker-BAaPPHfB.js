e6292fbdb1ddcd1c6bf0f742684939c3f42bad04
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-17 17:40:48 +0000

    450c1fc docs: revise and consolidate server rendering guides (#60383)

--- schematics/bundles/checker-BAaPPHfB.js
+++ schematics/bundles/checker-BAaPPHfB.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.2+sha-b154fb3
+ * @license Angular v20.0.0-next.2+sha-450c1fc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31467,7 +31467,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.2+sha-b154fb3');
+new Version('20.0.0-next.2+sha-450c1fc');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
