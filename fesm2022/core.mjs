0e7a9d22de7f3fc0b7807a457718bbf72dba6008
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:26:03 +0000

    9dffdd7 refactor(devtools): fix reading `resolutionPath` (#60403)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-b4f3352
+ * @license Angular v19.2.2+sha-9dffdd7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17866,7 +17866,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.2+sha-b4f3352']
+                ? ['ng-version', '19.2.2+sha-9dffdd7']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34577,7 +34577,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-b4f3352');
+const VERSION = new Version('19.2.2+sha-9dffdd7');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
