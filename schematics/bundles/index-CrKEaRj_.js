e3560e6e0f178f73f6b07505972474d1fd6a6f41
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:37:51 +0000

    9291d52 refactor(platform-server): expose internal `render` method for extensibility (#60416)

--- schematics/bundles/index-CrKEaRj_.js
+++ schematics/bundles/index-CrKEaRj_.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.2+sha-9dffdd7
+ * @license Angular v19.2.2+sha-9291d52
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.2+sha-9dffdd7');
+new checker.Version('19.2.2+sha-9291d52');
 
 var LogLevel;
 (function (LogLevel) {
