ffa794eb9c3fcb3ee6f62a26ae0972afb569ce0e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:37:52 +0000

    bf5d995 refactor(platform-server): expose internal `render` method for extensibility (#60416)

--- schematics/bundles/checker-BAaPPHfB.js
+++ schematics/bundles/checker-BAaPPHfB.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.2+sha-4930ed3
+ * @license Angular v20.0.0-next.2+sha-bf5d995
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31467,7 +31467,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.2+sha-4930ed3');
+new Version('20.0.0-next.2+sha-bf5d995');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/migrate_ts_type_references-iGQeGRgy.js
+++ schematics/bundles/migrate_ts_type_references-iGQeGRgy.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.2+sha-4930ed3
+ * @license Angular v20.0.0-next.2+sha-bf5d995
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
