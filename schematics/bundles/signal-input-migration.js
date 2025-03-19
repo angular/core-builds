ad2d77c7206d3a373963cec778845f352e92919c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 16:00:50 +0000

    3606aab fix(platform-browser-dynamic): ensure compiler is loaded before `@angular/common` (#60458)

--- schematics/bundles/signal-input-migration.js
+++ schematics/bundles/signal-input-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.2+sha-34f0453
+ * @license Angular v20.0.0-next.2+sha-3606aab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
