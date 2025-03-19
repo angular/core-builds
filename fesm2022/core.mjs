d3687877ca05a4c5bc2675b3325d89f06b358dad
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 19:33:16 +0000

    2be0089 release: cut the v19.2.3 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-2466fc9
+ * @license Angular v19.2.3+sha-2be0089
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17916,7 +17916,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.2+sha-2466fc9']
+                ? ['ng-version', '19.2.3+sha-2be0089']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34627,7 +34627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-2466fc9');
+const VERSION = new Version('19.2.3+sha-2be0089');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
