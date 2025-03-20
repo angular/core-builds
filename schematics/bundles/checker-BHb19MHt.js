cb836cea5fe5404ac144a08eb3955bef4f47f322
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-20 19:39:32 +0000

    70bdb88 fix(platform-browser): ensure `platformBrowserTesting` includes `platformBrowser` providers (#60480)

--- schematics/bundles/checker-BHb19MHt.js
+++ schematics/bundles/checker-BHb19MHt.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.3+sha-911ad40
+ * @license Angular v20.0.0-next.3+sha-70bdb88
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31467,7 +31467,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.3+sha-911ad40');
+new Version('20.0.0-next.3+sha-70bdb88');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/ng_decorators-DznZ5jMl.js
+++ schematics/bundles/ng_decorators-DznZ5jMl.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.3+sha-911ad40
+ * @license Angular v20.0.0-next.3+sha-70bdb88
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
