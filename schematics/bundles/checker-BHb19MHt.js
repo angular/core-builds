fcced2cb4536cfe4fb984c7a942fdcf218d38c29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-20 22:40:18 +0000

    9ffeff9 build: update cross-repo angular dependencies (#60490)

--- schematics/bundles/checker-BHb19MHt.js
+++ schematics/bundles/checker-BHb19MHt.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.3+sha-70bdb88
+ * @license Angular v20.0.0-next.3+sha-9ffeff9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31467,7 +31467,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.3+sha-70bdb88');
+new Version('20.0.0-next.3+sha-9ffeff9');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
