f35cee9b24c84a77c9ace483cf335c6b58bbcc76
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-24 07:01:40 +0000

    dcfa8a0 docs(docs-infra): disable shared-docs tests. (#60520)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.3+sha-13d1c8a
+ * @license Angular v20.0.0-next.3+sha-dcfa8a0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18415,7 +18415,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.3+sha-13d1c8a']
+        ? ['ng-version', '20.0.0-next.3+sha-dcfa8a0']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
@@ -35040,7 +35040,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.3+sha-13d1c8a');
+const VERSION = new Version('20.0.0-next.3+sha-dcfa8a0');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-k591b6WQ.js
+++ schematics/bundles/checker-k591b6WQ.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.3+sha-13d1c8a
+ * @license Angular v20.0.0-next.3+sha-dcfa8a0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31611,7 +31611,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.3+sha-13d1c8a');
+new Version('20.0.0-next.3+sha-dcfa8a0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
