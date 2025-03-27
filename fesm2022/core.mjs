84f44955b40991864872177e2be1e5a02166c3ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-27 18:23:46 +0000

    f7385b4 refactor(service-worker): drop `fromEvent` (#60424)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-98bf4d5
+ * @license Angular v20.0.0-next.4+sha-f7385b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18549,7 +18549,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.4+sha-98bf4d5']
+        ? ['ng-version', '20.0.0-next.4+sha-f7385b4']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
@@ -35085,7 +35085,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-98bf4d5');
+const VERSION = new Version('20.0.0-next.4+sha-f7385b4');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-BvSedeIB.js
+++ schematics/bundles/checker-BvSedeIB.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-98bf4d5
+ * @license Angular v20.0.0-next.4+sha-f7385b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31616,7 +31616,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.4+sha-98bf4d5');
+new Version('20.0.0-next.4+sha-f7385b4');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
