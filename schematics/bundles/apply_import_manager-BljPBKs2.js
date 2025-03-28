8e5ac825bf330d19d097b5e9ae5ca80c9f31c9fb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 19:54:00 +0000

    43d03f8 ci: fix over extraction of init symbols in symbol tests (#60493) (#60614)

--- schematics/bundles/apply_import_manager-BljPBKs2.js
+++ schematics/bundles/apply_import_manager-BljPBKs2.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-882f96f
+ * @license Angular v19.2.4+sha-43d03f8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/index-DwJw0djT.js
+++ schematics/bundles/index-DwJw0djT.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-882f96f
+ * @license Angular v19.2.4+sha-43d03f8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.4+sha-882f96f');
+new checker.Version('19.2.4+sha-43d03f8');
 
 var LogLevel;
 (function (LogLevel) {
