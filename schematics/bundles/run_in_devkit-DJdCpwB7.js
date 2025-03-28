71192c85f5c1f1dd4d4ced4740724000bf644f19
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 11:57:17 +0000

    1226eaa fix(router): Add missing types to transition (#60307)

--- schematics/bundles/run_in_devkit-DJdCpwB7.js
+++ schematics/bundles/run_in_devkit-DJdCpwB7.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-0fedf2c
+ * @license Angular v20.0.0-next.4+sha-1226eaa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
