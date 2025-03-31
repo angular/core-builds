0a9bb4ec49b4d12119392cb2d57ae516c7358538
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 17:16:42 +0000

    139fead fix(devtools): catch firefox non-top level frame error case when using utilities APIs (#60430)

--- schematics/bundles/checker-B_GCUUMg.js
+++ schematics/bundles/checker-B_GCUUMg.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-086b4a9
+ * @license Angular v19.2.4+sha-139fead
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.4+sha-086b4a9');
+new Version('19.2.4+sha-139fead');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/signal-queries-migration.js
+++ schematics/bundles/signal-queries-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-086b4a9
+ * @license Angular v19.2.4+sha-139fead
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
