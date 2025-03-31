ce79e99b969a2a811b89efe81c03d98a08a06167
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:04:23 +0000

    b222288 docs: remove todo comment (#59928)

--- schematics/bundles/index-CV0rzP_-.js
+++ schematics/bundles/index-CV0rzP_-.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-f3361fb
+ * @license Angular v19.2.4+sha-b222288
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.4+sha-f3361fb');
+new checker.Version('19.2.4+sha-b222288');
 
 var LogLevel;
 (function (LogLevel) {
--- schematics/bundles/migrate_ts_type_references-C_WqKzLy.js
+++ schematics/bundles/migrate_ts_type_references-C_WqKzLy.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-f3361fb
+ * @license Angular v19.2.4+sha-b222288
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
