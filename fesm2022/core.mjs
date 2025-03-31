aef9d06314bc3deb65956eb707ae02d467a3c47b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 16:45:14 +0000

    599c45c build: update lock files for integration test apps (#60594)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-52dfce6
+ * @license Angular v20.0.0-next.4+sha-599c45c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-52dfce6');
+const VERSION = new Version('20.0.0-next.4+sha-599c45c');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
