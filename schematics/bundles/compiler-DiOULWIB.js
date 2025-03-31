ba441f0c783197c787e4267a5b091806e9c3a7b6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 17:16:27 +0000

    c5d841a fix(devtools): catch firefox non-top level frame error case when using utilities APIs (#60430)

--- schematics/bundles/compiler-DiOULWIB.js
+++ schematics/bundles/compiler-DiOULWIB.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-8b529de
+ * @license Angular v20.0.0-next.4+sha-c5d841a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31596,7 +31596,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.4+sha-8b529de');
+new Version('20.0.0-next.4+sha-c5d841a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
