cfee410562a6b2211eab0e81b09210fccc02f0d2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 19:28:15 +0000

    be7c8d4 refactor(forms): convert scripts within `packages/forms` to relative imports (#60624)

--- schematics/bundles/signals.js
+++ schematics/bundles/signals.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-3767429
+ * @license Angular v20.0.0-next.4+sha-be7c8d4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
