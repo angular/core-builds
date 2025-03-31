8395e7881dd3312e37c9ddb6a2270bf8529a8620
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 17:03:33 +0000

    8b529de refactor(devtools): fix firefox breadcrumbs (#60552)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-599c45c
+ * @license Angular v20.0.0-next.4+sha-8b529de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-599c45c');
+const VERSION = new Version('20.0.0-next.4+sha-8b529de');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/compiler-DiOULWIB.js
+++ schematics/bundles/compiler-DiOULWIB.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-599c45c
+ * @license Angular v20.0.0-next.4+sha-8b529de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31596,7 +31596,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.4+sha-599c45c');
+new Version('20.0.0-next.4+sha-8b529de');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
