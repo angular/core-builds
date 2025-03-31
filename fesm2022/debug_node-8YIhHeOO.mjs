212c8f0dc54ba3261a4043c9a7a8fb596d063af5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 21:30:02 +0000

    97556e6 build: update io_bazel_rules_sass digest to b80aeb7 (#60653)

--- fesm2022/debug_node-8YIhHeOO.mjs
+++ fesm2022/debug_node-8YIhHeOO.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-1fed7dc
+ * @license Angular v20.0.0-next.4+sha-97556e6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14571,7 +14571,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.4+sha-1fed7dc']
+        ? ['ng-version', '20.0.0-next.4+sha-97556e6']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
