daea4b856e0573ba1062d675656b529c34979927
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 18:21:47 +0000

    b55f4de ci: update to latest dev-infra action versions (#60675)

--- schematics/bundles/compiler-DiOULWIB.js
+++ schematics/bundles/compiler-DiOULWIB.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-b927ce8
+ * @license Angular v20.0.0-next.4+sha-b55f4de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31596,7 +31596,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.4+sha-b927ce8');
+new Version('20.0.0-next.4+sha-b55f4de');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/inject-flags.js
+++ schematics/bundles/inject-flags.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-b927ce8
+ * @license Angular v20.0.0-next.4+sha-b55f4de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
