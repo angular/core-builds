437f2c732391bdfa6a49d242c3596042ceda7702
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 12:41:04 +0000

    9503722 build: update all non-major dependencies (#60668)

--- schematics/bundles/checker-B_GCUUMg.js
+++ schematics/bundles/checker-B_GCUUMg.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-b3c7282
+ * @license Angular v19.2.4+sha-9503722
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.4+sha-b3c7282');
+new Version('19.2.4+sha-9503722');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/index-CV0rzP_-.js
+++ schematics/bundles/index-CV0rzP_-.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-b3c7282
+ * @license Angular v19.2.4+sha-9503722
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.4+sha-b3c7282');
+new checker.Version('19.2.4+sha-9503722');
 
 var LogLevel;
 (function (LogLevel) {
