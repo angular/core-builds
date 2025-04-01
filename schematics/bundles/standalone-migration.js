7ae2356e93852f94fd0a3dff8f8411f7ef082c33
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 20:25:37 +0000

    916f768 build: update @angular/ng-dev digest to 10732a8 (#60678)

--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-e9c5e46
+ * @license Angular v20.0.0-next.4+sha-916f768
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
