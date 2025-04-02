a957ff2271c33a89bd3aa3ba8fb2a777d094d69d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 14:04:42 +0000

    0c5b697 docs: add a note about styles collision resolution (#59266) PR Close #59266

--- schematics/bundles/index-CH_G_p3D.js
+++ schematics/bundles/index-CH_G_p3D.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-f90dc69
+ * @license Angular v19.2.4+sha-0c5b697
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.4+sha-f90dc69');
+new checker.Version('19.2.4+sha-0c5b697');
 
 var LogLevel;
 (function (LogLevel) {
