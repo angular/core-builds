51e9619fe0955218c9daa0510843268f1233dba1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 12:43:18 +0000

    670dc2a docs(docs-infra): fix adev (#60695)

--- schematics/bundles/index-CH_G_p3D.js
+++ schematics/bundles/index-CH_G_p3D.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.4+sha-9604ec6
+ * @license Angular v19.2.4+sha-670dc2a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ require('path');
  * @description
  * Entry point for all public APIs of the compiler-cli package.
  */
-new checker.Version('19.2.4+sha-9604ec6');
+new checker.Version('19.2.4+sha-670dc2a');
 
 var LogLevel;
 (function (LogLevel) {
