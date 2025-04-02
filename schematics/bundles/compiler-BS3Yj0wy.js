605da50297d7285aa6d67ca508717e1ee4171146
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 15:06:32 +0000

    59d40f2 refactor(service-worker): pull less RxJS symbols (#60657)

--- schematics/bundles/compiler-BS3Yj0wy.js
+++ schematics/bundles/compiler-BS3Yj0wy.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-cdbc6e8
+ * @license Angular v20.0.0-next.4+sha-59d40f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31596,7 +31596,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.4+sha-cdbc6e8');
+new Version('20.0.0-next.4+sha-59d40f2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
