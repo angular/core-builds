923d303e95f0b517fc8218e23d854caa8e12cf60
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 14:04:19 +0000

    316fabb docs: add a note about styles collision resolution (#59266) PR Close #59266

--- schematics/bundles/compiler-BS3Yj0wy.js
+++ schematics/bundles/compiler-BS3Yj0wy.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.4+sha-d3fec85
+ * @license Angular v20.0.0-next.4+sha-316fabb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31596,7 +31596,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.4+sha-d3fec85');
+new Version('20.0.0-next.4+sha-316fabb');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
