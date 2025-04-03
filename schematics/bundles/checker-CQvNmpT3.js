ae2793a745ae368fa33069735b2933ec6245697e
Andrew Scott <atscott@google.com>
2025-04-03 17:15:26 +0000

    8d183ae docs: Fix Property Binding (#60725)

--- schematics/bundles/checker-CQvNmpT3.js
+++ schematics/bundles/checker-CQvNmpT3.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.5+sha-5b76da5
+ * @license Angular v19.2.5+sha-8d183ae
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.5+sha-5b76da5');
+new Version('19.2.5+sha-8d183ae');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
