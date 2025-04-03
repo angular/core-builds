239d4dd2b805abc40cf5172077e0d66e5e4c12cb
Andrew Scott <atscott@google.com>
2025-04-03 17:41:57 +0000

    a22b13f fix(language-service): Do not provide element completions in end tag (#60616)

--- fesm2022/debug_node-CRLynXSG.mjs
+++ fesm2022/debug_node-CRLynXSG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-866cea9
+ * @license Angular v20.0.0-next.5+sha-a22b13f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14549,7 +14549,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.5+sha-866cea9']
+        ? ['ng-version', '20.0.0-next.5+sha-a22b13f']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/compiler-CuoiHqkc.js
+++ schematics/bundles/compiler-CuoiHqkc.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.5+sha-866cea9
+ * @license Angular v20.0.0-next.5+sha-a22b13f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31597,7 +31597,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.5+sha-866cea9');
+new Version('20.0.0-next.5+sha-a22b13f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
