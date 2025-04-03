20b3da50b9778fb509689e9f9f28a938ecec3261
Andrew Scott <atscott@google.com>
2025-04-03 19:58:22 +0000

    5795e03 fix(http): Delay stabilization until next app synchronization (#60656)

--- schematics/bundles/compiler-CuoiHqkc.js
+++ schematics/bundles/compiler-CuoiHqkc.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.5+sha-2348f26
+ * @license Angular v20.0.0-next.5+sha-5795e03
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31597,7 +31597,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.5+sha-2348f26');
+new Version('20.0.0-next.5+sha-5795e03');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
