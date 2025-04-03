ba58343ffd287b6d69c7984e92bfebc9f47d23bd
Andrew Scott <atscott@google.com>
2025-04-03 17:58:41 +0000

    1232034 refactor(core): add `ApplicationRef.prototype.bootstrapImpl` with an `injector` parameter (#60622)

--- schematics/bundles/checker-CQvNmpT3.js
+++ schematics/bundles/checker-CQvNmpT3.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.5+sha-0e82d42
+ * @license Angular v19.2.5+sha-1232034
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.5+sha-0e82d42');
+new Version('19.2.5+sha-1232034');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
