39fa08da333442c61d3ee917932d41b3c0d3766b
Andrew Scott <atscott@google.com>
2025-04-04 00:02:58 +0000

    1899358 refactor(common): Add hashchange event for traversals (#60682)

--- schematics/bundles/compiler-CuoiHqkc.js
+++ schematics/bundles/compiler-CuoiHqkc.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.5+sha-1765ebf
+ * @license Angular v20.0.0-next.5+sha-1899358
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31597,7 +31597,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.5+sha-1765ebf');
+new Version('20.0.0-next.5+sha-1899358');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/index-CwFQSYXZ.js
+++ schematics/bundles/index-CwFQSYXZ.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.5+sha-1765ebf
+ * @license Angular v20.0.0-next.5+sha-1899358
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
