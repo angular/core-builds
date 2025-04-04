3bad5a1a1ab411a8c3de4a65c82ce7c265e77126
Andrew Scott <atscott@google.com>
2025-04-04 18:52:10 +0000

    9228a73 feat(docs-infra): Add Navigation API adapter to adev (#60722)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-92c4123
+ * @license Angular v20.0.0-next.5+sha-9228a73
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-92c4123');
+const VERSION = new Version('20.0.0-next.5+sha-9228a73');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
