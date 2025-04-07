cfe841ee28fbd4c05498dbdc8ffc09e533a657c9
Andrew Scott <atscott@google.com>
2025-04-07 18:38:03 +0000

    0ae1889 fix(core): run `ApplicationRef.prototype.bootstrap` in `NgZone` (#60720)

--- schematics/bundles/compiler-BQ7R7w2v.js
+++ schematics/bundles/compiler-BQ7R7w2v.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.5+sha-5039ed0
+ * @license Angular v20.0.0-next.5+sha-0ae1889
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32262,7 +32262,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.5+sha-5039ed0');
+new Version('20.0.0-next.5+sha-0ae1889');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/test-bed-get.js
+++ schematics/bundles/test-bed-get.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.5+sha-5039ed0
+ * @license Angular v20.0.0-next.5+sha-0ae1889
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
