4a49d98c0a9ece32c4b897e05d2e592e08e878c7
Andrew Scott <atscott@google.com>
2025-04-07 16:29:49 +0000

    d8e941b fix(core): fix docs for output migration (#60764)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-9228a73
+ * @license Angular v20.0.0-next.5+sha-d8e941b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-9228a73');
+const VERSION = new Version('20.0.0-next.5+sha-d8e941b');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/compiler-BQ7R7w2v.js
+++ schematics/bundles/compiler-BQ7R7w2v.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.5+sha-9228a73
+ * @license Angular v20.0.0-next.5+sha-d8e941b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32262,7 +32262,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.5+sha-9228a73');
+new Version('20.0.0-next.5+sha-d8e941b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
