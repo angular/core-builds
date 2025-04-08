b6143e6aecb8b1c3c4bc6fa85cb7ccd4da5962bb
Andrew Scott <atscott@google.com>
2025-04-08 23:32:09 +0000

    ae8a4c4 build: update github/codeql-action action to v3.28.15 (#60780)

--- schematics/bundles/checker-DoX_7XCa.js
+++ schematics/bundles/checker-DoX_7XCa.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.5+sha-9241615
+ * @license Angular v19.2.5+sha-ae8a4c4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30727,7 +30727,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.5+sha-9241615');
+new Version('19.2.5+sha-ae8a4c4');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
