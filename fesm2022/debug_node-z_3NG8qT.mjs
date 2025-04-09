d7e779c1940360b7cd2694689f2cbfdca377674a
Andrew Scott <atscott@google.com>
2025-04-09 21:30:43 +0000

    9f31947 fix(http): Include HTTP status code and headers when HTTP requests errored in `httpResource` (#60802)

--- fesm2022/debug_node-z_3NG8qT.mjs
+++ fesm2022/debug_node-z_3NG8qT.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-ea58402
+ * @license Angular v20.0.0-next.6+sha-9f31947
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14549,7 +14549,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.6+sha-ea58402']
+        ? ['ng-version', '20.0.0-next.6+sha-9f31947']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_paths-ql6qcf_c.js
+++ schematics/bundles/project_paths-ql6qcf_c.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.6+sha-ea58402
+ * @license Angular v20.0.0-next.6+sha-9f31947
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
