4480c00f2bbb2b326491e6e23bce299b5a4af57b
Andrew Scott <atscott@google.com>
2025-04-09 21:31:14 +0000

    d39e09d fix(http): Include HTTP status code and headers when HTTP requests errored in `httpResource` (#60802)

--- schematics/bundles/checker-DoX_7XCa.js
+++ schematics/bundles/checker-DoX_7XCa.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.6+sha-54f9269
+ * @license Angular v19.2.6+sha-d39e09d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30727,7 +30727,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.6+sha-54f9269');
+new Version('19.2.6+sha-d39e09d');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/control-flow-migration.js
+++ schematics/bundles/control-flow-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.6+sha-54f9269
+ * @license Angular v19.2.6+sha-d39e09d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
