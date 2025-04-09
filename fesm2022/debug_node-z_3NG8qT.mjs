8368635342b9b3f56b53ccdccae8477751570b75
Andrew Scott <atscott01@gmail.com>
2025-04-09 19:36:16 +0000

    c3113b9 docs: release notes for the v19.2.6 release

--- fesm2022/debug_node-z_3NG8qT.mjs
+++ fesm2022/debug_node-z_3NG8qT.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-8744c9a
+ * @license Angular v20.0.0-next.5+sha-c3113b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14549,7 +14549,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.5+sha-8744c9a']
+        ? ['ng-version', '20.0.0-next.5+sha-c3113b9']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/compiler-BQ7R7w2v.js
+++ schematics/bundles/compiler-BQ7R7w2v.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.5+sha-8744c9a
+ * @license Angular v20.0.0-next.5+sha-c3113b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32262,7 +32262,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.5+sha-8744c9a');
+new Version('20.0.0-next.5+sha-c3113b9');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
