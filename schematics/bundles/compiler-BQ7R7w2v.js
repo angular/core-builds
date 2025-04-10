dc7954e48c967ee32e1678272f8421e768b87c2f
kirjs <kirjs@users.noreply.github.com>
2025-04-10 15:28:41 +0000

    2021f64 refactor(devtools): reduce material form field density globally (#60633)

--- schematics/bundles/compiler-BQ7R7w2v.js
+++ schematics/bundles/compiler-BQ7R7w2v.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.6+sha-abe9691
+ * @license Angular v20.0.0-next.6+sha-2021f64
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32262,7 +32262,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.6+sha-abe9691');
+new Version('20.0.0-next.6+sha-2021f64');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/property_name-BBwFuqMe.js
+++ schematics/bundles/property_name-BBwFuqMe.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.6+sha-abe9691
+ * @license Angular v20.0.0-next.6+sha-2021f64
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
