475d034b0deea1ba1267a5247d000a36ab66e563
kirjs <kirjs@users.noreply.github.com>
2025-04-15 21:33:35 +0000

    070c09f fix(docs-infra): prevent processing node_modules .md files (#60872)

--- schematics/bundles/checker-BNmiXJIJ.js
+++ schematics/bundles/checker-BNmiXJIJ.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.6+sha-9171401
+ * @license Angular v19.2.6+sha-070c09f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.6+sha-9171401');
+new Version('19.2.6+sha-070c09f');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
