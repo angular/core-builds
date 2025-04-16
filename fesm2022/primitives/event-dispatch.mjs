606752e173f2e1f00c88c12d329a3545e2357a0f
Kirill Cherkashin <kirill@kcherkashin.org>
2025-04-16 21:08:53 +0000

    c7e9cb9 release: cut the v20.0.0-next.7 release

--- fesm2022/primitives/event-dispatch.mjs
+++ fesm2022/primitives/event-dispatch.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-58728c7
+ * @license Angular v20.0.0-next.7+sha-c7e9cb9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/document-core.js
+++ schematics/bundles/document-core.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.6+sha-58728c7
+ * @license Angular v20.0.0-next.7+sha-c7e9cb9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
