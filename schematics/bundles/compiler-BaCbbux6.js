a7db284f96e7348d257dade621e85239acebdd7f
Kirill Cherkashin <kirill@kcherkashin.org>
2025-04-16 20:50:21 +0000

    58728c7 docs: release notes for the v19.2.7 release

--- schematics/bundles/compiler-BaCbbux6.js
+++ schematics/bundles/compiler-BaCbbux6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.6+sha-5675223
+ * @license Angular v20.0.0-next.6+sha-58728c7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32263,7 +32263,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.6+sha-5675223');
+new Version('20.0.0-next.6+sha-58728c7');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/symbol-VPWguRxr.js
+++ schematics/bundles/symbol-VPWguRxr.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.6+sha-5675223
+ * @license Angular v20.0.0-next.6+sha-58728c7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
