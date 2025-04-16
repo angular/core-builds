d0d701ab270a046a6e7c69ecceee089bda1b5d5f
kirjs <kirjs@users.noreply.github.com>
2025-04-16 14:29:55 +0000

    f22c72b docs(docs-infra): first-app tutorial - step-14-http src-final correction (#60738)

--- fesm2022/debug_node-B3CixwNH.mjs
+++ fesm2022/debug_node-B3CixwNH.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-e90daf3
+ * @license Angular v20.0.0-next.6+sha-f22c72b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14483,7 +14483,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.6+sha-e90daf3']
+        ? ['ng-version', '20.0.0-next.6+sha-f22c72b']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/compiler-BaCbbux6.js
+++ schematics/bundles/compiler-BaCbbux6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.6+sha-e90daf3
+ * @license Angular v20.0.0-next.6+sha-f22c72b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32263,7 +32263,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.6+sha-e90daf3');
+new Version('20.0.0-next.6+sha-f22c72b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
