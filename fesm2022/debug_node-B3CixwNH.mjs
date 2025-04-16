30dea56003d77d383c7810931bd7de94cb6e56e9
kirjs <kirjs@users.noreply.github.com>
2025-04-16 16:08:08 +0000

    cbbea70 fix(common): issue a warning instead of an error when `NgOptimizedImage` exceeds the preload limit (#60879)

--- fesm2022/debug_node-B3CixwNH.mjs
+++ fesm2022/debug_node-B3CixwNH.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-f22c72b
+ * @license Angular v20.0.0-next.6+sha-cbbea70
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14483,7 +14483,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.6+sha-f22c72b']
+        ? ['ng-version', '20.0.0-next.6+sha-cbbea70']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
