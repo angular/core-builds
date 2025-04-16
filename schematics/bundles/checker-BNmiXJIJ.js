171bc64941d2ca27eeb89c19199c00c553d6aca7
kirjs <kirjs@users.noreply.github.com>
2025-04-16 16:52:46 +0000

    c90eea2 refactor(common): eliminate redundant iterator-to-array conversion (#60884)

--- schematics/bundles/checker-BNmiXJIJ.js
+++ schematics/bundles/checker-BNmiXJIJ.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.6+sha-37ab681
+ * @license Angular v19.2.6+sha-c90eea2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.6+sha-37ab681');
+new Version('19.2.6+sha-c90eea2');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
