889bd71e83ae8605501e96365203d3e7f762a53b
kirjs <kirjs@users.noreply.github.com>
2025-04-16 16:56:25 +0000

    bf8492b feat(core): stabilize withI18nSupport() api (#60889)

--- schematics/bundles/compiler-BaCbbux6.js
+++ schematics/bundles/compiler-BaCbbux6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.6+sha-cbbea70
+ * @license Angular v20.0.0-next.6+sha-bf8492b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32263,7 +32263,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.6+sha-cbbea70');
+new Version('20.0.0-next.6+sha-bf8492b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
