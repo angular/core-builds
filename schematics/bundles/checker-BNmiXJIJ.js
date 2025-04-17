4c819af83488a8678258b244c14644e2467e765c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-17 09:31:12 +0000

    763e3ca release: bump Angular DevTools version to 1.0.28 (#60893)

--- schematics/bundles/checker-BNmiXJIJ.js
+++ schematics/bundles/checker-BNmiXJIJ.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.7+sha-2c1f348
+ * @license Angular v19.2.7+sha-763e3ca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.7+sha-2c1f348');
+new Version('19.2.7+sha-763e3ca');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/project_paths-C-UAB3jT.js
+++ schematics/bundles/project_paths-C-UAB3jT.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.7+sha-2c1f348
+ * @license Angular v19.2.7+sha-763e3ca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
