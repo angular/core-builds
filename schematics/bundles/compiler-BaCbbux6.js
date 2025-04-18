0da4f86c09d3953db011815ac8c250cce4ab3a93
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 13:21:31 +0000

    1694739 build: update cross-repo angular dependencies (#60897)

--- schematics/bundles/compiler-BaCbbux6.js
+++ schematics/bundles/compiler-BaCbbux6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.7+sha-2f3f8d6
+ * @license Angular v20.0.0-next.7+sha-1694739
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32263,7 +32263,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.7+sha-2f3f8d6');
+new Version('20.0.0-next.7+sha-1694739');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
