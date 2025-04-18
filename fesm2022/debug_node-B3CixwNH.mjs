73ce355b36a204ccc42af7467b69f547ddbf8c91
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 13:41:58 +0000

    3a6121f docs: add new routing in-depth guide (#60006)

--- fesm2022/debug_node-B3CixwNH.mjs
+++ fesm2022/debug_node-B3CixwNH.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-1694739
+ * @license Angular v20.0.0-next.7+sha-3a6121f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14483,7 +14483,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.7+sha-1694739']
+        ? ['ng-version', '20.0.0-next.7+sha-3a6121f']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
