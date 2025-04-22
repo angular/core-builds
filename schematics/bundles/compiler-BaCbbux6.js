a777de26a3857a39043772de901df61ed1176663
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 14:42:03 +0000

    6c2fbad refactor(core): Improve NG0600 error message. (#60418)

--- schematics/bundles/compiler-BaCbbux6.js
+++ schematics/bundles/compiler-BaCbbux6.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.7+sha-00dff8b
+ * @license Angular v20.0.0-next.7+sha-6c2fbad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32263,7 +32263,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.7+sha-00dff8b');
+new Version('20.0.0-next.7+sha-6c2fbad');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
