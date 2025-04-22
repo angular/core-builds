adbd41bdbd7e5cf158953c30fc169ba539ad74ec
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 11:01:25 +0000

    e1b0e94 docs(docs-infra): remove style guide examples (#60929)

--- schematics/bundles/signals.js
+++ schematics/bundles/signals.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.7+sha-e53a374
+ * @license Angular v20.0.0-next.7+sha-e1b0e94
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
