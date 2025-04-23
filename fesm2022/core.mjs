5041a38221e822d8965540576e6b5e7a26a555a8
Miles Malerba <mmalerba@google.com>
2025-04-23 21:42:07 +0000

    5b0ec4a release: bump Angular DevTools version to 1.0.30 (#60992)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-5696451
+ * @license Angular v19.2.8+sha-5b0ec4a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17913,7 +17913,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.8+sha-5696451']
+                ? ['ng-version', '19.2.8+sha-5b0ec4a']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34653,7 +34653,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-5696451');
+const VERSION = new Version('19.2.8+sha-5b0ec4a');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
