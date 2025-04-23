dbeea6e29372cf07a6982b9f6e4ba1f79897bcf4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 14:35:14 +0000

    94b2a8c release: cut the v19.2.8 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-b557618
+ * @license Angular v19.2.8+sha-94b2a8c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17913,7 +17913,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.7+sha-b557618']
+                ? ['ng-version', '19.2.8+sha-94b2a8c']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34653,7 +34653,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-b557618');
+const VERSION = new Version('19.2.8+sha-94b2a8c');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-BNmiXJIJ.js
+++ schematics/bundles/checker-BNmiXJIJ.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.7+sha-b557618
+ * @license Angular v19.2.8+sha-94b2a8c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.7+sha-b557618');
+new Version('19.2.8+sha-94b2a8c');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
