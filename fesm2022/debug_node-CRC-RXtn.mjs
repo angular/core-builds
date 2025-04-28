d06403c9a149b64f5c1e9735ea148d15059fdd59
Miles Malerba <mmalerba@google.com>
2025-04-28 15:18:16 +0000

    11e3101 build: lock file maintenance (#61015)

--- fesm2022/debug_node-CRC-RXtn.mjs
+++ fesm2022/debug_node-CRC-RXtn.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-05a03d3
+ * @license Angular v20.0.0-next.8+sha-11e3101
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14536,7 +14536,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.8+sha-05a03d3']
+        ? ['ng-version', '20.0.0-next.8+sha-11e3101']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/compiler-D0Vc0aFl.js
+++ schematics/bundles/compiler-D0Vc0aFl.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.8+sha-05a03d3
+ * @license Angular v20.0.0-next.8+sha-11e3101
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32398,7 +32398,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.8+sha-05a03d3');
+new Version('20.0.0-next.8+sha-11e3101');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
