5370a0d960778741568cd1798c0b093cc9a7f62f
Miles Malerba <mmalerba@google.com>
2025-04-28 15:37:08 +0000

    0203e0b Update dependency-injection.md (#61019)

--- schematics/bundles/checker-BNmiXJIJ.js
+++ schematics/bundles/checker-BNmiXJIJ.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.8+sha-54465e8
+ * @license Angular v19.2.8+sha-0203e0b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30728,7 +30728,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.8+sha-54465e8');
+new Version('19.2.8+sha-0203e0b');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/explicit-standalone-flag.js
+++ schematics/bundles/explicit-standalone-flag.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.8+sha-54465e8
+ * @license Angular v19.2.8+sha-0203e0b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
