8bdc7d941401dc70eacf9a52a68220917779f525
Miles Malerba <mmalerba@google.com>
2025-04-29 19:28:57 +0000

    b7d3f3d feat(common): Allow passing ScrollOptions to ViewportScroller (#61002)

--- fesm2022/debug_node-CoJgG2nl.mjs
+++ fesm2022/debug_node-CoJgG2nl.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-b250bb0
+ * @license Angular v20.0.0-next.8+sha-b7d3f3d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14539,7 +14539,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.8+sha-b250bb0']
+        ? ['ng-version', '20.0.0-next.8+sha-b7d3f3d']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
