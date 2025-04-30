c3e0e76b61e8d96176fed2294bf65c1869c52346
Andrew Kushnir <akushnir@google.com>
2025-04-30 22:18:47 +0000

    05feabd build: update cross-repo angular dependencies (#61069)

--- schematics/bundles/compiler-BSv6JWRF.js
+++ schematics/bundles/compiler-BSv6JWRF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.9+sha-b8f80ab
+ * @license Angular v20.0.0-next.9+sha-05feabd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32364,7 +32364,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.9+sha-b8f80ab');
+new Version('20.0.0-next.9+sha-05feabd');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
