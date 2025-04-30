dcc9931e5af3625a566dca9a66e8072815d6906d
Miles Malerba <mmalerba@google.com>
2025-04-30 16:37:46 +0000

    bb00b93 build: update cross-repo angular dependencies (#61062)

--- fesm2022/debug_node-sP8Ihhla.mjs
+++ fesm2022/debug_node-sP8Ihhla.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-624be2e
+ * @license Angular v20.0.0-next.8+sha-bb00b93
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14551,7 +14551,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.8+sha-624be2e']
+        ? ['ng-version', '20.0.0-next.8+sha-bb00b93']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/compiler-B4MK7BP9.js
+++ schematics/bundles/compiler-B4MK7BP9.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.8+sha-624be2e
+ * @license Angular v20.0.0-next.8+sha-bb00b93
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32366,7 +32366,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.8+sha-624be2e');
+new Version('20.0.0-next.8+sha-bb00b93');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
