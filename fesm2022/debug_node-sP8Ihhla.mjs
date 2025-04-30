c0bad4420d78c66438d57a52b6973f7f115fc5cf
Miles Malerba <mmalerba@google.com>
2025-04-30 17:37:32 +0000

    829379e docs: release notes for the v19.2.9 release

--- fesm2022/debug_node-sP8Ihhla.mjs
+++ fesm2022/debug_node-sP8Ihhla.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-bb00b93
+ * @license Angular v20.0.0-next.8+sha-829379e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14551,7 +14551,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-next.8+sha-bb00b93']
+        ? ['ng-version', '20.0.0-next.8+sha-829379e']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/property_name-BBwFuqMe.js
+++ schematics/bundles/property_name-BBwFuqMe.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.8+sha-bb00b93
+ * @license Angular v20.0.0-next.8+sha-829379e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
