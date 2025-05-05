cfcf53fb9da5ca0e66c1742165d8e21918625e24
Andrew Kushnir <akushnir@google.com>
2025-05-05 15:52:22 +0000

    cc48424 docs: add guide for using native CSS animations (#61118)

--- schematics/bundles/checker-CGGdizaF.js
+++ schematics/bundles/checker-CGGdizaF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.9+sha-238441a
+ * @license Angular v19.2.9+sha-cc48424
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.9+sha-238441a');
+new Version('19.2.9+sha-cc48424');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/project_paths-CmS2U6KE.js
+++ schematics/bundles/project_paths-CmS2U6KE.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.9+sha-238441a
+ * @license Angular v19.2.9+sha-cc48424
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
