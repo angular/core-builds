6ae058746a3828608007691ae5f0851121e68021
Andrew Kushnir <akushnir@google.com>
2025-05-05 22:43:08 +0000

    8772236 build: update rules_angular digest to 42d4791 (#61126)

--- schematics/bundles/checker-CGGdizaF.js
+++ schematics/bundles/checker-CGGdizaF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.9+sha-ba5a3c0
+ * @license Angular v19.2.9+sha-8772236
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.9+sha-ba5a3c0');
+new Version('19.2.9+sha-8772236');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
