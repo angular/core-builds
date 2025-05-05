c3caa0029697029207745c0592664363353458d4
Andrew Kushnir <akushnir@google.com>
2025-05-05 22:42:47 +0000

    3b3466a build: update rules_angular digest to 42d4791 (#61126)

--- schematics/bundles/compiler-BSv6JWRF.js
+++ schematics/bundles/compiler-BSv6JWRF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.9+sha-63d10dc
+ * @license Angular v20.0.0-next.9+sha-3b3466a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32364,7 +32364,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.9+sha-63d10dc');
+new Version('20.0.0-next.9+sha-3b3466a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
