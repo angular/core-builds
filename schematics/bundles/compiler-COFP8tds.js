c8229f986ff4075055da36932ed0155351e3d96d
Andrew Kushnir <akushnir@google.com>
2025-05-05 22:42:44 +0000

    235f7c9 build: update rules_angular digest to 42d4791 (#61126)

--- schematics/bundles/compiler-COFP8tds.js
+++ schematics/bundles/compiler-COFP8tds.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-2c17145
+ * @license Angular v20.1.0-next.0+sha-235f7c9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32369,7 +32369,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-2c17145');
+new Version('20.1.0-next.0+sha-235f7c9');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/self-closing-tags-migration.js
+++ schematics/bundles/self-closing-tags-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-2c17145
+ * @license Angular v20.1.0-next.0+sha-235f7c9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
