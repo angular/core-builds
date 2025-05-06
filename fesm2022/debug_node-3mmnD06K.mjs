91ca48d98e521ad95c09e37ef9858d48c1b4da81
Andrew Kushnir <akushnir@google.com>
2025-05-06 00:08:32 +0000

    a2b6163 docs: update ssr docs to include prerendering wildcards (#61116)

--- fesm2022/debug_node-3mmnD06K.mjs
+++ fesm2022/debug_node-3mmnD06K.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-235f7c9
+ * @license Angular v20.1.0-next.0+sha-a2b6163
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14551,7 +14551,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-next.0+sha-235f7c9']
+        ? ['ng-version', '20.1.0-next.0+sha-a2b6163']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
