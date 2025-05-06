a5e7e617ccabaf4a6dcada6ac9f371b8966ea8c4
Andrew Kushnir <akushnir@google.com>
2025-05-06 20:51:39 +0000

    eda8909 fix(devtools): support defer blocks in IdentityTracker (#61139)

--- schematics/bundles/compiler-COFP8tds.js
+++ schematics/bundles/compiler-COFP8tds.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-c39e799
+ * @license Angular v20.1.0-next.0+sha-eda8909
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32369,7 +32369,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-c39e799');
+new Version('20.1.0-next.0+sha-eda8909');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
