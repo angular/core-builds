06dbbd90961164179296cf181dba33f41deb5888
Andrew Kushnir <akushnir@google.com>
2025-05-06 20:42:04 +0000

    e9e1c43 build: update tslint configuration (#61146)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-93e2735
+ * @license Angular v19.2.9+sha-e9e1c43
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17944,7 +17944,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.9+sha-93e2735']
+                ? ['ng-version', '19.2.9+sha-e9e1c43']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34683,7 +34683,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-93e2735');
+const VERSION = new Version('19.2.9+sha-e9e1c43');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-CGGdizaF.js
+++ schematics/bundles/checker-CGGdizaF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.9+sha-93e2735
+ * @license Angular v19.2.9+sha-e9e1c43
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.9+sha-93e2735');
+new Version('19.2.9+sha-e9e1c43');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
