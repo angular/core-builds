6ee07f679d6c772bd0c93e10308a4e083a2be0be
Andrew Kushnir <akushnir@google.com>
2025-05-06 17:32:03 +0000

    121e0e6 docs: fix whitespace after a link (#61136)

--- schematics/bundles/compiler-BSv6JWRF.js
+++ schematics/bundles/compiler-BSv6JWRF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.9+sha-2b7aa91
+ * @license Angular v20.0.0-next.9+sha-121e0e6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32364,7 +32364,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.9+sha-2b7aa91');
+new Version('20.0.0-next.9+sha-121e0e6');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
