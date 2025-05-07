7f2986db5625dd4b0fd43ce1e5746350eaacdff1
Andrew Kushnir <akushnir@google.com>
2025-05-07 18:38:06 +0000

    9f43232 refactor(router): Avoid unnecessary href updates on navigations for routerlink (#60875)

--- fesm2022/debug_node-3mmnD06K.mjs
+++ fesm2022/debug_node-3mmnD06K.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-9678a30
+ * @license Angular v20.0.0-rc.0+sha-9f43232
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14551,7 +14551,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.0+sha-9678a30']
+        ? ['ng-version', '20.0.0-rc.0+sha-9f43232']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
