89058c1c37d3e8ec11a97334b4d71b9cc0dc2b5d
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-05-07 17:58:41 +0000

    ea87eb4 release: cut the v20.0.0-rc.0 release

--- schematics/bundles/compiler-BSv6JWRF.js
+++ schematics/bundles/compiler-BSv6JWRF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-next.9+sha-803c94c
+ * @license Angular v20.0.0-rc.0+sha-ea87eb4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32364,7 +32364,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-next.9+sha-803c94c');
+new Version('20.0.0-rc.0+sha-ea87eb4');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
