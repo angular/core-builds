7d18ccee61925bfc4c7a1d22fed0c95e0919a1ee
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-05-07 18:00:09 +0000

    340728c docs: release notes for the v20.0.0-rc.0 release

--- schematics/bundles/compiler-COFP8tds.js
+++ schematics/bundles/compiler-COFP8tds.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-a1fdb44
+ * @license Angular v20.1.0-next.0+sha-340728c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32369,7 +32369,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-a1fdb44');
+new Version('20.1.0-next.0+sha-340728c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
