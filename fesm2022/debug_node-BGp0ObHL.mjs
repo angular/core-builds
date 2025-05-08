8035d1694c6356bd6d8492a44da900aee8cf9975
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 17:04:23 +0000

    11727dd docs:  update the description of example (#61205) PR Close #61205

--- fesm2022/debug_node-BGp0ObHL.mjs
+++ fesm2022/debug_node-BGp0ObHL.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-b3bb2a1
+ * @license Angular v20.1.0-next.0+sha-11727dd-with-local-changes
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14551,7 +14551,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-next.0+sha-b3bb2a1']
+        ? ['ng-version', '20.1.0-next.0+sha-11727dd-with-local-changes']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
