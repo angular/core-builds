003015e7d09a3a1a4645305c15f652c5678b95a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 16:33:51 +0000

    0f99d63 ci: fix step name (#61213)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-66753dc
+ * @license Angular v20.1.0-next.0+sha-0f99d63
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-66753dc');
+const VERSION = new Version('20.1.0-next.0+sha-0f99d63');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
