360cfe3dc8b18db5bf09ab14c3dfe8ab54265c32
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 11:36:39 +0000

    50993be ci: add workflow to sync non-default branches (#61201)

--- schematics/bundles/compiler-DCkUGCAs.js
+++ schematics/bundles/compiler-DCkUGCAs.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-0558575
+ * @license Angular v20.1.0-next.0+sha-50993be
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32468,7 +32468,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-0558575');
+new Version('20.1.0-next.0+sha-50993be');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
