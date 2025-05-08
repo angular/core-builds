21f40be594a0dc83a25bba3d13ef38a37c2c2dd2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 16:33:46 +0000

    5666d25 ci: fix step name (#61213)

--- schematics/bundles/apply_import_manager-Coc7Hewu.js
+++ schematics/bundles/apply_import_manager-Coc7Hewu.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.0+sha-bad2870
+ * @license Angular v20.0.0-rc.0+sha-5666d25
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
