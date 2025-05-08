eb78550095f3ac3d41ad2214bcc28efad917e133
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 11:36:17 +0000

    1dece6c ci: add workflow to sync non-default branches (#61201)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-9f43232
+ * @license Angular v20.0.0-rc.0+sha-1dece6c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-9f43232');
+const VERSION = new Version('20.0.0-rc.0+sha-1dece6c');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/compiler-BSv6JWRF.js
+++ schematics/bundles/compiler-BSv6JWRF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.0+sha-9f43232
+ * @license Angular v20.0.0-rc.0+sha-1dece6c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32364,7 +32364,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.0+sha-9f43232');
+new Version('20.0.0-rc.0+sha-1dece6c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
