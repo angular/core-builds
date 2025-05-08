328d8aad5b4291f0472772bf6d98df1cefcda259
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 19:03:52 +0000

    db06708 ci: update workflow to clone repo with history (#61223)

--- schematics/bundles/compiler-DCkUGCAs.js
+++ schematics/bundles/compiler-DCkUGCAs.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-adffb2e-with-local-changes
+ * @license Angular v20.1.0-next.0+sha-db06708-with-local-changes
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32468,7 +32468,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-adffb2e-with-local-changes');
+new Version('20.1.0-next.0+sha-db06708-with-local-changes');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/standalone-migration.js
+++ schematics/bundles/standalone-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-adffb2e-with-local-changes
+ * @license Angular v20.1.0-next.0+sha-db06708-with-local-changes
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
