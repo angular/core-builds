724614f59bd49715309a1c49dca6b6febc65b6e0
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 14:44:14 +0000

    3b9440b ci: use bash syntax for `CURRENT_BRANCH` in GitHub Actions (#61212)

--- schematics/bundles/checker-CGGdizaF.js
+++ schematics/bundles/checker-CGGdizaF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.10+sha-6255954
+ * @license Angular v19.2.10+sha-3b9440b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.10+sha-6255954');
+new Version('19.2.10+sha-3b9440b');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
