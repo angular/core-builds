c392ae62a5701d279ae9a61eb8927af144b29f7f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 19:03:09 +0000

    5cecf0d ci: update workflow to clone repo with history (#61223)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-3076117
+ * @license Angular v20.0.0-rc.0+sha-5cecf0d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-3076117');
+const VERSION = new Version('20.0.0-rc.0+sha-5cecf0d');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
