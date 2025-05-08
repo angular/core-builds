a42a7dc538325b6e901055a2764e683fefe35e22
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 18:39:42 +0000

    3076117 ci: use ` --force-with-lease`  when pushing to remote branch (#61222)

--- fesm2022/debug_node-3mmnD06K.mjs
+++ fesm2022/debug_node-3mmnD06K.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-23ee85d
+ * @license Angular v20.0.0-rc.0+sha-3076117
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14551,7 +14551,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.0+sha-23ee85d']
+        ? ['ng-version', '20.0.0-rc.0+sha-3076117']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
