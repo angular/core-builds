6904d64770f7be48e468f64466f01caea61f0205
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 18:40:00 +0000

    7820f4e ci: use ` --force-with-lease`  when pushing to remote branch (#61222)

--- schematics/bundles/checker-CGGdizaF.js
+++ schematics/bundles/checker-CGGdizaF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.10+sha-5b60442
+ * @license Angular v19.2.10+sha-7820f4e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.10+sha-5b60442');
+new Version('19.2.10+sha-7820f4e');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
