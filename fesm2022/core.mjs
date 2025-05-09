75bc202263ecc7ebf66250a677a779ae97917d71
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:49:36 +0000

    12a5b7b build: migrate upgrade package to use ts_project (#61245)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-5f1c08d
+ * @license Angular v20.1.0-next.0+sha-12a5b7b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-5f1c08d');
+const VERSION = new Version('20.1.0-next.0+sha-12a5b7b');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/ng_decorators-DznZ5jMl.js
+++ schematics/bundles/ng_decorators-DznZ5jMl.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-5f1c08d
+ * @license Angular v20.1.0-next.0+sha-12a5b7b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
