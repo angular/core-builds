7633a97f36d949adac06be8300337b77b1c75918
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:49:29 +0000

    5b77cf5 build: migrate upgrade package to use ts_project (#61245)

--- fesm2022/debug_node-3mmnD06K.mjs
+++ fesm2022/debug_node-3mmnD06K.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-345d3d8
+ * @license Angular v20.0.0-rc.0+sha-5b77cf5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14551,7 +14551,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.0+sha-345d3d8']
+        ? ['ng-version', '20.0.0-rc.0+sha-5b77cf5']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
