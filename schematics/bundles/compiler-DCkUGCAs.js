abf249525ddd05d24ddea43e9c4c34f10e67a598
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-09 10:00:43 +0000

    c8dfe6d ci: ensure pnpm-lock file and aspect lock files are up-to-date (#61224)

--- schematics/bundles/compiler-DCkUGCAs.js
+++ schematics/bundles/compiler-DCkUGCAs.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-db06708-with-local-changes
+ * @license Angular v20.1.0-next.0+sha-c8dfe6d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32468,7 +32468,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-db06708-with-local-changes');
+new Version('20.1.0-next.0+sha-c8dfe6d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/imports-CIX-JgAN.js
+++ schematics/bundles/imports-CIX-JgAN.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-db06708-with-local-changes
+ * @license Angular v20.1.0-next.0+sha-c8dfe6d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
