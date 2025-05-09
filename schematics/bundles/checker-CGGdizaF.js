8e8cf17d14c45f4cd70c0ee140204609e4ba67e1
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:50:34 +0000

    f0dd8f8 build: migrate upgrade package to use ts_project (#61245)

--- schematics/bundles/checker-CGGdizaF.js
+++ schematics/bundles/checker-CGGdizaF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.10+sha-84b66f3
+ * @license Angular v19.2.10+sha-f0dd8f8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.10+sha-84b66f3');
+new Version('19.2.10+sha-f0dd8f8');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
