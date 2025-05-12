99975896b840782747c018904aee93429325866e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-12 22:41:10 +0000

    7be6e5d refactor(migrations): remove unused code (#61260)

--- schematics/bundles/checker-CGGdizaF.js
+++ schematics/bundles/checker-CGGdizaF.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.10+sha-223ca9f
+ * @license Angular v19.2.10+sha-7be6e5d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.10+sha-223ca9f');
+new Version('19.2.10+sha-7be6e5d');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/property_name-BBwFuqMe.js
+++ schematics/bundles/property_name-BBwFuqMe.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.10+sha-223ca9f
+ * @license Angular v19.2.10+sha-7be6e5d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
