0da905c6a8a25301d3abadcbccdb82a81727a45e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-13 23:41:12 +0000

    c92c34d docs: update references to the cdk/testing apis (#60853)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-899cb4a
+ * @license Angular v19.2.10+sha-c92c34d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17950,7 +17950,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.10+sha-899cb4a']
+                ? ['ng-version', '19.2.10+sha-c92c34d']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34689,7 +34689,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-899cb4a');
+const VERSION = new Version('19.2.10+sha-c92c34d');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
