9206f2988e719505efc4253961a120f7fb5f9e9d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-14 08:23:00 +0000

    4058f8d refactor(core): remove leftover todos in integration tests (#61313)

--- fesm2022/debug_node-B_DFT2We.mjs
+++ fesm2022/debug_node-B_DFT2We.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-c34efcc
+ * @license Angular v20.1.0-next.0+sha-4058f8d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14543,7 +14543,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-next.0+sha-c34efcc']
+        ? ['ng-version', '20.1.0-next.0+sha-4058f8d']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/symbol-VPWguRxr.js
+++ schematics/bundles/symbol-VPWguRxr.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-c34efcc
+ * @license Angular v20.1.0-next.0+sha-4058f8d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
