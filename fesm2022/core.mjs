8ec561666dfbd73b01d464a2ebe76a18f0bbecfd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 16:41:46 +0000

    d835a44 build: migrate upgrade to use ng_project instead of ng_module (#61320)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-a07413a
+ * @license Angular v20.1.0-next.0+sha-d835a44
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-a07413a');
+const VERSION = new Version('20.1.0-next.0+sha-d835a44');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node-FPqH5owt.mjs
+++ fesm2022/debug_node-FPqH5owt.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-a07413a
+ * @license Angular v20.1.0-next.0+sha-d835a44
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14543,7 +14543,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-next.0+sha-a07413a']
+        ? ['ng-version', '20.1.0-next.0+sha-d835a44']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
