268f8846758f4cf22614d174c51c6fa6e2e7e9ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 16:04:22 +0000

    f7bfdc7 refactor(router): add return types to exported functions (#61310)

--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.0+sha-d7dbabe
+ * @license Angular v20.0.0-rc.0+sha-f7bfdc7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.0+sha-d7dbabe');
+new Version('20.0.0-rc.0+sha-f7bfdc7');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
