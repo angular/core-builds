f8e0282c9fd3dcedfb8550648382bb56aa49deeb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 13:42:42 +0000

    c028918 docs: document the use of `this` in template expression syntax (#61250)

--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.0+sha-48dc810
+ * @license Angular v20.0.0-rc.0+sha-c028918
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.0+sha-48dc810');
+new Version('20.0.0-rc.0+sha-c028918');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
