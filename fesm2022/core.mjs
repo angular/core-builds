01c035ccc713f8bb0efc53d477d372ed0c76561e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 15:54:42 +0000

    75e267d docs(docs-infra): fix update page styles to adhere to the new layout (#61256)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-3047638
+ * @license Angular v19.2.10+sha-75e267d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17950,7 +17950,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.10+sha-3047638']
+                ? ['ng-version', '19.2.10+sha-75e267d']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34690,7 +34690,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-3047638');
+const VERSION = new Version('19.2.10+sha-75e267d');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/nodes-B16H9JUd.js
+++ schematics/bundles/nodes-B16H9JUd.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.10+sha-3047638
+ * @license Angular v19.2.10+sha-75e267d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
