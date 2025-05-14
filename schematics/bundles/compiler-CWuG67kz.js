a2b7cd5f020033c0c3bbc8775a0dba71485607cd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-14 11:32:05 +0000

    48dc810 build: update dependency @rollup/plugin-node-resolve to v16 (#61327)

--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.0+sha-d7a1c19
+ * @license Angular v20.0.0-rc.0+sha-48dc810
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.0+sha-d7a1c19');
+new Version('20.0.0-rc.0+sha-48dc810');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/migrate_ts_type_references-DAudyiEk.js
+++ schematics/bundles/migrate_ts_type_references-DAudyiEk.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.0+sha-d7a1c19
+ * @license Angular v20.0.0-rc.0+sha-48dc810
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
