ac07381856b56d74e66c46817df26c1f265900f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 17:23:46 +0000

    ca6295e fix(core): handle different DI token types in Chrome DevTools integration (#61333)

--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-bc2cab7
+ * @license Angular v20.0.0-rc.1+sha-ca6295e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-bc2cab7');
+new Version('20.0.0-rc.1+sha-ca6295e');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
