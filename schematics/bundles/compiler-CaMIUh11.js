8c36762d48897c656f2e62229ebd23cd43e9ae67
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:00:59 +0000

    8e2ca25 fix(core): Testing should not throw when Zone does not patch test FW APIs (#61376)

--- schematics/bundles/compiler-CaMIUh11.js
+++ schematics/bundles/compiler-CaMIUh11.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-1ffe37d
+ * @license Angular v20.1.0-next.0+sha-8e2ca25
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32521,7 +32521,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-1ffe37d');
+new Version('20.1.0-next.0+sha-8e2ca25');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/control-flow-migration.js
+++ schematics/bundles/control-flow-migration.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-1ffe37d
+ * @license Angular v20.1.0-next.0+sha-8e2ca25
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
