fb2712506ee9870f0d85ef99b00a02db7668dc58
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 10:16:46 +0000

    f0e961c docs: fix note formatting about optionam chaining (#61251)

--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-095fa2f
+ * @license Angular v20.0.0-rc.1+sha-f0e961c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-095fa2f');
+new Version('20.0.0-rc.1+sha-f0e961c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
