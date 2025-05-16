ce26b741dafca8a86471a62350105de8bbe931e9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 14:05:43 +0000

    494d32a build: fix golden approval script (#61407)

--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-e13d6a3
+ * @license Angular v20.0.0-rc.1+sha-494d32a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-e13d6a3');
+new Version('20.0.0-rc.1+sha-494d32a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
