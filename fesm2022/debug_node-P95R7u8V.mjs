20ee3ff1ec9792ff24549f420e6c0ce1c73c16b4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:39:23 +0000

    337c7b8 ci: update step name in workflow (#61393)

--- fesm2022/debug_node-P95R7u8V.mjs
+++ fesm2022/debug_node-P95R7u8V.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-648d42f
+ * @license Angular v20.0.0-rc.1+sha-337c7b8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14584,7 +14584,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.1+sha-648d42f']
+        ? ['ng-version', '20.0.0-rc.1+sha-337c7b8']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
