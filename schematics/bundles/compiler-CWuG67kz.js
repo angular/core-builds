1cb8b00d7cc265be306f99ef65da313df4e528db
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 10:27:00 +0000

    a90c720 docs: fix typos (#61257)

--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-f0e961c
+ * @license Angular v20.0.0-rc.1+sha-a90c720
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-f0e961c');
+new Version('20.0.0-rc.1+sha-a90c720');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
