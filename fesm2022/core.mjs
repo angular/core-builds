776bcdc97103f774e6ad83b143f621161dee115d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 14:06:54 +0000

    fd29dab build: fix golden approval script (#61407)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-b2ade23
+ * @license Angular v19.2.11+sha-fd29dab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18000,7 +18000,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.11+sha-b2ade23']
+                ? ['ng-version', '19.2.11+sha-fd29dab']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34731,7 +34731,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-b2ade23');
+const VERSION = new Version('19.2.11+sha-fd29dab');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-BKhzGn-3.js
+++ schematics/bundles/checker-BKhzGn-3.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.11+sha-b2ade23
+ * @license Angular v19.2.11+sha-fd29dab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.11+sha-b2ade23');
+new Version('19.2.11+sha-fd29dab');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
