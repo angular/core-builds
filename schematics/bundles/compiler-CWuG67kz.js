8c535c7465f0f66fe860bc5c7e364371a4f8973b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:13:08 +0000

    6d8e57d perf(platform-server): speed up resolution of base (#61392)

--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-337c7b8
+ * @license Angular v20.0.0-rc.1+sha-6d8e57d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-337c7b8');
+new Version('20.0.0-rc.1+sha-6d8e57d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signals.js
+++ schematics/bundles/signals.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-337c7b8
+ * @license Angular v20.0.0-rc.1+sha-6d8e57d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
