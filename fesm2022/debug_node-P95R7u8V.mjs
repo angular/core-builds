1db18ee6a7c13fa412b393e7c4e3735cded1408c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 07:49:17 +0000

    15dcb2b ci: use current branch as base for pull request (#61390)

--- fesm2022/debug_node-P95R7u8V.mjs
+++ fesm2022/debug_node-P95R7u8V.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-7034b74
+ * @license Angular v20.0.0-rc.1+sha-15dcb2b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14584,7 +14584,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.1+sha-7034b74']
+        ? ['ng-version', '20.0.0-rc.1+sha-15dcb2b']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/compiler-CWuG67kz.js
+++ schematics/bundles/compiler-CWuG67kz.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-7034b74
+ * @license Angular v20.0.0-rc.1+sha-15dcb2b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-7034b74');
+new Version('20.0.0-rc.1+sha-15dcb2b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
