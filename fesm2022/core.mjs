c9889655f1fb0c00d34127eaf37de2165dfeb567
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 15:16:18 +0000

    74e39ea docs(docs-infra): llms-full.txt (#61356)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-fd29dab
+ * @license Angular v19.2.11+sha-74e39ea
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18000,7 +18000,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.11+sha-fd29dab']
+                ? ['ng-version', '19.2.11+sha-74e39ea']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34731,7 +34731,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-fd29dab');
+const VERSION = new Version('19.2.11+sha-74e39ea');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/provide-initializer.js
+++ schematics/bundles/provide-initializer.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.11+sha-fd29dab
+ * @license Angular v19.2.11+sha-74e39ea
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
