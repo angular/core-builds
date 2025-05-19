1dce2efb82da5622f7dd100ed6c6993135a6f0ed
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 10:00:34 +0000

    24ae05f build: update all non-major dependencies (#61458)

--- schematics/bundles/compiler-CE_UC0KP.cjs
+++ schematics/bundles/compiler-CE_UC0KP.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-7a308cc
+ * @license Angular v20.1.0-next.0+sha-24ae05f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32525,7 +32525,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-7a308cc');
+new Version('20.1.0-next.0+sha-24ae05f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
