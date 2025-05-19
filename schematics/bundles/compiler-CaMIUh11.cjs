83a3b470e2876994adee4339c18c46de2bd25b2a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 08:15:05 +0000

    41be5ee build: update dependency google-closure-compiler to v20250514 (#61439)

--- schematics/bundles/compiler-CaMIUh11.cjs
+++ schematics/bundles/compiler-CaMIUh11.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-13e3966
+ * @license Angular v20.1.0-next.0+sha-41be5ee
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32521,7 +32521,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-13e3966');
+new Version('20.1.0-next.0+sha-41be5ee');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
