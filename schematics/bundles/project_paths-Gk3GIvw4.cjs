b7b0683cdd4fbbc808e9253add8412475fead597
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 12:42:56 +0000

    76100ec build: `--config=debug` should not disable sandbox for all actions (#61468)

--- schematics/bundles/project_paths-Gk3GIvw4.cjs
+++ schematics/bundles/project_paths-Gk3GIvw4.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-a30fc96
+ * @license Angular v20.0.0-rc.1+sha-76100ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
