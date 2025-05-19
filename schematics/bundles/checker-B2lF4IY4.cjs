85370ac5dc6d981a9a55077209c7e3551a32f413
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 12:43:21 +0000

    0a25aba build: `--config=debug` should not disable sandbox for all actions (#61468)

--- schematics/bundles/checker-B2lF4IY4.cjs
+++ schematics/bundles/checker-B2lF4IY4.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.11+sha-bd2b454
+ * @license Angular v19.2.11+sha-0a25aba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.11+sha-bd2b454');
+new Version('19.2.11+sha-0a25aba');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
