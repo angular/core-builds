799a6800e9df752a94ea02fc158ced1e686965f7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 14:33:39 +0000

    43588f3 docs(docs-infra): fix substitution regex (#61515)

--- schematics/bundles/signals.cjs
+++ schematics/bundles/signals.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-30e0812
+ * @license Angular v20.0.0-rc.1+sha-43588f3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
