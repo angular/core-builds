6dfbd96f76c0354cbacc03d4b5f14da5b4224738
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:47:34 +0000

    f6b4f72 Revert "test: replace `platform-browser-dynamic` with  `platform-browser` (#61495)" (#61510)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-4430423
+ * @license Angular v19.2.11+sha-f6b4f72
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18010,7 +18010,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.11+sha-4430423']
+                ? ['ng-version', '19.2.11+sha-f6b4f72']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34731,7 +34731,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-4430423');
+const VERSION = new Version('19.2.11+sha-f6b4f72');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
