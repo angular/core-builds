e03c7fd94cbd174fdd4efae025077a3b4194d3af
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 11:14:52 +0000

    f0ce891 build: update cross-repo angular dependencies (#61504)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-f702f0e
+ * @license Angular v20.0.0-rc.1+sha-f0ce891
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-f702f0e');
+const VERSION = new Version('20.0.0-rc.1+sha-f0ce891');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/compiler-CWuG67kz.cjs
+++ schematics/bundles/compiler-CWuG67kz.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-f702f0e
+ * @license Angular v20.0.0-rc.1+sha-f0ce891
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-f702f0e');
+new Version('20.0.0-rc.1+sha-f0ce891');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
