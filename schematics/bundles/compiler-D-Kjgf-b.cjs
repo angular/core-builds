f97e0411bdd2746f6350db726802646306cf0ba3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 17:52:12 +0000

    bb220c0 build: update cross-repo angular dependencies (#61530)

--- schematics/bundles/compiler-D-Kjgf-b.cjs
+++ schematics/bundles/compiler-D-Kjgf-b.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-f1f3789
+ * @license Angular v20.1.0-next.0+sha-bb220c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32530,7 +32530,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-f1f3789');
+new Version('20.1.0-next.0+sha-bb220c0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
