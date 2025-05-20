62875db2df3e6421da3ba56df00ff88342de5267
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 10:08:01 +0000

    de7d8be test: replace `platform-browser-dynamic` with  `platform-browser` (#61495)

--- fesm2022/debug_node-DRwxodBq.mjs
+++ fesm2022/debug_node-DRwxodBq.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-82b51c6
+ * @license Angular v20.0.0-rc.1+sha-de7d8be
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14584,7 +14584,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.1+sha-82b51c6']
+        ? ['ng-version', '20.0.0-rc.1+sha-de7d8be']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
