afdada022ec73f8fb0195d11fc34c39d1304931b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:14:21 +0000

    2796f18 test: set the `noCheck` TS compiler option in declaration-only emission compliance tests (#61470)

--- schematics/bundles/compiler-D-Kjgf-b.cjs
+++ schematics/bundles/compiler-D-Kjgf-b.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-2e7032e
+ * @license Angular v20.1.0-next.0+sha-2796f18
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32530,7 +32530,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-2e7032e');
+new Version('20.1.0-next.0+sha-2796f18');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
