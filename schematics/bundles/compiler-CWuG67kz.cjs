7debef7cb99f99b78c7e2b75e7dbb7efaa89d530
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 10:28:52 +0000

    f702f0e build: update dependency conventional-changelog to v7 (#61491)

--- schematics/bundles/compiler-CWuG67kz.cjs
+++ schematics/bundles/compiler-CWuG67kz.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-b55fa4e
+ * @license Angular v20.0.0-rc.1+sha-f702f0e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32405,7 +32405,7 @@ var FactoryTarget;
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-b55fa4e');
+new Version('20.0.0-rc.1+sha-f702f0e');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
