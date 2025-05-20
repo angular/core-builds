fdfdbbf268576c1dc28c136c8c8c324aba610c34
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 10:14:58 +0000

    b55fa4e build: update dependency google-closure-compiler to v20250518 (#61492)

--- fesm2022/debug_node-DRwxodBq.mjs
+++ fesm2022/debug_node-DRwxodBq.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-de7d8be
+ * @license Angular v20.0.0-rc.1+sha-b55fa4e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14584,7 +14584,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.1+sha-de7d8be']
+        ? ['ng-version', '20.0.0-rc.1+sha-b55fa4e']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
