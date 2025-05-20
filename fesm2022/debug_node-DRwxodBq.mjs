ddfbc288f7ebcfa3aed1ebde4ac85de4981c0bb4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 14:24:26 +0000

    30e0812 fix(core): update min Node.js support to 20.19, 22.12, and 24.0 (#61500)

--- fesm2022/debug_node-DRwxodBq.mjs
+++ fesm2022/debug_node-DRwxodBq.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-6218b38
+ * @license Angular v20.0.0-rc.1+sha-30e0812
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14584,7 +14584,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.1+sha-6218b38']
+        ? ['ng-version', '20.0.0-rc.1+sha-30e0812']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/compiler-CqGwk5kK.cjs
+++ schematics/bundles/compiler-CqGwk5kK.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-6218b38
+ * @license Angular v20.0.0-rc.1+sha-30e0812
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32410,7 +32410,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-6218b38');
+new Version('20.0.0-rc.1+sha-30e0812');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
