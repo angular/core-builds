2770fd86e1b4a762fcefc21eff81767e432b2f69
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 17:51:58 +0000

    7a44727 build: update cross-repo angular dependencies (#61531)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-272ad04
+ * @license Angular v20.0.0-rc.1+sha-7a44727
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-272ad04');
+const VERSION = new Version('20.0.0-rc.1+sha-7a44727');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
