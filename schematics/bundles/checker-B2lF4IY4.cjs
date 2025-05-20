494067cd3bcd9983e41bb5a4964c2446214e6924
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 14:32:47 +0000

    af19705 docs(docs-infra): fix substitution regex (#61344)

--- schematics/bundles/checker-B2lF4IY4.cjs
+++ schematics/bundles/checker-B2lF4IY4.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.11+sha-b641107
+ * @license Angular v19.2.11+sha-af19705
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30726,7 +30726,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.11+sha-b641107');
+new Version('19.2.11+sha-af19705');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
--- schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
+++ schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.11+sha-b641107
+ * @license Angular v19.2.11+sha-af19705
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
