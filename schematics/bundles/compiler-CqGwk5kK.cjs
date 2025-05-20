3eaf7d840edf2917b59f1066888801a5ffca1327
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 15:17:25 +0000

    966df84 fix(common): prevent reading chunks if app is destroyed (#61354)

--- schematics/bundles/compiler-CqGwk5kK.cjs
+++ schematics/bundles/compiler-CqGwk5kK.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.1+sha-5dbeb42
+ * @license Angular v20.0.0-rc.1+sha-966df84
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32410,7 +32410,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0-rc.1+sha-5dbeb42');
+new Version('20.0.0-rc.1+sha-966df84');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
