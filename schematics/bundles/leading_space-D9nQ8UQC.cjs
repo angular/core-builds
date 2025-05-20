0606e7ef6e850241326c6498f63e261fa81ed38e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:35:56 +0000

    a62d3e7 build: update dependency @octokit/graphql to v9 (#61503)

--- schematics/bundles/leading_space-D9nQ8UQC.cjs
+++ schematics/bundles/leading_space-D9nQ8UQC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-2796f18
+ * @license Angular v20.1.0-next.0+sha-a62d3e7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
