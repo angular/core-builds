0fac4bd2f8320955c148d0f37dcf8303a31074ee
Andrew Scott <atscott01@gmail.com>
2025-05-21 20:00:27 +0000

    833c615 docs: release notes for the v19.2.12 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-905194f
+ * @license Angular v20.1.0-next.0+sha-833c615
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-905194f');
+const VERSION = new Version('20.1.0-next.0+sha-833c615');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/compiler-CwIAJJiH.cjs
+++ schematics/bundles/compiler-CwIAJJiH.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-905194f
+ * @license Angular v20.1.0-next.0+sha-833c615
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32532,7 +32532,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-905194f');
+new Version('20.1.0-next.0+sha-833c615');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
