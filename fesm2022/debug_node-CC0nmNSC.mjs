46150f4381675d507d8a8bce7e57c255246eae97
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 13:49:55 +0000

    545bd3e docs: fix capitalization error (#61554) PR Close #61554

--- fesm2022/debug_node-CC0nmNSC.mjs
+++ fesm2022/debug_node-CC0nmNSC.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-47f4a8e
+ * @license Angular v20.0.0-rc.1+sha-545bd3e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14606,7 +14606,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0-rc.1+sha-47f4a8e']
+        ? ['ng-version', '20.0.0-rc.1+sha-545bd3e']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
