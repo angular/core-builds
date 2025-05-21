9acb99b99efe4d7654ffefe2f5a7c927686fb3ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:13:37 +0000

    3a8bbe9 build: migrate language service to use rules_js (#61578)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-fc2483e
+ * @license Angular v19.2.11+sha-3a8bbe9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18010,7 +18010,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.11+sha-fc2483e']
+                ? ['ng-version', '19.2.11+sha-3a8bbe9']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34752,7 +34752,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-fc2483e');
+const VERSION = new Version('19.2.11+sha-3a8bbe9');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/apply_import_manager-Celp9ClC.cjs
+++ schematics/bundles/apply_import_manager-Celp9ClC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.11+sha-fc2483e
+ * @license Angular v19.2.11+sha-3a8bbe9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
