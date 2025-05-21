281bc05d4b096b05c359b8df39c1ccb33c6ddbe6
Andrew Scott <atscott@google.com>
2025-05-21 22:38:03 +0000

    6b7c8d1 build: update cross-repo angular dependencies to v20.0.0-rc.2 (#61588)

--- schematics/bundles/checker-DmucNdBi.cjs
+++ schematics/bundles/checker-DmucNdBi.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0-rc.2+sha-09f79e0
+ * @license Angular v20.0.0-rc.2+sha-6b7c8d1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
