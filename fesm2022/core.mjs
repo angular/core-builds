dc2b7a7959811210afd6bc784bb192f65119d670
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 09:21:32 +0000

    1007079 build: update compiler-cli to not be stamped when used for the compiler in ng_project (#61539)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-1071802
+ * @license Angular v19.2.11+sha-1007079
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18010,7 +18010,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.11+sha-1071802']
+                ? ['ng-version', '19.2.11+sha-1007079']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34731,7 +34731,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-1071802');
+const VERSION = new Version('19.2.11+sha-1007079');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
