ae8106a90bf22e474aaccf339625d495eb9f3c96
Andrew Scott <atscott@google.com>
2025-05-22 20:52:16 +0000

    d4d4be8 build: migrate localize package to use rules_js (#61615)

--- schematics/bundles/checker-5pyJrZ9G.cjs
+++ schematics/bundles/checker-5pyJrZ9G.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.12+sha-2ae69f7
+ * @license Angular v19.2.12+sha-d4d4be8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30744,7 +30744,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.12+sha-2ae69f7');
+new Version('19.2.12+sha-d4d4be8');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
