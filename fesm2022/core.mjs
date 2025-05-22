c59d1e685d33b50e338add307e76505e0d0755c4
Andrew Scott <atscott@google.com>
2025-05-22 17:59:07 +0000

    114253e release: bump Angular DevTools to version 1.0.32 (#61620)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-959258d
+ * @license Angular v20.0.0-rc.2+sha-114253e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-959258d');
+const VERSION = new Version('20.0.0-rc.2+sha-114253e');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
