5a3b5cc37cdcb3f3c35f6167cbf45eba6464c3ee
Andrew Scott <atscott@google.com>
2025-05-22 17:59:39 +0000

    a28cadc release: bump Angular DevTools to version 1.0.32 (#61620)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-ce916fe
+ * @license Angular v20.1.0-next.0+sha-a28cadc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-ce916fe');
+const VERSION = new Version('20.1.0-next.0+sha-a28cadc');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/signal-queries-migration.cjs
+++ schematics/bundles/signal-queries-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-ce916fe
+ * @license Angular v20.1.0-next.0+sha-a28cadc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
