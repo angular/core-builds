f5512c73f694ed3bfebf3bc559a85b25cbebfbe5
Andrew Scott <atscott@google.com>
2025-05-22 17:09:13 +0000

    b15bddf fix(service-worker): do not register service worker if app is destroyed before it is ready to register (#61101)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-5f1bd07
+ * @license Angular v19.2.12+sha-b15bddf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18010,7 +18010,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.12+sha-5f1bd07']
+                ? ['ng-version', '19.2.12+sha-b15bddf']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34752,7 +34752,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-5f1bd07');
+const VERSION = new Version('19.2.12+sha-b15bddf');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
