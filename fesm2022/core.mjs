401539f842886df59ccc72118487acd4d5f42515
Andrew Scott <atscott@google.com>
2025-05-22 20:47:09 +0000

    1ea39e0 refactor(zone.js): Add a `withProxyZone` helper that might be used for unpatched test frameworks (#61626)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-768e715
+ * @license Angular v20.1.0-next.0+sha-1ea39e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-768e715');
+const VERSION = new Version('20.1.0-next.0+sha-1ea39e0');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/compiler-CwIAJJiH.cjs
+++ schematics/bundles/compiler-CwIAJJiH.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-768e715
+ * @license Angular v20.1.0-next.0+sha-1ea39e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32532,7 +32532,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-768e715');
+new Version('20.1.0-next.0+sha-1ea39e0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
