1275137a801a2ecc234d84437a2e18eeeb44645d
Andrew Scott <atscott@google.com>
2025-05-22 03:49:38 +0000

    ad8931c fix(zone.js): classes that extend Error should retain cause property (#61599)

--- schematics/bundles/compiler-CwIAJJiH.cjs
+++ schematics/bundles/compiler-CwIAJJiH.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-5735ea7
+ * @license Angular v20.1.0-next.0+sha-ad8931c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32532,7 +32532,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-5735ea7');
+new Version('20.1.0-next.0+sha-ad8931c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
