e9f1cc4d53f33f3da67ddf3cba05a0d25b8f3f31
Andrew Scott <atscott@google.com>
2025-05-23 17:12:27 +0000

    2c876b4 fix(common): avoid injecting ApplicationRef in FetchBackend (#61649)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-a6d5479
+ * @license Angular v19.2.12+sha-2c876b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18010,7 +18010,7 @@ class ComponentFactory extends ComponentFactory$1 {
             const cmpDef = this.componentDef;
             ngDevMode && verifyNotAnOrphanComponent(cmpDef);
             const tAttributes = rootSelectorOrNode
-                ? ['ng-version', '19.2.12+sha-a6d5479']
+                ? ['ng-version', '19.2.12+sha-2c876b4']
                 : // Extract attributes and classes from the first selector only to match VE behavior.
                     extractAttrsAndClassesFromSelector(this.componentDef.selectors[0]);
             // Create the root view. Uses empty TView and ContentTemplate.
@@ -34752,7 +34752,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-a6d5479');
+const VERSION = new Version('19.2.12+sha-2c876b4');
 
 /**
  * Combination of NgModuleFactory and ComponentFactories.
--- schematics/bundles/checker-5pyJrZ9G.cjs
+++ schematics/bundles/checker-5pyJrZ9G.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v19.2.12+sha-a6d5479
+ * @license Angular v19.2.12+sha-2c876b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -30744,7 +30744,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('19.2.12+sha-a6d5479');
+new Version('19.2.12+sha-2c876b4');
 
 const _I18N_ATTR = 'i18n';
 const _I18N_ATTR_PREFIX = 'i18n-';
