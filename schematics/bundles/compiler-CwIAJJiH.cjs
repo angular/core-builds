ecf5a272363929e6359ee72543c7a8e0f436cfa9
Andrew Scott <atscott@google.com>
2025-05-23 17:10:46 +0000

    abcb865 fix(common): avoid injecting ApplicationRef in FetchBackend (#61649)

--- schematics/bundles/compiler-CwIAJJiH.cjs
+++ schematics/bundles/compiler-CwIAJJiH.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-4356e85
+ * @license Angular v20.1.0-next.0+sha-abcb865
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32532,7 +32532,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-4356e85');
+new Version('20.1.0-next.0+sha-abcb865');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
