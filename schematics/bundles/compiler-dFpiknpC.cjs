75cc2339ee4591849c1925ed57e878c369e20e0c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 12:30:26 +0000

    c11527f refactor(compiler): remove unused code (#61668)

--- schematics/bundles/compiler-dFpiknpC.cjs
+++ schematics/bundles/compiler-dFpiknpC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-ca26b23
+ * @license Angular v20.1.0-next.0+sha-c11527f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32265,7 +32265,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-ca26b23');
+new Version('20.1.0-next.0+sha-c11527f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
