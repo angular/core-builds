536a65e7b80ec3f3e4928e7ca76145b5a4a5e4ba
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 11:20:50 +0000

    833c69b refactor(platform-browser): replace `isPlatformServer` with `ngServerMode` (#59496)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-3731cce
+ * @license Angular v20.0.0-rc.2+sha-833c69b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-3731cce');
+const VERSION = new Version('20.0.0-rc.2+sha-833c69b');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
