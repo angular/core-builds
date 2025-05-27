1cad5983c2fac30d2ac9092ca5755ea72fadd280
Andrew Scott <atscott@google.com>
2025-05-27 18:39:48 +0000

    c77e198 docs(docs-infra): limit file interactions on tutorials. (#61696)

--- schematics/bundles/compiler-dFpiknpC.cjs
+++ schematics/bundles/compiler-dFpiknpC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-c11527f
+ * @license Angular v20.1.0-next.0+sha-c77e198
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32265,7 +32265,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-c11527f');
+new Version('20.1.0-next.0+sha-c77e198');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
