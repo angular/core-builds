8ddd16c1832a939a4dcab2a25d6c440724b49bd6
GitHub <noreply@github.com>
2025-05-28 17:42:44 +0000

    bf7816e build: bump peer deps for angular-in-memory-web-api (#61730)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-2c8de90
+ * @license Angular v20.0.0+sha-bf7816e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-2c8de90');
+const VERSION = new Version('20.0.0+sha-bf7816e');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-BHgMyU8j.cjs
+++ schematics/bundles/checker-BHgMyU8j.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0+sha-2c8de90
+ * @license Angular v20.0.0+sha-bf7816e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
