155236a2bec92d07b25f556e864717ab14f35733
Andrew Scott <atscott01@gmail.com>
2025-05-28 13:46:02 +0000

    1fd59c0 docs: release notes for the v19.2.14 release

--- schematics/bundles/compiler-dFpiknpC.cjs
+++ schematics/bundles/compiler-dFpiknpC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-d19669c
+ * @license Angular v20.1.0-next.0+sha-1fd59c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32265,7 +32265,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-d19669c');
+new Version('20.1.0-next.0+sha-1fd59c0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
