9aaf9f0e876605bb66abe9f9a4c0377ad39f10ee
Andrew Scott <atscott@google.com>
2025-05-28 17:35:59 +0000

    d5ac380 build: bump peer deps for angular-in-memory-web-api (#61729)

--- schematics/bundles/compiler-dFpiknpC.cjs
+++ schematics/bundles/compiler-dFpiknpC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-8cf7e23
+ * @license Angular v20.1.0-next.0+sha-d5ac380
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32265,7 +32265,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-8cf7e23');
+new Version('20.1.0-next.0+sha-d5ac380');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
