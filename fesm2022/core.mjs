a304003fef4d24792c95b7d31af3aef727a2dc60
Andrew Scott <atscott@google.com>
2025-05-28 18:17:50 +0000

    2a290ab docs: update versions.json with v20 (#61732)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-449c540
+ * @license Angular v20.1.0-next.0+sha-2a290ab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-449c540');
+const VERSION = new Version('20.1.0-next.0+sha-2a290ab');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
