c5b092a1cb84d95a164768a90a75a7c2e3696c1d
Andrew Scott <atscott@google.com>
2025-05-28 17:59:26 +0000

    1035240 release: bump version of in memory web api (#61731)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-bf7816e
+ * @license Angular v20.0.0+sha-1035240
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-bf7816e');
+const VERSION = new Version('20.0.0+sha-1035240');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/compiler-Dl11rH6-.cjs
+++ schematics/bundles/compiler-Dl11rH6-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0+sha-bf7816e
+ * @license Angular v20.0.0+sha-1035240
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32145,7 +32145,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0+sha-bf7816e');
+new Version('20.0.0+sha-1035240');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
