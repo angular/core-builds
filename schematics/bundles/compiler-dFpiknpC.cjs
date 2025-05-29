afe57a795c21953eaacf337b1cc6c49490c94970
kirjs <kirts@google.com>
2025-05-29 22:27:25 +0000

    5f187b2 docs: fix copy-paste error on defer page in interaction section (#61751) PR Close #61751

--- schematics/bundles/compiler-dFpiknpC.cjs
+++ schematics/bundles/compiler-dFpiknpC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-8fa75fe
+ * @license Angular v20.1.0-next.0+sha-5f187b2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32265,7 +32265,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-8fa75fe');
+new Version('20.1.0-next.0+sha-5f187b2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/index-DygRb0iL.cjs
+++ schematics/bundles/index-DygRb0iL.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-8fa75fe
+ * @license Angular v20.1.0-next.0+sha-5f187b2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
