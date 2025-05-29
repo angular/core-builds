617232bf70a8ce266753e0e9d10ca467af0b2290
Andrew Scott <atscott@google.com>
2025-05-29 00:51:13 +0000

    04b6881 docs: setup redirect for zoneless (#61737)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-9b514b7
+ * @license Angular v20.1.0-next.0+sha-04b6881
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-9b514b7');
+const VERSION = new Version('20.1.0-next.0+sha-04b6881');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node-D2t41i4O.mjs
+++ fesm2022/debug_node-D2t41i4O.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-9b514b7
+ * @license Angular v20.1.0-next.0+sha-04b6881
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13452,7 +13452,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-next.0+sha-9b514b7']
+        ? ['ng-version', '20.1.0-next.0+sha-04b6881']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
