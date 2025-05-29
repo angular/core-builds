e997dfbe34cda12726e68dd3dc6a93218ac580af
Andrew Scott <atscott@google.com>
2025-05-29 00:51:18 +0000

    f7d33ee docs: setup redirect for zoneless (#61737)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-a0d599e
+ * @license Angular v20.0.0+sha-f7d33ee
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-a0d599e');
+const VERSION = new Version('20.0.0+sha-f7d33ee');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/compiler-Dl11rH6-.cjs
+++ schematics/bundles/compiler-Dl11rH6-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0+sha-a0d599e
+ * @license Angular v20.0.0+sha-f7d33ee
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32145,7 +32145,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0+sha-a0d599e');
+new Version('20.0.0+sha-f7d33ee');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
