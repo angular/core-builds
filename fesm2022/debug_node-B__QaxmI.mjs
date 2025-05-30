26551c4483d3b48c49de6a658c5bb078f5215d5e
kirjs <kirts@google.com>
2025-05-30 14:02:46 +0000

    b561a50 refactor: expand DevTools reviewer list (#61709)

--- fesm2022/debug_node-B__QaxmI.mjs
+++ fesm2022/debug_node-B__QaxmI.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-e538862
+ * @license Angular v20.0.0+sha-b561a50
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13386,7 +13386,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0+sha-e538862']
+        ? ['ng-version', '20.0.0+sha-b561a50']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/compiler-Dl11rH6-.cjs
+++ schematics/bundles/compiler-Dl11rH6-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0+sha-e538862
+ * @license Angular v20.0.0+sha-b561a50
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32145,7 +32145,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0+sha-e538862');
+new Version('20.0.0+sha-b561a50');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
