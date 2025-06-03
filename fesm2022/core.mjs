843d981dc5a77e96c2c7b3a8aa2155bf1141f7eb
kirjs <kirts@google.com>
2025-06-03 18:06:08 +0000

    939ca07 docs(docs-infra): fix tutorial tooltip (#61782)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-9a24a2b
+ * @license Angular v20.0.0+sha-939ca07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-9a24a2b');
+const VERSION = new Version('20.0.0+sha-939ca07');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
