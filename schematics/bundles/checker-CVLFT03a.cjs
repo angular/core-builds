cccc7846e31eccddefc88ecf2dff82683afe2882
kirjs <kirts@google.com>
2025-06-03 15:04:35 +0000

    c663277 fix(service-worker): update service worker to handle seeking better for videos (#60029)

--- schematics/bundles/checker-CVLFT03a.cjs
+++ schematics/bundles/checker-CVLFT03a.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-4fa1dca
+ * @license Angular v20.1.0-next.0+sha-c663277
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32288,7 +32288,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-4fa1dca');
+new Version('20.1.0-next.0+sha-c663277');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
