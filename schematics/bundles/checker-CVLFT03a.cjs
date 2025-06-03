b5e0d59c1abbe928c95b31e81fbde608be7274ae
kirjs <kirts@google.com>
2025-06-03 11:41:23 +0000

    667a600 build: update dependency yargs to v18 (#61767)

--- schematics/bundles/checker-CVLFT03a.cjs
+++ schematics/bundles/checker-CVLFT03a.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-9630d79
+ * @license Angular v20.1.0-next.0+sha-667a600
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32288,7 +32288,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-9630d79');
+new Version('20.1.0-next.0+sha-667a600');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
