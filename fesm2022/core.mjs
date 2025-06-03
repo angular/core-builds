1aaa783d79cfdbb0de4914a5b74a43a5f2697c3b
kirjs <kirts@google.com>
2025-06-03 20:04:02 +0000

    6adf022 fix(service-worker): prevent duplicate fetches during concurrent update checks (#61443)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-c67dbda
+ * @license Angular v20.1.0-next.0+sha-6adf022
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-c67dbda');
+const VERSION = new Version('20.1.0-next.0+sha-6adf022');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/imports-CIX-JgAN.cjs
+++ schematics/bundles/imports-CIX-JgAN.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-c67dbda
+ * @license Angular v20.1.0-next.0+sha-6adf022
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
