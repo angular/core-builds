be071ba7f1945196a67bade033e57c3bc9cea393
kirjs <kirts@google.com>
2025-06-03 11:40:26 +0000

    30ea478 build: update cross-repo angular dependencies (#61704)

--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0+sha-0ea831c
+ * @license Angular v20.0.0+sha-30ea478
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0+sha-0ea831c');
+new Version('20.0.0+sha-30ea478');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/ng_decorators-B5HCqr20.cjs
+++ schematics/bundles/ng_decorators-B5HCqr20.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0+sha-0ea831c
+ * @license Angular v20.0.0+sha-30ea478
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
