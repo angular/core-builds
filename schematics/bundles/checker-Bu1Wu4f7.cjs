074ccba91aab374a11371fb5e78931be6a3a82b8
kirjs <kirts@google.com>
2025-06-03 20:04:40 +0000

    b93fa22 fix(service-worker): prevent duplicate fetches during concurrent update checks (#61443)

--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0+sha-939ca07
+ * @license Angular v20.0.0+sha-b93fa22
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0+sha-939ca07');
+new Version('20.0.0+sha-b93fa22');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
