ba45c55f826535046c9e0ff423d36158ac88f0f3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-06-04 06:38:30 +0000

    eb62a58 docs: open llms files in new tab (#61851)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-2bf7249
+ * @license Angular v20.0.0+sha-eb62a58
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-2bf7249');
+const VERSION = new Version('20.0.0+sha-eb62a58');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
