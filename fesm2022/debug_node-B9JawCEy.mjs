208fc57b8f5577f4d84c9686f1335121ac7ebba3
kirjs <kirts@google.com>
2025-06-04 18:23:06 +0000

    41b1039 refactor(service-worker): make `SwPush` and `SwUpdate` tree-shakable (#61670)

--- fesm2022/debug_node-B9JawCEy.mjs
+++ fesm2022/debug_node-B9JawCEy.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-bdf882c
+ * @license Angular v20.0.0+sha-41b1039
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13386,7 +13386,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.0+sha-bdf882c']
+        ? ['ng-version', '20.0.0+sha-41b1039']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.0+sha-bdf882c
+ * @license Angular v20.0.0+sha-41b1039
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.0+sha-bdf882c');
+new Version('20.0.0+sha-41b1039');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
