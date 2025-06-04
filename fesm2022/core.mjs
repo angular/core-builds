c64ff1d6a61ae9bd2fd29f5400144f4e2a51016c
kirjs <kirts@google.com>
2025-06-04 14:33:17 +0000

    3710b12 build: update cross-repo angular dependencies (#61855)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-5c31e7e
+ * @license Angular v20.0.0+sha-3710b12
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-5c31e7e');
+const VERSION = new Version('20.0.0+sha-3710b12');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
