6f5c833d76c4d40d6a1cfe4c47cc846cbf3af9b1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-06-04 06:38:56 +0000

    4178e82 docs: open llms files in new tab (#61851)

--- schematics/bundles/checker-BlxRNGK4.cjs
+++ schematics/bundles/checker-BlxRNGK4.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-3a9a70d
+ * @license Angular v20.1.0-next.0+sha-4178e82
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-3a9a70d');
+new Version('20.1.0-next.0+sha-4178e82');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
