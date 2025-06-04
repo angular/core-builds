bebb20d45c7380fc654b3e5bc8331f49083c0f03
kirjs <kirts@google.com>
2025-06-04 21:30:24 +0000

    49e273a build: update all non-major dependencies (#61701)

--- fesm2022/debug_node-B9JawCEy.mjs
+++ fesm2022/debug_node-B9JawCEy.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-66e9924
+ * @license Angular v20.0.1+sha-49e273a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13386,7 +13386,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.1+sha-66e9924']
+        ? ['ng-version', '20.0.1+sha-49e273a']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
