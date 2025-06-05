198abaf021499216dcd720f494b57f8fb70bbed2
Andrew Kushnir <akushnir@google.com>
2025-06-05 16:20:43 +0000

    9354efc build: remove unnecessary zone.js dep from various build targets (#61901)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-4f89f6e
+ * @license Angular v20.1.0-next.0+sha-9354efc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-4f89f6e');
+const VERSION = new Version('20.1.0-next.0+sha-9354efc');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
