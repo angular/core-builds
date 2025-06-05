7ebb72f81fdcf0e847cd63af3a3f835e1cc60ead
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 11:08:58 +0000

    935ce0e refactor(core): export signal setter and updater types for wiz (#61714)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-52aa336
+ * @license Angular v20.1.0-next.0+sha-935ce0e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-52aa336');
+const VERSION = new Version('20.1.0-next.0+sha-935ce0e');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
