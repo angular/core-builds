9cc235504f14c3c1d08052f1d797aa96e6243286
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 08:24:30 +0000

    f6841ea build: update cross-repo angular dependencies (#61881)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-50c6c70
+ * @license Angular v20.0.1+sha-f6841ea
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-50c6c70');
+const VERSION = new Version('20.0.1+sha-f6841ea');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
