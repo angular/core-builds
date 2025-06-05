2347cb16c4bf50352f24bd5b80aa81e653ee145b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 08:58:37 +0000

    59eb720 docs(docs-infra): use application builder for adev karma tests (#58291)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-f6841ea
+ * @license Angular v20.0.1+sha-59eb720
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-f6841ea');
+const VERSION = new Version('20.0.1+sha-59eb720');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
