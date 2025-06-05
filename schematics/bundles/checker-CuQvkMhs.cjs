0210fb6537b03958ef27cb60798ffac2460cd353
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 09:23:56 +0000

    e81ea0c fix(core): unregister `onDestroy` in `outputToObservable` (#61882)

--- schematics/bundles/checker-CuQvkMhs.cjs
+++ schematics/bundles/checker-CuQvkMhs.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-967c8fe
+ * @license Angular v20.1.0-next.0+sha-e81ea0c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-967c8fe');
+new Version('20.1.0-next.0+sha-e81ea0c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
