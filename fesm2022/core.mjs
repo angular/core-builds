6aa406942cfe9f2ed82a258e854f1421f0bcafd1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 09:33:45 +0000

    a6a193f docs: fix broken reveal answer button (#61925)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-9ae8027
+ * @license Angular v20.0.1+sha-a6a193f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-9ae8027');
+const VERSION = new Version('20.0.1+sha-a6a193f');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
