f6fe12bcaa7e4dd49e6070fa8433eeb7460bca7b
Andrew Kushnir <akushnir@google.com>
2025-06-06 00:53:36 +0000

    cfb0dc2 fix(docs-infra): Fix import name for house location (#61914)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-9354efc
+ * @license Angular v20.1.0-next.0+sha-cfb0dc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-9354efc');
+const VERSION = new Version('20.1.0-next.0+sha-cfb0dc2');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-CuQvkMhs.cjs
+++ schematics/bundles/checker-CuQvkMhs.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-9354efc
+ * @license Angular v20.1.0-next.0+sha-cfb0dc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-9354efc');
+new Version('20.1.0-next.0+sha-cfb0dc2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
