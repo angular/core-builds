4192b355ae2acd830f3adbecb1479524822f40a7
Andrew Kushnir <akushnir@google.com>
2025-06-06 14:55:32 +0000

    bb6ec10 Revert "refactor(service-worker): make `SwPush` and `SwUpdate` tree-shakable (#61670)" (#61923)

--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.1+sha-6ae07ad
+ * @license Angular v20.0.1+sha-bb6ec10
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.1+sha-6ae07ad');
+new Version('20.0.1+sha-bb6ec10');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/self-closing-tags-migration.cjs
+++ schematics/bundles/self-closing-tags-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.1+sha-6ae07ad
+ * @license Angular v20.0.1+sha-bb6ec10
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
