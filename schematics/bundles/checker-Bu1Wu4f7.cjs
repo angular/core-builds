402c9afeadfc6ab7f8763359b3ccfa59f115806e
Andrew Kushnir <akushnir@google.com>
2025-06-06 00:53:20 +0000

    33bd215 fix(docs-infra): Fix import name for house location (#61914)

--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.1+sha-8ccd496
+ * @license Angular v20.0.1+sha-33bd215
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.1+sha-8ccd496');
+new Version('20.0.1+sha-33bd215');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
