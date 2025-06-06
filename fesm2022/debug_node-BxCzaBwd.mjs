20da37b59f635995f34bfd6461ded962c34bff69
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 11:27:36 +0000

    0d198f2 docs: update first-app tutorial to signal inputs (#61876)

--- fesm2022/debug_node-BxCzaBwd.mjs
+++ fesm2022/debug_node-BxCzaBwd.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-a6a193f
+ * @license Angular v20.0.1+sha-0d198f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13396,7 +13396,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.1+sha-a6a193f']
+        ? ['ng-version', '20.0.1+sha-0d198f2']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.1+sha-a6a193f
+ * @license Angular v20.0.1+sha-0d198f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.1+sha-a6a193f');
+new Version('20.0.1+sha-0d198f2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
