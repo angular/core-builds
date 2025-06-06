c63a3a769e99a6809fbce93b10fb19e7e8caadbe
Andrew Kushnir <akushnir@google.com>
2025-06-06 19:49:43 +0000

    e557313 docs: Add error handling doc to the menu (#61945)

--- schematics/bundles/nodes-B16H9JUd.cjs
+++ schematics/bundles/nodes-B16H9JUd.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-bbd16f5
+ * @license Angular v20.1.0-next.0+sha-e557313
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
