06d5136798f8e2c3cc90291c8d61321c08bc8ceb
Andrew Kushnir <akushnir@google.com>
2025-06-09 19:04:21 +0000

    ee4acc2 build: update dependency google-closure-compiler to v20250603 (#61960)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-85e9505
+ * @license Angular v20.0.2+sha-ee4acc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-85e9505');
+const VERSION = new Version('20.0.2+sha-ee4acc2');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.2+sha-85e9505
+ * @license Angular v20.0.2+sha-ee4acc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.2+sha-85e9505');
+new Version('20.0.2+sha-ee4acc2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
