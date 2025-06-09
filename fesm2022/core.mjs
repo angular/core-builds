39c885c60321720d724b50aa9c5cbba5c438557a
Andrew Kushnir <akushnir@google.com>
2025-06-09 18:29:26 +0000

    3c216c1 fix(zone.js): waitForAsync should pass args to the test function (#61755)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-a3c6926
+ * @license Angular v20.1.0-next.0+sha-3c216c1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-a3c6926');
+const VERSION = new Version('20.1.0-next.0+sha-3c216c1');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
