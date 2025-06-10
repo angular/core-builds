7bbe61561755f087b64e55b99e38b8f8eafa491d
Andrew Kushnir <akushnir@google.com>
2025-06-10 21:50:53 +0000

    b9141da refactor(devtools): use import type to improve performance (#61966)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-4f0221e
+ * @license Angular v20.1.0-next.0+sha-b9141da
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-4f0221e');
+const VERSION = new Version('20.1.0-next.0+sha-b9141da');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/signal-input-migration.cjs
+++ schematics/bundles/signal-input-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-4f0221e
+ * @license Angular v20.1.0-next.0+sha-b9141da
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
