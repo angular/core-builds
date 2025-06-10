94fab38b03ef9a73168575c27010baaab65872b3
Andrew Kushnir <akushnir@google.com>
2025-06-10 23:01:35 +0000

    3852be6 ci: update to latest version of dev-infra actions (#61990)

--- schematics/bundles/document-core.cjs
+++ schematics/bundles/document-core.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-b9141da
+ * @license Angular v20.1.0-next.0+sha-3852be6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
