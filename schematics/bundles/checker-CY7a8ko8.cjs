6d86b72c0c834f30f6d856e8412fcee408a29449
Andrew Kushnir <akushnir@google.com>
2025-06-10 20:20:10 +0000

    4f0221e fix(forms): improve select performance (#61949)

--- schematics/bundles/checker-CY7a8ko8.cjs
+++ schematics/bundles/checker-CY7a8ko8.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-9cf12b0
+ * @license Angular v20.1.0-next.0+sha-4f0221e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-9cf12b0');
+new Version('20.1.0-next.0+sha-4f0221e');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
