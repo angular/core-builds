6ddf75ed0c89816b017fa7892ba1a2e631b638e8
Andrew Kushnir <akushnir@google.com>
2025-06-10 19:12:30 +0000

    135c594 refactor: use zone.js from npm instead of packages/zone.js throughout repo (#61978)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-ee4acc2
+ * @license Angular v20.0.2+sha-135c594
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-ee4acc2');
+const VERSION = new Version('20.0.2+sha-135c594');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
