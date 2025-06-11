270839661f4a6d4f2e0ed5ff2297032f09b4182f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 08:56:54 +0000

    e73f248 build: update dependency google-closure-compiler to v20250608 (#61997)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-ca478a2
+ * @license Angular v20.0.2+sha-e73f248
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-ca478a2');
+const VERSION = new Version('20.0.2+sha-e73f248');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node-JnOYh9kg.mjs
+++ fesm2022/debug_node-JnOYh9kg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-ca478a2
+ * @license Angular v20.0.2+sha-e73f248
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13396,7 +13396,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.2+sha-ca478a2']
+        ? ['ng-version', '20.0.2+sha-e73f248']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
