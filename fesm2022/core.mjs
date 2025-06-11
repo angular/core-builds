9ab6f92e063291baacea079e1f64bb957e45c4fa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 11:45:54 +0000

    0b1d045 docs(docs-infra): retain the results in the search dialog while typing (#61935)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-5053637
+ * @license Angular v20.1.0-next.0+sha-0b1d045
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-5053637');
+const VERSION = new Version('20.1.0-next.0+sha-0b1d045');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_paths-C5Df24y1.cjs
+++ schematics/bundles/project_paths-C5Df24y1.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-5053637
+ * @license Angular v20.1.0-next.0+sha-0b1d045
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
