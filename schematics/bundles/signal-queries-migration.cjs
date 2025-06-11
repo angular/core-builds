9a14fce40e15927cc49297ae9a074f17a23249c3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 08:51:41 +0000

    ce7f2e3 build: update all non-major dependencies (#61953)

--- schematics/bundles/signal-queries-migration.cjs
+++ schematics/bundles/signal-queries-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-0bd7c7d
+ * @license Angular v20.1.0-next.0+sha-ce7f2e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
