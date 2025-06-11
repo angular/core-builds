3b46b18db491ab7a443c5e244b8ea8c2c446bf01
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 10:29:44 +0000

    5053637 build: update dependency google-closure-compiler to v20250608 (#61996)

--- schematics/bundles/checker-CY7a8ko8.cjs
+++ schematics/bundles/checker-CY7a8ko8.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-9a81a0f
+ * @license Angular v20.1.0-next.0+sha-5053637
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-9a81a0f');
+new Version('20.1.0-next.0+sha-5053637');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
