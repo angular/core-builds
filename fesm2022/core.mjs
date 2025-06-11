71fd550fe462e3bbf3fd8007839db4433a15ed03
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 13:45:44 +0000

    8611630 release: cut the v20.1.0-next.1 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-0b1d045
+ * @license Angular v20.1.0-next.1+sha-8611630
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-0b1d045');
+const VERSION = new Version('20.1.0-next.1+sha-8611630');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-CY7a8ko8.cjs
+++ schematics/bundles/checker-CY7a8ko8.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-0b1d045
+ * @license Angular v20.1.0-next.1+sha-8611630
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-0b1d045');
+new Version('20.1.0-next.1+sha-8611630');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
