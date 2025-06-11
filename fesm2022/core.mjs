77544916562f9d2558d11023530e227fcd8cb239
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 10:16:27 +0000

    ff27f21 build: update all non-major dependencies (#61954)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-06979c6
+ * @license Angular v20.0.2+sha-ff27f21
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-06979c6');
+const VERSION = new Version('20.0.2+sha-ff27f21');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
+++ schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.2+sha-06979c6
+ * @license Angular v20.0.2+sha-ff27f21
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
