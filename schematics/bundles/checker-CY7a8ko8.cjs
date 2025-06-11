22cac9a7bd63446070ea0733c5affa6a31d09796
Andrew Kushnir <akushnir@google.com>
2025-06-11 01:37:03 +0000

    0bd7c7d Revert "docs(docs-infra): retain the results in the search dialog while typing (#61935)" (#61995)

--- schematics/bundles/checker-CY7a8ko8.cjs
+++ schematics/bundles/checker-CY7a8ko8.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-295d443
+ * @license Angular v20.1.0-next.0+sha-0bd7c7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.0+sha-295d443');
+new Version('20.1.0-next.0+sha-0bd7c7d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/test-bed-get.cjs
+++ schematics/bundles/test-bed-get.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.0+sha-295d443
+ * @license Angular v20.1.0-next.0+sha-0bd7c7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
