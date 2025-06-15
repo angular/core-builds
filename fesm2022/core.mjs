49db6f1790d1ad699191c8ca760b8a2941f68c2a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-15 10:50:27 +0000

    2e3925a refactor(localize): add tripleslash type of Application builder (#62037)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-be2747e
+ * @license Angular v20.1.0-next.1+sha-2e3925a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-be2747e');
+const VERSION = new Version('20.1.0-next.1+sha-2e3925a');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
