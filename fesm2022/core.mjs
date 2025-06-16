a28f9319745db52bd624c539b261e218172dd709
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-16 08:34:36 +0000

    8403bc7 build: migrate to usages of @devinfra// instead of @npm//@angular/build-tooling (#62050)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-f69ec36
+ * @license Angular v20.0.3+sha-8403bc7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-f69ec36');
+const VERSION = new Version('20.0.3+sha-8403bc7');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
