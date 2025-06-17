76695e8440957d8c10db56a00acc58488dfa8959
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 17:02:28 +0000

    18a7ce8 build: migrate back to using the @npm// sourced benchmark_test macro (#62101)

--- schematics/bundles/signal-input-migration.cjs
+++ schematics/bundles/signal-input-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.1+sha-2e0c98b
+ * @license Angular v20.1.0-next.1+sha-18a7ce8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
