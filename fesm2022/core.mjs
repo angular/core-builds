8c0775cfbb332e0bcd6d514c03d043631beba3ff
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 09:30:35 +0000

    fb56d51 docs(router): improve discoverability of route guard section (#62060)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-734c13a
+ * @license Angular v20.0.3+sha-fb56d51
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-734c13a');
+const VERSION = new Version('20.0.3+sha-fb56d51');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.3+sha-734c13a
+ * @license Angular v20.0.3+sha-fb56d51
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.3+sha-734c13a');
+new Version('20.0.3+sha-fb56d51');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
