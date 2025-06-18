777b82506eb1906bb8ff4695d10d0bdd58acc838
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 18:00:10 +0000

    085e29f release: cut the v20.1.0-next.2 release

--- fesm2022/debug_node-BFi6TVHr.mjs
+++ fesm2022/debug_node-BFi6TVHr.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-81d918a
+ * @license Angular v20.1.0-next.2+sha-085e29f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13450,7 +13450,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-next.1+sha-81d918a']
+        ? ['ng-version', '20.1.0-next.2+sha-085e29f']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
