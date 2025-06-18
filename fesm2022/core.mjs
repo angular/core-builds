94bae5b9d8635e05a48346a27980746de1a938aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-06-18 21:04:12 +0000

    127120a docs: update best-practices prompt styling (#62128)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-6a4fdba
+ * @license Angular v20.0.4+sha-127120a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-6a4fdba');
+const VERSION = new Version('20.0.4+sha-127120a');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
