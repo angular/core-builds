cec77062e82469bcc6588125d196c43a618568c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-06-18 21:03:53 +0000

    c3e11e5 docs: update best-practices prompt styling (#62128)

--- schematics/bundles/migrate_ts_type_references-MBd4NBjn.cjs
+++ schematics/bundles/migrate_ts_type_references-MBd4NBjn.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.2+sha-085e29f
+ * @license Angular v20.1.0-next.2+sha-c3e11e5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
