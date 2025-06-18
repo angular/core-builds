fc74dbab810423d8714cc09f8cde506e625c95d2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 08:57:33 +0000

    5885eca build: update dependency google-closure-compiler to v20250615 (#62114)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-2db7156
+ * @license Angular v20.1.0-next.1+sha-5885eca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-2db7156');
+const VERSION = new Version('20.1.0-next.1+sha-5885eca');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
