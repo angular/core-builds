b51d665c2af6de0f44f493aa24b2aca63324631c
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 10:48:45 +0000

    f042cb3 docs(docs-infra): fix code-editor flakiness (#62143)

--- schematics/bundles/checker-Bu1Wu4f7.cjs
+++ schematics/bundles/checker-Bu1Wu4f7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.4+sha-288700c
+ * @license Angular v20.0.4+sha-f042cb3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.4+sha-288700c');
+new Version('20.0.4+sha-f042cb3');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/symbol-VPWguRxr.cjs
+++ schematics/bundles/symbol-VPWguRxr.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.4+sha-288700c
+ * @license Angular v20.0.4+sha-f042cb3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
