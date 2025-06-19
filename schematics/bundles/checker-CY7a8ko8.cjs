8548da3ebe6740c9419641af78dec6315a885303
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 12:36:38 +0000

    900cd37 fix(upgrade): Make zoneless work with hybrid apps (#61660)

--- schematics/bundles/checker-CY7a8ko8.cjs
+++ schematics/bundles/checker-CY7a8ko8.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.2+sha-acfedce
+ * @license Angular v20.1.0-next.2+sha-900cd37
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.2+sha-acfedce');
+new Version('20.1.0-next.2+sha-900cd37');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
