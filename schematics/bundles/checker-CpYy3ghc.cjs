93e720580d270314c8219651d57178d30c077346
Andrew Kushnir <akushnir@google.com>
2025-06-20 22:40:47 +0000

    2ae2076 docs: embeded best practices in page (#62173)

--- schematics/bundles/checker-CpYy3ghc.cjs
+++ schematics/bundles/checker-CpYy3ghc.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.4+sha-9c41449
+ * @license Angular v20.0.4+sha-2ae2076
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.4+sha-9c41449');
+new Version('20.0.4+sha-2ae2076');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
