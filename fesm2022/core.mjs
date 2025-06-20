4f9a961c08f749745f0b0f60cc7cfece75c7c4a7
Andrew Kushnir <akushnir@google.com>
2025-06-20 21:54:28 +0000

    9c41449 build: update cross-repo angular dependencies (#62163)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-2020005
+ * @license Angular v20.0.4+sha-9c41449
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-2020005');
+const VERSION = new Version('20.0.4+sha-9c41449');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
