2a1f382c044d495e330a0aeca847d959c5502770
Andrew Kushnir <akushnir@google.com>
2025-06-20 20:16:20 +0000

    f11b3f9 docs: update Angular CLI help (#62166)

--- schematics/bundles/checker-ww1VasfL.cjs
+++ schematics/bundles/checker-ww1VasfL.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.2+sha-f8a90c9
+ * @license Angular v20.1.0-next.2+sha-f11b3f9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.2+sha-f8a90c9');
+new Version('20.1.0-next.2+sha-f11b3f9');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
