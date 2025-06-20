dfaf00c0dd3901e7f58cc8bea1ab4e27cb71b03d
Andrew Kushnir <akushnir@google.com>
2025-06-20 18:17:06 +0000

    3ecfa9c docs: add JetBrains Junie guidelines (#62161)

--- schematics/bundles/checker-ww1VasfL.cjs
+++ schematics/bundles/checker-ww1VasfL.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.2+sha-ed5062a
+ * @license Angular v20.1.0-next.2+sha-3ecfa9c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.2+sha-ed5062a');
+new Version('20.1.0-next.2+sha-3ecfa9c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/symbol-VPWguRxr.cjs
+++ schematics/bundles/symbol-VPWguRxr.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.2+sha-ed5062a
+ * @license Angular v20.1.0-next.2+sha-3ecfa9c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
