3ca036758af936c34ccd412f1588ec796623cd51
Andrew Kushnir <akushnir@google.com>
2025-06-20 20:15:15 +0000

    2020005 docs: update Angular CLI help (#62167)

--- schematics/bundles/checker-CpYy3ghc.cjs
+++ schematics/bundles/checker-CpYy3ghc.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.4+sha-dff1417
+ * @license Angular v20.0.4+sha-2020005
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.4+sha-dff1417');
+new Version('20.0.4+sha-2020005');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/imports-CIX-JgAN.cjs
+++ schematics/bundles/imports-CIX-JgAN.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.4+sha-dff1417
+ * @license Angular v20.0.4+sha-2020005
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
