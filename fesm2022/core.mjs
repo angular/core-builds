f87647a960a5d212ccb8286b471e8fb282406ea4
Andrew Kushnir <akushnir@google.com>
2025-06-21 01:36:36 +0000

    152ff0c docs: fix link to http-resource guide (#62180)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-286f9f5
+ * @license Angular v20.1.0-next.2+sha-152ff0c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-286f9f5');
+const VERSION = new Version('20.1.0-next.2+sha-152ff0c');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-ww1VasfL.cjs
+++ schematics/bundles/checker-ww1VasfL.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.2+sha-286f9f5
+ * @license Angular v20.1.0-next.2+sha-152ff0c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32082,7 +32082,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.2+sha-286f9f5');
+new Version('20.1.0-next.2+sha-152ff0c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
