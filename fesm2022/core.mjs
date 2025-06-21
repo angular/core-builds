af32514bf6e5e636c978ee5e9a6feaddad102edb
Andrew Kushnir <akushnir@google.com>
2025-06-21 01:36:49 +0000

    35ce544 docs: fix link to http-resource guide (#62180)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-2ae2076
+ * @license Angular v20.0.4+sha-35ce544
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-2ae2076');
+const VERSION = new Version('20.0.4+sha-35ce544');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
