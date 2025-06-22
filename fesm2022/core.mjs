0258b2a158e02cc095f0f165e6ba631537f6a51b
Andrew Kushnir <akushnir@google.com>
2025-06-22 16:25:07 +0000

    4c14503 Revert "docs: embeded best practices in page (#62173)" (#62193)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-152ff0c
+ * @license Angular v20.1.0-next.2+sha-4c14503
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-152ff0c');
+const VERSION = new Version('20.1.0-next.2+sha-4c14503');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
