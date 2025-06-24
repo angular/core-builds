c338bf1a6cb78e8c3addcfeecd39efedd11029ac
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 15:15:03 +0000

    9d20d6f Revert "test(devtools): revive cypress tests (#61972)" (#62243)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-a42169a
+ * @license Angular v20.0.4+sha-9d20d6f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-a42169a');
+const VERSION = new Version('20.0.4+sha-9d20d6f');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-bovuFVqM.cjs
+++ schematics/bundles/checker-bovuFVqM.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.4+sha-a42169a
+ * @license Angular v20.0.4+sha-9d20d6f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.4+sha-a42169a');
+new Version('20.0.4+sha-9d20d6f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
