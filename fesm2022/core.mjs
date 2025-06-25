348330fda2163ddf35efb516fb92a025f04b60ad
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-06-25 16:58:24 +0000

    c6f0f54 release: cut the v20.0.5 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-a2e6f31
+ * @license Angular v20.0.5+sha-c6f0f54
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-a2e6f31');
+const VERSION = new Version('20.0.5+sha-c6f0f54');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/leading_space-D9nQ8UQC.cjs
+++ schematics/bundles/leading_space-D9nQ8UQC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.4+sha-a2e6f31
+ * @license Angular v20.0.5+sha-c6f0f54
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
