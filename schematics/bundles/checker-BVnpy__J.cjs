994c145288b3b7323a2af4aef454556360e60c6f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 09:26:54 +0000

    73269cf feat(http): add keepalive support for fetch requests in httpResource (#61833)

--- schematics/bundles/checker-BVnpy__J.cjs
+++ schematics/bundles/checker-BVnpy__J.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.2+sha-20c1f99
+ * @license Angular v20.1.0-next.2+sha-73269cf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32199,7 +32199,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.2+sha-20c1f99');
+new Version('20.1.0-next.2+sha-73269cf');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
