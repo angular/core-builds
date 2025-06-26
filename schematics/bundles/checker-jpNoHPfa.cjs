4990254a5b2b83c45739188f63d350168079ad7c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 12:14:42 +0000

    13875d0 refactor(common): remove redundant `ɵloadImpl` check in client bundles (#62191)

--- schematics/bundles/checker-jpNoHPfa.cjs
+++ schematics/bundles/checker-jpNoHPfa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-349719c
+ * @license Angular v20.1.0-next.3+sha-13875d0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32203,7 +32203,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.3+sha-349719c');
+new Version('20.1.0-next.3+sha-13875d0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signals.cjs
+++ schematics/bundles/signals.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-349719c
+ * @license Angular v20.1.0-next.3+sha-13875d0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
