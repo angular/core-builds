783e520627be9cd3f688513ca9c2fd792420ffb4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 08:31:29 +0000

    c5c9b0b build: update rules_browsers digest to 0952071 (#62279)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-85fe323
+ * @license Angular v20.1.0-next.3+sha-c5c9b0b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-85fe323');
+const VERSION = new Version('20.1.0-next.3+sha-c5c9b0b');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
