ec7a399c7d2f366320bd8ffa34aa1e1882ec2a34
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 07:40:28 +0000

    5c2a942 ci: restore flamegraph dep (#62304)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-31e3eb2
+ * @license Angular v20.0.5+sha-5c2a942
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-31e3eb2');
+const VERSION = new Version('20.0.5+sha-5c2a942');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
