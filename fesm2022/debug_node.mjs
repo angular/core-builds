5589531806f5fe83a7da877207ae1b43631ef025
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 07:54:04 +0000

    ac7fe2c docs: remove list of APIs with a providers array. (#62296)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-5c2a942
+ * @license Angular v20.0.5+sha-ac7fe2c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13396,7 +13396,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.5+sha-5c2a942']
+        ? ['ng-version', '20.0.5+sha-ac7fe2c']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
