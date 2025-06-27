9d0b92ab2521a43cffa5f1988ee46bf3bdf1e38b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 12:59:10 +0000

    6ec2900 docs: Update documentation httpResource with cache & priority feature (#62334)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-7077520
+ * @license Angular v20.1.0-next.3+sha-6ec2900
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13476,7 +13476,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-next.3+sha-7077520']
+        ? ['ng-version', '20.1.0-next.3+sha-6ec2900']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
