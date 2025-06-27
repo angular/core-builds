826d989f8aacead556b0fc2f1ab49477ac931229
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 11:31:35 +0000

    82cdd5c build: update github/codeql-action action to v3.29.1 (#62332)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-74004f6
+ * @license Angular v20.0.5+sha-82cdd5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-74004f6');
+const VERSION = new Version('20.0.5+sha-82cdd5c');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
