a860f21a4351cb53a142b57cdfe2ed3d28febcfa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 09:07:08 +0000

    41230e6 build: update dependency marked to v16 (#62322)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-308c82d
+ * @license Angular v20.0.5+sha-41230e6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-308c82d');
+const VERSION = new Version('20.0.5+sha-41230e6');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
