de9a72744d7fd0249e635e2557f2a4045a572a7f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:37:25 +0000

    b2afa11 docs: Update documentation http making request (#62336)

--- schematics/bundles/checker-jpNoHPfa.cjs
+++ schematics/bundles/checker-jpNoHPfa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-f10e1bc
+ * @license Angular v20.1.0-next.3+sha-b2afa11
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32203,7 +32203,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.3+sha-f10e1bc');
+new Version('20.1.0-next.3+sha-b2afa11');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
