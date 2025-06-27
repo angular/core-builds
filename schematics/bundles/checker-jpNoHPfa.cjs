26c5fc042043a13bd0a7d87f7f9e7c9e9793932f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 09:34:29 +0000

    e8addbf refactor(devtools): improve flamegraph UI by employing the new flamegraph API (#62299)

--- schematics/bundles/checker-jpNoHPfa.cjs
+++ schematics/bundles/checker-jpNoHPfa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-5ccb81e
+ * @license Angular v20.1.0-next.3+sha-e8addbf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32203,7 +32203,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.3+sha-5ccb81e');
+new Version('20.1.0-next.3+sha-e8addbf');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
