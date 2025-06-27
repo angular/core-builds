f31ff5d74b996d9b5562862d6a436b03f122772c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 10:04:34 +0000

    c4cffe2 feat(http): Add timeout option to HTTP requests (#57194)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-e8addbf
+ * @license Angular v20.1.0-next.3+sha-c4cffe2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-e8addbf');
+const VERSION = new Version('20.1.0-next.3+sha-c4cffe2');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-jpNoHPfa.cjs
+++ schematics/bundles/checker-jpNoHPfa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-e8addbf
+ * @license Angular v20.1.0-next.3+sha-c4cffe2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32203,7 +32203,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.3+sha-e8addbf');
+new Version('20.1.0-next.3+sha-c4cffe2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
