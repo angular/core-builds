95ff67ede6da9222a86f533db40cdff9813f9f46
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 12:01:42 +0000

    7077520 docs: add page NG8115 (uninvokedTrackFunction) (#61962)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-599baa2
+ * @license Angular v20.1.0-next.3+sha-7077520
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-599baa2');
+const VERSION = new Version('20.1.0-next.3+sha-7077520');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
