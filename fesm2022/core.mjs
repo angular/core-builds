4549200056d2e24e8164e692d0750f494e99fbe3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:15:14 +0000

    f943505 refactor(devtools): main nav (#62242)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-6ec2900
+ * @license Angular v20.1.0-next.3+sha-f943505
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-6ec2900');
+const VERSION = new Version('20.1.0-next.3+sha-f943505');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-jpNoHPfa.cjs
+++ schematics/bundles/checker-jpNoHPfa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-6ec2900
+ * @license Angular v20.1.0-next.3+sha-f943505
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32203,7 +32203,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.3+sha-6ec2900');
+new Version('20.1.0-next.3+sha-f943505');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
