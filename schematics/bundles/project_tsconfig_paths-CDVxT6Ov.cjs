ef157a50ed09baba2fe9516b91f76be77560bdbc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 11:30:55 +0000

    599baa2 build: update github/codeql-action action to v3.29.1 (#62331)

--- schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
+++ schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-f849803
+ * @license Angular v20.1.0-next.3+sha-599baa2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
