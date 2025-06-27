dada43d387b31a6973f9b1cdf182918f3bc54cb1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 08:43:25 +0000

    df1a61f build: update all non-major dependencies (#62323)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-9afc00f
+ * @license Angular v20.1.0-next.3+sha-df1a61f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-9afc00f');
+const VERSION = new Version('20.1.0-next.3+sha-df1a61f');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
