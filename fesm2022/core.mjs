c150c6a329790423f4cab1d012536a97cb8effdd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:54:59 +0000

    4bc8187 build: update all non-major dependencies (#62356)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-85d6474
+ * @license Angular v20.0.5+sha-4bc8187
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-85d6474');
+const VERSION = new Version('20.0.5+sha-4bc8187');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/output-migration.cjs
+++ schematics/bundles/output-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.5+sha-85d6474
+ * @license Angular v20.0.5+sha-4bc8187
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
