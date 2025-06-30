e8e2f2fc8570e0dc3bc8c1d2fbb32d66ae116aa2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:30:48 +0000

    493f25b build: update all non-major dependencies (#62355)

--- schematics/bundles/inject-migration.cjs
+++ schematics/bundles/inject-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-664ed95
+ * @license Angular v20.1.0-next.3+sha-493f25b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
