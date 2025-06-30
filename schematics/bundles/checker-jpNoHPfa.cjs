b0094c5d436c3eb888f8708aebd2e7476c05f915
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:13:04 +0000

    e1856da docs: update router read-route-state.md file (#62367)

--- schematics/bundles/checker-jpNoHPfa.cjs
+++ schematics/bundles/checker-jpNoHPfa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-08e2d9d
+ * @license Angular v20.1.0-next.3+sha-e1856da
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32203,7 +32203,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-next.3+sha-08e2d9d');
+new Version('20.1.0-next.3+sha-e1856da');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
+++ schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-08e2d9d
+ * @license Angular v20.1.0-next.3+sha-e1856da
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
