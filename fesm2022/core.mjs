9b5e0da3c8dc9f9e5302443f82d7bef194719846
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:13:15 +0000

    e145a83 docs: update router read-route-state.md file (#62367)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-5edbb54
+ * @license Angular v20.0.5+sha-e145a83
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-5edbb54');
+const VERSION = new Version('20.0.5+sha-e145a83');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/output-migration.cjs
+++ schematics/bundles/output-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.5+sha-5edbb54
+ * @license Angular v20.0.5+sha-e145a83
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
