981c1cd42dc896b65ac857ac1db1e7a825b60455
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 10:14:08 +0000

    f0965c7 feat(http): Add support for fetch credentials options in HttpClient (#62354)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-d283595
+ * @license Angular v20.1.0-next.3+sha-f0965c7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-d283595');
+const VERSION = new Version('20.1.0-next.3+sha-f0965c7');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
