d3c2343a27520b4879585d92564ea8e27f8f5d02
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 07:51:29 +0000

    5edbb54 build: update rules_browsers digest to 800d91c (#62359)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-8d58150
+ * @license Angular v20.0.5+sha-5edbb54
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13396,7 +13396,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.0.5+sha-8d58150']
+        ? ['ng-version', '20.0.5+sha-5edbb54']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
