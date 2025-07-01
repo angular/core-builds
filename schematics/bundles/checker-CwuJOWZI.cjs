a9c30c2c7612f2bfb934df8907678fe6e85f8b4b
Andrew Kushnir <akushnir@google.com>
2025-07-01 19:28:19 +0000

    fb1bc99 ci: fix incorrect base branch in renovate config (#62406)

--- schematics/bundles/checker-CwuJOWZI.cjs
+++ schematics/bundles/checker-CwuJOWZI.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-4ac0147
+ * @license Angular v20.2.0-next.0+sha-fb1bc99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32204,7 +32204,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.0+sha-4ac0147');
+new Version('20.2.0-next.0+sha-fb1bc99');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
