2fd2c3fefdbb4b414682c2f8cd078c7e5181d8e1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-07-01 17:11:26 +0000

    41955cf release: cut the v20.1.0-rc.0 release

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-b8c6cfa
+ * @license Angular v20.1.0-rc.0+sha-41955cf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13476,7 +13476,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-next.3+sha-b8c6cfa']
+        ? ['ng-version', '20.1.0-rc.0+sha-41955cf']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/output-migration.cjs
+++ schematics/bundles/output-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-b8c6cfa
+ * @license Angular v20.1.0-rc.0+sha-41955cf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
