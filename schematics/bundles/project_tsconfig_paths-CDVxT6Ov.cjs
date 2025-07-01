0496983d11dc2da7b774da5eaea0f7902d720c83
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-07-01 17:12:22 +0000

    4ac0147 docs: release notes for the v20.1.0-rc.0 release

--- schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
+++ schematics/bundles/project_tsconfig_paths-CDVxT6Ov.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-next.3+sha-b8c6cfa
+ * @license Angular v20.2.0-next.0+sha-4ac0147
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
