4f8005dbda01c4ac2906416f5927018889d0f02f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 07:57:38 +0000

    f9abb55 Revert "fix(service-worker): update service worker to handle seeking better for videos (#60029)" (#62422)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-40214ac
+ * @license Angular v20.2.0-next.0+sha-f9abb55
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13476,7 +13476,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-next.0+sha-40214ac']
+        ? ['ng-version', '20.2.0-next.0+sha-f9abb55']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
