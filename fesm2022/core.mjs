9c20783136dbd1b13e21977f84f2a66f655c8852
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 07:57:06 +0000

    e0e78fe Revert "fix(service-worker): update service worker to handle seeking better for videos (#60029)" (#62422)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-5766ba6
+ * @license Angular v20.0.6+sha-e0e78fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-5766ba6');
+const VERSION = new Version('20.0.6+sha-e0e78fe');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/cleanup-unused-imports.cjs
+++ schematics/bundles/cleanup-unused-imports.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.6+sha-5766ba6
+ * @license Angular v20.0.6+sha-e0e78fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
