6dee44678c2e2a07ded8de3a216c7ebed5017a1d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:23:12 +0000

    ade8ddd ci: update pullapprove reviewers (#62448)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-5901a23
+ * @license Angular v20.0.6+sha-ade8ddd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-5901a23');
+const VERSION = new Version('20.0.6+sha-ade8ddd');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-a0VNmSrQ.cjs
+++ schematics/bundles/checker-a0VNmSrQ.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.6+sha-5901a23
+ * @license Angular v20.0.6+sha-ade8ddd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.6+sha-5901a23');
+new Version('20.0.6+sha-ade8ddd');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
