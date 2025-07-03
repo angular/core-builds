705d09974dc32e998fd84a79dd4e50022c66efc4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 08:20:19 +0000

    18816a6 refactor(docs-infra): removed unused imports (#62441)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-026efb1
+ * @license Angular v20.1.0-rc.0+sha-18816a6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13478,7 +13478,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-rc.0+sha-026efb1']
+        ? ['ng-version', '20.1.0-rc.0+sha-18816a6']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
