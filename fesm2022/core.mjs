cf51bce6bdc248315f7b3802a304a6ba97b0cae1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:23:22 +0000

    f5f80d8 ci: update pullapprove reviewers (#62448)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-f5a1020
+ * @license Angular v20.1.0-rc.0+sha-f5f80d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-f5a1020');
+const VERSION = new Version('20.1.0-rc.0+sha-f5f80d8');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
