ec30c77c9eb132ef738e307e450a4ac6dd8c6759
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 14:54:01 +0000

    9ed9375 refactor(devtools): remove `version_name` from `manifest.json` (#62351)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-5c54cb8
+ * @license Angular v20.2.0-next.0+sha-9ed9375
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-5c54cb8');
+const VERSION = new Version('20.2.0-next.0+sha-9ed9375');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
