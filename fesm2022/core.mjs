f7490c78e1e3ceec169f749b56e188a1522664e6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 08:20:52 +0000

    a7ce3cc refactor(docs-infra): removed unused imports (#62441)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-4c332c3
+ * @license Angular v20.0.6+sha-a7ce3cc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-4c332c3');
+const VERSION = new Version('20.0.6+sha-a7ce3cc');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
