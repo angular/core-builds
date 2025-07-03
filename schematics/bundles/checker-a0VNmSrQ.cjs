798e5a130896ad20c5d565fc2e1950e65771de78
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 08:43:01 +0000

    5901a23 docs: display deprecation message for top most symbol (#62427)

--- schematics/bundles/checker-a0VNmSrQ.cjs
+++ schematics/bundles/checker-a0VNmSrQ.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.0.6+sha-a7ce3cc
+ * @license Angular v20.0.6+sha-5901a23
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32166,7 +32166,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.0.6+sha-a7ce3cc');
+new Version('20.0.6+sha-5901a23');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
