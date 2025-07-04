31287fea63b83ff4c8a430d35d3fff5afb33862a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 08:55:42 +0000

    9ba5903 refactor(devtools): remove the standalone demo (#62455)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-720717e
+ * @license Angular v20.2.0-next.0+sha-9ba5903
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13478,7 +13478,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-next.0+sha-720717e']
+        ? ['ng-version', '20.2.0-next.0+sha-9ba5903']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/checker-CcfUr8Z5.cjs
+++ schematics/bundles/checker-CcfUr8Z5.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-720717e
+ * @license Angular v20.2.0-next.0+sha-9ba5903
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32217,7 +32217,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.0+sha-720717e');
+new Version('20.2.0-next.0+sha-9ba5903');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
