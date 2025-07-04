707408fc49b483872f00b9f5ff844ddf491e76f8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 07:29:58 +0000

    720717e docs: Update documentation http with integrity & referrer (#62462)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-9ed9375
+ * @license Angular v20.2.0-next.0+sha-720717e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-9ed9375');
+const VERSION = new Version('20.2.0-next.0+sha-720717e');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-CcfUr8Z5.cjs
+++ schematics/bundles/checker-CcfUr8Z5.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-9ed9375
+ * @license Angular v20.2.0-next.0+sha-720717e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32217,7 +32217,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.0+sha-9ed9375');
+new Version('20.2.0-next.0+sha-720717e');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
