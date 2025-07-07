06cf58cc773b1ac8efecb9d89d01282a90a69dc2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 20:02:26 +0000

    46c920b build: update cross-repo angular dependencies to v20.0.5 (#62501)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-2f45910
+ * @license Angular v20.1.0-rc.0+sha-46c920b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-2f45910');
+const VERSION = new Version('20.1.0-rc.0+sha-46c920b');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-2f45910
+ * @license Angular v20.1.0-rc.0+sha-46c920b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13478,7 +13478,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.0-rc.0+sha-2f45910']
+        ? ['ng-version', '20.1.0-rc.0+sha-46c920b']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
