a3b04ab6b48e54995d188e4da7f7212bde6c4d19
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 21:02:58 +0000

    efc6405 build: update devinfra digest to 6f54d14 (#62508)

--- schematics/bundles/ng_decorators-B5HCqr20.cjs
+++ schematics/bundles/ng_decorators-B5HCqr20.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-rc.0+sha-aee83d1
+ * @license Angular v20.1.0-rc.0+sha-efc6405
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
