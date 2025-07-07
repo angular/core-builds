eb4769fd65d937d1737871db3e7119fefa04a35c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 17:14:19 +0000

    415833f build: migrate angular-optimization from devinfra to devtools (#62503)

--- schematics/bundles/checker-9_8runkq.cjs
+++ schematics/bundles/checker-9_8runkq.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-65b9adf
+ * @license Angular v20.2.0-next.0+sha-415833f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32228,7 +32228,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.0+sha-65b9adf');
+new Version('20.2.0-next.0+sha-415833f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
