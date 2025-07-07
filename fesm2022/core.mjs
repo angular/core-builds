65557d898edc150a6da44318a5c6ef2c37c673f9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 18:18:19 +0000

    bf23414 build: lock file maintenance (#62495)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-5a60242
+ * @license Angular v20.1.0-rc.0+sha-bf23414
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-5a60242');
+const VERSION = new Version('20.1.0-rc.0+sha-bf23414');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
