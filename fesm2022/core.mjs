65d6ca4ba38f3a4a3965cb854d174dceff49f6ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 18:36:59 +0000

    2f45910 build: disable firefox tests in devtools (#62504)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-bf23414
+ * @license Angular v20.1.0-rc.0+sha-2f45910
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-bf23414');
+const VERSION = new Version('20.1.0-rc.0+sha-2f45910');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
