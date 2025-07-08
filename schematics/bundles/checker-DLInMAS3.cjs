7f645217aff1a37a8a70c8d3b8ad861309610805
Andrew Kushnir <akushnir@google.com>
2025-07-08 23:10:48 +0000

    fed402f build: update dependency aspect_rules_ts to v3.6.3 (#62514)

--- schematics/bundles/checker-DLInMAS3.cjs
+++ schematics/bundles/checker-DLInMAS3.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0-rc.0+sha-dda655f
+ * @license Angular v20.1.0-rc.0+sha-fed402f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32228,7 +32228,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0-rc.0+sha-dda655f');
+new Version('20.1.0-rc.0+sha-fed402f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
