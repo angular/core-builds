1d209b290a568521091671389ee6124053db0c4e
Andrew Kushnir <akushnir@google.com>
2025-07-08 23:09:12 +0000

    6adaf01 build: update dependency aspect_rules_ts to v3.6.3 (#62512)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-365ca13
+ * @license Angular v20.2.0-next.0+sha-6adaf01
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-365ca13');
+const VERSION = new Version('20.2.0-next.0+sha-6adaf01');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
