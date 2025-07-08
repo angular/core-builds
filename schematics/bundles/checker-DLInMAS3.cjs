a464b343e76f9a00551d8e1a66371405dcf133b2
Andrew Kushnir <akushnir@google.com>
2025-07-08 20:35:56 +0000

    365ca13 test: rename legacy animations integration tests (#62527)

--- schematics/bundles/checker-DLInMAS3.cjs
+++ schematics/bundles/checker-DLInMAS3.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-6953166
+ * @license Angular v20.2.0-next.0+sha-365ca13
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32228,7 +32228,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.0+sha-6953166');
+new Version('20.2.0-next.0+sha-365ca13');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
