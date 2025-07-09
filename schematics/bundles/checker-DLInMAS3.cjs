71d479add47d638662a838989d02cd5409fe5377
Andrew Kushnir <akushnir@google.com>
2025-07-09 19:52:40 +0000

    75a5d08 fix(http): do not display warnings `Angular detected that a `HttpClient` request with the `keepalive` option was sent using XHR` when option is not true (#62536)

--- schematics/bundles/checker-DLInMAS3.cjs
+++ schematics/bundles/checker-DLInMAS3.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-1b895e1
+ * @license Angular v20.2.0-next.0+sha-75a5d08
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32228,7 +32228,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.0+sha-1b895e1');
+new Version('20.2.0-next.0+sha-75a5d08');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
