f826bcb7b9a215efcae754362644f940f7feddd5
Andrew Kushnir <akushnir@google.com>
2025-07-10 20:00:04 +0000

    6241957 ci: adjust renovate teams (#62578)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-96014b5
+ * @license Angular v20.2.0-next.0+sha-6241957
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-96014b5');
+const VERSION = new Version('20.2.0-next.0+sha-6241957');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
