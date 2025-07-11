d7ed396685706ef3a8bacfd4a316bc4b2a65e9c3
Andrew Kushnir <akushnir@google.com>
2025-07-11 23:15:17 +0000

    1fa10c0 docs: update Angular CDK apis (#62601)

--- schematics/bundles/checker-DLInMAS3.cjs
+++ schematics/bundles/checker-DLInMAS3.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0+sha-db11cdd
+ * @license Angular v20.1.0+sha-1fa10c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32228,7 +32228,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0+sha-db11cdd');
+new Version('20.1.0+sha-1fa10c0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
