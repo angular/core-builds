af2f584b1fb8c5ff1c3e29c4865c72c7a9df4ec4
Andrew Kushnir <akushnir@google.com>
2025-07-11 17:52:42 +0000

    bece163 ci: add to public-api approvers list (#62591)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-15bb258
+ * @license Angular v20.2.0-next.0+sha-bece163
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13494,7 +13494,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-next.0+sha-15bb258']
+        ? ['ng-version', '20.2.0-next.0+sha-bece163']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
