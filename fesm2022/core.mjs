efbd89f6635f93c9178ef651e8c7247e8e9411b1
Andrew Kushnir <akushnir@google.com>
2025-07-14 17:10:53 +0000

    1cff7ca docs: update forgotten change to input signals (#62612) PR Close #62612

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-aa52cd4
+ * @license Angular v20.2.0-next.0+sha-1cff7ca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-aa52cd4');
+const VERSION = new Version('20.2.0-next.0+sha-1cff7ca');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-DLInMAS3.cjs
+++ schematics/bundles/checker-DLInMAS3.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-aa52cd4
+ * @license Angular v20.2.0-next.0+sha-1cff7ca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32228,7 +32228,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.0+sha-aa52cd4');
+new Version('20.2.0-next.0+sha-1cff7ca');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
