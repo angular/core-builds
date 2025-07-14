b9a1e88d5c47bce2cb28fcf60af028f615d03d5f
Andrew Kushnir <akushnir@google.com>
2025-07-14 22:56:46 +0000

    a81f0fa fix(core): InputBinding marks component a dirty. (#62613)

--- schematics/bundles/checker-DLInMAS3.cjs
+++ schematics/bundles/checker-DLInMAS3.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0+sha-74beeec
+ * @license Angular v20.1.0+sha-a81f0fa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32228,7 +32228,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0+sha-74beeec');
+new Version('20.1.0+sha-a81f0fa');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
