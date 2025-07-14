cedf5bd81d39723be028b2cd86c7f81f2900aab7
Andrew Kushnir <akushnir@google.com>
2025-07-14 21:56:41 +0000

    eca2334 docs: correctly look for examples in the bin_dir instead of execPath (#62632)

--- schematics/bundles/control-flow-migration.cjs
+++ schematics/bundles/control-flow-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-817d9df
+ * @license Angular v20.2.0-next.0+sha-eca2334
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
