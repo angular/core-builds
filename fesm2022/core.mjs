2d20eb957f6e3a987ac19aadb3d1f1ba60300840
Andrew Kushnir <akushnir@google.com>
2025-07-14 21:25:27 +0000

    f202160 docs: add dedicated data resolvers router guide (#62250)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-5949373
+ * @license Angular v20.1.0+sha-f202160
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-5949373');
+const VERSION = new Version('20.1.0+sha-f202160');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
