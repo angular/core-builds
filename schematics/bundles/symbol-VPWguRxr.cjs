cf5e27030dad5d2d87d00b72efbb8dce0ee7169e
Andrew Kushnir <akushnir@google.com>
2025-07-15 16:47:16 +0000

    4574095 build: update all non-major dependencies (#62642)

--- schematics/bundles/symbol-VPWguRxr.cjs
+++ schematics/bundles/symbol-VPWguRxr.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.0+sha-80da073
+ * @license Angular v20.2.0-next.0+sha-4574095
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
