127943865ed02282d460636abb87c55a256ff0c6
Andrew Kushnir <akushnir@google.com>
2025-07-15 16:43:19 +0000

    acf8b74 build: update all non-major dependencies (#62643)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-72605a4
+ * @license Angular v20.1.0+sha-acf8b74
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-72605a4');
+const VERSION = new Version('20.1.0+sha-acf8b74');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-DZh1DIQ6.cjs
+++ schematics/bundles/checker-DZh1DIQ6.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0+sha-72605a4
+ * @license Angular v20.1.0+sha-acf8b74
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32230,7 +32230,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0+sha-72605a4');
+new Version('20.1.0+sha-acf8b74');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
