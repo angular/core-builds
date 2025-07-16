01bc6b9ce1d229115578b483b4eb685118e4c30c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-07-16 18:05:36 +0000

    44784f1 release: cut the v20.1.1 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-9506cdf
+ * @license Angular v20.1.1+sha-44784f1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-9506cdf');
+const VERSION = new Version('20.1.1+sha-44784f1');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-CwUJ2eKt.cjs
+++ schematics/bundles/checker-CwUJ2eKt.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.0+sha-9506cdf
+ * @license Angular v20.1.1+sha-44784f1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32271,7 +32271,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.0+sha-9506cdf');
+new Version('20.1.1+sha-44784f1');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
