ba5703136bc11457311d27c84e6c9da9206e69aa
Kirill Cherkashin <kirjs@google.com>
2025-07-16 20:20:23 +0000

    f5f8f76 release: bump Angular DevTools version to 1.0.39 (#62670)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-a484a11
+ * @license Angular v20.2.0-next.1+sha-f5f8f76
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-a484a11');
+const VERSION = new Version('20.2.0-next.1+sha-f5f8f76');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-BVY3FNBy.cjs
+++ schematics/bundles/checker-BVY3FNBy.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.1+sha-a484a11
+ * @license Angular v20.2.0-next.1+sha-f5f8f76
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32271,7 +32271,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.1+sha-a484a11');
+new Version('20.2.0-next.1+sha-f5f8f76');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
