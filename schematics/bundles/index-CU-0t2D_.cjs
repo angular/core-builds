21633a13ff34d8871c486529565595154d56edbc
Kirill Cherkashin <kirjs@google.com>
2025-07-17 22:21:45 +0000

    23d5877 build: migrate to new toolchain usage for api goldens (#62688)

--- schematics/bundles/index-CU-0t2D_.cjs
+++ schematics/bundles/index-CU-0t2D_.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.1+sha-6f52e97
+ * @license Angular v20.2.0-next.1+sha-23d5877
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
