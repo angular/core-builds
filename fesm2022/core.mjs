f1f73108d559b3b39af22d410d2cae23796491f2
Kirill Cherkashin <kirjs@google.com>
2025-07-17 17:34:34 +0000

    1930e99 docs: Update versions.md to use canonical Baseline site (#62680)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-0db3613
+ * @license Angular v20.1.1+sha-1930e99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.1+sha-0db3613');
+const VERSION = new Version('20.1.1+sha-1930e99');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
