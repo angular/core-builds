467acefb5fb8cbf522366d7d3f658067652ad0aa
Kirill Cherkashin <kirjs@google.com>
2025-07-17 17:36:27 +0000

    2e6c2ff refactor(devtools): disable view source button if source function is not available (#62678)

--- schematics/bundles/document-core.cjs
+++ schematics/bundles/document-core.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.1+sha-fc8247d
+ * @license Angular v20.2.0-next.1+sha-2e6c2ff
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
