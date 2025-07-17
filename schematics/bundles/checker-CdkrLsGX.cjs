2b06d26dc30f93c214536a00e30bfacbef9312f1
Kirill Cherkashin <kirill@kcherkashin.org>
2025-07-17 21:56:58 +0000

    6f52e97 docs: release notes for the v20.1.2 release

--- schematics/bundles/checker-CdkrLsGX.cjs
+++ schematics/bundles/checker-CdkrLsGX.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.1+sha-16a9bea
+ * @license Angular v20.2.0-next.1+sha-6f52e97
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32582,7 +32582,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.1+sha-16a9bea');
+new Version('20.2.0-next.1+sha-6f52e97');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/document-core.cjs
+++ schematics/bundles/document-core.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.1+sha-16a9bea
+ * @license Angular v20.2.0-next.1+sha-6f52e97
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
