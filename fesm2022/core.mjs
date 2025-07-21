dba8e1522e3cd3f4d81b2e80fd553f5bf50f818d
Kirill Cherkashin <kirjs@google.com>
2025-07-21 17:46:15 +0000

    74e4778 docs: fix minor typos in styleguide (#62706) PR Close #62706

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-b388f1e
+ * @license Angular v20.1.2+sha-74e4778
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-b388f1e');
+const VERSION = new Version('20.1.2+sha-74e4778');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
