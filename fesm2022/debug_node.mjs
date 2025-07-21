dd690e92a6b50bbcc1819ad97ddbdeb23450d5ca
Kirill Cherkashin <kirjs@google.com>
2025-07-21 20:43:29 +0000

    d0cf8fc test(devtools): bump cypress timeout to 10 seconds (#62726)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-bb7422a
+ * @license Angular v20.2.0-next.1+sha-d0cf8fc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13496,7 +13496,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-next.1+sha-bb7422a']
+        ? ['ng-version', '20.2.0-next.1+sha-d0cf8fc']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
