422fbfb29fb6ef2a35262c80215bf66468db33ca
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:29:09 +0000

    cc468ef refactor(devtools): introduce docs-ref-button (#62685)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-f8c8ff2
+ * @license Angular v20.2.0-next.1+sha-cc468ef
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-f8c8ff2');
+const VERSION = new Version('20.2.0-next.1+sha-cc468ef');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
