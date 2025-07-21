4a4dd1b621418a4d28d86175cde343514f94e5ba
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:24:51 +0000

    aee5260 fix(devtools): stop reseting currentlyMatchedIndex when a node is selected in the component explorer (#62727)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-70d0c10
+ * @license Angular v20.1.2+sha-aee5260
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-70d0c10');
+const VERSION = new Version('20.1.2+sha-aee5260');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-B0RMVBjs.cjs
+++ schematics/bundles/checker-B0RMVBjs.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.2+sha-70d0c10
+ * @license Angular v20.1.2+sha-aee5260
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32271,7 +32271,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.2+sha-70d0c10');
+new Version('20.1.2+sha-aee5260');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
