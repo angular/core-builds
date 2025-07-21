7bd2dd3650214b265b5ce061abc1ba77a8f211e0
Kirill Cherkashin <kirjs@google.com>
2025-07-21 20:19:24 +0000

    f041042 build: update cross-repo angular dependencies (#62731)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-74e4778
+ * @license Angular v20.1.2+sha-f041042
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13496,7 +13496,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.2+sha-74e4778']
+        ? ['ng-version', '20.1.2+sha-f041042']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/checker-B0RMVBjs.cjs
+++ schematics/bundles/checker-B0RMVBjs.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.2+sha-74e4778
+ * @license Angular v20.1.2+sha-f041042
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32271,7 +32271,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.2+sha-74e4778');
+new Version('20.1.2+sha-f041042');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
