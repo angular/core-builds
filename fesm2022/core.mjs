72509190b155e317b483320eb2367c1a22287837
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:15:55 +0000

    a6b2d7d fix(devtools): add event tagging to prevent DDOS (#62645)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-97b1bd1
+ * @license Angular v20.2.0-next.1+sha-a6b2d7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-97b1bd1');
+const VERSION = new Version('20.2.0-next.1+sha-a6b2d7d');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-97b1bd1
+ * @license Angular v20.2.0-next.1+sha-a6b2d7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13496,7 +13496,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-next.1+sha-97b1bd1']
+        ? ['ng-version', '20.2.0-next.1+sha-a6b2d7d']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
