c775850ba6979ea81092c904f7490247bdcf6ffb
Kirill Cherkashin <kirjs@google.com>
2025-07-23 19:09:17 +0000

    063b5e1 fix(core): switch check to documentElement with chaining (#62773)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-9562b63
+ * @license Angular v20.2.0-next.1+sha-063b5e1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-9562b63');
+const VERSION = new Version('20.2.0-next.1+sha-063b5e1');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-9562b63
+ * @license Angular v20.2.0-next.1+sha-063b5e1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13511,7 +13511,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-next.1+sha-9562b63']
+        ? ['ng-version', '20.2.0-next.1+sha-063b5e1']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
@@ -21809,9 +21809,7 @@ const WS_REGEXP = /\s+/;
 const areAnimationSupported = (typeof ngServerMode === 'undefined' || !ngServerMode) &&
     typeof document !== 'undefined' &&
     // tslint:disable-next-line:no-toplevel-property-access
-    typeof document.body !== 'undefined' &&
-    // tslint:disable-next-line:no-toplevel-property-access
-    typeof document.body.getAnimations === 'function';
+    typeof document?.documentElement?.getAnimations === 'function';
 const noOpAnimationComplete = () => { };
 /**
  * Instruction to handle the `animate.enter` behavior for class bindings.
