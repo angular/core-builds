c343ab0839087b8c101e7c03059096cbf410c58f
Kirill Cherkashin <kirjs@google.com>
2025-07-23 11:31:59 +0000

    b7c745b docs: create MCP server setup guide (#62746)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-91bdd17
+ * @license Angular v20.1.2+sha-b7c745b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-91bdd17');
+const VERSION = new Version('20.1.2+sha-b7c745b');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-B0RMVBjs.cjs
+++ schematics/bundles/checker-B0RMVBjs.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.2+sha-91bdd17
+ * @license Angular v20.1.2+sha-b7c745b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32271,7 +32271,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.2+sha-91bdd17');
+new Version('20.1.2+sha-b7c745b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
