460b24fb408d67d947e0f32f1a635acdd5877c95
Kirill Cherkashin <kirill@kcherkashin.org>
2025-07-23 21:01:34 +0000

    d53649c release: cut the v20.1.3 release

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-006eaf3
+ * @license Angular v20.1.3+sha-d53649c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13502,7 +13502,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.2+sha-006eaf3']
+        ? ['ng-version', '20.1.3+sha-d53649c']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/checker-B0RMVBjs.cjs
+++ schematics/bundles/checker-B0RMVBjs.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.2+sha-006eaf3
+ * @license Angular v20.1.3+sha-d53649c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32271,7 +32271,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.2+sha-006eaf3');
+new Version('20.1.3+sha-d53649c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
