305d4a63dd84b22c5da63c04fb8f9f61c9aeb786
Kirill Cherkashin <kirjs@google.com>
2025-07-23 18:07:49 +0000

    155dbee docs: guard a resource error with `hasValue()` (#62708)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-6398baa
+ * @license Angular v20.1.2+sha-155dbee
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13502,7 +13502,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.2+sha-6398baa']
+        ? ['ng-version', '20.1.2+sha-155dbee']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/checker-B0RMVBjs.cjs
+++ schematics/bundles/checker-B0RMVBjs.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.2+sha-6398baa
+ * @license Angular v20.1.2+sha-155dbee
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32271,7 +32271,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.2+sha-6398baa');
+new Version('20.1.2+sha-155dbee');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
