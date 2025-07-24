608e0298b0423fe3115b68b8d40282ae3a2749f6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 13:32:37 +0000

    8ebc1e7 refactor(forms): Removes redundant destroy ref usage in value accessor in select control (#62738)

--- schematics/bundles/standalone-migration.cjs
+++ schematics/bundles/standalone-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.2+sha-6d01168
+ * @license Angular v20.2.0-next.2+sha-8ebc1e7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
