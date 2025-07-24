4f41b5c8df4ee6b632fa5ea2efb8e1a314874dea
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 10:11:56 +0000

    a967f92 test(router): Use autoTick feature in router tests (#62776)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-f2f1af9
+ * @license Angular v20.1.3+sha-a967f92
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-f2f1af9');
+const VERSION = new Version('20.1.3+sha-a967f92');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-Bj8lK7HB.cjs
+++ schematics/bundles/checker-Bj8lK7HB.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.3+sha-f2f1af9
+ * @license Angular v20.1.3+sha-a967f92
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32278,7 +32278,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.3+sha-f2f1af9');
+new Version('20.1.3+sha-a967f92');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
