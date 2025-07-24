81a81f5ddc883745bf976028a875e486d921229a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 11:38:35 +0000

    6d01168 feat(service-worker): notify clients about version failures (#62718)

--- schematics/bundles/checker-D66lhTA4.cjs
+++ schematics/bundles/checker-D66lhTA4.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.2+sha-94b0ef1
+ * @license Angular v20.2.0-next.2+sha-6d01168
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32769,7 +32769,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.2+sha-94b0ef1');
+new Version('20.2.0-next.2+sha-6d01168');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
