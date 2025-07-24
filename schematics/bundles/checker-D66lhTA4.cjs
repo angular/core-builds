1f13c8e9f5400fce6be71e1555f919218bae68d6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 14:07:21 +0000

    24e6f4d docs: add initial documentation for MCP server tools (#62777)

--- schematics/bundles/checker-D66lhTA4.cjs
+++ schematics/bundles/checker-D66lhTA4.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.2+sha-8ebc1e7
+ * @license Angular v20.2.0-next.2+sha-24e6f4d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32769,7 +32769,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.2+sha-8ebc1e7');
+new Version('20.2.0-next.2+sha-24e6f4d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
