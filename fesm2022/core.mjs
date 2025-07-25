4f4672e74953ed17770f452e5ecff261a478debc
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 15:02:12 +0000

    9a8b5c7 build: remove local zone.js build from usage in integration tests (#62800)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-c11f7b9
+ * @license Angular v20.1.3+sha-9a8b5c7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-c11f7b9');
+const VERSION = new Version('20.1.3+sha-9a8b5c7');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
