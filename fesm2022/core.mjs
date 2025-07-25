7b86e6515525d43c58f86ba932b992b17872da0e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 12:39:19 +0000

    c11f7b9 build: update cross-repo angular dependencies (#62807)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-dce0609
+ * @license Angular v20.1.3+sha-c11f7b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-dce0609');
+const VERSION = new Version('20.1.3+sha-c11f7b9');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-Bj8lK7HB.cjs
+++ schematics/bundles/checker-Bj8lK7HB.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.3+sha-dce0609
+ * @license Angular v20.1.3+sha-c11f7b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32278,7 +32278,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.3+sha-dce0609');
+new Version('20.1.3+sha-c11f7b9');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
