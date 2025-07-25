d15914f06c3ab540ff40925f728508532435aa37
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 15:03:44 +0000

    c66e9fe docs: fix link for NG8117 uninvokedFunctionInTextInterpolation (#62820)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-63a6b5a
+ * @license Angular v20.2.0-next.2+sha-c66e9fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-63a6b5a');
+const VERSION = new Version('20.2.0-next.2+sha-c66e9fe');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/signal-input-migration.cjs
+++ schematics/bundles/signal-input-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.2+sha-63a6b5a
+ * @license Angular v20.2.0-next.2+sha-c66e9fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
