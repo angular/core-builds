83d970364166345c65ca56cf6903297f8532bc69
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 08:15:40 +0000

    c19d0ae build: use ts_config rule from rules_ts only and always load from common location (#62808)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-b11e050
+ * @license Angular v20.2.0-next.2+sha-c19d0ae
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-b11e050');
+const VERSION = new Version('20.2.0-next.2+sha-c19d0ae');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-D66lhTA4.cjs
+++ schematics/bundles/checker-D66lhTA4.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.2+sha-b11e050
+ * @license Angular v20.2.0-next.2+sha-c19d0ae
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32769,7 +32769,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.2+sha-b11e050');
+new Version('20.2.0-next.2+sha-c19d0ae');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
