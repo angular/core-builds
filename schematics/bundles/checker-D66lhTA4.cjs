78409e84548b092da47d601680ef414b2246ea39
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-28 12:27:54 +0000

    3b759db docs: Update VersionFailedEvent in Service Worker docs (#62813)

--- schematics/bundles/checker-D66lhTA4.cjs
+++ schematics/bundles/checker-D66lhTA4.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.2+sha-c66e9fe
+ * @license Angular v20.2.0-next.2+sha-3b759db
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32769,7 +32769,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.2+sha-c66e9fe');
+new Version('20.2.0-next.2+sha-3b759db');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
