9ce24a8946baeafddef1a96d650296284a0169e7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 10:01:25 +0000

    f0fca61 refactor(common): replace aio links to adev (#62839)

--- schematics/bundles/checker-Bj8lK7HB.cjs
+++ schematics/bundles/checker-Bj8lK7HB.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.3+sha-cd256ef
+ * @license Angular v20.1.3+sha-f0fca61
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32278,7 +32278,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.3+sha-cd256ef');
+new Version('20.1.3+sha-f0fca61');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
