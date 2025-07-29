a093b2121ef3e797787331183bfaa32ef0620806
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-29 04:49:27 +0000

    622e108 build: standardize usage of js_binary (#62860)

--- schematics/bundles/checker-Bj8lK7HB.cjs
+++ schematics/bundles/checker-Bj8lK7HB.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.3+sha-88913b2
+ * @license Angular v20.1.3+sha-622e108
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32278,7 +32278,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.3+sha-88913b2');
+new Version('20.1.3+sha-622e108');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
