1d2689fdccb86340a253cbb686087271846452a2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 16:59:16 +0000

    ea1afec build: move http_server and generate_api_docs into defaults2.bzl (#62878)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-0b9f53a
+ * @license Angular v20.1.3+sha-ea1afec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-0b9f53a');
+const VERSION = new Version('20.1.3+sha-ea1afec');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/checker-Bj8lK7HB.cjs
+++ schematics/bundles/checker-Bj8lK7HB.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.3+sha-0b9f53a
+ * @license Angular v20.1.3+sha-ea1afec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32278,7 +32278,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.3+sha-0b9f53a');
+new Version('20.1.3+sha-ea1afec');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
