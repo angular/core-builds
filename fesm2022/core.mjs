6f345ab59627abb29cdac735fe430903abd92992
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 12:23:18 +0000

    0b9f53a docs: fix ActivateRoute example (#62855)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-f0fca61
+ * @license Angular v20.1.3+sha-0b9f53a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-f0fca61');
+const VERSION = new Version('20.1.3+sha-0b9f53a');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
