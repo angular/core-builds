7fdfc6526495bc819b062e5b58747031410a6097
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 14:13:26 +0000

    245494a fix(http): add missing http options allowed in fetch API (#62881)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-320de4e
+ * @license Angular v20.2.0-next.2+sha-245494a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13511,7 +13511,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-next.2+sha-320de4e']
+        ? ['ng-version', '20.2.0-next.2+sha-245494a']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
@@ -21786,7 +21786,7 @@ class ElementRegistry {
      */
     animate(el, removeFn) {
         if (!this.outElements.has(el))
-            return;
+            return removeFn();
         const details = this.outElements.get(el);
         let timeoutId;
         let called = false;
--- schematics/bundles/checker-DBomdQHo.cjs
+++ schematics/bundles/checker-DBomdQHo.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.2+sha-320de4e
+ * @license Angular v20.2.0-next.2+sha-245494a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32783,7 +32783,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.2+sha-320de4e');
+new Version('20.2.0-next.2+sha-245494a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
