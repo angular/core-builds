500e9d8b0ab4201c356f7be422ab972603972048
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 16:24:49 +0000

    335cdc7 build: update to latest devinfra in workspace (#62901)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-ecb50ad
+ * @license Angular v20.2.0-next.2+sha-335cdc7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-ecb50ad');
+const VERSION = new Version('20.2.0-next.2+sha-335cdc7');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
