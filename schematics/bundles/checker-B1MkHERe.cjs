22db866724e0d3e3932790de20150e24910418eb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 15:59:54 +0000

    280f1f5 test(language-service): update language-service legacy tests (#62900)

--- schematics/bundles/checker-B1MkHERe.cjs
+++ schematics/bundles/checker-B1MkHERe.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.3+sha-82fcc9c
+ * @license Angular v20.1.3+sha-280f1f5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32292,7 +32292,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.3+sha-82fcc9c');
+new Version('20.1.3+sha-280f1f5');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
