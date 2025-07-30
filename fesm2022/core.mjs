79dd528e504086daf06718ce5059b779b334f6f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 10:06:36 +0000

    55e575a docs(docs-infra): increase until timeout (#62894)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-a2e3bfc
+ * @license Angular v20.1.3+sha-55e575a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-a2e3bfc');
+const VERSION = new Version('20.1.3+sha-55e575a');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-a2e3bfc
+ * @license Angular v20.1.3+sha-55e575a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13502,7 +13502,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.3+sha-a2e3bfc']
+        ? ['ng-version', '20.1.3+sha-55e575a']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
