ec4ee148b7c4609058bb4213a8235f5791603d27
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-07-31 08:43:40 +0000

    6652f9f release: cut the v20.1.4 release

--- schematics/bundles/checker-B1MkHERe.cjs
+++ schematics/bundles/checker-B1MkHERe.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.3+sha-faa9ed9
+ * @license Angular v20.1.4+sha-6652f9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32292,7 +32292,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.3+sha-faa9ed9');
+new Version('20.1.4+sha-6652f9f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
