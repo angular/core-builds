a5a1261f4aface3d9cc1b54c8054aa29027438c4
Joey Perrott <josephperrott@gmail.com>
2025-07-31 22:11:43 +0000

    82cc576 build: use pnpm as the package manager instead of yarn (#62924)

--- schematics/bundles/project_tsconfig_paths-Cn4EEHpG.cjs
+++ schematics/bundles/project_tsconfig_paths-Cn4EEHpG.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.3+sha-892c2d1
+ * @license Angular v20.2.0-next.3+sha-82cc576
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32784,7 +32784,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.3+sha-892c2d1');
+new Version('20.2.0-next.3+sha-82cc576');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
