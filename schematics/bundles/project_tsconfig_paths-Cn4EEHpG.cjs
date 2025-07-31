985bd7966062b9d322eafcf93d2e499897646131
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-31 10:40:19 +0000

    892c2d1 build: update dependency shiki to v3.9.0 (#62920)

--- schematics/bundles/project_tsconfig_paths-Cn4EEHpG.cjs
+++ schematics/bundles/project_tsconfig_paths-Cn4EEHpG.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.3+sha-216caaf
+ * @license Angular v20.2.0-next.3+sha-892c2d1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32784,7 +32784,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.3+sha-216caaf');
+new Version('20.2.0-next.3+sha-892c2d1');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
