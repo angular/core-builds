f69e100925139552abbfaa3db75ff07915b825de
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 14:28:13 +0000

    3e6e1c1 docs: Add `MAX_ANIMATION_TIMEOUT` to the animate docs (#62949)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-857675f
+ * @license Angular v20.2.0-next.3+sha-3e6e1c1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-857675f');
+const VERSION = new Version('20.2.0-next.3+sha-3e6e1c1');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
