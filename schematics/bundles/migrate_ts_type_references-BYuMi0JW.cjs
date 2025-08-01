ec1f90b15f41fe61298324b10498868228d1d24a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 13:30:01 +0000

    f700bd9 build: update pnpm to v10.14.0 (#62946)

--- schematics/bundles/migrate_ts_type_references-BYuMi0JW.cjs
+++ schematics/bundles/migrate_ts_type_references-BYuMi0JW.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.4+sha-3a8fa72
+ * @license Angular v20.1.4+sha-f700bd9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-B1xzlbRF.cjs
+++ schematics/bundles/project_tsconfig_paths-B1xzlbRF.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.4+sha-3a8fa72
+ * @license Angular v20.1.4+sha-f700bd9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32293,7 +32293,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.4+sha-3a8fa72');
+new Version('20.1.4+sha-f700bd9');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
