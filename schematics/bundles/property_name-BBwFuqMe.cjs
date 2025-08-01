420cc89edc05cb6579234d77e3fca6a30346364d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 12:58:46 +0000

    857675f refactor(core): this delays removal of stylesheets when the element registry is present (#62943)

--- schematics/bundles/property_name-BBwFuqMe.cjs
+++ schematics/bundles/property_name-BBwFuqMe.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.3+sha-a2b4988
+ * @license Angular v20.2.0-next.3+sha-857675f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
