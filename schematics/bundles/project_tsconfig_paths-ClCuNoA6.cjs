d851c2ed56b169bd5704749dab574f716f51d1ea
Andrew Scott <atscott@google.com>
2025-08-07 16:35:28 +0000

    0d12eac build: remove unused old toolchain dependencies (#63039)

--- schematics/bundles/project_tsconfig_paths-ClCuNoA6.cjs
+++ schematics/bundles/project_tsconfig_paths-ClCuNoA6.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.5+sha-e524b33
+ * @license Angular v20.2.0-next.5+sha-0d12eac
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32788,7 +32788,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.5+sha-e524b33');
+new Version('20.2.0-next.5+sha-0d12eac');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
