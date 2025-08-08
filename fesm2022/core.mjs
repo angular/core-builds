1d86b95017d29380c0771f68596d061ac343eed0
Andrew Scott <atscott@google.com>
2025-08-08 21:22:40 +0000

    660444f fix(router): attempt to resolve component resources in JIT mode (#63062)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.6+sha-3b214d2
+ * @license Angular v20.2.0-next.6+sha-660444f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.6+sha-3b214d2');
+const VERSION = new Version('20.2.0-next.6+sha-660444f');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
