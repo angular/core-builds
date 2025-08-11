a10eb30ffc6bbf4c82c1cdc17cb9757a1c118ee5
Andrew Scott <atscott@google.com>
2025-08-11 19:20:20 +0000

    499fa8f docs: remove router testing from the testing scenarios guide (#63078)

--- schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
+++ schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.0+sha-79b9159
+ * @license Angular v20.2.0-rc.0+sha-499fa8f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.0+sha-79b9159');
+new Version('20.2.0-rc.0+sha-499fa8f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/property_name-BBwFuqMe.cjs
+++ schematics/bundles/property_name-BBwFuqMe.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.0+sha-79b9159
+ * @license Angular v20.2.0-rc.0+sha-499fa8f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
