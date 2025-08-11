90943354fa2b1a05e6b5fc947cafc2fcf61670e9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-08-11 08:46:12 +0000

    57184d0 docs: release notes for the v20.2.0-rc.0 release

--- schematics/bundles/project_tsconfig_paths-Cy7U7awR.cjs
+++ schematics/bundles/project_tsconfig_paths-Cy7U7awR.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.6+sha-660444f
+ * @license Angular v20.3.0-next.0+sha-57184d0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-next.6+sha-660444f');
+new Version('20.3.0-next.0+sha-57184d0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signals.cjs
+++ schematics/bundles/signals.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-next.6+sha-660444f
+ * @license Angular v20.3.0-next.0+sha-57184d0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
