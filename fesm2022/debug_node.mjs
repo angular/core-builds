3ca313f422630e29cb097b34b997e5da892cdf36
Andrew Scott <atscott@google.com>
2025-08-11 17:39:37 +0000

    b72ef65 build: update to the latest version of devinfra and rename npm2 workspace (#63093)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-5fd7942
+ * @license Angular v20.1.6+sha-b72ef65
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13502,7 +13502,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.6+sha-5fd7942']
+        ? ['ng-version', '20.1.6+sha-b72ef65']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
