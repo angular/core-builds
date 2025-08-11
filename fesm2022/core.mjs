d99f2a49c653202af47c07ddbb7dc43da66023c7
Andrew Scott <atscott@google.com>
2025-08-11 19:19:59 +0000

    fdce7e2 docs: remove router testing from the testing scenarios guide (#63078)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-d9e3790
+ * @license Angular v20.1.6+sha-fdce7e2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-d9e3790');
+const VERSION = new Version('20.1.6+sha-fdce7e2');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-CyFT0Qtu.cjs
+++ schematics/bundles/project_tsconfig_paths-CyFT0Qtu.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.6+sha-d9e3790
+ * @license Angular v20.1.6+sha-fdce7e2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32295,7 +32295,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.6+sha-d9e3790');
+new Version('20.1.6+sha-fdce7e2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
