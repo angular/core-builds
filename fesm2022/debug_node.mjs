cafa9fcd156f68b14721098e9ef5da180433056e
Andrew Scott <atscott@google.com>
2025-08-11 22:47:57 +0000

    f9a38a4 refactor(core): remove temporary resource setter function. (#63097)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-d47ea29
+ * @license Angular v20.3.0-next.0+sha-f9a38a4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13523,7 +13523,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.0-next.0+sha-d47ea29']
+        ? ['ng-version', '20.3.0-next.0+sha-f9a38a4']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
+++ schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0-next.0+sha-d47ea29
+ * @license Angular v20.3.0-next.0+sha-f9a38a4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.3.0-next.0+sha-d47ea29');
+new Version('20.3.0-next.0+sha-f9a38a4');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
