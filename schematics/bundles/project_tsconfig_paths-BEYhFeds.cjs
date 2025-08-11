e64ab434399faa81746a5643f86883dbbed63a65
Andrew Scott <atscott@google.com>
2025-08-11 19:30:54 +0000

    d47ea29 refactor(devtools): keep TreeVisualizer snapped node on focus (#62982)

--- schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
+++ schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0-next.0+sha-baa31fb
+ * @license Angular v20.3.0-next.0+sha-d47ea29
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.3.0-next.0+sha-baa31fb');
+new Version('20.3.0-next.0+sha-d47ea29');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
