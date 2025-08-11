68fb638f9302fc79ec93260f9c7acbab86314048
Andrew Scott <atscott@google.com>
2025-08-11 17:39:18 +0000

    c4dcfdf build: update to the latest version of devinfra and rename npm2 workspace (#63093)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-57184d0
+ * @license Angular v20.3.0-next.0+sha-c4dcfdf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-next.0+sha-57184d0');
+const VERSION = new Version('20.3.0-next.0+sha-c4dcfdf');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-Cy7U7awR.cjs
+++ schematics/bundles/project_tsconfig_paths-Cy7U7awR.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0-next.0+sha-57184d0
+ * @license Angular v20.3.0-next.0+sha-c4dcfdf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.3.0-next.0+sha-57184d0');
+new Version('20.3.0-next.0+sha-c4dcfdf');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
