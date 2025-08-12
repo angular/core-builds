49f3b7b74a71cac80bce744bf6108b59f3011684
Andrew Scott <atscott@google.com>
2025-08-12 18:08:55 +0000

    bb5a69c fix(docs-infra): put content in `<main>` element (#63121)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-f251c7d
+ * @license Angular v20.1.6+sha-bb5a69c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-f251c7d');
+const VERSION = new Version('20.1.6+sha-bb5a69c');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-f251c7d
+ * @license Angular v20.1.6+sha-bb5a69c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13502,7 +13502,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.6+sha-f251c7d']
+        ? ['ng-version', '20.1.6+sha-bb5a69c']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
