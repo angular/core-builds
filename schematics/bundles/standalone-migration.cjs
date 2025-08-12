e3e9bee859c93951b33bf26ddd21b16355f01deb
Andrew Scott <atscott@google.com>
2025-08-12 18:09:33 +0000

    4635095 fix(docs-infra): put content in `<main>` element (#63121)

--- schematics/bundles/standalone-migration.cjs
+++ schematics/bundles/standalone-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.0+sha-b13c3fc
+ * @license Angular v20.2.0-rc.0+sha-4635095
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
