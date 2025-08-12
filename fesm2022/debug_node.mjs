228783d051a31d088d2f73982b1e2b46fed62883
Andrew Scott <atscott@google.com>
2025-08-12 18:01:26 +0000

    b695ff0 feat(language-service): support auto-import for attribute completions (#62797)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-6c51c29
+ * @license Angular v20.3.0-next.0+sha-b695ff0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13523,7 +13523,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.0-next.0+sha-6c51c29']
+        ? ['ng-version', '20.3.0-next.0+sha-b695ff0']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
