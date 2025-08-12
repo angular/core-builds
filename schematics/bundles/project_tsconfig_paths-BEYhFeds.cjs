97d6456e8359c52a16f9a67f14c0f83a224a1735
Andrew Scott <atscott01@gmail.com>
2025-08-12 23:57:34 +0000

    d7a5ead build: update pnpm to v10 (#63087)

--- schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
+++ schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0-next.0+sha-ea78278
+ * @license Angular v20.3.0-next.0+sha-d7a5ead
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.3.0-next.0+sha-ea78278');
+new Version('20.3.0-next.0+sha-d7a5ead');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
