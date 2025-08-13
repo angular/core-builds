09c501bd1b85324426954fdd94a71d60796f1abd
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:24:39 +0000

    254b560 docs: update best-practices (#63132)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-0065ebb
+ * @license Angular v20.2.0-rc.0+sha-254b560
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-0065ebb');
+const VERSION = new Version('20.2.0-rc.0+sha-254b560');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/leading_space-D9nQ8UQC.cjs
+++ schematics/bundles/leading_space-D9nQ8UQC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.0+sha-0065ebb
+ * @license Angular v20.2.0-rc.0+sha-254b560
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
