00a0f32a377ef182482eca78af668b92b5cd0b7d
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:25:47 +0000

    21b0fd3 build: update cross-repo angular dependencies (#62952)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-32ba8b5
+ * @license Angular v21.0.0-next.0+sha-21b0fd3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-32ba8b5');
+const VERSION = new Version('21.0.0-next.0+sha-21b0fd3');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
+++ schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-32ba8b5
+ * @license Angular v21.0.0-next.0+sha-21b0fd3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-32ba8b5');
+new Version('21.0.0-next.0+sha-21b0fd3');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
