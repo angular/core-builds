53574ab5f227923d4955c7c30a2c2243fa791561
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:19:50 +0000

    32ba8b5 docs: add link to ai-config command (#63139)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-91b3466
+ * @license Angular v21.0.0-next.0+sha-32ba8b5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-91b3466');
+const VERSION = new Version('21.0.0-next.0+sha-32ba8b5');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
+++ schematics/bundles/project_tsconfig_paths-BEYhFeds.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-91b3466
+ * @license Angular v21.0.0-next.0+sha-32ba8b5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-91b3466');
+new Version('21.0.0-next.0+sha-32ba8b5');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
