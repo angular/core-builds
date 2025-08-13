9c1c25f3b4b9ab0dfc26c487fc2ae0b07806f7f9
Andrew Scott <atscott01@gmail.com>
2025-08-13 01:45:23 +0000

    538df44 docs: add topic for `takeUntilDestroyed` (#63122)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-5e9893d
+ * @license Angular v20.1.6+sha-538df44
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13502,7 +13502,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.1.6+sha-5e9893d']
+        ? ['ng-version', '20.1.6+sha-538df44']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
