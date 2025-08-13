78cd0febd54b15e4938c64d580d4f07fef776737
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:18:53 +0000

    0065ebb docs: add link to ai-config command (#63139)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-0211668
+ * @license Angular v20.2.0-rc.0+sha-0065ebb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13523,7 +13523,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-rc.0+sha-0211668']
+        ? ['ng-version', '20.2.0-rc.0+sha-0065ebb']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/signal-queries-migration.cjs
+++ schematics/bundles/signal-queries-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.0+sha-0211668
+ * @license Angular v20.2.0-rc.0+sha-0065ebb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
