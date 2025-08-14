c4fa18a223971691490380657e58ad6ef771b8f9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 20:14:45 +0000

    d733c69 build: update all non-major dependencies (#62935)

--- schematics/bundles/project_tsconfig_paths-BYM9Uq7C.cjs
+++ schematics/bundles/project_tsconfig_paths-BYM9Uq7C.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-4bed062
+ * @license Angular v21.0.0-next.0+sha-d733c69
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-4bed062');
+new Version('21.0.0-next.0+sha-d733c69');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
