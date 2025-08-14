d94d7f21f30e3a30e371e3041775312006dcab69
Joey Perrott <josephperrott@gmail.com>
2025-08-14 21:19:19 +0000

    5129dae ci: redirect adev to `index.csr.html` (#63168)

--- schematics/bundles/project_tsconfig_paths-BYM9Uq7C.cjs
+++ schematics/bundles/project_tsconfig_paths-BYM9Uq7C.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-d733c69
+ * @license Angular v21.0.0-next.0+sha-5129dae
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32791,7 +32791,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-d733c69');
+new Version('21.0.0-next.0+sha-5129dae');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
