7d404b09b6fd9337cf89aafc9c2bf2881ce3ff0d
Joey Perrott <josephperrott@gmail.com>
2025-08-14 21:19:23 +0000

    39e4f93 ci: redirect adev to `index.csr.html` (#63168)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-5e084e4
+ * @license Angular v20.1.7+sha-39e4f93
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-5e084e4');
+const VERSION = new Version('20.1.7+sha-39e4f93');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
