3ae28d8e6d997da4c24ed626c17415f1f8f76f93
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 11:54:28 +0000

    3f88fbd docs(docs-infra): replace deprecated router method (#63183)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6f91972
+ * @license Angular v21.0.0-next.0+sha-3f88fbd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-6f91972');
+const VERSION = new Version('21.0.0-next.0+sha-3f88fbd');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
