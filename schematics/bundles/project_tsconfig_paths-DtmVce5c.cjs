70913f4c1127da7eb3f91b9625da9f2785fa1fc5
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 10:36:11 +0000

    d3a0c25 docs: add --yes option to mcp commands (#63173)

--- schematics/bundles/project_tsconfig_paths-DtmVce5c.cjs
+++ schematics/bundles/project_tsconfig_paths-DtmVce5c.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-47b09f8
+ * @license Angular v21.0.0-next.0+sha-d3a0c25
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32811,7 +32811,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-47b09f8');
+new Version('21.0.0-next.0+sha-d3a0c25');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
