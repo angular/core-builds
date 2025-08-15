877c6ccdc775f65e8e7862870515e44fcc8d01de
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 10:36:03 +0000

    de6b641 docs: add --yes option to mcp commands (#63173)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-124d44f
+ * @license Angular v20.2.0-rc.1+sha-de6b641
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-124d44f');
+const VERSION = new Version('20.2.0-rc.1+sha-de6b641');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
+++ schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-124d44f
+ * @license Angular v20.2.0-rc.1+sha-de6b641
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32809,7 +32809,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.1+sha-124d44f');
+new Version('20.2.0-rc.1+sha-de6b641');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
