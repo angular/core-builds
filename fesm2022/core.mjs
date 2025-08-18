b294946c42fec5f906e2d47f11b3208e3866675e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 07:18:23 +0000

    1d5ea07 docs: revert remove of `build-and-link` (#63187)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-2d8ac7d
+ * @license Angular v20.2.0-rc.1+sha-1d5ea07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-2d8ac7d');
+const VERSION = new Version('20.2.0-rc.1+sha-1d5ea07');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
