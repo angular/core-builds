980d3bbb413a17c1f7dc0e63b205573e3ec921d2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:46:40 +0000

    5be80d3 fix(devtools): DOM traversal bug (#62719)

--- schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
+++ schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-1e3748b
+ * @license Angular v20.2.0-rc.1+sha-5be80d3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32809,7 +32809,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.1+sha-1e3748b');
+new Version('20.2.0-rc.1+sha-5be80d3');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
