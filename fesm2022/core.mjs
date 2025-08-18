08cb45c1ad54f402969aaf6a74d328bd63f14da4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:25:25 +0000

    1e3748b build: update cross-repo angular dependencies (#63221)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-87523e9
+ * @license Angular v20.2.0-rc.1+sha-1e3748b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-87523e9');
+const VERSION = new Version('20.2.0-rc.1+sha-1e3748b');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
+++ schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-87523e9
+ * @license Angular v20.2.0-rc.1+sha-1e3748b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32809,7 +32809,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.1+sha-87523e9');
+new Version('20.2.0-rc.1+sha-1e3748b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
