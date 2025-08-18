86878d4a812e14914d22ee869b2143acaf35a2f6
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 08:10:45 +0000

    5d31e20 docs(docs-infra): remove @adev/constant path (#63213)

--- schematics/bundles/project_tsconfig_paths-f3zDmoYh.cjs
+++ schematics/bundles/project_tsconfig_paths-f3zDmoYh.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.7+sha-a9bab9e
+ * @license Angular v20.1.7+sha-5d31e20
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32315,7 +32315,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.7+sha-a9bab9e');
+new Version('20.1.7+sha-5d31e20');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
