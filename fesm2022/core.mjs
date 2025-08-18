35754b14c2bd2d8b72418cdc0a79b9cd77eae191
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 07:48:10 +0000

    7a4bc79 build: remove extra comma in JSON (#63212)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-1d5ea07
+ * @license Angular v20.2.0-rc.1+sha-7a4bc79
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-1d5ea07');
+const VERSION = new Version('20.2.0-rc.1+sha-7a4bc79');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
