e35945d4e0ff603f15f1806295ff1cdae5c36929
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:12:19 +0000

    87523e9 build: update the failure message with the correct command (#63219)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-bdc3167
+ * @license Angular v20.2.0-rc.1+sha-87523e9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-bdc3167');
+const VERSION = new Version('20.2.0-rc.1+sha-87523e9');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
+++ schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-bdc3167
+ * @license Angular v20.2.0-rc.1+sha-87523e9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32809,7 +32809,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.1+sha-bdc3167');
+new Version('20.2.0-rc.1+sha-87523e9');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
