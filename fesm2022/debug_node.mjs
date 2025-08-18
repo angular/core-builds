3e39d63b91d2db3c0b4976168e1db91909e6763a
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 08:10:00 +0000

    9e828d5 docs(docs-infra): remove @adev/constant path (#63213)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-7a4bc79
+ * @license Angular v20.2.0-rc.1+sha-9e828d5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13515,7 +13515,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0-rc.1+sha-7a4bc79']
+        ? ['ng-version', '20.2.0-rc.1+sha-9e828d5']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
+++ schematics/bundles/project_tsconfig_paths-jRRUQzFS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-7a4bc79
+ * @license Angular v20.2.0-rc.1+sha-9e828d5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32809,7 +32809,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.1+sha-7a4bc79');
+new Version('20.2.0-rc.1+sha-9e828d5');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
