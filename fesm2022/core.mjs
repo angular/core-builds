ed7929e15294832b006f5d005fc863e4ea653af8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:20:37 +0000

    966d95d docs: update Angular CLI help (#63226)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-fe27810
+ * @license Angular v20.2.0-rc.1+sha-966d95d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-fe27810');
+const VERSION = new Version('20.2.0-rc.1+sha-966d95d');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
