ab174c32879d841d6fa50acb8ac1aec95419ae3a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:34:50 +0000

    8164b22 refactor(core): update NG0303 error message (#63222) Ensure consistency in error message wording by aligning the NG0303 error with other Angular error strings. This improves clarity and maintains a uniform developer experience

--- schematics/bundles/project_tsconfig_paths-CZS9Ynd-.cjs
+++ schematics/bundles/project_tsconfig_paths-CZS9Ynd-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-966d95d
+ * @license Angular v20.2.0-rc.1+sha-8164b22
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32815,7 +32815,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.1+sha-966d95d');
+new Version('20.2.0-rc.1+sha-8164b22');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
