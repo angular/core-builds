9f638598937b432bd7a4ba7e56cf966283bd1878
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 13:30:36 +0000

    25f593c refactor(common): remove`ngModuleFactory` input of `NgComponentOutlet` (#62838)

--- schematics/bundles/project_tsconfig_paths-CS-eSeHC.cjs
+++ schematics/bundles/project_tsconfig_paths-CS-eSeHC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-4dfa6f1
+ * @license Angular v21.0.0-next.0+sha-25f593c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32817,7 +32817,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-4dfa6f1');
+new Version('21.0.0-next.0+sha-25f593c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
