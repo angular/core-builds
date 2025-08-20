5f468e9811407f8ea43708371acf87019185deaf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 08:34:30 +0000

    1feadba docs: remove redundant Angular docs link in tailwind guide (#63205)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-8a6cc39
+ * @license Angular v21.0.0-next.0+sha-1feadba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13523,7 +13523,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.0+sha-8a6cc39']
+        ? ['ng-version', '21.0.0-next.0+sha-1feadba']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-CVNv_T5P.cjs
+++ schematics/bundles/project_tsconfig_paths-CVNv_T5P.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-8a6cc39
+ * @license Angular v21.0.0-next.0+sha-1feadba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32817,7 +32817,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-8a6cc39');
+new Version('21.0.0-next.0+sha-1feadba');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
