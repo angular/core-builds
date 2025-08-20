1b89ce98dc0e243c094e9bd5c4c29a7313c51e06
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:12:26 +0000

    9bdb4fe build: reenable firefox tests for devtools (#63288)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-11c6385
+ * @license Angular v20.2.0+sha-9bdb4fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0+sha-11c6385');
+const VERSION = new Version('20.2.0+sha-9bdb4fe');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-BIeidc3E.cjs
+++ schematics/bundles/project_tsconfig_paths-BIeidc3E.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0+sha-11c6385
+ * @license Angular v20.2.0+sha-9bdb4fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32815,7 +32815,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0+sha-11c6385');
+new Version('20.2.0+sha-9bdb4fe');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
