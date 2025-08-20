fe78e6d87dc5f9d62d29dcb612379ac596aa1a1e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:12:27 +0000

    009b068 build: reenable firefox tests for devtools (#63288)

--- schematics/bundles/project_tsconfig_paths-CS-eSeHC.cjs
+++ schematics/bundles/project_tsconfig_paths-CS-eSeHC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-1c5fb1e
+ * @license Angular v21.0.0-next.0+sha-009b068
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32817,7 +32817,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-1c5fb1e');
+new Version('21.0.0-next.0+sha-009b068');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
