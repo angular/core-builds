024a14c69fb2a90b7f651b3ea8e7f945a6ade319
Jan Martin <jan.krems@gmail.com>
2025-08-20 20:43:06 +0000

    62c00ab release: bump Angular DevTools version to 1.0.43 (#63299)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-edbbbb1
+ * @license Angular v21.0.0-next.0+sha-62c00ab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-edbbbb1');
+const VERSION = new Version('21.0.0-next.0+sha-62c00ab');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
