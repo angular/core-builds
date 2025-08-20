2ec16b0be660f53086f6b558a28699e0d8caeab8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 08:33:53 +0000

    c935d42 docs: remove redundant Angular docs link in tailwind guide (#63205)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-683ae03
+ * @license Angular v20.1.7+sha-c935d42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-683ae03');
+const VERSION = new Version('20.1.7+sha-c935d42');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-f3zDmoYh.cjs
+++ schematics/bundles/project_tsconfig_paths-f3zDmoYh.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.1.7+sha-683ae03
+ * @license Angular v20.1.7+sha-c935d42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32315,7 +32315,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.1.7+sha-683ae03');
+new Version('20.1.7+sha-c935d42');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
