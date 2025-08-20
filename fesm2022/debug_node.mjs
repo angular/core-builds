d46b53a2d12df6d399a85152f3dcd0d9ad8f5d61
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:19:57 +0000

    65c5a73 build: update @angular/ng-dev digest to c93dfc8 (#63290)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-9bdb4fe
+ * @license Angular v20.2.0+sha-65c5a73
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13515,7 +13515,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.0+sha-9bdb4fe']
+        ? ['ng-version', '20.2.0+sha-65c5a73']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
