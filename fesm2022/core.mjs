e877c4ca237de0f91e80a69a15c5b75db9764deb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-20 12:33:08 +0000

    9e172e0 release: cut the v20.2.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-878b99a
+ * @license Angular v20.2.0+sha-9e172e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-878b99a');
+const VERSION = new Version('20.2.0+sha-9e172e0');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-BIeidc3E.cjs
+++ schematics/bundles/project_tsconfig_paths-BIeidc3E.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-878b99a
+ * @license Angular v20.2.0+sha-9e172e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32815,7 +32815,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.1+sha-878b99a');
+new Version('20.2.0+sha-9e172e0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
