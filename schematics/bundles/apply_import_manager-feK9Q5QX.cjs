3c1fdd63bc3642f3a4360bc0377ed645eb9390ee
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:19:11 +0000

    9655ea7 build: update dependency playwright-core to v1.55.0 (#63273)

--- schematics/bundles/apply_import_manager-feK9Q5QX.cjs
+++ schematics/bundles/apply_import_manager-feK9Q5QX.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-d76a911
+ * @license Angular v20.2.0-rc.1+sha-9655ea7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-BIeidc3E.cjs
+++ schematics/bundles/project_tsconfig_paths-BIeidc3E.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0-rc.1+sha-d76a911
+ * @license Angular v20.2.0-rc.1+sha-9655ea7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32815,7 +32815,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0-rc.1+sha-d76a911');
+new Version('20.2.0-rc.1+sha-9655ea7');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
