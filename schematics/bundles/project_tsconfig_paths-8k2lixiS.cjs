5206ae29457c72695706173604b189b4667f704d
Andrew Kushnir <akushnir@google.com>
2025-08-21 17:20:23 +0000

    7d6ae95 docs(docs-infra): fix mermaid rendering (#63311)

--- schematics/bundles/project_tsconfig_paths-8k2lixiS.cjs
+++ schematics/bundles/project_tsconfig_paths-8k2lixiS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-0dd95c5
+ * @license Angular v21.0.0-next.0+sha-7d6ae95
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32834,7 +32834,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-0dd95c5');
+new Version('21.0.0-next.0+sha-7d6ae95');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
