dde719e36623f3504046968be4fca3fef3d98fb4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-08-21 10:56:28 +0000

    1c27fa9 release: cut the v20.2.1 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-a28672f
+ * @license Angular v20.2.1+sha-1c27fa9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0+sha-a28672f');
+const VERSION = new Version('20.2.1+sha-1c27fa9');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-COqholMT.cjs
+++ schematics/bundles/project_tsconfig_paths-COqholMT.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.0+sha-a28672f
+ * @license Angular v20.2.1+sha-1c27fa9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32832,7 +32832,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.0+sha-a28672f');
+new Version('20.2.1+sha-1c27fa9');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
