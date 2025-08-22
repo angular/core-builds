3dd81cd133fac2f12b8f1a6fb6a0f89b19a1bbec
Andrew Kushnir <akushnir@google.com>
2025-08-22 19:31:02 +0000

    983cc2f ci: add myself to review fw-general (#63337)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a842272
+ * @license Angular v21.0.0-next.0+sha-983cc2f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a842272');
+const VERSION = new Version('21.0.0-next.0+sha-983cc2f');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
