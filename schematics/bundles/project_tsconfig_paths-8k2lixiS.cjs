ba28b573020d17c5b4a9a5ec2702ae2fe930924f
Andrew Kushnir <akushnir@google.com>
2025-08-25 23:32:44 +0000

    67d4449 build: update cross-repo angular dependencies (#63379)

--- schematics/bundles/project_tsconfig_paths-8k2lixiS.cjs
+++ schematics/bundles/project_tsconfig_paths-8k2lixiS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-2fcafb6
+ * @license Angular v21.0.0-next.0+sha-67d4449
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32834,7 +32834,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-2fcafb6');
+new Version('21.0.0-next.0+sha-67d4449');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
