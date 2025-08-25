bbea1457370d40898be7d0c23a5b3e88c9c8fd9c
Andrew Kushnir <akushnir@google.com>
2025-08-25 22:48:27 +0000

    2fcafb6 build: rename defaults2.bzl to defaults.bzl (#63383)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-1febf52
+ * @license Angular v21.0.0-next.0+sha-2fcafb6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-1febf52');
+const VERSION = new Version('21.0.0-next.0+sha-2fcafb6');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
