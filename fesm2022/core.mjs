08f929fc103a1ad16fe4b35c31a47aafad25c797
Andrew Kushnir <akushnir@google.com>
2025-08-25 22:43:22 +0000

    1febf52 build: update dependency @bazel/ibazel to v0.26.10 (#63373)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-fca6122
+ * @license Angular v21.0.0-next.0+sha-1febf52
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-fca6122');
+const VERSION = new Version('21.0.0-next.0+sha-1febf52');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-8k2lixiS.cjs
+++ schematics/bundles/project_tsconfig_paths-8k2lixiS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-fca6122
+ * @license Angular v21.0.0-next.0+sha-1febf52
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32834,7 +32834,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-fca6122');
+new Version('21.0.0-next.0+sha-1febf52');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
