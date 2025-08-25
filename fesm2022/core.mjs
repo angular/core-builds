a895987633bb1d29c10bc1dc34e1e0e584053290
Andrew Kushnir <akushnir@google.com>
2025-08-25 22:39:25 +0000

    fca6122 build: lock file maintenance (#63359)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-5a11106
+ * @license Angular v21.0.0-next.0+sha-fca6122
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-5a11106');
+const VERSION = new Version('21.0.0-next.0+sha-fca6122');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-8k2lixiS.cjs
+++ schematics/bundles/project_tsconfig_paths-8k2lixiS.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.0+sha-5a11106
+ * @license Angular v21.0.0-next.0+sha-fca6122
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32834,7 +32834,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.0+sha-5a11106');
+new Version('21.0.0-next.0+sha-fca6122');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
