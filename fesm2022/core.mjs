75ce2e2fa379406f694f227b3b66384ff139d2d7
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:45:38 +0000

    aff8bb2 refactor(devtools): provide signalGraphEnabled via settings service (#63374)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-67d4449
+ * @license Angular v21.0.0-next.0+sha-aff8bb2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-67d4449');
+const VERSION = new Version('21.0.0-next.0+sha-aff8bb2');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-67d4449
+ * @license Angular v21.0.0-next.0+sha-aff8bb2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13516,7 +13516,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.0+sha-67d4449']
+        ? ['ng-version', '21.0.0-next.0+sha-aff8bb2']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
