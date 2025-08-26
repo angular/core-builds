4f303ad4ed8c8b83f9233a539a016d37dc8f9782
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:44:37 +0000

    c9b0f45 fix(devtools): prevent profiler bars flickering after change detection (#63350)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-15395a5
+ * @license Angular v20.2.1+sha-c9b0f45
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-15395a5');
+const VERSION = new Version('20.2.1+sha-c9b0f45');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/property_name-BBwFuqMe.cjs
+++ schematics/bundles/property_name-BBwFuqMe.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.1+sha-15395a5
+ * @license Angular v20.2.1+sha-c9b0f45
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
