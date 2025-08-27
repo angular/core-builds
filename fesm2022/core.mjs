ac1d27152297e1e2032b48057fdb22520348ff5c
Miles Malerba <mmalerba@google.com>
2025-08-27 23:16:29 +0000

    58cb2be build: update cross-repo angular dependencies (#63395)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1bb30c7
+ * @license Angular v21.0.0-next.1+sha-58cb2be
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-1bb30c7');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-58cb2be');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-D7xzGqRi.cjs
+++ schematics/bundles/project_tsconfig_paths-D7xzGqRi.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.1+sha-1bb30c7
+ * @license Angular v21.0.0-next.1+sha-58cb2be
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.1+sha-1bb30c7');
+new Version('21.0.0-next.1+sha-58cb2be');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
