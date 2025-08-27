65f57665cd7ad7db927326f02ddd2fa1bb074346
Miles Malerba <mmalerba@google.com>
2025-08-27 23:36:59 +0000

    0d57dfb docs: update Angular CLI help (#63419)

--- schematics/bundles/project_tsconfig_paths-D7xzGqRi.cjs
+++ schematics/bundles/project_tsconfig_paths-D7xzGqRi.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.1+sha-059e0d3
+ * @license Angular v21.0.0-next.1+sha-0d57dfb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.1+sha-059e0d3');
+new Version('21.0.0-next.1+sha-0d57dfb');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
