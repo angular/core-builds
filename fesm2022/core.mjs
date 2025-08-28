93b82430c0d38b86a03c2284374098f4b326a513
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:05:55 +0000

    421edfe build: update @bazel/ibazel to `0.26.10` (#63435)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-87bfe16
+ * @license Angular v20.2.2+sha-421edfe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.2.2+sha-87bfe16');
+const VERSION = /* @__PURE__ */ new Version('20.2.2+sha-421edfe');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-BDoOg2Cc.cjs
+++ schematics/bundles/project_tsconfig_paths-BDoOg2Cc.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.2+sha-87bfe16
+ * @license Angular v20.2.2+sha-421edfe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32838,7 +32838,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.2+sha-87bfe16');
+new Version('20.2.2+sha-421edfe');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
