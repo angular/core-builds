6c6c9ecaf6f5a1b40e0af7a70188cadeab65d063
Doug Parker <dgp1130@users.noreply.github.com>
2025-08-28 17:39:54 +0000

    147c743 release: bump Angular DevTools to 1.0.44 (#63452)

--- schematics/bundles/project_tsconfig_paths-D7xzGqRi.cjs
+++ schematics/bundles/project_tsconfig_paths-D7xzGqRi.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.1+sha-c9b215b
+ * @license Angular v21.0.0-next.1+sha-147c743
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.1+sha-c9b215b');
+new Version('21.0.0-next.1+sha-147c743');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signals.cjs
+++ schematics/bundles/signals.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.1+sha-c9b215b
+ * @license Angular v21.0.0-next.1+sha-147c743
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
