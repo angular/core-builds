a5494dfaf76be24facd3c256f11aa5970899883e
Miles Malerba <mmalerba@google.com>
2025-08-28 16:46:25 +0000

    2f5f975 refactor(forms): fix dependencies (#63451)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1f4c5f7
+ * @license Angular v21.0.0-next.1+sha-2f5f975
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-1f4c5f7');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-2f5f975');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
