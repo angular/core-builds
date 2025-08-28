0c47d932b3a896d8435ca56391fa8d6972267e61
Miles Malerba <mmalerba@google.com>
2025-08-28 16:25:42 +0000

    98468c0 build: update cross-repo angular dependencies (#63444)

--- schematics/bundles/project_tsconfig_paths-BDoOg2Cc.cjs
+++ schematics/bundles/project_tsconfig_paths-BDoOg2Cc.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.2+sha-cef1449
+ * @license Angular v20.2.2+sha-98468c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32838,7 +32838,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.2+sha-cef1449');
+new Version('20.2.2+sha-98468c0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
