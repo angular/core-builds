6bf2709a30d95ee5c77fb5342872f32ca96157af
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:49:09 +0000

    1abf6d5 build: update cross-repo angular dependencies to 06d3af5 (#63476)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-c357a03
+ * @license Angular v20.2.3+sha-1abf6d5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13521,7 +13521,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.3+sha-c357a03']
+        ? ['ng-version', '20.2.3+sha-1abf6d5']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-x7xrmbI9.cjs
+++ schematics/bundles/project_tsconfig_paths-x7xrmbI9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.3+sha-c357a03
+ * @license Angular v20.2.3+sha-1abf6d5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32838,7 +32838,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.3+sha-c357a03');
+new Version('20.2.3+sha-1abf6d5');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
