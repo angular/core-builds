4f210c3140c05c84fb0c452c0c7533f80b0c9723
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:05:52 +0000

    c357a03 docs(docs-infra): makes signal readonly (#63449)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-64ddde2
+ * @license Angular v20.2.3+sha-c357a03
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.2.3+sha-64ddde2');
+const VERSION = /* @__PURE__ */ new Version('20.2.3+sha-c357a03');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-x7xrmbI9.cjs
+++ schematics/bundles/project_tsconfig_paths-x7xrmbI9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.3+sha-64ddde2
+ * @license Angular v20.2.3+sha-c357a03
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32838,7 +32838,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.3+sha-64ddde2');
+new Version('20.2.3+sha-c357a03');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
