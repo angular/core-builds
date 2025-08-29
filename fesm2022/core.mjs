1fde13456061a0f0cad90999db0d2524a2578d94
Miles Malerba <mmalerba@google.com>
2025-08-29 22:15:00 +0000

    16fa983 ci: add more team members as approvers (#63495)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-db95a5c
+ * @license Angular v21.0.0-next.1+sha-16fa983
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-db95a5c');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-16fa983');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-db95a5c
+ * @license Angular v21.0.0-next.1+sha-16fa983
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13522,7 +13522,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.1+sha-db95a5c']
+        ? ['ng-version', '21.0.0-next.1+sha-16fa983']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
