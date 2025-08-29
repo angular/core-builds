3b9e86070fd28b84ff0ab0f9960609bbb48baee7
Miles Malerba <mmalerba@google.com>
2025-08-29 19:05:23 +0000

    bbb46e5 refactor(router): Add `firstValueFrom` helper (#63485)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-6e1599d
+ * @license Angular v21.0.0-next.1+sha-bbb46e5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13522,7 +13522,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.1+sha-6e1599d']
+        ? ['ng-version', '21.0.0-next.1+sha-bbb46e5']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
