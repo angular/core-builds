127fb3ac2af0152ee505b3dda3bd74ed4b68248b
Miles Malerba <mmalerba@google.com>
2025-09-02 16:47:13 +0000

    e63608d docs(docs-infra): signal property should be readonly (#63166)

--- effect.d.d.ts
+++ effect.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-668ced9
+ * @license Angular v21.0.0-next.1+sha-e63608d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-668ced9
+ * @license Angular v21.0.0-next.1+sha-e63608d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-668ced9');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-e63608d');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
