6d5e39fbbebed4abcd054fc6e9ebb597cd69fe3f
Miles Malerba <mmalerba@google.com>
2025-09-02 17:13:43 +0000

    957b367 build: update github/codeql-action action to v3.30.0 (#63540)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-c664af8
+ * @license Angular v20.2.3+sha-957b367
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.2.3+sha-c664af8');
+const VERSION = /* @__PURE__ */ new Version('20.2.3+sha-957b367');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-x7xrmbI9.cjs
+++ schematics/bundles/project_tsconfig_paths-x7xrmbI9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.3+sha-c664af8
+ * @license Angular v20.2.3+sha-957b367
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32838,7 +32838,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.3+sha-c664af8');
+new Version('20.2.3+sha-957b367');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
