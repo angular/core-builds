1ff1923b86aa23fe0ec6e8423436de7c6773c95f
Miles Malerba <mmalerba@google.com>
2025-09-02 16:40:00 +0000

    668ced9 docs(docs-infra): export class (#63506)

--- primitives/event-dispatch/index.d.ts
+++ primitives/event-dispatch/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1541606
+ * @license Angular v21.0.0-next.1+sha-668ced9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/symbol-VPWguRxr.cjs
+++ schematics/bundles/symbol-VPWguRxr.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.1+sha-1541606
+ * @license Angular v21.0.0-next.1+sha-668ced9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
