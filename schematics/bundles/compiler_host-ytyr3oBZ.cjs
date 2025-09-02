aa45c00f589d6d27eb369693668de50c241b9675
Miles Malerba <mmalerba@google.com>
2025-09-02 16:54:18 +0000

    7aa9350 build: use cldr typings (#63519)

--- schematics/bundles/compiler_host-ytyr3oBZ.cjs
+++ schematics/bundles/compiler_host-ytyr3oBZ.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.1+sha-e63608d
+ * @license Angular v21.0.0-next.1+sha-7aa9350
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
