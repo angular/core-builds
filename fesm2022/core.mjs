b696e3435a2290e0e0db7db313be83bd8ecd5791
Miles Malerba <mmalerba@google.com>
2025-09-02 17:11:57 +0000

    1e6d311 build: update cross-repo angular dependencies (#63548)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-7aa9350
+ * @license Angular v21.0.0-next.1+sha-1e6d311
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-7aa9350');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.1+sha-1e6d311');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/self-closing-tags-migration.cjs
+++ schematics/bundles/self-closing-tags-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.1+sha-7aa9350
+ * @license Angular v21.0.0-next.1+sha-1e6d311
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
