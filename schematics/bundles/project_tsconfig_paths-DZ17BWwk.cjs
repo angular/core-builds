8d3da903ea9faf03ad4fed6c7b677d5299a8fb9c
Andrew Scott <atscott@google.com>
2025-09-05 21:04:10 +0000

    bacda4f refactor(forms): add support for binding number fields to date inputs (#63585)

--- schematics/bundles/project_tsconfig_paths-DZ17BWwk.cjs
+++ schematics/bundles/project_tsconfig_paths-DZ17BWwk.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-c0791e1
+ * @license Angular v21.0.0-next.2+sha-bacda4f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.2+sha-c0791e1');
+new Version('21.0.0-next.2+sha-bacda4f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signal-queries-migration.cjs
+++ schematics/bundles/signal-queries-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-c0791e1
+ * @license Angular v21.0.0-next.2+sha-bacda4f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
