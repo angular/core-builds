c71db6501032b534b81ee1c891c8e7e87f8a2d4d
Andrew Scott <atscott@google.com>
2025-09-05 19:57:14 +0000

    f560536 test: increase mermaid spec timeout to reduce flakes (#63610)

--- schematics/bundles/project_tsconfig_paths-DZ17BWwk.cjs
+++ schematics/bundles/project_tsconfig_paths-DZ17BWwk.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-8401f89
+ * @license Angular v21.0.0-next.2+sha-f560536
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.2+sha-8401f89');
+new Version('21.0.0-next.2+sha-f560536');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
