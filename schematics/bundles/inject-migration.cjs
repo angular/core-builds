781e53022835ebd4d603db558dcefdfbd76dd632
Andrew Scott <atscott@google.com>
2025-09-08 19:56:00 +0000

    8ab078a docs: update template examples to use signal-based viewChild instead of decorator-based @ViewChild (#63630)

--- schematics/bundles/inject-migration.cjs
+++ schematics/bundles/inject-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-1078954
+ * @license Angular v21.0.0-next.2+sha-8ab078a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-DZ17BWwk.cjs
+++ schematics/bundles/project_tsconfig_paths-DZ17BWwk.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-1078954
+ * @license Angular v21.0.0-next.2+sha-8ab078a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.2+sha-1078954');
+new Version('21.0.0-next.2+sha-8ab078a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
