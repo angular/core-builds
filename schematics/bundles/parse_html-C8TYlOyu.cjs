3ada9388d7585bf9d8631f40a802f4af291e5a9a
Andrew Scott <atscott@google.com>
2025-09-08 19:13:49 +0000

    1f21de8 docs: improve headings of docs (#63604)

--- schematics/bundles/parse_html-C8TYlOyu.cjs
+++ schematics/bundles/parse_html-C8TYlOyu.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-160e065
+ * @license Angular v21.0.0-next.2+sha-1f21de8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
