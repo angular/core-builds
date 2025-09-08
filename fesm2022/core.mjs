48cac59c58de4c11218d4050d4c62e55d79764b4
Andrew Scott <atscott@google.com>
2025-09-08 17:18:12 +0000

    f2ec6fb docs(docs-infra): quote title attributes in links (#63578)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f18980c
+ * @license Angular v20.2.4+sha-f2ec6fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,7 +626,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.2.4+sha-f18980c');
+const VERSION = /* @__PURE__ */ new Version('20.2.4+sha-f2ec6fb');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-Cj3oe5Ih.cjs
+++ schematics/bundles/project_tsconfig_paths-Cj3oe5Ih.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.2.4+sha-f18980c
+ * @license Angular v20.2.4+sha-f2ec6fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32838,7 +32838,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.2.4+sha-f18980c');
+new Version('20.2.4+sha-f2ec6fb');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
