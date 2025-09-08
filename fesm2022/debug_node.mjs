e17071dffff88ec481728475919d19849e670ceb
Andrew Scott <atscott@google.com>
2025-09-08 23:35:08 +0000

    c7b84f5 docs: Update Language Service IDE list (#63647)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a857f72
+ * @license Angular v20.2.4+sha-c7b84f5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13521,7 +13521,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.4+sha-a857f72']
+        ? ['ng-version', '20.2.4+sha-c7b84f5']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
