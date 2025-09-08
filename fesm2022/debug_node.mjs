323c6b4845a6eeb72db37b46367487b8871f1d8d
Andrew Scott <atscott@google.com>
2025-09-08 18:21:24 +0000

    160e065 feat(devtools): store last selected tab in settings (#63594)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c6711a1
+ * @license Angular v21.0.0-next.2+sha-160e065
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13522,7 +13522,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.2+sha-c6711a1']
+        ? ['ng-version', '21.0.0-next.2+sha-160e065']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
