9ee75940f64c1ccccf1653687e147eecccd8d962
Andrew Scott <atscott@google.com>
2025-09-08 21:26:05 +0000

    c8c4399 docs: remove installation (#63508)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-00a1806
+ * @license Angular v21.0.0-next.2+sha-c8c4399
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.2+sha-00a1806');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.2+sha-c8c4399');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
--- schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
+++ schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-00a1806
+ * @license Angular v21.0.0-next.2+sha-c8c4399
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.2+sha-00a1806');
+new Version('21.0.0-next.2+sha-c8c4399');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
