4a78e86691216967818b834dfc753dbf67bf7d7a
Andrew Scott <atscott@google.com>
2025-09-09 20:00:55 +0000

    0214e98 docs: add signals tutorial (#62750)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f50b83f
+ * @license Angular v20.2.4+sha-0214e98
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13521,7 +13521,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.2.4+sha-f50b83f']
+        ? ['ng-version', '20.2.4+sha-0214e98']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
