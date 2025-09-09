7bcedc157bc068db3eda2b2f67c4685255e6a986
Andrew Scott <atscott@google.com>
2025-09-09 17:48:53 +0000

    6117cce feat(core): introduce `BootstrapContext` for improved server bootstrapping (#63636)

--- schematics/bundles/project_tsconfig_paths-RAvhwg8f.cjs
+++ schematics/bundles/project_tsconfig_paths-RAvhwg8f.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0-next.0+sha-90e798b
+ * @license Angular v20.3.0-next.0+sha-6117cce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32838,7 +32838,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('20.3.0-next.0+sha-90e798b');
+new Version('20.3.0-next.0+sha-6117cce');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
