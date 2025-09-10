9f8ce474d27c48c4cdc9f6bb7193ead4828e7727
Andrew Scott <atscott01@gmail.com>
2025-09-10 15:11:32 +0000

    9586d48 docs: release notes for the v20.3.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-0102575
+ * @license Angular v21.0.0-next.2+sha-9586d48
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.2+sha-0102575');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.2+sha-9586d48');
 
 function compileNgModuleFactory(injector, options, moduleType) {
     ngDevMode && assertNgModuleType(moduleType);
