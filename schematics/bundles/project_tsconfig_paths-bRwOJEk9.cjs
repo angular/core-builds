270787e36a29cb0f826b5971878e9962a95c00c4
Andrew Scott <atscott@google.com>
2025-09-10 17:34:21 +0000

    9487dc6 build: update `@angular/ng-dev` to latest SHA (#63696)

--- schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
+++ schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-48fee39
+ * @license Angular v21.0.0-next.3+sha-9487dc6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.3+sha-48fee39');
+new Version('21.0.0-next.3+sha-9487dc6');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
