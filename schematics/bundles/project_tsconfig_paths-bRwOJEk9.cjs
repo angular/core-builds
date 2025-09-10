394442d0c8c942dee1b30e56916a7509da71df06
Andrew Scott <atscott@google.com>
2025-09-10 20:33:42 +0000

    fb3082f build: fix symbol tests (#63709)

--- schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
+++ schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-f642fcf
+ * @license Angular v21.0.0-next.3+sha-fb3082f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.3+sha-f642fcf');
+new Version('21.0.0-next.3+sha-fb3082f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
