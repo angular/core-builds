fef3b3959d9f4ffc3642940235e94bbe50b978bc
Andrew Scott <atscott@google.com>
2025-09-10 14:15:30 +0000

    7569da1 build: update all non-major dependencies (#63661)

--- schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
+++ schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-0c64a06
+ * @license Angular v21.0.0-next.2+sha-7569da1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.2+sha-0c64a06');
+new Version('21.0.0-next.2+sha-7569da1');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
