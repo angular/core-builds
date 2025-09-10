c8e9776d0f03fdb31a79579e0ef4775eaa103078
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-10 23:05:06 +0000

    d9483fa docs(docs-infra): enable using Tailwind CSS in code examples (#63583)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-7a4b225
+ * @license Angular v21.0.0-next.3+sha-d9483fa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.3+sha-7a4b225');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.3+sha-d9483fa');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
+++ schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-7a4b225
+ * @license Angular v21.0.0-next.3+sha-d9483fa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-7a4b225');
+const VERSION = new Version('21.0.0-next.3+sha-d9483fa');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
