9d671560460abf304a3cbdec6f81e089d1477aa8
Andrew Scott <atscott01@gmail.com>
2025-09-10 15:16:11 +0000

    687b301 release: cut the v21.0.0-next.3 release

--- schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
+++ schematics/bundles/project_tsconfig_paths-bRwOJEk9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.2+sha-9586d48
+ * @license Angular v21.0.0-next.3+sha-687b301
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32840,7 +32840,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-new Version('21.0.0-next.2+sha-9586d48');
+new Version('21.0.0-next.3+sha-687b301');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
