e8b1ed927bb99aa5573e7bf1e255933a212e4d35
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 16:41:20 +0000

    19c2f9c build: update cross-repo angular dependencies (#63721)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-5a1c7ee
+ * @license Angular v21.0.0-next.3+sha-19c2f9c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13680,7 +13680,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-5a1c7ee']
+        ? ['ng-version', '21.0.0-next.3+sha-19c2f9c']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
