87daae6d5d3193d1e39a07d62833f2224fe266e7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:40:14 +0000

    32ff9eb build: mark `pnpapi` as external for esbuild (#63723)

--- schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
+++ schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-c7fd7f9
+ * @license Angular v20.3.0+sha-32ff9eb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-c7fd7f9');
+const VERSION = new Version('20.3.0+sha-32ff9eb');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
