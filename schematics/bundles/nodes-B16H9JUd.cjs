d0c2c8acaa28ca07412cce0ea940d42223c4265b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 21:54:32 +0000

    ff6f152 docs: add di synonym (#63743)

--- schematics/bundles/nodes-B16H9JUd.cjs
+++ schematics/bundles/nodes-B16H9JUd.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-334f489
+ * @license Angular v21.0.0-next.3+sha-ff6f152
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
+++ schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-334f489
+ * @license Angular v21.0.0-next.3+sha-ff6f152
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-334f489');
+const VERSION = new Version('21.0.0-next.3+sha-ff6f152');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
