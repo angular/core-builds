440f66c6b096fae3442535d48676e8ed9b3209fd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:45:16 +0000

    a8fae2a build: update all non-major dependencies (#63715)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-37e1eda
+ * @license Angular v21.0.0-next.3+sha-a8fae2a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13680,7 +13680,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-37e1eda']
+        ? ['ng-version', '21.0.0-next.3+sha-a8fae2a']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
