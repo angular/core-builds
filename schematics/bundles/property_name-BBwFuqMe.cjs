2a8be978923f974ea8c45af0d57795f6bf625bb3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 21:23:09 +0000

    334f489 build: update cross-repo angular dependencies to v21.0.0-next.3 (#63742)

--- schematics/bundles/property_name-BBwFuqMe.cjs
+++ schematics/bundles/property_name-BBwFuqMe.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-cbcfe94
+ * @license Angular v21.0.0-next.3+sha-334f489
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
