ddcfa138c6e7a58f3c229996da0557a348608bc2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 16:48:10 +0000

    a686e34 docs: update Angular CLI help (#63736)

--- schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
+++ schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-b7bc110
+ * @license Angular v20.3.0+sha-a686e34
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-b7bc110');
+const VERSION = new Version('20.3.0+sha-a686e34');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/self-closing-tags-migration.cjs
+++ schematics/bundles/self-closing-tags-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-b7bc110
+ * @license Angular v20.3.0+sha-a686e34
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
