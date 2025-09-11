7c2d433b4057077135ea57fa0d21c48d63f26bdc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:44:10 +0000

    c36cd73 ci: correctly set branchName for the CDK (#63718)

--- schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
+++ schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-32ff9eb
+ * @license Angular v20.3.0+sha-c36cd73
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-32ff9eb');
+const VERSION = new Version('20.3.0+sha-c36cd73');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
