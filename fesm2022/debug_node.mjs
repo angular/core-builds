a04665c6a5aaacc47a454e7ef0c4b2388b8c913c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 17:44:17 +0000

    cbcfe94 docs: Adds guide for extending Angular service worker (#63629)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-bd54106
+ * @license Angular v21.0.0-next.3+sha-cbcfe94
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13680,7 +13680,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-bd54106']
+        ? ['ng-version', '21.0.0-next.3+sha-cbcfe94']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
