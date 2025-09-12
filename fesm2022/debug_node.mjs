7a57a9b40af63beb15a8b13c9e66be4db11fccb7
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-09-12 07:54:43 +0000

    185ac24 docs: fix links and remove second banner (#63755)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-e5de410
+ * @license Angular v20.3.0+sha-185ac24
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13679,7 +13679,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.0+sha-e5de410']
+        ? ['ng-version', '20.3.0+sha-185ac24']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
+++ schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-e5de410
+ * @license Angular v20.3.0+sha-185ac24
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-e5de410');
+const VERSION = new Version('20.3.0+sha-185ac24');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
