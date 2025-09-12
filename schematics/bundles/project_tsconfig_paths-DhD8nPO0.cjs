b2e34408ff49abc86c86062f6519cb745f6db577
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 17:25:02 +0000

    284fc7d build: update cross-repo angular dependencies (#63767)

--- schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
+++ schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-f8cea65
+ * @license Angular v21.0.0-next.3+sha-284fc7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-f8cea65');
+const VERSION = new Version('21.0.0-next.3+sha-284fc7d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
