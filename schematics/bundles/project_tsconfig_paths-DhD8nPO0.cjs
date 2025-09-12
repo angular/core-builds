c5c1527df3a0108545145121e06ceffff66f4adf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:54:49 +0000

    26b55c1 docs: update Creating service with CLI (#63714)

--- schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
+++ schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-a4001c4
+ * @license Angular v21.0.0-next.3+sha-26b55c1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-a4001c4');
+const VERSION = new Version('21.0.0-next.3+sha-26b55c1');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
