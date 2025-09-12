f23229ffee5e6e10a2e4067df9ce0845a4426afa
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-09-12 07:54:39 +0000

    a7b0620 docs: fix links and remove second banner (#63755)

--- schematics/bundles/add-bootstrap-context-to-server-main.cjs
+++ schematics/bundles/add-bootstrap-context-to-server-main.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-ff6f152
+ * @license Angular v21.0.0-next.3+sha-a7b0620
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
