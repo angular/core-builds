9ab82fd5442337cb5a2c008013c903b30295081a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 17:48:20 +0000

    bd2a84b build: update pnpm to v10.16.0 (#63769)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-284fc7d
+ * @license Angular v21.0.0-next.3+sha-bd2a84b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13687,7 +13687,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-284fc7d']
+        ? ['ng-version', '21.0.0-next.3+sha-bd2a84b']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-284fc7d
+ * @license Angular v21.0.0-next.3+sha-bd2a84b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.3+sha-284fc7d');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.3+sha-bd2a84b');
 
 /**
  * Base URL for the error details page.
