19666241d0928b3c1ff0eb80bd359b3d7c324a41
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 14:47:00 +0000

    d191926 build: update pnpm to v10.16.1 (#63794)

--- schematics/bundles/control-flow-migration.cjs
+++ schematics/bundles/control-flow-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-2ebdd57
+ * @license Angular v20.3.0+sha-d191926
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
+++ schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-2ebdd57
+ * @license Angular v20.3.0+sha-d191926
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-2ebdd57');
+const VERSION = new Version('20.3.0+sha-d191926');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
