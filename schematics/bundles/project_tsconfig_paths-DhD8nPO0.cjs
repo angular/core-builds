90464e93208229189aedf0833f2b615d36b9258e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:07:01 +0000

    2b440f4 docs: correct typos in AI design patterns guide (#63788)

--- schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
+++ schematics/bundles/project_tsconfig_paths-DhD8nPO0.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-2327c3b
+ * @license Angular v21.0.0-next.3+sha-2b440f4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-2327c3b');
+const VERSION = new Version('21.0.0-next.3+sha-2b440f4');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
