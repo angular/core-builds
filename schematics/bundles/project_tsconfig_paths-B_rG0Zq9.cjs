6bfbdfcc107710c6cc88830fb729024146fabcf1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:29:13 +0000

    13e4255 fix(docs-infra): fix guide content width (#63798)

--- schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
+++ schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-c536035
+ * @license Angular v20.3.0+sha-13e4255
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-c536035');
+const VERSION = new Version('20.3.0+sha-13e4255');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signals.cjs
+++ schematics/bundles/signals.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-c536035
+ * @license Angular v20.3.0+sha-13e4255
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
