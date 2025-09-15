04a9f6cb6903cdaea900fd8b94f037d04ab902ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:49:47 +0000

    233094e docs: fix detail about `linkedSignal`'s previous value. (#63811)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-b05552f
+ * @license Angular v20.3.0+sha-233094e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13686,7 +13686,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.0+sha-b05552f']
+        ? ['ng-version', '20.3.0+sha-233094e']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
