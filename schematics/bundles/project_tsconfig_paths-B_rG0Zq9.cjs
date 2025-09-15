9eca013bd3658ed7efa462a1c314db403f030d1d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:12:35 +0000

    a2ea2ec docs: Update outputs.md to demonstrate how to use event data (#63732) PR Close #63732

--- schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
+++ schematics/bundles/project_tsconfig_paths-B_rG0Zq9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-cd36189
+ * @license Angular v20.3.0+sha-a2ea2ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-cd36189');
+const VERSION = new Version('20.3.0+sha-a2ea2ec');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signal-input-migration.cjs
+++ schematics/bundles/signal-input-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-cd36189
+ * @license Angular v20.3.0+sha-a2ea2ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
