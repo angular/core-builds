0c4b11503580ba57e8fdb742cee893d67f1cbada
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:06:48 +0000

    c536035 docs: correct typos in AI design patterns guide (#63788)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-d191926
+ * @license Angular v20.3.0+sha-c536035
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13686,7 +13686,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.0+sha-d191926']
+        ? ['ng-version', '20.3.0+sha-c536035']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/add-bootstrap-context-to-server-main.cjs
+++ schematics/bundles/add-bootstrap-context-to-server-main.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-d191926
+ * @license Angular v20.3.0+sha-c536035
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
