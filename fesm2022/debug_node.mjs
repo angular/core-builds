10da34b4e8907709e5ebdb5682bfa04fde4eba4e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:38:25 +0000

    d2163f3 test(platform-server): add test for transfer state data in nested defer blocks (#63801)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-fa69d33
+ * @license Angular v21.0.0-next.3+sha-d2163f3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13687,7 +13687,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-fa69d33']
+        ? ['ng-version', '21.0.0-next.3+sha-d2163f3']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
