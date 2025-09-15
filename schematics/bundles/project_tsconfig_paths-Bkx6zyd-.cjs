9fb70f72953c17e044b3fbdc3d8ed08fc10897af
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 21:16:14 +0000

    7e0cf41 refactor(core): wrap `checkNoChanges` body in `if (ngDevMode)` for better tree-shaking (#63387)

--- schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
+++ schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-321eecc
+ * @license Angular v21.0.0-next.3+sha-7e0cf41
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-321eecc');
+const VERSION = new Version('21.0.0-next.3+sha-7e0cf41');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
