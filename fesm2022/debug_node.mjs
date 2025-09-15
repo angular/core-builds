8c4c46b2287f216d9bed61c035032143cd17b05a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:07:23 +0000

    de8be97 ci: update pullapprove public-api rules (#63737)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-d201cd2
+ * @license Angular v21.0.0-next.3+sha-de8be97
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13687,7 +13687,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-d201cd2']
+        ? ['ng-version', '21.0.0-next.3+sha-de8be97']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
