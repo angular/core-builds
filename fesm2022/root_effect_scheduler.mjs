7dc2c2baa6a575d27931ade092d0914b6175b098
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:07:16 +0000

    cd36189 ci: update pullapprove public-api rules (#63737)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-38c9921
+ * @license Angular v20.3.0+sha-cd36189
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.0+sha-38c9921');
+const VERSION = /* @__PURE__ */ new Version('20.3.0+sha-cd36189');
 
 /**
  * Base URL for the error details page.
