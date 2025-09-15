ee5cddbd50a01d7a5398da065b49ca43fbeb0f51
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 20:39:50 +0000

    321eecc docs(benchpress): Remove TODOs in Statistic (#63534)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-8dc8914
+ * @license Angular v21.0.0-next.3+sha-321eecc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13687,7 +13687,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-8dc8914']
+        ? ['ng-version', '21.0.0-next.3+sha-321eecc']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
+++ schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-8dc8914
+ * @license Angular v21.0.0-next.3+sha-321eecc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-8dc8914');
+const VERSION = new Version('21.0.0-next.3+sha-321eecc');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
