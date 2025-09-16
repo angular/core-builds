c17d70c60c582d5a3291298aa119efc501707d3b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 16:25:17 +0000

    d53ac26 docs(docs-infra): new home page (#63662)

--- schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
+++ schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-4133b08
+ * @license Angular v21.0.0-next.3+sha-d53ac26
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-4133b08');
+const VERSION = new Version('21.0.0-next.3+sha-d53ac26');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
