d96981591c70f5a4863b6543ab6ed163b8e50af7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 17:40:37 +0000

    f612437 docs(docs-infra): Show package name for API references (#63831)

--- schematics/bundles/project_tsconfig_paths-hIclAxSD.cjs
+++ schematics/bundles/project_tsconfig_paths-hIclAxSD.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-83dbf0e
+ * @license Angular v20.3.0+sha-f612437
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-83dbf0e');
+const VERSION = new Version('20.3.0+sha-f612437');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
