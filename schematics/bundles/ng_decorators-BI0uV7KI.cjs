f9231ecfd664ddc1f4788805879cda143ee2ab70
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 22:20:39 +0000

    0d028e0 refactor(platform-browser): Remove zonejs compatibility detector (#63847)

--- schematics/bundles/ng_decorators-BI0uV7KI.cjs
+++ schematics/bundles/ng_decorators-BI0uV7KI.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-1fec2a2
+ * @license Angular v21.0.0-next.3+sha-0d028e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
+++ schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-1fec2a2
+ * @license Angular v21.0.0-next.3+sha-0d028e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-1fec2a2');
+const VERSION = new Version('21.0.0-next.3+sha-0d028e0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
