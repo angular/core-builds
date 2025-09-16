e4f47c2f88798311e32f959bda9a662aa860a807
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 14:57:34 +0000

    7eb5f9a docs: remove leading `$` from script examples (#63795)

--- schematics/bundles/project_tsconfig_paths-hIclAxSD.cjs
+++ schematics/bundles/project_tsconfig_paths-hIclAxSD.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-4a23f6e
+ * @license Angular v20.3.0+sha-7eb5f9a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-4a23f6e');
+const VERSION = new Version('20.3.0+sha-7eb5f9a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
