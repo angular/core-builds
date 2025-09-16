e7ee45675b63c2e51d05b6ec2822d2e14cca0270
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 20:52:18 +0000

    4cd56c2 build: remove bazel flag usage for adev (#63845)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-dd6e1c3
+ * @license Angular v20.3.0+sha-4cd56c2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13691,7 +13691,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.0+sha-dd6e1c3']
+        ? ['ng-version', '20.3.0+sha-4cd56c2']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-dd6e1c3
+ * @license Angular v20.3.0+sha-4cd56c2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.0+sha-dd6e1c3');
+const VERSION = /* @__PURE__ */ new Version('20.3.0+sha-4cd56c2');
 
 /**
  * Base URL for the error details page.
