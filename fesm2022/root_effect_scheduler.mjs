4f3036d0dd0d0d814aa7d001a28adf45318a1020
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:13:48 +0000

    a943e7c refactor(core): Update async-test.ts to use globalThis polyfill (#57505)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-5ce4d95
+ * @license Angular v21.0.0-next.3+sha-a943e7c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.3+sha-5ce4d95');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.3+sha-a943e7c');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
+++ schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-5ce4d95
+ * @license Angular v21.0.0-next.3+sha-a943e7c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-5ce4d95');
+const VERSION = new Version('21.0.0-next.3+sha-a943e7c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
