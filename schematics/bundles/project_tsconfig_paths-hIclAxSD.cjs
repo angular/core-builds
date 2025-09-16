186137b6d7539ade7f460afbacf7b2e3724e8396
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:13:35 +0000

    27b2a4a refactor(core): Update async-test.ts to use globalThis polyfill (#57505)

--- schematics/bundles/project_tsconfig_paths-hIclAxSD.cjs
+++ schematics/bundles/project_tsconfig_paths-hIclAxSD.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-c80d9ba
+ * @license Angular v20.3.0+sha-27b2a4a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.0+sha-c80d9ba');
+const VERSION = new Version('20.3.0+sha-27b2a4a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
