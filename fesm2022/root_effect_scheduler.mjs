8aa3c91921532a718f313288b75117b5455d519e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-09-17 20:58:19 +0000

    2a15d78 release: cut the v20.3.1 release

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-669c7c0
+ * @license Angular v20.3.1+sha-2a15d78
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.0+sha-669c7c0');
+const VERSION = /* @__PURE__ */ new Version('20.3.1+sha-2a15d78');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/migrate_ts_type_references-Ctl6-8qO.cjs
+++ schematics/bundles/migrate_ts_type_references-Ctl6-8qO.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-669c7c0
+ * @license Angular v20.3.1+sha-2a15d78
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
