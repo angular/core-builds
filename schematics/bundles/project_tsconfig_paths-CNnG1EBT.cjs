17f40f16db6c2981d89ace57f6d712f71dec4170
Jan Martin <jan.krems@gmail.com>
2025-09-17 18:00:54 +0000

    7d3919e release: bump Angular DevTools version to 1.2.1 (#63879)

--- schematics/bundles/project_tsconfig_paths-CNnG1EBT.cjs
+++ schematics/bundles/project_tsconfig_paths-CNnG1EBT.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-dee6ef8
+ * @license Angular v21.0.0-next.3+sha-7d3919e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32963,7 +32963,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-dee6ef8');
+const VERSION = new Version('21.0.0-next.3+sha-7d3919e');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
