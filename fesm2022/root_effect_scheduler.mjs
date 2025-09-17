7f3af1476670293d545e2a64c2203195a2fd818a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 17:40:17 +0000

    36b542d Revert "build: update dependency @types/node to v24.5.1 (#63853)" (#63878)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-32c98e0
+ * @license Angular v21.0.0-next.3+sha-36b542d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.3+sha-32c98e0');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.3+sha-36b542d');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-CNnG1EBT.cjs
+++ schematics/bundles/project_tsconfig_paths-CNnG1EBT.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-32c98e0
+ * @license Angular v21.0.0-next.3+sha-36b542d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32963,7 +32963,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-32c98e0');
+const VERSION = new Version('21.0.0-next.3+sha-36b542d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
