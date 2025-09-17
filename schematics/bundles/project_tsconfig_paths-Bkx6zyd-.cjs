7df8b081a1e5d40b6f52a0ca0fb5bb95059ca0f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:27:10 +0000

    af33647 refactor(core): remove usages of Promise.withResolvers (#63856)

--- schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
+++ schematics/bundles/project_tsconfig_paths-Bkx6zyd-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-e523384
+ * @license Angular v21.0.0-next.3+sha-af33647
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-e523384');
+const VERSION = new Version('21.0.0-next.3+sha-af33647');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
