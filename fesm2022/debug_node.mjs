a05957230fd28ab0435f3b5af6b360e8af17bcc0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 14:56:20 +0000

    5f41f14 docs: fix pluralization error in templates guide (#63854)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-5fa2c05
+ * @license Angular v21.0.0-next.3+sha-5f41f14
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13692,7 +13692,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-5fa2c05']
+        ? ['ng-version', '21.0.0-next.3+sha-5f41f14']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
