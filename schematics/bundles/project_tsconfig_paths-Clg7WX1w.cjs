d8e783c2f2ae9d4a06a7d85edaf4d14bba7d91a2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-09-17 20:59:35 +0000

    95207b6 docs: release notes for the v20.3.1 release

--- schematics/bundles/project_tsconfig_paths-Clg7WX1w.cjs
+++ schematics/bundles/project_tsconfig_paths-Clg7WX1w.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-a53ef1d
+ * @license Angular v21.0.0-next.3+sha-95207b6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-a53ef1d');
+const VERSION = new Version('21.0.0-next.3+sha-95207b6');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
