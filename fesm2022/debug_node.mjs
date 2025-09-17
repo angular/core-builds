d658eb81284ba447a718c89ec858375686fc98d3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 21:21:42 +0000

    8ad26ed refactor(platform-browser): Remove zone provideres from BrowserTestingModule (#63872)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-95207b6
+ * @license Angular v21.0.0-next.4+sha-8ad26ed
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13692,7 +13692,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.3+sha-95207b6']
+        ? ['ng-version', '21.0.0-next.4+sha-8ad26ed']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-Clg7WX1w.cjs
+++ schematics/bundles/project_tsconfig_paths-Clg7WX1w.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.3+sha-95207b6
+ * @license Angular v21.0.0-next.4+sha-8ad26ed
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.3+sha-95207b6');
+const VERSION = new Version('21.0.0-next.4+sha-8ad26ed');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
