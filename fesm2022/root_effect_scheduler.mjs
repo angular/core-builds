9d3767ecc07789641c771245a8b4ca8417c33fa7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 14:37:45 +0000

    8cd2d8b build: update cross-repo angular dependencies (#63865)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-b343430
+ * @license Angular v20.3.0+sha-8cd2d8b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.0+sha-b343430');
+const VERSION = /* @__PURE__ */ new Version('20.3.0+sha-8cd2d8b');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/signals.cjs
+++ schematics/bundles/signals.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.0+sha-b343430
+ * @license Angular v20.3.0+sha-8cd2d8b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
