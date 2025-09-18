ad25d3c865a93e1307cb2fb2a9e6b6a1f499957a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 15:12:26 +0000

    ccdb340 docs: apply grammar fixes to ai/overview.md (#63896)

--- schematics/bundles/project_tsconfig_paths-crm5NKE7.cjs
+++ schematics/bundles/project_tsconfig_paths-crm5NKE7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.1+sha-205d527
+ * @license Angular v20.3.1+sha-ccdb340
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.1+sha-205d527');
+const VERSION = new Version('20.3.1+sha-ccdb340');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
