e600cfc3d0978ca736263dc689dc4ef7973fb122
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 14:43:18 +0000

    950ffd0 build: remove yarn from integration tests, switch to pnpm (#63902)

--- schematics/bundles/project_tsconfig_paths-Clg7WX1w.cjs
+++ schematics/bundles/project_tsconfig_paths-Clg7WX1w.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.4+sha-8ad26ed
+ * @license Angular v21.0.0-next.4+sha-950ffd0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32776,7 +32776,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.4+sha-8ad26ed');
+const VERSION = new Version('21.0.0-next.4+sha-950ffd0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
