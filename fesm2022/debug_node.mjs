5d5b6c9df36c56b9beccf9fcb61300ed9b702f46
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 14:44:21 +0000

    4328ea8 build: update cross-repo angular dependencies (#63929)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-4c091ab
+ * @license Angular v21.0.0-next.4+sha-4328ea8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13692,7 +13692,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.4+sha-4c091ab']
+        ? ['ng-version', '21.0.0-next.4+sha-4328ea8']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-RdUBwKbj.cjs
+++ schematics/bundles/project_tsconfig_paths-RdUBwKbj.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.4+sha-4c091ab
+ * @license Angular v21.0.0-next.4+sha-4328ea8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32986,7 +32986,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.4+sha-4c091ab');
+const VERSION = new Version('21.0.0-next.4+sha-4328ea8');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
