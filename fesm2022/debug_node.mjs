f8003da55b0977ae455944badf6cd2bd3f2c775c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 14:41:09 +0000

    3c66209 ci: optimize adev CI workflows (#63941)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-ba40153
+ * @license Angular v20.3.1+sha-3c66209
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13691,7 +13691,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.1+sha-ba40153']
+        ? ['ng-version', '20.3.1+sha-3c66209']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
