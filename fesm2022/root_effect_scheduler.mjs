4ec109b6c3c2b8f96d6271cc5f78c92b96a5520e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 18:04:48 +0000

    ee2fb08 build: update to latest version of rules_browsers (#64009)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-50d9d55
+ * @license Angular v21.0.0-next.4+sha-ee2fb08
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.4+sha-50d9d55');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.4+sha-ee2fb08');
 
 /**
  * Base URL for the error details page.
