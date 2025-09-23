11bcc724d0f7c8763ee08b4ffc850f6ff696bc79
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 18:04:23 +0000

    0ebf1af build: update to latest version of rules_browsers (#64009)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-9515a70
+ * @license Angular v20.3.1+sha-0ebf1af
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.1+sha-9515a70');
+const VERSION = /* @__PURE__ */ new Version('20.3.1+sha-0ebf1af');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/symbol-VPWguRxr.cjs
+++ schematics/bundles/symbol-VPWguRxr.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.1+sha-9515a70
+ * @license Angular v20.3.1+sha-0ebf1af
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
