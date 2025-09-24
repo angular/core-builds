7437ca006ed7a59a184b793c12ca7a08a9d751fa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:29:24 +0000

    521c2a7 feat(devtools): Change lazy/eager visualization by using dashed lines and correctly positioned edge arrows (#63980)

--- schematics/bundles/project_tsconfig_paths-Cfrq3FPR.cjs
+++ schematics/bundles/project_tsconfig_paths-Cfrq3FPR.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.4+sha-f36a504
+ * @license Angular v21.0.0-next.4+sha-521c2a7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32964,7 +32964,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.4+sha-f36a504');
+const VERSION = new Version('21.0.0-next.4+sha-521c2a7');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
