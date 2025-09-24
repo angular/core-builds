75c1f6bf8df1c5d7580634af248c8444129a5383
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 21:31:09 +0000

    090c0bb build: update dependency vsce to v2 (#64054)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-e8beaa4
+ * @license Angular v21.0.0-next.4+sha-090c0bb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13706,7 +13706,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.4+sha-e8beaa4']
+        ? ['ng-version', '21.0.0-next.4+sha-090c0bb']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
