ad4d3a1e4f94b7ca90ab9673a66192f39fb8629e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 20:27:27 +0000

    f04a78a docs(docs-infra): scroll offset for headings (#64039)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-ce6c7d9
+ * @license Angular v20.3.1+sha-f04a78a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.1+sha-ce6c7d9');
+const VERSION = /* @__PURE__ */ new Version('20.3.1+sha-f04a78a');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-C11WBTJF.cjs
+++ schematics/bundles/project_tsconfig_paths-C11WBTJF.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.1+sha-ce6c7d9
+ * @license Angular v20.3.1+sha-f04a78a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32774,7 +32774,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.1+sha-ce6c7d9');
+const VERSION = new Version('20.3.1+sha-f04a78a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
