2e048b76e0c6e4dada4743fd06b240242f85618f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:34:14 +0000

    513539b docs(docs-infra): tutorial navigation list component (#64027)

--- schematics/bundles/project_tsconfig_paths-Cfrq3FPR.cjs
+++ schematics/bundles/project_tsconfig_paths-Cfrq3FPR.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.4+sha-521c2a7
+ * @license Angular v21.0.0-next.4+sha-513539b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32964,7 +32964,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.4+sha-521c2a7');
+const VERSION = new Version('21.0.0-next.4+sha-513539b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
