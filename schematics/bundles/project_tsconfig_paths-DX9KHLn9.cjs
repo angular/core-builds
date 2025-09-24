db5217c1058e9a57c0476517230c720e8fecd48d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 21:53:34 +0000

    057eff6 build: update dependency prettier to v3 (#64053)

--- schematics/bundles/project_tsconfig_paths-DX9KHLn9.cjs
+++ schematics/bundles/project_tsconfig_paths-DX9KHLn9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.4+sha-090c0bb
+ * @license Angular v21.0.0-next.4+sha-057eff6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32964,7 +32964,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.4+sha-090c0bb');
+const VERSION = new Version('21.0.0-next.4+sha-057eff6');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
