eae7491f6c66e962525e63446eb46fa08930ed57
kirjs <kirts@google.com>
2025-09-25 19:14:54 +0000

    566077f build: update all github actions (#64079)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-a45e6fd
+ * @license Angular v21.0.0-next.5+sha-566077f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-a45e6fd');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-566077f');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-DX9KHLn9.cjs
+++ schematics/bundles/project_tsconfig_paths-DX9KHLn9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-a45e6fd
+ * @license Angular v21.0.0-next.5+sha-566077f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32964,7 +32964,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.5+sha-a45e6fd');
+const VERSION = new Version('21.0.0-next.5+sha-566077f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
