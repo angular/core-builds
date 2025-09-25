3f1f9979995c9edf47d865730c625170cc70785b
kirjs <kirts@google.com>
2025-09-25 14:30:01 +0000

    95fa943 refactor: update dependencies and build configurations (#64073)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0e8e7ba
+ * @license Angular v21.0.0-next.5+sha-95fa943
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-0e8e7ba');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-95fa943');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-DX9KHLn9.cjs
+++ schematics/bundles/project_tsconfig_paths-DX9KHLn9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-0e8e7ba
+ * @license Angular v21.0.0-next.5+sha-95fa943
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32964,7 +32964,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.5+sha-0e8e7ba');
+const VERSION = new Version('21.0.0-next.5+sha-95fa943');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
