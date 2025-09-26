d5ca4d29ebfb349c051e83d7fe37cdaee06e69af
kirjs <kirts@google.com>
2025-09-26 13:59:11 +0000

    238ccc7 docs: update Angular CLI help (#64103)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-ee02976
+ * @license Angular v21.0.0-next.5+sha-238ccc7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13706,7 +13706,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.5+sha-ee02976']
+        ? ['ng-version', '21.0.0-next.5+sha-238ccc7']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
