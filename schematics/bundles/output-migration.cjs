62da69df9ed4349f982989267ae793f6890eccf8
kirjs <kirts@google.com>
2025-09-26 16:05:37 +0000

    8cf95ce build: update cross-repo angular dependencies (#64094)

--- schematics/bundles/output-migration.cjs
+++ schematics/bundles/output-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.2+sha-e02985a
+ * @license Angular v20.3.2+sha-8cf95ce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-D0uJQawE.cjs
+++ schematics/bundles/project_tsconfig_paths-D0uJQawE.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.2+sha-e02985a
+ * @license Angular v20.3.2+sha-8cf95ce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32762,7 +32762,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.2+sha-e02985a');
+const VERSION = new Version('20.3.2+sha-8cf95ce');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
