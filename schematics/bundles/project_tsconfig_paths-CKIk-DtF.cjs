ce806d09593ecc0845edc2c52b62b3358e61a099
kirjs <kirts@google.com>
2025-09-29 15:53:02 +0000

    633db24 build: update bazel dependencies (#64100)

--- schematics/bundles/project_tsconfig_paths-CKIk-DtF.cjs
+++ schematics/bundles/project_tsconfig_paths-CKIk-DtF.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.2+sha-e6c0d7e
+ * @license Angular v20.3.2+sha-633db24
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32769,7 +32769,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.2+sha-e6c0d7e');
+const VERSION = new Version('20.3.2+sha-633db24');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
