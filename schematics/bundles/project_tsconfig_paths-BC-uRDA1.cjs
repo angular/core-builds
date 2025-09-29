ad30b69f91b35fa922b43340b76bb70e422adade
kirjs <kirts@google.com>
2025-09-29 17:34:57 +0000

    377d1a0 docs: Adds guide for custom HTTP parameter encoding (#64119)

--- schematics/bundles/project_tsconfig_paths-BC-uRDA1.cjs
+++ schematics/bundles/project_tsconfig_paths-BC-uRDA1.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-824bab5
+ * @license Angular v21.0.0-next.5+sha-377d1a0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32971,7 +32971,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.5+sha-824bab5');
+const VERSION = new Version('21.0.0-next.5+sha-377d1a0');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
