0e768c3217dbb0d28dee71f25b88dcd5fbf85786
kirjs <kirts@google.com>
2025-09-29 13:28:16 +0000

    4952110 docs: remove note about node.js 17+ (#64129)

--- schematics/bundles/project_tsconfig_paths-CKIk-DtF.cjs
+++ schematics/bundles/project_tsconfig_paths-CKIk-DtF.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.2+sha-c680927
+ * @license Angular v20.3.2+sha-4952110
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32769,7 +32769,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.2+sha-c680927');
+const VERSION = new Version('20.3.2+sha-4952110');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
