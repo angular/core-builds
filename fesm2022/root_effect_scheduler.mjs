bb2de33f97ee985379e3cca0189a62f109ff5c76
kirjs <kirts@google.com>
2025-09-29 20:51:27 +0000

    3598855 docs(docs-infra): clean up code and use inferred types where possible (#64139)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-5c50e0b
+ * @license Angular v20.3.2+sha-3598855
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.2+sha-5c50e0b');
+const VERSION = /* @__PURE__ */ new Version('20.3.2+sha-3598855');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-CKIk-DtF.cjs
+++ schematics/bundles/project_tsconfig_paths-CKIk-DtF.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.2+sha-5c50e0b
+ * @license Angular v20.3.2+sha-3598855
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32769,7 +32769,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.2+sha-5c50e0b');
+const VERSION = new Version('20.3.2+sha-3598855');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
