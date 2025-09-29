82de74ccbc5e7ef3db0d0e4ab9f51a40a99165e6
kirjs <kirts@google.com>
2025-09-29 17:55:20 +0000

    5c50e0b build: update cross-repo angular dependencies (#64115)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-aa28aa2
+ * @license Angular v20.3.2+sha-5c50e0b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13691,7 +13691,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.2+sha-aa28aa2']
+        ? ['ng-version', '20.3.2+sha-5c50e0b']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
