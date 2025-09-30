a3da001f0e48dc4ba44951ae248f1b52954bb053
kirjs <kirts@google.com>
2025-09-30 14:35:12 +0000

    a10eb38 feat(devtools): Add possibility to log directive instances and their prop values (#64143)

--- fesm2022/debug_node-CqVltiZE.mjs
+++ fesm2022/debug_node-CqVltiZE.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-9a913dc
+ * @license Angular v21.0.0-next.5+sha-a10eb38
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13706,7 +13706,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.5+sha-9a913dc']
+        ? ['ng-version', '21.0.0-next.5+sha-a10eb38']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-DNUb5KOh.cjs
+++ schematics/bundles/project_tsconfig_paths-DNUb5KOh.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-9a913dc
+ * @license Angular v21.0.0-next.5+sha-a10eb38
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32910,7 +32910,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.5+sha-9a913dc');
+const VERSION = new Version('21.0.0-next.5+sha-a10eb38');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
