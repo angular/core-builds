76a344647caa4d524f31d4baf206149f4e9e2413
kirjs <kirts@google.com>
2025-09-30 17:40:16 +0000

    c1a922a docs: add mention of `host-context()` being spec deprecated but still supported (#64153)

--- schematics/bundles/project_tsconfig_paths-CKIk-DtF.cjs
+++ schematics/bundles/project_tsconfig_paths-CKIk-DtF.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.2+sha-3db8ef0
+ * @license Angular v20.3.2+sha-c1a922a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32769,7 +32769,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.2+sha-3db8ef0');
+const VERSION = new Version('20.3.2+sha-c1a922a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
