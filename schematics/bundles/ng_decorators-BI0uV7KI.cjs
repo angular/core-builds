81a0f3c1a8cd25368c75333fe761774421169dc8
kirjs <kirts@google.com>
2025-09-30 16:38:35 +0000

    20bb459 build: update dependencies and build config (#64154)

--- schematics/bundles/ng_decorators-BI0uV7KI.cjs
+++ schematics/bundles/ng_decorators-BI0uV7KI.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-563bc38
+ * @license Angular v21.0.0-next.5+sha-20bb459
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-DNUb5KOh.cjs
+++ schematics/bundles/project_tsconfig_paths-DNUb5KOh.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-563bc38
+ * @license Angular v21.0.0-next.5+sha-20bb459
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32910,7 +32910,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.5+sha-563bc38');
+const VERSION = new Version('21.0.0-next.5+sha-20bb459');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
