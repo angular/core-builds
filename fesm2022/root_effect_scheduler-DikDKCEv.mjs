5e9bdeed2784b5cd55404d933a4852048fd2655e
kirjs <kirts@google.com>
2025-09-30 17:40:13 +0000

    899c25f docs: add mention of `host-context()` being spec deprecated but still supported (#64153)

--- fesm2022/root_effect_scheduler-DikDKCEv.mjs
+++ fesm2022/root_effect_scheduler-DikDKCEv.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-20bb459
+ * @license Angular v21.0.0-next.5+sha-899c25f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-20bb459');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-899c25f');
 
 /**
  * Base URL for the error details page.
