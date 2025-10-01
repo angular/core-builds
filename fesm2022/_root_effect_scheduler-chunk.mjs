96ca440afbea82b1a4ef2dee275cd115de759a3c
kirjs <kirts@google.com>
2025-10-01 17:26:10 +0000

    61624fd refactor(common): update NgIf, ngComponentOutlet and ngTemplateOutlet examples and remove redundant standalone flag (#64155)

--- fesm2022/_root_effect_scheduler-chunk.mjs
+++ fesm2022/_root_effect_scheduler-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-ee7d49a
+ * @license Angular v21.0.0-next.5+sha-61624fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-ee7d49a');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-61624fd');
 
 /**
  * Base URL for the error details page.
