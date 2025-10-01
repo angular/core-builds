5119ce655e03a844031db3797f631b82c40e72f2
kirjs <kirts@google.com>
2025-10-01 15:57:22 +0000

    1b102c2 build: update dependency @types/jsdom to v27 (#64165)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-2f6caef
+ * @license Angular v21.0.0-next.5+sha-1b102c2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13758,7 +13758,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.5+sha-2f6caef']
+        ? ['ng-version', '21.0.0-next.5+sha-1b102c2']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
