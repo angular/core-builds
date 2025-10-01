d23c825c5fe39b90f4b475e5d6885fa1d182d682
Miles Malerba <mmalerba@google.com>
2025-10-01 22:52:07 +0000

    7bc4ec4 build: update cross-repo angular dependencies to v21.0.0-next.6 (#64187)

--- schematics/bundles/index-bqpULzLD.cjs
+++ schematics/bundles/index-bqpULzLD.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-d892aab
+ * @license Angular v21.0.0-next.5+sha-7bc4ec4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-sFatqIE5.cjs
+++ schematics/bundles/project_tsconfig_paths-sFatqIE5.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-d892aab
+ * @license Angular v21.0.0-next.5+sha-7bc4ec4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32909,7 +32909,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.5+sha-d892aab');
+const VERSION = new Version('21.0.0-next.5+sha-7bc4ec4');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
