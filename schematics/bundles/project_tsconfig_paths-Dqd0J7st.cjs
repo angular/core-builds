20c06b81beae414c348137daa577a95cb2deadc9
Miles Malerba <mmalerba@google.com>
2025-10-01 22:24:50 +0000

    952e3b1 build: update cross-repo angular dependencies to v20.2.7 (#64182)

--- schematics/bundles/project_tsconfig_paths-Dqd0J7st.cjs
+++ schematics/bundles/project_tsconfig_paths-Dqd0J7st.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.2+sha-96b3572
+ * @license Angular v20.3.2+sha-952e3b1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32768,7 +32768,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.2+sha-96b3572');
+const VERSION = new Version('20.3.2+sha-952e3b1');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
