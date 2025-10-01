af9344d4f5b3a1b3a9b4cef94067e278f82d4bab
kirjs <kirts@google.com>
2025-10-01 17:49:05 +0000

    c0e1c41 build: update ossf/scorecard-action action to v2.4.3 (#64164)

--- schematics/bundles/project_tsconfig_paths-sFatqIE5.cjs
+++ schematics/bundles/project_tsconfig_paths-sFatqIE5.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.5+sha-851c07b
+ * @license Angular v21.0.0-next.5+sha-c0e1c41
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32909,7 +32909,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.5+sha-851c07b');
+const VERSION = new Version('21.0.0-next.5+sha-c0e1c41');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
