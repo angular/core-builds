97dbd57b809334f8e253822f0b6c521e6aa2d469
kirjs <kirts@google.com>
2025-10-01 13:45:22 +0000

    3bd4362 docs: update APF docs for new `.d.ts` layout (#64168)

--- fesm2022/debug_node-Cx9vM328.mjs
+++ fesm2022/debug_node-Cx9vM328.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-1cb16fd
+ * @license Angular v21.0.0-next.5+sha-3bd4362
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13758,7 +13758,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.5+sha-1cb16fd']
+        ? ['ng-version', '21.0.0-next.5+sha-3bd4362']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- fesm2022/root_effect_scheduler-DreawIYf.mjs
+++ fesm2022/root_effect_scheduler-DreawIYf.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-1cb16fd
+ * @license Angular v21.0.0-next.5+sha-3bd4362
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-1cb16fd');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.5+sha-3bd4362');
 
 /**
  * Base URL for the error details page.
