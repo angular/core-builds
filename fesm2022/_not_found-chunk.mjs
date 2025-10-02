f63680a7a8e7520f6070fdd9c2603573799ce7b8
Miles Malerba <mmalerba@google.com>
2025-10-02 14:50:15 +0000

    6169fa5 fix(devtools): stop second component tree traversal, if devtools metadata is placed on the body tag (#64161)

--- fesm2022/_not_found-chunk.mjs
+++ fesm2022/_not_found-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-732635b
+ * @license Angular v21.0.0-next.6+sha-6169fa5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-sFatqIE5.cjs
+++ schematics/bundles/project_tsconfig_paths-sFatqIE5.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.6+sha-732635b
+ * @license Angular v21.0.0-next.6+sha-6169fa5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32909,7 +32909,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.6+sha-732635b');
+const VERSION = new Version('21.0.0-next.6+sha-6169fa5');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
