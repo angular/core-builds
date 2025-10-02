d5cf1870e8714ed2f62a251c246a93ba70b9ac55
Miles Malerba <mmalerba@google.com>
2025-10-02 20:06:55 +0000

    8146fc2 test: fix vscode language service e2e tests (#64197)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-5e61e8d
+ * @license Angular v21.0.0-next.6+sha-8146fc2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -13770,7 +13770,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.6+sha-5e61e8d']
+        ? ['ng-version', '21.0.0-next.6+sha-8146fc2']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- fesm2022/_root_effect_scheduler-chunk.mjs
+++ fesm2022/_root_effect_scheduler-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-5e61e8d
+ * @license Angular v21.0.0-next.6+sha-8146fc2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.6+sha-5e61e8d');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.6+sha-8146fc2');
 
 /**
  * Base URL for the error details page.
