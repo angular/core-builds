60015f7d585add4a8ca577a4cd6e1f4a8f23717f
Miles Malerba <mmalerba@google.com>
2025-10-06 19:08:52 +0000

    fd8383a ci: cache vscode binary in CI (#64219)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-effccff
+ * @license Angular v21.0.0-next.6+sha-fd8383a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -13770,7 +13770,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.6+sha-effccff']
+        ? ['ng-version', '21.0.0-next.6+sha-fd8383a']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/router-last-successful-navigation.cjs
+++ schematics/bundles/router-last-successful-navigation.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.6+sha-effccff
+ * @license Angular v21.0.0-next.6+sha-fd8383a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
