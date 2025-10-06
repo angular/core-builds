5f55fad8408f68f0863e3eede3998ca34dd417b0
Miles Malerba <mmalerba@google.com>
2025-10-06 19:15:04 +0000

    1ffc643 docs(docs-infra): fix @example in class members (#64249)

--- schematics/bundles/project_tsconfig_paths-BbVhi4fG.cjs
+++ schematics/bundles/project_tsconfig_paths-BbVhi4fG.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.6+sha-fd8383a
+ * @license Angular v21.0.0-next.6+sha-1ffc643
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32989,7 +32989,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.6+sha-fd8383a');
+const VERSION = new Version('21.0.0-next.6+sha-1ffc643');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signal-queries-migration.cjs
+++ schematics/bundles/signal-queries-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.6+sha-fd8383a
+ * @license Angular v21.0.0-next.6+sha-1ffc643
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
