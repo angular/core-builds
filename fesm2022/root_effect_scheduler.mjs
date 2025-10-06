fd8e40ec093781392510c033b94974762c791dda
Miles Malerba <mmalerba@google.com>
2025-10-06 19:01:30 +0000

    6e5be38 build: update pnpm to v10.18.0 (#64233)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-853ed16
+ * @license Angular v20.3.3+sha-6e5be38
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.3+sha-853ed16');
+const VERSION = /* @__PURE__ */ new Version('20.3.3+sha-6e5be38');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-Dqd0J7st.cjs
+++ schematics/bundles/project_tsconfig_paths-Dqd0J7st.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.3+sha-853ed16
+ * @license Angular v20.3.3+sha-6e5be38
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32768,7 +32768,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.3+sha-853ed16');
+const VERSION = new Version('20.3.3+sha-6e5be38');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
