623b8972b54206fc7930b7e4d03da452ae41c1e8
Miles Malerba <mmalerba@google.com>
2025-10-08 00:28:17 +0000

    a15688c build: update pnpm to v10.18.1 (#64264)

--- schematics/bundles/ngclass-to-class-migration.cjs
+++ schematics/bundles/ngclass-to-class-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.6+sha-ac0eb23
+ * @license Angular v21.0.0-next.6+sha-a15688c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- types/rxjs-interop.d.ts
+++ types/rxjs-interop.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-ac0eb23
+ * @license Angular v21.0.0-next.6+sha-a15688c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
