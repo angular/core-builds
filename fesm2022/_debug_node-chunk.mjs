11f87bdf8a8345d03b9d2fec4c78664f914467e6
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-08 10:15:33 +0000

    76c8d04 build: update `rules_sass` to `1184a80751a21af8348f308abc5b38a41f26850e` (#64288)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-47b66b1
+ * @license Angular v21.0.0-next.6+sha-76c8d04
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -13771,7 +13771,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.6+sha-47b66b1']
+        ? ['ng-version', '21.0.0-next.6+sha-76c8d04']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- fesm2022/_root_effect_scheduler-chunk.mjs
+++ fesm2022/_root_effect_scheduler-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-47b66b1
+ * @license Angular v21.0.0-next.6+sha-76c8d04
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.6+sha-47b66b1');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.6+sha-76c8d04');
 
 /**
  * Base URL for the error details page.
