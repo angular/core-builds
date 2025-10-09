60a0101fb151c927ed730cd262d69df1244446e3
Andrew Kushnir <akushnir@google.com>
2025-10-09 18:39:05 +0000

    d73d9ac build: fix broken test (#64319)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-e1da41f
+ * @license Angular v21.0.0-next.7+sha-d73d9ac
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -13809,7 +13809,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '21.0.0-next.7+sha-e1da41f']
+        ? ['ng-version', '21.0.0-next.7+sha-d73d9ac']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
