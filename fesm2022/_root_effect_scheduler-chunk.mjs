fd8977cf6abd2ff08e6b383eedaf2b2107bdbba3
Andrew Kushnir <akushnir@google.com>
2025-10-10 15:46:36 +0000

    e34776a build: correctly use snapshot build repo (#64341)

--- fesm2022/_root_effect_scheduler-chunk.mjs
+++ fesm2022/_root_effect_scheduler-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-2ecbc4e
+ * @license Angular v21.0.0-next.7+sha-e34776a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.7+sha-2ecbc4e');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.7+sha-e34776a');
 
 /**
  * Base URL for the error details page.
