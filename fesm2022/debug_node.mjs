4873f365c73437418bc4d8b6ebe5022bfd13bfb4
Andrew Kushnir <akushnir@google.com>
2025-10-13 23:52:54 +0000

    a2773b5 ci: update symbol golden file for `@defer` test case (#64396)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-fdd0856
+ * @license Angular v20.3.4+sha-a2773b5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -13756,7 +13756,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.4+sha-fdd0856']
+        ? ['ng-version', '20.3.4+sha-a2773b5']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-eBEiBXTc.cjs
+++ schematics/bundles/project_tsconfig_paths-eBEiBXTc.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.4+sha-fdd0856
+ * @license Angular v20.3.4+sha-a2773b5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.4+sha-fdd0856');
+const VERSION = new Version('20.3.4+sha-a2773b5');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
