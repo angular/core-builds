488b543a10d2187b6c9e6b284f84ebe77272408b
Andrew Kushnir <akushnir@google.com>
2025-10-13 18:33:23 +0000

    6a96844 build: update cross-repo angular dependencies (#64390)

--- schematics/bundles/ngstyle-to-style-migration.cjs
+++ schematics/bundles/ngstyle-to-style-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.7+sha-eb80c30
+ * @license Angular v21.0.0-next.7+sha-6a96844
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-BP6O7cP-.cjs
+++ schematics/bundles/project_tsconfig_paths-BP6O7cP-.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.7+sha-eb80c30
+ * @license Angular v21.0.0-next.7+sha-6a96844
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.7+sha-eb80c30');
+const VERSION = new Version('21.0.0-next.7+sha-6a96844');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
