51f991acdbf63fdc4e97537db5fe40d9b7d11c62
Andrew Kushnir <akushnir@google.com>
2025-10-13 18:34:07 +0000

    0c58b5f build: update cross-repo angular dependencies (#64391)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-f23df71
+ * @license Angular v20.3.4+sha-0c58b5f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -13756,7 +13756,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.4+sha-f23df71']
+        ? ['ng-version', '20.3.4+sha-0c58b5f']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-eBEiBXTc.cjs
+++ schematics/bundles/project_tsconfig_paths-eBEiBXTc.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.4+sha-f23df71
+ * @license Angular v20.3.4+sha-0c58b5f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.4+sha-f23df71');
+const VERSION = new Version('20.3.4+sha-0c58b5f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
