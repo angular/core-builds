657ddc60b8e738d40f14e4554627be741e9001dd
Andrew Kushnir <akushnir@google.com>
2025-10-14 22:45:26 +0000

    3d1777b build: update cross-repo angular dependencies (#64414)

--- fesm2022/_root_effect_scheduler-chunk.mjs
+++ fesm2022/_root_effect_scheduler-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-fc65107
+ * @license Angular v21.0.0-next.7+sha-3d1777b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.7+sha-fc65107');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.7+sha-3d1777b');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-CdhVNYMk.cjs
+++ schematics/bundles/project_tsconfig_paths-CdhVNYMk.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.7+sha-fc65107
+ * @license Angular v21.0.0-next.7+sha-3d1777b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.7+sha-fc65107');
+const VERSION = new Version('21.0.0-next.7+sha-3d1777b');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
