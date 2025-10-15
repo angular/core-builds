a0862c355a3a2eec899a80d47d2d92a59781fe62
Andrew Kushnir <akushnir@google.com>
2025-10-15 17:54:24 +0000

    43fe755 build: update cross-repo angular dependencies (#64439)

--- fesm2022/attribute.mjs
+++ fesm2022/attribute.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-69dabdb
+ * @license Angular v20.3.5+sha-43fe755
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
--- schematics/bundles/signals.cjs
+++ schematics/bundles/signals.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.5+sha-69dabdb
+ * @license Angular v20.3.5+sha-43fe755
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
