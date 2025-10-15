b472fd94e27b6bad786cf6e82b3a201b04c8af7d
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-10-15 17:19:50 +0000

    013effd release: cut the v21.0.0-next.8 release

--- fesm2022/_root_effect_scheduler-chunk.mjs
+++ fesm2022/_root_effect_scheduler-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-e3fa24d
+ * @license Angular v21.0.0-next.8+sha-013effd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.7+sha-e3fa24d');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.8+sha-013effd');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-CdhVNYMk.cjs
+++ schematics/bundles/project_tsconfig_paths-CdhVNYMk.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.7+sha-e3fa24d
+ * @license Angular v21.0.0-next.8+sha-013effd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.7+sha-e3fa24d');
+const VERSION = new Version('21.0.0-next.8+sha-013effd');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
