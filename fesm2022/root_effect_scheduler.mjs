ef3cc25ce188341b15fb538592d8b2bdea2de442
Andrew Kushnir <akushnir@google.com>
2025-10-15 18:51:11 +0000

    e6c0223 docs: update Angular CLI help (#64434)

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-43fe755
+ * @license Angular v20.3.5+sha-e6c0223
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.5+sha-43fe755');
+const VERSION = /* @__PURE__ */ new Version('20.3.5+sha-e6c0223');
 
 /**
  * Base URL for the error details page.
