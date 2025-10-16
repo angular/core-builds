f830995cf65c8559e1cb3b4a0f480d80404c6d05
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 14:58:31 +0000

    2d159af docs: Add docs for EVENT_MANAGER_PLUGINS (#64453)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-136e3de
+ * @license Angular v20.3.5+sha-2d159af
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -13763,7 +13763,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.5+sha-136e3de']
+        ? ['ng-version', '20.3.5+sha-2d159af']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-136e3de
+ * @license Angular v20.3.5+sha-2d159af
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.5+sha-136e3de');
+const VERSION = /* @__PURE__ */ new Version('20.3.5+sha-2d159af');
 
 /**
  * Base URL for the error details page.
