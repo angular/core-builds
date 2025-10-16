191c25598455d3109e39b83fc0da41fc7343ed51
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 18:26:05 +0000

    d342601 build: update cross-repo angular dependencies (#64475)

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-911d682
+ * @license Angular v20.3.5+sha-d342601
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14630,7 +14630,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.5+sha-911d682']
+        ? ['ng-version', '20.3.5+sha-d342601']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/self-closing-tags-migration.cjs
+++ schematics/bundles/self-closing-tags-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.5+sha-911d682
+ * @license Angular v20.3.5+sha-d342601
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
