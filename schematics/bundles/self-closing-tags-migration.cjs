b1033ef73e2404fb3c3b89a15320bcd4f2639870
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 15:02:41 +0000

    2e46596 refactor(zone.js): remove legacy browser support (#63511)

--- schematics/bundles/self-closing-tags-migration.cjs
+++ schematics/bundles/self-closing-tags-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-df780fa
+ * @license Angular v21.0.0-next.8+sha-2e46596
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
