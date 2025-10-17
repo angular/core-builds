a3dad67835f5392e021f310a52b638f1fde1fb39
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 18:20:37 +0000

    062a696 refactor(platform-server): use URL constructor for robust parsing (#64494)

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-cc5d698
+ * @license Angular v21.0.0-next.8+sha-062a696
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.8+sha-cc5d698');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.8+sha-062a696');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-BsnCX6yC.cjs
+++ schematics/bundles/project_tsconfig_paths-BsnCX6yC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-cc5d698
+ * @license Angular v21.0.0-next.8+sha-062a696
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.8+sha-cc5d698');
+const VERSION = new Version('21.0.0-next.8+sha-062a696');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
