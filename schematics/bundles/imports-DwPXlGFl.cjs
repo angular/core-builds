656fd154be56b31cd6f73939a70555dae665cc50
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:35:25 +0000

    3cfa6e8 build: remove unused commit message scopes (#64492)

--- schematics/bundles/imports-DwPXlGFl.cjs
+++ schematics/bundles/imports-DwPXlGFl.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-c241038
+ * @license Angular v21.0.0-next.8+sha-3cfa6e8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
