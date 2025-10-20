08bb7454ceb96da0eca074ca7eceb4b5e9939b1d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 18:46:15 +0000

    f6a73f1 fix(router): Respect custom `UrlSerializer` handling of query parameters (#64449)

--- schematics/bundles/project_tsconfig_paths-C0W8fCtx.cjs
+++ schematics/bundles/project_tsconfig_paths-C0W8fCtx.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-7fb7511
+ * @license Angular v21.0.0-next.8+sha-f6a73f1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.8+sha-7fb7511');
+const VERSION = new Version('21.0.0-next.8+sha-f6a73f1');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
