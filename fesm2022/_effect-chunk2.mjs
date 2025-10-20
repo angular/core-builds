665be9ae6394b724341e9102a0a860a992b2327c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 16:19:57 +0000

    fad6e13 build: update all non-major dependencies (#64514)

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-aca2f75
+ * @license Angular v21.0.0-next.8+sha-fad6e13
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.0.0-next.8+sha-aca2f75');
+const VERSION = /* @__PURE__ */ new Version('21.0.0-next.8+sha-fad6e13');
 
 /**
  * Base URL for the error details page.
