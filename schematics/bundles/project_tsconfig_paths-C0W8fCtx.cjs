6ec1a77bc316c0f1690b2a77063db08fcd274c64
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 14:56:21 +0000

    9c7029c build: update dependency @types/firefox-webext-browser to v143 (#64518)

--- schematics/bundles/project_tsconfig_paths-C0W8fCtx.cjs
+++ schematics/bundles/project_tsconfig_paths-C0W8fCtx.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-810505f
+ * @license Angular v21.0.0-next.8+sha-9c7029c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.8+sha-810505f');
+const VERSION = new Version('21.0.0-next.8+sha-9c7029c');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
