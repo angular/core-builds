6a7ab5b5a8e6fdcf9d217adb44f25442306040ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 18:12:43 +0000

    4c89a26 fix(core): pass element removal property through in all locations (#64565)

--- schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
+++ schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.6+sha-e3f1c52
+ * @license Angular v20.3.6+sha-4c89a26
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.6+sha-e3f1c52');
+const VERSION = new Version('20.3.6+sha-4c89a26');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
