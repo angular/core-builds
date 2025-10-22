685cbb79b6ef4081189118d1bdf85e382be544b1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 14:39:16 +0000

    9126d0d docs(docs-infra): fix example viewer (#64580)

--- schematics/bundles/project_tsconfig_paths-C0W8fCtx.cjs
+++ schematics/bundles/project_tsconfig_paths-C0W8fCtx.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-584dd4a
+ * @license Angular v21.0.0-next.8+sha-9126d0d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.8+sha-584dd4a');
+const VERSION = new Version('21.0.0-next.8+sha-9126d0d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/route-lazy-loading.cjs
+++ schematics/bundles/route-lazy-loading.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-584dd4a
+ * @license Angular v21.0.0-next.8+sha-9126d0d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
