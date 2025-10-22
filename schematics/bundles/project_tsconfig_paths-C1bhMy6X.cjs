1f6a2f4d1fb8962082e29d42cd7fee5177de9815
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 14:38:38 +0000

    0084202 docs(docs-infra): fix example viewer (#64580)

--- schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
+++ schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.6+sha-acadefb
+ * @license Angular v20.3.6+sha-0084202
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.6+sha-acadefb');
+const VERSION = new Version('20.3.6+sha-0084202');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
