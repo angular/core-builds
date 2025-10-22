81b21fd22a9b0be1c380bdd75de77f17e085032b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 16:40:30 +0000

    9d48e53 Revert "feat(router): allow router outlet to be set on ng-container (#64562)" (#64584)

--- schematics/bundles/leading_space-D9nQ8UQC.cjs
+++ schematics/bundles/leading_space-D9nQ8UQC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-9fef846
+ * @license Angular v21.0.0-next.8+sha-9d48e53
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
