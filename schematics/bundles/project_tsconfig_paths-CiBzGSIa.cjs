c2341089da722018752a06989a98c586b10ee946
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 18:31:56 +0000

    a3e91b2 refactor(forms): Add a test better documenting behavior of the error function in built-in validators (#63993)

--- schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
+++ schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.8+sha-e039c6b
+ * @license Angular v21.0.0-next.8+sha-a3e91b2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.8+sha-e039c6b');
+const VERSION = new Version('21.0.0-next.8+sha-a3e91b2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
