b9939e92aff6374fe763d627bf9de4663344e4f9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 14:06:17 +0000

    6a3e881 build: update cross-repo angular dependencies (#64616)

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3ace778
+ * @license Angular v21.1.0-next.0+sha-6a3e881
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.1.0-next.0+sha-3ace778');
+const VERSION = /* @__PURE__ */ new Version('21.1.0-next.0+sha-6a3e881');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
+++ schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.0+sha-3ace778
+ * @license Angular v21.1.0-next.0+sha-6a3e881
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.1.0-next.0+sha-3ace778');
+const VERSION = new Version('21.1.0-next.0+sha-6a3e881');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
