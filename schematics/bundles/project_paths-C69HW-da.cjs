a3031d56a0f6f73dce2d23d0fdf6a0d9a9f6daf2
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 10:39:22 +0000

    50d2670 docs: Add router config options (#64529)

--- schematics/bundles/project_paths-C69HW-da.cjs
+++ schematics/bundles/project_paths-C69HW-da.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.7+sha-2a249b7
+ * @license Angular v20.3.7+sha-50d2670
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
+++ schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.7+sha-2a249b7
+ * @license Angular v20.3.7+sha-50d2670
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.7+sha-2a249b7');
+const VERSION = new Version('20.3.7+sha-50d2670');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
