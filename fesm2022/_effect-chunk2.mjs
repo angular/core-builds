3e2e0576b93f4543276c9053cf4c334f60d76437
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 10:43:18 +0000

    3ace778 build: update cross-repo angular dependencies (#64609)

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ced2fa5
+ * @license Angular v21.1.0-next.0+sha-3ace778
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.1.0-next.0+sha-ced2fa5');
+const VERSION = /* @__PURE__ */ new Version('21.1.0-next.0+sha-3ace778');
 
 /**
  * Base URL for the error details page.
