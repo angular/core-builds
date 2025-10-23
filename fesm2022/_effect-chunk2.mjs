00ed6c1aa63f3d4f8e44c30cdc3e707af49aca59
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 16:33:43 +0000

    e47ef3e docs: update Angular CLI help (#64621)

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-13d8ccc
+ * @license Angular v21.1.0-next.0+sha-e47ef3e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.1.0-next.0+sha-13d8ccc');
+const VERSION = /* @__PURE__ */ new Version('21.1.0-next.0+sha-e47ef3e');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
+++ schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.0+sha-13d8ccc
+ * @license Angular v21.1.0-next.0+sha-e47ef3e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.1.0-next.0+sha-13d8ccc');
+const VERSION = new Version('21.1.0-next.0+sha-e47ef3e');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
