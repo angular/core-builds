d18090a9d20b2f4cf6b9f9c3386bc0a8f8486eb5
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 17:07:33 +0000

    e9f55a8 Revert "build: refactor location of best-practices.md"

--- schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
+++ schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-next.9+sha-5099ed4
+ * @license Angular v21.0.0-next.9+sha-e9f55a8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.0.0-next.9+sha-5099ed4');
+const VERSION = new Version('21.0.0-next.9+sha-e9f55a8');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
