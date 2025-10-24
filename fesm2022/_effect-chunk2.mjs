6b3ee2753a0480bb3373e48df08048f2e8030789
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-24 13:30:22 +0000

    1a0d2da build: update all non-major dependencies

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0d38cbf
+ * @license Angular v21.1.0-next.0+sha-1a0d2da
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('21.1.0-next.0+sha-0d38cbf');
+const VERSION = /* @__PURE__ */ new Version('21.1.0-next.0+sha-1a0d2da');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
+++ schematics/bundles/project_tsconfig_paths-CiBzGSIa.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.0+sha-0d38cbf
+ * @license Angular v21.1.0-next.0+sha-1a0d2da
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -33113,7 +33113,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('21.1.0-next.0+sha-0d38cbf');
+const VERSION = new Version('21.1.0-next.0+sha-1a0d2da');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
