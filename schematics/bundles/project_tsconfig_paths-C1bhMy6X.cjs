de77dee515b7a86c8ee94339ae65c3bcd10a11f0
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 08:33:08 +0000

    14b5590 fix(docs-infra): preserve shell class for multifile code blocks

--- schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
+++ schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.7+sha-6daabb0
+ * @license Angular v20.3.7+sha-14b5590
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.7+sha-6daabb0');
+const VERSION = new Version('20.3.7+sha-14b5590');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
