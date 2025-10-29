79bfa7e446a6b60d643bd05c4eb65abe8199edcc
Andrew Scott <atscott01@gmail.com>
2025-10-29 21:06:59 +0000

    b0c9c63 build: update cross-repo angular dependencies

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-fb569ef
+ * @license Angular v21.1.0-next.0+sha-b0c9c63
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-fb569ef');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b0c9c63');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
