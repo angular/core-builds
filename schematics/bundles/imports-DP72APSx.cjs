ef95e8f506f38f654e234fabfc65eca65149ad78
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:26:20 +0000

    fee4baf docs: update dependency injection guides (part 1) (#63652)

--- schematics/bundles/imports-DP72APSx.cjs
+++ schematics/bundles/imports-DP72APSx.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.0+sha-26f4944
+ * @license Angular v21.0.0-rc.0+sha-fee4baf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
