0d9dea22ebcd8502e4b4832cdbff003d8ee32ec2
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:28:48 +0000

    58f921d docs: add essential signal forms guide (#64665)

--- schematics/bundles/ng_component_template-Dsuq1Lw7.cjs
+++ schematics/bundles/ng_component_template-Dsuq1Lw7.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.0+sha-90452f5
+ * @license Angular v21.0.0-rc.0+sha-58f921d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
