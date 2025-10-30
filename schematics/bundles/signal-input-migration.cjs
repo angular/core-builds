1bc57d71f88407a124a816f7e009ba681893bd87
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:22:45 +0000

    26f4944 ci: refactor cross-repo documentation update scripts (#64792)

--- schematics/bundles/signal-input-migration.cjs
+++ schematics/bundles/signal-input-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.0+sha-d23bb3c
+ * @license Angular v21.0.0-rc.0+sha-26f4944
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
