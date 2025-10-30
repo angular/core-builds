29c1a3f0af2b44412beb4787958f2e37ac49b97b
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:46:48 +0000

    ca0e77b fix(docs-infra): Cannot find module message (#64701)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-58f921d
+ * @license Angular v21.0.0-rc.0+sha-ca0e77b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8211,7 +8211,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.0-rc.0+sha-58f921d'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.0-rc.0+sha-ca0e77b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- schematics/bundles/migrate_ts_type_references-UGIUl7En.cjs
+++ schematics/bundles/migrate_ts_type_references-UGIUl7En.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.0+sha-58f921d
+ * @license Angular v21.0.0-rc.0+sha-ca0e77b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
