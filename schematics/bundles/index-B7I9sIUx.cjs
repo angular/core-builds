0e44386493b558e4d1022c28772469813e01b577
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:37:19 +0000

    b9e2ccd refactor(common): remove unused import (#64699)

--- schematics/bundles/index-B7I9sIUx.cjs
+++ schematics/bundles/index-B7I9sIUx.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.0+sha-f321e22
+ * @license Angular v21.1.0-next.0+sha-b9e2ccd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
