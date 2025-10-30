8eb56f7ef71d609103dcee20e645f01f26f1441b
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:28:20 +0000

    040585d ci: make the github action fail when a firebase command fails (#64801)

--- schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
+++ schematics/bundles/project_tsconfig_paths-C1bhMy6X.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.9+sha-54ad3f6
+ * @license Angular v20.3.9+sha-040585d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.9+sha-54ad3f6');
+const VERSION = new Version('20.3.9+sha-040585d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
