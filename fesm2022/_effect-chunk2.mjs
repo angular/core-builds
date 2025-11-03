5b71f9dbc9872f0b1e9a7e6ed14757dd8d6f62d7
Andrew Scott <atscott01@gmail.com>
2025-11-03 20:23:08 +0000

    32a166b build: update bazel dependencies

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-5d75d0e
+ * @license Angular v21.0.0-rc.0+sha-32a166b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.0+sha-5d75d0e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.0+sha-32a166b');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- schematics/bundles/ngstyle-to-style-migration.cjs
+++ schematics/bundles/ngstyle-to-style-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.0+sha-5d75d0e
+ * @license Angular v21.0.0-rc.0+sha-32a166b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
