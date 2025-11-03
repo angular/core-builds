f089f1d78ee1b14e06f342c5d14b495333f2c9ec
Andrew Scott <atscott@google.com>
2025-11-03 22:34:50 +0000

    a57c03d docs(docs-infra): Improve font loading performance with preconnect

--- schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
+++ schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.9+sha-88952b5
+ * @license Angular v20.3.9+sha-a57c03d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.9+sha-88952b5');
+const VERSION = new Version('20.3.9+sha-a57c03d');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
