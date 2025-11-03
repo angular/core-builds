ea37a109eceb37d9ca1eda39d82d10ab41d21bd1
Andrew Scott <atscott@google.com>
2025-11-03 21:40:18 +0000

    92f7052 docs(docs-infra): Updates page title strategy to sync meta tags

--- schematics/bundles/project_paths-DvD50ouC.cjs
+++ schematics/bundles/project_paths-DvD50ouC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.0+sha-dc99f4c
+ * @license Angular v21.0.0-rc.0+sha-92f7052
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
