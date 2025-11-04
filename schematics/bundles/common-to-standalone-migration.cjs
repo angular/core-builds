1c52151bdbd75ce080fd45e32d1f7d8191ccef6d
Andrew Scott <atscott@google.com>
2025-11-04 00:30:38 +0000

    34f53aa docs: remove standalone: true since it's now the default

--- schematics/bundles/common-to-standalone-migration.cjs
+++ schematics/bundles/common-to-standalone-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.0+sha-a702c87
+ * @license Angular v21.0.0-rc.0+sha-34f53aa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
