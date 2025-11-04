2d361cfb90a615accc6cd585b9ada9cb75e75212
Andrew Scott <atscott@google.com>
2025-11-04 23:01:58 +0000

    9c59ea8 docs(docs-infra): add missing meta tags for author, keywords, and Twitter

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-9596020
+ * @license Angular v21.0.0-rc.0+sha-9c59ea8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.0+sha-9596020');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.0+sha-9c59ea8');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
