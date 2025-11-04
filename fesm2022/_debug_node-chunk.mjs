05aa10803ad048a2d30dffcc09ddc2886c3922fe
Andrew Scott <atscott01@gmail.com>
2025-11-04 19:21:16 +0000

    9596020 docs: update cross-repo adev docs

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-0b1339e
+ * @license Angular v21.0.0-rc.0+sha-9596020
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8203,7 +8203,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.0-rc.0+sha-0b1339e'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.0-rc.0+sha-9596020'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-0b1339e
+ * @license Angular v21.0.0-rc.0+sha-9596020
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.0+sha-0b1339e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.0+sha-9596020');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
