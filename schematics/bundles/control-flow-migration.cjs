a050f06466c7e37a5dd35fbad7cf81d3e4256b0d
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:41:31 +0000

    b61847f docs: simplify tutorial signals with self-closing component tags

--- schematics/bundles/control-flow-migration.cjs
+++ schematics/bundles/control-flow-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.0+sha-c4c7fb8
+ * @license Angular v21.1.0-next.0+sha-b61847f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
