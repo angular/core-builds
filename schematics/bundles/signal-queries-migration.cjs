45fa1acc6b64403eeb5090f139fd3d8e0c5d1dd1
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:04:43 +0000

    a702c87 build: update cross-repo angular dependencies

--- schematics/bundles/signal-queries-migration.cjs
+++ schematics/bundles/signal-queries-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.0+sha-6b3fdf3
+ * @license Angular v21.0.0-rc.0+sha-a702c87
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
