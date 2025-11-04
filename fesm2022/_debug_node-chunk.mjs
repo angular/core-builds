9cebd6605db9a4c60b9d3f5f84eeb476aabde4f6
Andrew Scott <atscott01@gmail.com>
2025-11-04 23:15:01 +0000

    c277214 fix(docs-infra): properly expose the prerendered routes for sitemap generation

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b5e37ff
+ * @license Angular v21.1.0-next.0+sha-c277214
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8203,7 +8203,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-b5e37ff'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-c277214'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
