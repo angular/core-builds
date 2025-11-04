a61d32f44f44e81bd37093fbdc2c571c446901d5
Andrew Scott <atscott01@gmail.com>
2025-11-04 18:00:45 +0000

    7f16902 refactor(router): Update pre/post wildcard matching to retain 0 or more segment match meaning

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8f49af9
+ * @license Angular v21.1.0-next.0+sha-7f16902
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8f49af9');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-7f16902');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
