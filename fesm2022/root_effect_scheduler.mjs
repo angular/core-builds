98adbf1ffc740def14c70541ec9b6d1567873d2b
Andrew Scott <atscott@google.com>
2025-11-04 23:01:40 +0000

    c374e80 docs(docs-infra): add missing meta tags for author, keywords, and Twitter

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-43f7f41
+ * @license Angular v20.3.9+sha-c374e80
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.9+sha-43f7f41');
+const VERSION = /* @__PURE__ */ new Version('20.3.9+sha-c374e80');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/test-bed-get.cjs
+++ schematics/bundles/test-bed-get.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.9+sha-43f7f41
+ * @license Angular v20.3.9+sha-c374e80
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
