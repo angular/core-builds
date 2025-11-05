bbc7d2c4210413e07bfac1ac80ff9a4826b66f75
Andrew Scott <atscott@google.com>
2025-11-05 00:19:54 +0000

    507b346 perf(forms): implement change detection for field control bindings

--- types/core.d.ts
+++ types/core.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-bb267d1
+ * @license Angular v21.0.0-rc.0+sha-507b346
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8230,7 +8230,7 @@ interface ɵFieldState<T> {
     /**
      * A signal indicating the patterns the field must match.
      */
-    readonly pattern?: Signal<readonly RegExp[]>;
+    readonly pattern: Signal<readonly RegExp[]>;
     /**
      * A signal indicating whether the field is currently readonly.
      */
@@ -8238,7 +8238,7 @@ interface ɵFieldState<T> {
     /**
      * A signal indicating whether the field is required.
      */
-    readonly required?: Signal<boolean>;
+    readonly required: Signal<boolean>;
     /**
      * A signal indicating whether the field has been touched by the user.
      */
