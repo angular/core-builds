5b75ee161e97a8d57abb37e1c0d6202e40f5412c
Andrew Scott <atscott@google.com>
2025-11-05 17:10:58 +0000

    744cd5c refactor(http): simplifies destruction tracking using destroyed property

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-a45e6b2
+ * @license Angular v20.3.9+sha-744cd5c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14632,7 +14632,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.9+sha-a45e6b2']
+        ? ['ng-version', '20.3.9+sha-744cd5c']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
