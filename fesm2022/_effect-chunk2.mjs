06ea9aa60c83c850bbaaacb55e6e8aa677c01eef
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 21:12:32 +0000

    6d1a978 Revert "build: restrain visibility to compiler-cli subpackages (#64732)"

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-70d8ed4
+ * @license Angular v21.1.0-next.0+sha-6d1a978
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-70d8ed4');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-6d1a978');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
