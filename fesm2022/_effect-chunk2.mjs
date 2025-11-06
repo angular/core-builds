6a097b38515ffb5740d38b461cfb3a471d2dfb42
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 15:57:27 +0000

    26995e2 fix(docs-infra): properly expose the prerendered routes for sitemap generation

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-f0ffd9b
+ * @license Angular v21.0.0-rc.1+sha-26995e2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-f0ffd9b');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-26995e2');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
