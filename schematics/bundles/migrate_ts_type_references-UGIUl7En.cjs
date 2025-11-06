c2f37b494ac919a1b652bf25ae130617376bc491
GitHub <noreply@github.com>
2025-11-06 18:11:22 +0000

    3bed9f0 build: format md files

--- schematics/bundles/migrate_ts_type_references-UGIUl7En.cjs
+++ schematics/bundles/migrate_ts_type_references-UGIUl7En.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.1+sha-60ce31f
+ * @license Angular v21.0.0-rc.1+sha-3bed9f0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
