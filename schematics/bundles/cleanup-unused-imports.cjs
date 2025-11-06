ef6e245e43ea47d844d16857644a70bd522ae188
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 19:04:48 +0000

    e90f9e4 refactor(forms): tree-shake `ngControlStatusHost` and `ngGroupStatusHost`

--- schematics/bundles/cleanup-unused-imports.cjs
+++ schematics/bundles/cleanup-unused-imports.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.1+sha-3bed9f0
+ * @license Angular v21.0.0-rc.1+sha-e90f9e4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
