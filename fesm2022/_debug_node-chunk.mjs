02697f088a001e1fdd6c304069d6d77699064bf6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-07 15:52:49 +0000

    0efb3f6 refactor(forms): nicer type errors

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ff3013c
+ * @license Angular v21.1.0-next.0+sha-0efb3f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8211,7 +8211,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-ff3013c'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-0efb3f6'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_resource-chunk.mjs
+++ fesm2022/_resource-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ff3013c
+ * @license Angular v21.1.0-next.0+sha-0efb3f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -125,23 +125,6 @@ class BaseWritableResource {
     }
     return this.value() !== undefined;
   });
-  _snapshot;
-  get snapshot() {
-    return this._snapshot ??= computed(() => {
-      const status = this.status();
-      if (status === 'error') {
-        return {
-          status: 'error',
-          error: this.error()
-        };
-      } else {
-        return {
-          status,
-          value: this.value()
-        };
-      }
-    });
-  }
   hasValue() {
     return this.isValueDefined();
   }
@@ -391,5 +374,5 @@ class ResourceWrappedError extends Error {
   }
 }
 
-export { OutputEmitterRef, ResourceImpl, ResourceValueError, computed, encapsulateResourceError, getOutputDestroyRef, linkedSignal, resource, untracked };
+export { OutputEmitterRef, ResourceImpl, computed, encapsulateResourceError, getOutputDestroyRef, linkedSignal, resource, untracked };
 //# sourceMappingURL=_resource-chunk.mjs.map
