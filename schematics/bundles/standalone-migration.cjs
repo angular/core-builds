098655aed1878325aa5a2923a0c7697007a0f68f
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:50:36 +0000

    f54cc4f fix(compiler): support commas in :host() argument

--- schematics/bundles/standalone-migration.cjs
+++ schematics/bundles/standalone-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.1+sha-0552e72
+ * @license Angular v21.0.0-rc.1+sha-f54cc4f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
