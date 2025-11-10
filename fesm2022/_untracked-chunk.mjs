53562ecbe8b659660405eeb4310d6b0de770a6fc
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-10 16:28:12 +0000

    afa2ac2 build: update cross-repo angular dependencies

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-5092a89
+ * @license Angular v21.0.0-rc.1+sha-afa2ac2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-5092a89');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-afa2ac2');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
