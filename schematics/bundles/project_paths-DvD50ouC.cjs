f87058b5055c86e64a5e2f3362ca68e3ea2406c8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 19:53:02 +0000

    dc11db1 docs: correct example for RouteReuseStrategy getRouteKey

--- schematics/bundles/project_paths-DvD50ouC.cjs
+++ schematics/bundles/project_paths-DvD50ouC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-129e16d
+ * @license Angular v21.0.0-rc.2+sha-dc11db1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
