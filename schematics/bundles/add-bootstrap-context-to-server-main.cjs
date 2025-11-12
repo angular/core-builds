d2124df96e4d1d3eab396325d235cab2dfa9bded
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 23:07:04 +0000

    31ca56a docs: Fixed mermaid diagram node label contrast in dark mode

--- schematics/bundles/add-bootstrap-context-to-server-main.cjs
+++ schematics/bundles/add-bootstrap-context-to-server-main.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-be37129
+ * @license Angular v21.0.0-rc.2+sha-31ca56a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
