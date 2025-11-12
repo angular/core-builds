300ed76cbdb713a4f870a0ab2e3842fb6ddb6835
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-12 16:27:28 +0000

    3bafe23 release: cut the v20.3.11 release

--- schematics/bundles/project_tsconfig_paths-CaweCOep.cjs
+++ schematics/bundles/project_tsconfig_paths-CaweCOep.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.10+sha-9419ea3
+ * @license Angular v20.3.11+sha-3bafe23
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32822,7 +32822,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.10+sha-9419ea3');
+const VERSION = new Version('20.3.11+sha-3bafe23');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/signal-input-migration.cjs
+++ schematics/bundles/signal-input-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.10+sha-9419ea3
+ * @license Angular v20.3.11+sha-3bafe23
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
