78ce123030958277f173c9b30a64b6a66a6dd354
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 17:11:08 +0000

    a586bd2 docs(docs-infra): improve playground layout

--- schematics/bundles/project_tsconfig_paths-CaweCOep.cjs
+++ schematics/bundles/project_tsconfig_paths-CaweCOep.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.11+sha-3c065f7
+ * @license Angular v20.3.11+sha-a586bd2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32822,7 +32822,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.11+sha-3c065f7');
+const VERSION = new Version('20.3.11+sha-a586bd2');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/router-current-navigation.cjs
+++ schematics/bundles/router-current-navigation.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.11+sha-3c065f7
+ * @license Angular v20.3.11+sha-a586bd2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
