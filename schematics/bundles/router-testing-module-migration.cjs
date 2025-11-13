3672cdf2a718523afa1cf935ba7332856092bbea
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:17:20 +0000

    4b0f208 ci: use hardcoded global approvers group

--- schematics/bundles/router-testing-module-migration.cjs
+++ schematics/bundles/router-testing-module-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-482e137
+ * @license Angular v21.0.0-rc.2+sha-4b0f208
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
