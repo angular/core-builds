db0575f15fca80b69e5b5189ef068769530a0443
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 22:08:43 +0000

    70da36f docs(docs-infra): improve theme picker accessibility and add animations

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-06bc146
+ * @license Angular v20.3.11+sha-70da36f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-06bc146');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-70da36f');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/project_tsconfig_paths-CaweCOep.cjs
+++ schematics/bundles/project_tsconfig_paths-CaweCOep.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.11+sha-06bc146
+ * @license Angular v20.3.11+sha-70da36f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32822,7 +32822,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.11+sha-06bc146');
+const VERSION = new Version('20.3.11+sha-70da36f');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
