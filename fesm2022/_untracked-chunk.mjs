a784a68d65415e700c96c0712258f86fd89017a8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 19:30:45 +0000

    8a5b284 docs: add draft of aria component guides

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-966bfae
+ * @license Angular v21.0.0-rc.2+sha-8a5b284
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-966bfae');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-8a5b284');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- schematics/bundles/common-to-standalone-migration.cjs
+++ schematics/bundles/common-to-standalone-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-966bfae
+ * @license Angular v21.0.0-rc.2+sha-8a5b284
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
