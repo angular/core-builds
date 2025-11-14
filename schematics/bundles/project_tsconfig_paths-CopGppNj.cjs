ecc5ed9e6b23c54fe6ca9905c776ed69a94071a3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:35:46 +0000

    e0a6dad docs: Uses the self-closing tag syntax

--- schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
+++ schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.12+sha-35e7b9a
+ * @license Angular v20.3.12+sha-e0a6dad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.12+sha-35e7b9a');
+const VERSION = new Version('20.3.12+sha-e0a6dad');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
