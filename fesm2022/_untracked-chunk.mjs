fdfb9117f4e21141a3804d041f1b7e29ea596105
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 16:46:13 +0000

    c137f1f docs: ExperimentalIsolatedShadowDom mentions

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8989536
+ * @license Angular v21.1.0-next.0+sha-c137f1f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8989536');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c137f1f');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
