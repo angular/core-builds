a8fe636fdfa523a936f0bfed10e197c96db4876b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 17:27:11 +0000

    966bfae build(forms): expose signal forms compat package

--- schematics/bundles/ngstyle-to-style-migration.cjs
+++ schematics/bundles/ngstyle-to-style-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-911999e
+ * @license Angular v21.0.0-rc.2+sha-966bfae
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
