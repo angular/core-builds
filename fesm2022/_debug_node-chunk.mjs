250402ecc1cc56f2ff827455a0c9490fc913e9aa
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:05:43 +0000

    c2a52fd docs: add aria toolbar component guide

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-afeba79
+ * @license Angular v21.0.0-rc.2+sha-c2a52fd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8213,7 +8213,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.0-rc.2+sha-afeba79'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.0-rc.2+sha-c2a52fd'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- schematics/bundles/leading_space-D9nQ8UQC.cjs
+++ schematics/bundles/leading_space-D9nQ8UQC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-afeba79
+ * @license Angular v21.0.0-rc.2+sha-c2a52fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
