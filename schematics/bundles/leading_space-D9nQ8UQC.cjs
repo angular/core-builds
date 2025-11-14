70684d7ddce03731524f0b7cdf649d3e47c0120e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 22:08:27 +0000

    e1a7c35 refactor(forms): improve typing on min & max (#65212)

--- schematics/bundles/leading_space-D9nQ8UQC.cjs
+++ schematics/bundles/leading_space-D9nQ8UQC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-3228cce
+ * @license Angular v21.0.0-rc.2+sha-e1a7c35
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
