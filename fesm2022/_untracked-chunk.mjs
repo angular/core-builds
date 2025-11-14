efb3c4a72e6e5da8311ed6315833a205e6854775
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:47:36 +0000

    f550f4a docs: ExperimentalIsolatedShadowDom mentions

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-c2a52fd
+ * @license Angular v21.0.0-rc.2+sha-f550f4a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-c2a52fd');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-f550f4a');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- schematics/bundles/standalone-migration.cjs
+++ schematics/bundles/standalone-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-c2a52fd
+ * @license Angular v21.0.0-rc.2+sha-f550f4a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
