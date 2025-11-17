3d4213fde6975e7840f4fa1d7cae9443f1dba579
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 18:14:44 +0000

    127cadd Revert "refactor(core): let the profiler handle asymmetric events leniently"

--- schematics/bundles/router-testing-module-migration.cjs
+++ schematics/bundles/router-testing-module-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-ecea909
+ * @license Angular v21.0.0-rc.2+sha-127cadd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
