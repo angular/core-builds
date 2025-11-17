c08029015b55de3fa42c598663ad64752b075d34
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:15:16 +0000

    59543d6 docs: add bun package manager and standardize order in testing guides

--- schematics/bundles/route-lazy-loading.cjs
+++ schematics/bundles/route-lazy-loading.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.2+sha-63ca0de
+ * @license Angular v21.0.0-rc.2+sha-59543d6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
