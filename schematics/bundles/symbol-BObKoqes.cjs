1549dfc27ca9afca2e90188e626a14d546dedba3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 22:59:31 +0000

    21e2889 docs: aria combobox examples

--- schematics/bundles/symbol-BObKoqes.cjs
+++ schematics/bundles/symbol-BObKoqes.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.3+sha-73e9700
+ * @license Angular v21.0.0-rc.3+sha-21e2889
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
