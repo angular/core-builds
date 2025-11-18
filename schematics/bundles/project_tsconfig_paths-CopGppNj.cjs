5854c365f60e79c3a426a4338083e3a59642fb71
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 16:19:10 +0000

    3686987 docs: simplify docs-code header by removing paths and keeping only file name and info

--- schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
+++ schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.12+sha-72b19d8
+ * @license Angular v20.3.12+sha-3686987
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.12+sha-72b19d8');
+const VERSION = new Version('20.3.12+sha-3686987');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
