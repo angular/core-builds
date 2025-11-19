1879bc2d900ac8c9188423b7183455d5f2d98f5e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 17:46:20 +0000

    eb2f665 docs: release notes for the v20.3.13 release

--- schematics/bundles/index-B7I9sIUx.cjs
+++ schematics/bundles/index-B7I9sIUx.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.0+sha-a51ba9c
+ * @license Angular v21.1.0-next.0+sha-eb2f665
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
