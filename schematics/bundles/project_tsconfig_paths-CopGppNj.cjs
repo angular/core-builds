7748d949ed979795ac47a98bb9530d3356fb65dc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:29:41 +0000

    24c999a fix(docs-infra): add moduleResolution to TypeScript compiler options for playground

--- schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
+++ schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.12+sha-96a8d58
+ * @license Angular v20.3.12+sha-24c999a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.12+sha-96a8d58');
+const VERSION = new Version('20.3.12+sha-24c999a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
