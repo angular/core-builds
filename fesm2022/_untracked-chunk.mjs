e4cfda8de5dd4d5103ca6d08470531f0249132c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:51:39 +0000

    8b492ce docs: update the actively supported versions

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-ac34824
+ * @license Angular v21.0.0-rc.3+sha-8b492ce
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-ac34824');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-8b492ce');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- schematics/bundles/signal-queries-migration.cjs
+++ schematics/bundles/signal-queries-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.3+sha-ac34824
+ * @license Angular v21.0.0-rc.3+sha-8b492ce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
