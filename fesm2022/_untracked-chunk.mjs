4951f1e314825c677ae1679992650e4465fea632
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:30:28 +0000

    ac34824 fix(docs-infra): add moduleResolution to TypeScript compiler options for playground

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-e69855a
+ * @license Angular v21.0.0-rc.3+sha-ac34824
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-e69855a');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-ac34824');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- schematics/bundles/router-testing-module-migration.cjs
+++ schematics/bundles/router-testing-module-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.3+sha-e69855a
+ * @license Angular v21.0.0-rc.3+sha-ac34824
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
