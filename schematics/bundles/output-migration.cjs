60055f6f1caf6410d2b4504da92a8a51d1f91773
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 01:05:48 +0000

    28c1a2d docs: add signal forms comparison guide

--- schematics/bundles/output-migration.cjs
+++ schematics/bundles/output-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0-rc.3+sha-012326e
+ * @license Angular v21.0.0-rc.3+sha-28c1a2d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
