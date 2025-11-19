4db22878c43dd65c6bf31fe255f2fcd07036cdac
Joey Perrott <josephperrott@gmail.com>
2025-11-19 21:38:48 +0000

    4e14a53 docs: add release blog to v21 page

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-6854776
+ * @license Angular v21.0.0+sha-4e14a53
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-6854776');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4e14a53');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
