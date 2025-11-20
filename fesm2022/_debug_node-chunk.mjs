f95298cfbccf8878f777ba9b08578649517bfae3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-20 00:45:07 +0000

    8904142 docs: adds a link to the signal forms example

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-34e1fe2
+ * @license Angular v21.1.0-next.0+sha-8904142
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8238,7 +8238,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-34e1fe2'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-8904142'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
