e7edf260e6c22494f8ee9d3a807c7ea192101c47
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 19:31:55 +0000

    5ddeafc docs: update cross-repo adev docs

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b8fa9cc
+ * @license Angular v21.1.0-next.0+sha-5ddeafc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b8fa9cc');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5ddeafc');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
