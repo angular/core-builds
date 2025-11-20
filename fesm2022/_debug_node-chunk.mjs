3370973121dbb508376f38b0c3865da2e9119363
kirjs <kirts@google.com>
2025-11-20 16:25:43 +0000

    f349a91 docs: fix standlone menu overlay

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-630a3b2
+ * @license Angular v21.0.0+sha-f349a91
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8238,7 +8238,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.0+sha-630a3b2'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.0+sha-f349a91'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- schematics/bundles/project_paths-DvD50ouC.cjs
+++ schematics/bundles/project_paths-DvD50ouC.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0+sha-630a3b2
+ * @license Angular v21.0.0+sha-f349a91
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
