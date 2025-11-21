d51d1cd5d3582a11b6513da568e87b1c64c98f4d
kirjs <kirts@google.com>
2025-11-21 00:25:16 +0000

    1925721 refactor(docs-infra): automate algolia index name based on package version

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-70fb1d6
+ * @license Angular v21.0.0+sha-1925721
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70fb1d6');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1925721');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
