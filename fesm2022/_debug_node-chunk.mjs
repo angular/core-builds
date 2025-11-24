502fb880e86fe7a72ef2434a38f1ab5520bdb382
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 16:15:40 +0000

    b5ae62c docs: update devtools guide - splits the injector inspector into its own guide - update the screenshots - add defer block section - add hydration section

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0203dad
+ * @license Angular v21.1.0-next.0+sha-b5ae62c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8247,7 +8247,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-0203dad'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-b5ae62c'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
