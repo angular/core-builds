74e519bedbc053fbe1e2be50f4210e85ba8cd39c
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 18:23:29 +0000

    2ac8268 docs: Update router docs to add references and components input fixed syntaxis

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a4f50bd
+ * @license Angular v21.1.0-next.0+sha-2ac8268
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8247,7 +8247,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-a4f50bd'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-2ac8268'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
