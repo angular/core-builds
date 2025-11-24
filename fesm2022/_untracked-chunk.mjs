2c92d8248f95fd16f8f8d1c58669aa3f0c6b16e0
kirjs <kirts@google.com>
2025-11-24 15:29:02 +0000

    1f20b54 docs(docs-infra): simplify tab selection

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-2d658b3
+ * @license Angular v21.0.0+sha-1f20b54
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2d658b3');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1f20b54');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
