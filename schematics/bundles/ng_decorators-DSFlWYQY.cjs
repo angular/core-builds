80d9336886ebc5e7584761077e497f9732b2a20c
kirjs <kirts@google.com>
2025-11-24 17:23:00 +0000

    f73cfd5 ci: adding myself as optional to the compiler scope

--- schematics/bundles/ng_decorators-DSFlWYQY.cjs
+++ schematics/bundles/ng_decorators-DSFlWYQY.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.0+sha-9852033
+ * @license Angular v21.0.0+sha-f73cfd5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- types/primitives-event-dispatch.d.ts
+++ types/primitives-event-dispatch.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-9852033
+ * @license Angular v21.0.0+sha-f73cfd5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
