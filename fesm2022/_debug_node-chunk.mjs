23abd04f04ab167e7a3e792bd56db62d1f4cd32b
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 14:20:10 +0000

    3a1eb07 fix(forms): allow dynamic `type` bindings on signal form controls

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a294e3b
+ * @license Angular v21.1.0-next.0+sha-3a1eb07
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8248,7 +8248,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-a294e3b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-3a1eb07'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
