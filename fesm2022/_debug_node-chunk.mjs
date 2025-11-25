841a0ffad45acc0375ccfc2d26173a041e412dde
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 12:47:39 +0000

    a294e3b docs: fix example imports and missing event URL reference

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-51ebe9a
+ * @license Angular v21.1.0-next.0+sha-a294e3b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8248,7 +8248,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-51ebe9a'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-a294e3b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
