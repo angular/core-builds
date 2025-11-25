33d816f5cff00fbbb784edbee44ce2ec0851beeb
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:18:03 +0000

    136e923 release: cut the v20.3.14 release

--- schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
+++ schematics/bundles/project_tsconfig_paths-CopGppNj.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.13+sha-0276479
+ * @license Angular v20.3.14+sha-136e923
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32792,7 +32792,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.13+sha-0276479');
+const VERSION = new Version('20.3.14+sha-136e923');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
--- schematics/bundles/self-closing-tags-migration.cjs
+++ schematics/bundles/self-closing-tags-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.13+sha-0276479
+ * @license Angular v20.3.14+sha-136e923
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
