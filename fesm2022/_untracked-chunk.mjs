97e707be9554487d6e182089b38d463169d131b1
kirjs <kirts@google.com>
2025-11-25 23:33:46 +0000

    10201b6 docs(docs-infra): renders inline code snippets in docs pills

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-47a7396
+ * @license Angular v21.0.1+sha-10201b6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-47a7396');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-10201b6');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
