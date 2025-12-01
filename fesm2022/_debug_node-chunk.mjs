86074dfecc2d0acff98d0ba4347454b72e405f80
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:37:35 +0000

    aea2465 docs: update notes to use consistent formatting in various guides

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-25bf417
+ * @license Angular v21.1.0-next.0+sha-aea2465
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8280,7 +8280,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-25bf417'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-aea2465'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
