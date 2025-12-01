d8f24f64f93ae7239983f6375f6c13e88e65712c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:02:38 +0000

    721080a docs: fix npm link URL in creating-libraries guide

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-242d332
+ * @license Angular v21.1.0-next.0+sha-721080a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-242d332');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-721080a');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
