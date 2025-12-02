3d18a2a3771e6e5c6c415b715d981c7aa6e2fb3f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 14:16:09 +0000

    e30e61b fix(compiler-cli): avoid allocating an object for signals in production mode

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f35b2ef
+ * @license Angular v21.1.0-next.0+sha-e30e61b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f35b2ef');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-e30e61b');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
