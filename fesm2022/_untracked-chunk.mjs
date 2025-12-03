6698416b282f7eee498088d7ec78ecd2b7728002
Alan Agius <alan.agius4@gmail.com>
2025-12-03 12:21:48 +0000

    3e89577 release: bump Angular DevTools version to 1.6.2

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0659d11
+ * @license Angular v21.1.0-next.0+sha-3e89577
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0659d11');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3e89577');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
