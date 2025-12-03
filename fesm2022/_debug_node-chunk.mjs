4116c26ddb7a82324e355cb3b7f8b11a6d5cb9f0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 09:17:54 +0000

    e26aa86 build: update bazel dependencies

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29223c4
+ * @license Angular v21.1.0-next.0+sha-e26aa86
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8281,7 +8281,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-29223c4'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.0+sha-e26aa86'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
