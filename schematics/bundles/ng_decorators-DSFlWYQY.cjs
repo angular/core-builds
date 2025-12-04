58f3209f55fe8ff45d1b895c4cc10db6f982cd6a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-04 19:49:58 +0000

    789f91b feat(docs-infra): forward preview error locations

--- schematics/bundles/ng_decorators-DSFlWYQY.cjs
+++ schematics/bundles/ng_decorators-DSFlWYQY.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.1+sha-5be3304
+ * @license Angular v21.1.0-next.1+sha-789f91b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
