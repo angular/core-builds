21934fabdb3d28fbc43c39c77fd4f10d85385bf8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 17:42:11 +0000

    dd47c57 build: update cross-repo angular dependencies

--- schematics/bundles/compiler_host-DBwYMlTo.cjs
+++ schematics/bundles/compiler_host-DBwYMlTo.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.1+sha-70e024e
+ * @license Angular v21.1.0-next.1+sha-dd47c57
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
