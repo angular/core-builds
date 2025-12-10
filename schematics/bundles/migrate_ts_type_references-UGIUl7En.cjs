717144525fa25b85d038d6b255ad99fb700b015d
Jan Olaf Martin <jan.krems@gmail.com>
2025-12-10 19:43:23 +0000

    954c546 release: bump Angular DevTools version to 1.6.3

--- schematics/bundles/migrate_ts_type_references-UGIUl7En.cjs
+++ schematics/bundles/migrate_ts_type_references-UGIUl7En.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.1+sha-5988bde
+ * @license Angular v21.1.0-next.1+sha-954c546
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
