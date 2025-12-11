7e76b3d11f149611c72a09a2eb151fbae3239729
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 01:03:59 +0000

    1636451 release: cut the v21.1.0-next.2 release

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-d343ff3
+ * @license Angular v21.1.0-next.2+sha-1636451
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d343ff3');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.2+sha-1636451');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
