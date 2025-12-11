7fb0c15ca3e7109403aac7ed5284244b575727a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:37:10 +0000

    4fd2b72 fix(forms): fix signal forms type error

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.4+sha-69d243a
+ * @license Angular v21.0.4+sha-4fd2b72
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.4+sha-69d243a');
+const VERSION = /* @__PURE__ */new Version('21.0.4+sha-4fd2b72');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- schematics/bundles/bootstrap-options-migration.cjs
+++ schematics/bundles/bootstrap-options-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.4+sha-69d243a
+ * @license Angular v21.0.4+sha-4fd2b72
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
