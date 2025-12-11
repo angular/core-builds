474b395497bbf63437e9b6c1110b9aec7010421b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:37:13 +0000

    d0097f7 fix(forms): fix signal forms type error

--- schematics/bundles/add-bootstrap-context-to-server-main.cjs
+++ schematics/bundles/add-bootstrap-context-to-server-main.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.2+sha-4f6014a
+ * @license Angular v21.1.0-next.2+sha-d0097f7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
