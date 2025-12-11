d39c2115d93bc868df48a870b2a1532626ae1c35
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:48:13 +0000

    a52c834 build: update ng-dev to latest version

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-d0c3806
+ * @license Angular v21.1.0-next.1+sha-a52c834
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d0c3806');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-a52c834');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
