d3b204dff1d3699e6afe7fdde9c2de9ca7da0c02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:45:47 +0000

    5a21465 release: cut the v21.0.5 release

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.4+sha-4fd2b72
+ * @license Angular v21.0.5+sha-5a21465
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.4+sha-4fd2b72');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-5a21465');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
