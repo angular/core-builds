44151b22bd73299ecc5a333d9bce5307a689e7f2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 18:04:44 +0000

    23fbe5c docs: replace `@debugging` tag with proper Markdown syntax

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-b5205c7
+ * @license Angular v21.1.0-next.3+sha-23fbe5c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-b5205c7');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-23fbe5c');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
@@ -498,7 +498,7 @@ function getPipeDef(type) {
 }
 function assertTypeDefined(type, symbolType) {
   if (type == null) {
-    throw new RuntimeError(919, (typeof ngDevMode === 'undefined' || ngDevMode) && `Cannot read ${symbolType} metadata. This can indicate a runtime ` + `circular dependency in your app that needs to be resolved.`);
+    throw new RuntimeError(-919, (typeof ngDevMode === 'undefined' || ngDevMode) && `Cannot read ${symbolType} metadata. This can indicate a runtime ` + `circular dependency in your app that needs to be resolved.`);
   }
 }
 function isStandalone(type) {
