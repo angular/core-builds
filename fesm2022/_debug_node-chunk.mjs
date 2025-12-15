db90bed06c443f059fd614a7db1d7fd8d40c9e9a
GitHub <noreply@github.com>
2025-12-15 22:06:54 +0000

    193aa33 docs(forms): improve JSDoc for Signal Forms Schema types

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-356092a
+ * @license Angular v21.1.0-next.3+sha-193aa33
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8310,7 +8310,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.3+sha-356092a'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.3+sha-193aa33'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- schematics/bundles/nodes-B16H9JUd.cjs
+++ schematics/bundles/nodes-B16H9JUd.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.3+sha-356092a
+ * @license Angular v21.1.0-next.3+sha-193aa33
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
