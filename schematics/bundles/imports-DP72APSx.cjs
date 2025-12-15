027fc7120cb605573ff132cd508cc2734229c47e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 17:49:12 +0000

    b5205c7 docs(docs-infra): throw when rendering absolute links to adev in our guides

--- schematics/bundles/imports-DP72APSx.cjs
+++ schematics/bundles/imports-DP72APSx.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.3+sha-8243bb3
+ * @license Angular v21.1.0-next.3+sha-b5205c7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
