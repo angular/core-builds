50908921318fafa8908c9cf3951c29bb9a74210f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 22:23:50 +0000

    11e271c build: update cross-repo angular dependencies

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-d4111ee
+ * @license Angular v21.1.0-next.3+sha-11e271c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8310,7 +8310,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.3+sha-d4111ee'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-next.3+sha-11e271c'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- schematics/bundles/apply_import_manager-1Zs_gpB6.cjs
+++ schematics/bundles/apply_import_manager-1Zs_gpB6.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.3+sha-d4111ee
+ * @license Angular v21.1.0-next.3+sha-11e271c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
