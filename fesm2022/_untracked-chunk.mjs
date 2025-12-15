60be47533d764356b1c54b238a39298d41e6451c
Andrew Kushnir <akushnir@google.com>
2025-12-15 19:49:08 +0000

    3151c05 refactor(forms): add signal forms to the type tests

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-0dd5417
+ * @license Angular v21.0.5+sha-3151c05
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-0dd5417');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-3151c05');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- types/_weak_ref-chunk.d.ts
+++ types/_weak_ref-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-0dd5417
+ * @license Angular v21.0.5+sha-3151c05
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
