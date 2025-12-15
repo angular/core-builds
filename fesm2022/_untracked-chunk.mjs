38280c98d2d3315e8b1ebbfa6c7242d9dd703b9b
Andrew Kushnir <akushnir@google.com>
2025-12-15 22:20:23 +0000

    31b9090 refactor(compiler): remove unnecessary sanitization for safe attributes

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-5afb1eb
+ * @license Angular v21.0.5+sha-31b9090
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-5afb1eb');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-31b9090');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
