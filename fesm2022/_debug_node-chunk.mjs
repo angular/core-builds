80f311833f9e55dafd32f113e4c80d3c0c172ede
GitHub <noreply@github.com>
2025-12-17 21:56:01 +0000

    eaa68c4 release: bump VSCode extension version to 21.0.1 (#66153)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-85317f4
+ * @license Angular v21.0.6+sha-eaa68c4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8310,7 +8310,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.6+sha-85317f4'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.6+sha-eaa68c4'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- schematics/bundles/ngstyle-to-style-migration.cjs
+++ schematics/bundles/ngstyle-to-style-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.6+sha-85317f4
+ * @license Angular v21.0.6+sha-eaa68c4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
