2f8f0da8f338e11dfaa8e35984faf1f5e27e71b7
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:01:07 +0000

    2e58b66 build: update pnpm to v10.26.0

--- schematics/bundles/property_name-BBwFuqMe.cjs
+++ schematics/bundles/property_name-BBwFuqMe.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.3+sha-d23ce76
+ * @license Angular v21.1.0-next.3+sha-2e58b66
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
