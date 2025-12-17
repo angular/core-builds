4542bc318a969b4088ba7602991c1ea055bacd9d
Andrew Kushnir <akushnir@google.com>
2025-12-17 17:49:39 +0000

    14b529e docs: align lightweight injection tokens with the style guide

--- schematics/bundles/parse_html-8VLCL37B.cjs
+++ schematics/bundles/parse_html-8VLCL37B.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.0.5+sha-dd38869
+ * @license Angular v21.0.5+sha-14b529e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
--- types/primitives-signals.d.ts
+++ types/primitives-signals.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-dd38869
+ * @license Angular v21.0.5+sha-14b529e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
