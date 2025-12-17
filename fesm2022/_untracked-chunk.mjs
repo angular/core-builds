4cbfc26e6a3975469a0d8f6e9539cc86a7190f7e
Andrew Kushnir <akushnir@google.com>
2025-12-17 17:44:50 +0000

    dd38869 docs(docs-infra): leverage the search on 404

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-a4d2de8
+ * @license Angular v21.0.5+sha-dd38869
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-a4d2de8');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-dd38869');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-a4d2de8
+ * @license Angular v21.0.5+sha-dd38869
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
