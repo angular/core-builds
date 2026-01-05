99d45b2164a719c474aea057cd9846976a4f3fb0
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 17:29:32 +0000

    6a94300 refactor(devtools): use `whenStable` instead of `detectChanges`

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-821d565
+ * @license Angular v21.1.0-next.4+sha-6a94300
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-821d565');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-6a94300');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
