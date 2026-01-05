6a90c03c92d38673d66720a4f98c021f424831c9
kirjs <kirts@google.com>
2026-01-05 17:00:18 +0000

    6c911ad docs: fix reference to parent from children resolvers

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-71a4373
+ * @license Angular v21.0.6+sha-6c911ad
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-71a4373');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-6c911ad');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
--- fesm2022/primitives-event-dispatch.mjs
+++ fesm2022/primitives-event-dispatch.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-71a4373
+ * @license Angular v21.0.6+sha-6c911ad
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
