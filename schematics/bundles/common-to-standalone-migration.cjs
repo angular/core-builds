5c21f8d31ab9bd2a52b73bc4e92d1d6477fe298b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 22:39:54 +0000

    9184146 build: update cross-repo angular dependencies

--- schematics/bundles/common-to-standalone-migration.cjs
+++ schematics/bundles/common-to-standalone-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.4+sha-df2aaaa
+ * @license Angular v21.1.0-next.4+sha-9184146
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
