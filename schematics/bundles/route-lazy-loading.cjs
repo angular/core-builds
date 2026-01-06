20b468d02a135215e5af0a0e0713145aa0858cdb
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 00:30:36 +0000

    456ca35 docs(forms): fix duplicate validator reference titles in AbstractControl

--- schematics/bundles/route-lazy-loading.cjs
+++ schematics/bundles/route-lazy-loading.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.4+sha-9184146
+ * @license Angular v21.1.0-next.4+sha-456ca35
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
