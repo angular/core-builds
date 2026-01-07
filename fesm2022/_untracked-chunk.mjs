13a46f87052fab23ee12031d53db2cf5566225f7
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 19:23:53 +0000

    090484c docs(docs-infra): Fix combobox dialog styles for dark theme

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-8c8c5b2
+ * @license Angular v21.1.0-next.4+sha-090484c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-8c8c5b2');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-090484c');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
