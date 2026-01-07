e828e41f6df40d1d799600baa36360f38165783c
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 14:40:19 +0000

    b17f93b docs(docs-infra): Use category for signal forms guides

--- schematics/bundles/imports-DP72APSx.cjs
+++ schematics/bundles/imports-DP72APSx.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.4+sha-640693d
+ * @license Angular v21.1.0-next.4+sha-b17f93b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
