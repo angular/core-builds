7a0e805a653942fdf8dc29e5a67eb291ec834737
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 22:18:21 +0000

    f8db6ad docs: release notes for the v19.2.18 release

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-414b647
+ * @license Angular v21.1.0-next.4+sha-f8db6ad
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-414b647');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-f8db6ad');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
