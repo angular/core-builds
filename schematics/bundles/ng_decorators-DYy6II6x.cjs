17f946f509d7ffaf58053d7bddd81d91291a3c99
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 22:25:42 +0000

    af4e521 docs: release notes for the v20.3.16 release

--- schematics/bundles/ng_decorators-DYy6II6x.cjs
+++ schematics/bundles/ng_decorators-DYy6II6x.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-next.4+sha-f8db6ad
+ * @license Angular v21.1.0-next.4+sha-af4e521
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
