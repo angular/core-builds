0373ebd62271cfa6a6f45e37333c73c1c74f1657
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 14:14:09 +0000

    f811c6c docs: fixed incorrect link in migrating-to-vitest.md

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-174d2da
+ * @license Angular v21.1.0-next.4+sha-f811c6c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-174d2da');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-f811c6c');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
