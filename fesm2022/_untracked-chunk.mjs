7960d3bcbd3628e6c217b33cc92539eac914d2bc
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 20:56:36 +0000

    9529362 docs: release notes for the v21.1.0-rc.0 release

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-3b8c4fc
+ * @license Angular v21.2.0-next.0+sha-9529362
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-3b8c4fc');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-9529362');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
