60458a5057852eb7032ebc4c5a85d00f671342f9
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 17:59:34 +0000

    ac865b5 build: update cross-repo angular dependencies

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-94b7079
+ * @license Angular v21.2.0-next.0+sha-ac865b5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94b7079');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ac865b5');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
