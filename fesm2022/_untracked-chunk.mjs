ad347c71ba3b3becb686b299147f3d7494350c08
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 17:42:20 +0000

    94b7079 docs(docs-infra): avoid persistent AbortSignal listener in wait debounce in search

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e3b3290
+ * @license Angular v21.2.0-next.0+sha-94b7079
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e3b3290');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94b7079');
 
 const ERROR_DETAILS_PAGE_BASE_URL = (() => {
   const versionSubDomain = VERSION.major !== '0' ? `v${VERSION.major}.` : '';
