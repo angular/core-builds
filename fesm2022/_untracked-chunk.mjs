7f43fd47e40881687687b7cd5b6ceb1b99730235
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:54:30 +0000

    e4dcd72 fix(docs-infra): improve symbolic green contrast for WCAG compliance

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-204aaca
+ * @license Angular v21.1.0-rc.0+sha-e4dcd72
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-204aaca');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-e4dcd72');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
