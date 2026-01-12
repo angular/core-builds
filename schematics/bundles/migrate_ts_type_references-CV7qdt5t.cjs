d8184bb49bad3dae2b7dc2d2630ce2e2497413ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:34:26 +0000

    addccea docs: change language section to "community translations"

--- schematics/bundles/migrate_ts_type_references-CV7qdt5t.cjs
+++ schematics/bundles/migrate_ts_type_references-CV7qdt5t.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-rc.0+sha-8f451b5
+ * @license Angular v21.1.0-rc.0+sha-addccea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
