ecf0e817616439dd5d761dece16fe6eb224e61e9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 21:57:35 +0000

    82d556a fix(forms): Ensure the control instruction comes after the other bindings

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-6a9d4d5
+ * @license Angular v21.0.8+sha-82d556a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8313,7 +8313,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.8+sha-6a9d4d5'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.8+sha-82d556a'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-6a9d4d5
+ * @license Angular v21.0.8+sha-82d556a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
