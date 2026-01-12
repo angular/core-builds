1c4c858c885c43f86de9bc67eca970b971d9ebfd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 18:20:22 +0000

    1f47e88 build(docs-infra): Update dependency @xterm/xterm to v6

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-d1f6bcd
+ * @license Angular v21.1.0-rc.0+sha-1f47e88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-d1f6bcd');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-1f47e88');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
