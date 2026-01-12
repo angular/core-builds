63bf91e2195dd20eaca369d52418597beaf77a42
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 17:34:14 +0000

    ad699ce docs: change language section to "community translations"

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-89d47d8
+ * @license Angular v21.2.0-next.0+sha-ad699ce
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-89d47d8');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ad699ce');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
