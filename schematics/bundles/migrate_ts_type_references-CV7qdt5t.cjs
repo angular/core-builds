5bac1a5c1b375dc76cd5eed1e6d0d0bb5cee7f94
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 00:39:52 +0000

    76a6984 release: bump VSCode extension version to 21.1.0

--- schematics/bundles/migrate_ts_type_references-CV7qdt5t.cjs
+++ schematics/bundles/migrate_ts_type_references-CV7qdt5t.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.0-rc.0+sha-f5ed514
+ * @license Angular v21.1.0-rc.0+sha-76a6984
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
