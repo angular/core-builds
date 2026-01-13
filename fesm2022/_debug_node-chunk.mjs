0756541355c26a359978f140c3ce1a6fe3f6e4ee
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 19:24:33 +0000

    9c1a5a5 build: update cherrypick changelog to stash

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-a649fc8
+ * @license Angular v21.1.0-rc.0+sha-9c1a5a5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8313,7 +8313,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-rc.0+sha-a649fc8'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.0-rc.0+sha-9c1a5a5'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
