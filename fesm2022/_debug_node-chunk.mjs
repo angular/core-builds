6267fde87236263b3857502cc4e406ea233c7003
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 17:09:19 +0000

    01e5bab docs: update OnChanges API reference w/ signal input

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-8f4cc84
+ * @license Angular v21.0.8+sha-01e5bab
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8313,7 +8313,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.8+sha-8f4cc84'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.0.8+sha-01e5bab'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
