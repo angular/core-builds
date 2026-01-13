d519dd2483c8cb9f1ea9c64ba64e3c02f690ae25
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 17:00:41 +0000

    6e6a022 build: lock file maintenance

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e8f8ac2
+ * @license Angular v21.2.0-next.0+sha-6e6a022
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e8f8ac2');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-6e6a022');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
