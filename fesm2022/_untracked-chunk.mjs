3c8d572e29c91df448d00ea05f4832d587a19d73
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 00:10:35 +0000

    69da639 build: improve changelog generation and tag fetching in release script

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-7875577
+ * @license Angular v21.2.0-next.0+sha-69da639
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-7875577');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-69da639');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
