5a21cfe8ff0d4174a59b9f54a40745cfac239c40
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-14 23:09:52 +0000

    ae4e2e0 build: update cross-repo angular dependencies to v21.1.0

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-7f99907
+ * @license Angular v21.1.0+sha-ae4e2e0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-7f99907');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-ae4e2e0');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
