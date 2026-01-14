d3a72c4a0acad2c5f546f377cda6e323cf08f773
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 16:05:01 +0000

    074666b docs(docs-infra): use Signals Forms focusBoundControl

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-227acdd
+ * @license Angular v21.2.0-next.0+sha-074666b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-227acdd');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-074666b');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
