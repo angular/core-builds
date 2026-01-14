01085af89adb3235d3a4bebc0281ba62f0796a2b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 16:16:03 +0000

    c9f584b docs: add IMPORTANT prefix to pipe usage warning and

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-074666b
+ * @license Angular v21.2.0-next.0+sha-c9f584b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -903,7 +903,6 @@ function isDevMode() {
 function enableProdMode() {
   if (typeof ngDevMode === 'undefined' || ngDevMode) {
     _global['ngDevMode'] = false;
-    _global['ng'] = undefined;
   }
 }
 
