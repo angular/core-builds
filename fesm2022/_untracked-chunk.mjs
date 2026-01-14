f5b33441cab2a8177eda73f32eda471c40a09e19
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 16:55:55 +0000

    e817f8c docs: update cross-repo adev docs

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-1ea5c97
+ * @license Angular v21.1.0-rc.0+sha-e817f8c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-1ea5c97');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-e817f8c');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-1ea5c97
+ * @license Angular v21.1.0-rc.0+sha-e817f8c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
