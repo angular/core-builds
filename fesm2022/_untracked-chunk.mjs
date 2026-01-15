19fa5aa07c453f02fb408c0e56c61f592a35117a
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 19:13:10 +0000

    0643f54 docs: clarify that FESM can depend on shared chunks

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-c0446da
+ * @license Angular v21.1.0+sha-0643f54
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-c0446da');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-0643f54');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
