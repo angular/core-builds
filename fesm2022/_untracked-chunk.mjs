3175a8df2f279de62839ac1ee9a92df146d84e1f
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 18:42:32 +0000

    223ff74 build: don't substitute binary files

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-b9c995e
+ * @license Angular v21.1.0+sha-223ff74
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-b9c995e');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-223ff74');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
