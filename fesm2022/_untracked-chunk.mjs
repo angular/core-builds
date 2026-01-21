9effe971771306bc75f5c93ccc1e8b5791991d5f
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 19:44:20 +0000

    1884226 docs(docs-infra): Handle additional description format

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-223ff74
+ * @license Angular v21.1.0+sha-1884226
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-223ff74');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-1884226');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
