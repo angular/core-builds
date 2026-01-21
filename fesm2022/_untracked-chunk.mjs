f8d4afb14480bf10b230e930752fdc06cf4bfe73
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 00:19:25 +0000

    94a8f79 fix(docs-infra): don't use private symbol

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ed78fa0
+ * @license Angular v21.2.0-next.0+sha-94a8f79
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ed78fa0');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94a8f79');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
