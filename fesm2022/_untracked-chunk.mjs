238bd78263e35e264facdba06eaff2eea1a754aa
Andrew Scott <atscott@google.com>
2026-01-23 19:53:51 +0000

    e6ea328 docs: update `bootstrapApplication` docs

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-b203ab0
+ * @license Angular v21.1.1+sha-e6ea328
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-b203ab0');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-e6ea328');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
