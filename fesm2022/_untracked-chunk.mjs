b65a4dded864b77b1ab0a4293dcdbc1edde39347
Andrew Scott <atscott01@gmail.com>
2026-01-23 18:48:08 +0000

    b203ab0 build: update bazel dependencies

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-f7eb996
+ * @license Angular v21.1.1+sha-b203ab0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-f7eb996');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-b203ab0');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
--- schematics/bundles/imports-CVmcbVA9.cjs
+++ schematics/bundles/imports-CVmcbVA9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.1+sha-f7eb996
+ * @license Angular v21.1.1+sha-b203ab0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
