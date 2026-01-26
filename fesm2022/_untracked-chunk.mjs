80485aa2d0c2ee5a8d3ad0424f32f57c296b91f6
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:40:36 +0000

    b51bab5 feat(router): Add partial ActivatedRouteSnapshot information to `canMatch` params

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-899df39
+ * @license Angular v21.2.0-next.0+sha-b51bab5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-899df39');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-b51bab5');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
