b1fdd85ebd69a3e8fc828b268192a9e03df24211
Andrew Scott <atscott01@gmail.com>
2026-01-28 20:54:05 +0000

    5a6fefb release: cut the v21.2.0-next.1 release

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-571747d
+ * @license Angular v21.2.0-next.1+sha-5a6fefb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-571747d');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-5a6fefb');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
