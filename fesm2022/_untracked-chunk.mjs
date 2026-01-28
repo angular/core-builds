ec1246c5178b061dcaf76256a3a9d2442d52dd30
Andrew Scott <atscott01@gmail.com>
2026-01-28 00:20:14 +0000

    01bfb83 test(forms): submit behavior while validation is pending

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-4448356
+ * @license Angular v21.2.0-next.0+sha-01bfb83
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-4448356');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-01bfb83');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
