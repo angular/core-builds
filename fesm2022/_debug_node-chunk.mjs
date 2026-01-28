738340631e622cc96cef7f98bbb0f0053b975fec
Andrew Scott <atscott01@gmail.com>
2026-01-28 18:45:37 +0000

    3de87e1 build: update pnpm to v10.28.2

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-affd236
+ * @license Angular v21.1.1+sha-3de87e1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8314,7 +8314,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.1+sha-affd236'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.1+sha-3de87e1'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- types/primitives-di.d.ts
+++ types/primitives-di.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-affd236
+ * @license Angular v21.1.1+sha-3de87e1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
