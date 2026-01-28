d00162f899122aaa227fc0d60f0f633dde4d0d7f
Andrew Scott <atscott01@gmail.com>
2026-01-28 18:02:37 +0000

    bea4cb7 build: update dependency @actions/github to v8

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-4123ebf
+ * @license Angular v21.2.0-next.0+sha-bea4cb7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-4123ebf');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-bea4cb7');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
