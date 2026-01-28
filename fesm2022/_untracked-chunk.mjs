d9f08973a2df6bf75d23ba1c2b587f2f6daa9c90
Andrew Scott <atscott01@gmail.com>
2026-01-28 19:03:31 +0000

    9f99b14 fix(forms): only touch visible, interactive fields on submit

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-3de87e1
+ * @license Angular v21.1.1+sha-9f99b14
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-3de87e1');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-9f99b14');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
