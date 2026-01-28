53139d14be9b37ede7b3d356fdd8d74623cb1200
GitHub <noreply@github.com>
2026-01-28 18:42:57 +0000

    0424710 refactor(devtools): snap to signal graph nodes selected from the properties panel

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-a4f6d40
+ * @license Angular v21.2.0-next.0+sha-0424710
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8314,7 +8314,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-next.0+sha-a4f6d40'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-next.0+sha-0424710'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
