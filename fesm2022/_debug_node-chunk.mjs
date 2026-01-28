f736ca9011f5dba318f9ac8bc4e61305e66db667
Andrew Scott <atscott01@gmail.com>
2026-01-28 00:20:21 +0000

    31e6f0d test(forms): submit behavior while validation is pending

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-6601f06
+ * @license Angular v21.1.1+sha-31e6f0d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8314,7 +8314,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.1+sha-6601f06'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.1+sha-31e6f0d'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- schematics/bundles/inject-migration.cjs
+++ schematics/bundles/inject-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.1+sha-6601f06
+ * @license Angular v21.1.1+sha-31e6f0d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
