869dc6653152cb8a60cd1d647c35d034c8eae57b
Andrew Scott <atscott01@gmail.com>
2026-01-28 19:29:57 +0000

    7bb241f refactor(language-service): introduce withFallback helper in ts_plugin

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e682e53
+ * @license Angular v21.2.0-next.0+sha-7bb241f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8314,7 +8314,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-next.0+sha-e682e53'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-next.0+sha-7bb241f'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
