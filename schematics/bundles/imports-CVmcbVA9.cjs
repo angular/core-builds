41cfeffc47dd971b033d61f0a965a2d17fcb476e
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:35:39 +0000

    8dfbb8b build: update cross-repo angular dependencies

--- schematics/bundles/imports-CVmcbVA9.cjs
+++ schematics/bundles/imports-CVmcbVA9.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.2+sha-0a4c647
+ * @license Angular v21.1.2+sha-8dfbb8b-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
