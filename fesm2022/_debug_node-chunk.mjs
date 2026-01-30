08f653a6d855da2e13f5989981d2e80303f40376
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-30 17:27:17 +0000

    b1bf535 fix(forms): Resolves debounce promise on abort in debounceForDuration

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-26d1215
+ * @license Angular v21.2.0-next.1+sha-b1bf535
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8458,7 +8458,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-next.1+sha-26d1215'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-next.1+sha-b1bf535'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-26d1215
+ * @license Angular v21.2.0-next.1+sha-b1bf535
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-26d1215');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-b1bf535');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
