5f40526e3423bb974d648f9a4ef2eec996532862
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-30 00:11:50 +0000

    b327d72 refactor(language-service): initial use of type-only imports and standard server types

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-74e7681
+ * @license Angular v21.2.0-next.1+sha-b327d72
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-74e7681');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-b327d72');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
