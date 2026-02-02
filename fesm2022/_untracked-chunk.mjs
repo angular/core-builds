feae7bdf00c4fca594d06e1d85a4f50cf5303112
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:22:50 +0000

    575f8a3 docs(animations): fix small typo in animations migrations docs

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-eb28032
+ * @license Angular v21.1.2+sha-575f8a3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-eb28032');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-575f8a3');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
--- schematics/bundles/signal-input-migration.cjs
+++ schematics/bundles/signal-input-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.2+sha-eb28032
+ * @license Angular v21.1.2+sha-575f8a3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
