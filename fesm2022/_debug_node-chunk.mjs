5a0ef168ef2375fe014af917370e23d751f9aab0
Andrew Kushnir <akushnir@google.com>
2026-02-06 01:02:23 +0000

    0729181 test(compiler): remove zone-based testing utilities

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-70668be
+ * @license Angular v21.1.3+sha-0729181
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8327,7 +8327,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.3+sha-70668be'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.3+sha-0729181'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
