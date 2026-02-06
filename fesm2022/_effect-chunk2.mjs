a779c9fcf0f5fde576f145f5c0e7a6353fe8ce92
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 23:15:40 +0000

    63d828a docs: Signal forms custom validator with 'fieldTree' field in validation result error Update docs for fieldTree property

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-460dbbb
+ * @license Angular v21.2.0-next.2+sha-63d828a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-460dbbb');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-63d828a');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
