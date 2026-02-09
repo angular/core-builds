369b7fe6050f683f108315446777a7a6dae4cef5
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 22:55:54 +0000

    51cc914 feat(common): support height in ImageLoaderConfig and built-in loaders

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-e53c8ab
+ * @license Angular v21.2.0-next.2+sha-51cc914
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-e53c8ab');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-51cc914');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
