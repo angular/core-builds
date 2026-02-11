cbe71d6a80fba43aa0e0200564ca7293f0f0b7c1
Matthew Beck <me@mattrb.com>
2026-02-11 23:27:58 +0000

    4ef0f1f build: add workspace rule for antigravity

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-7150df2
+ * @license Angular v21.1.4+sha-4ef0f1f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8327,7 +8327,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.4+sha-7150df2'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.4+sha-4ef0f1f'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-7150df2
+ * @license Angular v21.1.4+sha-4ef0f1f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-7150df2');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-4ef0f1f');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
