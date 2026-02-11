7b33ff9f4f9ca3a85ab9df92583d6b4733e82f4e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-11 17:55:15 +0000

    289ddc9 release: cut the v21.1.4 release

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-2b99eaa
+ * @license Angular v21.1.4+sha-289ddc9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-2b99eaa');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-289ddc9');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
