3877ec53988072cde3543a1b834eed6570865549
Matthew Beck <me@mattrb.com>
2026-02-11 21:45:48 +0000

    8868130 build: add skills in antigravity

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-289ddc9
+ * @license Angular v21.1.4+sha-8868130
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-289ddc9');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-8868130');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
