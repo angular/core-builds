fbdd8175ffccd6c1e4459a52a9c1029a454c6928
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 23:46:45 +0000

    a37b6c5 docs: fix formatting in pipes.md example

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a6a0347
+ * @license Angular v21.2.0-next.3+sha-a37b6c5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a6a0347');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a37b6c5');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
