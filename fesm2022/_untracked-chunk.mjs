dc1fac7679ea1ddac3b2cce4ed1a59a5635d0e7a
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 17:25:30 +0000

    50c5353 build: update pnpm to v10.29.3

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-55b501a
+ * @license Angular v21.1.4+sha-50c5353
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-55b501a');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-50c5353');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
