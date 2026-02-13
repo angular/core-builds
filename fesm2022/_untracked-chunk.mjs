c63bdc97fd11aefea50954c9bc9d0dbf2ed7c72f
Matthew Beck <me@mattrb.com>
2026-02-13 00:58:40 +0000

    55b501a refactor(common): improve image directive typings

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-5cf357b
+ * @license Angular v21.1.4+sha-55b501a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-5cf357b');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-55b501a');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
