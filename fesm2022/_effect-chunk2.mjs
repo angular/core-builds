29d3ac20d40a8d82370d3e5e42b6ba4df51f4854
Matthew Beck (Berry) <me@mattrb.com>
2026-02-17 16:38:43 +0000

    9f4c025 refactor(devtools): improve formatting of injector providers token labels

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-815e1a0
+ * @license Angular v21.2.0-next.3+sha-9f4c025
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-815e1a0');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-9f4c025');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
