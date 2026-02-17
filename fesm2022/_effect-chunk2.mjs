3b716abdd579d8a0ffa6f3377929cec4c3002a2e
Matthew Beck (Berry) <me@mattrb.com>
2026-02-17 19:37:40 +0000

    fc557f0 fix(zone.js): support passthrough of Promise.try API

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-95b3f37
+ * @license Angular v21.2.0-next.3+sha-fc557f0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-95b3f37');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-fc557f0');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
