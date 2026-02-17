5b87cffbfe641e3fa954b84d34bc8722c35084de
Matt Beck <mattrb@google.com>
2026-02-17 16:29:23 +0000

    24d00e9 docs: typo in documentation

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-a3588a0
+ * @license Angular v21.1.4+sha-24d00e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-a3588a0');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-24d00e9');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
