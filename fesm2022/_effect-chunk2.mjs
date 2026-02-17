f51fc38cf6c2e59772ccfad204b3f90d69a1e6fc
Matthew Beck (Berry) <me@mattrb.com>
2026-02-17 16:54:43 +0000

    a8aab64 refactor(core): remove outdated TODO comments referencing TypeScript 2.1

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-9f4c025
+ * @license Angular v21.2.0-next.3+sha-a8aab64
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-9f4c025');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a8aab64');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
