a42bc4cc223f961dbeff76f31a71585f5d877928
Douglas Parker <dgp1130@users.noreply.github.com>
2026-02-18 18:56:48 +0000

    47486f8 release: bump Angular DevTools version to 1.11.0

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-c36385c
+ * @license Angular v21.2.0-next.3+sha-47486f8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-c36385c');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-47486f8');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
