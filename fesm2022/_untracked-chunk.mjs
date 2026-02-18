bc86ddd8e96294cd8ec0ae145aa21de25d5aa60e
Matthew Beck <me@mattrb.com>
2026-02-18 19:44:54 +0000

    b90a0fc docs: update best practice performances nav structure to be more comprehensive

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-a4eb347
+ * @license Angular v21.1.4+sha-b90a0fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-a4eb347');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-b90a0fc');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
--- schematics/bundles/route-lazy-loading.cjs
+++ schematics/bundles/route-lazy-loading.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.4+sha-a4eb347
+ * @license Angular v21.1.4+sha-b90a0fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
