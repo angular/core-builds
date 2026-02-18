c89f5b3954d64f9656c3074ffdcf36287256b2c6
GitHub <noreply@github.com>
2026-02-18 23:19:37 +0000

    dd630ef build: update version target to prep for 21.2.0 release for vscode extension (#67132)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-13fd2b1
+ * @license Angular v21.2.0-rc.0+sha-dd630ef
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8712,7 +8712,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-rc.0+sha-13fd2b1'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-rc.0+sha-dd630ef'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-13fd2b1
+ * @license Angular v21.2.0-rc.0+sha-dd630ef
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-13fd2b1');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-dd630ef');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
