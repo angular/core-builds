8b157f1e84f331cabd02d266162af9f18d4e6888
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 17:16:43 +0000

    a5a1829 docs: improve lazy loading of homepage examples

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-a3731a1
+ * @license Angular v21.3.0-next.0+sha-a5a1829
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8712,7 +8712,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.3.0-next.0+sha-a3731a1'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.3.0-next.0+sha-a5a1829'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-a3731a1
+ * @license Angular v21.3.0-next.0+sha-a5a1829
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-a3731a1');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-a5a1829');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
