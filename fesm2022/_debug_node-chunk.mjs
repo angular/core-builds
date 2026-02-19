2fd2c6d3eb8c8d548c167c938a557b66160b3090
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 20:46:23 +0000

    d501506 ci: add `zone.js` to `minimumReleaseAgeExclude`

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-e8cc910
+ * @license Angular v21.3.0-next.0+sha-d501506
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8712,7 +8712,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.3.0-next.0+sha-e8cc910'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.3.0-next.0+sha-d501506'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
