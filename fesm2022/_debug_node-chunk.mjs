1a7ec4757be710bd0457f479e531ae565232da3d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 16:50:33 +0000

    6f922c1 fix(devtools): fix messaging bus

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-ac8b5ff
+ * @license Angular v21.3.0-next.0+sha-6f922c1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8712,7 +8712,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.3.0-next.0+sha-ac8b5ff'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.3.0-next.0+sha-6f922c1'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
