f1f741bbd302fc5ede6f91b3efa969603cdfe5d1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 00:04:45 +0000

    138526e docs: add section on reactive context and async operations

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-cccaed0
+ * @license Angular v21.1.5+sha-138526e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8327,7 +8327,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.5+sha-cccaed0'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.5+sha-138526e'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- schematics/bundles/application-config-core.cjs
+++ schematics/bundles/application-config-core.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v21.1.5+sha-cccaed0
+ * @license Angular v21.1.5+sha-138526e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
