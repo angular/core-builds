8bf42b6f4dcc17119e30c1cacbde3ec30d9db8d0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 00:05:57 +0000

    5b8a403 build: update rules_browsers digest to ceb5275

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-9f1a78c
+ * @license Angular v21.2.0-rc.0+sha-5b8a403
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-9f1a78c');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-5b8a403');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
