5aa3b66e9ba22e76f85cc15dc7748eabc1e185fd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-23 18:31:06 +0000

    5ac1c02 refactor(core): remove outdated TODO comments

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-9a3f5cb
+ * @license Angular v22.0.0-next.0+sha-5ac1c02
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8712,7 +8712,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.0+sha-9a3f5cb'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.0+sha-5ac1c02'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-9a3f5cb
+ * @license Angular v22.0.0-next.0+sha-5ac1c02
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-9a3f5cb');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-5ac1c02');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
