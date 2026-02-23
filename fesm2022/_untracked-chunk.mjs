329bfad8bf440a3e79a9dc919aa1f81f03cfaafa
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 18:33:03 +0000

    a8dcb28 refactor(core): remove outdated TODO comments

--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-0596a5b
+ * @license Angular v21.1.5+sha-a8dcb28
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-0596a5b');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-a8dcb28');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
