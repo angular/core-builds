ce8ab7378e53b52c73952feda69ae1ce9a28c45b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-23 21:22:41 +0000

    c9ecf9e build: update cross-repo angular dependencies to v21.2.0-rc.1

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-2f213de
+ * @license Angular v21.2.0-rc.0+sha-c9ecf9e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8712,7 +8712,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-rc.0+sha-2f213de'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-rc.0+sha-c9ecf9e'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
