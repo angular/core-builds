36008fd8ba35d6c0fb8216edd06b459bfdcac1b6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 22:29:52 +0000

    706cfec docs: update README to clarify app setup confirmation

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-63cb3f4
+ * @license Angular v21.2.0-rc.0+sha-706cfec
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-63cb3f4');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-706cfec');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
