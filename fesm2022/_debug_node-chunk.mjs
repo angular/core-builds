e2b140714e0b7509f611963c7a617fe16383e409
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 17:30:41 +0000

    ab1c84e docs: consolidate validation rules for `Host` and `X-Forwarded-Host` headers into a single point.

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-eb6bce0
+ * @license Angular v21.2.0-rc.0+sha-ab1c84e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8712,7 +8712,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-rc.0+sha-eb6bce0'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-rc.0+sha-ab1c84e'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-eb6bce0
+ * @license Angular v21.2.0-rc.0+sha-ab1c84e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-eb6bce0');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-ab1c84e');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
