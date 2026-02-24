e7087f65933526d8085e4294f95481bc13423339
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 22:29:07 +0000

    24b39ab docs: update README to clarify app setup confirmation

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-0101922
+ * @license Angular v21.1.5+sha-24b39ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8327,7 +8327,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.5+sha-0101922'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.1.5+sha-24b39ab'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_untracked-chunk.mjs
+++ fesm2022/_untracked-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-0101922
+ * @license Angular v21.1.5+sha-24b39ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-0101922');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-24b39ab');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
