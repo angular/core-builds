e9a5ffeb55e1c27adf552896a71762ba388b0a97
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 19:29:13 +0000

    787cd87 refactor(vscode-extension): Add support for exhaustive type check in the syntax

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-1a151f2
+ * @license Angular v21.2.0-rc.0+sha-787cd87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8712,7 +8712,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-rc.0+sha-1a151f2'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.0-rc.0+sha-787cd87'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-1a151f2
+ * @license Angular v21.2.0-rc.0+sha-787cd87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-1a151f2');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-787cd87');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
