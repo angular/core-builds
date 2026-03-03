8dd4a2886876a7d1170498cee22a0d0ccdf11e29
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-03 22:14:49 +0000

    778b748 refactor(router): Permit deferring commit of traversal navigations

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-2a11c69
+ * @license Angular v22.0.0-next.0+sha-778b748
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8730,7 +8730,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.0+sha-2a11c69'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.0+sha-778b748'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
