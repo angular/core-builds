8aececd15a482452f9e4a515b86c59a4f6f431a0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-03 22:32:19 +0000

    796b9a7 build: update cross-repo angular dependencies

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-84adb2f
+ * @license Angular v21.2.0+sha-796b9a7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-84adb2f');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-796b9a7');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
