cdb3dbf081cfd7ba2873cac26edbc53ae22cfd88
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 18:31:28 +0000

    4422ade build: update cross-repo angular dependencies to c4344a4

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-fda08b7
+ * @license Angular v22.0.0-next.0+sha-4422ade
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8733,7 +8733,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.0+sha-fda08b7'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.0+sha-4422ade'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
