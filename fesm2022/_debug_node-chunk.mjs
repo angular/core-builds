ce1f635f8db2abf1ba26ce8735a87727ccd2eb26
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 18:16:11 +0000

    9208421 docs: add customization options for browser URL in routing guide

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-5338b59
+ * @license Angular v22.0.0-next.0+sha-9208421
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8730,7 +8730,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.0+sha-5338b59'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.0+sha-9208421'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
