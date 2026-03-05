073334ac5cf9206731291d41d8a21593cfae7ae7
Andrew Scott <atscott01@gmail.com>
2026-03-05 21:35:57 +0000

    8217ef9 refactor(compiler-cli): abstract type check block metadata to be AST-free

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-3693687
+ * @license Angular v22.0.0-next.1+sha-8217ef9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-3693687');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-8217ef9');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
