012779ca60af4814aefa88ae45705e241760209c
Andrew Scott <atscott01@gmail.com>
2026-03-05 18:02:55 +0000

    670d166 feat(forms): add 'blur' option to debounce rule

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-8ca0237
+ * @license Angular v21.2.1+sha-670d166
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8733,7 +8733,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.1+sha-8ca0237'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.1+sha-670d166'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-8ca0237
+ * @license Angular v21.2.1+sha-670d166
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-8ca0237');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-670d166');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
