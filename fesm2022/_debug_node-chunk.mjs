abe2290aaa74483f39e21d8a3ae5b33156390706
GitHub <noreply@github.com>
2026-03-06 20:19:04 +0000

    786ea44 build: force external sourcemaps for esbuild targets

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-759ee22
+ * @license Angular v22.0.0-next.1+sha-786ea44
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8777,7 +8777,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.1+sha-759ee22'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.1+sha-786ea44'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
