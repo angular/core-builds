91ba1434bdf54f396b3733af89c097e5656b1dd8
Andrew Scott <atscott01@gmail.com>
2026-03-06 23:40:45 +0000

    276c12a build: update dependency node to v22.22.1

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-104ac11
+ * @license Angular v21.2.1+sha-276c12a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-104ac11');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-276c12a');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
