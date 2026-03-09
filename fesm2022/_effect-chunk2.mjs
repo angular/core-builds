d53f6a13969aa4ea5dd1f64982381c6c07981893
Andrew Scott <atscott01@gmail.com>
2026-03-09 23:34:34 +0000

    c499c57 ci: fix exclusion of manual_api_docs

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-0588133
+ * @license Angular v21.2.2+sha-c499c57
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-0588133');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-c499c57');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
