3f0b646efc1316f4424d9709283f5f99b729aadf
Andrew Scott <atscott@google.com>
2026-03-09 17:27:49 +0000

    79ac570 refactor(devtools): fix browser-specific styles infra

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-276c12a
+ * @license Angular v21.2.1+sha-79ac570
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-276c12a');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-79ac570');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
