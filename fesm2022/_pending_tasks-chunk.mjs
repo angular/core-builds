3a80c68791e285de05fad625b8343260a4594665
Andrew Scott <atscott01@gmail.com>
2026-03-11 21:25:29 +0000

    ec76985 build: update bazel dependencies

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.2+sha-88235a0
+ * @license Angular v22.0.0-next.2+sha-ec76985
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-88235a0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-ec76985');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
