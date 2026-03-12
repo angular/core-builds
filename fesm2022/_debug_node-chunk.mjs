83904ea026477c15762a05754364d64f2a7f4c73
Matthew Beck <me@mattrb.com>
2026-03-12 21:05:27 +0000

    5d755be refactor: ensure modules with 'declare global' are correctly handled by Rollup

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-8017617
+ * @license Angular v21.2.4+sha-5d755be
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8685,7 +8685,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.4+sha-8017617'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.4+sha-5d755be'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
