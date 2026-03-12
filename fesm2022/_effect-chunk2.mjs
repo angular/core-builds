205cddd718ad633c0832ed77918a111fa50225d9
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 19:05:05 +0000

    8017617 release: cut the v21.2.4 release

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.3+sha-ed2d324
+ * @license Angular v21.2.4+sha-8017617
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.3+sha-ed2d324');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-8017617');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
