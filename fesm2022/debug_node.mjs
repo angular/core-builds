bc9af873743d19d92e3993299b4479072863d59a
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 19:29:18 +0000

    cef3164 release: cut the v20.3.18 release

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.17+sha-02fbf08
+ * @license Angular v20.3.18+sha-cef3164
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14727,7 +14727,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.17+sha-02fbf08']
+        ? ['ng-version', '20.3.18+sha-cef3164']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
