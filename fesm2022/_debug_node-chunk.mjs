a4bc83bf676d30e785eb0683c63e8705ed57a030
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 19:32:34 +0000

    432a3de docs: release notes for the v20.3.18 release

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-4cbc1a1
+ * @license Angular v22.0.0-next.3+sha-432a3de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8685,7 +8685,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.3+sha-4cbc1a1'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.3+sha-432a3de'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
