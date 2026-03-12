d6a37ee7a3479ee6b048ef1cb1813d63e7373a9b
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 22:17:58 +0000

    a929494 docs: update cross-repo adev docs

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-bb628d8
+ * @license Angular v22.0.0-next.3+sha-a929494
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8685,7 +8685,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.3+sha-bb628d8'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.3+sha-a929494'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
