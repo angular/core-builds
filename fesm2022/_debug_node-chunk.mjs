50d0317bb345da73c86e0f3dcdc4911ea4aaa320
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 18:58:49 +0000

    334ae10 fix(compiler): ensure generated code compiles

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-6f41efc
+ * @license Angular v21.2.4+sha-334ae10
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8689,7 +8689,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.4+sha-6f41efc'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.4+sha-334ae10'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
