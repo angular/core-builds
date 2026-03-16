ef2d835a8a1df2ad66978282006b494191d1c748
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 17:00:48 +0000

    4d78651 build: update cross-repo angular dependencies

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-26c43d1
+ * @license Angular v21.2.4+sha-4d78651
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-26c43d1');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-4d78651');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
