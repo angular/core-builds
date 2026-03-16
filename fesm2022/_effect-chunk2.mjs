8b92c19a6d21050bdf915694b6754ce4f4022abb
Matthew Beck <me@mattrb.com>
2026-03-16 16:54:54 +0000

    26c43d1 fix(compiler-cli): escape template literal in TCB

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-1890c30
+ * @license Angular v21.2.4+sha-26c43d1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-1890c30');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-26c43d1');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
