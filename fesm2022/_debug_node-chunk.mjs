4a11202c64c05159247cfdb9fff957ad2e0e6551
Matthew Beck <me@mattrb.com>
2026-03-18 00:07:45 +0000

    076d41c fix(migrations): prevent trailing comma syntax errors after removing NgStyle

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-a8f80c1
+ * @license Angular v21.2.4+sha-076d41c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8689,7 +8689,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.4+sha-a8f80c1'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.4+sha-076d41c'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-a8f80c1
+ * @license Angular v21.2.4+sha-076d41c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-a8f80c1');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-076d41c');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
