172e36291144436797d7e713ad8b1d22830f7f60
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-20 22:27:56 +0000

    d048186 build: update cross-repo angular dependencies

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-880a57d
+ * @license Angular v21.2.5+sha-d048186
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-880a57d');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-d048186');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
