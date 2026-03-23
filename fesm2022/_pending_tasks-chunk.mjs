08fe8a35abf2a6f3f33d50c35798bba52043a154
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 16:55:28 +0000

    293ac66 ci: support publishing dev-skills to snapshot repository

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-d2054c7
+ * @license Angular v22.0.0-next.4+sha-293ac66
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-d2054c7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-293ac66');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
