43e17d4a8b3cb0eb9a219bb203977f156b8db807
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 17:40:16 +0000

    8818095 docs(docs-infra): fix aria-label parsing breaking headings

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-a821e06
+ * @license Angular v21.2.5+sha-8818095
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-a821e06');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-8818095');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
