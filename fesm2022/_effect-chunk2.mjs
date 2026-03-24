68ec4261f2fb9b2becc8f8907168412283f31eac
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 22:25:32 +0000

    a0ca5a5 docs(docs-infra): redefine /guide/pipes redirect

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-e731bbd
+ * @license Angular v21.2.5+sha-a0ca5a5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-e731bbd');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-a0ca5a5');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
