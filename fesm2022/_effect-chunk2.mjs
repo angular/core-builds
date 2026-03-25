8c93dbe2ccc1af36fe9e3d3df56b24478f3eb842
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 20:14:04 +0000

    a6c49e0 refactor(language-service): Export template target from API

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-2c7d3b5
+ * @license Angular v21.2.5+sha-a6c49e0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-2c7d3b5');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-a6c49e0');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
