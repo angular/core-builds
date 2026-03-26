d6e1472c8862cee81b66e707154a33f799a72fd0
Andrew Scott <atscott01@gmail.com>
2026-03-26 21:41:18 +0000

    7234432 refactor(language-service): Update getTcbNodesOfTemplateAtPosition to be usable without compiler (#67898)

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-d268df6
+ * @license Angular v22.0.0-next.5+sha-7234432
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-d268df6');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-7234432');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
