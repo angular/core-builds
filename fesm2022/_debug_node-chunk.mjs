8904ed963b6a9c0e2eaacf907af7b9a148b296be
Andrew Scott <atscott01@gmail.com>
2026-03-26 21:41:08 +0000

    b453c0c refactor(language-service): Update getTcbNodesOfTemplateAtPosition to be usable without compiler (#67898)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-807cabd
+ * @license Angular v21.2.6+sha-b453c0c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8691,7 +8691,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.6+sha-807cabd'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.6+sha-b453c0c'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
