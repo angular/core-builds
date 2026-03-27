9b536660e4389dbe79782918f43868a79fe1e668
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 13:18:40 +0000

    56c872d docs: update cross-repo adev docs

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-c8902de
+ * @license Angular v21.2.6+sha-56c872d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-c8902de');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-56c872d');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
