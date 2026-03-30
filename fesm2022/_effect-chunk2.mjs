e1181088199bfb674917deb35fbdfe0993774e78
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 09:17:43 +0000

    efbba94 docs: align list formatting and improve emphasis

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-94b1b53
+ * @license Angular v21.2.6+sha-efbba94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-94b1b53');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-efbba94');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
