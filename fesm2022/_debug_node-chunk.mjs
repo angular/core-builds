225d89b9e3a496923353ed2d670886bfb6238c8d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 10:49:46 +0000

    c950299 build: update pnpm to v10.33.0

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-0f960a5
+ * @license Angular v22.0.0-next.5+sha-c950299
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8890,7 +8890,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.5+sha-0f960a5'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.5+sha-c950299'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
