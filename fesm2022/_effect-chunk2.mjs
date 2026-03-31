009fae66753b5a7143e4ebcb9a20b7927167b39f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 11:58:27 +0000

    895c576 refactor(forms): use strict equality for pending status getter

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-5fd4f0f
+ * @license Angular v21.2.6+sha-895c576
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-5fd4f0f');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-895c576');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
