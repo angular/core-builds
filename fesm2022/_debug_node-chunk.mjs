ec6704c9c4155de868f1ba91e8639b42e3abd0b7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 10:07:31 +0000

    429513b build: update cross-repo angular dependencies

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-f22d9a7
+ * @license Angular v21.2.6+sha-429513b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8691,7 +8691,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.6+sha-f22d9a7'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.6+sha-429513b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
