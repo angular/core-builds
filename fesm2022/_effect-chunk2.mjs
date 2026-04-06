d2ec13ec4214a59431fc890ed3455f28cabd7625
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:01:43 +0000

    39827a0 fix(docs-infra): align search result icon with text on mobile

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-04f5841-with-local-changes
+ * @license Angular v21.2.7+sha-39827a0-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-04f5841-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-39827a0-with-local-changes');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
