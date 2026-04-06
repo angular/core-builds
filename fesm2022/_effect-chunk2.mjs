3d0cfec76a8d11886fa6f4316a0f99eccfd53733
Andrew Scott <atscott01@gmail.com>
2026-04-06 19:37:54 +0000

    d715c36 refactor(compiler-cli): fix failing tests

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-c91bd07-with-local-changes
+ * @license Angular v21.2.7+sha-d715c36-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-c91bd07-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-d715c36-with-local-changes');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
