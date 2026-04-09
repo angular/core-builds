48f7ba9e9a29d14282de1830f48fb4f42baf7778
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 15:32:04 +0000

    47fcbc4 feat(compiler): allow safe navigation to correctly narrow down nullables

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-b9265cc
+ * @license Angular v22.0.0-next.7+sha-47fcbc4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-b9265cc');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-47fcbc4');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
