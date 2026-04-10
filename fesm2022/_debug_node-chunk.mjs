f29d3f9a9c31954c0cfa5fd6bd6f9f09737fa7c4
kirjs <kirts@google.com>
2026-04-10 08:03:03 +0000

    dc95814 docs: add documentation for NG1002

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-05d9b97
+ * @license Angular v21.2.8+sha-dc95814
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8747,7 +8747,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.8+sha-05d9b97'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.8+sha-dc95814'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
