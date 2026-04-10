2c9ebcfec5b42e79e71e66e4fdf8ffd54cc7d715
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 17:20:17 +0000

    a268547 build: update rules_angular setup in MODULE.bazel

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-843f425
+ * @license Angular v22.0.0-next.7+sha-a268547
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-843f425');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-a268547');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
