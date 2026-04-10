36ecaed88e654573d5df84a5296200aa584bb06a
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 08:03:29 +0000

    0304228 docs: add documentation for NG1002

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-836094c
+ * @license Angular v22.0.0-next.7+sha-0304228
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-836094c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-0304228');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
