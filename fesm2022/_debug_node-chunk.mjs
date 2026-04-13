933811b0ad063d382dd96b0214410b5a778a460a
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 11:02:43 +0000

    70368ea refactor(http): Make `BrowserXhr`/`XhrFactory` tree-shakable

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-f25c7ce
+ * @license Angular v22.0.0-next.7+sha-70368ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1997,7 +1997,7 @@ class TransferState {
         }
       }
     }
-    return JSON.stringify(this.store).replace(/</g, '\\u003C');
+    return JSON.stringify(this.store).replace(/</g, '\\u003C').replace(/\//g, '\\u002F');
   }
 }
 function retrieveTransferredState(doc, appId) {
@@ -8958,7 +8958,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.7+sha-f25c7ce'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.7+sha-70368ea'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
