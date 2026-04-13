f7b4a8d5acc5147037b8e1a2bd916c9435a36dda
kirjs <kirts@google.com>
2026-04-13 08:14:51 +0000

    245bcdd docs(docs-infra): fix card container overflow on mobile viewports

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-e2e7211
+ * @license Angular v21.2.8+sha-245bcdd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-e2e7211');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-245bcdd');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
