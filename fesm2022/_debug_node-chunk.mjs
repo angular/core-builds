ea92d00c12f599cc1ccbf518be1a2eca3fdfe43d
kirjs <kirts@google.com>
2026-04-13 08:06:20 +0000

    e2e7211 docs(docs-infra): consolidate tab menu margins for phone breakpoint

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-b351d49
+ * @license Angular v21.2.8+sha-e2e7211
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8747,7 +8747,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.8+sha-b351d49'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.8+sha-e2e7211'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-b351d49
+ * @license Angular v21.2.8+sha-e2e7211
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-b351d49');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-e2e7211');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
