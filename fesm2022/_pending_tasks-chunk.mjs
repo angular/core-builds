005c6182ae1d17d4197a58d8a2efcf009ceda2c8
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 10:29:55 +0000

    f25c7ce docs(forms): update signal forms migration guide

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-5f74e7e
+ * @license Angular v22.0.0-next.7+sha-f25c7ce
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-5f74e7e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-f25c7ce');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
