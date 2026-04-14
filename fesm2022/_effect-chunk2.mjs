976ba45010d9346b0df1a11044c9eb8b6e40a659
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-14 09:25:50 +0000

    bb8cdd9 build: lock file maintenance

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-17ffa19
+ * @license Angular v21.2.8+sha-bb8cdd9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-17ffa19');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-bb8cdd9');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
