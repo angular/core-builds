c1e24ae8c4ab0ec18f20d87f2649346f4d0e18a4
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-14 09:40:07 +0000

    c90b6b3 fix(router): normalize multiple leading slashes in URL parser

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-2f5ab54
+ * @license Angular v22.0.0-next.7+sha-c90b6b3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8935,7 +8935,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.7+sha-2f5ab54'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.7+sha-c90b6b3'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -11731,7 +11731,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0-next.7+sha-2f5ab54';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0-next.7+sha-c90b6b3';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
