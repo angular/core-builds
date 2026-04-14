d4aa2ede3ffec566f2600d2fb1637d5c0e475add
kirjs <kirts@google.com>
2026-04-14 15:35:16 +0000

    c3d69ae fix(docs-infra): prevent inline code wrapping in CLI reference table

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-684e9fd
+ * @license Angular v21.2.8+sha-c3d69ae
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-684e9fd');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-c3d69ae');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
