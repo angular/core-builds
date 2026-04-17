7b8e3fd16302941f9189ba218cce3f3b52b6c6bb
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-17 21:30:52 +0000

    6d10b8b docs: add inject to structural directive imports

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-c738d45
+ * @license Angular v21.2.9+sha-6d10b8b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8747,7 +8747,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.9+sha-c738d45'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.9+sha-6d10b8b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-c738d45
+ * @license Angular v21.2.9+sha-6d10b8b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-c738d45');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-6d10b8b');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
