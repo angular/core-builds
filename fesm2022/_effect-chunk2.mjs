2e9aeb454d28ae6644948e7db38fcab6399a574b
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:46:17 +0000

    ce883d9 build: update peer dependencies and bump version

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-be74909
+ * @license Angular v21.2.9+sha-ce883d9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-be74909');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-ce883d9');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
