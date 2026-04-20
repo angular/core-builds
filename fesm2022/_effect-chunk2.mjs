2be89f283e3e58e6f8ee412cd333922625b7220e
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 23:53:40 +0000

    580212c fix(router): restore internal URL on popstate when `browserUrl` is used

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-a9ddc5d
+ * @license Angular v21.2.9+sha-580212c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-a9ddc5d');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-580212c');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
