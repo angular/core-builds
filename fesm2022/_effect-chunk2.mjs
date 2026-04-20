cc37707cf1fb28daf5816fb5f97b522639664a59
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:28:57 +0000

    0d08d9c docs(docs-infra): guard sandbox reset before initialization in playground

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-c9215b3
+ * @license Angular v21.2.9+sha-0d08d9c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-c9215b3');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-0d08d9c');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-c9215b3
+ * @license Angular v21.2.9+sha-0d08d9c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
