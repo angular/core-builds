2d67f2ff20d0a456c1c3539541899dd5902c49cb
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:38:20 +0000

    a9ddc5d docs: warn against storing secrets in environment files

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-0d08d9c
+ * @license Angular v21.2.9+sha-a9ddc5d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8761,7 +8761,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.9+sha-0d08d9c'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.9+sha-a9ddc5d'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-0d08d9c
+ * @license Angular v21.2.9+sha-a9ddc5d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-0d08d9c');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-a9ddc5d');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
