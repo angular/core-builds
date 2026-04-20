62af3ecd3aea0c049b91654cb3b761280d84b437
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:36:50 +0000

    a718e18 docs: improve clarity in dynamic forms guide

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.8+sha-a0d4563
+ * @license Angular v22.0.0-next.8+sha-a718e18
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9000,7 +9000,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.8+sha-a0d4563'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.8+sha-a718e18'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -11789,7 +11789,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0-next.8+sha-a0d4563';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0-next.8+sha-a718e18';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
