bdb0eb4c9c227b703dda7cf046dd2301c844fbd6
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-21 17:32:24 +0000

    4dc7bf5 build: lock file maintenance

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-751e4af
+ * @license Angular v21.2.9+sha-4dc7bf5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-751e4af');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-4dc7bf5');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
