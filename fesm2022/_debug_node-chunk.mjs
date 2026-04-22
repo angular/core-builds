3844193bc7e3368286cc54f806a410b6e5f86f0c
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 21:48:01 +0000

    4fd1a08 build: pin firebase-tools version and disable credential persistence in preview deploy workflow

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-750af5b
+ * @license Angular v21.2.9+sha-4fd1a08
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8761,7 +8761,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.9+sha-750af5b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.9+sha-4fd1a08'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-750af5b
+ * @license Angular v21.2.9+sha-4fd1a08
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-750af5b');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-4fd1a08');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
