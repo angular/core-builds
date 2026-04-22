549d06ce240899cddd9349aff08ffd76d027b8f0
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 23:58:50 +0000

    c64ee96 release: cut the v22.0.0-next.9 release

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.8+sha-602fe93
+ * @license Angular v22.0.0-next.9+sha-c64ee96
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-602fe93');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-c64ee96');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
