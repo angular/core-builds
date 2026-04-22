51df90a85188d885d87d1c44cf7671556197b4b1
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 21:49:05 +0000

    7f3f3d7 ci: remove remainings of saucelabs tests

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.8+sha-2c141c0
+ * @license Angular v22.0.0-next.8+sha-7f3f3d7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9010,7 +9010,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.8+sha-2c141c0'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-next.8+sha-7f3f3d7'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -11799,7 +11799,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0-next.8+sha-2c141c0';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0-next.8+sha-7f3f3d7';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
