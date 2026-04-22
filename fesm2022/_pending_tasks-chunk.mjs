2646db75657be9567e281ef93d00d30c66a866ea
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 18:21:40 +0000

    2c141c0 refactor(compiler-cli): simplify Angular decorator stripping

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.8+sha-b395173
+ * @license Angular v22.0.0-next.8+sha-2c141c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-b395173');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-2c141c0');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
