ed2ea5fc2fc8d59336421433df001c362e372389
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 17:16:47 +0000

    8ce8b93 build: update pnpm to v10.33.2

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-f3308dc
+ * @license Angular v21.2.10+sha-8ce8b93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8761,7 +8761,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.10+sha-f3308dc'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.10+sha-8ce8b93'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-f3308dc
+ * @license Angular v21.2.10+sha-8ce8b93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-f3308dc');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-8ce8b93');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
