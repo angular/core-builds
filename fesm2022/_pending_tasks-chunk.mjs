e095314840b4a8c732d0c254bc65482fbafc2035
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 17:17:57 +0000

    adbd1ab docs: update service documentation to include `@Service` decorator details

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-9203aca
+ * @license Angular v22.0.0-next.9+sha-adbd1ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-9203aca');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-adbd1ab');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
