464b133cbf0f2bb8e2c576b2b04c05eb50e32582
Alon Mishne <amishne@users.noreply.github.com>
2026-04-29 21:07:17 +0000

    6c6db5b docs(docs-infra): open update guide external links in new tabs

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-631e303
+ * @license Angular v21.2.10+sha-6c6db5b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-631e303');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-6c6db5b');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
