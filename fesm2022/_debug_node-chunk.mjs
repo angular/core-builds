fa4ebc961d31e34917f65d3bf66bcd3b96f21c9d
Alon Mishne <amishne@users.noreply.github.com>
2026-04-29 20:49:52 +0000

    631e303 docs: improve clarity in dependency injection guide

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-42d57c3
+ * @license Angular v21.2.10+sha-631e303
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8781,7 +8781,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.10+sha-42d57c3'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.10+sha-631e303'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-42d57c3
+ * @license Angular v21.2.10+sha-631e303
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-42d57c3');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-631e303');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
