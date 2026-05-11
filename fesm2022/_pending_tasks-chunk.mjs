bdb92c14d675dda0b8c76cbb3362a1a8c82d80a4
Matthew Beck <me@mattrb.com>
2026-05-11 19:16:40 +0000

    ff21eb9 docs: add new signal forms field metadata guide

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-5a6e946
+ * @license Angular v22.0.0-next.12+sha-ff21eb9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-5a6e946');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-ff21eb9');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
