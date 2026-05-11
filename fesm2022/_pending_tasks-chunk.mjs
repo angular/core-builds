9dca883a4cf15b3324d188f04b43fe4438663cda
Matthew Beck <me@mattrb.com>
2026-05-11 18:54:44 +0000

    5a6e946 docs: remove experimental warnings from resource/signal forms documentation

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-119a19e
+ * @license Angular v22.0.0-next.12+sha-5a6e946
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-119a19e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-5a6e946');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
