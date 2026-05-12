1014f3892a3e20176ddc5c2bf615da3c26743cb0
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 23:49:18 +0000

    f05d675 build: update cross-repo angular dependencies (main)

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-d42fdf1
+ * @license Angular v22.1.0-next.0+sha-f05d675
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-d42fdf1');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-f05d675');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
