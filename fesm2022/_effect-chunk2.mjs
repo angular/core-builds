5b69c7f74eec22d460a600b13cd679fe3e21251f
Matthew Beck <me@mattrb.com>
2026-05-12 17:55:25 +0000

    7ab78d5 ci: mark devversion as unavailable

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-49a133a
+ * @license Angular v21.2.12+sha-7ab78d5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-49a133a');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-7ab78d5');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
