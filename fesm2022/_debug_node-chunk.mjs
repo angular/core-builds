52939f8a8e3b205b61e4301ed124d7ad0694b42d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 21:31:14 +0000

    525e160 release: bump VSCode extension version to 21.2.4

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-4a41831
+ * @license Angular v21.2.14+sha-525e160
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8778,7 +8778,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.14+sha-4a41831'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.14+sha-525e160'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-4a41831
+ * @license Angular v21.2.14+sha-525e160
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-4a41831');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-525e160');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
