b93d9a015fe0e3981cd6a2ee2d8e0e473234e9a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 22:58:51 +0000

    db20819 release: cut the v20.3.22 release

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.21+sha-af04936
+ * @license Angular v20.3.22+sha-db20819
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-af04936');
+const VERSION = /* @__PURE__ */ new Version('20.3.22+sha-db20819');
 
 /**
  * Base URL for the error details page.
--- schematics/bundles/migrate_ts_type_references-TafxKegc.cjs
+++ schematics/bundles/migrate_ts_type_references-TafxKegc.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.21+sha-af04936
+ * @license Angular v20.3.22+sha-db20819
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
