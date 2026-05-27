0384e0d30ef9e2a99546c3ffa0a0133d0768c244
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 21:26:22 +0000

    618c850 fix(http): exclude withCredentials requests from transfer cache

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-e6cfaf5
+ * @license Angular v22.0.0-rc.1+sha-618c850
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9041,7 +9041,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-rc.1+sha-e6cfaf5'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0-rc.1+sha-618c850'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12159,7 +12159,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0-rc.1+sha-e6cfaf5';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0-rc.1+sha-618c850';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
