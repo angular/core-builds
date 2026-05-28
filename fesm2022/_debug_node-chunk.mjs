12d53e38b68a90252ec1de3b02d765d47b70ebe3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:13:41 +0000

    6b8202e refactor(docs-infra): extract magic 27 in navigation-list tooltip threshold

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-eb1cbbf
+ * @license Angular v21.2.14+sha-6b8202e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8808,7 +8808,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.14+sha-eb1cbbf'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.14+sha-6b8202e'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-eb1cbbf
+ * @license Angular v21.2.14+sha-6b8202e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-eb1cbbf');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-6b8202e');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
