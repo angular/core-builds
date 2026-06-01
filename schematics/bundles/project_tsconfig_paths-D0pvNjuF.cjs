bbe2bb81a281b65fb19c01317eee3331c3b387a5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 18:48:09 +0000

    6ca433e fix(platform-server): throw on suspicious URLs and restrict protocol-relative URLs

--- schematics/bundles/project_tsconfig_paths-D0pvNjuF.cjs
+++ schematics/bundles/project_tsconfig_paths-D0pvNjuF.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.23+sha-8680b51
+ * @license Angular v20.3.23+sha-6ca433e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32921,7 +32921,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.23+sha-8680b51');
+const VERSION = new Version('20.3.23+sha-6ca433e');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
