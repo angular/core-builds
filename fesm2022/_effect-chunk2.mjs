5569b5aa8a3e8c2db692dc60787ff4c1ccb358a0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 16:41:55 +0000

    b74fb76 docs(docs-infra): simplify code block styles

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.15+sha-ae1c8a1
+ * @license Angular v21.2.15+sha-b74fb76
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-ae1c8a1');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-b74fb76');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
