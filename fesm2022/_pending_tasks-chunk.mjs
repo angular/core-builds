a2a592d6f06904156e64e9ce385ee5df4fb23af9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 09:30:07 +0000

    a76bc3c docs: add testing guides to Angular Aria

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-3cc9e2b
+ * @license Angular v22.1.0-next.0+sha-a76bc3c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-3cc9e2b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a76bc3c');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
