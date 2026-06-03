3e0f7880749e9eb6858d862a0c3925c76e7b745c
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:16:13 +0000

    3d9ca2f fix(compiler-cli): bind switch exhaustive check expressions

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-32b348a
+ * @license Angular v22.0.0+sha-3d9ca2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-32b348a');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-3d9ca2f');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
