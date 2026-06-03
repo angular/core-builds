3a69c6c46e945a95d72470af7b66e946f39f3251
Andrew Scott <atscott01@gmail.com>
2026-06-03 18:10:38 +0000

    566ad05 fix(common): skip transfer cache for uncacheable HTTP traffic

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-768a349
+ * @license Angular v20.3.24+sha-566ad05
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14888,7 +14888,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.24+sha-768a349']
+        ? ['ng-version', '20.3.24+sha-566ad05']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-D0pvNjuF.cjs
+++ schematics/bundles/project_tsconfig_paths-D0pvNjuF.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.24+sha-768a349
+ * @license Angular v20.3.24+sha-566ad05
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32921,7 +32921,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.24+sha-768a349');
+const VERSION = new Version('20.3.24+sha-566ad05');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
