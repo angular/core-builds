cd9bede3eb11700dfb05aef9982733bcf4e1d9e8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 16:03:01 +0000

    4e42420 docs: update landing page for v22

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-24416ce
+ * @license Angular v22.0.0+sha-4e42420
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9041,7 +9041,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0+sha-24416ce'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0+sha-4e42420'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12159,7 +12159,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0+sha-24416ce';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0+sha-4e42420';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
