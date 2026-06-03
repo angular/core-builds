7a4426ca9118dc9d361ae6336f10a66137650316
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 17:08:30 +0000

    32b348a docs: link to v22 blog post in changelog

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-da07b3c
+ * @license Angular v22.0.0+sha-32b348a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-da07b3c');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-32b348a');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
