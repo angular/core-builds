dbc5349a9f8c12b85e337ec496b3b84a669f98e8
Andrew Scott <atscott01@gmail.com>
2026-06-04 21:54:02 +0000

    8f06057 build: lock file maintenance

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-55b7b5a
+ * @license Angular v22.0.0+sha-8f06057
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-55b7b5a');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-8f06057');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
