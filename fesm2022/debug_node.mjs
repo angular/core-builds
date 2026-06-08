d20587a075ca6be40cff3615539b767dd659fc95
Andrew Scott <atscott01@gmail.com>
2026-06-08 21:59:22 +0000

    9f443bc fix(common): Limits date format string length

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-fa940e1
+ * @license Angular v20.3.24+sha-9f443bc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14888,7 +14888,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.24+sha-fa940e1']
+        ? ['ng-version', '20.3.24+sha-9f443bc']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- schematics/bundles/project_tsconfig_paths-Cu6YNBK3.cjs
+++ schematics/bundles/project_tsconfig_paths-Cu6YNBK3.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.24+sha-fa940e1
+ * @license Angular v20.3.24+sha-9f443bc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32922,7 +32922,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.24+sha-fa940e1');
+const VERSION = new Version('20.3.24+sha-9f443bc');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
