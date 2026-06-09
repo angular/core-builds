64c0d0a787acbec3ccafc9fbf5365d5ac005c685
GitHub <noreply@github.com>
2026-06-09 17:11:01 +0000

    d55c94a refactor(platform-server): deprecate ServerXhr (#69256)

--- schematics/bundles/project_tsconfig_paths-Cu6YNBK3.cjs
+++ schematics/bundles/project_tsconfig_paths-Cu6YNBK3.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v20.3.24+sha-06be298
+ * @license Angular v20.3.24+sha-d55c94a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -32922,7 +32922,7 @@ function isAttrNode(ast) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.3.24+sha-06be298');
+const VERSION = new Version('20.3.24+sha-d55c94a');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
