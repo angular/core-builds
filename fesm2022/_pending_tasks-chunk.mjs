bd969f3d0d6ea220c7b56dca26a6841cf549d131
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:48:50 +0000

    3dd35c2 fix(common): escape CSS string-terminating characters in escapeCssUrl

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-c121407
+ * @license Angular v22.1.0-next.0+sha-3dd35c2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-c121407');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-3dd35c2');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
