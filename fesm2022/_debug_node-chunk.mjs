a7659c93e6ce645ead7f2417d59e4fcddd0a7ffd
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:21:10 +0000

    8b32559 release: cut the v22.0.1 release

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-2dd65d2
+ * @license Angular v22.0.1+sha-8b32559
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9069,7 +9069,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.0+sha-2dd65d2'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-8b32559'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12188,7 +12188,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.0+sha-2dd65d2';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-8b32559';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
