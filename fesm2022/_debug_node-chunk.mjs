a4cefbf85f41f9967ea4e73e9b0b7fce97bc684b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-11 17:09:32 +0000

    6867f77 fix(http): distinguish repeated transfer cache params

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-21bdff5
+ * @license Angular v22.0.1+sha-6867f77
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9069,7 +9069,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-21bdff5'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-6867f77'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12188,7 +12188,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-21bdff5';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-6867f77';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
