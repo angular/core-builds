2936e6d6b6c93096818d22ce7259a3cc613d53d4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-11 16:33:55 +0000

    cbcf31b build: update cross-repo angular dependencies

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-4d0091b
+ * @license Angular v22.0.1+sha-cbcf31b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9069,7 +9069,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-4d0091b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-cbcf31b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12188,7 +12188,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-4d0091b';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-cbcf31b';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
