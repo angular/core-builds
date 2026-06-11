0a8fd6ab648545ef1a9875b693705ea57a359741
GitHub <noreply@github.com>
2026-06-11 18:32:55 +0000

    3013982 release: bump VSCode extension version to 22.0.1 (#69318)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-3a48abc
+ * @license Angular v22.0.1+sha-3013982
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9095,7 +9095,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-3a48abc'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-3013982'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12214,7 +12214,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-3a48abc';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-3013982';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
