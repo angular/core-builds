fd7f8586c1808d01c8c4f62e761e84c0d6b2124e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 16:15:35 +0000

    b7aa8dc fix(docs-infra): add explicit font styles to docs-primary-btn

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-d45e318
+ * @license Angular v22.0.1+sha-b7aa8dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9095,7 +9095,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-d45e318'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-b7aa8dc'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12213,7 +12213,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-d45e318';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-b7aa8dc';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
