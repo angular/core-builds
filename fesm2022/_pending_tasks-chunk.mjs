8c29ae965e10f0bb8338a18947a088ce49785c20
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-17 17:38:45 +0000

    e3d392f ci: add id-token: write permission to release workflow

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-74f2b91
+ * @license Angular v22.0.1+sha-e3d392f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-74f2b91');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-e3d392f');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
