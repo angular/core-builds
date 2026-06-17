464e987f897246af46e51f56ec30458682afcd12
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 21:14:25 +0000

    aac8577 release: cut the v22.0.2 release

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-1d0256b
+ * @license Angular v22.0.2+sha-aac8577
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9110,7 +9110,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.1+sha-1d0256b'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.2+sha-aac8577'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12228,7 +12228,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.1+sha-1d0256b';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.2+sha-aac8577';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
