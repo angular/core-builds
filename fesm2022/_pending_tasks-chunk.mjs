0af65ba36a0ea1098ce7f0716659d8ca7d03ba66
kirjs <kirts@google.com>
2026-06-18 16:39:17 +0000

    3e7c83b Revert "fix(core): escape overlapping comment delimiters in escapeCommentText"

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-73880c5
+ * @license Angular v22.0.2+sha-3e7c83b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-73880c5');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-3e7c83b');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
