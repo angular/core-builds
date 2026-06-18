7e31ae2dada0ff4f88fdb97a79f1f7cca02717a8
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 14:52:28 +0000

    e7a4e25 docs: use the @Service decorator in the first-app and signals tutorials

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-c23ddd6
+ * @license Angular v22.1.0-next.1+sha-e7a4e25
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-c23ddd6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-e7a4e25');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
