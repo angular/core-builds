030182dc532c3371a418d489f0162f16bd7bdb73
kirjs <kirts@google.com>
2026-06-18 14:57:12 +0000

    73880c5 fix(docs-infra): correct select dropdown scrolling, sizing, and selection

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-ece8ba4
+ * @license Angular v22.0.2+sha-73880c5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9110,7 +9110,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.2+sha-ece8ba4'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.2+sha-73880c5'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12228,7 +12228,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.2+sha-ece8ba4';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.2+sha-73880c5';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
