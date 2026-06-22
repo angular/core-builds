ed0d2af20909136f7feac42fa38fdc3191948ac8
kirjs <kirts@google.com>
2026-06-22 20:54:20 +0000

    d817e56 docs: show selected menu item visually in context menu example

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-59ee51e
+ * @license Angular v22.0.2+sha-d817e56
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9110,7 +9110,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.2+sha-59ee51e'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.2+sha-d817e56'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12228,7 +12228,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.2+sha-59ee51e';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.2+sha-d817e56';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
