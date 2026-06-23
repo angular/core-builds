cb4c17d0ec1f2fb0b34b91438ecc12d31dad302a
kirjs <kirts@google.com>
2026-06-23 17:06:06 +0000

    2799304 fix(core): avoid uncaught promise errors in injectAsync prefetching

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-f598dcf
+ * @license Angular v22.0.2+sha-2799304
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -125,13 +125,13 @@ function injectAsync(loader, options) {
     return loadedPromise;
   };
   if (options?.prefetch) {
-    options.prefetch().then(() => load());
+    options.prefetch().then(() => load()).catch(() => {});
   }
   return () => load().then(loadedToken => injector.get(maybeUnwrapDefaultExport(loadedToken)));
 }
 function onIdle(options) {
   if (ngDevMode) {
-    assertInInjectionContext(injectAsync);
+    assertInInjectionContext(onIdle);
   }
   const idleService = inject(IDLE_SERVICE);
   const {
