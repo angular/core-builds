ae54f8ec89e4e3117c0c1fc585f267e7e6897570
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 15:36:09 +0000

    dead64f docs: add error guide for NG05101

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-e5d7b3a
+ * @license Angular v22.1.0-next.1+sha-dead64f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9169,7 +9169,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.0-next.1+sha-e5d7b3a'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.0-next.1+sha-dead64f'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12289,7 +12289,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.0-next.1+sha-e5d7b3a';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.0-next.1+sha-dead64f';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-e5d7b3a
+ * @license Angular v22.1.0-next.1+sha-dead64f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-e5d7b3a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-dead64f');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
