e3d7eca789c48d5e153cc10c440a2b585f6fa472
Alan Agius <alan.agius4@gmail.com>
2026-06-25 15:16:11 +0000

    5ba6443 release: cut the v22.0.3 release

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-f4f7f37
+ * @license Angular v22.0.3+sha-5ba6443
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9115,7 +9115,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.2+sha-f4f7f37'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.3+sha-5ba6443'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12233,7 +12233,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.2+sha-f4f7f37';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.3+sha-5ba6443';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-f4f7f37
+ * @license Angular v22.0.3+sha-5ba6443
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-f4f7f37');
+const VERSION = /* @__PURE__ */new Version('22.0.3+sha-5ba6443');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
