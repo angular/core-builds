b5320a72979d8c308c78e1d191d573dd915f42a1
Alan Agius <alan.agius4@gmail.com>
2026-06-25 15:17:04 +0000

    7f2d34a docs: release notes for the v22.0.3 release

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-ecd0475
+ * @license Angular v22.1.0-next.1+sha-7f2d34a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-ecd0475');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-7f2d34a');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
