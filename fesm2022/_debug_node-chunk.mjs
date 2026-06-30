bc72d7fac78df704887e2f259f528557c9c25732
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-30 20:32:38 +0000

    af7edd9 Revert "fix(compiler-cli): include toSignal in debugName transform"

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-b5ea340
+ * @license Angular v22.0.4+sha-af7edd9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9115,7 +9115,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.4+sha-b5ea340'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.4+sha-af7edd9'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12233,7 +12233,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.4+sha-b5ea340';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.4+sha-af7edd9';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
