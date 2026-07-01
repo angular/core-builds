196c141f25620d712f371e547f20d7cb45642ff0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 20:54:21 +0000

    2b3277f release: cut the v22.0.5 release

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-0554bf5
+ * @license Angular v22.0.5+sha-2b3277f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0554bf5');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2b3277f');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
