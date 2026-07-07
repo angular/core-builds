4e9234d410c1e070ed28f3046a7f77a1d3fdbaa1
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:20:29 +0000

    91df739 fix(http): prevent caching of responses with Set-Cookie headers

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.17+sha-8d22cc9
+ * @license Angular v21.2.17+sha-91df739
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8821,7 +8821,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.17+sha-8d22cc9'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.17+sha-91df739'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
