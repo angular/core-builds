a69489dc8d75cbb956f920b81b77e915aac72789
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 23:51:18 +0000

    95b7385 docs: fix import of `AdminPage` in Angular routing documentation. #69670

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.4+sha-eb2a8ff
+ * @license Angular v22.1.0-next.4+sha-95b7385
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9177,7 +9177,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.0-next.4+sha-eb2a8ff'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.0-next.4+sha-95b7385'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12316,7 +12316,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.0-next.4+sha-eb2a8ff';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.0-next.4+sha-95b7385';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
