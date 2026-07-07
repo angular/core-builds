6568d1ac76a9ca91317641c4824f8c7a2f82872f
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 16:59:42 +0000

    e839891 fix(docs-infra): disable nav-list tooltip on mobile

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-e878d7c
+ * @license Angular v22.0.5+sha-e839891
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9118,7 +9118,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.5+sha-e878d7c'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.5+sha-e839891'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12236,7 +12236,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.5+sha-e878d7c';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.5+sha-e839891';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
