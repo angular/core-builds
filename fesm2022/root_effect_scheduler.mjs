316b8ca50b21f7b42e3c24c60c8a8d34ba3a3186
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 18:21:33 +0000

    baa093b fix(service-worker): preserve referrer policy in asset requests

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.25+sha-b963f61
+ * @license Angular v20.3.25+sha-baa093b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.25+sha-b963f61');
+const VERSION = /* @__PURE__ */ new Version('20.3.25+sha-baa093b');
 
 /**
  * Base URL for the error details page.
