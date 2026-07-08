c6007b9a2a5efa262d19fe65f9fdcc6982fc194a
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 17:51:02 +0000

    2d52d53 docs: update optional chaining behavior

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-748faa4
+ * @license Angular v22.0.5+sha-2d52d53
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9118,7 +9118,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.5+sha-748faa4'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.5+sha-2d52d53'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12236,7 +12236,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.5+sha-748faa4';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.5+sha-2d52d53';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
