1481c903d0e1f7c64e7784385d9ae6a30ae075f1
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 18:33:51 +0000

    e86c31b fix(service-worker): preserve referrer policy in asset requests

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.17+sha-91df739
+ * @license Angular v21.2.17+sha-e86c31b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.17+sha-91df739');
+const VERSION = /* @__PURE__ */new Version('21.2.17+sha-e86c31b');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
