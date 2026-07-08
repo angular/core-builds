fafbff15d693e98e7e23f09948ccd5963a72eba4
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 19:51:17 +0000

    d1aaf1a feat(docs-infra): add Angie mascot to the not found page

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-2cb034f
+ * @license Angular v22.0.5+sha-d1aaf1a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9118,7 +9118,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.5+sha-2cb034f'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.5+sha-d1aaf1a'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12236,7 +12236,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.5+sha-2cb034f';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.5+sha-d1aaf1a';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-2cb034f
+ * @license Angular v22.0.5+sha-d1aaf1a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2cb034f');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-d1aaf1a');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
