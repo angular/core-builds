4ed81bf7043be1601c0f151f47f3d05a505a9879
Matthew Beck <me@mattrb.com>
2026-07-09 19:40:35 +0000

    8ce1fcf fix(localize): use Object.hasOwn for placeholder lookup in translate

--- fesm2022/_weak_ref-chunk.mjs
+++ fesm2022/_weak_ref-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-3a9351f
+ * @license Angular v22.0.6+sha-8ce1fcf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
--- types/core.d.ts
+++ types/core.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-3a9351f
+ * @license Angular v22.0.6+sha-8ce1fcf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -145,14 +145,14 @@ interface InputFunction {
      */ <T, TransformT>(initialValue: undefined, opts: InputOptionsWithTransform<T | undefined, TransformT>): InputSignalWithTransform<T | undefined, TransformT>;
     /**
      * Declares an input of type `T` with an initial value and a transform function
-     * that accepts values of the same type.
+     * that accepts values of the same type, or string values from static attributes.
      */
-    <T>(initialValue: T, opts: InputOptionsWithTransform<T, unknown>): InputSignalWithTransform<T, T>;
+    <T>(initialValue: T, opts: InputOptionsWithTransform<T, unknown>): InputSignalWithTransform<T, T | string>;
     /**
      * Declares an input of type `T|undefined` without an initial value and with a transform
-     * function that accepts values of the same type.
+     * function that accepts values of the same type, or string values from static attributes.
      */
-    <T>(initialValue: undefined, opts: InputOptionsWithTransform<T | undefined, unknown>): InputSignalWithTransform<T | undefined, T | undefined>;
+    <T>(initialValue: undefined, opts: InputOptionsWithTransform<T | undefined, unknown>): InputSignalWithTransform<T | undefined, T | undefined | string>;
     /**
      * Initializes a required input.
      *
