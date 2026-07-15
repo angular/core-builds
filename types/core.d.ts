9e73251000b4082d0c15f3ee53ba4d31c65fb5b5
Matthew Beck <me@mattrb.com>
2026-07-15 19:10:37 +0000

    0ae6d81 fix(core): preserve explicit input transform write type

--- types/core.d.ts
+++ types/core.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d997a96
+ * @license Angular v22.1.0-next.5+sha-0ae6d81
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -146,14 +146,14 @@ interface InputFunction {
      */ <T, TransformT>(initialValue: undefined, opts: InputOptionsWithTransform<T | undefined, TransformT>): InputSignalWithTransform<T | undefined, TransformT>;
     /**
      * Declares an input of type `T` with an initial value and a transform function
-     * that accepts values of the same type.
+     * that accepts values of type `unknown`.
      */
-    <T>(initialValue: T, opts: InputOptionsWithTransform<T, unknown>): InputSignalWithTransform<T, T>;
+    <T>(initialValue: T, opts: InputOptionsWithTransform<T, unknown>): InputSignalWithTransform<T, unknown>;
     /**
      * Declares an input of type `T|undefined` without an initial value and with a transform
-     * function that accepts values of the same type.
+     * function that accepts values of type `unknown`.
      */
-    <T>(initialValue: undefined, opts: InputOptionsWithTransform<T | undefined, unknown>): InputSignalWithTransform<T | undefined, T | undefined>;
+    <T>(initialValue: undefined, opts: InputOptionsWithTransform<T | undefined, unknown>): InputSignalWithTransform<T | undefined, unknown>;
     /**
      * Initializes a required input.
      *
