044a99582a1e00de188614fe9324b2d060268ac7
Matthew Beck <me@mattrb.com>
2026-07-16 04:29:47 +0000

    265de87 docs: release notes for the v22.0.7 release

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d43acaf
+ * @license Angular v22.1.0-next.5+sha-265de87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d43acaf');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-265de87');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
