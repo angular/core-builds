4a1dbf8d72f44e242b743224fd5fe35970ae3721
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:45:55 +0000

    3192dcc fix(http): prevent transfer cache key collisions

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-00699f3
+ * @license Angular v22.1.0-next.6+sha-3192dcc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-00699f3');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-3192dcc');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
