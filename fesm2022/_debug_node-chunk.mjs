8e5aa2615c6b295198a36ca3676eccf61225b646
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:44:58 +0000

    9d40f8a fix(http): prevent transfer cache key collisions

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-e02a0de
+ * @license Angular v22.0.7+sha-9d40f8a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9118,7 +9118,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.7+sha-e02a0de'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.7+sha-9d40f8a'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12236,7 +12236,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.7+sha-e02a0de';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.7+sha-9d40f8a';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
