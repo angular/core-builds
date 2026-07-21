b094b36c22f71ca6364a6ac54d019b03563c1a8c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 15:27:24 +0000

    9bf8b8c build: update cross-repo angular dependencies

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-3497c9b-with-local-changes
+ * @license Angular v22.1.0-next.6+sha-9bf8b8c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-3497c9b-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-9bf8b8c');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
