fb10e42d8ac0eb3588bf2c1cb82e61c6b58562d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 15:26:44 +0000

    fb68711 feat(docs-infra): react with Angie in the playground minigame

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-d302c7a
+ * @license Angular v22.0.7+sha-fb68711
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9120,7 +9120,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.7+sha-d302c7a'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.7+sha-fb68711'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12238,7 +12238,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.7+sha-d302c7a';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.7+sha-fb68711';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-d302c7a
+ * @license Angular v22.0.7+sha-fb68711
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-d302c7a');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-fb68711');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
