94b16c8df2180f07e96510db2027bc22b764f3b5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:39:57 +0000

    509cad6 docs: release notes for the v22.0.8 release

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-e428df2
+ * @license Angular v22.1.0-next.6+sha-509cad6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-e428df2');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-509cad6');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
