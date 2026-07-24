b6ae0c2ecdc434f00255a1965f3eba039e194aa7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 21:01:07 +0000

    d346905 fix(migrations): correctly detect `then`/`else` keywords in control flow migration

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-232cfea
+ * @license Angular v22.0.8+sha-d346905
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9120,7 +9120,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.8+sha-232cfea'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.8+sha-d346905'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12238,7 +12238,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.8+sha-232cfea';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.8+sha-d346905';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
