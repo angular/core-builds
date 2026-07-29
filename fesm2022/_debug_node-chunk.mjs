043f74002afd8c1b0522cace6f18b3edc56cd260
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 17:07:42 +0000

    420f7c4 docs: invoke signal inputs in adev examples and tutorials

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-f90690f
+ * @license Angular v22.0.8+sha-420f7c4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9064,7 +9064,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.8+sha-f90690f'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.0.8+sha-420f7c4'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12182,7 +12182,7 @@ let counter = 0;
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.8+sha-f90690f';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.0.8+sha-420f7c4';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-f90690f
+ * @license Angular v22.0.8+sha-420f7c4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.0.8+sha-f90690f');
+const VERSION = /* @__PURE__ */new Version('22.0.8+sha-420f7c4');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
