a8321abf908779c5d611fae0f3537dd5a573bc31
Douglas Parker <dgp1130@users.noreply.github.com>
2026-07-29 23:57:59 +0000

    1a2bcb2 fix(devtools): avoid initializing profiler in production mode

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-c6e4d5a
+ * @license Angular v22.2.0-next.0+sha-1a2bcb2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9123,7 +9123,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.0+sha-c6e4d5a'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.0+sha-1a2bcb2'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12262,7 +12262,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.0+sha-c6e4d5a';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.0+sha-1a2bcb2';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-c6e4d5a
+ * @license Angular v22.2.0-next.0+sha-1a2bcb2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-c6e4d5a');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-1a2bcb2');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
