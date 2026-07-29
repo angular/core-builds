e8d5db9025a9d4a77f94cd7efdaf6bf00408dd79
Alan Agius <alan.agius4@gmail.com>
2026-07-29 20:47:43 +0000

    d366e4b release: cut the v22.1.0 release

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-rc.0+sha-7825e60
+ * @license Angular v22.1.0+sha-d366e4b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-7825e60');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-d366e4b');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
