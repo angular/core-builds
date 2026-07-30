1c2eb3775a8e3c558d3e8f6c59c2af33a8618340
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-30 22:28:08 +0000

    a37c79d docs: remove duplicate aria-hidden attributes in aria examples

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-b63b903
+ * @license Angular v22.2.0-next.0+sha-a37c79d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9123,7 +9123,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.0+sha-b63b903'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.0+sha-a37c79d'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12262,7 +12262,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.0+sha-b63b903';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.0+sha-a37c79d';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
