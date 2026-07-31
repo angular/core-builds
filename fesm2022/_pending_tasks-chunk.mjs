075f64b2568a652b0d6f9548993b9eab5d87810b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 19:36:55 +0000

    aa6d318 fix(core): initialize hydration triggers after late runtime activation

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-d6ec7c7
+ * @license Angular v22.1.0+sha-aa6d318
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-d6ec7c7');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-aa6d318');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
