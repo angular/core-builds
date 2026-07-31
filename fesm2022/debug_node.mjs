b54a0d54f294165c5b9778f0db2f05007f6fdda5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 22:19:54 +0000

    e4c416c fix(http): run root interceptors in the terminal request chain

--- fesm2022/debug_node.mjs
+++ fesm2022/debug_node.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.27+sha-2f96c80
+ * @license Angular v20.3.27+sha-e4c416c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14793,7 +14793,7 @@ class ComponentFactory extends ComponentFactory$1 {
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
     const tAttributes = rootSelectorOrNode
-        ? ['ng-version', '20.3.27+sha-2f96c80']
+        ? ['ng-version', '20.3.27+sha-e4c416c']
         : // Extract attributes and classes from the first selector only to match VE behavior.
             extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
     let creationBindings = null;
--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.27+sha-2f96c80
+ * @license Angular v20.3.27+sha-e4c416c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.27+sha-2f96c80');
+const VERSION = /* @__PURE__ */ new Version('20.3.27+sha-e4c416c');
 
 /**
  * Base URL for the error details page.
