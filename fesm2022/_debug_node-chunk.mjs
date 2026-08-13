7b7e534007d15c1c93584d81f5d98fc7c2ca1d49
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 19:05:59 +0000

    148fee3 release: cut the v22.1.2 release

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-0a708e9
+ * @license Angular v22.1.2+sha-148fee3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9151,7 +9151,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.1+sha-0a708e9'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.2+sha-148fee3'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12290,7 +12290,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.1+sha-0a708e9';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.2+sha-148fee3';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
