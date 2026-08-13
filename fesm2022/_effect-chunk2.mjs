995eeff7792ff2f1c4f5418f5ff9698a09bffc2a
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 00:12:12 +0000

    dca0a77 release: cut the v21.2.20 release

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.19+sha-caf6166
+ * @license Angular v21.2.20+sha-dca0a77
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.19+sha-caf6166');
+const VERSION = /* @__PURE__ */new Version('21.2.20+sha-dca0a77');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
