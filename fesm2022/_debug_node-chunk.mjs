04916e232f1f327f265f3bacd22702885374e126
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 22:17:10 +0000

    46d2cb7 fix(common): preserve literal key union in KeyValuePipe.transform()

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-38d0932
+ * @license Angular v22.2.0-next.2+sha-46d2cb7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9151,7 +9151,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.2+sha-38d0932'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.2+sha-46d2cb7'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12324,7 +12324,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.2+sha-38d0932';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.2+sha-46d2cb7';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
