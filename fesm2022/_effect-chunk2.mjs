744431b0940aadcc9f5fbecddcf27e2bd4c0ee53
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 20:51:36 +0000

    c19a36c fix(platform-browser): disallow event handler attributes in Meta

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.20+sha-dca0a77
+ * @license Angular v21.2.20+sha-c19a36c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.20+sha-dca0a77');
+const VERSION = /* @__PURE__ */new Version('21.2.20+sha-c19a36c');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
