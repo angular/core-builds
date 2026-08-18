0b635350e80ed590f1c55eef91911846f841d59b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-18 16:51:52 +0000

    d364c83 ci: exclude markdown files from requiring dev-infra review

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-732e505
+ * @license Angular v22.2.0-next.2+sha-d364c83
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-732e505');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-d364c83');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
