5075d90c1665046d55e0144d684c8c0ab33277d0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-19 15:36:26 +0000

    84a210e refactor(router): add support for non-blocking router resources (#70211)

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-83450d2
+ * @license Angular v22.1.2+sha-84a210e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9154,7 +9154,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.2+sha-83450d2'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.2+sha-84a210e'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12293,7 +12293,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.2+sha-83450d2';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.2+sha-84a210e';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-83450d2
+ * @license Angular v22.1.2+sha-84a210e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-83450d2');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-84a210e');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
