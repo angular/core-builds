78287229a9e6678db6e04e68bb94bec4aa55f59c
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-21 18:21:55 +0000

    2e2c426 fix(compiler-cli): deduplicate deferred imports across multiple blocks

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-d609cf6
+ * @license Angular v22.2.0-next.3+sha-2e2c426
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9154,7 +9154,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.3+sha-d609cf6'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.3+sha-2e2c426'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12327,7 +12327,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.3+sha-d609cf6';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.3+sha-2e2c426';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-d609cf6
+ * @license Angular v22.2.0-next.3+sha-2e2c426
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-d609cf6');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-2e2c426');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
