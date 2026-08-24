9a5265b2e771734f779154d285e0fac3e93af454
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 17:49:35 +0000

    89b2056 fix(platform-server): update domino to latest version

--- fesm2022/root_effect_scheduler.mjs
+++ fesm2022/root_effect_scheduler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.29+sha-77f7b60
+ * @license Angular v20.3.29+sha-89b2056
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -31,7 +31,7 @@ class Version {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.29+sha-77f7b60');
+const VERSION = /* @__PURE__ */ new Version('20.3.29+sha-89b2056');
 
 /**
  * Base URL for the error details page.
