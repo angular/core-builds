7f0e8c2823204292a9277c1fbf5851334a0d88a1
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 17:51:30 +0000

    73d8bbd fix(platform-server): update domino to latest version

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.21+sha-992d627
+ * @license Angular v21.2.21+sha-73d8bbd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -8799,7 +8799,7 @@ class ComponentFactory extends ComponentFactory$1 {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.21+sha-992d627'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '21.2.21+sha-73d8bbd'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
