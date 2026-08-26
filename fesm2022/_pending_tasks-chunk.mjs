ea88500e41a6f10d6bccda37d7357a93989a08a3
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-26 22:49:14 +0000

    9bf7113 build: update cross-repo angular dependencies

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.3+sha-151a8f2
+ * @license Angular v22.1.3+sha-9bf7113
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-151a8f2');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-9bf7113');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
