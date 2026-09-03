78c43d2261e4aab7b6a4bda2f2a8c57bb75b088d
Matthew Beck <me@mattrb.com>
2026-09-03 02:12:23 +0000

    502fa13 fix(docs-infra): ignore external links when mapping navigation items to routes

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-69c8730
+ * @license Angular v22.2.0-next.4+sha-502fa13
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9209,7 +9209,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives, allowNonStandaloneDirectives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.4+sha-69c8730'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.4+sha-502fa13'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12512,7 +12512,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.4+sha-69c8730';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.4+sha-502fa13';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
