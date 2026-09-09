5c6a36dedb19fcbc44d99f558784f0e54c25a126
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:01:21 +0000

    81385ea release: cut the v21.2.23 release

--- fesm2022/_effect-chunk2.mjs
+++ fesm2022/_effect-chunk2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.22+sha-fc2e8fb
+ * @license Angular v21.2.23+sha-81385ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('21.2.22+sha-fc2e8fb');
+const VERSION = /* @__PURE__ */new Version('21.2.23+sha-81385ea');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
