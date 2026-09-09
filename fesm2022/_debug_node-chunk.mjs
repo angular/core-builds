89afe5063c52d089c7bbba204fcb6a27bfa71e2a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 15:30:42 +0000

    25228d0 test: fix failing boundary test

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.5+sha-2c71e88-with-local-changes
+ * @license Angular v22.2.0-next.5+sha-25228d0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9239,7 +9239,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives, allowNonStandaloneDirectives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.5+sha-2c71e88-with-local-changes'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0-next.5+sha-25228d0'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12554,7 +12554,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.5+sha-2c71e88-with-local-changes';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0-next.5+sha-25228d0';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
