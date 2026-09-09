133369b4be76b5a99812132d9483b86a87bd7b5a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 14:43:28 +0000

    05a05f5 build: update cross-repo angular dependencies

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.5+sha-cc4c2ce-with-local-changes
+ * @license Angular v22.1.5+sha-05a05f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.5+sha-cc4c2ce-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('22.1.5+sha-05a05f5');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
