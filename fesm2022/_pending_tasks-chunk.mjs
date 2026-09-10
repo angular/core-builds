28bbbe86130d1824d5ec5fcb0b1c34373aa09ad3
Andrew Scott <atscott01@gmail.com>
2026-09-10 20:32:33 +0000

    14dbbf9 fix(compiler): wrap `@for` collection expression before appending non-null assertion

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-8ca879e
+ * @license Angular v22.2.0-next.7+sha-14dbbf9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-8ca879e');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-14dbbf9');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
