eeb85e6024ea26d3379bdeddf72212c7f176992c
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:25:20 +0000

    f2cf65a fix(vscode-extension): handle template literals when detecting supported decorator fields

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-2dcdf9a
+ * @license Angular v22.2.0-next.7+sha-f2cf65a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-2dcdf9a');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-f2cf65a');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
