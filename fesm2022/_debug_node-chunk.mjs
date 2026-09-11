63b71264246dad6f4df61f9611017a4c4e1f0ee8
Andrew Scott <atscott01@gmail.com>
2026-09-11 17:14:34 +0000

    8a837f6 Revert "fix(compiler): wrap `@for` collection expression before appending non-null assertion"

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-23e5d30
+ * @license Angular v22.1.6+sha-8a837f6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9213,7 +9213,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.6+sha-23e5d30'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.6+sha-8a837f6'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12362,7 +12362,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.6+sha-23e5d30';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.6+sha-8a837f6';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
