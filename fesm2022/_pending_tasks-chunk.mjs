1ed9ec172d6221921e976b6cc0eddfcb2b5008f3
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-18 16:13:55 +0000

    f15be33 fix(docs-infra): render the signature of decorators without members

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-0b632de
+ * @license Angular v22.1.7+sha-f15be33
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-0b632de');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-f15be33');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
