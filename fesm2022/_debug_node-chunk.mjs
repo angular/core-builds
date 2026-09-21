9f8d2c40c38edd2a1dd4d9430e6efa1e79acf967
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 19:39:07 +0000

    5cd618f fix(dev-infra): handle missing arguments and gh failures in PR review script

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6647a07
+ * @license Angular v22.3.0-next.0+sha-5cd618f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9348,7 +9348,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives, allowNonStandaloneDirectives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.3.0-next.0+sha-6647a07'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.3.0-next.0+sha-5cd618f'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12663,7 +12663,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.3.0-next.0+sha-6647a07';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.3.0-next.0+sha-5cd618f';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6647a07
+ * @license Angular v22.3.0-next.0+sha-5cd618f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6647a07');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-5cd618f');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
