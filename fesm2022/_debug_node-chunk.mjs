57a64c40e0a5c4dcf97e1781dd4690c9e3690db6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 19:15:01 +0000

    805aa91 docs: update `disabledReasons` and `transformedValue` examples

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-78c7ab3
+ * @license Angular v22.1.7+sha-805aa91
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9213,7 +9213,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.7+sha-78c7ab3'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.7+sha-805aa91'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12362,7 +12362,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.7+sha-78c7ab3';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.7+sha-805aa91';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
@@ -13423,7 +13423,15 @@ async function triggerHydrationForBlockQueue(injector, hydrationQueue, replayQue
   if (replayQueuedEventsFn) {
     replayQueuedEventsFn(hydrationQueue);
   }
-  cleanupHydratedDeferBlocks(dehydratedBlockRegistry.get(lastBlockName), hydrationQueue, dehydratedBlockRegistry, injector.get(ApplicationRef));
+  const appRef = injector.get(ApplicationRef);
+  const lastDeferBlock = dehydratedBlockRegistry.get(lastBlockName);
+  if (pendingTasks.hasPendingTasks) {
+    await appRef.whenStable();
+    if (appRef.destroyed) {
+      return;
+    }
+  }
+  cleanupHydratedDeferBlocks(lastDeferBlock, hydrationQueue, dehydratedBlockRegistry, appRef);
 }
 function deferBlockHasErrored(deferBlock) {
   return getLDeferBlockDetails(deferBlock.lView, deferBlock.tNode)[DEFER_BLOCK_STATE] === DeferBlockState.Error;
