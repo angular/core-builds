4a1364e01204431067dc5c390f7280073a48876b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:16:16 +0000

    51f056f docs(router): clarify route resource parameters

--- schematics/bundles/router-testing-module-migration.cjs
+++ schematics/bundles/router-testing-module-migration.cjs
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * @license Angular v22.2.0-rc.0+sha-949d66c
+ * @license Angular v22.2.0-rc.0+sha-51f056f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
