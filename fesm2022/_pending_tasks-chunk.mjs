ade9decddc275bf8a16298804884952cabc5defd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:54:20 +0000

    59e2c2f docs(service-worker): render the SwPush and SwRegistrationOptions examples

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-726368b
+ * @license Angular v22.1.7+sha-59e2c2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-726368b');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-59e2c2f');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
