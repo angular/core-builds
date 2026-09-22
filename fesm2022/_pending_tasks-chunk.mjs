963642dd4d57a3b34aee81768368271a3b05cec0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 16:23:08 +0000

    fe75da1 test(core): cover boundary around projected content

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-8d8859c
+ * @license Angular v22.3.0-next.0+sha-fe75da1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-8d8859c');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-fe75da1');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
