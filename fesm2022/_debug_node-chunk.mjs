eb2cc84f928ec0b4489377aa6b3135cffd06a0db
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:15:13 +0000

    330083f fix(platform-server): preserve Unicode whitespace in ServerXhr URLs

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-71f84a8
+ * @license Angular v22.1.7+sha-330083f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9213,7 +9213,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.7+sha-71f84a8'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.1.7+sha-330083f'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12362,7 +12362,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.7+sha-71f84a8';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.1.7+sha-330083f';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
