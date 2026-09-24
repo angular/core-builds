52993c84ce401492fb7a4c04cb271138248f5409
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:44:03 +0000

    21851fc fix(core): add debugName on afterRenderEffect

--- fesm2022/_debug_node-chunk.mjs
+++ fesm2022/_debug_node-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0+sha-7f9c14d
+ * @license Angular v22.2.0+sha-21851fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9348,7 +9348,7 @@ class ComponentFactory {
   }
 }
 function createRootTView(rootSelectorOrNode, componentDef, componentBindings, directives, allowNonStandaloneDirectives) {
-  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0+sha-7f9c14d'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
+  const tAttributes = rootSelectorOrNode ? ['ng-version', '22.2.0+sha-21851fc'] : extractAttrsAndClassesFromSelector(componentDef.selectors[0]);
   let creationBindings = null;
   let updateBindings = null;
   let varsToAllocate = 0;
@@ -12663,7 +12663,7 @@ function getDeepLinkProperties(instance) {
 const eventsStack = [];
 function getBaseDocUrl() {
   const full = VERSION.full;
-  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0+sha-7f9c14d';
+  const isPreRelease = full.includes('-next') || full.includes('-rc') || full === '22.2.0+sha-21851fc';
   const prefix = isPreRelease ? 'next' : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 }
@@ -18381,6 +18381,7 @@ function recreateLView(importMeta, id, newDef, oldDef, lView) {
     removeViewFromDOM(lView[TVIEW$1], lView);
     resetProjectionState(tNode);
     renderView(newTView, newLView, instance);
+    newLView[FLAGS] &= -1025;
     refreshView(newTView, newLView, newTView.template, instance);
   };
   if (zone === null) {
