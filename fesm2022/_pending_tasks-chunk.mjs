03226f515b89fe0ac367e7a023c662a894f03507
GitHub <noreply@github.com>
2026-09-25 18:04:20 +0000

    e4fc745 docs: release notes for the vscode extension 22.2.0 release (#70951)

--- fesm2022/_pending_tasks-chunk.mjs
+++ fesm2022/_pending_tasks-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-846c73d
+ * @license Angular v22.3.0-next.0+sha-e4fc745
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -23,7 +23,7 @@ class Version {
     this.patch = parts.slice(2).join('.');
   }
 }
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-846c73d');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-e4fc745');
 
 const DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
